d9d6345e3ff1ac1638a3682310116715d6d3e120
Adrian Holovaty <adrian@holovaty.com>
2005-07-14 18:34:52 +0000

    Fixed bug in [29]
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@30 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/comments/urls/comments.py
+++ django/contrib/comments/urls/comments.py
@@ -1,12 +1,12 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('django.contrib.comments.views',
-    (r'^post/$', 'comments.comments.post_comment'),
-    (r'^postfree/$', 'comments.comments.post_free_comment'),
-    (r'^posted/$', 'comments.comments.comment_was_posted'),
-    (r'^karma/vote/(?P<comment_id>\d+)/(?P<vote>up|down)/$', 'comments.karma.vote'),
-    (r'^flag/(?P<comment_id>\d+)/$', 'comments.userflags.flag'),
-    (r'^flag/(?P<comment_id>\d+)/done/$', 'comments.userflags.flag_done'),
-    (r'^delete/(?P<comment_id>\d+)/$', 'comments.userflags.delete'),
-    (r'^delete/(?P<comment_id>\d+)/done/$', 'comments.userflags.delete_done'),
+urlpatterns = patterns('django.contrib.comments.comments',
+    (r'^post/$', 'comments.post_comment'),
+    (r'^postfree/$', 'comments.post_free_comment'),
+    (r'^posted/$', 'comments.comment_was_posted'),
+    (r'^karma/vote/(?P<comment_id>\d+)/(?P<vote>up|down)/$', 'karma.vote'),
+    (r'^flag/(?P<comment_id>\d+)/$', 'userflags.flag'),
+    (r'^flag/(?P<comment_id>\d+)/done/$', 'userflags.flag_done'),
+    (r'^delete/(?P<comment_id>\d+)/$', 'userflags.delete'),
+    (r'^delete/(?P<comment_id>\d+)/done/$', 'userflags.delete_done'),
 )
