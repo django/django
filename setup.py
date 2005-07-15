775f433988445fe5caa622593f58be14506e3143
Adrian Holovaty <adrian@holovaty.com>
2005-07-15 19:49:10 +0000

    Moved django.bin.setup to root level and converted it to use setuptools. Still not ready for prime time, though
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@65 bcc190cf-cafb-0310-a4f2-bffc1f526a37

---
+++ setup.py
@@ -0,0 +1,15 @@
+import ez_setup # From http://peak.telecommunity.com/DevCenter/setuptools
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "django",
+    version = "1.0.0",
+    url = 'http://www.djangoproject.com/',
+    author = 'World Online',
+    author_email = 'holovaty@gmail.com',
+    description = 'A high-level Python Web framework that encourages rapid development and clean, pragmatic design.',
+    license = 'BSD',
+    packages = find_packages(),
+)
