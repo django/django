c8cae5bed9cb719a5825e3e0d5523429aa967476
Adrian Holovaty <adrian@holovaty.com>
2005-07-15 19:18:09 +0000

    Removed gunk from legacy django.bin.setup, although distutils installation isn't done yet
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@60 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/bin/setup.py
+++ django/bin/setup.py
@@ -1,45 +1,11 @@
-"""
-Usage:
-
-python setup.py bdist
-python setup.py sdist
-"""
+# This doesn't work yet.
 
 from distutils.core import setup
-import os
-
-# Whether to include the .py files, rather than just .pyc's. Doesn't do anything yet.
-INCLUDE_SOURCE = True
-
-# Determines which apps are bundled with the distribution.
-INSTALLED_APPS = ('auth', 'categories', 'comments', 'core', 'media', 'news', 'polls', 'registration', 'search', 'sms', 'staff')
-
-# First, lump together all the generic, core packages that need to be included.
-packages = [
-    'django',
-    'django.core',
-    'django.templatetags',
-    'django.utils',
-    'django.views',
-]
-for a in INSTALLED_APPS:
-    for dirname in ('parts', 'templatetags', 'views'):
-        if os.path.exists('django/%s/%s/' % (dirname, a)):
-            packages.append('django.%s.%s' % (dirname, a))
-
-# Next, add individual modules.
-py_modules = [
-    'django.cron.daily_cleanup',
-    'django.cron.search_indexer',
-]
-py_modules += ['django.models.%s' % a for a in INSTALLED_APPS]
 
 setup(
     name = 'django',
-    version = '1.0',
-    packages = packages,
-    py_modules = py_modules,
-    url = 'http://www.ljworld.com/',
+    version = '1.0.0',
+    url = 'http://www.djangoproject.com/',
     author = 'World Online',
-    author_email = 'cms-support@ljworld.com',
+    author_email = 'holovaty@gmail.com',
 )
