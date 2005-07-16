40b8afaf324ee70bc753f74d6b450b3cbb7371ee
Adrian Holovaty <adrian@holovaty.com>
2005-07-16 03:12:07 +0000

    Added ROOT_URLCONF and MIDDLEWARE_CLASSES to project_template.settings.admin
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@91 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/project_template/settings/admin.py
+++ django/conf/project_template/settings/admin.py
@@ -5,3 +5,8 @@ from main import *
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates".
 )
+ROOT_URLCONF = 'django.conf.urls.admin'
+MIDDLEWARE_CLASSES = (
+    'django.middleware.admin.AdminUserRequired',
+    'django.middleware.common.CommonMiddleware',
+)
