907ae72e81b2908a86fc520af897eeb432e03d58
Jacob Kaplan-Moss <jacob@jacobian.org>
2005-07-21 13:57:42 +0000

    fixes #133 -- thanks Joeri
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@269 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/core/db/__init__.py
+++ django/core/db/__init__.py
@@ -17,7 +17,7 @@ from django.conf.settings import DATABASE_ENGINE
 
 try:
     dbmod = __import__('django.core.db.backends.%s' % DATABASE_ENGINE, '', '', [''])
-except ImportError:
+except ImportError, exc:
     # The database backend wasn't found. Display a helpful error message
     # listing all possible database backends.
     from django.core.exceptions import ImproperlyConfigured
@@ -25,8 +25,8 @@ except ImportError:
     backend_dir = os.path.join(__path__[0], 'backends')
     available_backends = [f[:-3] for f in os.listdir(backend_dir) if f.endswith('.py') and not f.startswith('__init__')]
     available_backends.sort()
-    raise ImproperlyConfigured, "Your DATABASE_ENGINE setting, %r, is invalid. Is it spelled correctly? Available options are: %s" % \
-        (DATABASE_ENGINE, ', '.join(map(repr, available_backends)))
+    raise ImproperlyConfigured, "Could not load database backend: %s. Is DATABASE_ENGINE (currently, %r) spelled correctly? Available options are: %s" % (exc, DATABASE_ENGINE, ", ".join(map(repr, available_backends)))
+        #(DATABASE_ENGINE, ', '.join(map(repr, available_backends)))k
 
 DatabaseError = dbmod.DatabaseError
 db = dbmod.DatabaseWrapper()
