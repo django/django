c5179609847733c4babcc32312686d9fabf37315
Adrian Holovaty <adrian@holovaty.com>
2005-08-05 22:15:14 +0000

    Made small cleanups to two default urlconfs, to be clearer.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@413 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/urls/admin_password_reset.py
+++ django/conf/urls/admin_password_reset.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('django.views',
-    (r'^/?$', 'registration.passwords.password_reset', {'is_admin_site' : True}),
+    (r'^$', 'registration.passwords.password_reset', {'is_admin_site' : True}),
     (r'^done/$', 'registration.passwords.password_reset_done'),
 )
