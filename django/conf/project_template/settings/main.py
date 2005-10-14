f2088d456c4ca394f59d179e990c6e611e92ff5f
Adrian Holovaty <adrian@holovaty.com>
2005-10-14 03:43:01 +0000

    Fixed #618 -- Added DATABASE_PORT setting. Thanks, Esaj
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@858 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/project_template/settings/main.py
+++ django/conf/project_template/settings/main.py
@@ -15,6 +15,7 @@ DATABASE_NAME = ''             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 SITE_ID = 1
 
