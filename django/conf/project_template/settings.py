56022ea3140192d0f96f3481758fe8b27979a835
Adrian Holovaty <adrian@holovaty.com>
2005-10-19 16:03:24 +0000

    Added TIME_ZONE and LANGUAGE_CODE to stock settings file created by 'django-admin startproject
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@957 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/project_template/settings.py
+++ django/conf/project_template/settings.py
@@ -17,6 +17,15 @@ DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
+# Local time zone for this installation. All choices can be found here:
+# http://www.postgresql.org/docs/current/static/datetime-keywords.html#DATETIME-TIMEZONE-SET-TABLE
+TIME_ZONE = 'America/Chicago'
+
+# Language code for this installation. All choices can be found here:
+# http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
+# http://blogs.law.harvard.edu/tech/stories/storyReader$15
+LANGUAGE_CODE = 'en-us'
+
 SITE_ID = 1
 
 # Absolute path to the directory that holds media.
