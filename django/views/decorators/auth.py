4f47ef85f8907f7b75ae12ff2c7ff4d4b3a3c26d
Adrian Holovaty <adrian@holovaty.com>
2005-10-22 00:04:55 +0000

    Added django.views.decorators.auth.user_passes_test, which is a more generic hook into authentication based on a test. Refactored login_required to use user_passes_test
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@988 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/views/decorators/auth.py
+++ django/views/decorators/auth.py
@@ -1,12 +1,19 @@
+def user_passes_test(view_func, test_func):
+    """
+    Decorator for views that checks that the user passes the given test,
+    redirecting to the log-in page if necessary. The test should be a callable
+    that takes the user object and returns True if the user passes.
+    """
+    from django.views.auth.login import redirect_to_login
+    def _checklogin(request, *args, **kwargs):
+        if test_func(request.user):
+            return view_func(request, *args, **kwargs)
+        return redirect_to_login(request.path)
+    return _checklogin
+
 def login_required(view_func):
     """
     Decorator for views that checks that the user is logged in, redirecting
     to the log-in page if necessary.
     """
-    from django.views.auth.login import redirect_to_login
-    def _checklogin(request, *args, **kwargs):
-        if request.user.is_anonymous():
-            return redirect_to_login(request.path)
-        else:
-            return view_func(request, *args, **kwargs)
-    return _checklogin
+    return user_passes_test(lambda u: not u.is_anonymous())
