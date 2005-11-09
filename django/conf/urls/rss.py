3aa236e10d43506a3739663a7a1d150908008a1d
Adrian Holovaty <adrian@holovaty.com>
2005-11-09 00:40:47 +0000

    Changed RSS urlconf to accept any character for param -- not just slashes and alphanumerics
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@1130 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/urls/rss.py
+++ django/conf/urls/rss.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('django.views',
-    (r'^(?P<slug>\w+)/$', 'rss.rss.feed'),
-    (r'^(?P<slug>\w+)/(?P<param>[\w/]+)/$', 'rss.rss.feed'),
+    (r'^(?P<slug>[^/]+)/$', 'rss.rss.feed'),
+    (r'^(?P<slug>[^/]+)/(?P<param>.+)/$', 'rss.rss.feed'),
 )
