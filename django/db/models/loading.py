9394d004db68dcc12ea72030574626c438283eb2
Robert Wittams <robert@wittams.com>
2005-12-16 12:28:24 +0000

    Starting to break up models/__init__.py
    git-svn-id: http://code.djangoproject.com/svn/django/branches/magic-removal@1693 bcc190cf-cafb-0310-a4f2-bffc1f526a37

---
+++ django/db/models/loading.py
@@ -0,0 +1,44 @@
+from django.conf import settings
+_installed_models_cache = None
+
+def get_installed_models():
+    """
+    Returns a list of installed "models" packages, such as foo.models,
+    ellington.news.models, etc. This does NOT include django.models.
+    """
+    global _installed_models_cache
+    if _installed_models_cache is not None:
+        return _installed_models_cache
+    _installed_models_cache = []
+    for a in settings.INSTALLED_APPS:
+        try:
+            _installed_models_cache.append(__import__(a + '.models', '', '', ['']))
+        except ImportError, e:
+            pass
+    return _installed_models_cache
+
+_installed_modules_cache = None
+
+def add_model_module(mod, modules):
+    if hasattr(mod, '_MODELS'):
+        modules.append(mod)
+    for name in getattr(mod, '__all__', []):
+        submod = __import__("%s.%s" % ( mod.__name__, name),'','',[''])
+        add_model_module(submod, modules)
+
+def get_installed_model_modules(core_models=None):
+    """
+    Returns a list of installed models, such as django.models.core,
+    ellington.news.models.news, foo.models.bar, etc.
+    """
+    global _installed_modules_cache
+    if _installed_modules_cache is not None:
+        return _installed_modules_cache
+    _installed_modules_cache = []
+
+    # django.models is a special case.
+    for submodule in (core_models or []):
+        _installed_modules_cache.append(__import__('django.models.%s' % submodule, '', '', ['']))
+    for mod in get_installed_models():
+        add_model_module(mod, _installed_modules_cache)
+    return _installed_modules_cache
