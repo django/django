d33099252a479059bcaa5282416c24b875e07201
Jacob Kaplan-Moss <jacob@jacobian.org>
2006-01-06 19:53:55 +0000

    Removed ellington cruft from admin urlconf.
    git-svn-id: http://code.djangoproject.com/svn/django/branches/magic-removal@1833 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/admin/urls/admin.py
+++ django/contrib/admin/urls/admin.py
@@ -1,7 +1,6 @@
 from django.conf.urls.defaults import *
-from django.conf.settings import INSTALLED_APPS
 
-urlpatterns = (
+urlpatterns = patterns('',
     ('^$', 'django.contrib.admin.views.main.index'),
     ('^jsi18n/$', 'django.views.i18n.javascript_catalog', {'packages': 'django.conf'}),
     ('^logout/$', 'django.views.auth.login.logout'),
@@ -21,37 +20,9 @@ urlpatterns = (
     ('^doc/models/(?P<model>[^/]+)/$', 'django.contrib.admin.views.doc.model_detail'),
 #    ('^doc/templates/$', 'django.views.admin.doc.template_index'),
     ('^doc/templates/(?P<template>.*)/$', 'django.contrib.admin.views.doc.template_detail'),
-)
-
-if 'ellington.events' in INSTALLED_APPS:
-    urlpatterns += (
-        ("^events/usersubmittedevents/(?P<object_id>\d+)/$", 'ellington.events.views.admin.user_submitted_event_change_stage'),
-        ("^events/usersubmittedevents/(?P<object_id>\d+)/delete/$", 'ellington.events.views.admin.user_submitted_event_delete_stage'),
-    )
-
-if 'ellington.news' in INSTALLED_APPS:
-    urlpatterns += (
-        ("^stories/preview/$", 'ellington.news.views.admin.story_preview'),
-        ("^stories/js/inlinecontrols/$", 'ellington.news.views.admin.inlinecontrols_js'),
-        ("^stories/js/inlinecontrols/(?P<label>[-\w]+)/$", 'ellington.news.views.admin.inlinecontrols_js_specific'),
-    )
-
-if 'ellington.alerts' in INSTALLED_APPS:
-    urlpatterns += (
-        ("^alerts/send/$", 'ellington.alerts.views.admin.send_alert_form'),
-        ("^alerts/send/do/$", 'ellington.alerts.views.admin.send_alert_action'),
-    )
-
-if 'ellington.media' in INSTALLED_APPS:
-    urlpatterns += (
-        ('^media/photos/caption/(?P<photo_id>\d+)/$', 'ellington.media.views.admin.get_exif_caption'),
-    )
-
-urlpatterns += (
     ('^((?:[^/]+/)+?)add/$', 'django.contrib.admin.views.stages.add.add_stage'),
     ('^((?:[^/]+/)+?)([^/]+)/history/$', 'django.contrib.admin.views.main.history'),
     ('^((?:[^/]+/)+?)([^/]+)/delete/$', 'django.contrib.admin.views.stages.delete.delete_stage'),
     ('^((?:[^/]+/)+?)([^/]+)/change/$', 'django.contrib.admin.views.stages.change.change_stage'),
     ('^((?:[^/]+/)+?)$', 'django.contrib.admin.views.changelist.change_list' ),
 )
-urlpatterns = patterns('', *urlpatterns)
