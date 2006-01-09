2b8f54fa33b98d914bd3a8d8f4c66f8004335452
Adrian Holovaty <adrian@holovaty.com>
2006-01-09 00:33:13 +0000

    magic-removal: Changed flatpages models.py to reflect new location of Site model
    git-svn-id: http://code.djangoproject.com/svn/django/branches/magic-removal@1869 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/flatpages/models.py
+++ django/contrib/flatpages/models.py
@@ -1,6 +1,6 @@
 from django.core import validators
 from django.db import models
-from django.models.core import Site
+from django.contrib.sites.models import Site
 from django.utils.translation import gettext_lazy as _
 
 class FlatPage(models.Model):
