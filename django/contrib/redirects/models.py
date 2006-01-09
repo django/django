49fa5cbf1f8699b06a61f604eb14bf17310228ad
Adrian Holovaty <adrian@holovaty.com>
2006-01-09 00:40:41 +0000

    magic-removal: Changed redirects models.py to reflect new location of Site model
    git-svn-id: http://code.djangoproject.com/svn/django/branches/magic-removal@1870 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/redirects/models.py
+++ django/contrib/redirects/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.models.core import Site
+from django.contrib.sites.models import Site
 from django.utils.translation import gettext_lazy as _
 
 class Redirect(models.Model):
