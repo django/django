92d2b01a447a6946fd14ea4304008f871227a83e
Adrian Holovaty <adrian@holovaty.com>
2006-01-21 04:36:35 +0000

    magic-removal: Continued simplification of AdminBoundManipulator -- Removed has_absolute_url attribute
    git-svn-id: http://code.djangoproject.com/svn/django/branches/magic-removal@2089 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/admin/views/main.py
+++ django/contrib/admin/views/main.py
@@ -152,7 +152,6 @@ class AdminBoundManipulator(BoundManipulator):
 
         self.auto_populated_fields = [f for f in self.opts.fields if f.prepopulate_from]
         self.javascript_imports = get_javascript_imports(self.opts, self.auto_populated_fields, field_sets);
-        self.has_absolute_url = hasattr(model, 'get_absolute_url')
         self.first_form_field_id = self.bound_field_sets[0].bound_field_lines[0].bound_fields[0].form_fields[0].get_id();
         self.ordered_object_pk_names = [o.pk.name for o in self.opts.get_ordered_objects()]
 
@@ -171,6 +170,7 @@ def render_change_form(model, manipulator, context, add=False, change=False, for
         'bound_manipulator': AdminBoundManipulator(model, manipulator, context['form']),
         'has_delete_permission': context['perms'][app_label][opts.get_delete_permission()],
         'has_file_field': opts.has_field_type(models.FileField),
+        'has_absolute_url': hasattr(model, 'get_absolute_url'),
         'form_url': form_url,
         'opts': opts,
     }
