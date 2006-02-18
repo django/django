4d1bb30480b74f83a144d46fdce626b818304a46
Adrian Holovaty <adrian@holovaty.com>
2006-02-18 22:06:42 +0000

    Reworded comment atop inspectdb-generated models to imply primary_key may now be introspected
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@2350 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/core/management.py
+++ django/core/management.py
@@ -581,7 +581,7 @@ def inspectdb(db_name):
     yield "# This is an auto-generated Django model module."
     yield "# You'll have to do the following manually to clean this up:"
     yield "#     * Rearrange models' order"
-    yield "#     * Add primary_key=True to one field in each model."
+    yield "#     * Make sure each model has one field with primary_key=True"
     yield "# Feel free to rename the models, but don't rename db_table values or field names."
     yield "#"
     yield "# Also note: You'll have to insert the output of 'django-admin.py sqlinitialdata [appname]'"
