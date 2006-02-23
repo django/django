0a3985868a893b9045577e3f291b8f2c6faa4335
Adrian Holovaty <adrian@holovaty.com>
2006-02-23 17:38:00 +0000

    magic-removal: Merged to [2368]
    git-svn-id: http://code.djangoproject.com/svn/django/branches/magic-removal@2369 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/core/handlers/wsgi.py
+++ django/core/handlers/wsgi.py
@@ -163,6 +163,10 @@ class WSGIHandler(BaseHandler):
         finally:
             dispatcher.send(signal=signals.request_finished)
 
+        # Apply response middleware
+        for middleware_method in self._response_middleware:
+            response = middleware_method(request, response)
+
         try:
             status_text = STATUS_CODE_TEXT[response.status_code]
         except KeyError:
