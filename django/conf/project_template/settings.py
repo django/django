f5dd6b9a459478cb5ac58e840dd05253b9ba7a58
Joseph Kocherhans <joseph@jkocherhans.com>
2006-03-07 01:43:31 +0000

    magic-removal: Changed RequestUserMiddleware to AuthenticationMiddleware in settings.py template. This should have been a part of [2490]
    git-svn-id: http://code.djangoproject.com/svn/django/branches/magic-removal@2492 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/project_template/settings.py
+++ django/conf/project_template/settings.py
@@ -53,7 +53,7 @@ TEMPLATE_LOADERS = (
 MIDDLEWARE_CLASSES = (
     "django.middleware.common.CommonMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.contrib.auth.middleware.RequestUserMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.middleware.doc.XViewMiddleware",
 )
 
