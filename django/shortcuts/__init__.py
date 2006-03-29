07131c390db60022373dbb9544e1288035f39530
Jacob Kaplan-Moss <jacob@jacobian.org>
2006-03-29 16:05:56 +0000

    magic-removal: fixed #1554 (thanks, Christopher Lenz)
    git-svn-id: http://code.djangoproject.com/svn/django/branches/magic-removal@2594 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/shortcuts/__init__.py
+++ django/shortcuts/__init__.py
@@ -17,7 +17,7 @@ def get_object_or_404(klass, **kwargs):
         raise Http404
 
 def get_list_or_404(klass, **kwargs):
-    obj_list = klass._default_manager.get_list(**kwargs)
+    obj_list = list(klass._default_manager.filter(**kwargs))
     if not obj_list:
         raise Http404
     return obj_list
