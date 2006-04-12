0244a836cdbb96993ede83ec05970a45e1f4776a
Adrian Holovaty <adrian@holovaty.com>
2006-04-12 03:37:30 +0000

    magic-removal: Fixed #1525 -- Got rid of a few 'import *' calls
    git-svn-id: http://code.djangoproject.com/svn/django/branches/magic-removal@2683 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/db/models/__init__.py
+++ django/db/models/__init__.py
@@ -2,12 +2,12 @@ from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist, ImproperlyConfigured
 from django.core import validators
 from django.db import backend, connection
-from django.db.models.loading import *
+from django.db.models.loading import get_apps, get_app, get_models, get_model, register_models
 from django.db.models.query import Q
 from django.db.models.manager import Manager
 from django.db.models.base import Model, AdminOptions
 from django.db.models.fields import *
-from django.db.models.fields.related import *
+from django.db.models.fields.related import ForeignKey, OneToOneField, ManyToManyField
 from django.db.models import signals
 from django.utils.functional import curry
 from django.utils.text import capfirst
