64d292e3feeeed336f117d1c40a3b3693c0e89b0
Joseph Kocherhans <joseph@jkocherhans.com>
2006-05-16 21:04:50 +0000

    multi-auth: Moved SettingsBackend to docs.
    git-svn-id: http://code.djangoproject.com/svn/django/branches/multi-auth@2924 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/auth/backends.py
+++ django/contrib/auth/backends.py
@@ -1,34 +1,4 @@
-from django.conf import settings
 from django.contrib.auth.models import User, check_password
-
-class SettingsBackend:
-    """
-    Authenticate against vars in settings.py Use the login name, and a hash 
-    of the password.
-    
-    ADMIN_LOGIN = 'admin'
-    ADMIN_PASSWORD = 'sha1$4e987$afbcf42e21bd417fb71db8c66b321e9fc33051de'
-    """
-    def authenticate(self, username=None, password=None):
-        login_valid = (settings.ADMIN_LOGIN == username)
-        pwd_valid = check_password(password, settings.ADMIN_PASSWORD)
-        if login_valid and pwd_valid:
-            # TODO: This should be abstracted out someplace else.
-            try:
-                user = User.objects.get(username=username)
-            except User.DoesNotExist:
-                user = User(username=username, password='')
-                user.is_staff = True
-                user.is_superuser = True
-                user.save()
-            return user
-        return None
-
-    def get_user(self, user_id):
-        try:
-            return User.objects.get(pk=user_id)
-        except User.DoesNotExist:
-            return None
 
 class ModelBackend:
     """
