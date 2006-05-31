2f513e0f0e95d8f146385803743c7128dbc9b885
Adrian Holovaty <adrian@holovaty.com>
2006-05-31 02:09:07 +0000

    Added support to django/contrib/auth/handlers/modpython.py for setting DJANGO_SETTINGS_MODULE via a PythonOption instead of SetEnv
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@3012 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/auth/handlers/modpython.py
+++ django/contrib/auth/handlers/modpython.py
@@ -10,8 +10,6 @@ def authenhandler(req, **kwargs):
     # that so that the following import works
     os.environ.update(req.subprocess_env)
 
-    from django.contrib.auth.models import User
-
     # check for PythonOptions
     _str_to_bool = lambda s: s.lower() in ('1', 'true', 'on', 'yes')
 
@@ -19,6 +17,11 @@ def authenhandler(req, **kwargs):
     permission_name = options.get('DjangoPermissionName', None)
     staff_only = _str_to_bool(options.get('DjangoRequireStaffStatus', "on"))
     superuser_only = _str_to_bool(options.get('DjangoRequireSuperuserStatus', "off"))
+    settings_module = options.get('DJANGO_SETTINGS_MODULE', None)
+    if settings_module:
+        os.environ['DJANGO_SETTINGS_MODULE'] = settings_module
+
+    from django.contrib.auth.models import User
 
     # check that the username is valid
     kwargs = {'username': req.user, 'is_active': True}
