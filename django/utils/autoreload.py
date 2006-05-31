8623bd126ddf85344aaf5aedd933e90b32c71148
Adrian Holovaty <adrian@holovaty.com>
2006-05-31 14:53:23 +0000

    Fixed #2036 -- autoreload.py no longer fails for uninstalled 'thread' module. Thanks, plmeister@gmail.com
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@3020 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/utils/autoreload.py
+++ django/utils/autoreload.py
@@ -28,7 +28,12 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import os, sys, thread, time
+import os, sys, time
+
+try:
+    import thread
+except ImportError:
+    import dummy_thread as thread
 
 RUN_RELOADER = True
 
