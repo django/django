5f471618318d0d02479dcc357a77fa90c15588d2
Adrian Holovaty <adrian@holovaty.com>
2006-06-20 14:11:59 +0000

    Added semicolon test to regressiontests/initial_sql_regress/sql/simple.sql
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@3180 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/initial_sql_regress/sql/simple.sql
+++ tests/regressiontests/initial_sql_regress/sql/simple.sql
@@ -3,4 +3,5 @@ INSERT INTO initial_sql_regress_simple (name) VALUES ('Paul');
 INSERT INTO initial_sql_regress_simple (name) VALUES ('Ringo');
 INSERT INTO initial_sql_regress_simple (name) VALUES ('George');
 INSERT INTO initial_sql_regress_simple (name) VALUES ('Miles O''Brien');
+INSERT INTO initial_sql_regress_simple (name) VALUES ('Semicolon;Man');
 
