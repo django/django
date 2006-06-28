bf629e5a4d1a51f938c84d35d768830158ad5ebd
Joseph Kocherhans <joseph@jkocherhans.com>
2006-06-28 03:57:40 +0000

    generic-auth: Initial implementation and tests for the new has_permission function
    git-svn-id: http://code.djangoproject.com/svn/django/branches/generic-auth@3222 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/auth/__init__.py
+++ django/contrib/auth/__init__.py
@@ -1,2 +1,29 @@
 LOGIN_URL = '/accounts/login/'
 REDIRECT_FIELD_NAME = 'next'
+
+class NoMatchFound(Exception): pass
+
+class HasPermission(object):
+    """
+    Function that supports multiple implementations via a type registry. The 
+    implemetation called depends on the argument types.
+    """
+    def __init__(self):
+        self.registry = {}
+
+    def __call__(self, user, permission, obj=None):
+        # TODO: this isn't very robust. Only matches on exact types. Support 
+        # for matching subclasses and caching registry hits would be helpful,
+        # but we'll add that later
+        types = (type(user), type(permission), type(obj))
+        func = self.registry.get(types)
+        if func is not None:
+            return func(user, permission, obj)
+        else:
+            raise NoMatchFound, "%s\n%s" % (self.registry, types)
+
+    def register(self, user_type, permission_type, obj_type, func):
+        types = (user_type, permission_type, obj_type)
+        self.registry[types] = func
+
+has_permission = HasPermission()
