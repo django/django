954050c196f7692cb88118f4947d49056ccfcba9
Adrian Holovaty <adrian@holovaty.com>
2006-07-28 21:24:19 +0000

    Removed bug from [3461] -- __name__ is not assignable
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@3478 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/auth/decorators.py
+++ django/contrib/auth/decorators.py
@@ -15,7 +15,6 @@ def user_passes_test(test_func, login_url=LOGIN_URL):
             return HttpResponseRedirect('%s?%s=%s' % (login_url, REDIRECT_FIELD_NAME, quote(request.get_full_path())))
         _checklogin.__doc__ = view_func.__doc__
         _checklogin.__dict__ = view_func.__dict__
-        _checklogin.__name__ = view_func.__name__
 
         return _checklogin
     return _dec
