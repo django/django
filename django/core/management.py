7a22a5194ccf81cc94ad03daf7f399f0b307430f
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2006-08-09 11:22:14 +0000

    Fixed #2469 -- Made the "drop index" constraint names match those we create in the first place.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@3543 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/core/management.py
+++ django/core/management.py
@@ -301,7 +301,7 @@ def get_sql_delete(app):
                         (style.SQL_KEYWORD('ALTER TABLE'),
                         style.SQL_TABLE(backend.quote_name(table)),
                         style.SQL_KEYWORD(backend.get_drop_foreignkey_sql()),
-                        style.SQL_FIELD(backend.quote_name("%s_referencing_%s_%s" % (col, r_table, r_col)))))
+                        style.SQL_FIELD(backend.quote_name('%s_refs_%s_%x' % (r_col, col, abs(hash((table, r_table))))))))
                 del references_to_delete[model]
 
     # Output DROP TABLE statements for many-to-many tables.
