caa51f4444338aaf92409ab45b1cbe885389eb2f
Adrian Holovaty <adrian@holovaty.com>
2006-08-10 16:58:26 +0000

    Fixed #2517 -- Improved inspectdb to handle table column names that contain spaces. Thanks, Karen Tracey
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@3549 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/core/management.py
+++ django/core/management.py
@@ -733,6 +733,10 @@ def inspectdb():
                 extra_params['db_column'] = att_name
                 att_name += '_field'
                 comment_notes.append('Field renamed because it was a Python reserved word.')
+            elif ' ' in att_name:
+                extra_params['db_column'] = att_name
+                att_name = att_name.replace(' ', '')
+                comment_notes.append('Field renamed to remove spaces.')
 
             if relations.has_key(i):
                 rel_to = relations[i][1] == table_name and "'self'" or table2model(relations[i][1])
