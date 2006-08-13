4f0118995c43fd3cb5f79ffcbaf10b1115d8e434
Christopher Long <indirecthit@gmail.com>
2006-08-13 21:36:38 +0000

    [per-object-permissions] Added "Apply Selected" button to RLP table that submits the selected row levle permissions to be changed.
    git-svn-id: http://code.djangoproject.com/svn/django/branches/per-object-permissions@3578 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/admin/media/js/row_level_permission.js
+++ django/contrib/admin/media/js/row_level_permission.js
@@ -1,13 +1,31 @@
 var row_level_permission = {
-	
-	
 	copyToNew: function (id)
 	{
-	    var newForm = add_row_level_permission.form;
+	    var newForm = document.getElementById("addRLPForm");
 	    var form = document.getElementById("editRLPForm-"+id);
 	    newForm.owner.selectedIndex = form.owner.selectedIndex;
 	    newForm.perm.selectedIndex = form.perm.selectedIndex;
 	    newForm.negative.checked = form.negative.checked;
-	}	
+	},
+	
+	apply_selected: function ()
+	{
+		var eleList = document.getElementsByName("apply_checkbox");
+		var formList = [];
+		for(var i=0; eleList.length; i++)
+		{
+			var ele = eleList[i];
+			if(ele.tagName == "INPUT") {
+				if(ele.checked)	{
+					ele.form.submit();
+				}
+			}
+		}
+		return false;
+	},
+	
+	init: function() {
+		
+	}
 }
 
