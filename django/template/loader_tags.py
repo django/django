8e52384b6c3d66ec2f256983c675af5f95983e72
Adrian Holovaty <adrian@holovaty.com>
2006-09-04 23:56:09 +0000

    Added small comment to django.template.loader_tags, to clarify change from [3465]
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@3719 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/template/loader_tags.py
+++ django/template/loader_tags.py
@@ -51,7 +51,7 @@ class ExtendsNode(Node):
                 error_msg += " Got this from the %r variable." % self.parent_name_expr #TODO nice repr.
             raise TemplateSyntaxError, error_msg
         if hasattr(parent, 'render'):
-            return parent
+            return parent # parent is a Template object
         try:
             source, origin = find_template_source(parent, self.template_dirs)
         except TemplateDoesNotExist:
