8aa6a94e1a6a457b3a62377ff68b64421cc4596f
Adrian Holovaty <adrian@holovaty.com>
2006-09-07 18:26:00 +0000

    Renamed 'func' argument in django.utils.functional.curry to reduce possibility that it will clash with a keyword argument name. Thanks for pointing this out, Michael Spencer
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@3735 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/utils/functional.py
+++ django/utils/functional.py
@@ -1,6 +1,6 @@
-def curry(func, *args, **kwargs):
+def curry(_curried_func, *args, **kwargs):
     def _curried(*moreargs, **morekwargs):
-        return func(*(args+moreargs), **dict(kwargs, **morekwargs))
+        return _curried_func(*(args+moreargs), **dict(kwargs, **morekwargs))
     return _curried
 
 class Promise:
