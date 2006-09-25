6dacb3d10b4a29cca54e2f0196b24f471a10ceb9
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2006-09-25 07:36:46 +0000

    Fixed #2729 -- Handle initial SQL with different line-ending styles (Windows vs. Unix vs. Mac). Thanks, Simon Greenhill.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@3821 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/core/management.py
+++ django/core/management.py
@@ -347,7 +347,7 @@ def get_sql_initial_data_for_model(model):
                  os.path.join(app_dir, "%s.sql" % opts.object_name.lower())]
     for sql_file in sql_files:
         if os.path.exists(sql_file):
-            fp = open(sql_file)
+            fp = open(sql_file, 'U')
             for statement in statements.split(fp.read()):
                 if statement.strip():
                     output.append(statement + ";")
