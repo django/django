c0eab791df84293ac5249784f39874ef44bbe4a6
Adrian Holovaty <adrian@holovaty.com>
2006-11-01 23:53:37 +0000

    Added PasswordInput, HiddenInput, FileInput, Select to django.newforms.widgets
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@3956 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/newforms/widgets.py
+++ django/newforms/widgets.py
@@ -2,9 +2,10 @@
 HTML Widget classes
 """
 
-__all__ = ('Widget', 'TextInput', 'Textarea', 'CheckboxInput')
+__all__ = ('Widget', 'TextInput', 'PasswordInput', 'HiddenInput', 'FileInput', 'Textarea', 'CheckboxInput', 'Select')
 
 from django.utils.html import escape
+from itertools import chain
 
 # Converts a dictionary to a single string with key="value", XML-style.
 # Assumes keys do not need to be XML-escaped.
@@ -17,14 +18,28 @@ class Widget(object):
     def render(self, name, value):
         raise NotImplementedError
 
-class TextInput(Widget):
+class Input(Widget):
+    "Base class for all <input> widgets (except type='checkbox', which is special)"
+    input_type = None # Subclasses must define this.
     def render(self, name, value, attrs=None):
         if value is None: value = ''
-        final_attrs = dict(self.attrs, type='text', name=name)
+        final_attrs = dict(self.attrs, type=self.input_type, name=name)
         if attrs:
             final_attrs.update(attrs)
         if value != '': final_attrs['value'] = value # Only add the 'value' attribute if a value is non-empty.
         return u'<input %s />' % flatatt(final_attrs)
+
+class TextInput(Input):
+    input_type = 'text'
+
+class PasswordInput(Input):
+    input_type = 'password'
+
+class HiddenInput(Input):
+    input_type = 'hidden'
+
+class FileInput(Input):
+    input_type = 'file'
 
 class Textarea(Widget):
     def render(self, name, value, attrs=None):
@@ -41,3 +56,31 @@ class CheckboxInput(Widget):
             final_attrs.update(attrs)
         if value: final_attrs['checked'] = 'checked'
         return u'<input %s />' % flatatt(final_attrs)
+
+class Select(Widget):
+    def __init__(self, attrs=None, choices=()):
+        # choices can be any iterable
+        self.attrs = attrs or {}
+        self.choices = choices
+
+    def render(self, name, value, attrs=None, choices=()):
+        if value is None: value = ''
+        final_attrs = dict(self.attrs, name=name)
+        if attrs:
+            final_attrs.update(attrs)
+        output = [u'<select %s>' % flatatt(final_attrs)]
+        str_value = str(value) # Normalize to string.
+        for option_value, option_label,  in chain(self.choices, choices):
+            selected_html = (str(option_value) == str_value) and ' selected="selected"' or ''
+            output.append(u'<option value="%s"%s>%s</option>' % (escape(option_value), selected_html, escape(option_label)))
+        output.append(u'</select>')
+        return u'\n'.join(output)
+
+class SelectMultiple(Widget):
+    pass
+
+class RadioSelect(Widget):
+    pass
+
+class CheckboxSelectMultiple(Widget):
+    pass
