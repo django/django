5a597d3bbb5c868ed6b48555f3b4f708dc7b032f
Adrian Holovaty <adrian@holovaty.com>
2006-11-15 20:16:04 +0000

    newforms: Cleaned up some un-DRYness by adding Widget.build_attrs(). Also slightly changed flatatt to include a leading space, so the spaces don't have to be hard-coded each time you embed flatatt() results in HTML. Thanks, SmileyChris. Refs #3023
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@4071 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/newforms/widgets.py
+++ django/newforms/widgets.py
@@ -16,9 +16,9 @@ try:
 except NameError:
     from sets import Set as set # Python 2.3 fallback
 
-# Converts a dictionary to a single string with key="value", XML-style.
-# Assumes keys do not need to be XML-escaped.
-flatatt = lambda attrs: ' '.join(['%s="%s"' % (k, escape(v)) for k, v in attrs.items()])
+# Converts a dictionary to a single string with key="value", XML-style with
+# a leading space. Assumes keys do not need to be XML-escaped.
+flatatt = lambda attrs: ''.join([' %s="%s"' % (k, escape(v)) for k, v in attrs.items()])
 
 class Widget(object):
     requires_data_list = False # Determines whether render()'s 'value' argument should be a list.
@@ -28,16 +28,20 @@ class Widget(object):
     def render(self, name, value):
         raise NotImplementedError
 
+    def build_attrs(self, extra_attrs=None, **kwargs):
+        attrs = dict(self.attrs, **kwargs)
+        if extra_attrs:
+            attrs.update(extra_attrs)
+        return attrs
+
 class Input(Widget):
     "Base class for all <input> widgets (except type='checkbox', which is special)"
     input_type = None # Subclasses must define this.
     def render(self, name, value, attrs=None):
         if value is None: value = ''
-        final_attrs = dict(self.attrs, type=self.input_type, name=name)
-        if attrs:
-            final_attrs.update(attrs)
+        final_attrs = self.build_attrs(attrs, type=self.input_type, name=name)
         if value != '': final_attrs['value'] = value # Only add the 'value' attribute if a value is non-empty.
-        return u'<input %s />' % flatatt(final_attrs)
+        return u'<input%s />' % flatatt(final_attrs)
 
 class TextInput(Input):
     input_type = 'text'
@@ -54,18 +58,14 @@ class FileInput(Input):
 class Textarea(Widget):
     def render(self, name, value, attrs=None):
         if value is None: value = ''
-        final_attrs = dict(self.attrs, name=name)
-        if attrs:
-            final_attrs.update(attrs)
-        return u'<textarea %s>%s</textarea>' % (flatatt(final_attrs), escape(value))
+        final_attrs = self.build_attrs(attrs, name=name)
+        return u'<textarea%s>%s</textarea>' % (flatatt(final_attrs), escape(value))
 
 class CheckboxInput(Widget):
     def render(self, name, value, attrs=None):
-        final_attrs = dict(self.attrs, type='checkbox', name=name)
-        if attrs:
-            final_attrs.update(attrs)
+        final_attrs = self.build_attrs(attrs, type='checkbox', name=name)
         if value: final_attrs['checked'] = 'checked'
-        return u'<input %s />' % flatatt(final_attrs)
+        return u'<input%s />' % flatatt(final_attrs)
 
 class Select(Widget):
     def __init__(self, attrs=None, choices=()):
@@ -75,10 +75,8 @@ class Select(Widget):
 
     def render(self, name, value, attrs=None, choices=()):
         if value is None: value = ''
-        final_attrs = dict(self.attrs, name=name)
-        if attrs:
-            final_attrs.update(attrs)
-        output = [u'<select %s>' % flatatt(final_attrs)]
+        final_attrs = self.build_attrs(attrs, name=name)
+        output = [u'<select%s>' % flatatt(final_attrs)]
         str_value = str(value) # Normalize to string.
         for option_value, option_label in chain(self.choices, choices):
             selected_html = (str(option_value) == str_value) and ' selected="selected"' or ''
@@ -95,10 +93,8 @@ class SelectMultiple(Widget):
 
     def render(self, name, value, attrs=None, choices=()):
         if value is None: value = []
-        final_attrs = dict(self.attrs, name=name)
-        if attrs:
-            final_attrs.update(attrs)
-        output = [u'<select multiple="multiple" %s>' % flatatt(final_attrs)]
+        final_attrs = self.build_attrs(attrs, name=name)
+        output = [u'<select multiple="multiple"%s>' % flatatt(final_attrs)]
         str_values = set([str(v) for v in value]) # Normalize to strings.
         for option_value, option_label in chain(self.choices, choices):
             selected_html = (str(option_value) in str_values) and ' selected="selected"' or ''
