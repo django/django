8379785e79c6d92baa02f5705009d38f1601c1e1
Adrian Holovaty <adrian@holovaty.com>
2006-11-25 06:33:59 +0000

    Moved newforms form_for_model and form_for_fields to django/newforms/models.py
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@4103 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/newforms/__init__.py
+++ django/newforms/__init__.py
@@ -14,15 +14,4 @@ from util import ValidationError
 from widgets import *
 from fields import *
 from forms import Form
-
-##########################
-# DATABASE API SHORTCUTS #
-##########################
-
-def form_for_model(model):
-    "Returns a Form instance for the given Django model class."
-    raise NotImplementedError
-
-def form_for_fields(field_list):
-    "Returns a Form instance for the given list of Django database field instances."
-    raise NotImplementedError
+from models import *
