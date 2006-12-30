d04a2e792c91016f1290754ff01af5f67917fcf1
Adrian Holovaty <adrian@holovaty.com>
2006-12-30 07:48:11 +0000

    Fixed typo in a docstring in django/contrib/admin/views/main.py
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@4269 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/admin/views/main.py
+++ django/contrib/admin/views/main.py
@@ -46,8 +46,8 @@ def quote(s):
     """
     Ensure that primary key values do not confuse the admin URLs by escaping
     any '/', '_' and ':' characters. Similar to urllib.quote, except that the
-    quoting is slightly different so that it doesn't get autoamtically
-    unquoted by the web browser.
+    quoting is slightly different so that it doesn't get automatically
+    unquoted by the Web browser.
     """
     if type(s) != type(''):
         return s
