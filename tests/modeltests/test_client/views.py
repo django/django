92e335549329919f978221b3ddced2814abde607
Adrian Holovaty <adrian@holovaty.com>
2007-02-02 20:06:20 +0000

    newforms-admin: Merged to [4456]
    git-svn-id: http://code.djangoproject.com/svn/django/branches/newforms-admin@4457 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/modeltests/test_client/views.py
+++ tests/modeltests/test_client/views.py
@@ -26,10 +26,10 @@ def redirect_view(request):
     "A view that redirects all requests to the GET view"
     return HttpResponseRedirect('/test_client/get_view/')
     
-@login_required
 def login_protected_view(request):
     "A simple view that is login protected."
     t = Template('This is a login protected test. Username is {{ user.username }}.', name='Login Template')
     c = Context({'user': request.user})
     
     return HttpResponse(t.render(c))
+login_protected_view = login_required(login_protected_view)
