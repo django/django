5e34ce97df3563883f1c0035e2150f90fe78ed30
Adrian Holovaty <adrian@holovaty.com>
2007-02-27 00:01:43 +0000

    newforms-admin: Merged to [4629]
    git-svn-id: http://code.djangoproject.com/svn/django/branches/newforms-admin@4630 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/runtests.py
+++ tests/runtests.py
@@ -148,5 +148,7 @@ if __name__ == "__main__":
     options, args = parser.parse_args()
     if options.settings:
         os.environ['DJANGO_SETTINGS_MODULE'] = options.settings
-
+    elif "DJANGO_SETTINGS_MODULE" not in os.environ:
+        parser.error("DJANGO_SETTINGS_MODULE is not set in the environment. "
+                      "Set it or use --settings.")
     django_tests(int(options.verbosity), args)
