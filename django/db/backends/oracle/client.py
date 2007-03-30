b86d69f52920adf8e065bf6952ab6b3814211d4e
Boulder Sprinters <boulder-sprinters@djangoproject.com>
2007-03-30 16:54:17 +0000

    boulder-oracle-sprint: Fixed "manage.py dbshell" for Oracle so sqlplus is invoked correctly.
    git-svn-id: http://code.djangoproject.com/svn/django/branches/boulder-oracle-sprint@4872 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/db/backends/oracle/client.py
+++ django/db/backends/oracle/client.py
@@ -2,9 +2,10 @@ from django.conf import settings
 import os
 
 def runshell():
-    args = ''
-    args += settings.DATABASE_USER
+    dsn = settings.DATABASE_USER
     if settings.DATABASE_PASSWORD:
-        args += "/%s" % settings.DATABASE_PASSWORD
-    args += "@%s" % settings.DATABASE_NAME
-    os.execvp('sqlplus', args)
+        dsn += "/%s" % settings.DATABASE_PASSWORD
+    if settings.DATABASE_NAME:
+        dsn += "@%s" % settings.DATABASE_NAME
+    args = ["sqlplus", "-L", dsn]
+    os.execvp("sqlplus", args)
