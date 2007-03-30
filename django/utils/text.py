df119b778654613f4a2fba0bfe8c4c011423866f
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2007-03-30 12:00:38 +0000

    Removed some trailing whitespace that had smuggled itself aboard in [4870].
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@4871 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/utils/text.py
+++ django/utils/text.py
@@ -192,8 +192,8 @@ def smart_split(text):
     backslashes. In the output, strings will keep their initial and trailing
     quote marks.
 
-    >>> list(smart_split('This is "a person\'s" test.')) 
-    ['This', 'is', '"a person\'s"', 'test.'] 
+    >>> list(smart_split('This is "a person\'s" test.'))
+    ['This', 'is', '"a person\'s"', 'test.']
     """
     for bit in smart_split_re.finditer(text):
         bit = bit.group(0)
