4a3db287f1c86f7e66408432b381190bc9d0557d
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2007-04-02 10:53:05 +0000

    Fixed #3600 -- Made smart_unicode respect deferred evaluation in the case of strings translated with gettext_lazy and friends.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@4904 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/forms/regressions.py
+++ tests/regressiontests/forms/regressions.py
@@ -10,4 +10,20 @@ It should be possible to re-use attribute dictionaries (#3810)
 ...     f2 = CharField(widget=TextInput(attrs=extra_attrs))
 >>> TestForm(auto_id=False).as_p()
 u'<p>F1: <input type="text" class="special" name="f1" maxlength="10" /></p>\n<p>F2: <input type="text" class="special" name="f2" /></p>'
+
+####################### 
+# Tests for form i18n # 
+####################### 
+There were some problems with form translations in #3600
+ 
+>>> from django.utils.translation import gettext_lazy, activate, deactivate
+>>> class SomeForm(Form):
+...     username = CharField(max_length=10, label=gettext_lazy('Username'))
+>>> f = SomeForm()
+>>> print f.as_p()
+<p><label for="id_username">Username:</label> <input id="id_username" type="text" name="username" maxlength="10" /></p>
+>>> activate('de')
+>>> print f.as_p()
+<p><label for="id_username">Benutzername:</label> <input id="id_username" type="text" name="username" maxlength="10" /></p>
+>>> deactivate()
 """
