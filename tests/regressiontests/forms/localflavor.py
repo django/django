5f2c513f9cfb7c4e20f3c970c602649e64de541d
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2007-04-04 11:40:08 +0000

    Fixed #3919 -- Added German identity card number validation to German localflavor. Thanks, Jannis Leidel.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@4922 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/forms/localflavor.py
+++ tests/regressiontests/forms/localflavor.py
@@ -869,4 +869,17 @@ ValidationError: [u'Enter a zip code in the format XXXXX.']
 >>> w = DEStateSelect()
 >>> w.render('states', 'TH')
 u'<select name="states">\n<option value="BW">Baden-W\xfcrttemberg</option>\n<option value="BY">Bavaria</option>\n<option value="BE">Berlin</option>\n<option value="BB">Brandenburg</option>\n<option value="HB">Bremen</option>\n<option value="HH">Hamburg</option>\n<option value="HE">Hessen</option>\n<option value="MV">Mecklenburg-Western Pomerania</option>\n<option value="NI">Lower Saxony</option>\n<option value="NW">North Rhine-Westphalia</option>\n<option value="RP">Rhineland-Palatinate</option>\n<option value="SL">Saarland</option>\n<option value="SN">Saxony</option>\n<option value="ST">Saxony-Anhalt</option>\n<option value="SH">Schleswig-Holstein</option>\n<option value="TH" selected="selected">Thuringia</option>\n</select>'
+
+# DEIdentityCardNumberField #################################################
+
+>>> from django.contrib.localflavor.de.forms import DEIdentityCardNumberField
+>>> f = DEIdentityCardNumberField()
+>>> f.clean('7549313035D-6004103-0903042-0')
+u'7549313035D-6004103-0903042-0'
+>>> f.clean('9786324830D 6104243 0910271 2')
+u'9786324830D-6104243-0910271-2'
+>>> f.clean('0434657485D-6407276-0508137-9')
+Traceback (most recent call last):
+...
+ValidationError: [u'Enter a valid German identity card number in XXXXXXXXXXX-XXXXXXX-XXXXXXX-X format']
 """
