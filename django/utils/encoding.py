56d56e5bac2923382fd10036dbe045412390362d
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2007-04-04 13:25:53 +0000

    Added a FIXME that I forgot to include earlier.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@4923 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/utils/encoding.py
+++ django/utils/encoding.py
@@ -6,6 +6,10 @@ def smart_unicode(s):
         # The input is the result of a gettext_lazy() call, or similar. It will
         # already be encoded in DEFAULT_CHARSET on evaluation and we don't want
         # to evaluate it until render time.
+        # FIXME: This isn't totally consistent, because it eventually returns a
+        # bytestring rather than a unicode object. It works wherever we use
+        # smart_unicode() at the moment. Fixing this requires work in the
+        # i18n internals.
         return s
     if not isinstance(s, basestring,):
         if hasattr(s, '__unicode__'):
