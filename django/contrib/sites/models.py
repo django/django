c1f84583de0993cba8f6a66008b59744e0ddc967
Adrian Holovaty <adrian@holovaty.com>
2007-04-07 04:39:05 +0000

    newforms-admin: Converted django.contrib.sites model admin options to use new syntax
    git-svn-id: http://code.djangoproject.com/svn/django/branches/newforms-admin@4951 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/sites/models.py
+++ django/contrib/sites/models.py
@@ -10,14 +10,24 @@ class Site(models.Model):
     domain = models.CharField(_('domain name'), maxlength=100)
     name = models.CharField(_('display name'), maxlength=50)
     objects = SiteManager()
+
     class Meta:
         db_table = 'django_site'
         verbose_name = _('site')
         verbose_name_plural = _('sites')
         ordering = ('domain',)
-    class Admin:
-        list_display = ('domain', 'name')
-        search_fields = ('domain', 'name')
 
     def __str__(self):
         return self.domain
+
+# Register the admin options for these models.
+# TODO: Maybe this should live in a separate module admin.py, but how would we
+# ensure that module was loaded?
+
+from django.contrib import admin
+
+class SiteAdmin(admin.ModelAdmin):
+    list_display = ('domain', 'name')
+    search_fields = ('domain', 'name')
+
+admin.site.register(Site, SiteAdmin)
