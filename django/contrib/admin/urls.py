e03e0214789bedcbfdda1dd6b6bf07b1e1b9ba0b
Adrian Holovaty <adrian@holovaty.com>
2007-04-07 02:28:48 +0000

    newforms-admin: Updated admin urls.py to reflect the status of the conversion to AdminSite
    git-svn-id: http://code.djangoproject.com/svn/django/branches/newforms-admin@4943 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/admin/urls.py
+++ django/contrib/admin/urls.py
@@ -1,18 +1,12 @@
-from django.conf import settings
 from django.conf.urls.defaults import *
 
-if settings.USE_I18N:
-    i18n_view = 'django.views.i18n.javascript_catalog'
-else:
-    i18n_view = 'django.views.i18n.null_javascript_catalog'
-
 urlpatterns = patterns('',
-    ('^$', 'django.contrib.admin.views.main.index'),
+    #('^$', 'django.contrib.admin.views.main.index'),
     ('^r/(\d+)/(.*)/$', 'django.views.defaults.shortcut'),
-    ('^jsi18n/$', i18n_view, {'packages': 'django.conf'}),
-    ('^logout/$', 'django.contrib.auth.views.logout'),
-    ('^password_change/$', 'django.contrib.auth.views.password_change'),
-    ('^password_change/done/$', 'django.contrib.auth.views.password_change_done'),
+    #('^jsi18n/$', i18n_view, {'packages': 'django.conf'}),
+    #('^logout/$', 'django.contrib.auth.views.logout'),
+    #('^password_change/$', 'django.contrib.auth.views.password_change'),
+    #('^password_change/done/$', 'django.contrib.auth.views.password_change_done'),
     ('^template_validator/$', 'django.contrib.admin.views.template.template_validator'),
 
     # "Add user" -- a special-case view
@@ -23,5 +17,3 @@ urlpatterns = patterns('',
     # Model-specific admin pages.
     ('^([^/]+)/([^/]+)/(?:(.+)/)?$', 'django.contrib.admin.views.main.model_admin_view'),
 )
-
-del i18n_view
