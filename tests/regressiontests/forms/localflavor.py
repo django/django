e9b66ce1d3b6964fb05874c3f86541b54c1f1adf
Russell Keith-Magee <russell@keith-magee.com>
2007-04-17 13:37:45 +0000

    Fixed #3866 -- Added Italian Social Security and VAT input fields to localflavor. Thanks, Massimiliano Ravelli.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@5018 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/forms/localflavor.py
+++ tests/regressiontests/forms/localflavor.py
@@ -633,7 +633,7 @@ u'00100'
 >>> f.clean(' 00100')
 Traceback (most recent call last):
 ...
-ValidationError: [u'Enter a zip code in the format XXXXX.']
+ValidationError: [u'Enter a valid zip code.']
 
 # ITRegionSelect #############################################################
 
@@ -641,6 +641,46 @@ ValidationError: [u'Enter a zip code in the format XXXXX.']
 >>> w = ITRegionSelect()
 >>> w.render('regions', 'PMN')
 u'<select name="regions">\n<option value="ABR">Abruzzo</option>\n<option value="BAS">Basilicata</option>\n<option value="CAL">Calabria</option>\n<option value="CAM">Campania</option>\n<option value="EMR">Emilia-Romagna</option>\n<option value="FVG">Friuli-Venezia Giulia</option>\n<option value="LAZ">Lazio</option>\n<option value="LIG">Liguria</option>\n<option value="LOM">Lombardia</option>\n<option value="MAR">Marche</option>\n<option value="MOL">Molise</option>\n<option value="PMN" selected="selected">Piemonte</option>\n<option value="PUG">Puglia</option>\n<option value="SAR">Sardegna</option>\n<option value="SIC">Sicilia</option>\n<option value="TOS">Toscana</option>\n<option value="TAA">Trentino-Alto Adige</option>\n<option value="UMB">Umbria</option>\n<option value="VAO">Valle d\u2019Aosta</option>\n<option value="VEN">Veneto</option>\n</select>'
+
+# ITSocialSecurityNumberField #################################################
+
+>>> from django.contrib.localflavor.it.forms import ITSocialSecurityNumberField
+>>> f = ITSocialSecurityNumberField()
+>>> f.clean('LVSGDU99T71H501L')
+u'LVSGDU99T71H501L'
+>>> f.clean('LBRRME11A01L736W')
+u'LBRRME11A01L736W'
+>>> f.clean('lbrrme11a01l736w')
+u'LBRRME11A01L736W'
+>>> f.clean('LBR RME 11A01 L736W')
+u'LBRRME11A01L736W'
+>>> f.clean('LBRRME11A01L736A')
+Traceback (most recent call last):
+...
+ValidationError: [u'Enter a valid Social Security number.']
+>>> f.clean('%BRRME11A01L736W')
+Traceback (most recent call last):
+...
+ValidationError: [u'Enter a valid Social Security number.']
+
+# ITVatNumberField ###########################################################
+
+>>> from django.contrib.localflavor.it.forms import ITVatNumberField
+>>> f = ITVatNumberField()
+>>> f.clean('07973780013')
+u'07973780013'
+>>> f.clean('7973780013')
+u'07973780013'
+>>> f.clean(7973780013)
+u'07973780013'
+>>> f.clean('07973780014')
+Traceback (most recent call last):
+...
+ValidationError: [u'Enter a valid VAT number.']
+>>> f.clean('A7973780013')
+Traceback (most recent call last):
+...
+ValidationError: [u'Enter a valid VAT number.']
 
 # FIZipCodeField #############################################################
 
