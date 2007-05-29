d9f6470f2dd452280b5dd1108510f3f5dba0cb9c
Russell Keith-Magee <russell@keith-magee.com>
2007-05-29 11:09:24 +0000

    Fixed #1278 -- Added a context processor that puts MEDIA_URL in the context, and added that context processor to the default set. Thanks to Ubernostrum for the patch.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@5379 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/global_settings.py
+++ django/conf/global_settings.py
@@ -144,6 +144,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.auth',
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
 #    'django.core.context_processors.request',
 )
 
