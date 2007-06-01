250b84dd0e30ae916e2f033790098281dff0b9e2
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2007-06-01 09:46:56 +0000

    Fixed #4427 -- Ported daily_cleanup.py to use model API for greater portability. Thanks, nick.lane.au@gmail.com.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@5403 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/bin/daily_cleanup.py
+++ django/bin/daily_cleanup.py
@@ -7,13 +7,13 @@ Can be run as a cronjob to clean out old data from the database (only expired
 sessions at the moment).
 """
 
-from django.db import backend, connection, transaction
+import datetime
+from django.db import transaction
+from django.contrib.sessions.models import Session
 
 def clean_up():
-    # Clean up old database records
-    cursor = connection.cursor()
-    cursor.execute("DELETE FROM %s WHERE %s < NOW()" % \
-        (backend.quote_name('django_session'), backend.quote_name('expire_date')))
+    """Clean up expired sessions."""
+    Session.objects.filter(expire_date__lt=datetime.datetime.now()).delete()
     transaction.commit_unless_managed()
 
 if __name__ == "__main__":
