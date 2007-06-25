17145eef4c4807f0ae385f48cf5cd3655a482154
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2007-06-25 16:10:33 +0000

    Removed unneeded import.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@5536 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/auth/models.py
+++ django/contrib/auth/models.py
@@ -209,7 +209,6 @@ class User(models.Model):
 
     def get_all_permissions(self):
         if not hasattr(self, '_perm_cache'):
-            import sets
             self._perm_cache = set(["%s.%s" % (p.content_type.app_label, p.codename) for p in self.user_permissions.select_related()])
             self._perm_cache.update(self.get_group_permissions())
         return self._perm_cache
