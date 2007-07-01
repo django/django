49d25eec2cf8daf29243d6f0c765666453984dc8
Adrian Holovaty <adrian@holovaty.com>
2007-07-01 02:06:07 +0000

    newforms-admin: Changed project_template urls.py to use the new style for including the admin
    git-svn-id: http://code.djangoproject.com/svn/django/branches/newforms-admin@5573 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/project_template/urls.py
+++ django/conf/project_template/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls.defaults import *
 
+# Uncomment this for admin:
+#from django.contrib import admin
+
 urlpatterns = patterns('',
     # Example:
     # (r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
@@ -8,5 +11,5 @@ urlpatterns = patterns('',
     #(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment this for admin:
-    #(r'^admin/', include('django.contrib.admin.urls')),
+    #('^admin/(.*)', admin.site.root),
 )
