3fef1f4c8d779ab40a13f13a1829a98e48e58c9c
Russell Keith-Magee <russell@keith-magee.com>
2007-07-20 12:07:58 +0000

    Fixed #4558 -- Modified XML serializer to handle whitespace better around None tags. Thanks to Bill Fenner <fenner@gmail.com> for the report and fix.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@5727 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/fixtures_regress/models.py
+++ tests/regressiontests/fixtures_regress/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Animal(models.Model):
     name = models.CharField(maxlength=150)
@@ -14,6 +15,13 @@ class Plant(models.Model):
         # For testing when upper case letter in app name; regression for #4057
         db_table = "Fixtures_regress_plant"
 
+class Stuff(models.Model):
+    name = models.CharField(maxlength=20, null=True)
+    owner = models.ForeignKey(User, null=True)
+    
+    def __unicode__(self):
+        return unicode(self.name) + u' is owned by ' + unicode(self.owner)
+
 __test__ = {'API_TESTS':"""
 >>> from django.core import management
 
@@ -25,5 +33,14 @@ __test__ = {'API_TESTS':"""
 # This is a regression test for ticket #3790.
 >>> animal = Animal(name='Platypus', latin_name='Ornithorhynchus anatinus')
 >>> animal.save()
+
+###############################################
+# Regression test for ticket #4558 -- pretty printing of XML fixtures
+# doesn't affect parsing of None values.
+
+# Load a pretty-printed XML fixture with Nulls.
+>>> management.load_data(['pretty.xml'], verbosity=0)
+>>> Stuff.objects.all()
+[<Stuff: None is owned by None>]
 
 """}
