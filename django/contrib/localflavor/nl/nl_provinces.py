a9fd677cb3167afbe0b27bf1a8fe5a16eaad5908
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2007-09-13 22:16:59 +0000

    Fixed #5403 -- Added Dutch localflavor. Thanks, Jan Rademaker.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@6139 bcc190cf-cafb-0310-a4f2-bffc1f526a37

---
+++ django/contrib/localflavor/nl/nl_provinces.py
@@ -0,0 +1,16 @@
+from django.utils.translation import ugettext_lazy as _
+
+PROVINCE_CHOICES = (
+    ('DR', _('Drente')),
+    ('FL', _('Flevoland')),
+    ('FR', _('Friesland')),
+    ('GL', _('Gelderland')),
+    ('GR', _('Groningen')),
+    ('LB', _('Limburg')),
+    ('NB', _('Noord-Brabant')),
+    ('NH', _('Noord-Holland')),
+    ('OV', _('Overijssel')),
+    ('UT', _('Utrecht')),
+    ('ZE', _('Zeeland')),
+    ('ZH', _('Zuid-Holland')),
+)
