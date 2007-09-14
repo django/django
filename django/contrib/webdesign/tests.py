59964c2c3fba3a59d43e7514e1478cca46852595
Adrian Holovaty <adrian@holovaty.com>
2007-09-14 22:03:47 +0000

    Fixed #5478 -- Fixed bug in lorem template tag. Thanks, racter
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@6228 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/webdesign/tests.py
+++ django/contrib/webdesign/tests.py
@@ -7,6 +7,10 @@ u'lorem ipsum dolor sit amet consectetur adipisicing'
 >>> paragraphs(1)
 ['Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.']
 
+>>> from django.template import loader, Context
+>>> t = loader.get_template_from_string("{% load webdesign %}{% lorem 3 w %}")
+>>> t.render(Context({}))
+u'lorem ipsum dolor'
 """
 
 from django.contrib.webdesign.lorem_ipsum import *
