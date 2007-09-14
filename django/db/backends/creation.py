f36e96cc9c6c1e99fb55cfc6940f04fa9a7c86ff
Adrian Holovaty <adrian@holovaty.com>
2007-09-14 16:44:53 +0000

    Added django/db/backends/creation.py, with BaseCreation. Refs #5461
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@6192 bcc190cf-cafb-0310-a4f2-bffc1f526a37

---
+++ django/db/backends/creation.py
@@ -0,0 +1,7 @@
+class BaseCreation(object):
+    """
+    This class encapsulates all backend-specific differences that pertain to
+    database *creation*, such as the column types to use for particular Django
+    Fields.
+    """
+    pass
