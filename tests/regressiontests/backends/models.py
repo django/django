5a5d8a90b54e88e561b4e1e3c65c67b9307244c2
Ian Kelly <ian.g.kelly@gmail.com>
2007-09-14 21:38:45 +0000

    Added test cases for [6218].
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@6220 bcc190cf-cafb-0310-a4f2-bffc1f526a37

---
+++ tests/regressiontests/backends/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class Square(models.Model):
+    root = models.IntegerField()
+    square = models.PositiveIntegerField()
+
+    def __unicode__(self):
+        return "%s ** 2 == %s" % (self.root, self.square)
+
+__test__ = {'API_TESTS': """
+
+#4896: Test cursor.executemany
+>>> from django.db import connection
+>>> cursor = connection.cursor()
+>>> cursor.executemany('INSERT INTO BACKENDS_SQUARE (ROOT, SQUARE) VALUES (%s, %s)',
+...                    [(i, i**2) for i in range(-5, 6)]) and None or None
+>>> Square.objects.order_by('root')
+[<Square: -5 ** 2 == 25>, <Square: -4 ** 2 == 16>, <Square: -3 ** 2 == 9>, <Square: -2 ** 2 == 4>, <Square: -1 ** 2 == 1>, <Square: 0 ** 2 == 0>, <Square: 1 ** 2 == 1>, <Square: 2 ** 2 == 4>, <Square: 3 ** 2 == 9>, <Square: 4 ** 2 == 16>, <Square: 5 ** 2 == 25>]
+
+#4765: executemany with params=None or params=[] does nothing
+>>> cursor.executemany('INSERT INTO BACKENDS_SQUARE (ROOT, SQUARE) VALUES (%s, %s)', None) and None or None
+>>> Square.objects.count()
+11
+
+>>> cursor.executemany('INSERT INTO BACKENDS_SQUARE (ROOT, SQUARE) VALUES (%s, %s)', []) and None or None
+>>> Square.objects.count()
+11
+
+"""}
