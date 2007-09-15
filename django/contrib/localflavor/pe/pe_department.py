b2f92dfcc511d5bb03e9bd81e073ee7a4e37e7d0
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2007-09-15 12:20:35 +0000

    Fixed #5462 -- Added Peruvian localflavor. Thanks, xbito.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@6283 bcc190cf-cafb-0310-a4f2-bffc1f526a37

---
+++ django/contrib/localflavor/pe/pe_department.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+A list of Peru departaments as `choices` in a
+formfield.
+
+This exists in this standalone file so that it's only imported into memory
+when explicitly needed.
+"""
+
+DEPARTMENT_CHOICES = (
+    ('AMA', u'Amazonas'),
+    ('ANC', u'Ancash'),
+    ('APU', u'Apurímac'),
+    ('ARE', u'Arequipa'),
+    ('AYA', u'Ayacucho'),
+    ('CAJ', u'Cajamarca'),
+    ('CUS', u'Cusco'),
+    ('HUV', u'Huancavelica'),
+    ('HUC', u'Huánuco'),
+    ('ICA', u'Ica'),
+    ('JUN', u'Junín'),
+    ('LAL', u'La Libertad'),
+    ('LAM', u'Lambayeque'),
+    ('LIM', u'Lima'),
+    ('LOR', u'Loreto'),
+    ('MDD', u'Madre de Dios'),
+    ('MOQ', u'Moquegua'),
+    ('PAS', u'Pasco'),
+    ('PIU', u'Piura'),
+    ('PUN', u'Puno'),
+    ('SAM', u'San Martín'),
+    ('TAC', u'Tacna'),
+    ('TUM', u'Tumbes'),
+    ('UCA', u'Ucayali'),
+)
