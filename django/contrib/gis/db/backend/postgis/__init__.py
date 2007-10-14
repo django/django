189335acaaae0fcf111cf566e1dc225e1697e44a
Justin Bronn <jbronn@gmail.com>
2007-10-14 20:48:33 +0000

    gis: removed import to the PostGIS proxy module.
    git-svn-id: http://code.djangoproject.com/svn/django/branches/gis@6509 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/gis/db/backend/postgis/__init__.py
+++ django/contrib/gis/db/backend/postgis/__init__.py
@@ -3,7 +3,6 @@
 """
 from django.contrib.gis.db.backend.postgis.creation import create_spatial_db
 from django.contrib.gis.db.backend.postgis.field import PostGISField, gqn
-from django.contrib.gis.db.backend.postgis.proxy import PostGISProxy
 from django.contrib.gis.db.backend.postgis.query import \
     get_geo_where_clause, GEOM_FUNC_PREFIX, POSTGIS_TERMS, \
     MAJOR_VERSION, MINOR_VERSION1, MINOR_VERSION2, \
