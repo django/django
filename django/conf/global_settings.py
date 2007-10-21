5f51964be2907f001d78e3daf06de5eb853f66de
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2007-10-21 16:20:02 +0000

    Changed the location of language codes to match the project settings file from [6123].
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@6581 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/global_settings.py
+++ django/conf/global_settings.py
@@ -30,7 +30,7 @@ INTERNAL_IPS = ()
 TIME_ZONE = 'America/Chicago'
 
 # Language code for this installation. All choices can be found here:
-# http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
+# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
 # Languages we provide translations for, out of the box. The language name
