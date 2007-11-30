7628381eb8f4f4555c2a6a70c10d5ff706940ddf
Adrian Holovaty <adrian@holovaty.com>
2007-11-30 05:14:09 +0000

    Edited docstring from [6505] -- single spaces separate sentences, not double spaces.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@6768 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/views/static.py
+++ django/views/static.py
@@ -1,5 +1,5 @@
 """
-Views and functions for serving static files.  These are only to be used
+Views and functions for serving static files. These are only to be used
 during development, and SHOULD NOT be used in a production setting.
 """
 
