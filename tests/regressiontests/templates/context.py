67373009e053aca469f857be99158564fc9b42f0
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2007-12-02 23:57:22 +0000

    Fixed #4563 -- Context.pop/push/update return the top-level dictionary (the new one for push() and update(), the one removed for pop()). Based on a patch from Brian Harring.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@6854 bcc190cf-cafb-0310-a4f2-bffc1f526a37

---
+++ tests/regressiontests/templates/context.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+context_tests = r"""
+>>> from django.template import Context
+>>> c = Context({'a': 1, 'b': 'xyzzy'})
+>>> c['a']
+1
+>>> c.push()
+{}
+>>> c['a'] = 2
+>>> c['a']
+2
+>>> c.pop()
+{'a': 2}
+>>> c['a']
+1
+"""
+
