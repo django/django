b53fac80525ef656dddaa0e88ade258df6f4a8b8
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2007-12-17 11:09:56 +0000

    Fixed [6932] so that "django-admin.py help dumpdata" works again.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@6937 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/core/management/commands/dumpdata.py
+++ django/core/management/commands/dumpdata.py
@@ -4,10 +4,9 @@ from django.core import serializers
 from optparse import make_option
 
 class Command(BaseCommand):
-    serializer_formats = serializers.get_public_serializer_formats()
     option_list = BaseCommand.option_list + (
         make_option('--format', default='json', dest='format',
-            help='Specifies the output serialization format for fixtures.  Formats available: %s' % serializer_formats),
+            help='Specifies the output serialization format for fixtures.'),
         make_option('--indent', default=None, dest='indent', type='int',
             help='Specifies the indent level to use when pretty-printing output'),
     )
@@ -28,7 +27,7 @@ class Command(BaseCommand):
 
         # Check that the serialization format exists; this is a shortcut to
         # avoid collating all the objects and _then_ failing.
-        if format not in self.serializer_formats:
+        if format not in serializers.get_public_serializer_formats():
             raise CommandError("Unknown serialization format: %s" % format)
 
         try:
