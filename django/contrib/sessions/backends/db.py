041e24dbde48a38e539f85c378842849f7dc00a1
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2008-01-06 12:53:09 +0000

    Fixed a subtle corner case whereby sending a bad session ID generates new (unused) session entries in the database table.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@7001 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/sessions/backends/db.py
+++ django/contrib/sessions/backends/db.py
@@ -10,38 +10,40 @@ class SessionStore(SessionBase):
     """
     def __init__(self, session_key=None):
         super(SessionStore, self).__init__(session_key)
-    
+
     def load(self):
         try:
             s = Session.objects.get(
-                session_key = self.session_key, 
+                session_key = self.session_key,
                 expire_date__gt=datetime.datetime.now()
             )
             return self.decode(s.session_data)
         except (Session.DoesNotExist, SuspiciousOperation):
-            
+
             # Create a new session_key for extra security.
             self.session_key = self._get_new_session_key()
             self._session_cache = {}
 
             # Save immediately to minimize collision
             self.save()
+            # Ensure the user is notified via a new cookie.
+            self.modified = True
             return {}
-            
+
     def exists(self, session_key):
         try:
             Session.objects.get(session_key=session_key)
         except Session.DoesNotExist:
             return False
         return True
-            
+
     def save(self):
         Session.objects.create(
             session_key = self.session_key,
             session_data = self.encode(self._session),
             expire_date = datetime.datetime.now() + datetime.timedelta(seconds=settings.SESSION_COOKIE_AGE)
         )
-    
+
     def delete(self, session_key):
         try:
             Session.objects.get(session_key=session_key).delete()
