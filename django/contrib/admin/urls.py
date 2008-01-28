a01ebb4a5095f69f8e4884598012e1667b13d0cf
Gary Wilson Jr <gary.wilson@gmail.com>
2008-01-28 05:36:44 +0000

    Fixed #6354 -- Fixed url in `django/conf/urls/shortcut.py` to work with non-integer primary keys (refs #2033) and made use of the URL file in the admin urls.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@7040 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/admin/urls.py
+++ django/contrib/admin/urls.py
@@ -8,7 +8,7 @@ else:
 
 urlpatterns = patterns('',
     ('^$', 'django.contrib.admin.views.main.index'),
-    ('^r/(\d+)/(.*)/$', 'django.views.defaults.shortcut'),
+    ('^r/', include('django.conf.urls.shortcut')),
     ('^jsi18n/$', i18n_view, {'packages': 'django.conf'}),
     ('^logout/$', 'django.contrib.auth.views.logout'),
     ('^password_change/$', 'django.contrib.auth.views.password_change'),
