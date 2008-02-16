770d587314fb0275f0570b05ee5bc746c2b2c685
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2008-02-16 06:56:10 +0000

    queryset-refactor: Two files were missed during the latest merge from trunk.
    git-svn-id: http://code.djangoproject.com/svn/django/branches/queryset-refactor@7123 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/template/loader_tags.py
+++ django/template/loader_tags.py
@@ -69,14 +69,10 @@ class ExtendsNode(Node):
 
     def render(self, context):
         compiled_parent = self.get_parent(context)
-        if len(compiled_parent.nodelist) > 1:
-            n0, n1 = compiled_parent.nodelist[:2]
-        else:
-            n0, n1 = compiled_parent.nodelist[0], None
-        parent_is_child = (isinstance(n0, ExtendsNode) or
-                (isinstance(n0, TextNode) and isinstance(n1, ExtendsNode)))
-        if parent_is_child:
-            extend_node = int(not isinstance(n0, ExtendsNode))
+        pos = 0
+        while isinstance(compiled_parent.nodelist[pos], TextNode):
+            pos += 1
+        parent_is_child = isinstance(compiled_parent.nodelist[pos], ExtendsNode)
         parent_blocks = dict([(n.name, n) for n in compiled_parent.nodelist.get_nodes_by_type(BlockNode)])
         for block_node in self.nodelist.get_nodes_by_type(BlockNode):
             # Check for a BlockNode with this node's name, and replace it if found.
@@ -88,7 +84,7 @@ class ExtendsNode(Node):
                 # add this BlockNode to the parent's ExtendsNode nodelist, so
                 # it'll be checked when the parent node's render() is called.
                 if parent_is_child:
-                    compiled_parent.nodelist[extend_node].nodelist.append(block_node)
+                    compiled_parent.nodelist[pos].nodelist.append(block_node)
             else:
                 # Keep any existing parents and add a new one. Used by BlockNode.
                 parent_block.parent = block_node.parent
