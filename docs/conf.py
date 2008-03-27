4c3fcbc4bc901db6f3b92a33bccd45fe069f9617
Jacob Kaplan-Moss <jacob@jacobian.org>
2008-03-27 15:14:57 +0000

    [docs] OK, I give up.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@7374 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ copyright = '2008, Django Software Foundation'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '$Revision$'.split()[1]
+version = 'SVN'
 # The full version, including alpha/beta/rc tags.
 release = version
 
