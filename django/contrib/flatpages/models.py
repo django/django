3a662052ecd4ec7099a264b9aad5498f4b8953ad
Gary Wilson Jr <gary.wilson@gmail.com>
2008-03-29 16:42:39 +0000

    Django code styling fixes.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@7382 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/flatpages/models.py
+++ django/contrib/flatpages/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.sites.models import Site
 from django.utils.translation import ugettext_lazy as _
 
+
 class FlatPage(models.Model):
     url = models.CharField(_('URL'), max_length=100, validator_list=[validators.isAlphaNumericURL], db_index=True,
         help_text=_("Example: '/about/contact/'. Make sure to have leading and trailing slashes."))
@@ -13,11 +14,13 @@ class FlatPage(models.Model):
         help_text=_("Example: 'flatpages/contact_page.html'. If this isn't provided, the system will use 'flatpages/default.html'."))
     registration_required = models.BooleanField(_('registration required'), help_text=_("If this is checked, only logged-in users will be able to view the page."))
     sites = models.ManyToManyField(Site)
+
     class Meta:
         db_table = 'django_flatpage'
         verbose_name = _('flat page')
         verbose_name_plural = _('flat pages')
         ordering = ('url',)
+
     class Admin:
         fields = (
             (None, {'fields': ('url', 'title', 'content', 'sites')}),
