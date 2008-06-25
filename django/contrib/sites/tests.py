87a704cf50d4ac122b89a02f5a68f11ad5026524
Russell Keith-Magee <russell@keith-magee.com>
2008-06-25 12:09:18 +0000

    Fixed #7531 -- Modified the sites test so that user-provided initial data doesn't cause breakage when executed from within user applications. Thanks for the report, madkinder@gmail.com.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@7738 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/sites/tests.py
+++ django/contrib/sites/tests.py
@@ -2,8 +2,8 @@
 >>> # Make sure that get_current() does not return a deleted Site object.
 >>> from django.contrib.sites.models import Site
 >>> s = Site.objects.get_current()
->>> s
-<Site: example.com>
+>>> isinstance(s, Site)
+True
 
 >>> s.delete()
 >>> Site.objects.get_current()
