825622d912aa8e87ddcda1af65edef9bb765e389
Russell Keith-Magee <russell@keith-magee.com>
2008-07-19 02:38:41 +0000

    Fixed #7773 -- Added some simple tests for EmailMessage. Thanks to serialx for the patch.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@7975 bcc190cf-cafb-0310-a4f2-bffc1f526a37

---
+++ tests/regressiontests/mail/tests.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+r"""
+# Tests for the django.core.mail.
+
+>>> from django.core.mail import EmailMessage
+
+# Test normal ascii character case:
+
+>>> email = EmailMessage('Subject', 'Content', 'from@example.com', ['to@example.com'])
+>>> message = email.message()
+>>> message['Subject']
+'Subject'
+>>> message.get_payload()
+'Content'
+>>> message['From']
+'from@example.com'
+>>> message['To']
+'to@example.com'
+
+# Test multiple-recipient case
+
+>>> email = EmailMessage('Subject', 'Content', 'from@example.com', ['to@example.com','other@example.com'])
+>>> message = email.message()
+>>> message['Subject']
+'Subject'
+>>> message.get_payload()
+'Content'
+>>> message['From']
+'from@example.com'
+>>> message['To']
+'to@example.com, other@example.com'
+
+# Test for header injection
+
+>>> email = EmailMessage('Subject\nInjection Test', 'Content', 'from@example.com', ['to@example.com'])
+>>> message = email.message()
+Traceback (most recent call last):
+    ...
+BadHeaderError: Header values can't contain newlines (got 'Subject\nInjection Test' for header 'Subject')
+
+"""
