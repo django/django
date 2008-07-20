effa2993427cd49c40acc3982f658c0d4f78c4b5
Brian Rosner <brosner@gmail.com>
2008-07-20 18:09:27 +0000

    Added admin.autodiscover() to project template urls.py. It is left commented out intentionally. Thanks tome for the suggestion. Refs #7824.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@8009 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/project_template/urls.py
+++ django/conf/project_template/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls.defaults import *
 # Uncomment this for admin:
 # from django.contrib import admin
 
+# Uncomment to load INSTALLED_APPS admin.py module for default AdminSite instance.
+# admin.autodiscover()
+
 urlpatterns = patterns('',
     # Example:
     # (r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
