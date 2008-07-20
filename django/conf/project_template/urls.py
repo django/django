a53a1ba5419422e8611caefbab13bd4db8c53bc3
Brian Rosner <brosner@gmail.com>
2008-07-20 18:05:11 +0000

    Fixed #7842 -- Added a raw string identifier in project template urls.py for consistency sake. Added spaces for consistency too. Thanks yoshi.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@8008 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/project_template/urls.py
+++ django/conf/project_template/urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls.defaults import *
 
 # Uncomment this for admin:
-#from django.contrib import admin
+# from django.contrib import admin
 
 urlpatterns = patterns('',
     # Example:
     # (r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
 
     # Uncomment this for admin docs:
-    #(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment this for admin:
-    #('^admin/(.*)', admin.site.root),
+    # (r'^admin/(.*)', admin.site.root),
 )
