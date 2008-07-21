f3ec0c18b170074dadb8d21375b1f080b18dc54e
Jacob Kaplan-Moss <jacob@jacobian.org>
2008-07-21 17:10:27 +0000

    Modified the comments in the skeleton urls.py to be more clear about how to enable the admin. Refs #7824.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@8026 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/project_template/urls.py
+++ django/conf/project_template/urls.py
@@ -1,18 +1,16 @@
 from django.conf.urls.defaults import *
 
-# Uncomment this for admin:
+# Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
-
-# Uncomment to load INSTALLED_APPS admin.py module for default AdminSite instance.
 # admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
     # (r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
 
-    # Uncomment this for admin docs:
+    # Uncomment the next line to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment this for admin:
+    # Uncomment the next line for to enable the admin:
     # (r'^admin/(.*)', admin.site.root),
 )
