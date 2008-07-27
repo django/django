4774c8d6739da02a7890f1ff80a87b6ca87821a7
Gary Wilson Jr <gary.wilson@gmail.com>
2008-07-27 17:25:37 +0000

    Corrected spacing in warning message.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@8106 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/views/generic/create_update.py
+++ django/views/generic/create_update.py
@@ -17,8 +17,8 @@ def deprecate_follow(follow):
     if follow is not None:
         import warnings
         msg = ("Generic views have been changed to use newforms, and the"
-               "'follow' argument is no longer used.  Please update your code"
-               "to not use the 'follow' argument.")
+               " 'follow' argument is no longer used.  Please update your code"
+               " to not use the 'follow' argument.")
         warnings.warn(msg, DeprecationWarning, stacklevel=3)
 
 def apply_extra_context(extra_context, context):
