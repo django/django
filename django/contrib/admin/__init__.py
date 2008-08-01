525e1e379af20e53819b3b34b27380eb27ca0ac8
Brian Rosner <brosner@gmail.com>
2008-08-01 19:10:55 +0000

    Fixed #7922 -- admin.autodiscover() no longer eats ImportErrors for breakfast. Thanks Jan Rademaker and Alex Gaynor for their work on the ticket.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@8174 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/admin/__init__.py
+++ django/contrib/admin/__init__.py
@@ -8,9 +8,12 @@ def autodiscover():
     not present. This forces an import on them to register any admin bits they
     may want.
     """
+    import imp
     from django.conf import settings
     for app in settings.INSTALLED_APPS:
         try:
-            __import__("%s.admin" % app)
+            imp.find_module("admin", app.split("."))
         except ImportError:
-            pass
+            # there is no admin.py in app, skip it.
+            continue
+        __import__("%s.admin" % app)
