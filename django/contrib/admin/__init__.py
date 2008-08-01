85af2641125dfdd141f744f37be616499108ace9
Brian Rosner <brosner@gmail.com>
2008-08-01 22:46:15 +0000

    Reverted [8174]. It is causing some serious breakage right now. Need to investigate more.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@8183 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/admin/__init__.py
+++ django/contrib/admin/__init__.py
@@ -8,12 +8,9 @@ def autodiscover():
     not present. This forces an import on them to register any admin bits they
     may want.
     """
-    import imp
     from django.conf import settings
     for app in settings.INSTALLED_APPS:
         try:
-            imp.find_module("admin", app.split("."))
+            __import__("%s.admin" % app)
         except ImportError:
-            # there is no admin.py in app, skip it.
-            continue
-        __import__("%s.admin" % app)
+            pass
