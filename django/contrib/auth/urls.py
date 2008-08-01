64f5ab67a57f7373cee2cc675ae03d74e878b645
Luke Plant <L.Plant.98@cantab.net>
2008-08-01 16:18:24 +0000

    Changed some URL conf regexes to rawstrings for consistency
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@8172 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/auth/urls.py
+++ django/contrib/auth/urls.py
@@ -5,12 +5,12 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    ('^logout/$', 'django.contrib.auth.views.logout'),
-    ('^password_change/$', 'django.contrib.auth.views.password_change'),
-    ('^password_change/done/$', 'django.contrib.auth.views.password_change_done'),
-    ('^password_reset/$', 'django.contrib.auth.views.password_reset'),
-    ('^password_reset/done/$', 'django.contrib.auth.views.password_reset_done'),
-    ('^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm'),
-    ('^reset/done/$', 'django.contrib.auth.views.password_reset_complete'),
+    (r'^logout/$', 'django.contrib.auth.views.logout'),
+    (r'^password_change/$', 'django.contrib.auth.views.password_change'),
+    (r'^password_change/done/$', 'django.contrib.auth.views.password_change_done'),
+    (r'^password_reset/$', 'django.contrib.auth.views.password_reset'),
+    (r'^password_reset/done/$', 'django.contrib.auth.views.password_reset_done'),
+    (r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm'),
+    (r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete'),
 )
 
