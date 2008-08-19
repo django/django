e7769c36e9a95312879e74c2dd32695d3165037b
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2008-08-19 20:35:56 +0000

    Fixed #8410 -- Added a missing piece of value encoding for the memcached backend. Patch from trbs.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@8444 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/core/cache/backends/memcached.py
+++ django/core/cache/backends/memcached.py
@@ -17,7 +17,9 @@ class CacheClass(BaseCache):
         self._cache = memcache.Client(server.split(';'))
 
     def add(self, key, value, timeout=0):
-        return self._cache.add(key.encode('ascii', 'ignore'), value, timeout or self.default_timeout)
+        if isinstance(value, unicode):
+            value = value.encode('utf-8')
+        return self._cache.add(smart_str(key), value, timeout or self.default_timeout)
 
     def get(self, key, default=None):
         val = self._cache.get(smart_str(key))
