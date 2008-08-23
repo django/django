64ca36bf0832d9d72fc26293f3aebed7d8b24a52
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2008-08-23 18:01:16 +0000

    Fixed #8214 -- Added back a stringification that was dropped during [8131]. This restores filtering on dates in the admin, for example. Thanks to akfarrell and a small team of workers at the Portland Sprint.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@8494 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/db/backends/sqlite3/base.py
+++ django/db/backends/sqlite3/base.py
@@ -182,7 +182,7 @@ def _sqlite_extract(lookup_type, dt):
         dt = util.typecast_timestamp(dt)
     except (ValueError, TypeError):
         return None
-    return getattr(dt, lookup_type)
+    return unicode(getattr(dt, lookup_type))
 
 def _sqlite_date_trunc(lookup_type, dt):
     try:
