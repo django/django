640f198111fc1e770700a938c43e6df2eff300ca
Adrian Holovaty <adrian@holovaty.com>
2008-09-09 02:18:06 +0000

    Fixed #8978 -- We now print a helpful error message for 'manage.py dbshell' if the client executable is not found. Previously we were displaying a traceback
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@8990 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/core/management/commands/dbshell.py
+++ django/core/management/commands/dbshell.py
@@ -1,4 +1,4 @@
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import NoArgsCommand, CommandError
 
 class Command(NoArgsCommand):
     help = "Runs the command-line client for the current DATABASE_ENGINE."
@@ -7,4 +7,12 @@ class Command(NoArgsCommand):
 
     def handle_noargs(self, **options):
         from django.db import connection
-        connection.client.runshell()
+        try:
+            connection.client.runshell()
+        except OSError:
+            # Note that we're assuming OSError means that the client program
+            # isn't installed. There's a possibility OSError would be raised
+            # for some other reason, in which case this error message would be
+            # inaccurate. Still, this message catches the common case.
+            raise CommandError('You appear not to have the %r program installed or on your path.' % \
+                connection.client.executable_name)
