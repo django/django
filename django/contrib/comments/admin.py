b79cb53d766993a8a424fbd164bc1302ce18e767
Wilson Miner <wminer@gmail.com>
2008-09-16 00:25:21 +0000

    Fixed #8917 -- Comments are now displayed in reverse order by submit_date in the admin. Thanks to arien for the patch.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@9038 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/comments/admin.py
+++ django/contrib/comments/admin.py
@@ -16,9 +16,10 @@ class CommentsAdmin(admin.ModelAdmin):
         ),
      )
 
-    list_display = ('name', 'content_type', 'object_pk', 'ip_address', 'is_public', 'is_removed')
+    list_display = ('name', 'content_type', 'object_pk', 'ip_address', 'submit_date', 'is_public', 'is_removed')
     list_filter = ('submit_date', 'site', 'is_public', 'is_removed')
     date_hierarchy = 'submit_date'
+    ordering = ('-submit_date',)
     search_fields = ('comment', 'user__username', 'user_name', 'user_email', 'user_url', 'ip_address')
 
 admin.site.register(Comment, CommentsAdmin)
