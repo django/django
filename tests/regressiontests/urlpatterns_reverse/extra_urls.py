b17453bbc97bace0ce0a875d217663c3af801677
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2008-09-30 05:57:13 +0000

    [1.0.X] Fixed another case of reverse URL resolving that wasn't working.
    This is a similar situation to that fixed in r9087. We weren't merging
    multiple levels of include() calls together correctly.
    
    Backported from r9099 on trunk.
    
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.0.X@9100 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/urlpatterns_reverse/extra_urls.py
+++ tests/regressiontests/urlpatterns_reverse/extra_urls.py
@@ -8,4 +8,6 @@ from views import empty_view
 urlpatterns = patterns('',
     url(r'^e-places/(\d+)/$', empty_view, name='extra-places'),
     url(r'^e-people/(?P<name>\w+)/$', empty_view, name="extra-people"),
+    url('', include('regressiontests.urlpatterns_reverse.included_urls2')),
+    url(r'^prefix/(?P<prefix>\w+)/', include('regressiontests.urlpatterns_reverse.included_urls2')),
 )
---
+++ tests/regressiontests/urlpatterns_reverse/included_urls2.py
@@ -0,0 +1,14 @@
+"""
+These URL patterns are included in two different ways in the main urls.py, with
+an extra argument present in one case. Thus, there are two different ways for
+each name to resolve and Django must distinguish the possibilities based on the
+argument list.
+"""
+
+from django.conf.urls.defaults import *
+from views import empty_view
+
+urlpatterns = patterns('',
+    url(r'^part/(?P<value>\w+)/$', empty_view, name="part"),
+    url(r'^part2/(?:(?P<value>\w+)/)?$', empty_view, name="part2"),
+)
