ba592950689a06893c77bc97040770f89dd5a317
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2008-09-30 03:58:09 +0000

    Fixed #9221 -- Small optimisation to caching middleware handling.
    In the slightly unusual case that CACHE_MIDDLEWARE_SECONDS is set to 0, don't
    bother storing a copy in the local cache.
    
    
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@9098 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/middleware/cache.py
+++ django/middleware/cache.py
@@ -89,8 +89,9 @@ class UpdateCacheMiddleware(object):
             # max-age was set to 0, don't bother caching.
             return response
         patch_response_headers(response, timeout)
-        cache_key = learn_cache_key(request, response, timeout, self.key_prefix)
-        cache.set(cache_key, response, timeout)
+        if timeout:
+            cache_key = learn_cache_key(request, response, timeout, self.key_prefix)
+            cache.set(cache_key, response, timeout)
         return response
 
 class FetchFromCacheMiddleware(object):
