8fb1459b5294fb9327b241fffec8576c5aa3fc7e
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2008-10-06 06:34:54 +0000

    Added some better error reporting and path handling when creating template paths.
    We now raise UnicodeDecodeError for non-UTF-8 bytestrings (thanks to Daniel
    Pope for diagnosing this was being swallowed by ValueError) and allow UTF-8
    bytestrings as template directories.
    
    Refs #8965.
    
    
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@9161 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/template/loaders/filesystem.py
+++ django/template/loaders/filesystem.py
@@ -7,13 +7,23 @@ from django.template import TemplateDoesNotExist
 from django.utils._os import safe_join
 
 def get_template_sources(template_name, template_dirs=None):
+    """
+    Returns the absolute paths to "template_name", when appended to each
+    directory in "template_dirs". Any paths that don't lie inside one of the
+    template dirs are excluded from the result set, for security reasons.
+    """
     if not template_dirs:
         template_dirs = settings.TEMPLATE_DIRS
     for template_dir in template_dirs:
         try:
             yield safe_join(template_dir, template_name)
+        except UnicodeDecodeError:
+            # The template dir name was a bytestring that wasn't valid UTF-8.
+            raise
         except ValueError:
-            # The joined path was located outside of template_dir.
+            # The joined path was located outside of this particular
+            # template_dir (it might be inside another one, so this isn't
+            # fatal).
             pass
 
 def load_template_source(template_name, template_dirs=None):
