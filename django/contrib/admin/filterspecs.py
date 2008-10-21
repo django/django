118383c6071a55683a1789dcd6f80bf979ac83da
Karen Tracey <kmtracey@gmail.com>
2008-10-21 19:06:44 +0000

    [1.0.X] Fixed #3096 -- Make admin list_filters respect limit_choices_to.
    Backport of [9241] from trunk.
    
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.0.X@9242 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/admin/filterspecs.py
+++ django/contrib/admin/filterspecs.py
@@ -60,7 +60,7 @@ class RelatedFilterSpec(FilterSpec):
             self.lookup_title = f.verbose_name
         self.lookup_kwarg = '%s__%s__exact' % (f.name, f.rel.to._meta.pk.name)
         self.lookup_val = request.GET.get(self.lookup_kwarg, None)
-        self.lookup_choices = f.rel.to._default_manager.all()
+        self.lookup_choices = f.get_choices(include_blank=False)
 
     def has_output(self):
         return len(self.lookup_choices) > 1
@@ -72,8 +72,7 @@ class RelatedFilterSpec(FilterSpec):
         yield {'selected': self.lookup_val is None,
                'query_string': cl.get_query_string({}, [self.lookup_kwarg]),
                'display': _('All')}
-        for val in self.lookup_choices:
-            pk_val = getattr(val, self.field.rel.to._meta.pk.attname)
+        for pk_val, val in self.lookup_choices:
             yield {'selected': self.lookup_val == smart_unicode(pk_val),
                    'query_string': cl.get_query_string({self.lookup_kwarg: pk_val}),
                    'display': val}
