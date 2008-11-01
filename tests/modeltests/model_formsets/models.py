6c069d2a71e19955b34a9c5aecc4d2075a5d430a
Brian Rosner <brosner@gmail.com>
2008-11-01 22:26:44 +0000

    [1.0.X] Fixed #9494 -- Ensure the foreign key in an inline formset is always present on the forms. Thanks Fugazi for the report.
    Backport of r9326 from trunk
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.0.X@9327 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/modeltests/model_formsets/models.py
+++ tests/modeltests/model_formsets/models.py
@@ -696,6 +696,21 @@ False
 >>> formset.errors
 [{'__all__': [u'Revision with this Repository and Revision already exists.']}]
 
+# unique_together with inlineformset_factory with overridden form fields
+# Also see #9494
+
+>>> FormSet = inlineformset_factory(Repository, Revision, fields=('revision',), extra=1)
+>>> data = {
+...     'revision_set-TOTAL_FORMS': '1',
+...     'revision_set-INITIAL_FORMS': '0',
+...     'revision_set-0-repository': repository.pk,
+...     'revision_set-0-revision': '146239817507f148d448db38840db7c3cbf47c76',
+...     'revision_set-0-DELETE': '',
+... }
+>>> formset = FormSet(data, instance=repository)
+>>> formset.is_valid()
+False
+
 # Use of callable defaults (see bug #7975).
 
 >>> person = Person.objects.create(name='Ringo')
