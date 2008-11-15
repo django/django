781a1763d94c7e867d9ba80b3f075526510a3482
Brian Rosner <brosner@gmail.com>
2008-11-15 02:21:32 +0000

    [1.0.X] Fixed #9258 -- Use _default_manager in ForeignKeyRawIdWidget.label_for_value. Thanks nullie for the patch.
    Backport of r9444 from trunk.
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.0.X@9445 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/admin/widgets.py
+++ django/contrib/admin/widgets.py
@@ -146,7 +146,7 @@ class ForeignKeyRawIdWidget(forms.TextInput):
             
     def label_for_value(self, value):
         key = self.rel.get_related_field().name
-        obj = self.rel.to.objects.get(**{key: value})
+        obj = self.rel.to._default_manager.get(**{key: value})
         return '&nbsp;<strong>%s</strong>' % truncate_words(obj, 14)
 
 class ManyToManyRawIdWidget(ForeignKeyRawIdWidget):
