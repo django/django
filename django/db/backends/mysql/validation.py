01e04e90e0f69e3b350301015f17a6cb4aa7d39a
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2008-12-16 06:49:43 +0000

    [1.0.X] Fixed #9431 -- Added extra validation for VARCHAR-based fields on MySQL. max_length > 255 and unique=True is not permitted. Based on a patch from adamnelson.
    Backport of r9470 from trunk.
    
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.0.X@9652 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/db/backends/mysql/validation.py
+++ django/db/backends/mysql/validation.py
@@ -2,12 +2,27 @@ from django.db.backends import BaseDatabaseValidation
 
 class DatabaseValidation(BaseDatabaseValidation):
     def validate_field(self, errors, opts, f):
-        "Prior to MySQL 5.0.3, character fields could not exceed 255 characters"
+        """
+        There are some field length restrictions for MySQL:
+
+        - Prior to version 5.0.3, character fields could not exceed 255
+          characters in length.
+        - No character (varchar) fields can have a length exceeding 255
+          characters if they have a unique index on them.
+        """
         from django.db import models
         from django.db import connection
         db_version = connection.get_server_version()
-        if db_version < (5, 0, 3) and isinstance(f, (models.CharField, models.CommaSeparatedIntegerField, models.SlugField)) and f.max_length > 255:
-            errors.add(opts,
-                '"%s": %s cannot have a "max_length" greater than 255 when you are using a version of MySQL prior to 5.0.3 (you are using %s).' % 
-                (f.name, f.__class__.__name__, '.'.join([str(n) for n in db_version[:3]])))
-    
+        varchar_fields = (models.CharField, models.CommaSeparatedIntegerField,
+                models.SlugField)
+        if isinstance(f, varchar_fields) and f.max_length > 255:
+            if db_version < (5, 0, 3):
+                msg = '"%(name)s": %(cls)s cannot have a "max_length" greater than 255 when you are using a version of MySQL prior to 5.0.3 (you are using %(version)s).'
+            if f.unique == True:
+                msg = '"%(name)s": %(cls)s cannot have a "max_length" greater than 255 when using "unique=True".'
+            else:
+                msg = None
+
+            if msg:
+                errors.add(opts, msg % {'name': f.name, 'cls': f.__class__.__name__, 'version': '.'.join([str(n) for n in db_version[:3]])})
+
