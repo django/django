e77c4cca26050ee599dfdd78c600b10b0d0b74ec
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2009-01-06 05:27:39 +0000

    Fixed #9963 - Added a trivial tests.py file to the files produced by "startapp".
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@9708 bcc190cf-cafb-0310-a4f2-bffc1f526a37

---
+++ django/conf/app_template/tests.py
@@ -0,0 +1,23 @@
+"""
+This file demonstrates two different styles of tests (one doctest and one
+unittest). These will both pass when you run "manage.py test".
+
+Replace these with more appropriate tests for your application.
+"""
+
+from django.test import TestCase
+
+class SimpleTest(TestCase):
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.failUnlessEqual(1 + 1, 2)
+
+__test__ = {"doctest": """
+Another way to test that 1 + 1 is equal to 2.
+
+>>> 1 + 1 == 2
+True
+"""}
+
