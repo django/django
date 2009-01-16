b120c74032c1131e46e4073db1298cf92c4e34c3
Matt Boersma <matt@sprout.org>
2009-01-16 21:18:21 +0000

    Changed "date" column to "start_date"--Oracle won't name columns with reserved keywords.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@9764 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/admin_widgets/tests.py
+++ tests/regressiontests/admin_widgets/tests.py
@@ -19,89 +19,89 @@ class AdminFormfieldForDBFieldTests(TestCase):
         class MyModelAdmin(admin.ModelAdmin): pass
         for k in admin_overrides:
             setattr(MyModelAdmin, k, admin_overrides[k])
-        
+
         # Construct the admin, and ask it for a formfield
         ma = MyModelAdmin(model, admin.site)
         ff = ma.formfield_for_dbfield(model._meta.get_field(fieldname), request=None)
-        
+
         # "unwrap" the widget wrapper, if needed
         if isinstance(ff.widget, widgets.RelatedFieldWidgetWrapper):
             widget = ff.widget.widget
         else:
             widget = ff.widget
-        
+
         # Check that we got a field of the right type
         self.assert_(
-            isinstance(widget, widgetclass), 
+            isinstance(widget, widgetclass),
             "Wrong widget for %s.%s: expected %s, got %s" % \
                 (model.__class__.__name__, fieldname, widgetclass, type(widget))
         )
-            
+
         # Return the formfield so that other tests can continue
         return ff
-    
+
     def testDateField(self):
-        self.assertFormfield(models.Event, 'date', widgets.AdminDateWidget)
-        
+        self.assertFormfield(models.Event, 'start_date', widgets.AdminDateWidget)
+
     def testDateTimeField(self):
         self.assertFormfield(models.Member, 'birthdate', widgets.AdminSplitDateTime)
-        
+
     def testTimeField(self):
         self.assertFormfield(models.Event, 'start_time', widgets.AdminTimeWidget)
 
     def testTextField(self):
         self.assertFormfield(models.Event, 'description', widgets.AdminTextareaWidget)
-    
+
     def testURLField(self):
         self.assertFormfield(models.Event, 'link', widgets.AdminURLFieldWidget)
 
     def testIntegerField(self):
         self.assertFormfield(models.Event, 'min_age', widgets.AdminIntegerFieldWidget)
-        
+
     def testCharField(self):
         self.assertFormfield(models.Member, 'name', widgets.AdminTextInputWidget)
-        
+
     def testFileField(self):
         self.assertFormfield(models.Album, 'cover_art', widgets.AdminFileWidget)
-        
+
     def testForeignKey(self):
         self.assertFormfield(models.Event, 'band', forms.Select)
-        
+
     def testRawIDForeignKey(self):
         self.assertFormfield(models.Event, 'band', widgets.ForeignKeyRawIdWidget,
                              raw_id_fields=['band'])
-    
+
     def testRadioFieldsForeignKey(self):
-        ff = self.assertFormfield(models.Event, 'band', widgets.AdminRadioSelect, 
+        ff = self.assertFormfield(models.Event, 'band', widgets.AdminRadioSelect,
                                   radio_fields={'band':admin.VERTICAL})
         self.assertEqual(ff.empty_label, None)
-        
+
     def testManyToMany(self):
         self.assertFormfield(models.Band, 'members', forms.SelectMultiple)
-    
+
     def testRawIDManyTOMany(self):
         self.assertFormfield(models.Band, 'members', widgets.ManyToManyRawIdWidget,
                              raw_id_fields=['members'])
-    
+
     def testFilteredManyToMany(self):
         self.assertFormfield(models.Band, 'members', widgets.FilteredSelectMultiple,
                              filter_vertical=['members'])
-    
+
     def testFormfieldOverrides(self):
-        self.assertFormfield(models.Event, 'date', forms.TextInput, 
+        self.assertFormfield(models.Event, 'start_date', forms.TextInput,
                              formfield_overrides={'widget': forms.TextInput})
-                             
+
     def testFieldWithChoices(self):
         self.assertFormfield(models.Member, 'gender', forms.Select)
-        
+
     def testChoicesWithRadioFields(self):
-        self.assertFormfield(models.Member, 'gender', widgets.AdminRadioSelect, 
+        self.assertFormfield(models.Member, 'gender', widgets.AdminRadioSelect,
                              radio_fields={'gender':admin.VERTICAL})
 
 
 class AdminFormfieldForDBFieldWithRequestTests(DjangoTestCase):
     fixtures = ["admin-widgets-users.xml"]
-    
+
     def testFilterChoicesByRequestUser(self):
         """
         Ensure the user can only see their own cars in the foreign key dropdown.
