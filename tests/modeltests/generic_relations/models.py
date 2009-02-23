93b3f89c312ad0f744e8351c27bd96a57ea5f284
Brian Rosner <brosner@gmail.com>
2009-02-23 04:31:29 +0000

    [1.0.X] Fixed #10274 -- Fixed an embarrassing mistake in BaseGenericInlineFormSet.get_queryset. Thanks Zak Johnson for the report and akaihola for the patch.
    Backport of r9886 from trunk.
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.0.X@9887 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/modeltests/generic_relations/models.py
+++ tests/modeltests/generic_relations/models.py
@@ -219,6 +219,11 @@ __test__ = {'API_TESTS':"""
 >>> from django.contrib.contenttypes.generic import generic_inlineformset_factory
 
 >>> GenericFormSet = generic_inlineformset_factory(TaggedItem, extra=1)
+>>> formset = GenericFormSet()
+>>> for form in formset.forms:
+...     print form.as_p()
+<p><label for="id_generic_relations-taggeditem-content_type-object_id-0-tag">Tag:</label> <input id="id_generic_relations-taggeditem-content_type-object_id-0-tag" type="text" name="generic_relations-taggeditem-content_type-object_id-0-tag" maxlength="50" /></p> 
+<p><label for="id_generic_relations-taggeditem-content_type-object_id-0-DELETE">Delete:</label> <input type="checkbox" name="generic_relations-taggeditem-content_type-object_id-0-DELETE" id="id_generic_relations-taggeditem-content_type-object_id-0-DELETE" /><input type="hidden" name="generic_relations-taggeditem-content_type-object_id-0-id" id="id_generic_relations-taggeditem-content_type-object_id-0-id" /></p>
 >>> formset = GenericFormSet(instance=Animal())
 >>> for form in formset.forms:
 ...     print form.as_p()
