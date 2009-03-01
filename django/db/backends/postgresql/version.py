70911bf98d5df4125c86a6c9e21e1df635979f81
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2009-03-01 07:18:57 +0000

    Fixed #8737 -- Made the PostgreSQL version number detection more flexible.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@9931 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/db/backends/postgresql/version.py
+++ django/db/backends/postgresql/version.py
@@ -4,7 +4,12 @@ Extracts the version of the PostgreSQL server.
 
 import re
 
-VERSION_RE = re.compile(r'PostgreSQL (\d+)\.(\d+)\.')
+# This reg-exp is intentionally fairly flexible here. Require only the major
+# and minor version numbers, but need to be able to handle standard stuff like:
+#   PostgreSQL 8.3.6
+#   EnterpriseDB 8.3
+#   PostgreSQL 8.3 beta4
+VERSION_RE = re.compile(r'\S+ (\d+)\.(\d+)')
 
 def get_version(cursor):
     """
