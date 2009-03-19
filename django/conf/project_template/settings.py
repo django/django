2d287247308ea5e40214f4564b91b633959902a9
Luke Plant <L.Plant.98@cantab.net>
2009-03-19 23:14:20 +0000

    Added CSRF middleware to default settings and updated docs.
    Updated docs to reflect the change, and the fact that using the
    two separate middleware is preferred to using the combined one.
    
    
    
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@10094 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/project_template/settings.py
+++ django/conf/project_template/settings.py
@@ -59,6 +59,8 @@ TEMPLATE_LOADERS = (
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
+    'django.contrib.csrf.middleware.CsrfViewMiddleware',
+    'django.contrib.csrf.middleware.CsrfResponseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
