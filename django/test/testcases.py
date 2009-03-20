420a35b9b752f5030c1c01616f41601812250aad
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2009-03-20 01:37:34 +0000

    Python 2.3 fix: assertTrue *still* doesn't exist in Python 2.3
    The tests and testing framework should use failUnless() instead.
    
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@10100 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/test/testcases.py
+++ django/test/testcases.py
@@ -278,7 +278,7 @@ class TransactionTestCase(unittest.TestCase):
         """
         if hasattr(response, 'redirect_chain'):
             # The request was a followed redirect
-            self.assertTrue(len(response.redirect_chain) > 0,
+            self.failUnless(len(response.redirect_chain) > 0,
                 ("Response didn't redirect as expected: Response code was %d"
                 " (expected %d)" % (response.status_code, status_code)))
 
