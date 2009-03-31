b3b541b68ea83164aee967539e4aa44cbdb88b6e
Gary Wilson Jr <gary.wilson@gmail.com>
2009-03-31 16:09:22 +0000

    [1.0.X]: Fixed #10553 -- Corrected several uses of `URLconf` in documentation and comments, according to the Django style guide.  Based on patch from rduffield.
    Backport of r10256 from trunk.
    
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.0.X@10257 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/auth/tests/views.py
+++ django/contrib/auth/tests/views.py
@@ -47,8 +47,8 @@ class PasswordResetTest(TestCase):
 
     def test_confirm_invalid(self):
         url, path = self._test_confirm_start()
-        # Lets munge the token in the path, but keep the same length,
-        # in case the URL conf will reject a different length
+        # Let's munge the token in the path, but keep the same length,
+        # in case the URLconf will reject a different length.
         path = path[:-5] + ("0"*4) + path[-1]
 
         response = self.client.get(path)
