afcb3c887290fb4df73e72780f864b1ec82a2ac2
Honza Král <honza.kral@gmail.com>
2009-06-01 15:42:13 +0000

    [soc2009/model-validation] moved validation tests to tests.py out of models.py
    git-svn-id: http://code.djangoproject.com/svn/django/branches/soc2009/model-validation@10878 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/modeltests/validation/models.py
+++ tests/modeltests/validation/models.py
@@ -14,20 +14,3 @@ class ModelToValidate(models.Model):
         if self.number == 11:
             raise ValidationError('Invalid number supplied!')
 
-class BaseModelValidationTests(TestCase):
-    def test_missing_required_field_raises_error(self):
-        mtv = ModelToValidate()
-        self.assertRaises(ValidationError, mtv.clean)
-        try:
-            mtv.clean()
-        except ValidationError, e:
-            self.assertEquals(['name', 'number'], sorted(e.message_dict.keys()))
-    
-    def test_with_correct_value_model_validates(self):
-        mtv = ModelToValidate(number=10, name='Some Name')
-        self.assertEqual(None, mtv.clean())
-
-    def test_custom_validate_method_is_called(self):
-        mtv = ModelToValidate(number=11)
-        self.assertRaises(ValidationError, mtv.clean)
-
