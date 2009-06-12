1ff9ec9f126e3a4346ec2fd07038b00b37952b74
Justin Bronn <jbronn@gmail.com>
2009-06-12 17:19:19 +0000

    [1.0.X] Fixed support for GDAL 1.6 on Windows.  Thanks to jtia for spotting this.
    Backport of r10985 from trunk.
    
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.0.X@10986 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/gis/gdal/libgdal.py
+++ django/contrib/gis/gdal/libgdal.py
@@ -14,7 +14,7 @@ if lib_path:
     lib_names = None
 elif os.name == 'nt':
     # Windows NT shared library
-    lib_names = ['gdal15']
+    lib_names = ['gdal16', 'gdal15']
 elif os.name == 'posix':
     # *NIX library names.
     lib_names = ['gdal', 'GDAL', 'gdal1.6.0', 'gdal1.5.0', 'gdal1.4.0']
