bbac0cc965ea90f31a365891d4e21b1bffa58f4a
Ian Kelly <ian.g.kelly@gmail.com>
2009-07-21 21:20:18 +0000

    Fixed #11487: pass long strings to Oracle as CLOB rather than NCLOB to prevent an encoding bug that occurs in some installations.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@11285 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/backends/tests.py
+++ tests/regressiontests/backends/tests.py
@@ -17,6 +17,21 @@ class Callproc(unittest.TestCase):
             return True
         else:
             return True
+            
+class LongString(unittest.TestCase):
+
+    def test_long_string(self):
+        # If the backend is Oracle, test that we can save a text longer
+        # than 4000 chars and read it properly
+        if settings.DATABASE_ENGINE == 'oracle':
+            c = connection.cursor()
+            c.execute('CREATE TABLE ltext ("TEXT" NCLOB)')
+            long_str = ''.join([unicode(x) for x in xrange(4000)])
+            c.execute('INSERT INTO ltext VALUES (%s)',[long_str])
+            c.execute('SELECT text FROM ltext')
+            row = c.fetchone()
+            c.execute('DROP TABLE ltext')
+            self.assertEquals(long_str, row[0].read())
 
 def connection_created_test(sender, **kwargs):
     print 'connection_created signal'
