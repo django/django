b8997d66be0420c78ecc0c73efeecc1e06cbfc97
James Bennett <ubernostrum@gmail.com>
2009-07-29 04:11:33 +0000

    [1.0.X] We don't need to put the 'final' bit in the version number, and it messes up some pypi stuff. Backport of [11358] from trunk.
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.0.X@11359 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/__init__.py
+++ django/__init__.py
@@ -7,9 +7,8 @@ def get_version():
     if VERSION[3:] == ('alpha', 0):
         version = '%s pre-alpha' % version
     else:
-        version = '%s %s' % (version, VERSION[3])
         if VERSION[3] != 'final':
-            version = '%s %s' % (version, VERSION[4])
+            version = '%s %s %s' % (version, VERSION[3], VERSION[4])
     from django.utils.version import get_svn_revision
     svn_rev = get_svn_revision()
     if svn_rev != u'SVN-unknown':
