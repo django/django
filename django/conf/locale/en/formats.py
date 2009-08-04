d05ad1420afb5c284c1d1e1669d40af69eeb4538
Marc Garcia <garcia.marc@gmail.com>
2009-08-04 14:28:03 +0000

    [soc2009/i18n] format files finished from what imported from cldr for ca, en, es
    git-svn-id: http://code.djangoproject.com/svn/django/branches/soc2009/i18n-improvements@11387 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/locale/en/formats.py
+++ django/conf/locale/en/formats.py
@@ -1,14 +1,15 @@
 # This file is distributed under the same license as the Django package.
 #
 
-DATE_FORMAT = 'F j, Y'
-TIME_FORMAT = 'g:i:s A'
-DATETIME_FORMAT = 'F j, Y g:i:s A'
+DATE_FORMAT = 'N j, Y'
+TIME_FORMAT = 'P'
+DATETIME_FORMAT = 'N j, Y, P'
 YEAR_MONTH_FORMAT = 'F Y'
 MONTH_DAY_FORMAT = 'F j'
-SHORT_DATE_FORMAT = 'M j, Y'
-SHORT_DATETIME_FORMAT = 'M j, Y g:i:s A'
-# FIRST_DAY_OF_WEEK = 
+SHORT_DATE_FORMAT = 'm/d/Y'
+SHORT_DATETIME_FORMAT = 'm/d/Y h:i A'
+FIRST_DAY_OF_WEEK = 0 # Sunday
 DECIMAL_SEPARATOR = '.'
 THOUSAND_SEPARATOR = ','
-# NUMBER_GROUPING = 
+NUMBER_GROUPING = 3
+
