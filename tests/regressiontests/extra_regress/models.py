fde0bfe929bdb3ac6046b270052da183a223d7d8
Alex Gaynor <alex.gaynor@gmail.com>
2009-08-09 02:55:45 +0000

    [soc2009/multidb] Fix the extra_regress tests to be more forward compatible.
    git-svn-id: http://code.djangoproject.com/svn/django/branches/soc2009/multidb@11422 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/extra_regress/models.py
+++ tests/regressiontests/extra_regress/models.py
@@ -19,7 +19,9 @@ class RevisionableModel(models.Model):
         super(RevisionableModel, self).save(*args, **kwargs)
         if not self.base:
             self.base = self
-            super(RevisionableModel, self).save(using=kwargs.pop('using', None))
+            kwargs.pop('force_insert', None)
+            kwargs.pop('force_update', None)
+            super(RevisionableModel, self).save(*args, **kwargs)
 
     def new_revision(self):
         new_revision = copy.copy(self)
