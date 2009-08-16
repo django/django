1d5aad87f4cd4ccffd24fffe25d6fa6be0bfbdf4
Marc Garcia <garcia.marc@gmail.com>
2009-08-16 19:57:19 +0000

    [soc2009/i18n] locales updated for da, de, nl, ka and ko.
    git-svn-id: http://code.djangoproject.com/svn/django/branches/soc2009/i18n-improvements@11463 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/locale/da/formats.py
+++ django/conf/locale/da/formats.py
@@ -2,17 +2,25 @@
 # This file is distributed under the same license as the Django package.
 #
 
-DATE_FORMAT = 'j. M Y'
-TIME_FORMAT = 'H.i.s'
-# DATETIME_FORMAT = 
+DATE_FORMAT = 'j. F Y'
+TIME_FORMAT = 'H:i'
+DATETIME_FORMAT = 'j. F Y H:i'
 YEAR_MONTH_FORMAT = 'F Y'
 MONTH_DAY_FORMAT = 'j. F'
-SHORT_DATE_FORMAT = 'd/m/Y'
-# SHORT_DATETIME_FORMAT = 
-# FIRST_DAY_OF_WEEK = 
-# DATE_INPUT_FORMATS = 
-# TIME_INPUT_FORMATS = 
-# DATETIME_INPUT_FORMATS = 
+SHORT_DATE_FORMAT = 'd.m.Y'
+SHORT_DATETIME_FORMAT = 'd.m.Y H:i'
+FIRST_DAY_OF_WEEK = 1
+DATE_INPUT_FORMATS = (
+    '%d.%m.%Y', 			# '25.10.2006'
+)
+TIME_INPUT_FORMATS = (
+    '%H:%M:%S',     			# '14:30:59'
+    '%H:%M',        			# '14:30'
+)
+DATETIME_INPUT_FORMATS = (
+    '%d.%m.%Y %H:%M:%S',     		# '25.10.2006 14:30:59'
+    '%d.%m.%Y %H:%M',    	    	# '25.10.2006 14:30'
+)
 DECIMAL_SEPARATOR = ','
 THOUSAND_SEPARATOR = '.'
-# NUMBER_GROUPING = 
+NUMBER_GROUPING = 3
