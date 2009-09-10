4a5630fe75ac47556c7fd15adfa6a7d9c23d18c8
Luke Plant <L.Plant.98@cantab.net>
2009-09-10 09:51:27 +0000

    Removed unnecessary keys in django.contrib.auth.test.__test__
    Only doctest tests, and not TestCases, need to be in __test__ AFAICS.
    
    
    
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@11481 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/auth/tests/__init__.py
+++ django/contrib/auth/tests/__init__.py
@@ -10,10 +10,6 @@ from django.contrib.auth.tests.tokens import TOKEN_GENERATOR_TESTS
 
 __test__ = {
     'BASIC_TESTS': BASIC_TESTS,
-    'PASSWORDRESET_TESTS': PasswordResetTest,
     'FORM_TESTS': FORM_TESTS,
     'TOKEN_GENERATOR_TESTS': TOKEN_GENERATOR_TESTS,
-    'CHANGEPASSWORD_TESTS': ChangePasswordTest,
-    'LOGIN_TESTS': LoginTest,
-    'LOGOUT_TESTS': LogoutTest,
 }
