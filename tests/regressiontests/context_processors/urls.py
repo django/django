c161bf21f0ab9f2945ae7a44e5757b071f7eb712
Luke Plant <L.Plant.98@cantab.net>
2009-10-14 18:09:13 +0000

    Fixed #6552, #12031 - Make django.core.context_processors.auth lazy to avoid "Vary: Cookie"
    Thanks to olau@iola.dk, Suor for the report
    
    
    
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@11623 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/context_processors/urls.py
+++ tests/regressiontests/context_processors/urls.py
@@ -5,4 +5,9 @@ import views
 
 urlpatterns = patterns('',
     (r'^request_attrs/$', views.request_processor),
+    (r'^auth_processor_no_attr_access/$', views.auth_processor_no_attr_access),
+    (r'^auth_processor_attr_access/$', views.auth_processor_attr_access),
+    (r'^auth_processor_user/$', views.auth_processor_user),
+    (r'^auth_processor_perms/$', views.auth_processor_perms),
+    (r'^auth_processor_messages/$', views.auth_processor_messages),
 )
