ce7c61d61edc2c9f7d4c13811664bd226864bf30
Russell Keith-Magee <russell@keith-magee.com>
2009-12-02 00:30:00 +0000

    Refs #11109 -- Touched some whitespace so Trac will display some files correctly.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@11785 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/bash_completion/management/commands/test_command.py
+++ tests/regressiontests/bash_completion/management/commands/test_command.py
@@ -3,6 +3,7 @@ from optparse import OptionParser, make_option
 
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option("--list", action="store_true", dest="list",
