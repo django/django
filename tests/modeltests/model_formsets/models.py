31618e3ea43b5bc17dd2a09be3d0631d4559f641
Karen Tracey <kmtracey@gmail.com>
2009-12-12 20:55:46 +0000

    [1.1.X] Fixed #11632: Fixed a test broken by r11827 that relied on the exact (invalid HTML) id of an initial hidden input field.
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.1.X@11836 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/modeltests/model_formsets/models.py
+++ tests/modeltests/model_formsets/models.py
@@ -885,7 +885,7 @@ False
 >>> form = formset.forms[0] # this formset only has one form
 >>> now = form.fields['date_joined'].initial
 >>> print form.as_p()
-<p><label for="id_membership_set-0-date_joined">Date joined:</label> <input type="text" name="membership_set-0-date_joined" value="..." id="id_membership_set-0-date_joined" /><input type="hidden" name="initial-membership_set-0-date_joined" value="..." id="id_membership_set-0-date_joined" /></p>
+<p><label for="id_membership_set-0-date_joined">Date joined:</label> <input type="text" name="membership_set-0-date_joined" value="..." id="id_membership_set-0-date_joined" /><input type="hidden" name="initial-membership_set-0-date_joined" value="..." id="initial-membership_set-0-id_membership_set-0-date_joined" /></p>
 <p><label for="id_membership_set-0-karma">Karma:</label> <input type="text" name="membership_set-0-karma" id="id_membership_set-0-karma" /><input type="hidden" name="membership_set-0-person" value="1" id="id_membership_set-0-person" /><input type="hidden" name="membership_set-0-id" id="id_membership_set-0-id" /></p>
 
 # test for validation with callable defaults. Validations rely on hidden fields
