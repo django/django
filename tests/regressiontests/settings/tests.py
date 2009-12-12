c1a8bd6779c2ff907d2d80b98d79c8ab10d642ae
Jacob Kaplan-Moss <jacob@jacobian.org>
2009-12-12 17:53:32 +0000

    [1.1.X] Fixed #10130: you may now delete attributes on `settings`. Thanks, jcassee.
    Backport of r11824.
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.1.X@11825 bcc190cf-cafb-0310-a4f2-bffc1f526a37

---
+++ tests/regressiontests/settings/tests.py
@@ -0,0 +1,17 @@
+import unittest
+from django.conf import settings
+
+class SettingsTests(unittest.TestCase):
+
+    #
+    # Regression tests for #10130: deleting settings.
+    #
+
+    def test_settings_delete(self):
+        settings.TEST = 'test'
+        self.assertEqual('test', settings.TEST)
+        del settings.TEST
+        self.assertRaises(AttributeError, getattr, settings, 'TEST')
+
+    def test_settings_delete_wrapped(self):
+        self.assertRaises(TypeError, delattr, settings, '_wrapped')
