ac8da7b36f353ffc225ee27b0aeb96f8912b9362
Jacob Kaplan-Moss <jacob@jacobian.org>
2009-12-12 17:51:51 +0000

    FIxed #10130: you may now delete attributes on `settings`. Thanks, jcassee.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@11824 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/utils/functional.py
+++ django/utils/functional.py
@@ -277,6 +277,13 @@ class LazyObject(object):
                 self._setup()
             setattr(self._wrapped, name, value)
 
+    def __delattr__(self, name):
+        if name == "_wrapped":
+            raise TypeError("can't delete _wrapped.")
+        if self._wrapped is None:
+            self._setup()
+        delattr(self._wrapped, name)
+
     def _setup(self):
         """
         Must be implemented by subclasses to initialise the wrapped object.
