d48f530b0e286888fe99303ca58772e129a63476
Jannis Leidel <jannis@leidel.info>
2010-01-01 21:35:47 +0000

    Fixed code duplication caused in [11857].
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@12048 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/localflavor/forms.py
+++ tests/regressiontests/localflavor/forms.py
@@ -5,10 +5,3 @@ class PlaceForm(ModelForm):
     """docstring for PlaceForm"""
     class Meta:
         model = Place
-from django.forms import ModelForm
-from models import Place
-
-class PlaceForm(ModelForm):
-    """docstring for PlaceForm"""
-    class Meta:
-        model = Place
--- tests/regressiontests/localflavor/models.py
+++ tests/regressiontests/localflavor/models.py
@@ -6,11 +6,3 @@ class Place(models.Model):
     state_req = USStateField()
     state_default = USStateField(default="CA", blank=True)
     name = models.CharField(max_length=20)
-from django.db import models
-from django.contrib.localflavor.us.models import USStateField
-
-class Place(models.Model):
-    state = USStateField(blank=True)
-    state_req = USStateField()
-    state_default = USStateField(default="CA", blank=True)
-    name = models.CharField(max_length=20)
