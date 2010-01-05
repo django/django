84ccb97c717a47da9289f962c5a4c95db469ecb4
Ramiro Morales <cramm0@gmail.com>
2010-01-05 23:22:11 +0000

    Fixed escaping in some es_AR format strings. Decision about their final form will be made before 1.2.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@12108 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/locale/es_AR/formats.py
+++ django/conf/locale/es_AR/formats.py
@@ -2,11 +2,11 @@
 # This file is distributed under the same license as the Django package.
 #
 
-DATE_FORMAT = 'j \de F \de Y'
+DATE_FORMAT = 'j \\de F \\de Y'
 TIME_FORMAT = 'H:i:s'
-DATETIME_FORMAT = 'j \de F \de Y \a \l\a\s H:i'
-YEAR_MONTH_FORMAT = 'F \de Y'
-MONTH_DAY_FORMAT = 'j \de F'
+DATETIME_FORMAT = 'j \\de F \\de Y \\a \\l\\a\s H:i'
+YEAR_MONTH_FORMAT = 'F \\de Y'
+MONTH_DAY_FORMAT = 'j \\de F'
 SHORT_DATE_FORMAT = 'd/m/Y'
 SHORT_DATETIME_FORMAT = 'd/m/Y H:i'
 FIRST_DAY_OF_WEEK = 0 # 0: Sunday, 1: Monday
