0216255632138df9bc7761d3c7a819c1f0c53a43
Jannis Leidel <jannis@leidel.info>
2010-01-17 22:10:19 +0000

    [1.1.X] Fixed #10717 - Escape result of admin_media_prefix template tag.
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.1.X@12239 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/admin/templatetags/adminmedia.py
+++ django/contrib/admin/templatetags/adminmedia.py
@@ -1,4 +1,5 @@
 from django.template import Library
+from django.utils.encoding import iri_to_uri
 
 register = Library()
 
@@ -10,5 +11,5 @@ def admin_media_prefix():
         from django.conf import settings
     except ImportError:
         return ''
-    return settings.ADMIN_MEDIA_PREFIX
+    return iri_to_uri(settings.ADMIN_MEDIA_PREFIX)
 admin_media_prefix = register.simple_tag(admin_media_prefix)
