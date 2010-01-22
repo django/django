da954e876b75be18b5635aa553af17e791971445
Justin Bronn <jbronn@gmail.com>
2010-01-22 19:43:14 +0000

    [1.1.X] Fixed #12664 -- Fixed `GenericRelation.m2m_reverse_name` to return the correct pk column name.
    Backport of r12276 from trunk.
    
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.1.X@12277 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/generic_relations_regress/models.py
+++ tests/regressiontests/generic_relations_regress/models.py
@@ -13,10 +13,30 @@ class Link(models.Model):
 class Place(models.Model):
     name = models.CharField(max_length=100)
     links = generic.GenericRelation(Link)
-    
+
     def __unicode__(self):
         return "Place: %s" % self.name
-    
-class Restaurant(Place): 
+
+class Restaurant(Place):
     def __unicode__(self):
         return "Restaurant: %s" % self.name
+
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
+    zipcode = models.CharField(max_length=5)
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey()
+
+    def __unicode__(self):
+        return '%s %s, %s %s' % (self.street, self.city, self.state, self.zipcode)
+
+class Person(models.Model):
+    account = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=128)
+    addresses = generic.GenericRelation(Address)
+
+    def __unicode__(self):
+        return self.name
