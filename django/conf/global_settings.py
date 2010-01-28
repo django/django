ab638181ae5e5a2de8434ff081324c9f87bb8903
Russell Keith-Magee <russell@keith-magee.com>
2010-01-28 02:01:28 +0000

    Fixed #12711 -- Added the start of a Frisian locale. Thanks to Wim Benes.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@12324 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/global_settings.py
+++ django/conf/global_settings.py
@@ -59,6 +59,7 @@ LANGUAGES = (
     ('fa', gettext_noop('Persian')),
     ('fi', gettext_noop('Finnish')),
     ('fr', gettext_noop('French')),
+    ('fy-nl'), gettext_noop('Frisian')),
     ('ga', gettext_noop('Irish')),
     ('gl', gettext_noop('Galician')),
     ('he', gettext_noop('Hebrew')),
