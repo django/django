2e91309c55a78e52acedc225d91b3d18bbf68cda
Jannis Leidel <jannis@leidel.info>
2010-02-21 23:39:45 +0000

    Fixed #12675 - Add the admin app to the project template settings.py for consistency with the urls.py. Thanks, stephanhoyer.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@12464 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/project_template/settings.py
+++ django/conf/project_template/settings.py
@@ -83,4 +83,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+    # Uncomment the next line to enable the admin:
+    # 'django.contrib.admin',
 )
