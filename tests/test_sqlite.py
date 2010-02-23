c8fa1100930e9741848160fa1fd516e2eb315018
Jannis Leidel <jannis@leidel.info>
2010-02-23 20:45:50 +0000

    Fixed #6298 - Added sample settings file to run the Django test suite (SQLite). Thanks, Eric Holscher.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@12547 bcc190cf-cafb-0310-a4f2-bffc1f526a37

---
+++ tests/test_sqlite.py
@@ -0,0 +1,23 @@
+# This is an example test settings file for use with the Django test suite.
+#
+# The 'sqlite3' backend requires only the ENGINE setting (an in-
+# memory database will be used). All other backends will require a
+# NAME and potentially authentication information. See the
+# following section in the docs for more information:
+#
+# http://docs.djangoproject.com/en/dev/internals/contributing/#unit-tests
+#
+# The different databases that Django supports behave differently in certain
+# situations, so it is recommended to run the test suite against as many
+# database backends as possible.  You may want to create a separate settings
+# file for each of the backends you test against.
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3'
+    },
+    'other': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'TEST_NAME': 'other_db'
+    }
+}
