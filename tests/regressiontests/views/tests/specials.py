125c748cf6d59012ac6c9e6935d7692f615d0e14
Karen Tracey <kmtracey@gmail.com>
2010-03-02 19:37:48 +0000

    Fixed #11522: Restored ability of http redirect responses to correctly handle redirect locations with non-ASCII chars.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@12659 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/views/tests/specials.py
+++ tests/regressiontests/views/tests/specials.py
@@ -5,11 +5,31 @@ class URLHandling(TestCase):
     """
     Tests for URL handling in views and responses.
     """
-    def test_iri_redirect(self):
+    redirect_target = "/views/%E4%B8%AD%E6%96%87/target/"
+
+    def test_combining_redirect(self):
         """
         Tests that redirecting to an IRI, requiring encoding before we use it
-        in an HTTP response, is handled correctly.
+        in an HTTP response, is handled correctly. In this case the arg to
+        HttpRedirect is ASCII but the current request path contains non-ASCII
+        characters so this test ensures the creation of the full path with a
+        base non-ASCII part is handled correctly.
         """
         response = self.client.get(u'/views/中文/')
-        self.assertRedirects(response, "/views/%E4%B8%AD%E6%96%87/target/")
+        self.assertRedirects(response, self.redirect_target)
+
+    def test_nonascii_redirect(self):
+        """
+        Tests that a non-ASCII argument to HttpRedirect is handled properly.
+        """
+        response = self.client.get('/views/nonascii_redirect/')
+        self.assertRedirects(response, self.redirect_target)
+
+    def test_permanent_nonascii_redirect(self):
+        """
+        Tests that a non-ASCII argument to HttpPermanentRedirect is handled
+        properly.
+        """
+        response = self.client.get('/views/permanent_nonascii_redirect/')
+        self.assertRedirects(response, self.redirect_target, status_code=301)
 
