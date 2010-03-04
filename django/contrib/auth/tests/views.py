1fedc0b9ebf8694036581cc4a75601c89a50c727
Karen Tracey <kmtracey@gmail.com>
2010-03-04 01:11:37 +0000

    [1.1.X] Made Python 2.3 compatibility fixes for some recent test additions.
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.1.X@12678 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/auth/tests/views.py
+++ django/contrib/auth/tests/views.py
@@ -182,7 +182,7 @@ class LoginTest(AuthViewsTestCase):
         site = Site.objects.get_current()
         self.assertEquals(response.context['site'], site)
         self.assertEquals(response.context['site_name'], site.name)
-        self.assert_(isinstance(response.context['form'], AuthenticationForm), 
+        self.assert_(isinstance(response.context['form'], AuthenticationForm),
                      'Login form is not an AuthenticationForm')
 
     def test_security_check(self, password='password'):
@@ -205,7 +205,7 @@ class LoginTest(AuthViewsTestCase):
                 }
             )
             self.assertEquals(response.status_code, 302)
-            self.assertFalse(bad_url in response['Location'], "%s should be blocked" % bad_url)
+            self.failIf(bad_url in response['Location'], "%s should be blocked" % bad_url)
 
         # Now, these URLs have an other URL as a GET parameter and therefore
         # should be allowed
@@ -222,9 +222,9 @@ class LoginTest(AuthViewsTestCase):
                 }
             )
             self.assertEquals(response.status_code, 302)
-            self.assertTrue('/view/?param=%s' % url_ in response['Location'], "/view/?param=%s should be allowed" % url_)
+            self.failUnless('/view/?param=%s' % url_ in response['Location'], "/view/?param=%s should be allowed" % url_)
 
-        
+
 class LogoutTest(AuthViewsTestCase):
     urls = 'django.contrib.auth.tests.urls'
 
@@ -249,7 +249,7 @@ class LogoutTest(AuthViewsTestCase):
         self.assert_('Logged out' in response.content)
         self.confirm_logged_out()
 
-    def test_logout_with_next_page_specified(self): 
+    def test_logout_with_next_page_specified(self):
         "Logout with next_page option given redirects to specified resource"
         self.login()
         response = self.client.get('/logout/next_page/')
