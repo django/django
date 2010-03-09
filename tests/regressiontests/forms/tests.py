693dcd90139a87462fc636c0b698743bfce677a0
Luke Plant <L.Plant.98@cantab.net>
2010-03-09 22:59:02 +0000

    [1.1.X] Fixed #12048 - MultiWidget does not define `__deepcopy__`
    Thanks to powderflask for report, test case and initial patch.
    
    Backport of [12739] from trunk
    
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.1.X@12740 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/forms/tests.py
+++ tests/regressiontests/forms/tests.py
@@ -32,6 +32,8 @@ from widgets import tests as widgets_tests
 from formsets import tests as formset_tests
 from media import media_tests
 
+from widgets import WidgetTests
+
 __test__ = {
     'extra_tests': extra_tests,
     'fields_tests': fields_tests,
