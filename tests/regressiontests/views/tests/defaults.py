ef92f4fdd91ec980618b35f907178655b5182ae3
Karen Tracey <kmtracey@gmail.com>
2010-03-14 20:30:18 +0000

    [1.1.X] Made a Python 2.3 compatibility test fix.
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.1.X@12777 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/views/tests/defaults.py
+++ tests/regressiontests/views/tests/defaults.py
@@ -62,8 +62,8 @@ class DefaultsTests(TestCase):
 
     def test_get_absolute_url_attributes(self):
         "A model can set attributes on the get_absolute_url method"
-        self.assertTrue(getattr(UrlArticle.get_absolute_url, 'purge', False),
+        self.failUnless(getattr(UrlArticle.get_absolute_url, 'purge', False),
                         'The attributes of the original get_absolute_url must be added.')
         article = UrlArticle.objects.get(pk=1)
-        self.assertTrue(getattr(article.get_absolute_url, 'purge', False),
+        self.failUnless(getattr(article.get_absolute_url, 'purge', False),
                         'The attributes of the original get_absolute_url must be added.')
