10884ec2ebee152350c661c1356519ecd21084a5
Karen Tracey <kmtracey@gmail.com>
2010-03-20 20:01:06 +0000

    Fixed #12239, again: Refined the original fix to avoid the test errors introduced.  Thanks pmclanahan.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@12821 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/db/models/fields/__init__.py
+++ django/db/models/fields/__init__.py
@@ -579,7 +579,7 @@ class CharField(Field):
 
     def get_prep_value(self, value):
         return self.to_python(value)
-    
+
     def formfield(self, **kwargs):
         # Passing max_length to forms.CharField means that the value's length
         # will be validated twice. This is considered acceptable since we want
@@ -884,8 +884,9 @@ class IntegerField(Field):
         return int(value)
 
     def get_prep_lookup(self, lookup_type, value):
-        if lookup_type == 'gte' or lookup_type == 'lt':
-            value = math.ceil(value)
+        if (lookup_type == 'gte' or lookup_type == 'lt') \
+           and isinstance(value, float):
+                value = math.ceil(value)
         return super(IntegerField, self).get_prep_lookup(lookup_type, value)
 
     def get_internal_type(self):
