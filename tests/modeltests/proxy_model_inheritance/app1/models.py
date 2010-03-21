72ea8fc4eb79d4dfbbbf65161486c06f411ab8c6
Karen Tracey <kmtracey@gmail.com>
2010-03-21 20:29:11 +0000

    Refs: #12286 -- Add a test to verify that a proxied model's table is created in a situation where it did not used to be.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@12828 bcc190cf-cafb-0310-a4f2-bffc1f526a37

---
+++ tests/modeltests/proxy_model_inheritance/app1/models.py
@@ -0,0 +1,5 @@
+from app2.models import NiceModel
+
+class ProxyModel(NiceModel):
+    class Meta:
+        proxy = True
---
+++ tests/modeltests/proxy_model_inheritance/app2/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+class NiceModel(models.Model):
+    pass
