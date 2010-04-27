b031fa2376f489406b44f0dabed18b5d318c2e83
Russell Keith-Magee <russell@keith-magee.com>
2010-04-27 15:04:41 +0000

    Reverted r13021 -- Trunk no longer supports Python 2.3, so the special case isn't required.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@13033 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/bug8245/tests.py
+++ tests/regressiontests/bug8245/tests.py
@@ -9,25 +9,22 @@ class Bug8245Test(TestCase):
     Test for bug #8245 - don't raise an AlreadyRegistered exception when using
     autodiscover() and an admin.py module contains an error.
     """
-    if sys.version_info[1] >= 4:
-        # Due to a bug in Python 2.3, this test will fail. The actual
-        # feature works fine; it's just a testing problem. See #13362 for details.
-        def test_bug_8245(self):
-            # The first time autodiscover is called, we should get our real error.
-            try:
-                admin.autodiscover()
-            except Exception, e:
-                self.failUnlessEqual(str(e), "Bad admin module")
-            else:
-                self.fail(
-                    'autodiscover should have raised a "Bad admin module" error.')
+    def test_bug_8245(self):
+        # The first time autodiscover is called, we should get our real error.
+        try:
+            admin.autodiscover()
+        except Exception, e:
+            self.failUnlessEqual(str(e), "Bad admin module")
+        else:
+            self.fail(
+                'autodiscover should have raised a "Bad admin module" error.')
 
-            # Calling autodiscover again should raise the very same error it did
-            # the first time, not an AlreadyRegistered error.
-            try:
-                admin.autodiscover()
-            except Exception, e:
-                self.failUnlessEqual(str(e), "Bad admin module")
-            else:
-                self.fail(
-                    'autodiscover should have raised a "Bad admin module" error.')
+        # Calling autodiscover again should raise the very same error it did
+        # the first time, not an AlreadyRegistered error.
+        try:
+            admin.autodiscover()
+        except Exception, e:
+            self.failUnlessEqual(str(e), "Bad admin module")
+        else:
+            self.fail(
+                'autodiscover should have raised a "Bad admin module" error.')
