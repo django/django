2eb832a7e65b1c24904c140fa024169524d101f8
Russell Keith-Magee <russell@keith-magee.com>
2010-04-30 14:12:12 +0000

    [1.1.X] Fixed #12851 -- Corrected the loading of values when select_related() is used on inherited models. Thanks to phxx for the report and test case.
    Backport of r13054 from trunk.
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.1.X@13055 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/db/models/sql/query.py
+++ django/db/models/sql/query.py
@@ -621,7 +621,7 @@ class BaseQuery(object):
             # models.
             workset = {}
             for model, values in seen.iteritems():
-                for field in model._meta.local_fields:
+                for field in model._meta.fields:
                     if field in values:
                         continue
                     add_to_dict(workset, model, field)
