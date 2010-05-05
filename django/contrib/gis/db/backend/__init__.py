33d4aa8df121f1fa3019aa6663d02b547d38aa80
Justin Bronn <jbronn@gmail.com>
2010-05-05 16:06:39 +0000

    Removed duplicated code from `django.contrib.gis.db.backend` and added a deprecation note about the module.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@13103 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/gis/db/backend/__init__.py
+++ django/contrib/gis/db/backend/__init__.py
@@ -9,16 +9,3 @@ if hasattr(connection.ops, 'spatial_version'):
          'backend.  A `SpatialBackend` alias is provided here for '
          'backwards-compatibility, but will be removed in 1.3.')
     SpatialBackend = connection.ops
-
-from django.db import connection
-
-if hasattr(connection.ops, 'spatial_version'):
-    from warnings import warn
-    warn('The `django.contrib.gis.db.backend` module was refactored and '
-         'renamed to `django.contrib.gis.db.backends` in 1.2.  '
-         'All functionality of `SpatialBackend` '
-         'has been moved to the `ops` attribute of the spatial database '
-         'backend.  A `SpatialBackend` alias is provided here for '
-         'backwards-compatibility, but will be removed in 1.3.')
-    SpatialBackend = connection.ops
-
