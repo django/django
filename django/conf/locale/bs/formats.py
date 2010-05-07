0b25cebcc6466d02044533db3c569636998231bb
Jannis Leidel <jannis@leidel.info>
2010-05-07 18:53:19 +0000

    Added preliminary Bosnian formats.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@13121 bcc190cf-cafb-0310-a4f2-bffc1f526a37

---
+++ django/conf/locale/bs/formats.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+# This file is distributed under the same license as the Django package.
+#
+
+DATE_FORMAT = 'j. N Y.'
+TIME_FORMAT = 'G:i'
+DATETIME_FORMAT = 'j. N. Y. G:i T'
+YEAR_MONTH_FORMAT = 'F Y.'
+MONTH_DAY_FORMAT = 'j. F'
+SHORT_DATE_FORMAT = 'Y M j'
+# SHORT_DATETIME_FORMAT = 
+# FIRST_DAY_OF_WEEK = 
+# DATE_INPUT_FORMATS = 
+# TIME_INPUT_FORMATS = 
+# DATETIME_INPUT_FORMATS = 
+DECIMAL_SEPARATOR = ','
+THOUSAND_SEPARATOR = '.'
+# NUMBER_GROUPING = 
