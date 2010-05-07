9fe8cad54ead41cadfb45cdc4570ab7ae2ecde3f
Jannis Leidel <jannis@leidel.info>
2010-05-07 18:53:37 +0000

    Fixed #13485 - Updated Swedish formats. Thanks, algestam.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@13122 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/locale/sv/formats.py
+++ django/conf/locale/sv/formats.py
@@ -11,13 +11,24 @@ SHORT_DATE_FORMAT = 'Y-m-d'
 SHORT_DATETIME_FORMAT = 'Y-m-d H:i'
 FIRST_DAY_OF_WEEK = 1
 DATE_INPUT_FORMATS = (
-    '%Y-%m-%d',                     # '2006-10-25'
+    '%Y-%m-%d',              # '2006-10-25'
+    '%m/%d/%Y',              # '10/25/2006'
+    '%m/%d/%y',              # '10/25/06'
 )
 TIME_INPUT_FORMATS = (
-    '%H:%i',                        # '14:30'
+    '%H:%M:%S',     # '14:30:59'
+    '%H:%M',        # '14:30'
 )
 DATETIME_INPUT_FORMATS = (
-    '%Y-%m-%d %H:%i',               # '2006-10-25 14:30'
+    '%Y-%m-%d %H:%M:%S',     # '2006-10-25 14:30:59'
+    '%Y-%m-%d %H:%M',        # '2006-10-25 14:30'
+    '%Y-%m-%d',              # '2006-10-25'
+    '%m/%d/%Y %H:%M:%S',     # '10/25/2006 14:30:59'
+    '%m/%d/%Y %H:%M',        # '10/25/2006 14:30'
+    '%m/%d/%Y',              # '10/25/2006'
+    '%m/%d/%y %H:%M:%S',     # '10/25/06 14:30:59'
+    '%m/%d/%y %H:%M',        # '10/25/06 14:30'
+    '%m/%d/%y',              # '10/25/06'
 )
 DECIMAL_SEPARATOR = '.'
 THOUSAND_SEPARATOR = ' '
