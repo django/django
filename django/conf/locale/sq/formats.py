b4f82b161fded3fde2d41137feb9348474b8db4a
Jannis Leidel <jannis@leidel.info>
2010-05-07 18:52:59 +0000

    Added preliminary Albanian formats.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@13120 bcc190cf-cafb-0310-a4f2-bffc1f526a37

---
+++ django/conf/locale/sq/formats.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+# This file is distributed under the same license as the Django package.
+#
+
+DATE_FORMAT = 'd F Y'
+TIME_FORMAT = 'g.i.s.A'
+# DATETIME_FORMAT = 
+YEAR_MONTH_FORMAT = 'F Y'
+MONTH_DAY_FORMAT = 'j F'
+SHORT_DATE_FORMAT = 'Y-m-d'
+# SHORT_DATETIME_FORMAT = 
+# FIRST_DAY_OF_WEEK = 
+# DATE_INPUT_FORMATS = 
+# TIME_INPUT_FORMATS = 
+# DATETIME_INPUT_FORMATS = 
+DECIMAL_SEPARATOR = ','
+THOUSAND_SEPARATOR = '.'
+# NUMBER_GROUPING = 
