362722b2a11ef2b91bf32dac066f4e69163e01cc
Russell Keith-Magee <russell@keith-magee.com>
2010-05-09 03:45:09 +0000

    Fixed #13498 -- Updated Slovak translation. Thanks to Martin Kosír.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@13142 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/locale/sk/formats.py
+++ django/conf/locale/sk/formats.py
@@ -4,15 +4,29 @@
 
 DATE_FORMAT = 'j. F Y'
 TIME_FORMAT = 'G:i:s'
-# DATETIME_FORMAT = 
+DATETIME_FORMAT = 'j. F Y G:i:s'
 YEAR_MONTH_FORMAT = 'F Y'
 MONTH_DAY_FORMAT = 'j. F'
-SHORT_DATE_FORMAT = 'j.n.Y'
-# SHORT_DATETIME_FORMAT = 
-# FIRST_DAY_OF_WEEK = 
-# DATE_INPUT_FORMATS = 
-# TIME_INPUT_FORMATS = 
-# DATETIME_INPUT_FORMATS = 
+SHORT_DATE_FORMAT = 'd.m.Y'
+SHORT_DATETIME_FORMAT = 'd.m.Y G:i:s'
+FIRST_DAY_OF_WEEK = 1 # Monday
+DATE_INPUT_FORMATS = (
+    '%d.%m.%Y', '%d.%m.%y',     # '25.10.2006', '25.10.06'
+    '%Y-%m-%d', '%y-%m-%d',     # '2006-10-25', '06-10-25'
+    # '%d. %B %Y', '%d. %b. %Y',  # '25. October 2006', '25. Oct. 2006'
+)
+TIME_INPUT_FORMATS = (
+    '%H:%M:%S', # '14:30:59'
+    '%H:%M',    # '14:30'
+)
+DATETIME_INPUT_FORMATS = (
+    '%d.%m.%Y %H:%M:%S',    # '25.10.2006 14:30:59'
+    '%d.%m.%Y %H:%M',       # '25.10.2006 14:30'
+    '%d.%m.%Y',             # '25.10.2006'
+    '%Y-%m-%d %H:%M:%S',    # '2006-10-25 14:30:59'
+    '%Y-%m-%d %H:%M',       # '2006-10-25 14:30'
+    '%Y-%m-%d',             # '2006-10-25'
+)
 DECIMAL_SEPARATOR = ','
-THOUSAND_SEPARATOR = ' '
-# NUMBER_GROUPING = 
+THOUSAND_SEPARATOR = ' '
+NUMBER_GROUPING = 3
