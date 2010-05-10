6c2f14e9fd71d698f3db43f6222dd85186b9e84e
Aljosa Mohorovic <aljosa.mohorovic@gmail.com>
2010-05-10 17:12:36 +0000

    updated croatian translation
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@13228 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/locale/hr/formats.py
+++ django/conf/locale/hr/formats.py
@@ -4,15 +4,39 @@
 
 DATE_FORMAT = 'j. F Y.'
 TIME_FORMAT = 'H:i:s'
-# DATETIME_FORMAT = 
+DATETIME_FORMAT = 'j. F Y. H:i'
 YEAR_MONTH_FORMAT = 'F Y.'
 MONTH_DAY_FORMAT = 'j. F'
-SHORT_DATE_FORMAT = 'j.n.Y.'
-# SHORT_DATETIME_FORMAT = 
-# FIRST_DAY_OF_WEEK = 
-# DATE_INPUT_FORMATS = 
-# TIME_INPUT_FORMATS = 
-# DATETIME_INPUT_FORMATS = 
-DECIMAL_SEPARATOR = ','
-THOUSAND_SEPARATOR = '.'
-# NUMBER_GROUPING = 
+SHORT_DATE_FORMAT = 'j.m.Y.'
+SHORT_DATETIME_FORMAT = 'j.m.Y. H:i'
+FIRST_DAY_OF_WEEK = 1
+DATE_INPUT_FORMATS = (
+    '%Y-%m-%d',                     # '2006-10-25'
+    '%d.%m.%Y.', '%d.%m.%y.',       # '25.10.2006.', '25.10.06.'
+    '%d. %m. %Y.', '%d. %m. %y.',   # '25. 10. 2006.', '25. 10. 06.'
+)
+TIME_INPUT_FORMATS = (
+    '%H:%M:%S',     # '14:30:59'
+    '%H:%M',        # '14:30'
+)
+DATETIME_INPUT_FORMATS = (
+    '%Y-%m-%d %H:%M:%S',      # '2006-10-25 14:30:59'
+    '%Y-%m-%d %H:%M',         # '2006-10-25 14:30'
+    '%Y-%m-%d',               # '2006-10-25'
+    '%d.%m.%Y. %H:%M:%S',     # '25.10.2006. 14:30:59'
+    '%d.%m.%Y. %H:%M',        # '25.10.2006. 14:30'
+    '%d.%m.%Y.',              # '25.10.2006.'
+    '%d.%m.%y. %H:%M:%S',     # '25.10.06. 14:30:59'
+    '%d.%m.%y. %H:%M',        # '25.10.06. 14:30'
+    '%d.%m.%y.',              # '25.10.06.'
+    '%d. %m. %Y. %H:%M:%S',   # '25. 10. 2006. 14:30:59'
+    '%d. %m. %Y. %H:%M',      # '25. 10. 2006. 14:30'
+    '%d. %m. %Y.',            # '25. 10. 2006.'
+    '%d. %m. %y. %H:%M:%S',   # '25. 10. 06. 14:30:59'
+    '%d. %m. %y. %H:%M',      # '25. 10. 06. 14:30'
+    '%d. %m. %y.',            # '25. 10. 06.'
+)
+
+DECIMAL_SEPARATOR = '.'
+THOUSAND_SEPARATOR = ','
+NUMBER_GROUPING = 3
