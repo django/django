b8ed8279529fecd408a3043e205aced6f7d23089
Russell Keith-Magee <russell@keith-magee.com>
2010-05-12 11:07:50 +0000

    Fixed #13517 -- Added Latvian L10N description. Thanks to fest.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@13240 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/locale/lv/formats.py
+++ django/conf/locale/lv/formats.py
@@ -4,15 +4,33 @@
 
 DATE_FORMAT = r'Y. \g\a\d\a j. F'
 TIME_FORMAT = 'H:i:s'
-# DATETIME_FORMAT = 
+DATETIME_FORMAT = r'Y. \g\a\d\a j. F, H:i:s'
 YEAR_MONTH_FORMAT = r'Y. \g. F'
 MONTH_DAY_FORMAT = 'j. F'
-SHORT_DATE_FORMAT = r'Y. \g\a\d\a j. M'
-# SHORT_DATETIME_FORMAT = 
-# FIRST_DAY_OF_WEEK = 
-# DATE_INPUT_FORMATS = 
-# TIME_INPUT_FORMATS = 
-# DATETIME_INPUT_FORMATS = 
+SHORT_DATE_FORMAT = r'j.m.Y'
+SHORT_DATETIME_FORMAT = 'j.m.Y H:i:s'
+FIRST_DAY_OF_WEEK = 1 #Monday
+DATE_INPUT_FORMATS = (
+    '%Y-%m-%d', '%d.%m.%Y', '%d.%m.%y', # '2006-10-25', '25.10.2006', '25.10.06'
+)
+TIME_INPUT_FORMATS = (
+    '%H:%M:%S',     # '14:30:59'
+    '%H:%M',     # '14:30'
+    '%H.%M.%S', # '14.30.59'
+    '%H.%M', # '14.30'
+)
+DATETIME_INPUT_FORMATS = (
+    '%Y-%m-%d %H:%M:%S',     # '2006-10-25 14:30:59'
+    '%Y-%m-%d %H:%M',        # '2006-10-25 14:30'
+    '%d.%m.%Y %H:%M:%S',     # '25.10.2006 14:30:59'
+    '%d.%m.%Y %H:%M',        # '25.10.2006 14:30'
+    '%d.%m.%Y',              # '25.10.2006'
+    '%d.%m.%y %H:%M:%S',     # '25.10.06 14:30:59'
+    '%d.%m.%y %H:%M',        # '25.10.06 14:30'
+    '%d.%m.%y %H.%M.%S',     # '25.10.06 14.30.59'
+    '%d.%m.%y %H.%M',        # '25.10.06 14.30'
+    '%d.%m.%y',              # '25.10.06'
+)
 DECIMAL_SEPARATOR = ','
 THOUSAND_SEPARATOR = ' '
-# NUMBER_GROUPING = 
+NUMBER_GROUPING = 3
