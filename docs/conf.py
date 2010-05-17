ffcffcb96b7068718eb6e06bb168d194f367e643
Jacob Kaplan-Moss <jacob@jacobian.org>
2010-05-17 18:51:50 +0000

    Updated the docs conf.py to have the correct version number.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@13287 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- docs/conf.py
+++ docs/conf.py
@@ -41,11 +41,11 @@ copyright = 'Django Software Foundation and contributors'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '1.1'
+version = '1.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 # The next version to be released
-django_next_version = '1.2'
+django_next_version = '1.3'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
