7289013cf18905ad2022eb21a51be91b02284a56
Paul McMillan <Paul@McMillan.ws>
2010-06-09 22:13:03 +0000

    Converted modeltests.choices to unittests
    git-svn-id: http://code.djangoproject.com/svn/django/branches/soc2010/test-refactor@13344 bcc190cf-cafb-0310-a4f2-bffc1f526a37

---
+++ tests/modeltests/choices/tests.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+
+from models import Person
+
+class ChoicesFieldTestCase(TestCase):
+    fixtures = ['initial_data.json']
+
+    def setUp(self):
+        self.a = Person.objects.get(name='Adrian')
+        self.s = Person.objects.get(name='Sara')
+
+    def test_choice_storage(self):
+        self.assertEqual(self.a.gender,
+                         'M')
+        self.assertEqual(self.s.gender,
+                         'F')
+
+    def test_gender_display(self):
+        self.assertEqual(self.a.get_gender_display(),
+                         u'Male')
+        self.assertEqual(self.s.get_gender_display(),
+                         u'Female')
+        
+        # If the value for the field doesn't correspond to a valid choice,
+        # the value itself is provided as a display value.
+        self.a.gender = ''
+        self.assertEqual(self.a.get_gender_display(),
+                         u'')
+        self.a.gender = 'U'
+        self.assertEqual(self.a.get_gender_display(),
+                         u'U')
