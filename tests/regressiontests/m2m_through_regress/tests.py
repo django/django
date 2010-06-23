6eafd412fcb41dc2866f6a1f27dfb4941f999aa7
Jacob Kaplan-Moss <jacob@jacobian.org>
2010-06-23 20:03:41 +0000

    Fixed #13799, a test failure on Postgres. Thanks, Alex.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@13400 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/m2m_through_regress/tests.py
+++ tests/regressiontests/m2m_through_regress/tests.py
@@ -66,11 +66,13 @@ class M2MThroughTestCase(TestCase):
 
         p = Person.objects.create(name="Bob")
         g = Group.objects.create(name="Roll")
-        Membership.objects.create(person=p, group=g)
+        m =Membership.objects.create(person=p, group=g)
+
+        pks = {"p_pk": p.pk, "g_pk": g.pk, "m_pk": m.pk}
 
         out = StringIO()
         management.call_command("dumpdata", "m2m_through_regress", format="json", stdout=out)
-        self.assertEqual(out.getvalue().strip(), """[{"pk": 1, "model": "m2m_through_regress.membership", "fields": {"person": 1, "price": 100, "group": 1}}, {"pk": 1, "model": "m2m_through_regress.person", "fields": {"name": "Bob"}}, {"pk": 1, "model": "m2m_through_regress.group", "fields": {"name": "Roll"}}]""")
+        self.assertEqual(out.getvalue().strip(), """[{"pk": %(m_pk)s, "model": "m2m_through_regress.membership", "fields": {"person": %(p_pk)s, "price": 100, "group": %(g_pk)s}}, {"pk": %(p_pk)s, "model": "m2m_through_regress.person", "fields": {"name": "Bob"}}, {"pk": %(g_pk)s, "model": "m2m_through_regress.group", "fields": {"name": "Roll"}}]""" % pks)
 
         out = StringIO()
         management.call_command("dumpdata", "m2m_through_regress", format="xml",
@@ -78,19 +80,19 @@ class M2MThroughTestCase(TestCase):
         self.assertEqual(out.getvalue().strip(), """
 <?xml version="1.0" encoding="utf-8"?>
 <django-objects version="1.0">
-  <object pk="1" model="m2m_through_regress.membership">
-    <field to="m2m_through_regress.person" name="person" rel="ManyToOneRel">1</field>
-    <field to="m2m_through_regress.group" name="group" rel="ManyToOneRel">1</field>
+  <object pk="%(m_pk)s" model="m2m_through_regress.membership">
+    <field to="m2m_through_regress.person" name="person" rel="ManyToOneRel">%(p_pk)s</field>
+    <field to="m2m_through_regress.group" name="group" rel="ManyToOneRel">%(g_pk)s</field>
     <field type="IntegerField" name="price">100</field>
   </object>
-  <object pk="1" model="m2m_through_regress.person">
+  <object pk="%(p_pk)s" model="m2m_through_regress.person">
     <field type="CharField" name="name">Bob</field>
   </object>
-  <object pk="1" model="m2m_through_regress.group">
+  <object pk="%(g_pk)s" model="m2m_through_regress.group">
     <field type="CharField" name="name">Roll</field>
   </object>
 </django-objects>
-        """.strip())
+        """.strip() % pks)
 
     def test_join_trimming(self):
         "Check that we don't involve too many copies of the intermediate table when doing a join. Refs #8046, #8254"
