2370a1038071bacd713721f5ad7a0811225bb040
Russell Keith-Magee <russell@keith-magee.com>
2010-08-27 13:59:46 +0000

    [1.2.X] Fixed #14156 -- Modified the way CSRF protection is applied to flatpages so that the flatpage middleware doesn't cause all POSTs resulting in 404s to turn into 403s. Thanks to patrys for the report.
    Backport of r13641 from trunk.
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.2.X@13643 bcc190cf-cafb-0310-a4f2-bffc1f526a37

---
+++ django/contrib/flatpages/tests/middleware.py
@@ -0,0 +1,56 @@
+import os
+from django.conf import settings
+from django.test import TestCase
+
+class FlatpageMiddlewareTests(TestCase):
+    fixtures = ['sample_flatpages']
+    urls = 'django.contrib.flatpages.tests.urls'
+
+    def setUp(self):
+        self.old_MIDDLEWARE_CLASSES = settings.MIDDLEWARE_CLASSES
+        flatpage_middleware_class = 'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware'
+        if flatpage_middleware_class not in settings.MIDDLEWARE_CLASSES:
+            settings.MIDDLEWARE_CLASSES += (flatpage_middleware_class,)
+        self.old_TEMPLATE_DIRS = settings.TEMPLATE_DIRS
+        settings.TEMPLATE_DIRS = (
+            os.path.join(
+                os.path.dirname(__file__),
+                'templates'
+            ),
+        )
+
+    def tearDown(self):
+        settings.MIDDLEWARE_CLASSES = self.old_MIDDLEWARE_CLASSES
+        settings.TEMPLATE_DIRS = self.old_TEMPLATE_DIRS
+
+    def test_view_flatpage(self):
+        "A flatpage can be served through a view, even when the middleware is in use"
+        response = self.client.get('/flatpage_root/flatpage/')
+        self.assertEquals(response.status_code, 200)
+        self.assertContains(response, "<p>Isn't it flat!</p>")
+
+    def test_view_non_existent_flatpage(self):
+        "A non-existent flatpage raises 404 when served through a view, even when the middleware is in use"
+        response = self.client.get('/flatpage_root/no_such_flatpage/')
+        self.assertEquals(response.status_code, 404)
+
+    def test_view_authenticated_flatpage(self):
+        "A flatpage served through a view can require authentication"
+        response = self.client.get('/flatpage_root/sekrit/')
+        self.assertRedirects(response, '/accounts/login/?next=/flatpage_root/sekrit/')
+
+    def test_fallback_flatpage(self):
+        "A flatpage can be served by the fallback middlware"
+        response = self.client.get('/flatpage/')
+        self.assertEquals(response.status_code, 200)
+        self.assertContains(response, "<p>Isn't it flat!</p>")
+
+    def test_fallback_non_existent_flatpage(self):
+        "A non-existent flatpage raises a 404 when served by the fallback middlware"
+        response = self.client.get('/no_such_flatpage/')
+        self.assertEquals(response.status_code, 404)
+
+    def test_fallback_authenticated_flatpage(self):
+        "A flatpage served by the middleware can require authentication"
+        response = self.client.get('/sekrit/')
+        self.assertRedirects(response, '/accounts/login/?next=/sekrit/')
