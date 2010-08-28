72a716743929afd613fb5242d2525265ec27b2c5
Russell Keith-Magee <russell@keith-magee.com>
2010-08-28 12:25:54 +0000

    Fixed #5192 -- Modified flatpage admin form to allow ~ and . characters in flatpage names. Thanks to marco.giusti@gmail.com for the report, Idan Gazit for summarizing the issue on the ticket, and ctrochalakis for the initial patch.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@13655 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/flatpages/tests/views.py
+++ django/contrib/flatpages/tests/views.py
@@ -1,6 +1,7 @@
 import os
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.contrib.flatpages.models import FlatPage
 from django.test import TestCase
 
 class FlatpageViewTests(TestCase):
@@ -54,3 +55,18 @@ class FlatpageViewTests(TestCase):
         "A non-existent flatpage won't be served if the fallback middlware is disabled"
         response = self.client.get('/no_such_flatpage/')
         self.assertEquals(response.status_code, 404)
+
+    def test_view_flatpage_special_chars(self):
+        "A flatpage with special chars in the URL can be served through a view"
+        fp = FlatPage.objects.create(
+            url="/some.very_special~chars-here/",
+            title="A very special page",
+            content="Isn't it special!",
+            enable_comments=False,
+            registration_required=False,
+        )
+        fp.sites.add(1)
+
+        response = self.client.get('/flatpage_root/some.very_special~chars-here/')
+        self.assertEquals(response.status_code, 200)
+        self.assertContains(response, "<p>Isn't it special!</p>")
