45651dcb05a863f87a440a895544736b5e8ba10a
Russell Keith-Magee <russell@keith-magee.com>
2010-09-12 20:05:07 +0000

    Migrated empty doctests. Thanks to Alex Gaynor.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@13789 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/modeltests/empty/models.py
+++ tests/modeltests/empty/models.py
@@ -7,20 +7,6 @@ no fields.
 
 from django.db import models
 
+
 class Empty(models.Model):
     pass
-
-__test__ = {'API_TESTS':"""
->>> m = Empty()
->>> m.id
->>> m.save()
->>> m2 = Empty()
->>> m2.save()
->>> len(Empty.objects.all())
-2
->>> m.id is not None
-True
->>> existing = Empty(m.id)
->>> existing.save()
-
-"""}
