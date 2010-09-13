11f77de5337ba692985907d41e2186ac34ea87e5
Russell Keith-Magee <russell@keith-magee.com>
2010-09-13 05:47:52 +0000

    [1.2.X] Migrated reverse_lookup doctests. Thanks to Eric Florenzano
    Backport of r13829 from trunk.
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.2.X@13838 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/modeltests/reverse_lookup/models.py
+++ tests/modeltests/reverse_lookup/models.py
@@ -26,34 +26,3 @@ class Choice(models.Model):
 
     def __unicode__(self):
         return self.name
-
-__test__ = {'API_TESTS':"""
->>> john = User(name="John Doe")
->>> john.save()
->>> jim = User(name="Jim Bo")
->>> jim.save()
->>> first_poll = Poll(question="What's the first question?", creator=john)
->>> first_poll.save()
->>> second_poll = Poll(question="What's the second question?", creator=jim)
->>> second_poll.save()
->>> new_choice = Choice(poll=first_poll, related_poll=second_poll, name="This is the answer.")
->>> new_choice.save()
-
->>> # Reverse lookups by field name:
->>> User.objects.get(poll__question__exact="What's the first question?")
-<User: John Doe>
->>> User.objects.get(poll__question__exact="What's the second question?")
-<User: Jim Bo>
-
->>> # Reverse lookups by related_name:
->>> Poll.objects.get(poll_choice__name__exact="This is the answer.")
-<Poll: What's the first question?>
->>> Poll.objects.get(related_choice__name__exact="This is the answer.")
-<Poll: What's the second question?>
-
->>> # If a related_name is given you can't use the field name instead:
->>> Poll.objects.get(choice__name__exact="This is the answer")
-Traceback (most recent call last):
-    ...
-FieldError: Cannot resolve keyword 'choice' into field. Choices are: creator, id, poll_choice, question, related_choice
-"""}
