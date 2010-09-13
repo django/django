b7cd1682f5b58bf0f4bb7af3055d5a4f26fd60b2
Russell Keith-Magee <russell@keith-magee.com>
2010-09-13 05:29:09 +0000

    Migrated reserved_names doctests. Thanks to Eric Florenzano.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@13830 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/modeltests/reserved_names/models.py
+++ tests/modeltests/reserved_names/models.py
@@ -23,32 +23,3 @@ class Thing(models.Model):
 
     def __unicode__(self):
         return self.when
-
-__test__ = {'API_TESTS':"""
->>> import datetime
->>> day1 = datetime.date(2005, 1, 1)
->>> day2 = datetime.date(2006, 2, 2)
->>> t = Thing(when='a', join='b', like='c', drop='d', alter='e', having='f', where=day1, has_hyphen='h')
->>> t.save()
->>> print t.when
-a
-
->>> u = Thing(when='h', join='i', like='j', drop='k', alter='l', having='m', where=day2)
->>> u.save()
->>> print u.when
-h
-
->>> Thing.objects.order_by('when')
-[<Thing: a>, <Thing: h>]
->>> v = Thing.objects.get(pk='a')
->>> print v.join
-b
->>> print v.where
-2005-01-01
-
->>> Thing.objects.dates('where', 'year')
-[datetime.datetime(2005, 1, 1, 0, 0), datetime.datetime(2006, 1, 1, 0, 0)]
-
->>> Thing.objects.filter(where__month=1)
-[<Thing: a>]
-"""}
