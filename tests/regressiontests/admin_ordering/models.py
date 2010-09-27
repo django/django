5ec1f0e1501e09899b1fc39088bf69d8e40d0c1b
Russell Keith-Magee <russell@keith-magee.com>
2010-09-27 15:11:29 +0000

    Migrated admin_ordering doctests. Thanks to Sebastian Hillig.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@13881 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/admin_ordering/models.py
+++ tests/regressiontests/admin_ordering/models.py
@@ -8,39 +8,3 @@ class Band(models.Model):
 
     class Meta:
         ordering = ('name',)
-
-__test__ = {'API_TESTS': """
-
-Let's make sure that ModelAdmin.queryset uses the ordering we define in
-ModelAdmin rather that ordering defined in the model's inner Meta
-class.
-
->>> from django.contrib.admin.options import ModelAdmin
-
->>> b1 = Band(name='Aerosmith', bio='', rank=3)
->>> b1.save()
->>> b2 = Band(name='Radiohead', bio='', rank=1)
->>> b2.save()
->>> b3 = Band(name='Van Halen', bio='', rank=2)
->>> b3.save()
-
-The default ordering should be by name, as specified in the inner Meta class.
-
->>> ma = ModelAdmin(Band, None)
->>> [b.name for b in ma.queryset(None)]
-[u'Aerosmith', u'Radiohead', u'Van Halen']
-
-
-Let's use a custom ModelAdmin that changes the ordering, and make sure it
-actually changes.
-
->>> class BandAdmin(ModelAdmin):
-...     ordering = ('rank',) # default ordering is ('name',)
-...
-
->>> ma = BandAdmin(Band, None)
->>> [b.name for b in ma.queryset(None)]
-[u'Radiohead', u'Van Halen', u'Aerosmith']
-
-"""
-}
