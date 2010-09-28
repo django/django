7230b609c3619cbcd503062fecf00871a6087e4d
Russell Keith-Magee <russell@keith-magee.com>
2010-09-28 07:09:40 +0000

    [1.2.X] Migrated reverse_single_related doctests. Thanks to Stephan Jaekel.
    Backport of r13926 from trunk.
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.2.X@13929 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/reverse_single_related/models.py
+++ tests/regressiontests/reverse_single_related/models.py
@@ -1,10 +1,4 @@
-"""
-Regression tests for an object that cannot access a single related object due
-to a restrictive default manager.
-"""
-
 from django.db import models
-
 
 class SourceManager(models.Manager):
     def get_query_set(self):
@@ -16,39 +10,3 @@ class Source(models.Model):
 
 class Item(models.Model):
     source = models.ForeignKey(Source)
-
-
-__test__ = {'API_TESTS':"""
-
->>> public_source = Source.objects.create(is_public=True)
->>> public_item = Item.objects.create(source=public_source)
-
->>> private_source = Source.objects.create(is_public=False)
->>> private_item = Item.objects.create(source=private_source)
-
-# Only one source is available via all() due to the custom default manager.
-
->>> Source.objects.all()
-[<Source: Source object>]
-
->>> public_item.source
-<Source: Source object>
-
-# Make sure that an item can still access its related source even if the default
-# manager doesn't normally allow it.
-
->>> private_item.source
-<Source: Source object>
-
-# If the manager is marked "use_for_related_fields", it'll get used instead
-# of the "bare" queryset. Usually you'd define this as a property on the class,
-# but this approximates that in a way that's easier in tests.
-
->>> Source.objects.use_for_related_fields = True
->>> private_item = Item.objects.get(pk=private_item.pk)
->>> private_item.source
-Traceback (most recent call last):
-    ...
-DoesNotExist: Source matching query does not exist.
-
-"""}
