a0cabd766f9cbbf776e3a776a9eb41265502864d
Jannis Leidel <jannis@leidel.info>
2010-10-01 02:39:45 +0000

    [1.2.X] Fixed #13568 -- Fixed the blocktrans tag to not raise a KeyError if the number of variables in the singular and the plural block differ. Thanks, deloide.
    Backport from trunk (r13967).
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.2.X@13973 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/templatetags/i18n.py
+++ django/templatetags/i18n.py
@@ -76,8 +76,10 @@ class BlockTranslateNode(Node):
         if self.plural and self.countervar and self.counter:
             count = self.counter.resolve(context)
             context[self.countervar] = count
-            plural, vars = self.render_token_list(self.plural)
+            plural, plural_vars = self.render_token_list(self.plural)
             result = translation.ungettext(singular, plural, count)
+            if count != 1:
+                vars = plural_vars
         else:
             result = translation.ugettext(singular)
         # Escape all isolated '%' before substituting in the context.
