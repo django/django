3ab1a0c7d6bd88063e526a8ec36e1468b9364f9e
Alex Gaynor <alex.gaynor@gmail.com>
2010-10-08 23:48:07 +0000

    [1.2.X] Converted order_with_respect_to to unittests.  We have always been at war with doctests.  Backport of [14043].
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.2.X@14044 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/modeltests/order_with_respect_to/models.py
+++ tests/modeltests/order_with_respect_to/models.py
@@ -4,6 +4,7 @@ Tests for the order_with_respect_to Meta attribute.
 
 from django.db import models
 
+
 class Question(models.Model):
     text = models.CharField(max_length=200)
 
@@ -16,63 +17,3 @@ class Answer(models.Model):
 
     def __unicode__(self):
         return unicode(self.text)
-
-__test__ = {'API_TESTS': """
->>> q1 = Question(text="Which Beatle starts with the letter 'R'?")
->>> q1.save()
->>> q2 = Question(text="What is your name?")
->>> q2.save()
->>> Answer(text="John", question=q1).save()
->>> Answer(text="Jonno",question=q2).save()
->>> Answer(text="Paul", question=q1).save()
->>> Answer(text="Paulo", question=q2).save()
->>> Answer(text="George", question=q1).save()
->>> Answer(text="Ringo", question=q1).save()
-
-The answers will always be ordered in the order they were inserted.
-
->>> q1.answer_set.all()
-[<Answer: John>, <Answer: Paul>, <Answer: George>, <Answer: Ringo>]
-
-We can retrieve the answers related to a particular object, in the order
-they were created, once we have a particular object.
-
->>> a1 = Answer.objects.filter(question=q1)[0]
->>> a1
-<Answer: John>
->>> a2 = a1.get_next_in_order()
->>> a2
-<Answer: Paul>
->>> a4 = list(Answer.objects.filter(question=q1))[-1]
->>> a4
-<Answer: Ringo>
->>> a4.get_previous_in_order()
-<Answer: George>
-
-Determining (and setting) the ordering for a particular item is also possible.
-
->>> id_list = [o.pk for o in q1.answer_set.all()]
->>> a2.question.get_answer_order() == id_list
-True
-
->>> a5 = Answer(text="Number five", question=q1)
->>> a5.save()
-
-It doesn't matter which answer we use to check the order, it will always be the same.
-
->>> a2.question.get_answer_order() == a5.question.get_answer_order()
-True
-
-The ordering can be altered:
-
->>> id_list = [o.pk for o in q1.answer_set.all()]
->>> x = id_list.pop()
->>> id_list.insert(-1, x)
->>> a5.question.get_answer_order() == id_list
-False
->>> a5.question.set_answer_order(id_list)
->>> q1.answer_set.all()
-[<Answer: John>, <Answer: Paul>, <Answer: George>, <Answer: Number five>, <Answer: Ringo>]
-
-"""
-}
