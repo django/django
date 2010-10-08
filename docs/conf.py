480c6c6958dbb6843a7effdd943700b63761c9df
Jannis Leidel <jannis@leidel.info>
2010-10-08 15:33:27 +0000

    Disabled unused Sphinx configuration option that triggered a warning.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@14014 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- docs/conf.py
+++ docs/conf.py
@@ -125,7 +125,7 @@ html_theme_path = ["_theme"]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+#html_static_path = ["_static"]
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
