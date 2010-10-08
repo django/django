ccc43508e3d15c683d941f96c23e1e6ebd32ea5d
Alex Gaynor <alex.gaynor@gmail.com>
2010-10-08 23:54:43 +0000

    Fixed #13241.  order_with_respect_to now works with ForeignKeys who refer to their model lazily (i.e. with a string).  Thanks to Gabriel Grant for the patch.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@14045 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/modeltests/order_with_respect_to/tests.py
+++ tests/modeltests/order_with_respect_to/tests.py
@@ -2,7 +2,7 @@ from operator import attrgetter
 
 from django.test import TestCase
 
-from models import Question, Answer
+from models import Post, Question, Answer
 
 
 class OrderWithRespectToTests(TestCase):
@@ -60,3 +60,12 @@ class OrderWithRespectToTests(TestCase):
             ],
             attrgetter("text")
         )
+
+    def test_recursive_ordering(self):
+        p1 = Post.objects.create(title='1')
+        p2 = Post.objects.create(title='2')
+        p1_1 = Post.objects.create(title="1.1", parent=p1)
+        p1_2 = Post.objects.create(title="1.2", parent=p1)
+        p2_1 = Post.objects.create(title="2.1", parent=p2)
+        p1_3 = Post.objects.create(title="1.3", parent=p1)
+        self.assertEqual(p1.get_post_order(), [p1_1.pk, p1_2.pk, p1_3.pk])
