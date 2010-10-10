a92da6e80b840ba7874e6faebd54d50be85ab1ae
Luke Plant <L.Plant.98@cantab.net>
2010-10-10 01:09:34 +0000

    [1.2.X] Converted contrib/webdesign doctests to unittests.  We thoroughly deplore those doctests.
    Backport of [14101] from trunk.
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.2.X@14103 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/webdesign/tests.py
+++ django/contrib/webdesign/tests.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
 
-r"""
->>> words(7)
-u'lorem ipsum dolor sit amet consectetur adipisicing'
-
->>> paragraphs(1)
-['Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.']
-
->>> from django.template import loader, Context
->>> t = loader.get_template_from_string("{% load webdesign %}{% lorem 3 w %}")
->>> t.render(Context({}))
-u'lorem ipsum dolor'
-"""
+import unittest
 
 from django.contrib.webdesign.lorem_ipsum import *
+from django.template import loader, Context
 
-if __name__ == '__main__':
-    import doctest
-    doctest.testmod()
+
+class WebdesignTest(unittest.TestCase):
+
+    def test_words(self):
+        self.assertEqual(words(7), u'lorem ipsum dolor sit amet consectetur adipisicing')
+
+    def test_paragraphs(self):
+        self.assertEqual(paragraphs(1),
+                         ['Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'])
+
+    def test_lorem_tag(self):
+        t = loader.get_template_from_string("{% load webdesign %}{% lorem 3 w %}")
+        self.assertEqual(t.render(Context({})),
+                         u'lorem ipsum dolor')
