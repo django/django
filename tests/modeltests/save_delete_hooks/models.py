6d34d308aa5efb5492a032d6b9d1042a0145bdea
Alex Gaynor <alex.gaynor@gmail.com>
2010-10-11 17:41:59 +0000

    [1.2.X] Converted save_delete_hooks tests from doctests to unittests.  We have always been at war with doctests.  Backport of [14145].
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.2.X@14146 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/modeltests/save_delete_hooks/models.py
+++ tests/modeltests/save_delete_hooks/models.py
@@ -7,37 +7,26 @@ the methods.
 
 from django.db import models
 
+
 class Person(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
 
+    def __init__(self, *args, **kwargs):
+        super(Person, self).__init__(*args, **kwargs)
+        self.data = []
+
     def __unicode__(self):
         return u"%s %s" % (self.first_name, self.last_name)
 
-    def save(self, force_insert=False, force_update=False):
-        print "Before save"
+    def save(self, *args, **kwargs):
+        self.data.append("Before save")
          # Call the "real" save() method
-        super(Person, self).save(force_insert, force_update)
-        print "After save"
+        super(Person, self).save(*args, **kwargs)
+        self.data.append("After save")
 
     def delete(self):
-        print "Before deletion"
-        super(Person, self).delete() # Call the "real" delete() method
-        print "After deletion"
-
-__test__ = {'API_TESTS':"""
->>> p1 = Person(first_name='John', last_name='Smith')
->>> p1.save()
-Before save
-After save
-
->>> Person.objects.all()
-[<Person: John Smith>]
-
->>> p1.delete()
-Before deletion
-After deletion
-
->>> Person.objects.all()
-[]
-"""}
+        self.data.append("Before deletion")
+        # Call the "real" delete() method
+        super(Person, self).delete()
+        self.data.append("After deletion")
