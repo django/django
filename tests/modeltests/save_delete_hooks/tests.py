3879c5907454e14dd0128ef33c927580c0fe383c
Alex Gaynor <alex.gaynor@gmail.com>
2010-10-11 17:41:44 +0000

    Converted save_delete_hooks tests from doctests to unittests.  We have always been at war with doctests.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@14145 bcc190cf-cafb-0310-a4f2-bffc1f526a37

---
+++ tests/modeltests/save_delete_hooks/tests.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+
+from models import Person
+
+
+class SaveDeleteHookTests(TestCase):
+    def test_basic(self):
+        p = Person(first_name="John", last_name="Smith")
+        self.assertEqual(p.data, [])
+        p.save()
+        self.assertEqual(p.data, [
+            "Before save",
+            "After save",
+        ])
+
+        self.assertQuerysetEqual(
+            Person.objects.all(), [
+                "John Smith",
+            ],
+            unicode
+        )
+
+        p.delete()
+        self.assertEqual(p.data, [
+            "Before save",
+            "After save",
+            "Before deletion",
+            "After deletion",
+        ])
+        self.assertQuerysetEqual(Person.objects.all(), [])
