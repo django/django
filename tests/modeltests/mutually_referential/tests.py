cdfbe796742a93aed1094db82450c6faefe5cf65
Russell Keith-Magee <russell@keith-magee.com>
2010-10-12 00:55:05 +0000

    Migrated the mutually_referential doctests. Thanks to George Sakkis for the patch.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@14164 bcc190cf-cafb-0310-a4f2-bffc1f526a37

---
+++ tests/modeltests/mutually_referential/tests.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from models import Parent, Child
+
+class MutuallyReferentialTests(TestCase):
+
+    def test_mutually_referential(self):
+        # Create a Parent
+        q = Parent(name='Elizabeth')
+        q.save()
+
+        # Create some children
+        c = q.child_set.create(name='Charles')
+        e = q.child_set.create(name='Edward')
+
+        # Set the best child
+        # No assertion require here; if basic assignment and
+        # deletion works, the test passes.
+        q.bestchild = c
+        q.save()
+        q.delete()
