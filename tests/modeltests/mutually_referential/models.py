d83af03c595772f7570d9169f8f80e064b68b8ea
Russell Keith-Magee <russell@keith-magee.com>
2010-10-12 01:01:25 +0000

    [1.2.X] Migrated the mutually_referential doctests. Thanks to George Sakkis for the patch.
    Backport of r14164 from trunk.
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.2.X@14170 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/modeltests/mutually_referential/models.py
+++ tests/modeltests/mutually_referential/models.py
@@ -8,29 +8,12 @@ from django.db.models import *
 
 class Parent(Model):
     name = CharField(max_length=100)
-    
+
     # Use a simple string for forward declarations.
     bestchild = ForeignKey("Child", null=True, related_name="favoured_by")
 
 class Child(Model):
     name = CharField(max_length=100)
-    
+
     # You can also explicitally specify the related app.
     parent = ForeignKey("mutually_referential.Parent")
-
-__test__ = {'API_TESTS':"""
-# Create a Parent
->>> q = Parent(name='Elizabeth')
->>> q.save()
-
-# Create some children
->>> c = q.child_set.create(name='Charles')
->>> e = q.child_set.create(name='Edward')
-
-# Set the best child
->>> q.bestchild = c
->>> q.save()
-
->>> q.delete()
-
-"""}
