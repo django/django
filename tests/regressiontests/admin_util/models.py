0e4737ec240f144dc072d17e99b12143d872e577
Jannis Leidel <jannis@leidel.info>
2010-10-17 15:40:41 +0000

    [1.2.X] Fixed #13963 -- Use the correct verbose name of a reverse relation field in the admin. Thanks, sfllaw and d0ugal.
    Backport from trunk (r14244).
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.2.X@14245 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/admin_util/models.py
+++ tests/regressiontests/admin_util/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-
-
 class Article(models.Model):
     """
     A simple Article model for testing
@@ -20,3 +18,16 @@ class Article(models.Model):
 
 class Count(models.Model):
     num = models.PositiveSmallIntegerField()
+
+class Event(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+
+class Location(models.Model):
+    event = models.OneToOneField(Event, verbose_name='awesome event')
+
+class Guest(models.Model):
+    event = models.OneToOneField(Event)
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "awesome guest"
--- tests/regressiontests/admin_util/tests.py
+++ tests/regressiontests/admin_util/tests.py
@@ -12,7 +12,7 @@ from django.contrib.admin.views.main import EMPTY_CHANGELIST_VALUE
 from django.contrib.sites.models import Site
 from django.contrib.admin.util import NestedObjects
 
-from models import Article, Count
+from models import Article, Count, Event, Location
 
 
 class NestedObjectsTests(TestCase):
@@ -219,4 +219,21 @@ class UtilTests(unittest.TestCase):
                 return_attr = True
             ),
             ("not Really the Model", MockModelAdmin.test_from_model)
+        )
+
+    def test_related_name(self):
+        """
+        Regression test for #13963
+        """
+        self.assertEquals(
+            label_for_field('location', Event, return_attr=True),
+            ('location', None),
+        )
+        self.assertEquals(
+            label_for_field('event', Location, return_attr=True),
+            ('awesome event', None),
+        )
+        self.assertEquals(
+            label_for_field('guest', Event, return_attr=True),
+            ('awesome guest', None),
         )
