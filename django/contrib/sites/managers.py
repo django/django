efcb7776a0209447cc8ac3eb12760c197eeed7bb
Honza Král <honza.kral@gmail.com>
2010-10-18 05:02:28 +0000

    [1.2.X] Fixed #13790 -- auto detection of m2m fields to Site. Thanks, gabrielhurley!
    Backport from trunk (r14251).
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.2.X@14252 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/sites/managers.py
+++ django/contrib/sites/managers.py
@@ -4,17 +4,38 @@ from django.db.models.fields import FieldDoesNotExist
 
 class CurrentSiteManager(models.Manager):
     "Use this to limit objects to those associated with the current site."
-    def __init__(self, field_name='site'):
+    def __init__(self, field_name=None):
         super(CurrentSiteManager, self).__init__()
         self.__field_name = field_name
         self.__is_validated = False
-
+        
+    def _validate_field_name(self):
+        field_names = self.model._meta.get_all_field_names()
+        
+        # If a custom name is provided, make sure the field exists on the model
+        if self.__field_name is not None and self.__field_name not in field_names:
+            raise ValueError("%s couldn't find a field named %s in %s." % \
+                (self.__class__.__name__, self.__field_name, self.model._meta.object_name))
+        
+        # Otherwise, see if there is a field called either 'site' or 'sites'
+        else:
+            for potential_name in ['site', 'sites']:
+                if potential_name in field_names:
+                    self.__field_name = potential_name
+                    self.__is_validated = True
+                    break
+        
+        # Now do a type check on the field (FK or M2M only)
+        try:
+            field = self.model._meta.get_field(self.__field_name)
+            if not isinstance(field, (models.ForeignKey, models.ManyToManyField)):
+                raise TypeError("%s must be a ForeignKey or ManyToManyField." %self.__field_name)
+        except FieldDoesNotExist:
+            raise ValueError("%s couldn't find a field named %s in %s." % \
+                    (self.__class__.__name__, self.__field_name, self.model._meta.object_name))
+        self.__is_validated = True
+    
     def get_query_set(self):
         if not self.__is_validated:
-            try:
-                self.model._meta.get_field(self.__field_name)
-            except FieldDoesNotExist:
-                raise ValueError("%s couldn't find a field named %s in %s." % \
-                    (self.__class__.__name__, self.__field_name, self.model._meta.object_name))
-            self.__is_validated = True
+            self._validate_field_name()
         return super(CurrentSiteManager, self).get_query_set().filter(**{self.__field_name + '__id__exact': settings.SITE_ID})
---
+++ tests/regressiontests/sites_framework/models.py
@@ -0,0 +1,36 @@
+from django.contrib.sites.managers import CurrentSiteManager
+from django.contrib.sites.models import Site
+from django.db import models
+
+class AbstractArticle(models.Model):
+    title = models.CharField(max_length=50)
+
+    objects = models.Manager()
+    on_site = CurrentSiteManager()
+
+    class Meta:
+        abstract = True
+
+    def __unicode__(self):
+        return self.title
+
+class SyndicatedArticle(AbstractArticle):
+    sites = models.ManyToManyField(Site)
+
+class ExclusiveArticle(AbstractArticle):
+    site = models.ForeignKey(Site)
+
+class CustomArticle(AbstractArticle):
+    places_this_article_should_appear = models.ForeignKey(Site)
+
+    objects = models.Manager()
+    on_site = CurrentSiteManager("places_this_article_should_appear")
+
+class InvalidArticle(AbstractArticle):
+    site = models.ForeignKey(Site)
+
+    objects = models.Manager()
+    on_site = CurrentSiteManager("places_this_article_should_appear")
+
+class ConfusedArticle(AbstractArticle):
+    site = models.IntegerField()
