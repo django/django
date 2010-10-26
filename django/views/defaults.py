cbc110a4675e987de5de075aa23b32296f5343ac
Luke Plant <L.Plant.98@cantab.net>
2010-10-26 18:11:22 +0000

    [1.2.X] Fixed #14565 - No csrf_token on 404 pages
    Thanks to gvangool for report and patch.
    
    Backport of [14356] from trunk.
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.2.X@14357 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/views/defaults.py
+++ django/views/defaults.py
@@ -1,6 +1,8 @@
 from django import http
 from django.template import Context, RequestContext, loader
+from django.views.decorators.csrf import csrf_protect
 
+@csrf_protect
 def page_not_found(request, template_name='404.html'):
     """
     Default 404 handler.
@@ -13,6 +15,7 @@ def page_not_found(request, template_name='404.html'):
     t = loader.get_template(template_name) # You need to create a 404.html template.
     return http.HttpResponseNotFound(t.render(RequestContext(request, {'request_path': request.path})))
 
+@csrf_protect
 def server_error(request, template_name='500.html'):
     """
     500 error handler.
