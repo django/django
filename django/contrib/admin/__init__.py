e716a71bad4e02410e2a0908d630abbee1d4c691
Chris Beaven <smileychris@gmail.com>
2010-10-27 07:08:15 +0000

    Revert the removal of an unused import (in [14175]) that was referenced in documentation. Thanks for noticing, clong.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@14359 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/admin/__init__.py
+++ django/contrib/admin/__init__.py
@@ -1,3 +1,6 @@
+# ACTION_CHECKBOX_NAME is unused, but should stay since its import from here
+# has been referenced in documentation.
+from django.contrib.admin.helpers import ACTION_CHECKBOX_NAME
 from django.contrib.admin.options import ModelAdmin, HORIZONTAL, VERTICAL
 from django.contrib.admin.options import StackedInline, TabularInline
 from django.contrib.admin.sites import AdminSite, site
