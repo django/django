6f30440b7291deac7e7de0c0e422f2bcc89a3c6d
Alex Gaynor <alex.gaynor@gmail.com>
2010-11-02 18:41:23 +0000

    [1.2.X] Fixed #10728 -- corrected subclassing of Fields who use the SubfieldBase metaclass.  Thanks to G2P and George Sakkis for their work on the patch.  Backport of [14443].
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.2.X@14444 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/modeltests/field_subclassing/models.py
+++ tests/modeltests/field_subclassing/models.py
@@ -5,7 +5,7 @@ Tests for field subclassing.
 from django.db import models
 from django.utils.encoding import force_unicode
 
-from fields import Small, SmallField, JSONField
+from fields import Small, SmallField, SmallerField, JSONField
 
 
 class MyModel(models.Model):
@@ -14,6 +14,9 @@ class MyModel(models.Model):
 
     def __unicode__(self):
         return force_unicode(self.name)
+
+class OtherModel(models.Model):
+    data = SmallerField()
 
 class DataModel(models.Model):
     data = JSONField()
