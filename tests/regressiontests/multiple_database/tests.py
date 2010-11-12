9892f4c5d58857c3fbbf45394b13c7bc0fce5161
Ian Kelly <ian.g.kelly@gmail.com>
2010-11-12 18:48:39 +0000

    Fixed a multiple_database test case that was failing in Oracle due to forced table name capitalization.  The test case now matches the style used by the raw_query tests.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@14545 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/multiple_database/tests.py
+++ tests/regressiontests/multiple_database/tests.py
@@ -853,10 +853,10 @@ class QueryTestCase(TestCase):
         "test the raw() method across databases"
         dive = Book.objects.using('other').create(title="Dive into Python",
             published=datetime.date(2009, 5, 4))
-        val = Book.objects.db_manager("other").raw('SELECT id FROM "multiple_database_book"')
+        val = Book.objects.db_manager("other").raw('SELECT id FROM multiple_database_book')
         self.assertEqual(map(lambda o: o.pk, val), [dive.pk])
 
-        val = Book.objects.raw('SELECT id FROM "multiple_database_book"').using('other')
+        val = Book.objects.raw('SELECT id FROM multiple_database_book').using('other')
         self.assertEqual(map(lambda o: o.pk, val), [dive.pk])
 
     def test_select_related(self):
