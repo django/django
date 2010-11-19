8d3e66fad61c96aa2269a2a3ca38fb3f5cab1189
Ramiro Morales <cramm0@gmail.com>
2010-11-19 23:23:17 +0000

    [1.2.X] Removed compatibility with Sphinx < 1.0 workarounds in our extension code. Also, started to use the 'needs_sphinx' Sphinx config var.
    Follow up on r14624.
    
    Backport of [14631] from trunk
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.2.X@14633 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "_ext"))
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+needs_sphinx = '1.0'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
