81a85a0c2b6741b82819687db6a5bc092d7c4508
Russell Keith-Magee <russell@keith-magee.com>
2010-12-04 07:05:04 +0000

    [1.2.X] Fixed #13190 -- Improved error handling for the case where no authentication backends are defined. Thanks to Joel3000 for the report, and Łukasz Rekucki for the final patch.
    Backport of r14793 from trunk.
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.2.X@14799 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/auth/tests/auth_backends.py
+++ django/contrib/auth/tests/auth_backends.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib.auth.models import User, Group, Permission, AnonymousUser
 from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ImproperlyConfigured
 from django.test import TestCase
 
 
@@ -245,3 +246,18 @@ class NoAnonymousUserBackendTest(TestCase):
 
     def test_get_all_permissions(self):
         self.assertEqual(self.user1.get_all_permissions(TestObj()), set())
+
+class NoBackendsTest(TestCase):
+    """
+    Tests that an appropriate error is raised if no auth backends are provided.
+    """
+    def setUp(self):
+        self.old_AUTHENTICATION_BACKENDS = settings.AUTHENTICATION_BACKENDS
+        settings.AUTHENTICATION_BACKENDS = []
+        self.user = User.objects.create_user('test', 'test@example.com', 'test')
+
+    def tearDown(self):
+        settings.AUTHENTICATION_BACKENDS = self.old_AUTHENTICATION_BACKENDS
+
+    def test_raises_exception(self):
+        self.assertRaises(ImproperlyConfigured, self.user.has_perm, ('perm', TestObj(),))
