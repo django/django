4adec427044dcc7b8cd8a11a6f53157e1d4854dd
Alex Gaynor <alex.gaynor@gmail.com>
2010-12-18 20:33:13 +0000

    Converted United Kingdom localflavor doctests into unittests.  We have always been at war with doctests.  Thanks to Idan Gazit.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@14953 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/forms/localflavor/uk.py
+++ tests/regressiontests/forms/localflavor/uk.py
@@ -1,66 +1,30 @@
-# -*- coding: utf-8 -*-
-# Tests for the contrib/localflavor/ UK form fields.
+from django.contrib.localflavor.uk.forms import UKPostcodeField
 
-tests = r"""
-# UKPostcodeField #############################################################
+from utils import LocalFlavorTestCase
 
-UKPostcodeField validates that the data is a valid UK postcode.
->>> from django.contrib.localflavor.uk.forms import UKPostcodeField
->>> f = UKPostcodeField()
->>> f.clean('BT32 4PX')
-u'BT32 4PX'
->>> f.clean('GIR 0AA')
-u'GIR 0AA'
->>> f.clean('BT324PX')
-u'BT32 4PX'
->>> f.clean('1NV 4L1D')
-Traceback (most recent call last):
-...
-ValidationError: [u'Enter a valid postcode.']
->>> f.clean('1NV4L1D')
-Traceback (most recent call last):
-...
-ValidationError: [u'Enter a valid postcode.']
->>> f.clean(None)
-Traceback (most recent call last):
-...
-ValidationError: [u'This field is required.']
->>> f.clean('')
-Traceback (most recent call last):
-...
-ValidationError: [u'This field is required.']
->>> f.clean(' so11aa ')
-u'SO1 1AA'
->>> f.clean(' so1  1aa ')
-u'SO1 1AA'
->>> f.clean('G2 3wt')
-u'G2 3WT'
->>> f.clean('EC1A 1BB')
-u'EC1A 1BB'
->>> f.clean('Ec1a1BB')
-u'EC1A 1BB'
->>> f.clean(' b0gUS')
-Traceback (most recent call last):
-...
-ValidationError: [u'Enter a valid postcode.']
->>> f = UKPostcodeField(required=False)
->>> f.clean('BT32 4PX')
-u'BT32 4PX'
->>> f.clean('GIR 0AA')
-u'GIR 0AA'
->>> f.clean('1NV 4L1D')
-Traceback (most recent call last):
-...
-ValidationError: [u'Enter a valid postcode.']
->>> f.clean('BT324PX')
-u'BT32 4PX'
->>> f.clean(None)
-u''
->>> f.clean('')
-u''
->>> f = UKPostcodeField(error_messages={'invalid': 'Enter a bloody postcode!'})
->>> f.clean('1NV 4L1D')
-Traceback (most recent call last):
-...
-ValidationError: [u'Enter a bloody postcode!']
-"""
+
+class UKLocalFlavorTests(LocalFlavorTestCase):
+    def test_UKPostcodeField(self):
+        error_invalid = [u'Enter a valid postcode.']
+        valid = {
+            'BT32 4PX': 'BT32 4PX',
+            'GIR 0AA': 'GIR 0AA',
+            'BT324PX': 'BT32 4PX',
+            ' so11aa ': 'SO1 1AA',
+            ' so1  1aa ': 'SO1 1AA',
+            'G2 3wt': 'G2 3WT',
+            'EC1A 1BB': 'EC1A 1BB',
+            'Ec1a1BB': 'EC1A 1BB',
+        }
+        invalid = {
+            '1NV 4L1D': error_invalid,
+            '1NV4L1D': error_invalid,
+            ' b0gUS': error_invalid,
+        }
+        self.assertFieldOutput(UKPostcodeField, valid, invalid)
+        valid = {}
+        invalid = {
+            '1NV 4L1D': [u'Enter a bloody postcode!'],
+        }
+        kwargs = {'error_messages': {'invalid': 'Enter a bloody postcode!'}}
+        self.assertFieldOutput(UKPostcodeField, valid, invalid, field_kwargs=kwargs)
--- tests/regressiontests/forms/tests/__init__.py
+++ tests/regressiontests/forms/tests/__init__.py
@@ -39,4 +39,5 @@ from regressiontests.forms.localflavortests import (
     ROLocalFlavorTests,
     SKLocalFlavorTests,
     TRLocalFlavorTests,
+    UKLocalFlavorTests,
 )
