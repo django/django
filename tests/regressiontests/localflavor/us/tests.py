6c54bb72f0b30adaf10c6d8507e7975ca83f0c04
Jannis Leidel <jannis@leidel.info>
2010-12-31 14:38:19 +0000

    Fixed the rest of the US localflavor tests that were forgotten in r15058. Refs r15029.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@15121 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/localflavor/us/tests.py
+++ tests/regressiontests/localflavor/us/tests.py
@@ -24,8 +24,12 @@ class USLocalflavorTests(TestCase):
 <option value="">---------</option>
 <option value="AL">Alabama</option>
 <option value="AK">Alaska</option>
+<option value="AS">American Samoa</option>
 <option value="AZ">Arizona</option>
 <option value="AR">Arkansas</option>
+<option value="AA">Armed Forces Americas</option>
+<option value="AE">Armed Forces Europe</option>
+<option value="AP">Armed Forces Pacific</option>
 <option value="CA">California</option>
 <option value="CO">Colorado</option>
 <option value="CT">Connecticut</option>
@@ -33,6 +37,7 @@ class USLocalflavorTests(TestCase):
 <option value="DC">District of Columbia</option>
 <option value="FL">Florida</option>
 <option value="GA" selected="selected">Georgia</option>
+<option value="GU">Guam</option>
 <option value="HI">Hawaii</option>
 <option value="ID">Idaho</option>
 <option value="IL">Illinois</option>
@@ -57,10 +62,12 @@ class USLocalflavorTests(TestCase):
 <option value="NY">New York</option>
 <option value="NC">North Carolina</option>
 <option value="ND">North Dakota</option>
+<option value="MP">Northern Mariana Islands</option>
 <option value="OH">Ohio</option>
 <option value="OK">Oklahoma</option>
 <option value="OR">Oregon</option>
 <option value="PA">Pennsylvania</option>
+<option value="PR">Puerto Rico</option>
 <option value="RI">Rhode Island</option>
 <option value="SC">South Carolina</option>
 <option value="SD">South Dakota</option>
@@ -68,19 +75,12 @@ class USLocalflavorTests(TestCase):
 <option value="TX">Texas</option>
 <option value="UT">Utah</option>
 <option value="VT">Vermont</option>
+<option value="VI">Virgin Islands</option>
 <option value="VA">Virginia</option>
 <option value="WA">Washington</option>
 <option value="WV">West Virginia</option>
 <option value="WI">Wisconsin</option>
 <option value="WY">Wyoming</option>
-<option value="AS">American Samoa</option>
-<option value="GU">Guam</option>
-<option value="MP">Northern Mariana Islands</option>
-<option value="PR">Puerto Rico</option>
-<option value="VI">Virgin Islands</option>
-<option value="AA">Armed Forces Americas</option>
-<option value="AE">Armed Forces Europe</option>
-<option value="AP">Armed Forces Pacific</option>
 </select>"""
         self.assertEqual(str(self.form['state']), state_select_html)
 
@@ -91,15 +91,21 @@ class USLocalflavorTests(TestCase):
 <option value="">---------</option>
 <option value="AL">Alabama</option>
 <option value="AK">Alaska</option>
+<option value="AS">American Samoa</option>
 <option value="AZ">Arizona</option>
 <option value="AR">Arkansas</option>
+<option value="AA">Armed Forces Americas</option>
+<option value="AE">Armed Forces Europe</option>
+<option value="AP">Armed Forces Pacific</option>
 <option value="CA">California</option>
 <option value="CO">Colorado</option>
 <option value="CT">Connecticut</option>
 <option value="DE">Delaware</option>
 <option value="DC">District of Columbia</option>
+<option value="FM">Federated States of Micronesia</option>
 <option value="FL">Florida</option>
 <option value="GA" selected="selected">Georgia</option>
+<option value="GU">Guam</option>
 <option value="HI">Hawaii</option>
 <option value="ID">Idaho</option>
 <option value="IL">Illinois</option>
@@ -109,6 +115,7 @@ class USLocalflavorTests(TestCase):
 <option value="KY">Kentucky</option>
 <option value="LA">Louisiana</option>
 <option value="ME">Maine</option>
+<option value="MH">Marshall Islands</option>
 <option value="MD">Maryland</option>
 <option value="MA">Massachusetts</option>
 <option value="MI">Michigan</option>
@@ -124,10 +131,13 @@ class USLocalflavorTests(TestCase):
 <option value="NY">New York</option>
 <option value="NC">North Carolina</option>
 <option value="ND">North Dakota</option>
+<option value="MP">Northern Mariana Islands</option>
 <option value="OH">Ohio</option>
 <option value="OK">Oklahoma</option>
 <option value="OR">Oregon</option>
+<option value="PW">Palau</option>
 <option value="PA">Pennsylvania</option>
+<option value="PR">Puerto Rico</option>
 <option value="RI">Rhode Island</option>
 <option value="SC">South Carolina</option>
 <option value="SD">South Dakota</option>
@@ -135,21 +145,11 @@ class USLocalflavorTests(TestCase):
 <option value="TX">Texas</option>
 <option value="UT">Utah</option>
 <option value="VT">Vermont</option>
+<option value="VI">Virgin Islands</option>
 <option value="VA">Virginia</option>
 <option value="WA">Washington</option>
 <option value="WV">West Virginia</option>
 <option value="WI">Wisconsin</option>
 <option value="WY">Wyoming</option>
-<option value="AS">American Samoa</option>
-<option value="GU">Guam</option>
-<option value="MP">Northern Mariana Islands</option>
-<option value="PR">Puerto Rico</option>
-<option value="VI">Virgin Islands</option>
-<option value="AA">Armed Forces Americas</option>
-<option value="AE">Armed Forces Europe</option>
-<option value="AP">Armed Forces Pacific</option>
-<option value="FM">Federated States of Micronesia</option>
-<option value="MH">Marshall Islands</option>
-<option value="PW">Palau</option>
 </select>"""
         self.assertEqual(str(self.form['postal_code']), usps_select_html)
