0958f32209d680ae8002852b62c560e04bc09118
Russell Keith-Magee <russell@keith-magee.com>
2011-01-18 16:55:00 +0000

    [1.2.X] Refs #14661 -- Clarified the handling of initial data injected via custom SQL.
    This is BACKWARDS INCOMPATIBLE CHANGE for anyone relying on SQL-injected initial data in a test case.
    
    Backport of r15239 from trunk.
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.2.X@15241 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/initial_sql_regress/tests.py
+++ tests/regressiontests/initial_sql_regress/tests.py
@@ -5,4 +5,11 @@ from models import Simple
 
 class InitialSQLTests(TestCase):
     def test_initial_sql(self):
-        self.assertEqual(Simple.objects.count(), 7)
+        # The format of the included SQL file for this test suite is important.
+        # It must end with a trailing newline in order to test the fix for #2161.
+
+        # However, as pointed out by #14661, test data loaded by custom SQL
+        # can't be relied upon; as a result, the test framework flushes the
+        # data contents before every test. This test validates that this has
+        # occurred.
+        self.assertEqual(Simple.objects.count(), 0)
