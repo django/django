09a63632c5072695d3b3293a046c197ea3c3299a
Alex Gaynor <alex.gaynor@gmail.com>
2011-01-24 15:18:56 +0000

    Simplify the tests for [15296].
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@15297 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/inspectdb/models.py
+++ tests/regressiontests/inspectdb/models.py
@@ -1,1 +1,8 @@
+from django.db import models
 
+
+class People(models.Model):
+    name = models.CharField(max_length=255)
+
+class Message(models.Model):
+    from_field = models.ForeignKey(People, db_column='from_id')
