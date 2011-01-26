9a317938ef2109da5e8d9cec5ee356d413e7ff47
Alex Gaynor <alex.gaynor@gmail.com>
2011-01-26 03:22:16 +0000

    Fixed #15154 -- removed doctests from the tests.py generated with createapp.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@15314 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/app_template/tests.py
+++ django/conf/app_template/tests.py
@@ -1,23 +1,16 @@
 """
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
+This file demonstrates writing tests using the unittest module. These will pass
+when you run "manage.py test".
 
-Replace these with more appropriate tests for your application.
+Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
         """
-        self.failUnlessEqual(1 + 1, 2)
-
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
-
+        self.assertEqual(1 + 1, 2)
