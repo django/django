0ffa39f744d339756b4743d240f32430b7c35cd4
Carl Meyer <carl@oddbird.net>
2011-02-04 17:37:25 +0000

    Refs #14675 - Added import of url() function in project template urls.py, added commented sample named url, and converted existing sample patterns to use url().
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@15415 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/project_template/urls.py
+++ django/conf/project_template/urls.py
@@ -1,16 +1,17 @@
-from django.conf.urls.defaults import patterns, include
+from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
+    # Examples:
+    # url(r'^$', '{{ project_name }}.views.home', name='home'),
+    # url(r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 )
