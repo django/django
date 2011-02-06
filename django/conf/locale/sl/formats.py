1291888153927072fb2c9ac735c42ce23a297668
Russell Keith-Magee <russell@keith-magee.com>
2011-02-06 06:07:06 +0000

    Fixed #15227 -- Updated Slovenian localization. Thanks to Gasper Zejn.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@15434 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/locale/sl/formats.py
+++ django/conf/locale/sl/formats.py
@@ -6,18 +6,44 @@
 # see http://docs.djangoproject.com/en/dev/ref/templates/builtins/#date
 DATE_FORMAT = 'd. F Y'
 TIME_FORMAT = 'H:i:s'
-# DATETIME_FORMAT = 
-# YEAR_MONTH_FORMAT = 
+DATETIME_FORMAT = 'j. F Y. H:i'
+YEAR_MONTH_FORMAT = 'F Y'
 MONTH_DAY_FORMAT = 'j. F'
 SHORT_DATE_FORMAT = 'j. M. Y'
-# SHORT_DATETIME_FORMAT = 
-# FIRST_DAY_OF_WEEK = 
+SHORT_DATETIME_FORMAT = 'j.n.Y. H:i'
+FIRST_DAY_OF_WEEK = 0
 
 # The *_INPUT_FORMATS strings use the Python strftime format syntax,
 # see http://docs.python.org/library/datetime.html#strftime-strptime-behavior
-# DATE_INPUT_FORMATS = 
-# TIME_INPUT_FORMATS = 
-# DATETIME_INPUT_FORMATS = 
+DATE_INPUT_FORMATS = (
+    '%d.%m.%Y', '%d.%m.%y',         # '25.10.2006', '25.10.06'
+    '%d-%m-%Y',                     # '25-10-2006'
+    '%d. %m. %Y', '%d. %m. %y',     # '25. 10. 2006', '25. 10. 06'
+)
+
+TIME_INPUT_FORMATS = (
+    '%H:%M:%S',     # '14:30:59'
+    '%H:%M',        # '14:30'
+)
+
+DATETIME_INPUT_FORMATS = (
+    '%d.%m.%Y %H:%M:%S',            # '25.10.2006 14:30:59'
+    '%d.%m.%Y %H:%M',               # '25.10.2006 14:30'
+    '%d.%m.%Y',                     # '25.10.2006'
+    '%d.%m.%y %H:%M:%S',            # '25.10.06 14:30:59'
+    '%d.%m.%y %H:%M',                # '25.10.06 14:30'
+    '%d.%m.%y',                     # '25.10.06'
+    '%d-%m-%Y %H:%M:%S',            # '25-10-2006 14:30:59'
+    '%d-%m-%Y %H:%M',               # '25-10-2006 14:30'
+    '%d-%m-%Y',                     # '25-10-2006'
+    '%d. %m. %Y %H:%M:%S',          # '25. 10. 2006 14:30:59'
+    '%d. %m. %Y %H:%M',             # '25. 10. 2006 14:30'
+    '%d. %m. %Y',                   # '25. 10. 2006'
+    '%d. %m. %y %H:%M:%S',          # '25. 10. 06 14:30:59'
+    '%d. %m. %y %H:%M',             # '25. 10. 06 14:30'
+    '%d. %m. %y',                   # '25. 10. 06'
+)
+
 DECIMAL_SEPARATOR = ','
 THOUSAND_SEPARATOR = '.'
-# NUMBER_GROUPING = 
+NUMBER_GROUPING = 3
