a797b7dba0644558ca7bf825b22268f2b4d9d3b5
Ramiro Morales <cramm0@gmail.com>
2011-02-13 01:56:12 +0000

    Fixed #14130 -- Made manage.py error reporting more useful when the settings.py file triggers import errors (in new projects). Thanks Setok for the report, mk and steph for their work.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@15522 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/project_template/manage.py
+++ django/conf/project_template/manage.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 from django.core.management import execute_manager
+import imp
 try:
-    import settings # Assumed to be in the same directory.
+    imp.find_module('settings') # Assumed to be in the same directory.
 except ImportError:
     import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
     sys.exit(1)
+
+import settings
 
 if __name__ == "__main__":
     execute_manager(settings)
