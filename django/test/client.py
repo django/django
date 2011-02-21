a6114e1a1442e9d87c3fdb073a34b75092794d21
Luke Plant <L.Plant.98@cantab.net>
2011-02-21 22:56:01 +0000

    [1.2.X] Fixed error that was introduced in [15601] causing lots of test failures on 1.2.X
    In 1.2.X and earlier, the test client adds the 'template' attribute to
    responses, while in trunk it adds 'templates', hence the confusion.
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.2.X@15622 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/test/client.py
+++ django/test/client.py
@@ -97,7 +97,7 @@ def store_rendered_templates(store, signal, sender, template, context, **kwargs)
     The context is copied so that it is an accurate representation at the time
     of rendering.
     """
-    store.setdefault('templates', []).append(template)
+    store.setdefault('template', []).append(template)
     store.setdefault('context', ContextList()).append(copy(context))
 
 def encode_multipart(boundary, data):
