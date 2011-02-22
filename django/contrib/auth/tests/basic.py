b9a20d1e3bacd9e461dd8b82c219b5100c4b7876
Russell Keith-Magee <russell@keith-magee.com>
2011-02-22 11:33:04 +0000

    Fixed #15371 -- Ensure that a superuser created with the createsuperuser management command with --noinput has an invalid password, not a blank password. Thanks to yishaibeeri for the report and patch.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@15631 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/auth/tests/basic.py
+++ django/contrib/auth/tests/basic.py
@@ -62,7 +62,9 @@ class BasicTestCase(TestCase):
         self.assertEqual(command_output, 'Superuser created successfully.')
         u = User.objects.get(username="joe")
         self.assertEquals(u.email, 'joe@somewhere.org')
-        self.assertTrue(u.check_password(''))
+
+        # created password should be unusable
+        self.assertFalse(u.has_usable_password())
 
         # We can supress output on the management command
         new_io = StringIO()
@@ -77,7 +79,8 @@ class BasicTestCase(TestCase):
         self.assertEqual(command_output, '')
         u = User.objects.get(username="joe2")
         self.assertEquals(u.email, 'joe2@somewhere.org')
-        self.assertTrue(u.check_password(''))
+        self.assertFalse(u.has_usable_password())
+
 
         new_io = StringIO()
         call_command("createsuperuser",
@@ -88,5 +91,5 @@ class BasicTestCase(TestCase):
         )
         u = User.objects.get(username="joe+admin@somewhere.org")
         self.assertEquals(u.email, 'joe@somewhere.org')
-        self.assertTrue(u.check_password(''))
+        self.assertFalse(u.has_usable_password())
 
