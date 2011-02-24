fa1a74ff3c7069c6e442b31c0c3842a24a01fc8a
Ramiro Morales <cramm0@gmail.com>
2011-02-24 01:09:23 +0000

    [1.2.X] Fixed #14012 (again) -- Admin app: Don't show the full user edition view after adding a user in a FK popup. Thanks dburke for reporting this regression introduced in r14628.
    Backport of [15637] from trunk.
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.2.X@15638 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/auth/admin.py
+++ django/contrib/auth/admin.py
@@ -142,9 +142,12 @@ class UserAdmin(admin.ModelAdmin):
         its superclass implementation but is customized because the User model
         has a slightly different workflow.
         """
-        if '_addanother' not in request.POST:
-            # The 'Save' button should act like the 'Save and continue
-            # editing' button
+        # We should allow further modification of the user just added i.e. the
+        # 'Save' button should behave like the 'Save and continue editing'
+        # button except in two scenarios:
+        # * The user has pressed the 'Save and add another' button
+        # * We are adding a user in a popup
+        if '_addanother' not in request.POST and '_popup' not in request.POST:
             request.POST['_continue'] = 1
         return super(UserAdmin, self).response_add(request, obj, post_url_continue)
 
