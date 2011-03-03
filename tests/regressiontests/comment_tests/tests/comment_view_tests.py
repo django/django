c0fb9bd00b16bf384b726adb3c0dec34fc90e615
Jannis Leidel <jannis@leidel.info>
2011-03-03 13:03:53 +0000

    Fixed #13411 -- Made sure URL fragments are correctly handled by the next_redirect utility of the comments apps. Thanks, timesong, dpn and Julien Phalip.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@15720 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/comment_tests/tests/comment_view_tests.py
+++ tests/regressiontests/comment_tests/tests/comment_view_tests.py
@@ -256,3 +256,27 @@ class CommentViewTests(CommentTestCase):
         broken_location = location + u"\ufffd"
         response = self.client.get(broken_location)
         self.assertEqual(response.status_code, 200)
+
+    def testCommentNextWithQueryStringAndAnchor(self):
+        """
+        The `next` key needs to handle already having an anchor. Refs #13411.
+        """
+        # With a query string also.
+        a = Article.objects.get(pk=1)
+        data = self.getValidData(a)
+        data["next"] = "/somewhere/else/?foo=bar#baz"
+        data["comment"] = "This is another comment"
+        response = self.client.post("/post/", data)
+        location = response["Location"]
+        match = re.search(r"^http://testserver/somewhere/else/\?foo=bar&c=\d+#baz$", location)
+        self.failUnless(match != None, "Unexpected redirect location: %s" % location)
+
+        # Without a query string
+        a = Article.objects.get(pk=1)
+        data = self.getValidData(a)
+        data["next"] = "/somewhere/else/#baz"
+        data["comment"] = "This is another comment"
+        response = self.client.post("/post/", data)
+        location = response["Location"]
+        match = re.search(r"^http://testserver/somewhere/else/\?c=\d+#baz$", location)
+        self.failUnless(match != None, "Unexpected redirect location: %s" % location)
