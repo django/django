5d6ee6cb18675000a97331e60f04ff1a394dac13
Adrian Holovaty <adrian@holovaty.com>
2011-03-28 02:13:05 +0000

    Removed Python 2.4 classifier from setup.py
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@15928 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- setup.py
+++ setup.py
@@ -89,7 +89,6 @@ setup(
                    'License :: OSI Approved :: BSD License',
                    'Operating System :: OS Independent',
                    'Programming Language :: Python',
-                   'Programming Language :: Python :: 2.4',
                    'Programming Language :: Python :: 2.5',
                    'Programming Language :: Python :: 2.6',
                    'Programming Language :: Python :: 2.7',
