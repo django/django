fe124152315ef21c0673b12ecd2d5f0bd6f271b2
Russell Keith-Magee <russell@keith-magee.com>
2011-04-02 08:33:40 +0000

    Removed deprecated contrib.csrf app.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@15971 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/csrf/middleware.py
+++
@@ -1,7 +0,0 @@
-from django.middleware.csrf import CsrfMiddleware, CsrfViewMiddleware, CsrfResponseMiddleware
-from django.views.decorators.csrf import csrf_exempt, csrf_view_exempt, csrf_response_exempt
-
-import warnings
-warnings.warn("This import for CSRF functionality is deprecated.  Please use django.middleware.csrf for the middleware and django.views.decorators.csrf for decorators.",
-              DeprecationWarning
-              )
