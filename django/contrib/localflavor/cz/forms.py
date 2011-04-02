c349aad4129ee115a0ce4832009bab56b3f10781
Russell Keith-Magee <russell@keith-magee.com>
2011-04-02 08:35:25 +0000

    Advanced deprecations in contrib.localflavor.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@15974 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/localflavor/cz/forms.py
+++ django/contrib/localflavor/cz/forms.py
@@ -73,7 +73,7 @@ class CZBirthNumberField(Field):
             import warnings
             warnings.warn(
                 "Support for validating the gender of a CZ Birth number has been deprecated.",
-                PendingDeprecationWarning)
+                DeprecationWarning)
             if gender == 'f':
                 female_const = 50
             elif gender == 'm':
