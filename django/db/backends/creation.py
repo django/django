d3c08fd8139416942da30bb22e529357e7708e2c
Ian Kelly <ian.g.kelly@gmail.com>
2011-04-15 21:49:22 +0000

    Fixed #15573: Forced the default site id to be 1 when creating test databases, to prevent a large number of errors when running the tests using the oracle backend.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@16027 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/db/backends/creation.py
+++ django/db/backends/creation.py
@@ -247,6 +247,14 @@ class BaseDatabaseCreation(object):
             verbosity=max(verbosity - 1, 0),
             interactive=False,
             database=self.connection.alias)
+        
+        # One effect of calling syncdb followed by flush is that the id of the
+        # default site may or may not be 1, depending on how the sequence was
+        # reset.  If the sites app is loaded, then we coerce it.
+        from django.db.models import get_model
+        Site = get_model('sites', 'Site')
+        if Site is not None and Site.objects.using(self.connection.alias).count() == 1:
+            Site.objects.using(self.connection.alias).update(id=settings.SITE_ID)
 
         from django.core.cache import get_cache
         from django.core.cache.backends.db import BaseDatabaseCache
