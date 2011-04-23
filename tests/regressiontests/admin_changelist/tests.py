django/django

commit 337b6786fdfcc7a8b2f8eced0de6b966ab5553de

Fixed #13902 -- Fixed admin list_filter so it doesn't show duplicate results when it includes a field spec that involves m2m relationships with an intermediate model. Thanks Iván Raskovsky for the report and patch.
git-svn-id: http://code.djangoproject.com/svn/django/trunk@15526 bcc190cf-cafb-0310-a4f2-bffc1f526a37

django/contrib/admin/views/main.py
@@ -169,6 +169,8 @@ class ChangeList(object):
         return order_field, order_type
 
     def get_query_set(self):
+        use_distinct = False
+
         qs = self.root_query_set
         lookup_params = self.params.copy() # a dictionary of the query string
         for i in (ALL_VAR, ORDER_VAR, ORDER_TYPE_VAR, SEARCH_VAR, IS_POPUP_VAR):
@@ -180,6 +182,17 @@ class ChangeList(object):
                 # requires it to be a string.
                 del lookup_params[key]
                 lookup_params[smart_str(key)] = value
+
+            if not use_distinct:
+                # Check if it's a relationship that might return more than one
+                # instance
+                field_name = key.split('__', 1)[0]
+                try:
+                    f = self.lookup_opts.get_field_by_name(field_name)[0]
+                except models.FieldDoesNotExist:
+                    raise IncorrectLookupParameters
+                if hasattr(f, 'rel') and isinstance(f.rel, models.ManyToManyRel):
+                    use_distinct = True
 
             # if key ends with __in, split parameter into separate values
             if key.endswith('__in'):
@@ -246,12 +259,18 @@ class ChangeList(object):
             for bit in self.query.split():
                 or_queries = [models.Q(**{construct_search(str(field_name)): bit}) for field_name in self.search_fields]
                 qs = qs.filter(reduce(operator.or_, or_queries))
-            for field_name in self.search_fields:
-                if '__' in field_name:
-                    qs = qs.distinct()
-                    break
+            if not use_distinct:
+                for search_field in self.search_fields:
+                    field_name = search_field.split('__', 1)[0]
+                    f = self.lookup_opts.get_field_by_name(field_name)[0]
+                    if hasattr(f, 'rel') and isinstance(f.rel, models.ManyToManyRel):
+                        use_distinct = True
+                        break
 
-        return qs
+        if use_distinct:
+            return qs.distinct()
+        else:
+            return qs
 
     def url_for_result(self, result):
         return "%s/" % quote(getattr(result, self.pk_attname))

tests/regressiontests/admin_changelist/tests.py
@@ -5,7 +5,8 @@ from django.core.paginator import Paginator
 from django.template import Context, Template
 from django.test import TransactionTestCase
 
-from regressiontests.admin_changelist.models import Child, Parent
+from models import (Child, Parent, Genre, Band, Musician, Group, Quartet,
+    Membership, ChordsMusician, ChordsBand, Invitation)
 
 
 class ChangeListTests(TransactionTestCase):
@@ -135,18 +136,122 @@ class ChangeListTests(TransactionTestCase):
         cl.get_results(request)
         self.assertIsInstance(cl.paginator, CustomPaginator)
 
+    def test_distinct_for_m2m_in_list_filter(self):
+        """
+        Regression test for #13902: When using a ManyToMany in list_filter,
+        results shouldn't apper more than once. Basic ManyToMany.
+        """
+        blues = Genre.objects.create(name='Blues')
+        band = Band.objects.create(name='B.B. King Review', nr_of_members=11)
+
+        band.genres.add(blues)
+        band.genres.add(blues)
+
+        m = BandAdmin(Band, admin.site)
+        cl = ChangeList(MockFilteredRequestA(), Band, m.list_display,
+                m.list_display_links, m.list_filter, m.date_hierarchy,
+                m.search_fields, m.list_select_related, m.list_per_page,
+                m.list_editable, m)
+
+        cl.get_results(MockFilteredRequestA())
+
+        # There's only one Group instance
+        self.assertEqual(cl.result_count, 1)
+
+    def test_distinct_for_through_m2m_in_list_filter(self):
+        """
+        Regression test for #13902: When using a ManyToMany in list_filter,
+        results shouldn't apper more than once. With an intermediate model.
+        """
+        lead = Musician.objects.create(name='Vox')
+        band = Group.objects.create(name='The Hype')
+        Membership.objects.create(group=band, music=lead, role='lead voice')
+        Membership.objects.create(group=band, music=lead, role='bass player')
+
+        m = GroupAdmin(Group, admin.site)
+        cl = ChangeList(MockFilteredRequestB(), Group, m.list_display,
+                m.list_display_links, m.list_filter, m.date_hierarchy,
+                m.search_fields, m.list_select_related, m.list_per_page,
+                m.list_editable, m)
+
+        cl.get_results(MockFilteredRequestB())
+
+        # There's only one Group instance
+        self.assertEqual(cl.result_count, 1)
+
+    def test_distinct_for_inherited_m2m_in_list_filter(self):
+        """
+        Regression test for #13902: When using a ManyToMany in list_filter,
+        results shouldn't apper more than once. Model managed in the
+        admin inherits from the one that defins the relationship.
+        """
+        lead = Musician.objects.create(name='John')
+        four = Quartet.objects.create(name='The Beatles')
+        Membership.objects.create(group=four, music=lead, role='lead voice')
+        Membership.objects.create(group=four, music=lead, role='guitar player')
+
+        m = QuartetAdmin(Quartet, admin.site)
+        cl = ChangeList(MockFilteredRequestB(), Quartet, m.list_display,
+                m.list_display_links, m.list_filter, m.date_hierarchy,
+                m.search_fields, m.list_select_related, m.list_per_page,
+                m.list_editable, m)
+
+        cl.get_results(MockFilteredRequestB())
+
+        # There's only one Quartet instance
+        self.assertEqual(cl.result_count, 1)
+
+    def test_distinct_for_m2m_to_inherited_in_list_filter(self):
+        """
+        Regression test for #13902: When using a ManyToMany in list_filter,
+        results shouldn't apper more than once. Target of the relationship
+        inherits from another.
+        """
+        lead = ChordsMusician.objects.create(name='Player A')
+        three = ChordsBand.objects.create(name='The Chords Trio')
+        Invitation.objects.create(band=three, player=lead, instrument='guitar')
+        Invitation.objects.create(band=three, player=lead, instrument='bass')
+
+        m = ChordsBandAdmin(ChordsBand, admin.site)
+        cl = ChangeList(MockFilteredRequestB(), ChordsBand, m.list_display,
+                m.list_display_links, m.list_filter, m.date_hierarchy,
+                m.search_fields, m.list_select_related, m.list_per_page,
+                m.list_editable, m)
+
+        cl.get_results(MockFilteredRequestB())
+
+        # There's only one ChordsBand instance
+        self.assertEqual(cl.result_count, 1)
+
 
 class ChildAdmin(admin.ModelAdmin):
     list_display = ['name', 'parent']
     def queryset(self, request):
         return super(ChildAdmin, self).queryset(request).select_related("parent__name")
 
-
 class MockRequest(object):
     GET = {}
-
 
 class CustomPaginator(Paginator):
     def __init__(self, queryset, page_size, orphans=0, allow_empty_first_page=True):
         super(CustomPaginator, self).__init__(queryset, 5, orphans=2,
             allow_empty_first_page=allow_empty_first_page)
+
+
+class BandAdmin(admin.ModelAdmin):
+    list_filter = ['genres']
+
+class GroupAdmin(admin.ModelAdmin):
+    list_filter = ['members']
+
+class QuartetAdmin(admin.ModelAdmin):
+    list_filter = ['members']
+
+class ChordsBandAdmin(admin.ModelAdmin):
+    list_filter = ['members']
+
+class MockFilteredRequestA(object):
+    GET = { 'genres': 1 }
+
+class MockFilteredRequestB(object):
+    GET = { 'members': 1 }

tests/regressiontests/admin_changelist/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib import admin
 
 class Parent(models.Model):
     name = models.CharField(max_length=128)
@@ -7,3 +6,44 @@ class Parent(models.Model):
 class Child(models.Model):
     parent = models.ForeignKey(Parent, editable=False, null=True)
     name = models.CharField(max_length=30, blank=True)
+
+class Genre(models.Model):
+    name = models.CharField(max_length=20)
+
+class Band(models.Model):
+    name = models.CharField(max_length=20)
+    nr_of_members = models.PositiveIntegerField()
+    genres = models.ManyToManyField(Genre)
+
+class Musician(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return self.name
+
+class Group(models.Model):
+    name = models.CharField(max_length=30)
+    members = models.ManyToManyField(Musician, through='Membership')
+
+    def __unicode__(self):
+        return self.name
+
+class Membership(models.Model):
+    music = models.ForeignKey(Musician)
+    group = models.ForeignKey(Group)
+    role = models.CharField(max_length=15)
+
+class Quartet(Group):
+    pass
+
+class ChordsMusician(Musician):
+    pass
+
+class ChordsBand(models.Model):
+    name = models.CharField(max_length=30)
+    members = models.ManyToManyField(ChordsMusician, through='Invitation')
+
+class Invitation(models.Model):
+    player = models.ForeignKey(ChordsMusician)
+    band = models.ForeignKey(ChordsBand)
+    instrument = models.CharField(max_length=15)

commit 065e6b6153a2656a9d60ca7da72236de0843e6a5

Fixed #15819 - Fixed 1.3 regression from r15526 causing duplicate search results in admin with search_fields traversing to non-M2M related models. Thanks to Adam Kochanowski for the report and Ryan Kaskel for the patch.
git-svn-id: http://code.djangoproject.com/svn/django/trunk@16093 bcc190cf-cafb-0310-a4f2-bffc1f526a37

django/contrib/admin/views/main.py
@@ -26,6 +26,15 @@ ERROR_FLAG = 'e'
 # Text to display within change-list table cells if the value is blank.
 EMPTY_CHANGELIST_VALUE = ugettext_lazy('(None)')
 
+def field_needs_distinct(field):
+    if ((hasattr(field, 'rel') and
+         isinstance(field.rel, models.ManyToManyRel)) or
+        (isinstance(field, models.related.RelatedObject) and
+         not field.field.unique)):
+         return True
+    return False
+
+
 class ChangeList(object):
     def __init__(self, request, model, list_display, list_display_links, list_filter, date_hierarchy, search_fields, list_select_related, list_per_page, list_editable, model_admin):
         self.model = model
@@ -189,8 +198,7 @@ class ChangeList(object):
                     f = self.lookup_opts.get_field_by_name(field_name)[0]
                 except models.FieldDoesNotExist:
                     raise IncorrectLookupParameters
-                if hasattr(f, 'rel') and isinstance(f.rel, models.ManyToManyRel):
-                    use_distinct = True
+                use_distinct = field_needs_distinct(f)
 
             # if key ends with __in, split parameter into separate values
             if key.endswith('__in'):
@@ -264,7 +272,7 @@ class ChangeList(object):
                 for search_spec in orm_lookups:
                     field_name = search_spec.split('__', 1)[0]
                     f = self.lookup_opts.get_field_by_name(field_name)[0]
-                    if hasattr(f, 'rel') and isinstance(f.rel, models.ManyToManyRel):
+                    if field_needs_distinct(f):
                         use_distinct = True
                         break
 

tests/regressiontests/admin_changelist/tests.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.admin.options import IncorrectLookupParameters
-from django.contrib.admin.views.main import ChangeList
+from django.contrib.admin.views.main import ChangeList, SEARCH_VAR
 from django.core.paginator import Paginator
 from django.template import Context, Template
 from django.test import TransactionTestCase
@@ -148,12 +148,14 @@ class ChangeListTests(TransactionTestCase):
         band.genres.add(blues)
 
         m = BandAdmin(Band, admin.site)
-        cl = ChangeList(MockFilteredRequestA(blues.pk), Band, m.list_display,
+        request = MockFilterRequest('genres', blues.pk)
+
+        cl = ChangeList(request, Band, m.list_display,
                 m.list_display_links, m.list_filter, m.date_hierarchy,
                 m.search_fields, m.list_select_related, m.list_per_page,
                 m.list_editable, m)
 
-        cl.get_results(MockFilteredRequestA(blues.pk))
+        cl.get_results(request)
 
         # There's only one Group instance
         self.assertEqual(cl.result_count, 1)
@@ -169,12 +171,14 @@ class ChangeListTests(TransactionTestCase):
         Membership.objects.create(group=band, music=lead, role='bass player')
 
         m = GroupAdmin(Group, admin.site)
-        cl = ChangeList(MockFilteredRequestB(lead.pk), Group, m.list_display,
+        request = MockFilterRequest('members', lead.pk)
+
+        cl = ChangeList(request, Group, m.list_display,
                 m.list_display_links, m.list_filter, m.date_hierarchy,
                 m.search_fields, m.list_select_related, m.list_per_page,
                 m.list_editable, m)
 
-        cl.get_results(MockFilteredRequestB(lead.pk))
+        cl.get_results(request)
 
         # There's only one Group instance
         self.assertEqual(cl.result_count, 1)
@@ -191,12 +195,14 @@ class ChangeListTests(TransactionTestCase):
         Membership.objects.create(group=four, music=lead, role='guitar player')
 
         m = QuartetAdmin(Quartet, admin.site)
-        cl = ChangeList(MockFilteredRequestB(lead.pk), Quartet, m.list_display,
+        request = MockFilterRequest('members', lead.pk)
+
+        cl = ChangeList(request, Quartet, m.list_display,
                 m.list_display_links, m.list_filter, m.date_hierarchy,
                 m.search_fields, m.list_select_related, m.list_per_page,
                 m.list_editable, m)
 
-        cl.get_results(MockFilteredRequestB(lead.pk))
+        cl.get_results(request)
 
         # There's only one Quartet instance
         self.assertEqual(cl.result_count, 1)
@@ -213,15 +219,58 @@ class ChangeListTests(TransactionTestCase):
         Invitation.objects.create(band=three, player=lead, instrument='bass')
 
         m = ChordsBandAdmin(ChordsBand, admin.site)
-        cl = ChangeList(MockFilteredRequestB(lead.pk), ChordsBand, m.list_display,
+        request = MockFilterRequest('members', lead.pk)
+
+        cl = ChangeList(request, ChordsBand, m.list_display,
                 m.list_display_links, m.list_filter, m.date_hierarchy,
                 m.search_fields, m.list_select_related, m.list_per_page,
                 m.list_editable, m)
 
-        cl.get_results(MockFilteredRequestB(lead.pk))
+        cl.get_results(request)
 
         # There's only one ChordsBand instance
         self.assertEqual(cl.result_count, 1)
+
+    def test_distinct_for_non_unique_related_object_in_list_filter(self):
+        """
+        Regressions tests for #15819: If a field listed in list_filters
+        is a non-unique related object, distinct() must be called.
+        """
+        parent = Parent.objects.create(name='Mary')
+        # Two children with the same name
+        Child.objects.create(parent=parent, name='Daniel')
+        Child.objects.create(parent=parent, name='Daniel')
+
+        m = ParentAdmin(Parent, admin.site)
+        request = MockFilterRequest('child__name', 'Daniel')
+
+        cl = ChangeList(request, Parent, m.list_display, m.list_display_links,
+                        m.list_filter, m.date_hierarchy, m.search_fields,
+                        m.list_select_related, m.list_per_page,
+                        m.list_editable, m)
+
+        # Make sure distinct() was called
+        self.assertEqual(cl.query_set.count(), 1)
+
+    def test_distinct_for_non_unique_related_object_in_search_fields(self):
+        """
+        Regressions tests for #15819: If a field listed in search_fields
+        is a non-unique related object, distinct() must be called.
+        """
+        parent = Parent.objects.create(name='Mary')
+        Child.objects.create(parent=parent, name='Danielle')
+        Child.objects.create(parent=parent, name='Daniel')
+
+        m = ParentAdmin(Parent, admin.site)
+        request = MockSearchRequest('daniel')
+
+        cl = ChangeList(request, Parent, m.list_display, m.list_display_links,
+                        m.list_filter, m.date_hierarchy, m.search_fields,
+                        m.list_select_related, m.list_per_page,
+                        m.list_editable, m)
+
+        # Make sure distinct() was called
+        self.assertEqual(cl.query_set.count(), 1)
 
     def test_pagination(self):
         """
@@ -252,6 +301,11 @@ class ChangeListTests(TransactionTestCase):
         self.assertEqual(cl.query_set.count(), 30)
         self.assertEqual(cl.paginator.count, 30)
         self.assertEqual(cl.paginator.page_range, [1, 2, 3])
+
+
+class ParentAdmin(admin.ModelAdmin):
+    list_filter = ['child__name']
+    search_fields = ['child__name']
 
 
 class ChildAdmin(admin.ModelAdmin):
@@ -288,10 +342,10 @@ class QuartetAdmin(admin.ModelAdmin):
 class ChordsBandAdmin(admin.ModelAdmin):
     list_filter = ['members']
 
-class MockFilteredRequestA(object):
-    def __init__(self, pk):
-        self.GET = { 'genres' : pk }
+class MockFilterRequest(object):
+    def __init__(self, filter, q):
+        self.GET = {filter: q}
 
-class MockFilteredRequestB(object):
-    def __init__(self, pk):
-        self.GET = { 'members': pk }
+class MockSearchRequest(object):
+    def __init__(self, q):
+        self.GET = {SEARCH_VAR: q}