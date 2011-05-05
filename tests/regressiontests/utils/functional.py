23b32c75545e1f48e059fae125f94314a99dcb89
Luke Plant <L.Plant.98@cantab.net>
2011-05-05 00:09:51 +0000

    Fixed #15811 - lazy() doesn't take into account methods defined in parents
    Thanks to abki for the report and patch.
    
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@16157 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/utils/functional.py
+++ tests/regressiontests/utils/functional.py
@@ -7,3 +7,16 @@ class FunctionalTestCase(unittest.TestCase):
         t = lazy(lambda: tuple(range(3)), list, tuple)
         for a, b in zip(t(), range(3)):
             self.assertEqual(a, b)
+
+    def test_lazy_base_class(self):
+        """Test that lazy also finds base class methods in the proxy object"""
+
+        class Base(object):
+            def base_method(self):
+                pass
+
+        class Klazz(Base):
+            pass
+
+        t = lazy(lambda: Klazz(), Klazz)()
+        self.assertTrue('base_method' in dir(t))
