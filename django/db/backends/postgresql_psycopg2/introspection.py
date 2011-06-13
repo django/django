479f2bce3c1dbfc53f37627ed751673b170dccc0
Jannis Leidel <jannis@leidel.info>
2011-06-13 20:15:04 +0000

    Removed duplicate entry in introspection PostgreSQL db backend.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@16394 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/db/backends/postgresql_psycopg2/introspection.py
+++ django/db/backends/postgresql_psycopg2/introspection.py
@@ -11,7 +11,6 @@ class DatabaseIntrospection(BaseDatabaseIntrospection):
         25: 'TextField',
         700: 'FloatField',
         701: 'FloatField',
-        869: 'IPAddressField',
         869: 'GenericIPAddressField',
         1043: 'CharField',
         1082: 'DateField',
