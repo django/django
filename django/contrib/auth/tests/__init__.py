230dab85c50e2e35781fe1a37b04ecffe6680003
Jannis Leidel <jannis@leidel.info>
2011-06-28 14:04:36 +0000

    Removed stale import from auth tests. Thanks, Ramiro.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@16483 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/auth/tests/__init__.py
+++ django/contrib/auth/tests/__init__.py
@@ -15,6 +15,5 @@ from django.contrib.auth.tests.signals import SignalTestCase
 from django.contrib.auth.tests.tokens import TokenGeneratorTest
 from django.contrib.auth.tests.views import (PasswordResetTest,
     ChangePasswordTest, LoginTest, LogoutTest, LoginURLSettings)
-from django.contrib.auth.tests.permissions import TestAuthPermissions
 
 # The password for the fixture data users is 'password'
