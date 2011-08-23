8b42dfa47e448ed4d6755b32425b1c33821f5776
Russell Keith-Magee <russell@keith-magee.com>
2011-08-23 15:57:19 +0000

    [1.3.X] Corrected the setup and teardown of the refactored invalid_models test so that it guarantees that stdout is restored, and purges all the temporary models from the app cache after running the test.
    Backport of r16670 from trunk.
    
    git-svn-id: http://code.djangoproject.com/svn/django/branches/releases/1.3.X@16677 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/modeltests/invalid_models/tests.py
+++ tests/modeltests/invalid_models/tests.py
@@ -1,3 +1,8 @@
+import copy
+
+from django.core.management.validation import get_validation_errors
+from django.db.models.loading import cache, load_app
+from cStringIO import StringIO
 import sys
 
 from django.utils import unittest
@@ -6,26 +11,36 @@ from django.utils import unittest
 class InvalidModelTestCase(unittest.TestCase):
     """Import an appliation with invalid models and test the exceptions."""
 
+    def setUp(self):
+        # Make sure sys.stdout is not a tty so that we get errors without
+        # coloring attached (makes matching the results easier). We restore
+        # sys.stderr afterwards.
+        self.old_stdout = sys.stdout
+        self.stdout = StringIO()
+        sys.stdout = self.stdout
+
+        # This test adds dummy applications to the app cache. These
+        # need to be removed in order to prevent bad interactions
+        # with the flush operation in other tests.
+        self.old_app_models = copy.deepcopy(cache.app_models)
+        self.old_app_store = copy.deepcopy(cache.app_store)
+
+    def tearDown(self):
+        cache.app_models = self.old_app_models
+        cache.app_store = self.old_app_store
+        cache._get_models_cache = {}
+        sys.stdout = self.old_stdout
+
     def test_invalid_models(self):
-        from django.core.management.validation import get_validation_errors
-        from django.db.models.loading import load_app
-        from cStringIO import StringIO
 
         try:
             module = load_app("modeltests.invalid_models.invalid_models")
         except Exception, e:
             self.fail('Unable to load invalid model module')
 
-        # Make sure sys.stdout is not a tty so that we get errors without
-        # coloring attached (makes matching the results easier). We restore
-        # sys.stderr afterwards.
-        orig_stdout = sys.stdout
-        s = StringIO()
-        sys.stdout = s
-        count = get_validation_errors(s, module)
-        sys.stdout = orig_stdout
-        s.seek(0)
-        error_log = s.read()
+        count = get_validation_errors(self.stdout, module)
+        self.stdout.seek(0)
+        error_log = self.stdout.read()
         actual = error_log.split('\n')
         expected = module.model_errors.split('\n')
 
@@ -33,5 +48,3 @@ class InvalidModelTestCase(unittest.TestCase):
         missing = [err for err in expected if err not in actual]
         self.assertFalse(unexpected, "Unexpected Errors: " + '\n'.join(unexpected))
         self.assertFalse(missing, "Missing Errors: " + '\n'.join(missing))
-
-
