a3fd9cf288e71b9c8e5fb6479eda3d33bc0a9d65
Julien Phalip <jphalip@gmail.com>
2011-08-24 12:30:59 +0000

    Fixed #15837. Consolidated all the locaflavor tests into a single, logical, place (regressiontests/localflavor/).
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@16680 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/localflavor/au/tests.py
+++ tests/regressiontests/localflavor/au/tests.py
@@ -1,6 +1,9 @@
 import re
 
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.contrib.localflavor.au.forms import (AUPostCodeField,
+    AUPhoneNumberField, AUStateSelect)
+
 from forms import AustralianPlaceForm
 
 SELECTED_OPTION_PATTERN = r'<option value="%s" selected="selected">'
@@ -8,7 +11,8 @@ BLANK_OPTION_PATTERN = r'<option value="">'
 INPUT_VALUE_PATTERN = r'<input[^>]*value="%s"[^>]*>'
 
 
-class AULocalflavorTests(TestCase):
+class AULocalflavorTests(SimpleTestCase):
+
     def setUp(self):
         self.form = AustralianPlaceForm(
             {'state':'WA',
@@ -56,3 +60,46 @@ class AULocalflavorTests(TestCase):
                                   str(self.form['postcode'])))
         self.assertTrue(re.search(INPUT_VALUE_PATTERN % '4321',
                                   str(self.form['postcode_required'])))
+
+    def test_AUStateSelect(self):
+        f = AUStateSelect()
+        out = u'''<select name="state">
+<option value="ACT">Australian Capital Territory</option>
+<option value="NSW" selected="selected">New South Wales</option>
+<option value="NT">Northern Territory</option>
+<option value="QLD">Queensland</option>
+<option value="SA">South Australia</option>
+<option value="TAS">Tasmania</option>
+<option value="VIC">Victoria</option>
+<option value="WA">Western Australia</option>
+</select>'''
+        self.assertEqual(f.render('state', 'NSW'), out)
+
+    def test_AUPostCodeField(self):
+        error_format = [u'Enter a 4 digit postcode.']
+        valid = {
+            '1234': '1234',
+            '2000': '2000',
+        }
+        invalid = {
+            'abcd': error_format,
+            '20001': [u'Ensure this value has at most 4 characters (it has 5).'] + error_format,
+        }
+        self.assertFieldOutput(AUPostCodeField, valid, invalid)
+
+    def test_AUPhoneNumberField(self):
+        error_format = [u'Phone numbers must contain 10 digits.']
+        valid = {
+            '1234567890': '1234567890',
+            '0213456789': '0213456789',
+            '02 13 45 67 89': '0213456789',
+            '(02) 1345 6789': '0213456789',
+            '(02) 1345-6789': '0213456789',
+            '(02)1345-6789': '0213456789',
+            '0408 123 456': '0408123456',
+        }
+        invalid = {
+            '123': error_format,
+            '1800DJANGO': error_format,
+        }
+        self.assertFieldOutput(AUPhoneNumberField, valid, invalid)
--- tests/regressiontests/localflavor/mk/tests.py
+++ tests/regressiontests/localflavor/mk/tests.py
@@ -1,7 +1,11 @@
-from django.test import TestCase
+from django.contrib.localflavor.mk.forms import (
+    MKIdentityCardNumberField, MKMunicipalitySelect, UMCNField)
+from django.test import SimpleTestCase
+
 from forms import MKPersonForm
 
-class MKLocalflavorTests(TestCase):
+class MKLocalflavorTests(SimpleTestCase):
+
     def setUp(self):
         self.form = MKPersonForm({
             'first_name':'Someone',
@@ -174,3 +178,127 @@ class MKLocalflavorTests(TestCase):
 <option value="SO">\xc5\xa0uto Orizari</option>
 </select>"""
         self.assertEqual(str(self.form['municipality']), municipality_select_html)
+
+    def test_MKIdentityCardNumberField(self):
+        error_invalid  = [u'Identity card numbers must contain either 4 to 7 '
+                          'digits or an uppercase letter and 7 digits.']
+        valid = {
+            'L0018077':'L0018077',
+            'A0078315' : 'A0078315',
+        }
+        invalid = {
+            '123': error_invalid,
+            'abcdf': error_invalid,
+            '234390a': error_invalid,
+        }
+        self.assertFieldOutput(MKIdentityCardNumberField, valid, invalid)
+
+    def test_MKMunicipalitySelect(self):
+        f = MKMunicipalitySelect()
+        out=u'''<select name="municipality">
+<option value="AD">Aerodrom</option>
+<option value="AR">Ara\u010dinovo</option>
+<option value="BR">Berovo</option>
+<option value="TL">Bitola</option>
+<option value="BG">Bogdanci</option>
+<option value="VJ">Bogovinje</option>
+<option value="BS">Bosilovo</option>
+<option value="BN">Brvenica</option>
+<option value="BU">Butel</option>
+<option value="VA">Valandovo</option>
+<option value="VL">Vasilevo</option>
+<option value="VV">Vev\u010dani</option>
+<option value="VE">Veles</option>
+<option value="NI">Vinica</option>
+<option value="VC">Vrane\u0161tica</option>
+<option value="VH">Vrap\u010di\u0161te</option>
+<option value="GB">Gazi Baba</option>
+<option value="GV">Gevgelija</option>
+<option value="GT">Gostivar</option>
+<option value="GR">Gradsko</option>
+<option value="DB">Debar</option>
+<option value="DA">Debarca</option>
+<option value="DL" selected="selected">Del\u010devo</option>
+<option value="DK">Demir Kapija</option>
+<option value="DM">Demir Hisar</option>
+<option value="DE">Dolneni</option>
+<option value="DR">Drugovo</option>
+<option value="GP">Gjor\u010de Petrov</option>
+<option value="ZE">\u017delino</option>
+<option value="ZA">Zajas</option>
+<option value="ZK">Zelenikovo</option>
+<option value="ZR">Zrnovci</option>
+<option value="IL">Ilinden</option>
+<option value="JG">Jegunovce</option>
+<option value="AV">Kavadarci</option>
+<option value="KB">Karbinci</option>
+<option value="KX">Karpo\u0161</option>
+<option value="VD">Kisela Voda</option>
+<option value="KH">Ki\u010devo</option>
+<option value="KN">Kon\u010de</option>
+<option value="OC">Ko\u0107ani</option>
+<option value="KY">Kratovo</option>
+<option value="KZ">Kriva Palanka</option>
+<option value="KG">Krivoga\u0161tani</option>
+<option value="KS">Kru\u0161evo</option>
+<option value="UM">Kumanovo</option>
+<option value="LI">Lipkovo</option>
+<option value="LO">Lozovo</option>
+<option value="MR">Mavrovo i Rostu\u0161a</option>
+<option value="MK">Makedonska Kamenica</option>
+<option value="MD">Makedonski Brod</option>
+<option value="MG">Mogila</option>
+<option value="NG">Negotino</option>
+<option value="NV">Novaci</option>
+<option value="NS">Novo Selo</option>
+<option value="OS">Oslomej</option>
+<option value="OD">Ohrid</option>
+<option value="PE">Petrovec</option>
+<option value="PH">Peh\u010devo</option>
+<option value="PN">Plasnica</option>
+<option value="PP">Prilep</option>
+<option value="PT">Probi\u0161tip</option>
+<option value="RV">Radovi\u0161</option>
+<option value="RN">Rankovce</option>
+<option value="RE">Resen</option>
+<option value="RO">Rosoman</option>
+<option value="AJ">Saraj</option>
+<option value="SL">Sveti Nikole</option>
+<option value="SS">Sopi\u0161te</option>
+<option value="SD">Star Dojran</option>
+<option value="NA">Staro Nagori\u010dane</option>
+<option value="UG">Struga</option>
+<option value="RU">Strumica</option>
+<option value="SU">Studeni\u010dani</option>
+<option value="TR">Tearce</option>
+<option value="ET">Tetovo</option>
+<option value="CE">Centar</option>
+<option value="CZ">Centar-\u017dupa</option>
+<option value="CI">\u010cair</option>
+<option value="CA">\u010ca\u0161ka</option>
+<option value="CH">\u010ce\u0161inovo-Oble\u0161evo</option>
+<option value="CS">\u010cu\u010der-Sandevo</option>
+<option value="ST">\u0160tip</option>
+<option value="SO">\u0160uto Orizari</option>
+</select>'''
+        self.assertEqual(f.render('municipality', 'DL' ), out)
+
+    def test_UMCNField(self):
+        error_invalid = [u'This field should contain exactly 13 digits.']
+        error_checksum = [u'The UMCN is not valid.']
+        error_date =  [u'The first 7 digits of the UMCN '
+                        'must represent a valid past date.']
+        valid = {
+            '2402983450006': '2402983450006',
+            '2803984430038': '2803984430038',
+            '1909982045004': '1909982045004',
+        }
+        invalid = {
+            '240298345': error_invalid,
+            'abcdefghj': error_invalid,
+            '2402082450006': error_date,
+            '3002982450006': error_date,
+            '2402983450007': error_checksum,
+            '2402982450006': error_checksum,
+        }
+        self.assertFieldOutput(UMCNField, valid, invalid)
