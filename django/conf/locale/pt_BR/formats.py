414c762c3abfebb2670a3b26aba7028d62303cfe
Jannis Leidel <jannis@leidel.info>
2011-09-08 13:24:49 +0000

    Fixed #16349 -- Fixed Brazilian localization formats. Thanks, Claude Paroz, semente and Guiljerme Gondim.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@16724 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/locale/pt_BR/formats.py
+++ django/conf/locale/pt_BR/formats.py
@@ -4,9 +4,9 @@
 
 # The *_FORMAT strings use the Django date format syntax,
 # see http://docs.djangoproject.com/en/dev/ref/templates/builtins/#date
-DATE_FORMAT = r'j \de N \de Y'
+DATE_FORMAT = r'j \de F \de Y'
 TIME_FORMAT = 'H:i'
-DATETIME_FORMAT = r'j \de N \de Y à\s H:i'
+DATETIME_FORMAT = r'j \de F \de Y à\s H:i'
 YEAR_MONTH_FORMAT = r'F \de Y'
 MONTH_DAY_FORMAT = r'j \de F'
 SHORT_DATE_FORMAT = 'd/m/Y'
@@ -16,7 +16,7 @@ FIRST_DAY_OF_WEEK = 0  # Sunday
 # The *_INPUT_FORMATS strings use the Python strftime format syntax,
 # see http://docs.python.org/library/datetime.html#strftime-strptime-behavior
 DATE_INPUT_FORMATS = (
-    '%Y-%m-%d', '%d/%m/%Y', '%d/%m/%y', # '2006-10-25', '25/10/2006', '25/10/06'
+    '%d/%m/%Y', '%d/%m/%y', '%Y-%m-%d', # '25/10/2006', '25/10/06', '2006-10-25'
     # '%d de %b de %Y', '%d de %b, %Y',   # '25 de Out de 2006', '25 Out, 2006'
     # '%d de %B de %Y', '%d de %B, %Y',   # '25 de Outubro de 2006', '25 de Outubro, 2006'
 )
