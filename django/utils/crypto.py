eeb63b68b85f497c8a2f276ce5977c4fcedc15a5
Yarko Tymciurak <yarkot@uchicago.edu>
2011-09-10 15:27:47 -0700

    removed base62 function (we are using baseconv.py utilities instead)
    Conflicts:
    	django/utils/crypto.py
    
    Broken. Missing _build_token
    
    Signed-off-by: Rohan Jain <crodjer@gmail.com>

--- django/utils/crypto.py
+++ django/utils/crypto.py
@@ -56,23 +56,6 @@ ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 DEFAULT_TOKEN_LENGTH = 32
 
 
-def base62_encode(num, alphabet=ALPHABET):
-    """Encode a number in Base X
-
-    `num`: The number to encode
-    `alphabet`: The alphabet to use for encoding
-    """
-    if (num == 0):
-        return alphabet[0]
-    arr = []
-    base = len(alphabet)
-    while num:
-        rem = num % base
-        num = num // base
-        arr.append(alphabet[rem])
-    arr.reverse()
-    return ''.join(arr)
-
 class RandomToken():
     """
     Object that creates randomized token.
