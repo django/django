5cf18e9026642600b9c874ee8fccd36cb1165c3f
Yarko Tymciurak <yarkot@uchicago.edu>
2011-09-12 01:23:24 -0500

    fixed conversion typo; updated test script; all hashlib changes, select md5, but not all sh1;  tests now failing (next step)  (WIP)
    Conflicts:
    	django/middleware/csrf.py
    
    Still not using tokens in CSRF
    
    Signed-off-by: Rohan Jain <crodjer@gmail.com>

--- tests/testset.sh
+++ tests/testset.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 # - 60 second test subset, for security group to use prior to pushing commits
+# There is no 'develop' mode for python tree - be sure to install in the virtualenv:
+(cd ..; python setup.py install)
 TEST_SET="admin_registration admin_util admin_validation admin_views backends cache csrf_tests file_storage inspectdb generic_inline_admin managers_regress middleware model_permalink modeladmin queryset_pickle serializers_regress signed_cookies_tests signing transactions_regress utils"
 ./runtests.py --settings=test_sqlite $TEST_SET
