e5aeba018f9e6b5793612ad47a1167b8101b1973
Justin Bronn <jbronn@gmail.com>
2011-09-18 22:28:17 +0000

    Updated geoadmin tests to reflect that OSM js is no longer required as of r16847.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@16853 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/gis/tests/geoadmin/tests.py
+++ django/contrib/gis/tests/geoadmin/tests.py
@@ -8,7 +8,5 @@ class GeoAdminTest(TestCase):
     def test01_ensure_geographic_media(self):
         geoadmin = admin.site._registry[City]
         admin_js = geoadmin.media.render_js()
-        osm_url = geoadmin.extra_js[0]
         self.assertTrue(any([geoadmin.openlayers_url in js for js in admin_js]))
-        self.assertTrue(any([osm_url in js for js in admin_js]))
         
