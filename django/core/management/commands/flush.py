d0979a5faf4fdf86650567540f623e4bbc68e09a
Paul McMillan <Paul@McMillan.ws>
2011-09-22 05:21:51 +0000

    Fixed #6011 -- Improved help text for flush command. Thanks Julien for the patch.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@16877 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/core/management/commands/flush.py
+++ django/core/management/commands/flush.py
@@ -17,7 +17,10 @@ class Command(NoArgsCommand):
             default=DEFAULT_DB_ALIAS, help='Nominates a database to flush. '
                 'Defaults to the "default" database.'),
     )
-    help = "Executes ``sqlflush`` on the current database."
+    help = ('Returns the database to the state it was in immediately after '
+           'syncdb was executed. This means that all data will be removed '
+           'from the database, any post-synchronization handlers will be '
+           're-executed, and the initial_data fixture will be re-installed.')
 
     def handle_noargs(self, **options):
         db = options.get('database', DEFAULT_DB_ALIAS)
