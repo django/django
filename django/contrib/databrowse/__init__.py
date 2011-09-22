5a01324f20305213cb4429b8ef4c376fe19a7fd7
Paul McMillan <Paul@McMillan.ws>
2011-09-22 04:10:02 +0000

    Fixed #16907 -- Deprecate databrowse.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@16871 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/databrowse/__init__.py
+++ django/contrib/databrowse/__init__.py
@@ -1,1 +1,6 @@
+import warnings
 from django.contrib.databrowse.sites import DatabrowsePlugin, ModelDatabrowse, DatabrowseSite, site
+
+
+warnings.warn("The Databrowse contrib app is deprecated",
+    PendingDeprecationWarning)
