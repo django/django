01964fd7e4ecd1998a2288e1759a63bd9a5fb2df
Aymeric Augustin <aymeric.augustin@m4x.org>
2011-10-31 18:54:24 +0000

    Fixed an obsolete comment.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@17060 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/contrib/gis/gdal/field.py
+++ django/contrib/gis/gdal/field.py
@@ -144,7 +144,7 @@ class OFTDateTime(Field):
     def value(self):
         "Returns a Python `datetime` object for this OFTDateTime field."
         # TODO: Adapt timezone information.
-        #  See http://lists.maptools.org/pipermail/gdal-dev/2006-February/007990.html
+        #  See http://lists.osgeo.org/pipermail/gdal-dev/2006-February/007990.html
         #  The `tz` variable has values of: 0=unknown, 1=localtime (ambiguous),
         #  100=GMT, 104=GMT+1, 80=GMT-5, etc.
         try:
