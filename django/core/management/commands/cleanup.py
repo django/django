119e188717db82d3ce661fb5d1e0d3df1c91eb3a
Aymeric Augustin <aymeric.augustin@m4x.org>
2011-11-20 11:04:37 +0000

    Upgraded manage.py cleanup to be compatible with time zone support. See also r17121.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@17124 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/core/management/commands/cleanup.py
+++ django/core/management/commands/cleanup.py
@@ -1,5 +1,5 @@
-import datetime
 from django.core.management.base import NoArgsCommand
+from django.utils import timezone
 
 class Command(NoArgsCommand):
     help = "Can be run as a cronjob or directly to clean out old data from the database (only expired sessions at the moment)."
@@ -7,5 +7,5 @@ class Command(NoArgsCommand):
     def handle_noargs(self, **options):
         from django.db import transaction
         from django.contrib.sessions.models import Session
-        Session.objects.filter(expire_date__lt=datetime.datetime.now()).delete()
+        Session.objects.filter(expire_date__lt=timezone.now()).delete()
         transaction.commit_unless_managed()
