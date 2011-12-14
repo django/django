46c1d1551fce77323b3de477fe63aab0dbcf0257
Julien Phalip <jphalip@gmail.com>
2011-12-14 14:49:00 +0000

    Fixed a url to the contributing guide in a code comment.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@17201 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/test_sqlite.py
+++ tests/test_sqlite.py
@@ -5,7 +5,7 @@
 # NAME and potentially authentication information. See the
 # following section in the docs for more information:
 #
-# http://docs.djangoproject.com/en/dev/internals/contributing/#unit-tests
+# https://docs.djangoproject.com/en/dev/internals/contributing/writing-code/unit-tests/
 #
 # The different databases that Django supports behave differently in certain
 # situations, so it is recommended to run the test suite against as many
