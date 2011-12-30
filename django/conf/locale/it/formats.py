e5999ce234c2e2af05ba5ca5f7e44f35c864603e
Aymeric Augustin <aymeric.augustin@m4x.org>
2011-12-30 12:41:24 +0000

    Fixed #17440 -- Used a more natural default date format in the Italian locale. Thanks rm_ and tekNico.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@17292 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/conf/locale/it/formats.py
+++ django/conf/locale/it/formats.py
@@ -16,8 +16,8 @@ FIRST_DAY_OF_WEEK = 1 # Lunedì
 # The *_INPUT_FORMATS strings use the Python strftime format syntax,
 # see http://docs.python.org/library/datetime.html#strftime-strptime-behavior
 DATE_INPUT_FORMATS = (
-    '%Y-%m-%d', '%Y/%m/%d',  # '2008-10-25', '2008/10/25'
-    '%d-%m-%Y', '%d/%m/%Y',  # '25-10-2006', '25/10/2006'
+    '%d/%m/%Y', '%Y/%m/%d',  # '25/10/2006', '2008/10/25'
+    '%d-%m-%Y', '%Y-%m-%d',  # '25-10-2006', '2008-10-25'
     '%d-%m-%y', '%d/%m/%y',  # '25-10-06', '25/10/06'
 )
 TIME_INPUT_FORMATS = (
@@ -25,6 +25,12 @@ TIME_INPUT_FORMATS = (
     '%H:%M',        # '14:30'
 )
 DATETIME_INPUT_FORMATS = (
+    '%d/%m/%Y %H:%M:%S',     # '25/10/2006 14:30:59'
+    '%d/%m/%Y %H:%M',        # '25/10/2006 14:30'
+    '%d/%m/%Y',              # '25/10/2006'
+    '%d/%m/%y %H:%M:%S',     # '25/10/06 14:30:59'
+    '%d/%m/%y %H:%M',        # '25/10/06 14:30'
+    '%d/%m/%y',              # '25/10/06'
     '%Y-%m-%d %H:%M:%S',     # '2006-10-25 14:30:59'
     '%Y-%m-%d %H:%M',        # '2006-10-25 14:30'
     '%Y-%m-%d',              # '2006-10-25'
@@ -34,12 +40,6 @@ DATETIME_INPUT_FORMATS = (
     '%d-%m-%y %H:%M:%S',     # '25-10-06 14:30:59'
     '%d-%m-%y %H:%M',        # '25-10-06 14:30'
     '%d-%m-%y',              # '25-10-06'
-    '%d/%m/%Y %H:%M:%S',     # '25/10/2006 14:30:59'
-    '%d/%m/%Y %H:%M',        # '25/10/2006 14:30'
-    '%d/%m/%Y',              # '25/10/2006'
-    '%d/%m/%y %H:%M:%S',     # '25/10/06 14:30:59'
-    '%d/%m/%y %H:%M',        # '25/10/06 14:30'
-    '%d/%m/%y',              # '25/10/06'
 )
 DECIMAL_SEPARATOR = ','
 THOUSAND_SEPARATOR = '.'
