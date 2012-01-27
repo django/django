1d54e3a95676e04ef3bcf7a251ba5a425d7e583f
Aymeric Augustin <aymeric.augustin@m4x.org>
2012-01-27 22:25:44 +0000

    Fixed #15828 -- Removed explicit implementation of empty setUp / tearDown methods. Backport of http://hg.python.org/unittest2/rev/3d33f92496fa.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@17398 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/utils/unittest/case.py
+++ django/utils/unittest/case.py
@@ -241,9 +241,6 @@ class TestCase(unittest.TestCase):
         Cleanup items are called even if setUp fails (unlike tearDown)."""
         self._cleanups.append((function, args, kwargs))
 
-    def setUp(self):
-        "Hook method for setting up the test fixture before exercising it."
-
     @classmethod
     def setUpClass(cls):
         "Hook method for setting up class fixture before running tests in the class."
@@ -251,9 +248,6 @@ class TestCase(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         "Hook method for deconstructing the class fixture after running all tests in the class."
-
-    def tearDown(self):
-        "Hook method for deconstructing the test fixture after testing it."
 
     def countTestCases(self):
         return 1
