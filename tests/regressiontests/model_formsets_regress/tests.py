878cc62bd49db0800e132051252b368acdeaf55a
Jannis Leidel <jannis@leidel.info>
2012-02-04 18:27:32 +0000

    Added with_statement import forgotten in r17434. Thanks to Julien for noticing.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@17442 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- tests/regressiontests/model_formsets_regress/tests.py
+++ tests/regressiontests/model_formsets_regress/tests.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import
+from __future__ import absolute_import, with_statement
 
 from django import forms
 from django.forms.formsets import BaseFormSet, DELETION_FIELD_NAME
