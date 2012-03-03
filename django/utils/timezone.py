7ce1ad7c0364102523e4696a6926c099b7f0584e
Aymeric Augustin <aymeric.augustin@m4x.org>
2012-03-03 19:57:25 +0000

    Made django.utils.timezone.localtime a private API -- it's too specific to the template engine.
    git-svn-id: http://code.djangoproject.com/svn/django/trunk@17642 bcc190cf-cafb-0310-a4f2-bffc1f526a37

--- django/utils/timezone.py
+++ django/utils/timezone.py
@@ -17,7 +17,7 @@ from django.conf import settings
 __all__ = [
     'utc', 'get_default_timezone', 'get_current_timezone',
     'activate', 'deactivate', 'override',
-    'localtime', 'is_naive', 'is_aware', 'make_aware', 'make_naive',
+    'is_naive', 'is_aware', 'make_aware', 'make_naive',
 ]
 
 
@@ -204,7 +204,7 @@ class override(object):
             del _active.value
 
 
-# Utilities
+# Templates
 
 def localtime(value, use_tz=None):
     """
@@ -212,6 +212,8 @@ def localtime(value, use_tz=None):
 
     If use_tz is provided and is not None, that will force the value to
     be converted (or not), overriding the value of settings.USE_TZ.
+
+    This function is designed for use by the template engine.
     """
     if (isinstance(value, datetime)
         and (settings.USE_TZ if use_tz is None else use_tz)
@@ -223,6 +225,9 @@ def localtime(value, use_tz=None):
             # available for pytz time zones
             value = timezone.normalize(value)
     return value
+
+
+# Utilities
 
 def now():
     """
