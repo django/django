bf611f14ec13312aa822beec72c63bd04950613c
Anssi Kääriäinen <akaariai@gmail.com>
2012-04-29 19:48:43 +0300

    [1.4.x] Fixed #20905 -- Fixed an Oracle-specific test case failure
    Made a test checking ORM-generated query string case-insensitive.
    
    Backport of ee0a7c741e from master

--- tests/modeltests/prefetch_related/tests.py
+++ tests/modeltests/prefetch_related/tests.py
@@ -364,7 +364,9 @@ class MultiTableInheritanceTest(TestCase):
             l = [a.authorwithage for a in Author.objects.prefetch_related('authorwithage')]
 
         # Regression for #18090: the prefetching query must include an IN clause.
-        self.assertIn('authorwithage', connection.queries[-1]['sql'])
+        # Note that on Oracle the table name is upper case in the generated SQL,
+        # thus the .lower() call.
+        self.assertIn('authorwithage', connection.queries[-1]['sql'].lower())
         self.assertIn(' IN ', connection.queries[-1]['sql'])
 
         self.assertEqual(l, [a.authorwithage for a in Author.objects.all()])
