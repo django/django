02a5b41db4ff8544f93a5d9854b346a9aae4f556
Aymeric Augustin <aymeric.augustin@m4x.org>
2012-04-29 13:40:10 +0200

    Fixed #18224 -- Changed the dev version number.
    Following the move from SVN to git.

--- django/__init__.py
+++ django/__init__.py
@@ -1,4 +1,10 @@
+import datetime
+import os
+import subprocess
+
+
 VERSION = (1, 5, 0, 'alpha', 0)
+
 
 def get_version(version=None):
     """Derives a PEP386-compliant version number from VERSION."""
@@ -17,14 +23,31 @@ def get_version(version=None):
 
     sub = ''
     if version[3] == 'alpha' and version[4] == 0:
-        # At the toplevel, this would cause an import loop.
-        from django.utils.version import get_svn_revision
-        svn_revision = get_svn_revision()[4:]
-        if svn_revision != 'unknown':
-            sub = '.dev%s' % svn_revision
+        git_changeset = get_git_changeset()
+        if git_changeset:
+            sub = '.dev%s' % git_changeset
 
     elif version[3] != 'final':
         mapping = {'alpha': 'a', 'beta': 'b', 'rc': 'c'}
         sub = mapping[version[3]] + str(version[4])
 
     return main + sub
+
+
+def get_git_changeset():
+    """Returns a numeric identifier of the latest git changeset.
+
+    The result is the UTC timestamp of the changeset in YYYYMMDDHHMMSS format.
+    This value isn't guaranteed to be unique but collisions are very unlikely,
+    so it's sufficient for generating the development version numbers.
+    """
+    repo_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    git_show = subprocess.Popen('git show --pretty=format:%ct --quiet HEAD',
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+            shell=True, cwd=repo_dir, universal_newlines=True)
+    timestamp = git_show.communicate()[0].partition('\n')[0]
+    try:
+        timestamp = datetime.datetime.utcfromtimestamp(int(timestamp))
+    except ValueError:
+        return None
+    return timestamp.strftime('%Y%m%d%H%M%S')
--- tests/regressiontests/version/tests.py
+++ tests/regressiontests/version/tests.py
@@ -8,7 +8,7 @@ class VersionTests(TestCase):
     def test_development(self):
         ver_tuple = (1, 4, 0, 'alpha', 0)
         # This will return a different result when it's run within or outside
-        # of a SVN checkout: 1.4.devNNNNN or 1.4.
+        # of a git clone: 1.4.devYYYYMMDDHHMMSS or 1.4.
         ver_string = get_version(ver_tuple)
         self.assertRegexpMatches(ver_string, r'1\.4(\.dev\d+)?')
 
