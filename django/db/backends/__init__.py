b0bdaf4b88b96d182d2a7b64a383000566c978b2
Merlijn van Deen <valhallasw@gmail.com>
2012-05-16 12:36:30 +0200

    Fix for #13844: convert using Field.to_python
    Instead of adding another option in convert_value, I think it is
    more sensible (and certainly more friendly to third party Fields)
    to use functionality in Fields instead of in the database layer.
    
    This version passes all current aggregation and aggregation_regress
    tests under both sqlite and postgresql. Note that sqlite currently
    overrides convert_value, which means it does *not* use to_python
    as fields define them.

--- django/db/backends/__init__.py
+++ django/db/backends/__init__.py
@@ -844,16 +844,7 @@ class BaseDatabaseOperations(object):
         """Coerce the value returned by the database backend into a consistent type that
         is compatible with the field type.
         """
-        internal_type = field.get_internal_type()
-        if internal_type == 'DecimalField':
-            return value
-        elif internal_type and internal_type.endswith('IntegerField') or internal_type == 'AutoField':
-            return int(value)
-        elif internal_type in ('DateField', 'DateTimeField', 'TimeField'):
-            return value
-        # No field, or the field isn't known to be a decimal or integer
-        # Default to a float
-        return float(value)
+        return field.to_python(value)
 
     def check_aggregate_support(self, aggregate_func):
         """Check that the backend supports the provided aggregate
