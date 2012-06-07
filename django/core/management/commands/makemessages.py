5a63482f104b109b3ae799d8b008730437764a5e
Craig Blaszczyk <masterjakul@gmail.com>
2012-06-07 11:23:25 +0200

    Made (make|compile)messages commands accept multiple locales at once.
    Thanks Craig Blaszczyk for the initial patch. Refs #17181.

--- django/core/management/commands/makemessages.py
+++ django/core/management/commands/makemessages.py
@@ -304,7 +304,7 @@ def make_messages(locale=None, domain='django', verbosity=1, all=False,
 
     locales = []
     if locale is not None:
-        locales.append(str(locale))
+        locales += locale.split(',') if not isinstance(locale, list) else locale
     elif all:
         locale_dirs = filter(os.path.isdir, glob.glob('%s/*' % localedir))
         locales = [os.path.basename(l) for l in locale_dirs]
@@ -341,8 +341,8 @@ def make_messages(locale=None, domain='django', verbosity=1, all=False,
 
 class Command(NoArgsCommand):
     option_list = NoArgsCommand.option_list + (
-        make_option('--locale', '-l', default=None, dest='locale',
-            help='Creates or updates the message files for the given locale (e.g. pt_BR).'),
+        make_option('--locale', '-l', default=None, dest='locale', action='append',
+            help='Creates or updates the message files for the given locale(s) (e.g. pt_BR). Can be used multiple times, accepts a comma-separated list of locale names.'),
         make_option('--domain', '-d', default='django', dest='domain',
             help='The domain of the message files (default: "django").'),
         make_option('--all', '-a', action='store_true', dest='all',
