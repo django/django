4cdbdc4c61015daef5af98adcf4093b67b060203
Mikhail Andreev (aka adw0rd) <x11org@gmail.com>
2012-06-25 23:42:17 +0400

    Added the implementation for MySQL for specifying of several fields in distinct()

--- django/db/backends/mysql/compiler.py
+++ django/db/backends/mysql/compiler.py
@@ -1,4 +1,5 @@
 from django.db.models.sql import compiler
+from django.db.models.sql.constants import LOOKUP_SEP
 
 class SQLCompiler(compiler.SQLCompiler):
     def resolve_columns(self, row, fields=()):
@@ -10,6 +11,34 @@ class SQLCompiler(compiler.SQLCompiler):
                 value = bool(value)
             values.append(value)
         return row[:index_extra_select] + tuple(values)
+
+    def get_distinct(self):
+        """MySQL DISTINCT for several fields.
+
+        Implementation in MySQL:
+        SELECT `field1`, `field2`, ..., `fieldN` FROM `table` GROUP BY `field1`, `field2`, ..., `fieldN` ORDER BY NULL;
+
+        For a similar statement in PostgreSQL:
+        SELECT DISTINCT ON(`field1`, `field2`, ..., `fieldN`) `field1`, `field2`, ..., `fieldN` FROM `table`;
+        """
+
+        qn = self.quote_name_unless_alias
+        qn2 = self.connection.ops.quote_name
+        result = []
+        opts = self.query.model._meta
+
+        self.query.group_by = self.query.group_by or []
+        for name in self.query.distinct_fields:
+            parts = name.split(LOOKUP_SEP)
+            field, col, alias, _, _ = self._setup_joins(parts, opts, None)
+            col, alias = self._final_join_removal(col, alias)
+            self.query.group_by.append((qn(alias), qn2(col)))  # self.query.model._meta.db_table, field[0].column
+
+        # Suppression: NotImplementedError("annotate() + distinct(fields) not implemented.")
+        if self.query.distinct_fields:
+            return []
+        else:
+            return None
 
 class SQLInsertCompiler(compiler.SQLInsertCompiler, SQLCompiler):
     pass
