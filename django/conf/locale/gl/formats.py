e4c51ad76a73c0e6a27345412a3affae7f9c61d6
Guttorm Flatabø <post@guttormflatabo.com>
2012-06-28 16:11:15 +0300

    New Galician date and time formats according to request at ttp://translatewiki.net/wiki/Thread:User_talk:Toliño/Thank_you_for_your_translation_of_Waymarked_Trails_to_Galician!/reply I have no knowledge of Galician, but request is made by translator and changes similar to those of related Spanish formats.py All formats tested individually on 1.4.

--- django/conf/locale/gl/formats.py
+++ django/conf/locale/gl/formats.py
@@ -4,14 +4,14 @@
 
 # The *_FORMAT strings use the Django date format syntax,
 # see http://docs.djangoproject.com/en/dev/ref/templates/builtins/#date
-DATE_FORMAT = 'd F Y'
+DATE_FORMAT = 'j \d\e F \d\e Y'
 TIME_FORMAT = 'H:i:s'
-# DATETIME_FORMAT = 
-YEAR_MONTH_FORMAT = 'F Y'
-MONTH_DAY_FORMAT = 'j F'
-SHORT_DATE_FORMAT = 'j M, Y'
-# SHORT_DATETIME_FORMAT = 
-# FIRST_DAY_OF_WEEK = 
+DATETIME_FORMAT = 'j \d\e F \d\e Y \á\s H:i'
+YEAR_MONTH_FORMAT = 'F \d\e Y'
+MONTH_DAY_FORMAT = 'j \d\e F'
+SHORT_DATE_FORMAT = 'd-m-Y'
+SHORT_DATETIME_FORMAT = 'd-m-Y, H:i'
+FIRST_DAY_OF_WEEK = 1 # Monday
 
 # The *_INPUT_FORMATS strings use the Python strftime format syntax,
 # see http://docs.python.org/library/datetime.html#strftime-strptime-behavior
