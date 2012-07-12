436bd6f66da31e3613c5855de6e146a12ffc9247
Michael Farrell <micolous+git@gmail.com>
2012-07-12 11:45:27 +0930

    Implemented unit tests for login_fail signal

--- django/contrib/auth/tests/signals.py
+++ django/contrib/auth/tests/signals.py
@@ -14,26 +14,38 @@ class SignalTestCase(TestCase):
     def listener_logout(self, user, **kwargs):
         self.logged_out.append(user)
 
+    def listener_login_fail(self, sender, credentials, **kwargs):
+        self.login_fail.append(credentials['username'])
+
     def setUp(self):
         """Set up the listeners and reset the logged in/logged out counters"""
         self.logged_in = []
         self.logged_out = []
+        self.login_fail = []
         signals.user_logged_in.connect(self.listener_login)
         signals.user_logged_out.connect(self.listener_logout)
+        signals.user_login_fail.connect(self.listener_login_fail)
 
     def tearDown(self):
         """Disconnect the listeners"""
         signals.user_logged_in.disconnect(self.listener_login)
         signals.user_logged_out.disconnect(self.listener_logout)
+        signals.user_login_fail.disconnect(self.listener_login_fail)
 
     def test_login(self):
-        # Only a successful login will trigger the signal.
+        # Only a successful login will trigger the success signal.
         self.client.login(username='testclient', password='bad')
         self.assertEqual(len(self.logged_in), 0)
+        self.assertEqual(len(self.login_fail), 1)
+        self.assertEqual(self.login_fail[0], 'testclient')
+
         # Like this:
         self.client.login(username='testclient', password='password')
         self.assertEqual(len(self.logged_in), 1)
         self.assertEqual(self.logged_in[0].username, 'testclient')
+
+        # Ensure there were no more failures.
+        self.assertEqual(len(self.login_fail), 1)
 
     def test_logout_anonymous(self):
         # The log_out function will still trigger the signal for anonymous
