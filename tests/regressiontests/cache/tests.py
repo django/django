985b2259c18325491aa47e982c92a0d82ca147c8
Rohan Jain <crodjer@gmail.com>
2012-07-23 11:55:59 +0530

    Cache Test: Custom key generator encoding
    Otherwise, this fails with a unicode decode error for memcache key
    
    Signed-off-by: Rohan Jain <crodjer@gmail.com>

--- tests/regressiontests/cache/tests.py
+++ tests/regressiontests/cache/tests.py
@@ -785,7 +785,7 @@ class BaseCacheTests(object):
 
 def custom_key_func(key, key_prefix, version):
     "A customized cache key function"
-    return 'CUSTOM-' + '-'.join([key_prefix, str(version), key])
+    return str('CUSTOM-' + '-'.join([key_prefix, str(version), key]))
 
 
 class DBCacheTests(BaseCacheTests, TransactionTestCase):
