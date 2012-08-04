86c5c0154f69728eba4aad6204621f07cdd3459d
Tim Graham <timograham@gmail.com>
2012-08-04 18:56:43 -0400

    Fixed a mistake in function documentation 'django.utils.functional.partition'
    Thanks Raman Barkholenka for the patch.

--- django/utils/functional.py
+++ django/utils/functional.py
@@ -312,8 +312,8 @@ def partition(predicate, values):
     Splits the values into two sets, based on the return value of the function
     (True/False). e.g.:
 
-        >>> partition(lambda: x > 3, range(5))
-        [1, 2, 3], [4]
+        >>> partition(lambda x: x > 3, range(5))
+        [0, 1, 2, 3], [4]
     """
     results = ([], [])
     for item in values:
