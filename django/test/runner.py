2c9103e2f5e29bd16731e0fb4164536ca5cac9a5
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2012-08-08 00:29:31 -0400

    adding changes to this discovery hawtness

--- django/test/runner.py
+++ django/test/runner.py
@@ -1,21 +1,5 @@
-"""
-An alternative Django ``TEST_RUNNER`` which uses unittest2 test discovery from
-a base path specified in settings, rather than requiring all tests to be in
-``tests`` module of an app.
+import os
 
-If you just run ``./manage.py test``, it'll discover and run all tests
-underneath the ``TEST_DISCOVERY_ROOT`` setting (a path). If you run
-``./manage.py test full.dotted.path.to.test_module``, it'll run the tests in
-that module (you can also pass multiple modules).
-
-And (new in this updated version), if you give it a single dotted path to a
-package, and that package does not itself directly contain any tests, it'll do
-test discovery in all sub-modules of that package.
-
-This code doesn't modify the default unittest2 test discovery behavior, which
-only searches for tests in files named "test*.py".
-
-"""
 from django.conf import settings
 from django.test import TestCase
 from django.test.simple import DjangoTestSuiteRunner, reorder_suite
@@ -27,20 +11,23 @@ class DiscoveryRunner(DjangoTestSuiteRunner):
     """A test suite runner that uses unittest2 test discovery."""
     def build_suite(self, test_labels, extra_tests=None, **kwargs):
         suite = None
-        discovery_root = settings.TEST_DISCOVERY_ROOT
+        root = settings.TEST_DISCOVERY_ROOT
+        pattern = settings.TEST_DISCOVERY_PATTERN
+        top_level = settings.TEST_DISCOVERY_TOP_LEVEL
 
         if test_labels:
             suite = defaultTestLoader.loadTestsFromNames(test_labels)
             # if single named module has no tests, do discovery within the
-            # model itself
+            # module itself
             if not suite.countTestCases() and len(test_labels) == 1:
                 suite = None
-                discovery_root = import_module(test_labels[0]).__path__[0]
+                root = import_module(test_labels[0]).__path__[0]
+                top_level = os.path.dirname(root)
 
         if suite is None:
-            suite = defaultTestLoader.discover(
-                discovery_root,
-                top_level_dir=settings.BASE_PATH,
+            suite = defaultTestLoader.discover(root,
+                pattern=pattern,
+                top_level_dir=top_level,
                 )
 
         if extra_tests:
