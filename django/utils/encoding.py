a0a0203a392f67832ba7a8a2f099e70d7db2d19e
Aymeric Augustin <aymeric.augustin@m4x.org>
2012-08-11 15:34:51 +0200

    [py3] Added python_2_unicode_compatible decorator.

--- django/utils/encoding.py
+++ django/utils/encoding.py
@@ -39,6 +39,19 @@ class StrAndUnicode(object):
         def __str__(self):
             return self.__unicode__().encode('utf-8')
 
+def python_2_unicode_compatible(klass):
+    """
+    A decorator that defines __unicode__ and __str__ methods under Python 2.
+    Under Python 3 it does nothing.
+
+    To support Python 2 and 3 with a single code base, define a __str__ method
+    returning text and apply this decorator to the class.
+    """
+    if not six.PY3:
+        klass.__unicode__ = klass.__str__
+        klass.__str__ = lambda self: self.__unicode__().encode('utf-8')
+    return klass
+
 def smart_text(s, encoding='utf-8', strings_only=False, errors='strict'):
     """
     Returns a text object representing 's' -- unicode on Python 2 and str on
