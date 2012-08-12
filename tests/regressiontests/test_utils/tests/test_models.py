31ab1e3fb74168c61b99ebc86f1e17e498214114
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2012-08-12 04:04:42 -0400

    adding tests directory with two types of test files

---
+++ tests/regressiontests/test_utils/tests/test_models.py
@@ -0,0 +1,16 @@
+"""
+This file demonstrates writing tests using the unittest module. These will pass
+when you run "manage.py test".
+
+Replace this with more appropriate tests for your application.
+"""
+
+from django.test import TestCase
+
+
+class SimpleTest(TestCase):
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
