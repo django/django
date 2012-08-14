62a9ed0ac761bc737ba355519bb209517b330039
Anssi Kääriäinen <akaariai@gmail.com>
2012-08-14 15:43:33 +0300

    [py3] Fixed F-expression division operators
    In Python 3 dividing by int will call obj.__truediv__(). This operator
    was missing from F-expressions.

--- django/db/models/expressions.py
+++ django/db/models/expressions.py
@@ -58,8 +58,9 @@ class ExpressionNode(tree.Node):
     def __mul__(self, other):
         return self._combine(other, self.MUL, False)
 
-    def __div__(self, other):
+    def __truediv__(self, other):
         return self._combine(other, self.DIV, False)
+    __div__ = __truediv__ # Python 2 compatibility
 
     def __mod__(self, other):
         return self._combine(other, self.MOD, False)
