d1d393f975f5b14ce465fd85309520bd3c7d9162
Alex Gaynor <alex.gaynor@gmail.com>
2012-08-15 02:11:55 -0700

    Allow tests using a LiveServer to get closer to working.

--- django/test/testcases.py
+++ django/test/testcases.py
@@ -1134,7 +1134,7 @@ class LiveServerTestCase(TransactionTestCase):
             host, port_ranges = specified_address.split(':')
             for port_range in port_ranges.split(','):
                 # A port range can be of either form: '8000' or '8000-8010'.
-                extremes = map(int, port_range.split('-'))
+                extremes = list(map(int, port_range.split('-')))
                 assert len(extremes) in [1, 2]
                 if len(extremes) == 1:
                     # Port range of the form '8000'
