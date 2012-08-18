d5ff3243920693cd55934b65a2a2900114b9d86d
Marc Tamlyn <marc.tamlyn@gmail.com>
2012-08-18 12:52:05 +0100

    Fixed #17228 -- params context variable is inconsistent
    Remove the params variable from the context and just put the variables
    in directly.
    
    This had not been committed previously as the original pattern was used
    in the functional generic views and we wanted consistency between them,
    but django.views.generic.simple.direct_to_template is now gone so we can
    do it 'right'.

--- django/views/generic/base.py
+++ django/views/generic/base.py
@@ -140,11 +140,11 @@ class TemplateResponseMixin(object):
 
 class TemplateView(TemplateResponseMixin, ContextMixin, View):
     """
-    A view that renders a template.  This view is different from all the others
-    insofar as it also passes ``kwargs`` as ``params`` to the template context.
+    A view that renders a template.  This view will also pass into the context
+    any keyword arguments passed by the url conf.
     """
     def get(self, request, *args, **kwargs):
-        context = self.get_context_data(params=kwargs)
+        context = self.get_context_data(**kwargs)
         return self.render_to_response(context)
 
 
