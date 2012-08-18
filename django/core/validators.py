bfa9fc69bf0afae907af0ba3ef2cab8e10a4fa61
Marc Tamlyn <marc.tamlyn@gmail.com>
2012-08-18 12:00:52 +0100

    Fixed #18779 -- URLValidator can't validate url with ipv6.
    Validation is reasonably 'soft', as for the ipv4. False positives don't
    matter too much here.

--- django/core/validators.py
+++ django/core/validators.py
@@ -44,7 +44,8 @@ class URLValidator(RegexValidator):
         r'^(?:http|ftp)s?://' # http:// or https://
         r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
         r'localhost|' #localhost...
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|' # ...or ipv4
+        r'\[?[A-F0-9]*:[A-F0-9:]+\]?)' # ...or ipv6
         r'(?::\d+)?' # optional port
         r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
