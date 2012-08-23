03671ad7e30749ecde70a7af04fff0b57476b1e0
Aymeric Augustin <aymeric.augustin@m4x.org>
2012-08-23 10:23:23 +0200

    Reordered imports
    to avoid an error introduced in 5301a9d7b1.

--- django/utils/text.py
+++ django/utils/text.py
@@ -7,15 +7,16 @@ from gzip import GzipFile
 from io import BytesIO
 
 from django.utils.encoding import force_text
-if not six.PY3:
-    # Import force_unicode even though this module doesn't use it, because some
-    # people rely on it being here.
-    from django.utils.encoding import force_unicode
 from django.utils.functional import allow_lazy, SimpleLazyObject
 from django.utils import six
 from django.utils.six.moves import html_entities
 from django.utils.translation import ugettext_lazy, ugettext as _, pgettext
 from django.utils.safestring import mark_safe
+
+if not six.PY3:
+    # Import force_unicode even though this module doesn't use it, because some
+    # people rely on it being here.
+    from django.utils.encoding import force_unicode
 
 # Capitalizes the first letter of a string.
 capfirst = lambda x: x and force_text(x)[0].upper() + force_text(x)[1:]
