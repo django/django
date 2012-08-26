9735b82108a9ff3aa5957c3747df99b6bc479e02
Simon Blanchard <simonb@yaean.com>
2012-08-26 17:18:38 +0800

    define less than or equal unicode character for latex

--- docs/conf.py
+++ docs/conf.py
@@ -191,7 +191,8 @@ modindex_common_prefix = ["django."]
 # -- Options for LaTeX output --------------------------------------------------
 
 latex_elements = {
-    'preamble': '\\DeclareUnicodeCharacter{2265}{\\ensuremath{\\ge}}'
+    'preamble': '''\\DeclareUnicodeCharacter{2265}{\\ensuremath{\\ge}}
+\\DeclareUnicodeCharacter{2264}{\\ensuremath{\\le}}'''
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
