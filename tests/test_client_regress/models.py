639056a1ce361c1b1407d018403d91cd14ffc04d
Pavel Ponomarev <i.haz.teh.codez@gmail.com>
2012-09-04 22:02:45 +0300

    Fixed #18924 -- Made test.Client.logout send user_logged_out signal.
    Thanks Pavel Ponomarev for the initial patch
    
    refactor to take advantage of default logout()
    Remove unneeded session.delete.
    remove unused import
    made tests work with custom user models

---
+++ tests/test_client_regress/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(verbose_name='email address', max_length=255, unique=True)
+    custom_objects = BaseUserManager()
+
+    USERNAME_FIELD = 'email'
+
+    class Meta:
+        app_label = 'test_client_regress'
