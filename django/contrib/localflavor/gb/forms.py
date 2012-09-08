1f9399888130a1e8da56f7f3d19c49dae8e90f67
Brad Pitcher <bradpitcher@gmail.com>
2012-09-08 10:23:40 -0400

    GBPhoneNumberField: more pythonic and simplified

--- django/contrib/localflavor/gb/forms.py
+++ django/contrib/localflavor/gb/forms.py
@@ -64,195 +64,97 @@ class GBPhoneNumberField(CharField):
         super(GBPhoneNumberField, self).clean(value)
         if value in EMPTY_VALUES:
             return ''
-        error = gb_phone_number_validation(value)
-        if not error:
-            return format_gb_phone_number(value)
-        else:
-            raise ValidationError(self.default_error_messages[error])
 
-def gb_phone_number_validation(phone_number):
-    """
-    Verifies that phonenumber is a United Kingdom phone number in a valid
-    number range. Rejects numbers that are too long or too short or are in
-    a non-valid range. Accepts a wide range of input formats and a number
-    of different dial prefixes.
-    created by @g1smd
-    @param string phonenumber
-    @return string Returns a string containing the error code, if any
-    """
-    # Check if number entered matches a valid format
-    if not valid_gb_phone_pattern(phone_number):
-        return 'number_format'
-    else:
+        number_parts = {'prefix': '+44', 'NSN': '', 'extension': None}
+        gb_number_parts = re.compile(r'^(\(?(?:0(?:0|11)\)?[\s-]?\(?|\+)(44)\)?[\s-]?)?\(?0?(?:\)[\s-]?)?([1-9]\d{1,4}\)?[\d\s-]+)((?:x|ext\.?|\#)\d{3,4})?$', re.X)
+        valid_gb_pattern = re.compile(r'^(?:(?:\(?(?:0(?:0|11)\)?[\s-]?\(?|\+)44\)?[\s-]?(?:\(?0\)?[\s-]?)?)|(?:\(?0))(?:(?:\d{5}\)?[\s-]?\d{4,5})|(?:\d{4}\)?[\s-]?(?:\d{5}|\d{3}[\s-]?\d{3}))|(?:\d{3}\)?[\s-]?\d{3}[\s-]?\d{3,4})|(?:\d{2}\)?[\s-]?\d{4}[\s-]?\d{4}))(?:[\s-]?(?:x|ext\.?|\#)\d{3,4})?$', re.X)
+
+        # Check if number entered matches a valid format
+        if not re.search(valid_gb_pattern, value):
+            raise ValidationError(self.default_error_messages['number_format'])
+
         # Extract number parts: prefix, NSN, extension
-        phone_number_parts = extract_gb_phone_parts(phone_number)
-        if not phone_number_parts:
-            return 'number_format'
-        else:
-            phone_number_nsn = phone_number_parts['NSN']
-            # Check if NSN entered is in a valid range
-            if not valid_gb_phone_range(phone_number_nsn):
-                return 'number_range'
-            else:
-                return ''
+        # group(2) contains "44" or None depending on whether number entered in
+        #  international or national format
+        # group(3) contains NSN
+        # group(4) contains extension
+        m = re.search(gb_number_parts, value)
+        if m.group:
+            # Extract NSN part of GB number
+            if m.group(3):
+                # Trim NSN and remove space, hyphen or ')' if present
+                translate_table = dict((ord(char), u'') for char in u')- ')
+                number_parts['NSN'] = m.group(3).translate(
+                    translate_table).strip()
 
-def format_gb_phone_number(phonenumber):
+                # Extract extension
+                if m.group(4):
+                    number_parts['extension'] = m.group(4)
+        if not number_parts:
+            raise ValidationError(self.default_error_messages['number_format'])
+
+        phone_number_nsn = number_parts['NSN']
+        # Check if NSN entered is in a valid range
+        if not valid_gb_phone_range(phone_number_nsn):
+            raise ValidationError(self.default_error_messages['number_range'])
+
+        return format_gb_phone_number(number_parts)
+
+def valid_gb_phone_range(phone_number_nsn):
     """
-    Convert a valid United Kingdom phone number into standard +44 20 3000 5555 #0001,
-    +44 121 555 7788, +44 1970 223344, +44 1750 62555, +44 19467 55555 or +44 16977 2333
-    international format or into national format with 0, according to entry format.
-    Accepts a wide range of input formats and prefixes and re-formats the number taking
-    into account the required 2+8, 3+7, 4+6, 4+5, 5+5, 5+4 and 3+6 formats by number range.
-    created by @g1smd
-    @param string phonenumber must be a valid nine or ten-digit number (with optional extension)
-    @return string phonenumber
-    """
-    phonenumberPrefix = ''
-    phonenumberNSNraw = ''
-    phonenumberNSNformatted = ''
-    phonenumberExtension = ''
-
-    # Extract optional country prefix, NSN, and optional extension
-    phonenumberPartsArray = extract_gb_phone_parts(phonenumber)
-    if phonenumberPartsArray:
-        # Grab the NSN part of GB number
-        phonenumberNSN = phonenumberPartsArray['NSN']
-        if not phonenumberNSN:
-            return phonenumber
-
-        # Remove spaces, hyphens, and brackets from NSN part of GB number
-        translate_table = dict((ord(char), u'') for char in u')- ')
-        phonenumberNSN = phonenumberNSN.translate(translate_table).strip()
-        # Format NSN part of GB number
-        phonenumberNSNformatted = format_gb_nsn(phonenumberNSN)
-
-        # Set prefix (will be +44 or 0)
-        if 'prefix' in phonenumberPartsArray and phonenumberPartsArray['prefix'] is not None:
-            phonenumberPrefix = phonenumberPartsArray['prefix']
-
-        # Grab extension and trim it
-        phonenumberHasExtension = False
-        phonenumberExtension = None
-        if 'extension' in phonenumberPartsArray and phonenumberPartsArray['extension'] is not None:
-            phonenumberHasExtension = true
-            phonenumberExtension = " " + phonenumberPartsArray['extension'].strip()
-
-        # Add prefix back on to NSN
-        phonenumber = phonenumberPrefix + phonenumberNSNformatted
-
-        # Add extension back on to number
-        if phonenumberHasExtension:
-            phonenumber += phonenumberExtension
-
-    return phonenumber
-
-def valid_gb_phone_pattern(phonenumber):
-    """
-    Verifies that phonenumber uses a valid UK phone number input pattern.
-    Pattern matches any number entered as 2+8, 3+7, 4+6, 4+5, 5+5, 5+4, 3+6
-    with or without spaces, with a variety of prefixes and optional extension.
-    RegEx patterns are based on
-    http://www.aa-asterisk.org.uk/index.php/Number_format
-    http://www.aa-asterisk.org.uk/index.php/Regular_Expressions_for_Validating_and_Formatting_UK_Telephone_Numbers
-    created by @g1smd
-    @param string phonenumber
-    @return boolean Returns boolean FALSE if the phone number is not valid.
-    """
-    validGBpattern = re.compile(r'^(?:(?:\(?(?:0(?:0|11)\)?[\s-]?\(?|\+)44\)?[\s-]?(?:\(?0\)?[\s-]?)?)|(?:\(?0))(?:(?:\d{5}\)?[\s-]?\d{4,5})|(?:\d{4}\)?[\s-]?(?:\d{5}|\d{3}[\s-]?\d{3}))|(?:\d{3}\)?[\s-]?\d{3}[\s-]?\d{3,4})|(?:\d{2}\)?[\s-]?\d{4}[\s-]?\d{4}))(?:[\s-]?(?:x|ext\.?|\#)\d{3,4})?$', re.X)
-
-    # Test number entered for matching format
-    return re.search(validGBpattern, phonenumber)
-
-def extract_gb_phone_parts(phonenumber):
-    """
-    Extract parts from GB phone number: prefix, NSN and optional extension.
-    Accepts a wide range of input formats and prefixes. This function also
-    cleans up the NSN part by removing spaces, hyphens and brackets.
-    Returned prefix is either +44 with space or a 0 without space.
-    created by @g1smd
-    @param string phonenumber must be a valid UK phone number (with optional extension)
-    @return array phonenumberPartsArray Returns prefix, NSN and extension in array.
-    """
-    result = {'NSN': '', 'prefix': '+44', 'extension': None}
-    patternGBnumberparts = re.compile(r'^(\(?(?:0(?:0|11)\)?[\s-]?\(?|\+)(44)\)?[\s-]?)?\(?0?(?:\)[\s-]?)?([1-9]\d{1,4}\)?[\d\s-]+)((?:x|ext\.?|\#)\d{3,4})?$', re.X)
-
-    # group(2) contains "44" or None depending on whether number entered in
-    #  international or national format
-    # group(3) contains NSN
-    # group(4) contains extension
-    m = re.search(patternGBnumberparts, phonenumber)
-    if m.group:
-        # Extract NSN part of GB number
-        if m.group(3):
-            # Trim NSN and remove space, hyphen or ')' if present
-            translate_table = dict((ord(char), u'') for char in u')- ')
-            result['NSN'] = m.group(3).translate(translate_table).strip()
-
-            # Extract extension
-            if m.group(4):
-                result['extension'] = m.group(4)
-
-    return result
-
-def valid_gb_phone_range(phonenumberNSN):
-    """
-    Verifies that phonenumberNSN is a valid UK phone number range by initial
+    Verifies that phone_number_nsn is a valid UK phone number range by initial
     digits and length. Tests the NSN part for length and number range. Based on
     http://www.aa-asterisk.org.uk/index.php/Number_format
     http://www.aa-asterisk.org.uk/index.php/Regular_Expressions_for_Validating_and_Formatting_UK_Telephone_Numbers
-    created by @g1smd
-    @param string phonenumberNSN
-    @return boolean Returns boolean FALSE if the phone number is not valid.
+    @param string phone_number_nsn
+    @return boolean Returns boolean False if the phone number is not valid.
     """
-    patternGBvalidrange = re.compile(r"""
-    ^
-    (         # 2d with 10 digits [2+8] Landlines
+    return re.match(re.compile(r"""
+    ^(       # 2d with 10 digits [2+8] Landlines
         2(?:0[01378]|3[0189]|4[017]|8[0-46-9]|9[012])\d{7}
-        |     # 11d, 1d1 with 10 digits [3+7] Landlines
+        |    # 11d, 1d1 with 10 digits [3+7] Landlines
         1(?:(?:1(?:3[0-48]|[46][0-4]|5[012789]|7[0-49]|8[01349])|21[0-7]|31[0-8]|[459]1\d|61[0-46-9]))\d{6}
-        |     # 1ddd (and 1dddd) with 10 digits [4+6][5+5] Landlines
+        |    # 1ddd (and 1dddd) with 10 digits [4+6][5+5] Landlines
         1(?:2(?:0[024-9]|2[3-9]|3[3-79]|4[1-689]|[58][02-9]|6[0-4789]|7[013-9]|9\d)|3(?:0\d|[25][02-9]|3[02-579]|[468][0-46-9]|7[1235679]|9[24578])|4(?:0[03-9]|2[02-5789]|[37]\d|4[02-69]|5[0-8]|[69][0-79]|8[0-5789])|5(?:0[1235-9]|2[024-9]|3[0145689]|4[02-9]|5[03-9]|6\d|7[0-35-9]|8[0-468]|9[0-5789])|6(?:0[034689]|2[0-689]|[38][013-9]|4[1-467]|5[0-69]|6[13-9]|7[0-8]|9[0124578])|7(?:0[0246-9]|2\d|3[023678]|4[03-9]|5[0-46-9]|6[013-9]|7[0-35-9]|8[024-9]|9[02-9])|8(?:0[35-9]|2[1-5789]|3[02-578]|4[0-578]|5[124-9]|6[2-69]|7\d|8[02-9]|9[02569])|9(?:0[02-589]|2[02-689]|3[1-5789]|4[2-9]|5[0-579]|6[234789]|7[0124578]|8\d|9[2-57]))\d{6}
-        |     # 1ddd with 9 digits [4+5] Landlines
+        |    # 1ddd with 9 digits [4+5] Landlines
         1(?:2(?:0(?:46[1-4]|87[2-9])|545[1-79]|76(?:2\d|3[1-8]|6[1-6])|9(?:7(?:2[0-4]|3[2-5])|8(?:2[2-8]|7[0-4789]|8[345])))|3(?:638[2-5]|647[23]|8(?:47[04-9]|64[015789]))|4(?:044[1-7]|20(?:2[23]|8\d)|6(?:0(?:30|5[2-57]|6[1-8]|7[2-8])|140)|8(?:052|87[123]))|5(?:24(?:3[2-79]|6\d)|276\d|6(?:26[06-9]|686))|6(?:06(?:4\d|7[4-79])|295[567]|35[34]\d|47(?:24|61)|59(?:5[08]|6[67]|74)|955[0-4])|7(?:26(?:6[13-9]|7[0-7])|442\d|50(?:2[0-3]|[3-68]2|76))|8(?:27[56]\d|37(?:5[2-5]|8[239])|84(?:3[2-58]))|9(?:0(?:0(?:6[1-8]|85)|52\d)|3583|4(?:66[1-8]|9(?:2[01]|81))|63(?:23|3[1-4])|9561))\d{3}
-        |     # 1ddd with 9 digits [4+5] Landlines (special case)
+        |    # 1ddd with 9 digits [4+5] Landlines (special case)
         176888[234678]\d{2}
-        |     # 1dddd with 9 digits [5+4] Landlines
+        |    # 1dddd with 9 digits [5+4] Landlines
         16977[23]\d{3}
-        |     # 7ddd (including 7624) (not 70, 76) with 10 digits [4+6] Mobile phones
+        |    # 7ddd (including 7624) (not 70, 76) with 10 digits [4+6] Mobile phones
         7(?:[1-4]\d\d|5(?:0[0-8]|[13-9]\d|2[0-35-9])|624|7(?:0[1-9]|[1-7]\d|8[02-9]|9[0-689])|8(?:[014-9]\d|[23][0-8])|9(?:[04-9]\d|1[02-9]|2[0-35-9]|3[0-689]))\d{6}
-        |     # 76 (excluding 7624) with 10 digits [2+8] Pagers
+        |    # 76 (excluding 7624) with 10 digits [2+8] Pagers
         76(?:0[012]|2[356]|4[0134]|5[49]|6[0-369]|77|81|9[39])\d{6}
-        |     # 800 with 9 or 10 digits, 808 with 10 digits, 500 with 9 digits [3+7][3+6] Freephone
+        |    # 800 with 9 or 10 digits, 808 with 10 digits, 500 with 9 digits [3+7][3+6] Freephone
         80(?:0\d{6,7}|8\d{7})|500\d{6}
-        |     # 871, 872, 873, 90d, 91d, 980, 981, 982, 983 with 10 digits [3+7] Premium rate
+        |    # 871, 872, 873, 90d, 91d, 980, 981, 982, 983 with 10 digits [3+7] Premium rate
         (?:87[123]|9(?:[01]\d|8[0-3]))\d{7}
         |     # 842, 843, 844, 845, 870 with 10 digits [3+7] Business rate
         8(?:4[2-5]|70)\d{7}
-        |     # 70 with 10 digits [2+8] Personal numbers
+        |    # 70 with 10 digits [2+8] Personal numbers
         70\d{8}
-        |     # 56 with 10 digits [2+8] LIECS&VoIP
+        |    # 56 with 10 digits [2+8] LIECS&VoIP
         56\d{8}
-        |     # 30d, 33d, 34d, 37d, 55 with 10 digits [3+7] UAN and [2+8] Corporate
+        |    # 30d, 33d, 34d, 37d, 55 with 10 digits [3+7] UAN and [2+8] Corporate
         (?:3[0347]|55)\d{8}
-        |     # 800 1111, 845 46 4d with 7 digits [3+4] Freephone helplines
+        |    # 800 1111, 845 46 4d with 7 digits [3+4] Freephone helplines
         8(?:001111|45464\d)
-    )
-    $
-    """, re.X)
+    )$
+    """, re.X), phone_number_nsn)
 
-    # Test NSN to see if it matches a valid number range
-    return re.match(patternGBvalidrange, phonenumberNSN)
-
-def format_gb_nsn(phonenumberNSN):
+def format_gb_nsn(phone_number_nsn):
     """
     Format GB phone numbers in correct format per number range. Based on
     http://www.aa-asterisk.org.uk/index.php/Number_format
     http://www.aa-asterisk.org.uk/index.php/Regular_Expressions_for_Validating_and_Formatting_UK_Telephone_Numbers
     created by @g1smd
-    @param string phonenumberNSN Must be the 10 or 9 digit NSN part of the number.
-    @return string phonenumberNSN Returns correctly formatted NSN by length and range.
+    @param string phone_number_nsn Must be the 10 or 9 digit NSN part of the
+        number.
+    @return string phone_number_nsn Returns correctly formatted NSN by length
+        and range.
     """
-    phonenumberNSNLength = len(phonenumberNSN)
+    nsn_length = len(phone_number_nsn)
     # RegEx patterns to define formatting by length and initial digits
     # [2+8] 2d, 55, 56, 70, 76 (not 7624) with 10 digits
     pattern28 = re.compile(r"^(?:2|5[56]|7(?:0|6(?:[013-9]|2[0-35-9])))")
@@ -279,42 +181,80 @@ def format_gb_nsn(phonenumberNSN):
     pattern34 = re.compile(r"^(?:8(?:001111|45464\d))")
     capture34 = re.compile(r"^(\d{3})(\d{4})$")
     # Format numbers by leading digits and length
-    if phonenumberNSNLength is 10 and re.match(pattern28, phonenumberNSN):
-        m = (re.search(capture28, phonenumberNSN))
+    if nsn_length is 10 and re.match(pattern28, phone_number_nsn):
+        m = (re.search(capture28, phone_number_nsn))
         if m.group:
-            phonenumberNSN = m.group(1) + m.group(2) + m.group(3)
-    elif phonenumberNSNLength is 10 and re.match(pattern37, phonenumberNSN):
-        m = (re.search(capture37, phonenumberNSN))
+            phone_number_nsn = m.group(1) + m.group(2) + m.group(3)
+    elif nsn_length is 10 and re.match(pattern37, phone_number_nsn):
+        m = (re.search(capture37, phone_number_nsn))
         if m.group:
-            phonenumberNSN = m.group(1) + m.group(2) + m.group(3)
-    elif phonenumberNSNLength is 10 and re.match(pattern55, phonenumberNSN):
-        m = (re.search(capture55, phonenumberNSN))
+            phone_number_nsn = m.group(1) + m.group(2) + m.group(3)
+    elif nsn_length is 10 and re.match(pattern55, phone_number_nsn):
+        m = (re.search(capture55, phone_number_nsn))
         if m.group:
-            phonenumberNSN = m.group(1) + m.group(2)
-    elif phonenumberNSNLength is 9  and re.match(pattern54, phonenumberNSN):
-        m = (re.search(capture54, phonenumberNSN))
+            phone_number_nsn = m.group(1) + m.group(2)
+    elif nsn_length is 9  and re.match(pattern54, phone_number_nsn):
+        m = (re.search(capture54, phone_number_nsn))
         if m.group:
-            phonenumberNSN = m.group(1) + m.group(2)
-    elif phonenumberNSNLength is 10 and re.match(pattern46, phonenumberNSN):
-        m = (re.search(capture46, phonenumberNSN))
+            phone_number_nsn = m.group(1) + m.group(2)
+    elif nsn_length is 10 and re.match(pattern46, phone_number_nsn):
+        m = (re.search(capture46, phone_number_nsn))
         if m.group:
-            phonenumberNSN = m.group(1) + m.group(2)
-    elif phonenumberNSNLength is 9  and re.match(pattern45, phonenumberNSN):
-        m = (re.search(capture45, phonenumberNSN))
+            phone_number_nsn = m.group(1) + m.group(2)
+    elif nsn_length is 9  and re.match(pattern45, phone_number_nsn):
+        m = (re.search(capture45, phone_number_nsn))
         if m.group:
-            phonenumberNSN = m.group(1) + m.group(2)
-    elif phonenumberNSNLength is 9  and re.match(pattern36, phonenumberNSN):
-        m = (re.search(capture36, phonenumberNSN))
+            phone_number_nsn = m.group(1) + m.group(2)
+    elif nsn_length is 9  and re.match(pattern36, phone_number_nsn):
+        m = (re.search(capture36, phone_number_nsn))
         if m.group:
-            phonenumberNSN = m.group(1) + m.group(2)
-    elif phonenumberNSNLength is 7  and re.match(pattern34, phonenumberNSN):
-        m = (re.search(capture34, phonenumberNSN))
+            phone_number_nsn = m.group(1) + m.group(2)
+    elif nsn_length is 7  and re.match(pattern34, phone_number_nsn):
+        m = (re.search(capture34, phone_number_nsn))
         if m.group:
-            phonenumberNSN = m.group(1) + m.group(2)
-    elif phonenumberNSNLength > 5:
+            phone_number_nsn = m.group(1) + m.group(2)
+    elif nsn_length > 5:
         # Default format for non-valid numbers (shouldn't ever get here)
-        m = (re.search("^(\d)(\d{4})(\d*)$", phonenumberNSN))
+        m = (re.search("^(\d)(\d{4})(\d*)$", phone_number_nsn))
         if m.group:
-            phonenumberNSN = m.group(1) + m.group(2) + m.group(3)
+            phone_number_nsn = m.group(1) + m.group(2) + m.group(3)
 
-    return phonenumberNSN
+    return phone_number_nsn
+
+def format_gb_phone_number(number_parts):
+    """
+    Convert a valid United Kingdom phone number into standard +44 20 3000 5555
+    #0001, +44 121 555 7788, +44 1970 223344, +44 1750 62555, +44 19467 55555
+    or +44 16977 2333 international format or into national format with 0,
+    according to entry format. Accepts a wide range of input formats and
+    prefixes and re-formats the number taking into account the required 2+8,
+    3+7, 4+6, 4+5, 5+5, 5+4 and 3+6 formats by number range.
+
+    @param dict number_parts must be a valid nine or ten-digit number split
+        into it's constituent parts
+    @return string phone_number
+    """
+    phone_number = number_parts['prefix'] + number_parts['NSN'] + \
+        str(number_parts['extension'])
+
+    if number_parts:
+        # Grab the NSN part of GB number
+        phone_number_nsn = number_parts['NSN']
+        if not phone_number_nsn:
+            return phone_number
+
+        # Set prefix (will be +44 or 0)
+        if 'prefix' in number_parts and number_parts['prefix'] is not None:
+            phone_number = number_parts['prefix']
+
+        # Remove spaces, hyphens, and brackets from NSN part of GB number
+        translate_table = dict((ord(char), u'') for char in u')- ')
+        phone_number_nsn = phone_number_nsn.translate(translate_table).strip()
+        # Format NSN part of GB number
+        phone_number += format_gb_nsn(phone_number_nsn)
+
+        # Grab extension and trim it
+        if 'extension' in number_parts and number_parts['extension'] is not None:
+            phone_number += ' ' + number_parts['extension'].strip()
+
+    return phone_number
