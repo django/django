50e3b2850244dc9f9ac1b3534a3a8448f6b0b066
Brad Pitcher <bradpitcher@gmail.com>
2012-09-08 10:53:34 -0400

    fix docs merge conflict

--- django/contrib/auth/signals.py
+++ django/contrib/auth/signals.py
@@ -1,4 +1,5 @@
 from django.dispatch import Signal
 
 user_logged_in = Signal(providing_args=['request', 'user'])
+user_login_failed = Signal(providing_args=['credentials'])
 user_logged_out = Signal(providing_args=['request', 'user'])
