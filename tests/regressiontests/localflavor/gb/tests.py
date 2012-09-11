ebc5b7b987cb3e938d0978895f515fbca593abc0
g1smd <g1smd.email@gmail.com>
2012-09-11 03:23:39 +0000

    test_GBPhoneNumberField. (from brad/django branch)

--- tests/regressiontests/localflavor/gb/tests.py
+++ tests/regressiontests/localflavor/gb/tests.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 
-from django.contrib.localflavor.gb.forms import GBPostcodeField
+from django.contrib.localflavor.gb.forms import (
+    GBPostcodeField, GBPhoneNumberField
+)
 
 from django.test import SimpleTestCase
 
@@ -30,3 +32,37 @@ class GBLocalFlavorTests(SimpleTestCase):
         }
         kwargs = {'error_messages': {'invalid': 'Enter a bloody postcode!'}}
         self.assertFieldOutput(GBPostcodeField, valid, invalid, field_kwargs=kwargs)
+
+    def test_GBPhoneNumberField(self):
+        error_format = ['Phone numbers must be in +XXXXXXXXXXX format.']
+        valid = {
+            '020 3000 5555': '+442030005555',
+            '(020) 3000 5555': '+442030005555',
+            '+44 20 3000 5555': '+442030005555',
+            '0203 000 5555': '+442030005555',
+            '(0203) 000 5555': '+442030005555',
+            '02030 005 555': '+442030005555',
+            '+44 (0) 20 3000 5555': '+442030005555',
+            '+44(0)203 000 5555': '+442030005555',
+            '00 (44) 2030 005 555': '+442030005555',
+            '(+44 203) 000 5555': '+442030005555',
+            '(+44) 203 000 5555': '+442030005555',
+            '011 44 203 000 5555': '+442030005555',
+            '020-3000-5555': '+442030005555',
+            '(020)-3000-5555': '+442030005555',
+            '+44-20-3000-5555': '+442030005555',
+            '0203-000-5555': '+442030005555',
+            '(0203)-000-5555': '+442030005555',
+            '02030-005-555': '+442030005555',
+            '+44-(0)-20-3000-5555': '+442030005555',
+            '+44(0)203-000-5555': '+442030005555',
+            '00-(44)-2030-005-555': '+442030005555',
+            '(+44-203)-000-5555': '+442030005555',
+            '(+44)-203-000-5555': '+442030005555',
+            '011-44-203-000-5555': '+442030005555'
+        }
+        invalid = {
+            '011 44 203 000 5555 5': error_format,
+            '+44 20 300 5555': error_format,
+        }
+        self.assertFieldOutput(GBPhoneNumberField, valid, invalid)
