3a4af74e29960201166b525685060f1312e37370
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2012-10-03 20:18:19 +0200

    Moved session clean up to django.session.contrib

--- django/core/management/commands/cleanup.py
+++ django/core/management/commands/cleanup.py
@@ -1,11 +1,9 @@
+from django.core import management
 from django.core.management.base import NoArgsCommand
-from django.utils import timezone
+
 
 class Command(NoArgsCommand):
     help = "Can be run as a cronjob or directly to clean out old data from the database (only expired sessions at the moment)."
 
     def handle_noargs(self, **options):
-        from django.db import transaction
-        from django.contrib.sessions.models import Session
-        Session.objects.filter(expire_date__lt=timezone.now()).delete()
-        transaction.commit_unless_managed()
+        management.call_command('cleansessions', **options)
