43530384b7f2a18b44e06e7043b988427b334653
Russell Keith-Magee <russell@keith-magee.com>
2012-10-03 09:14:55 +0800

    Fixed #19049 -- Corrected dumb logic negation error from earlier patch.

--- django/db/models/fields/related.py
+++ django/db/models/fields/related.py
@@ -1026,7 +1026,7 @@ class ForeignKey(RelatedField, Field):
     def contribute_to_related_class(self, cls, related):
         # Internal FK's - i.e., those with a related name ending with '+' -
         # and swapped models don't get a related descriptor.
-        if not self.rel.is_hidden() and related.model._meta.swapped:
+        if not self.rel.is_hidden() and not related.model._meta.swapped:
             setattr(cls, related.get_accessor_name(), ForeignRelatedObjectsDescriptor(related))
             if self.rel.limit_choices_to:
                 cls._meta.related_fkey_lookups.append(self.rel.limit_choices_to)
