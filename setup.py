50dfd4b96959e89ea4372b2bba2e411e08ae04c9
James Bennett <james@b-list.org>
2012-10-25 14:21:02 -0500

    [1.5.x] We actually only support 3.2+ on Py3k.

--- setup.py
+++ setup.py
@@ -107,8 +107,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.0',
-        'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Topic :: Internet :: WWW/HTTP',
