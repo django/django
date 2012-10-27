b760503a270484679ad0e06b73e3ed417a113221
Aymeric Augustin <aymeric.augustin@m4x.org>
2012-10-27 11:49:46 +0200

    [1.5.x] Fixed #18978 -- Moved cleanup command to sessions.
    This removes a dependency of 'core' on 'contrib'.
    
    Backport of 83ba0a9 from master.
    
    This deprecation occurs after the alpha, but it's a prerequisite
    for fixing decently #18194 which is a release blocker.

---
+++ django/contrib/sessions/management/commands/clearsessions.py
@@ -0,0 +1,11 @@
+from django.core.management.base import NoArgsCommand
+from django.utils import timezone
+
+class Command(NoArgsCommand):
+    help = "Can be run as a cronjob or directly to clean out expired sessions (only with the database backend at the moment)."
+
+    def handle_noargs(self, **options):
+        from django.db import transaction
+        from django.contrib.sessions.models import Session
+        Session.objects.filter(expire_date__lt=timezone.now()).delete()
+        transaction.commit_unless_managed()
