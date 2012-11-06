e48c95ff2cf2dd125c044aaf0a38e9b75117e11c
Marty Woodlee <marty.woodlee@mapmyfitness.com>
2012-11-06 17:34:08 -0600

    Use SHA and random.randrange to generate session keys

--- django/contrib/sessions/backends/base.py
+++ django/contrib/sessions/backends/base.py
@@ -11,14 +11,10 @@ except ImportError:
 
 from django.conf import settings
 from django.core.exceptions import SuspiciousOperation
-from django.utils.hashcompat import md5_constructor
+from django.utils.hashcompat import md5_constructor, sha_constructor
 from django.utils.crypto import constant_time_compare, salted_hmac
 
-# Use the system (hardware-based) random number generator if it exists.
-if hasattr(random, 'SystemRandom'):
-    randrange = random.SystemRandom().randrange
-else:
-    randrange = random.randrange
+
 MAX_SESSION_KEY = 18446744073709551616L     # 2 << 63
 
 class CreateError(Exception):
@@ -161,8 +157,8 @@ class SessionBase(object):
             # No getpid() in Jython, for example
             pid = 1
         while 1:
-            session_key = md5_constructor("%s%s%s%s"
-                    % (randrange(0, MAX_SESSION_KEY), pid, time.time(),
+            session_key = sha_constructor("%s%s%s%s"
+                    % (random.randrange(0, MAX_SESSION_KEY), pid, time.time(),
                        settings.SECRET_KEY)).hexdigest()
             if not self.exists(session_key):
                 break
