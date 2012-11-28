514ce1d35f8ed0f82484ddbc5039e6d927d646b7
Ramiro Morales <cramm0@gmail.com>
2012-11-28 20:28:30 -0300

    Fixed two docstring/comment typos.

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ from __future__ import unicode_literals
 import sys
 from os.path import abspath, dirname, join
 
-# Make sure we iget the version of this copy of Django
+# Make sure we get the version of this copy of Django
 sys.path.insert(1, dirname(dirname(abspath(__file__))))
 
 # If extensions (or modules to document with autodoc) are in another directory,
