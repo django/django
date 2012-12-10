82d4786544e2331b23de468aba875e9600162d76
James Bennett <james@b-list.org>
2012-12-10 15:49:48 -0600

    [1.5.x] Bump version numbers for security release.

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ copyright = 'Django Software Foundation and contributors'
 # The short X.Y version.
 version = '1.5'
 # The full version, including alpha/beta/rc tags.
-release = '1.5b1'
+release = '1.5b2'
 # The next version to be released
 django_next_version = '1.6'
 
