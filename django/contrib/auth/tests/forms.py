9facca28b619ad94680cf07774ee7a377034ca42
Russell Keith-Magee <russell@keith-magee.com>
2012-12-16 07:18:45 +0800

    Corrected tests depending on the error message on the AuthenticationForm.
    Refs #19368, and the fix introduced in 27f8129d64292868f6a328f7bf9a1bed67967ff3.

--- django/contrib/auth/tests/forms.py
+++ django/contrib/auth/tests/forms.py
@@ -100,7 +100,9 @@ class AuthenticationFormTest(TestCase):
         form = AuthenticationForm(None, data)
         self.assertFalse(form.is_valid())
         self.assertEqual(form.non_field_errors(),
-                         [force_text(form.error_messages['invalid_login'])])
+                [force_text(form.error_messages['invalid_login'] % {
+                   'username': User._meta.get_field('username').verbose_name
+                })])
 
     def test_inactive_user(self):
         # The user is inactive.
