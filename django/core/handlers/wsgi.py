80758e7e0af1275bdaa9940512c4ebac249bc3b5
Aymeric Augustin <aymeric.augustin@m4x.org>
2012-12-17 10:49:26 +0100

    Fixed #19468 -- Decoded request.path correctly on Python 3.
    Thanks aliva for the report and claudep for the feedback.

--- django/core/handlers/wsgi.py
+++ django/core/handlers/wsgi.py
@@ -128,7 +128,7 @@ class LimitedStream(object):
 class WSGIRequest(http.HttpRequest):
     def __init__(self, environ):
         script_name = base.get_script_name(environ)
-        path_info = force_text(environ.get('PATH_INFO', '/'))
+        path_info = base.get_path_info(environ)
         if not path_info or path_info == script_name:
             # Sometimes PATH_INFO exists, but is empty (e.g. accessing
             # the SCRIPT_NAME URL without a trailing slash). We really need to
