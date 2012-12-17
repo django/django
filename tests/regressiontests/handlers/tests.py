1e4a27d08790c96f657d2e960c8142d1ca69aede
Aymeric Augustin <aymeric.augustin@m4x.org>
2012-12-17 10:49:26 +0100

    Fixed #19468 -- Decoded request.path correctly on Python 3.
    Thanks aliva for the report and claudep for the feedback.

--- tests/regressiontests/handlers/tests.py
+++ tests/regressiontests/handlers/tests.py
@@ -1,6 +1,7 @@
 from django.core.handlers.wsgi import WSGIHandler
 from django.test import RequestFactory
 from django.test.utils import override_settings
+from django.utils import six
 from django.utils import unittest
 
 class HandlerTests(unittest.TestCase):
@@ -22,7 +23,7 @@ class HandlerTests(unittest.TestCase):
     def test_bad_path_info(self):
         """Tests for bug #15672 ('request' referenced before assignment)"""
         environ = RequestFactory().get('/').environ
-        environ['PATH_INFO'] = b'\xed'
+        environ['PATH_INFO'] = '\xed'
         handler = WSGIHandler()
         response = handler(environ, lambda *a, **k: None)
         self.assertEqual(response.status_code, 400)
