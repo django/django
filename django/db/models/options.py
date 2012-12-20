c04c03daa3620dc5106740a976738ada35203ab5
Russell Keith-Magee <russell@keith-magee.com>
2012-12-20 16:10:19 +0800

    Fixed #19401 -- Ensure that swappable model references are case insensitive.
    This is necessary because get_model() checks are case insensitive, and if the swapable check isn't, the
    swappable logic gets tied up in knots with models that are partially swapped out.
    
    Thanks to chris@cogdon.org for the report and extensive analysis, and Preston for his work on the draft patch.

--- django/db/models/options.py
+++ django/db/models/options.py
@@ -213,12 +213,25 @@ class Options(object):
         """
         Has this model been swapped out for another? If so, return the model
         name of the replacement; otherwise, return None.
+
+        For historical reasons, model name lookups using get_model() are
+        case insensitive, so we make sure we are case insensitive here.
         """
         if self.swappable:
-            model_label = '%s.%s' % (self.app_label, self.object_name)
+            model_label = '%s.%s' % (self.app_label, self.object_name.lower())
             swapped_for = getattr(settings, self.swappable, None)
-            if swapped_for not in (None, model_label):
-                return swapped_for
+            if swapped_for:
+                try:
+                    swapped_label, swapped_object = swapped_for.split('.')
+                except ValueError:
+                    # setting not in the format app_label.model_name
+                    # raising ImproperlyConfigured here causes problems with
+                    # test cleanup code - instead it is raised in get_user_model
+                    # or as part of validation.
+                    return swapped_for
+
+                if '%s.%s' % (swapped_label, swapped_object.lower()) not in (None, model_label):
+                    return swapped_for
         return None
     swapped = property(_swapped)
 
