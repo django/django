f27a4ee3270bd57299ce02d622978ac4d839137e
Aymeric Augustin <aymeric.augustin@m4x.org>
2012-12-24 23:10:40 +0100

    Removed django.contrib.localflavor.
    Each localflavor lives on as a separate app.

--- django/contrib/localflavor/be/be_regions.py
+++
@@ -1,8 +0,0 @@
-from django.utils.translation import ugettext_lazy as _
-
-# ISO codes
-REGION_CHOICES = (
-    ('BRU', _('Brussels Capital Region')),
-    ('VLG', _('Flemish Region')),
-    ('WAL', _('Wallonia'))
-)
--- tests/regressiontests/localflavor/tests.py
+++
@@ -1,3 +0,0 @@
-from __future__ import absolute_import
-
-from .generic.tests import GenericLocalFlavorTests
