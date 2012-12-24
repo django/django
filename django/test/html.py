2164cd00ecdb61774d05f5f278078a28c926779f
Luke Plant <L.Plant.98@cantab.net>
2012-12-24 02:11:32 +0000

    [1.5.x] Fixed HTML comparisons of class="foo bar" and class="bar foo" in tests
    Refs #17758
    
    Backport of 8bc410b44536e03ee38a0087256faf367dd98dd9 from master

--- django/test/html.py
+++ django/test/html.py
@@ -182,6 +182,14 @@ class Parser(HTMLParser):
             self.handle_endtag(tag)
 
     def handle_starttag(self, tag, attrs):
+        # Special case handling of 'class' attribute, so that comparisons of DOM
+        # instances are not sensitive to ordering of classes.
+        attrs = [
+            (name, " ".join(sorted(value.split(" "))))
+            if name == "class"
+            else (name, value)
+            for name, value in attrs
+            ]
         element = Element(tag, attrs)
         self.current.append(element)
         if tag not in self.SELF_CLOSING_TAGS:
