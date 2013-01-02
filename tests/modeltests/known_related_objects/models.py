3625d4d521c92ad25bbd1fb40e433ece792774c1
Aymeric Augustin <aymeric.augustin@m4x.org>
2013-01-02 21:42:52 +0100

    Fixed #19547 -- Caching of related instances.
    When &'ing or |'ing querysets, wrong values could be cached, and crashes
    could happen.
    
    Thanks Marc Tamlyn for figuring out the problem and writing the patch.

--- tests/modeltests/known_related_objects/models.py
+++ tests/modeltests/known_related_objects/models.py
@@ -9,9 +9,13 @@ from django.db import models
 class Tournament(models.Model):
     name = models.CharField(max_length=30)
 
+class Organiser(models.Model):
+    name = models.CharField(max_length=30)
+
 class Pool(models.Model):
     name = models.CharField(max_length=30)
     tournament = models.ForeignKey(Tournament)
+    organiser = models.ForeignKey(Organiser)
 
 class PoolStyle(models.Model):
     name = models.CharField(max_length=30)
