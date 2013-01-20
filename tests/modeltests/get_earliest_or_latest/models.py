7aa538357c8d94df3d5811706bf6dfe5d21421ca
Anssi Kääriäinen <akaariai@gmail.com>
2013-01-20 08:53:45 +0200

    Cleaned up testing  models.py added for earliest()
    The main cleanup was removal of non-necessary __unicode__ method. The
    tests didn't break on py3 as the string representation was never used
    in the tests.
    
    Refs #17813. Thanks to Simon Charette for spotting this issue.

--- tests/modeltests/get_earliest_or_latest/models.py
+++ tests/modeltests/get_earliest_or_latest/models.py
@@ -1,13 +1,3 @@
-"""
-8. get_latest_by
-
-Models can have a ``get_latest_by`` attribute, which should be set to the name
-of a ``DateField`` or ``DateTimeField``. If ``get_latest_by`` exists, the
-model's manager will get a ``latest()`` method, which will return the latest
-object in the database according to that field. "Latest" means "having the date
-farthest into the future."
-"""
-
 from django.db import models
 
 
@@ -15,18 +5,12 @@ class Article(models.Model):
     headline = models.CharField(max_length=100)
     pub_date = models.DateField()
     expire_date = models.DateField()
+
     class Meta:
         get_latest_by = 'pub_date'
-
-    def __unicode__(self):
-        return self.headline
 
 
 class Person(models.Model):
     name = models.CharField(max_length=30)
     birthday = models.DateField()
-
     # Note that this model doesn't have "get_latest_by" set.
-
-    def __unicode__(self):
-        return self.name
