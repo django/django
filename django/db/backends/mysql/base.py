a0ff46bb9a0faf9a622302108f2496f2605d4967
Mateusz Fiołna <matf@op.pl>
2013-01-31 00:14:21 +0100

    Fixes #19702: mysql backend not compatible with MySQL 4 ('SHOW TABLE STATUS ...' syntax)

--- django/db/backends/mysql/base.py
+++ django/db/backends/mysql/base.py
@@ -177,7 +177,7 @@ class DatabaseFeatures(BaseDatabaseFeatures):
             # will tell you the default table type of the created
             # table. Since all Django's test tables will have the same
             # table type, that's enough to evaluate the feature.
-            cursor.execute("SHOW TABLE STATUS WHERE Name='INTROSPECT_TEST'")
+            cursor.execute("SHOW TABLE STATUS LIKE 'INTROSPECT_TEST'")
             result = cursor.fetchone()
             cursor.execute('DROP TABLE INTROSPECT_TEST')
             self._storage_engine = result[1]
