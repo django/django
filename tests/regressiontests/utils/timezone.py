5f8ccdf663084bcbfe2a81008e83c174dc577a00
Rafal Stozek <say4ne@gmail.com>
2013-01-31 14:18:28 +0100

    Fixed #19708 -- django.utils.timezone.override helper may raise AttributeError

--- tests/regressiontests/utils/timezone.py
+++ tests/regressiontests/utils/timezone.py
@@ -4,10 +4,27 @@ import pickle
 from django.test.utils import override_settings
 from django.utils import timezone
 from django.utils import unittest
+from django.utils.tzinfo import FixedOffset
+
+
+UTC = timezone.utc
+EAT = FixedOffset(180)      # Africa/Nairobi
+ICT = FixedOffset(420)      # Asia/Bangkok
 
 
 class TimezoneTests(unittest.TestCase):
-
+    def setUp(self):
+        super(TimezoneTests, self).setUp()
+        self._has_active_value = hasattr(timezone._active, 'value')
+        self._active_value = getattr(timezone._active, 'value', None)
+    
+    def tearDown(self):
+        super(TimezoneTests, self).tearDown()
+        if self._has_active_value:
+            setattr(timezone._active, 'value', self._active_value)
+        elif hasattr(timezone._active, 'value'):
+            del timezone._active.value
+    
     def test_localtime(self):
         now = datetime.datetime.utcnow().replace(tzinfo=timezone.utc)
         local_tz = timezone.LocalTimezone()
@@ -31,3 +48,30 @@ class TimezoneTests(unittest.TestCase):
     def test_pickling_unpickling(self):
         self.assertIsInstance(pickle.loads(pickle.dumps(timezone.UTC())), timezone.UTC)
         self.assertIsInstance(pickle.loads(pickle.dumps(timezone.LocalTimezone())), timezone.LocalTimezone)
+    
+    @override_settings(TIMEZONE='Africa/Nairobi')
+    def test_override(self):
+        # timezone is None, old_timezone is not None
+        timezone.activate(ICT)
+        with timezone.override(None):
+            self.assertIs(timezone.get_default_timezone(), timezone.get_current_timezone())
+        self.assertIs(ICT, timezone.get_current_timezone())
+        
+        # timezone is not None, old_timezone is not None
+        timezone.activate(ICT)
+        with timezone.override(UTC):
+            self.assertIs(UTC, timezone.get_current_timezone())
+        self.assertIs(ICT, timezone.get_current_timezone())
+        
+        # timezone is not None, old_timezone is None
+        timezone.deactivate()
+        with timezone.override(UTC):
+            self.assertIs(UTC, timezone.get_current_timezone())
+        self.assertIs(timezone.get_default_timezone(), timezone.get_current_timezone())
+        
+        # timezone is None, old_timezone is None
+        timezone.deactivate()
+        with timezone.override(None):
+            self.assertIs(timezone.get_default_timezone(), timezone.get_current_timezone())
+        self.assertIs(timezone.get_default_timezone(), timezone.get_current_timezone())
+    
