bc7a219b1e7979ebf0b511d8a3775a0af6dbad33
Julien Phalip <jphalip@gmail.com>
2013-02-02 20:57:38 -0800

    Fixed Python 3.2 compatibility for a test.

--- tests/regressiontests/test_client_regress/tests.py
+++ tests/regressiontests/test_client_regress/tests.py
@@ -810,7 +810,7 @@ class RequestMethodStringDataTests(TestCase):
     def test_patch(self):
         "Request a view with string data via request method PATCH"
         # Regression test for #17797
-        data = u'{"test": "json"}'
+        data = '{"test": "json"}'
         response = self.client.patch('/test_client_regress/request_methods/', data=data, content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, b'request method: PATCH')
