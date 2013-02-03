3330183e24a8200720f13f6aacf90941c99ca890
Ramiro Morales <cramm0@gmail.com>
2013-02-02 21:08:45 -0300

    Fixed #14305 -- Switched inspectdb to create unmanaged models.
    Thanks Ian Kelly for the report and initial patch.

--- django/core/management/commands/inspectdb.py
+++ django/core/management/commands/inspectdb.py
@@ -40,8 +40,9 @@ class Command(NoArgsCommand):
         cursor = connection.cursor()
         yield "# This is an auto-generated Django model module."
         yield "# You'll have to do the following manually to clean this up:"
-        yield "#     * Rearrange models' order"
-        yield "#     * Make sure each model has one field with primary_key=True"
+        yield "#   * Rearrange models' order"
+        yield "#   * Make sure each model has one field with primary_key=True"
+        yield "#   * Remove `managed = False` lines for those models you wish to give write DB access"
         yield "# Feel free to rename the models, but don't rename db_table values or field names."
         yield "#"
         yield "# Also note: You'll have to insert the output of 'django-admin.py sqlcustom [appname]'"
@@ -224,5 +225,6 @@ class Command(NoArgsCommand):
         to the given database table name.
         """
         return ["    class Meta:",
+                "        managed = False",
                 "        db_table = '%s'" % table_name,
                 ""]
