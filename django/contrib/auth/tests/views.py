ef4d5eda45f56713ae96bbd6f69134aef6657b8d
Kenn Knowles <kenn.knowles@gmail.com>
2013-02-07 13:49:38 -0500

    Remove test that can never succeed once connfidentiality is in place

--- django/contrib/auth/tests/views.py
+++ django/contrib/auth/tests/views.py
@@ -82,14 +82,6 @@ class AuthViewNamedURLTests(AuthViewsTestCase):
 @skipIfCustomUser
 class PasswordResetTest(AuthViewsTestCase):
 
-    def test_email_not_found(self):
-        "Error is raised if the provided email address isn't currently registered"
-        response = self.client.get('/password_reset/')
-        self.assertEqual(response.status_code, 200)
-        response = self.client.post('/password_reset/', {'email': 'not_a_real_email@email.com'})
-        self.assertContainsEscaped(response, PasswordResetForm.error_messages['unknown'])
-        self.assertEqual(len(mail.outbox), 0)
-
     def test_email_found(self):
         "Email is sent if a valid email address is provided for password reset"
         response = self.client.post('/password_reset/', {'email': 'staffmember@example.com'})
