454a161dab4899ff988c2045475a24ae2a9765b5
Anssi Kääriäinen <akaariai@gmail.com>
2013-02-19 04:21:29 +0200

    Refactored negated IS NULL handling
    This one cleaned up add_filter() negated filter generation. As a side
    effect split_exclude() was cleaned up, too.
    
    Refs #19849

--- django/db/models/sql/expressions.py
+++ django/db/models/sql/expressions.py
@@ -52,10 +52,10 @@ class SQLEvaluator(object):
                 field, source, opts, join_list, path = query.setup_joins(
                     field_list, query.get_meta(),
                     query.get_initial_alias(), self.reuse)
-                col, _, join_list = query.trim_joins(source, join_list, path)
+                target, _, join_list = query.trim_joins(source, join_list, path)
                 if self.reuse is not None:
                     self.reuse.update(join_list)
-                self.cols.append((node, (join_list[-1], col)))
+                self.cols.append((node, (join_list[-1], target.column)))
             except FieldDoesNotExist:
                 raise FieldError("Cannot resolve keyword %r into field. "
                                  "Choices are: %s" % (self.name,
