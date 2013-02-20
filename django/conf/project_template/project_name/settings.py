8d4342f2c97db93c54a1a5fef26a526caac62df1
Carl Meyer <carl@oddbird.net>
2013-02-19 18:43:28 -0700

    [1.5.x] Fixed #19857 -- Fixed broken docs link in project template.

--- django/conf/project_template/project_name/settings.py
+++ django/conf/project_template/project_name/settings.py
@@ -22,7 +22,7 @@ DATABASES = {
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#allowed-hosts
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
 
 # Local time zone for this installation. Choices can be found here:
