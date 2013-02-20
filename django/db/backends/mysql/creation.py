fd3a066ae319956d14a35445d83293fecdfdcce7
Alex Gaynor <alex.gaynor@rd.io>
2013-02-19 18:05:02 -0800

    This function is unused and should have been removed a few releases ago.

--- django/db/backends/mysql/creation.py
+++ django/db/backends/mysql/creation.py
@@ -41,26 +41,3 @@ class DatabaseCreation(BaseDatabaseCreation):
     def sql_for_inline_foreign_key_references(self, model, field, known_models, style):
         "All inline references are pending under MySQL"
         return [], True
-
-    def sql_for_inline_many_to_many_references(self, model, field, style):
-        from django.db import models
-        opts = model._meta
-        qn = self.connection.ops.quote_name
-
-        table_output = [
-            '    %s %s %s,' %
-                (style.SQL_FIELD(qn(field.m2m_column_name())),
-                style.SQL_COLTYPE(models.ForeignKey(model).db_type(connection=self.connection)),
-                style.SQL_KEYWORD('NOT NULL')),
-            '    %s %s %s,' %
-            (style.SQL_FIELD(qn(field.m2m_reverse_name())),
-            style.SQL_COLTYPE(models.ForeignKey(field.rel.to).db_type(connection=self.connection)),
-            style.SQL_KEYWORD('NOT NULL'))
-        ]
-        deferred = [
-            (field.m2m_db_table(), field.m2m_column_name(), opts.db_table,
-                opts.pk.column),
-            (field.m2m_db_table(), field.m2m_reverse_name(),
-                field.rel.to._meta.db_table, field.rel.to._meta.pk.column)
-            ]
-        return table_output, deferred
