2f4a4703e1931fadf5ed81387b26cf84caf5bef9
Horst Gutmann <hgutmann@netconomy.net>
2013-02-23 13:39:21 +0100

    Fixed #19758 -- Avoided leaking email existence through the password reset form.

--- django/contrib/auth/tests/views.py
+++ django/contrib/auth/tests/views.py
@@ -86,11 +86,12 @@ class AuthViewNamedURLTests(AuthViewsTestCase):
 class PasswordResetTest(AuthViewsTestCase):
 
     def test_email_not_found(self):
-        "Error is raised if the provided email address isn't currently registered"
+        """If the provided email is not registered, don't raise any error but
+        also don't send any email."""
         response = self.client.get('/password_reset/')
         self.assertEqual(response.status_code, 200)
         response = self.client.post('/password_reset/', {'email': 'not_a_real_email@email.com'})
-        self.assertFormError(response, PasswordResetForm.error_messages['unknown'])
+        self.assertEqual(response.status_code, 302)
         self.assertEqual(len(mail.outbox), 0)
 
     def test_email_found(self):
