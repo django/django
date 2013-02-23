4450165cb65a374129c86fe75e3ddfd653865f37
Gokmen Gorgen <gokmen@alageek.com>
2013-02-23 13:35:24 +0200

    Refs #5405 -- pep8ize.

--- django/contrib/admindocs/tests/__init__.py
+++ django/contrib/admindocs/tests/__init__.py
@@ -14,8 +14,7 @@ class TestFieldType(unittest.TestCase):
 
     def test_field_name(self):
         self.assertRaises(AttributeError,
-            views.get_readable_field_data_type, "NotAField"
-        )
+                          views.get_readable_field_data_type, "NotAField")
 
     def test_builtin_fields(self):
         self.assertEqual(
@@ -29,7 +28,8 @@ class TestFieldType(unittest.TestCase):
             'A custom field type'
         )
         self.assertEqual(
-            views.get_readable_field_data_type(fields.DescriptionLackingField()),
+            views.get_readable_field_data_type(
+                fields.DescriptionLackingField()),
             _('Field of type: %(field_type)s') % {
                 'field_type': 'DescriptionLackingField'
             }
