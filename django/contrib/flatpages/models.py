6a1e6c5a1d470175f8241497ef90dbc8da848d78
Aymeric Augustin <aymeric.augustin@m4x.org>
2013-02-24 13:12:41 +0100

    URL-encoded URLs in FlatPage.get_absolute_url.

--- django/contrib/flatpages/models.py
+++ django/contrib/flatpages/models.py
@@ -3,8 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.sites.models import Site
 from django.utils.translation import ugettext_lazy as _
-from django.utils.encoding import python_2_unicode_compatible
-
+from django.utils.encoding import iri_to_uri, python_2_unicode_compatible
 
 @python_2_unicode_compatible
 class FlatPage(models.Model):
@@ -27,4 +26,4 @@ class FlatPage(models.Model):
         return "%s -- %s" % (self.url, self.title)
 
     def get_absolute_url(self):
-        return self.url
+        return iri_to_uri(self.url)
