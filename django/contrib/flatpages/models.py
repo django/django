b552d80e641875d10a624d4748a624acd2f8fa52
Aymeric Augustin <aymeric.augustin@m4x.org>
2013-02-24 13:25:17 +0100

    Fixed #18862 -- Honored script prefix in FlatPage.get_absolute_url.

--- django/contrib/flatpages/models.py
+++ django/contrib/flatpages/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.sites.models import Site
+from django.core.urlresolvers import get_script_prefix
 from django.utils.translation import ugettext_lazy as _
 from django.utils.encoding import iri_to_uri, python_2_unicode_compatible
 
@@ -26,4 +27,5 @@ class FlatPage(models.Model):
         return "%s -- %s" % (self.url, self.title)
 
     def get_absolute_url(self):
-        return iri_to_uri(self.url)
+        # Handle script prefix manually because we bypass reverse()
+        return iri_to_uri(get_script_prefix().rstrip('/') + self.url)
