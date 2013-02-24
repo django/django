457026a85df02c23b51c4ec0b942fc297e933474
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2013-02-24 16:41:10 +0100

    Fixes #6412 - More details about template loader.
    Report more details about template files in loader postmortem.

--- tests/regressiontests/views/tests/__init__.py
+++ tests/regressiontests/views/tests/__init__.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import
 
-from .debug import (DebugViewTests, ExceptionReporterTests,
-    ExceptionReporterTests, PlainTextReportTests, ExceptionReporterFilterTests,
-    AjaxResponseExceptionReporterFilter)
+from .debug import (
+    DebugViewTests, ExceptionReporterTests, PlainTextReportTests,
+    ExceptionReporterFilterTests, AjaxResponseExceptionReporterFilter)
 from .defaults import DefaultsTests
 from .i18n import JsI18NTests, I18NTests, JsI18NTestsMultiPackage, JavascriptI18nTests
 from .shortcuts import ShortcutTests
