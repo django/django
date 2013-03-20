84fde70af89e3f6365b666e3bd015d25fd5c579c
Marc Tamlyn <marc.tamlyn@gmail.com>
2013-03-20 10:47:56 +0000

    Fixed #20094 - Be more careful when checking for Iterator
    Python 2.6 has some different behaviour when checking
    isinstance(foo, collections.Iterator).

--- django/db/models/sql/where.py
+++ django/db/models/sql/where.py
@@ -4,7 +4,6 @@ Code to manage the creation and SQL rendering of 'where' constraints.
 
 from __future__ import absolute_import
 
-import collections
 import datetime
 from itertools import repeat
 
@@ -12,6 +11,7 @@ from django.conf import settings
 from django.db.models.fields import DateTimeField, Field
 from django.db.models.sql.datastructures import EmptyResultSet, Empty
 from django.db.models.sql.aggregates import Aggregate
+from django.utils.itercompat import is_iterator
 from django.utils.six.moves import xrange
 from django.utils import timezone
 from django.utils import tree
@@ -58,7 +58,7 @@ class WhereNode(tree.Node):
         if not isinstance(data, (list, tuple)):
             return data
         obj, lookup_type, value = data
-        if isinstance(value, collections.Iterator):
+        if is_iterator(value):
             # Consume any generators immediately, so that we can determine
             # emptiness and transform any non-empty values correctly.
             value = list(value)
