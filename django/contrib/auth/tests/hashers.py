25f2acfed0fc110f88abbfffb5c5c62a76670db0
Donald Stufft <donald@stufft.io>
2013-03-26 11:44:26 -0400

    Fixed #20138 -- Added BCryptSHA256PasswordHasher
    BCryptSHA256PasswordHasher pre-hashes the users password using
    SHA256 to prevent the 72 byte truncation inherient in the BCrypt
    algorithm.

--- django/contrib/auth/tests/hashers.py
+++ django/contrib/auth/tests/hashers.py
@@ -93,6 +93,22 @@ class TestUtilsHashPass(unittest.TestCase):
         self.assertEqual(identify_hasher(encoded).algorithm, "crypt")
 
     @skipUnless(bcrypt, "py-bcrypt not installed")
+    def test_bcrypt_sha256(self):
+        encoded = make_password('lètmein', hasher='bcrypt_sha256')
+        self.assertTrue(is_password_usable(encoded))
+        self.assertTrue(encoded.startswith('bcrypt_sha256$'))
+        self.assertTrue(check_password('lètmein', encoded))
+        self.assertFalse(check_password('lètmeinz', encoded))
+        self.assertEqual(identify_hasher(encoded).algorithm, "bcrypt_sha256")
+
+        # Verify that password truncation no longer works
+        password = ('VSK0UYV6FFQVZ0KG88DYN9WADAADZO1CTSIVDJUNZSUML6IBX7LN7ZS3R5'
+                    'JGB3RGZ7VI7G7DJQ9NI8BQFSRPTG6UWTTVESA5ZPUN')
+        encoded = make_password(password, hasher='bcrypt_sha256')
+        self.assertTrue(check_password(password, encoded))
+        self.assertFalse(check_password(password[:72], encoded))
+
+    @skipUnless(bcrypt, "py-bcrypt not installed")
     def test_bcrypt(self):
         encoded = make_password('lètmein', hasher='bcrypt')
         self.assertTrue(is_password_usable(encoded))
