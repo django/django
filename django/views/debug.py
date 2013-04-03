a3d0057e244bf90bffeafdba577aa29dda008146
rikatee <rikatee@googlemail.com>
2013-04-03 23:51:23 +0200

    Update debug.py
    500 error debug response is nerfed as of Django 1.4. The change 
    resulted in 500 server errors only returning the traceback body, and 
    not the nice interactive debug response.
    
    This change makes working with AJAX in >1.4 very difficult compared with
    <1.4. 
    
    My change makes this nerfing optional. Just set NERF_DEBUG_ON_AJAX
    to False in your settings.py

--- django/views/debug.py
+++ django/views/debug.py
@@ -62,7 +62,7 @@ def technical_500_response(request, exc_type, exc_value, tb):
     the values returned from sys.exc_info() and friends.
     """
     reporter = ExceptionReporter(request, exc_type, exc_value, tb)
-    if request.is_ajax():
+    if request.is_ajax() and settings.NERF_DEBUG_ON_AJAX:
         text = reporter.get_traceback_text()
         return HttpResponseServerError(text, content_type='text/plain')
     else:
