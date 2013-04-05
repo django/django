216580e03499e035b93283df00fcc03b346fef7b
Simon Charette <charette.s@gmail.com>
2013-04-05 13:59:15 -0400

    Fixed #20207 -- Handle ManyToManyField with a unicode name correctly.

--- tests/many_to_many/models.py
+++ tests/many_to_many/models.py
@@ -6,6 +6,7 @@ To define a many-to-many relationship, use ``ManyToManyField()``.
 In this example, an ``Article`` can be published in multiple ``Publication``
 objects, and a ``Publication`` has multiple ``Article`` objects.
 """
+from __future__ import unicode_literals
 
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
@@ -24,7 +25,9 @@ class Publication(models.Model):
 @python_2_unicode_compatible
 class Article(models.Model):
     headline = models.CharField(max_length=100)
-    publications = models.ManyToManyField(Publication)
+    # Assign a unicode string as name to make sure the intermediary model is
+    # correctly created. Refs #20207
+    publications = models.ManyToManyField(Publication, name='publications')
 
     def __str__(self):
         return self.headline
