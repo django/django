f554ced631986da30f20c0d77b4aa299edb8c671
Preston Timmons <prestontimmons@gmail.com>
2013-04-06 14:04:06 -0500

    Modified view_tests for unittest2 discovery.

--- tests/view_tests/tests/__init__.py
+++ tests/view_tests/tests/__init__.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import
 
-from .debug import (DebugViewTests, ExceptionReporterTests,
+from .test_debug import (DebugViewTests, ExceptionReporterTests,
     ExceptionReporterTests, PlainTextReportTests, ExceptionReporterFilterTests,
     AjaxResponseExceptionReporterFilter)
 from .defaults import DefaultsTests
-from .i18n import JsI18NTests, I18NTests, JsI18NTestsMultiPackage, JavascriptI18nTests
-from .shortcuts import ShortcutTests
-from .specials import URLHandling
-from .static import StaticHelperTest, StaticUtilsTests, StaticTests
+from .test_i18n import JsI18NTests, I18NTests, JsI18NTestsMultiPackage, JavascriptI18nTests
+from .test_shortcuts import ShortcutTests
+from .test_specials import URLHandling
+from .test_static import StaticHelperTest, StaticUtilsTests, StaticTests
