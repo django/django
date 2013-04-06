cdf520ee86e7613f29421fb2c99646ce00899b3d
Preston Timmons <prestontimmons@gmail.com>
2013-04-06 11:44:58 -0500

    Modified forms_tests to work with unittest2 discovery.

--- tests/forms_tests/tests/__init__.py
+++ tests/forms_tests/tests/__init__.py
@@ -1,21 +1,21 @@
 from __future__ import absolute_import
 
-from .error_messages import (FormsErrorMessagesTestCase,
+from .test_error_messages import (FormsErrorMessagesTestCase,
     ModelChoiceFieldErrorMessagesTestCase)
-from .extra import FormsExtraTestCase, FormsExtraL10NTestCase
-from .fields import FieldsTests
-from .forms import FormsTestCase
-from .formsets import (FormsFormsetTestCase, FormsetAsFooTests,
+from .test_extra import FormsExtraTestCase, FormsExtraL10NTestCase
+from .test_fields import FieldsTests
+from .test_forms import FormsTestCase
+from .test_formsets import (FormsFormsetTestCase, FormsetAsFooTests,
     TestIsBoundBehavior, TestEmptyFormSet)
-from .input_formats import (LocalizedTimeTests, CustomTimeInputFormatsTests,
+from .test_input_formats import (LocalizedTimeTests, CustomTimeInputFormatsTests,
     SimpleTimeFormatTests, LocalizedDateTests, CustomDateInputFormatsTests,
     SimpleDateFormatTests, LocalizedDateTimeTests,
     CustomDateTimeInputFormatsTests, SimpleDateTimeFormatTests)
-from .media import FormsMediaTestCase, StaticFormsMediaTestCase
+from .test_media import FormsMediaTestCase, StaticFormsMediaTestCase
 from .tests import (TestTicket12510, ModelFormCallableModelDefault,
     FormsModelTestCase, RelatedModelFormTests)
-from .regressions import FormsRegressionsTestCase
-from .util import FormsUtilTestCase
-from .validators import TestFieldWithValidators
-from .widgets import (FormsWidgetTestCase, FormsI18NWidgetsTestCase,
+from .test_regressions import FormsRegressionsTestCase
+from .test_util import FormsUtilTestCase
+from .test_validators import TestFieldWithValidators
+from .test_widgets import (FormsWidgetTestCase, FormsI18NWidgetsTestCase,
     WidgetTests, LiveWidgetTests, ClearableFileInputTests)
