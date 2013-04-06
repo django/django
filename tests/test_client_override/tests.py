244b047b8e47ae4ce5f63893be366a06df6a2c8b
Preston Timmons <prestontimmons@gmail.com>
2013-04-06 12:56:53 -0500

    Modified test_client_override to work with unittest2 discovery.

--- tests/test_client_override/tests.py
+++ tests/test_client_override/tests.py
@@ -1,21 +1,34 @@
+from django.db.models import get_app
+from django.test.simple import build_suite
 from django.utils import unittest
 
 
 def suite():
     """
-    Validate that you can override the default test suite
-    Define a suite that deliberately ignores a test defined in
-    this module.
+    Validate that you can define a custom suite when running tests
+    with `django.test.simple.DjangoTestSuiteRunner`.
     """
 
     testSuite = unittest.TestSuite()
-    testSuite.addTest(SampleTests('testGoodStuff'))
+    testSuite.addTest(SuiteOverrideTest('test_suite_override'))
     return testSuite
 
 
+class SuiteOverrideTest(unittest.TestCase):
+
+    def test_suite_override(self):
+        app = get_app("test_client_override")
+        suite = build_suite(app)
+        self.assertEqual(suite.countTestCases(), 1)
+
+
 class SampleTests(unittest.TestCase):
-    def testGoodStuff(self):
+
+    def test_one(self):
         pass
 
-    def testBadStuff(self):
-        self.fail("This test shouldn't run")
+    def test_two(self):
+        pass
+
+    def test_threee(self):
+        pass
