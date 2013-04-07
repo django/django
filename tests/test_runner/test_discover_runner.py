4b1a9b1b7d2bda3c0d7cb5613bca7098424c4446
Preston Timmons <prestontimmons@gmail.com>
2013-04-06 20:06:00 -0500

    Fixed up test_discover_runner tests

--- tests/test_runner/test_discover_runner.py
+++ tests/test_runner/test_discover_runner.py
@@ -1,10 +1,9 @@
-from os.path import dirname, join, realpath
-
 from django.test import TestCase
 from django.test.runner import DiscoverRunner
 
 
 class DiscoverRunnerTest(TestCase):
+
     def test_dotted_test_module(self):
         count = DiscoverRunner().build_suite(
             ["test_discovery_sample.tests_sample"],
@@ -27,31 +26,8 @@ class DiscoverRunnerTest(TestCase):
         self.assertEqual(count, 1)
 
     def test_pattern(self):
-        root = realpath(join(dirname(__file__), "../test_discovery_sample"))
-
         count = DiscoverRunner(
             pattern="*_tests.py",
-            root=root,
-        ).build_suite().countTestCases()
-
-        self.assertEqual(count, 1)
-
-    def test_root(self):
-        root = realpath(join(dirname(__file__), "../test_discovery_sample2"))
-
-        count = DiscoverRunner(
-            root=root,
-        ).build_suite().countTestCases()
-
-        self.assertEqual(count, 1)
-
-    def test_top(self):
-        top = realpath(join(dirname(__file__), ".."))
-        root = realpath(join(top, "test_discovery_sample2"))
-
-        count = DiscoverRunner(
-            root=root,
-            top_level=top,
-        ).build_suite().countTestCases()
+        ).build_suite(["test_discovery_sample"]).countTestCases()
 
         self.assertEqual(count, 1)
