8969366cdb24499f12f1d67a22b0cfe77c85c8d8
Carl Meyer <carl@oddbird.net>
2013-05-08 14:43:09 -0600

    More thorough test runner tests, fix using vanilla unittest TestCase classes on Python 2.6.

--- tests/test_discovery_sample/tests_sample.py
+++ tests/test_discovery_sample/tests_sample.py
@@ -4,19 +4,19 @@ from django.test import TestCase as DjangoTestCase
 from django.utils.unittest import TestCase as UT2TestCase
 
 
-class Test1(UnitTestCase):
+class TestVanillaUnittest(UnitTestCase):
 
     def test_sample(self):
         self.assertEqual(1, 1)
 
 
-class Test2(UT2TestCase):
+class TestUnittest2(UT2TestCase):
 
     def test_sample(self):
         self.assertEqual(1, 1)
 
 
-class Test3(DjangoTestCase):
+class TestDjangoTestCase(DjangoTestCase):
 
     def test_sample(self):
         self.assertEqual(1, 1)
--- tests/test_runner/test_discover_runner.py
+++ tests/test_runner/test_discover_runner.py
@@ -11,16 +11,44 @@ class DiscoverRunnerTest(TestCase):
 
         self.assertEqual(count, 3)
 
-    def test_dotted_test_class(self):
+    def test_dotted_test_class_vanilla_unittest(self):
         count = DiscoverRunner().build_suite(
-            ["test_discovery_sample.tests_sample.Test1"],
+            ["test_discovery_sample.tests_sample.TestVanillaUnittest"],
         ).countTestCases()
 
         self.assertEqual(count, 1)
 
-    def test_dotted_test_method(self):
+    def test_dotted_test_class_unittest2(self):
         count = DiscoverRunner().build_suite(
-            ["test_discovery_sample.tests_sample.Test1.test_sample"],
+            ["test_discovery_sample.tests_sample.TestUnittest2"],
+        ).countTestCases()
+
+        self.assertEqual(count, 1)
+
+    def test_dotted_test_class_django_testcase(self):
+        count = DiscoverRunner().build_suite(
+            ["test_discovery_sample.tests_sample.TestDjangoTestCase"],
+        ).countTestCases()
+
+        self.assertEqual(count, 1)
+
+    def test_dotted_test_method_vanilla_unittest(self):
+        count = DiscoverRunner().build_suite(
+            ["test_discovery_sample.tests_sample.TestVanillaUnittest.test_sample"],
+        ).countTestCases()
+
+        self.assertEqual(count, 1)
+
+    def test_dotted_test_method_unittest2(self):
+        count = DiscoverRunner().build_suite(
+            ["test_discovery_sample.tests_sample.TestUnittest2.test_sample"],
+        ).countTestCases()
+
+        self.assertEqual(count, 1)
+
+    def test_dotted_test_method_django_testcase(self):
+        count = DiscoverRunner().build_suite(
+            ["test_discovery_sample.tests_sample.TestDjangoTestCase.test_sample"],
         ).countTestCases()
 
         self.assertEqual(count, 1)
