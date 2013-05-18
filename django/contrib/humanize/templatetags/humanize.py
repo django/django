6dc5303d1158015b8ee0f73b095f7e5b316ea619
Emil Stenström <em@kth.se>
2013-05-18 17:11:15 +0200

    Add parameter to disable timesince and time until filters. Use said parameter to make humanize tests behave like before.

--- django/contrib/humanize/templatetags/humanize.py
+++ django/contrib/humanize/templatetags/humanize.py
@@ -189,7 +189,7 @@ def naturaltime(value):
         if delta.days != 0:
             return pgettext(
                 'naturaltime', '%(delta)s ago'
-            ) % {'delta': defaultfilters.timesince(value, now)}
+            ) % {'delta': defaultfilters.timesince(value, now, avoid_wrapping=False)}
         elif delta.seconds == 0:
             return _('now')
         elif delta.seconds < 60:
@@ -211,7 +211,7 @@ def naturaltime(value):
         if delta.days != 0:
             return pgettext(
                 'naturaltime', '%(delta)s from now'
-            ) % {'delta': defaultfilters.timeuntil(value, now)}
+            ) % {'delta': defaultfilters.timeuntil(value, now, avoid_wrapping=False)}
         elif delta.seconds == 0:
             return _('now')
         elif delta.seconds < 60:
