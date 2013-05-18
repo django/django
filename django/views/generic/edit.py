55759d0e4c48b9b53db7b60dbb37199b86d21951
Karol Sikora <elektrrrus@gmail.com>
2013-05-18 12:42:18 +0200

    removed unneded code in get_context_data in FormMixin

--- django/views/generic/edit.py
+++ django/views/generic/edit.py
@@ -144,9 +144,6 @@ class ModelFormMixin(FormMixin, SingleObjectMixin):
         context = {}
         if self.object:
             context['object'] = self.object
-            context_object_name = self.get_context_object_name(self.object)
-            if context_object_name:
-                context[context_object_name] = self.object
         context.update(kwargs)
         return super(ModelFormMixin, self).get_context_data(**context)
 
