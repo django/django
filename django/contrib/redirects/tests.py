6b0fa7d57b2f0009460467fddd40840468e002b9
Ryan Kaskel <dev@ryankaskel.com>
2013-05-20 20:22:38 +0100

    Allow redirect middleware HTTP responses to be overridden. refs #19321

--- django/contrib/redirects/tests.py
+++ django/contrib/redirects/tests.py
@@ -1,3 +1,4 @@
+from django import http
 from django.conf import settings
 from django.contrib.sites.models import Site
 from django.core.exceptions import ImproperlyConfigured
@@ -61,3 +62,32 @@ class RedirectTests(TestCase):
     def test_sites_not_installed(self):
         with self.assertRaises(ImproperlyConfigured):
             RedirectFallbackMiddleware()
+
+
+class OverriddenRedirectFallbackMiddleware(RedirectFallbackMiddleware):
+    # Use HTTP responses different from the defaults
+    response_gone_class = http.HttpResponseForbidden
+    response_redirect_class = http.HttpResponseRedirect
+
+
+@override_settings(
+    MIDDLEWARE_CLASSES=list(settings.MIDDLEWARE_CLASSES) +
+        ['django.contrib.redirects.tests.OverriddenRedirectFallbackMiddleware'],
+    SITE_ID=1,
+)
+class OverriddenRedirectMiddlewareTests(TestCase):
+
+    def setUp(self):
+        self.site = Site.objects.get(pk=settings.SITE_ID)
+
+    def test_response_gone_class(self):
+        Redirect.objects.create(
+            site=self.site, old_path='/initial/', new_path='')
+        response = self.client.get('/initial/')
+        self.assertEqual(response.status_code, 403)
+
+    def test_response_redirect_class(self):
+        Redirect.objects.create(
+            site=self.site, old_path='/initial/', new_path='/new_target/')
+        response = self.client.get('/initial/')
+        self.assertEqual(response.status_code, 302)
