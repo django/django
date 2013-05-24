7b21f65a5d6f8905cac14da65c730637d87ef08b
Preston Holmes <preston@ptone.com>
2013-05-23 17:18:59 -0700

    Made case of exception name more consistant

--- django/contrib/admin/exceptions.py
+++ django/contrib/admin/exceptions.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import SuspiciousOperation
 
 
-class DisallowedModeladminLookup(SuspiciousOperation):
     "Invalid filter was passed to admin view via URL querystring"
+class DisallowedModelAdminLookup(SuspiciousOperation):
     pass
