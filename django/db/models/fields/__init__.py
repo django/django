48493cff73fd01870306965f1c48193602754a78
Andrew Godwin <andrew@aeracode.org>
2013-06-22 12:56:37 +0100

    Remove EmailField max_length default removal in deconstruct()

--- django/db/models/fields/__init__.py
+++ django/db/models/fields/__init__.py
@@ -1129,8 +1129,8 @@ class EmailField(CharField):
 
     def deconstruct(self):
         name, path, args, kwargs = super(EmailField, self).deconstruct()
-        if kwargs.get("max_length", None) == 75:
-            del kwargs['max_length']
+        # We do not exclude max_length if it matches default as we want to change
+        # the default in future.
         return name, path, args, kwargs
 
     def formfield(self, **kwargs):
