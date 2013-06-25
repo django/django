441c292e5e28279d51c693af5683730cb6c035a5
Christopher Medrela <chris.medrela@gmail.com>
2013-06-25 22:06:46 +0200

    first tests pass (tests checking CharField.max_length attribute)
    - Added Error.__eq__(self, other) method
    - Changed behaviour of Field.check(self, **kwargs) -- added setting `obj`
      attribute for each error raised by `check_*` methods.
    - Written CharField.check_max_length_attribute method.
    - Deleted relevant tests from invalid_models.old_invalid_models module.

--- django/core/checks/base.py
+++ django/core/checks/base.py
@@ -1,8 +1,16 @@
+# -*- coding: utf8 -*-
+
+
 class _Error(object):
     def __init__(self, msg, **kwargs):
         assert 'hint' in kwargs
         self.msg = msg
         self.hint = kwargs['hint']
+        self.obj = None
+
+    def __eq__(self, other):
+        return all(getattr(self, attr) == getattr(other, attr)
+            for attr in ['msg', 'hint'])
 
 
 class Error(_Error):
