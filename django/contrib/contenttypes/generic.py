b930733a67cbc1235daf675de06d73e3b6406113
Tim Graham <timograham@gmail.com>
2013-06-29 14:14:32 -0400

    [1.6.x] Fixed #20677 - Typos in generic_inlineformset_factory docs.
    Thanks Riley Strong for the report.
    
    Backport of 3fd0ee5b46 from master

--- django/contrib/contenttypes/generic.py
+++ django/contrib/contenttypes/generic.py
@@ -434,8 +434,8 @@ def generic_inlineformset_factory(model, form=ModelForm,
     """
     Returns a ``GenericInlineFormSet`` for the given kwargs.
 
-    You must provide ``ct_field`` and ``object_id`` if they different from the
-    defaults ``content_type`` and ``object_id`` respectively.
+    You must provide ``ct_field`` and ``fk_field`` if they are different from
+    the defaults ``content_type`` and ``object_id`` respectively.
     """
     opts = model._meta
     # if there is no field called `ct_field` let the exception propagate
