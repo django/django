09b446dfe86f01f9d21e8c6351a31c5587a5b7a9
Aymeric Augustin <aymeric.augustin@m4x.org>
2013-07-01 22:28:45 +0200

    This doesn't need to be a package any more.
    Refs #20680.

--- django/utils/unittest.py
+++ django/utils/unittest.py
@@ -1,7 +1,9 @@
+from __future__ import absolute_import
+
 import warnings
 
 warnings.warn("django.utils.unittest will be removed in Django 1.9.",
-    PendingDeprecationWarning)
+    PendingDeprecationWarning, stacklevel=2)
 
 try:
     from unittest2 import *
