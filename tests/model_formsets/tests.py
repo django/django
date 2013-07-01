4c1029971e810cde32ee4cd489627239b9b1b6ed
Tim Graham <timograham@gmail.com>
2013-07-01 09:36:31 -0400

    [1.6.x] Fixed a couple form/formset deprecation warnings in tests.
    Backport of a521d10322 from master.

--- tests/model_formsets/tests.py
+++ tests/model_formsets/tests.py
@@ -399,7 +399,7 @@ class ModelFormsetTest(TestCase):
                 super(BaseAuthorFormSet, self).__init__(*args, **kwargs)
                 self.queryset = Author.objects.filter(name__startswith='Charles')
 
-        AuthorFormSet = modelformset_factory(Author, formset=BaseAuthorFormSet)
+        AuthorFormSet = modelformset_factory(Author, fields='__all__', formset=BaseAuthorFormSet)
         formset = AuthorFormSet()
         self.assertEqual(len(formset.get_queryset()), 1)
 
