71476b9bb1727a24fbae974fa69e8175673cb032
Christopher Medrela <chris.medrela@gmail.com>
2013-07-05 20:24:10 +0200

    clean up: deleted unused imports and models

--- django/core/management/validation.py
+++ django/core/management/validation.py
@@ -25,9 +25,8 @@ def get_validation_errors(outfile, app=None):
     validates all models of all installed apps. Writes errors, if any, to outfile.
     Returns number of errors.
     """
-    from django.db import models, connection
+    from django.db import models
     from django.db.models.loading import get_app_errors
-    from django.db.models.deletion import SET_NULL, SET_DEFAULT
 
     e = ModelErrorCollection(outfile)
 
