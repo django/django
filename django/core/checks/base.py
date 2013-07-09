142e15fc56f8e8871e52da21f4740f355ada9808
Christopher Medrela <chris.medrela@gmail.com>
2013-07-09 14:00:43 +0200

    added BaseCheckError.__ne__ method

--- django/core/checks/base.py
+++ django/core/checks/base.py
@@ -11,6 +11,9 @@ class BaseCheckError(object):
         return all(getattr(self, attr) == getattr(other, attr)
             for attr in ['msg', 'hint', 'obj'])
 
+    def __ne__(self, other):
+        return not (self == other)
+
     def __repr__(self):
         return "<%s: msg=%r, hint=%r, obj=%r>" % \
             (self.__class__.__name__, self.msg, self.hint, self.obj)
