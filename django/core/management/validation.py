a3a3a2251c666aa206d982f969d96338bc04f713
Christopher Medrela <chris.medrela@gmail.com>
2013-07-11 10:33:39 +0200

    rewritten ImageField checks

--- django/core/management/validation.py
+++ django/core/management/validation.py
@@ -2,6 +2,7 @@ import collections
 import sys
 
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 from django.core.management.color import color_style
 from django.utils.encoding import force_str
 from django.utils.itercompat import is_iterable
@@ -64,12 +65,6 @@ def get_validation_errors(outfile, app=None):
                 e.add(opts, "Field '%s' has column name '%s' that is already used." % (f.name, column_name))
             else:
                 used_column_names.append(column_name)
-
-            if isinstance(f, models.ImageField):
-                try:
-                    from django.utils.image import Image
-                except ImportError:
-                    e.add('%s: "%s": To use ImageFields, you need to install Pillow. Get it at https://pypi.python.org/pypi/Pillow.' % (opts, f.name))
 
         seen_intermediary_signatures = []
         for f in opts.local_many_to_many:
