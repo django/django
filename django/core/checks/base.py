e97f5c6ccab137f1be5e6b3f76a005e47f2596a5
Christopher Medrela <chris.medrela@gmail.com>
2013-07-11 15:58:50 +0200

    changed get_validation_errors and its callers; added BaseCheckError.__str__

--- django/core/checks/base.py
+++ django/core/checks/base.py
@@ -1,6 +1,10 @@
 # -*- coding: utf8 -*-
+from __future__ import unicode_literals
+
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class BaseCheckError(object):
     def __init__(self, msg, hint, obj=None):
         self.msg = msg
@@ -13,6 +17,27 @@ class BaseCheckError(object):
 
     def __ne__(self, other):
         return not (self == other)
+
+    def __str__(self):
+        from django.db import models
+        from django.db.models.fields import Field
+
+        result = ""
+        if isinstance(self.obj, Field):
+            model = self.obj.model
+            app = model._meta.app_label
+            result += '%s.%s.%s: ' \
+                % (app, model._meta.object_name, self.obj.name)
+        elif isinstance(self.obj, models.base.ModelBase):
+            model = self.obj
+            app = model._meta.app_label
+            result += '%s.%s: ' % (app, model._meta.object_name)
+        else:
+            result += '?: '
+        result += self.msg
+        if self.hint:
+            result += "\nHINT: %s" % self.hint
+        return result
 
     def __repr__(self):
         return "<%s: msg=%r, hint=%r, obj=%r>" % \
--- django/core/management/validation.py
+++ django/core/management/validation.py
@@ -1,43 +1,16 @@
-import sys
-
-from django.core.management.color import color_style
-from django.utils.encoding import force_str
+# -*- encoding: utf-8 -*-
+from __future__ import unicode_literals
 
 
-class ModelErrorCollection:
-    def __init__(self, outfile=sys.stdout):
-        self.errors = 0
-        self.outfile = outfile
-        self.style = color_style()
-
-    def add(self, msg):
-        self.errors += 1
-        self.outfile.write(self.style.ERROR(force_str(msg)))
-
-
-def get_validation_errors(outfile, app=None):
+def get_validation_errors(app=None):
     """
     Validates all models that are part of the specified app. If no app name is
     provided, validates all models of all installed apps. Writes errors, if any,
     to outfile. Returns number of errors.
     """
     from django.db import models
-    from django.db.models.loading import get_app_errors
 
-    e = ModelErrorCollection(outfile)
-    all_errors = []
-
-    for (app_name, error) in get_app_errors().items():
-        e.add(app_name, error)
-
-    # Trigger checking framework for each model
+    errors = []
     for cls in models.get_models(app, include_swapped=True):
-        errors = cls.check()
-        for error in errors:
-            msg = error.msg
-            if error.hint:
-                msg += '\n%s' % error.hint
-            e.add(msg)
-        all_errors.extend(errors)
-
-    return all_errors
+        errors.extend(cls.check())
+    return errors
