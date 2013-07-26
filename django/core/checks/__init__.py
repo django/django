ed1458233080ede6ab0a5a67d85e1c66db97627b
Christopher Medrela <chris.medrela@gmail.com>
2013-07-26 18:29:13 +0200

    introduced message levels

--- django/core/checks/__init__.py
+++ django/core/checks/__init__.py
@@ -1,7 +1,9 @@
 # -*- coding: utf8 -*-
 from __future__ import unicode_literals
 
-from .messages import Error, Warning
+from .messages import (CheckMessage,
+        Debug, Info, Warning, Error, Critical,
+        DEBUG, INFO, WARNING, ERROR, CRITICAL)
 from .registration import register, run_checks
 from .default_checks import check_all_models
 
