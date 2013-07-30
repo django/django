d40bb7ad63a1a56e1be6ae5ed62ccd254e62d3ba
Tim Graham <timograham@gmail.com>
2013-07-30 08:24:13 -0400

    Fixed #12103 -- Added AuthenticationForm.confirm_login_allowed to allow customizing the logic policy.
    Thanks ejucovy and lasko for work on the patch.

--- django/contrib/auth/forms.py
+++ django/contrib/auth/forms.py
@@ -191,12 +191,27 @@ class AuthenticationForm(forms.Form):
                     code='invalid_login',
                     params={'username': self.username_field.verbose_name},
                 )
-            elif not self.user_cache.is_active:
-                raise forms.ValidationError(
-                    self.error_messages['inactive'],
-                    code='inactive',
-                )
+            else:
+                self.confirm_login_allowed(self.user_cache)
+
         return self.cleaned_data
+
+    def confirm_login_allowed(self, user):
+        """
+        Controls whether the given User may log in. This is a policy setting,
+        independent of end-user authentication. This default behavior is to
+        allow login by active users, and reject login by inactive users.
+
+        If the given user cannot log in, this method should raise a
+        ``forms.ValidationError``.
+
+        If the given user may log in, this method should return None.
+        """
+        if not user.is_active:
+            raise forms.ValidationError(
+                self.error_messages['inactive'],
+                code='inactive',
+            )
 
     def get_user_id(self):
         if self.user_cache:
