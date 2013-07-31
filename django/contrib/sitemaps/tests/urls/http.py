9bc9a5badf684269439b1f60c0d8580eee06dc02
Julian Bez <julian.bez@uhura.de>
2013-07-31 10:34:02 +0200

    Fix for python 3 and clearify condition

--- django/contrib/sitemaps/tests/urls/http.py
+++ django/contrib/sitemaps/tests/urls/http.py
@@ -28,7 +28,7 @@ class FixedLastmodMixedSitemap(Sitemap):
 
     def items(self):
         o1 = TestModel()
-        o1.lastmod = datetime(2013, 03, 13, 10, 0, 0)
+        o1.lastmod = datetime(2013, 3, 13, 10, 0, 0)
         o2 = TestModel()
         return [o1, o2]
 
