2ac89012d8ff750ea5443b6f6f347dacb697e059
SusanTan <onceuponatimeforever@gmail.com>
2013-08-07 01:07:16 -0700

    Fixed #12288 -- Added unique validation for INSTALLED_APPS

--- tests/settings_tests/tests.py
+++ tests/settings_tests/tests.py
@@ -226,6 +226,27 @@ class TestComplexSettingOverride(TestCase):
             self.assertEqual('Overriding setting TEST_WARN can lead to unexpected behaviour.', str(w[-1].message))
 
 
+class UniqueSettngsTests(TestCase):
+    """
+    Tests for the INSTALLED_APPS setting.
+    """
+    settings_module = settings
+
+    def setUp(self):
+        self._installed_apps = self.settings_module.INSTALLED_APPS
+
+    def tearDown(self):
+        self.settings_module.INSTALLED_APPS = self._installed_apps
+
+    def test_unique(self):
+        """
+        An ImproperlyConfigured exception is raised if the INSTALLED_APPS contains
+        any duplicate strings.
+        """
+        with self.assertRaises(ImproperlyConfigured):
+            self.settings_module.INSTALLED_APPS = ("myApp1", "myApp1", "myApp2", "myApp3")
+
+
 class TrailingSlashURLTests(TestCase):
     """
     Tests for the MEDIA_URL and STATIC_URL settings.
