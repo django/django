d985fd7a189cdbfa2093c1225bdeb7eefaeb6978
Andrew Godwin <andrew@aeracode.org>
2013-08-11 14:27:42 +0100

    Fix tablespace command

--- django/db/backends/schema.py
+++ django/db/backends/schema.py
@@ -312,7 +312,7 @@ class BaseDatabaseSchemaEditor(object):
         """
         Moves a model's table between tablespaces
         """
-        self.execute(self.sql_rename_table % {
+        self.execute(self.sql_retablespace_table % {
             "table": self.quote_name(model._meta.db_table),
             "old_tablespace": self.quote_name(old_db_tablespace),
             "new_tablespace": self.quote_name(new_db_tablespace),
