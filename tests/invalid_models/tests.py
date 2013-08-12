a0f03b42f2d4dbf2870621d829bcd180a22a1f04
Christopher Medrela <chris.medrela@gmail.com>
2013-08-12 17:55:05 +0200

    added test for DatabaseValidation.validate_field

--- tests/invalid_models/tests.py
+++ tests/invalid_models/tests.py
@@ -728,19 +728,23 @@ class FilePathFieldTests(IsolatedModelsTestCase):
 
 class BackendSpecificChecksTests(IsolatedModelsTestCase):
 
-    def test(self):
-        error = Error('an error', hint=None)
-        mock = lambda self, field, **kwargs: [error]
+    def test_check_field(self):
+        """ Test if backend specific checks are performed. """
 
-        # Mock connection.validation.check_field method.
-        v = connection.validation
-        old_check_field = v.check_field
-        v.check_field = MethodType(mock, v)
+        error = Error('an error', hint=None)
+
+        def mock(self, field, **kwargs):
+            return [error]
 
         class Model(models.Model):
             field = models.IntegerField()
 
         field = Model._meta.get_field('field')
+
+        # Mock connection.validation.check_field method.
+        v = connection.validation
+        old_check_field = v.check_field
+        v.check_field = MethodType(mock, v)
         try:
             errors = field.check()
         finally:
@@ -748,6 +752,37 @@ class BackendSpecificChecksTests(IsolatedModelsTestCase):
             v.check_field = old_check_field
 
         self.assertEqual(errors, [error])
+
+    def test_validate_field(self):
+        """ Errors raised by deprecated `validate_field` method should be
+        collected. """
+
+        def mock(self, errors, opts, field):
+            errors.add(opts, "An error!")
+
+        class Model(models.Model):
+            field = models.IntegerField()
+
+        field = Model._meta.get_field('field')
+        expected = [
+            Error(
+                "An error!",
+                hint=None,
+                obj=field,
+            )
+        ]
+
+        # Mock connection.validation.validate_field method.
+        v = connection.validation
+        old_validate_field = v.validate_field
+        v.validate_field = MethodType(mock, v)
+        try:
+            errors = field.check()
+        finally:
+            # Unmock connection.validation.validate_field method.
+            v.validate_field = old_validate_field
+
+        self.assertEqual(errors, expected)
 
 
 class AccessorClashTests(IsolatedModelsTestCase):
