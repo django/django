d22db05b2d068aaa1ed568f90099c26ab944094a
Ramiro Morales <cramm0@gmail.com>
2013-08-13 19:58:05 -0300

    Add docstring to new unittest test case class.

--- django/contrib/staticfiles/testing.py
+++ django/contrib/staticfiles/testing.py
@@ -4,5 +4,11 @@ from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 
 class StaticLiveServerCase(LiveServerTestCase):
+    """
+    This class extends django.test.LiveServerTestCase to transparently overlay
+    at test execution-time the assets provided by the staticfiles app finders.
+    This means you don't need to run collectstatic before or as a part of your
+    tests setup.
+    """
 
     static_handler = StaticFilesHandler
