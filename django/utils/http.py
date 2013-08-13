ae3535169af804352517b7fea94a42a1c9c4b762
Jacob Kaplan-Moss <jacob@jacobian.org>
2013-08-13 11:06:22 -0500

    Fixed is_safe_url() to reject URLs that use a scheme other than HTTP/S.
    This is a security fix; disclosure to follow shortly.

--- django/utils/http.py
+++ django/utils/http.py
@@ -252,11 +252,12 @@ def same_origin(url1, url2):
 def is_safe_url(url, host=None):
     """
     Return ``True`` if the url is a safe redirection (i.e. it doesn't point to
-    a different host).
+    a different host and uses a safe scheme).
 
     Always returns ``False`` on an empty url.
     """
     if not url:
         return False
-    netloc = urllib_parse.urlparse(url)[1]
-    return not netloc or netloc == host
+    url_info = urllib_parse.urlparse(url)
+    return (not url_info.netloc or url_info.netloc == host) and \
+        (not url_info.scheme or url_info.scheme in ['http', 'https'])
