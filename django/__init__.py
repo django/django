30e17be1f64f5edaf9200e65748db3c8643b26ad
Jacob Kaplan-Moss <jacob@jacobian.org>
2013-08-13 11:09:05 -0500

    Bumped version numbers for 1.4.6.

--- django/__init__.py
+++ django/__init__.py
@@ -1,4 +1,4 @@
-VERSION = (1, 4, 6, 'alpha', 0)
+VERSION = (1, 4, 6, 'final', 0)
 
 def get_version(version=None):
     """Derives a PEP386-compliant version number from VERSION."""
--- docs/conf.py
+++ docs/conf.py
@@ -50,9 +50,9 @@ copyright = 'Django Software Foundation and contributors'
 # built documents.
 #
 # The short X.Y version.
-version = '1.4.5'
+version = '1.4.6'
 # The full version, including alpha/beta/rc tags.
-release = '1.4.5'
+release = '1.4.6'
 # The next version to be released
 django_next_version = '1.5'
 
