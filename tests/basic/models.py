6bdab63ebee13603e50ca743d90ba64983cba026
Anssi Kääriäinen <akaariai@gmail.com>
2013-08-16 15:13:00 +0300

    [1.6.x] Fixed #20988 -- Added model meta option select_on_save
    The option can be used to force pre 1.6 style SELECT on save behaviour.
    This is needed in case the database returns zero updated rows even if
    there is a matching row in the DB. One such case is PostgreSQL update
    trigger that returns NULL.
    
    Refs #16649
    
    Backpatch of f65c814f5a1303a233b82b515774990245512f28 from master.
    
    Conflicts:
    	django/db/models/options.py
    	tests/basic/tests.py

--- tests/basic/models.py
+++ tests/basic/models.py
@@ -19,6 +19,11 @@ class Article(models.Model):
     def __str__(self):
         return self.headline
 
+class ArticleSelectOnSave(Article):
+    class Meta:
+        proxy = True
+        select_on_save = True
+
 @python_2_unicode_compatible
 class SelfRef(models.Model):
     selfref = models.ForeignKey('self', null=True, blank=True,
