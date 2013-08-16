0eedaf205ad5f6b9f347ee7b1c24c0cee96cfc09
Christopher Medrela <chris.medrela@gmail.com>
2013-08-16 19:46:48 +0200

    deprecated 'validate' command; changed 'check' command

--- django/core/management/commands/check.py
+++ django/core/management/commands/check.py
@@ -1,13 +1,13 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-import sys
 
 from django.core.management.base import NoArgsCommand
 
 
 class Command(NoArgsCommand):
-    help = "Checks your configuration's compatibility with this version " + \
-           "of Django."
+    help = "Uses the system check framework to validate entire Django project."
+
+    requires_system_checks = False
 
     def handle_noargs(self, **options):
-        sys.stdout.write("Sorry -- not implemented yet.")
+        self.check(display_num_errors=True)
