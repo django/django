deebb1a97767b69249a107848661456bf452d1f1
Ramiro Morales <cramm0@gmail.com>
2013-08-20 14:59:23 -0300

    Import test case classes from their public API module.

--- django/contrib/messages/tests/test_mixins.py
+++ django/contrib/messages/tests/test_mixins.py
@@ -1,4 +1,4 @@
-from django.test.testcases import TestCase
+from django.test import TestCase
 from django.contrib.messages.tests.urls import ContactFormViewWithMsg
 from django.core.urlresolvers import reverse
 
