3ca784663a40a57615ddc98cb9e8c9965c72c9a5
Loic Bistuer <loic.bistuer@sixmedia.com>
2013-09-02 01:21:08 +0700

    Fixed #20978 -- Made deletion.SET_NULL more friendly for MigrationWriter.serialize.

--- django/db/models/deletion.py
+++ django/db/models/deletion.py
@@ -38,7 +38,8 @@ def SET(value):
     return set_on_delete
 
 
-SET_NULL = SET(None)
+def SET_NULL(collector, field, sub_objs, using):
+    collector.add_field_update(field, None, sub_objs)
 
 
 def SET_DEFAULT(collector, field, sub_objs, using):
