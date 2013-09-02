c8ce16146937fe82ebbf81a3edf3424741154944
Loic Bistuer <loic.bistuer@sixmedia.com>
2013-09-02 13:02:07 +0700

    Made MigrationWriter look for a "deconstruct" attribute on functions.
    Refs #20978.

--- django/db/models/deletion.py
+++ django/db/models/deletion.py
@@ -35,6 +35,7 @@ def SET(value):
     else:
         def set_on_delete(collector, field, sub_objs, using):
             collector.add_field_update(field, value, sub_objs)
+    set_on_delete.deconstruct = lambda: ('django.db.models.SET', (value,), {})
     return set_on_delete
 
 
