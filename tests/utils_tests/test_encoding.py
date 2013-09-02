cfbc29408794b98387480fa327b4c29d4a0b1d08
Vlad Starostin <drtyrsa@yandex.ru>
2013-09-02 11:33:04 +0400

    Fixed #21019 force_text fails on class object with __unicode__ instance method

--- tests/utils_tests/test_encoding.py
+++ tests/utils_tests/test_encoding.py
@@ -3,7 +3,8 @@ from __future__ import unicode_literals
 
 import unittest
 
-from django.utils.encoding import force_bytes, filepath_to_uri
+from django.utils.encoding import force_bytes, force_text, filepath_to_uri
+from django.utils import six
 
 
 class TestEncodingUtils(unittest.TestCase):
@@ -22,3 +23,10 @@ class TestEncodingUtils(unittest.TestCase):
             'upload/%D1%87%D1%83%D0%B1%D0%B0%D0%BA%D0%B0.mp4')
         self.assertEqual(filepath_to_uri('upload\\чубака.mp4'.encode('utf-8')),
             'upload/%D1%87%D1%83%D0%B1%D0%B0%D0%BA%D0%B0.mp4')
+
+    def test_force_text_on_object_with_unbound__unicode__method(self):
+        class SomeClass(object):
+            def __unicode__(self):
+                return u'hi there'
+
+        self.assertEqual(force_text(SomeClass), six.text_type(SomeClass))
