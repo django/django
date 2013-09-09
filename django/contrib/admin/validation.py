c9cce7ca5a9a4156e8cae630841f971adfa73c15
Christopher Medrela <chris.medrela@gmail.com>
2013-09-09 22:59:28 +0200

    added test for validator of model admin

--- django/contrib/admin/validation.py
+++ django/contrib/admin/validation.py
@@ -4,7 +4,9 @@ from __future__ import unicode_literals
 
 class BaseValidator(object):
     def validate(self, cls, model):
-        pass
+        for m in dir(self):
+            if m.startswith('validate_'):
+                getattr(self, m)(cls, model)
 
 
 class ModelAdminValidator(BaseValidator):
