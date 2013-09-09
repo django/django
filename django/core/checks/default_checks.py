3b087d8235cb488f8d58b27935380cc5738f3567
Christopher Medrela <chris.medrela@gmail.com>
2013-09-09 21:49:24 +0200

    made 'register' a decorator

--- django/core/checks/default_checks.py
+++ django/core/checks/default_checks.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from . import Warning, tag
+from . import Warning, register
 
 
 # All these checks are registered in __init__.py file.
 
-@tag('models')
+@register('models')
 def check_all_models(apps=None, **kwargs):
     from django.db import models
 
@@ -16,7 +16,7 @@ def check_all_models(apps=None, **kwargs):
     return errors
 
 
-@tag('compatibility')
+@register('compatibility')
 def check_1_6_compatibility(**kwargs):
     return _check_test_runner(**kwargs) + _check_boolean_field_default_value(**kwargs)
 
