4c4954a3c133e824390296f6bb444622a8b25331
Anssi Kääriäinen <anssi.kaariainen@thl.fi>
2013-09-14 10:33:12 +0300

    [1.6.x] Added tests for double-pickling a QuerySet
    Refs #21102.
    
    Backpatch of 74b91b3888383fca28dea00e0e1ffb5aecec7c0f

--- tests/queryset_pickle/tests.py
+++ tests/queryset_pickle/tests.py
@@ -90,3 +90,15 @@ class PickleabilityTestCase(TestCase):
         reloaded = pickle.loads(dumped)
         self.assertEqual(original, reloaded)
         self.assertIs(reloaded.__class__, dynclass)
+
+    def test_pickle_prefetch_related_idempotence(self):
+        g = Group.objects.create(name='foo')
+        groups = Group.objects.prefetch_related('event_set')
+
+        # First pickling
+        groups = pickle.loads(pickle.dumps(groups))
+        self.assertQuerysetEqual(groups, [g], lambda x: x)
+
+        # Second pickling
+        groups = pickle.loads(pickle.dumps(groups))
+        self.assertQuerysetEqual(groups, [g], lambda x: x)
