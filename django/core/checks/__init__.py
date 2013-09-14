fa170ee1e9fc7624035c84b3eba45e45fce39ae3
Christopher Medrela <chris.medrela@gmail.com>
2013-09-14 20:55:56 +0200

    renamed django/core/checks/{default_checks.py -> model_checks.py}

--- django/core/checks/__init__.py
+++ django/core/checks/__init__.py
@@ -5,5 +5,5 @@ from .messages import (CheckMessage,
         Debug, Info, Warning, Error, Critical,
         DEBUG, INFO, WARNING, ERROR, CRITICAL)
 from .registration import register, run_checks, tag_exists
-import django.core.checks.default_checks
 import django.core.checks.compatibility.django_1_6_0
+import django.core.checks.model_checks
