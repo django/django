7d84847f06ff7d9fbc3c46e8f888efa41e9399d1
Christopher Medrela <chris.medrela@gmail.com>
2013-09-14 20:54:34 +0200

    reorganized 'check' package

---
+++ django/core/checks/compatibility/django_1_6_0.py
@@ -0,0 +1,68 @@
+# -*- encoding: utf-8 -*-
+from __future__ import unicode_literals
+
+from .. import Warning, register
+
+
+@register('compatibility')
+def check_1_6_compatibility(**kwargs):
+    errors = []
+    errors.extend(_check_test_runner(**kwargs))
+    errors.extend(_check_boolean_field_default_value(**kwargs))
+    return errors
+
+
+def _check_test_runner(apps=None, **kwargs):
+    """ Warn an user if the user has *not* set explicitly the ``TEST_RUNNER``
+    setting. """
+
+    if apps is not None:
+        return []
+
+    from django.conf import settings
+
+    user_settings = settings._wrapped
+    if 'TEST_RUNNER' not in user_settings.__dict__:
+        return [
+            Warning(
+                'You have not explicitly set "TEST_RUNNER". In Django 1.6, '
+                'there is a new test runner ("django.test.runner.DiscoverRunner") '
+                'by default. You should ensure your tests are still all '
+                'running & behaving as expected. See '
+                'https://docs.djangoproject.com/en/dev/releases/1.6/#discovery-of-tests-in-any-test-module '
+                'for more information.',
+                hint=None,
+                obj=None,
+                id='W047',
+            )
+        ]
+    else:
+        return []
+
+
+def _check_boolean_field_default_value(apps=None, **kwargs):
+    """
+    Checks if there are any BooleanFields without a default value, &
+    warns the user that the default has changed from False to Null.
+    """
+
+    from django.db import models
+
+    invalid_fields = [field
+        for model in models.get_models(**kwargs)
+        if apps is None or model._meta.app_label in apps
+        for field in model._meta.local_fields
+        if isinstance(field, models.BooleanField) and not field.has_default()]
+
+    return [
+        Warning(
+            'The field has not set a default value. In Django 1.6 '
+                'the default value of BooleanField was changed from '
+                'False to Null when Field.default is not defined. '
+                'See https://docs.djangoproject.com/en/1.6/ref/models/fields/#booleanfield '
+                'for more information.',
+            hint=None,
+            obj=field,
+            id='W048',
+        )
+        for field in invalid_fields]
--- django/core/checks/default_checks.py
+++ django/core/checks/default_checks.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from itertools import chain
+
 from . import Warning, register
 
 
@@ -8,81 +10,7 @@ from . import Warning, register
 def check_all_models(apps=None, **kwargs):
     from django.db import models
 
-    errors = []
-    for cls in _get_models_from_apps(apps, include_swapped=True):
-        errors.extend(cls.check())
-    return errors
-
-
-@register('compatibility')
-def check_1_6_compatibility(**kwargs):
-    return _check_test_runner(**kwargs) + _check_boolean_field_default_value(**kwargs)
-
-
-def _check_test_runner(apps=None, **kwargs):
-    """ Warn an user if the user has *not* set explicitly the ``TEST_RUNNER``
-    setting. """
-
-    if apps is not None:
-        return []
-
-    from django.conf import settings
-
-    user_settings = settings._wrapped
-    if 'TEST_RUNNER' not in user_settings.__dict__:
-        return [
-            Warning(
-                'You have not explicitly set "TEST_RUNNER". In Django 1.6, '
-                'there is a new test runner ("django.test.runner.DiscoverRunner") '
-                'by default. You should ensure your tests are still all '
-                'running & behaving as expected. See '
-                'https://docs.djangoproject.com/en/dev/releases/1.6/#discovery-of-tests-in-any-test-module '
-                'for more information.',
-                hint=None,
-                obj=None,
-                id='W047',
-            )
-        ]
-    else:
-        return []
-
-
-def _check_boolean_field_default_value(apps=None, **kwargs):
-    """
-    Checks if there are any BooleanFields without a default value, &
-    warns the user that the default has changed from False to Null.
-    """
-
-    from django.db import models
-
-    invalid_fields = [field
-        for cls in _get_models_from_apps(apps)
-        for field in cls._meta.local_fields
-        if isinstance(field, models.BooleanField) and not field.has_default()]
-
-    return [
-        Warning(
-            'The field has not set a default value. In Django 1.6 '
-                'the default value of BooleanField was changed from '
-                'False to Null when Field.default is not defined. '
-                'See https://docs.djangoproject.com/en/1.6/ref/models/fields/#booleanfield '
-                'for more information.',
-            hint=None,
-            obj=field,
-            id='W048',
-        )
-        for field in invalid_fields]
-
-
-def _get_models_from_apps(apps, **kwargs):
-    """ Return all models from a given set of apps. If apps is None, return all
-    installed models. """
-
-    from django.db import models
-
-    if apps is None:
-        return models.get_models(**kwargs)
-    else:
-        return [model
-                for model in models.get_models(**kwargs)
-                if model._meta.app_label in apps]
+    errors = [model.check(**kwargs)
+        for model in models.get_models(**kwargs)
+        if apps is None or model._meta.app_label in apps]
+    return list(chain(*errors))
