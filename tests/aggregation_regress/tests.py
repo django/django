9a041807fcde7ff7245696a743196d31ec7c7b5c
Michael Manfre <mmanfre@gmail.com>
2013-09-23 20:52:58 -0400

    Fixed #18333 - Quoted annotated column names

--- tests/aggregation_regress/tests.py
+++ tests/aggregation_regress/tests.py
@@ -1114,3 +1114,13 @@ class AggregationTests(TestCase):
         self.assertQuerysetEqual(
             qs, ['Sams Teach Yourself Django in 24 Hours'],
             lambda b: b.name)
+
+    def test_annotate_reserved_word(self):
+        """
+        Regression #18333 - Ensure annotated column name is properly quoted.
+        """
+        vals = Book.objects.annotate(select=Count('authors__id')).aggregate(Sum('select'), Avg('select'))
+        self.assertEqual(vals, {
+            'select__sum': 10,
+            'select__avg': Approximate(1.666, places=2),
+        })
