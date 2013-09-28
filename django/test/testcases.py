783620ccc827057d1d0f2c6f4808350be9193bf9
Ramiro Morales <cramm0@gmail.com>
2013-09-27 21:45:25 -0300

    Fixed #21162 -- Better emulation of staticfiles middleware.
    Code had been added in e909ceae9b. Solves test suite failures observed
    on Windows.
    
    Thanks Michael Manfre for the report. Refs #20739.

--- django/test/testcases.py
+++ django/test/testcases.py
@@ -958,7 +958,11 @@ class FSFilesHandler(WSGIHandler):
 
     def serve(self, request):
         os_rel_path = self.file_path(request.path)
-        final_rel_path = posixpath.normpath(unquote(os_rel_path)).lstrip('/')
+        os_rel_path = posixpath.normpath(unquote(os_rel_path))
+        # Emulate behavior of django.contrib.staticfiles.views.serve() when it
+        # invokes staticfiles' finders functionality.
+        # TODO: Modify if/when that internal API is refactored
+        final_rel_path = os_rel_path.replace('\\', '/').lstrip('/')
         return serve(request, final_rel_path, document_root=self.get_base_dir())
 
     def __call__(self, environ, start_response):
