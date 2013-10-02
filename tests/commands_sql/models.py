8b7349cbf2a4c5d598b694c8b5e161d9ec138e60
Borislav Borisov <chmodas@gmail.com>
2013-10-02 17:13:15 +0300

    Fixed #21198 -- Add explicit check for __str__ definition in @python_2_unicode_compatible

--- tests/commands_sql/models.py
+++ tests/commands_sql/models.py
@@ -4,10 +4,14 @@ from django.utils.encoding import python_2_unicode_compatible
 
 @python_2_unicode_compatible
 class Comment(models.Model):
-    pass
+    def __str__(self):
+        return super(Comment, self).__str__()
 
 
 @python_2_unicode_compatible
 class Book(models.Model):
     title = models.CharField(max_length=100, db_index=True)
     comments = models.ManyToManyField(Comment)
+
+    def __str__(self):
+        return super(Book, self).__str__()
