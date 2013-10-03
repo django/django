9feffa7948fd26411fb48a3cb80d30da0641b756
Unai Zalakain <unai@gisa-elkartea.org>
2013-10-04 00:51:22 +0200

    Fixed #21213 -- Django's mailing lists documentation
    Added docs/internals/mailing-lists.txt documenting the use of django's
    mailing lists. All references across docs changed to point to this page.
    
    The referencing makes use of substitution because there's no way to make
    a :ref: link in a non-inline fashion in Sphinx. It also makes use of
    rst_epilog Sphinx conf for making this substitutions across all the
    docs.

--- docs/conf.py
+++ docs/conf.py
@@ -206,6 +206,14 @@ htmlhelp_basename = 'Djangodoc'
 
 modindex_common_prefix = ["django."]
 
+# Appended to every page
+rst_epilog = """
+.. |django-users| replace:: :ref:`django-users <django-users-mailing-list>`
+.. |django-core-mentorship| replace:: :ref:`django-core-mentorship <django-core-mentorship-mailing-list>`
+.. |django-developers| replace:: :ref:`django-developers <django-developers-mailing-list>`
+.. |django-announce| replace:: :ref:`django-announce <django-announce-mailing-list>`
+.. |django-updates| replace:: :ref:`django-updates <django-updates-mailing-list>`
+"""
 
 # -- Options for LaTeX output --------------------------------------------------
 
