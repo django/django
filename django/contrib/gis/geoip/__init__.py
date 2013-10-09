django/django

commit 20472aa827669d2b83b74e521504e88e18d086a1

Fixed #21189: Cleaned up usage of bare except clauses.
Thanks to berkerpeksag for the report and to claudep
for the review.

django/contrib/gis/geoip/__init__.py
@@ -14,5 +14,5 @@
 try:
     from .base import GeoIP, GeoIPException
     HAS_GEOIP = True
-except:
+except ImportError:
     HAS_GEOIP = False

django/contrib/gis/utils/__init__.py
@@ -7,11 +7,12 @@ if HAS_GDAL:
     from django.contrib.gis.utils.ogrinfo import ogrinfo, sample
     from django.contrib.gis.utils.ogrinspect import mapping, ogrinspect
     from django.contrib.gis.utils.srs import add_postgis_srs, add_srs_entry
+    from django.core.exceptions import ImproperlyConfigured
     try:
         # LayerMapping requires DJANGO_SETTINGS_MODULE to be set,
         # so this needs to be in try/except.
         from django.contrib.gis.utils.layermapping import LayerMapping, LayerMapError
-    except:
+    except ImproperlyConfigured:
         pass
 
 from django.contrib.gis.utils.wkt import precision_wkt

django/core/management/__init__.py
@@ -209,7 +209,7 @@ class LaxOptionParser(OptionParser):
                     # dealing with options
                     del rargs[0]
                     raise Exception
-            except:
+            except:  # Needed because we might need to catch a SystemExit
                 largs.append(arg)
 
 class ManagementUtility(object):
@@ -360,7 +360,7 @@ class ManagementUtility(object):
         try:
             options, args = parser.parse_args(self.argv)
             handle_default_options(options)
-        except:
+        except:  # Needed because parser.parse_args can raise SystemExit
             pass # Ignore any option errors at this point.
 
         try:

django/contrib/flatpages/middleware.py
@@ -12,7 +12,7 @@ class FlatpageFallbackMiddleware(object):
         # is a middleware, we can't assume the errors will be caught elsewhere.
         except Http404:
             return response
-        except:
+        except Exception:
             if settings.DEBUG:
                 raise
             return response

django/contrib/gis/db/backends/base.py
@@ -345,5 +345,5 @@ class SpatialRefSysMixin(object):
         """
         try:
             return six.text_type(self.srs)
-        except:
+        except Exception:
             return six.text_type(self.wkt)

django/contrib/gis/db/backends/spatialite/operations.py
@@ -238,15 +238,12 @@ class SpatiaLiteOperations(DatabaseOperations, BaseSpatialOperations):
         """
         Helper routine for calling SpatiaLite functions and returning
         their result.
+        Any error occuring in this method should be handled by the caller.
         """
         cursor = self.connection._cursor()
         try:
-            try:
-                cursor.execute('SELECT %s' % func)
-                row = cursor.fetchone()
-            except:
-                # Responsibility of caller to perform error handling.
-                raise
+            cursor.execute('SELECT %s' % func)
+            row = cursor.fetchone()
         finally:
             cursor.close()
         return row[0]

django/contrib/gis/forms/fields.py
@@ -73,7 +73,7 @@ class GeometryField(forms.Field):
             elif self.srid != -1 and self.srid != geom.srid:
                 try:
                     geom.transform(self.srid)
-                except:
+                except GEOSException:
                     raise forms.ValidationError(self.error_messages['transform_error'], code='transform_error')
 
         return geom

django/contrib/gis/geos/libgeos.py
@@ -65,7 +65,7 @@ def notice_h(fmt, lst):
     fmt, lst = fmt.decode(), lst.decode()
     try:
         warn_msg = fmt % lst
-    except:
+    except TypeError:
         warn_msg = fmt
     logger.warning('GEOS_NOTICE: %s\n' % warn_msg)
 notice_h = NOTICEFUNC(notice_h)
@@ -75,7 +75,7 @@ def error_h(fmt, lst):
     fmt, lst = fmt.decode(), lst.decode()
     try:
         err_msg = fmt % lst
-    except:
+    except TypeError:
         err_msg = fmt
     logger.error('GEOS_ERROR: %s\n' % err_msg)
 error_h = ERRORFUNC(error_h)

commit 24f99676195333aa345fb955cdc8d2d0cd08efa3

Fixed regression introduced by 20472aa827669d2b83b74e521504e88e18d086a1.
The wrong exception was being caught when detecting the presence of GeoIP.
Thanks to ramiro for the report.

django/contrib/gis/geoip/__init__.py
@@ -14,5 +14,5 @@
 try:
     from .base import GeoIP, GeoIPException
     HAS_GEOIP = True
-except ImportError:
+except RuntimeError:  # libgeoip.py raises a RuntimeError if no GeoIP library is found
     HAS_GEOIP = False