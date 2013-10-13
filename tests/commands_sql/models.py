f0c7649b1692f8441eb9b9b923b2bed8e95f9185
Aymeric Augustin <aymeric.augustin@m4x.org>
2013-10-13 18:06:58 +0200

    Fixed #21198 -- Prevented invalid use of @python_2_unicode_compatible.
    Thanks jpic for the report and chmodas for working on a patch.
    
    Reverts 2ea80b94. Refs #19362.
    
    Conflicts:
    	tests/utils_tests/test_encoding.py

--- tests/commands_sql/models.py
+++ tests/commands_sql/models.py
@@ -1,13 +1,10 @@
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 
 
-@python_2_unicode_compatible
 class Comment(models.Model):
     pass
 
 
-@python_2_unicode_compatible
 class Book(models.Model):
     title = models.CharField(max_length=100, db_index=True)
     comments = models.ManyToManyField(Comment)
--- tests/utils_tests/test_encoding.py
+++ tests/utils_tests/test_encoding.py
@@ -2,7 +2,8 @@
 from __future__ import unicode_literals
 
 from django.utils import unittest
-from django.utils.encoding import force_bytes, filepath_to_uri
+from django.utils.encoding import force_bytes, filepath_to_uri, python_2_unicode_compatible
+from django.utils import six
 
 
 class TestEncodingUtils(unittest.TestCase):
@@ -21,3 +22,10 @@ class TestEncodingUtils(unittest.TestCase):
             'upload/%D1%87%D1%83%D0%B1%D0%B0%D0%BA%D0%B0.mp4')
         self.assertEqual(filepath_to_uri('upload\\чубака.mp4'.encode('utf-8')),
             'upload/%D1%87%D1%83%D0%B1%D0%B0%D0%BA%D0%B0.mp4')
+
+    @unittest.skipIf(six.PY3, "tests a class not defining __str__ under Python 2")
+    def test_decorated_class_without_str(self):
+        with self.assertRaises(ValueError):
+            @python_2_unicode_compatible
+            class NoStr(object):
+                pass
