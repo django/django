9e894f5f726e1ec7f2d2111e4f9f826fe8207d0d
Kathryn Killebrew <kathryn.killebrew@centerffs.org>
2013-10-17 12:57:10 -0400

    Formatting

--- django/contrib/formtools/tests/models.py
+++ django/contrib/formtools/tests/models.py
@@ -2,23 +2,25 @@
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
+
 @python_2_unicode_compatible
 class Poet(models.Model):
-    class Meta:
-        app_label = 'formtools'
-    
     name = models.CharField(max_length=100)
 
+    class Meta:
+        app_label = 'formtools'
+        
     def __str__(self):
         return self.name
 
+
 @python_2_unicode_compatible
 class Poem(models.Model):
-    class Meta:
-        app_label = 'formtools'
-    
     poet = models.ForeignKey(Poet)
     name = models.CharField(max_length=100)
 
+    class Meta:
+        app_label = 'formtools'
+    
     def __str__(self):
         return self.name
