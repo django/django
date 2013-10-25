e052ada0f6f21208d0f215d2f9184c4069ac1419
Andrey Antukh <niwi@niwi.be>
2013-10-26 01:07:40 +0200

    [1.6.x] Fixed #21335 -- Use importlib from python3 when using python3
    Backport of 3351e94ffa from master.

--- django/utils/importlib.py
+++ django/utils/importlib.py
@@ -1,6 +1,8 @@
 # Taken from Python 2.7 with permission from/by the original author.
 import sys
 
+from django.utils import six
+
 def _resolve_name(name, package, level):
     """Return the absolute name of the module to be imported."""
     if not hasattr(package, 'rindex'):
@@ -15,22 +17,25 @@ def _resolve_name(name, package, level):
     return "%s.%s" % (package[:dot], name)
 
 
-def import_module(name, package=None):
-    """Import a module.
+if six.PY3:
+    from importlib import import_module
+else:
+    def import_module(name, package=None):
+        """Import a module.
 
-    The 'package' argument is required when performing a relative import. It
-    specifies the package to use as the anchor point from which to resolve the
-    relative import to an absolute import.
+        The 'package' argument is required when performing a relative import. It
+        specifies the package to use as the anchor point from which to resolve the
+        relative import to an absolute import.
 
-    """
-    if name.startswith('.'):
-        if not package:
-            raise TypeError("relative imports require the 'package' argument")
-        level = 0
-        for character in name:
-            if character != '.':
-                break
-            level += 1
-        name = _resolve_name(name[level:], package, level)
-    __import__(name)
-    return sys.modules[name]
+        """
+        if name.startswith('.'):
+            if not package:
+                raise TypeError("relative imports require the 'package' argument")
+            level = 0
+            for character in name:
+                if character != '.':
+                    break
+                level += 1
+            name = _resolve_name(name[level:], package, level)
+        __import__(name)
+        return sys.modules[name]
