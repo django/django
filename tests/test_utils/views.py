ccd11c09c3bea966606f5c68dd2246b0c6757f65
Alex Gaynor <alex.gaynor@gmail.com>
2013-11-08 09:03:59 -0800

    Flake8 fix -- correct number of newlines between top level definitions

--- tests/test_utils/views.py
+++ tests/test_utils/views.py
@@ -9,6 +9,7 @@ def get_person(request, pk):
     person = get_object_or_404(Person, pk=pk)
     return HttpResponse(person.name)
 
+
 def no_template_used(request):
     template = loader.get_template_from_string("This is a string-based template")
     return HttpResponse(template.render(Context({})))
