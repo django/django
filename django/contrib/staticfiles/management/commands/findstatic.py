b38b1cadb7ff10bca9e2530bbd3370ee3d3f0bd3
Vajrasky Kok <sky.kok@speaklikeaking.com>
2013-11-16 22:55:50 +0800

    Fixed #19879 -- Have 'findstatic' says on which directories it searched the relative paths.
    Added get_searched_locations in finders module. Added verbosity flag level 2 on 'findstatic'
    command that will output the directories on which it searched the relative paths.
    
    Reported by ccurvey. Initial patch by Jonas Svensson.

--- django/contrib/staticfiles/management/commands/findstatic.py
+++ django/contrib/staticfiles/management/commands/findstatic.py
@@ -21,15 +21,23 @@ class Command(LabelCommand):
         verbosity = int(options.get('verbosity', 1))
         result = finders.find(path, all=options['all'])
         path = force_text(path)
+        if verbosity >= 2:
+            searched_locations = finders.get_searched_locations()
+            searched_locations_message = "\nLooking in the following locations:\n  " +\
+                    "\n  ".join(force_text(location) for location in searched_locations)
+        else:
+            searched_locations_message = ""
         if result:
             if not isinstance(result, (list, tuple)):
                 result = [result]
             result = (force_text(os.path.realpath(path)) for path in result)
             if verbosity >= 1:
                 output = '\n  '.join(result)
-                return "Found '%s' here:\n  %s" % (path, output)
+                return "Found '%s' here:\n  %s" % (path, output) +\
+                        searched_locations_message
             else:
-                return '\n'.join(result)
+                return '\n'.join(result) + searched_locations_message
         else:
             if verbosity >= 1:
-                self.stderr.write("No matching file found for '%s'." % path)
+                self.stderr.write("No matching file found for '%s'." % path +\
+                        searched_locations_message)
