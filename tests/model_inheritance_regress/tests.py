6c2e550c35008eea28c5269d4ba18c4ba9618fe6
Anssi Kääriäinen <akaariai@gmail.com>
2013-11-17 15:50:14 +0200

    Fixed #21413 -- resolve_columns fields misalignment

--- tests/model_inheritance_regress/tests.py
+++ tests/model_inheritance_regress/tests.py
@@ -460,3 +460,9 @@ class ModelInheritanceTest(TestCase):
         )
 
         Senator.objects.get(pk=senator.pk)
+
+    def test_inheritance_resolve_columns(self):
+        Restaurant.objects.create(name='Bobs Cafe', address="Somewhere",
+                                  serves_pizza=True, serves_hot_dogs=True)
+        p = Place.objects.all().select_related('restaurant')[0]
+        self.assertIsInstance(p.restaurant.serves_pizza, bool)
