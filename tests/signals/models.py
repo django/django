632d729f923f94605731cd03bf4e2a6e63aa1a6b
Loic Bistuer <loic.bistuer@sixmedia.com>
2013-11-19 00:27:09 +0700

    Merged the signals and signals_regress test packages.
    This patch also made the tests less likely to pollute the global state
    in case of failure.

--- tests/signals/models.py
+++ tests/signals/models.py
@@ -23,3 +23,20 @@ class Car(models.Model):
 
     def __str__(self):
         return "%s %s" % (self.make, self.model)
+
+
+@python_2_unicode_compatible
+class Author(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+@python_2_unicode_compatible
+class Book(models.Model):
+    name = models.CharField(max_length=20)
+    authors = models.ManyToManyField(Author)
+
+    def __str__(self):
+        return self.name
