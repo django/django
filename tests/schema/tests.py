7349a22d291c5c8e10cb9842809ab5675e42660b
Alex Gaynor <alex.gaynor@gmail.com>
2013-11-25 09:21:25 -0600

    flake8 fix

--- tests/schema/tests.py
+++ tests/schema/tests.py
@@ -676,7 +676,7 @@ class SchemaTests(TransactionTestCase):
             editor.create_model(BookWithLongName)
         # Find the properly shortened column name
         column_name = connection.ops.quote_name("author_foreign_key_with_really_long_field_name_id")
-        column_name = column_name[1:-1].lower() # unquote, and, for Oracle, un-upcase
+        column_name = column_name[1:-1].lower()  # unquote, and, for Oracle, un-upcase
         # Ensure the table is there and has an index on the column
         self.assertIn(
             column_name,
