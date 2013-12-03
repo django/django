12615dab78cb6fc7d8c74b7b65a4136b0feeb33f
Ramiro Morales <cramm0@gmail.com>
2013-12-02 23:11:59 -0300

    Fixed #13476 -- Added support for color in console output under Windows.
    Detect and use the services of the ANSICON third-party tool if it's
    available.

--- django/core/management/color.py
+++ django/core/management/color.py
@@ -13,10 +13,12 @@ def supports_color():
     Returns True if the running system's terminal supports color, and False
     otherwise.
     """
-    unsupported_platform = (sys.platform in ('win32', 'Pocket PC'))
+    plat = sys.platform
+    supported_platform = plat != 'Pocket PC' and (plat != 'win32' or
+                                                  'ANSICON' in os.environ)
     # isatty is not always implemented, #6223.
     is_a_tty = hasattr(sys.stdout, 'isatty') and sys.stdout.isatty()
-    if unsupported_platform or not is_a_tty:
+    if not supported_platform or not is_a_tty:
         return False
     return True
 
