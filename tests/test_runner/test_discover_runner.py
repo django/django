aef019de6113e6c750e3ba49dc1e9f2a179eb0ca
Preston Timmons <prestontimmons@gmail.com>
2013-12-16 10:04:28 -0600

    Fixed #21206 -- No longer run discovery if the test label doesn't point to a package or directory.
    Thanks thepapermen for the report and Carl Meyer for the review.

--- tests/test_runner/test_discover_runner.py
+++ tests/test_runner/test_discover_runner.py
@@ -68,6 +68,34 @@ class DiscoverRunnerTest(TestCase):
 
         self.assertEqual(count, 3)
 
+    def test_empty_test_case(self):
+        count = DiscoverRunner().build_suite(
+            ["test_discovery_sample.tests_sample.EmptyTestCase"],
+        ).countTestCases()
+
+        self.assertEqual(count, 0)
+
+    def test_discovery_on_package(self):
+        count = DiscoverRunner().build_suite(
+            ["test_discovery_sample.tests"],
+        ).countTestCases()
+
+        self.assertEqual(count, 1)
+
+    def test_ignore_adjacent(self):
+        """
+        When given a dotted path to a module, unittest discovery searches
+        not just the module, but also the directory containing the module.
+
+        This results in tests from adjacent modules being run when they
+        should not. The discover runner avoids this behavior.
+        """
+        count = DiscoverRunner().build_suite(
+            ["test_discovery_sample.empty"],
+        ).countTestCases()
+
+        self.assertEqual(count, 0)
+
     def test_overrideable_test_suite(self):
         self.assertEqual(DiscoverRunner().test_suite, TestSuite)
 
