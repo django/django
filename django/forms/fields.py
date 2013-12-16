fe1389e911b0cdc487e5547c09c920c12f4e1ce0
Artur Barseghyan <artur.barseghyan@gmail.com>
2013-12-16 23:07:39 +0100

    Removed duplicated `TimeField` in __all__.

--- django/forms/fields.py
+++ django/forms/fields.py
@@ -35,7 +35,7 @@ from django.core.validators import EMPTY_VALUES  # NOQA
 
 __all__ = (
     'Field', 'CharField', 'IntegerField',
-    'DateField', 'TimeField', 'DateTimeField', 'TimeField',
+    'DateField', 'TimeField', 'DateTimeField',
     'RegexField', 'EmailField', 'FileField', 'ImageField', 'URLField',
     'BooleanField', 'NullBooleanField', 'ChoiceField', 'MultipleChoiceField',
     'ComboField', 'MultiValueField', 'FloatField', 'DecimalField',
