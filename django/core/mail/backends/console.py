bfe9052831c6d7ad7501b71c884525b3b471eebc
Florian Apolloner <florian@apolloner.eu>
2013-12-30 23:54:12 +0100

    Decode mails using the message encoding.

--- django/core/mail/backends/console.py
+++ django/core/mail/backends/console.py
@@ -15,10 +15,12 @@ class EmailBackend(BaseEmailBackend):
         super(EmailBackend, self).__init__(*args, **kwargs)
 
     def write_message(self, message):
-        msg = message.message().as_bytes()
+        msg = message.message()
+        msg_data = msg.as_bytes()
         if six.PY3:
-            msg = msg.decode()
-        self.stream.write('%s\n' % msg)
+            charset = msg.get_charset().get_output_charset() if msg.get_charset() else 'utf-8'
+            msg_data = msg_data.decode(charset)
+        self.stream.write('%s\n' % msg_data)
         self.stream.write('-' * 79)
         self.stream.write('\n')
 
