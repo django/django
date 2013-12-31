a95f74e7078e2f38935e3de5e3229b8ff585ae00
Aymeric Augustin <aymeric.augustin@m4x.org>
2013-12-31 13:16:34 +0100

    Oops.

--- django/core/management/__init__.py
+++ django/core/management/__init__.py
@@ -6,6 +6,7 @@ import os
 import sys
 
 import django
+from django.apps import apps
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.core.management.base import BaseCommand, CommandError, handle_default_options
