a878bf9b093bf15d751b070d132fec52a7523a47
Baptiste Mispelon <bmispelon@gmail.com>
2014-02-05 05:16:39 +0100

    Revert "Fixed #20296 -- Allowed SafeData and EscapeData to be lazy"
    This reverts commit 2ee447fb5f8974b432d3dd421af9a242215aea44.
    
    That commit introduced a regression (#21882) and didn't really
    do what it was supposed to: while it did delay the evaluation
    of lazy objects passed to mark_safe(), they weren't actually
    marked as such so they could end up being escaped twice.
    
    Refs #21882.

--- tests/utils_tests/test_safestring.py
+++ tests/utils_tests/test_safestring.py
@@ -3,8 +3,8 @@ from __future__ import unicode_literals
 from django.template import Template, Context
 from django.test import TestCase
 from django.utils.encoding import force_text, force_bytes
-from django.utils.functional import lazy, Promise
-from django.utils.safestring import mark_safe, mark_for_escaping
+from django.utils.functional import lazy
+from django.utils.safestring import mark_safe, mark_for_escaping, SafeData, EscapeData
 from django.utils import six
 from django.utils import translation
 
@@ -28,8 +28,8 @@ class SafeStringTest(TestCase):
         s = lazystr('a&b')
         b = lazybytes(b'a&b')
 
-        self.assertIsInstance(mark_safe(s), Promise)
-        self.assertIsInstance(mark_safe(b), Promise)
+        self.assertIsInstance(mark_safe(s), SafeData)
+        self.assertIsInstance(mark_safe(b), SafeData)
         self.assertRenderEqual('{{ s }}', 'a&b', s=mark_safe(s))
 
     def test_mark_for_escaping(self):
@@ -41,14 +41,9 @@ class SafeStringTest(TestCase):
         s = lazystr('a&b')
         b = lazybytes(b'a&b')
 
-        self.assertIsInstance(mark_for_escaping(s), Promise)
-        self.assertIsInstance(mark_for_escaping(b), Promise)
+        self.assertIsInstance(mark_for_escaping(s), EscapeData)
+        self.assertIsInstance(mark_for_escaping(b), EscapeData)
         self.assertRenderEqual('{% autoescape off %}{{ s }}{% endautoescape %}', 'a&amp;b', s=mark_for_escaping(s))
-
-    def test_regression_20296(self):
-        s = mark_safe(translation.ugettext_lazy("username"))
-        with translation.override('fr'):
-            self.assertRenderEqual('{{ s }}', "nom d'utilisateur", s=s)
 
     def test_html(self):
         s = '<h1>interop</h1>'
