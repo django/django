ebabd9a253e23c8d106e04c9969b990351227354
Christopher Medrela <chris.medrela@gmail.com>
2014-02-07 22:34:56 +0100

    Fixed #17673 -- forbidden field shadowing

--- tests/invalid_models_tests/test_models.py
+++ tests/invalid_models_tests/test_models.py
@@ -191,31 +191,9 @@ class UniqueTogetherTests(IsolatedModelsTestCase):
         self.assertEqual(errors, expected)
 
 
-class OtherModelTests(IsolatedModelsTestCase):
+class FieldNamesTests(IsolatedModelsTestCase):
 
-    def test_unique_primary_key(self):
-        class Model(models.Model):
-            id = models.IntegerField(primary_key=False)
-
-        errors = Model.check()
-        expected = [
-            Error(
-                ('You cannot use "id" as a field name, because each model '
-                 'automatically gets an "id" field if none of the fields '
-                 'have primary_key=True.'),
-                hint='Remove or rename "id" field or add primary_key=True to a field.',
-                obj=Model,
-                id='E005',
-            ),
-            Error(
-                'Field "id" has column name "id" that is already used.',
-                hint=None,
-                obj=Model,
-            )
-        ]
-        self.assertEqual(errors, expected)
-
-    def test_field_names_ending_with_underscore(self):
+    def test_ending_with_underscore(self):
         class Model(models.Model):
             field_ = models.CharField(max_length=10)
             m2m_ = models.ManyToManyField('self')
@@ -233,6 +211,138 @@ class OtherModelTests(IsolatedModelsTestCase):
                 hint=None,
                 obj=Model._meta.get_field('m2m_'),
                 id='E001',
+            ),
+        ]
+        self.assertEqual(errors, expected)
+
+    def test_including_separator(self):
+        class Model(models.Model):
+            some__field = models.IntegerField()
+
+        errors = Model.check()
+        expected = [
+            Error(
+                'Field names must not contain "__".',
+                hint=None,
+                obj=Model._meta.get_field('some__field'),
+                id='E052',
+            )
+        ]
+        self.assertEqual(errors, expected)
+
+    def test_pk(self):
+        class Model(models.Model):
+            pk = models.IntegerField()
+
+        errors = Model.check()
+        expected = [
+            Error(
+                'Cannot use "pk" as a field name since it is a reserved name.',
+                hint=None,
+                obj=Model._meta.get_field('pk'),
+                id='E051',
+            )
+        ]
+        self.assertEqual(errors, expected)
+
+
+class ShadowingFieldsTests(IsolatedModelsTestCase):
+
+    def test_multiinheritance_clash(self):
+        class Mother(models.Model):
+            clash = models.IntegerField()
+
+        class Father(models.Model):
+            clash = models.IntegerField()
+
+        class Child(Mother, Father):
+            # Here we have two clashed: id (automatic field) and clash, because
+            # both parents define these fields.
+            pass
+
+        errors = Child.check()
+        expected = [
+            Error(
+                ('The field "id" from parent model '
+                 'invalid_models_tests.mother clashes with the field "id" '
+                 'from parent model invalid_models_tests.father.'),
+                hint=None,
+                obj=Child,
+                id='E053',
+            ),
+            Error(
+                ('The field "clash" from parent model '
+                 'invalid_models_tests.mother clashes with the field "clash" '
+                 'from parent model invalid_models_tests.father.'),
+                hint=None,
+                obj=Child,
+                id='E053',
+            )
+        ]
+        self.assertEqual(errors, expected)
+
+    def test_inheritance_clash(self):
+        class Parent(models.Model):
+            f_id = models.IntegerField()
+
+        class Target(models.Model):
+            # This field doesn't result in a clash.
+            f_id = models.IntegerField()
+
+        class Child(Parent):
+            # This field clashes with parent "f_id" field.
+            f = models.ForeignKey(Target)
+
+        errors = Child.check()
+        expected = [
+            Error(
+                ('The field clashes with the field "f_id" '
+                 'from model invalid_models_tests.parent.'),
+                hint=None,
+                obj=Child._meta.get_field('f'),
+                id='E054',
+            )
+        ]
+        self.assertEqual(errors, expected)
+
+    def test_id_clash(self):
+        class Target(models.Model):
+            pass
+
+        class Model(models.Model):
+            fk = models.ForeignKey(Target)
+            fk_id = models.IntegerField()
+
+        errors = Model.check()
+        expected = [
+            Error(
+                ('The field clashes with the field "fk" from model '
+                 'invalid_models_tests.model.'),
+                hint=None,
+                obj=Model._meta.get_field('fk_id'),
+                id='E054',
+            )
+        ]
+        self.assertEqual(errors, expected)
+
+
+class OtherModelTests(IsolatedModelsTestCase):
+
+    def test_unique_primary_key(self):
+        invalid_id = models.IntegerField(primary_key=False)
+
+        class Model(models.Model):
+            id = invalid_id
+
+        errors = Model.check()
+        expected = [
+            Error(
+                ('You cannot use "id" as a field name, because each model '
+                 'automatically gets an "id" field if none of the fields '
+                 'have primary_key=True.'),
+                hint='Remove or rename "id" field or add primary_key=True to a field.',
+                obj=Model,
+                id='E005',
             ),
         ]
         self.assertEqual(errors, expected)
