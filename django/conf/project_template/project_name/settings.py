a718fcf201b04ba254e9073be82f51ae1ae3a853
Florian Apolloner <florian@apolloner.eu>
2014-02-11 09:00:51 +0100

    Reordered INSTALLED_APPS in default template, refs #22005

--- django/conf/project_template/project_name/settings.py
+++ django/conf/project_template/project_name/settings.py
@@ -30,9 +30,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.auth',
+    'django.contrib.admin',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
