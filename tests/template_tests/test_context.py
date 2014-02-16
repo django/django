8274fa60f88607eb0e81908f049c391455956dd8
Marek Wywiał <onjinx@gmail.com>
2014-02-16 14:50:27 +0100

    Made the new template.Context.flatten() method a public API.
    That method was introduced in 9db4271bd11ac23a5a5652bbcdf8fb6d4b997651.
    
    Refs #21765.

--- tests/template_tests/test_context.py
+++ tests/template_tests/test_context.py
@@ -50,6 +50,17 @@ class ContextTests(TestCase):
             test_context['fruit']
         self.assertIsNone(test_context.get('fruit'))
 
+    def test_flatten_context(self):
+        a = Context()
+        a.update({'a': 2})
+        a.update({'b': 4})
+        a.update({'c': 8})
+
+        self.assertEqual(a.flatten(), {
+            'False': False, 'None': None, 'True': True,
+            'a': 2, 'b': 4, 'c': 8
+        })
+
     def test_context_comparable(self):
         test_data = {'x': 'y', 'v': 'z', 'd': {'o': object, 'a': 'b'}}
 
