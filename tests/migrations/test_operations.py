cd2e3917a26672e4b57381119a19e303faee7a8e
Baptiste Mispelon <bmispelon@gmail.com>
2014-02-28 13:14:09 +0100

    Fixed #22168 -- Fixed migrations failing on sqlite when coulumn names are SQL keywords
    Thanks to trac user fallen_flint for the report and initial patch.

--- tests/migrations/test_operations.py
+++ tests/migrations/test_operations.py
@@ -195,6 +195,23 @@ class OperationTests(MigrationTestBase):
             operation.database_backwards("test_adfl", editor, new_state, project_state)
         self.assertColumnNotExists("test_adfl_pony", "height")
 
+    def test_column_name_quoting(self):
+        """
+        Column names that are SQL keywords shouldn't cause problems when used
+        in migrations (#22168).
+        """
+        project_state = self.set_up_test_model("test_regr22168")
+        operation = migrations.AddField(
+            "Pony",
+            "order",
+            models.IntegerField(default=0),
+        )
+        new_state = project_state.clone()
+        operation.state_forwards("test_regr22168", new_state)
+        with connection.schema_editor() as editor:
+            operation.database_forwards("test_regr22168", editor, project_state, new_state)
+        self.assertColumnExists("test_regr22168_pony", "order")
+
     def test_add_field_preserve_default(self):
         """
         Tests the AddField operation's state alteration
