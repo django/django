81e6b8e803f74ba0d810faa2e4250acf8545892f
Shai Berger <shai@platonix.com>
2014-03-12 23:43:45 +0200

    [1.6.x] Fixed #20292: Pass datetime objects (not formatted dates) as params to Oracle
    This seems worthwhile in its own right, but also works around an Oracle
    bug (in versions 10 -- 11.1) where the use of Unicode would reset the
    date/time formats, causing ORA-01843 errors.
    
    Thanks Trac users CarstenF for the report, jtiai for the initial patch,
    and everyone who contributed to the discussion on the ticket.
    
    Backport of 6983201 from master.

--- tests/model_fields/models.py
+++ tests/model_fields/models.py
@@ -61,6 +61,12 @@ class BooleanModel(models.Model):
     bfield = models.BooleanField(default=None)
     string = models.CharField(max_length=10, default='abc')
 
+class DateTimeModel(models.Model):
+    d = models.DateField()
+    dt = models.DateTimeField()
+    t = models.TimeField()
+
+
 class FksToBooleans(models.Model):
     """Model wih FKs to models with {Null,}BooleanField's, #15040"""
     bf = models.ForeignKey(BooleanModel)
