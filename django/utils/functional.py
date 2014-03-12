5c5b266b5478aa4e8bd92f141c56f3a0997749eb
Baptiste Mispelon <bmispelon@gmail.com>
2014-03-12 23:09:18 +0100

    Simplified implementation of collectstatic command.
    Since d2e242d16c6dde6f4736086fb38057424bed3edb made isinstance()
    calls work correctly on LazyObject, we can simplify the
    implementation of is_local_storage added in
    7e27885c6e7588471fd94a4def16b7081577bdfc.

--- django/utils/functional.py
+++ django/utils/functional.py
@@ -367,7 +367,7 @@ class SimpleLazyObject(LazyObject):
 
     def __deepcopy__(self, memo):
         if self._wrapped is empty:
-            # We have to use type(self), not self.__class__, because the
+            # We have to use SimpleLazyObject, not self.__class__, because the
             # latter is proxied.
             result = SimpleLazyObject(self._setupfunc)
             memo[id(self)] = result
