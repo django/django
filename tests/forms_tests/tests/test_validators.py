4d0c5f61427a8e67552ee2d777fffbadc7aff3b2
Dejan Noveski <dr.mote@gmail.com>
2014-03-13 12:23:12 +0100

    Fixed #22255 -- Added support for specifying re flags in RegexValidator

--- tests/forms_tests/tests/test_validators.py
+++ tests/forms_tests/tests/test_validators.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+import re
 from unittest import TestCase
 
 from django import forms
@@ -24,11 +25,22 @@ class UserForm(forms.Form):
             )
         ]
     )
+    ignore_case_string = forms.CharField(
+        max_length=50,
+        validators=[
+            validators.RegexValidator(
+                    regex='^[a-z]*$',
+                    message="Letters only.",
+                    flags=re.IGNORECASE,
+                )
+        ]
+
+    )
 
 
 class TestFieldWithValidators(TestCase):
     def test_all_errors_get_reported(self):
-        form = UserForm({'full_name': 'not int nor mail', 'string': '2 is not correct'})
+        form = UserForm({'full_name': 'not int nor mail', 'string': '2 is not correct', 'ignore_case_string': "IgnORE Case strIng"})
         self.assertRaises(ValidationError, form.fields['full_name'].clean, 'not int nor mail')
 
         try:
@@ -37,4 +49,5 @@ class TestFieldWithValidators(TestCase):
             self.assertEqual(2, len(e.messages))
 
         self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['string'], ["Letters only."])
         self.assertEqual(form.errors['string'], ["Letters only."])
