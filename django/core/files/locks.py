04551fb26e343af830fad6b810eff5f197baea52
smallcode <14190635@qq.com>
2014-03-18 12:52:35 +0800

    remove line

--- django/core/files/locks.py
+++ django/core/files/locks.py
@@ -25,7 +25,6 @@ def _fd(f):
     """Get a filedescriptor from something which could be a file or an fd."""
     return f.fileno() if hasattr(f, 'fileno') else f
     
-
 if os.name == 'nt':
     import msvcrt
     from ctypes import (sizeof, c_ulong, c_void_p, c_int64,
