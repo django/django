253e8ac29fa4eb86bfc327fd4b68291370c9011c
Aymeric Augustin <aymeric.augustin@m4x.org>
2014-03-21 22:07:39 +0100

    Removed django.middleware.doc. Refs #20126.
    Small doc changes missed in 66076268.

--- django/middleware/doc.py
+++
@@ -1,9 +0,0 @@
-"""XViewMiddleware has been moved to django.contrib.admindocs.middleware."""
-
-import warnings
-
-from django.utils.deprecation import RemovedInDjango18Warning
-
-warnings.warn(__doc__, RemovedInDjango18Warning, stacklevel=2)
-
-from django.contrib.admindocs.middleware import XViewMiddleware  # NOQA
