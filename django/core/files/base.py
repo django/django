09cbcd65f08842bbc86195ff535ce362ca388d75
Aymeric Augustin <aymeric.augustin@m4x.org>
2014-03-22 11:17:06 +0100

    Fixed a failing test introduced in 918a16bc.
    Refs #22307.

--- django/core/files/base.py
+++ django/core/files/base.py
@@ -37,18 +37,21 @@ class File(FileProxyMixin):
         return self.size
 
     def _get_size(self):
-        if not hasattr(self, '_size'):
-            if hasattr(self.file, 'size'):
-                self._size = self.file.size
-            elif hasattr(self.file, 'name') and self.file.name is not None and os.path.exists(self.file.name):
+        if not hasattr(self, '_size') and hasattr(self.file, 'size'):
+            self._size = self.file.size
+        if not hasattr(self, '_size') and hasattr(self.file, 'name'):
+            try:
                 self._size = os.path.getsize(self.file.name)
-            elif hasattr(self.file, 'tell') and hasattr(self.file, 'seek'):
-                pos = self.file.tell()
-                self.file.seek(0, os.SEEK_END)
-                self._size = self.file.tell()
-                self.file.seek(pos)
-            else:
-                raise AttributeError("Unable to determine the file's size.")
+            except (OSError, TypeError):
+                pass
+        if not hasattr(self, '_size') and (hasattr(self.file, 'tell') and
+                                           hasattr(self.file, 'seek')):
+            pos = self.file.tell()
+            self.file.seek(0, os.SEEK_END)
+            self._size = self.file.tell()
+            self.file.seek(pos)
+        if not hasattr(self, '_size'):
+            raise AttributeError("Unable to determine the file's size.")
         return self._size
 
     def _set_size(self, size):
