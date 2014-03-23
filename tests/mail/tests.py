4aa80149e71d02947164a89ec5da6fd035393425
Daniel Neuhäuser <dasdasich@gmail.com>
2014-03-23 17:29:10 +0100

    Fixed #22327 -- Turned BaseEmailBackend into a context manager
    Changed the BaseEmailBackend to allow usage as context manager to open
    and close connections.

--- tests/mail/tests.py
+++ tests/mail/tests.py
@@ -638,6 +638,27 @@ class BaseEmailBackendTests(HeadersCheckMixin, object):
         except Exception as e:
             self.fail("close() unexpectedly raised an exception: %s" % e)
 
+    def test_use_as_contextmanager(self):
+        """
+        Test that the connection can be used as a contextmanager.
+        """
+        opened = [False]
+        closed = [False]
+        conn = mail.get_connection(username='', password='')
+
+        def open():
+            opened[0] = True
+        conn.open = open
+
+        def close():
+            closed[0] = True
+        conn.close = close
+        with conn as same_conn:
+            self.assertTrue(opened[0])
+            self.assertIs(same_conn, conn)
+            self.assertFalse(closed[0])
+        self.assertTrue(closed[0])
+
 
 class LocmemBackendTests(BaseEmailBackendTests, SimpleTestCase):
     email_backend = 'django.core.mail.backends.locmem.EmailBackend'
