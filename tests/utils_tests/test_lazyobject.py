3d74f1f64a78e5aeebb91c76e44d8e318a1c0023
Tim Graham <timograham@gmail.com>
2014-03-27 06:59:54 -0400

    [1.7.x] Fixed a deprecation warning on Python 3.
    Backport of c19bbefca2 from master

--- tests/utils_tests/test_lazyobject.py
+++ tests/utils_tests/test_lazyobject.py
@@ -218,7 +218,7 @@ class SimpleLazyObjectTestCase(LazyObjectTestCase):
         obj = self.lazy_wrap(42)
         # __repr__ contains __repr__ of setup function and does not evaluate
         # the SimpleLazyObject
-        self.assertRegexpMatches(repr(obj), '^<SimpleLazyObject:')
+        six.assertRegex(self, repr(obj), '^<SimpleLazyObject:')
         self.assertIs(obj._wrapped, empty)  # make sure evaluation hasn't been triggered
 
         self.assertEqual(obj, 42)  # evaluate the lazy object
