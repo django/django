f356b6ecc3fe15634d504690c1793460e5fe9bee
Aymeric Augustin <aymeric.augustin@m4x.org>
2014-03-29 19:44:11 +0100

    Increased memoization cache size for language codes.
    There may be more than 100 (default maxsize) commonly seen xx-yy values
    on some sites. The additional memory consumption isn't significant.
    
    Also added a comment explaining why this cache must have a maxsize.

--- django/utils/translation/trans_real.py
+++ django/utils/translation/trans_real.py
@@ -389,12 +389,16 @@ def all_locale_paths():
     return [globalpath] + list(settings.LOCALE_PATHS)
 
 
-@lru_cache.lru_cache()
+@lru_cache.lru_cache(maxsize=1000)
 def check_for_language(lang_code):
     """
     Checks whether there is a global language file for the given language
     code. This is used to decide whether a user-provided language is
     available.
+
+    lru_cache should have a maxsize to prevent from memory exhaustion attacks,
+    as the provided language codes are taken from the HTTP request. See also
+    <https://www.djangoproject.com/weblog/2007/oct/26/security-fix/>.
     """
     # First, a quick check to make sure lang_code is well-formed (#21458)
     if not language_code_re.search(lang_code):
