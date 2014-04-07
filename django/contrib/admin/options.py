3cba90f22a9120ebf9b00d1d074b8fb2152ecd2e
Tim Graham <timograham@gmail.com>
2014-04-07 09:04:49 -0400

    Fixed #21084 -- Used proxy model's content type for admin log entries.
    (the previous commit included only the test)

--- django/contrib/admin/options.py
+++ django/contrib/admin/options.py
@@ -60,7 +60,7 @@ def get_content_type_for_model(obj):
     # Since this module gets imported in the application's root package,
     # it cannot import models from other applications at the module level.
     from django.contrib.contenttypes.models import ContentType
-    return ContentType.objects.get_for_model(obj)
+    return ContentType.objects.get_for_model(obj, for_concrete_model=False)
 
 
 def get_ul_class(radio_style):
