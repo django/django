ae1586f61f6a4e79bb2667fd832d250bed568973
Caroline Simpson <github@hoojiboo.com>
2014-04-16 15:36:17 -0400

    Fixed #21936 -- Refactored DeleteView to be compatible with SuccessMessageMixin
    Refactored DeleteView to use the form related base classes and mixins, like the
    other class-based generic editting views.  Added tests for the handling the
    DELETE http method and ensuring the SuccessMessageMixin works. Updated DeleteView
    documentation to reflect the new base classes.
    The majority of the code changes were provided by Simon Charette.

--- django/contrib/messages/tests/test_mixins.py
+++ django/contrib/messages/tests/test_mixins.py
@@ -1,4 +1,7 @@
-from django.contrib.messages.tests.urls import ContactFormViewWithMsg
+from django.contrib.messages.tests.models import SomeObject
+from django.contrib.messages.tests.urls import (
+    ContactFormViewWithMsg, DeleteFormViewWithMsg,
+)
 from django.core.urlresolvers import reverse
 from django.test import TestCase, override_settings
 
@@ -12,4 +15,12 @@ class SuccessMessageMixinTests(TestCase):
         add_url = reverse('add_success_msg')
         req = self.client.post(add_url, author)
         self.assertIn(ContactFormViewWithMsg.success_message % author,
+                      req.cookies['messages'].value)
+
+    def test_set_messages_success_on_delete(self):
+        object_to_delete = SomeObject.objects.create(name="MyObject")
+
+        delete_url = reverse('success_msg_on_delete', args=[object_to_delete.pk])
+        req = self.client.delete(delete_url)
+        self.assertIn(DeleteFormViewWithMsg.success_message,
                       req.cookies['messages'].value)
