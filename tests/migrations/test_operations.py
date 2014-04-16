71c8ae543ffec847ec275859bc406202f5029604
Motiejus Jakštys <motiejus.jakstys@spilgames.com>
2014-04-16 16:55:34 +0200

    [1.7.x] Fixed #22460 -- Explicity remove constraints when dropping a related field.
    Backport of 0e45669fa9 from master

--- tests/migrations/test_operations.py
+++ tests/migrations/test_operations.py
@@ -416,6 +416,22 @@ class OperationTests(MigrationTestBase):
             operation.database_backwards("test_rmfl", editor, new_state, project_state)
         self.assertColumnExists("test_rmfl_pony", "pink")
 
+    def test_remove_fk(self):
+        """
+        Tests the RemoveField operation on a foreign key.
+        """
+        project_state = self.set_up_test_model("test_rfk", related_model=True)
+        self.assertColumnExists("test_rfk_rider", "pony_id")
+        operation = migrations.RemoveField("Rider", "pony")
+        new_state = project_state.clone()
+        operation.state_forwards("test_rfk", new_state)
+        with connection.schema_editor() as editor:
+            operation.database_forwards("test_rfk", editor, project_state, new_state)
+        self.assertColumnNotExists("test_rfk_rider", "pony_id")
+        with connection.schema_editor() as editor:
+            operation.database_backwards("test_rfk", editor, new_state, project_state)
+        self.assertColumnExists("test_rfk_rider", "pony_id")
+
     def test_alter_model_table(self):
         """
         Tests the AlterModelTable operation.
