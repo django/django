0dad0ca55ec6081d3e92ac8340ed9f8d5c65a606
Aymeric Augustin <aymeric.augustin@m4x.org>
2014-04-21 11:50:53 +0200

    [1.7.x] Further consolidated the model_inheritance tests.
    Backport of 3f01e82 from master

--- tests/model_inheritance_regress/tests.py
+++ tests/model_inheritance_regress/tests.py
@@ -466,3 +466,25 @@ class ModelInheritanceTest(TestCase):
                                   serves_pizza=True, serves_hot_dogs=True)
         p = Place.objects.all().select_related('restaurant')[0]
         self.assertIsInstance(p.restaurant.serves_pizza, bool)
+
+    def test_inheritance_select_related(self):
+        # Regression test for #7246
+        r1 = Restaurant.objects.create(
+            name="Nobu", serves_hot_dogs=True, serves_pizza=False
+        )
+        r2 = Restaurant.objects.create(
+            name="Craft", serves_hot_dogs=False, serves_pizza=True
+        )
+        Supplier.objects.create(name="John", restaurant=r1)
+        Supplier.objects.create(name="Jane", restaurant=r2)
+
+        self.assertQuerysetEqual(
+            Supplier.objects.order_by("name").select_related(), [
+                "Jane",
+                "John",
+            ],
+            attrgetter("name")
+        )
+
+        jane = Supplier.objects.order_by("name").select_related("restaurant")[0]
+        self.assertEqual(jane.restaurant.name, "Craft")
