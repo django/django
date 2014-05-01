e2e4cdba1178ebcfd95c19eb063abd55b4afbcbb
Yehonatan Daniv <maggotfish@gmail.com>
2014-05-01 12:55:52 +0300

    Fixed #22539 -- Copied exclude argument in Model.full_clean() to prevent side effects.

--- tests/validation/tests.py
+++ tests/validation/tests.py
@@ -59,6 +59,13 @@ class BaseModelValidationTests(ValidationTestCase):
         mtv = ModelToValidate(number=10, name='Some Name', slug='##invalid##')
         self.assertFailsValidation(mtv.full_clean, ['slug'])
 
+    def test_full_clean_does_not_mutate_exclude(self):
+        mtv = ModelToValidate(f_with_custom_validator=42)
+        exclude = ['number']
+        self.assertFailsValidation(mtv.full_clean, ['name'], exclude=exclude)
+        self.assertEqual(len(exclude), 1)
+        self.assertEqual(exclude[0], 'number')
+
 
 class ArticleForm(forms.ModelForm):
     class Meta:
