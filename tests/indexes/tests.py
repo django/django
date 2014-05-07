43a80f4812a762c2ea96b3de45662ace04404287
Aymeric Augustin <aymeric.augustin@m4x.org>
2014-05-07 21:49:45 +0200

    Simplified indexes tests.

--- tests/indexes/tests.py
+++ tests/indexes/tests.py
@@ -1,7 +1,7 @@
 from unittest import skipUnless
 
 from django.core.management.color import no_style
-from django.db import connections, DEFAULT_DB_ALIAS
+from django.db import connection
 from django.test import TestCase
 
 from .models import Article, IndexTogetherSingleList
@@ -9,22 +9,19 @@ from .models import Article, IndexTogetherSingleList
 
 class IndexesTests(TestCase):
     def test_index_together(self):
-        connection = connections[DEFAULT_DB_ALIAS]
         index_sql = connection.creation.sql_indexes_for_model(Article, no_style())
         self.assertEqual(len(index_sql), 1)
 
     def test_index_together_single_list(self):
         # Test for using index_together with a single list (#22172)
-        connection = connections[DEFAULT_DB_ALIAS]
         index_sql = connection.creation.sql_indexes_for_model(IndexTogetherSingleList, no_style())
         self.assertEqual(len(index_sql), 1)
 
-    @skipUnless(connections[DEFAULT_DB_ALIAS].vendor == 'postgresql',
+    @skipUnless(connection.vendor == 'postgresql',
         "This is a postgresql-specific issue")
     def test_postgresql_text_indexes(self):
         """Test creation of PostgreSQL-specific text indexes (#12234)"""
         from .models import IndexedArticle
-        connection = connections[DEFAULT_DB_ALIAS]
         index_sql = connection.creation.sql_indexes_for_model(IndexedArticle, no_style())
         self.assertEqual(len(index_sql), 5)
         self.assertIn('("headline" varchar_pattern_ops)', index_sql[1])
