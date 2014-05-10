380b864fe9fcac906c474d484d845a55e7c8ea17
Aymeric Augustin <aymeric.augustin@m4x.org>
2014-05-10 16:39:20 +0200

    Fixed #22508 -- Avoided overwriting select_related.
    Previously, known related objects overwrote related objects loaded
    though select_related. This could cancel the effect of select_related
    when it was used over more than one level.
    
    Thanks boxm for the bug report and timo for bisecting the regression.

--- tests/select_related_regress/tests.py
+++ tests/select_related_regress/tests.py
@@ -188,3 +188,13 @@ class SelectRelatedRegressTests(TestCase):
             self.assertEqual(qs_c.c_b.lots_of_text, 'lots_of_text_b')
             self.assertEqual(qs_c.c_a.name, 'a')
             self.assertEqual(qs_c.c_b.name, 'b')
+
+    def test_regression_22508(self):
+        building = Building.objects.create(name='101')
+        device = Device.objects.create(name="router", building=building)
+        Port.objects.create(port_number='1', device=device)
+
+        device = Device.objects.get()
+        port = device.port_set.select_related('device__building').get()
+        with self.assertNumQueries(0):
+            port.device.building
