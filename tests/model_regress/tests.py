6bb6df29431b606937637be5837cc671836b142b
Shai Berger <shai@platonix.com>
2014-05-16 20:04:26 +0300

    Fix storing of binary fields and unicode textfields for Oracle/Python3

--- tests/model_regress/tests.py
+++ tests/model_regress/tests.py
@@ -66,6 +66,17 @@ class ModelTests(TestCase):
         a = Article.objects.get(pk=a.pk)
         self.assertEqual(len(a.article_text), 5000)
 
+    def test_long_unicode_textfield(self):
+        # TextFields can hold more than 4000 bytes also when they are
+        # less than 4000 characters
+        a = Article.objects.create(
+            headline="Really, really big",
+            pub_date=datetime.datetime.now(),
+            article_text='\u05d0\u05d1\u05d2' * 1000
+        )
+        a = Article.objects.get(pk=a.pk)
+        self.assertEqual(len(a.article_text), 3000)
+
     def test_date_lookup(self):
         # Regression test for #659
         Party.objects.create(when=datetime.datetime(1999, 12, 31))
