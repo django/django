c017e9b3669cf2980e5fd4211ad14bf378748dd9
Stanislas Guerra <stanislas.guerra@gmail.com>
2014-05-21 12:32:19 +0200

    ticket_16311: Add a RelatedOnlyFieldListFilter class in admin.filters.
    - add a RelatedFieldListFilter.field_choices() hook method overriden in RelatedOnlyFieldListFilter to construct a limit_choices_to
      param to filter the related objects list to display in the admin filter on the ones presents in that very relation (instead of
      all the possible objects).
    - add a limit_choices_to param to the Field.get_choices() method. If None (default), fallback on the normal behavior.
    - Regression tests.

--- django/contrib/admin/filters.py
+++ django/contrib/admin/filters.py
@@ -169,7 +169,7 @@ class RelatedFieldListFilter(FieldListFilter):
         self.lookup_kwarg_isnull = '%s__isnull' % field_path
         self.lookup_val = request.GET.get(self.lookup_kwarg)
         self.lookup_val_isnull = request.GET.get(self.lookup_kwarg_isnull)
-        self.lookup_choices = field.get_choices(include_blank=False)
+        self.lookup_choices = self.field_choices(field, request, model_admin)
         super(RelatedFieldListFilter, self).__init__(
             field, request, params, model, model_admin, field_path)
         if hasattr(field, 'verbose_name'):
@@ -189,6 +189,9 @@ class RelatedFieldListFilter(FieldListFilter):
 
     def expected_parameters(self):
         return [self.lookup_kwarg, self.lookup_kwarg_isnull]
+
+    def field_choices(self, field, request, model_admin):
+        return field.get_choices(include_blank=False)
 
     def choices(self, cl):
         from django.contrib.admin.views.main import EMPTY_CHANGELIST_VALUE
@@ -409,3 +412,9 @@ class AllValuesFieldListFilter(FieldListFilter):
             }
 
 FieldListFilter.register(lambda f: True, AllValuesFieldListFilter)
+
+
+class RelatedOnlyFieldListFilter(RelatedFieldListFilter):
+    def field_choices(self, field, request, model_admin):
+        limit_choices_to = {'pk__in': set(model_admin.get_queryset(request).values_list(field.name, flat=True))}
+        return field.get_choices(include_blank=False, limit_choices_to=limit_choices_to)
