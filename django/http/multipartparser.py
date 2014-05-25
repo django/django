47923fb1b37c7f96ca1bdb7a59d568e4490c33e1
Florian Apolloner <florian@apolloner.eu>
2014-05-25 22:52:47 +0200

    Fixed #22680 --  I/O operation on closed file.
    This patch is two-fold; first it ensure that Django does close everything in
    request.FILES at the end of the request and secondly the storage system should
    no longer close any files during save, it's up to the caller to handle that --
    or let Django close the files at the end of the request.

--- django/http/multipartparser.py
+++ django/http/multipartparser.py
@@ -228,6 +228,7 @@ class MultiPartParser(object):
                                     break
 
                     except SkipFile:
+                        self._close_files()
                         # Just use up the rest of this file...
                         exhaust(field_stream)
                     else:
@@ -237,6 +238,7 @@ class MultiPartParser(object):
                     # If this is neither a FIELD or a FILE, just exhaust the stream.
                     exhaust(stream)
         except StopUpload as e:
+            self._close_files()
             if not e.connection_reset:
                 exhaust(self._input_data)
         else:
@@ -267,6 +269,14 @@ class MultiPartParser(object):
     def IE_sanitize(self, filename):
         """Cleanup filename from Internet Explorer full paths."""
         return filename and filename[filename.rfind("\\") + 1:].strip()
+
+    def _close_files(self):
+        # Free up all file handles.
+        # FIXME: this currently assumes that upload handlers store the file as 'file'
+        # We should document that... (Maybe add handler.free_file to complement new_file)
+        for handler in self._upload_handlers:
+            if hasattr(handler, 'file'):
+                handler.file.close()
 
 
 class LazyStream(six.Iterator):
