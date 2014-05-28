504c89e8008c557a1e83c45535b549f77a3503b2
Maxime Turcotte <maxime.turcotte@savoirfairelinux.com>
2014-05-28 12:07:27 -0400

    Fixed #6327 -- Added has_module_permission method to BaseModelAdmin
    Thanks chrj for the suggestion.

--- django/contrib/admin/options.py
+++ django/contrib/admin/options.py
@@ -473,6 +473,19 @@ class BaseModelAdmin(six.with_metaclass(forms.MediaDefiningClass)):
         codename = get_permission_codename('delete', opts)
         return request.user.has_perm("%s.%s" % (opts.app_label, codename))
 
+    def has_module_permission(self, request):
+        """
+        Returns True if the given request has any permission in the given
+        app label.
+
+        Can be overridden by the user in subclasses. In such case it should
+        return True if the given request has permission to view the module on
+        the admin index page and access the module's index page. Overriding it
+        does not restrict access to the add, change or delete views. Use
+        `ModelAdmin.has_(add|change|delete)_permission` for that.
+        """
+        return request.user.has_module_perms(self.opts.app_label)
+
 
 @python_2_unicode_compatible
 class ModelAdmin(BaseModelAdmin):
