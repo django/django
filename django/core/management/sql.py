ab3c96a06c02d2f279a8126facb348a63bfe05a2
Víðir Valberg Guðmundsson <valberg@orn.li>
2014-05-29 19:15:42 +0200

    Fixed #22611 -- Making SQL management commands migration aware.

--- django/core/management/sql.py
+++ django/core/management/sql.py
@@ -10,10 +10,20 @@ from django.conf import settings
 from django.core.management.base import CommandError
 from django.db import models, router
 from django.utils.deprecation import RemovedInDjango19Warning
+from django.db.migrations.loader import MigrationLoader
 
 
-def sql_create(app_config, style, connection):
+def check_for_migrations(app_config, connection):
+    loader = MigrationLoader(connection)
+
+    if app_config.label in loader.migrated_apps:
+        raise CommandError("App '%s' has migrations. Only the sqlmigrate and sqlflush commands can be used when an app has migrations." % app_config.label)
+
+
+def sql_create(app_config, connection):
     "Returns a list of the CREATE TABLE SQL statements for the given app."
+
+    check_for_migrations(app_config, connection)
 
     if connection.settings_dict['ENGINE'] == 'django.db.backends.dummy':
         # This must be the "dummy" database backend, which means the user
@@ -27,39 +37,18 @@ def sql_create(app_config, style, connection):
     # generate invalid SQL (leaving models out of known_models is harmless, so
     # we can be conservative).
     app_models = app_config.get_models(include_auto_created=True)
-    final_output = []
-    tables = connection.introspection.table_names()
-    known_models = set(model for model in connection.introspection.installed_models(tables) if model not in app_models)
-    pending_references = {}
 
-    for model in router.get_migratable_models(app_config, connection.alias, include_auto_created=True):
-        output, references = connection.creation.sql_create_model(model, style, known_models)
-        final_output.extend(output)
-        for refto, refs in references.items():
-            pending_references.setdefault(refto, []).extend(refs)
-            if refto in known_models:
-                final_output.extend(connection.creation.sql_for_pending_references(refto, style, pending_references))
-        final_output.extend(connection.creation.sql_for_pending_references(model, style, pending_references))
-        # Keep track of the fact that we've created the table for this model.
-        known_models.add(model)
+    with connection.schema_editor(collect_sql=True) as schema_editor:
+        for model in router.get_migratable_models(app_config, connection.alias):
+            schema_editor.create_model(model)
 
-    # Handle references to tables that are from other apps
-    # but don't exist physically.
-    not_installed_models = set(pending_references.keys())
-    if not_installed_models:
-        alter_sql = []
-        for model in not_installed_models:
-            alter_sql.extend(['-- ' + sql for sql in
-                connection.creation.sql_for_pending_references(model, style, pending_references)])
-        if alter_sql:
-            final_output.append('-- The following references should be added but depend on non-existent tables:')
-            final_output.extend(alter_sql)
-
-    return final_output
+    return schema_editor.collected_sql
 
 
-def sql_delete(app_config, style, connection):
+def sql_delete(app_config, connection):
     "Returns a list of the DROP TABLE SQL statements for the given app."
+
+    check_for_migrations(app_config, connection)
 
     # This should work even if a connection isn't available
     try:
@@ -76,24 +65,15 @@ def sql_delete(app_config, style, connection):
 
         output = []
 
-        # Output DROP TABLE statements for standard application tables.
-        to_delete = set()
+        app_models = router.get_migratable_models(app_config, connection.alias)
 
-        references_to_delete = {}
-        app_models = router.get_migratable_models(app_config, connection.alias, include_auto_created=True)
-        for model in app_models:
-            if cursor and connection.introspection.table_name_converter(model._meta.db_table) in table_names:
-                # The table exists, so it needs to be dropped
-                opts = model._meta
-                for f in opts.local_fields:
-                    if f.rel and f.rel.to not in to_delete:
-                        references_to_delete.setdefault(f.rel.to, []).append((model, f))
+        with connection.schema_editor(collect_sql=True) as schema_editor:
+            for model in app_models:
+                if connection.introspection.table_name_converter(model._meta.db_table) in table_names:
+                    schema_editor.delete_model(model)
 
-                to_delete.add(model)
+        output.extend(schema_editor.collected_sql)
 
-        for model in app_models:
-            if connection.introspection.table_name_converter(model._meta.db_table) in table_names:
-                output.extend(connection.creation.sql_destroy_model(model, references_to_delete, style))
     finally:
         # Close database connection explicitly, in case this output is being piped
         # directly into a database client, to avoid locking issues.
@@ -111,6 +91,7 @@ def sql_flush(style, connection, only_django=False, reset_sequences=True, allow_
     If only_django is True, then only table names that have associated Django
     models and are in INSTALLED_APPS will be included.
     """
+
     if only_django:
         tables = connection.introspection.django_table_names(only_existing=True)
     else:
@@ -122,6 +103,9 @@ def sql_flush(style, connection, only_django=False, reset_sequences=True, allow_
 
 def sql_custom(app_config, style, connection):
     "Returns a list of the custom table modifying SQL statements for the given app."
+
+    check_for_migrations(app_config, connection)
+
     output = []
 
     app_models = router.get_migratable_models(app_config, connection.alias)
@@ -134,6 +118,9 @@ def sql_custom(app_config, style, connection):
 
 def sql_indexes(app_config, style, connection):
     "Returns a list of the CREATE INDEX SQL statements for all models in the given app."
+
+    check_for_migrations(app_config, connection)
+
     output = []
     for model in router.get_migratable_models(app_config, connection.alias, include_auto_created=True):
         output.extend(connection.creation.sql_indexes_for_model(model, style))
@@ -142,6 +129,9 @@ def sql_indexes(app_config, style, connection):
 
 def sql_destroy_indexes(app_config, style, connection):
     "Returns a list of the DROP INDEX SQL statements for all models in the given app."
+
+    check_for_migrations(app_config, connection)
+
     output = []
     for model in router.get_migratable_models(app_config, connection.alias, include_auto_created=True):
         output.extend(connection.creation.sql_destroy_indexes_for_model(model, style))
@@ -150,7 +140,10 @@ def sql_destroy_indexes(app_config, style, connection):
 
 def sql_all(app_config, style, connection):
     "Returns a list of CREATE TABLE SQL, initial-data inserts, and CREATE INDEX SQL for the given module."
-    return sql_create(app_config, style, connection) + sql_custom(app_config, style, connection) + sql_indexes(app_config, style, connection)
+
+    check_for_migrations(app_config, connection)
+
+    return sql_create(app_config, connection) + sql_custom(app_config, style, connection)
 
 
 def _split_statements(content):
