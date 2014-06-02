9b62865458e1c6995ad46611c11cb4f01921bba8
Daniel Pyrathon <pirosb3@gmail.com>
2014-06-02 14:08:01 +0200

    added tests for related objects

--- tests/model_options/test.py
+++ tests/model_options/test.py
@@ -4,9 +4,9 @@ from django.db.models.fields.related import ManyToManyRel
 
 from .models import (
     Person, Quartet, Group, Reporter, Musician,
-    SuperData,
+    SuperData, M2MModel,
     SuperM2MModel,
-    RelatedModel
+    RelatedModel, BaseRelatedModel
 )
 
 
@@ -36,3 +36,51 @@ class DataTests(OptionsBaseTests):
         ])
         self.assertTrue(all([f.column is not None
                              for f in fields]))
+
+    def test_many_to_many(self):
+        fields = SuperM2MModel._meta.many_to_many
+        self.assertEquals([f.attname for f in fields], [
+            'members',
+            'members_super'
+        ])
+        self.assertTrue(all([isinstance(f.rel, ManyToManyRel)
+                             for f in fields]))
+
+    def test_many_to_many_with_model(self):
+        models = dict(SuperM2MModel._meta.get_m2m_with_model()).values()
+        self.assertEquals(len(models), 2)
+        self.assertEquals(models[0], M2MModel)
+        self.assertEquals(models[1], None)
+
+    def test_related_objects(self):
+        objects = RelatedModel._meta.get_all_related_objects_with_model()
+        fields, models = dict(objects).keys(), dict(objects).values()
+        self.assertEquals([f.name for f in fields], [
+            'model_options:secondrelatingobject',
+            'model_options:firstrelatingobject',
+        ])
+        self.assertEquals(models, [
+            None, BaseRelatedModel
+        ])
+
+    def test_related_objects_local(self):
+        objects = RelatedModel._meta.get_all_related_objects_with_model(
+            local_only=True)
+        fields, models = dict(objects).keys(), dict(objects).values()
+        self.assertEquals([f.name for f in fields], [
+            'model_options:secondrelatingobject'
+        ])
+        self.assertEquals(models, [None])
+
+    def test_related_objects_include_hidden(self):
+        objects = RelatedModel._meta.get_all_related_objects_with_model(
+            include_hidden=True)
+        fields, models = dict(objects).keys(), dict(objects).values()
+        self.assertEquals([f.name for f in fields], [
+            'model_options:secondrelatingobject',
+            'model_options:secondrelatinghiddenobject',
+            'model_options:firstrelatingobject',
+            'model_options:firstrelatinghiddenobject'
+        ])
+        self.assertEquals(models, [None, None, BaseRelatedModel,
+                                   BaseRelatedModel])
