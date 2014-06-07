fd4ccd045cf8edf0bf10ee0c576c7b7c5819170c
Duncan Parkes <duncan@mysociety.org>
2014-06-08 00:47:43 +0100

    Fixed #22799 -- Made GET and POST on HttpRequest QueryDicts, and FILES a MultiValueDict.
    Previously, GET, POST, and FILES on an HttpRequest were created in
    the __init__ method as dictionaries. This was not something you would
    usually notice causing trouble in production as you'd only see a
    WSGIRequest, but in testing using the test client, calling .getlist
    on GET, POST, or FILES for a request with no get/post data resulted in
    an AttributeError.
    
    Changed GET and POST on an HttpRequest object to be mutable
    QueryDicts (mutable because the Django tests, and probably many
    third party tests, were expecting it).

--- django/http/request.py
+++ django/http/request.py
@@ -49,7 +49,12 @@ class HttpRequest(object):
         # Any variable assignment made here should also happen in
         # `WSGIRequest.__init__()`.
 
-        self.GET, self.POST, self.COOKIES, self.META, self.FILES = {}, {}, {}, {}, {}
+        self.GET = QueryDict(mutable=True)
+        self.POST = QueryDict(mutable=True)
+        self.COOKIES = {}
+        self.META = {}
+        self.FILES = MultiValueDict()
+
         self.path = ''
         self.path_info = ''
         self.method = None
