7018bcfb71847a3f016d8f65cb60f72fcbb22f5a
Andrew Godwin <andrew@aeracode.org>
2014-06-08 20:07:35 -0700

    Fix broken alias testing test

--- tests/test_runner/tests.py
+++ tests/test_runner/tests.py
@@ -309,8 +309,8 @@ class AliasedDatabaseTeardownTest(unittest.TestCase):
         old_create_test_db = DatabaseCreation.create_test_db
         try:
             destroyed_names = []
-            DatabaseCreation.destroy_test_db = lambda self, old_database_name, verbosity=1, keepdb=False: destroyed_names.append(old_database_name)
-            DatabaseCreation.create_test_db = lambda self, verbosity=1, autoclobber=False, keepdb=False: self._get_test_db_name()
+            DatabaseCreation.destroy_test_db = lambda self, old_database_name, verbosity=1, keepdb=False, serialize=True: destroyed_names.append(old_database_name)
+            DatabaseCreation.create_test_db = lambda self, verbosity=1, autoclobber=False, keepdb=False, serialize=True: self._get_test_db_name()
 
             db.connections = db.ConnectionHandler({
                 'default': {
