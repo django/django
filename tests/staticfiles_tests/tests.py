9980f67154ed3b10493e159d62c72ab6394769fe
Tim Graham <timograham@gmail.com>
2014-06-12 13:08:27 -0400

    Fixed a staticfiles test on Windows.

--- tests/staticfiles_tests/tests.py
+++ tests/staticfiles_tests/tests.py
@@ -678,16 +678,17 @@ class TestCollectionManifestStorage(TestHashedFiles, BaseCollectionTestCase,
         self.assertEqual(hashed_files, manifest)
 
     def test_clear_empties_manifest(self):
+        cleared_file_name = os.path.join('test', 'cleared.txt')
         # collect the additional file
         self.run_collectstatic()
 
         hashed_files = storage.staticfiles_storage.hashed_files
-        self.assertIn('test/cleared.txt', hashed_files)
+        self.assertIn(cleared_file_name, hashed_files)
 
         manifest_content = storage.staticfiles_storage.load_manifest()
-        self.assertIn('test/cleared.txt', manifest_content)
+        self.assertIn(cleared_file_name, manifest_content)
 
-        original_path = storage.staticfiles_storage.path('test/cleared.txt')
+        original_path = storage.staticfiles_storage.path(cleared_file_name)
         self.assertTrue(os.path.exists(original_path))
 
         # delete the original file form the app, collect with clear
@@ -697,10 +698,10 @@ class TestCollectionManifestStorage(TestHashedFiles, BaseCollectionTestCase,
         self.assertFileNotFound(original_path)
 
         hashed_files = storage.staticfiles_storage.hashed_files
-        self.assertNotIn('test/cleared.txt', hashed_files)
+        self.assertNotIn(cleared_file_name, hashed_files)
 
         manifest_content = storage.staticfiles_storage.load_manifest()
-        self.assertNotIn('test/cleared.txt', manifest_content)
+        self.assertNotIn(cleared_file_name, manifest_content)
 
 
 # we set DEBUG to False here since the template tag wouldn't work otherwise
