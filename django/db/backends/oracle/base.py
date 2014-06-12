84714dfed726212122ac9a143ddbe6719dac5884
Tim Graham <timograham@gmail.com>
2014-06-12 11:55:31 -0400

    [1.7.x] Fixed test failures due to refs #20420.
    cfcca7ccce3dc527d16757ff6dc978e50c4a2e61 renamed connection.queries
    to queries_log and this wasn't backported to stable/1.7.x.

--- django/db/backends/oracle/base.py
+++ django/db/backends/oracle/base.py
@@ -694,7 +694,7 @@ class DatabaseWrapper(BaseDatabaseWrapper):
     # logging is enabled to keep query counts consistent with other backends.
     def _savepoint_commit(self, sid):
         if self.queries_logged:
-            self.queries_log.append({
+            self.queries.append({
                 'sql': '-- RELEASE SAVEPOINT %s (faked)' % self.ops.quote_name(sid),
                 'time': '0.000',
             })
