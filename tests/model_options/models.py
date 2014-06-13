0c07ee8abf0763b8c63e1fb57530fcfe6fc62a9a
Daniel Pyrathon <pirosb3@gmail.com>
2014-06-13 15:41:23 +0200

    added field commenting in model_options test models

--- tests/model_options/models.py
+++ tests/model_options/models.py
@@ -4,7 +4,6 @@ from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelatio
 from django.contrib.contenttypes.models import ContentType
 
 
-# VIRTUAL field models
 class Relation(models.Model):
     pass
 
@@ -14,12 +13,15 @@ class AbstractPerson(models.Model):
     class Meta:
         abstract = True
 
+    # DATA fields
     data_abstract = models.CharField(max_length=10)
-    fk_abstract = models.ForeignKey(Relation,
-                                    related_name='fk_abstract_rel')
-    m2m_abstract = models.ManyToManyField(Relation,
-                                    related_name='m2m_abstract_rel')
+    fk_abstract = models.ForeignKey(Relation, related_name='fk_abstract_rel')
 
+    # M2M fields
+    m2m_abstract = models.ManyToManyField(Relation,
+                                          related_name='m2m_abstract_rel')
+
+    # VIRTUAL fields
     data_not_concrete_abstract = models.ForeignObject(
         Relation,
         from_fields=['abstract_non_concrete_id'],
@@ -27,59 +29,69 @@ class AbstractPerson(models.Model):
         related_name='fo_abstract_rel'
     )
 
-    content_type_abstract = models.ForeignKey(ContentType,
-                                              related_name='+')
+    # GFK fields
+    content_type_abstract = models.ForeignKey(ContentType, related_name='+')
     object_id_abstract = models.PositiveIntegerField()
     content_object_abstract = GenericForeignKey('content_type_abstract',
-                                       'object_id_abstract')
+                                                'object_id_abstract')
 
+    # GR fields
     generic_relation_abstract = GenericRelation(Relation)
 
 
 class BasePerson(AbstractPerson):
+    # DATA fields
     data_base = models.CharField(max_length=10)
-    fk_base = models.ForeignKey(Relation,
-                                related_name='fk_base_rel')
-    m2m_base = models.ManyToManyField(Relation,
-                                      related_name='m2m_base_rel')
+    fk_base = models.ForeignKey(Relation, related_name='fk_base_rel')
 
+    # M2M fields
+    m2m_base = models.ManyToManyField(Relation, related_name='m2m_base_rel')
+    friends = models.ManyToManyField('self', related_name='friends',
+                                     symmetrical=True)
+    following = models.ManyToManyField('self', related_name='followers',
+                                       symmetrical=False)
+
+    # VIRTUAL fields
     data_not_concrete_base = models.ForeignObject(
         Relation,
         from_fields=['base_non_concrete_id'], to_fields=['id'],
         related_name='fo_base_rel'
     )
 
-    friends = models.ManyToManyField(
-        'self', related_name='friends', symmetrical=True)
-    following = models.ManyToManyField(
-        'self', related_name='followers', symmetrical=False)
-
+    # GFK fields
     content_type_base = models.ForeignKey(ContentType,
                                           related_name='+')
     object_id_base = models.PositiveIntegerField()
     content_object_base = GenericForeignKey('content_type_base',
-                                       'object_id_base')
+                                            'object_id_base')
+
+    # GR fields
     generic_relation_base = GenericRelation(Relation)
 
 
 class Person(BasePerson):
+    # DATA fields
     data_inherited = models.CharField(max_length=10)
-    fk_inherited = models.ForeignKey(Relation,
-                                     related_name='fk_concrete_rel')
-    m2m_inherited = models.ManyToManyField(Relation,
-                                     related_name='m2m_concrete_rel')
+    fk_inherited = models.ForeignKey(Relation, related_name='fk_concrete_rel')
 
+    # M2M Fields
+    m2m_inherited = models.ManyToManyField(Relation,
+                                           related_name='m2m_concrete_rel')
+
+    # VIRTUAL fields
     data_not_concrete_inherited = models.ForeignObject(
         Relation,
         from_fields=['model_non_concrete_id'], to_fields=['id'],
         related_name='fo_concrete_rel'
     )
 
-    content_type_concrete = models.ForeignKey(ContentType,
-                                          related_name='+')
+    # GFK fields
+    content_type_concrete = models.ForeignKey(ContentType, related_name='+')
     object_id_concrete = models.PositiveIntegerField()
     content_object_concrete = GenericForeignKey('content_type_concrete',
-                                       'object_id_concrete')
+                                                'object_id_concrete')
+
+    # GR fields
     generic_relation_concrete = GenericRelation(Relation)
 
 
@@ -90,6 +102,7 @@ class ProxyPerson(Person):
 
 # Models with FK pointing to Person
 class Relating(models.Model):
+
     # ForeignKey to BasePerson
     baseperson = models.ForeignKey(BasePerson,
                                related_name='relating_baseperson')
