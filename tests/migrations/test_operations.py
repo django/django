4f039b9990f98a5a0dc7c1995412b8253f05dbc2
Andrew Godwin <andrew@aeracode.org>
2014-06-16 00:27:14 -0700

    Fixed #22844: Duplicate SQL for SQLite FKs

--- tests/migrations/test_operations.py
+++ tests/migrations/test_operations.py
@@ -156,6 +156,49 @@ class OperationTests(MigrationTestBase):
         self.assertEqual(len(definition[2]), 0)
         self.assertEqual(definition[1][0], "Pony")
 
+    def test_create_model_with_unique_after(self):
+        """
+        Tests the CreateModel operation directly followed by an
+        AlterUniqueTogether (bug #22844 - sqlite remake issues)
+        """
+        operation1 = migrations.CreateModel(
+            "Pony",
+            [
+                ("id", models.AutoField(primary_key=True)),
+                ("pink", models.IntegerField(default=1)),
+            ],
+        )
+        operation2 = migrations.CreateModel(
+            "Rider",
+            [
+                ("id", models.AutoField(primary_key=True)),
+                ("number", models.IntegerField(default=1)),
+                ("pony", models.ForeignKey("test_crmoua.Pony")),
+            ],
+        )
+        operation3 = migrations.AlterUniqueTogether(
+            "Rider",
+            [
+                ("number", "pony"),
+            ],
+        )
+        # Test the database alteration
+        project_state = ProjectState()
+        self.assertTableNotExists("test_crmoua_pony")
+        self.assertTableNotExists("test_crmoua_rider")
+        with connection.schema_editor() as editor:
+            new_state = project_state.clone()
+            operation1.state_forwards("test_crmoua", new_state)
+            operation1.database_forwards("test_crmoua", editor, project_state, new_state)
+            project_state, new_state = new_state, new_state.clone()
+            operation2.state_forwards("test_crmoua", new_state)
+            operation2.database_forwards("test_crmoua", editor, project_state, new_state)
+            project_state, new_state = new_state, new_state.clone()
+            operation3.state_forwards("test_crmoua", new_state)
+            operation3.database_forwards("test_crmoua", editor, project_state, new_state)
+        self.assertTableExists("test_crmoua_pony")
+        self.assertTableExists("test_crmoua_rider")
+
     def test_create_model_m2m(self):
         """
         Test the creation of a model with a ManyToMany field and the
