5c94cd6b3e8752332a0ca44af16a0a444554220e
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2014-06-24 15:07:36 +0200

    admin: make add related behaviour customizable
    Decouple behaviour and style from the RelatedFieldWidgetWrapper
    widget so it's easier to customize.
    This makes life easier for people that want to say use a modal
    or just style it differently.
    
    While at it in change_form.html start consolidating document ready
    event listeners in one place.

--- django/contrib/admin/widgets.py
+++ django/contrib/admin/widgets.py
@@ -265,19 +265,17 @@ class RelatedFieldWidgetWrapper(forms.Widget):
 
     def render(self, name, value, *args, **kwargs):
         from django.contrib.admin.views.main import TO_FIELD_VAR
-        rel_to = self.rel.to
-        info = (rel_to._meta.app_label, rel_to._meta.model_name)
         self.widget.choices = self.choices
         output = [self.widget.render(name, value, *args, **kwargs)]
         if self.can_add_related:
+            rel_to = self.rel.to
+            info = (rel_to._meta.app_label, rel_to._meta.model_name)
             related_url = reverse('admin:%s_%s_add' % info, current_app=self.admin_site.name)
             url_params = '?%s=%s' % (TO_FIELD_VAR, self.rel.get_related_field().name)
             # TODO: "add_id_" is hard-coded here. This should instead use the
             # correct API to determine the ID dynamically.
-            output.append('<a href="%s%s" class="add-another" id="add_id_%s" onclick="return showAddAnotherPopup(this);"> '
-                          % (related_url, url_params, name))
-            output.append('<img src="%s" width="10" height="10" alt="%s"/></a>'
-                          % (static('admin/img/icon_addlink.gif'), _('Add Another')))
+            output.append('<a href="%s%s" class="add-another" id="add_id_%s" title="%s"></a>'
+                          % (related_url, url_params, name, _('Add Another')))
         return mark_safe(''.join(output))
 
     def build_attrs(self, extra_attrs=None, **kwargs):
