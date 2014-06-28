d75b49e5535a3e03a2a8c221404a7617ad652395
Ben Davis <bendavis78@gmail.com>
2014-06-28 04:37:19 -0500

    Fixed #22920 - AppConfig.create swallows informative exceptions during import time

---
+++ tests/apps/misbehaving_module/bar.py
@@ -0,0 +1,5 @@
+from apps import Trouble
+
+
+def cause_trouble():
+    raise Trouble("No one will ever know...")
---
+++ tests/apps/misbehaving_module/foo.py
@@ -0,0 +1,5 @@
+from apps.misbehaving_module import bar
+
+WHO_DUNNIT = "don't look at me"
+
+bar.cause_trouble()
