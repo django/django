8d25f28692a750b6b7efe847fabea949eeb9e048
Daniel Pyrathon <pirosb3@gmail.com>
2014-07-07 16:27:11 +0200

    restored old test and added sorted for correct key output order

--- tests/many_to_one/tests.py
+++ tests/many_to_one/tests.py
@@ -436,11 +436,10 @@ class ManyToOneTests(TestCase):
         expected_message = "Cannot resolve keyword 'notafield' into field. Choices are: %s"
 
         self.assertRaisesMessage(FieldError,
-                                 expected_message % ', '.join(Reporter._meta.get_all_field_names()),
+                                 expected_message % ', '.join(sorted(Reporter._meta.get_all_field_names())),
                                  Article.objects.values_list,
                                  'reporter__notafield')
-        # This test relies on non-deterministic dict order
-        #self.assertRaisesMessage(FieldError,
-                                 #expected_message % ', '.join(['EXTRA'] + Article._meta.get_all_field_names()),
-                                 #Article.objects.extra(select={'EXTRA': 'EXTRA_SELECT'}).values_list,
-                                 #'notafield')
+        self.assertRaisesMessage(FieldError,
+                                 expected_message % ', '.join(['EXTRA'] + sorted(Article._meta.get_all_field_names())),
+                                 Article.objects.extra(select={'EXTRA': 'EXTRA_SELECT'}).values_list,
+                                 'notafield')
