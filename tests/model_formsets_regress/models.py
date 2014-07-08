685582940bf4dfb5caf09bd8a73e6f74745190a8
Tim Graham <timograham@gmail.com>
2014-07-08 15:58:14 -0400

    [1.6.x] Fixed #13794 -- Fixed to_field usage in BaseInlineFormSet.
    Thanks sebastien at clarisys.fr for the report and gautier
    for the patch.
    
    Backport of 5e2c4a4bd1 from master

--- tests/model_formsets_regress/models.py
+++ tests/model_formsets_regress/models.py
@@ -10,6 +10,15 @@ class UserSite(models.Model):
     user = models.ForeignKey(User, to_field="username")
     data = models.IntegerField()
 
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, unique=True, to_field="username")
+    about = models.TextField()
+
+class ProfileNetwork(models.Model):
+    profile = models.ForeignKey(UserProfile, to_field="user")
+    network = models.IntegerField()
+    identifier = models.IntegerField()
+
 class Place(models.Model):
     name = models.CharField(max_length=50)
 
