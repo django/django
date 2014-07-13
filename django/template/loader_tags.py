38c3cb1eef6f505456102e986d955411ef0b3197
Anubhav Joshi <anubhav9042@gmail.com>
2014-07-13 14:28:36 +0530

    Fixed #21707 -- Added helpful error message when using {{ block.super }} in base template.
    Thanks mitar for the suggestion.

--- django/template/loader_tags.py
+++ django/template/loader_tags.py
@@ -68,6 +68,11 @@ class BlockNode(Node):
         return result
 
     def super(self):
+        if not hasattr(self, 'context'):
+            raise TemplateSyntaxError(
+                "'%s' object has no attribute 'context'. Did you use "
+                "{{ block.super }} in a base template?" % self.__class__.__name__
+            )
         render_context = self.context.render_context
         if (BLOCK_CONTEXT_KEY in render_context and
                 render_context[BLOCK_CONTEXT_KEY].get_block(self.name) is not None):
