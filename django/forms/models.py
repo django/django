aa9c45c2e425662d980cca82974da0986fdbe406
Ramiro Morales <cramm0@gmail.com>
2014-07-14 21:09:38 -0300

    [1.4.x] Revert "Fixed #13794 -- Fixed to_field usage in BaseInlineFormSet."
    This reverts commit b44519072e8a0ef56a0ae9e6e4a1fb04273eb0eb.
    
    stable/1.4.x branch is in security-fixes-only mode.

--- django/forms/models.py
+++ django/forms/models.py
@@ -712,12 +712,8 @@ class BaseInlineFormSet(BaseModelFormSet):
             # Remove the foreign key from the form's data
             form.data[form.add_prefix(self.fk.name)] = None
 
-        # Set the fk value here so that the form can do its validation.
-        fk_value = self.instance.pk
-        if self.fk.rel.field_name != self.fk.rel.to._meta.pk.name:
-            fk_value = getattr(self.instance, self.fk.rel.field_name)
-            fk_value = getattr(fk_value, 'pk', fk_value)
-        setattr(form.instance, self.fk.get_attname(), fk_value)
+        # Set the fk value here so that the form can do it's validation.
+        setattr(form.instance, self.fk.get_attname(), self.instance.pk)
         return form
 
     @classmethod
