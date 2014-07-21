e2220c1c086675e5c3a87d2f09cb71e6562ad733
Andrew Godwin <andrew@aeracode.org>
2014-07-21 11:36:34 +0100

    [1.7.x] Fixed #23014: Renaming not atomic with unique together

--- django/db/migrations/autodetector.py
+++ django/db/migrations/autodetector.py
@@ -791,7 +791,18 @@ class MigrationAutodetector(object):
             old_model_name = self.renamed_models.get((app_label, model_name), model_name)
             old_model_state = self.from_state.models[app_label, old_model_name]
             new_model_state = self.to_state.models[app_label, model_name]
-            if old_model_state.options.get(option_name) != new_model_state.options.get(option_name):
+            # We run the old version through the field renames to account for those
+            if old_model_state.options.get(option_name) is None:
+                old_value = None
+            else:
+                old_value = [
+                    [
+                        self.renamed_fields.get((app_label, model_name, n), n)
+                        for n in unique
+                    ]
+                    for unique in old_model_state.options[option_name]
+                ]
+            if old_value != new_model_state.options.get(option_name):
                 self.add_operation(
                     app_label,
                     operation(
