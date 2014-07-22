4fc0863ff74a80c2b8441da7812654f344e9c2be
Christian Schmitt <c.schmitt@envisia.de>
2014-07-22 22:24:34 +0200

    Fixed #23078 -- Removed the interactive=True variable I removed the interactive=True variable since it blocked the kwargs kwargs.interactive was always be False due to the fact that there was already a named interactive variable

--- django/contrib/contenttypes/management.py
+++ django/contrib/contenttypes/management.py
@@ -6,7 +6,7 @@ from django.utils import six
 from django.utils.six.moves import input
 
 
-def update_contenttypes(app_config, verbosity=2, interactive=True, using=DEFAULT_DB_ALIAS, **kwargs):
+def update_contenttypes(app_config, verbosity=2, using=DEFAULT_DB_ALIAS, **kwargs):
     """
     Creates content types for models in the given app, removing any model
     entries that no longer have a matching model class.
@@ -60,7 +60,7 @@ def update_contenttypes(app_config, verbosity=2, interactive=True, using=DEFAULT
 
     # Confirm that the content type is stale before deletion.
     if to_remove:
-        if kwargs.get('interactive', False):
+        if kwargs.get('interactive', True):
             content_type_display = '\n'.join(
                 '    %s | %s' % (ct.app_label, ct.model)
                 for ct in to_remove
