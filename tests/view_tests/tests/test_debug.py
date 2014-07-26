355c37dc1800904b58c2778738c00b29fce2b6c0
Hiroki KIYOHARA <hirokiky@gmail.com>
2014-07-26 12:01:44 +0200

    Added test for the #23070 problem

--- tests/view_tests/tests/test_debug.py
+++ tests/view_tests/tests/test_debug.py
@@ -660,6 +660,21 @@ class ExceptionReporterFilterTests(TestCase, ExceptionReportTestMixin):
             response = self.client.get('/raises500/')
             self.assertNotContains(response, "This should not be displayed", status_code=500)
 
+    def test_callable_settings_forbidding_to_set_attributes(self):
+        """
+        Callable settings which forbid to set attributes should not break
+        the debug page (#23070).
+        """
+        class CallableSettingWithSlots(object):
+            __slots__ = []
+
+            def __call__(self):
+                return "This should not be displayed"
+
+        with self.settings(DEBUG=True, WITH_SLOTS=CallableSettingWithSlots()):
+            response = self.client.get('/raises500/')
+            self.assertNotContains(response, "This should not be displayed", status_code=500)
+
     def test_dict_setting_with_non_str_key(self):
         """
         A dict setting containing a non-string key should not break the
