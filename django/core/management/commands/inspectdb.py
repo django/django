cd1dc533cf7465028f67dfc9f943ab2bacf8ec37
Christoph Heer <Christoph.Heer@gmail.com>
2014-07-26 13:21:52 +0200

    Fixed #23067 -- Updated docs to use django-admin

--- django/core/management/commands/inspectdb.py
+++ django/core/management/commands/inspectdb.py
@@ -43,7 +43,7 @@ class Command(BaseCommand):
             yield "#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table"
             yield "# Feel free to rename the models, but don't rename db_table values or field names."
             yield "#"
-            yield "# Also note: You'll have to insert the output of 'django-admin.py sqlcustom [app_label]'"
+            yield "# Also note: You'll have to insert the output of 'django-admin sqlcustom [app_label]'"
             yield "# into your database."
             yield "from __future__ import unicode_literals"
             yield ''
