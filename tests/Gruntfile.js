750f49d9e4c51da2632930d4c2cee324c4b81e99
Nick Sandford <nick@sandford.id.au>
2014-08-01 01:24:18 +0100

    Finished timeparse tests, added istanbul for coverage

--- tests/Gruntfile.js
+++ tests/Gruntfile.js
@@ -2,6 +2,30 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     qunit: {
+      options: {
+        '--web-security': 'no',
+        coverage: {
+          disposeCollector: true,
+          src: [
+            '../django/contrib/admin/static/admin/js/admin/DateTimeShortcuts.js',
+            '../django/contrib/admin/static/admin/js/admin/RelatedObjectLookups.js',
+            '../django/contrib/admin/static/admin/js/SelectBox.js',
+            '../django/contrib/admin/static/admin/js/SelectFilter2.js',
+            '../django/contrib/admin/static/admin/js/actions.js',
+            '../django/contrib/admin/static/admin/js/calendar.js',
+            '../django/contrib/admin/static/admin/js/collapse.js',
+            '../django/contrib/admin/static/admin/js/core.js',
+            '../django/contrib/admin/static/admin/js/inlines.js',
+            '../django/contrib/admin/static/admin/js/prepopulate.js',
+            '../django/contrib/admin/static/admin/js/timeparse.js',
+            '../django/contrib/admin/static/admin/js/urlify.js'
+          ],
+          instrumentedFiles: 'temp/',
+          htmlReport: 'report/coverage',
+          coberturaReport: 'report/',
+          linesThresholdPct: 85
+        }
+      },
       all: ['javascript/*.html']
     },
     jshint: {
@@ -23,7 +47,7 @@ module.exports = function(grunt) {
     },
   });
 
-  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-qunit-istanbul');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.registerTask('default', ['qunit']);
 };
--- tests/javascript/admin/timeparse.js
+++ tests/javascript/admin/timeparse.js
@@ -11,10 +11,17 @@ test('parseTimeString', function(assert) {
     time("9.00", "09:00");
     time("3 am", "03:00");
     time("3 a.m.", "03:00");
+    time("12 am", "00:00");
+    time("11 am", "11:00");
+    time("12 pm", "12:00");
     time("3am", "03:00");
     time("3.30 am", "03:30");
     time("3:15 a.m.", "03:15");
     time("3.00am", "03:00");
+    time("12.00am", "00:00");
+    time("11.00am", "11:00");
+    time("12.00pm", "12:00");
     time("noon", "12:00");
     time("midnight", "00:00");
+    time("something else", "something else");
 });
