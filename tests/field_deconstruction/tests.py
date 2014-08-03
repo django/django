74325339327e5b8a2480aee7f482bc71f73c71f9
Baptiste Mispelon <bmispelon@gmail.com>
2014-08-03 14:02:21 +0200

    Added missing logic to TimeField.deconstruct().
    If auto_now or auto_now_add was used then the serialized
    field in the migration contained unnecessary `editable` and
    `blank` arguments.

--- tests/field_deconstruction/tests.py
+++ tests/field_deconstruction/tests.py
@@ -336,6 +336,23 @@ class FieldDeconstructionTests(TestCase):
         self.assertEqual(args, [])
         self.assertEqual(kwargs, {})
 
+    def test_time_field(self):
+        field = models.TimeField()
+        name, path, args, kwargs = field.deconstruct()
+        self.assertEqual(path, "django.db.models.TimeField")
+        self.assertEqual(args, [])
+        self.assertEqual(kwargs, {})
+
+        field = models.TimeField(auto_now=True)
+        name, path, args, kwargs = field.deconstruct()
+        self.assertEqual(args, [])
+        self.assertEqual(kwargs, {'auto_now': True})
+
+        field = models.TimeField(auto_now_add=True)
+        name, path, args, kwargs = field.deconstruct()
+        self.assertEqual(args, [])
+        self.assertEqual(kwargs, {'auto_now_add': True})
+
     def test_url_field(self):
         field = models.URLField()
         name, path, args, kwargs = field.deconstruct()
