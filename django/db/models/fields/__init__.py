4afe43c82677125fdcff872b0396c54edcbfaea2
Baptiste Mispelon <bmispelon@gmail.com>
2014-08-03 14:02:21 +0200

    Added missing logic to TimeField.deconstruct().
    If auto_now or auto_now_add was used then the serialized
    field in the migration contained unnecessary `editable` and
    `blank` arguments.

--- django/db/models/fields/__init__.py
+++ django/db/models/fields/__init__.py
@@ -2083,6 +2083,9 @@ class TimeField(DateTimeCheckMixin, Field):
             kwargs["auto_now"] = self.auto_now
         if self.auto_now_add is not False:
             kwargs["auto_now_add"] = self.auto_now_add
+        if self.auto_now or self.auto_now_add:
+            del kwargs['blank']
+            del kwargs['editable']
         return name, path, args, kwargs
 
     def get_internal_type(self):
