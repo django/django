3993d31eae51413f5d4464024bb60eeee96b4bbe
Pavel Shpilev <p.shpilev@gmail.com>
2014-08-05 20:07:22 +1000

    Fixed #9893 -- Filename could exceed max_length on collision
    Passing max_length argument into the Storage's save() and
    get_available_name() methods.
    Rewriting get_available_name() so filenames do not exceed max_length
    when there is a collision.
    Adding test for the case.

--- django/core/files/storage.py
+++ django/core/files/storage.py
@@ -75,7 +75,6 @@ class Storage(object):
         while self.exists(name):
             # file_ext includes the dot.
             name = os.path.join(dir_name, "%s_%s%s" % (file_root, get_random_string(7), file_ext))
-
         return name
 
     def path(self, name):
