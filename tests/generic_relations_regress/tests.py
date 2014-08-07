5e4988fbe78ff83d8ed1518ead81942b7333eee6
Jacob Haslehurst <jacob@haslehurst.net>
2014-08-07 16:06:44 +1000

    Fixed #22982 -- Added GenericRelatedObjectManager.__str__. to prevent crash.
    Thanks bendavis78 for the report.
    
    Forwardport of 29585e9b6a from stable/1.7.x

--- tests/generic_relations_regress/tests.py
+++ tests/generic_relations_regress/tests.py
@@ -260,3 +260,7 @@ class GenericRelationTests(TestCase):
         # where the pre_delete signal should fire and prevent deletion.
         with self.assertRaises(ProtectedError):
             related.delete()
+
+    def test_ticket_22982(self):
+        place = Place.objects.create(name='My Place')
+        self.assertIn('GenericRelatedObjectManager', str(place.links))
