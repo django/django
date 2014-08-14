d871949c33290213f3eb0df32b35d76d9debe836
Ramiro Morales <cramm0@gmail.com>
2014-08-14 08:16:15 -0300

    Fixed #23291 -- Don't add Unicode varname/value to environment.
    Windows doesn't like that and it causes 114 failures in the
    admin_scripts tests with Python 2.x.

--- django/core/management/base.py
+++ django/core/management/base.py
@@ -380,7 +380,7 @@ class BaseCommand(object):
         if options.get('no_color'):
             self.style = no_style()
             self.stderr = OutputWrapper(options.get('stderr', sys.stderr))
-            os.environ["DJANGO_COLORS"] = "nocolor"
+            os.environ[str("DJANGO_COLORS")] = str("nocolor")
         else:
             self.stderr = OutputWrapper(options.get('stderr', sys.stderr), self.style.ERROR)
 
