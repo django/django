d8306650071369ee9d7cad71120d13c22e900a10
Ian Foote <ian@incuna.com>
2014-08-21 12:53:22 +0100

    [1.7.x] Fixed #23333 -- Made urlsafe_base64_decode() return proper type on Python 3.
    Backport of 03d89168a2 from master

--- django/utils/http.py
+++ django/utils/http.py
@@ -10,7 +10,7 @@ from binascii import Error as BinasciiError
 from email.utils import formatdate
 
 from django.utils.datastructures import MultiValueDict
-from django.utils.encoding import force_str, force_text
+from django.utils.encoding import force_bytes, force_str, force_text
 from django.utils.functional import allow_lazy
 from django.utils import six
 from django.utils.six.moves.urllib.parse import (
@@ -224,7 +224,7 @@ def urlsafe_base64_decode(s):
     Decodes a base64 encoded string, adding back any trailing equal signs that
     might have been stripped.
     """
-    s = s.encode('utf-8')  # base64encode should only return ASCII.
+    s = force_bytes(s)
     try:
         return base64.urlsafe_b64decode(s.ljust(len(s) + len(s) % 4, b'='))
     except (LookupError, BinasciiError) as e:
