9f9721041a0ff81e00ae91c08913a429104cbc44
Gavin Wahl <gwahl@fusionbox.com>
2014-08-22 10:31:26 -0600

    Fixed #23346 -- Fixed lazy() to lookup methods on the real object, not resultclasses.
    Co-Authored-By: Rocky Meza <rmeza@fusionbox.com>

--- tests/utils_tests/test_functional.py
+++ tests/utils_tests/test_functional.py
@@ -22,6 +22,20 @@ class FunctionalTestCase(unittest.TestCase):
         t = lazy(lambda: Klazz(), Klazz)()
         self.assertIn('base_method', dir(t))
 
+    def test_lazy_base_class_override(self):
+        """Test that lazy finds the correct (overridden) method implementation"""
+
+        class Base(object):
+            def method(self):
+                return 'Base'
+
+        class Klazz(Base):
+            def method(self):
+                return 'Klazz'
+
+        t = lazy(lambda: Klazz(), Base)()
+        self.assertEqual(t.method(), 'Klazz')
+
     def test_lazy_property(self):
 
         class A(object):
