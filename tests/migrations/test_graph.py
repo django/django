1b2740105bf97374ade888e8781427a9f2dcaee4
Raffaele Salmaso <raffaele@salmaso.org>
2014-08-23 11:37:25 +0200

    Fixed #23352 -- Added tests for MigrationGraph.{forwards,backwards}_plan

--- tests/migrations/test_graph.py
+++ tests/migrations/test_graph.py
@@ -133,3 +133,16 @@ class GraphTests(TestCase):
             CircularDependencyError,
             graph.forwards_plan, ("app_a", "0003"),
         )
+
+    def test_plan_invalid_node(self):
+        """
+        Tests for forwards/backwards_plan of nonexistent node.
+        """
+        graph = MigrationGraph()
+        message = "Node ('app_b', '0001') not a valid node"
+
+        with self.assertRaisesMessage(ValueError, message):
+            graph.forwards_plan(("app_b", "0001"))
+
+        with self.assertRaisesMessage(ValueError, message):
+            graph.backwards_plan(("app_b", "0001"))
