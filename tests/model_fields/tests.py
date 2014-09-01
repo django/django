9a1bcd5aaa265045704ef6567b7ba56be983ed31
Daniel Pyrathon <pirosb3@gmail.com>
2014-09-01 12:10:23 +0200

    refactored fields to also include vitual fields in tests

--- tests/model_fields/tests.py
+++ tests/model_fields/tests.py
@@ -659,11 +659,15 @@ class BinaryFieldTests(test.TestCase):
 class FieldFlagsTests(test.TestCase):
 
     def setUp(self):
-        self.fields = AllFieldsModel._meta.fields
+        self.fields = AllFieldsModel._meta.fields + AllFieldsModel._meta.virtual_fields
 
     def test_each_field_should_have_a_concrete_attribute(self):
         self.assertTrue(all(f.concrete.__class__ == bool
                         for f in self.fields))
+
+    #def test_each_field_should_have_an_editable_attribute(self):
+        #self.assertTrue(all(f.concrete.__class__ == bool
+                        #for f in self.fields))
 
     #def test_each_field_should_have_an_editable_attribute(self):
         #for f in ALL_FIELDS:
