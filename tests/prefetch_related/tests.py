c5c8f025ce65f8182db2a65ebd95f3b6c259c25e
Aymeric Augustin <aymeric.augustin@oscaro.com>
2014-09-08 21:16:59 +0200

    Avoid the word "stupid".
    There's always a better word.

--- tests/prefetch_related/tests.py
+++ tests/prefetch_related/tests.py
@@ -683,7 +683,7 @@ class GenericRelationTests(TestCase):
     def test_prefetch_GFK(self):
         TaggedItem.objects.create(tag="awesome", content_object=self.book1)
         TaggedItem.objects.create(tag="great", content_object=self.reader1)
-        TaggedItem.objects.create(tag="stupid", content_object=self.book2)
+        TaggedItem.objects.create(tag="outstanding", content_object=self.book2)
         TaggedItem.objects.create(tag="amazing", content_object=self.reader3)
 
         # 1 for TaggedItem table, 1 for Book table, 1 for Reader table
