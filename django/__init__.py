74c7e358356f9b71bf6ec95f01552c890ab0a4a8
Tim Graham <timograham@gmail.com>
2014-09-09 14:26:03 -0400

    Fixed #23442 -- Added django.__version__ per PEP 396.
    Thanks abhillman for the suggestion and Claude Paroz for review.

--- django/__init__.py
+++ django/__init__.py
@@ -2,10 +2,12 @@ VERSION = (1, 8, 0, 'alpha', 0)
 
 
 def get_version(*args, **kwargs):
-    # Don't litter django/__init__.py with all the get_version stuff.
-    # Only import if it's actually called.
+    # Avoid circular import
     from django.utils.version import get_version
     return get_version(*args, **kwargs)
+
+
+__version__ = get_version(VERSION)
 
 
 def setup():
