f8033df6ed510a30e085d2f75a7c9f18a218d0c4
valtron <valtron2000@gmail.com>
2014-09-15 10:17:28 -0600

    Fixed #23474 -- Prevented migrating backwards from unapplying the wrong migrations.

---
+++ tests/migrations/test_migrations_backwards_deps_1/0001_initial.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    operations = []
---
+++ tests/migrations2/test_migrations_backwards_deps_2/0001_initial.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [('migrations', '0001_initial')]
+    operations = [
+        migrations.CreateModel(
+            "Example",
+            [
+                ("id", models.AutoField(primary_key=True)),
+            ],
+        ),
+    ]
