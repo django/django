737d53a95db49a38f8933f0a27e4127d6735051b
Aymeric Augustin <aymeric.augustin@m4x.org>
2014-09-28 13:32:26 +0200

    Fixed #23436 -- Made BASE_DIR absolute in settings template.
    This ensures consistency between Python 3.4+ and earlier versions.
    
    Thanks Harry Percival for the report.

--- django/conf/project_template/project_name/settings.py
+++ django/conf/project_template/project_name/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
