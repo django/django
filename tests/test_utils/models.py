c7b5883c781608f20f42ebedbfc260ea082709d6
Tim Graham <timograham@gmail.com>
2014-09-30 13:24:20 -0400

    Fixed flake8 warnings.

--- tests/test_utils/models.py
+++ tests/test_utils/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
+
 @python_2_unicode_compatible
 class Car(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
 
 @python_2_unicode_compatible
 class Person(models.Model):
@@ -15,6 +17,7 @@ class Person(models.Model):
 
     def __str__(self):
         return self.name
+
 
 @python_2_unicode_compatible
 class PossessedCar(models.Model):
