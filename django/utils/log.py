86429d765af46653e566ae05198f431662be6973
Tim Graham <timograham@gmail.com>
2014-10-06 16:07:20 -0400

    Fixed #23593 -- Fixed crash in AdminEmailHandler with non-ASCII characters in request.
    Thanks edevil for the report and Simon Charette for review.

--- django/utils/log.py
+++ django/utils/log.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 import logging
 import sys
 import warnings
@@ -6,6 +8,7 @@ from django.conf import settings
 from django.core import mail
 from django.core.mail import get_connection
 from django.utils.deprecation import RemovedInNextVersionWarning
+from django.utils.encoding import force_text
 from django.utils.module_loading import import_string
 from django.views.debug import ExceptionReporter, get_exception_reporter_filter
 
@@ -106,7 +109,7 @@ class AdminEmailHandler(logging.Handler):
                 record.getMessage()
             )
             filter = get_exception_reporter_filter(request)
-            request_repr = '\n{0}'.format(filter.get_request_repr(request))
+            request_repr = '\n{0}'.format(force_text(filter.get_request_repr(request)))
         except Exception:
             subject = '%s: %s' % (
                 record.levelname,
