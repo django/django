593cabbf0ff72908b8a12679d2704237243b9725
Daniel Pyrathon <pirosb3@gmail.com>
2014-10-08 19:46:56 +0200

    Refactored imports

--- django/db/models/base.py
+++ django/db/models/base.py
@@ -14,9 +14,8 @@ from django.core.exceptions import (ObjectDoesNotExist,
     MultipleObjectsReturned, FieldError, ValidationError, NON_FIELD_ERRORS)
 from django.db import (router, connections, transaction, DatabaseError,
     DEFAULT_DB_ALIAS, DJANGO_VERSION_PICKLE_KEY)
-from django.db.models import FieldDoesNotExist
 from django.db.models.deletion import Collector
-from django.db.models.fields import AutoField
+from django.db.models.fields import AutoField, FieldDoesNotExist
 from django.db.models.fields.related import (ForeignObjectRel, ManyToOneRel,
     OneToOneField, add_lazy_relation)
 from django.db.models.manager import ensure_default_manager
