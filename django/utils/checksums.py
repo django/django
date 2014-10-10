443dc8fdac0a5d29ec80543717e98f4df738b7ce
Jaap Roes <jaap@eight.nl>
2014-10-10 12:40:13 +0200

    Fixed #23613 -- Deprecated django.utils.checksums

--- django/utils/checksums.py
+++ django/utils/checksums.py
@@ -4,7 +4,16 @@ Common checksum routines.
 
 __all__ = ['luhn']
 
+import warnings
+
 from django.utils import six
+from django.utils.deprecation import RemovedInDjango20Warning
+
+warnings.warn(
+    "django.utils.checksums will be removed in Django 2.0. The "
+    "luhn() function is now included in django-localflavor 1.1+.",
+    RemovedInDjango20Warning
+)
 
 LUHN_ODD_LOOKUP = (0, 2, 4, 6, 8, 1, 3, 5, 7, 9)  # sum_of_digits(index * 2)
 
--- tests/utils_tests/test_checksums.py
+++ tests/utils_tests/test_checksums.py
@@ -1,6 +1,13 @@
 import unittest
+import warnings
 
-from django.utils import checksums
+from django.utils.deprecation import RemovedInDjango20Warning
+
+with warnings.catch_warnings():
+    warnings.filterwarnings(
+        'ignore', 'django.utils.checksums will be removed in Django 2.0.',
+        RemovedInDjango20Warning)
+    from django.utils import checksums
 
 
 class TestUtilsChecksums(unittest.TestCase):
