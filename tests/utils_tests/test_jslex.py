db63baafc0e0e37f7819495815d9e5af33b3fc17
Florian Apolloner <florian@apolloner.eu>
2014-10-14 21:55:54 +0200

    Added unicode_literals to the jslexer.
    This ensure that ''.join(c) in jslex.py always returns text.

--- tests/utils_tests/test_jslex.py
+++ tests/utils_tests/test_jslex.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Tests for jslex."""
 # originally from https://bitbucket.org/ned/jslex
+from __future__ import unicode_literals
 
 from django.test import TestCase
 from django.utils.jslex import JsLexer, prepare_js_for_gettext
@@ -10,7 +11,7 @@ class JsTokensTest(TestCase):
     LEX_CASES = [
         # ids
         ("a ABC $ _ a123", ["id a", "id ABC", "id $", "id _", "id a123"]),
-        (r"\u1234 abc\u0020 \u0065_\u0067", [r"id \u1234", r"id abc\u0020", r"id \u0065_\u0067"]),
+        ("\\u1234 abc\\u0020 \\u0065_\\u0067", ["id \\u1234", "id abc\\u0020", "id \\u0065_\\u0067"]),
         # numbers
         ("123 1.234 0.123e-3 0 1E+40 1e1 .123", ["dnum 123", "dnum 1.234", "dnum 0.123e-3", "dnum 0", "dnum 1E+40", "dnum 1e1", "dnum .123"]),
         ("0x1 0xabCD 0XABcd", ["hnum 0x1", "hnum 0xabCD", "hnum 0XABcd"]),
