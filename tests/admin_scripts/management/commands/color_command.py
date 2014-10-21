95d900fbcbc94d6239a880d86ca5152b453e03db
Loic Bistuer <loic.bistuer@gmail.com>
2014-10-22 01:11:31 +0700

    Fixed #23663 -- Initialize output streams for BaseCommand in __init__().
    This helps with testability of management commands.
    
    Thanks to trac username daveoncode for the report and to
    Tim Graham and Claude Paroz for the reviews.

--- tests/admin_scripts/management/commands/color_command.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-
-
-class Command(BaseCommand):
-    help = "Test color output"
-    requires_system_checks = False
-
-    def handle(self, **options):
-        return self.style.SQL_KEYWORD('BEGIN')
