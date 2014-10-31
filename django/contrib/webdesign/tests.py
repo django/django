c0c1bb9e64d7e1cea50dc4818ffcf229b568578b
Aymeric Augustin <aymeric.augustin@m4x.org>
2014-10-31 11:38:53 +0100

    Avoided using private API get_template_from_string.

--- django/contrib/webdesign/tests.py
+++ django/contrib/webdesign/tests.py
@@ -3,12 +3,12 @@ from __future__ import unicode_literals
 
 import unittest
 
-from django.template import loader, Context
+from django.template import Context, Template
 
 
 class WebdesignTest(unittest.TestCase):
 
     def test_lorem_tag(self):
-        t = loader.get_template_from_string("{% load webdesign %}{% lorem 3 w %}")
+        t = Template("{% load webdesign %}{% lorem 3 w %}")
         self.assertEqual(t.render(Context({})),
                          'lorem ipsum dolor')
