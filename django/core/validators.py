7f5ddfe159088c85d0a051365846a8546e08aa31
Danilo Bargen <mail@dbrgn.ch>
2014-11-03 18:01:31 +0100

    Fixed #20003 -- Improved and extended URLValidator
    This adds support for `user:password` in URLValidator supported URLs.
    
    Additionally, the test suite has been improved by adding test URLs from
    http://mathiasbynens.be/demo/url-regex (with a few adjustments, like
    allowing local and reserved IPs).
    
    The previous URL validation RegEx failed this test suite on 13
    occasions, so I also updated the URLValidator based on
    https://gist.github.com/dperini/729294 to correctly validate all URLs in
    the test suite. Among other improvements, this also adds support for
    IPv4 mapped IPv6 addresses like `::ffff:192.0.2.128`.

--- django/core/validators.py
+++ django/core/validators.py
@@ -66,14 +66,24 @@ class RegexValidator(object):
 
 @deconstructible
 class URLValidator(RegexValidator):
+    ul = '\u00a1-\uffff'  # unicode letters range (must be a unicode string, not a raw string)
     regex = re.compile(
         r'^(?:[a-z0-9\.\-]*)://'  # scheme is validated separately
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}(?<!-)\.?)|'  # domain...
-        r'localhost|'  # localhost...
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|'  # ...or ipv4
-        r'\[?[A-F0-9]*:[A-F0-9:]+\]?)'  # ...or ipv6
-        r'(?::\d+)?'  # optional port
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+        r'(?:\S+(?::\S*)?@)?'  # user:pass authentication
+        r'(?:'
+            r'(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}'  # IPv4
+        r'|'
+            r'\[[0-9a-f:\.]+\]'  # IPv6 (simple regex, validated later)
+        r'|'
+            r'[a-z' + ul + r'0-9](?:[a-z' + ul + r'0-9-]*[a-z' + ul + r'0-9])?'  # host name
+            r'(?:\.[a-z' + ul + r'0-9]+(?:[a-z' + ul + r'0-9-]*[a-z' + ul + r'0-9]+)*)*'  # domain name
+            r'\.[a-z' + ul + r']{2,}\.?'  # TLD
+        r'|'
+            r'localhost'  # localhost
+        r')'
+        r'(?::\d{2,5})?'  # port
+        r'(?:[/?#][^\s]*)?'  # resource path
+        r'$', re.IGNORECASE)
     message = _('Enter a valid URL.')
     schemes = ['http', 'https', 'ftp', 'ftps']
 
@@ -105,6 +115,14 @@ class URLValidator(RegexValidator):
             else:
                 raise
         else:
+            # Now verify IPv6 in the netloc part
+            host_match = re.search(r'^\[(.+)\](?::\d{2,5})?$', urlsplit(value).netloc)
+            if host_match:
+                potential_ip = host_match.groups()[0]
+                try:
+                    validate_ipv6_address(potential_ip)
+                except ValidationError:
+                    raise ValidationError(self.message, code=self.code)
             url = value
 
 
