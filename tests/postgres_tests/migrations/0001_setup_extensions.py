0a4d9cfde1cd1f8d60c1670aba0d41214f30b84e
Tim Graham <timograham@gmail.com>
2014-11-04 11:27:35 -0500

    Removed unused import.

--- tests/postgres_tests/migrations/0001_setup_extensions.py
+++ tests/postgres_tests/migrations/0001_setup_extensions.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib.postgres.operations import HStoreExtension
-from django.db import models, migrations
+from django.db import migrations
 
 
 class Migration(migrations.Migration):
