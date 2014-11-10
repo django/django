901c08ac90edd7241f3135f397944b57e397b6c8
Thomas Chaumeny <t.chaumeny@gmail.com>
2014-11-10 19:33:49 +0100

    Fixed #23792 -- Added test.utils.freeze_time() context manager.

--- django/test/utils.py
+++ django/test/utils.py
@@ -555,3 +555,20 @@ def reset_warning_registry():
     for mod in sys.modules.values():
         if hasattr(mod, key):
             getattr(mod, key).clear()
+
+
+@contextmanager
+def freeze_time(t):
+    """
+    Context manager to temporarily freeze time.time(). This temporarily
+    modifies the time function of the time module. Modules which import the
+    time function directly (e.g. `from time import time`) won't be affected
+    This isn't meant as a public API, but helps reduce some repetitive code in
+    Django's test suite.
+    """
+    _real_time = time.time
+    time.time = lambda: t
+    try:
+        yield
+    finally:
+        time.time = _real_time
