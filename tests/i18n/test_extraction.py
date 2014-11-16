30cc5b2f7f48c0d3d08a38ece96ff5e1572f8134
Ilja Maas <iljamaas@dreamsolution.nl>
2014-11-16 22:14:15 +0100

    Fixed test to test both domains seperately

--- tests/i18n/test_extraction.py
+++ tests/i18n/test_extraction.py
@@ -352,12 +352,21 @@ class BasicExtractorTests(ExtractorTests):
         cmd = Command()
         cmd.ignore_patterns = ['CVS', '.*', '*~', '*.pyc']
         cmd.symlinks = False
+        cmd.domain = 'django'
+        cmd.extensions = ['html', 'txt', 'py']
         cmd.verbosity = 0
         cmd.locale_paths = []
         cmd.default_locale_path = os.path.join(self.test_dir, 'locale')
         found_files = cmd.find_files(self.test_dir)
         found_exts = set([os.path.splitext(tfile.file)[1] for tfile in found_files])
-        self.assertEqual(found_exts.difference({'.py', '.html', '.txt', '.js'}), set())
+        self.assertEqual(found_exts.difference({'.py', '.html', '.txt'}), set())
+
+        cmd.extensions = ['js']
+        cmd.domain = 'djangojs'
+        found_files = cmd.find_files(self.test_dir)
+        found_exts = set([os.path.splitext(tfile.file)[1] for tfile in found_files])
+        self.assertEqual(found_exts.difference({'.js'}), set())
+
 
 class JavascriptExtractorTests(ExtractorTests):
 
