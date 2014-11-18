ffe5a0cd65be0d4364f28379a22f603d3aa931c0
Daniel Pyrathon <pirosb3@gmail.com>
2014-11-18 11:54:19 +0100

    ensure all fields have a name

--- tests/model_fields/tests.py
+++ tests/model_fields/tests.py
@@ -757,6 +757,10 @@ class FieldFlagsTests(test.TestCase):
             else:
                 self.assertFalse(field.is_reverse_object)
 
+    def test_field_names_should_always_be_available(self):
+        for field in self.fields_and_reverse_objects:
+            self.assertTrue(field.name)
+
 
 class GenericIPAddressFieldTests(test.TestCase):
     def test_genericipaddressfield_formfield_protocol(self):
