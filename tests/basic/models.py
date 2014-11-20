a29d0db4f6ddd9e2fe73f874c4be63b580f53eef
Anssi Kääriäinen <akaariai@gmail.com>
2014-11-20 16:25:49 -0500

    Added a comment for test of refs #20278.

--- tests/basic/models.py
+++ tests/basic/models.py
@@ -32,4 +32,6 @@ class SelfRef(models.Model):
                                 related_name='+')
 
     def __str__(self):
+        # This method intentionally doesn't work for all cases - part
+        # of the test for ticket #20278
         return SelfRef.objects.get(selfref=self).pk
