0ca4de30bbc82a20d91c3c097c592867023d5a9d
Jay <jay@Jays-MacBook-Pro.local>
2014-11-21 16:34:19 +0530

    Added a test for model pre/post_init signals.

--- tests/signals/tests.py
+++ tests/signals/tests.py
@@ -32,6 +32,20 @@ class BaseSignalTest(TestCase):
 
 
 class SignalTests(BaseSignalTest):
+    def test_model_pre_init_and_post_init(self):
+        data = []
+
+        def pre_init_callback(sender, args, **kwargs):
+            data.append(kwargs['kwargs'])
+        signals.pre_init.connect(pre_init_callback)
+
+        def post_init_callback(sender, instance, **kwargs):
+            data.append(instance)
+        signals.post_init.connect(post_init_callback)
+
+        p1 = Person(first_name="John", last_name="Doe")
+        self.assertEqual(data, [{}, p1])
+
     def test_save_signals(self):
         data = []
 
