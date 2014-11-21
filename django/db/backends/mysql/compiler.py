ad9a0948ac7f4a14cdd733bda8aa2dfe7f31d2f5
Collin Anderson <cmawebsite@gmail.com>
2014-11-21 11:38:02 -0500

    Fixed #23889 -- Use quote_name_unless_alias on mysql

--- django/db/backends/mysql/compiler.py
+++ django/db/backends/mysql/compiler.py
@@ -2,7 +2,8 @@ from django.db.models.sql import compiler
 
 
 class SQLCompiler(compiler.SQLCompiler):
-    def as_subquery_condition(self, alias, columns, qn):
+    def as_subquery_condition(self, alias, columns, compiler):
+        qn = compiler.quote_name_unless_alias
         qn2 = self.connection.ops.quote_name
         sql, params = self.as_sql()
         return '(%s) IN (%s)' % (', '.join('%s.%s' % (qn(alias), qn2(column)) for column in columns), sql), params
