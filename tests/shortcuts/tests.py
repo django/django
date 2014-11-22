bf1bd0fbc9d4920327ad998ad11facf842492e23
Aymeric Augustin <aymeric.augustin@m4x.org>
2014-11-22 17:52:12 +0100

    Moved tests for render shortcuts to their own app.

--- tests/shortcuts/tests.py
+++ tests/shortcuts/tests.py
@@ -4,67 +4,68 @@ from django.test import TestCase, override_settings
 @override_settings(
     TEMPLATE_CONTEXT_PROCESSORS=('django.core.context_processors.static',),
     STATIC_URL='/path/to/static/media/',
-    ROOT_URLCONF='view_tests.generic_urls',
+    ROOT_URLCONF='shortcuts.urls',
 )
 class ShortcutTests(TestCase):
 
     def test_render_to_response(self):
-        response = self.client.get('/shortcuts/render_to_response/')
+        response = self.client.get('/render_to_response/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, b'FOO.BAR..\n')
         self.assertEqual(response['Content-Type'], 'text/html; charset=utf-8')
 
     def test_render_to_response_with_request_context(self):
-        response = self.client.get('/shortcuts/render_to_response/request_context/')
+        response = self.client.get('/render_to_response/request_context/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, b'FOO.BAR../path/to/static/media/\n')
         self.assertEqual(response['Content-Type'], 'text/html; charset=utf-8')
 
     def test_render_to_response_with_content_type(self):
-        response = self.client.get('/shortcuts/render_to_response/content_type/')
+        response = self.client.get('/render_to_response/content_type/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, b'FOO.BAR..\n')
         self.assertEqual(response['Content-Type'], 'application/x-rendertest')
 
     def test_render_to_response_with_dirs(self):
-        response = self.client.get('/shortcuts/render_to_response/dirs/')
+        response = self.client.get('/render_to_response/dirs/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, b'spam eggs\n')
         self.assertEqual(response['Content-Type'], 'text/html; charset=utf-8')
 
     def test_render(self):
-        response = self.client.get('/shortcuts/render/')
+        response = self.client.get('/render/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, b'FOO.BAR../path/to/static/media/\n')
         self.assertEqual(response['Content-Type'], 'text/html; charset=utf-8')
         self.assertEqual(response.context.current_app, None)
 
     def test_render_with_base_context(self):
-        response = self.client.get('/shortcuts/render/base_context/')
+        response = self.client.get('/render/base_context/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, b'FOO.BAR..\n')
         self.assertEqual(response['Content-Type'], 'text/html; charset=utf-8')
 
     def test_render_with_content_type(self):
-        response = self.client.get('/shortcuts/render/content_type/')
+        response = self.client.get('/render/content_type/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, b'FOO.BAR../path/to/static/media/\n')
         self.assertEqual(response['Content-Type'], 'application/x-rendertest')
 
     def test_render_with_status(self):
-        response = self.client.get('/shortcuts/render/status/')
+        response = self.client.get('/render/status/')
         self.assertEqual(response.status_code, 403)
         self.assertEqual(response.content, b'FOO.BAR../path/to/static/media/\n')
 
     def test_render_with_current_app(self):
-        response = self.client.get('/shortcuts/render/current_app/')
+        response = self.client.get('/render/current_app/')
         self.assertEqual(response.context.current_app, "foobar_app")
 
     def test_render_with_dirs(self):
-        response = self.client.get('/shortcuts/render/dirs/')
+        response = self.client.get('/render/dirs/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, b'spam eggs\n')
         self.assertEqual(response['Content-Type'], 'text/html; charset=utf-8')
 
     def test_render_with_current_app_conflict(self):
-        self.assertRaises(ValueError, self.client.get, '/shortcuts/render/current_app_conflict/')
+        with self.assertRaises(ValueError):
+            self.client.get('/render/current_app_conflict/')
