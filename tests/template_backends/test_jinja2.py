023eef0e336f1fc681d6291d6ea290f5b2e82e29
Tim Graham <timograham@gmail.com>
2014-11-24 08:34:02 -0500

    Removed compatibility with Python 3.2.

--- tests/template_backends/test_jinja2.py
+++ tests/template_backends/test_jinja2.py
@@ -2,22 +2,15 @@
 # silence an ImportWarning warning on Python 2.
 from __future__ import absolute_import
 
-import sys
-
 from unittest import skipIf
 
-# Jinja2 doesn't run on Python 3.2 because it uses u-prefixed unicode strings.
-if sys.version_info[:2] == (2, 7) or sys.version_info[:2] >= (3, 3):
-    try:
-        import jinja2
-    except ImportError:
-        jinja2 = None
-        Jinja2 = None
-    else:
-        from django.template.backends.jinja2 import Jinja2
-else:
+try:
+    import jinja2
+except ImportError:
     jinja2 = None
     Jinja2 = None
+else:
+    from django.template.backends.jinja2 import Jinja2
 
 from .test_dummy import TemplateStringsTests
 
