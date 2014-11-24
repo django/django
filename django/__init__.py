0b16c1322f71eaaa4a05572d48413b3068866e97
Tim Graham <timograham@gmail.com>
2014-11-24 07:02:29 -0500

    Simplified get_version() in django/__init__.py
    Thanks Luke Plant for the report.

--- django/__init__.py
+++ django/__init__.py
@@ -1,11 +1,6 @@
+from django.utils.version import get_version
+
 VERSION = (1, 8, 0, 'alpha', 0)
-
-
-def get_version(*args, **kwargs):
-    # Avoid circular import
-    from django.utils.version import get_version
-    return get_version(*args, **kwargs)
-
 
 __version__ = get_version(VERSION)
 
