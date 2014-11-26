958d125696fc029b3b4aa9bcbe4a2c8fa613e801
Tim Graham <timograham@gmail.com>
2014-11-26 09:56:33 -0500

    Fixed #23677 -- Removed contrib.formtools

--- django/contrib/formtools/models.py
+++
@@ -1,2 +0,0 @@
-# This file is required to pretend formtools has models.
-# Otherwise test models cannot be registered.
--- django/contrib/formtools/wizard/storage/exceptions.py
+++
@@ -1,9 +0,0 @@
-from django.core.exceptions import ImproperlyConfigured
-
-
-class MissingStorage(ImproperlyConfigured):
-    pass
-
-
-class NoFileStorageConfigured(ImproperlyConfigured):
-    pass
