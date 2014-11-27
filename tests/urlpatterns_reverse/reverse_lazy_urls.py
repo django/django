1b0fcf9a47eab19babdef3f409c72244119c918a
Berker Peksag <berker.peksag@gmail.com>
2014-11-27 05:44:56 +0200

    Fixed #21587 -- Raised deprecation warnings correctly.

--- tests/urlpatterns_reverse/reverse_lazy_urls.py
+++ tests/urlpatterns_reverse/reverse_lazy_urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^redirected_to/$', empty_view, name='named-lazy-url-redirected-to'),
     url(r'^login/$', empty_view, name='some-login-page'),
     url(r'^login_required_view/$', login_required_view),
-    url(r'^redirect/$', LazyRedirectView.as_view()),
+    url(r'^redirect/$', LazyRedirectView.as_view(permanent=True)),
 ]
