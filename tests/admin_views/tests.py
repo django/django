86e16e783c3654a725d47866d9378fc825361081
Tim Graham <timograham@gmail.com>
2014-12-04 08:22:45 -0500

    Fixed #23857 -- Fixed admin crash with "save as new" and deleting inline.
    Thanks amarandon for the report.

--- tests/admin_views/tests.py
+++ tests/admin_views/tests.py
@@ -250,6 +250,23 @@ class AdminViewBasicTest(AdminViewBasicTestCase):
         response = self.client.post('/test_admin/%s/admin_views/section/1/' % self.urlbit, post_data)
         self.assertEqual(response.status_code, 302)  # redirect somewhere
 
+    def test_edit_save_as_delete_inline(self):
+        """
+        Should be able to "Save as new" while also deleting an inline.
+        """
+        post_data = self.inline_post_data.copy()
+        post_data.update({
+            '_saveasnew': 'Save+as+new',
+            "article_set-1-section": "1",
+            "article_set-2-section": "1",
+            "article_set-2-DELETE": "1",
+            "article_set-3-section": "1",
+        })
+        response = self.client.post('/test_admin/%s/admin_views/section/1/' % self.urlbit, post_data)
+        self.assertEqual(response.status_code, 302)
+        # started with 3 articles, one was deleted.
+        self.assertEqual(Section.objects.latest('id').article_set.count(), 2)
+
     def test_change_list_sorting_callable(self):
         """
         Ensure we can sort on a list_display field that is a callable
