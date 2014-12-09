335726c358f109d713ce0210b1195f0ee044c2d0
Marc Tamlyn <marc.tamlyn@gmail.com>
2014-12-09 14:30:19 +0000

    Fix flake8 bug.

--- tests/model_fields/test_durationfield.py
+++ tests/model_fields/test_durationfield.py
@@ -60,7 +60,8 @@ class TestValidation(TestCase):
         with self.assertRaises(exceptions.ValidationError) as cm:
             field.clean('not a datetime', None)
         self.assertEqual(cm.exception.code, 'invalid')
-        self.assertEqual(cm.exception.message % cm.exception.params,
+        self.assertEqual(
+            cm.exception.message % cm.exception.params,
             "'not a datetime' value has an invalid format. "
             "It must be in [DD] [HH:[MM:]]ss[.uuuuuu] format."
         )
