18d8c5f7b84fd06e23337b1ec57475b7b3762c56
Markus Holtermann <info@markusholtermann.eu>
2014-12-11 17:52:42 +0100

    Fixed #23956 -- Fixed migration creation for multiple table inheritance

--- tests/migrations/test_autodetector.py
+++ tests/migrations/test_autodetector.py
@@ -1446,6 +1446,29 @@ class AutodetectorTests(TestCase):
         self.assertOperationAttributes(changes, 'testapp', 0, 0, name="Author")
         self.assertOperationAttributes(changes, 'testapp', 0, 1, name="Aardvark")
 
+    def test_multiple_bases(self):
+        """#23956 - Tests that inheriting models doesn't move *_ptr fields into AddField operations."""
+        A = ModelState("app", "A", [("a_id", models.AutoField(primary_key=True))])
+        B = ModelState("app", "B", [("b_id", models.AutoField(primary_key=True))])
+        C = ModelState("app", "C", [], bases=("app.A", "app.B"))
+        D = ModelState("app", "D", [], bases=("app.A", "app.B"))
+        E = ModelState("app", "E", [], bases=("app.A", "app.B"))
+        # Make state
+        before = self.make_project_state([])
+        after = self.make_project_state([A, B, C, D, E])
+        autodetector = MigrationAutodetector(before, after)
+        changes = autodetector._detect_changes()
+        # Right number/type of migrations?
+        self.assertNumberMigrations(changes, "app", 1)
+        self.assertOperationTypes(changes, "app", 0, [
+            "CreateModel", "CreateModel", "CreateModel", "CreateModel", "CreateModel"
+        ])
+        self.assertOperationAttributes(changes, "app", 0, 0, name="A")
+        self.assertOperationAttributes(changes, "app", 0, 1, name="B")
+        self.assertOperationAttributes(changes, "app", 0, 2, name="C")
+        self.assertOperationAttributes(changes, "app", 0, 3, name="D")
+        self.assertOperationAttributes(changes, "app", 0, 4, name="E")
+
     def test_proxy_bases_first(self):
         """Tests that bases of proxies come first."""
         # Make state
