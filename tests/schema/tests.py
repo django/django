66c0529b3edb40e25713cbf32ea8794befc829ae
Tim Graham <timograham@gmail.com>
2014-12-15 18:15:55 -0500

    [1.7.x] Fixed refs #23987 test on Oracle.
    Backport of ac5f2a4ef7b9993502ebc02f487cbb06bfb9bf0a from master

--- tests/schema/tests.py
+++ tests/schema/tests.py
@@ -1203,4 +1203,4 @@ class SchemaTests(TransactionTestCase):
         with connection.cursor() as cursor:
             cursor.execute("SELECT surname FROM schema_author;")
             item = cursor.fetchall()[0]
-            self.assertEqual(item[0], '')
+            self.assertEqual(item[0], None if connection.features.interprets_empty_strings_as_nulls else '')
