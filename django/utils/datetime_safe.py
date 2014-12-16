602643431f9949350be4fe2440f0b4b15754a3f0
Oscar Ramirez <tuxskar@gmail.com>
2014-12-17 00:30:26 +0100

    Fixed #23998 -- Added datetime.time support to migrations questioner.

--- django/utils/datetime_safe.py
+++ django/utils/datetime_safe.py
@@ -7,9 +7,9 @@
 # >>> datetime_safe.date(1850, 8, 2).strftime("%Y/%m/%d was a %A")
 # '1850/08/02 was a Friday'
 
-from datetime import date as real_date, datetime as real_datetime
+from datetime import date as real_date, time as real_time, datetime as real_datetime
 import re
-import time
+import time as ttime
 
 
 class date(real_date):
@@ -29,6 +29,10 @@ class datetime(real_datetime):
 
     def date(self):
         return date(self.year, self.month, self.day)
+
+
+class time(real_time):
+    pass
 
 
 def new_date(d):
@@ -80,10 +84,10 @@ def strftime(dt, fmt):
     # Move to around the year 2000
     year = year + ((2000 - year) // 28) * 28
     timetuple = dt.timetuple()
-    s1 = time.strftime(fmt, (year,) + timetuple[1:])
+    s1 = ttime.strftime(fmt, (year,) + timetuple[1:])
     sites1 = _findall(s1, str(year))
 
-    s2 = time.strftime(fmt, (year + 28,) + timetuple[1:])
+    s2 = ttime.strftime(fmt, (year + 28,) + timetuple[1:])
     sites2 = _findall(s2, str(year + 28))
 
     sites = []
