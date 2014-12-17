aea5e1abac5ca9de6ed5667c922b986832880313
Aymeric Augustin <aymeric.augustin@m4x.org>
2014-12-17 23:36:32 +0100

    Deprecated TEMPLATE_CONTEXT_PROCESSORS.

--- tests/test_client_regress/tests.py
+++ tests/test_client_regress/tests.py
@@ -1004,9 +1004,15 @@ class ContextTests(TestCase):
             # Need to insert a context processor that assumes certain things about
             # the request instance. This triggers a bug caused by some ways of
             # copying RequestContext.
-            with self.settings(TEMPLATE_CONTEXT_PROCESSORS=(
-                'test_client_regress.context_processors.special',
-            )):
+            with self.settings(TEMPLATES=[{
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'APP_DIRS': True,
+                'OPTIONS': {
+                    'context_processors': [
+                        'test_client_regress.context_processors.special',
+                    ],
+                },
+            }]):
                 response = self.client.get("/request_context_view/")
                 self.assertContains(response, 'Path: /request_context_view/')
 
