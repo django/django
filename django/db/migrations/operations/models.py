51ea30a43bafad40b4a24ad0be346796a9c7ab6a
Tim Graham <timograham@gmail.com>
2014-12-23 12:27:49 -0500

    [1.7.x] Fixed #24037 -- Prevented data loss possibility when changing Meta.managed.
    The migrations autodetector now issues AlterModelOptions operations for
    Meta.managed changes instead of DeleteModel + CreateModel.
    
    Thanks iambibhas for the report and Simon and Markus for review.
    
    Backport of 061caa5b386681dc7bdef16918873043224a299c from master

--- django/db/migrations/operations/models.py
+++ django/db/migrations/operations/models.py
@@ -356,6 +356,7 @@ class AlterModelOptions(Operation):
     # Model options we want to compare and preserve in an AlterModelOptions op
     ALTER_OPTION_KEYS = [
         "get_latest_by",
+        "managed",
         "ordering",
         "permissions",
         "default_permissions",
