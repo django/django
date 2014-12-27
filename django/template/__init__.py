badd76770e31a77bad03d8967d3756af5e52c20c
Aymeric Augustin <aymeric.augustin@m4x.org>
2014-12-27 18:57:01 +0100

    Explained the structure of django.template.

--- django/template/__init__.py
+++ django/template/__init__.py
@@ -1,3 +1,44 @@
+"""
+Django's support for templates.
+
+The django.template namespace contains two independent subsystems:
+
+1. Multiple Template Engines: support for pluggable template backends,
+   built-in backends and backend-independent APIs
+2. Django Template Langage: Django's own template engine, including its
+   built-in loaders, context processors, tags and filters.
+
+Ideally these subsystems would be implemented in distinct packages. However
+keeping them together made the implementation of Multiple Template Engines
+less disruptive .
+
+Here's a breakdown of which modules belong to which subsystem.
+
+Multiple Template Engines:
+
+- django.template.backends.*
+- django.template.loader
+- django.template.response
+
+Django Template Language:
+
+- django.template.base
+- django.template.context
+- django.template.context_processors
+- django.template.loaders.*
+- django.template.debug
+- django.template.defaultfilters
+- django.template.defaulttags
+- django.template.engine
+- django.template.loader_tags
+- django.template.smartif
+
+Shared:
+
+- django.template.utils
+
+"""
+
 ### Multiple Template Engines
 
 from .utils import EngineHandler
