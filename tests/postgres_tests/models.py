48ad288679a0cb2e2cfb17f128903e6c5b1c4870
Marc Tamlyn <marc.tamlyn@gmail.com>
2015-01-10 16:14:20 +0000

    Fixed #24001 -- Added range fields for PostgreSQL.
    Added support for PostgreSQL range types to contrib.postgres.
    
    - 5 new model fields
    - 4 new form fields
    - New validators
    - Uses psycopg2's range type implementation in python

--- tests/postgres_tests/models.py
+++ tests/postgres_tests/models.py
@@ -1,5 +1,8 @@
-from django.contrib.postgres.fields import ArrayField, HStoreField
-from django.db import models
+from django.contrib.postgres.fields import (
+    ArrayField, HStoreField, IntegerRangeField, BigIntegerRangeField,
+    FloatRangeField, DateTimeRangeField, DateRangeField,
+)
+from django.db import connection, models
 
 
 class IntegerArrayModel(models.Model):
@@ -32,6 +35,20 @@ class CharFieldModel(models.Model):
 
 class TextFieldModel(models.Model):
     field = models.TextField()
+
+
+# Only create this model for databases which support it
+if connection.vendor == 'postgresql' and connection.pg_version >= 90200:
+    class RangesModel(models.Model):
+        ints = IntegerRangeField(blank=True, null=True)
+        bigints = BigIntegerRangeField(blank=True, null=True)
+        floats = FloatRangeField(blank=True, null=True)
+        timestamps = DateTimeRangeField(blank=True, null=True)
+        dates = DateRangeField(blank=True, null=True)
+else:
+    # create an object with this name so we don't have failing imports
+    class RangesModel(object):
+        pass
 
 
 class ArrayFieldSubclass(ArrayField):
