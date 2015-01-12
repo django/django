26a92619f62aeb6f60e15c62e8322c96744eff26
Collin Anderson <cmawebsite@gmail.com>
2015-01-12 10:48:49 -0500

    Fixed #24124 -- Changed context_processors in the default settings.py

--- django/conf/project_template/project_name/settings.py
+++ django/conf/project_template/project_name/settings.py
@@ -61,10 +61,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.i18n',
-                'django.template.context_processors.tz',
-                'django.template.context_processors.media',
-                'django.template.context_processors.static',
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
