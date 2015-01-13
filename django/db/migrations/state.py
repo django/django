41d05733ab21182a77518443f509cef5d6f08560
Markus Holtermann <info@markusholtermann.eu>
2015-01-13 18:18:23 +0100

    Fixed #24147 -- Prevented managers leaking model during migrations
    Thanks Tim Graham for the review.

--- django/db/migrations/state.py
+++ django/db/migrations/state.py
@@ -400,6 +400,19 @@ class ModelState(object):
             field_class = import_string(path)
             yield name, field_class(*args, **kwargs)
 
+    def construct_managers(self):
+        "Deep-clone the managers using deconstruction"
+        # Sort all managers by their creation counter
+        sorted_managers = sorted(self.managers, key=lambda v: v[1].creation_counter)
+        for mgr_name, manager in sorted_managers:
+            as_manager, manager_path, qs_path, args, kwargs = manager.deconstruct()
+            if as_manager:
+                qs_class = import_string(qs_path)
+                yield mgr_name, qs_class.as_manager()
+            else:
+                manager_class = import_string(manager_path)
+                yield mgr_name, manager_class(*args, **kwargs)
+
     def clone(self):
         "Returns an exact copy of this ModelState"
         return self.__class__(
@@ -408,7 +421,7 @@ class ModelState(object):
             fields=list(self.construct_fields()),
             options=dict(self.options),
             bases=self.bases,
-            managers=self.managers,
+            managers=list(self.construct_managers()),
         )
 
     def render(self, apps):
@@ -431,8 +444,7 @@ class ModelState(object):
         body['__module__'] = "__fake__"
 
         # Restore managers
-        for mgr_name, manager in self.managers:
-            body[mgr_name] = manager
+        body.update(self.construct_managers())
 
         # Then, make a Model object (apps.register_model is called in __new__)
         return type(
