80c70cf0f9c16ffeb7c5d340c03e2e5a57346fa2
Collin Anderson <cmawebsite@gmail.com>
2015-01-14 13:40:31 -0500

    Fixed incorrect error message in Options.get_fields()

--- tests/model_meta/tests.py
+++ tests/model_meta/tests.py
@@ -169,7 +169,7 @@ class GetFieldByNameTests(OptionsBaseTests):
         self.assertEqual(field_info[1:], (None, True, False))
         self.assertIsInstance(field_info[0], GenericRelation)
 
-    def test_get_fields_only_searaches_forward_on_apps_not_ready(self):
+    def test_get_fields_only_searches_forward_on_apps_not_ready(self):
         opts = Person._meta
         # If apps registry is not ready, get_field() searches over only
         # forward fields.
@@ -179,8 +179,8 @@ class GetFieldByNameTests(OptionsBaseTests):
             self.assertTrue(opts.get_field('data_abstract'))
             msg = (
                 "Person has no field named 'relating_baseperson'. The app "
-                "cache isn't ready yet, so if this is a forward field, it "
-                "won't be available yet."
+                "cache isn't ready yet, so if this is an auto-created related "
+                "field, it won't be available yet."
             )
             # 'data_abstract' is a reverse field, and will raise an exception
             with self.assertRaisesMessage(FieldDoesNotExist, msg):
