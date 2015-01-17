c51258882bbf388f5c4cfc379340097ebe9beda9
Tim Graham <timograham@gmail.com>
2015-01-16 19:27:10 -0500

    Increased the default PBKDF2 iterations.

--- django/contrib/auth/hashers.py
+++ django/contrib/auth/hashers.py
@@ -226,7 +226,7 @@ class PBKDF2PasswordHasher(BasePasswordHasher):
     safely but you must rename the algorithm if you change SHA256.
     """
     algorithm = "pbkdf2_sha256"
-    iterations = 20000
+    iterations = 24000
     digest = hashlib.sha256
 
     def encode(self, password, salt, iterations=None):
