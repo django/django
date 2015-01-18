78d4e838de546d3f2192375a3c6e4ab8de81f6c3
Josh Smeaton <josh.smeaton@gmail.com>
2015-01-19 10:35:10 +1100

    Fixed #24174 -- Fixed extra order by descending

--- tests/ordering/tests.py
+++ tests/ordering/tests.py
@@ -166,6 +166,26 @@ class OrderingTests(TestCase):
             attrgetter("headline")
         )
 
+    def test_extra_ordering_with_table_name(self):
+        self.assertQuerysetEqual(
+            Article.objects.extra(order_by=['ordering_article.headline']), [
+                "Article 1",
+                "Article 2",
+                "Article 3",
+                "Article 4",
+            ],
+            attrgetter("headline")
+        )
+        self.assertQuerysetEqual(
+            Article.objects.extra(order_by=['-ordering_article.headline']), [
+                "Article 4",
+                "Article 3",
+                "Article 2",
+                "Article 1",
+            ],
+            attrgetter("headline")
+        )
+
     def test_order_by_pk(self):
         """
         Ensure that 'pk' works as an ordering option in Meta.
