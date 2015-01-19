8a3f3b30c1ef36aa4d652709a3326875056110de
A.J. May <amay@northps.com>
2015-01-19 14:29:45 -0500

    Changes to the generatesecret command

--- django/core/management/commands/generatesecret.py
+++ django/core/management/commands/generatesecret.py
@@ -5,30 +5,22 @@ from django.core.management.base import BaseCommand, CommandError
 from django.utils.secret_key import generate_secret_key, create_secret_key_file
 
 import os
+import sys
 
 
 class Command(BaseCommand):
     help = "Generates and outputs a random secret key to a file or stdout."
-    args = "[output_file]"
-
     requires_system_checks = False
-
-    # Can't import settings during this command, because they haven't
-    # necessarily been created.
     can_import_settings = False
-
-    # Can't perform any active locale changes during this command, because
-    # setting might not be available at all.
     leave_locale_alone = True
 
-    def handle(self, *output_file, **options):
-        try:
-            output_file = output_file[0]
-        except IndexError:
-            raise CommandError("You must specify a file path.")
+    def add_arguments(self, parser):
+        parser.add_argument('outfile', nargs='+')
 
-        if output_file == '-':
-            self.stdout.write(generate_secret_key())
-            return
+    def handle(self, *args, **options):
+        for outfile in options['outfile']:
+            if outfile == '-':
+                self.stdout.write(generate_secret_key())
+                sys.exit(0)
 
-        create_secret_key_file(output_file)
+            create_secret_key_file(outfile)
