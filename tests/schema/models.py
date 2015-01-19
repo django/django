cf7c7a2a7d28187ba01e483feddaf9c52456f2e0
Markus Holtermann <info@markusholtermann.eu>
2015-01-19 15:31:23 +0100

    Fixed #24163 -- Removed unique constraint after index on MySQL
    Thanks Łukasz Harasimowicz for the report.

--- tests/schema/models.py
+++ tests/schema/models.py
@@ -67,6 +67,16 @@ class BookWeak(models.Model):
         apps = new_apps
 
 
+class BookWithO2O(models.Model):
+    author = models.OneToOneField(Author)
+    title = models.CharField(max_length=100, db_index=True)
+    pub_date = models.DateTimeField()
+
+    class Meta:
+        apps = new_apps
+        db_table = "schema_book"
+
+
 class BookWithM2M(models.Model):
     author = models.ForeignKey(Author)
     title = models.CharField(max_length=100, db_index=True)
