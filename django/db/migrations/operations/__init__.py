b16975d2651fb4aaf895524a69a1e07ee7e6e09f
Peter Lauri <peterlauri@gmail.com>
2015-01-22 20:19:22 +0100

    initial ideas for how multiple AddField could be optimised to one AddFieldList

--- django/db/migrations/operations/__init__.py
+++ django/db/migrations/operations/__init__.py
@@ -1,13 +1,13 @@
 from .models import (CreateModel, DeleteModel, AlterModelTable,
     AlterUniqueTogether, AlterIndexTogether, RenameModel, AlterModelOptions,
     AlterOrderWithRespectTo, AlterModelManagers)
-from .fields import AddField, RemoveField, AlterField, RenameField
+from .fields import AddField, RemoveField, AlterField, RenameField, AddFieldList
 from .special import SeparateDatabaseAndState, RunSQL, RunPython
 
 __all__ = [
     'CreateModel', 'DeleteModel', 'AlterModelTable', 'AlterUniqueTogether',
     'RenameModel', 'AlterIndexTogether', 'AlterModelOptions',
-    'AddField', 'RemoveField', 'AlterField', 'RenameField',
+    'AddField', 'RemoveField', 'AlterField', 'RenameField', 'AddFieldList',
     'SeparateDatabaseAndState', 'RunSQL', 'RunPython',
     'AlterOrderWithRespectTo', 'AlterModelManagers',
 ]
