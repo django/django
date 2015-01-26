a2eae3de78cf7076361f7f4e366beea971ea5740
Emin Mastizada <emin@mastizada.com>
2015-01-26 03:09:12 +0200

    Update __init__.py

--- django/conf/locale/__init__.py
+++ django/conf/locale/__init__.py
@@ -33,7 +33,7 @@ LANG_INFO = {
         'bidi': True,
         'code': 'az',
         'name': 'Azerbaijani',
-        'name_local': 'Azərbaycan dili',
+        'name_local': 'Azərbaycanca',
     },
     'be': {
         'bidi': False,
