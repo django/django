fad9ca4420b5a73e11228cb2f9b96d2c9c55e59f
Emin Mastizada <emin@mastizada.com>
2015-01-26 03:08:00 +0200

    Azerbaijani language name
    Just added i18n to Mozilla Community page and there was problem with language switch. English (en) and azərbaycan dili (az). Due to rules of Azerbaijani language and according to wikipedia page of Azerbaijan, language's name should be written with big letter.

--- django/conf/locale/__init__.py
+++ django/conf/locale/__init__.py
@@ -33,7 +33,7 @@ LANG_INFO = {
         'bidi': True,
         'code': 'az',
         'name': 'Azerbaijani',
-        'name_local': 'azərbaycan dili',
+        'name_local': 'Azərbaycan dili',
     },
     'be': {
         'bidi': False,
