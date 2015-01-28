dce53abd4d8dede486d9197dd45d73110af34878
Anssi Kääriäinen <anssi.kaariainen@thl.fi>
2015-01-28 13:40:48 +0200

    Fixed #23617 -- Added get_pk_value_on_save()
    The method is mainly intended for use with UUIDField. For UUIDField we
    want to call the field's default even when primary key value is
    explicitly set to None to match the behavior of AutoField.
    
    Thanks to Marc Tamlyn and Tim Graham for review.

--- django/db/models/base.py
+++ django/db/models/base.py
@@ -771,6 +771,9 @@ class Model(six.with_metaclass(ModelBase)):
                        if f.name in update_fields or f.attname in update_fields]
 
         pk_val = self._get_pk_val(meta)
+        if pk_val is None:
+            pk_val = meta.pk.get_pk_value_on_save(self)
+            setattr(self, meta.pk.attname, pk_val)
         pk_set = pk_val is not None
         if not pk_set and (force_update or update_fields):
             raise ValueError("Cannot force an update in save() with no primary key.")
