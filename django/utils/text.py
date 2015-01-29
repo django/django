bd61b7ea13ba4c0979137d5541ee1e38c1e5c13c
Matthew Somerville <matthew-github@dracos.co.uk>
2015-01-29 07:59:41 +0000

    Fixed #24242 -- Do not flush zfile each write.
    Flushing after every write can lead to the gzipped streamed content
    being larger than the original content; each flush adds a 5/6 byte type
    0 block. Removing this means buf.read() may return nothing, so only
    yield if that has some data. Testing shows without the flush() the
    buffer is being flushed every 17k or so, and compresses the same as if
    it had been done as a whole string.

--- django/utils/text.py
+++ django/utils/text.py
@@ -302,6 +302,8 @@ class StreamingBuffer(object):
         self.vals.append(val)
 
     def read(self):
+        if not self.vals:
+            return b''
         ret = b''.join(self.vals)
         self.vals = []
         return ret
@@ -321,8 +323,9 @@ def compress_sequence(sequence):
     yield buf.read()
     for item in sequence:
         zfile.write(item)
-        zfile.flush()
-        yield buf.read()
+        data = buf.read()
+        if data:
+            yield data
     zfile.close()
     yield buf.read()
 
