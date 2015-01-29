62fb7c32b64d4abb7b5e186e4af53caa19101587
Tim Graham <timograham@gmail.com>
2015-01-29 07:20:56 -0500

    Fixed #24164 -- Fixed Oracle GIS limited aggregation test failure.

--- django/db/backends/oracle/compiler.py
+++ django/db/backends/oracle/compiler.py
@@ -2,7 +2,7 @@ from django.db.models.sql import compiler
 
 
 class SQLCompiler(compiler.SQLCompiler):
-    def as_sql(self, with_limits=True, with_col_aliases=False):
+    def as_sql(self, with_limits=True, with_col_aliases=False, subquery=False):
         """
         Creates the SQL for this query. Returns the SQL string and list
         of parameters.  This is overridden from the original Query class
@@ -20,12 +20,17 @@ class SQLCompiler(compiler.SQLCompiler):
         do_offset = with_limits and (self.query.high_mark is not None
                                      or self.query.low_mark)
         if not do_offset:
-            sql, params = super(SQLCompiler, self).as_sql(with_limits=False,
-                    with_col_aliases=with_col_aliases)
+            sql, params = super(SQLCompiler, self).as_sql(
+                with_limits=False,
+                with_col_aliases=with_col_aliases,
+                subquery=subquery,
+            )
         else:
-            sql, params = super(SQLCompiler, self).as_sql(with_limits=False,
-                                                    with_col_aliases=True)
-
+            sql, params = super(SQLCompiler, self).as_sql(
+                with_limits=False,
+                with_col_aliases=True,
+                subquery=subquery,
+            )
             # Wrap the base query in an outer SELECT * with boundaries on
             # the "_RN" column.  This is the canonical way to emulate LIMIT
             # and OFFSET on Oracle.
