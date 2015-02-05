django/django

commit e3702dc3f2af9e8cc6c1155b248f458899be334e

[1.8.x] Cleaned up schema tests
Thanks Tim Graham for the review.

Backport of 0204714b0bdf10d7558ee106de9a718407f3ec5a from master

tests/schema/fields.py
@@ -52,3 +52,7 @@ class CustomManyToManyField(RelatedField):
     _get_m2m_attr = ManyToManyField.__dict__['_get_m2m_attr']
     _get_m2m_reverse_attr = ManyToManyField.__dict__['_get_m2m_reverse_attr']
     _get_m2m_db_table = ManyToManyField.__dict__['_get_m2m_db_table']
+
+
+class InheritedManyToManyField(ManyToManyField):
+    pass

tests/schema/models.py
@@ -25,24 +25,9 @@ class AuthorWithDefaultHeight(models.Model):
         apps = new_apps
 
 
-class AuthorWithM2M(models.Model):
+class AuthorWithEvenLongerName(models.Model):
     name = models.CharField(max_length=255)
-
-    class Meta:
-        apps = new_apps
-
-
-class AuthorWithM2MThrough(models.Model):
-    name = models.CharField(max_length=255)
-    tags = models.ManyToManyField("schema.TagM2MTest", related_name="authors", through="AuthorTag")
-
-    class Meta:
-        apps = new_apps
-
-
-class AuthorTag(models.Model):
-    author = models.ForeignKey("schema.AuthorWithM2MThrough")
-    tag = models.ForeignKey("schema.TagM2MTest")
+    height = models.PositiveIntegerField(null=True, blank=True)
 
     class Meta:
         apps = new_apps
@@ -67,6 +52,13 @@ class BookWeak(models.Model):
         apps = new_apps
 
 
+class BookWithLongName(models.Model):
+    author_foreign_key_with_really_long_field_name = models.ForeignKey(AuthorWithEvenLongerName)
+
+    class Meta:
+        apps = new_apps
+
+
 class BookWithO2O(models.Model):
     author = models.OneToOneField(Author)
     title = models.CharField(max_length=100, db_index=True)
@@ -75,31 +67,6 @@ class BookWithO2O(models.Model):
     class Meta:
         apps = new_apps
         db_table = "schema_book"
-
-
-class BookWithM2M(models.Model):
-    author = models.ForeignKey(Author)
-    title = models.CharField(max_length=100, db_index=True)
-    pub_date = models.DateTimeField()
-    tags = models.ManyToManyField("TagM2MTest", related_name="books")
-
-    class Meta:
-        apps = new_apps
-
-
-class TagThrough(models.Model):
-    book = models.ForeignKey("schema.BookWithM2MThrough")
-    tag = models.ForeignKey("schema.TagM2MTest")
-
-    class Meta:
-        apps = new_apps
-
-
-class BookWithM2MThrough(models.Model):
-    tags = models.ManyToManyField("TagM2MTest", related_name="books", through=TagThrough)
-
-    class Meta:
-        apps = new_apps
 
 
 class BookWithSlug(models.Model):
@@ -113,15 +80,11 @@ class BookWithSlug(models.Model):
         db_table = "schema_book"
 
 
+class Note(models.Model):
+    info = models.TextField()
+
+
 class Tag(models.Model):
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True)
-
-    class Meta:
-        apps = new_apps
-
-
-class TagM2MTest(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
 
@@ -138,6 +101,14 @@ class TagIndexed(models.Model):
         index_together = [["slug", "title"]]
 
 
+class TagM2MTest(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        apps = new_apps
+
+
 class TagUniqueRename(models.Model):
     title = models.CharField(max_length=255)
     slug2 = models.SlugField(unique=True)
@@ -145,30 +116,6 @@ class TagUniqueRename(models.Model):
     class Meta:
         apps = new_apps
         db_table = "schema_tag"
-
-
-class UniqueTest(models.Model):
-    year = models.IntegerField()
-    slug = models.SlugField(unique=False)
-
-    class Meta:
-        apps = new_apps
-        unique_together = ["year", "slug"]
-
-
-class AuthorWithEvenLongerName(models.Model):
-    name = models.CharField(max_length=255)
-    height = models.PositiveIntegerField(null=True, blank=True)
-
-    class Meta:
-        apps = new_apps
-
-
-class BookWithLongName(models.Model):
-    author_foreign_key_with_really_long_field_name = models.ForeignKey(AuthorWithEvenLongerName)
-
-    class Meta:
-        apps = new_apps
 
 
 # Based on tests/reserved_names/models.py
@@ -183,5 +130,10 @@ class Thing(models.Model):
         return self.when
 
 
-class Note(models.Model):
-    info = models.TextField()
+class UniqueTest(models.Model):
+    year = models.IntegerField()
+    slug = models.SlugField(unique=False)
+
+    class Meta:
+        apps = new_apps
+        unique_together = ["year", "slug"]

commit c9addfd4bfadde043a1aec7854b285aa212bdd5a

[1.8.x] Fixed small regression caused by e3702dc3f2af9e8cc6c1155b248f458899be334e
Since 1.7 models need to declare an explicit app_label if they are not
in an application in INSTALLED_APPS or were imported before their
application was loaded.

Backport of 235124d3eaf6cc7ab9f97f1d1467d1caf18e1016 from master

tests/schema/tests.py
@@ -218,6 +218,7 @@ class SchemaTests(TransactionTestCase):
             name = CharField(max_length=255)
 
             class Meta:
+                app_label = 'schema'
                 apps = new_apps
 
         self.local_models = [LocalAuthorWithM2M]
@@ -652,6 +653,7 @@ class SchemaTests(TransactionTestCase):
             tags = M2MFieldClass("TagM2MTest", related_name="books")
 
             class Meta:
+                app_label = 'schema'
                 apps = new_apps
 
         self.local_models = [LocalBookWithM2M]
@@ -683,12 +685,14 @@ class SchemaTests(TransactionTestCase):
             tag = ForeignKey("schema.TagM2MTest")
 
             class Meta:
+                app_label = 'schema'
                 apps = new_apps
 
         class LocalBookWithM2MThrough(Model):
             tags = M2MFieldClass("TagM2MTest", related_name="books", through=LocalTagThrough)
 
             class Meta:
+                app_label = 'schema'
                 apps = new_apps
 
         self.local_models = [LocalTagThrough, LocalBookWithM2MThrough]
@@ -720,6 +724,7 @@ class SchemaTests(TransactionTestCase):
             name = CharField(max_length=255)
 
             class Meta:
+                app_label = 'schema'
                 apps = new_apps
 
         self.local_models = [LocalAuthorWithM2M]
@@ -768,6 +773,7 @@ class SchemaTests(TransactionTestCase):
             tag = ForeignKey("schema.TagM2MTest")
 
             class Meta:
+                app_label = 'schema'
                 apps = new_apps
 
         class LocalAuthorWithM2MThrough(Model):
@@ -775,6 +781,7 @@ class SchemaTests(TransactionTestCase):
             tags = M2MFieldClass("schema.TagM2MTest", related_name="authors", through=LocalAuthorTag)
 
             class Meta:
+                app_label = 'schema'
                 apps = new_apps
 
         self.local_models = [LocalAuthorTag, LocalAuthorWithM2MThrough]
@@ -815,6 +822,7 @@ class SchemaTests(TransactionTestCase):
             tags = M2MFieldClass("TagM2MTest", related_name="books")
 
             class Meta:
+                app_label = 'schema'
                 apps = new_apps
 
         self.local_models = [LocalBookWithM2M]