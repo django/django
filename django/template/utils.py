a81c77ba089bf334f8575157b78a5dc9178f3c4f
Aymeric Augustin <aymeric.augustin@m4x.org>
2015-02-05 11:53:04 +0100

    Fixed #24265 -- Preserved template backend loading exceptions.
    If importing or initializing a template backend fails, attempting to
    access this template backend again must raise the same exception.

--- django/template/utils.py
+++ django/template/utils.py
@@ -90,6 +90,10 @@ class EngineHandler(object):
                     "Could not find config for '{}' "
                     "in settings.TEMPLATES".format(alias))
 
+            # If importing or initializing the backend raises an exception,
+            # self._engines[alias] isn't set and this code may get executed
+            # again, so we must preserve the original params. See #24265.
+            params = params.copy()
             backend = params.pop('BACKEND')
             engine_cls = import_string(backend)
             engine = engine_cls(params)
