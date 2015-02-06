81c2d9f60b9206c1291e5b1c3c8686f24a7726e1
Andriy Sokolovskiy <sokandpal@yandex.ru>
2015-02-07 01:16:26 +0200

    Fixed #15579 -- Added ability to delete only child models in multi-table inheritance.

--- tests/delete/tests.py
+++ tests/delete/tests.py
@@ -349,6 +349,13 @@ class DeletionTests(TestCase):
         self.assertFalse(S.objects.exists())
         self.assertFalse(T.objects.exists())
 
+    def test_delete_with_keeping_parents(self):
+        child = RChild.objects.create()
+        parent_id = child.r_ptr_id
+        child.delete(keep_parents=True)
+        self.assertFalse(RChild.objects.filter(id=child.id).exists())
+        self.assertTrue(R.objects.filter(id=parent_id).exists())
+
 
 class FastDeleteTests(TestCase):
 
