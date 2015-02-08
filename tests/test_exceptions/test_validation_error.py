1072bf9c221d032e54a039349911affd369fa449
Andrew Pinkham <code@andrewsforge.com>
2015-02-08 10:56:29 -0600

    Fixed #24639 -- ValidationError respects SafeData.

--- tests/test_exceptions/test_validation_error.py
+++ tests/test_exceptions/test_validation_error.py
@@ -1,6 +1,8 @@
 import unittest
 
 from django.core.exceptions import ValidationError
+from django.utils import six
+from django.utils.safestring import SafeData, mark_safe
 
 
 class TestValidationError(unittest.TestCase):
@@ -14,3 +16,20 @@ class TestValidationError(unittest.TestCase):
         message_dict['field2'] = ['E3', 'E4']
         exception = ValidationError(message_dict)
         self.assertEqual(sorted(exception.messages), ['E1', 'E2', 'E3', 'E4'])
+
+    def test_iter_ouput_respects_safe_data(self):
+        ve = ValidationError(mark_safe('safe'))
+        self.assertIsInstance(list(ve)[0], SafeData)
+        ve = ValidationError(mark_safe('%(r)s'), params={'r': 'replaced'})
+        self.assertIsInstance(list(ve)[0], six.text_type)
+        ve = ValidationError(mark_safe('%(r)s'),
+                             params={'r': mark_safe('safe replaced')})
+        self.assertIsInstance(list(ve)[0], SafeData)
+        ve = ValidationError(mark_safe('%(r)s %(s)s'),
+                             params={'r': mark_safe('actually'),
+                                     's': 'not safe'})
+        self.assertIsInstance(list(ve)[0], six.text_type)
+        ve = ValidationError(mark_safe('%(r)s %(s)s'),
+                             params={'r': mark_safe('this is'),
+                                     's': mark_safe('safe')})
+        self.assertIsInstance(list(ve)[0], SafeData)
