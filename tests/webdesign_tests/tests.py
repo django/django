78a26116fd4e498d61e949e5b33629a13edbc6eb
Tim Graham <timograham@gmail.com>
2015-02-11 12:29:20 -0500

    Moved contrib.webdesign tests out of contrib.

--- tests/webdesign_tests/tests.py
+++ tests/webdesign_tests/tests.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import unittest
-
 from django.template import Context, Template
+from django.test import SimpleTestCase, modify_settings
 
 
-class WebdesignTest(unittest.TestCase):
+@modify_settings(INSTALLED_APPS={'append': 'django.contrib.webdesign'})
+class WebdesignTest(SimpleTestCase):
 
     def test_lorem_tag(self):
         t = Template("{% load webdesign %}{% lorem 3 w %}")
