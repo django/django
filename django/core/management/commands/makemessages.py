5720733b417559fee21c2f3de59ea55f2314cebd
Tim Graham <timograham@gmail.com>
2015-02-15 18:59:39 -0500

    Refs #24324 -- Fixed makemessages crash when Django is installed in a non-ASCII path.

--- django/core/management/commands/makemessages.py
+++ django/core/management/commands/makemessages.py
@@ -16,6 +16,7 @@ from django.core.management.utils import (
     find_command, handle_extensions, popen_wrapper,
 )
 from django.utils import six
+from django.utils._os import upath
 from django.utils.encoding import force_str
 from django.utils.functional import cached_property, total_ordering
 from django.utils.jslex import prepare_js_for_gettext
@@ -472,7 +473,7 @@ class Command(BaseCommand):
         the msgs string, inserting it at the right place. msgs should be the
         contents of a newly created .po file.
         """
-        django_dir = os.path.normpath(os.path.join(os.path.dirname(django.__file__)))
+        django_dir = os.path.normpath(os.path.join(os.path.dirname(upath(django.__file__))))
         if self.domain == 'djangojs':
             domains = ('djangojs', 'django')
         else:
