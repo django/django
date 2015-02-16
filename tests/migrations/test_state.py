906d682ba00473b1df73c5542ccc308755e9f46a
Markus Holtermann <info@markusholtermann.eu>
2015-02-16 20:34:37 +0100

    Fixed regression in test introduced in d538e37e1b14603d1d0c94f185f378fc0ed1e53f

--- tests/migrations/test_state.py
+++ tests/migrations/test_state.py
@@ -765,12 +765,12 @@ class RelatedModelsTests(SimpleTestCase):
     def test_direct_m2m(self):
         A = self.create_model("A", foreign_keys=[models.ManyToManyField('B')])
         B = self.create_model("B")
-        self.assertRelated(A, [A.a_1.rel.through, B])
-        self.assertRelated(B, [A, A.a_1.rel.through])
+        self.assertRelated(A, [A.a_1.through, B])
+        self.assertRelated(B, [A, A.a_1.through])
 
     def test_direct_m2m_self(self):
         A = self.create_model("A", foreign_keys=[models.ManyToManyField('A')])
-        self.assertRelated(A, [A.a_1.rel.through])
+        self.assertRelated(A, [A.a_1.through])
 
     def test_intermediate_m2m_self(self):
         A = self.create_model("A", foreign_keys=[models.ManyToManyField('A', through='T')])
