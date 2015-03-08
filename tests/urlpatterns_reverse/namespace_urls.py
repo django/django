a332eb5a8a964259b3a0aa0e4f06dc9b814738af
Bas Peschier <basp@fabrique.nl>
2015-03-08 18:26:30 +0100

    Fixed #12943 -- Allowed unnamed arguments to be propagated in includes
    Propagated unnamed arguments as positional arguments into included
    URLconfs if no named arguments are defined. Positional and keyword
    arguments are never combined.

--- tests/urlpatterns_reverse/namespace_urls.py
+++ tests/urlpatterns_reverse/namespace_urls.py
@@ -50,6 +50,7 @@ urlpatterns = [
 
     url(r'^included/', include('urlpatterns_reverse.included_namespace_urls')),
     url(r'^inc(?P<outer>[0-9]+)/', include('urlpatterns_reverse.included_urls', namespace='inc-ns5')),
+    url(r'^included/([0-9]+)/', include('urlpatterns_reverse.included_namespace_urls')),
 
     url(r'^ns-outer/(?P<outer>[0-9]+)/', include('urlpatterns_reverse.included_namespace_urls', namespace='inc-outer')),
 
