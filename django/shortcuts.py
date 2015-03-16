c088b99c75f72baa4d4446406075382ec7bb455d
dhilipsiva <dhilipsiva@gmail.com>
2015-03-16 12:56:46 +0530

    Implement `get_object_or_none` for convenience

--- django/shortcuts.py
+++ django/shortcuts.py
@@ -119,7 +119,7 @@ def redirect(to, *args, **kwargs):
 def _get_queryset(klass):
     """
     Returns a QuerySet from a Model, Manager, or QuerySet. Created to make
-    get_object_or_404 and get_list_or_404 more DRY.
+    get_object_or_404, get_object_or_none and get_list_or_404 more DRY.
 
     Raises a ValueError if klass is not a Model, Manager, or QuerySet.
     """
@@ -155,6 +155,23 @@ def get_object_or_404(klass, *args, **kwargs):
         return queryset.get(*args, **kwargs)
     except queryset.model.DoesNotExist:
         raise Http404('No %s matches the given query.' % queryset.model._meta.object_name)
+
+
+def get_object_or_none(klass, *args, **kwargs):
+    """
+    Uses get() to return an object, or None if the object does not exist.
+
+    klass may be a Model, Manager, or QuerySet object. All other passed
+    arguments and keyword arguments are used in the get() query.
+
+    Note: Like with get(), an MultipleObjectsReturned will be raised if more
+    than one object is found.
+    """
+    queryset = _get_queryset(klass)
+    try:
+        return queryset.get(*args, **kwargs)
+    except queryset.model.DoesNotExist:
+        raise None
 
 
 def get_list_or_404(klass, *args, **kwargs):
