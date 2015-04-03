7ae7600d2bc9d831bd8be5b669416d05760991d7
Preston Timmons <prestontimmons@gmail.com>
2015-04-02 22:29:23 -0500

    [1.8.x] Updated docstring for firstof tag to reflect new auto-escaping behavior.
    Backport of a1842e3299daf3bde0ab5d418c912a265e7479c9 from master

--- django/template/defaulttags.py
+++ django/template/defaulttags.py
@@ -770,11 +770,15 @@ def firstof(parser, token):
 
         {% firstof var1 var2 var3 "fallback value" %}
 
-    If you want to escape the output, use a filter tag::
+    If you want to disable auto-escaping of variables you can use::
 
-        {% filter force_escape %}
-            {% firstof var1 var2 var3 "fallback value" %}
-        {% endfilter %}
+        {% autoescape off %}
+            {% firstof var1 var2 var3 "<strong>fallback value</strong>" %}
+        {% autoescape %}
+
+    Or if only some variables should be escaped, you can use::
+
+        {% firstof var1 var2|safe var3 "<strong>fallback value</strong>"|safe %}
 
     """
     bits = token.split_contents()[1:]
