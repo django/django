039d7881b441c6b358a963d652ffa2eada3a8892
Marten Kenbeek <marten.knbk@gmail.com>
2015-04-06 18:02:46 +0200

    Refs #24397 -- Sped up model reloading in ProjectState.
    Created bulk_update() context manager on StateApps. Sped up unregistering
    models in reload_models() by using this context mananger.

--- tests/migrations/test_state.py
+++ tests/migrations/test_state.py
@@ -160,6 +160,22 @@ class StateTests(TestCase):
         self.assertEqual([mgr.args for name, mgr in food_order_manager_state.managers],
                          [('a', 'b', 1, 2), ('x', 'y', 3, 4)])
 
+    def test_apps_bulk_update(self):
+        """
+        StateApps.bulk_update() should update apps.ready to False and reset
+        the value afterwards.
+        """
+        project_state = ProjectState()
+        apps = project_state.apps
+        with apps.bulk_update():
+            self.assertFalse(apps.ready)
+        self.assertTrue(apps.ready)
+        with self.assertRaises(ValueError):
+            with apps.bulk_update():
+                self.assertFalse(apps.ready)
+                raise ValueError()
+        self.assertTrue(apps.ready)
+
     def test_render(self):
         """
         Tests rendering a ProjectState into an Apps.
