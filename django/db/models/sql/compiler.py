496800b3bf4460e697dee942696eaee5c56f87a0
Jay Wineinger <jay.wineinger@gmail.com>
2015-04-09 13:47:05 -0500

    [1.8.x] Fixed #24611 -- Fixed update() crash with related UUID pk object.
    Backport of 923da0274a9a8c4ef2ac9776f71bd14628e39fc3 from master

--- django/db/models/sql/compiler.py
+++ django/db/models/sql/compiler.py
@@ -1006,7 +1006,10 @@ class SQLUpdateCompiler(SQLCompiler):
                     raise FieldError("Aggregate functions are not allowed in this query")
             elif hasattr(val, 'prepare_database_save'):
                 if field.rel:
-                    val = val.prepare_database_save(field)
+                    val = field.get_db_prep_save(
+                        val.prepare_database_save(field),
+                        connection=self.connection,
+                    )
                 else:
                     raise TypeError("Database is trying to update a relational field "
                                     "of type %s with a value of type %s. Make sure "
