bed407e90106d05b27292f61d51c4510fc02c7d6
Andrew Pinkham <code@andrewsforge.com>
2015-04-13 12:17:38 -0400

    Fixed #24635 -- Updated default project template.

--- django/conf/project_template/project_name/urls.py
+++ django/conf/project_template/project_name/urls.py
@@ -2,9 +2,15 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', '{{ project_name }}.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    # Example 1: Function Views
+    #   Step 1. Import view above:  from {{ project_name }}.views import home
+    #   Step 2. In this list:       url(r'^$', home, name='home'),
+    # Example 2: Class-Based Views
+    #   Step 1. Import view above:  from {{ project_name }}.views import Home
+    #   Step 2. In this list:       url(r'^$', Home.as_view(), name='home'),
+    # Example 3: Include URL Configuration
+    #   Step 1. Import above:       from blog import urls as blog_urls
+    #   Step 2. In this list:       url(r'^blog/', include(blog_urls)),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
