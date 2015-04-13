b1d83209d0a0df698e66d1cb5fa2a87b9859c3ed
Andrew Pinkham <code@andrewsforge.com>
2015-04-13 12:17:38 -0400

    [1.8.x] Fixed #24635 -- Updated deprecated urls.py examples in default project template.
    Backport of 0b5abb03218ff0c575cee0c83e446bab81cbfadd from master

--- django/conf/project_template/project_name/urls.py
+++ django/conf/project_template/project_name/urls.py
@@ -1,10 +1,21 @@
+"""{{ project_name }} URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/{{ docs_version }}/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
 from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', '{{ project_name }}.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 ]
