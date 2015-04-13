765dcc08c710d7bb73032bdb617c0890f982738c
Jay Wineinger <jay.wineinger@gmail.com>
2015-04-13 07:30:59 -0500

    Fix test broken in 99d1636

--- tests/migrations/test_autodetector.py
+++ tests/migrations/test_autodetector.py
@@ -1386,13 +1386,12 @@ class AutodetectorTests(TestCase):
         # Right number/type of migrations?
         self.assertNumberMigrations(changes, "testapp", 1)
         self.assertOperationTypes(changes, "testapp", 0, [
-            "RemoveField", "RemoveField", "RemoveField", "DeleteModel", "DeleteModel"
+            "RemoveField", "RemoveField", "DeleteModel", "DeleteModel"
         ])
-        self.assertOperationAttributes(changes, "testapp", 0, 0, name="publishers", model_name='author')
-        self.assertOperationAttributes(changes, "testapp", 0, 1, name="author", model_name='contract')
-        self.assertOperationAttributes(changes, "testapp", 0, 2, name="publisher", model_name='contract')
-        self.assertOperationAttributes(changes, "testapp", 0, 3, name="Author")
-        self.assertOperationAttributes(changes, "testapp", 0, 4, name="Contract")
+        self.assertOperationAttributes(changes, "testapp", 0, 0, name="author",  model_name='contract')
+        self.assertOperationAttributes(changes, "testapp", 0, 1, name="publisher",  model_name='contract')
+        self.assertOperationAttributes(changes, "testapp", 0, 2, name="Author")
+        self.assertOperationAttributes(changes, "testapp", 0, 3, name="Contract")
 
     def test_concrete_field_changed_to_many_to_many(self):
         """
