439eb7289b9a4cbd4e864019c0b5518a9a5b5e89
Tim Graham <timograham@gmail.com>
2015-04-16 13:44:07 -0400

    Fixed fixtures tests failure on Python 2 with non-ASCII path.

--- tests/fixtures/tests.py
+++ tests/fixtures/tests.py
@@ -345,7 +345,7 @@ class FixtureLoadingTests(DumpDataAssertMixin, TestCase):
     def test_loaddata_verbosity_three(self):
         output = six.StringIO()
         management.call_command('loaddata', 'fixture1.json', verbosity=3, stdout=output, stderr=output)
-        command_output = output.getvalue()
+        command_output = force_text(output.getvalue())
         self.assertIn(
             "\rProcessed 1 object(s).\rProcessed 2 object(s)."
             "\rProcessed 3 object(s).\rProcessed 4 object(s).\n",
