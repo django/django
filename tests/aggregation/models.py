d64fc25abdce6c83ebbc5d19a48c6669a5fb8ac5
Tim Graham <timograham@gmail.com>
2015-04-23 20:13:24 -0400

    Fixed #24649 -- Allowed using Avg aggregate on non-numeric field types.

--- tests/aggregation/models.py
+++ tests/aggregation/models.py
@@ -17,6 +17,7 @@ class Author(models.Model):
 class Publisher(models.Model):
     name = models.CharField(max_length=255)
     num_awards = models.IntegerField()
+    duration = models.DurationField(blank=True, null=True)
 
     def __str__(self):
         return self.name
