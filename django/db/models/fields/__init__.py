142383c08e992469665547665cb8bbba569b0a39
Pradeek <jpradeek@gmail.com>
2015-04-27 21:52:48 +0530

    Fixed #24708 -- Handled non-string values in GenericIPAddressField.to_python()

--- django/db/models/fields/__init__.py
+++ django/db/models/fields/__init__.py
@@ -1963,7 +1963,12 @@ class GenericIPAddressField(Field):
         return "GenericIPAddressField"
 
     def to_python(self, value):
-        if value and ':' in value:
+        if value is None:
+            return None
+        if not isinstance(value, six.string_types):
+            value = force_text(value)
+        value = value.strip()
+        if ':' in value:
             return clean_ipv6_address(value,
                 self.unpack_ipv4, self.error_messages['invalid'])
         return value
