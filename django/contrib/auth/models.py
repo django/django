e83f083b727b75ee57fae01b5d5e2dca6cfc98ee
Luis Del Giudice <luis.dg19@gmail.com>
2015-05-02 03:45:42 -0400

    Fixed Unnecesary kwarg passed in UserManager's _create_user

--- django/contrib/auth/models.py
+++ django/contrib/auth/models.py
@@ -170,14 +170,12 @@ class UserManager(BaseUserManager):
         """
         Creates and saves a User with the given username, email and password.
         """
-        now = timezone.now()
         if not username:
             raise ValueError('The given username must be set')
         email = self.normalize_email(email)
         user = self.model(username=username, email=email,
-                          is_staff=is_staff, is_active=True,
-                          is_superuser=is_superuser,
-                          date_joined=now, **extra_fields)
+                          is_staff=is_staff, is_superuser=is_superuser,
+                          **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
