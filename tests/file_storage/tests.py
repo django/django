121cf15a8ae82403b8f774425497a479262ff980
Abhaya Agarwal <abhaya@instascribe.com>
2015-05-03 09:40:24 +0530

    Fixed #24105 -- Called Storage.get_valid_name() when upload_to is callable

--- tests/file_storage/tests.py
+++ tests/file_storage/tests.py
@@ -597,6 +597,16 @@ class FileFieldStorageTests(SimpleTestCase):
         self.assertTrue(obj.random.name.endswith("/random_file"))
         obj.random.close()
 
+    def test_custom_valid_name_callable_upload_to(self):
+        """
+        Storage.get_valid_name() should be called when upload_to is a callable.
+        """
+        obj = Storage()
+        obj.custom_valid_name.save("random_file", ContentFile("random content"))
+        # CustomValidNameStorage.get_valid_name() appends '_valid' to the name
+        self.assertTrue(obj.custom_valid_name.name.endswith("/random_file_valid"))
+        obj.custom_valid_name.close()
+
     def test_filefield_pickling(self):
         # Push an object into the cache to make sure it pickles properly
         obj = Storage()
