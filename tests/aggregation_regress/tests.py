adc57632bc26cc8fe42bdb6aff463f883214980a
Anssi Kääriäinen <akaariai@gmail.com>
2015-05-05 14:44:33 +0300

    Fixed #24748 -- Fixed incorrect GROUP BY on MySQL in some queries
    When the query's model had a self-referential foreign key, the
    compiler.get_group_by() code incorrectly used the self-referential
    foreign key's column (for example parent_id) as GROUP BY clause
    when it should have used the model's primary key column (id).

--- tests/aggregation_regress/tests.py
+++ tests/aggregation_regress/tests.py
@@ -17,7 +17,7 @@ from django.utils import six
 
 from .models import (
     Alfa, Author, Book, Bravo, Charlie, Clues, Entries, HardbackBook, ItemTag,
-    Publisher, Store, WithManualPK,
+    Publisher, SelfRefFK, Store, WithManualPK,
 )
 
 
@@ -1277,3 +1277,15 @@ class JoinPromotionTests(TestCase):
     def test_non_nullable_fk_not_promoted(self):
         qs = Book.objects.annotate(Count('contact__name'))
         self.assertIn(' INNER JOIN ', str(qs.query))
+
+
+class SelfReferentialFKTests(TestCase):
+    def test_ticket_24748(self):
+        t1 = SelfRefFK.objects.create(name='t1')
+        SelfRefFK.objects.create(name='t2', parent=t1)
+        SelfRefFK.objects.create(name='t3', parent=t1)
+        self.assertQuerysetEqual(
+            SelfRefFK.objects.annotate(num_children=Count('children')).order_by('name'),
+            [('t1', 2), ('t2', 0), ('t3', 0)],
+            lambda x: (x.name, x.num_children)
+        )
