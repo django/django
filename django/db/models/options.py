6a7b27c74309fff77db3aab49eb6fc7ce9f37fb4
Tim Graham <timograham@gmail.com>
2015-05-08 13:18:48 -0400

    [1.8.x] Fixed #24758 -- Corrected Options.get_fields() docstring.
    Backport of b99114f67f0e5eba3b1d31641906b75a14fd2fa0 from master

--- django/db/models/options.py
+++ django/db/models/options.py
@@ -727,9 +727,9 @@ class Options(object):
 
     def get_fields(self, include_parents=True, include_hidden=False):
         """
-        Returns a list of fields associated to the model. By default will only
-        return forward fields. This can be changed by enabling or disabling
-        field types using the parameters:
+        Returns a list of fields associated to the model. By default, includes
+        forward and reverse fields, fields derived from inheritance, but not
+        hidden fields. The returned fields can be changed using the parameters:
 
         - include_parents: include fields derived from inheritance
         - include_hidden:  include fields that have a related_name that
