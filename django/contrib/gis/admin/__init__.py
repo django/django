0dc22eeaa8c88e38f76f1cc917f681764fb337c6
Tim Graham <timograham@gmail.com>
2015-05-15 09:33:14 -0400

    Added contrib.gis.admin.__all__

--- django/contrib/gis/admin/__init__.py
+++ django/contrib/gis/admin/__init__.py
@@ -1,8 +1,14 @@
 # Getting the normal admin routines, classes, and `site` instance.
-from django.contrib.admin import (  # NOQA: flake8 detects only the last __all__
+from django.contrib.admin import (
     autodiscover, site, AdminSite, ModelAdmin, StackedInline, TabularInline,
     HORIZONTAL, VERTICAL,
 )
 # Geographic admin options classes and widgets.
-from django.contrib.gis.admin.options import GeoModelAdmin, OSMGeoAdmin  # NOQA
-from django.contrib.gis.admin.widgets import OpenLayersWidget   # NOQA
+from django.contrib.gis.admin.options import GeoModelAdmin, OSMGeoAdmin
+from django.contrib.gis.admin.widgets import OpenLayersWidget
+
+__all__ = [
+    'autodiscover', 'site', 'AdminSite', 'ModelAdmin', 'StackedInline',
+    'TabularInline', 'HORIZONTAL', 'VERTICAL', 'GeoModelAdmin', 'OSMGeoAdmin',
+    'OpenLayersWidget',
+]
