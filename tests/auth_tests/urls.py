ae1efb853c159ae4747d105405694245f8508b4b
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2015-05-19 11:10:04 +0200

    Refs #24553 -- Fixed urlpatterns leakage in auth_tests

--- tests/auth_tests/urls.py
+++ tests/auth_tests/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.contrib.auth import views
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.urls import urlpatterns
+from django.contrib.auth.urls import urlpatterns as auth_urlpatterns
 from django.contrib.messages.api import info
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
@@ -65,7 +65,7 @@ def custom_request_auth_login(request):
     return views.login(request, authentication_form=CustomRequestAuthenticationForm)
 
 # special urls for auth test cases
-urlpatterns += [
+urlpatterns = auth_urlpatterns + [
     url(r'^logout/custom_query/$', views.logout, dict(redirect_field_name='follow')),
     url(r'^logout/next_page/$', views.logout, dict(next_page='/somewhere/')),
     url(r'^logout/next_page/named/$', views.logout, dict(next_page='password_reset')),
