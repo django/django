525fd2f4055b206a57324480bbbe5b3e8f88ff91
Tim Graham <timograham@gmail.com>
2015-05-20 13:05:41 -0400

    [1.8.x] Fixed typo in file_storage tests.
    Backport of e2b77aceddbda9071fcfc38f90fb50d091d0b5fc from master

--- tests/file_storage/tests.py
+++ tests/file_storage/tests.py
@@ -249,7 +249,7 @@ class FileStorageTests(unittest.TestCase):
         self.assertEqual(self.storage.url(r"""~!*()'@#$%^&*abc`+ =.file"""),
             """/test_media_url/~!*()'%40%23%24%25%5E%26*abc%60%2B%20%3D.file""")
 
-        # should stanslate os path separator(s) to the url path separator
+        # should translate os path separator(s) to the url path separator
         self.assertEqual(self.storage.url("""a/b\\c.file"""),
             """/test_media_url/a/b/c.file""")
 
