c83bfa90be8ae0af4df139b3ef418233e73db0bc
Villiers Strauss <villiers.strauss@logimeter.com>
2015-05-22 20:18:15 +0200

    Remove debug line

--- tests/postgres_tests/test_ranges.py
+++ tests/postgres_tests/test_ranges.py
@@ -472,7 +472,6 @@ class TestFormField(TestCase):
         field = pg_forms.DateTimeRangeField()
         value = field.prepare_value(DateTimeTZRange(
             datetime.datetime(2015, 5, 22, 16, 6, 33, tzinfo=timezone.utc), None))
-        print value
         self.assertEqual(value, [datetime.datetime(2015, 5, 22, 18, 6, 33), None])
 
     def test_model_field_formfield_integer(self):
