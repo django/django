614bec41b5306b32b8de20d66f047ff8d6f35256
Villiers Strauss <villiers.strauss@logimeter.com>
2015-05-22 15:54:31 +0200

    Fixed #24841 -- Made BaseRangeField.prepare_value() call base_field's prepare_value()

--- django/contrib/postgres/forms/ranges.py
+++ django/contrib/postgres/forms/ranges.py
@@ -22,10 +22,17 @@ class BaseRangeField(forms.MultiValueField):
         super(BaseRangeField, self).__init__(**kwargs)
 
     def prepare_value(self, value):
+        lower_base, upper_base = self.fields
         if isinstance(value, self.range_type):
-            return [value.lower, value.upper]
+            return [
+                lower_base.prepare_value(value.lower),
+                upper_base.prepare_value(value.upper),
+            ]
         if value is None:
-            return [None, None]
+            return [
+                lower_base.prepare_value(None),
+                upper_base.prepare_value(None),
+            ]
         return value
 
     def compress(self, values):
