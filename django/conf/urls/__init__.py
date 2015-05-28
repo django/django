99d725fd02e9cc3326dafdcd958cf7af0bc1c3b4
Marten Kenbeek <marten.knbk@gmail.com>
2015-05-28 17:25:52 +0200

    Fixed #21927 -- Made application and instance namespaces more distinct.
    Made URL application namespaces be set in the included URLconf, and
    instance namespaces in the call to include(). Deprecated other ways
    to set application and instance namespaces.

--- django/conf/urls/__init__.py
+++ django/conf/urls/__init__.py
@@ -5,7 +5,7 @@ from django.core.urlresolvers import (RegexURLPattern,
     RegexURLResolver, LocaleRegexURLResolver)
 from django.core.exceptions import ImproperlyConfigured
 from django.utils import six
-from django.utils.deprecation import RemovedInDjango20Warning
+from django.utils.deprecation import RemovedInDjango20Warning, RemovedInDjango21Warning
 
 
 __all__ = ['handler400', 'handler403', 'handler404', 'handler500', 'include', 'patterns', 'url']
@@ -19,12 +19,29 @@ handler500 = 'django.views.defaults.server_error'
 def include(arg, namespace=None, app_name=None):
     if app_name and not namespace:
         raise ValueError('Must specify a namespace if specifying app_name.')
+    if app_name:
+        warnings.warn(
+            'The app_name argument to django.conf.urls.include() is deprecated. '
+            'Set the app_name in the included URLconf instead.',
+            RemovedInDjango21Warning, stacklevel=2
+        )
 
     if isinstance(arg, tuple):
         # callable returning a namespace hint
-        if namespace:
-            raise ImproperlyConfigured('Cannot override the namespace for a dynamic module that provides a namespace')
-        urlconf_module, app_name, namespace = arg
+        try:
+            urlconf_module, app_name = arg
+        except ValueError:
+            if namespace:
+                raise ImproperlyConfigured(
+                    'Cannot override the namespace for a dynamic module that provides a namespace'
+                )
+            warnings.warn(
+                'Passing a 3-tuple to django.conf.urls.include() is deprecated. '
+                'Pass a 2-tuple containing the list of patterns and app_name, '
+                'and provide the namespace argument to include() instead.',
+                RemovedInDjango21Warning, stacklevel=2
+            )
+            urlconf_module, app_name, namespace = arg
     else:
         # No namespace hint - use manually provided namespace
         urlconf_module = arg
@@ -32,6 +49,17 @@ def include(arg, namespace=None, app_name=None):
     if isinstance(urlconf_module, six.string_types):
         urlconf_module = import_module(urlconf_module)
     patterns = getattr(urlconf_module, 'urlpatterns', urlconf_module)
+    app_name = getattr(urlconf_module, 'app_name', app_name)
+    if namespace and not app_name:
+        warnings.warn(
+            'Specifying a namespace in django.conf.urls.include() without '
+            'providing an app_name is deprecated. Set the app_name attribute '
+            'in the included module, or pass a 2-tuple containing the list of '
+            'patterns and app_name instead.',
+            RemovedInDjango21Warning, stacklevel=2
+        )
+
+    namespace = namespace or app_name
 
     # Make sure we can iterate through the patterns (without this, some
     # testcases will break).
