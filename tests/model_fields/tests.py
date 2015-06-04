461473873131141955882318aa577d5ba9de30bc
Rolo <rolo@wildfish.com>
2015-06-04 12:09:18 +0100

    Switched TextField test to use TextField rather than CharField.

--- tests/model_fields/tests.py
+++ tests/model_fields/tests.py
@@ -249,7 +249,7 @@ class ManyToManyFieldTests(test.SimpleTestCase):
 class TextFieldTests(test.TestCase):
     def test_to_python(self):
         """TextField.to_python should return a string"""
-        f = models.CharField()
+        f = models.TextField()
         self.assertEqual(f.to_python(1), '1')
 
 
