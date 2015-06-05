5c35b4a8c90fbd5658352ef3ce69bcc7ba25324b
Russell Keith-Magee <russell@keith-magee.com>
2015-06-05 11:42:58 +0100

    Added release notes for feature added in fe21fb81

--- django/forms/formsets.py
+++ django/forms/formsets.py
@@ -147,6 +147,9 @@ class BaseFormSet(object):
     def get_form_kwargs(self, index):
         """
         Return additional keyword arguments for each individual formset form.
+
+        index will be None if the form being constructed is a new empty
+        form.
         """
         return self.form_kwargs.copy()
 
