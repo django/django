208d941d9a7935df33d8b1d8f579ee0eb5ec7baa
Markus Holtermann <info@markusholtermann.eu>
2015-06-05 02:06:13 +0200

    Fixed #24927 -- Relied on six's python_2_unicode_compatible implementation

--- django/utils/encoding.py
+++ django/utils/encoding.py
@@ -8,6 +8,7 @@ from decimal import Decimal
 
 from django.utils import six
 from django.utils.functional import Promise
+from django.utils.six import python_2_unicode_compatible  # NOQA
 from django.utils.six.moves.urllib.parse import quote, unquote
 
 if six.PY3:
@@ -23,24 +24,6 @@ class DjangoUnicodeDecodeError(UnicodeDecodeError):
         original = UnicodeDecodeError.__str__(self)
         return '%s. You passed in %r (%s)' % (original, self.obj,
                 type(self.obj))
-
-
-def python_2_unicode_compatible(klass):
-    """
-    A decorator that defines __unicode__ and __str__ methods under Python 2.
-    Under Python 3 it does nothing.
-
-    To support Python 2 and 3 with a single code base, define a __str__ method
-    returning text and apply this decorator to the class.
-    """
-    if six.PY2:
-        if '__str__' not in klass.__dict__:
-            raise ValueError("@python_2_unicode_compatible cannot be applied "
-                             "to %s because it doesn't define __str__()." %
-                             klass.__name__)
-        klass.__unicode__ = klass.__str__
-        klass.__str__ = lambda self: self.__unicode__().encode('utf-8')
-    return klass
 
 
 def smart_text(s, encoding='utf-8', strings_only=False, errors='strict'):
