a50b66da30320887c23c73927f6b2ab41e0301bf
Jason Hoos <jhoos@maestrohealth.com>
2015-06-05 15:04:24 -0500

    Fixed #24958 -- Fixed inline forms using UUID-PK parents with auto-PK children.

--- tests/model_formsets/test_uuid.py
+++ tests/model_formsets/test_uuid.py
@@ -1,7 +1,11 @@
 from django.forms.models import inlineformset_factory
 from django.test import TestCase
 
-from .models import UUIDPKChild, UUIDPKParent
+from .models import (
+    AutoPKChildOfUUIDPKParent, AutoPKParent, ChildRelatedViaAK,
+    ChildWithEditablePK, ParentWithUUIDAlternateKey, UUIDPKChild,
+    UUIDPKChildOfAutoPKParent, UUIDPKParent,
+)
 
 
 class InlineFormsetTests(TestCase):
@@ -10,6 +14,8 @@ class InlineFormsetTests(TestCase):
         #24377 - If we're adding a new object, a parent's auto-generated pk
         from the model field default should be ignored as it's regenerated on
         the save request.
+
+        Tests the case where both the parent and child have a UUID primary key.
         """
         FormSet = inlineformset_factory(UUIDPKParent, UUIDPKChild, fields='__all__')
         formset = FormSet()
@@ -30,3 +36,43 @@ class InlineFormsetTests(TestCase):
             'uuidpkchild_set-2-name': '',
         })
         self.assertTrue(formset.is_valid())
+
+    def test_inlineformset_factory_nulls_default_pks_uuid_parent_auto_child(self):
+        """
+        #24958 - Variant of test_inlineformset_factory_nulls_default_pks for
+        the case of a parent object with a UUID primary key and a child object
+        with an AutoField primary key.
+        """
+        FormSet = inlineformset_factory(UUIDPKParent, AutoPKChildOfUUIDPKParent, fields='__all__')
+        formset = FormSet()
+        self.assertIsNone(formset.forms[0].fields['parent'].initial)
+
+    def test_inlineformset_factory_nulls_default_pks_auto_parent_uuid_child(self):
+        """
+        #24958 - Variant of test_inlineformset_factory_nulls_default_pks for
+        the case of a parent object with an AutoField primary key and a child
+        object with a UUID primary key.
+        """
+        FormSet = inlineformset_factory(AutoPKParent, UUIDPKChildOfAutoPKParent, fields='__all__')
+        formset = FormSet()
+        self.assertIsNone(formset.forms[0].fields['parent'].initial)
+
+    def test_inlineformset_factory_nulls_default_pks_child_editable_pk(self):
+        """
+        #24958 - Variant of test_inlineformset_factory_nulls_default_pks for
+        the case of a parent object with a UUID primary key and a child
+        object with an editable natural key for a primary key.
+        """
+        FormSet = inlineformset_factory(UUIDPKParent, ChildWithEditablePK, fields='__all__')
+        formset = FormSet()
+        self.assertIsNone(formset.forms[0].fields['parent'].initial)
+
+    def test_inlineformset_factory_nulls_default_pks_alternate_key_relation(self):
+        """
+        #24958 - Variant of test_inlineformset_factory_nulls_default_pks for
+        the case of a parent object with a UUID alternate key and a child
+        object that relates to that alternate key.
+        """
+        FormSet = inlineformset_factory(ParentWithUUIDAlternateKey, ChildRelatedViaAK, fields='__all__')
+        formset = FormSet()
+        self.assertIsNone(formset.forms[0].fields['parent'].initial)
