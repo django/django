471756d69639e05044404617c703e3f53a38ab18
Ian Foote <ian@incuna.com>
2015-06-05 17:57:37 +0100

    Add WIP fix for Cast on MySQL

--- django/db/models/expressions.py
+++ django/db/models/expressions.py
@@ -957,10 +957,21 @@ class OrderBy(BaseExpression):
 
 class Cast(Func):
     """Coerce a lookup to a new field type."""
+    mysql_types = {
+        fields.CharField: 'char',
+        fields.IntegerField: 'signed integer',
+        fields.FloatField: 'signed',
+    }
+
     def __init__(self, lookup, output_field):
         super(Cast, self).__init__(lookup, output_field=output_field)
 
     def as_sql(self, compiler, connection):
         sql, params = compiler.compile(self.get_source_expressions()[0])
         database_type = self._output_field.db_type(connection)
+        return 'Cast({} as {})'.format(sql, database_type), params
+
+    def as_mysql(self, compiler, connection):
+        sql, params = compiler.compile(self.get_source_expressions()[0])
+        database_type = self.mysql_types[type(self._output_field)]
         return 'Cast({} as {})'.format(sql, database_type), params
