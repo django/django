2d8b7204d238c6f2660061179b101013a3e420f3
Marten Kenbeek <marten.knbk@gmail.com>
2015-06-15 16:06:32 +0200

    URL post-nuke.

--- django/core/urls/utils.py
+++ django/core/urls/utils.py
@@ -53,76 +53,13 @@ class URL(object):
         return "<URL '%s'>" % urlunsplit((self.scheme, self.host, self.path, self.query_string, self.fragment))
 
     def __str__(self):
-        return self.get_full_path()
-
-    def get_full_path(self, force_append_slash=False):
-        return '%s%s%s%s' % (
+        return iri_to_uri(urlunsplit((
+            self.scheme,
+            self.host,
             escape_uri_path(self.path) or '/',
-            '/' if force_append_slash and not self.path.endswith('/') else '',
-            ('?' + escape_query_string(self.query_string)) if self.query_string else '',
-            ('#' + iri_to_uri(self.fragment)) if self.fragment else '',
-        )
-
-    def build_absolute_path(self, location):
-        """
-        Build an absolute URL path for location, relative to current location.
-        """
-        if not location:
-            path = self.path
-        elif location.startswith('/'):
-            path = location
-        else:
-            path = '%s/%s' % (self.path.rsplit('/', 1)[0], location)
-        return resolve_dot_segments(path) or '/'
-
-    def build_relative_url(self, location=None):
-        """
-        Build a URL for location, relative to the current location. May be
-        scheme- and host-relative for same-domain URLs, but always build an
-        absolute path.
-        """
-        if location is None:
-            return self.get_full_path()
-        if isinstance(location, URL):
-            location = location.build_absolute_url()
-        bits = urlsplit(location)
-        set_scheme = not (bits.scheme == self.scheme or not bits.scheme)
-        set_host = not (bits.netloc.lower() == self.host or not bits.netloc)
-        if not set_scheme and not set_host:
-            return self.build_absolute_path(location)
-        if not set_scheme or not self.scheme:
-            current_uri = '//{host}{path}'.format(host=self.host,
-                                                  path=self.path)
-        else:
-            current_uri = '{scheme}://{host}{path}'.format(scheme=self.scheme,
-                                                           host=self.host,
-                                                           path=self.path)
-        return urljoin(current_uri, location)
-
-    def build_absolute_url(self, location=None):
-        """
-        Builds an absolute URI from the location and the variables available in
-        this request. If no ``location`` is specified, the absolute URI is
-        built on ``request.get_full_path()``. Anyway, if the location is
-        absolute, it is simply converted to an RFC 3987 compliant URI and
-        returned and if location is relative or is scheme-relative (i.e.,
-        ``//example.com/``), it is urljoined to a base URL constructed from the
-        request variables.
-        """
-        if location is None:
-            # Make it an absolute url (but schemeless and domainless) for the
-            # edge case that the path starts with '//'.
-            location = '//%s' % self.get_full_path()
-        bits = urlsplit(location)
-        if not (bits.scheme and bits.netloc):
-            current_uri = '{scheme}://{host}{path}'.format(scheme=self.scheme,
-                                                           host=self.host,
-                                                           path=self.path)
-            # Join the constructed URL with the provided location, which will
-            # allow the provided ``location`` to apply query strings to the
-            # base path as well as override the host, if it begins with //
-            location = urljoin(current_uri, location)
-        return location
+            escape_query_string(self.query_string),
+            iri_to_uri(self.fragment)
+        )))
 
     def copy(self):
         return type(self)(
--- tests/urls/test_utils.py
+++ tests/urls/test_utils.py
@@ -4,224 +4,36 @@ from __future__ import unicode_literals
 from django.core.urls import URL
 from django.test import SimpleTestCase
 
-get_full_path_data = (
-    # format: (path, query_string, fragment, force_append_slash, expected)
-    ('/test/', 'query=true', 'fragment', False, '/test/?query=true#fragment'),
+full_path_data = (
+    # format: (path, query_string, fragment, expected)
+    ('/test/', 'query=true', 'fragment', '/test/?query=true#fragment'),
     # and empty path still returns a slash
-    ('', '', '', False, '/'),
+    ('', '', '', '/'),
     # ?, = and ; in the path must be encoded
-    ('/test/?=;', '', '', False, '/test/%3F%3D%3B'),
+    ('/test/?=;', '', '', '/test/%3F%3D%3B'),
     # other "reserved" characters don't have to be encoded
-    ('/test/@&$', '', '', False, '/test/@&$'),
+    ('/test/@&$', '', '', '/test/@&$'),
     # # in query string must be encoded
-    ('/test/', 'query=#hashtag', 'fragment', False, '/test/?query=%23hashtag#fragment'),
+    ('/test/', 'query=#hashtag', 'fragment', '/test/?query=%23hashtag#fragment'),
     # / and ? in query string are allowed
-    ('/test/', 'query/string=what?', '', False, '/test/?query/string=what?'),
+    ('/test/', 'query/string=what?', '', '/test/?query/string=what?'),
     # # in fragment is allowed
-    ('/test/', '', '#fragment#', False, '/test/##fragment#'),
-    # force_append_slash adds a slash if path doesn't end with one
-    ('/test', '', '', True, '/test/'),
-    # even with a query string and/or fragment
-    ('/test', 'query=true', 'fragment', True, '/test/?query=true#fragment'),
-    # but not if it already ends with a slash
-    ('/test/', '', '', True, '/test/'),
+    ('/test/', '', '#fragment#', '/test/##fragment#'),
     # characters outside ASCII range are encoded in the path
-    ('/I ♥ Django/', '', '', False, '/I%20%E2%99%A5%20Django/'),
+    ('/I ♥ Django/', '', '', '/I%20%E2%99%A5%20Django/'),
     # and in the query string
-    ('/test/', 'django=♥', '', False, '/test/?django=%E2%99%A5'),
+    ('/test/', 'django=♥', '', '/test/?django=%E2%99%A5'),
     # and in the fragment
-    ('/test/', '', '♥', False, '/test/#%E2%99%A5'),
-)
-
-build_absolute_path_data = (
-    # format: (current_path, location, expected)
-    ('', '', '/'),
-    ('/', '/test/', '/test/'),
-    ('/', 'test/', '/test/'),
-    ('/test/', '', '/test/'),
-    ('/test/', '.', '/test/'),
-    ('/test/', '../', '/'),
-    ('/test/', 'relative', '/test/relative'),
-    ('/test/', 'relative/', '/test/relative/'),
-    ('/test', 'relative', '/relative'),
-    ('/test', 'relative/', '/relative/'),
-    ('/test', '.', '/'),
-    ('/test', '..', '/'),
-    # Examples from RFC3986 section 5.4
-    ('/b/c/d', 'g', '/b/c/g'),
-    ('/b/c/d', './g', '/b/c/g'),
-    ('/b/c/d', 'g/', '/b/c/g/'),
-    ('/b/c/d', 'g', '/b/c/g'),
-    ('/b/c/d', '', '/b/c/d'),
-    ('/b/c/d', '.', '/b/c/'),
-    ('/b/c/d', './', '/b/c/'),
-    ('/b/c/d', '..', '/b/'),
-    ('/b/c/d', '../', '/b/'),
-    ('/b/c/d', '../g', '/b/g'),
-    ('/b/c/d', '../..', '/'),
-    ('/b/c/d', '../../', '/'),
-    ('/b/c/d', '../../g', '/g'),
-
-    ('/b/c/d', '../../../g', '/g'),
-    ('/b/c/d', '../../../../g', '/g'),
-    ('/b/c/d', '/./g', '/g'),
-    ('/b/c/d', '/../g', '/g'),
-    ('/b/c/d', 'g.', '/b/c/g.'),
-    ('/b/c/d', '.g', '/b/c/.g'),
-    ('/b/c/d', 'g..', '/b/c/g..'),
-    ('/b/c/d', '..g', '/b/c/..g'),
-
-    ('/b/c/d', './../g', '/b/g'),
-    ('/b/c/d', './g/.', '/b/c/g/'),
-    ('/b/c/d', 'g/./h', '/b/c/g/h'),
-    ('/b/c/d', 'g/../h', '/b/c/h'),
-    ('/b/c/d', 'g', '/b/c/g'),
-)
-
-
-build_absolute_uri_data = (
-    ('http://example.org/current/path/', '/test/', 'http://example.org/test/'),
-    ('http://example.org/current/path/', 'test/', 'http://example.org/current/path/test/'),
-    ('http://example.org/current/path', 'test/', 'http://example.org/current/test/'),
-    ('http://example.org:8000/current/path/', '/test/', 'http://example.org:8000/test/'),
-    ('http://example.org:8000/current/path/', 'test/', 'http://example.org:8000/current/path/test/'),
-    ('http://example.org:8000/current/path', 'test/', 'http://example.org:8000/current/test/'),
-
-    ('http://example.org/current/path/', '//test.com/test/', 'http://test.com/test/'),
-    ('http://example.org/current/path/', 'test/', 'http://example.org/current/path/test/'),
-    ('http://example.org/current/path/', 'test/', 'http://example.org/current/path/test/'),
-    ('http://example.org/current/path/', 'test/', 'http://example.org/current/path/test/'),
-    ('http://example.org/current/path/', 'test/', 'http://example.org/current/path/test/'),
-    ('http://example.org/current/path/', 'test/', 'http://example.org/current/path/test/'),
-    ('http://example.org/current/path/', 'test/', 'http://example.org/current/path/test/'),
-    ('http://example.org/current/path/', 'test/', 'http://example.org/current/path/test/'),
-    ('/test/', '/', '/'),
-    ('//example.org/', '/test/', '//example.org/test/'),
-    # Examples from RFC 3986 section 5.4
-    ("http://a/b/c/d;p?q", "g:h", "g:h"),
-    ("http://a/b/c/d;p?q", "g", "http://a/b/c/g"),
-    ("http://a/b/c/d;p?q", "./g", "http://a/b/c/g"),
-    ("http://a/b/c/d;p?q", "g/", "http://a/b/c/g/"),
-    ("http://a/b/c/d;p?q", "/g", "http://a/g"),
-    ("http://a/b/c/d;p?q", "//g", "http://g"),
-    ("http://a/b/c/d;p?q", "?y", "http://a/b/c/d;p?y"),
-    ("http://a/b/c/d;p?q", "g?y", "http://a/b/c/g?y"),
-    ("http://a/b/c/d;p?q", "#s", "http://a/b/c/d;p?q#s"),
-    ("http://a/b/c/d;p?q", "g#s", "http://a/b/c/g#s"),
-    ("http://a/b/c/d;p?q", "g?y#s", "http://a/b/c/g?y#s"),
-    ("http://a/b/c/d;p?q", ";x", "http://a/b/c/;x"),
-    ("http://a/b/c/d;p?q", "g;x",  "http://a/b/c/g;x"),
-    ("http://a/b/c/d;p?q", "g;x?y#s", "http://a/b/c/g;x?y#s"),
-    ("http://a/b/c/d;p?q", "", "http://a/b/c/d;p?q"),
-    ("http://a/b/c/d;p?q", ".", "http://a/b/c/"),
-    ("http://a/b/c/d;p?q", "./", "http://a/b/c/"),
-    ("http://a/b/c/d;p?q", "..", "http://a/b/"),
-    ("http://a/b/c/d;p?q", "../", "http://a/b/"),
-    ("http://a/b/c/d;p?q", "../g", "http://a/b/g"),
-    ("http://a/b/c/d;p?q", "../..", "http://a/"),
-    ("http://a/b/c/d;p?q", "../../", "http://a/"),
-    ("http://a/b/c/d;p?q", "../../g", "http://a/g"),
-    ("http://a/b/c/d;p?q", "../../../g", "http://a/g"),
-    ("http://a/b/c/d;p?q", "../../../../g", "http://a/g"),
-    ("http://a/b/c/d;p?q", "/./g", "http://a/g"),
-    ("http://a/b/c/d;p?q", "/../g", "http://a/g"),
-    ("http://a/b/c/d;p?q", "g.", "http://a/b/c/g."),
-    ("http://a/b/c/d;p?q", ".g", "http://a/b/c/.g"),
-    ("http://a/b/c/d;p?q", "g..", "http://a/b/c/g.."),
-    ("http://a/b/c/d;p?q", "..g", "http://a/b/c/..g"),
-    ("http://a/b/c/d;p?q", "./../g", "http://a/b/g"),
-    ("http://a/b/c/d;p?q", "./g/.", "http://a/b/c/g/"),
-    ("http://a/b/c/d;p?q", "g/./h", "http://a/b/c/g/h"),
-    ("http://a/b/c/d;p?q", "g/../h", "http://a/b/c/h"),
-    ("http://a/b/c/d;p?q", "g;x=1/./y", "http://a/b/c/g;x=1/y"),
-    ("http://a/b/c/d;p?q", "g;x=1/../y", "http://a/b/c/y"),
-    ("http://a/b/c/d;p?q", "g?y/./x", "http://a/b/c/g?y/./x"),
-    ("http://a/b/c/d;p?q", "g?y/../x", "http://a/b/c/g?y/../x"),
-    ("http://a/b/c/d;p?q", "g#s/./x", "http://a/b/c/g#s/./x"),
-    ("http://a/b/c/d;p?q", "g#s/../x", "http://a/b/c/g#s/../x"),
-)
-
-
-build_relative_uri_data = (
-
-    # Examples from RFC 3986 section 5.4
-    ("http://a/b/c/d;p?q", "g:h", "g:h"),
-    ("http://a/b/c/d;p?q", "g", "/b/c/g"),
-    ("http://a/b/c/d;p?q", "./g", "/b/c/g"),
-    ("http://a/b/c/d;p?q", "g/", "/b/c/g/"),
-    ("http://a/b/c/d;p?q", "/g", "/g"),
-    ("http://a/b/c/d;p?q", "//g", "//g"),
-    ("http://a/b/c/d;p?q", "?y", "/b/c/d;p?y"),
-    ("http://a/b/c/d;p?q", "g?y", "/b/c/g?y"),
-    ("http://a/b/c/d;p?q", "#s", "/b/c/d;p?q#s"),
-    ("http://a/b/c/d;p?q", "g#s", "/b/c/g#s"),
-    ("http://a/b/c/d;p?q", "g?y#s", "/b/c/g?y#s"),
-    ("http://a/b/c/d;p?q", ";x", "/b/c/;x"),
-    ("http://a/b/c/d;p?q", "g;x",  "/b/c/g;x"),
-    ("http://a/b/c/d;p?q", "g;x?y#s", "/b/c/g;x?y#s"),
-    ("http://a/b/c/d;p?q", "", "/b/c/d;p?q"),
-    ("http://a/b/c/d;p?q", ".", "/b/c/"),
-    ("http://a/b/c/d;p?q", "./", "/b/c/"),
-    ("http://a/b/c/d;p?q", "..", "/b/"),
-    ("http://a/b/c/d;p?q", "../", "/b/"),
-    ("http://a/b/c/d;p?q", "../g", "/b/g"),
-    ("http://a/b/c/d;p?q", "../..", "/"),
-    ("http://a/b/c/d;p?q", "../../", "/"),
-    ("http://a/b/c/d;p?q", "../../g", "/g"),
-    ("http://a/b/c/d;p?q", "../../../g", "/g"),
-    ("http://a/b/c/d;p?q", "../../../../g", "/g"),
-    ("http://a/b/c/d;p?q", "/./g", "/g"),
-    ("http://a/b/c/d;p?q", "/../g", "/g"),
-    ("http://a/b/c/d;p?q", "g.", "/b/c/g."),
-    ("http://a/b/c/d;p?q", ".g", "/b/c/.g"),
-    ("http://a/b/c/d;p?q", "g..", "/b/c/g.."),
-    ("http://a/b/c/d;p?q", "..g", "/b/c/..g"),
-    ("http://a/b/c/d;p?q", "./../g", "/b/g"),
-    ("http://a/b/c/d;p?q", "./g/.", "/b/c/g/"),
-    ("http://a/b/c/d;p?q", "g/./h", "/b/c/g/h"),
-    ("http://a/b/c/d;p?q", "g/../h", "/b/c/h"),
-    ("http://a/b/c/d;p?q", "g;x=1/./y", "/b/c/g;x=1/y"),
-    ("http://a/b/c/d;p?q", "g;x=1/../y", "/b/c/y"),
-    ("http://a/b/c/d;p?q", "g?y/./x", "/b/c/g?y/./x"),
-    ("http://a/b/c/d;p?q", "g?y/../x", "/b/c/g?y/../x"),
-    ("http://a/b/c/d;p?q", "g#s/./x", "/b/c/g#s/./x"),
-    ("http://a/b/c/d;p?q", "g#s/../x", "/b/c/g#s/../x"),
+    ('/test/', '', '♥', '/test/#%E2%99%A5'),
 )
 
 
 class URLTests(SimpleTestCase):
-    def test_url_get_full_path(self):
-        self.assertTrue(get_full_path_data, "get_full_path_data is empty.")
-        for path, query_string, fragment, force_append_slash, expected in get_full_path_data:
-            url = URL(path=path, query_string=query_string, fragment=fragment)
-            self.assertEqual(url.get_full_path(force_append_slash), expected)
-
-    def test_url_build_absolute_path(self):
-        self.assertTrue(build_absolute_path_data, "build_absolute_path_data is empty.")
-        for current_path, location, expected in build_absolute_path_data:
-            url = URL(path=current_path)
+    def test_url_path(self):
+        for path, query, fragment, expected in full_path_data:
+            url = URL(path=path, query_string=query, fragment=fragment)
             self.assertEqual(
-                url.build_absolute_path(location), expected,
-                'URL(path=%r).build_absolute_path(%r) did not resolve to %r' %
-                (current_path, location, expected)
-            )
-
-    def test_url_build_absolute_uri(self):
-        self.assertTrue(build_absolute_uri_data, "build_absolute_uri_data is empty.")
-        for current_uri, location, expected in build_absolute_uri_data:
-            url = URL.from_location(current_uri)
-            self.assertEqual(
-                url.build_absolute_url(location), expected,
-                "URL.from_location(%r).build_absolute_url(%r) did not resolve to %r" %
-                (current_uri, location, expected)
-            )
-
-    def test_url_build_relative_uri(self):
-        self.assertTrue(build_relative_uri_data, "build_relative_uri_data is empty.")
-        for current_uri, location, expected in build_relative_uri_data:
-            url = URL.from_location(current_uri)
-            self.assertEqual(
-                url.build_relative_url(location), expected,
-                "URL.from_location(%r).build_relative_url(%r) did not resolve to %r" %
-                (current_uri, location, expected)
+                str(url), expected,
+                "URL(path=%s, query_string=%s, fragment=%s) does not match %s" %
+                (path, query, fragment, expected),
             )
