f2ec7e89cff08daeb8deb3790be6f67b185a2d9a
Noam <noamkush@gmail.com>
2015-06-25 13:46:11 +0300

    Fixed #25031 -- Fixed a regression in the unordered_list template filter.

--- tests/template_tests/filter_tests/test_unordered_list.py
+++ tests/template_tests/filter_tests/test_unordered_list.py
@@ -83,6 +83,13 @@ class FunctionTests(SimpleTestCase):
             '</li>\n\t</ul>\n\t</li>\n\t<li>item 2</li>',
         )
 
+    def test_nested3(self):
+        self.assertEqual(
+            unordered_list(['item 1', 'item 2', ['item 2.1']]),
+            '\t<li>item 1</li>\n\t<li>item 2\n\t<ul>\n\t\t<li>item 2.1'
+            '</li>\n\t</ul>\n\t</li>',
+        )
+
     def test_nested_multiple(self):
         self.assertEqual(
             unordered_list(['item 1', ['item 1.1', ['item 1.1.1', ['item 1.1.1.1']]]]),
