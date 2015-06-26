639dc47dc88b6c7ae0b28b668560d8187342b879
Marc Tamlyn <marc.tamlyn@gmail.com>
2015-06-26 18:47:06 +0100

    Robustness of test setup.

--- django/test/testcases.py
+++ django/test/testcases.py
@@ -160,6 +160,7 @@ class SimpleTestCase(unittest.TestCase):
     client_class = Client
     _overridden_settings = None
     _modified_settings = None
+    _extensions = []
 
     # Tests shouldn't be allowed to query the database since
     # this base class doesn't enforce any isolation.
