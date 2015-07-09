11e6bf9bdfaecd5325686599af5b21b5b945e61c
Simon Charette <charette.s@gmail.com>
2015-07-09 14:44:16 -0400

    Fixed a CaptureQueriesContext usage in queries tests.

--- tests/queries/tests.py
+++ tests/queries/tests.py
@@ -1950,7 +1950,7 @@ class ExistsSql(TestCase):
             self.assertFalse(Tag.objects.exists())
         # Ok - so the exist query worked - but did it include too many columns?
         self.assertEqual(len(captured_queries), 1)
-        qstr = captured_queries[0]
+        qstr = captured_queries[0]['sql']
         id, name = connection.ops.quote_name('id'), connection.ops.quote_name('name')
         self.assertNotIn(id, qstr)
         self.assertNotIn(name, qstr)
