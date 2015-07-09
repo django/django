a57baa8912903bc113f7bc3ebb97834feb680920
Vlastimil Zíma <vlastimil.zima@nic.cz>
2015-07-09 17:27:07 +0200

    Fixed #25099 -- Cleaned up HttpRequest representations in error reporting.

--- django/http/__init__.py
+++ django/http/__init__.py
@@ -1,7 +1,6 @@
 from django.http.cookie import SimpleCookie, parse_cookie
 from django.http.request import (
     HttpRequest, QueryDict, RawPostDataException, UnreadablePostError,
-    build_request_repr,
 )
 from django.http.response import (
     BadHeaderError, FileResponse, Http404, HttpResponse,
@@ -14,7 +13,7 @@ from django.http.utils import conditional_content_removal
 
 __all__ = [
     'SimpleCookie', 'parse_cookie', 'HttpRequest', 'QueryDict',
-    'RawPostDataException', 'UnreadablePostError', 'build_request_repr',
+    'RawPostDataException', 'UnreadablePostError',
     'HttpResponse', 'StreamingHttpResponse', 'HttpResponseRedirect',
     'HttpResponsePermanentRedirect', 'HttpResponseNotModified',
     'HttpResponseBadRequest', 'HttpResponseForbidden', 'HttpResponseNotFound',
