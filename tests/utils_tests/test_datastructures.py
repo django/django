f675afa13ce89ea25a9798b2f7a57b091bb83dfa
darkryder <sambhav13085@iiitd.ac.in>
2015-07-09 23:30:36 +0530

    Fixed #25093 -- Added utils.datastructures.OrderedSet.__len__()

--- tests/utils_tests/test_datastructures.py
+++ tests/utils_tests/test_datastructures.py
@@ -21,6 +21,14 @@ class OrderedSetTests(SimpleTestCase):
         s.add(1)
         self.assertTrue(s)
 
+    def test_len(self):
+        s = OrderedSet()
+        self.assertEqual(len(s), 0)
+        s.add(1)
+        s.add(2)
+        s.add(2)
+        self.assertEqual(len(s), 2)
+
 
 class MultiValueDictTests(SimpleTestCase):
 
