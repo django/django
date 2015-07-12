269315d81a955b9352777961a76763a47fb61667
Yosuke Yasuda <double.y.919.quick@gmail.com>
2015-07-12 12:48:13 +0900

    [1.8.x] Fixed #25108 -- Fixed a test which failed on Pillow 2.9+
    Backport of a2b999dfcac9bc92513a36ec6b3033ded1561c66 from master

--- tests/forms_tests/tests/test_fields.py
+++ tests/forms_tests/tests/test_fields.py
@@ -812,19 +812,23 @@ class FieldsTests(SimpleTestCase):
         This also tests the situation when Pillow doesn't detect the MIME type
         of the image (#24948).
         """
-        f = ImageField()
+        from PIL.BmpImagePlugin import BmpImageFile
+        try:
+            Image.register_mime(BmpImageFile.format, None)
+            f = ImageField()
+            img_path = os.path.dirname(upath(__file__)) + '/filepath_test_files/1x1.bmp'
+            with open(img_path, 'rb') as img_file:
+                img_data = img_file.read()
 
-        img_path = os.path.dirname(upath(__file__)) + '/filepath_test_files/1x1.bmp'
-        with open(img_path, 'rb') as img_file:
-            img_data = img_file.read()
+            img_file = SimpleUploadedFile('1x1.bmp', img_data)
+            img_file.content_type = 'text/plain'
 
-        img_file = SimpleUploadedFile('1x1.bmp', img_data)
-        img_file.content_type = 'text/plain'
+            uploaded_file = f.clean(img_file)
 
-        uploaded_file = f.clean(img_file)
-
-        self.assertEqual('BMP', uploaded_file.image.format)
-        self.assertIsNone(uploaded_file.content_type)
+            self.assertEqual('BMP', uploaded_file.image.format)
+            self.assertIsNone(uploaded_file.content_type)
+        finally:
+            Image.register_mime(BmpImageFile.format, 'image/bmp')
 
     # URLField ##################################################################
 
