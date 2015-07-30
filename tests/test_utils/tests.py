1c7c782d6e7cdce09a982f05d468d538698c3004
Tim Graham <timograham@gmail.com>
2015-07-30 15:00:24 -0400

    Fixed #25188 -- Improved message raised by SimpleTestCase.assertRaisesMessage().
    Thanks Chris Jerdonek for the suggestion and help with the patch.

--- tests/test_utils/tests.py
+++ tests/test_utils/tests.py
@@ -749,6 +749,20 @@ class SkippingExtraTests(TestCase):
 
 class AssertRaisesMsgTest(SimpleTestCase):
 
+    def test_assert_raises_message(self):
+        msg = "'Expected message' not found in 'Unexpected message'"
+        # context manager form of assertRaisesMessage()
+        with self.assertRaisesMessage(AssertionError, msg):
+            with self.assertRaisesMessage(ValueError, "Expected message"):
+                raise ValueError("Unexpected message")
+
+        # callable form
+        def func():
+            raise ValueError("Unexpected message")
+
+        with self.assertRaisesMessage(AssertionError, msg):
+            self.assertRaisesMessage(ValueError, "Expected message", func)
+
     def test_special_re_chars(self):
         """assertRaisesMessage shouldn't interpret RE special chars."""
         def func1():
