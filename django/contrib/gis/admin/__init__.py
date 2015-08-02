9eb5cfebe7bcd502402fff55e89eeb18f24f2da4
aroche <aroche@users.noreply.github.com>
2015-08-02 11:26:45 +0200

    Made the @register decorator importable from contrib.gis.admin.

--- django/contrib/gis/admin/__init__.py
+++ django/contrib/gis/admin/__init__.py
@@ -1,12 +1,12 @@
 from django.contrib.admin import (
     HORIZONTAL, VERTICAL, AdminSite, ModelAdmin, StackedInline, TabularInline,
-    autodiscover, site,
+    autodiscover, register, site,
 )
 from django.contrib.gis.admin.options import GeoModelAdmin, OSMGeoAdmin
 from django.contrib.gis.admin.widgets import OpenLayersWidget
 
 __all__ = [
-    'autodiscover', 'site', 'AdminSite', 'ModelAdmin', 'StackedInline',
-    'TabularInline', 'HORIZONTAL', 'VERTICAL', 'GeoModelAdmin', 'OSMGeoAdmin',
-    'OpenLayersWidget',
+    'HORIZONTAL', 'VERTICAL', 'AdminSite', 'ModelAdmin', 'StackedInline',
+    'TabularInline', 'autodiscover', 'register', 'site',
+    'GeoModelAdmin', 'OSMGeoAdmin', 'OpenLayersWidget',
 ]
