ff899ad669250ef00b5cfd2e7b944be8c5b3bc1c
Caio Ariede <caio.ariede@gmail.com>
2015-08-02 17:07:31 -0300

    Fixed #25191 -- Added string diff to SimpleTestCase.assertXMLEqual() message.

--- tests/test_utils/tests.py
+++ tests/test_utils/tests.py
@@ -710,6 +710,19 @@ class XMLEqualTests(SimpleTestCase):
         with self.assertRaises(AssertionError):
             self.assertXMLEqual(xml1, xml2)
 
+    def test_simple_equal_raises_message(self):
+        xml1 = "<elem attr1='a' />"
+        xml2 = "<elem attr2='b' attr1='a' />"
+
+        msg = '''{xml1} != {xml2}
+- <elem attr1='a' />
++ <elem attr2='b' attr1='a' />
+?      ++++++++++
+'''.format(xml1=repr(xml1), xml2=repr(xml2))
+
+        with self.assertRaisesMessage(AssertionError, msg):
+            self.assertXMLEqual(xml1, xml2)
+
     def test_simple_not_equal(self):
         xml1 = "<elem attr1='a' attr2='c' />"
         xml2 = "<elem attr1='a' attr2='b' />"
