04b96037f0cb2f1e369ed9f5fb79d9fdcf4caecc
aroche <aroche@users.noreply.github.com>
2015-08-02 11:26:45 +0200

    allow to use the @register decorator with geoDjango admin

--- django/contrib/gis/admin/__init__.py
+++ django/contrib/gis/admin/__init__.py
@@ -1,6 +1,6 @@
 from django.contrib.admin import (
     HORIZONTAL, VERTICAL, AdminSite, ModelAdmin, StackedInline, TabularInline,
-    autodiscover, site,
+    autodiscover, site, register,
 )
 from django.contrib.gis.admin.options import GeoModelAdmin, OSMGeoAdmin
 from django.contrib.gis.admin.widgets import OpenLayersWidget
@@ -8,5 +8,5 @@ from django.contrib.gis.admin.widgets import OpenLayersWidget
 __all__ = [
     'autodiscover', 'site', 'AdminSite', 'ModelAdmin', 'StackedInline',
     'TabularInline', 'HORIZONTAL', 'VERTICAL', 'GeoModelAdmin', 'OSMGeoAdmin',
-    'OpenLayersWidget',
+    'OpenLayersWidget', 'register',
 ]
