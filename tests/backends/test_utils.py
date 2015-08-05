6975521b6571915e13f99da950ff526bbc0d9157
Caio Ariede <caio.ariede@gmail.com>
2015-08-05 11:08:56 -0300

    Fixed #25175 -- Create an alias for the postgresql_psycopg2 database backend

--- tests/backends/test_utils.py
+++ tests/backends/test_utils.py
@@ -9,7 +9,7 @@ class TestLoadBackend(SimpleTestCase):
         msg = (
             "'foo' isn't an available database backend.\n"
             "Try using 'django.db.backends.XXX', where XXX is one of:\n"
-            "    'mysql', 'oracle', 'postgresql_psycopg2', 'sqlite3'\n"
+            "    'mysql', 'oracle', 'postgresql', 'sqlite3'\n"
             "Error was: No module named %s"
         ) % "foo.base" if six.PY2 else "'foo'"
         with self.assertRaisesMessage(ImproperlyConfigured, msg):
