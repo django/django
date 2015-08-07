72b02b810b4e9b4c148c1a3c6c4085e0c3d1faab
Tim Graham <timograham@gmail.com>
2015-08-07 07:18:55 -0400

    Refs #25236 -- Removed ifequal/ifnotequal tags and docs.

--- tests/template_tests/test_nodelist.py
+++ tests/template_tests/test_nodelist.py
@@ -22,11 +22,6 @@ class NodelistTest(TestCase):
         vars = template.nodelist.get_nodes_by_type(VariableNode)
         self.assertEqual(len(vars), 1)
 
-    def test_ifequal(self):
-        template = self.engine.from_string('{% ifequal x y %}{{ a }}{% endifequal %}')
-        vars = template.nodelist.get_nodes_by_type(VariableNode)
-        self.assertEqual(len(vars), 1)
-
     def test_ifchanged(self):
         template = self.engine.from_string('{% ifchanged x %}{{ a }}{% endifchanged %}')
         vars = template.nodelist.get_nodes_by_type(VariableNode)
