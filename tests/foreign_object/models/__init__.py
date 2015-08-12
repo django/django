825429c1f7cb1856b23ba194a66acf9dc2cde4be
Tim Graham <timograham@gmail.com>
2015-08-12 11:10:52 -0400

    Moved foreign_object models.py into a module.

---
+++ tests/foreign_object/models/__init__.py
@@ -0,0 +1,9 @@
+from .article import (
+    Article, ArticleIdea, ArticleTag, ArticleTranslation, NewsArticle,
+)
+from .person import Country, Friendship, Group, Membership, Person
+
+__all__ = [
+    'Article', 'ArticleIdea', 'ArticleTag', 'ArticleTranslation', 'Country',
+    'Friendship', 'Group', 'Membership', 'NewsArticle', 'Person',
+]
