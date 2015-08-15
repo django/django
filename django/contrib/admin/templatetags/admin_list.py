5879c3883d7e63718f3762639ca9c0769b37d503
elky <mansellfan@gmail.com>
2015-08-16 01:38:57 +0500

    Replaced gif icons with svg

--- django/contrib/admin/templatetags/admin_list.py
+++ django/contrib/admin/templatetags/admin_list.py
@@ -174,7 +174,7 @@ def result_headers(cl):
 
 
 def _boolean_icon(field_val):
-    icon_url = static('admin/img/icon-%s.gif' %
+    icon_url = static('admin/img/svg/icon-%s.svg' %
                       {True: 'yes', False: 'no', None: 'unknown'}[field_val])
     return format_html('<img src="{}" alt="{}" />', icon_url, field_val)
 
