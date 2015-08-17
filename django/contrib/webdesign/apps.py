3af9b70028487be81c4f6ca65ca0d1f2be337e4f
Tim Graham <timograham@gmail.com>
2015-08-17 09:01:59 -0400

    Refs #22789 -- Removed contrib.webdesign per deprecation timeline.

--- django/contrib/webdesign/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import ugettext_lazy as _
-
-
-class WebDesignConfig(AppConfig):
-    name = 'django.contrib.webdesign'
-    verbose_name = _("Web Design")
