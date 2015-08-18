0f74f0ed33bf9f6de0614a37818c25cfaecbe00e
Zoe Jin <zoe.jin@britny.se>
2015-08-18 10:58:10 +0200

    activate current language when clicking view_on_site button in admin page

--- django/contrib/admin/options.py
+++ django/contrib/admin/options.py
@@ -46,6 +46,8 @@ from django.utils.translation import string_concat, ugettext as _, ungettext
 from django.views.decorators.csrf import csrf_protect
 from django.views.generic import RedirectView
 
+from django.utils.translation import activate, get_language
+
 IS_POPUP_VAR = '_popup'
 TO_FIELD_VAR = '_to_field'
 
@@ -268,10 +270,14 @@ class BaseModelAdmin(six.with_metaclass(forms.MediaDefiningClass)):
             return self.view_on_site(obj)
         elif self.view_on_site and hasattr(obj, 'get_absolute_url'):
             # use the ContentType lookup if view_on_site is True
-            return reverse('admin:view_on_site', kwargs={
+            current_lang = get_language()
+            activate(obj.language_code)
+            url = reverse('admin:view_on_site', kwargs={
                 'content_type_id': get_content_type_for_model(obj).pk,
                 'object_id': obj.pk
             })
+            activate(current_lang)
+            return url
 
     def get_empty_value_display(self):
         """
