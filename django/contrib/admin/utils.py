3cc67a637a3cd1426d2ac87d7418600fa247ebb2
Tim Graham <timograham@gmail.com>
2015-08-24 11:52:58 -0400

    [1.8.x] Fixed #25299 -- Fixed crash with ModelAdmin.list_display value that clashes with a model reverse accessor.
    Backport of 9607a0404188bbe612f05216f5a82df26f4b4e80 from master

--- django/contrib/admin/utils.py
+++ django/contrib/admin/utils.py
@@ -293,9 +293,14 @@ def _get_non_gfk_field(opts, name):
     """
     For historical reasons, the admin app relies on GenericForeignKeys as being
     "not found" by get_field(). This could likely be cleaned up.
+
+    Reverse relations should also be excluded as these aren't attributes of the
+    model (rather something like `foo_set`).
     """
     field = opts.get_field(name)
-    if field.is_relation and field.many_to_one and not field.related_model:
+    if (field.is_relation and
+            # Generic foreign keys OR reverse relations
+            ((field.many_to_one and not field.related_model) or field.one_to_many)):
         raise FieldDoesNotExist()
     return field
 
