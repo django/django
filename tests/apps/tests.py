6a98396b9da56bf5eb1c94e037b844eb9bdd1a45
Caio Ariede <caio.ariede@gmail.com>
2015-08-23 21:07:00 -0300

    Fixed #25246 -- Guarded against duplicate paths in AppConfig.

--- tests/apps/tests.py
+++ tests/apps/tests.py
@@ -365,6 +365,14 @@ class AppConfigTests(SimpleTestCase):
         with self.assertRaises(ImproperlyConfigured):
             AppConfig('label', Stub(__path__=['a', 'b']))
 
+    def test_duplicate_dunder_path_no_dunder_file(self):
+        """
+        If the __path__ attr contains duplicate paths and there is no
+        __file__, they duplicates should be deduplicated (#25246).
+        """
+        ac = AppConfig('label', Stub(__path__=['a', 'a']))
+        self.assertEqual(ac.path, 'a')
+
 
 @skipUnless(six.PY3, "Namespace packages sans __init__.py were added in Python 3.3")
 class NamespacePackageAppTests(SimpleTestCase):
