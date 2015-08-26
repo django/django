429f55ac67f0ea41d9102768ab5ec1f46d206ec8
Jonas Haag <jonas@lophus.org>
2015-08-26 15:57:36 +0200

    Sort imports

--- django/core/validators.py
+++ django/core/validators.py
@@ -4,9 +4,9 @@ import re
 
 from django.core.exceptions import ValidationError
 from django.utils import six
-from django.utils.functional import SimpleLazyObject
 from django.utils.deconstruct import deconstructible
 from django.utils.encoding import force_text
+from django.utils.functional import SimpleLazyObject
 from django.utils.ipv6 import is_valid_ipv6_address
 from django.utils.six.moves.urllib.parse import urlsplit, urlunsplit
 from django.utils.translation import ugettext_lazy as _, ungettext_lazy
