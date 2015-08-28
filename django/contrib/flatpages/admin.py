b649f68649b0cba2de9e6bb8cf3d3fe468372c2e
Y3K <dboy.y3k@gmail.com>
2015-08-28 14:13:41 -0500

    Fixed #25262 -- Removed the enable_comments field from FlatPageAdmin.

--- django/contrib/flatpages/admin.py
+++ django/contrib/flatpages/admin.py
@@ -8,11 +8,13 @@ class FlatPageAdmin(admin.ModelAdmin):
     form = FlatpageForm
     fieldsets = (
         (None, {'fields': ('url', 'title', 'content', 'sites')}),
-        (_('Advanced options'), {'classes': ('collapse',),
-        'fields': ('enable_comments', 'registration_required', 'template_name')}),
+        (_('Advanced options'), {
+            'classes': ('collapse',),
+            'fields': ('registration_required', 'template_name'),
+        }),
     )
     list_display = ('url', 'title')
-    list_filter = ('sites', 'enable_comments', 'registration_required')
+    list_filter = ('sites', 'registration_required')
     search_fields = ('url', 'title')
 
 admin.site.register(FlatPage, FlatPageAdmin)
