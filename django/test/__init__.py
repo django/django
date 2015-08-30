db8c7c5c16cb30a603fd8ff239e660f4697a8b5a
Jakub Gocławski <gocek13@gmail.com>
2015-08-30 14:49:38 +0200

    Fixed #25328 -- Added LiveHTTPSServerTestCase.

--- django/test/__init__.py
+++ django/test/__init__.py
@@ -4,8 +4,9 @@ Django Unit Test and Doctest framework.
 
 from django.test.client import Client, RequestFactory
 from django.test.testcases import (
-    LiveServerTestCase, SimpleTestCase, TestCase, TransactionTestCase,
-    skipIfDBFeature, skipUnlessAnyDBFeature, skipUnlessDBFeature,
+    LiveHTTPSServerTestCase, LiveServerTestCase, SimpleTestCase, TestCase,
+    TransactionTestCase, skipIfDBFeature, skipUnlessAnyDBFeature,
+    skipUnlessDBFeature,
 )
 from django.test.utils import (
     ignore_warnings, modify_settings, override_settings,
@@ -14,9 +15,10 @@ from django.test.utils import (
 
 __all__ = [
     'Client', 'RequestFactory', 'TestCase', 'TransactionTestCase',
-    'SimpleTestCase', 'LiveServerTestCase', 'skipIfDBFeature',
-    'skipUnlessAnyDBFeature', 'skipUnlessDBFeature', 'ignore_warnings',
-    'modify_settings', 'override_settings', 'override_system_checks'
+    'SimpleTestCase', 'LiveHTTPSServerTestCase', 'LiveServerTestCase',
+    'skipIfDBFeature', 'skipUnlessAnyDBFeature', 'skipUnlessDBFeature',
+    'ignore_warnings', 'modify_settings', 'override_settings',
+    'override_system_checks'
 ]
 
 # To simplify Django's test suite; not meant as a public API
