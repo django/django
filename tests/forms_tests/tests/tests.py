f9315660c5c1d51af9c6e19519a97b2aea236283
Dražen Odobašić <dodobas@candela-it.com>
2015-09-02 18:15:45 +0200

    Enforce maximum line length in tests.forms_tests

--- tests/forms_tests/tests/tests.py
+++ tests/forms_tests/tests/tests.py
@@ -101,7 +101,9 @@ class ModelFormCallableModelDefault(TestCase):
         ChoiceOptionModel.objects.create(id=1, name='default')
         ChoiceOptionModel.objects.create(id=2, name='option 2')
         ChoiceOptionModel.objects.create(id=3, name='option 3')
-        self.assertHTMLEqual(ChoiceFieldForm().as_p(), """<p><label for="id_choice">Choice:</label> <select name="choice" id="id_choice">
+        self.assertHTMLEqual(
+            ChoiceFieldForm().as_p(),
+            """<p><label for="id_choice">Choice:</label> <select name="choice" id="id_choice">
 <option value="1" selected="selected">ChoiceOption 1</option>
 <option value="2">ChoiceOption 2</option>
 <option value="3">ChoiceOption 3</option>
@@ -120,19 +122,23 @@ class ModelFormCallableModelDefault(TestCase):
 <option value="1" selected="selected">ChoiceOption 1</option>
 <option value="2">ChoiceOption 2</option>
 <option value="3">ChoiceOption 3</option>
-</select><input type="hidden" name="initial-multi_choice_int" value="1" id="initial-id_multi_choice_int_0" /></p>""")
+</select><input type="hidden" name="initial-multi_choice_int" value="1" id="initial-id_multi_choice_int_0" /></p>"""  # NOQA
+        )
 
     def test_initial_instance_value(self):
         "Initial instances for model fields may also be instances (refs #7287)"
         ChoiceOptionModel.objects.create(id=1, name='default')
         obj2 = ChoiceOptionModel.objects.create(id=2, name='option 2')
         obj3 = ChoiceOptionModel.objects.create(id=3, name='option 3')
-        self.assertHTMLEqual(ChoiceFieldForm(initial={
-            'choice': obj2,
-            'choice_int': obj2,
-            'multi_choice': [obj2, obj3],
-            'multi_choice_int': ChoiceOptionModel.objects.exclude(name="default"),
-        }).as_p(), """<p><label for="id_choice">Choice:</label> <select name="choice" id="id_choice">
+        self.assertHTMLEqual(
+            ChoiceFieldForm(initial={
+                'choice': obj2,
+                'choice_int': obj2,
+                'multi_choice': [obj2, obj3],
+                'multi_choice_int': ChoiceOptionModel.objects.exclude(name="default"),
+            }
+            ).as_p(),
+            """<p><label for="id_choice">Choice:</label> <select name="choice" id="id_choice">
 <option value="1">ChoiceOption 1</option>
 <option value="2" selected="selected">ChoiceOption 2</option>
 <option value="3">ChoiceOption 3</option>
@@ -153,13 +159,20 @@ class ModelFormCallableModelDefault(TestCase):
 <option value="2" selected="selected">ChoiceOption 2</option>
 <option value="3" selected="selected">ChoiceOption 3</option>
 </select><input type="hidden" name="initial-multi_choice_int" value="2" id="initial-id_multi_choice_int_0" />
-<input type="hidden" name="initial-multi_choice_int" value="3" id="initial-id_multi_choice_int_1" /></p>""")
+<input type="hidden" name="initial-multi_choice_int" value="3" id="initial-id_multi_choice_int_1" /></p>"""  # NOQA
+        )
 
 
 class FormsModelTestCase(TestCase):
     def test_unicode_filename(self):
         # FileModel with unicode filename and data #########################
-        f = FileForm(data={}, files={'file1': SimpleUploadedFile('我隻氣墊船裝滿晒鱔.txt', 'मेरी मँडराने वाली नाव सर्पमीनों से भरी ह'.encode('utf-8'))}, auto_id=False)
+        f = FileForm(
+            data={}, files={
+                'file1': SimpleUploadedFile(
+                    '我隻氣墊船裝滿晒鱔.txt', 'मेरी मँडराने वाली नाव सर्पमीनों से भरी ह'.encode('utf-8')
+                )
+            }, auto_id=False
+        )
         self.assertTrue(f.is_valid())
         self.assertIn('file1', f.cleaned_data)
         m = FileModel.objects.create(file=f.cleaned_data['file1'])
@@ -287,23 +300,27 @@ class ManyToManyExclusionTestCase(TestCase):
 class EmptyLabelTestCase(TestCase):
     def test_empty_field_char(self):
         f = EmptyCharLabelChoiceForm()
-        self.assertHTMLEqual(f.as_p(),
+        self.assertHTMLEqual(
+            f.as_p(),
             """<p><label for="id_name">Name:</label> <input id="id_name" maxlength="10" name="name" type="text" /></p>
 <p><label for="id_choice">Choice:</label> <select id="id_choice" name="choice">
 <option value="" selected="selected">No Preference</option>
 <option value="f">Foo</option>
 <option value="b">Bar</option>
-</select></p>""")
+</select></p>"""
+        )
 
     def test_empty_field_char_none(self):
         f = EmptyCharLabelNoneChoiceForm()
-        self.assertHTMLEqual(f.as_p(),
+        self.assertHTMLEqual(
+            f.as_p(),
             """<p><label for="id_name">Name:</label> <input id="id_name" maxlength="10" name="name" type="text" /></p>
 <p><label for="id_choice_string_w_none">Choice string w none:</label> <select id="id_choice_string_w_none" name="choice_string_w_none">
 <option value="" selected="selected">No Preference</option>
 <option value="f">Foo</option>
 <option value="b">Bar</option>
-</select></p>""")
+</select></p>"""  # NOQA
+        )
 
     def test_save_empty_label_forms(self):
         # Test that saving a form with a blank choice results in the expected
@@ -324,13 +341,15 @@ class EmptyLabelTestCase(TestCase):
 
     def test_empty_field_integer(self):
         f = EmptyIntegerLabelChoiceForm()
-        self.assertHTMLEqual(f.as_p(),
+        self.assertHTMLEqual(
+            f.as_p(),
             """<p><label for="id_name">Name:</label> <input id="id_name" maxlength="10" name="name" type="text" /></p>
 <p><label for="id_choice_integer">Choice integer:</label> <select id="id_choice_integer" name="choice_integer">
 <option value="" selected="selected">No Preference</option>
 <option value="1">Foo</option>
 <option value="2">Bar</option>
-</select></p>""")
+</select></p>"""
+        )
 
     def test_get_display_value_on_none(self):
         m = ChoiceModel.objects.create(name='test', choice='', choice_integer=None)
@@ -340,20 +359,24 @@ class EmptyLabelTestCase(TestCase):
     def test_html_rendering_of_prepopulated_models(self):
         none_model = ChoiceModel(name='none-test', choice_integer=None)
         f = EmptyIntegerLabelChoiceForm(instance=none_model)
-        self.assertHTMLEqual(f.as_p(),
+        self.assertHTMLEqual(
+            f.as_p(),
             """<p><label for="id_name">Name:</label> <input id="id_name" maxlength="10" name="name" type="text" value="none-test"/></p>
 <p><label for="id_choice_integer">Choice integer:</label> <select id="id_choice_integer" name="choice_integer">
 <option value="" selected="selected">No Preference</option>
 <option value="1">Foo</option>
 <option value="2">Bar</option>
-</select></p>""")
+</select></p>"""  # NOQA
+        )
 
         foo_model = ChoiceModel(name='foo-test', choice_integer=1)
         f = EmptyIntegerLabelChoiceForm(instance=foo_model)
-        self.assertHTMLEqual(f.as_p(),
+        self.assertHTMLEqual(
+            f.as_p(),
             """<p><label for="id_name">Name:</label> <input id="id_name" maxlength="10" name="name" type="text" value="foo-test"/></p>
 <p><label for="id_choice_integer">Choice integer:</label> <select id="id_choice_integer" name="choice_integer">
 <option value="">No Preference</option>
 <option value="1" selected="selected">Foo</option>
 <option value="2">Bar</option>
-</select></p>""")
+</select></p>"""  # NOQA
+        )
