1bbca7961cee20c4ddd453a7d74d316e84f4bbb5
Raphael Michel <mail@raphaelmichel.de>
2015-09-07 15:17:55 +0200

    Fixed #25350 -- Added alias --no-input for --noinput to management commands.

--- django/core/management/commands/makemigrations.py
+++ django/core/management/commands/makemigrations.py
@@ -29,7 +29,8 @@ class Command(BaseCommand):
             help="Enable fixing of migration conflicts.")
         parser.add_argument('--empty', action='store_true', dest='empty', default=False,
             help="Create an empty migration.")
-        parser.add_argument('--noinput', action='store_false', dest='interactive', default=True,
+        parser.add_argument('--noinput', '--no-input',
+            action='store_false', dest='interactive', default=True,
             help='Tells Django to NOT prompt the user for input of any kind.')
         parser.add_argument('-n', '--name', action='store', dest='name', default=None,
             help="Use this name for migration file(s).")
--- django/core/management/commands/test.py
+++ django/core/management/commands/test.py
@@ -32,7 +32,7 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('args', metavar='test_label', nargs='*',
             help='Module paths to test; can be modulename, modulename.TestCase or modulename.TestCase.test_method')
-        parser.add_argument('--noinput',
+        parser.add_argument('--noinput', '--no-input',
             action='store_false', dest='interactive', default=True,
             help='Tells Django to NOT prompt the user for input of any kind.'),
         parser.add_argument('--failfast',
