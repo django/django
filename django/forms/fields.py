8550161e531a603d57723850fb09c4c9b7ca60b9
Moritz Sichert <moritz.sichert@googlemail.com>
2015-09-09 15:09:59 +0200

    Refs #25294 -- Moved BoundField to django.forms.boundfield.

--- django/forms/fields.py
+++ django/forms/fields.py
@@ -18,6 +18,7 @@ from django.core import validators
 from django.core.exceptions import ValidationError
 # Provide this import for backwards compatibility.
 from django.core.validators import EMPTY_VALUES  # NOQA
+from django.forms.boundfield import BoundField
 from django.forms.utils import from_current_timezone, to_current_timezone
 from django.forms.widgets import (
     FILE_INPUT_CONTRADICTION, CheckboxInput, ClearableFileInput, DateInput,
@@ -209,7 +210,6 @@ class Field(six.with_metaclass(RenameFieldMethods, object)):
         Return a BoundField instance that will be used when accessing the form
         field in a template.
         """
-        from django.forms.forms import BoundField
         return BoundField(form, self, field_name)
 
     def __deepcopy__(self, memo):
