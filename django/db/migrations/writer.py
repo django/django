3cc5cc79584b66dafac54264d072ac1206426471
Markus Holtermann <info@markusholtermann.eu>
2015-09-12 12:04:27 +1000

    [1.8.x] Fixed #25384 -- Ordered imports in newly created migration files

--- django/db/migrations/writer.py
+++ django/db/migrations/writer.py
@@ -470,7 +470,7 @@ MIGRATION_TEMPLATE = """\
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
+from django.db import migrations, models
 %(imports)s
 
 class Migration(migrations.Migration):
