105d9fb7133c2d85c1b05dd948f0790f9d02099d
Ville Skyttä <ville.skytta@iki.fi>
2015-09-12 22:06:35 +0300

    Fixed #25393 -- Fixed MySQL crash when adding text/blob field with unhashable default.

--- tests/schema/tests.py
+++ tests/schema/tests.py
@@ -1673,3 +1673,15 @@ class SchemaTests(TransactionTestCase):
             )
             if connection.features.can_introspect_default:
                 self.assertIsNone(field.default)
+
+    def test_add_textfield_unhashable_default(self):
+        # Create the table
+        with connection.schema_editor() as editor:
+            editor.create_model(Author)
+        # Create a row
+        Author.objects.create(name='Anonymous1')
+        # Create a field that has an unhashable default
+        new_field = TextField(default={})
+        new_field.set_attributes_from_name("info")
+        with connection.schema_editor() as editor:
+            editor.add_field(Author, new_field)
