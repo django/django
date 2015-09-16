688f8de7b612545e4dd25f4707bb18ccecc255ce
Matthew Crowson <matthew.d.crowson@gmail.com>
2015-09-16 09:19:31 -0400

    [1.8.x] Fixed typo in django/core/cache/backends/base.py docstring.
    Backport of 8615e415861ea93afb5a84365895bd7b9af7be6f from master

--- django/core/cache/backends/base.py
+++ django/core/cache/backends/base.py
@@ -91,7 +91,7 @@ class BaseCache(object):
     def make_key(self, key, version=None):
         """Constructs the key used by all other methods. By default it
         uses the key_func to generate a key (which, by default,
-        prepends the `key_prefix' and 'version'). An different key
+        prepends the `key_prefix' and 'version'). A different key
         function can be provided at the time of cache construction;
         alternatively, you can subclass the cache backend to provide
         custom key making behavior.
