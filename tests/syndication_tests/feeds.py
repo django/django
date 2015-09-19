a4b80e242162a7d6ac2337a2a621a82027c549b7
Unai Zalakain <unai@gisa-elkartea.org>
2015-09-19 07:59:18 +0200

    Refs #13110 -- Fixed mistakes in the new multiple enclosure feed tests

--- tests/syndication_tests/feeds.py
+++ tests/syndication_tests/feeds.py
@@ -208,6 +208,6 @@ class TestMultipleEnclosureAtomFeed(TestAtomFeed):
     """
     def item_enclosures(self, item):
         return [
-            feedgenerator.Enclosure('http://example.com/hello.png', 0, 'image/png'),
-            feedgenerator.Enclosure('http://example.com/goodbye.png', 0, 'image/png'),
+            feedgenerator.Enclosure('http://example.com/hello.png', '0', 'image/png'),
+            feedgenerator.Enclosure('http://example.com/goodbye.png', '0', 'image/png'),
         ]
