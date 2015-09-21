6751bed8d626e4f30a639b4f1076cb4896d574ba
Alasdair Nicol <alasdair@thenicols.net>
2015-09-21 18:45:56 +0100

    Refs #23813 -- Moved URLconfs into module and tidied docstrings

--- tests/check_framework/test_urls.py
+++ tests/check_framework/test_urls.py
@@ -4,12 +4,12 @@ from django.test.utils import override_settings
 
 
 class CheckUrlsTest(SimpleTestCase):
-    @override_settings(ROOT_URLCONF='check_framework.urls_no_warnings')
-    def test_include_no_warnings(self):
+    @override_settings(ROOT_URLCONF='check_framework.urls.no_warnings')
+    def test_no_warnings(self):
         result = check_url_config(None)
         self.assertEqual(result, [])
 
-    @override_settings(ROOT_URLCONF='check_framework.urls_include')
+    @override_settings(ROOT_URLCONF='check_framework.urls.include_with_dollar')
     def test_include_with_dollar(self):
         result = check_url_config(None)
         self.assertEqual(len(result), 1)
@@ -18,8 +18,8 @@ class CheckUrlsTest(SimpleTestCase):
         expected_msg = "Your URL pattern '^include-with-dollar$' uses include with a regex ending with a '$'."
         self.assertIn(expected_msg, warning.msg)
 
-    @override_settings(ROOT_URLCONF='check_framework.urls_slash')
-    def test_url_beginning_with_slash(self):
+    @override_settings(ROOT_URLCONF='check_framework.urls.beginning_with_slash')
+    def test_beginning_with_slash(self):
         result = check_url_config(None)
         self.assertEqual(len(result), 1)
         warning = result[0]
@@ -27,8 +27,8 @@ class CheckUrlsTest(SimpleTestCase):
         expected_msg = "Your URL pattern '/starting-with-slash/$' has a regex beginning with a '/'"
         self.assertIn(expected_msg, warning.msg)
 
-    @override_settings(ROOT_URLCONF='check_framework.urls_name')
-    def test_url_pattern_name_with_colon(self):
+    @override_settings(ROOT_URLCONF='check_framework.urls.name_with_colon')
+    def test_name_with_colon(self):
         result = check_url_config(None)
         self.assertEqual(len(result), 1)
         warning = result[0]
