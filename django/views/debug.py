fd1e54b923871d9596f9692c0fc576cd40ce3e38
Tim Graham <timograham@gmail.com>
2015-09-24 08:17:40 -0400

    Removed unused views.debug.linebreak_iter() function.

--- django/views/debug.py
+++ django/views/debug.py
@@ -24,15 +24,6 @@ HIDDEN_SETTINGS = re.compile('API|TOKEN|KEY|SECRET|PASS|SIGNATURE')
 CLEANSED_SUBSTITUTE = '********************'
 
 
-def linebreak_iter(template_source):
-    yield 0
-    p = template_source.find('\n')
-    while p >= 0:
-        yield p + 1
-        p = template_source.find('\n', p + 1)
-    yield len(template_source) + 1
-
-
 class CallableSettingWrapper(object):
     """ Object to wrap callable appearing in settings
 
