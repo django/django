f05722a08a11b2e6c6c9acdf2bacd6a6128ccbb0
James Pulec <jpulec@gmail.com>
2015-09-29 10:52:26 -0700

    Fixed #25354 -- Added class/app_label interpolation for related_query_name.

--- tests/model_inheritance/tests.py
+++ tests/model_inheritance/tests.py
@@ -72,6 +72,15 @@ class ModelInheritanceTests(TestCase):
             AttributeError, getattr, post, "attached_%(class)s_set"
         )
 
+    def test_model_with_distinct_related_query_name(self):
+        self.assertQuerysetEqual(Post.objects.filter(attached_model_inheritance_comments__is_spam=True), [])
+
+        # The Post model doesn't have a related query accessor based on
+        # related_name (attached_comment_set).
+        msg = "Cannot resolve keyword 'attached_comment_set' into field."
+        with self.assertRaisesMessage(FieldError, msg):
+            Post.objects.filter(attached_comment_set__is_spam=True)
+
     def test_meta_fields_and_ordering(self):
         # Make sure Restaurant and ItalianRestaurant have the right fields in
         # the right order.
