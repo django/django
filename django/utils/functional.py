4b95f1b0fba1fd9f184de247f96703e664f7f77c
Tim Graham <timograham@gmail.com>
2015-10-05 08:25:49 -0400

    [1.9.x] Removed SimpleLazyObject workaround for a Python bug.
    Backport of 4accffd8fdaa59597dc40b47be031e7116c5b2a5 from master

--- django/utils/functional.py
+++ django/utils/functional.py
@@ -317,10 +317,6 @@ def unpickle_lazyobject(wrapped):
 unpickle_lazyobject.__safe_for_unpickling__ = True
 
 
-# Workaround for http://bugs.python.org/issue12370
-_super = super
-
-
 class SimpleLazyObject(LazyObject):
     """
     A lazy object initialized from any function.
@@ -338,7 +334,7 @@ class SimpleLazyObject(LazyObject):
         value.
         """
         self.__dict__['_setupfunc'] = func
-        _super(SimpleLazyObject, self).__init__()
+        super(SimpleLazyObject, self).__init__()
 
     def _setup(self):
         self._wrapped = self._setupfunc()
