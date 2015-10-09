5f1a3dbbb53589c66d145862db17c62bf0897a32
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2015-10-09 12:53:11 +0100

    Fixed #25533 -- Changed dataype mapping for GDALRasters

--- tests/gis_tests/gdal_tests/test_raster.py
+++ tests/gis_tests/gdal_tests/test_raster.py
@@ -121,6 +121,27 @@ class GDALRasterTests(unittest.TestCase):
         self.assertEqual(len(self.rs.bands), 1)
         self.assertIsInstance(self.rs.bands[0], GDALBand)
 
+    def test_memory_based_raster_creation(self):
+        # Create uint8 raster with full pixel data range (0-255)
+        rast = GDALRaster({
+            'datatype': 1,
+            'width': 16,
+            'height': 16,
+            'srid': 4326,
+            'bands': [{
+                'data': range(256),
+                'nodata_value': 255,
+            }],
+        })
+
+        # Get array from raster
+        result = rast.bands[0].data()
+        if numpy:
+            result = result.flatten().tolist()
+
+        # Assert data is same as original input
+        self.assertEqual(result, list(range(256)))
+
     def test_file_based_raster_creation(self):
         # Prepare tempfile
         rstfile = tempfile.NamedTemporaryFile(suffix='.tif')
