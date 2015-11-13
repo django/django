9bc0c21b1ce6520dd365251eeb8b4b493d8ec2b7
Baptiste Mispelon <bmispelon@gmail.com>
2015-11-13 17:06:22 +0100

    Made BaseCommand.get_version() docstring consistent with docs.

--- django/core/management/base.py
+++ django/core/management/base.py
@@ -212,9 +212,9 @@ class BaseCommand(object):
 
     def get_version(self):
         """
-        Return the Django version, which should be correct for all
-        built-in Django commands. User-supplied commands should
-        override this method.
+        Return the Django version, which should be correct for all built-in
+        Django commands. User-supplied commands can override this method to
+        return their own version.
         """
         return django.get_version()
 
