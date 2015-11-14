a14a409605bdf483facf24c6fb23e9b741f065c4
Niels Van Och <niels.vanoch@gmail.com>
2015-11-14 13:15:50 +0100

    Add test

--- tests/migrations/test_commands.py
+++ tests/migrations/test_commands.py
@@ -1009,6 +1009,16 @@ class MakeMigrationsTests(MigrationTestBase):
         with self.temporary_migration_module(module="migrations.test_migrations_no_changes"):
             call_command("makemigrations", "--check", "migrations", verbosity=0)
 
+    def test_makemigrations_output(self):
+        """
+        makemigrations should print the relative paths to the migrations unless they are
+        outside of the current tree, in which case the absolute path should be shown.
+        """
+        out = six.StringIO()
+        apps.register_model('migrations', UnicodeModel)
+        with self.temporary_migration_module() as migration_dir:
+            call_command("makemigrations", "migrations", stdout=out)
+            self.assertIn('%s/%s' % (migration_dir, '0001_initial.py'), out.getvalue())
 
 class SquashMigrationsTests(MigrationTestBase):
     """
