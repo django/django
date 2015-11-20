eb7a329fb65e93dcd6c773c167353af9825d7b6e
Simon Charette <charette.s@gmail.com>
2015-11-20 14:18:47 -0500

    [1.9.x] Fixed #25685 -- Fixed a duplicate query regression on deletion of proxied models.
    Thanks to Trac alias ppetrid for the report and Tim for the review.
    
    Conflicts:
    	tests/delete/tests.py
    
    Forward port of 7c3ef19978b36b61db88a519f799f1ce8d019679 from stable/1.8.x

--- tests/delete/tests.py
+++ tests/delete/tests.py
@@ -413,6 +413,17 @@ class DeletionTests(TestCase):
         for k, v in existed_objs.items():
             self.assertEqual(deleted_objs[k], v)
 
+    def test_proxied_model_duplicate_queries(self):
+        """
+        #25685 - Deleting instances of a model with existing proxy
+        classes should not issue multiple queries during cascade
+        deletion of referring models.
+        """
+        avatar = Avatar.objects.create()
+        # One query for the Avatar table and a second for the User one.
+        with self.assertNumQueries(2):
+            avatar.delete()
+
 
 class FastDeleteTests(TestCase):
 
