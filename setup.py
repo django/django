c3e2e63ec8bc0e4475626a15e25c22ba3462cf2d
Tim Graham <timograham@gmail.com>
2015-12-01 19:07:19 -0500

    [1.8.x] Added Python 3.5 trove classifier.

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Internet :: WWW/HTTP :: WSGI',
