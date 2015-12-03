d79a3a61b0ab3b384af41e00c8847c58da49c5ed
auvipy <auvipy@gmail.com>
2015-12-03 19:10:54 +0600

    Allowed importing views.generic.View from views.View

--- django/views/__init__.py
+++ django/views/__init__.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-
+from django.views.generic.base import View
 
 __all__ = ['View']
-=======
-from django.views.generic.base import View
->>>>>>> 3b403668b22bea5315bc1c6fbb8237527d43ebb4
+
