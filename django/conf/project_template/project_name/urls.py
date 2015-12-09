4211f8c9d1cd543d9c6fe4b789aea7dddf07c44a
Tim Graham <timograham@gmail.com>
2015-12-09 11:27:16 -0500

    [1.8.x] Fixed #25711 -- Updated the project template's include() example.
    Referencing URLs by dotted path is a stronger convention than
    importing the URLs.
    
    Backport of 179fbab7e05946b59ec9815d6e9b1901826ed610 from master

--- django/conf/project_template/project_name/urls.py
+++ django/conf/project_template/project_name/urls.py
@@ -10,8 +10,7 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+    1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
 from django.contrib import admin
