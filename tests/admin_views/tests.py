515f149e4d51ddcf32db0be440da726c0ca867b4
Sergey Fedoseev <fedoseev.sergey@gmail.com>
2015-12-10 12:06:01 +0500

    [1.9.x] Fixed #25883 -- Fixed admin deletion page summary counts for related objects.
    Backport of 8ab58b80529c5206654c1042a4ddcf2da364f8ec from master

--- tests/admin_views/tests.py
+++ tests/admin_views/tests.py
@@ -1297,7 +1297,8 @@ class AdminViewPermissionsTest(TestCase):
         )
         cls.s1 = Section.objects.create(name='Test section')
         cls.a1 = Article.objects.create(
-            content='<p>Middle content</p>', date=datetime.datetime(2008, 3, 18, 11, 54, 58), section=cls.s1
+            content='<p>Middle content</p>', date=datetime.datetime(2008, 3, 18, 11, 54, 58), section=cls.s1,
+            another_section=cls.s1,
         )
         cls.a2 = Article.objects.create(
             content='<p>Oldest content</p>', date=datetime.datetime(2000, 3, 18, 11, 54, 58), section=cls.s1
@@ -3192,7 +3193,7 @@ class AdminActionsTest(TestCase):
         self.assertIsInstance(confirmation, TemplateResponse)
         self.assertContains(confirmation, "Are you sure you want to delete the selected subscribers?")
         self.assertContains(confirmation, "<h2>Summary</h2>")
-        self.assertContains(confirmation, "<li>Subscribers: 3</li>")
+        self.assertContains(confirmation, "<li>Subscribers: 2</li>")
         self.assertContains(confirmation, "<li>External subscribers: 1</li>")
         self.assertContains(confirmation, ACTION_CHECKBOX_NAME, count=2)
         self.client.post(reverse('admin:admin_views_subscriber_changelist'), delete_confirmation_data)
