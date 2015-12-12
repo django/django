f7889b83b0e62f3dd7e16c0f54b03985de900ffb
Sergey Fedoseev <fedoseev.sergey@gmail.com>
2015-12-12 20:00:46 +0500

    Fixed #25924 -- Removed gis.utils.srs.add_postgis_srs() backwards-compatibility alias.

--- django/contrib/gis/utils/__init__.py
+++ django/contrib/gis/utils/__init__.py
@@ -7,7 +7,7 @@ from django.contrib.gis.utils.wkt import precision_wkt  # NOQA
 if HAS_GDAL:
     from django.contrib.gis.utils.ogrinfo import ogrinfo, sample  # NOQA
     from django.contrib.gis.utils.ogrinspect import mapping, ogrinspect  # NOQA
-    from django.contrib.gis.utils.srs import add_postgis_srs, add_srs_entry  # NOQA
+    from django.contrib.gis.utils.srs import add_srs_entry  # NOQA
     from django.core.exceptions import ImproperlyConfigured
     try:
         # LayerMapping requires DJANGO_SETTINGS_MODULE to be set,
