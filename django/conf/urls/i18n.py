0c38e61e9267fa57655f29bd829316bd5eb4e767
Krzysztof Urbaniak <urban@fail.pl>
2015-12-14 23:53:45 +0100

    Use prefix_default_language param in i18n_patterns
    That's the prefered way of configuring i18n_patterns to not prefix the default language.

--- django/conf/urls/i18n.py
+++ django/conf/urls/i18n.py
@@ -4,7 +4,7 @@ from django.core.urlresolvers import LocaleRegexURLResolver
 from django.views.i18n import set_language
 
 
-def i18n_patterns(*urls):
+def i18n_patterns(*urls, prefix_default_language=True):
     """
     Adds the language code prefix to every URL pattern within this
     function. This may only be used in the root URLconf, not in an included
@@ -12,7 +12,10 @@ def i18n_patterns(*urls):
     """
     if not settings.USE_I18N:
         return urls
-    return [LocaleRegexURLResolver(list(urls))]
+    return [LocaleRegexURLResolver(
+        list(urls),
+        prefix_default_language=prefix_default_language,
+    )]
 
 
 urlpatterns = [
