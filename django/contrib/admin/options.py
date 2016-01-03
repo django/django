6e3d20e6b1fd3826dd57fd29259ff1b205816ebf
Alexander Gaevsky <sasha@sasha0.ru>
2016-01-03 02:43:48 +0200

    Refs #25165 -- Fixed JSON serialization for add/edit using popup in the admin

--- django/contrib/admin/options.py
+++ django/contrib/admin/options.py
@@ -1083,7 +1083,7 @@ class ModelAdmin(BaseModelAdmin):
                 attr = obj._meta.pk.attname
             value = obj.serializable_value(attr)
             popup_response_data = json.dumps({
-                'value': value,
+                'value': six.text_type(value),
                 'obj': six.text_type(obj),
             })
             return SimpleTemplateResponse('admin/popup_response.html', {
@@ -1135,9 +1135,9 @@ class ModelAdmin(BaseModelAdmin):
             new_value = obj.serializable_value(attr)
             popup_response_data = json.dumps({
                 'action': 'change',
-                'value': value,
+                'value': six.text_type(value),
                 'obj': six.text_type(obj),
-                'new_value': new_value,
+                'new_value': six.text_type(new_value),
             })
             return SimpleTemplateResponse('admin/popup_response.html', {
                 'popup_response_data': popup_response_data,
