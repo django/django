cb96d0c92a04cd6287cea5b904709edd1dbe6010
Alexander Gaevsky <sasha@sasha0.ru>
2016-01-08 18:49:03 -0500

    [1.9.x] Added a test for adding a UUID pk object using the "Add related" admin popup.
    Follow up to refs #25997 but this case wasn't broken.
    
    Backport of 5052f79df45d843d1e44dcc47152ed503220098f from master

--- tests/admin_views/tests.py
+++ tests/admin_views/tests.py
@@ -4586,6 +4586,21 @@ class SeleniumAdminViewsFirefoxTests(AdminSeleniumWebDriverTestCase):
         self.assertEqual(select.first_selected_option.text, str(parent.id))
         self.assertEqual(select.first_selected_option.get_attribute('value'), str(parent.id))
 
+    def test_inline_uuid_pk_add_with_popup(self):
+        from selenium.webdriver.support.ui import Select
+        self.admin_login(username='super', password='secret', login_url=reverse('admin:index'))
+        self.selenium.get(self.live_server_url + reverse('admin:admin_views_relatedwithuuidpkmodel_add'))
+        self.selenium.find_element_by_id('add_id_parent').click()
+        self.wait_for_popup()
+        self.selenium.switch_to.window(self.selenium.window_handles[-1])
+        self.selenium.find_element_by_id('id_title').send_keys('test')
+        self.selenium.find_element_by_xpath('//input[@value="Save"]').click()
+        self.selenium.switch_to.window(self.selenium.window_handles[0])
+        select = Select(self.selenium.find_element_by_id('id_parent'))
+        uuid_id = str(ParentWithUUIDPK.objects.first().id)
+        self.assertEqual(select.first_selected_option.text, uuid_id)
+        self.assertEqual(select.first_selected_option.get_attribute('value'), uuid_id)
+
 
 class SeleniumAdminViewsChromeTests(SeleniumAdminViewsFirefoxTests):
     webdriver_class = 'selenium.webdriver.chrome.webdriver.WebDriver'
