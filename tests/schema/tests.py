6919586bcd87089d37cb95367eea39b63ec697e9
Tim Graham <timograham@gmail.com>
2016-01-22 10:55:31 -0500

    [1.8.x] Fixed #26116 -- Corrected schema's test_alter_implicit_id_to_explicit.
    AUTOINCREMENT is dropped converting an AutoField to IntegerField
    which isn't the point of this test. MySQL would warn or error about
    this.
    
    Backport of b49cc8664306f8b44e9e12ebb9e43791d508ec74 from master

--- tests/schema/tests.py
+++ tests/schema/tests.py
@@ -722,7 +722,7 @@ class SchemaTests(TransactionTestCase):
             editor.create_model(Author)
 
         old_field = Author._meta.get_field("id")
-        new_field = IntegerField(primary_key=True)
+        new_field = AutoField(primary_key=True)
         new_field.set_attributes_from_name("id")
         new_field.model = Author
         with connection.schema_editor() as editor:
@@ -730,6 +730,7 @@ class SchemaTests(TransactionTestCase):
         # This will fail if DROP DEFAULT is inadvertently executed on this
         # field which drops the id sequence, at least on PostgreSQL.
         Author.objects.create(name='Foo')
+        Author.objects.create(name='Bar')
 
     def test_alter_int_pk_to_autofield_pk(self):
         """
