01db3249e5048093c85de33e5395ba737d330d0a
Tim Graham <timograham@gmail.com>
2016-01-28 13:49:51 -0500

    [1.9.x] Fixed #26129 -- Made invalid forms display initial values of disabled fields.
    Backport of 04564eb74d2d92eaf88b22ab8cec7ef45978111e from master

--- django/forms/fields.py
+++ django/forms/fields.py
@@ -177,6 +177,8 @@ class Field(six.with_metaclass(RenameFieldMethods, object)):
         For most fields, this will simply be data; FileFields need to handle it
         a bit differently.
         """
+        if self.disabled:
+            return initial
         return data
 
     def widget_attrs(self, widget):
