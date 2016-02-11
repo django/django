38a069e8dc003051cf0d8a8a357cee84d5a2346a
Jeffrey Riaboy <dakusan@castledragmire.com>
2016-02-11 03:29:28 -0600

    Fixed sql_create_index for mysql
    This needs to use "alter table" instead of "create index" due to this 6 year old bug in mysql:
    https://bugs.mysql.com/bug.php?id=48875

--- django/db/backends/mysql/schema.py
+++ django/db/backends/mysql/schema.py
@@ -24,6 +24,8 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
     sql_create_pk = "ALTER TABLE %(table)s ADD CONSTRAINT %(name)s PRIMARY KEY (%(columns)s)"
     sql_delete_pk = "ALTER TABLE %(table)s DROP PRIMARY KEY"
 
+    sql_create_index = "ALTER TABLE %(table)s ADD KEY %(name)s (%(columns)s) %(extra)s"
+
     def quote_value(self, value):
         # Inner import to allow module to fail to load gracefully
         import MySQLdb.converters
