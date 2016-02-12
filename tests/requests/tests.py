900c629cdf6860e284bb274512df864818d9c932
Berker Peksag <berker.peksag@gmail.com>
2016-02-12 08:54:23 +0200

    Fixed #26126 -- Fixed transient failure of test_max_age_expiration

--- tests/requests/tests.py
+++ tests/requests/tests.py
@@ -14,7 +14,7 @@ from django.http import (
 )
 from django.test import RequestFactory, SimpleTestCase, override_settings
 from django.test.client import FakePayload
-from django.test.utils import str_prefix
+from django.test.utils import freeze_time, str_prefix
 from django.utils import six
 from django.utils.encoding import force_str
 from django.utils.http import cookie_date, urlencode
@@ -246,11 +246,14 @@ class RequestsTests(SimpleTestCase):
 
     def test_max_age_expiration(self):
         "Cookie will expire if max_age is provided"
-        response = HttpResponse()
-        response.set_cookie('max_age', max_age=10)
+        current_time = time.time()
+        with freeze_time(current_time):
+            response = HttpResponse()
+            response.set_cookie('max_age', max_age=10)
+            expected_date = cookie_date(current_time + 10)
         max_age_cookie = response.cookies['max_age']
         self.assertEqual(max_age_cookie['max-age'], 10)
-        self.assertEqual(max_age_cookie['expires'], cookie_date(time.time() + 10))
+        self.assertEqual(max_age_cookie['expires'], expected_date)
 
     def test_httponly_cookie(self):
         response = HttpResponse()
