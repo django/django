8fc0fe1ef4de72026c99dc7008b0089e100305c3
Tim Graham <timograham@gmail.com>
2016-02-17 14:00:54 -0500

    Fixed flake8 typo.

--- tests/raw_query/tests.py
+++ tests/raw_query/tests.py
@@ -311,5 +311,5 @@ class RawQueryTests(TestCase):
 
     def test_decimal_parameter(self):
         c = Coffee.objects.create(brand='starbucks', price=20.5)
-        qs =  Coffee.objects.raw("SELECT * FROM raw_query_coffee WHERE price >= %s", params=[Decimal(20)])
+        qs = Coffee.objects.raw("SELECT * FROM raw_query_coffee WHERE price >= %s", params=[Decimal(20)])
         self.assertEqual(list(qs), [c])
