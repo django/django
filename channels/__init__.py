ca0d9e9651e51797ad317e54c58a174bcb351610
Andrew Godwin <andrew@aeracode.org>
2016-02-21 13:18:57 +0000

    Fix version import during pip install

--- channels/__init__.py
+++ channels/__init__.py
@@ -3,5 +3,8 @@ __version__ = "0.9"
 default_app_config = 'channels.apps.ChannelsConfig'
 DEFAULT_CHANNEL_LAYER = 'default'
 
-from .asgi import channel_layers  # NOQA isort:skip
-from .channel import Channel, Group  # NOQA isort:skip
+try:
+    from .asgi import channel_layers  # NOQA isort:skip
+    from .channel import Channel, Group  # NOQA isort:skip
+except ImportError:  # No django installed, allow vars to be read
+    pass
