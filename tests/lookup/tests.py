1021432958d62550adbded39e5c37f8b0c92ad14
Edwar Baron <edwar.baron@gmail.com>
2016-02-25 23:32:43 -0430

    Fixed #25811 -- Added a helpful error when making _in queries across different databases.

--- tests/lookup/tests.py
+++ tests/lookup/tests.py
@@ -476,6 +476,14 @@ class LookupTests(TestCase):
                 '<Article: Article 1>',
             ])
 
+    def test_in_different_database(self):
+        with self.assertRaisesMessage(
+            ValueError,
+            "Subqueries aren't allowed across different databases. Instead, "
+            "force the inner query to be evaluated using `list(inner_query)`."
+        ):
+            list(Article.objects.filter(id__in=Article.objects.using('other').all()))
+
     def test_error_messages(self):
         # Programming errors are pointed out with nice error messages
         try:
