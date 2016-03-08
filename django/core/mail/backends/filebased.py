635c4a43d8579567a5ae8f4145495d2edb086275
James Hiew <james@hiew.net>
2016-03-08 22:57:40 +0000

    Use .eml extension for file-based EmailBackend

--- django/core/mail/backends/filebased.py
+++ django/core/mail/backends/filebased.py
@@ -52,7 +52,7 @@ class EmailBackend(ConsoleEmailBackend):
         """Return a unique file name."""
         if self._fname is None:
             timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-            fname = "%s-%s.log" % (timestamp, abs(id(self)))
+            fname = "%s-%s.eml" % (timestamp, abs(id(self)))
             self._fname = os.path.join(self.file_path, fname)
         return self._fname
 
