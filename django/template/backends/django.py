f49cfb76c7b24a4fa5f8ac36dfa0a82ab66336c5
Alex Hill <alex@hill.net.au>
2016-03-09 11:35:39 +0800

    [1.9.x] Fixed #26306 -- Fixed memory leak in cached template loader.
    Backport of ecb59cc6579402b68ddfd4499bf30edacf5963be from master

--- django/template/backends/django.py
+++ django/template/backends/django.py
@@ -97,13 +97,24 @@ class Template(object):
             reraise(exc, self.backend)
 
 
+def copy_exception(exc, backend=None):
+    """
+    Create a new TemplateDoesNotExist. Preserve its declared attributes and
+    template debug data but discard __traceback__, __context__, and __cause__
+    to make this object suitable for keeping around (in a cache, for example).
+    """
+    backend = backend or exc.backend
+    new = exc.__class__(*exc.args, tried=exc.tried, backend=backend, chain=exc.chain)
+    if hasattr(exc, 'template_debug'):
+        new.template_debug = exc.template_debug
+    return new
+
+
 def reraise(exc, backend):
     """
     Reraise TemplateDoesNotExist while maintaining template debug information.
     """
-    new = exc.__class__(*exc.args, tried=exc.tried, backend=backend)
-    if hasattr(exc, 'template_debug'):
-        new.template_debug = exc.template_debug
+    new = copy_exception(exc, backend)
     six.reraise(exc.__class__, new, sys.exc_info()[2])
 
 
