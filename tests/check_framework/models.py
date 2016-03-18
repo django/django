5eff8a77838540b27b6bef024dfccfd76008fd4c
Adam Chainz <adam@adamj.eu>
2016-03-18 14:24:13 +0000

    Fixed #25415 -- Made DiscoverRunner run system checks.

--- tests/check_framework/models.py
+++ tests/check_framework/models.py
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.core.checks import register
 from django.db import models
 
 
 class SimpleModel(models.Model):
     field = models.IntegerField()
     manager = models.manager.Manager()
+
+
+@register('tests')
+def my_check(app_configs, **kwargs):
+    my_check.did_run = True
+    return []
+
+
+my_check.did_run = False
