abf07355aa5d85f4bb4dd9912d70fa62511fa40f
Vincenzo Pandolfo <pandolfovince@gmail.com>
2016-03-18 12:48:14 +0000

    Fixed #26365 -- Added a system check to ensure "string_is_invalid" is a string.

--- django/core/checks/templates.py
+++ django/core/checks/templates.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import copy
+
 from django.conf import settings
+from django.utils import six
 
 from . import Error, Tags, register
 
@@ -9,6 +12,10 @@ E001 = Error(
     "You have 'APP_DIRS': True in your TEMPLATES but also specify 'loaders' "
     "in OPTIONS. Either remove APP_DIRS or remove the 'loaders' option.",
     id='templates.E001',
+)
+E002 = Error(
+    "'string_if_invalid' in TEMPLATES OPTIONS must be a string but got: {} ({}).",
+    id="templates.E002",
 )
 
 
@@ -21,3 +28,15 @@ def check_setting_app_dirs_loaders(app_configs, **kwargs):
         if 'loaders' in conf.get('OPTIONS', {}):
             passed_check = False
     return [] if passed_check else [E001]
+
+
+@register(Tags.templates)
+def check_string_if_invalid_is_string(app_configs, **kwargs):
+    errors = []
+    for conf in settings.TEMPLATES:
+        string_if_invalid = conf.get('OPTIONS', {}).get('string_if_invalid', '')
+        if not isinstance(string_if_invalid, six.string_types):
+            error = copy.copy(E002)
+            error.msg = error.msg.format(string_if_invalid, type(string_if_invalid).__name__)
+            errors.append(error)
+    return errors
