4509017668d1dce531234b269ec4e01663810af2
Vincenzo Pandolfo <pandolfovince@gmail.com>
2016-03-19 11:15:41 +0000

    Added support and tests to the case when len(TEMPLATES) > 1 as suggested by review

--- django/core/checks/templates.py
+++ django/core/checks/templates.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import copy
+
 from django.conf import settings
 from django.utils import six
 
@@ -12,8 +14,7 @@ E001 = Error(
     id='templates.E001',
 )
 E002 = Error(
-    "'string_if_invalid' in TEMPLATES OPTIONS "
-    "must be a string (got ({}) ({}))",
+    "'string_if_invalid' in TEMPLATES OPTIONS must be a string but got: {} ({}).",
     id="templates.E002",
 )
 
@@ -31,13 +32,14 @@ def check_setting_app_dirs_loaders(app_configs, **kwargs):
 
 @register(Tags.templates)
 def check_string_if_invalid_is_string(app_configs, **kwargs):
-    passed_check = True
+    errors = []
     for conf in settings.TEMPLATES:
-        string_if_invalid = conf.get('OPTIONS', {}).get('string_if_invalid', "")
+        string_if_invalid = conf.get('OPTIONS', {}).get('string_if_invalid', '')
         if not isinstance(string_if_invalid, six.string_types):
-            passed_check = False
-            E002.msg = E002.msg.format(
+            error = copy.copy(E002)
+            error.msg = error.msg.format(
                 string_if_invalid,
                 type(string_if_invalid).__name__
             )
-    return [] if passed_check else [E002]
+            errors.append(error)
+    return errors
--- tests/check_framework/test_templates.py
+++ tests/check_framework/test_templates.py
@@ -1,4 +1,4 @@
-from copy import deepcopy
+from copy import copy, deepcopy
 
 from django.core.checks.templates import E001, E002
 from django.test import SimpleTestCase
@@ -49,7 +49,25 @@ class CheckTemplateStringIfInvalidTest(SimpleTestCase):
                 'string_if_invalid': False,
             },
         },
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'OPTIONS': {
+                'string_if_invalid': 42,
+            },
+        },
     ]
+
+    @classmethod
+    def setUpClass(self):
+        super(CheckTemplateStringIfInvalidTest, self).setUpClass()
+        self.error1 = copy(E002)
+        self.error2 = copy(E002)
+
+        string_if_invalid1 = self.TEMPLATES_STRING_IF_INVALID[0]['OPTIONS']['string_if_invalid']
+        string_if_invalid2 = self.TEMPLATES_STRING_IF_INVALID[1]['OPTIONS']['string_if_invalid']
+
+        self.error1.msg = self.error1.msg.format(string_if_invalid1, type(string_if_invalid1).__name__)
+        self.error2.msg = self.error2.msg.format(string_if_invalid2, type(string_if_invalid2).__name__)
 
     @property
     def func(self):
@@ -58,14 +76,29 @@ class CheckTemplateStringIfInvalidTest(SimpleTestCase):
 
     @override_settings(TEMPLATES=TEMPLATES_STRING_IF_INVALID)
     def test_string_if_invalid_not_string(self):
-        self.assertEqual(self.func(None), [E002])
+        self.assertEqual(self.func(None), [self.error1, self.error2])
 
-    def test_string_if_invalid_is_string(self):
+    def test_string_if_invalid_first_is_string(self):
         TEMPLATES = deepcopy(self.TEMPLATES_STRING_IF_INVALID)
         TEMPLATES[0]['OPTIONS']['string_if_invalid'] = 'test'
-        self.assertEqual(self.func(None), [])
+        with self.settings(TEMPLATES=TEMPLATES):
+            self.assertEqual(self.func(None), [self.error2])
 
-    def test_string_if_invalid_not_specified(self):
+    def test_string_if_invalid_both_are_strings(self):
+        TEMPLATES = deepcopy(self.TEMPLATES_STRING_IF_INVALID)
+        TEMPLATES[0]['OPTIONS']['string_if_invalid'] = 'test'
+        TEMPLATES[1]['OPTIONS']['string_if_invalid'] = 'test'
+        with self.settings(TEMPLATES=TEMPLATES):
+            self.assertEqual(self.func(None), [])
+
+    def test_string_if_invalid_first_not_specified(self):
         TEMPLATES = deepcopy(self.TEMPLATES_STRING_IF_INVALID)
         del TEMPLATES[0]['OPTIONS']['string_if_invalid']
-        self.assertEqual(self.func(None), [])
+        with self.settings(TEMPLATES=TEMPLATES):
+            self.assertEqual(self.func(None), [self.error2])
+
+    def test_string_if_invalid_second_not_specified(self):
+        TEMPLATES = deepcopy(self.TEMPLATES_STRING_IF_INVALID)
+        del TEMPLATES[1]['OPTIONS']['string_if_invalid']
+        with self.settings(TEMPLATES=TEMPLATES):
+            self.assertEqual(self.func(None), [self.error1])
