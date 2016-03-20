aa31eeae4c598e0ef81b1b6b4570dd44df3dfb63
Michal Petrucha <michal.petrucha@koniiiik.org>
2016-03-20 18:10:55 +0100

    Refs #16508 -- Renamed the current "virtual" fields to "private".
    The only reason why GenericForeignKey and GenericRelation are stored
    separately inside _meta is that they need to be cloned for every model
    subclass, but that's not true for any other virtual field. Actually,
    it's only true for GenericRelation.

--- tests/model_meta/tests.py
+++ tests/model_meta/tests.py
@@ -157,11 +157,11 @@ class RelatedObjectsTests(OptionsBaseTests):
             )
 
 
-class VirtualFieldsTests(OptionsBaseTests):
+class PrivateFieldsTests(OptionsBaseTests):
 
-    def test_virtual_fields(self):
-        for model, expected_names in TEST_RESULTS['virtual_fields'].items():
-            objects = model._meta.virtual_fields
+    def test_private_fields(self):
+        for model, expected_names in TEST_RESULTS['private_fields'].items():
+            objects = model._meta.private_fields
             self.assertEqual(sorted([f.name for f in objects]), sorted(expected_names))
 
 
