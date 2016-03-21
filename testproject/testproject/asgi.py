caa589ae708a1a66ba1bdcd24f5fd473040772bd
Andrew Godwin <andrew@aeracode.org>
2016-03-20 17:12:19 -0700

    Correct project name in testproject.asgi

--- testproject/testproject/asgi.py
+++ testproject/testproject/asgi.py
@@ -1,6 +1,5 @@
 import os
 from channels.asgi import get_channel_layer
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_project.settings")
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testproject.settings")
 channel_layer = get_channel_layer()
