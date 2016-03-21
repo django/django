aabee26d24898b3569502a333435ce628b476cc4
Cristiano <cristianocca@hotmail.com>
2016-03-20 22:51:17 -0300

    Fixed #26058 -- Delegated os.path bits of FileField's filename generation to the Storage.

--- tests/file_storage/tests.py
+++ tests/file_storage/tests.py
@@ -817,7 +817,7 @@ class FileFieldStorageTests(TestCase):
         # upload_to can be empty, meaning it does not use subdirectory.
         obj = Storage()
         obj.empty.save('django_test.txt', ContentFile('more content'))
-        self.assertEqual(obj.empty.name, "./django_test.txt")
+        self.assertEqual(obj.empty.name, "django_test.txt")
         self.assertEqual(obj.empty.read(), b"more content")
         obj.empty.close()
 
