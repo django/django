a70b27f98e14631fef5ce0eee7202ba4a6c54118
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2016-03-21 15:10:30 +0300

    tests for template relative paths patch

---
+++ tests/template_tests/test_extends_relative.py
@@ -0,0 +1,59 @@
+import os
+
+from django.template import Context, Engine, TemplateDoesNotExist
+from django.template.loader_tags import ExtendsError
+from django.template.loaders.base import Loader
+from django.test import SimpleTestCase, ignore_warnings
+
+from .utils import ROOT
+
+RELATIVE = os.path.join(ROOT, 'relative_templates')
+
+
+class ExtendsRelativeBehaviorTests(SimpleTestCase):
+
+    def test_normal_extend(self):
+        engine = Engine(dirs=[RELATIVE])
+        template = engine.get_template('one.html')
+        output = template.render(Context({}))
+        self.assertEqual(output.strip(), 'three two one')
+
+    def test_dir1_extend(self):
+        engine = Engine(dirs=[RELATIVE])
+        template = engine.get_template('dir1/one.html')
+        output = template.render(Context({}))
+        self.assertEqual(output.strip(), 'three two one dir1 one')
+
+    def test_dir2_extend(self):
+        engine = Engine(dirs=[RELATIVE])
+        template = engine.get_template('dir1/dir2/one.html')
+        output = template.render(Context({}))
+        self.assertEqual(output.strip(), 'three two one dir2 one')
+
+    def test_extend_error(self):
+        engine = Engine(dirs=[RELATIVE])
+        msg = 'Error'
+
+        with self.assertRaisesMessage(ExtendsError, msg):
+            engine.render_to_string('error_extends.html')
+
+class IncludeRelativeBehaviorTests(SimpleTestCase):
+
+    def test_normal_include(self):
+        engine = Engine(dirs=[RELATIVE])
+        template = engine.get_template('dir1/dir2/inc2.html')
+        output = template.render(Context({}))
+        self.assertEqual(output.strip(), 'dir2 include')
+
+    def test_dir2_include(self):
+        engine = Engine(dirs=[RELATIVE])
+        template = engine.get_template('dir1/dir2/inc1.html')
+        output = template.render(Context({}))
+        self.assertEqual(output.strip(), 'three')
+
+    def test_include_error(self):
+        engine = Engine(dirs=[RELATIVE])
+        msg = 'Error'
+
+        with self.assertRaisesMessage(ExtendsError, msg):
+            engine.render_to_string('error_include.html')
