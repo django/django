db4622c9665e89f93fd9bcc70f865de6da142052
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2016-03-21 16:29:22 +0300

    fix error messages

--- tests/template_tests/test_extends_relative.py
+++ tests/template_tests/test_extends_relative.py
@@ -32,7 +32,7 @@ class ExtendsRelativeBehaviorTests(SimpleTestCase):
 
     def test_extend_error(self):
         engine = Engine(dirs=[RELATIVE])
-        msg = "Relative name '..two.html' have more parent folders, then given name 'error_extends.html'"
+        msg = "Relative name '\"..two.html\"' have more parent folders, then given name 'error_extends.html'"
 
         with self.assertRaisesMessage(TemplateSyntaxError, msg):
             engine.render_to_string('error_extends.html')
@@ -53,7 +53,7 @@ class IncludeRelativeBehaviorTests(SimpleTestCase):
 
     def test_include_error(self):
         engine = Engine(dirs=[RELATIVE])
-        msg = "Relative name '..three.html' have more parent folders, then given name 'error_include.html'"
+        msg = "Relative name '\"..three.html\"' have more parent folders, then given name 'error_include.html'"
 
         with self.assertRaisesMessage(TemplateSyntaxError, msg):
             engine.render_to_string('error_include.html')
