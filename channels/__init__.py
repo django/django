13d6c88c7fcfb4d922cd1f3b5237780f0fe1ec75
Andrew Godwin <andrew@aeracode.org>
2016-03-21 16:46:42 -0700

    Releasing 0.10.0

--- channels/__init__.py
+++ channels/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.9.5"
+__version__ = "0.10.0"
 
 default_app_config = 'channels.apps.ChannelsConfig'
 DEFAULT_CHANNEL_LAYER = 'default'
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'Django>=1.8',
-        'asgiref>=0.9',
-        'daphne>=0.9.2',
+        'asgiref>=0.10',
+        'daphne>=0.10',
     ]
 )
