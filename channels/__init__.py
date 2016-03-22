10224ff06c7ea7db462690bd7b50c85a43073240
Andrew Godwin <andrew@aeracode.org>
2016-03-22 10:20:15 -0700

    Flake, stop worrying about this file.

--- channels/__init__.py
+++ channels/__init__.py
@@ -6,6 +6,6 @@ DEFAULT_CHANNEL_LAYER = 'default'
 try:
     from .asgi import channel_layers  # NOQA isort:skip
     from .channel import Channel, Group  # NOQA isort:skip
-    from .routing import route, include
+    from .routing import route, include  # NOQA isort:skip
 except ImportError:  # No django installed, allow vars to be read
     pass
