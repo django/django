32c8e43ef1e859fa55af2f470a2a1120f51afabe
Amine Yaiche <yaiche.amin@gmail.com>
2016-03-22 19:39:09 +0100

    Fixed #26378 -- Allowed a left byte of zero in mixed IPv4/IPv6 validation.

--- tests/utils_tests/test_ipv6.py
+++ tests/utils_tests/test_ipv6.py
@@ -50,6 +50,8 @@ class TestUtilsIPv6(unittest.TestCase):
         self.assertEqual(clean_ipv6_address('::ffff:0a0a:0a0a'), '::ffff:10.10.10.10')
         self.assertEqual(clean_ipv6_address('::ffff:1234:1234'), '::ffff:18.52.18.52')
         self.assertEqual(clean_ipv6_address('::ffff:18.52.18.52'), '::ffff:18.52.18.52')
+        self.assertEqual(clean_ipv6_address('::ffff:0.52.18.52'), '::ffff:0.52.18.52')
+        self.assertEqual(clean_ipv6_address('::ffff:0.0.0.0'), '::ffff:0.0.0.0')
 
     def test_unpacks_ipv4(self):
         self.assertEqual(clean_ipv6_address('::ffff:0a0a:0a0a', unpack_ipv4=True), '10.10.10.10')
