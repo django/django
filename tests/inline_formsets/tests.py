d35c0bcc03a0dd35b595b3cd6ac416caedab1b80
Simon Charette <charette.s@gmail.com>
2016-03-24 20:04:25 -0400

    Fixed #25987 -- Made inline formset validation respect unique_together with an unsaved parent object.
    Thanks Anton Kuzmichev for the report and Tim for the review.

--- tests/inline_formsets/tests.py
+++ tests/inline_formsets/tests.py
@@ -162,3 +162,17 @@ class InlineFormsetFactoryTest(TestCase):
         PoemFormSet = inlineformset_factory(Poet, Poem, fields="__all__", extra=0)
         formset = PoemFormSet(None, instance=poet)
         self.assertEqual(len(formset.forms), 1)
+
+    def test_unsaved_fk_validate_unique(self):
+        poet = Poet(name='unsaved')
+        PoemFormSet = inlineformset_factory(Poet, Poem, fields=['name'])
+        data = {
+            'poem_set-TOTAL_FORMS': '2',
+            'poem_set-INITIAL_FORMS': '0',
+            'poem_set-MAX_NUM_FORMS': '2',
+            'poem_set-0-name': 'Poem',
+            'poem_set-1-name': 'Poem',
+        }
+        formset = PoemFormSet(data, instance=poet)
+        self.assertFalse(formset.is_valid())
+        self.assertEqual(formset.non_form_errors(), ['Please correct the duplicate data for name.'])
