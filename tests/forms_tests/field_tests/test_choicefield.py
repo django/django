bc0b828c263095a02e867ed807934b0f083a5969
Jon Dufresne <jon.dufresne@gmail.com>
2016-03-28 11:02:04 -0700

    Fixed #22383 -- Added support for HTML5 required attribute on required form fields.

--- tests/forms_tests/field_tests/test_choicefield.py
+++ tests/forms_tests/field_tests/test_choicefield.py
@@ -81,6 +81,6 @@ class ChoiceFieldTest(FormFieldAssertionsMixin, SimpleTestCase):
         f = ChoiceField(choices=[('J', 'John'), ('P', 'Paul')], disabled=True)
         self.assertWidgetRendersTo(
             f,
-            '<select id="id_f" name="f" disabled><option value="J">John</option>'
+            '<select id="id_f" name="f" disabled required><option value="J">John</option>'
             '<option value="P">Paul</option></select>'
         )
--- tests/forms_tests/field_tests/test_durationfield.py
+++ tests/forms_tests/field_tests/test_durationfield.py
@@ -24,7 +24,7 @@ class DurationFieldTest(FormFieldAssertionsMixin, SimpleTestCase):
     def test_durationfield_render(self):
         self.assertWidgetRendersTo(
             DurationField(initial=datetime.timedelta(hours=1)),
-            '<input id="id_f" type="text" name="f" value="01:00:00">',
+            '<input id="id_f" type="text" name="f" value="01:00:00" required>',
         )
 
     def test_durationfield_integer_value(self):
