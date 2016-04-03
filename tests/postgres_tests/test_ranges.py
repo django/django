6e44096f5a11b8b4bf0acbbd90a2a1a815ea2fb7
anna <annarschneider@gmail.com>
2016-04-03 12:39:18 +0200

    Fixed #26101 -- Allowed introspection of base_field.model in RangeField
    Used the same test and fix as in #25867.
    This required initializing base_field in RangeField.__init__,
    not when setting the attribute.

--- tests/postgres_tests/test_ranges.py
+++ tests/postgres_tests/test_ranges.py
@@ -80,6 +80,12 @@ class TestSaveLoad(PostgreSQLTestCase):
         loaded = RangesModel.objects.get()
         self.assertIsNone(loaded.ints)
 
+    def test_model_set_on_base_field(self):
+        instance = RangesModel()
+        field = instance._meta.get_field('ints')
+        self.assertEqual(field.model, RangesModel)
+        self.assertEqual(field.base_field.model, RangesModel)
+
 
 class TestQuerying(PostgreSQLTestCase):
 
