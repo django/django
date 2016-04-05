4bab456c6148f9ea9b909f9e875d81ec49552f47
Andrew Godwin <andrew@aeracode.org>
2016-04-05 16:44:26 -0700

    Fail to work if you have channel-enabled Django

--- channels/apps.py
+++ channels/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.core.exceptions import ImproperlyConfigured
 
 
 class ChannelsConfig(AppConfig):
@@ -7,6 +8,13 @@ class ChannelsConfig(AppConfig):
     verbose_name = "Channels"
 
     def ready(self):
+        # Check you're not running 1.10 or above
+        try:
+            from django import channels
+        except ImportError:
+            pass
+        else:
+            raise ImproperlyConfigured("You have Django 1.10 or above; use the builtin django.channels!")
         # Do django monkeypatches
         from .hacks import monkeypatch_django
         monkeypatch_django()
