829746c80d0541cce0e479fac579d4c5011fe5c3
Carl Worth <cworth@cworth.org>
2016-04-13 14:38:17 -0700

    Add logging_tests for 404 warnings are logged
    Simply ensuring that an existing page generates no warning, while a
    non-existent page does result in a warning in the log.
    
    Similarly, for a language-prefixed page, we also ensure that a warning
    is logged if a page is not found.
    
    All of these tests are expected to pass currently. (Notice that there
    is no test for ensuring that a language-prefixed page does not
    generate a warning if the page is found---there's currently a bug in
    that case, so a test will be added for that along with the bug-fix in
    a subsequent commit.)

--- tests/logging_tests/tests.py
+++ tests/logging_tests/tests.py
@@ -66,6 +66,7 @@ class LoggingFiltersTest(SimpleTestCase):
             self.assertEqual(filter_.filter("record is not used"), False)
 
 
+@override_settings(ROOT_URLCONF='logging_tests.urls')
 class DefaultLoggingTest(LoggingCaptureMixin, SimpleTestCase):
 
     @classmethod
@@ -104,6 +105,47 @@ class DefaultLoggingTest(LoggingCaptureMixin, SimpleTestCase):
         with self.settings(DEBUG=True):
             self.logger.debug('debug')
             self.assertEqual(self.logger_output.getvalue(), '')
+
+    def test_page_found_no_warning(self):
+        with self.settings(DEBUG=True):
+            self.client.get('/innocent/')
+            self.assertEqual(self.logger_output.getvalue(), '')
+
+    def test_page_not_found_warning(self):
+        with self.settings(DEBUG=True):
+            self.client.get('/does_not_exist/')
+            self.assertEqual(self.logger_output.getvalue(), 'Not Found: /does_not_exist/\n')
+
+
+@override_settings(
+    USE_I18N=True,
+    LANGUAGES=[
+        ('en', 'English'),
+    ],
+    MIDDLEWARE_CLASSES=[
+        'django.middleware.locale.LocaleMiddleware',
+        'django.middleware.common.CommonMiddleware',
+    ],
+    ROOT_URLCONF='logging_tests.urls_i18n',
+)
+class I18NLoggingTest(LoggingCaptureMixin, SimpleTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(I18NLoggingTest, cls).setUpClass()
+        cls._logging = settings.LOGGING
+        logging.config.dictConfig(DEFAULT_LOGGING)
+
+    @classmethod
+    def tearDownClass(cls):
+        super(I18NLoggingTest, cls).tearDownClass()
+        logging.config.dictConfig(cls._logging)
+
+    def test_i18n_page_not_found_warning(self):
+        with self.settings(DEBUG=True):
+            self.client.get('/this_does_not/')
+            self.client.get('/en/nor_this/')
+            self.assertEqual(self.logger_output.getvalue(), 'Not Found: /this_does_not/\nNot Found: /en/nor_this/\n')
 
 
 class WarningLoggerTests(SimpleTestCase):
