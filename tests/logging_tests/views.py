5e00b14403db75d83a274864ecd008d0e826c61d
Carl Worth <cworth@cworth.org>
2016-04-13 14:38:17 -0700

    Added tests for logging of Http404 warnings.

--- tests/logging_tests/views.py
+++ tests/logging_tests/views.py
@@ -1,6 +1,11 @@
 from __future__ import unicode_literals
 
 from django.core.exceptions import DisallowedHost, SuspiciousOperation
+from django.http import HttpResponse
+
+
+def innocent(request):
+    return HttpResponse('innocent')
 
 
 def suspicious(request):
