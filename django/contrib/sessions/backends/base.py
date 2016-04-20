845d43e37ed0086baa55ba791a5d386da54e943b
Tobias Kroenke <tobias@kroenke.de>
2016-04-20 10:36:59 +0200

    [1.9.x] Fixed #26520 -- Fixed a regression where SessionBase.pop() didn't return a KeyError.
    Backport of b040ac06ebba2348cece7390b88f746d2c91d07b from master

--- django/contrib/sessions/backends/base.py
+++ django/contrib/sessions/backends/base.py
@@ -35,6 +35,8 @@ class SessionBase(object):
     TEST_COOKIE_NAME = 'testcookie'
     TEST_COOKIE_VALUE = 'worked'
 
+    __not_given = object()
+
     def __init__(self, session_key=None):
         self._session_key = session_key
         self.accessed = False
@@ -58,9 +60,10 @@ class SessionBase(object):
     def get(self, key, default=None):
         return self._session.get(key, default)
 
-    def pop(self, key, default=None):
+    def pop(self, key, default=__not_given):
         self.modified = self.modified or key in self._session
-        return self._session.pop(key, default)
+        args = () if default is self.__not_given else (default,)
+        return self._session.pop(key, *args)
 
     def setdefault(self, key, value):
         if key in self._session:
