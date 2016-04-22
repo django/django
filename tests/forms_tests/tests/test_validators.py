055953ab0c47c1db10456027dcce4d1fa8782df9
Loïc Bistuer <loic.bistuer@gmail.com>
2016-04-22 07:18:43 +0700

    Fixed #26528 -- Allowed any iterable (e.g. tuple) as validators kwarg for form/model fields.

--- tests/forms_tests/tests/test_validators.py
+++ tests/forms_tests/tests/test_validators.py
@@ -52,3 +52,17 @@ class TestFieldWithValidators(TestCase):
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['string'], ["Letters only."])
         self.assertEqual(form.errors['string'], ["Letters only."])
+
+    def test_field_validators_can_be_any_iterable(self):
+        class UserForm(forms.Form):
+            full_name = forms.CharField(
+                max_length=50,
+                validators=(
+                    validators.validate_integer,
+                    validators.validate_email,
+                )
+            )
+
+        form = UserForm({'full_name': 'not int nor mail'})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['full_name'], ['Enter a valid integer.', 'Enter a valid email address.'])
