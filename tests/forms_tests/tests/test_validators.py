cb7924f9743951dcea7bc5279db376763b43a9df
Loïc Bistuer <loic.bistuer@gmail.com>
2016-04-22 07:23:09 +0700

    Moved declaration of test form inside the relevant test for clarity.

--- tests/forms_tests/tests/test_validators.py
+++ tests/forms_tests/tests/test_validators.py
@@ -8,38 +8,36 @@ from django.core import validators
 from django.core.exceptions import ValidationError
 
 
-class UserForm(forms.Form):
-    full_name = forms.CharField(
-        max_length=50,
-        validators=[
-            validators.validate_integer,
-            validators.validate_email,
-        ]
-    )
-    string = forms.CharField(
-        max_length=50,
-        validators=[
-            validators.RegexValidator(
-                regex='^[a-zA-Z]*$',
-                message="Letters only.",
-            )
-        ]
-    )
-    ignore_case_string = forms.CharField(
-        max_length=50,
-        validators=[
-            validators.RegexValidator(
-                regex='^[a-z]*$',
-                message="Letters only.",
-                flags=re.IGNORECASE,
-            )
-        ]
-
-    )
-
-
 class TestFieldWithValidators(TestCase):
     def test_all_errors_get_reported(self):
+        class UserForm(forms.Form):
+            full_name = forms.CharField(
+                max_length=50,
+                validators=[
+                    validators.validate_integer,
+                    validators.validate_email,
+                ]
+            )
+            string = forms.CharField(
+                max_length=50,
+                validators=[
+                    validators.RegexValidator(
+                        regex='^[a-zA-Z]*$',
+                        message="Letters only.",
+                    )
+                ]
+            )
+            ignore_case_string = forms.CharField(
+                max_length=50,
+                validators=[
+                    validators.RegexValidator(
+                        regex='^[a-z]*$',
+                        message="Letters only.",
+                        flags=re.IGNORECASE,
+                    )
+                ]
+            )
+
         form = UserForm({
             'full_name': 'not int nor mail',
             'string': '2 is not correct',
