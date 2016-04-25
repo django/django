0c03e5cbd7888a323b107b7650fd8083f3c6fe6a
Conrad Kramer <conrad@kramerapps.com>
2016-04-25 16:30:48 -0700

    Fixed #26542 -- Fixed quoting in CreateExtension operation.

--- tests/postgres_tests/migrations/0001_setup_extensions.py
+++ tests/postgres_tests/migrations/0001_setup_extensions.py
@@ -5,20 +5,21 @@ from django.db import migrations
 
 try:
     from django.contrib.postgres.operations import (
-        HStoreExtension, UnaccentExtension,
+        CreateExtension, HStoreExtension, UnaccentExtension,
     )
 except ImportError:
     from django.test import mock
+    CreateExtension = mock.Mock()
     HStoreExtension = mock.Mock()
     UnaccentExtension = mock.Mock()
 
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-    ]
-
     operations = [
+        # Ensure CreateExtension quotes extension names by creating one with a
+        # dash in its name.
+        CreateExtension('uuid-ossp'),
         HStoreExtension(),
         UnaccentExtension(),
     ]
