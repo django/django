bad76fdd92ba4b52f0409dc5c3916783647597a6
Eugene Makarov <it.pm.kiev@gmail.com>
2016-04-26 19:31:50 +0000

    Fixed #26431 -- translate_url() creates an incorrect URL when optional named groups are missing in the URL pattern

--- tests/i18n/patterns/tests.py
+++ tests/i18n/patterns/tests.py
@@ -156,10 +156,10 @@ class URLTranslationTests(URLTestCaseBase):
             self.assertEqual(translate_url('/en/non-existent/', 'nl'), '/en/non-existent/')
             self.assertEqual(translate_url('/en/users/', 'nl'), '/nl/gebruikers/')
             # URL with arguments
-            self.assertEqual(translate_url('/en/with-arguments/regular-argument/', 'nl'), 
-                                           '/nl/with-arguments/regular-argument/')
-            self.assertEqual(translate_url('/en/with-arguments/regular-argument/optional-argument.html', 'nl'), 
-                                           '/nl/with-arguments/regular-argument/optional-argument.html')
+            self.assertEqual(translate_url('/en/with-arguments/regular-argument/', 'nl'),
+                             '/nl/with-arguments/regular-argument/')
+            self.assertEqual(translate_url('/en/with-arguments/regular-argument/optional-argument.html', 'nl'),
+                             '/nl/with-arguments/regular-argument/optional-argument.html')
             # Namespaced URL
             self.assertEqual(translate_url('/en/account/register/', 'nl'), '/nl/profiel/registeren/')
             self.assertEqual(translation.get_language(), 'en')
