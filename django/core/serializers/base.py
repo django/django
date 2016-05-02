6323da4d98f3dc6120f225159f57fba12b56903c
Offby-One Kenobi <gunbei@10xengineers.org>
2016-05-02 15:43:03 +0000

    Fixed docstring typos.

--- django/core/serializers/base.py
+++ django/core/serializers/base.py
@@ -22,7 +22,7 @@ class DeserializationError(Exception):
     def WithData(cls, original_exc, model, fk, field_value):
         """
         Factory method for creating a deserialization error which has a more
-        explanatory messsage.
+        explanatory message.
         """
         return cls("%s: (%s:pk=%s) field_value was '%s'" % (original_exc, model, fk, field_value))
 
