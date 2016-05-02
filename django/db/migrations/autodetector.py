64206b8540db82ff9f6df8a5ebccb597174f61af
Alexander van Ratingen <alexandervanratingen@gmail.com>
2016-05-02 15:57:00 +0200

    Fixed support of ForeignKey subclasses.
    Allows subclasses of ForeignKey which omit the 'to' argument.

--- django/db/migrations/autodetector.py
+++ django/db/migrations/autodetector.py
@@ -100,7 +100,7 @@ class MigrationAutodetector(object):
         for name, field in sorted(fields):
             deconstruction = self.deep_deconstruct(field)
             if field.remote_field and field.remote_field.model:
-                del deconstruction[2]['to']
+                deconstruction[2].pop('to', None)
             fields_def.append(deconstruction)
         return fields_def
 
