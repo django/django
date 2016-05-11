d536683c9458e3b3b4842c5487b273aaafd0519d
Nicolas Noé <nicolas@niconoe.eu>
2016-05-11 15:42:28 +0200

    Moved tests to gis_tests/geos_tests/test_geos.py

--- tests/gis_tests/geos_tests/test_geos.py
+++ tests/gis_tests/geos_tests/test_geos.py
@@ -1259,6 +1259,80 @@ class GEOSTest(SimpleTestCase, TestDataMixin):
             self.assertEqual(m.group('version'), v_geos)
             self.assertEqual(m.group('capi_version'), v_capi)
 
+    def test_deconstructible(self):
+        """
+        Ticket #26333: Make sure GIS geometries classes are deconstructible.
+        """
+
+        # Test against Point
+        point = Point(4.337844, 50.827537, srid=4326)
+        path, args, kwargs = point.deconstruct()
+        self.assertEqual(path, 'django.contrib.gis.geos.point.Point')
+        self.assertEqual(args, (4.337844, 50.827537))
+        self.assertEqual(kwargs, {'srid': 4326})
+
+        # LineString
+        ls = LineString(((0, 0), (1, 1)))
+        path, args, kwargs = ls.deconstruct()
+        self.assertEqual(path, 'django.contrib.gis.geos.linestring.LineString')
+        self.assertEqual(args, (((0, 0), (1, 1)),))
+        self.assertEqual(kwargs, {})
+
+        ls2 = LineString([Point(0, 0), Point(1, 1)], srid=4326)
+        path, args, kwargs = ls2.deconstruct()
+        self.assertEqual(path, 'django.contrib.gis.geos.linestring.LineString')
+        self.assertEqual(args, ([Point(0, 0), Point(1, 1)], ))
+        self.assertEqual(kwargs, {'srid': 4326})
+
+        # Polygon
+        ext_coords = ((0, 0), (0, 1), (1, 1), (1, 0), (0, 0))
+        int_coords = ((0.4, 0.4), (0.4, 0.6), (0.6, 0.6), (0.6, 0.4), (0.4, 0.4))
+        poly = Polygon(ext_coords, int_coords)
+        path, args, kwargs = poly.deconstruct()
+        self.assertEqual(path, 'django.contrib.gis.geos.polygon.Polygon')
+        self.assertEqual(args, (ext_coords, int_coords, ))
+        self.assertEqual(kwargs, {})
+
+        # LinearRing
+        lr = LinearRing((0, 0), (0, 1), (1, 1), (0, 0))
+        path, args, kwargs = lr.deconstruct()
+        self.assertEqual(path, 'django.contrib.gis.geos.linestring.LinearRing')
+        self.assertEqual(args, ((0, 0), (0, 1), (1, 1), (0, 0)))
+        self.assertEqual(kwargs, {})
+
+        # MultiPoint
+        mp = MultiPoint(Point(0, 0), Point(1, 1))
+        path, args, kwargs = mp.deconstruct()
+        self.assertEqual(path, 'django.contrib.gis.geos.collections.MultiPoint')
+        self.assertEqual(args, (Point(0, 0), Point(1, 1), ))
+        self.assertEqual(kwargs, {})
+
+        # MultiLineString
+        ls1 = LineString((0, 0), (1, 1))
+        ls2 = LineString((2, 2), (3, 3))
+        mls = MultiLineString(ls1, ls2)
+        path, args, kwargs = mls.deconstruct()
+        self.assertEqual(path, 'django.contrib.gis.geos.collections.MultiLineString')
+        self.assertEqual(args, (ls1, ls2, ))
+        self.assertEqual(kwargs, {})
+
+        # MultiPolygon
+        p1 = Polygon(((0, 0), (0, 1), (1, 1), (0, 0)))
+        p2 = Polygon(((1, 1), (1, 2), (2, 2), (1, 1)))
+        mp = MultiPolygon(p1, p2)
+        path, args, kwargs = mp.deconstruct()
+        self.assertEqual(path, 'django.contrib.gis.geos.collections.MultiPolygon')
+        self.assertEqual(args, (p1, p2, ))
+        self.assertEqual(kwargs, {})
+
+        # GeometryCollection
+        poly = Polygon(((0, 0), (0, 1), (1, 1), (0, 0)))
+        gc = GeometryCollection(Point(0, 0), MultiPoint(Point(0, 0), Point(1, 1)), poly)
+        path, args, kwargs = gc.deconstruct()
+        self.assertEqual(path, 'django.contrib.gis.geos.collections.GeometryCollection')
+        self.assertEqual(args, (Point(0, 0), MultiPoint(Point(0, 0), Point(1, 1)), poly, ))
+        self.assertEqual(kwargs, {})
+
     @ignore_warnings(category=RemovedInDjango20Warning)
     def test_deprecated_srid_getters_setters(self):
         p = Point(1, 2, srid=123)
--- tests/gis_tests/test_geoforms.py
+++ tests/gis_tests/test_geoforms.py
@@ -2,10 +2,7 @@ from unittest import skipUnless
 
 from django.contrib.gis import forms
 from django.contrib.gis.gdal import HAS_GDAL
-from django.contrib.gis.geos import (
-    GeometryCollection, GEOSGeometry, LinearRing, LineString, MultiLineString,
-    MultiPoint, MultiPolygon, Point, Polygon,
-)
+from django.contrib.gis.geos import GEOSGeometry
 from django.forms import ValidationError
 from django.test import SimpleTestCase, override_settings, skipUnlessDBFeature
 from django.utils.html import escape
@@ -14,79 +11,6 @@ from django.utils.html import escape
 @skipUnless(HAS_GDAL, "GeometryFieldTest needs GDAL support")
 @skipUnlessDBFeature("gis_enabled")
 class GeometryFieldTest(SimpleTestCase):
-    def test_deconstructible(self):
-        """
-        Ticket #26333: Make sure GIS geometries classes are deconstructible.
-        """
-
-        # Test against Point
-        point = Point(4.337844, 50.827537, srid=4326)
-        path, args, kwargs = point.deconstruct()
-        self.assertEqual(path, 'django.contrib.gis.geos.point.Point')
-        self.assertEqual(args, (4.337844, 50.827537))
-        self.assertEqual(kwargs, {'srid': 4326})
-
-        # LineString
-        ls = LineString(((0, 0), (1, 1)))
-        path, args, kwargs = ls.deconstruct()
-        self.assertEqual(path, 'django.contrib.gis.geos.linestring.LineString')
-        self.assertEqual(args, (((0, 0), (1, 1)),))
-        self.assertEqual(kwargs, {})
-
-        ls2 = LineString([Point(0, 0), Point(1, 1)], srid=4326)
-        path, args, kwargs = ls2.deconstruct()
-        self.assertEqual(path, 'django.contrib.gis.geos.linestring.LineString')
-        self.assertEqual(args, ([Point(0, 0), Point(1, 1)], ))
-        self.assertEqual(kwargs, {'srid': 4326})
-
-        # Polygon
-        ext_coords = ((0, 0), (0, 1), (1, 1), (1, 0), (0, 0))
-        int_coords = ((0.4, 0.4), (0.4, 0.6), (0.6, 0.6), (0.6, 0.4), (0.4, 0.4))
-        poly = Polygon(ext_coords, int_coords)
-        path, args, kwargs = poly.deconstruct()
-        self.assertEqual(path, 'django.contrib.gis.geos.polygon.Polygon')
-        self.assertEqual(args, (ext_coords, int_coords, ))
-        self.assertEqual(kwargs, {})
-
-        # LinearRing
-        lr = LinearRing((0, 0), (0, 1), (1, 1), (0, 0))
-        path, args, kwargs = lr.deconstruct()
-        self.assertEqual(path, 'django.contrib.gis.geos.linestring.LinearRing')
-        self.assertEqual(args, ((0, 0), (0, 1), (1, 1), (0, 0)))
-        self.assertEqual(kwargs, {})
-
-        # MultiPoint
-        mp = MultiPoint(Point(0, 0), Point(1, 1))
-        path, args, kwargs = mp.deconstruct()
-        self.assertEqual(path, 'django.contrib.gis.geos.collections.MultiPoint')
-        self.assertEqual(args, (Point(0, 0), Point(1, 1), ))
-        self.assertEqual(kwargs, {})
-
-        # MultiLineString
-        ls1 = LineString((0, 0), (1, 1))
-        ls2 = LineString((2, 2), (3, 3))
-        mls = MultiLineString(ls1, ls2)
-        path, args, kwargs = mls.deconstruct()
-        self.assertEqual(path, 'django.contrib.gis.geos.collections.MultiLineString')
-        self.assertEqual(args, (ls1, ls2, ))
-        self.assertEqual(kwargs, {})
-
-        # MultiPolygon
-        p1 = Polygon(((0, 0), (0, 1), (1, 1), (0, 0)))
-        p2 = Polygon(((1, 1), (1, 2), (2, 2), (1, 1)))
-        mp = MultiPolygon(p1, p2)
-        path, args, kwargs = mp.deconstruct()
-        self.assertEqual(path, 'django.contrib.gis.geos.collections.MultiPolygon')
-        self.assertEqual(args, (p1, p2, ))
-        self.assertEqual(kwargs, {})
-
-        # GeometryCollection
-        poly = Polygon(((0, 0), (0, 1), (1, 1), (0, 0)))
-        gc = GeometryCollection(Point(0, 0), MultiPoint(Point(0, 0), Point(1, 1)), poly)
-        path, args, kwargs = gc.deconstruct()
-        self.assertEqual(path, 'django.contrib.gis.geos.collections.GeometryCollection')
-        self.assertEqual(args, (Point(0, 0), MultiPoint(Point(0, 0), Point(1, 1)), poly, ))
-        self.assertEqual(kwargs, {})
 
     def test_init(self):
         "Testing GeometryField initialization with defaults."
