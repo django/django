72b2daa463e89467f7fd73caf13a9f0c34a8776b
boaz85@gmail.com <boaz85@gmail.com>
2016-05-12 22:58:07 +0300

    Prefix the default test db name with test_

--- django/db/backends/base/creation.py
+++ django/db/backends/base/creation.py
@@ -295,12 +295,10 @@ class BaseDatabaseCreation(object):
         accordingly to the RDBMS particularities.
         """
         settings_dict = self.connection.settings_dict
-        test_settings_dict = settings_dict.get('TEST', {})
-        test_db_name = test_settings_dict.get('NAME')
 
         return (
             settings_dict['HOST'],
             settings_dict['PORT'],
             settings_dict['ENGINE'],
-            test_db_name if test_db_name is not None else settings_dict['NAME']
+            self._get_test_db_name()
         )
