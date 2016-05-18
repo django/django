ed316eb720965b3ba796494b9ee0d3b816de4cc0
Jon Dufresne <jon.dufresne@gmail.com>
2016-05-18 07:30:42 -0700

    Fixed #4136 -- When saving nullable models.CharFields through model forms, save empty values as NULL
    This helps with CharFields that have null=True, blank=True, and
    unique=True. Previously, the empty string would be saved through
    forms. When the field was left blank, this caused unique constraint
    violations as multiple objects had '' for the unique field. NULL does
    not have this problem.

--- django/db/models/fields/__init__.py
+++ django/db/models/fields/__init__.py
@@ -1086,6 +1086,9 @@ class CharField(Field):
         # will be validated twice. This is considered acceptable since we want
         # the value in the form field (to pass into widget for example).
         defaults = {'max_length': self.max_length}
+        # TODO: Handle multiple backends with different feature flags.
+        if self.null and not connection.features.interprets_empty_strings_as_nulls:
+            defaults['empty_value'] = None
         defaults.update(kwargs)
         return super(CharField, self).formfield(**defaults)
 
