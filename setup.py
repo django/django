f248427af2f7183588f13a44a345986397520e68
Tim Graham <timograham@gmail.com>
2016-05-20 08:40:51 -0400

    [1.10.x] Bumped version to 1.10a1.

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     },
     zip_safe=False,
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
