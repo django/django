django/django

commit c7b1b81352533be9b7f4637c12219f59c0964a82

Fixed #26666 -- Added more useful error message to assertRedirects

tests/test_client/tests.py
@@ -595,6 +595,15 @@ class ClientTest(TestCase):
         response = self.client.get('/django_project_redirect/')
         self.assertRedirects(response, 'https://www.djangoproject.com/', fetch_redirect_response=False)
 
+    def test_external_redirect_with_fetch_error_msg(self):
+        """
+        Check that assertRedirects without fetch_redirect_response=False raises
+        a relevant ValueError rather than a non-descript AssertionError.
+        """
+        response = self.client.get('/django_project_redirect/')
+        with self.assertRaisesMessage(ValueError, 'unable to fetch'):
+            self.assertRedirects(response, 'https://www.djangoproject.com/')
+
     def test_session_modifying_view(self):
         "Request a page that modifies the session"
         # Session value isn't set initially

django/test/testcases.py
@@ -303,6 +303,11 @@ class SimpleTestCase(unittest.TestCase):
                 path = urljoin(response.request['PATH_INFO'], path)
 
             if fetch_redirect_response:
+                if netloc:
+                    raise ValueError(
+                        "The Django test client is unable to fetch remote URLs (got %s). "
+                        "Use assertRedirects(..., fetch_redirect_response=False) instead." % url
+                    )
                 redirect_response = response.client.get(path, QueryDict(query), secure=(scheme == 'https'))
 
                 # Get the redirection page, using the same client that was used

commit 5b1c01ca717a3d3c69a1cb795ccd3c9e1d8ce68b

Refs #26666 -- Fixed regression introduced in c7b1b81352533be9b7f4637c12219f59c0964a82

tests/contenttypes_tests/tests.py
@@ -60,7 +60,7 @@ class ContentTypesViewsTests(TestCase):
             short_url = '/shortcut/%s/%s/' % (ContentType.objects.get_for_model(Author).id, obj.pk)
             response = self.client.get(short_url)
             self.assertRedirects(response, 'http://testserver%s' % obj.get_absolute_url(),
-                                 status_code=302, target_status_code=404)
+                                 status_code=302, target_status_code=404, fetch_redirect_response=False)
 
     def test_shortcut_with_absolute_url_including_scheme(self):
         """

tests/test_client_regress/tests.py
@@ -504,9 +504,15 @@ class AssertRedirectsTests(SimpleTestCase):
             # always redirects to https
             response = self.client.get('/https_redirect_view/', follow=follow, secure=secure)
             # the goal scheme is https
-            self.assertRedirects(response, 'https://testserver/secure_view/', status_code=302)
+            self.assertRedirects(
+                response, 'https://testserver/secure_view/',
+                status_code=302, fetch_redirect_response=False,
+            )
             with self.assertRaises(AssertionError):
-                self.assertRedirects(response, 'http://testserver/secure_view/', status_code=302)
+                self.assertRedirects(
+                    response, 'http://testserver/secure_view/',
+                    status_code=302, fetch_redirect_response=False,
+                )
 
     @ignore_warnings(category=RemovedInDjango20Warning)
     def test_full_path_in_expected_urls(self):