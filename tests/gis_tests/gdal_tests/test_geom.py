7670f06a607c784b04036ee5abd64a74fbc79546
Sergey Fedoseev <fedoseev.sergey@gmail.com>
2016-06-10 11:50:07 +0500

    Fixed #25940 -- Added OGRGeometry.from_gml() and GEOSGeometry.from_gml() class methods.

--- tests/gis_tests/gdal_tests/test_geom.py
+++ tests/gis_tests/gdal_tests/test_geom.py
@@ -543,3 +543,13 @@ class OGRGeomTest(unittest.TestCase, TestDataMixin):
             OGRGeometry('POINT(0.5 0.5)').within(OGRGeometry('POLYGON ((0 0, 0 1, 1 1, 1 0, 0 0))')), True
         )
         self.assertIs(OGRGeometry('POINT(0 0)').within(OGRGeometry('POINT(0 1)')), False)
+
+    def test_from_gml(self):
+        self.assertEqual(
+            OGRGeometry('POINT(0 0)'),
+            OGRGeometry.from_gml(
+                '<gml:Point gml:id="p21" srsName="http://www.opengis.net/def/crs/EPSG/0/4326">'
+                '    <gml:pos srsDimension="2">0 0</gml:pos>'
+                '</gml:Point>'
+            ),
+        )
