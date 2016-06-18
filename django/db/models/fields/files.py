bdc29b7188af0cda31a2e37759236d06d996eb44
Tim Graham <timograham@gmail.com>
2016-06-17 21:04:02 -0400

    [1.10.x] Reverted "Fixed #26398 -- Made FieldFile.open() respect its mode argument."
    This reverts commit a52a531a8b34f049fba11c3ee7b010af7534bf90 due to
    regressions described in refs #26772.
    
    Backport of 7def55c3f6716fcfa40a3bd5d0fbb2090588d81e from master

--- django/db/models/fields/files.py
+++ django/db/models/fields/files.py
@@ -45,10 +45,10 @@ class FieldFile(File):
         if not self:
             raise ValueError("The '%s' attribute has no file associated with it." % self.field.name)
 
-    def _get_file(self, mode='rb'):
+    def _get_file(self):
         self._require_file()
         if not hasattr(self, '_file') or self._file is None:
-            self._file = self.storage.open(self.name, mode)
+            self._file = self.storage.open(self.name, 'rb')
         return self._file
 
     def _set_file(self, file):
@@ -77,7 +77,8 @@ class FieldFile(File):
     size = property(_get_size)
 
     def open(self, mode='rb'):
-        self._get_file(mode)
+        self._require_file()
+        self.file.open(mode)
     # open() doesn't alter the file's contents, but it does reset the pointer
     open.alters_data = True
 
