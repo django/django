28de25c249754401eaf237159ab88bb67577396a
Eduard Stepanov <e.an.stepanov@gmail.com>
2016-06-23 12:22:19 +0300

    [1.10.x] Fixed #26786 -- Avoided redundant max value validators on integer fields.
    Backport of 49b4596cb4744e4b68d56e6a540a3e15c1582963 from master

--- django/db/models/fields/__init__.py
+++ django/db/models/fields/__init__.py
@@ -1808,14 +1808,22 @@ class IntegerField(Field):
     def validators(self):
         # These validators can't be added at field initialization time since
         # they're based on values retrieved from `connection`.
-        range_validators = []
+        validators_ = super(IntegerField, self).validators
         internal_type = self.get_internal_type()
         min_value, max_value = connection.ops.integer_field_range(internal_type)
         if min_value is not None:
-            range_validators.append(validators.MinValueValidator(min_value))
+            for validator in validators_:
+                if isinstance(validator, validators.MinValueValidator) and validator.limit_value >= min_value:
+                    break
+            else:
+                validators_.append(validators.MinValueValidator(min_value))
         if max_value is not None:
-            range_validators.append(validators.MaxValueValidator(max_value))
-        return super(IntegerField, self).validators + range_validators
+            for validator in validators_:
+                if isinstance(validator, validators.MaxValueValidator) and validator.limit_value <= max_value:
+                    break
+            else:
+                validators_.append(validators.MaxValueValidator(max_value))
+        return validators_
 
     def get_prep_value(self, value):
         value = super(IntegerField, self).get_prep_value(value)
