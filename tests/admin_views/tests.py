b387189d86aa6f0b8dd5e9ff2facb725e6beb7a9
Stanislav Karpov <stkrp@yandex.ru>
2016-07-20 15:38:07 +0400

    Fixed #26919 -- Added the action form's media in the changelist view.

--- tests/admin_views/tests.py
+++ tests/admin_views/tests.py
@@ -43,6 +43,7 @@ from django.utils.six.moves.urllib.parse import parse_qsl, urljoin, urlparse
 
 from . import customadmin
 from .admin import CityAdmin, site, site2
+from .forms import MediaActionForm
 from .models import (
     Actor, AdminOrderedAdminMethod, AdminOrderedCallable, AdminOrderedField,
     AdminOrderedModelMethod, Answer, Article, BarAccount, Book, Bookmark,
@@ -3373,6 +3374,17 @@ action)</option>
         # Send mail, don't delete.
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].subject, 'Greetings from a function action')
+
+    def test_media_from_actions_form(self):
+        """
+        The action form's media is included in changelist view's media.
+        """
+        response = self.client.get(reverse('admin:admin_views_subscriber_changelist'))
+        media_path = MediaActionForm.Media.js[0]
+        self.assertIsInstance(response.context['action_form'], MediaActionForm)
+        self.assertIn('media', response.context)
+        self.assertIn(media_path, response.context['media']._js)
+        self.assertContains(response, media_path)
 
     def test_user_message_on_none_selected(self):
         """
