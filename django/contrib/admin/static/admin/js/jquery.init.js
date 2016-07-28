cba577f7e5d215d8394966ba41c68a6ef82dc275
Kit Randel <kit@nocturne.net.nz>
2016-07-29 09:25:22 +1200

    Make existing comment more accurate.

--- django/contrib/admin/static/admin/js/jquery.init.js
+++ django/contrib/admin/static/admin/js/jquery.init.js
@@ -1,7 +1,8 @@
 /*global django:true, jQuery:false*/
 /* Puts the included jQuery into our own namespace using noConflict and passing
  * it 'true'. This ensures that the included jQuery doesn't pollute the global
- * namespace.
+ * namespace (i.e. this preserves pre-existing values for window.$ if window.$
+ * is not a jQuery object, but removes jQuery from the global namespace).
  */
 var django = django || {};
 django.jQuery = jQuery.noConflict(true);
