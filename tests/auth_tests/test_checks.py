4e64e3bb6e96a50b057bc1144fba3efdee7dfc10
Sergey Yurchenko <urchenko88@gmail.com>
2016-08-02 22:19:01 +0300

    Fixed #26997 -- Fixed checks crash with empty Meta.default_permissions.

--- tests/auth_tests/test_checks.py
+++ tests/auth_tests/test_checks.py
@@ -195,3 +195,10 @@ class ModelsPermissionsChecksTests(SimpleTestCase):
                 id='auth.E008',
             ),
         ])
+
+    def test_empty_default_permissions(self):
+        class Checked(models.Model):
+            class Meta:
+                default_permissions = ()
+
+        self.assertEqual(checks.run_checks(self.apps.get_app_configs()), [])
