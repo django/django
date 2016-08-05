2f18cbc3130d1d601ab6c40d26930b8cfbd77a75
Sjoerd Job Postmus <sjoerdjob@sjec.nl>
2016-08-05 23:57:55 +0200

    [1.10.x] Fixed #27026 -- Fixed state initialization of bulk_create() objects if can_return_ids_from_bulk_insert.
    Backport of 3246d2b4bb981a8d782a349a99e9b89206028cee from master

--- tests/bulk_create/tests.py
+++ tests/bulk_create/tests.py
@@ -217,3 +217,13 @@ class BulkCreateTests(TestCase):
         self.assertEqual(Country.objects.get(pk=countries[1].pk), countries[1])
         self.assertEqual(Country.objects.get(pk=countries[2].pk), countries[2])
         self.assertEqual(Country.objects.get(pk=countries[3].pk), countries[3])
+
+    @skipUnlessDBFeature('can_return_ids_from_bulk_insert')
+    def test_set_state(self):
+        country_nl = Country(name='Netherlands', iso_two_letter='NL')
+        country_be = Country(name='Belgium', iso_two_letter='BE')
+        Country.objects.bulk_create([country_nl])
+        country_be.save()
+        # Objects save via bulk_create() and save() should have equal state.
+        self.assertEqual(country_nl._state.adding, country_be._state.adding)
+        self.assertEqual(country_nl._state.db, country_be._state.db)
