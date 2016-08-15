b07fbca02688a0f8eb159f0dde132e7498aa40cc
Chris Jerdonek <chris.jerdonek@gmail.com>
2016-08-15 16:37:14 -0700

    Fixed #22414 -- Ensured that LiveServerTestCase closes connections.

--- django/test/testcases.py
+++ django/test/testcases.py
@@ -1262,6 +1262,8 @@ class LiveServerThread(threading.Thread):
         except Exception as e:
             self.error = e
             self.is_ready.set()
+        finally:
+            connections.close_all()
 
     def _create_server(self, port):
         return WSGIServer((self.host, port), QuietWSGIRequestHandler, allow_reuse_address=False)
@@ -1271,6 +1273,7 @@ class LiveServerThread(threading.Thread):
             # Stop the WSGI server
             self.httpd.shutdown()
             self.httpd.server_close()
+        self.join()
 
 
 class LiveServerTestCase(TransactionTestCase):
@@ -1335,7 +1338,6 @@ class LiveServerTestCase(TransactionTestCase):
         if hasattr(cls, 'server_thread'):
             # Terminate the live server's thread
             cls.server_thread.terminate()
-            cls.server_thread.join()
 
         # Restore sqlite in-memory database connections' non-shareability
         for conn in connections.all():
