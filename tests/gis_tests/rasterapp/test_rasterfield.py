760c546f80a0634e2e923c39c9ca5a34c6a0c4a7
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2016-08-15 18:21:12 +0100

    Added detailed error messages for GIS DB functions when used with rasters.

--- tests/gis_tests/rasterapp/test_rasterfield.py
+++ tests/gis_tests/rasterapp/test_rasterfield.py
@@ -1,5 +1,6 @@
 import json
 
+from django.contrib.gis.db.models.functions import Distance
 from django.contrib.gis.db.models.lookups import (
     DistanceLookupBase, gis_lookups,
 )
@@ -326,3 +327,18 @@ class RasterFieldTest(TransactionTestCase):
         msg = "Couldn't create spatial object from lookup value '%s'." % obj
         with self.assertRaisesMessage(ValueError, msg):
             RasterModel.objects.filter(geom__intersects=obj)
+
+    def test_db_function_errors(self):
+        """
+        Type errors are raised when using DB functions with raster content.
+        """
+        point = GEOSGeometry("SRID=3086;POINT (-697024.9213808845 683729.1705516104)")
+        rast = GDALRaster(json.loads(JSON_RASTER))
+        msg = "Please provide a geometry object."
+        with self.assertRaisesMessage(TypeError, msg):
+            RasterModel.objects.annotate(distance_from_point=Distance("geom", rast))
+        with self.assertRaisesMessage(TypeError, msg):
+            RasterModel.objects.annotate(distance_from_point=Distance("rastprojected", rast))
+        msg = "Geometry functions not supported for raster fields."
+        with self.assertRaisesMessage(TypeError, msg):
+            RasterModel.objects.annotate(distance_from_point=Distance("rastprojected", point)).count()
