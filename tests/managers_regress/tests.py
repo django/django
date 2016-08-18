d4eefc7e2af0d93283ed1c03e0af0a482982b6f0
Loïc Bistuer <loic.bistuer@gmail.com>
2016-08-19 01:24:45 +0700

    Fixed #27073 -- Removed duplicated managers in `Model._meta.managers`.

--- tests/managers_regress/tests.py
+++ tests/managers_regress/tests.py
@@ -286,6 +286,22 @@ class TestManagerInheritance(TestCase):
 
         self.assertIsInstance(MTIModel._base_manager, CustomManager)
 
+    def test_manager_no_duplicates(self):
+        class CustomManager(models.Manager):
+            pass
+
+        class AbstractModel(models.Model):
+            custom_manager = models.Manager()
+
+            class Meta:
+                abstract = True
+
+        class TestModel(AbstractModel):
+            custom_manager = CustomManager()
+
+        self.assertEqual(TestModel._meta.managers, (TestModel.custom_manager,))
+        self.assertEqual(TestModel._meta.managers_map, {'custom_manager': TestModel.custom_manager})
+
 
 @isolate_apps('managers_regress')
 class TestManagerDeprecations(TestCase):
