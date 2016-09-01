d9c083cfee853272ded14c6c87623e910c9e81c4
Alex Hill <alex@hill.net.au>
2016-09-01 13:18:41 +0800

    Refs #27039 -- Fixed regression with field defaults in prefixed forms.

--- tests/model_forms/tests.py
+++ tests/model_forms/tests.py
@@ -601,6 +601,22 @@ class ModelFormBaseTest(TestCase):
         self.assertIsInstance(mf1.fields['active'].widget, forms.CheckboxInput)
         self.assertIs(m1._meta.get_field('active').get_default(), True)
 
+    def test_prefixed_form_with_default_field(self):
+        class PubForm(forms.ModelForm):
+            prefix = 'form-prefix'
+
+            class Meta:
+                model = PublicationDefaults
+                fields = ('mode',)
+
+        mode = 'de'
+        self.assertNotEqual(mode, PublicationDefaults._meta.get_field('mode').get_default())
+
+        mf1 = PubForm({'form-prefix-mode': mode})
+        self.assertEqual(mf1.errors, {})
+        m1 = mf1.save(commit=False)
+        self.assertEqual(m1.mode, mode)
+
 
 class FieldOverridesByFormMetaForm(forms.ModelForm):
     class Meta:
