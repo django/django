1169a2c926e6c39e4d442d1efc5c7ca25edabdbf
Jani Tiainen <jtiai@code.nowarelabs.mywire.org>
2016-09-10 12:25:42 +0300

    Fixed #27198 -- MultiValueDict getlist returns now a new list

--- tests/utils_tests/test_datastructures.py
+++ tests/utils_tests/test_datastructures.py
@@ -101,6 +101,24 @@ class MultiValueDictTests(SimpleTestCase):
 
         self.assertEqual({}, MultiValueDict().dict())
 
+    def test_getlist_doesnt_mutate(self):
+        x = MultiValueDict({'a': ['1', '2'], 'b': ['3']})
+        values = x.getlist('a')
+        values += x.getlist('b')
+        self.assertEqual(x.getlist('a'), ['1', '2'])
+
+    def test_internal_getlist_does_mutate(self):
+        x = MultiValueDict({'a': ['1', '2'], 'b': ['3']})
+        values = x._getlist('a')
+        values += x._getlist('b')
+        self.assertEqual(x._getlist('a'), ['1', '2', '3'])
+
+    def test_getlist_default(self):
+        x = MultiValueDict({'a': [1]})
+        MISSING = object()
+        values = x.getlist('b', default=MISSING)
+        self.assertIs(values, MISSING)
+
 
 class ImmutableListTests(SimpleTestCase):
 
