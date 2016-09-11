b495d8e334c2487eef6282bac5dd0c1f43da71ef
Simon Charette <charette.s@gmail.com>
2016-09-10 20:30:14 -0400

    [1.10.x] Fixed #27407 -- Made Model.delete(keep_parents=True) preserve parent reverse relationships.
    Thanks Tim for the review.
    
    Backport of 31a56e30cfb3c8e1a9373e5abdd12b5b23d5d910 from master

--- django/db/models/deletion.py
+++ django/db/models/deletion.py
@@ -210,7 +210,11 @@ class Collector(object):
                                  collect_related=False,
                                  reverse_dependency=True)
         if collect_related:
+            parents = model._meta.parents
             for related in get_candidate_relations_to_delete(model._meta):
+                # Preserve parent reverse relationships if keep_parents=True.
+                if keep_parents and related.model in parents:
+                    continue
                 field = related.field
                 if field.remote_field.on_delete == DO_NOTHING:
                     continue
