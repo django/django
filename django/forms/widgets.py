87c5e7efebd040aef0f0479ccf86877155bb5cea
Tim Graham <timograham@gmail.com>
2016-09-30 14:49:50 -0400

    Refs #27186 -- Fixed model form default fallback for CheckboxSelectMultiple.

--- django/forms/widgets.py
+++ django/forms/widgets.py
@@ -817,6 +817,11 @@ class CheckboxSelectMultiple(RendererMixin, SelectMultiple):
         # require all checkboxes to be checked instead of at least one.
         return False
 
+    def value_omitted_from_data(self, data, files, name):
+        # HTML checkboxes don't appear in POST data if not checked, so it's
+        # never known if the value is actually omitted.
+        return False
+
 
 class MultiWidget(Widget):
     """
