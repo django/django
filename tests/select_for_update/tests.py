254312c8228e63e08a1bb4a782abaddd9c43a3dd
François Freitag <francois.freitag@polyconseil.fr>
2016-10-03 20:14:27 -0700

    Wrap select_for_update subquery in a transaction

--- tests/select_for_update/tests.py
+++ tests/select_for_update/tests.py
@@ -306,5 +306,6 @@ class SelectForUpdateTests(TransactionTestCase):
         Subqueries should respect ordering as an ORDER BY clause may be useful
         to specify a row locking order to prevent deadlocks (#27193).
         """
-        qs = Person.objects.filter(id__in=Person.objects.order_by('-id').select_for_update())
-        self.assertIn('ORDER BY', str(qs.query))
+        with transaction.atomic():
+            qs = Person.objects.filter(id__in=Person.objects.order_by('-id').select_for_update())
+            self.assertIn('ORDER BY', str(qs.query))
