241e70b096ec305f08d97aa32913ab922e53a9da
Zach Borboa <zachborboa@gmail.com>
2016-10-07 01:39:57 -0700

    Add tests for changes to InlineModelAdmin.get_formset()

--- tests/modeladmin/tests.py
+++ tests/modeladmin/tests.py
@@ -379,6 +379,53 @@ class ModelAdminTests(TestCase):
             list(list(ma.get_formsets_with_inlines(request))[0][0]().forms[0].fields),
             ['main_band', 'day', 'transport', 'id', 'DELETE'])
 
+    def test_formset_overriding_get_exclude(self):
+        class AdminConcertForm(forms.ModelForm):
+            class Meta:
+                model = Concert
+                fields = ['main_band', 'opening_band', 'day', 'transport']
+
+        class ConcertInline(TabularInline):
+            form = AdminConcertForm
+            fk_name = 'main_band'
+            model = Concert
+
+            def get_exclude(self, request, obj=None):
+                return ['opening_band']
+
+        class BandAdmin(ModelAdmin):
+            inlines = [
+                ConcertInline
+            ]
+
+        ma = BandAdmin(Band, self.site)
+        self.assertEqual(
+            list(list(ma.get_formsets_with_inlines(request))[0][0]().forms[0].fields),
+            ['main_band', 'day', 'transport', 'id', 'DELETE'])
+
+        class AdminConcertForm(forms.ModelForm):
+            class Meta:
+                model = Concert
+                exclude = ['day']
+
+        class ConcertInline(TabularInline):
+            form = AdminConcertForm
+            fk_name = 'main_band'
+            model = Concert
+
+            def get_exclude(self, request, obj=None):
+                return ['opening_band']
+
+        class BandAdmin(ModelAdmin):
+            inlines = [
+                ConcertInline
+            ]
+
+        ma = BandAdmin(Band, self.site)
+        self.assertEqual(
+            list(list(ma.get_formsets_with_inlines(request))[0][0]().forms[0].fields),
+            ['main_band', 'day', 'transport', 'id', 'DELETE'])
+
     def test_queryset_override(self):
         # If we need to override the queryset of a ModelChoiceField in our custom form
         # make sure that RelatedFieldWidgetWrapper doesn't mess that up.
