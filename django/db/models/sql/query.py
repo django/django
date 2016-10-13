3b2db6ec12ce7d4b32f60dd7713e5f23cac498b7
Tim Graham <timograham@gmail.com>
2016-10-13 16:56:54 -0400

    Removed unneeded try/except in Query.names_to_path().

--- django/db/models/sql/query.py
+++ django/db/models/sql/query.py
@@ -1324,10 +1324,7 @@ class Query(object):
                         "querying. If it is a GenericForeignKey, consider "
                         "adding a GenericRelation." % name
                     )
-                try:
-                    model = field.model._meta.concrete_model
-                except AttributeError:
-                    model = None
+                model = field.model._meta.concrete_model
             else:
                 # We didn't find the current field, so move position back
                 # one step.
