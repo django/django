3a416e4ba9f7094be55110dae8e5a6451984d226
Alvin Lindstam <alvin@kundo.se>
2016-10-22 19:20:24 -0400

    [1.10.x] Fixed #27374 -- Made JavaScriptCatalog respect the packages argument.
    Backport of 6b5106b1ceec03d945c1104b21feed3e25470fe0 from master

--- django/views/i18n.py
+++ django/views/i18n.py
@@ -355,7 +355,8 @@ class JavaScriptCatalog(View):
         domain = kwargs.get('domain', self.domain)
         # If packages are not provided, default to all installed packages, as
         # DjangoTranslation without localedirs harvests them all.
-        packages = kwargs.get('packages', '').split('+') or self.packages
+        packages = kwargs.get('packages', '')
+        packages = packages.split('+') if packages else self.packages
         paths = self.get_paths(packages) if packages else None
         self.translation = DjangoTranslation(locale, domain=domain, localedirs=paths)
         context = self.get_context_data(**kwargs)
