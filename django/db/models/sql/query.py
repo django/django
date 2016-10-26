e124d2da94cc1233729d8f0200809589f6c5afc8
François Freitag <mail@franek.fr>
2016-10-26 18:01:37 -0400

    Fixed #26551 -- Fixed negated Q() queries that span relations.
    Prevented queries from reusing trimmed joins.

--- django/db/models/sql/query.py
+++ django/db/models/sql/query.py
@@ -1192,10 +1192,12 @@ class Query:
             return self.split_exclude(filter_expr, LOOKUP_SEP.join(parts[:e.level]),
                                       can_reuse, e.names_with_path)
 
-        if can_reuse is not None:
-            can_reuse.update(join_list)
+        # Update used_joins before trimming since they are reused to determine
+        # which joins could be later promoted to INNER.
         used_joins = set(used_joins).union(set(join_list))
         targets, alias, join_list = self.trim_joins(sources, join_list, path)
+        if can_reuse is not None:
+            can_reuse.update(join_list)
 
         if field.is_relation:
             # No support for transforms for relational fields
--- tests/queries/tests.py
+++ tests/queries/tests.py
@@ -3147,6 +3147,25 @@ class JoinReuseTest(TestCase):
         qs = Author.objects.filter(report__name='r4').filter(report__name='r1')
         self.assertEqual(str(qs.query).count('JOIN'), 2)
 
+    def test_inverted_q_across_relations(self):
+        """
+        When a trimmable join is specified in the query (here school__), the
+        ORM detects it and removes unnecessary joins. The set of reusable joins
+        are updated after trimming the query so that other lookups don't
+        consider that the outer query's filters are in effect for the subquery
+        (#26551).
+        """
+        springfield_elementary = School.objects.create()
+        hogward = School.objects.create()
+        Student.objects.create(school=springfield_elementary)
+        hp = Student.objects.create(school=hogward)
+        Classroom.objects.create(school=hogward, name='Potion')
+        Classroom.objects.create(school=springfield_elementary, name='Main')
+        qs = Student.objects.filter(
+            ~(Q(school__classroom__name='Main') & Q(school__classroom__has_blackboard=None))
+        )
+        self.assertSequenceEqual(qs, [hp])
+
 
 class DisjunctionPromotionTests(TestCase):
     def test_disjunction_promotion_select_related(self):
