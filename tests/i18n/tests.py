81b5971b1ce044448466791b3c6f795b66c666d7
Krzysztof Urbaniak <urban@fail.pl>
2016-11-05 12:56:34 +0100

    [1.10.x] Fixed #27402 -- Fixed incorrect LocaleMiddleware redirects with prefix_default_language=False.
    Backport of b8a815e9dfea89034ede7ff786551f89af84a31b from master

--- tests/i18n/tests.py
+++ tests/i18n/tests.py
@@ -1843,6 +1843,20 @@ class UnprefixedDefaultLanguageTests(SimpleTestCase):
         response = self.client.get('/de-simple-page/')
         self.assertEqual(response.content, b'Yes')
 
+    def test_no_redirect_on_404(self):
+        """
+        A request for a nonexistent URL shouldn't cause a redirect to
+        /<defaut_language>/<request_url> when prefix_default_language=False and
+        /<default_language>/<request_url> has a URL match (#27402).
+        """
+        # A match for /group1/group2/ must exist for this to act as a
+        # regression test.
+        response = self.client.get('/group1/group2/')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/nonexistent/')
+        self.assertEqual(response.status_code, 404)
+
 
 @override_settings(
     USE_I18N=True,
