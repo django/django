50f9e736fa701664c5659d0653aff75b7f4489b6
Paweł Marczewski <pwmarcz@gmail.com>
2016-11-05 12:31:37 +0100

    Fixed #27438 -- Added the diffsettings --default option.

--- tests/admin_scripts/tests.py
+++ tests/admin_scripts/tests.py
@@ -2128,6 +2128,20 @@ class DiffSettings(AdminScriptTestCase):
         self.assertNoOutput(err)
         self.assertOutput(out, "### STATIC_URL = None")
 
+    def test_custom_default(self):
+        """
+        The --default option specifies an alternate settings module for
+        comparison.
+        """
+        self.write_settings('settings_default.py', sdict={'FOO': '"foo"', 'BAR': '"bar1"'})
+        self.addCleanup(self.remove_settings, 'settings_default.py')
+        self.write_settings('settings_to_diff.py', sdict={'FOO': '"foo"', 'BAR': '"bar2"'})
+        self.addCleanup(self.remove_settings, 'settings_to_diff.py')
+        out, err = self.run_manage(['diffsettings', '--settings=settings_to_diff', '--default=settings_default'])
+        self.assertNoOutput(err)
+        self.assertNotInOutput(out, "FOO")
+        self.assertOutput(out, "BAR = 'bar2'")
+
 
 class Dumpdata(AdminScriptTestCase):
     """Tests for dumpdata management command."""
