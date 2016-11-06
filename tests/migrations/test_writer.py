cb3fb34b86b17349ec428e789585d0da9604804a
Maxime Lorant <maxime.lorant@gmail.com>
2016-11-06 13:53:00 +0100

    Fixed #27378 -- Added support for serialization of uuid.UUID in migrations.
    Thanks Yuriy Korobko for the initial patch and Tobias McNulty for review.

--- tests/migrations/test_writer.py
+++ tests/migrations/test_writer.py
@@ -10,6 +10,7 @@ import re
 import sys
 import tokenize
 import unittest
+import uuid
 
 import custom_migration_operations.more_operations
 import custom_migration_operations.operations
@@ -319,6 +320,31 @@ class WriterTests(SimpleTestCase):
             "(1, migrations.test_writer.IntEnum(1)), "
             "(2, migrations.test_writer.IntEnum(2))], "
             "default=migrations.test_writer.IntEnum(1))"
+        )
+
+    def test_serialize_uuid(self):
+        self.assertSerializedEqual(uuid.uuid1())
+        self.assertSerializedEqual(uuid.uuid4())
+
+        uuid_a = uuid.UUID('5c859437-d061-4847-b3f7-e6b78852f8c8')
+        uuid_b = uuid.UUID('c7853ec1-2ea3-4359-b02d-b54e8f1bcee2')
+        self.assertSerializedResultEqual(
+            uuid_a,
+            ("uuid.UUID('5c859437-d061-4847-b3f7-e6b78852f8c8')", {'import uuid'})
+        )
+        self.assertSerializedResultEqual(
+            uuid_b,
+            ("uuid.UUID('c7853ec1-2ea3-4359-b02d-b54e8f1bcee2')", {'import uuid'})
+        )
+
+        field = models.UUIDField(choices=((uuid_a, 'UUID A'), (uuid_b, 'UUID B')), default=uuid_a)
+        string = MigrationWriter.serialize(field)[0]
+        self.assertEqual(
+            string,
+            "models.UUIDField(choices=["
+            "(uuid.UUID('5c859437-d061-4847-b3f7-e6b78852f8c8'), 'UUID A'), "
+            "(uuid.UUID('c7853ec1-2ea3-4359-b02d-b54e8f1bcee2'), 'UUID B')], "
+            "default=uuid.UUID('5c859437-d061-4847-b3f7-e6b78852f8c8'))"
         )
 
     def test_serialize_functions(self):
