f94ce0d21de484a5783a3dcb122dc51757ed225b
Zbigniew Siciarz <zbigniew@siciarz.net>
2016-11-09 00:06:17 +0100

    Fixed #27436 -- Fixed makemigrations crash when creating migrations on a separate drive than where Django is installed.

--- tests/migrations/test_commands.py
+++ tests/migrations/test_commands.py
@@ -1152,6 +1152,19 @@ class MakeMigrationsTests(MigrationTestBase):
             call_command("makemigrations", "migrations", stdout=out)
             self.assertIn(os.path.join(migration_dir, '0001_initial.py'), out.getvalue())
 
+    def test_makemigrations_migration_path_output_valueerror(self):
+        """
+        makemigrations prints the absolute path if os.path.relpath() raises a
+        ValueError when it's impossible to obtain a relative path, e.g. on
+        Windows if Django is installed on a different drive than where the
+        migration files are created.
+        """
+        out = six.StringIO()
+        with self.temporary_migration_module() as migration_dir:
+            with mock.patch('os.path.relpath', side_effect=ValueError):
+                call_command('makemigrations', 'migrations', stdout=out)
+        self.assertIn(os.path.join(migration_dir, '0001_initial.py'), out.getvalue())
+
     def test_makemigrations_inconsistent_history(self):
         """
         makemigrations should raise InconsistentMigrationHistory exception if
