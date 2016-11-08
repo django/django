a9eaf2bfb884314bd4239976defe8c1cb0b748e5
Jackie Leng <jccleng@gmail.com>
2016-11-08 01:06:21 +0100

    Added requested PR changes.

--- tests/gis_tests/geoapp/tests.py
+++ tests/gis_tests/geoapp/tests.py
@@ -63,8 +63,7 @@ class GeoModelTest(TestCase):
         nullcity.point.x = 23
         nullcity.point.y = 5
         # Checking assignments pre & post-save.
-        self.assertNotEqual(Point(23, 5, srid=4326),
-                            City.objects.get(name='NullCity').point)
+        self.assertNotEqual(Point(23, 5, srid=4326), City.objects.get(name='NullCity').point)
         nullcity.save()
         self.assertEqual(Point(23, 5, srid=4326),
                          City.objects.get(name='NullCity').point)
