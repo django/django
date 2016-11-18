06c3670223839752693ec37aeab845f7b01e05f6
Nicolas Noé <nicolas@niconoe.eu>
2016-11-18 15:36:16 +0100

    Fixed #27493 -- Second case of the same precision issue

--- tests/gis_tests/gdal_tests/test_raster.py
+++ tests/gis_tests/gdal_tests/test_raster.py
@@ -67,6 +67,7 @@ class GDALRasterTests(unittest.TestCase):
     def setUp(self):
         self.rs_path = os.path.join(os.path.dirname(upath(__file__)),
                                     '../data/rasters/raster.tif')
+
         self.rs = GDALRaster(self.rs_path)
 
     def test_rs_name_repr(self):
@@ -360,8 +361,10 @@ class GDALRasterTests(unittest.TestCase):
         self.assertEqual(target.width, 7)
         self.assertEqual(target.height, 7)
         self.assertEqual(target.bands[0].datatype(), source.bands[0].datatype())
-        self.assertEqual(target.origin, [9124842.791079799, 1589911.6476407414])
-        self.assertEqual(target.scale, [223824.82664250192, -223824.82664250192])
+        self.assertAlmostEqual(target.origin[0], 9124842.791079799)
+        self.assertAlmostEqual(target.origin[1], 1589911.6476407414)
+        self.assertAlmostEqual(target.scale[0], 223824.82664250192)
+        self.assertAlmostEqual(target.scale[1], -223824.82664250192)
         self.assertEqual(target.skew, [0, 0])
 
         result = target.bands[0].data()
