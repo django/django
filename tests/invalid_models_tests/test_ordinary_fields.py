7cddd8a02e60332c0d02f565c450b0eea0d88438
Henry Dang <henrydangprg@gmail.com>
2016-11-26 13:23:03 -0500

    Fixed #27358 -- Added a system check to prevent FileField's upload_to from starting with a slash.
    Thanks Frank Bijlsma for the initial patch.

--- tests/invalid_models_tests/test_ordinary_fields.py
+++ tests/invalid_models_tests/test_ordinary_fields.py
@@ -455,6 +455,31 @@ class FileFieldTests(SimpleTestCase):
         ]
         self.assertEqual(errors, expected)
 
+    def test_upload_to_starts_with_slash(self):
+        class Model(models.Model):
+            field = models.FileField(upload_to='/somewhere')
+
+        field = Model._meta.get_field('field')
+        self.assertEqual(field.check(), [
+            Error(
+                "FileField's 'upload_to' argument must be a relative path, not "
+                "an absolute path.",
+                obj=field,
+                id='fields.E202',
+                hint='Remove the leading slash.',
+            )
+        ])
+
+    def test_upload_to_callable_not_checked(self):
+        def callable(instance, filename):
+            return '/' + filename
+
+        class Model(models.Model):
+            field = models.FileField(upload_to=callable)
+
+        field = Model._meta.get_field('field')
+        self.assertEqual(field.check(), [])
+
 
 @isolate_apps('invalid_models_tests')
 class FilePathFieldTests(SimpleTestCase):
