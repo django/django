57a66f1032178fbfac56964c694e473a970a9735
Adam Chainz <adam@adamj.eu>
2016-11-29 21:54:45 +0000

    Fixed #27555 -- Removed django.utils.functional.lazy_property

--- django/utils/functional.py
+++ django/utils/functional.py
@@ -412,27 +412,6 @@ class SimpleLazyObject(LazyObject):
         return copy.deepcopy(self._wrapped, memo)
 
 
-class lazy_property(property):
-    """
-    A property that works with subclasses by wrapping the decorated
-    functions of the base class.
-    """
-    def __new__(cls, fget=None, fset=None, fdel=None, doc=None):
-        if fget is not None:
-            @wraps(fget)
-            def fget(instance, instance_type=None, name=fget.__name__):
-                return getattr(instance, name)()
-        if fset is not None:
-            @wraps(fset)
-            def fset(instance, value, name=fset.__name__):
-                return getattr(instance, name)(value)
-        if fdel is not None:
-            @wraps(fdel)
-            def fdel(instance, name=fdel.__name__):
-                return getattr(instance, name)()
-        return property(fget, fset, fdel, doc)
-
-
 def partition(predicate, values):
     """
     Splits the values into two sets, based on the return value of the function
