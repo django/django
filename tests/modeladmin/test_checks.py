da792400503265a80e5307f17e59b65ec88694aa
Anton Samarchyan <anton.samarchyan@savoirfairelinux.com>
2016-11-30 10:16:40 -0500

    Fixed #26632 -- Skipped admin.E111 list_display_links check if get_list_display() is overridden.

--- tests/modeladmin/test_checks.py
+++ tests/modeladmin/test_checks.py
@@ -526,6 +526,35 @@ class ListDisplayLinksCheckTests(CheckTestCase):
 
         self.assertIsValid(TestModelAdmin, ValidationTestModel)
 
+    def test_list_display_links_check_skipped_if_get_list_display_overridden(self):
+        """
+        list_display_links check is skipped if get_list_display() is overridden.
+        """
+        class TestModelAdmin(ModelAdmin):
+            list_display_links = ['name', 'subtitle']
+
+            def get_list_display(self, request):
+                pass
+
+        self.assertIsValid(TestModelAdmin, ValidationTestModel)
+
+    def test_list_display_link_checked_for_list_tuple_if_get_list_display_overridden(self):
+        """
+        list_display_links is checked for list/tuple/None even if
+        get_list_display() is overridden.
+        """
+        class TestModelAdmin(ModelAdmin):
+            list_display_links = 'non-list/tuple'
+
+            def get_list_display(self, request):
+                pass
+
+        self.assertIsInvalid(
+            TestModelAdmin, ValidationTestModel,
+            "The value of 'list_display_links' must be a list, a tuple, or None.",
+            'admin.E110'
+        )
+
 
 class ListFilterTests(CheckTestCase):
 
