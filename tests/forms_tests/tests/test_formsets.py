71c04374fe03e109d3d2ee3b1224a681acfda81f
Tim Graham <timograham@gmail.com>
2016-11-30 20:02:06 -0500

    Minor refactor

--- tests/forms_tests/tests/test_formsets.py
+++ tests/forms_tests/tests/test_formsets.py
@@ -168,30 +168,29 @@ class FormsFormsetTestCase(SimpleTestCase):
 
     def test_formset_validation_count(self):
         """
-        ManagementForm of a FormSet is validated once per FormSet.is_valid call
-        and each form of the FormSet is cleaned once.
+        A formset's ManagementForm is validated once per FormSet.is_valid()
+        call and each form of the formset is cleaned once.
         """
+        def make_method_counter(func):
+            """Add a counter to func for the number of times it's called."""
+            counter = Counter()
+            counter.call_count = 0
+
+            def mocked_func(*args, **kwargs):
+                counter.call_count += 1
+                return func(*args, **kwargs)
+
+            return mocked_func, counter
+
+        mocked_is_valid, is_valid_counter = make_method_counter(formsets.ManagementForm.is_valid)
+        mocked_full_clean, full_clean_counter = make_method_counter(BaseForm.full_clean)
         formset = self.make_choiceformset([('Calexico', '100'), ('Any1', '42'), ('Any2', '101')])
-        orig_is_valid = formsets.ManagementForm.is_valid
-        orig_full_clean = BaseForm.full_clean
-        is_valid_counter = Counter()
-        is_valid_counter.call_count = 0
-        full_clean_counter = Counter()
-        full_clean_counter.call_count = 0
 
-        def mocked_is_valid(*args, **kwargs):
-            is_valid_counter.call_count += 1
-            return orig_is_valid(*args, **kwargs)
-
-        def mocked_full_clean(*args, **kwargs):
-            full_clean_counter.call_count += 1
-            return orig_full_clean(*args, **kwargs)
-
-        with mock.patch("django.forms.formsets.ManagementForm.is_valid", mocked_is_valid):
-            with mock.patch("django.forms.forms.BaseForm.full_clean", mocked_full_clean):
-                self.assertTrue(formset.is_valid())
-                self.assertEqual(is_valid_counter.call_count, 1)
-                self.assertEqual(full_clean_counter.call_count, 4)
+        with mock.patch('django.forms.formsets.ManagementForm.is_valid', mocked_is_valid), \
+                mock.patch('django.forms.forms.BaseForm.full_clean', mocked_full_clean):
+            self.assertTrue(formset.is_valid())
+        self.assertEqual(is_valid_counter.call_count, 1)
+        self.assertEqual(full_clean_counter.call_count, 4)
 
     def test_formset_has_changed(self):
         # FormSet instances has_changed method will be True if any data is
