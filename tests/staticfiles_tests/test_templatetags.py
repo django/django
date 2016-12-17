374e6230ca9f9bb84cc9dd760dfb6395fbb5ff0f
alix- <alix-@users.noreply.github.com>
2016-12-17 16:03:44 +0100

    Fixed #25484 -- Made {% static %} render escaped URLs.

--- tests/staticfiles_tests/test_templatetags.py
+++ tests/staticfiles_tests/test_templatetags.py
@@ -8,3 +8,5 @@ class TestTemplateTag(StaticFilesTestCase):
     def test_template_tag(self):
         self.assertStaticRenders("does/not/exist.png", "/static/does/not/exist.png")
         self.assertStaticRenders("testfile.txt", "/static/testfile.txt")
+        self.assertStaticRenders("test.html?foo=1&bar=2", "/static/test.html?foo=1&bar=2", autoescape=False)
+        self.assertStaticRenders("test.html?foo=1&bar=2", "/static/test.html?foo=1&amp;bar=2", autoescape=True)
