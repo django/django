cd7efa20338cb6f3ede4780e00590c0a6dd48ca2
Simon Charette <charette.s@gmail.com>
2016-12-17 15:06:47 -0500

    Fixed #25492 -- Checked deferred foreign key constraints before dropping them.
    This allows running foreign key data and schema altering operations in the
    same migration on PostgreSQL.
    
    Thanks Tim for review.

--- tests/schema/tests.py
+++ tests/schema/tests.py
@@ -532,6 +532,23 @@ class SchemaTests(TransactionTestCase):
         with connection.schema_editor() as editor:
             editor.alter_field(Note, old_field, new_field, strict=True)
 
+    @skipUnlessDBFeature('can_defer_constraint_checks', 'can_rollback_ddl')
+    def test_alter_fk_checks_deferred_constraints(self):
+        """
+        #25492 - Altering a foreign key's structure and data in the same
+        transaction.
+        """
+        with connection.schema_editor() as editor:
+            editor.create_model(Node)
+        old_field = Node._meta.get_field('parent')
+        new_field = ForeignKey(Node, CASCADE)
+        new_field.set_attributes_from_name('parent')
+        parent = Node.objects.create()
+        with connection.schema_editor() as editor:
+            # Update the parent FK to create a deferred constraint check.
+            Node.objects.update(parent=parent)
+            editor.alter_field(Node, old_field, new_field, strict=True)
+
     def test_alter_text_field_to_date_field(self):
         """
         #25002 - Test conversion of text field to date field.
