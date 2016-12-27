4701abd56e382bca72d4ff8e0b015948c9ab3feb
Tim Graham <timograham@gmail.com>
2016-12-27 12:01:24 -0500

    Refs #27025 -- Documented Python 3.6 compatibility for Django 1.11.

--- setup.py
+++ setup.py
@@ -66,6 +66,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Internet :: WWW/HTTP :: WSGI',
