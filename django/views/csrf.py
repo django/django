c2dbd215828ffd2d3f0bcab2c6b955d1ad9e8e1e
Tim Graham <timograham@gmail.com>
2016-12-28 15:32:31 -0500

    Fixed #27258 -- Prohibited django.Template.render() with non-dict context.
    Thanks Shivang Bharadwaj for the initial patch.

--- django/views/csrf.py
+++ django/views/csrf.py
@@ -105,7 +105,7 @@ def csrf_failure(request, reason="", template_name=CSRF_FAILURE_TEMPLATE_NAME):
     Default view used when request fails CSRF protection
     """
     from django.middleware.csrf import REASON_NO_REFERER, REASON_NO_CSRF_COOKIE
-    c = Context({
+    c = {
         'title': _("Forbidden"),
         'main': _("CSRF verification failed. Request aborted."),
         'reason': reason,
@@ -132,13 +132,14 @@ def csrf_failure(request, reason="", template_name=CSRF_FAILURE_TEMPLATE_NAME):
         'DEBUG': settings.DEBUG,
         'docs_version': get_docs_version(),
         'more': _("More information is available with DEBUG=True."),
-    })
+    }
     try:
         t = loader.get_template(template_name)
     except TemplateDoesNotExist:
         if template_name == CSRF_FAILURE_TEMPLATE_NAME:
             # If the default template doesn't exist, use the string template.
             t = Engine().from_string(CSRF_FAILURE_TEMPLATE)
+            c = Context(c)
         else:
             # Raise if a developer-specified template doesn't exist.
             raise
