6a7495051304d75865add6ff96422018984e1663
Shivang Bharadwaj <reficul31@gmail.com>
2016-12-29 02:33:20 +0530

    Fixed #27258 -- Prohibited django.Template.render() with non-dict context.
    Thanks Shivang Bharadwaj for the initial patch.

--- django/template/context.py
+++ django/template/context.py
@@ -281,6 +281,8 @@ def make_context(context, request=None, **kwargs):
     """
     Create a suitable Context from a plain dict and optionally an HttpRequest.
     """
+    if context is not None and not isinstance(context, dict):
+        raise TypeError('context must be a dict rather than %s.' % context.__class__.__name__)
     if request is None:
         context = Context(context, **kwargs)
     else:
