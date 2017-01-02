387c73fa271d782b76a1e89eb0c1bcd62a594bc7
Artem Skoretskiy <artem.skoretskiy@gmail.com>
2017-01-02 17:14:34 +0100

    Fixed import to resolve RemovedInDjango20Warning (#457)
    * Fixed import to resolve RemovedInDjango20Warning
    
    That resolves: "RemovedInDjango20Warning: Importing from django.core.urlresolvers is deprecated in favor of django.urls."
    
    * Fixed syntax error
    
    Fixed indent
    
    * Updated import order

--- channels/handler.py
+++ channels/handler.py
@@ -12,12 +12,18 @@ from django import http
 from django.conf import settings
 from django.core import signals
 from django.core.handlers import base
-from django.core.urlresolvers import set_script_prefix
+
 from django.http import FileResponse, HttpResponse, HttpResponseServerError
 from django.utils import six
 from django.utils.functional import cached_property
 
 from channels.exceptions import RequestAborted, RequestTimeout, ResponseLater as ResponseLaterOuter
+
+try:
+    from django.urls import set_script_prefix
+except ImportError:
+    # Django < 1.10
+    from django.core.urlresolvers import set_script_prefix
 
 logger = logging.getLogger('django.request')
 
