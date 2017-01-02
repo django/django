56e7b7a4447e99bfbe1f2125193c394823ba41e2
Raffaele Salmaso <raffaele@salmaso.org>
2017-01-03 00:44:05 +0100

    Fixed #11974 -- Allowed overriding admin's submit_row templatetag's template.

--- django/contrib/admin/templatetags/admin_modify.py
+++ django/contrib/admin/templatetags/admin_modify.py
@@ -2,6 +2,7 @@ import json
 
 from django import template
 from django.template.context import Context
+from django.template.library import InclusionNode
 
 register = template.Library()
 
@@ -39,31 +40,57 @@ def prepopulated_fields_js(context):
     return context
 
 
-@register.inclusion_tag('admin/submit_line.html', takes_context=True)
-def submit_row(context):
+class SubmitRowNode(InclusionNode):
+    def __init__(self):
+        super(SubmitRowNode, self).__init__(
+            func=SubmitRowNode._get_context, takes_context=True, args=[], kwargs={}, filename=None
+        )
+
+    def render(self, context):
+        opts = context['opts']
+        app_label = opts.app_label.lower()
+        object_name = opts.object_name.lower()
+        self.filename = [
+            "admin/%s/%s/submit_line.html" % (app_label, object_name),
+            "admin/%s/submit_line.html" % (app_label,),
+            "admin/submit_line.html",
+        ]
+
+        return super(SubmitRowNode, self).render(context)
+
+    @staticmethod
+    def _get_context(context):
+        """
+        Displays the row of buttons for delete and save.
+        """
+        change = context['change']
+        is_popup = context['is_popup']
+        save_as = context['save_as']
+        show_save = context.get('show_save', True)
+        show_save_and_continue = context.get('show_save_and_continue', True)
+        ctx = Context(context)
+        ctx.update({
+            'show_delete_link': (
+                not is_popup and context['has_delete_permission'] and
+                change and context.get('show_delete', True)
+            ),
+            'show_save_as_new': not is_popup and change and save_as,
+            'show_save_and_add_another': (
+                context['has_add_permission'] and not is_popup and
+                (not save_as or context['add'])
+            ),
+            'show_save_and_continue': not is_popup and context['has_change_permission'] and show_save_and_continue,
+            'show_save': show_save,
+        })
+        return ctx
+
+
+@register.tag
+def submit_row(parser, token):
     """
     Displays the row of buttons for delete and save.
     """
-    change = context['change']
-    is_popup = context['is_popup']
-    save_as = context['save_as']
-    show_save = context.get('show_save', True)
-    show_save_and_continue = context.get('show_save_and_continue', True)
-    ctx = Context(context)
-    ctx.update({
-        'show_delete_link': (
-            not is_popup and context['has_delete_permission'] and
-            change and context.get('show_delete', True)
-        ),
-        'show_save_as_new': not is_popup and change and save_as,
-        'show_save_and_add_another': (
-            context['has_add_permission'] and not is_popup and
-            (not save_as or context['add'])
-        ),
-        'show_save_and_continue': not is_popup and context['has_change_permission'] and show_save_and_continue,
-        'show_save': show_save,
-    })
-    return ctx
+    return SubmitRowNode()
 
 
 @register.filter
