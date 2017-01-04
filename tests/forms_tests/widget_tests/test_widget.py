201227886ab59852a22f9d0de0dbb6d8b45062d8
Tim Graham <timograham@gmail.com>
2017-01-04 11:17:47 -0500

    Refs #15667 -- Removed trailing newline from widget attrs.html template.
    Thanks Dmitry Ivanchenko for the report and Preston Timmons for advice.

--- tests/forms_tests/widget_tests/test_widget.py
+++ tests/forms_tests/widget_tests/test_widget.py
@@ -1,12 +1,17 @@
 from __future__ import unicode_literals
 
 from django.forms import Widget
-from django.test import SimpleTestCase
+from django.forms.widgets import Input
+
+from .base import WidgetTest
 
 
-class WidgetTests(SimpleTestCase):
+class WidgetTests(WidgetTest):
 
     def test_value_omitted_from_data(self):
         widget = Widget()
         self.assertIs(widget.value_omitted_from_data({}, {}, 'field'), True)
         self.assertIs(widget.value_omitted_from_data({'field': 'value'}, {}, 'field'), False)
+
+    def test_no_trailing_newline_in_attrs(self):
+        self.check_html(Input(), 'name', 'value', strict=True, html='<input type="None" name="name" value="value" />')
