9932e1bf52a8167af1bfce0db56e68ca60255814
Tim Graham <timograham@gmail.com>
2017-01-09 08:02:09 -0500

    Fixed test isolation in a couple migrations tests.
    Without this, tests from refs #27432 fail when running with --reverse.

--- tests/migrations/test_executor.py
+++ tests/migrations/test_executor.py
@@ -512,6 +512,9 @@ class ExecutorTests(MigrationTestBase):
             ('mutate_state_a', None),
         ])
         self.assertIn('added', dict(state.models['mutate_state_b', 'b'].fields))
+        executor.migrate([
+            ('mutate_state_b', None),
+        ])
 
     @override_settings(MIGRATION_MODULES={"migrations": "migrations.test_migrations"})
     def test_process_callback(self):
@@ -592,6 +595,7 @@ class ExecutorTests(MigrationTestBase):
                 editor.execute(editor.sql_delete_table % {"table": "author_app_author"})
             self.assertTableNotExists("author_app_author")
             self.assertTableNotExists("book_app_book")
+            executor.migrate([("author_app", None)], fake=True)
 
     @override_settings(MIGRATION_MODULES={"migrations": "migrations.test_migrations_squashed"})
     def test_apply_all_replaced_marks_replacement_as_applied(self):
