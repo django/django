ab1fe85dab13afb017496a5ab5286806698f0049
hardik1997 <hardikdobariya1997@gmail.com>
2017-01-10 14:42:16 +0530

    regex for negative values

--- django/utils/dateparse.py
+++ django/utils/dateparse.py
@@ -30,8 +30,8 @@ datetime_re = re.compile(
 standard_duration_re = re.compile(
     r'^'
     r'(?:(?P<days>-?\d+) (days?, )?)?'
-    r'((?:(?P<hours>-?\d+):)(?=-?\d+:-?\d+))?'
-    r'(?:(?P<minutes>-?\d+):)?'
+    r'((?:(?P<hours>-?\d+):)(?=\d+:\d+))?'
+    r'((?:(?P<minutes>-?\d+):)(?=\d+))?'
     r'(?P<seconds>-?\d+)'
     r'(?:\.(?P<microseconds>\d{1,6})\d{0,6})?'
     r'$'
