0034e9af18f3d393a6dd2389ffbba4c919b1d7d7
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2017-01-13 12:48:30 +0100

    Fixed #5851 -- Allowed specifying different HTML attrs for SplitDateTimeWidget subwidgets.
    Thanks Tim Graham and Nick Pope for review.

--- tests/forms_tests/widget_tests/test_splitdatetimewidget.py
+++ tests/forms_tests/widget_tests/test_splitdatetimewidget.py
@@ -37,6 +37,18 @@ class SplitDateTimeWidgetTest(WidgetTest):
             '<input type="text" class="pretty" value="07:30:00" name="date_1" />'
         ))
 
+    def test_constructor_different_attrs(self):
+        html = (
+            '<input type="text" class="foo" value="2006-01-10" name="date_0" />'
+            '<input type="text" class="bar" value="07:30:00" name="date_1" />'
+        )
+        widget = SplitDateTimeWidget(date_attrs={'class': 'foo'}, time_attrs={'class': 'bar'})
+        self.check_html(widget, 'date', datetime(2006, 1, 10, 7, 30), html=html)
+        widget = SplitDateTimeWidget(date_attrs={'class': 'foo'}, attrs={'class': 'bar'})
+        self.check_html(widget, 'date', datetime(2006, 1, 10, 7, 30), html=html)
+        widget = SplitDateTimeWidget(time_attrs={'class': 'bar'}, attrs={'class': 'foo'})
+        self.check_html(widget, 'date', datetime(2006, 1, 10, 7, 30), html=html)
+
     def test_formatting(self):
         """
         Use 'date_format' and 'time_format' to change the way a value is
