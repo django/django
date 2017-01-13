856f300a9f8e888a60193346a6360e6232c25ae8
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2017-01-13 12:48:30 +0100

    Fixed #5851 -- Added different attrs for date and time fields in SplitDateTimeWidget and SplitHiddenDateTimeWidget.
    Thanks Tim Graham and Nick Pope for review.

--- tests/forms_tests/widget_tests/test_splithiddendatetimewidget.py
+++ tests/forms_tests/widget_tests/test_splithiddendatetimewidget.py
@@ -40,3 +40,15 @@ class SplitHiddenDateTimeWidgetTest(WidgetTest):
             <input type="hidden" name="date_1" value="12:51:00" />
             """
         ))
+
+    def test_constructor_different_attrs(self):
+        html = (
+            '<input type="hidden" class="foo" value="2006-01-10" name="date_0" />'
+            '<input type="hidden" class="bar" value="07:30:00" name="date_1" />'
+        )
+        widget = SplitHiddenDateTimeWidget(date_attrs={'class': 'foo'}, time_attrs={'class': 'bar'})
+        self.check_html(widget, 'date', datetime(2006, 1, 10, 7, 30), html=html)
+        widget = SplitHiddenDateTimeWidget(date_attrs={'class': 'foo'}, attrs={'class': 'bar'})
+        self.check_html(widget, 'date', datetime(2006, 1, 10, 7, 30), html=html)
+        widget = SplitHiddenDateTimeWidget(time_attrs={'class': 'bar'}, attrs={'class': 'foo'})
+        self.check_html(widget, 'date', datetime(2006, 1, 10, 7, 30), html=html)
