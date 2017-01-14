f4c0eec713a6ffe7616fdc5f4744b40110d4fed1
Jinank Jain <jinank94@gmail.com>
2017-01-14 11:17:54 +0100

    Fixed #27699 -- Added negative timedelta support to parse_duration()

--- tests/utils_tests/test_dateparse.py
+++ tests/utils_tests/test_dateparse.py
@@ -108,6 +108,10 @@ class DurationParseTests(unittest.TestCase):
 
     def test_negative(self):
         self.assertEqual(parse_duration('-4 15:30'), timedelta(days=-4, minutes=15, seconds=30))
+        self.assertEqual(parse_duration('-172800'), timedelta(days=-2))
+        self.assertEqual(parse_duration('-15:30'), timedelta(minutes=-15, seconds=30))
+        self.assertEqual(parse_duration('-1:15:30'), timedelta(hours=-1, minutes=15, seconds=30))
+        self.assertEqual(parse_duration('-30.1'), timedelta(seconds=-30, milliseconds=-100))
 
     def test_iso_8601(self):
         self.assertIsNone(parse_duration('P4Y'))
