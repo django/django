6192bffb130132461e55e5fe7a7eaaa9a166d08f
Tim Graham <timograham@gmail.com>
2017-01-17 10:06:02 -0500

    Refs #26601 -- Removed obsolete system check for MIDDLEWARE_CLASSES setting.

--- django/core/checks/__init__.py
+++ django/core/checks/__init__.py
@@ -9,7 +9,6 @@ from .registry import Tags, register, run_checks, tag_exists
 
 # Import these to force registration of checks
 import django.core.checks.caches  # NOQA isort:skip
-import django.core.checks.compatibility.django_1_10  # NOQA isort:skip
 import django.core.checks.database  # NOQA isort:skip
 import django.core.checks.model_checks  # NOQA isort:skip
 import django.core.checks.security.base  # NOQA isort:skip
