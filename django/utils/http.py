d29fd3f9a63f1e515fa8ed863b08a5389938b05f
Tim Graham <timograham@gmail.com>
2017-01-19 13:21:54 -0500

    Fixed django/utils/http.py comment typo.

--- django/utils/http.py
+++ django/utils/http.py
@@ -185,7 +185,7 @@ def base36_to_int(s):
     input won't fit into an int.
     """
     # To prevent overconsumption of server resources, reject any
-    # base36 string that is long than 13 base36 digits (13 digits
+    # base36 string that is longer than 13 base36 digits (13 digits
     # is sufficient to base36-encode any 64-bit integer)
     if len(s) > 13:
         raise ValueError("Base36 input too large")
