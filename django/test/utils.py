e5c67f099159f522d140148fa1a6c876d38288f4
Tim Graham <timograham@gmail.com>
2017-01-19 10:56:00 -0500

    Refs #23919 -- Removed reset_warning_registry() workaround for Python < 3.4.2.

--- django/test/utils.py
+++ django/test/utils.py
@@ -768,20 +768,6 @@ def captured_stdin():
     return captured_output("stdin")
 
 
-def reset_warning_registry():
-    """
-    Clear warning registry for all modules. This is required in some tests
-    because of a bug in Python that prevents warnings.simplefilter("always")
-    from always making warnings appear: http://bugs.python.org/issue4180
-
-    The bug was fixed in Python 3.4.2.
-    """
-    key = "__warningregistry__"
-    for mod in sys.modules.values():
-        if hasattr(mod, key):
-            getattr(mod, key).clear()
-
-
 @contextmanager
 def freeze_time(t):
     """
