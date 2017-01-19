b7621bbe8052fce7c0c179e3bde2d5c2b3758480
james mike dupont <jamesmikedupont@gmail.com>
2017-01-19 05:00:41 -0500

    [1.11.x] Fixed spelling mistakes in code and comments.
    Backport of 7d200949968002d2c2b4390953e8bda7153a49c9 from master

--- django/db/backends/utils.py
+++ django/db/backends/utils.py
@@ -36,9 +36,9 @@ class CursorWrapper(object):
         return self
 
     def __exit__(self, type, value, traceback):
-        # Ticket #17671 - Close instead of passing thru to avoid backend
-        # specific behavior. Catch errors liberally because errors in cleanup
-        # code aren't useful.
+        # Close instead of passing through to avoid backend-specific behavior
+        # (#17671). Catch errors liberally because errors in cleanup code
+        # aren't useful.
         try:
             self.close()
         except self.db.Database.Error:
