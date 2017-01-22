5fa390ee81c5a963fc4476e8de6c1e5e1fc57e22
Jon Dufresne <jon.dufresne@gmail.com>
2017-01-22 10:15:56 -0800

    Fixed #27759 -- Prevented forms attrs.html template from rendering False attrs.
    Regression in b52c73008a9d67e9ddbb841872dc15cdd3d6ee01.

--- tests/forms_tests/widget_tests/test_widget.py
+++ tests/forms_tests/widget_tests/test_widget.py
@@ -13,3 +13,7 @@ class WidgetTests(WidgetTest):
 
     def test_no_trailing_newline_in_attrs(self):
         self.check_html(Input(), 'name', 'value', strict=True, html='<input type="None" name="name" value="value" />')
+
+    def test_attr_false_not_rendered(self):
+        html = '<input type="None" name="name" value="value" />'
+        self.check_html(Input(), 'name', 'value', html=html, attrs={'readonly': False})
