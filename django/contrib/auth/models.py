d5b573d872bbe0d5825efaee2d41cb3cdc75417b
Thom Wiggers <thom@thomwiggers.nl>
2017-01-22 15:45:42 +0100

    Fixed #26993 -- Increased User.last_name max_length to 150 characters.

--- django/contrib/auth/models.py
+++ django/contrib/auth/models.py
@@ -311,7 +311,7 @@ class AbstractUser(AbstractBaseUser, PermissionsMixin):
         },
     )
     first_name = models.CharField(_('first name'), max_length=30, blank=True)
-    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=150, blank=True)
     email = models.EmailField(_('email address'), blank=True)
     is_staff = models.BooleanField(
         _('staff status'),
