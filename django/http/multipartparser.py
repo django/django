90db4bb0d72e4731052bd33500840fff00834768
Tim Graham <timograham@gmail.com>
2017-01-25 13:00:00 -0500

    Corrected http.multipartparser.exhaust() docstring.
    MultiPartParserError was removed in
    ebf34c3cdcd2c75349c60a064427ac255958bf9b.

--- django/http/multipartparser.py
+++ django/http/multipartparser.py
@@ -566,11 +566,7 @@ class BoundaryIter:
 
 
 def exhaust(stream_or_iterable):
-    """
-    Completely exhausts an iterator or stream.
-
-    Raise a MultiPartParserError if the argument is not a stream or an iterable.
-    """
+    """Exhaust an iterator or stream."""
     try:
         iterator = iter(stream_or_iterable)
     except TypeError:
