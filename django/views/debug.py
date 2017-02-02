bc95e6a0bf7400688887a963b659aeb8f91ba035
Tim Graham <timograham@gmail.com>
2017-02-01 19:38:25 -0500

    [1.11.x] Removed ExceptionReporter support for string exceptions.
    Reverted refs #6423 since raising string exceptions is prohibited
    since Python 2.5.
    
    Backport of 0205e04ce7a554a7b9b27b412288cc6a0e75e48f from master

--- django/views/debug.py
+++ django/views/debug.py
@@ -247,11 +247,6 @@ class ExceptionReporter(object):
         self.template_does_not_exist = False
         self.postmortem = None
 
-        # Handle deprecated string exceptions
-        if isinstance(self.exc_type, six.string_types):
-            self.exc_value = Exception('Deprecated String Exception: %r' % self.exc_type)
-            self.exc_type = type(self.exc_value)
-
     def get_traceback_data(self):
         """Return a dictionary containing traceback information."""
         if self.exc_type and issubclass(self.exc_type, TemplateDoesNotExist):
