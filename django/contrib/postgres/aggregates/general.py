238487c0be757f3e59f157a3c8d059cbf0c564a4
orf <tom@tomforb.es>
2017-02-16 18:34:00 +0000

    Add a variant of ArrayAgg that supports filtering

--- django/contrib/postgres/aggregates/general.py
+++ django/contrib/postgres/aggregates/general.py
@@ -17,7 +17,51 @@ class ArrayAgg(Aggregate):
         if not value:
             return []
         return value
+    
+    
+class ArrayAggFilter(ArrayAgg):
+    template = '%(function)s(%(expressions)s) FILTER (WHERE %(conditions)s)'
 
+    def __init__(self, *args, where=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.where = where
+
+    def resolve_expression(self, query=None, allow_joins=True, reuse=None, summarize=False, for_save=False):
+        expr = super().resolve_expression(query, allow_joins, reuse, summarize, for_save)
+        expr.where = expr.where.resolve_expression(query, allow_joins, reuse, summarize, False)
+        return expr
+
+    # http://cathyreisenwitz.com/wp-content/uploads/2016/01/no.jpg
+    # So I kind of have to copy + paste the as_sql implementation here, as you can't access `params` or `data`
+    # easily. This works, lets leave it be for now.
+
+    def as_sql(self, compiler, connection, function=None, template=None, arg_joiner=None, **extra_context):
+        connection.ops.check_expression_support(self)
+        sql_parts = []
+        params = []
+        for arg in self.source_expressions:
+            arg_sql, arg_params = compiler.compile(arg)
+            sql_parts.append(arg_sql)
+            params.extend(arg_params)
+        data = self.extra.copy()
+        data.update(**extra_context)
+        # Use the first supplied value in this order: the parameter to this
+        # method, a value supplied in __init__()'s **extra (the value in
+        # `data`), or the value defined on the class.
+        if function is not None:
+            data['function'] = function
+        else:
+            data.setdefault('function', self.function)
+        template = template or data.get('template', self.template)
+        arg_joiner = arg_joiner or data.get('arg_joiner', self.arg_joiner)
+        data['expressions'] = data['field'] = arg_joiner.join(sql_parts)
+
+        filter_sql, filter_params = compiler.compile(self.where)
+
+        data['conditions'] = filter_sql
+        params.extend(filter_params)
+        return template % data, params
+    
 
 class BitAnd(Aggregate):
     function = 'BIT_AND'
