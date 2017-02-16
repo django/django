a66f448f11c0c7a4e074dbc7a49b2a490d9101e0
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2017-02-16 23:14:02 +0100

    [1.11.x] Added tests for db.backends.utils.truncate_name().
    Backport of 1f7ca858664491589ba400419a491dd0a9af5dff from master

--- tests/backends/test_utils.py
+++ tests/backends/test_utils.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ImproperlyConfigured
+from django.db.backends.utils import truncate_name
 from django.db.utils import load_backend
 from django.test import SimpleTestCase
 from django.utils import six
@@ -14,3 +15,9 @@ class TestLoadBackend(SimpleTestCase):
         ) % "foo.base" if six.PY2 else "'foo'"
         with self.assertRaisesMessage(ImproperlyConfigured, msg):
             load_backend('foo')
+
+    def test_truncate_name(self):
+        self.assertEqual(truncate_name('some_table', 10), 'some_table')
+        self.assertEqual(truncate_name('some_long_table', 10), 'some_la38a')
+        self.assertEqual(truncate_name('some_long_table', 10, 3), 'some_loa38')
+        self.assertEqual(truncate_name('some_long_table'), 'some_long_table')
