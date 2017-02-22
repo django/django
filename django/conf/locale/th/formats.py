46e51632dc2946ad735e9bba6d11080d81af8008
Manatsawin Hanmongkolchai <manatsawin@gmail.com>
2017-02-22 22:19:02 +0700

    Updated Thai (th) locale formats.

--- django/conf/locale/th/formats.py
+++ django/conf/locale/th/formats.py
@@ -9,13 +9,25 @@ YEAR_MONTH_FORMAT = 'F Y'
 MONTH_DAY_FORMAT = 'j F'
 SHORT_DATE_FORMAT = 'j M Y'
 SHORT_DATETIME_FORMAT = 'j M Y, G:i'
-# FIRST_DAY_OF_WEEK =
+FIRST_DAY_OF_WEEK = 0  # Sunday
 
 # The *_INPUT_FORMATS strings use the Python strftime format syntax,
 # see http://docs.python.org/library/datetime.html#strftime-strptime-behavior
-# DATE_INPUT_FORMATS =
-# TIME_INPUT_FORMATS =
-# DATETIME_INPUT_FORMATS =
+DATE_INPUT_FORMATS = [
+    '%d/%m/%Y',  # 25/10/2006
+    '%d %b %Y',  # 25 ต.ค. 2006
+    '%d %B %Y',  # 25 ตุลาคม 2006
+]
+TIME_INPUT_FORMATS = [
+    '%H:%M:%S.%f',  # 14:30:59.000200
+    '%H:%M:%S',  # 14:30:59
+    '%H:%M',  # 14:30
+]
+DATETIME_INPUT_FORMATS = [
+    '%d/%m/%Y %H:%M:%S.%f',  # 25/10/2006 14:30:59.000200
+    '%d/%m/%Y %H:%M:%S',  # 25/10/2006 14:30:59
+    '%d/%m/%Y %H:%M',  # 25/10/2006 14:30
+]
 DECIMAL_SEPARATOR = '.'
 THOUSAND_SEPARATOR = ','
-# NUMBER_GROUPING =
+NUMBER_GROUPING = 3
