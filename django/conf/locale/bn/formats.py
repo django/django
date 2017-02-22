faa14f358a6e3b04499c923044596b5ee8947d90
Asif Saifuddin Auvi <auvipy@users.noreply.github.com>
2017-02-22 20:27:53 +0600

    [1.11.x] Updated Bengali (bn) locale formats.
    Backport of 9d984f88063b0323c12e158f7615ece2e5893566 from master

--- django/conf/locale/bn/formats.py
+++ django/conf/locale/bn/formats.py
@@ -12,13 +12,24 @@ YEAR_MONTH_FORMAT = 'F Y'
 MONTH_DAY_FORMAT = 'j F'
 SHORT_DATE_FORMAT = 'j M, Y'
 # SHORT_DATETIME_FORMAT =
-# FIRST_DAY_OF_WEEK =
+FIRST_DAY_OF_WEEK = 6  # Saturday
 
 # The *_INPUT_FORMATS strings use the Python strftime format syntax,
 # see http://docs.python.org/library/datetime.html#strftime-strptime-behavior
-# DATE_INPUT_FORMATS =
-# TIME_INPUT_FORMATS =
-# DATETIME_INPUT_FORMATS =
+DATE_INPUT_FORMATS = [
+    '%d/%m/%Y',  # 25/10/2016
+    '%d/%m/%y',  # 25/10/16
+    '%d-%m-%Y',  # 25-10-2016
+    '%d-%m-%y',  # 25-10-16
+]
+TIME_INPUT_FORMATS = [
+    '%H:%M:%S',  # 14:30:59
+    '%H:%M',  # 14:30
+]
+DATETIME_INPUT_FORMATS = [
+    '%d/%m/%Y %H:%M:%S',  # 25/10/2006 14:30:59
+    '%d/%m/%Y %H:%M',  # 25/10/2006 14:30
+]
 DECIMAL_SEPARATOR = '.'
 THOUSAND_SEPARATOR = ','
 # NUMBER_GROUPING =
