0417bf47a6fd00d9a6a4c711780ce01f35f62d59
Chris Lamb <chris@chris-lamb.co.uk>
2017-02-24 23:13:48 +0800

    [1.11.x] Fixed #27874 -- Fixed URL namespace warning (urls.W005) for nested namespaces.
    Backport of 8d4885ede578b5beef7626cd31bbd19a95bd678a from master

--- tests/check_framework/urls/unique_namespaces.py
+++ tests/check_framework/urls/unique_namespaces.py
@@ -5,7 +5,14 @@ common_url_patterns = ([
     url(r'^app-url/', include([])),
 ], 'common')
 
+nested_url_patterns = ([
+    url(r'^common/', include(common_url_patterns, namespace='nested')),
+], 'nested')
+
 urlpatterns = [
     url(r'^app-ns1-0/', include(common_url_patterns, namespace='app-include-1')),
-    url(r'^app-ns1-1/', include(common_url_patterns, namespace='app-include-2'))
+    url(r'^app-ns1-1/', include(common_url_patterns, namespace='app-include-2')),
+    # 'nested' is included twice but namespaced by nested-1 and nested-2.
+    url(r'^app-ns1-2/', include(nested_url_patterns, namespace='nested-1')),
+    url(r'^app-ns1-3/', include(nested_url_patterns, namespace='nested-2')),
 ]
