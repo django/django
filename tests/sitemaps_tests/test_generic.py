12745d8a4f89b00f360cb6102a25ab244135098c
chex <levisolympus@gmail.com>
2017-02-25 17:54:17 +0500

    Added tests for contrib.sitemaps.GenericSitemap.

--- tests/sitemaps_tests/test_generic.py
+++ tests/sitemaps_tests/test_generic.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from django.contrib.sitemaps import GenericSitemap
 from django.test import override_settings
 
 from .base import SitemapTestsBase
@@ -6,6 +9,27 @@ from .models import TestModel
 
 @override_settings(ABSOLUTE_URL_OVERRIDES={})
 class GenericViewsSitemapTests(SitemapTestsBase):
+
+    def test_generic_sitemap_attributes(self):
+        datetime_value = datetime.now()
+        queryset = TestModel.objects.all()
+        generic_sitemap = GenericSitemap(
+            info_dict={
+                'queryset': queryset,
+                'date_field': datetime_value,
+            },
+            priority=0.6,
+            changefreq='monthly',
+        )
+        attr_values = (
+            ('date_field', datetime_value),
+            ('priority', 0.6),
+            ('changefreq', 'monthly'),
+        )
+        for attr_name, expected_value in attr_values:
+            with self.subTest(attr_name=attr_name):
+                self.assertEqual(getattr(generic_sitemap, attr_name), expected_value)
+        self.assertCountEqual(generic_sitemap.queryset, queryset)
 
     def test_generic_sitemap(self):
         "A minimal generic sitemap can be rendered"
