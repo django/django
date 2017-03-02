83b866cd926bcda97c7c9991f4aa1c6c70ae941d
Asif Saifuddin Auvi <auvipy@gmail.com>
2017-03-03 02:35:43 +0600

    Refactored DeleteView to be compatibale with SuccessMessageMixin

--- tests/messages_tests/test_models.py
+++ tests/messages_tests/test_models.py
@@ -4,8 +4,5 @@ from django.db import models
 class SomeObjects(models.Model):
     name = models.CharField(max_length=255)
 
-    class Meta:
-        app_level = "messages"
-
     def __str__(self):
         return self.name
