c4b8c46345305ad975e12f6a9a086874e639575c
Raphael Merx <raphael.merx@gmail.com>
2017-03-07 12:22:45 +0900

    [1.10.x] Fixed #27905 -- Added RelatedFieldWidgetWrapper.value_omitted_from_data().
    Backport of fd75c8f2b7070774f6e90f408cb5cddf65b8734e from master

--- tests/admin_widgets/tests.py
+++ tests/admin_widgets/tests.py
@@ -631,6 +631,15 @@ class RelatedFieldWidgetWrapperTests(SimpleTestCase):
         self.assertTrue(wrapper.can_change_related)
         self.assertFalse(wrapper.can_delete_related)
 
+    def test_widget_delegates_value_omitted_from_data(self):
+        class CustomWidget(forms.Select):
+            def value_omitted_from_data(self, data, files, name):
+                return False
+        rel = models.Album._meta.get_field('band').remote_field
+        widget = CustomWidget()
+        wrapper = widgets.RelatedFieldWidgetWrapper(widget, rel, widget_admin_site)
+        self.assertIs(wrapper.value_omitted_from_data({}, {}, 'band'), False)
+
 
 @override_settings(ROOT_URLCONF='admin_widgets.urls')
 class AdminWidgetSeleniumTestCase(AdminSeleniumTestCase):
