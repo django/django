1a5635af8afba3b8f876d1c5ef6623ff0017f00d
Chris Lamb <chris@chris-lamb.co.uk>
2017-03-08 08:54:17 +0000

    Fixes #27881 -- Add a --mode=unified to diffsettings management command.

--- tests/admin_scripts/tests.py
+++ tests/admin_scripts/tests.py
@@ -2121,6 +2121,30 @@ class DiffSettings(AdminScriptTestCase):
         self.assertNotInOutput(out, "FOO")
         self.assertOutput(out, "BAR = 'bar2'")
 
+    def test_unified(self):
+        """Runs without error and emits settings diff in unified mode"""
+        self.write_settings('settings_to_diff.py', sdict={'FOO': '"bar"'})
+        self.addCleanup(self.remove_settings, 'settings_to_diff.py')
+        args = ['diffsettings', '--settings=settings_to_diff', '--output=unified']
+        out, err = self.run_manage(args)
+        self.assertNoOutput(err)
+        self.assertOutput(out, "+ FOO = 'bar'")
+        self.assertOutput(out, "- SECRET_KEY = ''")
+        self.assertOutput(out, "+ SECRET_KEY = 'django_tests_secret_key'")
+        self.assertNotInOutput(out, "  APPEND_SLASH = True")
+
+    def test_unified_all(self):
+        """
+        Runs without error and emits settings diff in unified mode, including
+        settings with the default value.
+        """
+        self.write_settings('settings_to_diff.py', sdict={'FOO': '"bar"'})
+        self.addCleanup(self.remove_settings, 'settings_to_diff.py')
+        args = ['diffsettings', '--settings=settings_to_diff', '--output=unified', '--all']
+        out, err = self.run_manage(args)
+        self.assertNoOutput(err)
+        self.assertOutput(out, "  APPEND_SLASH = True")
+
 
 class Dumpdata(AdminScriptTestCase):
     """Tests for dumpdata management command."""
