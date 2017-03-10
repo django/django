9970046745d5bc41bd159f743c54c44fd00af62f
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2017-03-10 15:12:53 +0100

    Fixed #27924 -- Added support for cx_Oracle 5.3.
    - Fixed Oracle backend due to cx_Oracle 5.3 change in the
    Cursor.description behavior i.e. "Use None instead of 0 for items in the
    Cursor.description attribute that do not have any validity."
    - Used cx_Oracle.Object.size() instead of len().
    
    Thanks Tim Graham for the review.

--- django/db/backends/oracle/base.py
+++ django/db/backends/oracle/base.py
@@ -530,7 +530,8 @@ def _rowfactory(row, cursor):
     casted = []
     for value, desc in zip(row, cursor.description):
         if value is not None and desc[1] is Database.NUMBER:
-            precision, scale = desc[4:6]
+            precision = desc[4] or 0
+            scale = desc[5] or 0
             if scale == -127:
                 if precision == 0:
                     # NUMBER column: decimal-precision floating point
