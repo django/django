972602205380173a05b58e08e4f7c4237e429381
Jaap Vermeulen <verjaap@gmail.com>
2017-03-15 16:05:05 -0600

    Ticket #22673 -- Allow postgres schemas
    Add a feature for schemas, and an sql operation for schema SQL.
    Modify base code to split table names that contain a '.' into schema.table.
    For every table created, create the schema if it hasn't already been created.
    Enable feature for postgres, fix quoting ("schema"."table"), and fix work-around for #7783.
    Added simple test for creating and quering a table in a non-standard schema.
    Added corresponding documentation.

---
+++ tests/postgres_tests/test_schemas.py
@@ -0,0 +1,23 @@
+from django.test import modify_settings
+
+from . import PostgreSQLTestCase
+from .models import SchemaModel
+
+
+@modify_settings(INSTALLED_APPS={'append': 'django.contrib.postgres'})
+class SchemaTest(PostgreSQLTestCase):
+
+    Model = SchemaModel
+
+    def setUp(self):
+        self.Model.objects.bulk_create([
+            self.Model(int1=1),
+            self.Model(int1=2),
+            self.Model(int1=3),
+        ])
+
+    def test_values(self):
+        index = 1
+        for object in self.Model.objects.all():
+            self.assertEqual(object.int1, index)
+            index += 1
