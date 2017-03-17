0c1355e9bb50868c5a5980b975d8f9a76b190f0b
Anton Samarchyan <anton.samarchyan@savoirfairelinux.com>
2017-03-17 15:34:04 -0400

    Completed test coverage for RedirectFallbackMiddleware.

--- tests/redirects_tests/tests.py
+++ tests/redirects_tests/tests.py
@@ -8,7 +8,7 @@ from django.test import TestCase, modify_settings, override_settings
 
 
 @modify_settings(MIDDLEWARE={'append': 'django.contrib.redirects.middleware.RedirectFallbackMiddleware'})
-@override_settings(APPEND_SLASH=False, SITE_ID=1)
+@override_settings(APPEND_SLASH=False, ROOT_URLCONF='redirects_tests.urls', SITE_ID=1)
 class RedirectTests(TestCase):
 
     def setUp(self):
@@ -34,6 +34,20 @@ class RedirectTests(TestCase):
         Redirect.objects.create(site=self.site, old_path='/initial/?foo', new_path='/new_target/')
         response = self.client.get('/initial?foo')
         self.assertRedirects(response, '/new_target/', status_code=301, target_status_code=404)
+
+    @override_settings(APPEND_SLASH=True)
+    def test_redirect_not_found_with_append_slash(self):
+        """
+        Exercise the second Redirect.DoesNotExist branch in
+        RedirectFallbackMiddleware.
+        """
+        response = self.client.get('/test')
+        self.assertEqual(response.status_code, 404)
+
+    def test_redirect_shortcircuits_non_404_response(self):
+        """RedirectFallbackMiddleware short-circuits on non-404 requests."""
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
 
     def test_response_gone(self):
         """When the redirect target is '', return a 410"""
