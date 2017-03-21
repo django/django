a8c540accc7eb526e44ec2bc05881eb300f54530
Kapil Garg <kapilgarg1996@gmail.com>
2017-03-22 04:59:14 +0530

    [1.11.x] Fixed #27969 -- Fixed models.Field.formfield() setting 'disabled' for fields with choices.
    Backport of 7e09fa7f519daf2f9c26587c3da5dc044d84b032 from master

--- tests/model_fields/tests.py
+++ tests/model_fields/tests.py
@@ -54,6 +54,12 @@ class BasicFieldTests(TestCase):
         klass = forms.TypedMultipleChoiceField
         self.assertIsInstance(field.formfield(choices_form_class=klass), klass)
 
+    def test_formfield_disabled(self):
+        """Field.formfield() sets disabled for fields with choices."""
+        field = models.CharField(choices=[('a', 'b')])
+        form_field = field.formfield(disabled=True)
+        self.assertIs(form_field.disabled, True)
+
     def test_field_str(self):
         f = models.Field()
         self.assertEqual(str(f), '<django.db.models.fields.Field>')
