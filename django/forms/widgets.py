4f3d824b04aef6df0f1c1594929d6531d74d5ac4
heathervm <heathervmurray@gmail.com>
2017-03-29 19:41:12 -0700

    Fixed #27993 -- Fixed model form default fallback for SelectMultiple.

--- django/forms/widgets.py
+++ django/forms/widgets.py
@@ -714,6 +714,11 @@ class SelectMultiple(Select):
             getter = data.get
         return getter(name)
 
+    def value_omitted_from_data(self, data, files, name):
+        # An unselected <select multiple> doesn't appear in POST data, so it's
+        # never known if the value is actually omitted.
+        return False
+
 
 class RadioSelect(ChoiceWidget):
     input_type = 'radio'
--- tests/forms_tests/widget_tests/test_selectmultiple.py
+++ tests/forms_tests/widget_tests/test_selectmultiple.py
@@ -123,3 +123,8 @@ class SelectMultipleTest(WidgetTest):
             </optgroup>
             </select>"""
         ))
+
+    def test_value_omitted_from_data(self):
+        widget = self.widget(choices=self.beatles)
+        self.assertIs(widget.value_omitted_from_data({}, {}, 'field'), False)
+        self.assertIs(widget.value_omitted_from_data({'field': 'value'}, {}, 'field'), False)
