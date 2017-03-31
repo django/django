7d1e23775344cc3dead03bd4af45f4fdf134b819
heathervm <heathervmurray@gmail.com>
2017-03-31 07:10:08 -0700

    Fixed #27993 -- Fixed model form default fallback for SelectMultiple.

--- tests/model_forms/tests.py
+++ tests/model_forms/tests.py
@@ -614,6 +614,22 @@ class ModelFormBaseTest(TestCase):
         self.assertEqual(m1.mode, '')
         self.assertEqual(m1._meta.get_field('mode').get_default(), 'di')
 
+    def test_default_not_populated_on_selectmultiple(self):
+        class PubForm(forms.ModelForm):
+            mode = forms.CharField(required=False, widget=forms.SelectMultiple)
+
+            class Meta:
+                model = PublicationDefaults
+                fields = ('mode',)
+
+        # Empty data doesn't use the model default because an unselected
+        # SelectMultiple doesn't have a value in HTML form submission.
+        mf1 = PubForm({})
+        self.assertEqual(mf1.errors, {})
+        m1 = mf1.save(commit=False)
+        self.assertEqual(m1.mode, '')
+        self.assertEqual(m1._meta.get_field('mode').get_default(), 'di')
+
     def test_prefixed_form_with_default_field(self):
         class PubForm(forms.ModelForm):
             prefix = 'form-prefix'
