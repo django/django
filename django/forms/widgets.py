83331d1ed0fde144767e8f35f2d9e512bdd2afb1
heathervm <heathervmurray@gmail.com>
2017-03-31 07:10:08 -0700

    [1.10.x] Fixed #27993 -- Fixed model form default fallback for SelectMultiple.
    Backport of 7d1e23775344cc3dead03bd4af45f4fdf134b819 from master

--- django/forms/widgets.py
+++ django/forms/widgets.py
@@ -631,6 +631,11 @@ class SelectMultiple(Select):
             return data.getlist(name)
         return data.get(name)
 
+    def value_omitted_from_data(self, data, files, name):
+        # An unselected <select multiple> doesn't appear in POST data, so it's
+        # never known if the value is actually omitted.
+        return False
+
 
 @html_safe
 @python_2_unicode_compatible
