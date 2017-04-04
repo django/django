7f5be2dc84fc8e7fa81e386ac85dcb65ab6c345d
Tim Graham <timograham@gmail.com>
2017-04-03 22:33:46 -0400

    [1.11.x] Fixed flake8 warning on Python 2.

--- tests/backends/test_postgresql.py
+++ tests/backends/test_postgresql.py
@@ -21,7 +21,7 @@ class ServerSideCursorsPostgres(TestCase):
         with connection.cursor() as cursor:
             cursor.execute('SELECT {fields} FROM pg_cursors;'.format(fields=self.cursor_fields))
             cursors = cursor.fetchall()
-        return [self.PostgresCursor._make(cursor) for cursor in cursors]
+        return [self.PostgresCursor._make(c) for c in cursors]
 
     def test_server_side_cursor(self):
         persons = Person.objects.iterator()
