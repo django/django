a41d7e73801f407c0517dd10e3bac9b19c33cc55
Tim Graham <timograham@gmail.com>
2017-04-04 12:16:32 -0400

    [1.10.x] Post-release version bump.

--- django/__init__.py
+++ django/__init__.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from django.utils.version import get_version
 
-VERSION = (1, 10, 7, 'final', 0)
+VERSION = (1, 10, 8, 'alpha', 0)
 
 __version__ = get_version(VERSION)
 
