fa91d29058b6f357382fcc945957e52035b1eb8e
Marc Tamlyn <marc.tamlyn@gmail.com>
2017-04-07 09:42:58 +0200

    Review feedback.

--- django/contrib/postgres/indexes.py
+++ django/contrib/postgres/indexes.py
@@ -1,6 +1,6 @@
 from django.db.models import Index
 
-__all__ = ['BrinIndex', 'GinIndex']
+__all__ = ['BrinIndex', 'GinIndex', 'GistIndex']
 
 
 class BrinIndex(Index):
@@ -49,6 +49,11 @@ class GinIndex(Index):
 
 class GistIndex(Index):
     suffix = 'gist'
+    # Allow an index name longer than 30 characters since the suffix is 4
+    # characters (usual limit is 3). Since this index can only be used on
+    # PostgreSQL, the 30 character limit for cross-database compatibility isn't
+    # applicable.
+    max_name_length = 31
 
     def create_sql(self, model, schema_editor):
         return super().create_sql(model, schema_editor, using=' USING gist')
