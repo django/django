493f7e9e1ed7d68df102de6c5926a55521b145c9
Matthew Schinckel <matt@schinckel.net>
2017-04-13 11:33:35 +0930

    Fixed #28076 -- Added support for PostgreSQL's interval format to parse_duration().

--- tests/utils_tests/test_dateparse.py
+++ tests/utils_tests/test_dateparse.py
@@ -65,6 +65,21 @@ class DurationParseTests(unittest.TestCase):
             with self.subTest(delta=delta):
                 self.assertEqual(parse_duration(format(delta)), delta)
 
+    def test_parse_postgresql_format(self):
+        test_values = (
+            ('1 day', timedelta(1)),
+            ('1 day 0:00:01', timedelta(days=1, seconds=1)),
+            ('1 day -0:00:01', timedelta(days=1, seconds=-1)),
+            ('-1 day -0:00:01', timedelta(days=-1, seconds=-1)),
+            ('-1 day +0:00:01', timedelta(days=-1, seconds=1)),
+            ('4 days 0:15:30.1', timedelta(days=4, minutes=15, seconds=30, milliseconds=100)),
+            ('4 days 0:15:30.0001', timedelta(days=4, minutes=15, seconds=30, microseconds=100)),
+            ('-4 days -15:00:30', timedelta(days=-4, hours=-15, seconds=-30)),
+        )
+        for source, expected in test_values:
+            with self.subTest(source=source):
+                self.assertEqual(parse_duration(source), expected)
+
     def test_seconds(self):
         self.assertEqual(parse_duration('30'), timedelta(seconds=30))
 
