9dd18d52111196d6e4ce95e222f94ec30563afbb
Leon Matthews <leon@digitaladvisor.nz>
2017-04-17 11:38:03 +1200

    Match code-style to existing

--- django/views/generic/dates.py
+++ django/views/generic/dates.py
@@ -285,13 +285,12 @@ class BaseDateListView(MultipleObjectMixin, DateMixin, View):
     """Abstract base class for date-based views displaying a list of objects."""
     allow_empty = False
     date_list_period = 'year'
-    
+
     def get(self, request, *args, **kwargs):
         self.date_list, self.object_list, extra_context = self.get_dated_items()
-        context = self.get_context_data(
-            object_list=self.object_list,
-            date_list=self.date_list,
-            **extra_context)
+        context = self.get_context_data(object_list=self.object_list,
+                                        date_list=self.date_list,
+                                        **extra_context)
         return self.render_to_response(context)
 
     def get_dated_items(self):
