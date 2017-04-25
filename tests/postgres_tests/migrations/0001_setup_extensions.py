a78fd2abd48811a824d9ac61e61ce106567380a8
Paolo Melchiorre <paolo@melchiorre.org>
2017-04-25 18:49:29 -0400

    Alphabetized CITextExtension in test migration.

--- tests/postgres_tests/migrations/0001_setup_extensions.py
+++ tests/postgres_tests/migrations/0001_setup_extensions.py
@@ -9,22 +9,22 @@ try:
     )
 except ImportError:
     BtreeGinExtension = mock.Mock()
+    CITextExtension = mock.Mock()
     CreateExtension = mock.Mock()
     HStoreExtension = mock.Mock()
     TrigramExtension = mock.Mock()
     UnaccentExtension = mock.Mock()
-    CITextExtension = mock.Mock()
 
 
 class Migration(migrations.Migration):
 
     operations = [
         BtreeGinExtension(),
+        CITextExtension(),
         # Ensure CreateExtension quotes extension names by creating one with a
         # dash in its name.
         CreateExtension('uuid-ossp'),
         HStoreExtension(),
         TrigramExtension(),
         UnaccentExtension(),
-        CITextExtension(),
     ]
