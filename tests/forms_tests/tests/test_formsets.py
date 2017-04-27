django/django

commit f5c6295797b8332134fd89e0209a18a1d1d45e0c

Fixed #26844 -- Made formset's validate_min validation ignore empty forms.

django/forms/formsets.py
@@ -336,11 +336,15 @@ class BaseFormSet(object):
         """
         self._errors = []
         self._non_form_errors = self.error_class()
+        empty_forms_count = 0
 
         if not self.is_bound:  # Stop further processing.
             return
         for i in range(0, self.total_form_count()):
             form = self.forms[i]
+            if not form.has_changed():
+                empty_forms_count += 1
+
             self._errors.append(form.errors)
         try:
             if (self.validate_max and
@@ -352,7 +356,7 @@ class BaseFormSet(object):
                     code='too_many_forms',
                 )
             if (self.validate_min and
-                    self.total_form_count() - len(self.deleted_forms) < self.min_num):
+                    self.total_form_count() - len(self.deleted_forms) - empty_forms_count < self.min_num):
                 raise ValidationError(ungettext(
                     "Please submit %d or more forms.",
                     "Please submit %d or more forms.", self.min_num) % self.min_num,

tests/forms_tests/tests/test_formsets.py
@@ -377,6 +377,17 @@ class FormsFormsetTestCase(SimpleTestCase):
         self.assertFalse(formset.is_valid())
         self.assertEqual(formset.non_form_errors(), ['Please submit 3 or more forms.'])
 
+    def test_formset_validate_min_excludes_empty_forms(self):
+        data = {
+            'choices-TOTAL_FORMS': '2',
+            'choices-INITIAL_FORMS': '0',
+        }
+        ChoiceFormSet = formset_factory(Choice, extra=2, min_num=1, validate_min=True, can_delete=True)
+        formset = ChoiceFormSet(data, prefix='choices')
+        self.assertFalse(formset.has_changed())
+        self.assertFalse(formset.is_valid())
+        self.assertEqual(formset.non_form_errors(), ['Please submit 1 or more forms.'])
+
     def test_second_form_partially_filled_2(self):
         # And once again, if we try to partially complete a form, validation will fail.
 

commit 9d20293bb1f572a1c5a7519f5b35146e9b066406

Fixed #28130 -- Fixed formset min_num validation with initial, unchanged forms.
Regression in f5c6295797b8332134fd89e0209a18a1d1d45e0c.

django/forms/formsets.py
@@ -322,7 +322,8 @@ class BaseFormSet:
             return
         for i in range(0, self.total_form_count()):
             form = self.forms[i]
-            if not form.has_changed():
+            # Empty forms are unchanged forms beyond those with initial data.
+            if not form.has_changed() and i >= self.initial_form_count():
                 empty_forms_count += 1
 
             self._errors.append(form.errors)

tests/forms_tests/tests/test_formsets.py
@@ -402,6 +402,31 @@ class FormsFormsetTestCase(SimpleTestCase):
         self.assertFalse(formset.is_valid())
         self.assertEqual(formset.non_form_errors(), ['Please submit 3 or more forms.'])
 
+    def test_formset_validate_min_unchanged_forms(self):
+        """
+        min_num validation doesn't consider unchanged forms with initial data
+        as "empty".
+        """
+        initial = [
+            {'choice': 'Zero', 'votes': 0},
+            {'choice': 'One', 'votes': 0},
+        ]
+        data = {
+            'choices-TOTAL_FORMS': '2',
+            'choices-INITIAL_FORMS': '2',
+            'choices-MIN_NUM_FORMS': '0',
+            'choices-MAX_NUM_FORMS': '2',
+            'choices-0-choice': 'Zero',
+            'choices-0-votes': '0',
+            'choices-1-choice': 'One',
+            'choices-1-votes': '1',  # changed from initial
+        }
+        ChoiceFormSet = formset_factory(Choice, min_num=2, validate_min=True)
+        formset = ChoiceFormSet(data, auto_id=False, prefix='choices', initial=initial)
+        self.assertFalse(formset.forms[0].has_changed())
+        self.assertTrue(formset.forms[1].has_changed())
+        self.assertTrue(formset.is_valid())
+
     def test_formset_validate_min_excludes_empty_forms(self):
         data = {
             'choices-TOTAL_FORMS': '2',