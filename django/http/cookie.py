342959b995dcab719b6ae350ae214bb46bee8470
Pawel Krawczyk (qubes) <pawel.krawczyk@hush.com>
2017-05-04 13:25:10 +0100

    patch Morsel and SimpleCookie classes to support SameSite cookie flag

--- django/http/cookie.py
+++ django/http/cookie.py
@@ -1,34 +1,35 @@
 import sys
 from http import cookies
 
-# Cookie pickling bug is fixed in Python 3.4.3+
-# http://bugs.python.org/issue22775
 if sys.version_info < (3, 7, 0):
-    # Monkey patching the Morsel object
+    # Monkey patch the Morsel and SimpleCookie objects
     # to support SameSite attribute
     # added in 3.7 https://github.com/python/cpython/pull/214
     class Morsel(cookies.Morsel):
-        _reserved = {
-            "expires": "expires",
-            "path": "Path",
-            "comment": "Comment",
-            "domain": "Domain",
-            "max-age": "Max-Age",
-            "secure": "Secure",
-            "httponly": "HttpOnly",
-            "version": "Version",
-            "samesite": "SameSite",
-        }
+        def __init__(self):
+            super().__init__()
+            self._reserved['samesite'] = 'SameSite'
 
     class SimpleCookie(cookies.SimpleCookie):
-        def __setitem__(self, key, value):
-            if isinstance(value, Morsel):
-                # allow assignment of constructed Morsels (e.g. for pickling)
-                dict.__setitem__(self, key, value)
-            else:
-                super().__setitem__(key, value)
 
+        def __set(self, key, real_value, coded_value):
+            """Private method for setting a cookie's value"""
+            # use the patched Morsel
+            M = self.get(key, Morsel())
+            M.set(key, real_value, coded_value)
+            dict.__setitem__(self, key, M)
+
+        # Cookie pickling bug is fixed in Python 3.4.3+
+        # http://bugs.python.org/issue22775
+        if sys.version_info >= (3, 4, 3):
+            def __setitem__(self, key, value):
+                if isinstance(value, Morsel):
+                    # allow assignment of constructed Morsels (e.g. for pickling)
+                    dict.__setitem__(self, key, value)
+                else:
+                    super().__setitem__(key, value)
 else:
+    # Python 3.7+
     SimpleCookie = cookies.SimpleCookie
 
 
