aa83edeff761cb49904af9ba60d1532082b1e7fe
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2017-05-13 12:48:25 +0200

    Refs #27859 -- Refactored BaseDatabaseValidation to use check_field_type().
    Thanks Tim Graham for the review.

--- django/db/backends/base/validation.py
+++ django/db/backends/base/validation.py
@@ -7,4 +7,19 @@ class BaseDatabaseValidation:
         return []
 
     def check_field(self, field, **kwargs):
-        return []
+        errors = []
+        # Backends may implement a check_field_type() method.
+        if (hasattr(self, 'check_field_type') and
+                # Ignore any related fields.
+                not getattr(field, 'remote_field', None)):
+            # Ignore fields with unsupported features.
+            db_supports_all_required_features = all(
+                getattr(self.connection.features, feature, False)
+                for feature in field.model._meta.required_db_features
+            )
+            if db_supports_all_required_features:
+                field_type = field.db_type(self.connection)
+                # Ignore non-concrete fields.
+                if field_type is not None:
+                    errors.extend(self.check_field_type(field, field_type))
+        return errors
