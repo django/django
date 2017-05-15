650bf6714d5648811a8bc08ce53b7fa86cc38d40
Tim Graham <timograham@gmail.com>
2017-05-15 09:26:38 -0400

    Refs #27996 -- Skipped RandomUUID test on PostgreSQL 9.3.

--- tests/postgres_tests/test_functions.py
+++ tests/postgres_tests/test_functions.py
@@ -4,7 +4,7 @@ from time import sleep
 
 from django.contrib.postgres.functions import RandomUUID, TransactionNow
 
-from . import PostgreSQLTestCase
+from . import PostgreSQLTestCase, skipUnlessPG94
 from .models import NowTestModel, UUIDTestModel
 
 
@@ -29,6 +29,7 @@ class TestTransactionNow(PostgreSQLTestCase):
         self.assertEqual(m1.when, m2.when)
 
 
+@skipUnlessPG94
 class TestRandomUUID(PostgreSQLTestCase):
 
     def test_random_uuid(self):
