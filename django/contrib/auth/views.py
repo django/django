16431b03f801788d791bbb24d6fb266c3591ab07
Mikhail Golubev <qsolo825@gmail.com>
2017-05-22 14:52:56 -0700

    [1.11.x] Fixed #28229 -- Fixed the value of LoginView's "next" template variable.
    Backport of e7dc39fb65e51d7613c941f7e5768b621dea4e76 from master

--- django/contrib/auth/views.py
+++ django/contrib/auth/views.py
@@ -90,7 +90,11 @@ class LoginView(SuccessURLAllowedHostsMixin, FormView):
         return super(LoginView, self).dispatch(request, *args, **kwargs)
 
     def get_success_url(self):
-        """Ensure the user-originating redirection URL is safe."""
+        url = self.get_redirect_url()
+        return url or resolve_url(settings.LOGIN_REDIRECT_URL)
+
+    def get_redirect_url(self):
+        """Return the user-originating redirect URL if it's safe."""
         redirect_to = self.request.POST.get(
             self.redirect_field_name,
             self.request.GET.get(self.redirect_field_name, '')
@@ -100,9 +104,7 @@ class LoginView(SuccessURLAllowedHostsMixin, FormView):
             allowed_hosts=self.get_success_url_allowed_hosts(),
             require_https=self.request.is_secure(),
         )
-        if not url_is_safe:
-            return resolve_url(settings.LOGIN_REDIRECT_URL)
-        return redirect_to
+        return redirect_to if url_is_safe else ''
 
     def get_form_class(self):
         return self.authentication_form or self.form_class
@@ -121,7 +123,7 @@ class LoginView(SuccessURLAllowedHostsMixin, FormView):
         context = super(LoginView, self).get_context_data(**kwargs)
         current_site = get_current_site(self.request)
         context.update({
-            self.redirect_field_name: self.get_success_url(),
+            self.redirect_field_name: self.get_redirect_url(),
             'site': current_site,
             'site_name': current_site.name,
         })
