ac0cdabab24902e17bc0cf1e1e4f3b680a18eae8
Curtis Maloney <curtis@tinbrain.net>
2017-06-01 10:14:32 +1000

    Tidy for flake8

--- django/contrib/auth/middleware.py
+++ django/contrib/auth/middleware.py
@@ -1,8 +1,6 @@
-from django.conf import settings
 from django.contrib import auth
 from django.contrib.auth import load_backend
 from django.contrib.auth.backends import RemoteUserBackend
-from django.core.exceptions import ImproperlyConfigured
 from django.utils.deprecation import MiddlewareMixin
 
 
