ce4edd260bfa790418eea7de0112ce7c16feb304
Tim Graham <timograham@gmail.com>
2017-06-01 11:22:36 -0400

    [1.11.x] Bumped version for 1.11.2 release.

--- django/__init__.py
+++ django/__init__.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from django.utils.version import get_version
 
-VERSION = (1, 11, 2, 'alpha', 0)
+VERSION = (1, 11, 2, 'final', 0)
 
 __version__ = get_version(VERSION)
 
