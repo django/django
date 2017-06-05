525dc283a68c0d47f5eb2192cc4a20111d561ae0
kakulukia <andy@freilandkiwis.de>
2017-06-05 22:17:10 +0200

    Fixed #28278 -- Fixed invalid HTML for a required AdminFileWidget.

--- tests/admin_widgets/tests.py
+++ tests/admin_widgets/tests.py
@@ -431,6 +431,18 @@ class AdminFileWidgetTests(TestDataMixin, TestCase):
             '<input type="file" name="test" />',
         )
 
+    def test_render_required(self):
+        widget = widgets.AdminFileWidget()
+        widget.is_required = True
+        self.assertHTMLEqual(
+            widget.render('test', self.album.cover_art),
+            '<p class="file-upload">Currently: <a href="%(STORAGE_URL)salbums/'
+            r'hybrid_theory.jpg">albums\hybrid_theory.jpg</a><br />'
+            'Change: <input type="file" name="test" /></p>' % {
+                'STORAGE_URL': default_storage.url(''),
+            },
+        )
+
     def test_readonly_fields(self):
         """
         File widgets should render as a link when they're marked "read only."
