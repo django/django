876de7c34d8af35d6e4c35e2a5a14d3b97c26e95
Joe Tsoi <joetsoi@users.noreply.github.com>
2017-06-15 17:38:20 +0100

    Add weighting to lexemes

--- django/contrib/postgres/search.py
+++ django/contrib/postgres/search.py
@@ -335,16 +335,24 @@ class LexemeCombinable(Expression):
 class Lexeme(LexemeCombinable, Value):
     _output_field = SearchQueryField()
 
-    def __init__(self, value, output_field=None, *, invert=False, prefix=False):
+    def __init__(self, value, output_field=None, *, invert=False, prefix=False, weight=None):
         self.prefix = prefix
         self.invert = invert
+        self.weight = weight
         super().__init__(value, output_field=output_field)
 
     def as_sql(self, compiler, connection):
         param = self.value
         template = '%s'
+
+        label = ''
         if self.prefix:
-            param = '{}:*'.format(param)
+            label += '*'
+        if self.weight:
+            label += self.weight
+
+        if label:
+            param = '{}:{}'.format(param, label)
         if self.invert:
             param = '!{}'.format(param)
         params = [param]
