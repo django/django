c4329271607637373e42f081bd632e653aa99ea3
Paulo Alvarado <commonzenpython@gmail.com>
2017-06-26 12:11:20 -0400

    Fixed #26362 -- Fixed update of the inherited id field of an object when its parent changes.

--- django/db/models/fields/related_descriptors.py
+++ django/db/models/fields/related_descriptors.py
@@ -277,6 +277,22 @@ class ForwardOneToOneDescriptor(ForwardManyToOneDescriptor):
                 return obj
         return super().get_object(instance)
 
+    def __set__(self, instance, value):
+        super().__set__(instance, value)
+        # If the primary key is a link to a parent model and a parent instance
+        # is being set, update the value of the inherited pk(s).
+        if self.field.primary_key and self.field.remote_field.parent_link:
+            opts = instance._meta
+            # Inherited primary key fields from this object's base classes.
+            inherited_pk_fields = [
+                field for field in opts.concrete_fields
+                if field.primary_key and field.remote_field
+            ]
+            for field in inherited_pk_fields:
+                rel_model_pk_name = field.remote_field.model._meta.pk.attname
+                raw_value = getattr(value, rel_model_pk_name) if value is not None else None
+                setattr(instance, rel_model_pk_name, raw_value)
+
 
 class ReverseOneToOneDescriptor:
     """
