a1be12fe193c8f3de8a0b0820f460a302472375f
Jon Dufresne <jon.dufresne@gmail.com>
2017-06-28 20:30:19 -0700

    Fixed #28345 -- Applied limit_choices_to during ModelForm.__init__().
    field_for_model() now has an additional keyword argument,
    apply_limit_choices_to, allowing it to continue to be used to create
    form fields dynamically after ModelForm.__init__() is called.
    
    Thanks Tim Graham for the review.

--- tests/model_forms/tests.py
+++ tests/model_forms/tests.py
@@ -1,7 +1,7 @@
 import datetime
 import os
 from decimal import Decimal
-from unittest import skipUnless
+from unittest import mock, skipUnless
 
 from django import forms
 from django.core.exceptions import (
@@ -2905,6 +2905,16 @@ class LimitChoicesToTests(TestCase):
     def test_fields_for_model_applies_limit_choices_to(self):
         fields = fields_for_model(StumpJoke, ['has_fooled_today'])
         self.assertSequenceEqual(fields['has_fooled_today'].queryset, [self.threepwood])
+
+    def test_callable_called_each_time_form_is_instantiated(self):
+        field = StumpJokeForm.base_fields['most_recently_fooled']
+        with mock.patch.object(field, 'limit_choices_to') as today_callable_dict:
+            StumpJokeForm()
+            self.assertEqual(today_callable_dict.call_count, 1)
+            StumpJokeForm()
+            self.assertEqual(today_callable_dict.call_count, 2)
+            StumpJokeForm()
+            self.assertEqual(today_callable_dict.call_count, 3)
 
 
 class FormFieldCallbackTests(SimpleTestCase):
