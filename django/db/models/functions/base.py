8e41373c81cbd914d84b362ca4c85d3ed4fcff43
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2017-07-27 02:26:58 +0200

    Allowed database backends to specify data types for Cast().
    A small refactor ahead of refs #28371.

--- django/db/models/functions/base.py
+++ django/db/models/functions/base.py
@@ -9,31 +9,12 @@ class Cast(Func):
     function = 'CAST'
     template = '%(function)s(%(expressions)s AS %(db_type)s)'
 
-    mysql_types = {
-        fields.CharField: 'char(%(max_length)s)',
-        fields.IntegerField: 'signed integer',
-        fields.BigIntegerField: 'signed integer',
-        fields.SmallIntegerField: 'signed integer',
-        fields.FloatField: 'signed',
-        fields.PositiveIntegerField: 'unsigned integer',
-        fields.PositiveSmallIntegerField: 'unsigned integer',
-    }
-
     def __init__(self, expression, output_field):
         super().__init__(expression, output_field=output_field)
 
     def as_sql(self, compiler, connection, **extra_context):
-        if 'db_type' not in extra_context:
-            extra_context['db_type'] = self.output_field.db_type(connection)
+        extra_context['db_type'] = self.output_field.cast_db_type(connection)
         return super().as_sql(compiler, connection, **extra_context)
-
-    def as_mysql(self, compiler, connection):
-        extra_context = {}
-        output_field_class = type(self.output_field)
-        if output_field_class in self.mysql_types:
-            data = self.output_field.db_type_parameters(connection)
-            extra_context['db_type'] = self.mysql_types[output_field_class] % data
-        return self.as_sql(compiler, connection, **extra_context)
 
     def as_postgresql(self, compiler, connection):
         # CAST would be valid too, but the :: shortcut syntax is more readable.
