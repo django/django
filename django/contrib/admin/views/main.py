74a6702242dcca223b87b8d06fe7589c2f4b2a41
Matthew Betts <matt.je.betts@gmail.com>
2017-08-07 14:22:41 +1000

    Disable count in admin pagination

--- django/contrib/admin/views/main.py
+++ django/contrib/admin/views/main.py
@@ -170,16 +170,24 @@ class ChangeList:
 
     def get_results(self, request):
         paginator = self.model_admin.get_paginator(request, self.queryset, self.list_per_page)
-        # Get the number of objects, with admin filters applied.
-        result_count = paginator.count
 
-        # Get the total number of objects, with no admin filters applied.
-        if self.model_admin.show_full_result_count:
-            full_result_count = self.root_queryset.count()
-        else:
+        # Get the number of objects, with admin filters applied.
+        if self.model_admin.no_count:
+            result_count = None
             full_result_count = None
-        can_show_all = result_count <= self.list_max_show_all
-        multi_page = result_count > self.list_per_page
+            multi_page = True
+            can_show_all = False
+        else:
+            result_count = paginator.count
+
+            # Get the total number of objects, with no admin filters applied.
+            if self.model_admin.show_full_result_count:
+                full_result_count = self.root_queryset.count()
+            else:
+                full_result_count = None
+
+            can_show_all = result_count <= self.list_max_show_all
+            multi_page = result_count > self.list_per_page
 
         # Get the list of objects to display on this page.
         if (self.show_all and can_show_all) or not multi_page:
@@ -199,6 +207,7 @@ class ChangeList:
         self.result_list = result_list
         self.can_show_all = can_show_all
         self.multi_page = multi_page
+        self.no_count = self.model_admin.no_count
         self.paginator = paginator
 
     def _get_default_ordering(self):
