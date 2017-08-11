97cb3bd16d8e50d96eb184f8d67dd1723f776d2a
Nerl~ <nerlin57@gmail.com>
2017-08-11 22:27:25 +0300

    Fixed #28456 -- Allowed customizing Model pickling by overriding __getstate__().

--- tests/model_regress/test_pickle.py
+++ tests/model_regress/test_pickle.py
@@ -43,3 +43,20 @@ class ModelPickleTestCase(TestCase):
         msg = "Pickled model instance's Django version 1.0 does not match the current version %s." % get_version()
         with self.assertRaisesMessage(RuntimeWarning, msg):
             pickle.loads(pickle.dumps(p))
+
+    def test_with_getstate(self):
+        """
+        A model may override __getstate__() to choose the attributes to pickle.
+        """
+        class PickledModel(models.Model):
+            def __getstate__(self):
+                state = super().__getstate__().copy()
+                del state['dont_pickle']
+                return state
+
+        m = PickledModel()
+        m.dont_pickle = 1
+        dumped = pickle.dumps(m)
+        self.assertEqual(m.dont_pickle, 1)
+        reloaded = pickle.loads(dumped)
+        self.assertFalse(hasattr(reloaded, 'dont_pickle'))
