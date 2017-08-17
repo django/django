5848305218a08eb0c0a32086813d14dc6f46b463
Sebastian Sassi <ssassi@sophilabs.com>
2017-08-17 18:55:58 -0300

    Fixed #28082 -- Made BaseDateListView pass context from get_dated_items() to subclasses.
    Thanks leon-matthews for the report and fix.

--- django/views/generic/dates.py
+++ django/views/generic/dates.py
@@ -297,9 +297,11 @@ class BaseDateListView(MultipleObjectMixin, DateMixin, View):
 
     def get(self, request, *args, **kwargs):
         self.date_list, self.object_list, extra_context = self.get_dated_items()
-        context = self.get_context_data(object_list=self.object_list,
-                                        date_list=self.date_list)
-        context.update(extra_context)
+        context = self.get_context_data(
+            object_list=self.object_list,
+            date_list=self.date_list,
+            **extra_context
+        )
         return self.render_to_response(context)
 
     def get_dated_items(self):
