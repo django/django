e054580e101c4085b51cb4018cfd5e73d4f828ad
Mads Jensen <mje@inducks.org>
2017-08-26 15:21:04 +0200

    Refs #27869 -- Only non-null values in deconstruct kwargs.

--- django/contrib/postgres/indexes.py
+++ django/contrib/postgres/indexes.py
@@ -41,8 +41,10 @@ class GinIndex(Index):
 
     def deconstruct(self):
         path, args, kwargs = super().deconstruct()
-        kwargs['fastupdate'] = self.fastupdate
-        kwargs['gin_pending_list_limit'] = self.gin_pending_list_limit
+        if self.fastupdate is not None:
+            kwargs['fastupdate'] = self.fastupdate
+        if self.gin_pending_list_limit is not None:
+            kwargs['gin_pending_list_limit'] = self.gin_pending_list_limit
         return path, args, kwargs
 
     def create_sql(self, model, schema_editor, using=''):
