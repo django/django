68f0e8d8b1f9bdf9fc1c26ca5ec62a88c691ca20
caleb logan <clogan202@gmail.com>
2017-08-30 21:22:26 -0700

    Fixed #28500 -- Fixed crash in FileBasedCache._is_expired() if the cache file is empty.

--- tests/cache/tests.py
+++ tests/cache/tests.py
@@ -1366,6 +1366,13 @@ class FileBasedCacheTests(BaseCacheTests, TestCase):
             with self.assertRaises(IOError):
                 cache.get('foo')
 
+    def test_empty_cache_file_considered_expired(self):
+        cache_file = cache._key_to_file('foo')
+        with open(cache_file, 'wb') as fh:
+            fh.write(b'')
+        with open(cache_file, 'rb') as fh:
+            self.assertIs(cache._is_expired(fh), True)
+
 
 @override_settings(CACHES={
     'default': {
