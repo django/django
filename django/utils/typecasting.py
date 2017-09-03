2bb68a00055786b6cb46b5c718ffeb55995ab20b
Lynn Cyrin <firemagelynn@gmail.com>
2017-09-02 18:01:22 -0700

    update boolean type casting

--- django/utils/typecasting.py
+++ django/utils/typecasting.py
@@ -1,20 +1,42 @@
 from distutils.util import strtobool
 
+INVALID_TYPE_ERROR = "'{value}' could not be cast to a boolean value."
+
+
+def _force_cast_boolean_string(value):
+    if value == '':
+        return None
+    else:
+        return bool(strtobool(value))
+
+def _cast_boolean_string(value):
+    try:
+        return _force_cast_boolean_string(value)
+    except BaseException:
+        return bool(value)
+
 def cast_boolean(value):
+    '''
+    basic casting is used for form field inputs, where invalid input
+    should be passsed through, and exceptions ignored
+    '''
     if value in (0, 1):
         return bool(value)
-    try:
-        return bool(strtobool(value))
-    except BaseException:
+    elif isinstance(value, str):
+        return _cast_boolean_string(value)
+    else:
         return value
 
 def force_cast_boolean(value):
-    cast_value = cast_boolean(value)
-    if isinstance(cast_value, bool):
-        return cast_value
+    '''
+    forced casting is used for model field inputs, where invalid input
+    should raise an exception
+    '''
+    if isinstance(value, str):
+        return _force_cast_boolean_string(value)
+    elif isinstance(cast_boolean(value), bool):
+        return cast_boolean(value)
+    elif cast_boolean(value) == None:
+        return None
     else:
-        raise TypeError(
-            "'%(value)s' could not be cast to a boolean value.",
-            code='typeerror',
-            params={'value': value},
-        )
+        raise TypeError(INVALID_TYPE_ERROR.format(value=value))
--- tests/utils_tests/test_typecasting.py
+++ tests/utils_tests/test_typecasting.py
@@ -9,33 +9,61 @@ from django.test import SimpleTestCase
 
 class BooleanCastingTests(SimpleTestCase):
 
-    def test_int_casting(self):
-        self.assertIsInstance(cast_boolean(1), bool)
-        self.assertEqual(cast_boolean(1), True)
-        self.assertIsInstance(cast_boolean(0), bool)
-        self.assertEqual(cast_boolean(0), False)
-
-    def test_valid_string_casting(self):
+    def test_truthy_string_casting(self):
         self.assertIsInstance(cast_boolean('False'), bool)
-        self.assertEqual(cast_boolean('False'), False)
+        self.assertIs(cast_boolean('False'), False)
+
+    def test_string_casting_uses_strtobool(self):
         with patch('django.utils.typecasting.strtobool') as mock_strtobool:
             cast_boolean('False')
             mock_strtobool.assert_has_calls([call('False')])
 
-    def test_invalid_string_casting(self):
-        self.assertEqual(cast_boolean('kittens'), 'kittens')
+    def test_string_casting(self):
+        self.assertIs(cast_boolean('kittens'), True)
+
+    def test_empty_string_casting(self):
+        self.assertIs(cast_boolean(''), None)
+
+    def test_none_casting(self):
+        self.assertIs(cast_boolean(None), None)
+
+    def test_int_casting(self):
+        self.assertIsInstance(cast_boolean(1), bool)
+        self.assertIs(cast_boolean(1), True)
+        self.assertIsInstance(cast_boolean(0), bool)
+        self.assertIs(cast_boolean(0), False)
 
     def test_invalid_object_casting(self):
         self.assertEqual(cast_boolean([]), [])
 
-    def test_forced_valid_string_casting(self):
-        self.assertIsInstance(force_cast_boolean('True'), bool)
-        self.assertEqual(force_cast_boolean('True'), True)
 
-    def test_forced_invalid_string_casting(self):
-        with self.assertRaises(TypeError):
+class BooleanForcedCastingTests(SimpleTestCase):
+
+    def test_forced_truthy_string_casting(self):
+        self.assertIsInstance(force_cast_boolean('False'), bool)
+        self.assertIs(force_cast_boolean('False'), False)
+
+    def test_forced_string_casting_uses_strtobool(self):
+        with patch('django.utils.typecasting.strtobool') as mock_strtobool:
+            force_cast_boolean('False')
+            mock_strtobool.assert_has_calls([call('False')])
+
+    def test_forced_string_casting(self):
+        with self.assertRaises(BaseException):
             force_cast_boolean('kittens')
 
+    def test_forced_empty_string_casting(self):
+        self.assertIs(force_cast_boolean(''), None)
+
+    def test_forced_none_casting(self):
+        self.assertIs(force_cast_boolean(None), None)
+
+    def test_forced_int_casting(self):
+        self.assertIsInstance(force_cast_boolean(1), bool)
+        self.assertIs(force_cast_boolean(1), True)
+        self.assertIsInstance(force_cast_boolean(0), bool)
+        self.assertIs(force_cast_boolean(0), False)
+
     def test_forced_invalid_object_casting(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(BaseException):
             force_cast_boolean([])
