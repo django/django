a4092af3d33b2e7a5f05f6be3bdf8298d32d9ff8
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2017-09-10 21:26:06 +0200

    Removed unnecessary parens in _get_app_label_and_model_name().

--- django/db/migrations/state.py
+++ django/db/migrations/state.py
@@ -20,7 +20,7 @@ from .exceptions import InvalidBasesError
 def _get_app_label_and_model_name(model, app_label=''):
     if isinstance(model, str):
         split = model.split('.', 1)
-        return (tuple(split) if len(split) == 2 else (app_label, split[0]))
+        return tuple(split) if len(split) == 2 else (app_label, split[0])
     else:
         return model._meta.app_label, model._meta.model_name
 
