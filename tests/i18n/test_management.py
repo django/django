c0d968ea1fe8a564199048e4c0083bc559a037bb
Mads Jensen <mje@inducks.org>
2017-09-22 20:05:26 +0200

    Added a test for TranslatableFile.__eq__().

--- tests/i18n/test_management.py
+++ tests/i18n/test_management.py
@@ -11,3 +11,12 @@ class TranslatableFileTests(SimpleTestCase):
         file_name = 'example'
         trans_file = TranslatableFile(dirpath=dirpath, file_name=file_name, locale_dir=None)
         self.assertEqual(repr(trans_file), '<TranslatableFile: %s>' % os.path.join(dirpath, file_name))
+
+    def test_eq(self):
+        dirpath = 'dir'
+        file_name = 'example'
+        trans_file = TranslatableFile(dirpath=dirpath, file_name=file_name, locale_dir=None)
+        trans_file_eq = TranslatableFile(dirpath=dirpath, file_name=file_name, locale_dir=None)
+        trans_file_not_eq = TranslatableFile(dirpath='tmp', file_name=file_name, locale_dir=None)
+        self.assertEqual(trans_file, trans_file_eq)
+        self.assertNotEqual(trans_file, trans_file_not_eq)
