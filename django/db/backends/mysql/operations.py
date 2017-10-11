6dff90ee06222e5e4d12c5b9718aaa9c7702b8f5
messfish <messfish@users.noreply.github.com>
2017-10-10 17:03:37 -0700

    Update operations.py

--- django/db/backends/mysql/operations.py
+++ django/db/backends/mysql/operations.py
@@ -58,7 +58,7 @@ class DatabaseOperations(BaseDatabaseOperations):
 
     def _convert_field_to_tz(self, field_name, tzname):
         if settings.USE_TZ:
-            field_name = "CONVERT_TZ(%s, 'UTC', %s)" % (field_name, self.connection.timezone_name)
+            field_name = "CONVERT_TZ(%s, 'UTC', '%s')" % (field_name, self.connection.timezone_name)
         return field_name
 
     def datetime_cast_date_sql(self, field_name, tzname):
