a1044b4114d27d852732ff909115637fbeb64f79
Andrew Godwin <andrew@aeracode.org>
2017-10-17 00:08:08 -0700

    Start on a URL router

--- channels/routing.py
+++ channels/routing.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
+import re
+
 from channels.http import AsgiHandler
 
 
@@ -26,3 +28,44 @@ class ProtocolTypeRouter:
             return self.application_mapping[scope["type"]](scope)
         else:
             raise ValueError("No application configured for scope type %r" % scope["type"])
+
+
+class URLRouter:
+    """
+    Routes to different applications/consumers based on the URL path.
+
+    Works with anything that has a ``path`` key, but intended for WebSocket
+    and HTTP.
+    """
+
+    def __init__(self, routes):
+        self.routes = routes
+
+    def __call__(self, scope):
+        # Get the path
+        path = scope.get("path", None)
+        if path is None:
+            raise ValueError("No 'path' key in connection scope, cannot route URLs")
+        # Run through the routes we have until one matches
+        for route in self.routes:
+            match = route.match(path)
+            if match is not None:
+                return match(scope)
+        else:
+            raise ValueError("No route found for path %r." % path)
+
+
+class URLRoute:
+    """
+    Represents a URL based route to an application
+    """
+
+    def __init__(self, regex, application):
+        self.application = application
+        self.regex = re.compile(regex)
+
+    def match(self, url):
+        if self.regex.match(url.lstrip("/")):
+            return self.application
+
+route = URLRoute
