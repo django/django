a33620d31e53b76247795705dbaa732aec784e4b
Mauricio Cortazar <mauriciocm9@gmail.com>
2017-10-21 00:12:11 -0500

    Update utils.py

--- django/db/utils.py
+++ django/db/utils.py
@@ -81,10 +81,10 @@ class DatabaseErrorWrapper:
         ):
             db_exc_type = getattr(self.wrapper.Database, dj_exc_type.__name__)
             if issubclass(exc_type, db_exc_type):
-                if exc_value.diag.table_name:
+                try exc_value.diag.table_name:
                     argument, tb_name = exc_value.args + ("In the table '%s'" % exc_value.diag.table_name,)
                     dj_exc_value = dj_exc_type(argument + tb_name)
-                else:
+                except ProgrammingError:
                     dj_exc_value = dj_exc_type(*exc_value.args)
                 # Only set the 'errors_occurred' flag for errors that may make
                 # the connection unusable.
