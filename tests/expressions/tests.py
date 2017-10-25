f5184ae5dc370892d1b96576f68962b2c1c6c979
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2017-10-25 21:52:38 +0200

    [2.0.x] Fixed #28689 -- Fixed unquoted table names in Subquery SQL when using OuterRef.
    Regression in f48bc7c3dbd204eefb3c19016b1e4906ac26bee3.
    
    Backport of 81e357a7e19f35235cc998459a10213532727d4e from master

--- tests/expressions/tests.py
+++ tests/expressions/tests.py
@@ -545,6 +545,11 @@ class BasicExpressionsTests(TestCase):
         expr = FuncB(FuncA())
         self.assertEqual(expr.output_field, FuncA.output_field)
 
+    def test_outerref_mixed_case_table_name(self):
+        inner = Result.objects.filter(result_time__gte=OuterRef('experiment__assigned'))
+        outer = Result.objects.filter(pk__in=Subquery(inner.values('pk')))
+        self.assertFalse(outer.exists())
+
 
 class IterableLookupInnerExpressionsTests(TestCase):
     @classmethod
