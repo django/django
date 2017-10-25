django/django

commit f48bc7c3dbd204eefb3c19016b1e4906ac26bee3

Fixed #27862 -- Fixed incorrectly quoted table aliases in Subquery SQL.
Add aliases from resolved querysets to the parent query's external
aliases to prevent those aliases from being quoted.

Thanks to Vasily Stepanov for the report and Tim Graham for the review.

django/db/models/expressions.py
@@ -940,10 +940,15 @@ class Subquery(Expression):
 
         def resolve(child):
             if hasattr(child, 'resolve_expression'):
-                return child.resolve_expression(
+                resolved = child.resolve_expression(
                     query=query, allow_joins=allow_joins, reuse=reuse,
                     summarize=summarize, for_save=for_save,
                 )
+                # Add table alias to the parent query's aliases to prevent
+                # quoting.
+                if hasattr(resolved, 'alias'):
+                    clone.queryset.query.external_aliases.add(resolved.alias)
+                return resolved
             return child
 
         resolve_all(clone.queryset.query.where)

tests/expressions/tests.py
@@ -516,6 +516,15 @@ class BasicExpressionsTests(TestCase):
             [{'salary': 10, 'total_employees': 2300}, {'salary': 20, 'total_employees': 35}],
         )
 
+    def test_subquery_references_joined_table_twice(self):
+        inner = Company.objects.filter(
+            num_chairs__gte=OuterRef('ceo__salary'),
+            num_employees__gte=OuterRef('point_of_contact__salary'),
+        )
+        # Another contrived example (there is no need to have a subquery here)
+        outer = Company.objects.filter(pk__in=Subquery(inner.values('pk')))
+        self.assertFalse(outer.exists())
+
 
 class IterableLookupInnerExpressionsTests(TestCase):
     @classmethod

commit 474bfe9fb914709d122aafbf3212acad3435f6a4

Fixed #28689 -- Fixed unquoted table names in Subquery SQL when using OuterRef.
Regression in f48bc7c3dbd204eefb3c19016b1e4906ac26bee3.

django/db/models/expressions.py
@@ -985,7 +985,7 @@ class Subquery(Expression):
                 )
                 # Add table alias to the parent query's aliases to prevent
                 # quoting.
-                if hasattr(resolved, 'alias'):
+                if hasattr(resolved, 'alias') and resolved.alias != resolved.target.model._meta.db_table:
                     clone.queryset.query.external_aliases.add(resolved.alias)
                 return resolved
             return child

tests/expressions/models.py
@@ -50,6 +50,7 @@ class Experiment(models.Model):
     end = models.DateTimeField()
 
     class Meta:
+        db_table = 'expressions_ExPeRiMeNt'
         ordering = ('name',)
 
     def duration(self):

tests/expressions/tests.py
@@ -545,6 +545,11 @@ class BasicExpressionsTests(TestCase):
         expr = FuncB(FuncA())
         self.assertEqual(expr.output_field, FuncA.output_field)
 
+    def test_outerref_mixed_case_table_name(self):
+        inner = Result.objects.filter(result_time__gte=OuterRef('experiment__assigned'))
+        outer = Result.objects.filter(pk__in=Subquery(inner.values('pk')))
+        self.assertFalse(outer.exists())
+
 
 class IterableLookupInnerExpressionsTests(TestCase):
     @classmethod