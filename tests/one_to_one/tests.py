f2d5417d3b7a4012298fcffc6eab618cc09d0344
Paulo <commonzenpython@gmail.com>
2017-10-29 12:01:04 +0100

    [2.0.x] Fixed #28742 -- Fixed AttributeError crash when assigning None to cached reverse relations.
    Backport of fcfcf8aae470d893b0d2ef176434461edf9e9c4d from master

--- tests/one_to_one/tests.py
+++ tests/one_to_one/tests.py
@@ -196,6 +196,15 @@ class OneToOneTests(TestCase):
         # UndergroundBar.
         p.undergroundbar = None
 
+    def test_assign_none_to_null_cached_reverse_relation(self):
+        p = Place.objects.get(name='Demon Dogs')
+        # Prime the relation's cache with a value of None.
+        with self.assertRaises(Place.undergroundbar.RelatedObjectDoesNotExist):
+            getattr(p, 'undergroundbar')
+        # Assigning None works if there isn't a related UndergroundBar and the
+        # reverse cache has a value of None.
+        p.undergroundbar = None
+
     def test_related_object_cache(self):
         """ Regression test for #6886 (the related-object cache) """
 
