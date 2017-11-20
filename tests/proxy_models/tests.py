54e5c4a00e116ff4be257accdc9aa9e068c7f4ee
Yan Mitrofanov <mmxlviii@mail.ru>
2017-11-20 18:25:01 +0300

    Fixed #28820 -- Eliminated an extra query with QuerySet.update() on proxy models.

--- tests/proxy_models/tests.py
+++ tests/proxy_models/tests.py
@@ -276,6 +276,13 @@ class ProxyModelTests(TestCase):
         resp = [u.name for u in UserProxy.objects.all()]
         self.assertEqual(resp, ['Bruce'])
 
+    def test_proxy_update(self):
+        user = User.objects.create(name='Bruce')
+        with self.assertNumQueries(1):
+            UserProxy.objects.filter(id=user.id).update(name='George')
+        user.refresh_from_db()
+        self.assertEqual(user.name, 'George')
+
     def test_select_related(self):
         """
         We can still use `select_related()` to include related models in our
