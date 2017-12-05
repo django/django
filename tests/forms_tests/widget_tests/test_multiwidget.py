c1d57615ac60171a73e1922a48ebc27fe513357e
Nick Pope <nick.pope@flightdataservices.com>
2017-12-05 14:42:10 +0000

    [2.0.x] Fixed #28890 -- Removed newlines between MultiWidget's subwidgets.
    Regression in b52c73008a9d67e9ddbb841872dc15cdd3d6ee01.
    
    Backport of e014f91a70aa3ccdddb363a733c76e35597424fa from master

--- tests/forms_tests/widget_tests/test_multiwidget.py
+++ tests/forms_tests/widget_tests/test_multiwidget.py
@@ -166,6 +166,13 @@ class MultiWidgetTest(WidgetTest):
             """
         ))
 
+    def test_no_whitespace_between_widgets(self):
+        widget = MyMultiWidget(widgets=(TextInput, TextInput()))
+        self.check_html(widget, 'code', None, html=(
+            '<input type="text" name="code_0" />'
+            '<input type="text" name="code_1" />'
+        ), strict=True)
+
     def test_deepcopy(self):
         """
         MultiWidget should define __deepcopy__() (#12048).
