1f0813ca7e8a15d10fcdaad1a5d66b7eec28dedb
Nick Pope <nick.pope@flightdataservices.com>
2017-12-05 09:52:21 +0000

    Fixed string format specifier for fillfactor in GistIndex.

--- django/contrib/postgres/indexes.py
+++ django/contrib/postgres/indexes.py
@@ -96,5 +96,5 @@ class GistIndex(PostgresIndex):
         if self.buffering is not None:
             with_params.append('buffering = %s' % ('on' if self.buffering else 'off'))
         if self.fillfactor is not None:
-            with_params.append('fillfactor = %s' % self.fillfactor)
+            with_params.append('fillfactor = %d' % self.fillfactor)
         return with_params
