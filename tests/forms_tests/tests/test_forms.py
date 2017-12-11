7c7bc6391a3e83566f9ace59955e63503bc76cee
Daniil <daniilr@users.noreply.github.com>
2017-12-11 22:30:47 +1000

    Fixed #28874 -- Prevented double escaping of errors on hidden form fields.

--- tests/forms_tests/tests/test_forms.py
+++ tests/forms_tests/tests/test_forms.py
@@ -3398,6 +3398,27 @@ Good luck picking a username that doesn&#39;t already exist.</p>
 <div class="errorlist"><div class="error">This field is required.</div></div>
 <p>Comment: <input type="text" name="comment" required /></p>""")
 
+    def test_error_escaping(self):
+        class TestForm(Form):
+            hidden = CharField(widget=HiddenInput(), required=False)
+            visible = CharField()
+
+            def clean_hidden(self):
+                raise ValidationError('Foo & "bar"!')
+
+            clean_visible = clean_hidden
+
+        form = TestForm({'hidden': 'a', 'visible': 'b'})
+        form.is_valid()
+        self.assertHTMLEqual(
+            form.as_ul(),
+            '<li><ul class="errorlist nonfield"><li>(Hidden field hidden) Foo &amp; &quot;bar&quot;!</li></ul></li>'
+            '<li><ul class="errorlist"><li>Foo &amp; &quot;bar&quot;!</li></ul>'
+            '<label for="id_visible">Visible:</label> '
+            '<input type="text" name="visible" value="b" id="id_visible" required />'
+            '<input type="hidden" name="hidden" value="a" id="id_hidden" /></li>'
+        )
+
     def test_baseform_repr(self):
         """
         BaseForm.__repr__() should contain some basic information about the
