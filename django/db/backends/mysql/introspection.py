5c20251e1ed47fcfaf6097dd790ef88657b9f428
Nick Pope <nick.pope@flightdataservices.com>
2017-12-12 09:17:59 +0000

    fixup! Refs #28909 -- Use unpacking generalizations in more cases.

--- django/db/backends/mysql/introspection.py
+++ django/db/backends/mysql/introspection.py
@@ -85,16 +85,16 @@ class DatabaseIntrospection(BaseDatabaseIntrospection):
 
         fields = []
         for line in cursor.description:
-            col_name = line[0]
+            info = field_info[line[0]]
             fields.append(FieldInfo(
                 *line[:3],
-                to_int(field_info[col_name].max_len) or line[3],
-                to_int(field_info[col_name].num_prec) or line[4],
-                to_int(field_info[col_name].num_scale) or line[5],
+                to_int(info.max_len) or line[3],
+                to_int(info.num_prec) or line[4],
+                to_int(info.num_scale) or line[5],
                 line[6],
-                field_info[col_name].column_default,
-                field_info[col_name].extra,
-                field_info[col_name].is_unsigned,
+                info.column_default,
+                info.extra,
+                info.is_unsigned,
             ))
         return fields
 
