d92f899cb5708f7309dda4fa9e0a17a660ccf9ee
Tim Graham <timograham@gmail.com>
2018-01-08 10:00:21 -0500

    [1.8.x] Fixed GeoIP test failure with the latest data.

--- tests/gis_tests/test_geoip.py
+++ tests/gis_tests/test_geoip.py
@@ -114,7 +114,7 @@ class GeoIPTest(unittest.TestCase):
         "Testing that GeoIP strings are properly encoded, see #16553."
         g = GeoIP()
         d = g.city("messe-duesseldorf.com")
-        self.assertEqual('Düsseldorf', d['city'])
+        self.assertEqual('Essen', d['city'])
         d = g.country('200.26.205.1')
         # Some databases have only unaccented countries
         self.assertIn(d['country_name'], ('Curaçao', 'Curacao'))
