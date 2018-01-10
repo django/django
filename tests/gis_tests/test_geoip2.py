66d74676e23c32bc676fb0706af8580b391953b6
Tim Graham <timograham@gmail.com>
2018-01-10 09:39:41 -0500

    Fixed a GeoIP2 test failure with the latest GeoIP2 database.

--- tests/gis_tests/test_geoip2.py
+++ tests/gis_tests/test_geoip2.py
@@ -134,10 +134,10 @@ class GeoIPTest(unittest.TestCase):
     @mock.patch('socket.gethostbyname')
     def test05_unicode_response(self, gethostbyname):
         "GeoIP strings should be properly encoded (#16553)."
-        gethostbyname.return_value = '194.27.42.76'
+        gethostbyname.return_value = '191.252.51.69'
         g = GeoIP2()
-        d = g.city("nigde.edu.tr")
-        self.assertEqual('Niğde', d['city'])
+        d = g.city('www.fasano.com.br')
+        self.assertEqual(d['city'], 'São José dos Campos')
         d = g.country('200.26.205.1')
         # Some databases have only unaccented countries
         self.assertIn(d['country_name'], ('Curaçao', 'Curacao'))
