44571f0e7f1f2081b354d2f8028f575deed28f8b
Simon Charette <charette.s@gmail.com>
2018-01-17 01:26:39 -0500

    Stopped copying _prefetch_related_lookups on QuerySet._clone().
    QuerySet._prefetch_related_lookups is a tuple and thus immutable anyway.

--- django/db/models/query.py
+++ django/db/models/query.py
@@ -1160,7 +1160,7 @@ class QuerySet:
         c = self.__class__(model=self.model, query=self.query.chain(), using=self._db, hints=self._hints)
         c._sticky_filter = self._sticky_filter
         c._for_write = self._for_write
-        c._prefetch_related_lookups = self._prefetch_related_lookups[:]
+        c._prefetch_related_lookups = self._prefetch_related_lookups
         c._known_related_objects = self._known_related_objects
         c._iterable_class = self._iterable_class
         c._fields = self._fields
