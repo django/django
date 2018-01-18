90ca9412e4d2f0b543938a1fa3fbc43c2c7ac57f
Jon Dufresne <jon.dufresne@gmail.com>
2018-01-18 08:23:06 -0800

    Removed unnecessary microsecond truncation in SplitDateTimeWidget.
    The microseconds are already truncated by the TimeInput subwidget.

--- django/forms/widgets.py
+++ django/forms/widgets.py
@@ -888,7 +888,7 @@ class SplitDateTimeWidget(MultiWidget):
     def decompress(self, value):
         if value:
             value = to_current_timezone(value)
-            return [value.date(), value.time().replace(microsecond=0)]
+            return [value.date(), value.time()]
         return [None, None]
 
 
