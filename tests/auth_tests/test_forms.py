57b95fedad5e0b83fc9c81466b7d1751c6427aae
Tim Graham <timograham@gmail.com>
2018-01-23 13:20:18 -0500

    [1.11.x] Fixed CVE-2018-6188 -- Fixed information leakage in AuthenticationForm.
    Reverted 359370a8b8ca0efe99b1d4630b291ec060b69225 (refs #28645).
    
    This is a security fix.

--- tests/auth_tests/test_forms.py
+++ tests/auth_tests/test_forms.py
@@ -249,6 +249,9 @@ class UserCreationFormTest(TestDataMixin, TestCase):
         )
 
 
+# To verify that the login form rejects inactive users, use an authentication
+# backend that allows them.
+@override_settings(AUTHENTICATION_BACKENDS=['django.contrib.auth.backends.AllowAllUsersModelBackend'])
 class AuthenticationFormTest(TestDataMixin, TestCase):
 
     def test_invalid_username(self):
@@ -277,6 +280,24 @@ class AuthenticationFormTest(TestDataMixin, TestCase):
         form = AuthenticationForm(None, data)
         self.assertFalse(form.is_valid())
         self.assertEqual(form.non_field_errors(), [force_text(form.error_messages['inactive'])])
+
+    # Use an authentication backend that rejects inactive users.
+    @override_settings(AUTHENTICATION_BACKENDS=['django.contrib.auth.backends.ModelBackend'])
+    def test_inactive_user_incorrect_password(self):
+        """An invalid login doesn't leak the inactive status of a user."""
+        data = {
+            'username': 'inactive',
+            'password': 'incorrect',
+        }
+        form = AuthenticationForm(None, data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(
+            form.non_field_errors(), [
+                form.error_messages['invalid_login'] % {
+                    'username': User._meta.get_field('username').verbose_name
+                }
+            ]
+        )
 
     def test_login_failed(self):
         signal_calls = []
