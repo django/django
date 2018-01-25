d0b44c44ef6ac8b6a7e332b83b9932f23dee41fd
Roger Gammans <rgammans@gammascience.co.uk>
2018-01-25 12:14:44 +0000

    Fixed #29060 -- Made {% firstof %} assign '' to the asvar if all inputs are false.

--- django/template/defaulttags.py
+++ django/template/defaulttags.py
@@ -118,15 +118,16 @@ class FirstOfNode(Node):
         self.asvar = asvar
 
     def render(self, context):
+        first = ''
         for var in self.vars:
             value = var.resolve(context, ignore_failures=True)
             if value:
                 first = render_value_in_context(value, context)
-                if self.asvar:
-                    context[self.asvar] = first
-                    return ''
-                return first
-        return ''
+                break
+        if self.asvar:
+            context[self.asvar] = first
+            return ''
+        return first
 
 
 class ForNode(Node):
