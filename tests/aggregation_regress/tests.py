django/django

commit 4dfd6b88d520b43b6363946e5ee58ba14cd1efe6

Refs #28459 -- Improved performance of FlatValuesListIterable.

django/db/models/query.py
@@ -8,6 +8,7 @@ import sys
 import warnings
 from collections import OrderedDict, deque
 from contextlib import suppress
+from itertools import chain
 
 from django.conf import settings
 from django.core import exceptions
@@ -145,8 +146,7 @@ class FlatValuesListIterable(BaseIterable):
     def __iter__(self):
         queryset = self.queryset
         compiler = queryset.query.get_compiler(queryset.db)
-        for row in compiler.results_iter():
-            yield row[0]
+        return chain.from_iterable(compiler.results_iter())
 
 
 class QuerySet:
@@ -302,6 +302,9 @@ class QuerySet:
     # METHODS THAT DO DATABASE QUERIES #
     ####################################
 
+    def _iterator(self, use_chunked_fetch, chunk_size):
+        yield from self._iterable_class(self, chunked_fetch=use_chunked_fetch, chunk_size=chunk_size)
+
     def iterator(self, chunk_size=2000):
         """
         An iterator over the results from applying this QuerySet to the
@@ -310,7 +313,7 @@ class QuerySet:
         if chunk_size <= 0:
             raise ValueError('Chunk size must be strictly positive.')
         use_chunked_fetch = not connections[self.db].settings_dict.get('DISABLE_SERVER_SIDE_CURSORS')
-        return iter(self._iterable_class(self, chunked_fetch=use_chunked_fetch, chunk_size=chunk_size))
+        return self._iterator(use_chunked_fetch, chunk_size)
 
     def aggregate(self, *args, **kwargs):
         """

commit 61c74ae74f6e23e5c6468c1bc2adb348c60cc289

[2.0.x] Fixed #29067 -- Fixed regression in QuerySet.values_list(..., flat=True) followed by annotate().
Regression in 4dfd6b88d520b43b6363946e5ee58ba14cd1efe6.

Backport of 3187c89d6f8c60ca7e78093d5b37e0709e71cea9 from master

django/db/models/query.py
@@ -7,7 +7,6 @@ import operator
 import warnings
 from collections import OrderedDict, namedtuple
 from functools import lru_cache
-from itertools import chain
 
 from django.conf import settings
 from django.core import exceptions
@@ -176,7 +175,8 @@ class FlatValuesListIterable(BaseIterable):
     def __iter__(self):
         queryset = self.queryset
         compiler = queryset.query.get_compiler(queryset.db)
-        return chain.from_iterable(compiler.results_iter(chunked_fetch=self.chunked_fetch, chunk_size=self.chunk_size))
+        for row in compiler.results_iter(chunked_fetch=self.chunked_fetch, chunk_size=self.chunk_size):
+            yield row[0]
 
 
 class QuerySet:

tests/aggregation_regress/tests.py
@@ -1475,6 +1475,11 @@ class AggregationTests(TestCase):
         vals2 = Book.objects.aggregate(result=Sum('rating') - Value(4.0))
         self.assertEqual(vals1, vals2)
 
+    def test_annotate_values_list_flat(self):
+        """Find ages that are shared by at least two authors."""
+        qs = Author.objects.values_list('age', flat=True).annotate(age_count=Count('age')).filter(age_count__gt=1)
+        self.assertSequenceEqual(qs, [29])
+
 
 class JoinPromotionTests(TestCase):
     def test_ticket_21150(self):