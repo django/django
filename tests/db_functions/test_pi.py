25defb0c25edd0f92a255b5fc586ef92b7379bcd
jjy <jjy@localhost.localdomain>
2018-02-01 20:58:07 -0500

    reformat tests

--- tests/db_functions/test_pi.py
+++ tests/db_functions/test_pi.py
@@ -3,12 +3,12 @@ import math
 from django.db.models.functions import Pi
 from django.test import TestCase
 
-from ..models import FloatModel
+from .models import FloatModel
 
 
 class PiTests(TestCase):
 
-    def test(self):
+    def test_Pi(self):
         FloatModel.objects.create(f1=2.5, f2=15.9)
         obj = FloatModel.objects.annotate(pi=Pi()).first()
         self.assertAlmostEqual(obj.pi, math.pi, places=5)
