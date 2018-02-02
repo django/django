fcf86864b39bb67a777068cdac57f43b2362205c
Andrew Godwin <andrew@aeracode.org>
2018-02-01 23:20:57 -0800

    Remove Python 2.7 classifiers

--- setup.py
+++ setup.py
@@ -32,11 +32,9 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
     ],
 )
