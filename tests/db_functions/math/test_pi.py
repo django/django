a9e15e383db4aac912e51f89b0b05bb089572136
jjy <jjy@localhost.localdomain>
2018-02-01 20:15:22 -0500

    update tests and 2.1.txt

--- tests/db_functions/math/test_pi.py
+++ tests/db_functions/math/test_pi.py
@@ -3,7 +3,11 @@ import math
 from django.db.models.functions import Pi
 from django.test import TestCase
 
+<<<<<<< 6481064d067f4644786d98777438ff7f05dc8fb9:tests/db_functions/math/test_pi.py
 from ..models import FloatModel
+=======
+from .models import FloatModel
+>>>>>>> update tests and 2.1.txt:tests/db_functions/test_pi.py
 
 
 class PiTests(TestCase):
