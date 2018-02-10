1a1264f1494976c562c7cb832fe47f3e1e765b8f
Manatsawin Hanmongkolchai <manatsawin@gmail.com>
2018-02-11 04:05:41 +0700

    Fixed #29109 -- Fixed the admin time picker widget for the Thai locale.

--- tests/i18n/tests.py
+++ tests/i18n/tests.py
@@ -3,6 +3,7 @@ import decimal
 import gettext as gettext_module
 import os
 import pickle
+import re
 from contextlib import contextmanager
 from importlib import import_module
 from threading import local
@@ -1094,6 +1095,22 @@ class FormattingTests(SimpleTestCase):
         with self.settings(FORMAT_MODULE_PATH='i18n.other.locale'):
             with translation.override('fr', deactivate=True):
                 self.assertEqual('d/m/Y CUSTOM', get_format('CUSTOM_DAY_FORMAT'))
+
+    def test_admin_javascript_supported_input_formats(self):
+        """
+        The first input format for DATE_INPUT_FORMATS, TIME_INPUT_FORMATS, and
+        DATETIME_INPUT_FORMATS must not contain %f since that's unsupported by
+        the admin's time picker widget.
+        """
+        regex = re.compile('%([^BcdHImMpSwxXyY%])')
+        for language_code, language_name in settings.LANGUAGES:
+            for format_name in ('DATE_INPUT_FORMATS', 'TIME_INPUT_FORMATS', 'DATETIME_INPUT_FORMATS'):
+                with self.subTest(language=language_code, format=format_name):
+                    formatter = get_format(format_name, lang=language_code)[0]
+                    self.assertEqual(
+                        regex.findall(formatter), [],
+                        "%s locale's %s uses an unsupported format code." % (language_code, format_name)
+                    )
 
 
 class MiscTests(SimpleTestCase):
