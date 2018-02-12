aeb35548dc3a669aebec45f8f51a9cd3fbfc8801
Tim Graham <timograham@gmail.com>
2018-02-12 14:00:29 -0500

    [2.0.x] Fixed #29125 -- Made Q.deconstruct() deterministic with multiple keyword arguments.
    Backport of b95c49c954e3b75678bb258e9fb2ec30d0d960bb from master

--- tests/queries/test_q.py
+++ tests/queries/test_q.py
@@ -60,6 +60,15 @@ class QTests(SimpleTestCase):
         ))
         self.assertEqual(kwargs, {'_connector': 'AND'})
 
+    def test_deconstruct_multiple_kwargs(self):
+        q = Q(price__gt=F('discounted_price'), price=F('discounted_price'))
+        path, args, kwargs = q.deconstruct()
+        self.assertEqual(args, (
+            ('price', F('discounted_price')),
+            ('price__gt', F('discounted_price')),
+        ))
+        self.assertEqual(kwargs, {'_connector': 'AND'})
+
     def test_deconstruct_nested(self):
         q = Q(Q(price__gt=F('discounted_price')))
         path, args, kwargs = q.deconstruct()
