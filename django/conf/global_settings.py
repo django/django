f142a8ac1fea3e3e72b4e7283f464ac015397bbe
Tim Graham <timograham@gmail.com>
2018-02-24 10:22:31 -0500

    Fixed #29161 -- Removed BCryptPasswordHasher from PASSWORD_HASHERS.

--- django/conf/global_settings.py
+++ django/conf/global_settings.py
@@ -512,7 +512,6 @@ PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
     'django.contrib.auth.hashers.Argon2PasswordHasher',
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-    'django.contrib.auth.hashers.BCryptPasswordHasher',
 ]
 
 AUTH_PASSWORD_VALIDATORS = []
