97b7dd59bb8d6163239061e2022153c3415d146d
Tim Graham <timograham@gmail.com>
2018-02-24 16:22:43 -0500

    Fixed CVE-2018-7537 -- Fixed catastrophic backtracking in django.utils.text.Truncator.
    Thanks James Davis for suggesting the fix.

--- tests/utils_tests/test_text.py
+++ tests/utils_tests/test_text.py
@@ -136,6 +136,10 @@ class TestUtilsText(SimpleTestCase):
         truncator = text.Truncator('<p>I &lt;3 python, what about you?</p>')
         self.assertEqual('<p>I &lt;3 python...</p>', truncator.words(3, '...', html=True))
 
+        re_tag_catastrophic_test = ('</a' + '\t' * 50000) + '//>'
+        truncator = text.Truncator(re_tag_catastrophic_test)
+        self.assertEqual(re_tag_catastrophic_test, truncator.words(500, html=True))
+
     def test_wrap(self):
         digits = '1234 67 9'
         self.assertEqual(text.wrap(digits, 100), '1234 67 9')
