a1a3e515616da102fc48a1e1af8a5b2f429f747e
Alex Tomic <atomic777@gmail.com>
2018-03-02 12:25:08 -0500

    Fixed #29133 -- Fixed call_command() crash if a required option is passed in options.

--- tests/user_commands/tests.py
+++ tests/user_commands/tests.py
@@ -194,6 +194,18 @@ class CommandTests(SimpleTestCase):
         with self.assertRaisesMessage(TypeError, msg):
             management.call_command('dance', unrecognized=1, unrecognized2=1)
 
+    def test_call_command_with_required_parameters_in_options(self):
+        out = StringIO()
+        management.call_command('required_option', need_me='foo', needme2='bar', stdout=out)
+        self.assertIn('need_me', out.getvalue())
+        self.assertIn('needme2', out.getvalue())
+
+    def test_call_command_with_required_parameters_in_mixed_options(self):
+        out = StringIO()
+        management.call_command('required_option', '--need-me=foo', needme2='bar', stdout=out)
+        self.assertIn('need_me', out.getvalue())
+        self.assertIn('needme2', out.getvalue())
+
 
 class CommandRunTests(AdminScriptTestCase):
     """
