ea385c873a0d84d21af2d402212a99841742c4e3
ovalseven8 <ovalseven8@users.noreply.github.com>
2018-03-04 18:47:58 +0100

    Update models.py
    Because User is an AbstractUser the required fields are the same.

--- django/contrib/auth/models.py
+++ django/contrib/auth/models.py
@@ -358,7 +358,7 @@ class User(AbstractUser):
     Users within the Django authentication system are represented by this
     model.
 
-    Username, password and email are required. Other fields are optional.
+    Username and password are required. Other fields are optional.
     """
     class Meta(AbstractUser.Meta):
         swappable = 'AUTH_USER_MODEL'
