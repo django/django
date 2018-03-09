445269c359e41808537531ad7d00083679ec527f
Matthaus Woolard <matthaus.woolard@gmail.com>
2018-03-09 16:20:20 +0300

    Add login, logout for scope based operations (#923)
    Provides async login and logout functions and improves get_user code. Fixes #917.

--- channels/auth.py
+++ channels/auth.py
@@ -1,16 +1,126 @@
-from django.contrib import auth
+from django.conf import settings
+from django.contrib.auth import (
+    BACKEND_SESSION_KEY, HASH_SESSION_KEY, SESSION_KEY, _get_backends, get_user_model, load_backend, user_logged_in,
+    user_logged_out,
+)
+from django.contrib.auth.models import AnonymousUser
+from django.utils.crypto import constant_time_compare
 from django.utils.functional import SimpleLazyObject
+from django.utils.translation import LANGUAGE_SESSION_KEY
 
+from asgiref.sync import async_to_sync
+from channels.db import database_sync_to_async
 from channels.sessions import CookieMiddleware, SessionMiddleware
 
 
+@database_sync_to_async
 def get_user(scope):
-    if "_cached_user" not in scope:
-        # We need to fake a request so the auth code works until we get to
-        # refactor it to take sessions, not requests.
-        fake_request = type("FakeRequest", (object, ), {"session": scope["session"]})
-        scope["_cached_user"] = auth.get_user(fake_request)
-    return scope["_cached_user"]
+    """
+    Return the user model instance associated with the given scope.
+    If no user is retrieved, return an instance of `AnonymousUser`.
+    """
+    if "session" not in scope:
+        raise ValueError("Cannot find session in scope. You should wrap your consumer in SessionMiddleware.")
+    session = scope["session"]
+    user = None
+    try:
+        user_id = _get_user_session_key(session)
+        backend_path = session[BACKEND_SESSION_KEY]
+    except KeyError:
+        pass
+    else:
+        if backend_path in settings.AUTHENTICATION_BACKENDS:
+            backend = load_backend(backend_path)
+            user = backend.get_user(user_id)
+            # Verify the session
+            if hasattr(user, "get_session_auth_hash"):
+                session_hash = session.get(HASH_SESSION_KEY)
+                session_hash_verified = session_hash and constant_time_compare(
+                    session_hash,
+                    user.get_session_auth_hash()
+                )
+                if not session_hash_verified:
+                    session.flush()
+                    user = None
+    return user or AnonymousUser()
+
+
+@database_sync_to_async
+def login(scope, user, backend=None):
+    """
+    Persist a user id and a backend in the request.
+    This way a user doesn't have to re-authenticate on every request.
+    Note that data set during the anonymous session is retained when the user logs in.
+    """
+    if "session" not in scope:
+        raise ValueError("Cannot find session in scope. You should wrap your consumer in SessionMiddleware.")
+    session = scope["session"]
+    session_auth_hash = ""
+    if user is None:
+        user = scope.get("user", None)
+    if user is None:
+        raise ValueError("User must be passed as an argument or must be present in the scope.")
+    if hasattr(user, "get_session_auth_hash"):
+        session_auth_hash = user.get_session_auth_hash()
+    if SESSION_KEY in session:
+        if _get_user_session_key(session) != user.pk or (
+                session_auth_hash and not
+                constant_time_compare(session.get(HASH_SESSION_KEY, ""), session_auth_hash)):
+            # To avoid reusing another user's session, create a new, empty
+            # session if the existing session corresponds to a different
+            # authenticated user.
+            session.flush()
+    else:
+        session.cycle_key()
+    try:
+        backend = backend or user.backend
+    except AttributeError:
+        backends = _get_backends(return_tuples=True)
+        if len(backends) == 1:
+            _, backend = backends[0]
+        else:
+            raise ValueError(
+                "You have multiple authentication backends configured and therefore must provide the `backend` "
+                "argument or set the `backend` attribute on the user."
+            )
+    session[SESSION_KEY] = user._meta.pk.value_to_string(user)
+    session[BACKEND_SESSION_KEY] = backend
+    session[HASH_SESSION_KEY] = session_auth_hash
+    scope["user"] = user
+    # note this does not reset the CSRF_COOKIE/Token
+    user_logged_in.send(sender=user.__class__, request=None, user=user)
+
+
+@database_sync_to_async
+def logout(scope):
+    """
+    Remove the authenticated user's ID from the request and flush their session data.
+    """
+    if "session" not in scope:
+        raise ValueError(
+            "Login cannot find session in scope. You should wrap your consumer in SessionMiddleware."
+        )
+    session = scope["session"]
+    # Dispatch the signal before the user is logged out so the receivers have a
+    # chance to find out *who* logged out.
+    user = scope.get("user", None)
+    if hasattr(user, "is_authenticated") and not user.is_authenticated:
+        user = None
+    if user is not None:
+        user_logged_out.send(sender=user.__class__, request=None, user=user)
+    # remember language choice saved to session
+    language = session.get(LANGUAGE_SESSION_KEY)
+    session.flush()
+    if language is not None:
+        session[LANGUAGE_SESSION_KEY] = language
+    if "user" in scope:
+        scope["user"] = AnonymousUser()
+
+
+def _get_user_session_key(session):
+    # This value in the session is always serialized to a string, so we need
+    # to convert it back to Python whenever we access it.
+    return get_user_model()._meta.pk.to_python(session[SESSION_KEY])
 
 
 class AuthMiddleware:
@@ -28,7 +138,7 @@ class AuthMiddleware:
             raise ValueError("AuthMiddleware cannot find session in scope. SessionMiddleware must be above it.")
         # Add it to the scope if it's not there already
         if "user" not in scope:
-            scope["user"] = SimpleLazyObject(lambda: get_user(scope))
+            scope["user"] = SimpleLazyObject(lambda: async_to_sync(get_user)(scope))
         # Pass control to inner application
         return self.inner(scope)
 
---
+++ tests/security/test_auth.py
@@ -0,0 +1,249 @@
+from importlib import import_module
+from unittest import mock
+
+import pytest
+from django.conf import settings
+from django.contrib.auth import (
+    BACKEND_SESSION_KEY, HASH_SESSION_KEY, SESSION_KEY, get_user_model, user_logged_in, user_logged_out,
+)
+from django.contrib.auth.models import AnonymousUser
+
+from asgiref.sync import sync_to_async
+from channels.auth import get_user, login, logout
+from channels.db import database_sync_to_async
+
+
+class CatchSignal:
+    """
+    Capture (and detect) a django signal event.
+    This should be used as a Contextmanager.
+
+    :Example:
+
+    with CatchSignal(user_logged_in) as handler:
+        # do the django action here that will create the signal
+        assert handler.called
+
+
+    :Async Example:
+
+    async with CatchSignal(user_logged_in) as handler:
+        await ... # the django action the creates the signal
+         assert handler.called
+    """
+    def __init__(self, signal):
+        self.handler = mock.Mock()
+        self.signal = signal
+
+    async def __aenter__(self):
+        await sync_to_async(self.signal.connect)(self.handler)
+        return self.handler
+
+    async def __aexit__(self, exc_type, exc, tb):
+        await sync_to_async(self.signal.disconnect)(self.handler)
+
+    def __enter__(self):
+        self.signal.connect(self.handler)
+        return self.handler
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.signal.disconnect(self.handler)
+
+
+@pytest.fixture
+def user_bob():
+    return get_user_model().objects.create(username="bob", email="bob@example.com")
+
+
+@pytest.fixture
+def user_bill():
+    return get_user_model().objects.create(username="bill", email="bill@example.com")
+
+
+@pytest.fixture
+def session():
+    SessionStore = import_module(settings.SESSION_ENGINE).SessionStore
+    session = SessionStore()
+    session.create()
+    return session
+
+
+async def assert_is_logged_in(scope, user):
+    """
+    Assert that the provided user is logged in to the session contained within the scope.
+    """
+    assert "user" in scope
+    assert scope["user"] == user
+    session = scope["session"]
+
+    # logged in!
+    assert SESSION_KEY in session
+    assert BACKEND_SESSION_KEY in session
+    assert HASH_SESSION_KEY in session
+
+    assert isinstance(await get_user(scope), await database_sync_to_async(get_user_model)())
+    assert await get_user(scope) == user
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def test_login_no_session_in_scope():
+    """
+    Test to ensure that a `ValueError` is raised if when tying to login a user to a scope that has no session.
+    """
+
+    with pytest.raises(
+            ValueError,
+            match="Cannot find session in scope. You should wrap your consumer in SessionMiddleware."
+            ):
+        await login(scope={}, user=None)
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def test_login_no_user_in_scope(session):
+    """
+    Test the login method to ensure it raises a `ValueError` if no user is passed and is no user in the scope.
+    """
+    scope = {
+        "session": session
+    }
+
+    with pytest.raises(ValueError, match="User must be passed as an argument or must be present in the scope."):
+        await login(scope, user=None)
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def test_login_user_as_argument(session, user_bob):
+    """
+    Test that one can login to a scope that has a session by passing the scope and user as arguments to the login
+     function.
+    """
+    scope = {
+        "session": session
+    }
+
+    assert isinstance(await get_user(scope), AnonymousUser)
+    # not logged in
+    assert SESSION_KEY not in session
+
+    async with CatchSignal(user_logged_in) as handler:
+        assert not handler.called
+        await login(scope, user=user_bob)
+        assert handler.called
+
+    await assert_is_logged_in(scope, user_bob)
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def test_login_user_on_scope(session, user_bob):
+    """
+    Test that in the absence of a user being passed to the `login` function the function will use the user set on the
+     scope.
+    """
+    scope = {
+        "session": session,
+        "user": user_bob
+    }
+
+    # check that we are not logged in on the session
+    assert isinstance(await get_user(scope), AnonymousUser)
+
+    async with CatchSignal(user_logged_in) as handler:
+        assert not handler.called
+        await login(scope, user=None)
+        assert handler.called
+
+    await assert_is_logged_in(scope, user_bob)
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def test_login_change_user(session, user_bob, user_bill):
+    """
+    Test logging in a second user into a scope were another user is already logged in.
+    """
+    scope = {
+        "session": session,
+    }
+
+    # check that we are not logged in on the session
+    assert isinstance(await get_user(scope), AnonymousUser)
+
+    async with CatchSignal(user_logged_in) as handler:
+        assert not handler.called
+        await login(scope, user=user_bob)
+        assert handler.called
+
+    await assert_is_logged_in(scope, user_bob)
+
+    session_key = session[SESSION_KEY]
+    assert session_key
+
+    async with CatchSignal(user_logged_in) as handler:
+        assert not handler.called
+        await login(scope, user=user_bill)
+        assert handler.called
+
+    await assert_is_logged_in(scope, user_bill)
+
+    assert session_key != session[SESSION_KEY]
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def test_logout(session, user_bob):
+    """
+    Test that one can logout a user from a logged in session.
+    """
+    scope = {
+        "session": session,
+    }
+
+    # check that we are not logged in on the session
+    assert isinstance(await get_user(scope), AnonymousUser)
+
+    async with CatchSignal(user_logged_in) as handler:
+        assert not handler.called
+        await login(scope, user=user_bob)
+        assert handler.called
+
+    await assert_is_logged_in(scope, user_bob)
+
+    assert SESSION_KEY in session
+    session_key = session[SESSION_KEY]
+    assert session_key
+
+    async with CatchSignal(user_logged_out) as handler:
+        assert not handler.called
+        await logout(scope)
+        assert handler.called
+
+    assert isinstance(await get_user(scope), AnonymousUser)
+    assert isinstance(scope["user"], AnonymousUser)
+
+    assert SESSION_KEY not in session
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def test_logout_not_logged_in(session):
+    """
+    Test that the `logout` function does nothing in the case were there is no user logged in.
+    """
+    scope = {
+        "session": session,
+    }
+
+    # check that we are not logged in on the session
+    assert isinstance(await get_user(scope), AnonymousUser)
+
+    async with CatchSignal(user_logged_out) as handler:
+        assert not handler.called
+        await logout(scope)
+        assert not handler.called
+
+    assert "user" not in scope
+    assert isinstance(await get_user(scope), AnonymousUser)
