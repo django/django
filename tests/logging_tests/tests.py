fb8fd535c0f47cffb4da0c5900f3f66e1ec8d432
Tim Graham <timograham@gmail.com>
2018-03-14 09:38:16 -0400

    Moved SchemaLoggingTest to tests/schema.

--- tests/logging_tests/tests.py
+++ tests/logging_tests/tests.py
@@ -8,7 +8,6 @@ from django.conf import settings
 from django.core import mail
 from django.core.files.temp import NamedTemporaryFile
 from django.core.management import color
-from django.db import connection
 from django.test import RequestFactory, SimpleTestCase, override_settings
 from django.test.utils import LoggingCaptureMixin, patch_logger
 from django.utils.log import (
@@ -461,26 +460,6 @@ format=%(message)s
         out, err = self.run_manage(['check'])
         self.assertNoOutput(err)
         self.assertOutput(out, "System check identified no issues (0 silenced).")
-
-
-class SchemaLoggerTests(SimpleTestCase):
-
-    def test_extra_args(self):
-        editor = connection.schema_editor(collect_sql=True)
-        sql = "SELECT * FROM foo WHERE id in (%s, %s)"
-        params = [42, 1337]
-        with patch_logger('django.db.backends.schema', 'debug', log_kwargs=True) as logger:
-            editor.execute(sql, params)
-        self.assertEqual(
-            logger,
-            [(
-                'SELECT * FROM foo WHERE id in (%s, %s); (params [42, 1337])',
-                {'extra': {
-                    'sql': 'SELECT * FROM foo WHERE id in (%s, %s)',
-                    'params': [42, 1337],
-                }},
-            )]
-        )
 
 
 class LogFormattersTests(SimpleTestCase):
