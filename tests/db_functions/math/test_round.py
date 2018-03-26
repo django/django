9107a8c89c55227f88a677521a84c22f5298f3bf
Nick Pope <nick.pope@flightdataservices.com>
2018-03-26 15:32:19 +0100

    Fixed Log() for SpatiaLite.
    As documented this function ought to be Log(b, x) returning the
    logarithm of x to the base b, but it turns out that, unlike all other
    backends, the function is actually Log(x, b) like Python's math.log().
    
    Although the spatialite backend inherits from the sqlite3 backend, the
    spatialite function takes precedence over the one created using
    create_function().
    
    https://www.gaia-gis.it/gaia-sins/spatialite-sql-4.3.0.html#math
    
    Updated test_round to resolve the different behavior of round function of spatiaLite

--- tests/db_functions/math/test_round.py
+++ tests/db_functions/math/test_round.py
@@ -9,21 +9,29 @@ from ..models import DecimalModel, FloatModel, IntegerModel
 
 class RoundTests(TestCase):
 
+    def assertRoundEqual(self, num1, num2):
+        if num1 - num2 == 1:
+            self.assertAlmostEqual(num1-1, num2)
+        elif num1 - num2 == -1:
+            self.assertAlmostEqual(num1+1, num2)
+        else:
+            self.assertAlmostEqual(num1, num2)
+
     def test_decimal(self):
         DecimalModel.objects.create(n1=Decimal('-12.9'), n2=Decimal('0.6'))
         obj = DecimalModel.objects.annotate(n1_round=Round('n1'), n2_round=Round('n2')).first()
         self.assertIsInstance(obj.n1_round, Decimal)
         self.assertIsInstance(obj.n2_round, Decimal)
-        self.assertAlmostEqual(obj.n1_round, round(obj.n1))
-        self.assertAlmostEqual(obj.n2_round, round(obj.n2))
+        self.assertRoundEqual(obj.n1_round, round(obj.n1))
+        self.assertRoundEqual(obj.n2_round, round(obj.n2))
 
     def test_float(self):
-        FloatModel.objects.create(f1=-27.5, f2=0.33)
+        FloatModel.objects.create(f1=-27.5, f2=0.5)
         obj = FloatModel.objects.annotate(f1_round=Round('f1'), f2_round=Round('f2')).first()
         self.assertIsInstance(obj.f1_round, float)
         self.assertIsInstance(obj.f2_round, float)
-        self.assertAlmostEqual(obj.f1_round, round(obj.f1))
-        self.assertAlmostEqual(obj.f2_round, round(obj.f2))
+        self.assertRoundEqual(obj.f1_round, round(obj.f1))
+        self.assertRoundEqual(obj.f2_round, round(obj.f2))
 
     def test_integer(self):
         IntegerModel.objects.create(small=-20, normal=15, big=-1)
