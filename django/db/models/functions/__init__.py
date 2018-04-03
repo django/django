55cc26941a1eb6093bf9602e67a2b5fdf7e68683
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2018-04-03 19:36:12 +0200

    Refs #28643 -- Added Repeat database function.
    Thanks Tim Graham and Nick Pope for reviews.

--- django/db/models/functions/__init__.py
+++ django/db/models/functions/__init__.py
@@ -6,8 +6,8 @@ from .datetime import (
     TruncQuarter, TruncSecond, TruncTime, TruncWeek, TruncYear,
 )
 from .text import (
-    Chr, Concat, ConcatPair, Left, Length, Lower, LPad, LTrim, Ord, Replace,
-    Right, RPad, RTrim, StrIndex, Substr, Trim, Upper,
+    Chr, Concat, ConcatPair, Left, Length, Lower, LPad, LTrim, Ord, Repeat,
+    Replace, Right, RPad, RTrim, StrIndex, Substr, Trim, Upper,
 )
 from .window import (
     CumeDist, DenseRank, FirstValue, Lag, LastValue, Lead, NthValue, Ntile,
@@ -25,8 +25,8 @@ __all__ = [
     'TruncWeek', 'TruncYear',
     # text
     'Chr', 'Concat', 'ConcatPair', 'Left', 'Length', 'Lower', 'LPad', 'LTrim',
-    'Ord', 'Replace', 'Right', 'RPad', 'RTrim', 'StrIndex', 'Substr', 'Trim',
-    'Upper',
+    'Ord', 'Repeat', 'Replace', 'Right', 'RPad', 'RTrim', 'StrIndex', 'Substr',
+    'Trim', 'Upper',
     # window
     'CumeDist', 'DenseRank', 'FirstValue', 'Lag', 'LastValue', 'Lead',
     'NthValue', 'Ntile', 'PercentRank', 'Rank', 'RowNumber',
