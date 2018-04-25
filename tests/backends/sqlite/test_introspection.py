e82b567de79f6c26a79dabeb87ae9b3c2e8f5b51
ztroop <zackary.troop@esentire.com>
2018-04-25 16:33:41 -0400

    Add support for square bracket and grave accents, recommended by @claudep.

--- tests/backends/sqlite/test_introspection.py
+++ tests/backends/sqlite/test_introspection.py
@@ -8,10 +8,13 @@ from django.test import TestCase
 class IntrospectionTests(TestCase):
     def test_get_primary_key_column(self):
         """Get the primary key column regardless of whether or not it has quotation."""
+        testable_primary_key_columns = ('id', '[id]', '`id`', '"id"')
         with connection.cursor() as cursor:
-            try:
-                cursor.execute('CREATE TABLE `test_primary` (id int PRIMARY KEY NOT NULL);')
-                field = connection.introspection.get_primary_key_column(cursor, 'test_primary')
-                self.assertEqual(field, 'id')
-            finally:
-                cursor.execute('DROP TABLE `test_primary`;')
+            for column in testable_primary_key_columns:
+                sql = "CREATE TABLE `test_primary` (%s int PRIMARY KEY NOT NULL);" % column
+                try:
+                    cursor.execute(sql)
+                    field = connection.introspection.get_primary_key_column(cursor, 'test_primary')
+                    self.assertEqual(field, 'id')
+                finally:
+                    cursor.execute('DROP TABLE `test_primary`;')
