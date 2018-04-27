25e53b48ede79e2e22d280d0c0908f5ffa922f3e
ztroop <zackary.troop@esentire.com>
2018-04-27 14:51:00 -0400

    Various corrections as recommended by @felixxm.

--- tests/backends/sqlite/test_introspection.py
+++ tests/backends/sqlite/test_introspection.py
@@ -7,19 +7,21 @@ from django.test import TestCase
 @unittest.skipUnless(connection.vendor == 'sqlite', 'SQLite tests')
 class IntrospectionTests(TestCase):
     def test_get_primary_key_column(self):
-        """Get the primary key column regardless of whether or not it has quotation."""
+        """
+        Get the primary key column regardless of whether or not it has
+        quotation.
+        """
         testable_column_strings = (
             ('id', 'id'), ('[id]', 'id'), ('`id`', 'id'), ('"id"', 'id'),
             ('[id col]', 'id col'), ('`id col`', 'id col'), ('"id col"', 'id col')
         )
         with connection.cursor() as cursor:
-            for test in testable_column_strings:
-                column, expected_string = test
-                sql = "CREATE TABLE `test_primary` (%s int PRIMARY KEY NOT NULL);" % column
+            for column, expected_string in testable_column_strings:
+                sql = 'CREATE TABLE test_primary (%s int PRIMARY KEY NOT NULL)' % column
                 with self.subTest(column=column):
                     try:
                         cursor.execute(sql)
                         field = connection.introspection.get_primary_key_column(cursor, 'test_primary')
                         self.assertEqual(field, expected_string)
                     finally:
-                        cursor.execute('DROP TABLE `test_primary`;')
+                        cursor.execute('DROP TABLE test_primary')
