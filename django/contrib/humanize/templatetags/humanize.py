0e2759459b0d3979aa30c8025c58e865d332003d
Maximilian Merz <git@mxmerz.de>
2018-04-28 16:31:55 +0200

    Add support for languages like Hungarian
    Add support in the humanize tag for languages that need different translations depending on whether the date lies in the past or future

--- django/contrib/humanize/templatetags/humanize.py
+++ django/contrib/humanize/templatetags/humanize.py
@@ -221,7 +221,7 @@ def naturaltime(value):
         if delta.days != 0:
             return pgettext(
                 'naturaltime', '%(delta)s ago'
-            ) % {'delta': defaultfilters.timesince(value, now, context='naturaltime')}
+            ) % {'delta': defaultfilters.timesince(value, now, context='naturaltime-past')}
         elif delta.seconds == 0:
             return _('now')
         elif delta.seconds < 60:
@@ -249,7 +249,7 @@ def naturaltime(value):
         if delta.days != 0:
             return pgettext(
                 'naturaltime', '%(delta)s from now'
-            ) % {'delta': defaultfilters.timeuntil(value, now, context='naturaltime')}
+            ) % {'delta': defaultfilters.timeuntil(value, now, context='naturaltime-future')}
         elif delta.seconds == 0:
             return _('now')
         elif delta.seconds < 60:
