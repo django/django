django/django

commit 825f0beda804e48e9197fcf3b0d909f9f548aa47

Fixed #8936 -- Added a view permission and a read-only admin.
Co-authored-by: Petr Dlouhy <petr.dlouhy@email.cz>
Co-authored-by: Olivier Dalang <olivier.dalang@gmail.com>

django/contrib/admin/helpers.py
@@ -224,7 +224,9 @@ class InlineAdminFormSet:
     A wrapper around an inline formset for use in the admin system.
     """
     def __init__(self, inline, formset, fieldsets, prepopulated_fields=None,
-                 readonly_fields=None, model_admin=None):
+                 readonly_fields=None, model_admin=None, has_add_permission=True,
+                 has_change_permission=True, has_delete_permission=True,
+                 has_view_permission=True):
         self.opts = inline
         self.formset = formset
         self.fieldsets = fieldsets
@@ -236,13 +238,21 @@ class InlineAdminFormSet:
             prepopulated_fields = {}
         self.prepopulated_fields = prepopulated_fields
         self.classes = ' '.join(inline.classes) if inline.classes else ''
+        self.has_add_permission = has_add_permission
+        self.has_change_permission = has_change_permission
+        self.has_delete_permission = has_delete_permission
+        self.has_view_permission = has_view_permission
 
     def __iter__(self):
+        readonly_fields_for_editing = self.readonly_fields
+        if not self.has_change_permission:
+            readonly_fields_for_editing += flatten_fieldsets(self.fieldsets)
+
         for form, original in zip(self.formset.initial_forms, self.formset.get_queryset()):
             view_on_site_url = self.opts.get_view_on_site_url(original)
             yield InlineAdminForm(
                 self.formset, form, self.fieldsets, self.prepopulated_fields,
-                original, self.readonly_fields, model_admin=self.opts,
+                original, readonly_fields_for_editing, model_admin=self.opts,
                 view_on_site_url=view_on_site_url,
             )
         for form in self.formset.extra_forms:
@@ -250,11 +260,12 @@ class InlineAdminFormSet:
                 self.formset, form, self.fieldsets, self.prepopulated_fields,
                 None, self.readonly_fields, model_admin=self.opts,
             )
-        yield InlineAdminForm(
-            self.formset, self.formset.empty_form,
-            self.fieldsets, self.prepopulated_fields, None,
-            self.readonly_fields, model_admin=self.opts,
-        )
+        if self.has_add_permission:
+            yield InlineAdminForm(
+                self.formset, self.formset.empty_form,
+                self.fieldsets, self.prepopulated_fields, None,
+                self.readonly_fields, model_admin=self.opts,
+            )
 
     def fields(self):
         fk = getattr(self.formset, "fk", None)
@@ -264,7 +275,7 @@ class InlineAdminFormSet:
         for i, field_name in enumerate(flatten_fieldsets(self.fieldsets)):
             if fk and fk.name == field_name:
                 continue
-            if field_name in self.readonly_fields:
+            if not self.has_change_permission or field_name in self.readonly_fields:
                 yield {
                     'label': meta_labels.get(field_name) or label_for_field(field_name, self.opts.model, self.opts),
                     'widget': {'is_hidden': False},

tests/admin_changelist/tests.py
@@ -50,6 +50,7 @@ class ChangeListTests(TestCase):
 
     def setUp(self):
         self.factory = RequestFactory()
+        self.superuser = User.objects.create_superuser(username='super', email='a@b.com', password='xxx')
 
     def _create_superuser(self, username):
         return User.objects.create_superuser(username=username, email='a@b.com', password='xxx')
@@ -70,6 +71,7 @@ class ChangeListTests(TestCase):
 
         m = OrderedByFBandAdmin(Band, custom_site)
         request = self.factory.get('/band/')
+        request.user = self.superuser
         cl = m.get_changelist_instance(request)
         self.assertEqual(cl.get_ordering_field_columns(), {3: 'desc', 2: 'asc'})
 
@@ -80,12 +82,14 @@ class ChangeListTests(TestCase):
         """
         m = ChildAdmin(Child, custom_site)
         request = self.factory.get('/child/')
+        request.user = self.superuser
         cl = m.get_changelist_instance(request)
         self.assertEqual(cl.queryset.query.select_related, {'parent': {}})
 
     def test_select_related_as_tuple(self):
         ia = InvitationAdmin(Invitation, custom_site)
         request = self.factory.get('/invitation/')
+        request.user = self.superuser
         cl = ia.get_changelist_instance(request)
         self.assertEqual(cl.queryset.query.select_related, {'player': {}})
 
@@ -93,6 +97,7 @@ class ChangeListTests(TestCase):
         ia = InvitationAdmin(Invitation, custom_site)
         ia.list_select_related = ()
         request = self.factory.get('/invitation/')
+        request.user = self.superuser
         cl = ia.get_changelist_instance(request)
         self.assertIs(cl.queryset.query.select_related, False)
 
@@ -105,6 +110,7 @@ class ChangeListTests(TestCase):
 
         ia = GetListSelectRelatedAdmin(Invitation, custom_site)
         request = self.factory.get('/invitation/')
+        request.user = self.superuser
         cl = ia.get_changelist_instance(request)
         self.assertEqual(cl.queryset.query.select_related, {'player': {}, 'band': {}})
 
@@ -115,6 +121,7 @@ class ChangeListTests(TestCase):
         """
         new_child = Child.objects.create(name='name', parent=None)
         request = self.factory.get('/child/')
+        request.user = self.superuser
         m = ChildAdmin(Child, custom_site)
         cl = m.get_changelist_instance(request)
         cl.formset = None
@@ -131,6 +138,7 @@ class ChangeListTests(TestCase):
         """
         new_child = Child.objects.create(name='name', parent=None)
         request = self.factory.get('/child/')
+        request.user = self.superuser
         # Set a new empty display value on AdminSite.
         admin.site.empty_value_display = '???'
         m = ChildAdmin(Child, admin.site)
@@ -149,6 +157,7 @@ class ChangeListTests(TestCase):
         """
         new_child = Child.objects.create(name='name', parent=None)
         request = self.factory.get('/child/')
+        request.user = self.superuser
         m = EmptyValueChildAdmin(Child, admin.site)
         cl = m.get_changelist_instance(request)
         cl.formset = None
@@ -172,6 +181,7 @@ class ChangeListTests(TestCase):
         new_parent = Parent.objects.create(name='parent')
         new_child = Child.objects.create(name='name', parent=new_parent)
         request = self.factory.get('/child/')
+        request.user = self.superuser
         m = ChildAdmin(Child, custom_site)
         cl = m.get_changelist_instance(request)
         cl.formset = None
@@ -194,6 +204,7 @@ class ChangeListTests(TestCase):
         new_parent = Parent.objects.create(name='parent')
         new_child = Child.objects.create(name='name', parent=new_parent)
         request = self.factory.get('/child/')
+        request.user = self.superuser
         m = ChildAdmin(Child, custom_site)
 
         # Test with list_editable fields
@@ -233,6 +244,7 @@ class ChangeListTests(TestCase):
         for i in range(200):
             Child.objects.create(name='name %s' % i, parent=new_parent)
         request = self.factory.get('/child/', data={'p': -1})  # Anything outside range
+        request.user = self.superuser
         m = ChildAdmin(Child, custom_site)
 
         # Test with list_editable fields
@@ -248,6 +260,7 @@ class ChangeListTests(TestCase):
             Child.objects.create(name='name %s' % i, parent=new_parent)
 
         request = self.factory.get('/child/')
+        request.user = self.superuser
         m = CustomPaginationAdmin(Child, custom_site)
 
         cl = m.get_changelist_instance(request)
@@ -267,6 +280,7 @@ class ChangeListTests(TestCase):
 
         m = BandAdmin(Band, custom_site)
         request = self.factory.get('/band/', data={'genres': blues.pk})
+        request.user = self.superuser
 
         cl = m.get_changelist_instance(request)
         cl.get_results(request)
@@ -286,6 +300,7 @@ class ChangeListTests(TestCase):
 
         m = GroupAdmin(Group, custom_site)
         request = self.factory.get('/group/', data={'members': lead.pk})
+        request.user = self.superuser
 
         cl = m.get_changelist_instance(request)
         cl.get_results(request)
@@ -307,6 +322,7 @@ class ChangeListTests(TestCase):
 
         m = ConcertAdmin(Concert, custom_site)
         request = self.factory.get('/concert/', data={'group__members': lead.pk})
+        request.user = self.superuser
 
         cl = m.get_changelist_instance(request)
         cl.get_results(request)
@@ -327,6 +343,7 @@ class ChangeListTests(TestCase):
 
         m = QuartetAdmin(Quartet, custom_site)
         request = self.factory.get('/quartet/', data={'members': lead.pk})
+        request.user = self.superuser
 
         cl = m.get_changelist_instance(request)
         cl.get_results(request)
@@ -347,6 +364,7 @@ class ChangeListTests(TestCase):
 
         m = ChordsBandAdmin(ChordsBand, custom_site)
         request = self.factory.get('/chordsband/', data={'members': lead.pk})
+        request.user = self.superuser
 
         cl = m.get_changelist_instance(request)
         cl.get_results(request)
@@ -366,6 +384,7 @@ class ChangeListTests(TestCase):
 
         m = ParentAdmin(Parent, custom_site)
         request = self.factory.get('/parent/', data={'child__name': 'Daniel'})
+        request.user = self.superuser
 
         cl = m.get_changelist_instance(request)
         # Make sure distinct() was called
@@ -382,6 +401,7 @@ class ChangeListTests(TestCase):
 
         m = ParentAdmin(Parent, custom_site)
         request = self.factory.get('/parent/', data={SEARCH_VAR: 'daniel'})
+        request.user = self.superuser
 
         cl = m.get_changelist_instance(request)
         # Make sure distinct() was called
@@ -401,6 +421,7 @@ class ChangeListTests(TestCase):
 
         m = ConcertAdmin(Concert, custom_site)
         request = self.factory.get('/concert/', data={SEARCH_VAR: 'vox'})
+        request.user = self.superuser
 
         cl = m.get_changelist_instance(request)
         # There's only one Concert instance
@@ -414,10 +435,12 @@ class ChangeListTests(TestCase):
         m.search_fields = ['group__pk']
 
         request = self.factory.get('/concert/', data={SEARCH_VAR: band.pk})
+        request.user = self.superuser
         cl = m.get_changelist_instance(request)
         self.assertEqual(cl.queryset.count(), 1)
 
         request = self.factory.get('/concert/', data={SEARCH_VAR: band.pk + 5})
+        request.user = self.superuser
         cl = m.get_changelist_instance(request)
         self.assertEqual(cl.queryset.count(), 0)
 
@@ -429,10 +452,12 @@ class ChangeListTests(TestCase):
         m.search_fields = ['name__iexact']
 
         request = self.factory.get('/', data={SEARCH_VAR: 'woodstock'})
+        request.user = self.superuser
         cl = m.get_changelist_instance(request)
         self.assertCountEqual(cl.queryset, [concert])
 
         request = self.factory.get('/', data={SEARCH_VAR: 'wood'})
+        request.user = self.superuser
         cl = m.get_changelist_instance(request)
         self.assertCountEqual(cl.queryset, [])
 
@@ -445,10 +470,12 @@ class ChangeListTests(TestCase):
         Field.register_lookup(Contains, 'cc')
         try:
             request = self.factory.get('/', data={SEARCH_VAR: 'Hype'})
+            request.user = self.superuser
             cl = m.get_changelist_instance(request)
             self.assertCountEqual(cl.queryset, [concert])
 
             request = self.factory.get('/', data={SEARCH_VAR: 'Woodstock'})
+            request.user = self.superuser
             cl = m.get_changelist_instance(request)
             self.assertCountEqual(cl.queryset, [])
         finally:
@@ -467,10 +494,12 @@ class ChangeListTests(TestCase):
             m.search_fields = ['group__members__age__exactly']
 
             request = self.factory.get('/', data={SEARCH_VAR: '20'})
+            request.user = self.superuser
             cl = m.get_changelist_instance(request)
             self.assertCountEqual(cl.queryset, [concert])
 
             request = self.factory.get('/', data={SEARCH_VAR: '21'})
+            request.user = self.superuser
             cl = m.get_changelist_instance(request)
             self.assertCountEqual(cl.queryset, [])
         finally:
@@ -486,10 +515,12 @@ class ChangeListTests(TestCase):
         m.search_fields = ['pk__exact']
 
         request = self.factory.get('/', data={SEARCH_VAR: 'abc'})
+        request.user = self.superuser
         cl = m.get_changelist_instance(request)
         self.assertCountEqual(cl.queryset, [abc])
 
         request = self.factory.get('/', data={SEARCH_VAR: 'abcd'})
+        request.user = self.superuser
         cl = m.get_changelist_instance(request)
         self.assertCountEqual(cl.queryset, [abcd])
 
@@ -501,11 +532,13 @@ class ChangeListTests(TestCase):
         m = BandAdmin(Band, custom_site)
         for lookup_params in ({}, {'name': 'test'}):
             request = self.factory.get('/band/', lookup_params)
+            request.user = self.superuser
             cl = m.get_changelist_instance(request)
             self.assertFalse(cl.queryset.query.distinct)
 
         # A ManyToManyField in params does have distinct applied.
         request = self.factory.get('/band/', {'genres': '0'})
+        request.user = self.superuser
         cl = m.get_changelist_instance(request)
         self.assertTrue(cl.queryset.query.distinct)
 
@@ -520,6 +553,7 @@ class ChangeListTests(TestCase):
             Child.objects.create(name='filtered %s' % i, parent=parent)
 
         request = self.factory.get('/child/')
+        request.user = self.superuser
 
         # Test default queryset
         m = ChildAdmin(Child, custom_site)
@@ -540,8 +574,7 @@ class ChangeListTests(TestCase):
         Regression test for #13196: output of functions should be  localized
         in the changelist.
         """
-        superuser = User.objects.create_superuser(username='super', email='super@localhost', password='secret')
-        self.client.force_login(superuser)
+        self.client.force_login(self.superuser)
         event = Event.objects.create(date=datetime.date.today())
         response = self.client.get(reverse('admin:admin_changelist_event_changelist'))
         self.assertContains(response, formats.localize(event.date))
@@ -597,6 +630,7 @@ class ChangeListTests(TestCase):
 
         # Add "show all" parameter to request
         request = self.factory.get('/child/', data={ALL_VAR: ''})
+        request.user = self.superuser
 
         # Test valid "show all" request (number of total objects is under max)
         m = ChildAdmin(Child, custom_site)
@@ -856,6 +890,7 @@ class ChangeListTests(TestCase):
         # instantiating and setting up ChangeList object
         m = GroupAdmin(Group, custom_site)
         request = self.factory.get('/group/')
+        request.user = self.superuser
         cl = m.get_changelist_instance(request)
         per_page = cl.list_per_page = 10
 

tests/admin_views/admin.py
@@ -156,6 +156,10 @@ class RowLevelChangePermissionModelAdmin(admin.ModelAdmin):
         """ Only allow changing objects with even id number """
         return request.user.is_staff and (obj is not None) and (obj.id % 2 == 0)
 
+    def has_view_permission(self, request, obj=None):
+        """Only allow viewing objects if id is a multiple of 3."""
+        return request.user.is_staff and obj is not None and obj.id % 3 == 0
+
 
 class CustomArticleAdmin(admin.ModelAdmin):
     """

commit e37ad73c867460d23b30d886feb69de62d482eda

Fixed #29414 -- Restored form inputs on admin inlines if the change permission isn't present.
Regression in 825f0beda804e48e9197fcf3b0d909f9f548aa47.

django/contrib/admin/helpers.py
@@ -244,9 +244,10 @@ class InlineAdminFormSet:
         self.has_view_permission = has_view_permission
 
     def __iter__(self):
-        readonly_fields_for_editing = self.readonly_fields
-        if not self.has_change_permission:
-            readonly_fields_for_editing += flatten_fieldsets(self.fieldsets)
+        if self.has_change_permission:
+            readonly_fields_for_editing = self.readonly_fields
+        else:
+            readonly_fields_for_editing = self.readonly_fields + flatten_fieldsets(self.fieldsets)
 
         for form, original in zip(self.formset.initial_forms, self.formset.get_queryset()):
             view_on_site_url = self.opts.get_view_on_site_url(original)

tests/admin_inlines/admin.py
@@ -7,9 +7,9 @@ from .models import (
     Consigliere, EditablePKBook, ExtraTerrestrial, Fashionista, Holder,
     Holder2, Holder3, Holder4, Inner, Inner2, Inner3, Inner4Stacked,
     Inner4Tabular, NonAutoPKBook, NonAutoPKBookChild, Novel,
-    ParentModelWithCustomPk, Poll, Profile, ProfileCollection, Question,
-    ReadOnlyInline, ShoppingWeakness, Sighting, SomeChildModel,
-    SomeParentModel, SottoCapo, Title, TitleCollection,
+    NovelReadonlyChapter, ParentModelWithCustomPk, Poll, Profile,
+    ProfileCollection, Question, ReadOnlyInline, ShoppingWeakness, Sighting,
+    SomeChildModel, SomeParentModel, SottoCapo, Title, TitleCollection,
 )
 
 site = admin.AdminSite(name="admin")
@@ -153,6 +153,17 @@ class NovelAdmin(admin.ModelAdmin):
     inlines = [ChapterInline]
 
 
+class ReadOnlyChapterInline(admin.TabularInline):
+    model = Chapter
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+class NovelReadonlyChapterAdmin(admin.ModelAdmin):
+    inlines = [ReadOnlyChapterInline]
+
+
 class ConsigliereInline(admin.TabularInline):
     model = Consigliere
 
@@ -231,6 +242,7 @@ site.register(Holder3, inlines=[InnerInline3])
 
 site.register(Poll, PollAdmin)
 site.register(Novel, NovelAdmin)
+site.register(NovelReadonlyChapter, NovelReadonlyChapterAdmin)
 site.register(Fashionista, inlines=[InlineWeakness])
 site.register(Holder4, Holder4Admin)
 site.register(Author, AuthorAdmin)

tests/admin_inlines/models.py
@@ -159,6 +159,12 @@ class Novel(models.Model):
     name = models.CharField(max_length=40)
 
 
+class NovelReadonlyChapter(Novel):
+
+    class Meta:
+        proxy = True
+
+
 class Chapter(models.Model):
     name = models.CharField(max_length=40)
     novel = models.ForeignKey(Novel, models.CASCADE)

tests/admin_inlines/tests.py
@@ -447,6 +447,16 @@ class TestInline(TestDataMixin, TestCase):
         self.assertTrue(response.context['inline_admin_formset'].opts.has_registered_model)
         self.assertNotContains(response, INLINE_CHANGELINK_HTML)
 
+    def test_noneditable_inline_has_field_inputs(self):
+        """Inlines without change permission shows field inputs on add form."""
+        response = self.client.get(reverse('admin:admin_inlines_novelreadonlychapter_add'))
+        self.assertContains(
+            response,
+            '<input type="text" name="chapter_set-0-name" '
+            'class="vTextField" maxlength="40" id="id_chapter_set-0-name">',
+            html=True
+        )
+
 
 @override_settings(ROOT_URLCONF='admin_inlines.urls')
 class TestInlineMedia(TestDataMixin, TestCase):