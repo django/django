django/django

commit b3cffde5559c4fa97625512d7ec41a674be26076

Fixed #29324 -- Made Settings raise ImproperlyConfigured if SECRET_KEY is accessed and not set.

django/conf/__init__.py
@@ -115,14 +115,14 @@ class Settings:
             if setting.isupper():
                 setting_value = getattr(mod, setting)
 
+                if setting == 'SECRET_KEY' and not setting_value:
+                    raise ImproperlyConfigured('The SECRET_KEY setting must not be empty.')
+
                 if (setting in tuple_settings and
                         not isinstance(setting_value, (list, tuple))):
                     raise ImproperlyConfigured("The %s setting must be a list or a tuple. " % setting)
                 setattr(self, setting, setting_value)
                 self._explicit_settings.add(setting)
-
-        if not self.SECRET_KEY:
-            raise ImproperlyConfigured("The SECRET_KEY setting must not be empty.")
 
         if self.is_overridden('DEFAULT_CONTENT_TYPE'):
             warnings.warn('The DEFAULT_CONTENT_TYPE setting is deprecated.', RemovedInDjango30Warning)
@@ -138,6 +138,11 @@ class Settings:
             # we don't do this unconditionally (breaks Windows).
             os.environ['TZ'] = self.TIME_ZONE
             time.tzset()
+
+    def __getattr__(self, name):
+        if name == 'SECRET_KEY':
+            raise ImproperlyConfigured('The SECRET_KEY setting must be set.')
+        raise AttributeError("'%s' object has no attribute '%s'" % (self.__class__.__name__, name))
 
     def is_overridden(self, setting):
         return setting in self._explicit_settings

tests/settings_tests/tests.py
@@ -292,8 +292,20 @@ class SettingsTests(SimpleTestCase):
     def test_no_secret_key(self):
         settings_module = ModuleType('fake_settings_module')
         sys.modules['fake_settings_module'] = settings_module
-        msg = 'The SECRET_KEY setting must not be empty.'
+        msg = 'The SECRET_KEY setting must be set.'
         try:
+            settings = Settings('fake_settings_module')
+            with self.assertRaisesMessage(ImproperlyConfigured, msg):
+                settings.SECRET_KEY
+        finally:
+            del sys.modules['fake_settings_module']
+
+    def test_secret_key_empty_string(self):
+        settings_module = ModuleType('fake_settings_module')
+        settings_module.SECRET_KEY = ''
+        sys.modules['fake_settings_module'] = settings_module
+        try:
+            msg = 'The SECRET_KEY setting must not be empty.'
             with self.assertRaisesMessage(ImproperlyConfigured, msg):
                 Settings('fake_settings_module')
         finally:

tests/admin_scripts/tests.py
@@ -2213,7 +2213,11 @@ class DiffSettings(AdminScriptTestCase):
         out, err = self.run_manage(args)
         self.assertNoOutput(err)
         self.assertOutput(out, "+ FOO = 'bar'")
-        self.assertOutput(out, "- SECRET_KEY = ''")
+        self.assertOutput(out, "- INSTALLED_APPS = []")
+        self.assertOutput(
+            out,
+            "+ INSTALLED_APPS = ['django.contrib.auth', 'django.contrib.contenttypes', 'admin_scripts']"
+        )
         self.assertOutput(out, "+ SECRET_KEY = 'django_tests_secret_key'")
         self.assertNotInOutput(out, "  APPEND_SLASH = True")
 
@@ -2229,7 +2233,12 @@ class DiffSettings(AdminScriptTestCase):
         self.assertNoOutput(err)
         self.assertOutput(out, "  APPEND_SLASH = True")
         self.assertOutput(out, "+ FOO = 'bar'")
-        self.assertOutput(out, "- SECRET_KEY = ''")
+        self.assertOutput(out, "- INSTALLED_APPS = []")
+        self.assertOutput(
+            out,
+            "+ INSTALLED_APPS = ['django.contrib.auth', 'django.contrib.contenttypes', 'admin_scripts']"
+        )
+        self.assertOutput(out, "+ SECRET_KEY = 'django_tests_secret_key'")
 
 
 class Dumpdata(AdminScriptTestCase):

django/conf/global_settings.py
@@ -256,11 +256,6 @@ ABSOLUTE_URL_OVERRIDES = {}
 #    ]
 IGNORABLE_404_URLS = []
 
-# A secret key for this particular Django installation. Used in secret-key
-# hashing algorithms. Set this in your settings, or Django will complain
-# loudly.
-SECRET_KEY = ''
-
 # Default file storage mechanism that holds media.
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 

commit 26b74e3c755ca8a6ac3f9836405d14d6547b8cab

Refs #29324 -- Fixed SECRET_KEY validation when using settings.configure().
Regression in b3cffde5559c4fa97625512d7ec41a674be26076.

django/conf/__init__.py
@@ -56,6 +56,8 @@ class LazySettings(LazyObject):
         if self._wrapped is empty:
             self._setup(name)
         val = getattr(self._wrapped, name)
+        if name == 'SECRET_KEY' and not val:
+            raise ImproperlyConfigured('The SECRET_KEY setting must not be empty.')
         self.__dict__[name] = val
         return val
 
@@ -172,7 +174,13 @@ class UserSettingsHolder:
     def __getattr__(self, name):
         if name in self._deleted:
             raise AttributeError
-        return getattr(self.default_settings, name)
+        try:
+            value = getattr(self.default_settings, name)
+        except AttributeError:
+            if name == 'SECRET_KEY':
+                raise ImproperlyConfigured('The SECRET_KEY setting must be set.')
+            raise
+        return value
 
     def __setattr__(self, name, value):
         self._deleted.discard(name)

django/core/checks/security/base.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 
 from .. import Tags, Warning, register
 
@@ -182,11 +183,16 @@ def check_ssl_redirect(app_configs, **kwargs):
 
 @register(Tags.security, deploy=True)
 def check_secret_key(app_configs, **kwargs):
-    passed_check = (
-        getattr(settings, 'SECRET_KEY', None) and
-        len(set(settings.SECRET_KEY)) >= SECRET_KEY_MIN_UNIQUE_CHARACTERS and
-        len(settings.SECRET_KEY) >= SECRET_KEY_MIN_LENGTH
-    )
+    try:
+        getattr(settings, 'SECRET_KEY')
+    except (AttributeError, ImproperlyConfigured):
+        passed_check = False
+    else:
+        passed_check = (
+            settings.SECRET_KEY and
+            len(set(settings.SECRET_KEY)) >= SECRET_KEY_MIN_UNIQUE_CHARACTERS and
+            len(settings.SECRET_KEY) >= SECRET_KEY_MIN_LENGTH
+        )
     return [] if passed_check else [W009]
 
 

tests/settings_tests/tests.py
@@ -299,6 +299,13 @@ class SettingsTests(SimpleTestCase):
         finally:
             del sys.modules['fake_settings_module']
 
+    def test_lazy_settings_no_secret_key(self):
+        lazy_settings = LazySettings()
+        lazy_settings.configure()
+        msg = 'The SECRET_KEY setting must be set.'
+        with self.assertRaisesMessage(ImproperlyConfigured, msg):
+            lazy_settings.SECRET_KEY
+
     def test_secret_key_empty_string(self):
         settings_module = ModuleType('fake_settings_module')
         settings_module.SECRET_KEY = ''
@@ -309,6 +316,13 @@ class SettingsTests(SimpleTestCase):
                 Settings('fake_settings_module')
         finally:
             del sys.modules['fake_settings_module']
+
+    def test_lazy_settings_secret_key_empty_string(self):
+        lazy_settings = LazySettings()
+        lazy_settings.configure(SECRET_KEY='')
+        msg = 'The SECRET_KEY setting must not be empty.'
+        with self.assertRaisesMessage(ImproperlyConfigured, msg):
+            lazy_settings.SECRET_KEY
 
     def test_no_settings_module(self):
         msg = (