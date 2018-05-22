70e77b503de5cf7aad973a600c600fddd6933459
mackong <mackonghp@gmail.com>
2018-05-22 15:36:17 +0800

    Fixed #29426 -- Made UUID inputs in the admin match the width of a UUID.

--- tests/admin_widgets/tests.py
+++ tests/admin_widgets/tests.py
@@ -408,6 +408,20 @@ class AdminURLWidgetTest(SimpleTestCase):
         )
 
 
+class AdminUUIDWidgetTests(SimpleTestCase):
+    def test_attrs(self):
+        w = widgets.AdminUUIDInputWidget()
+        self.assertHTMLEqual(
+            w.render('test', '550e8400-e29b-41d4-a716-446655440000'),
+            '<input value="550e8400-e29b-41d4-a716-446655440000" type="text" class="vUUIDField" name="test">',
+        )
+        w = widgets.AdminUUIDInputWidget(attrs={'class': 'myUUIDInput'})
+        self.assertHTMLEqual(
+            w.render('test', '550e8400-e29b-41d4-a716-446655440000'),
+            '<input value="550e8400-e29b-41d4-a716-446655440000" type="text" class="myUUIDInput" name="test">',
+        )
+
+
 @override_settings(ROOT_URLCONF='admin_widgets.urls')
 class AdminFileWidgetTests(TestDataMixin, TestCase):
 
