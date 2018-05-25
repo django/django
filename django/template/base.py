c2a1af883e18b93a77080650feb9e959536d51ca
Ryan Rubin <ryanmrubin@gmail.com>
2018-05-25 10:11:46 -0500

    [2.0.x] Fixed #29400 -- Fixed crash in custom template filters that use decorated functions.
    Regression in 620e9dd31a2146d70de740f96a8cb9a6db054fc7.
    
    Backport of a8d12bc28069d56e0306770ab9c73738293663f7 from master

--- django/template/base.py
+++ django/template/base.py
@@ -51,7 +51,7 @@ times with multiple contexts)
 
 import logging
 import re
-from inspect import getcallargs, getfullargspec
+from inspect import getcallargs, getfullargspec, unwrap
 
 from django.template.context import (  # NOQA: imported for backwards compatibility
     BaseContext, Context, ContextPopException, RequestContext,
@@ -712,7 +712,7 @@ class FilterExpression:
         # First argument, filter input, is implied.
         plen = len(provided) + 1
         # Check to see if a decorator is providing the real function.
-        func = getattr(func, '_decorated_function', func)
+        func = unwrap(func)
 
         args, _, _, defaults, _, _, _ = getfullargspec(func)
         alen = len(args)
