django/django

commit 3aa9ab39cce6b2a27d6334ad0148c8f37b6f5986

Refs #28748 -- Reallowed lazy values in model field choices.
Regression in f9844f484186fa13399bf8b96f58616687fe158a.

Thanks Matthias Kestenholz for the report and suggestions.

django/db/models/fields/__init__.py
@@ -245,7 +245,7 @@ class Field(RegisterLookupMixin):
             return []
 
         def is_value(value):
-            return isinstance(value, str) or not is_iterable(value)
+            return isinstance(value, (str, Promise)) or not is_iterable(value)
 
         if is_value(self.choices):
             return [

tests/invalid_models_tests/test_ordinary_fields.py
@@ -5,6 +5,7 @@ from django.db import connection, models
 from django.test import SimpleTestCase, TestCase, skipIfDBFeature
 from django.test.utils import isolate_apps, override_settings
 from django.utils.timezone import now
+from django.utils.translation import gettext_lazy as _
 
 
 @isolate_apps('invalid_models_tests')
@@ -198,6 +199,12 @@ class CharFieldTests(TestCase):
             ),
         ])
 
+    def test_choices_containing_lazy(self):
+        class Model(models.Model):
+            field = models.CharField(max_length=10, choices=[['1', _('1')], ['2', _('2')]])
+
+        self.assertEqual(Model._meta.get_field('field').check(), [])
+
     def test_choices_named_group(self):
         class Model(models.Model):
             field = models.CharField(
@@ -247,6 +254,17 @@ class CharFieldTests(TestCase):
                 id='fields.E005',
             ),
         ])
+
+    def test_choices_named_group_lazy(self):
+        class Model(models.Model):
+            field = models.CharField(
+                max_length=10, choices=[
+                    [_('knights'), [['L', _('Lancelot')], ['G', _('Galahad')]]],
+                    ['R', _('Random character')],
+                ],
+            )
+
+        self.assertEqual(Model._meta.get_field('field').check(), [])
 
     def test_bad_db_index_value(self):
         class Model(models.Model):

commit 41b53fb8d439faa93a75038337c0508909d6d017

Refs #28748 -- Reallowed lazy model field choices.
Regression in 3aa9ab39cce6b2a27d6334ad0148c8f37b6f5986.

django/db/models/fields/__init__.py
@@ -244,10 +244,10 @@ class Field(RegisterLookupMixin):
         if not self.choices:
             return []
 
-        def is_value(value):
-            return isinstance(value, (str, Promise)) or not is_iterable(value)
+        def is_value(value, accept_promise=True):
+            return isinstance(value, (str, Promise) if accept_promise else str) or not is_iterable(value)
 
-        if is_value(self.choices):
+        if is_value(self.choices, accept_promise=False):
             return [
                 checks.Error(
                     "'choices' must be an iterable (e.g., a list or tuple).",

tests/invalid_models_tests/test_ordinary_fields.py
@@ -4,6 +4,7 @@ from django.core.checks import Error, Warning as DjangoWarning
 from django.db import connection, models
 from django.test import SimpleTestCase, TestCase, skipIfDBFeature
 from django.test.utils import isolate_apps, override_settings
+from django.utils.functional import lazy
 from django.utils.timezone import now
 from django.utils.translation import gettext_lazy as _
 
@@ -185,6 +186,12 @@ class CharFieldTests(TestCase):
     def test_choices_containing_lazy(self):
         class Model(models.Model):
             field = models.CharField(max_length=10, choices=[['1', _('1')], ['2', _('2')]])
+
+        self.assertEqual(Model._meta.get_field('field').check(), [])
+
+    def test_lazy_choices(self):
+        class Model(models.Model):
+            field = models.CharField(max_length=10, choices=lazy(lambda: [[1, '1'], [2, '2']], tuple)())
 
         self.assertEqual(Model._meta.get_field('field').check(), [])
 