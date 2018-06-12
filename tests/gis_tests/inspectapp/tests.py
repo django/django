9e4f26bb40abcade301ec836aa0b84acf5f60041
Tim Graham <timograham@gmail.com>
2018-06-12 13:34:58 -0400

    Fixed #29483 -- Confirmed support for GDAL 2.3.

--- tests/gis_tests/inspectapp/tests.py
+++ tests/gis_tests/inspectapp/tests.py
@@ -88,7 +88,8 @@ class OGRInspectTest(TestCase):
         # Same test with a 25D-type geometry field
         shp_file = os.path.join(TEST_DATA, 'gas_lines', 'gas_leitung.shp')
         model_def = ogrinspect(shp_file, 'MyModel', multi_geom=True)
-        self.assertIn('geom = models.MultiLineStringField(srid=-1)', model_def)
+        srid = '-1' if GDAL_VERSION < (2, 3) else '31253'
+        self.assertIn('geom = models.MultiLineStringField(srid=%s)' % srid, model_def)
 
     def test_date_field(self):
         shp_file = os.path.join(TEST_DATA, 'cities', 'cities.shp')
