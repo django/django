63f90f55f30d599d601d729060cdb0ebd2abdeb8
Tim Graham <timograham@gmail.com>
2018-06-15 13:56:10 -0400

    Fixed #29498 -- Fixed a missing pyc test file in source distribution.

--- tests/migrations/test_loader.py
+++ tests/migrations/test_loader.py
@@ -521,7 +521,12 @@ class PycLoaderTests(MigrationTestBase):
         MigrationLoader reraises ImportErrors caused by "bad magic number" pyc
         files with a more helpful message.
         """
-        with self.temporary_migration_module(module='migrations.test_migrations_bad_pyc'):
+        with self.temporary_migration_module(module='migrations.test_migrations_bad_pyc') as migration_dir:
+            # The -tpl suffix is to avoid the pyc exclusion in MANIFEST.in.
+            os.rename(
+                os.path.join(migration_dir, '0001_initial.pyc-tpl'),
+                os.path.join(migration_dir, '0001_initial.pyc'),
+            )
             msg = (
                 r"Couldn't import '\w+.migrations.0001_initial' as it appears "
                 "to be a stale .pyc file."
