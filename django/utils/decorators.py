da46599143408be58c1845b8c82cacbae0bb56c0
Chris Jerdonek <chris.jerdonek@gmail.com>
2018-06-27 08:46:07 -0700

    [2.1.x] Refs #29253 -- Fixed method_decorator() crash if decorator sets a new attribute.
    Regression in fdc936c9130cf4fb5d59869674b9a31cc79a7999.
    
    Backport of f434f5b84f7fcea9a76a551621ecce70786e2899 from master

--- django/utils/decorators.py
+++ django/utils/decorators.py
@@ -2,7 +2,7 @@
 
 # For backwards compatibility in Django 2.0.
 from contextlib import ContextDecorator  # noqa
-from functools import WRAPPER_ASSIGNMENTS, update_wrapper, wraps
+from functools import WRAPPER_ASSIGNMENTS, partial, update_wrapper, wraps
 
 
 class classonlymethod(classmethod):
@@ -36,8 +36,10 @@ def _multi_decorate(decorators, method):
 
     def _wrapper(self, *args, **kwargs):
         # bound_method has the signature that 'decorator' expects i.e. no
-        # 'self' argument.
-        bound_method = method.__get__(self, type(self))
+        # 'self' argument, but it's a closure over self so it can call
+        # 'func'. Also, wrap method.__get__() in a function because new
+        # attributes can't be set on bound method objects, only on functions.
+        bound_method = partial(method.__get__(self, type(self)))
         for dec in decorators:
             bound_method = dec(bound_method)
         return bound_method(*args, **kwargs)
