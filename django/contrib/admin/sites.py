50b1fb19cbb0c7244a54d34ac27fe49986c75108
ramez <ramezashraf@gmail.com>
2018-06-30 07:41:30 +0200

    A new url pattern for app_index

--- django/contrib/admin/sites.py
+++ django/contrib/admin/sites.py
@@ -64,7 +64,6 @@ class AdminSite:
         self.name = name
         self._actions = {'delete_selected': actions.delete_selected}
         self._global_actions = self._actions.copy()
-        self._app_label_url_map = {}  # to be able to reverse custom app_label urls
         all_sites.add(self)
 
     def check(self, app_configs):
@@ -265,14 +264,17 @@ class AdminSite:
         # Add in each model's views, and create a list of valid URLS for the
         # app_index
         valid_app_labels = []
+        app_label_url_pair = []
         for model, model_admin in self._registry.items():
             app_url, model_url = self.get_url_for_app_model(model, model_admin)
             urlpatterns += [
                 path('%s/%s/' % (app_url, model_url), include(model_admin.urls)),
             ]
-            if app_url not in valid_app_labels:
-                valid_app_labels.append(app_url)
-                self._app_label_url_map[app_url] = model._meta.app_label
+            if model._meta.app_label not in valid_app_labels:
+                valid_app_labels.append(model._meta.app_label)
+
+            if (model._meta.app_label, app_url) not in app_label_url_pair:
+                app_label_url_pair.append((model._meta.app_label, app_url))
 
         # If there were ModelAdmins registered, we should have a list of app
         # labels for which we need to allow access to the app_index view,
@@ -281,6 +283,15 @@ class AdminSite:
             urlpatterns += [
                 re_path(regex, wrap(self.app_index), name='app_list'),
             ]
+
+            # A new url pattern, <app>_index.
+            # reverse('admin:polls_index') = /admin/survey/
+            # "polls" is the actual app_label while "survey" is the chosen url for that app.
+            for app_label, app_url in app_label_url_pair:
+                urlpatterns += [
+                    path('%s/' % app_url, self.app_index, name='%s_index' % app_label,
+                         kwargs={'app_label': app_label})
+                ]
         return urlpatterns
 
     def get_url_for_app_model(self, model, model_admin, **kwargs):
@@ -505,7 +516,6 @@ class AdminSite:
         return TemplateResponse(request, self.index_template or 'admin/index.html', context)
 
     def app_index(self, request, app_label, extra_context=None):
-        app_label = self._app_label_url_map[app_label]
         app_dict = self._build_app_dict(request, app_label)
         if not app_dict:
             raise Http404('The requested admin page does not exist.')
