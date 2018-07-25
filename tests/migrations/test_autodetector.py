addae1f826ca1e8e126fe8ae4ae3eea4c0459b1d
Simon Charette <charette.s@gmail.com>
2018-07-24 23:51:59 -0400

    Refs #24424 -- Added regression tests for MTI-inheritance model removal.
    The actual issue was fixed as a side effect of implementing RemoveField's redution
    of DeleteModel to a DeleteModel in ad82900ad94ed4bbad050b9993373dafbe66b610.

--- tests/migrations/test_autodetector.py
+++ tests/migrations/test_autodetector.py
@@ -2354,3 +2354,13 @@ class AutodetectorTests(TestCase):
         self.assertNumberMigrations(changes, 'testapp', 1)
         self.assertOperationTypes(changes, 'testapp', 0, ["AddField", "AddField"])
         self.assertOperationAttributes(changes, 'testapp', 0, 0)
+
+    def test_mti_inheritance_model_removal(self):
+        Animal = ModelState('app', 'Animal', [
+            ("id", models.AutoField(primary_key=True)),
+        ])
+        Dog = ModelState('app', 'Dog', [], bases=('app.Animal',))
+        changes = self.get_changes([Animal, Dog], [Animal])
+        self.assertNumberMigrations(changes, 'app', 1)
+        self.assertOperationTypes(changes, 'app', 0, ['DeleteModel'])
+        self.assertOperationAttributes(changes, 'app', 0, 0, name='Dog')
