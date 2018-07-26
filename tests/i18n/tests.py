013d439ff038ca8e750e6d1dcd6965f33ad4cf3e
Tim Graham <timograham@gmail.com>
2018-07-26 16:38:08 -0400

    Refs #29600 -- Added test for datetime_safe usage in localize_input().

--- tests/i18n/tests.py
+++ tests/i18n/tests.py
@@ -949,8 +949,15 @@ class FormattingTests(SimpleTestCase):
                 )
 
     def test_localized_input_func(self):
+        tests = (
+            (True, 'True'),
+            (datetime.date(1, 1, 1), '0001-01-01'),
+            (datetime.datetime(1, 1, 1), '0001-01-01 00:00:00'),
+        )
         with self.settings(USE_THOUSAND_SEPARATOR=True):
-            self.assertEqual(localize_input(True), 'True')
+            for value, expected in tests:
+                with self.subTest(value=value):
+                    self.assertEqual(localize_input(value), expected)
 
     def test_sanitize_separators(self):
         """
