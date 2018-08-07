c169b0cfe601e2dacd79df46530e269352fa18e7
Andrew Godwin <andrew@aeracode.org>
2018-08-06 18:14:20 -0700

    Pin to pytest 3.6 because of problems with async_generator in 3.7

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     ],
     extras_require={
         'tests': [
-            'pytest~=3.6',
+            'pytest~=3.6.0',
             "pytest-django~=3.1",
             "pytest-asyncio~=0.8",
             "async_generator~=1.8",
