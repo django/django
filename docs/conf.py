e553e8e7aebabfc3b02bec8ca0819310231db6ee
Andrew Godwin <andrew@aeracode.org>
2018-08-16 22:09:09 -0700

    Fix docs copyright

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Channels'
-copyright = u'2017, Andrew Godwin'
+copyright = u'2018, Django Software Foundation'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
