ef87b38ef7b07a5a9b4ee424a04a9811836dee39
Jon Dufresne <jon.dufresne@gmail.com>
2018-08-21 07:02:03 -0700

    Fixed #29696 -- Prevented BaseModelFormSet.initial_form_count()'s from treating data={} as unbound.

--- django/forms/models.py
+++ django/forms/models.py
@@ -570,7 +570,7 @@ class BaseModelFormSet(BaseFormSet):
 
     def initial_form_count(self):
         """Return the number of forms that are required in this FormSet."""
-        if not (self.data or self.files):
+        if not self.is_bound:
             return len(self.get_queryset())
         return super().initial_form_count()
 
