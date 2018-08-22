c69d40f94308d52243133def3b6be7ff403df6f1
winkidney <winkidney@gmail.com>
2018-08-22 19:34:51 +0800

    Fixed #29705 -- Fixed ImageField RuntimeError crash for WebP files.

--- tests/files/tests.py
+++ tests/files/tests.py
@@ -343,6 +343,12 @@ class GetImageDimensionsTests(unittest.TestCase):
                 size = images.get_image_dimensions(fh)
                 self.assertEqual(size, (None, None))
 
+    def test_webp(self):
+        img_path = os.path.join(os.path.dirname(__file__), 'test.webp')
+        with open(img_path, 'rb') as fh:
+            size = images.get_image_dimensions(fh)
+        self.assertEqual(size, (540, 405))
+
 
 class FileMoveSafeTests(unittest.TestCase):
     def test_file_move_overwrite(self):
