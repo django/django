36c5c11eb0ebd4b779ab6ab807292635c61d4961
Nick Pope <nick.pope@flightdataservices.com>
2018-09-03 15:11:28 +0100

    Refs #29713 -- Improved error message from translation system check.

--- django/core/checks/translation.py
+++ django/core/checks/translation.py
@@ -1,25 +1,19 @@
-import re
-
 from django.conf import settings
 from django.utils.translation.trans_real import language_code_re
 
 from . import Error, Tags, register
 
+E001 = Error(
+    'You have provided an invalid value for the LANGUAGE_CODE setting.',
+    id='translation.E001',
+)
+
 
 @register(Tags.translation)
 def check_setting_language_code(app_configs, **kwargs):
     """
-    Errors if language code is in the wrong format. Language codes specification outlined by
-    https://en.wikipedia.org/wiki/IETF_language_tag#Syntax_of_language_tags
+    Errors if language code setting is invalid.
     """
-    match_result = re.match(language_code_re, settings.LANGUAGE_CODE)
-    errors = []
-    if not match_result:
-        errors.append(Error(
-            "LANGUAGE_CODE in settings.py is {}. It should be in the form ll or ll-cc where ll is the language and cc "
-            "is the country. Examples include: it, de-at, es, pt-br. The full set of language codes specifications is "
-            "outlined by https://en.wikipedia.org/wiki/IETF_language_tag#Syntax_of_language_tags".format(
-                settings.LANGUAGE_CODE),
-            id="translation.E001",
-        ))
-    return errors
+    if not language_code_re.match(settings.LANGUAGE_CODE):
+        return [E001]
+    return []
