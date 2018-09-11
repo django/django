1b9b6a103d11f8bb2305794a138498f0eb005db2
Tim Graham <timograham@gmail.com>
2018-09-11 11:38:19 -0400

    Fixed #29749 -- Made the migrations loader ignore files starting with a tilde or underscore.
    Regression in 29150d5da880ac1db15e47052330790cf1b802d2.

--- tests/migrations/test_loader.py
+++ tests/migrations/test_loader.py
@@ -500,6 +500,14 @@ class LoaderTests(TestCase):
         }
         self.assertEqual(plan, expected_plan)
 
+    @override_settings(MIGRATION_MODULES={'migrations': 'migrations.test_migrations_private'})
+    def test_ignore_files(self):
+        """Files prefixed with underscore, tilde, or dot aren't loaded."""
+        loader = MigrationLoader(connection)
+        loader.load_disk()
+        migrations = [name for app, name in loader.disk_migrations if app == 'migrations']
+        self.assertEqual(migrations, ['0001_initial'])
+
 
 class PycLoaderTests(MigrationTestBase):
 
