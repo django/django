ae3baefd33b83f9001b79d2a74e4da805ce87bf3
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2018-10-10 22:23:21 +0200

    Fixed #29836 -- Bumped required cx_Oracle to 6.0.

--- django/db/backends/oracle/base.py
+++ django/db/backends/oracle/base.py
@@ -390,8 +390,6 @@ class FormatStylePlaceholderCursor:
     def __init__(self, connection):
         self.cursor = connection.cursor()
         self.cursor.outputtypehandler = self._output_type_handler
-        # The default for cx_Oracle < 5.3 is 50.
-        self.cursor.arraysize = 100
 
     @staticmethod
     def _output_number_converter(value):
