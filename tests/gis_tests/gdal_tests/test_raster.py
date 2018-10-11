5a6b16302f59397a52a3ee08582fae2aa775148a
Tom Forbes <tom@tomforb.es>
2018-10-11 13:51:21 +0100

    Update comment in the test

--- tests/gis_tests/gdal_tests/test_raster.py
+++ tests/gis_tests/gdal_tests/test_raster.py
@@ -582,7 +582,8 @@ class GDALBandTests(SimpleTestCase):
             self.assertAlmostEqual(self.band.mean, 2.828326634228898)
             self.assertAlmostEqual(self.band.std, 2.4260526986669095)
 
-            # Statistics are persisted into PAM file on band close when the object is deleted.
+            # Statistics are persisted into PAM file on band close when the object 
+            # is garbage collected or reclaimed from memory.
             self.band = None
             gc.collect()
             self.assertTrue(os.path.isfile(pam_file))
