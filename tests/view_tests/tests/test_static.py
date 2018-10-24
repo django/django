6a8b57df6a5b7fd470e18880b0735af3b0ce349c
Hasan Ramezani <hasan.r67@gmail.com>
2018-10-24 02:44:27 +0200

    Completed test coverage of views.static.directory_name().

--- tests/view_tests/tests/test_static.py
+++ tests/view_tests/tests/test_static.py
@@ -111,6 +111,14 @@ class StaticTests(SimpleTestCase):
     def test_index(self):
         response = self.client.get('/%s/' % self.prefix)
         self.assertContains(response, 'Index of ./')
+        # Directories have a trailing slash.
+        self.assertIn('subdir/', response.context['file_list'])
+
+    def test_index_subdir(self):
+        response = self.client.get('/%s/subdir/' % self.prefix)
+        self.assertContains(response, 'Index of subdir/')
+        # File with a leading dot (e.g. .hidden) aren't displayed.
+        self.assertEqual(response.context['file_list'], ['visible'])
 
     @override_settings(TEMPLATES=[{
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
