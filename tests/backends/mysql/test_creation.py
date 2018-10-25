django/django

commit e1253bc26facfa1d0fca161f43925e99c2591ced

Refs #25406 -- Removed exception hiding in MySQL test database creation during --keepdb.
Thanks Adam Johnson, Simon Charette and Tim Graham for reviews.

django/db/backends/mysql/creation.py
@@ -17,24 +17,45 @@ class DatabaseCreation(BaseDatabaseCreation):
             suffix.append('COLLATE %s' % test_settings['COLLATION'])
         return ' '.join(suffix)
 
+    def _execute_create_test_db(self, cursor, parameters, keepdb=False):
+        try:
+            if keepdb:
+                # If the database should be kept, add "IF NOT EXISTS" to avoid
+                # "database exists" error, also temporarily disable "database
+                # exists" warning.
+                cursor.execute('''
+                    SET @_tmp_sql_notes := @@sql_notes, sql_notes = 0;
+                    CREATE DATABASE IF NOT EXISTS %(dbname)s %(suffix)s;
+                    SET sql_notes = @_tmp_sql_notes;
+                ''' % parameters)
+            else:
+                super()._execute_create_test_db(cursor, parameters, keepdb)
+        except Exception as e:
+            if len(e.args) < 1 or e.args[0] != 1007:
+                # All errors except "database exists" (1007) cancel tests.
+                sys.stderr.write('Got an error creating the test database: %s\n' % e)
+                sys.exit(2)
+            else:
+                raise e
+
     def _clone_test_db(self, number, verbosity, keepdb=False):
-        qn = self.connection.ops.quote_name
         source_database_name = self.connection.settings_dict['NAME']
         target_database_name = self.get_test_db_clone_settings(number)['NAME']
-
+        test_db_params = {
+            'dbname': self.connection.ops.quote_name(target_database_name),
+            'suffix': self.sql_table_creation_suffix(),
+        }
         with self._nodb_connection.cursor() as cursor:
             try:
-                cursor.execute("CREATE DATABASE %s" % qn(target_database_name))
+                self._execute_create_test_db(cursor, test_db_params, keepdb)
             except Exception:
-                if keepdb:
-                    return
                 try:
                     if verbosity >= 1:
                         print("Destroying old test database for alias %s..." % (
                             self._get_database_display_str(verbosity, target_database_name),
                         ))
-                    cursor.execute("DROP DATABASE %s" % qn(target_database_name))
-                    cursor.execute("CREATE DATABASE %s" % qn(target_database_name))
+                    cursor.execute('DROP DATABASE %(dbname)s' % test_db_params)
+                    self._execute_create_test_db(cursor, test_db_params, keepdb)
                 except Exception as e:
                     sys.stderr.write("Got an error recreating the test database: %s\n" % e)
                     sys.exit(2)

tests/backends/test_creation.py
@@ -8,6 +8,8 @@ from django.db import DEFAULT_DB_ALIAS, connection, connections
 from django.db.backends.base.creation import (
     TEST_DATABASE_PREFIX, BaseDatabaseCreation,
 )
+from django.db.backends.mysql.creation import \
+    DatabaseCreation as MySQLDatabaseCreation
 from django.db.backends.oracle.creation import \
     DatabaseCreation as OracleDatabaseCreation
 from django.db.backends.postgresql.creation import \
@@ -191,3 +193,39 @@ class OracleDatabaseCreationTests(TestCase):
                     creation._create_test_db(verbosity=0, keepdb=False)
                 with self.assertRaises(SystemExit):
                     creation._create_test_db(verbosity=0, keepdb=True)
+
+
+@unittest.skipUnless(connection.vendor == 'mysql', "MySQL specific tests")
+class MySQLDatabaseCreationTests(SimpleTestCase):
+
+    def _execute_raise_database_exists(self, cursor, parameters, keepdb=False):
+        raise DatabaseError(1007, "Can't create database '%s'; database exists" % parameters['dbname'])
+
+    def _execute_raise_access_denied(self, cursor, parameters, keepdb=False):
+        raise DatabaseError(1044, "Access denied for user")
+
+    def patch_test_db_creation(self, execute_create_test_db):
+        return mock.patch.object(BaseDatabaseCreation, '_execute_create_test_db', execute_create_test_db)
+
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    @mock.patch('sys.stderr', new_callable=StringIO)
+    def test_create_test_db_database_exists(self, *mocked_objects):
+        # Simulate test database creation raising "database exists"
+        creation = MySQLDatabaseCreation(connection)
+        with self.patch_test_db_creation(self._execute_raise_database_exists):
+            with mock.patch('builtins.input', return_value='no'):
+                with self.assertRaises(SystemExit):
+                    # SystemExit is raised if the user answers "no" to the
+                    # prompt asking if it's okay to delete the test database.
+                    creation._create_test_db(verbosity=0, autoclobber=False, keepdb=False)
+            # "Database exists" shouldn't appear when keepdb is on
+            creation._create_test_db(verbosity=0, autoclobber=False, keepdb=True)
+
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    @mock.patch('sys.stderr', new_callable=StringIO)
+    def test_create_test_db_unexpected_error(self, *mocked_objects):
+        # Simulate test database creation raising unexpected error
+        creation = MySQLDatabaseCreation(connection)
+        with self.patch_test_db_creation(self._execute_raise_access_denied):
+            with self.assertRaises(SystemExit):
+                creation._create_test_db(verbosity=0, autoclobber=False, keepdb=False)

commit c976daf5b1e3e667188e9183b3aa7e32baf46fc9

Fixed #29827 -- Fixed reuse of test databases with --keepdb on MySQL.
Regression in e1253bc26facfa1d0fca161f43925e99c2591ced.

django/db/backends/mysql/creation.py
@@ -19,17 +19,7 @@ class DatabaseCreation(BaseDatabaseCreation):
 
     def _execute_create_test_db(self, cursor, parameters, keepdb=False):
         try:
-            if keepdb:
-                # If the database should be kept, add "IF NOT EXISTS" to avoid
-                # "database exists" error, also temporarily disable "database
-                # exists" warning.
-                cursor.execute('''
-                    SET @_tmp_sql_notes := @@sql_notes, sql_notes = 0;
-                    CREATE DATABASE IF NOT EXISTS %(dbname)s %(suffix)s;
-                    SET sql_notes = @_tmp_sql_notes;
-                ''' % parameters)
-            else:
-                super()._execute_create_test_db(cursor, parameters, keepdb)
+            super()._execute_create_test_db(cursor, parameters, keepdb)
         except Exception as e:
             if len(e.args) < 1 or e.args[0] != 1007:
                 # All errors except "database exists" (1007) cancel tests.
@@ -49,6 +39,9 @@ class DatabaseCreation(BaseDatabaseCreation):
             try:
                 self._execute_create_test_db(cursor, test_db_params, keepdb)
             except Exception:
+                if keepdb:
+                    # If the database should be kept, skip everything else.
+                    return
                 try:
                     if verbosity >= 1:
                         self.log('Destroying old test database for alias %s…' % (
@@ -59,7 +52,9 @@ class DatabaseCreation(BaseDatabaseCreation):
                 except Exception as e:
                     self.log('Got an error recreating the test database: %s' % e)
                     sys.exit(2)
+        self._clone_db(source_database_name, target_database_name)
 
+    def _clone_db(self, source_database_name, target_database_name):
         dump_cmd = DatabaseClient.settings_to_cmd_args(self.connection.settings_dict)
         dump_cmd[0] = 'mysqldump'
         dump_cmd[-1] = source_database_name

tests/backends/mysql/test_creation.py
@@ -43,3 +43,10 @@ class DatabaseCreationTests(SimpleTestCase):
         with self.patch_test_db_creation(self._execute_raise_access_denied):
             with self.assertRaises(SystemExit):
                 creation._create_test_db(verbosity=0, autoclobber=False, keepdb=False)
+
+    def test_clone_test_db_database_exists(self):
+        creation = DatabaseCreation(connection)
+        with self.patch_test_db_creation(self._execute_raise_database_exists):
+            with mock.patch.object(DatabaseCreation, '_clone_db') as _clone_db:
+                creation._clone_test_db('suffix', verbosity=0, keepdb=True)
+                _clone_db.assert_not_called()