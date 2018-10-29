810677974b2597439e847aa4590152725cb7d911
Dan Davis <dan@danizen.net>
2018-10-28 22:58:55 -0400

    Fixed #29882 - add --routines when cloning mysql
    - Added '--routines' to mysqldump when cloning for parallel test run.
    - No tests provided as mysqldump is external.
    - Ticket #29882 has a reference to a repository that reproduced issue and shows it is fixed.

--- django/db/backends/mysql/creation.py
+++ django/db/backends/mysql/creation.py
@@ -57,6 +57,7 @@ class DatabaseCreation(BaseDatabaseCreation):
     def _clone_db(self, source_database_name, target_database_name):
         dump_cmd = DatabaseClient.settings_to_cmd_args(self.connection.settings_dict)
         dump_cmd[0] = 'mysqldump'
+        dump_cmd[-2] = '--routines'
         dump_cmd[-1] = source_database_name
         load_cmd = DatabaseClient.settings_to_cmd_args(self.connection.settings_dict)
         load_cmd[-1] = target_database_name
