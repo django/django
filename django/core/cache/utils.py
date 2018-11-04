c60d1630f02aa05f8b531867798ec94d9456ceeb
Joshua Cornutt <jcornutt@orocktech.com>
2018-11-03 20:52:39 -0400

    Allow make_template_fragment_key() to work on FIPS systems
    isort ordering failure on tests
    
    flake8 compliance

--- django/core/cache/utils.py
+++ django/core/cache/utils.py
@@ -8,5 +8,13 @@ def make_template_fragment_key(fragment_name, vary_on=None):
     if vary_on is None:
         vary_on = ()
     key = ':'.join(quote(str(var)) for var in vary_on)
-    args = hashlib.md5(key.encode())
+
+    # Try to generate an MD5 hash but fall up to SHA256 if in FIPS mode
+    try:
+        args = hashlib.md5(key.encode())
+    except ValueError as exc:
+        if 'disabled for fips' not in exc.message:
+            raise
+        args = hashlib.sha256(key.encode())
+
     return TEMPLATE_FRAGMENT_KEY_TEMPLATE % (fragment_name, args.hexdigest())
