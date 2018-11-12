2f120ac51722a257219a7577759702605cefddf4
Simon Charette <charettes@users.noreply.github.com>
2018-11-12 11:15:48 -0500

    Fixed #29945 -- Moved contrib.postgres uninstallation logic to the app config.

--- tests/postgres_tests/__init__.py
+++ tests/postgres_tests/__init__.py
@@ -3,19 +3,12 @@ import unittest
 from forms_tests.widget_tests.base import WidgetTest
 
 from django.db import connection
-from django.db.backends.signals import connection_created
 from django.test import TestCase, modify_settings
 
 
 @unittest.skipUnless(connection.vendor == 'postgresql', "PostgreSQL specific tests")
 class PostgreSQLTestCase(TestCase):
-    @classmethod
-    def tearDownClass(cls):
-        # No need to keep that signal overhead for non PostgreSQL-related tests.
-        from django.contrib.postgres.signals import register_type_handlers
-
-        connection_created.disconnect(register_type_handlers)
-        super().tearDownClass()
+    pass
 
 
 @unittest.skipUnless(connection.vendor == 'postgresql', "PostgreSQL specific tests")
