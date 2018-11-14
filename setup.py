c11a7b4907e58447daab4632a50f5e72d838715e
Asif Saifuddin Auvi <auvipy@gmail.com>
2018-11-14 18:44:52 +0100

    [1.11.x] Refs #28814 -- Documented Python 3.7 compatibility.
    Backport of 2f7cd7f8ecb01d30c1dfdaefa1c1714db76d2553 from master

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Internet :: WWW/HTTP :: WSGI',
