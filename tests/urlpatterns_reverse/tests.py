4d9f51f2b5a9557f3b26d023148b47d6e146a192
Xavier Fernandez <xavier.fernandez@polyconseil.fr>
2018-11-28 14:17:01 -0500

    Added test for ResolverMatch.__repr__().

--- tests/urlpatterns_reverse/tests.py
+++ tests/urlpatterns_reverse/tests.py
@@ -1125,6 +1125,14 @@ class ResolverMatchTests(SimpleTestCase):
         request = HttpRequest()
         self.assertIsNone(request.resolver_match)
 
+    def test_repr(self):
+        self.assertEqual(
+            repr(resolve('/no_kwargs/42/37/')),
+            "ResolverMatch(func=urlpatterns_reverse.views.empty_view, "
+            "args=('42', '37'), kwargs={}, url_name=no-kwargs, app_names=[], "
+            "namespaces=[])"
+        )
+
 
 @override_settings(ROOT_URLCONF='urlpatterns_reverse.erroneous_urls')
 class ErroneousViewTests(SimpleTestCase):
