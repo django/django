5a77190e6661c17384151247991e0069f6bd9d65
jtiai <jani.tiainen@tintti.net>
2018-12-21 17:44:34 -0500

    Fixed #29792 -- Made GeometryField.deconstruct() handle 'extent' and 'tolerance' args.

--- tests/gis_tests/test_fields.py
+++ tests/gis_tests/test_fields.py
@@ -27,10 +27,14 @@ class GeometryFieldTests(SimpleTestCase):
             srid=4067,
             dim=3,
             geography=True,
+            extent=(50199.4814, 6582464.0358, -50000.0, 761274.6247, 7799839.8902, 50000.0),
+            tolerance=0.01,
         )
         *_, kwargs = field.deconstruct()
         self.assertEqual(kwargs, {
             'srid': 4067,
             'dim': 3,
             'geography': True,
+            'extent': (50199.4814, 6582464.0358, -50000.0, 761274.6247, 7799839.8902, 50000.0),
+            'tolerance': 0.01,
         })
