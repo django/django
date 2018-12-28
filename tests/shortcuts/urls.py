573ec714e5f52eccde01bbac3836d0edafcb2c20
Tim Graham <timograham@gmail.com>
2018-12-27 19:23:09 -0500

    Refs #25978 -- Removed shortcuts.render_to_response() per deprecation timeline.

--- tests/shortcuts/urls.py
+++ tests/shortcuts/urls.py
@@ -3,11 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('render_to_response/', views.render_to_response_view),
-    path('render_to_response/multiple_templates/', views.render_to_response_view_with_multiple_templates),
-    path('render_to_response/content_type/', views.render_to_response_view_with_content_type),
-    path('render_to_response/status/', views.render_to_response_view_with_status),
-    path('render_to_response/using/', views.render_to_response_view_with_using),
     path('render/', views.render_view),
     path('render/multiple_templates/', views.render_view_with_multiple_templates),
     path('render/content_type/', views.render_view_with_content_type),
