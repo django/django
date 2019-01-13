5112f5319910bd5632d96a263191263a204ea621
Sergey Fedoseev <fedoseev.sergey@gmail.com>
2019-01-13 23:53:03 +0500

    Fixed #30093 -- Fixed ordering of combined queryset ordered by F expressions.

--- tests/queries/test_qs_combinators.py
+++ tests/queries/test_qs_combinators.py
@@ -110,6 +110,11 @@ class QuerySetSetOperationTests(TestCase):
         qs2 = Number.objects.filter(num__gte=2, num__lte=3)
         self.assertNumbersEqual(qs1.union(qs2).order_by('-num'), [3, 2, 1, 0])
 
+    def test_ordering_by_f_expression(self):
+        qs1 = Number.objects.filter(num__lte=1)
+        qs2 = Number.objects.filter(num__gte=2, num__lte=3)
+        self.assertNumbersEqual(qs1.union(qs2).order_by(F('num').desc()), [3, 2, 1, 0])
+
     def test_union_with_values(self):
         ReservedName.objects.create(name='a', order=2)
         qs1 = ReservedName.objects.all()
