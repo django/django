03219b5f709dcd5b0bfacd963508625557ec1ef0
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2019-01-19 13:31:15 +0100

    [2.1.x] Fixed #30117 -- Fixed SchemaEditor.quote_value() test for mysqlclient 1.4.0+.
    Backport of f05c02c4b8d4e423e57d453c4bd699dc5ff7eaa7 from master

--- tests/backends/mysql/test_schema.py
+++ tests/backends/mysql/test_schema.py
@@ -13,7 +13,7 @@ class SchemaEditorTests(TestCase):
             ('string', "'string'"),
             (42, '42'),
             (1.754, '1.754e0' if MySQLdb.version_info >= (1, 3, 14) else '1.754'),
-            (False, '0'),
+            (False, b'0' if MySQLdb.version_info >= (1, 4, 0) else '0'),
         ]
         for value, expected in tested_values:
             with self.subTest(value=value):
