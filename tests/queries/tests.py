1933e56eca1ad17de7dd133bfb7cbee9858a75a3
Sergey Fedoseev <fedoseev.sergey@gmail.com>
2019-02-09 19:18:48 +0500

    Removed uneeded generator expressions and list comprehensions.

--- tests/queries/tests.py
+++ tests/queries/tests.py
@@ -1935,9 +1935,9 @@ class RawQueriesTests(TestCase):
 
 class GeneratorExpressionTests(SimpleTestCase):
     def test_ticket10432(self):
-        # Using an empty generator expression as the rvalue for an "__in"
+        # Using an empty iterator as the rvalue for an "__in"
         # lookup is legal.
-        self.assertCountEqual(Note.objects.filter(pk__in=(x for x in ())), [])
+        self.assertCountEqual(Note.objects.filter(pk__in=iter(())), [])
 
 
 class ComparisonTests(TestCase):
