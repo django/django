a92c83828785f12dcf90477413c2d04e1855fbb9
Bernhard M. Wiedemann <bwiedemann@suse.de>
2019-02-14 10:38:18 +0100

    Fix tests in 2028
    staying in 2038 to not fail on 32-bit systems

--- tests/responses/test_cookie.py
+++ tests/responses/test_cookie.py
@@ -47,12 +47,12 @@ class SetCookieTests(SimpleTestCase):
     def test_far_expiration(self):
         """Cookie will expire when a distant expiration time is provided."""
         response = HttpResponse()
-        response.set_cookie('datetime', expires=datetime(2028, 1, 1, 4, 5, 6))
+        response.set_cookie('datetime', expires=datetime(2038, 1, 1, 4, 5, 6))
         datetime_cookie = response.cookies['datetime']
         self.assertIn(
             datetime_cookie['expires'],
             # assertIn accounts for slight time dependency (#23450)
-            ('Sat, 01 Jan 2028 04:05:06 GMT', 'Sat, 01 Jan 2028 04:05:07 GMT')
+            ('Fri, 01 Jan 2038 04:05:06 GMT', 'Fri, 01 Jan 2038 04:05:07 GMT')
         )
 
     def test_max_age_expiration(self):
