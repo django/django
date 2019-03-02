d11fd8b733866b3e11e2433d1c2ef8740fcc2554
Seth Thoburn <seththoburn@gmail.com>
2019-03-02 15:11:17 -0800

    Fixed #6135 - Added autoescape_aware decorator

---
+++ tests/template_tests/decorator_tests/test_autoescape_aware.py
@@ -0,0 +1,24 @@
+from django.template.defaultfilters import unordered_list
+from django.template.defaultfilters import autoescape_aware
+from django.test import SimpleTestCase
+
+from ..utils import setup
+
+class AutoescapeAwareTests(SimpleTestCase):
+
+    def test_autoescape(self):
+        self.assertEqual(
+            self.custom_filter('<p>test</p>'),
+            '&lt;p&gt;test&lt;/p&gt;',
+        )
+
+    def test_autoescape_off(self):
+        self.assertEqual(
+            self.custom_filter('<p>test</p>', autoescape=False),
+            '<p>test</p>',
+        )
+
+    @staticmethod
+    @autoescape_aware
+    def custom_filter(value, test=False, autoescape=True):
+        return value
