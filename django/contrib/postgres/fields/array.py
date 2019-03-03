1999c0a00aec9736a58a235d90c9ef5a6f9cff34
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2019-03-03 19:33:48 +0100

    [2.1.x] Reverted "Fixed relative paths imports per isort 4.3.5."
    This reverts commit 463fe11bc8b2d068e447c5df677e7a31c2af7e03 due to
    restore of relative paths sorting from isort < 4.3.5 in isort 4.3.10.
    Backport of b435f82939edf70674856e0e1cd63973c2e0a1d1 from master

--- django/contrib/postgres/fields/array.py
+++ django/contrib/postgres/fields/array.py
@@ -9,9 +9,9 @@ from django.db.models.lookups import Exact, In
 from django.utils.inspect import func_supports_parameter
 from django.utils.translation import gettext_lazy as _
 
+from ..utils import prefix_validation_error
 from .mixins import CheckFieldDefaultMixin
 from .utils import AttributeSetter
-from ..utils import prefix_validation_error
 
 __all__ = ['ArrayField']
 
