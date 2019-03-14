b150d9946013758a2a13eee751ac828d8582d944
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2019-03-15 00:42:01 +0100

    [2.2.x] Simplified test_transform()s in db_functions.math.
    Backport of 258110d6cdea5050f8df0bbc9af3fcd9bd342d29 from master.

--- tests/db_functions/math/test_floor.py
+++ tests/db_functions/math/test_floor.py
@@ -50,5 +50,5 @@ class FloorTests(TestCase):
         with register_lookup(DecimalField, Floor):
             DecimalModel.objects.create(n1=Decimal('5.4'), n2=Decimal('0'))
             DecimalModel.objects.create(n1=Decimal('3.4'), n2=Decimal('0'))
-            objs = DecimalModel.objects.filter(n1__floor__gt=4)
-            self.assertQuerysetEqual(objs, [Decimal('5.4')], lambda a: a.n1)
+            obj = DecimalModel.objects.filter(n1__floor__gt=4).get()
+            self.assertEqual(obj.n1, Decimal('5.4'))
--- tests/db_functions/math/test_round.py
+++ tests/db_functions/math/test_round.py
@@ -49,5 +49,5 @@ class RoundTests(TestCase):
         with register_lookup(DecimalField, Round):
             DecimalModel.objects.create(n1=Decimal('2.0'), n2=Decimal('0'))
             DecimalModel.objects.create(n1=Decimal('-1.0'), n2=Decimal('0'))
-            objs = DecimalModel.objects.filter(n1__round__gt=0)
-            self.assertQuerysetEqual(objs, [Decimal('2.0')], lambda a: a.n1)
+            obj = DecimalModel.objects.filter(n1__round__gt=0).get()
+            self.assertEqual(obj.n1, Decimal('2.0'))
