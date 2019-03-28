3cd3bebe8921e14b911b36b2a1cbceef8fb6294e
Benjy Weinberger <benjyw@gmail.com>
2019-03-28 13:47:05 -0700

    Fixed #30300 -- Allowed migrations to be loaded from directories without __init__.py file.

--- tests/migrations/test_loader.py
+++ tests/migrations/test_loader.py
@@ -508,6 +508,17 @@ class LoaderTests(TestCase):
         migrations = [name for app, name in loader.disk_migrations if app == 'migrations']
         self.assertEqual(migrations, ['0001_initial'])
 
+    @override_settings(
+        MIGRATION_MODULES={'migrations': 'migrations.test_migrations_namespace_package'},
+    )
+    def test_loading_namespace_package(self):
+        """Migration directories without an __init__.py file are loaded."""
+        migration_loader = MigrationLoader(connection)
+        self.assertEqual(
+            migration_loader.graph.forwards_plan(('migrations', '0001_initial')),
+            [('migrations', '0001_initial')],
+        )
+
 
 class PycLoaderTests(MigrationTestBase):
 
