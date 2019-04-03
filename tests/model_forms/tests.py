1ffddfc233e2d5139cc6ec31a4ec6ef70b10f87f
belegnar <admin@zilantkon.ru>
2019-04-03 09:05:15 +0200

    Fixed #30302 -- Fixed forms.model_to_dict() result if empty list of fields is passed.

--- tests/model_forms/tests.py
+++ tests/model_forms/tests.py
@@ -1814,6 +1814,7 @@ class ModelOneToOneFieldTests(TestCase):
 
         bw = BetterWriter.objects.create(name='Joe Better', score=10)
         self.assertEqual(sorted(model_to_dict(bw)), ['id', 'name', 'score', 'writer_ptr'])
+        self.assertEqual(sorted(model_to_dict(bw, fields=[])), [])
         self.assertEqual(sorted(model_to_dict(bw, fields=['id', 'name'])), ['id', 'name'])
         self.assertEqual(sorted(model_to_dict(bw, exclude=[])), ['id', 'name', 'score', 'writer_ptr'])
         self.assertEqual(sorted(model_to_dict(bw, exclude=['id', 'name'])), ['score', 'writer_ptr'])
