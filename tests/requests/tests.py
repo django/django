948d9a62827dc8a057eb5473a281e0b5b4ff2b23
Mark Tranchant <mark@tranchant.co.uk>
2019-04-03 12:22:09 +0100

    Fixed #30310 -- Added support for looking up HttpHeaders.headers using underscores.

--- tests/requests/tests.py
+++ tests/requests/tests.py
@@ -896,6 +896,7 @@ class RequestHeadersTests(SimpleTestCase):
         request = WSGIRequest(self.ENVIRON)
         self.assertEqual(request.headers['User-Agent'], 'python-requests/1.2.0')
         self.assertEqual(request.headers['user-agent'], 'python-requests/1.2.0')
+        self.assertEqual(request.headers['user_agent'], 'python-requests/1.2.0')
         self.assertEqual(request.headers['Content-Type'], 'text/html')
         self.assertEqual(request.headers['Content-Length'], '100')
 
