7be7632dfa826cb70ebc14425a545c992a95c57f
Nick Pope <nick.pope@flightdataservices.com>
2019-04-05 14:45:41 +0200

    Refs #30324 -- Forced utf-8 encoding when loading templates for the technical 404 debug and congrats page.

--- django/views/debug.py
+++ django/views/debug.py
@@ -497,7 +497,7 @@ def technical_404_response(request, exception):
             module = obj.__module__
             caller = '%s.%s' % (module, caller)
 
-    with Path(CURRENT_DIR, 'templates', 'technical_404.html').open() as fh:
+    with Path(CURRENT_DIR, 'templates', 'technical_404.html').open(encoding='utf-8') as fh:
         t = DEBUG_ENGINE.from_string(fh.read())
     c = Context({
         'urlconf': urlconf,
@@ -514,7 +514,7 @@ def technical_404_response(request, exception):
 
 def default_urlconf(request):
     """Create an empty URLconf 404 error response."""
-    with Path(CURRENT_DIR, 'templates', 'default_urlconf.html').open() as fh:
+    with Path(CURRENT_DIR, 'templates', 'default_urlconf.html').open(encoding='utf-8') as fh:
         t = DEBUG_ENGINE.from_string(fh.read())
     c = Context({
         'version': get_docs_version(),
