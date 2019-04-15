6a0fe43bb76e573efdc976fc69b3d6307a2fd23a
Vinny Do <vdo.code@gmail.com>
2019-04-15 16:34:13 +1000

    Fixed #30368 -- Fixed prefetch_related() for GenericForeignKey that uses FK as PK.

--- tests/prefetch_related/tests.py
+++ tests/prefetch_related/tests.py
@@ -891,6 +891,13 @@ class GenericRelationTests(TestCase):
         qs = Comment.objects.prefetch_related('content_object_uuid')
         self.assertEqual([c.content_object_uuid for c in qs], [article])
 
+    def test_prefetch_GFK_fk_pk(self):
+        book = Book.objects.create(title='Poems')
+        book_with_year = BookWithYear.objects.create(book=book, published_year=2019)
+        Comment.objects.create(comment='awesome', content_object=book_with_year)
+        qs = Comment.objects.prefetch_related('content_object')
+        self.assertEqual([c.content_object for c in qs], [book_with_year])
+
     def test_traverse_GFK(self):
         """
         A 'content_object' can be traversed with prefetch_related() and
