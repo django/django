95dcc9e41dc2c84bb442e6c6b1c832f4eb578109
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2019-04-19 10:17:39 +0200

    Fixed #30380 -- Handled bytes in MySQL backend for PyMySQL support.
    This commit partly reverts efd8a82e268a82b3ad0be77bd5b4548c30bcb4d7.

--- django/db/backends/mysql/schema.py
+++ django/db/backends/mysql/schema.py
@@ -31,8 +31,9 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
 
     def quote_value(self, value):
         self.connection.ensure_connection()
+        # MySQLdb escapes to string, PyMySQL to bytes.
         quoted = self.connection.connection.escape(value, self.connection.connection.encoders)
-        if isinstance(value, str):
+        if isinstance(value, str) and isinstance(quoted, bytes):
             quoted = quoted.decode()
         return quoted
 
