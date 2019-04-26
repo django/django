6866c91b638de5368c18713fa851bfe56253ea55
Jon Dufresne <jon.dufresne@gmail.com>
2019-04-26 16:37:57 -0700

    Fixed #30418 -- Added --skip-checks management command option.

--- tests/user_commands/tests.py
+++ tests/user_commands/tests.py
@@ -253,6 +253,16 @@ class CommandRunTests(AdminScriptTestCase):
         self.assertNoOutput(err)
         self.assertEqual(out.strip(), 'Set foo')
 
+    def test_skip_checks(self):
+        self.write_settings('settings.py', apps=['django.contrib.staticfiles', 'user_commands'], sdict={
+            # (staticfiles.E001) The STATICFILES_DIRS setting is not a tuple or
+            # list.
+            'STATICFILES_DIRS': '"foo"',
+        })
+        out, err = self.run_manage(['set_option', '--skip-checks', '--set', 'foo'])
+        self.assertNoOutput(err)
+        self.assertEqual(out.strip(), 'Set foo')
+
 
 class UtilsTests(SimpleTestCase):
 
