c7ba2e3cbb478d5337dc160610e4b05d062e8a03
Ran Benita <ran234@gmail.com>
2019-05-03 13:00:37 +0300

    Removed unnecessary branch in __mod__() from proxy class in lazy().
    Unnecessary since c716fe87821df00f9f03ecc761c914d1682591a2 and
    7b2f2e74adb36a4334e83130f6abc2f79d395235.

--- django/utils/functional.py
+++ django/utils/functional.py
@@ -165,8 +165,6 @@ def lazy(func, *resultclasses):
             return hash(self.__cast())
 
         def __mod__(self, rhs):
-            if self._delegate_text:
-                return str(self) % rhs
             return self.__cast() % rhs
 
         def __add__(self, other):
