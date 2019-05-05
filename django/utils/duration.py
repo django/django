d475bd84e526341d1f24e06efed66f6b55f4e64a
Udbhav Govil <udbhavgovil@gmail.com>
2019-05-05 16:47:24 +0530

    fixed duration_string for negative duration
    print(duration_string(datetime.timedelta(seconds=-5))
    before fix output:
    -1 23:59:55
    after fix output:
    - 0:00:05

--- django/utils/duration.py
+++ django/utils/duration.py
@@ -17,14 +17,21 @@ def _get_duration_components(duration):
 
 def duration_string(duration):
     """Version of str(timedelta) which is not English specific."""
-    days, hours, minutes, seconds, microseconds = _get_duration_components(duration)
+    if duration < datetime.timedelta(0):
+        sign = '-'
+        duration *= -1
+    else:
+        sign = ''
 
+    days, hours, minutes, seconds, microseconds = _get_duration_components(duration)
     string = '{:02d}:{:02d}:{:02d}'.format(hours, minutes, seconds)
     if days:
         string = '{} '.format(days) + string
     if microseconds:
         string += '.{:06d}'.format(microseconds)
 
+    if sign:
+        string = sign + string
     return string
 
 
