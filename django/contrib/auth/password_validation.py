ae05cc1f2f6bd6bd91798a2344c2731ee5c96f82
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2019-05-22 09:09:05 +0200

    Forced utf-8 encoding when loading common passwords in CommonPasswordValidator.
    Follow up to 28eac41510eb9de728bdfbc22a36f33ac75394f2.

--- django/contrib/auth/password_validation.py
+++ django/contrib/auth/password_validation.py
@@ -171,7 +171,7 @@ class CommonPasswordValidator:
 
     def __init__(self, password_list_path=DEFAULT_PASSWORD_LIST_PATH):
         try:
-            with gzip.open(password_list_path, 'rt') as f:
+            with gzip.open(password_list_path, 'rt', encoding='utf-8') as f:
                 self.passwords = {x.strip() for x in f}
         except OSError:
             with open(password_list_path) as f:
