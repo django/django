34ec52269ade54af31a021b12969913129571a3f
Carlton Gibson <carlton.gibson@noumenal.es>
2019-05-27 11:07:46 +0200

    Applied jQuery patch for CVE-2019-11358.

--- django/contrib/admin/static/admin/js/vendor/jquery/jquery.js
+++ django/contrib/admin/static/admin/js/vendor/jquery/jquery.js
@@ -261,8 +261,9 @@ jQuery.extend = jQuery.fn.extend = function() {
 				src = target[ name ];
 				copy = options[ name ];
 
+				// Prevent Object.prototype pollution
 				// Prevent never-ending loop
-				if ( target === copy ) {
+				if ( name === "__proto__" || target === copy ) {
 					continue;
 				}
 
