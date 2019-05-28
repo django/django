fcbc502af93f0ee75522c45ae6ec2925da9f2145
葛汉斌 <xiaomiba0904@gmail.com>
2019-05-28 21:40:31 +0800

    Simplified Model.save() a bit.

--- django/db/models/base.py
+++ django/db/models/base.py
@@ -678,28 +678,29 @@ class Model(metaclass=ModelBase):
             # been assigned and there's no need to worry about this check.
             if field.is_relation and field.is_cached(self):
                 obj = getattr(self, field.name, None)
+                if not obj:
+                    continue
                 # A pk may have been assigned manually to a model instance not
                 # saved to the database (or auto-generated in a case like
                 # UUIDField), but we allow the save to proceed and rely on the
                 # database to raise an IntegrityError if applicable. If
                 # constraints aren't supported by the database, there's the
                 # unavoidable risk of data corruption.
-                if obj:
-                    if obj.pk is None:
-                        # Remove the object from a related instance cache.
-                        if not field.remote_field.multiple:
-                            field.remote_field.delete_cached_value(obj)
-                        raise ValueError(
-                            "save() prohibited to prevent data loss due to "
-                            "unsaved related object '%s'." % field.name
-                        )
-                    elif getattr(self, field.attname) is None:
-                        # Use pk from related object if it has been saved after
-                        # an assignment.
-                        setattr(self, field.attname, obj.pk)
+                if obj.pk is None:
+                    # Remove the object from a related instance cache.
+                    if not field.remote_field.multiple:
+                        field.remote_field.delete_cached_value(obj)
+                    raise ValueError(
+                        "save() prohibited to prevent data loss due to "
+                        "unsaved related object '%s'." % field.name
+                    )
+                elif getattr(self, field.attname) is None:
+                    # Use pk from related object if it has been saved after
+                    # an assignment.
+                    setattr(self, field.attname, obj.pk)
                 # If the relationship's pk/to_field was changed, clear the
                 # cached relationship.
-                if obj and getattr(obj, field.target_field.attname) != getattr(self, field.attname):
+                if getattr(obj, field.target_field.attname) != getattr(self, field.attname):
                     field.delete_cached_value(self)
 
         using = using or router.db_for_write(self.__class__, instance=self)
