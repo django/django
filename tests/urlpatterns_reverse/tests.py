b24e7638465867a206f3654f61b6fb0abcc96022
daniel a rios <misterrios@gmail.com>
2019-06-19 10:59:55 +0200

    Added tests for terminated path with optional arguments in django.urls.reverse().

--- tests/urlpatterns_reverse/tests.py
+++ tests/urlpatterns_reverse/tests.py
@@ -180,6 +180,8 @@ test_data = (
     ('named_optional', '/optional/1/', [], {'arg1': 1}),
     ('named_optional', '/optional/1/2/', [1, 2], {}),
     ('named_optional', '/optional/1/2/', [], {'arg1': 1, 'arg2': 2}),
+    ('named_optional_terminated', '/optional/1/', [1], {}),
+    ('named_optional_terminated', '/optional/1/', [], {'arg1': 1}),
     ('named_optional_terminated', '/optional/1/2/', [1, 2], {}),
     ('named_optional_terminated', '/optional/1/2/', [], {'arg1': 1, 'arg2': 2}),
     ('hardcoded', '/hardcoded/', [], {}),
