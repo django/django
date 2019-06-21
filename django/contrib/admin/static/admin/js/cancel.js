60740a335b0b2bd1486a2f0eb887b852453e4d33
Markus Holtermann <info@markusholtermann.eu>
2019-06-21 12:41:37 +0200

    Bumped ESLint to 4.18.2

--- django/contrib/admin/static/admin/js/cancel.js
+++ django/contrib/admin/static/admin/js/cancel.js
@@ -4,7 +4,7 @@
         $('.cancel-link').on('click', function(e) {
             e.preventDefault();
             if (window.location.search.indexOf('&_popup=1') === -1) {
-                window.history.back();  // Go back if not a popup.
+                window.history.back(); // Go back if not a popup.
             } else {
                 window.close(); // Otherwise, close the popup.
             }
