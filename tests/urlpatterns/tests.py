d640c71fa35640b4c13107c074be2f52c52ad861
daniel a rios <misterrios@gmail.com>
2019-06-21 17:37:41 +0200

    Refs #26431 -- Added tests for resolving URL and translate_url() with provided optional parameter.

--- tests/urlpatterns/tests.py
+++ tests/urlpatterns/tests.py
@@ -54,6 +54,12 @@ class SimplifiedURLTests(SimpleTestCase):
         self.assertEqual(match.kwargs, {'pk': '1'})
         self.assertEqual(match.route, '^regex/(?P<pk>[0-9]+)/$')
 
+    def test_re_path_with_optional_parameter(self):
+        match = resolve('/regex_optional/1/2/')
+        self.assertEqual(match.url_name, 'regex_optional')
+        self.assertEqual(match.kwargs, {'arg1': '1', 'arg2': '2'})
+        self.assertEqual(match.route, r'^regex_optional/(?P<arg1>\d+)/(?:(?P<arg2>\d+)/)?')
+
     def test_path_lookup_with_inclusion(self):
         match = resolve('/included_urls/extra/something/')
         self.assertEqual(match.url_name, 'inner-extra')
