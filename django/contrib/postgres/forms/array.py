c238e65e29494042d6ccf925c0a06a3135619799
Chason Chaffin <chason@gmail.com>
2019-07-03 18:32:43 +0900

    Fixed #30596 -- Fixed SplitArrayField.has_changed() for non-string base fields.
    Thanks to Evgeniy Krysanov for the report and the idea to use to_python.
    Thanks to Mariusz Felisiak for the test case.

--- django/contrib/postgres/forms/array.py
+++ django/contrib/postgres/forms/array.py
@@ -178,6 +178,10 @@ class SplitArrayField(forms.Field):
         kwargs.setdefault('widget', widget)
         super().__init__(**kwargs)
 
+    def to_python(self, value):
+        value = super().to_python(value)
+        return [self.base_field.to_python(item) for item in value]
+
     def clean(self, value):
         cleaned_data = []
         errors = []
