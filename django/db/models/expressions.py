52545e788d664040abf4f1a5d77cdfc61152ffca
can <cansarigol@derinbilgi.com.tr>
2019-07-10 15:07:48 +0300

    Fixed #28289 -- Fixed crash of RawSQL annotations on inherited model fields.

--- django/db/models/expressions.py
+++ django/db/models/expressions.py
@@ -699,6 +699,16 @@ class RawSQL(Expression):
     def get_group_by_cols(self, alias=None):
         return [self]
 
+    def resolve_expression(self, query=None, allow_joins=True, reuse=None, summarize=False, for_save=False):
+        # Resolve parents fields used in raw SQL.
+        for parent in query.model._meta.get_parent_list():
+            for parent_field in parent._meta.local_fields:
+                _, column_name = parent_field.get_attname_column()
+                if column_name.lower() in self.sql.lower():
+                    query.resolve_ref(parent_field.name, allow_joins, reuse, summarize)
+                    break
+        return super().resolve_expression(query, allow_joins, reuse, summarize, for_save)
+
 
 class Star(Expression):
     def __repr__(self):
