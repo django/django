7991111af12056ec9a856f35935d273526338c1f
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2019-07-10 10:33:36 +0200

    Fixed #30621 -- Fixed crash of __contains lookup for Date/DateTimeRangeField when the right hand side is the same type.
    Thanks Tilman Koschnick for the report and initial patch.
    Thanks Carlton Gibson the review.
    
    Regression in 6b048b364ca1e0e56a0d3815bf2be33ac9998355.

--- django/contrib/postgres/fields/ranges.py
+++ django/contrib/postgres/fields/ranges.py
@@ -170,7 +170,12 @@ class DateTimeRangeContains(models.Lookup):
         params = lhs_params + rhs_params
         # Cast the rhs if needed.
         cast_sql = ''
-        if isinstance(self.rhs, models.Expression) and self.rhs._output_field_or_none:
+        if (
+            isinstance(self.rhs, models.Expression) and
+            self.rhs._output_field_or_none and
+            # Skip cast if rhs has a matching range type.
+            not isinstance(self.rhs._output_field_or_none, self.lhs.output_field.__class__)
+        ):
             cast_internal_type = self.lhs.output_field.base_field.get_internal_type()
             cast_sql = '::{}'.format(connection.data_types.get(cast_internal_type))
         return '%s @> %s%s' % (lhs, rhs, cast_sql), params
