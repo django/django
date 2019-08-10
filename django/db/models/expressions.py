088a6fab1ccd1efcd4ce28b353df3c03dcfa0caf
Simon Charette <simon.charette@zapier.com>
2019-08-09 21:17:45 -0400

    Refs #24793 -- Removed bogus connection argument from SQLCompiler.compile() calls.
    The method doesn't expect a connection object to be passed as its second
    argument.

--- django/db/models/expressions.py
+++ django/db/models/expressions.py
@@ -492,8 +492,8 @@ class TemporalSubtraction(CombinedExpression):
 
     def as_sql(self, compiler, connection):
         connection.ops.check_expression_support(self)
-        lhs = compiler.compile(self.lhs, connection)
-        rhs = compiler.compile(self.rhs, connection)
+        lhs = compiler.compile(self.lhs)
+        rhs = compiler.compile(self.rhs)
         return connection.ops.subtract_temporals(self.lhs.output_field.get_internal_type(), lhs, rhs)
 
 
