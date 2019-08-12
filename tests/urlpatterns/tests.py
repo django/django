4f7328ce8a35160d155c41d362c3d674f8ef4d2d
Min ho Kim <minho42@gmail.com>
2019-08-12 20:53:36 +1000

    Corrected multiple typos.

--- tests/urlpatterns/tests.py
+++ tests/urlpatterns/tests.py
@@ -35,7 +35,7 @@ class SimplifiedURLTests(SimpleTestCase):
         self.assertEqual(match.kwargs, {'year': 2015})
         self.assertEqual(match.route, 'articles/<int:year>/')
 
-    def test_path_lookup_with_multiple_paramaters(self):
+    def test_path_lookup_with_multiple_parameters(self):
         match = resolve('/articles/2015/04/12/')
         self.assertEqual(match.url_name, 'articles-year-month-day')
         self.assertEqual(match.args, ())
