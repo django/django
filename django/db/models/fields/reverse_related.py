00035672a460b6eb5442d2837bc783f8af28c6f3
zeyneloz <ozdemir.zynl@gmail.com>
2019-08-15 06:54:41 +0200

    Fixed #30449 -- Fixed RelatedFieldListFilter/RelatedOnlyFieldListFilter to respect model's Meta.ordering.
    Regression in 6d4e5feb79f7eabe8a0c7c4b87f25b1a7f87ca0b.
    
    Co-Authored-By: Mariusz Felisiak <felisiak.mariusz@gmail.com>

--- django/db/models/fields/reverse_related.py
+++ django/db/models/fields/reverse_related.py
@@ -122,8 +122,11 @@ class ForeignObjectRel(FieldCacheMixin):
         Analog of django.db.models.fields.Field.get_choices(), provided
         initially for utilization by RelatedFieldListFilter.
         """
+        qs = self.related_model._default_manager.all()
+        if ordering:
+            qs = qs.order_by(*ordering)
         return (blank_choice if include_blank else []) + [
-            (x.pk, str(x)) for x in self.related_model._default_manager.order_by(*ordering)
+            (x.pk, str(x)) for x in qs
         ]
 
     def is_hidden(self):
