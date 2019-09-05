0f2f517298efbb204ac3ae284d2fbbaa5f70cd38
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2019-09-05 14:15:25 +0200

    Bumped version; master is now 3.1 pre-alpha.

--- docs/conf.py
+++ docs/conf.py
@@ -76,7 +76,7 @@ copyright = 'Django Software Foundation and contributors'
 # built documents.
 #
 # The short X.Y version.
-version = '3.0'
+version = '3.1'
 # The full version, including alpha/beta/rc tags.
 try:
     from django import VERSION, get_version
@@ -92,7 +92,7 @@ else:
     release = django_release()
 
 # The "development version" of Django
-django_next_version = '3.0'
+django_next_version = '3.1'
 
 extlinks = {
     'commit': ('https://github.com/django/django/commit/%s', ''),
