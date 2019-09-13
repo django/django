7fe660e9a2b2fd7d3818c6f3fb2c4bd3e5fab1bb
bunseokbot <bunseokbot@gmail.com>
2019-09-13 17:49:47 +0900

    Refs #30420 -- Change autoreload sorting

--- django/core/handlers/wsgi.py
+++ django/core/handlers/wsgi.py
@@ -6,9 +6,9 @@ from django.core import signals
 from django.core.handlers import base
 from django.http import HttpRequest, QueryDict, parse_cookie
 from django.urls import set_script_prefix
+from django.utils.autoreload import request_locker
 from django.utils.encoding import repercent_broken_unicode
 from django.utils.functional import cached_property
-from django.utils.autoreload import request_locker
 
 _slashes_re = re.compile(br'/+')
 
