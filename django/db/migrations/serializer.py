df0c2ac3586a80a5e37cca3bcb2d4e7ed333d28e
Hasan Ramezani <hasan.r67@gmail.com>
2019-09-24 23:42:35 +0200

    [3.0.x] Fixed #30774 -- Made serialization in migrations use members names for Enums.
    Backport of f0adf3b9b7a19cdee05368ff0c0c2d087f011180 from master

--- django/db/migrations/serializer.py
+++ django/db/migrations/serializer.py
@@ -120,9 +120,10 @@ class EnumSerializer(BaseSerializer):
     def serialize(self):
         enum_class = self.value.__class__
         module = enum_class.__module__
-        v_string, v_imports = serializer_factory(self.value.value).serialize()
-        imports = {'import %s' % module, *v_imports}
-        return "%s.%s(%s)" % (module, enum_class.__name__, v_string), imports
+        return (
+            '%s.%s[%r]' % (module, enum_class.__name__, self.value.name),
+            {'import %s' % module},
+        )
 
 
 class FloatSerializer(BaseSimpleSerializer):
