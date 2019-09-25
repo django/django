fe3ac38f075604338a817d0c264a2773ee2a2e6a
Farhaan Bukhsh <farhaan.bukhsh@gmail.com>
2019-09-25 16:17:22 -0400

    Fixed #30803 -- Allowed comma separators for milliseconds in django.utils.dateparse functions.
    Co-Authored-By: Ben Wilber <benwilber@gmail.com>

--- django/utils/dateparse.py
+++ django/utils/dateparse.py
@@ -16,13 +16,13 @@ date_re = _lazy_re_compile(
 
 time_re = _lazy_re_compile(
     r'(?P<hour>\d{1,2}):(?P<minute>\d{1,2})'
-    r'(?::(?P<second>\d{1,2})(?:\.(?P<microsecond>\d{1,6})\d{0,6})?)?'
+    r'(?::(?P<second>\d{1,2})(?:[\.,](?P<microsecond>\d{1,6})\d{0,6})?)?'
 )
 
 datetime_re = _lazy_re_compile(
     r'(?P<year>\d{4})-(?P<month>\d{1,2})-(?P<day>\d{1,2})'
     r'[T ](?P<hour>\d{1,2}):(?P<minute>\d{1,2})'
-    r'(?::(?P<second>\d{1,2})(?:\.(?P<microsecond>\d{1,6})\d{0,6})?)?'
+    r'(?::(?P<second>\d{1,2})(?:[\.,](?P<microsecond>\d{1,6})\d{0,6})?)?'
     r'(?P<tzinfo>Z|[+-]\d{2}(?::?\d{2})?)?$'
 )
 
@@ -33,7 +33,7 @@ standard_duration_re = _lazy_re_compile(
     r'((?:(?P<hours>\d+):)(?=\d+:\d+))?'
     r'(?:(?P<minutes>\d+):)?'
     r'(?P<seconds>\d+)'
-    r'(?:\.(?P<microseconds>\d{1,6})\d{0,6})?'
+    r'(?:[\.,](?P<microseconds>\d{1,6})\d{0,6})?'
     r'$'
 )
 
