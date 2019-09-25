e9dcf20daf36e5fb971a5eb7e5365f9fca553f86
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2019-09-25 08:39:46 +0200

    Fixed #30800 -- Fixed migrations crash when altering a field with custom db_type().
    Regression in 1378d665a1c85897d951f2ca9618b848fdbba2e7.

--- tests/schema/tests.py
+++ tests/schema/tests.py
@@ -875,6 +875,26 @@ class SchemaTests(TransactionTestCase):
             with self.assertRaisesMessage(DataError, msg):
                 editor.alter_field(Author, old_field, new_field, strict=True)
 
+    @unittest.skipUnless(connection.vendor == 'postgresql', 'PostgreSQL specific')
+    def test_alter_field_with_custom_db_type(self):
+        from django.contrib.postgres.fields import ArrayField
+
+        class Foo(Model):
+            field = ArrayField(CharField(max_length=255))
+
+            class Meta:
+                app_label = 'schema'
+
+        with connection.schema_editor() as editor:
+            editor.create_model(Foo)
+        self.isolated_local_models = [Foo]
+        old_field = Foo._meta.get_field('field')
+        new_field = ArrayField(CharField(max_length=16))
+        new_field.set_attributes_from_name('field')
+        new_field.model = Foo
+        with connection.schema_editor() as editor:
+            editor.alter_field(Foo, old_field, new_field, strict=True)
+
     def test_alter_textfield_to_null(self):
         """
         #24307 - Should skip an alter statement on databases with
