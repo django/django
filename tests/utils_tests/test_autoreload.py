38af2579881ed476ecd6bbde5b2329fa22d54210
Pablo García <poli@devartis.com>
2019-09-26 14:55:25 -0700

    [2.2.x] Fixed #30810 -- Fixed WatchmanReloaderTests.test_setting_timeout_from_environment_variable test.
    client_timeout is an instance attribute.
    
    Backport of 2fd610eb301dc01e100befed891e8007fd2e981f from master

--- tests/utils_tests/test_autoreload.py
+++ tests/utils_tests/test_autoreload.py
@@ -666,7 +666,7 @@ class WatchmanReloaderTests(ReloaderTests, IntegrationTests):
 
     @mock.patch.dict(os.environ, {'DJANGO_WATCHMAN_TIMEOUT': '10'})
     def test_setting_timeout_from_environment_variable(self):
-        self.assertEqual(self.RELOADER_CLS.client_timeout, 10)
+        self.assertEqual(self.RELOADER_CLS().client_timeout, 10)
 
 
 class StatReloaderTests(ReloaderTests, IntegrationTests):
