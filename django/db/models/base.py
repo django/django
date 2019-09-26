c7944628a1979453468d67818c63957532d396d8
Hasan Ramezani <hasan.r67@gmail.com>
2019-09-26 10:33:54 +0200

    Refs #30798 -- Prevented chaining fields from the same related model multiple times in model Meta.ordering.

--- django/db/models/base.py
+++ django/db/models/base.py
@@ -1711,6 +1711,8 @@ class Model(metaclass=ModelBase):
                     fld = _cls._meta.get_field(part)
                     if fld.is_relation:
                         _cls = fld.get_path_info()[-1].to_opts.model
+                    else:
+                        _cls = None
                 except (FieldDoesNotExist, AttributeError):
                     if fld is None or fld.get_transform(part) is None:
                         errors.append(
