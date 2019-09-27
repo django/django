95a11578ce41de1e152fe7d173f109e280aebd6d
Hasan Ramezani <hasan.r67@gmail.com>
2019-09-27 12:16:26 +0200

    Fixed #30798 -- Fixed Meta.ordering validation for pk of related fields.
    Regression in 440505cb2cadbe1a5b9fba246bcde6c04f51d07e.

--- tests/invalid_models_tests/test_models.py
+++ tests/invalid_models_tests/test_models.py
@@ -844,6 +844,18 @@ class OtherModelTests(SimpleTestCase):
         with register_lookup(models.CharField, Lower):
             self.assertEqual(Model.check(), [])
 
+    def test_ordering_pointing_to_related_model_pk(self):
+        class Parent(models.Model):
+            pass
+
+        class Child(models.Model):
+            parent = models.ForeignKey(Parent, models.CASCADE)
+
+            class Meta:
+                ordering = ('parent__pk',)
+
+        self.assertEqual(Child.check(), [])
+
     def test_ordering_pointing_to_foreignkey_field(self):
         class Parent(models.Model):
             pass
