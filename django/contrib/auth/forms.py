632ab36436b0d1cbe1d9c8262cf3ff75867e9098
Thijs <thijss@pm.me>
2019-10-05 18:14:19 +0200

    fix syntax error

--- django/contrib/auth/forms.py
+++ django/contrib/auth/forms.py
@@ -12,7 +12,7 @@ from django.contrib.auth.tokens import default_token_generator
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import EmailMultiAlternatives
 from django.template import loader
-from django template.exceptions import TemplateDoesNotExist
+from django.template.exceptions import TemplateDoesNotExist
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 from django.utils.text import capfirst
