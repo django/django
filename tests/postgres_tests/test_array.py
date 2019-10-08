5227101787007b255b0de9eda9b261bcbbe81b15
Simon Charette <simon.charette@zapier.com>
2019-10-08 09:56:36 -0400

    Defined output_field of Func() in test_grouping_by_annotations_with_array_field_param().
    output_field cannot be automatically determined because the first
    argument passed to ARRAY_LEN is an ArrayField and the second one is an
    integer.

--- tests/postgres_tests/test_array.py
+++ tests/postgres_tests/test_array.py
@@ -489,7 +489,9 @@ class TestQuerying(PostgreSQLTestCase):
         value = models.Value([1], output_field=ArrayField(models.IntegerField()))
         self.assertEqual(
             NullableIntegerArrayModel.objects.annotate(
-                array_length=models.Func(value, 1, function='ARRAY_LENGTH'),
+                array_length=models.Func(
+                    value, 1, function='ARRAY_LENGTH', output_field=models.IntegerField(),
+                ),
             ).values('array_length').annotate(
                 count=models.Count('pk'),
             ).get()['array_length'],
