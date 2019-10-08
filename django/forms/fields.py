7c70aa8f6376d761bd6d4bc075d232ee6e91f8f2
Hasan Ramezani <hasan.r67@gmail.com>
2019-10-08 09:38:28 +0200

    [3.0.x] Fixed #30839 -- Fixed Field.__deepcopy__() so forms don't share error messages.
    Backport of a28d1b38e55cf588cfaae97de6a575d5c9f90a96 from master

--- django/forms/fields.py
+++ django/forms/fields.py
@@ -201,6 +201,7 @@ class Field:
         result = copy.copy(self)
         memo[id(self)] = result
         result.widget = copy.deepcopy(self.widget, memo)
+        result.error_messages = self.error_messages.copy()
         result.validators = self.validators[:]
         return result
 
