a28d1b38e55cf588cfaae97de6a575d5c9f90a96
Hasan Ramezani <hasan.r67@gmail.com>
2019-10-08 09:38:28 +0200

    Fixed #30839 -- Fixed Field.__deepcopy__() so forms don't share error messages.

--- django/forms/fields.py
+++ django/forms/fields.py
@@ -199,6 +199,7 @@ class Field:
         result = copy.copy(self)
         memo[id(self)] = result
         result.widget = copy.deepcopy(self.widget, memo)
+        result.error_messages = self.error_messages.copy()
         result.validators = self.validators[:]
         return result
 
--- tests/forms_tests/tests/test_forms.py
+++ tests/forms_tests/tests/test_forms.py
@@ -3685,6 +3685,17 @@ Good luck picking a username that doesn&#x27;t already exist.</p>
         self.assertIsInstance(p.data, MultiValueDict)
         self.assertIsInstance(p.files, MultiValueDict)
 
+    def test_field_deep_copy_error_messages(self):
+        class CustomCharField(CharField):
+            def __init__(self, **kwargs):
+                kwargs['error_messages'] = {'invalid': 'Form custom error message.'}
+                super().__init__(**kwargs)
+
+        field = CustomCharField()
+        field_copy = copy.deepcopy(field)
+        self.assertIsInstance(field_copy, CustomCharField)
+        self.assertIsNot(field_copy.error_messages, field.error_messages)
+
 
 class CustomRenderer(DjangoTemplates):
     pass
