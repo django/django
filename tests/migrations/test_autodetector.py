bab3ad54ff30848a16f08d72e70d8edbab019a63
Simon Charette <simon.charette@zapier.com>
2019-10-13 21:54:10 -0400

    [3.0.x] Fixed #30868 -- Prevented unnecessary AlterField when renaming a referenced pk.
    Regression introduced by dcdd219ee1, refs #25817.
    
    Thanks Carlos E. C. Leite for the report and Mariusz for the bisect.
    
    Backport of 2839659b42ef80038152768b6cedae1016c59d90 from master

--- tests/migrations/test_autodetector.py
+++ tests/migrations/test_autodetector.py
@@ -932,6 +932,30 @@ class AutodetectorTests(TestCase):
             changes, 'app', 0, 1, model_name='bar', old_name='second', new_name='second_renamed',
         )
 
+    def test_rename_referenced_primary_key(self):
+        before = [
+            ModelState('app', 'Foo', [
+                ('id', models.CharField(primary_key=True, serialize=False)),
+            ]),
+            ModelState('app', 'Bar', [
+                ('id', models.AutoField(primary_key=True)),
+                ('foo', models.ForeignKey('app.Foo', models.CASCADE)),
+            ]),
+        ]
+        after = [
+            ModelState('app', 'Foo', [
+                ('renamed_id', models.CharField(primary_key=True, serialize=False))
+            ]),
+            ModelState('app', 'Bar', [
+                ('id', models.AutoField(primary_key=True)),
+                ('foo', models.ForeignKey('app.Foo', models.CASCADE)),
+            ]),
+        ]
+        changes = self.get_changes(before, after, MigrationQuestioner({'ask_rename': True}))
+        self.assertNumberMigrations(changes, 'app', 1)
+        self.assertOperationTypes(changes, 'app', 0, ['RenameField'])
+        self.assertOperationAttributes(changes, 'app', 0, 0, old_name='id', new_name='renamed_id')
+
     def test_rename_field_preserved_db_column(self):
         """
         RenameField is used if a field is renamed and db_column equal to the
