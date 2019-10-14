d2f02aa56b57f8b3de1b624f8b1a909c67b6823a
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2019-10-14 09:37:45 +0200

    [2.2.x] Fixed #30870 -- Fixed showing that RunPython operations are irreversible by migrate --plan.
    Thanks Hasan Ramezani for the initial patch and Kyle Dickerson for the
    report.
    
    Backport of 06d34aab7cfb1632a1538a243db81f24498525ff from master.

--- django/core/management/commands/migrate.py
+++ django/core/management/commands/migrate.py
@@ -344,21 +344,21 @@ class Command(BaseCommand):
     def describe_operation(operation, backwards):
         """Return a string that describes a migration operation for --plan."""
         prefix = ''
+        is_error = False
         if hasattr(operation, 'code'):
             code = operation.reverse_code if backwards else operation.code
-            action = code.__doc__ if code else ''
+            action = (code.__doc__ or '') if code else None
         elif hasattr(operation, 'sql'):
             action = operation.reverse_sql if backwards else operation.sql
         else:
             action = ''
             if backwards:
                 prefix = 'Undo '
-        if action is None:
+        if action is not None:
+            action = str(action).replace('\n', '')
+        elif backwards:
             action = 'IRREVERSIBLE'
             is_error = True
-        else:
-            action = str(action).replace('\n', '')
-            is_error = False
         if action:
             action = ' -> ' + action
         truncated = Truncator(action)
