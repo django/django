873a1879cd104e13c052b49ba2c59aaf857bec81
HassanKadhem <hassan.kadhem+github@gmail.com>
2019-10-18 23:56:29 +0300

    added a custom remote user backend
    CaseInsensitiveRemoteUserBackend guarantees that all users created using an external authentication service are created with an all-lower case username. This is important because some external authentication services return a case-sensitive username upon login. This is problematic with Django as the built-in user model is case-sensitive. This means a single remote user may have two different records in that database (e.g. 'Username' is different than 'username').

--- django/contrib/auth/backends.py
+++ django/contrib/auth/backends.py
@@ -231,3 +231,16 @@ class RemoteUserBackend(ModelBackend):
 class AllowAllUsersRemoteUserBackend(RemoteUserBackend):
     def user_can_authenticate(self, user):
         return True
+    
+    
+class LowerCaseRemoteUserBackend(RemoteUserBackend):
+    """
+    This backend makes sure all users created using an external authentication
+    service are created with an all lower-case username to avoid case-sensitivty 
+    problems. 
+    """
+    def clean_username(self, username):
+        """
+        Returns an all lower-case username
+        """
+        return username.lower()
