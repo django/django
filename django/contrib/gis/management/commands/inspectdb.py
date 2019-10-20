933f0ab9833dc09db1106d2640e28447c5168ecf
Pavel White <pahaz.blinov@gmail.com>
2019-10-20 22:28:32 +0300

    Fix inspectdb for contrib.gis

--- django/contrib/gis/management/commands/inspectdb.py
+++ django/contrib/gis/management/commands/inspectdb.py
@@ -14,3 +14,13 @@ class Command(InspectDBCommand):
             field_type, geo_params = connection.introspection.get_geometry_type(table_name, row)
             field_params.update(geo_params)
         return field_type, field_params, field_notes
+
+    def _get_field_constrains_params(self, connection, constraints, table_name, column_name,
+                                     is_relation, is_primary_key, row):
+        extra_params, field_notes = super()._get_field_constrains_params(
+            connection, constraints, table_name, column_name,
+            is_relation, is_primary_key, row)
+        field_type, _, _ = super().get_field_type(connection, table_name, row)
+        if field_type == 'GeometryField' and extra_params.get('db_index'):
+            del extra_params['db_index']
+        return extra_params, field_notes
