dbcd7b064e7278614f29fc45468d461e263d4da7
Carlton Gibson <carlton.gibson@noumenal.es>
2019-10-23 16:14:06 +0200

    Fixed #30902 -- Added __str__() for model choice enums.
    Allows expected behavior when cast to str, also matching behaviour of
    created instances with those fetched from the DB.
    
    Thanks to Simon Charette, Nick Pope, and Shai Berger for reviews.

--- tests/model_enums/tests.py
+++ tests/model_enums/tests.py
@@ -143,6 +143,12 @@ class ChoicesTests(SimpleTestCase):
                 APPLE = 1, 'Apple'
                 PINEAPPLE = 1, 'Pineapple'
 
+    def test_str(self):
+        for test in [Gender, Suit, YearInSchool, Vehicle]:
+            for member in test:
+                with self.subTest(member=member):
+                    self.assertEqual(str(test[member.name]), str(member.value))
+
 
 class Separator(bytes, models.Choices):
     FS = b'\x1c', 'File Separator'
