e60f7d6170a4f44cac149f8a2a2ea99cb72fa1db
Bastien Vallet <bastien.vallet@gmail.com>
2019-10-24 13:08:52 +0200

    [2.2.x] Refs #29926 -- Added Python 3.8 to classifiers and tox.ini.
    Backport of 37f02c47f8eb4d6e7b3c22401dba57b00780dc5d from master.

--- setup.py
+++ setup.py
@@ -101,6 +101,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
