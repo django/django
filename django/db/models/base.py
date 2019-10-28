63e6ee1f996e16a1a6238fed16fdb28bce156bc6
chetan22 <ck.chetan20@gmail.com>
2019-10-28 10:58:40 +0530

    Fixed #29871 -- Allowed setting pk=None on a child model to create a copy.
    Thanks Simon Charette and Tim Graham for the initial patch.

--- django/db/models/base.py
+++ django/db/models/base.py
@@ -569,6 +569,9 @@ class Model(metaclass=ModelBase):
         return getattr(self, meta.pk.attname)
 
     def _set_pk_val(self, value):
+        for parent_link in self._meta.parents.values():
+            if parent_link and parent_link != self._meta.pk:
+                setattr(self, parent_link.target_field.attname, value)
         return setattr(self, self._meta.pk.attname, value)
 
     pk = property(_get_pk_val, _set_pk_val)
