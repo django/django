85efc14a2edac532df1a9ad4dd9b6d4a4dcf583e
Jon Dufresne <jon.dufresne@gmail.com>
2019-11-07 15:53:30 +0100

    Fixed #30948 -- Changed packaging to use declarative config in setup.cfg.
    Co-authored-by: Nick Pope <nick.pope@flightdataservices.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import sys
 from distutils.sysconfig import get_python_lib
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 CURRENT_PYTHON = sys.version_info[:2]
 REQUIRED_PYTHON = (3, 6)
@@ -52,64 +52,7 @@ if "install" in sys.argv:
             break
 
 
-# Dynamically calculate the version based on django.VERSION.
-version = __import__('django').get_version()
-
-
-def read(fname):
-    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
-        return f.read()
-
-
-setup(
-    name='Django',
-    version=version,
-    python_requires='>={}.{}'.format(*REQUIRED_PYTHON),
-    url='https://www.djangoproject.com/',
-    author='Django Software Foundation',
-    author_email='foundation@djangoproject.com',
-    description=('A high-level Python Web framework that encourages '
-                 'rapid development and clean, pragmatic design.'),
-    long_description=read('README.rst'),
-    license='BSD-3-Clause',
-    packages=find_packages(),
-    include_package_data=True,
-    scripts=['django/bin/django-admin.py'],
-    entry_points={'console_scripts': [
-        'django-admin = django.core.management:execute_from_command_line',
-    ]},
-    install_requires=['pytz', 'sqlparse >= 0.2.2', 'asgiref'],
-    extras_require={
-        "bcrypt": ["bcrypt"],
-        "argon2": ["argon2-cffi >= 16.1.0"],
-    },
-    zip_safe=False,
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Internet :: WWW/HTTP :: WSGI',
-        'Topic :: Software Development :: Libraries :: Application Frameworks',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
-    project_urls={
-        'Documentation': 'https://docs.djangoproject.com/',
-        'Funding': 'https://www.djangoproject.com/fundraising/',
-        'Source': 'https://github.com/django/django',
-        'Tracker': 'https://code.djangoproject.com/',
-    },
-)
+setup()
 
 
 if overlay_warning:
