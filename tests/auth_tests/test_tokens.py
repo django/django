ca0d50f34ad14be2eca3067011c659ef0e66a0bc
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2019-11-13 10:33:32 +0100

    Fixed random auth_tests.test_tokens.TokenGeneratorTest.test_10265 failures.
    Random failures depended on the current timestamp.

--- tests/auth_tests/test_tokens.py
+++ tests/auth_tests/test_tokens.py
@@ -27,12 +27,11 @@ class TokenGeneratorTest(TestCase):
         The token generated for a user created in the same request
         will work correctly.
         """
-        # See ticket #10265
         user = User.objects.create_user('comebackkid', 'test3@example.com', 'testpw')
-        p0 = PasswordResetTokenGenerator()
+        user_reload = User.objects.get(username='comebackkid')
+        p0 = MockedPasswordResetTokenGenerator(datetime.now())
         tk1 = p0.make_token(user)
-        reload = User.objects.get(username='comebackkid')
-        tk2 = p0.make_token(reload)
+        tk2 = p0.make_token(user_reload)
         self.assertEqual(tk1, tk2)
 
     def test_timeout(self):
