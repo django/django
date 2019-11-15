1c75af9090a955339658d790fdcf9117e2989fbb
Baptiste Mispelon <bmispelon@gmail.com>
2019-11-15 17:28:48 +0100

    [3.0.x] Fixed #30990 -- Fixed example output in 'z' date format docs.
    Backport of 1185c6172b4dd5482b7bc76b12d7a0588320e027 from master

--- django/utils/dateformat.py
+++ django/utils/dateformat.py
@@ -348,7 +348,7 @@ class DateFormat(TimeFormat):
         return self.data.year
 
     def z(self):
-        "Day of the year; i.e. '0' to '365'"
+        """Day of the year, i.e. 1 to 366."""
         doy = self.year_days[self.data.month] + self.data.day
         if self.L() and self.data.month > 2:
             doy += 1
