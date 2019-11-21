django/django

commit d2afa5eb2308e672b6313876856e32e2561b90f3

Fixed #28860 -- Removed unnecessary len() calls.

docs/_ext/djangodocs.py
@@ -277,7 +277,7 @@ class DjangoHTMLTranslator(HTMLTranslator):
         if version_text:
             title = "%s%s" % (
                 version_text % node['version'],
-                ":" if len(node) else "."
+                ":" if node else "."
             )
             self.body.append('<span class="title">%s</span> ' % title)
 

django/contrib/admin/tests.py
@@ -158,7 +158,7 @@ class AdminSeleniumTestCase(SeleniumTestCase, StaticLiveServerTestCase):
             # to be the case.
             with self.disable_implicit_wait():
                 self.wait_until(
-                    lambda driver: len(driver.find_elements_by_css_selector(options_selector)) == 0
+                    lambda driver: not driver.find_elements_by_css_selector(options_selector)
                 )
 
     def assertSelectOptions(self, selector, values):

django/contrib/contenttypes/admin.py
@@ -25,7 +25,7 @@ class GenericInlineModelAdminChecks(InlineModelAdminChecks):
             f for f in obj.model._meta.private_fields
             if isinstance(f, GenericForeignKey)
         ]
-        if len(gfks) == 0:
+        if not gfks:
             return [
                 checks.Error(
                     "'%s.%s' has no GenericForeignKey." % (

django/contrib/flatpages/templatetags/flatpages.py
@@ -74,8 +74,7 @@ def get_flatpages(parser, token):
                       "['url_starts_with'] [for user] as context_name" %
                       {'tag_name': bits[0]})
     # Must have at 3-6 bits in the tag
-    if len(bits) >= 3 and len(bits) <= 6:
-
+    if 3 <= len(bits) <= 6:
         # If there's an even number of bits, there's no prefix
         if len(bits) % 2 == 0:
             prefix = bits[1]

django/contrib/gis/geoip2/resources.py
@@ -9,7 +9,7 @@ def City(response):
         'latitude': response.location.latitude,
         'longitude': response.location.longitude,
         'postal_code': response.postal.code,
-        'region': response.subdivisions[0].iso_code if len(response.subdivisions) else None,
+        'region': response.subdivisions[0].iso_code if response.subdivisions else None,
         'time_zone': response.location.time_zone,
     }
 

django/contrib/gis/geos/polygon.py
@@ -37,7 +37,7 @@ class Polygon(GEOSGeometry):
 
         # If initialized as Polygon(shell, (LinearRing, LinearRing)) [for backward-compatibility]
         if n_holes == 1 and isinstance(init_holes[0], (tuple, list)):
-            if len(init_holes[0]) == 0:
+            if not init_holes[0]:
                 init_holes = ()
                 n_holes = 0
             elif isinstance(init_holes[0][0], LinearRing):

django/core/management/__init__.py
@@ -351,7 +351,7 @@ class ManagementUtility:
         if subcommand == 'help':
             if '--commands' in args:
                 sys.stdout.write(self.main_help_text(commands_only=True) + '\n')
-            elif len(options.args) < 1:
+            elif not options.args:
                 sys.stdout.write(self.main_help_text() + '\n')
             else:
                 self.fetch_command(options.args[0]).print_help(self.prog_name, options.args[0])

django/core/management/commands/createcachetable.py
@@ -33,7 +33,7 @@ class Command(BaseCommand):
         db = options['database']
         self.verbosity = options['verbosity']
         dry_run = options['dry_run']
-        if len(tablenames):
+        if tablenames:
             # Legacy behavior, tablename specified as argument
             for tablename in tablenames:
                 self.create_table(db, tablename, dry_run)

commit 9eb5cbaf53f0460e17f2e6800bef00d493344f64

Made versionadded/versionchanged end with ".", but not with ":" when there is no content.
Regression in d2afa5eb2308e672b6313876856e32e2561b90f3.

docs/_ext/djangodocs.py
@@ -153,7 +153,7 @@ class DjangoHTMLTranslator(HTMLTranslator):
         if version_text:
             title = "%s%s" % (
                 version_text % node['version'],
-                ":" if node else "."
+                ":" if len(node) else "."
             )
             self.body.append('<span class="title">%s</span> ' % title)
 