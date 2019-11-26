2633c5341e94083dd802c5961b97b048adbd5940
Hasan Ramezani <hasan.r67@gmail.com>
2019-11-27 00:00:56 +0100

    Fixed #22261 -- Fixed resolving namespaced URLs for flatpages.

--- django/contrib/flatpages/models.py
+++ django/contrib/flatpages/models.py
@@ -1,6 +1,6 @@
 from django.contrib.sites.models import Site
 from django.db import models
-from django.urls import get_script_prefix
+from django.urls import NoReverseMatch, get_script_prefix, reverse
 from django.utils.encoding import iri_to_uri
 from django.utils.translation import gettext_lazy as _
 
@@ -36,5 +36,12 @@ class FlatPage(models.Model):
         return "%s -- %s" % (self.url, self.title)
 
     def get_absolute_url(self):
+        from .views import flatpage
+
+        for url in (self.url.lstrip('/'), self.url):
+            try:
+                return reverse(flatpage, kwargs={'url': url})
+            except NoReverseMatch:
+                pass
         # Handle script prefix manually because we bypass reverse()
         return iri_to_uri(get_script_prefix().rstrip('/') + self.url)
