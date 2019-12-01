4f216e4f8ea9bac61948cb63066cd4747f6d05fb
Matthijs Kooijman <matthijs@stdin.nl>
2019-12-02 00:50:59 +0100

    Fixed #31051 -- Allowed dumpdata to handle circular references in natural keys.
    Since #26291 forward references in natural keys are properly handled by
    loaddata, so sorting depenencies in dumpdata doesn't need to break on
    cycles. This patch allows circular references in natural keys by
    breaking sort_depenencies() on loops.

--- django/core/management/commands/dumpdata.py
+++ django/core/management/commands/dumpdata.py
@@ -144,7 +144,7 @@ class Command(BaseCommand):
             Collate the objects to be serialized. If count_only is True, just
             count the number of objects to be serialized.
             """
-            models = serializers.sort_dependencies(app_list.items())
+            models = serializers.sort_dependencies(app_list.items(), allow_cycles=True)
             for model in models:
                 if model in excluded_models:
                     continue
