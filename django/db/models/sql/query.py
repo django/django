723fc7fcf63cbb3c3267ec15ce197d45a7203c4d
Alex Aktsipetrov <alex.akts@gmail.com>
2019-12-05 19:46:59 +0300

    Made Query.check_filterable() use bool by default instead of str.

--- django/db/models/sql/query.py
+++ django/db/models/sql/query.py
@@ -1114,7 +1114,7 @@ class Query(BaseExpression):
 
     def check_filterable(self, expression):
         """Raise an error if expression cannot be used in a WHERE clause."""
-        if not getattr(expression, 'filterable', 'True'):
+        if not getattr(expression, 'filterable', True):
             raise NotSupportedError(
                 expression.__class__.__name__ + ' is disallowed in the filter '
                 'clause.'
