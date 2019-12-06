82a88d2f48e13ef5d472741d5ed1c183230cfe4c
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2019-12-06 09:32:51 +0100

    Fixed #31061 -- Ignored positional args in django.urls.resolve() when all optional named parameters are missing.
    Regression in 76b993a117b61c41584e95149a67d8a1e9f49dd1.
    
    Thanks Claude Paroz for the report and Carlton Gibson for reviews.

--- django/urls/resolvers.py
+++ django/urls/resolvers.py
@@ -158,8 +158,9 @@ class RegexPattern(CheckURLMixin):
             # If there are any named groups, use those as kwargs, ignoring
             # non-named groups. Otherwise, pass all non-named arguments as
             # positional arguments.
-            kwargs = {k: v for k, v in match.groupdict().items() if v is not None}
+            kwargs = match.groupdict()
             args = () if kwargs else match.groups()
+            kwargs = {k: v for k, v in kwargs.items() if v is not None}
             return path[match.end():], args, kwargs
         return None
 
