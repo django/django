74e14541137d444b01fa947d7408f9d4a049ad9d
Peter Andersen <peter@porksmash.com>
2019-12-09 08:54:40 -0800

    [3.0.x] Fixed #31073 -- Prevented CheckboxInput.get_context() from mutating attrs.
    Backport of 02eff7ef60466da108b1a33f1e4dc01eec45c99d from master

--- django/forms/widgets.py
+++ django/forms/widgets.py
@@ -522,9 +522,7 @@ class CheckboxInput(Input):
 
     def get_context(self, name, value, attrs):
         if self.check_test(value):
-            if attrs is None:
-                attrs = {}
-            attrs['checked'] = True
+            attrs = {**(attrs or {}), 'checked': True}
         return super().get_context(name, value, attrs)
 
     def value_from_datadict(self, data, files, name):
