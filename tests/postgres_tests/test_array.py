02eff7ef60466da108b1a33f1e4dc01eec45c99d
Peter Andersen <peter@porksmash.com>
2019-12-09 08:54:40 -0800

    Fixed #31073 -- Prevented CheckboxInput.get_context() from mutating attrs.

--- tests/postgres_tests/test_array.py
+++ tests/postgres_tests/test_array.py
@@ -1103,6 +1103,17 @@ class TestSplitFormWidget(PostgreSQLWidgetTestCase):
             }
         )
 
+    def test_checkbox_get_context_attrs(self):
+        context = SplitArrayWidget(
+            forms.CheckboxInput(),
+            size=2,
+        ).get_context('name', [True, False])
+        self.assertEqual(context['widget']['value'], '[True, False]')
+        self.assertEqual(
+            [subwidget['attrs'] for subwidget in context['widget']['subwidgets']],
+            [{'checked': True}, {}]
+        )
+
     def test_render(self):
         self.check_html(
             SplitArrayWidget(forms.TextInput(), size=2), 'array', None,
