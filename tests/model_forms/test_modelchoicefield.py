8d087f9a179c287e5bb1e24558b097e0a761bc28
Jon Dufresne <jon.dufresne@gmail.com>
2019-12-11 06:15:12 -0800

    Fixed typo in ModelChoiceFieldTests.
    The subindex and attrs arguments should be passed through to the parent,
    not overridden as None.

--- tests/model_forms/test_modelchoicefield.py
+++ tests/model_forms/test_modelchoicefield.py
@@ -275,7 +275,7 @@ class ModelChoiceFieldTests(TestCase):
 
         class CustomCheckboxSelectMultiple(CheckboxSelectMultiple):
             def create_option(self, name, value, label, selected, index, subindex=None, attrs=None):
-                option = super().create_option(name, value, label, selected, index, subindex=None, attrs=None)
+                option = super().create_option(name, value, label, selected, index, subindex, attrs)
                 # Modify the HTML based on the object being rendered.
                 c = value.obj
                 option['attrs']['data-slug'] = c.slug
