972d93a95ec8b37fab84400417070b7439414967
Ram Rachum <ram@rachum.com>
2019-12-16 12:22:45 +0200

    Optimized containment check in ChoicesMeta.

--- django/db/models/enums.py
+++ django/db/models/enums.py
@@ -36,7 +36,7 @@ class ChoicesMeta(enum.EnumMeta):
     def __contains__(cls, member):
         if not isinstance(member, enum.Enum):
             # Allow non-enums to match against member values.
-            return member in {x.value for x in cls}
+            return any(x.value == member for x in cls)
         return super().__contains__(member)
 
     @property
