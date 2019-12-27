7fedda3359097ba3250891a1242f23a918dbb047
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2019-12-27 14:37:35 +0100

    Fixed random models_fields.test_uuid.TestQuerying failures.
    Random failures depended on a generated UUID.

--- tests/model_fields/test_uuid.py
+++ tests/model_fields/test_uuid.py
@@ -87,7 +87,9 @@ class TestQuerying(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.objs = [
-            NullableUUIDModel.objects.create(field=uuid.uuid4()),
+            NullableUUIDModel.objects.create(
+                field=uuid.UUID('25d405be-4895-4d50-9b2e-d6695359ce47'),
+            ),
             NullableUUIDModel.objects.create(field='550e8400e29b41d4a716446655440000'),
             NullableUUIDModel.objects.create(field=None),
         ]
