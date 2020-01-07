1163ee0c8d3363032b499ca4a0551e86669f5a92
Enrique Matías Sánchez (Quique) <cronopios@gmail.com>
2020-01-07 19:30:05 +0100

    Don't raise E004 on a sublanguage if the base language is available
    fixes: 31141

--- django/core/checks/translation.py
+++ django/core/checks/translation.py
@@ -56,6 +56,9 @@ def check_setting_languages_bidi(app_configs, **kwargs):
 def check_language_settings_consistent(app_configs, **kwargs):
     """Error if language settings are not consistent with each other."""
     available_tags = {i for i, _ in settings.LANGUAGES} | {'en-us'}
-    if settings.LANGUAGE_CODE not in available_tags:
+    if not any(
+        tag in available_tags
+        for tag in [settings.LANGUAGE_CODE, settings.LANGUAGE_CODE.split('-')[0]]
+    ):
         return [E004]
     return []
