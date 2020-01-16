bf77669453b9e9f64291da6701fe06fd5ba47e29
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2020-01-16 08:06:16 +0100

    Fixed #29998 -- Allowed multiple OneToOneFields to the parent model.
    We assumed that any OneToOneField's in a child model must be the
    parent link and raised an error when parent_link=True was not
    specified. This patch allows to specify multiple OneToOneField's to
    the parent model.
    
    OneToOneField's without a custom related_name will raise fields.E304
    and fields.E305 so this should warn users when they try to override
    the auto-created OneToOneField.

--- django/db/models/base.py
+++ django/db/models/base.py
@@ -202,7 +202,7 @@ class ModelBase(type):
                 continue
             # Locate OneToOneField instances.
             for field in base._meta.local_fields:
-                if isinstance(field, OneToOneField):
+                if isinstance(field, OneToOneField) and field.remote_field.parent_link:
                     related = resolve_relation(new_class, field.remote_field.model)
                     parent_links[make_model_tuple(related)] = field
 
