django/django

commit dffa3e1992562ba60512d96d1eb5859ffff2ceb5

Fixed #30493 -- Fixed prefetch_related() for GenericRelation with different content types.
Co-Authored-By: Mariusz Felisiak <felisiak.mariusz@gmail.com>

Thanks Simon Charette for the review.

django/contrib/contenttypes/fields.py
@@ -1,4 +1,6 @@
 import functools
+import itertools
+import operator
 from collections import defaultdict
 
 from django.contrib.contenttypes.models import ContentType
@@ -566,19 +568,28 @@ def create_generic_related_manager(superclass, rel):
 
             queryset._add_hints(instance=instances[0])
             queryset = queryset.using(queryset._db or self._db)
-
-            query = {
-                '%s__pk' % self.content_type_field_name: self.content_type.id,
-                '%s__in' % self.object_id_field_name: {obj.pk for obj in instances}
-            }
-
+            # Group instances by content types.
+            content_type_queries = (
+                models.Q(**{
+                    '%s__pk' % self.content_type_field_name: content_type_id,
+                    '%s__in' % self.object_id_field_name: {obj.pk for obj in objs}
+                })
+                for content_type_id, objs in itertools.groupby(
+                    sorted(instances, key=lambda obj: self.get_content_type(obj).pk),
+                    lambda obj: self.get_content_type(obj).pk,
+                )
+            )
+            query = functools.reduce(operator.or_, content_type_queries)
             # We (possibly) need to convert object IDs to the type of the
             # instances' PK in order to match up instances:
             object_id_converter = instances[0]._meta.pk.to_python
             return (
-                queryset.filter(**query),
-                lambda relobj: object_id_converter(getattr(relobj, self.object_id_field_name)),
-                lambda obj: obj.pk,
+                queryset.filter(query),
+                lambda relobj: (
+                    object_id_converter(getattr(relobj, self.object_id_field_name)),
+                    relobj.content_type_id
+                ),
+                lambda obj: (obj.pk, self.get_content_type(obj).pk),
                 False,
                 self.prefetch_cache_name,
                 False,

tests/generic_relations/tests.py
@@ -546,6 +546,24 @@ class GenericRelationsTests(TestCase):
         platypus.tags.remove(weird_tag)
         self.assertSequenceEqual(platypus.tags.all(), [furry_tag])
 
+    def test_prefetch_related_different_content_types(self):
+        TaggedItem.objects.create(content_object=self.platypus, tag='prefetch_tag_1')
+        TaggedItem.objects.create(
+            content_object=Vegetable.objects.create(name='Broccoli'),
+            tag='prefetch_tag_2',
+        )
+        TaggedItem.objects.create(
+            content_object=Animal.objects.create(common_name='Bear'),
+            tag='prefetch_tag_3',
+        )
+        qs = TaggedItem.objects.filter(
+            tag__startswith='prefetch_tag_',
+        ).prefetch_related('content_object', 'content_object__tags')
+        with self.assertNumQueries(4):
+            tags = list(qs)
+        for tag in tags:
+            self.assertSequenceEqual(tag.content_object.tags.all(), [tag])
+
 
 class ProxyRelatedModelTest(TestCase):
     def test_default_behavior(self):

commit 0b013564ef0609d95b1d263626f2e15bccda1a50

Fixed #31190 -- Fixed prefetch_related() crash for GenericForeignKey with custom ContentType foreign key.
Regression in dffa3e1992562ba60512d96d1eb5859ffff2ceb5.

django/contrib/contenttypes/fields.py
@@ -583,11 +583,12 @@ def create_generic_related_manager(superclass, rel):
             # We (possibly) need to convert object IDs to the type of the
             # instances' PK in order to match up instances:
             object_id_converter = instances[0]._meta.pk.to_python
+            content_type_id_field_name = '%s_id' % self.content_type_field_name
             return (
                 queryset.filter(query),
                 lambda relobj: (
                     object_id_converter(getattr(relobj, self.object_id_field_name)),
-                    relobj.content_type_id
+                    getattr(relobj, content_type_id_field_name),
                 ),
                 lambda obj: (obj.pk, self.get_content_type(obj).pk),
                 False,

tests/generic_relations/tests.py
@@ -564,6 +564,19 @@ class GenericRelationsTests(TestCase):
         for tag in tags:
             self.assertSequenceEqual(tag.content_object.tags.all(), [tag])
 
+    def test_prefetch_related_custom_object_id(self):
+        tiger = Animal.objects.create(common_name='tiger')
+        cheetah = Animal.objects.create(common_name='cheetah')
+        Comparison.objects.create(
+            first_obj=cheetah, other_obj=tiger, comparative='faster',
+        )
+        Comparison.objects.create(
+            first_obj=tiger, other_obj=cheetah, comparative='cooler',
+        )
+        qs = Comparison.objects.prefetch_related('first_obj__comparisons')
+        for comparison in qs:
+            self.assertSequenceEqual(comparison.first_obj.comparisons.all(), [comparison])
+
 
 class ProxyRelatedModelTest(TestCase):
     def test_default_behavior(self):