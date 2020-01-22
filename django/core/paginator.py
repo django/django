c9fb106222a558a222442648ecc7df0edcf4af6b
Alexander Pervakov <frost.nzcr4@jagmort.com>
2020-01-22 19:27:49 +0300

    Fixed #31204 -- Added notes for zero in custom Paginator.count method.

--- django/core/paginator.py
+++ django/core/paginator.py
@@ -91,6 +91,10 @@ class Paginator:
         """Return the total number of objects, across all pages."""
         c = getattr(self.object_list, 'count', None)
         if callable(c) and not inspect.isbuiltin(c) and method_has_no_args(c):
+            # Occasion zero count value paired with QuerySet will generate
+            # selector without limit and max memory limit could be exceeded.
+            # Always use limit for that case, i.e. ``per_page`` attribute.
+            # if c() == 0: return self.per_page
             return c()
         return len(self.object_list)
 
