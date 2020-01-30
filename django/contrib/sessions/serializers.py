bfe4d03d6bdd3b5dc04d1e2cf7c8af1f7ba5a67b
Adam Johnson <me@adamj.eu>
2020-01-30 09:28:32 +0000

    Refs #29708 -- Deprecated PickleSerializer

--- django/contrib/sessions/serializers.py
+++ django/contrib/sessions/serializers.py
@@ -1,6 +1,8 @@
 import pickle
+import warnings
 
 from django.core.signing import JSONSerializer as BaseJSONSerializer
+from django.utils.deprecation import RemovedInDjango40Warning
 
 
 class PickleSerializer:
@@ -8,6 +10,16 @@ class PickleSerializer:
     Simple wrapper around pickle to be used in signing.dumps and
     signing.loads.
     """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        warnings.warn(
+            (
+                "PickleSerializer is deprecated due to its security risk. "
+                "Use JSONSerializer instead."
+            ),
+            RemovedInDjango40Warning,
+        )
+
     protocol = pickle.HIGHEST_PROTOCOL
 
     def dumps(self, obj):
