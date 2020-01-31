a260001a2d3b2d9a7e872c24295e793bdbd10698
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2020-01-31 20:42:16 +0100

    Refs #30943 -- Fixed postgres_tests on PostgreSQL 9.5.
    Bloom extension is available on PostgreSQL 9.6+.

--- tests/postgres_tests/migrations/0001_setup_extensions.py
+++ tests/postgres_tests/migrations/0001_setup_extensions.py
@@ -1,6 +1,6 @@
 from unittest import mock
 
-from django.db import migrations
+from django.db import connection, migrations
 
 try:
     from django.contrib.postgres.operations import (
@@ -23,7 +23,11 @@ except ImportError:
 class Migration(migrations.Migration):
 
     operations = [
-        BloomExtension(),
+        (
+            BloomExtension()
+            if getattr(connection.features, 'has_bloom_index', False)
+            else mock.Mock()
+        ),
         BtreeGinExtension(),
         BtreeGistExtension(),
         CITextExtension(),
