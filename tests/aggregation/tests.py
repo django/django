django/django

commit 59b4e99dd00b9c36d56055b889f96885995e4240

Refs #31136 -- Made QuerySet.values()/values_list() group only by selected annotation.
Regression in 0f843fdd5b9b2f2307148465cd60f4e1b2befbb4.

django/db/models/sql/query.py
@@ -2093,13 +2093,6 @@ class Query(BaseExpression):
         self.clear_deferred_loading()
         self.clear_select_fields()
 
-        if self.group_by is True:
-            self.add_fields((f.attname for f in self.model._meta.concrete_fields), False)
-            # Disable GROUP BY aliases to avoid orphaning references to the
-            # SELECT clause which is about to be cleared.
-            self.set_group_by(allow_aliases=False)
-            self.clear_select_fields()
-
         if fields:
             field_names = []
             extra_names = []
@@ -2121,6 +2114,14 @@ class Query(BaseExpression):
             self.set_annotation_mask(annotation_names)
         else:
             field_names = [f.attname for f in self.model._meta.concrete_fields]
+        # Selected annotations must be known before setting the GROUP BY
+        # clause.
+        if self.group_by is True:
+            self.add_fields((f.attname for f in self.model._meta.concrete_fields), False)
+            # Disable GROUP BY aliases to avoid orphaning references to the
+            # SELECT clause which is about to be cleared.
+            self.set_group_by(allow_aliases=False)
+            self.clear_select_fields()
 
         self.values_select = tuple(field_names)
         self.add_fields(field_names, True)

tests/aggregation/tests.py
@@ -1165,6 +1165,31 @@ class AggregateTestCase(TestCase):
             'Sams',
         ])
 
+    def test_aggregation_subquery_annotation_values(self):
+        """
+        Subquery annotations and external aliases are excluded from the GROUP
+        BY if they are not selected.
+        """
+        books_qs = Book.objects.annotate(
+            first_author_the_same_age=Subquery(
+                Author.objects.filter(
+                    age=OuterRef('contact__friends__age'),
+                ).order_by('age').values('id')[:1],
+            )
+        ).filter(
+            publisher=self.p1,
+            first_author_the_same_age__isnull=False,
+        ).annotate(
+            min_age=Min('contact__friends__age'),
+        ).values('name', 'min_age').order_by('name')
+        self.assertEqual(list(books_qs), [
+            {'name': 'Practical Django Projects', 'min_age': 34},
+            {
+                'name': 'The Definitive Guide to Django: Web Development Done Right',
+                'min_age': 29,
+            },
+        ])
+
     @skipUnlessDBFeature('supports_subqueries_in_group_by')
     def test_group_by_subquery_annotation(self):
         """

commit 6c9d4ce4f1424941548e4b50309f8e8e465529d3

Fixed #31217 -- Made QuerySet.values()/values_list() group by not selected annotations used in order_by().
Regression in 59b4e99dd00b9c36d56055b889f96885995e4240.

Thanks Jon Dufresne for the report.

django/db/models/sql/query.py
@@ -1928,8 +1928,17 @@ class Query(BaseExpression):
         will be made automatically.
         """
         group_by = list(self.select)
-        if self.annotation_select:
-            for alias, annotation in self.annotation_select.items():
+        annotations_group_by = self.annotation_select.copy()
+        # Add not selected annotations used only in the ORDER BY clause.
+        if self.order_by and self.annotations:
+            for item in self.order_by:
+                if not hasattr(item, 'resolve_expression'):
+                    alias = item[1:] if item.startswith('-') else item
+                    if alias in self.annotations and alias not in self.annotation_select:
+                        annotations_group_by[alias] = self.annotations[alias]
+        # Expand the GROUP BY clause by required columns.
+        if annotations_group_by:
+            for alias, annotation in annotations_group_by.items():
                 signature = inspect.signature(annotation.get_group_by_cols)
                 if 'alias' not in signature.parameters:
                     annotation_class = annotation.__class__

tests/aggregation/tests.py
@@ -9,6 +9,7 @@ from django.db.models import (
     Max, Min, Sum, Value,
 )
 from django.db.models.expressions import Case, Exists, OuterRef, Subquery, When
+from django.db.models.functions import Coalesce
 from django.test import TestCase
 from django.test.testcases import skipUnlessDBFeature
 from django.test.utils import Approximate, CaptureQueriesContext
@@ -1189,6 +1190,32 @@ class AggregateTestCase(TestCase):
                 'min_age': 29,
             },
         ])
+
+    def test_aggregation_order_by_not_selected_annotation_values(self):
+        result_asc = [
+            self.b4.pk,
+            self.b3.pk,
+            self.b1.pk,
+            self.b2.pk,
+            self.b5.pk,
+            self.b6.pk,
+        ]
+        result_desc = result_asc[::-1]
+        tests = [
+            ('min_related_age', result_asc),
+            ('-min_related_age', result_desc),
+            (F('min_related_age'), result_asc),
+            (F('min_related_age').asc(), result_asc),
+            (F('min_related_age').desc(), result_desc),
+        ]
+        for ordering, expected_result in tests:
+            with self.subTest(ordering=ordering):
+                books_qs = Book.objects.annotate(
+                    min_age=Min('authors__age'),
+                ).annotate(
+                    min_related_age=Coalesce('min_age', 'contact__age'),
+                ).order_by(ordering).values_list('pk', flat=True)
+                self.assertEqual(list(books_qs), expected_result)
 
     @skipUnlessDBFeature('supports_subqueries_in_group_by')
     def test_group_by_subquery_annotation(self):