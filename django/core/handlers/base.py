4e8d6a1bafbbebc0720c5d1ed751521356364fe9
Florian Apolloner <florian@apolloner.eu>
2020-02-07 12:55:59 +0100

    [3.0.x] Fixed #31240 -- Properly closed FileResponse when wsgi.file_wrapper is used.
    Thanks to Oskar Persson for the report.
    
    Backport of 41a3b3d18647b258331104520e76f977406c590d from master

--- django/core/handlers/base.py
+++ django/core/handlers/base.py
@@ -73,7 +73,7 @@ class BaseHandler:
         # Setup default url resolver for this thread
         set_urlconf(settings.ROOT_URLCONF)
         response = self._middleware_chain(request)
-        response._closable_objects.append(request)
+        response._resource_closers.append(request.close)
         if response.status_code >= 400:
             log_response(
                 '%s: %s', response.reason_phrase, request.path,
