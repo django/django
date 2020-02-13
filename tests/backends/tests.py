50bf56f8fa80b7bdee7a303ca8adb5e5da64bbba
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2020-02-13 11:46:15 +0000

    Fixed #31271 -- Preserved ordering when unifying query parameters on Oracle.
    This caused misplacing misplacing parameters in logged SQL queries.
    
    Regression in 79065b55a70cd220820a260a1c54851b7be0615a.
    
    Thanks Hans Aarne Liblik for the report.

--- tests/backends/tests.py
+++ tests/backends/tests.py
@@ -79,6 +79,10 @@ class LastExecutedQueryTest(TestCase):
         for qs in (
             Article.objects.filter(pk=1),
             Article.objects.filter(pk__in=(1, 2), reporter__pk=3),
+            Article.objects.filter(
+                pk=1,
+                reporter__pk=9,
+            ).exclude(reporter__pk__in=[2, 1]),
         ):
             sql, params = qs.query.sql_with_params()
             with qs.query.get_compiler(DEFAULT_DB_ALIAS).execute_sql(CURSOR) as cursor:
