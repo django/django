81f905d23acbc201952db8709bd75702f85e016d
Carlton Gibson <carlton.gibson@noumenal.es>
2020-02-18 10:50:19 +0100

    Refs #31282 -- Clarified M2O add/remove/set with PK behaviour.
    Improved error message for remove() and added tests.

--- django/db/models/fields/related_descriptors.py
+++ django/db/models/fields/related_descriptors.py
@@ -697,6 +697,10 @@ def create_reverse_many_to_one_manager(superclass, rel):
                 val = self.field.get_foreign_related_value(self.instance)
                 old_ids = set()
                 for obj in objs:
+                    if not isinstance(obj, self.model):
+                        raise TypeError("'%s' instance expected, got %r" % (
+                            self.model._meta.object_name, obj,
+                        ))
                     # Is obj actually part of this descriptor set?
                     if self.field.get_local_related_value(obj) == val:
                         old_ids.add(obj.pk)
