2448b3182c75cce5d22dd80be0fb5cba6104c364
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2020-02-18 11:45:12 +0100

    [3.0.x] Fixed #31271 -- Preserved ordering when unifying query parameters on Oracle.
    This caused misplacing parameters in logged SQL queries.
    
    Regression in 79065b55a70cd220820a260a1c54851b7be0615a.
    
    Thanks Hans Aarne Liblik for the report.
    Backport of 2a038521c4eabdc5f6d5026d3dd6d22868e329cd from master

--- django/db/backends/oracle/base.py
+++ django/db/backends/oracle/base.py
@@ -497,7 +497,10 @@ class FormatStylePlaceholderCursor:
             # params_dict = {0.75: ':arg0', 2: ':arg1', 'sth': ':arg2'}
             # args = [':arg0', ':arg1', ':arg0', ':arg2', ':arg0']
             # params = {':arg0': 0.75, ':arg1': 2, ':arg2': 'sth'}
-            params_dict = {param: ':arg%d' % i for i, param in enumerate(set(params))}
+            params_dict = {
+                param: ':arg%d' % i
+                for i, param in enumerate(dict.fromkeys(params))
+            }
             args = [params_dict[param] for param in params]
             params = {value: key for key, value in params_dict.items()}
             query = query % tuple(args)
