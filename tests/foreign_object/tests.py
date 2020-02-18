4ef107e34f752d51fa2cadff30fdd6fd718b026e
Yoo In Keun <keun0390@naver.com>
2020-02-18 21:54:41 +0900

    Fixed typos in docs and a test comment.

--- tests/foreign_object/tests.py
+++ tests/foreign_object/tests.py
@@ -47,7 +47,7 @@ class MultiColumnFKTests(TestCase):
         self.assertEqual((person.id, person.name), (self.bob.id, "Bob"))
 
     def test_get_fails_on_multicolumn_mismatch(self):
-        # Membership objects returns DoesNotExist error when the there is no
+        # Membership objects returns DoesNotExist error when there is no
         # Person with the same id and country_id
         membership = Membership.objects.create(
             membership_country_id=self.usa.id, person_id=self.jane.id, group_id=self.cia.id)
