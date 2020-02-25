3d62ddb02695cfafc847951b45e6e1ec5dc0b149
Simon Charette <charette.s@gmail.com>
2020-02-24 23:10:50 -0500

    Refs #3254 -- Removed unnecessary truth check in SearchVectorExact.as_sql().
    Direct usage of the @@ operator is perfectly allowed.

--- django/contrib/postgres/search.py
+++ django/contrib/postgres/search.py
@@ -19,7 +19,7 @@ class SearchVectorExact(Lookup):
         lhs, lhs_params = self.process_lhs(qn, connection)
         rhs, rhs_params = self.process_rhs(qn, connection)
         params = lhs_params + rhs_params
-        return '%s @@ %s = true' % (lhs, rhs), params
+        return '%s @@ %s' % (lhs, rhs), params
 
 
 class SearchVectorField(Field):
