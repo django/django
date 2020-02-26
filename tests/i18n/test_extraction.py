984531f96eb901f2dca732087171bfe8ced986c3
Cristobal Mackenzie <c.mack04@gmail.com>
2020-02-26 18:07:17 -0300

    Fixed #31314 -- Raised CommandError when locale is not specified in makemessages.
    Regression in 0707b824fe77e08ca8b75fcc3738ada694f2a3a6.

--- tests/i18n/test_extraction.py
+++ tests/i18n/test_extraction.py
@@ -142,6 +142,16 @@ class BasicExtractorTests(ExtractorTests):
             self.assertIn('#. Translators: One-line translator comment #1', po_contents)
             self.assertIn('msgctxt "Special trans context #1"', po_contents)
 
+    def test_no_option(self):
+        # One of either the --locale, --exclude, or --all options is required.
+        msg = "Type 'manage.py help makemessages' for usage information."
+        with mock.patch(
+            'django.core.management.commands.makemessages.sys.argv',
+            ['manage.py', 'makemessages'],
+        ):
+            with self.assertRaisesRegex(CommandError, msg):
+                management.call_command('makemessages')
+
     def test_comments_extractor(self):
         management.call_command('makemessages', locale=[LOCALE], verbosity=0)
         self.assertTrue(os.path.exists(self.PO_FILE))
