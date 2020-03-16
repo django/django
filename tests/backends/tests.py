b23c5868177c8dcfa4571152fef1223bcba5292f
Tim Graham <timograham@gmail.com>
2020-03-15 20:39:35 -0400

    Edits to backends tests

--- tests/backends/tests.py
+++ tests/backends/tests.py
@@ -344,7 +344,7 @@ class BackendTestCase(TransactionTestCase):
 
     def test_duplicate_table_error(self):
         """ Creating an existing table returns a DatabaseError """
-        query = 'CREATE TABLE %s (id INTEGER);' % Article._meta.db_table
+        query = 'CREATE TABLE %s (id INT64) PRIMARY KEY(id)' % Article._meta.db_table
         with connection.cursor() as cursor:
             with self.assertRaises(DatabaseError):
                 cursor.execute(query)
