045b61982177d0d7817a68fb6808704adeff0f40
SoungRyoul Kim <soungryoul.kim@deliveryhero.co.kr>
2020-03-22 18:56:15 +0900

    add fun " generate_altered_db_table_comment" to autodetector.py

--- django/db/migrations/autodetector.py
+++ django/db/migrations/autodetector.py
@@ -1143,6 +1143,24 @@ class MigrationAutodetector:
                     )
                 )
 
+    def generate_altered_db_table_comment(self):
+        models_to_check = self.kept_model_keys.union(self.kept_proxy_keys, self.kept_unmanaged_keys)
+        for app_label, model_name in sorted(models_to_check):
+            old_model_name = self.renamed_models.get((app_label, model_name), model_name)
+            old_model_state = self.from_state.models[app_label, old_model_name]
+            new_model_state = self.to_state.models[app_label, model_name]
+
+            old_db_table_comment = old_model_state.options.get('db_table_comment')
+            new_db_table_comment = new_model_state.options.get('db_table_comment')
+            if old_db_table_comment != new_db_table_comment:
+                self.add_operation(
+                    app_label,
+                    operations.AlterModelTableComment(
+                        name=model_name,
+                        table_comment=new_db_table_comment,
+                    )
+                )
+
     def generate_altered_options(self):
         """
         Work out if any non-schema-affecting options have changed and make an
