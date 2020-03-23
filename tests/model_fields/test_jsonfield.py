47651eadb8ca7aacddad41da4df64fd2af11faae
Hasan Ramezani <hasan.r67@gmail.com>
2020-03-23 19:31:49 +0100

    Fixed #30583 -- Fixed handling JSONFields in XML serializer.
    Co-authored-by: Chason Chaffin <chason@gmail.com>

--- tests/model_fields/test_jsonfield.py
+++ tests/model_fields/test_jsonfield.py
@@ -149,6 +149,21 @@ class TestSerialization(SimpleTestCase):
                 )[0].object
                 self.assertEqual(instance.value, value)
 
+    def test_xml_serialization(self):
+        test_xml_data = (
+            '<django-objects version="1.0">'
+            '<object model="model_fields.nullablejsonmodel">'
+            '<field name="value" type="JSONField">%s'
+            '</field></object></django-objects>'
+        )
+        for value, serialized in self.test_values:
+            with self.subTest(value=value):
+                instance = NullableJSONModel(value=value)
+                data = serializers.serialize('xml', [instance], fields=['value'])
+                self.assertXMLEqual(data, test_xml_data % serialized)
+                new_instance = list(serializers.deserialize('xml', data))[0].object
+                self.assertEqual(new_instance.value, instance.value)
+
 
 @skipUnlessDBFeature('supports_json_field')
 class TestSaveLoad(TestCase):
