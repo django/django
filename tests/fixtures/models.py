6570589cbaf760c24c4b0362e28943dbbe31ac1c
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2020-04-03 09:35:03 +0200

    Enforced uniqueness of natural keys used in tests.

--- tests/fixtures/models.py
+++ tests/fixtures/models.py
@@ -66,7 +66,7 @@ class PersonManager(models.Manager):
 
 class Person(models.Model):
     objects = PersonManager()
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     class Meta:
         ordering = ('name',)
@@ -119,7 +119,7 @@ class PrimaryKeyUUIDModel(models.Model):
 
 
 class NaturalKeyThing(models.Model):
-    key = models.CharField(max_length=100)
+    key = models.CharField(max_length=100, unique=True)
     other_thing = models.ForeignKey('NaturalKeyThing', on_delete=models.CASCADE, null=True)
     other_things = models.ManyToManyField('NaturalKeyThing', related_name='thing_m2m_set')
 
