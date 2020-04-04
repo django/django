22a2e97fc30487af89d7c34b753853e7b510083d
Hasan Ramezani <hasan.r67@gmail.com>
2020-04-04 20:55:20 +0200

    [3.0.x] Fixed #31420 -- Fixed crash when filtering subquery annotation against a SimpleLazyObject.
    Thanks Simon Charette for the solution and analysis.
    
    Backport of 4237050684427db45ea834fe89d9e11c0520201e from master

--- django/db/models/sql/query.py
+++ django/db/models/sql/query.py
@@ -239,7 +239,8 @@ class Query(BaseExpression):
     @property
     def output_field(self):
         if len(self.select) == 1:
-            return self.select[0].field
+            select = self.select[0]
+            return getattr(select, 'target', None) or select.field
         elif len(self.annotation_select) == 1:
             return next(iter(self.annotation_select.values())).output_field
 
