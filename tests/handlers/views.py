d82fb93452ec7aa500050a62a088ad61a145696a
mark <mark.korput@gmail.com>
2020-04-07 18:17:02 +0200

    Reduced line-length, removed newline and capitalized comment

--- tests/handlers/views.py
+++ tests/handlers/views.py
@@ -54,8 +54,7 @@ async def async_regular(request):
 class CoroutineClearingView:
     def __call__(self, request):
         """Return an unawaited coroutine (common error for async views)."""
-
-        # store coroutine to suppress 'unawaited' warning message
+        # Store coroutine to suppress 'unawaited' warning message
         self._unawaited_coroutine = asyncio.sleep(0)
         return self._unawaited_coroutine
 
