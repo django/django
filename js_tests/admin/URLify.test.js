086029c5fc2d5c79a1aaafee19f1f1e4db977585
Scott Cranfill <scott.cranfill@cfpb.gov>
2020-04-23 17:38:50 -0400

    Fixed #31511 -- Removed part of URLify that removes English stop words
    This behavior seems outdated and no longer desirable, for the reasons 
    presented in the ticket and earlier django-developers mailing list 
    conversation.
    
    Co-Authored-By: Andy Chosak <andy@chosak.org>

--- js_tests/admin/URLify.test.js
+++ js_tests/admin/URLify.test.js
@@ -8,10 +8,6 @@ QUnit.test('empty string', function(assert) {
     assert.strictEqual(URLify('', 8, true), '');
 });
 
-QUnit.test('strip nonessential words', function(assert) {
-    assert.strictEqual(URLify('the D is silent', 8, true), 'd-silent');
-});
-
 QUnit.test('strip non-URL characters', function(assert) {
     assert.strictEqual(URLify('D#silent@', 7, true), 'dsilent');
 });
@@ -22,9 +18,4 @@ QUnit.test('merge adjacent whitespace', function(assert) {
 
 QUnit.test('trim trailing hyphens', function(assert) {
     assert.strictEqual(URLify('D silent always', 9, true), 'd-silent');
-});
-
-QUnit.test('do not remove English words if the string contains non-ASCII', function(assert) {
-    // If removing English words wasn't skipped, the last 'a' would be removed.
-    assert.strictEqual(URLify('Kaupa-miða', 255, true), 'kaupa-miða');
 });
