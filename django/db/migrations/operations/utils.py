fd325b9dee95f0930f5e8d582892b899508021fe
David Wobrock <david.wobrock@gmail.com>
2020-04-25 13:30:58 +0200

    Refs #29899 -- Moved resolve_relation() to django.db.migrations.utils.

--- django/db/migrations/operations/utils.py
+++ django/db/migrations/operations/utils.py
@@ -1,34 +1,6 @@
 from collections import namedtuple
 
-from django.db.models.fields.related import RECURSIVE_RELATIONSHIP_CONSTANT
-
-
-def resolve_relation(model, app_label=None, model_name=None):
-    """
-    Turn a model class or model reference string and return a model tuple.
-
-    app_label and model_name are used to resolve the scope of recursive and
-    unscoped model relationship.
-    """
-    if isinstance(model, str):
-        if model == RECURSIVE_RELATIONSHIP_CONSTANT:
-            if app_label is None or model_name is None:
-                raise TypeError(
-                    'app_label and model_name must be provided to resolve '
-                    'recursive relationships.'
-                )
-            return app_label, model_name
-        if '.' in model:
-            app_label, model_name = model.split('.', 1)
-            return app_label, model_name.lower()
-        if app_label is None:
-            raise TypeError(
-                'app_label must be provided to resolve unscoped model '
-                'relationships.'
-            )
-        return app_label, model.lower()
-    return model._meta.app_label, model._meta.model_name
-
+from django.db.migrations.utils import resolve_relation
 
 FieldReference = namedtuple('FieldReference', 'to through')
 
