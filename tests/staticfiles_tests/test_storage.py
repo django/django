df22eba8033b247ac7c8533312f154b90f9dd5f6
Gordon Pendleton <wgordonw1@gmail.com>
2020-04-28 12:42:33 -0400

    tested hashed names for existing / non-existing files when not strict

--- tests/staticfiles_tests/test_storage.py
+++ tests/staticfiles_tests/test_storage.py
@@ -363,15 +363,13 @@ class TestCollectionManifestStorage(TestHashedFiles, CollectionTestCase):
 
         configured_storage.manifest_strict = False
         # File doesn't exist on disk
-        err_msg = "The file '%s' could not be found with %r." % (missing_file_name, configured_storage._wrapped)
-        with self.assertRaisesMessage(ValueError, err_msg):
-            self.hashed_file_path(missing_file_name)
+        self.assertEqual(self.hashed_file_path(missing_file_name), 'cached/missing.css')
 
         content = StringIO()
         content.write('Found')
         configured_storage.save(missing_file_name, content)
         # File exists on disk
-        self.hashed_file_path(missing_file_name)
+        self.assertEqual(self.hashed_file_path(missing_file_name), 'cached/missing.5d695cc28c6a.css')
 
     def test_intermediate_files(self):
         cached_files = os.listdir(os.path.join(settings.STATIC_ROOT, 'cached'))
