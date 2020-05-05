b1507e20a77eb6e027d242b35a229c9fd81f059e
Adam Johnson <me@adamj.eu>
2020-05-05 10:45:12 +0100

    Fixed usage of a/an for "SQL"

--- tests/backends/base/test_operations.py
+++ tests/backends/base/test_operations.py
@@ -41,7 +41,7 @@ class SimpleDatabaseOperationTests(SimpleTestCase):
         self.assertEqual(self.ops.set_time_zone_sql(), '')
 
     def test_sql_flush(self):
-        msg = 'subclasses of BaseDatabaseOperations must provide a sql_flush() method'
+        msg = 'subclasses of BaseDatabaseOperations must provide an sql_flush() method'
         with self.assertRaisesMessage(NotImplementedError, msg):
             self.ops.sql_flush(None, None)
 
