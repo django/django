2d67222472f80f251607ae1b720527afceba06ad
tapan gujjar <tapan@stablehacks.com>
2020-05-05 15:40:46 -0400

    Fixed #31522 -- Made admin's SelectBox preserve scroll position.

--- js_tests/admin/SelectBox.test.js
+++ js_tests/admin/SelectBox.test.js
@@ -21,3 +21,27 @@ QUnit.test('filter', function(assert) {
     assert.equal($('#id option').length, 1);
     assert.equal($('#id option').text(), "A");
 });
+
+QUnit.test('preserve scroll position', function(assert) {
+    const $ = django.jQuery;
+    const optionsCount = 100;
+    $('<select id="from_id" multiple></select>').appendTo('#qunit-fixture');
+    $('<select id="to_id" multiple></select>').appendTo('#qunit-fixture');
+    const fromSelectBox = document.getElementById('from_id');
+    const toSelectBox = document.getElementById('to_id');
+    for (let i = 0; i < optionsCount; i++) {
+        fromSelectBox.appendChild(new Option());
+    }
+    SelectBox.init('from_id');
+    SelectBox.init('to_id');
+    const selectedOptions = [97, 98, 99];
+    for (const index of selectedOptions) {
+        fromSelectBox.options[index].selected = true;
+        fromSelectBox.options[index].scrollIntoView();
+    }
+    assert.equal(fromSelectBox.options.length, optionsCount);
+    SelectBox.move('from_id', 'to_id');
+    assert.equal(fromSelectBox.options.length, optionsCount - selectedOptions.length);
+    assert.equal(toSelectBox.options.length, selectedOptions.length);
+    assert.notEqual(fromSelectBox.scrollTop, 0);
+});
