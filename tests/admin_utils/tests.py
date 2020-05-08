675ce4dd35702ce353006611ba30799c0c4b015f
dbxnr <dbxnr@users.noreply.github.com>
2020-05-08 08:44:33 +0200

    Refs #31157 -- Added tests for display_for_field() with empty JSONField.

--- tests/admin_utils/tests.py
+++ tests/admin_utils/tests.py
@@ -176,6 +176,9 @@ class UtilsTests(SimpleTestCase):
         display_value = display_for_field(None, models.FloatField(), self.empty_value)
         self.assertEqual(display_value, self.empty_value)
 
+        display_value = display_for_field(None, models.JSONField(), self.empty_value)
+        self.assertEqual(display_value, self.empty_value)
+
     def test_number_formats_display_for_field(self):
         display_value = display_for_field(12345.6789, models.FloatField(), self.empty_value)
         self.assertEqual(display_value, '12345.6789')
