2e48cf6bd9499f888a6cebf9f18c92717f1df55c
François Freitag <mail@franek.fr>
2020-05-09 19:37:44 +0200

    Fixed fixtures loading isolation in tests.
    fixture1.json and fixture2.json exist in both "fixtures" and
    "fixtures_model_package". Both apps are listed in "INSTALLED_APPS". The
    loaddata management command loads from installed apps, thus loads both
    fixtures when a test runs loaddata with any of these fixtures.

--- tests/fixtures_model_package/tests.py
+++ tests/fixtures_model_package/tests.py
@@ -6,7 +6,7 @@ from .models import Article
 
 
 class SampleTestCase(TestCase):
-    fixtures = ['fixture1.json', 'fixture2.json']
+    fixtures = ['model_package_fixture1.json', 'model_package_fixture2.json']
 
     def test_class_fixtures(self):
         "Test cases can load fixture objects into models defined in packages"
@@ -25,7 +25,7 @@ class FixtureTestCase(TestCase):
     def test_loaddata(self):
         "Fixtures can load data into models defined in packages"
         # Load fixture 1. Single JSON file, with two objects
-        management.call_command("loaddata", "fixture1.json", verbosity=0)
+        management.call_command('loaddata', 'model_package_fixture1.json', verbosity=0)
         self.assertQuerysetEqual(
             Article.objects.all(), [
                 "Time to reform copyright",
@@ -36,7 +36,7 @@ class FixtureTestCase(TestCase):
 
         # Load fixture 2. JSON file imported by default. Overwrites some
         # existing objects
-        management.call_command("loaddata", "fixture2.json", verbosity=0)
+        management.call_command('loaddata', 'model_package_fixture2.json', verbosity=0)
         self.assertQuerysetEqual(
             Article.objects.all(), [
                 "Django conquers world!",
