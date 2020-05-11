f8454abbea05c364d1d31a06822e7a577734bed5
dinceraslancom <58322566+dinceraslancom@users.noreply.github.com>
2020-05-11 17:34:59 +0300

    Update forms.py
    Updated UserChangeForm and UserCreationForm models from User to UserModel

--- django/contrib/auth/forms.py
+++ django/contrib/auth/forms.py
@@ -101,7 +101,7 @@ class UserCreationForm(forms.ModelForm):
     )
 
     class Meta:
-        model = User
+        model = UserModel
         fields = ("username",)
         field_classes = {'username': UsernameField}
 
@@ -150,7 +150,7 @@ class UserChangeForm(forms.ModelForm):
     )
 
     class Meta:
-        model = User
+        model = UserModel
         fields = '__all__'
         field_classes = {'username': UsernameField}
 
