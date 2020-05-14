1c2d0fdf3eb903d2323b39e0ca596e5322922a87
Jon Dufresne <jon.dufresne@gmail.com>
2020-05-14 10:27:35 +0200

    [3.1.x] Fixed #31575 -- Added system check for admin sidebar request context processor dependency.
    Co-authored-by: Carlton Gibson <carlton.gibson@noumenal.es>
    
    Backport of d522b51c401429c169d88742178a9b3777903d9e from master

--- django/contrib/admin/checks.py
+++ django/contrib/admin/checks.py
@@ -59,6 +59,7 @@ def check_dependencies(**kwargs):
     """
     Check that the admin's dependencies are correctly installed.
     """
+    from django.contrib.admin.sites import all_sites
     if not apps.is_installed('django.contrib.admin'):
         return []
     errors = []
@@ -104,6 +105,15 @@ def check_dependencies(**kwargs):
                 "be enabled in DjangoTemplates (TEMPLATES) in order to use "
                 "the admin application.",
                 id='admin.E404',
+            ))
+        sidebar_enabled = any(site.enable_nav_sidebar for site in all_sites)
+        if (sidebar_enabled and 'django.template.context_processors.request'
+                not in django_templates_instance.context_processors):
+            errors.append(checks.Warning(
+                "'django.template.context_processors.request' must be enabled "
+                "in DjangoTemplates (TEMPLATES) in order to use the admin "
+                "navigation sidebar.",
+                id='admin.W411',
             ))
 
     if not _contains_subclass('django.contrib.auth.middleware.AuthenticationMiddleware', settings.MIDDLEWARE):
--- tests/admin_scripts/tests.py
+++ tests/admin_scripts/tests.py
@@ -1124,6 +1124,7 @@ class ManageCheck(AdminScriptTestCase):
                         'APP_DIRS': True,
                         'OPTIONS': {
                             'context_processors': [
+                                'django.template.context_processors.request',
                                 'django.contrib.auth.context_processors.auth',
                                 'django.contrib.messages.context_processors.messages',
                             ],
