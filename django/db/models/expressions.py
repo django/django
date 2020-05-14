9215664d881cedfad06b725e8cda63d26fa01453
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2020-05-14 12:04:22 +0200

    Fixed #31584 -- Fixed crash when chaining values()/values_list() after Exists() annotation and aggregation on Oracle.
    Oracle requires the EXISTS expression to be wrapped in a CASE WHEN in
    the GROUP BY clause.
    
    Regression in efa1908f662c19038a944129c81462485c4a9fe8.

--- django/db/models/expressions.py
+++ django/db/models/expressions.py
@@ -1089,7 +1089,8 @@ class Exists(Subquery):
 
     def select_format(self, compiler, sql, params):
         # Wrap EXISTS() with a CASE WHEN expression if a database backend
-        # (e.g. Oracle) doesn't support boolean expression in the SELECT list.
+        # (e.g. Oracle) doesn't support boolean expression in SELECT or GROUP
+        # BY list.
         if not compiler.connection.features.supports_boolean_expr_in_select_clause:
             sql = 'CASE WHEN {} THEN 1 ELSE 0 END'.format(sql)
         return sql, params
