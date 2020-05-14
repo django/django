3fb69756eac31a07d903cf1f97fc1cce0d3494a5
Joshua Massover <massover@simplebet.io>
2020-05-14 19:26:32 -0400

    [3.1.x] Fixed #31594 -- Added ASGIStaticFilesHandler.get_response_async().
    Backport of 92309e53d9921a60e667656d8dd65e59eb5cf81c from master

---
+++ tests/staticfiles_tests/test_handlers.py
@@ -0,0 +1,22 @@
+from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
+from django.core.handlers.asgi import ASGIHandler
+from django.test import AsyncRequestFactory
+
+from .cases import StaticFilesTestCase
+
+
+class TestASGIStaticFilesHandler(StaticFilesTestCase):
+    async_request_factory = AsyncRequestFactory()
+
+    async def test_get_async_response(self):
+        request = self.async_request_factory.get('/static/test/file.txt')
+        handler = ASGIStaticFilesHandler(ASGIHandler())
+        response = await handler.get_response_async(request)
+        response.close()
+        self.assertEqual(response.status_code, 200)
+
+    async def test_get_async_response_not_found(self):
+        request = self.async_request_factory.get('/static/test/not-found.txt')
+        handler = ASGIStaticFilesHandler(ASGIHandler())
+        response = await handler.get_response_async(request)
+        self.assertEqual(response.status_code, 404)
