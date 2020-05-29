09db98dea829c1faf5ccdbfd9828cde5f238a3b6
InvalidInterrupt <InvalidInterrupt@users.noreply.github.com>
2020-05-29 00:57:39 -0700

    Test several cases altering fields between serial and auto/int types
    These tests could probably use some improvement, I just wrote some basic
    checks by mimicking other tests I saw.

--- tests/postgres_tests/__init__.py
+++ tests/postgres_tests/__init__.py
@@ -3,7 +3,9 @@ import unittest
 from forms_tests.widget_tests.base import WidgetTest
 
 from django.db import connection
-from django.test import SimpleTestCase, TestCase, modify_settings
+from django.test import (
+    SimpleTestCase, TestCase, TransactionTestCase, modify_settings,
+)
 
 
 @unittest.skipUnless(connection.vendor == 'postgresql', "PostgreSQL specific tests")
@@ -13,6 +15,11 @@ class PostgreSQLSimpleTestCase(SimpleTestCase):
 
 @unittest.skipUnless(connection.vendor == 'postgresql', "PostgreSQL specific tests")
 class PostgreSQLTestCase(TestCase):
+    pass
+
+
+@unittest.skipUnless(connection.vendor == 'postgresql', "PostgreSQL specific tests")
+class PostgreSQLTransactionTestCase(TransactionTestCase):
     pass
 
 
