fdd2b01e8e12857aad2219a46a41bd9051ec8f8d
Thodoris Sotiropoulos <theosotr@aueb.gr>
2020-06-03 00:14:01 +0300

    [3.1.x] Fixed #31651 -- Made ExpressionWrapper use grouping columns from wrapped expression.
    Backport of df32fd42b84cc6dbba173201f244491b0d154a63 from master

--- django/db/models/expressions.py
+++ django/db/models/expressions.py
@@ -863,6 +863,9 @@ class ExpressionWrapper(Expression):
     def get_source_expressions(self):
         return [self.expression]
 
+    def get_group_by_cols(self, alias=None):
+        return self.expression.get_group_by_cols(alias=alias)
+
     def as_sql(self, compiler, connection):
         return self.expression.as_sql(compiler, connection)
 
--- tests/expressions/tests.py
+++ tests/expressions/tests.py
@@ -1828,3 +1828,13 @@ class CombinableTests(SimpleTestCase):
     def test_reversed_or(self):
         with self.assertRaisesMessage(NotImplementedError, self.bitwise_msg):
             object() | Combinable()
+
+
+class ExpressionWrapperTests(SimpleTestCase):
+    def test_empty_group_by(self):
+        expr = ExpressionWrapper(Value(3), output_field=IntegerField())
+        self.assertEqual(expr.get_group_by_cols(alias=None), [])
+
+    def test_non_empty_group_by(self):
+        expr = ExpressionWrapper(Lower(Value('f')), output_field=IntegerField())
+        self.assertEqual(expr.get_group_by_cols(alias=None), [expr.expression])
