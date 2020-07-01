ed6b14d4591e536985222b61cb8b83908d58140d
Sergey Fedoseev <fedoseev.sergey@gmail.com>
2020-07-01 14:01:46 +0500

    Refs #28621 -- Fixed crash of annotations with nested OuterRef.

--- tests/expressions/tests.py
+++ tests/expressions/tests.py
@@ -710,6 +710,22 @@ class BasicExpressionsTests(TestCase):
         ).get(pk=self.gmbh.pk)
         self.assertEqual(gmbh_salary.max_ceo_salary_raise, 2332)
 
+    def test_annotation_with_nested_outerref(self):
+        self.gmbh.point_of_contact = Employee.objects.get(lastname='Meyer')
+        self.gmbh.save()
+        inner = Employee.objects.annotate(
+            outer_lastname=OuterRef(OuterRef('lastname')),
+        ).filter(lastname__startswith=Left('outer_lastname', 1))
+        qs = Employee.objects.annotate(
+            ceo_company=Subquery(
+                Company.objects.filter(
+                    point_of_contact__in=inner,
+                    ceo__pk=OuterRef('pk'),
+                ).values('name'),
+            ),
+        ).filter(ceo_company__isnull=False)
+        self.assertEqual(qs.get().ceo_company, 'Test GmbH')
+
     def test_pickle_expression(self):
         expr = Value(1, output_field=IntegerField())
         expr.convert_value  # populate cached property
