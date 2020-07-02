37389ae99eb17ee8e2db39545d0e13f5ab8f006a
Sergey Fedoseev <fedoseev.sergey@gmail.com>
2020-07-03 00:26:01 +0500

    Fixed #31758 -- Removed unneeded BytesToCharFieldConversionMixin.
    Bug was fixed in mysqlclient 1.3.13.

--- django/db/backends/base/features.py
+++ django/db/backends/base/features.py
@@ -253,9 +253,6 @@ class BaseDatabaseFeatures:
     # Does the backend support keyword parameters for cursor.callproc()?
     supports_callproc_kwargs = False
 
-    # Convert CharField results from bytes to str in database functions.
-    db_functions_convert_bytes_to_str = False
-
     # What formats does the backend EXPLAIN syntax support?
     supported_explain_formats = set()
 
