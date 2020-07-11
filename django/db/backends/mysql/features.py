183cad348f9a43458e26c046a0e5cc85de27603e
sage <laymonage@gmail.com>
2020-07-11 16:30:46 +0700

    Fixed #31779 -- Bumped MariaDB requirement for JSONField to 10.2.16.

--- django/db/backends/mysql/features.py
+++ django/db/backends/mysql/features.py
@@ -149,7 +149,7 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     @cached_property
     def supports_json_field(self):
         if self.connection.mysql_is_mariadb:
-            return self.connection.mysql_version >= (10, 2, 7)
+            return self.connection.mysql_version >= (10, 2, 16)
         return self.connection.mysql_version >= (5, 7, 8)
 
     @cached_property
