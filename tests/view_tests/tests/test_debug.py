f36862b69c3325da8ba6892a6057bbd9470efd70
Tom Forbes <tom@tomforb.es>
2020-07-13 08:01:59 +0200

    Fixed #31674 -- Made technical 500 debug page respect __suppress_context__.

--- tests/view_tests/tests/test_debug.py
+++ tests/view_tests/tests/test_debug.py
@@ -391,6 +391,26 @@ class ExceptionReporterTests(SimpleTestCase):
         self.assertIn('<h2>Request information</h2>', html)
         self.assertNotIn('<p>Request data not supplied</p>', html)
 
+    def test_suppressed_context(self):
+        try:
+            try:
+                raise RuntimeError("Can't find my keys")
+            except RuntimeError:
+                raise ValueError("Can't find my keys") from None
+        except ValueError:
+            exc_type, exc_value, tb = sys.exc_info()
+
+        reporter = ExceptionReporter(None, exc_type, exc_value, tb)
+        html = reporter.get_traceback_html()
+        self.assertInHTML('<h1>ValueError</h1>', html)
+        self.assertIn('<pre class="exception_value">Can&#x27;t find my keys</pre>', html)
+        self.assertIn('<th>Exception Type:</th>', html)
+        self.assertIn('<th>Exception Value:</th>', html)
+        self.assertIn('<h2>Traceback ', html)
+        self.assertIn('<h2>Request information</h2>', html)
+        self.assertIn('<p>Request data not supplied</p>', html)
+        self.assertNotIn('During handling of the above exception', html)
+
     def test_reporting_of_nested_exceptions(self):
         request = self.rf.get('/test_view/')
         try:
