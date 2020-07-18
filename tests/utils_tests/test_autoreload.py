b830cc02ede9d4f41a574664b76916c8fe41de16
Tom Forbes <tom@tomforb.es>
2020-07-18 19:42:35 +0100

    [3.1.x] Used temporary directory in RestartWithReloaderTests.test_manage_py().
    Using the current directory can cause a PermissionError.
    
    Backport of 730711e8282893723f993f55d3e3b0c823cfdb9a from master

--- tests/utils_tests/test_autoreload.py
+++ tests/utils_tests/test_autoreload.py
@@ -410,14 +410,17 @@ class RestartWithReloaderTests(SimpleTestCase):
         return mock_call
 
     def test_manage_py(self):
-        script = Path('manage.py')
-        script.touch()
-        self.addCleanup(script.unlink)
-        argv = ['./manage.py', 'runserver']
-        mock_call = self.patch_autoreload(argv)
-        autoreload.restart_with_reloader()
-        self.assertEqual(mock_call.call_count, 1)
-        self.assertEqual(mock_call.call_args[0][0], [self.executable, '-Wall'] + argv)
+        with tempfile.TemporaryDirectory() as temp_dir:
+            script = Path(temp_dir) / 'manage.py'
+            script.touch()
+            argv = [script, 'runserver']
+            mock_call = self.patch_autoreload(argv)
+            autoreload.restart_with_reloader()
+            self.assertEqual(mock_call.call_count, 1)
+            self.assertEqual(
+                mock_call.call_args[0][0],
+                [self.executable, '-Wall'] + argv,
+            )
 
     def test_python_m_django(self):
         main = '/usr/lib/pythonX.Y/site-packages/django/__main__.py'
