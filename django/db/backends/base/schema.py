7f4c9222dfe2f28ff8a7ffc56c28ccbadf19cf6f
Iuri de Silvio <iurisilvio@gmail.com>
2020-07-24 18:08:40 -0300

    Fixed #31825 -- Made RenameField operation a noop for fields with db_column.

--- django/db/backends/base/schema.py
+++ django/db/backends/base/schema.py
@@ -538,6 +538,8 @@ class BaseDatabaseSchemaEditor:
         If `strict` is True, raise errors if the old column does not match
         `old_field` precisely.
         """
+        if not self._field_should_be_altered(old_field, new_field):
+            return
         # Ensure this field is even column-based
         old_db_params = old_field.db_parameters(connection=self.connection)
         old_type = old_db_params['type']
@@ -1033,6 +1035,13 @@ class BaseDatabaseSchemaEditor:
         if self._field_should_be_indexed(model, field):
             output.append(self._create_index_sql(model, [field]))
         return output
+
+    def _field_should_be_altered(self, old_field, new_field):
+        # Don't alter when changing only a field name.
+        return (
+            old_field.column != new_field.column or
+            old_field.deconstruct()[1:] != new_field.deconstruct()[1:]
+        )
 
     def _field_should_be_indexed(self, model, field):
         return field.db_index and not field.unique
--- tests/migrations/test_operations.py
+++ tests/migrations/test_operations.py
@@ -1632,6 +1632,48 @@ class OperationTests(OperationTestBase):
         self.assertEqual(definition[1], [])
         self.assertEqual(definition[2], {'model_name': "Pony", 'old_name': "pink", 'new_name': "blue"})
 
+    def test_rename_field_with_db_column(self):
+        project_state = self.apply_operations('test_rfwdbc', ProjectState(), operations=[
+            migrations.CreateModel('Pony', fields=[
+                ('id', models.AutoField(primary_key=True)),
+                ('field', models.IntegerField(db_column='db_field')),
+                ('fk_field', models.ForeignKey(
+                    'Pony',
+                    models.CASCADE,
+                    db_column='db_fk_field',
+                )),
+            ]),
+        ])
+        new_state = project_state.clone()
+        operation = migrations.RenameField('Pony', 'field', 'renamed_field')
+        operation.state_forwards('test_rfwdbc', new_state)
+        self.assertIn('renamed_field', new_state.models['test_rfwdbc', 'pony'].fields)
+        self.assertNotIn('field', new_state.models['test_rfwdbc', 'pony'].fields)
+        self.assertColumnExists('test_rfwdbc_pony', 'db_field')
+        with connection.schema_editor() as editor:
+            with self.assertNumQueries(0):
+                operation.database_forwards('test_rfwdbc', editor, project_state, new_state)
+        self.assertColumnExists('test_rfwdbc_pony', 'db_field')
+        with connection.schema_editor() as editor:
+            with self.assertNumQueries(0):
+                operation.database_backwards('test_rfwdbc', editor, new_state, project_state)
+        self.assertColumnExists('test_rfwdbc_pony', 'db_field')
+
+        new_state = project_state.clone()
+        operation = migrations.RenameField('Pony', 'fk_field', 'renamed_fk_field')
+        operation.state_forwards('test_rfwdbc', new_state)
+        self.assertIn('renamed_fk_field', new_state.models['test_rfwdbc', 'pony'].fields)
+        self.assertNotIn('fk_field', new_state.models['test_rfwdbc', 'pony'].fields)
+        self.assertColumnExists('test_rfwdbc_pony', 'db_fk_field')
+        with connection.schema_editor() as editor:
+            with self.assertNumQueries(0):
+                operation.database_forwards('test_rfwdbc', editor, project_state, new_state)
+        self.assertColumnExists('test_rfwdbc_pony', 'db_fk_field')
+        with connection.schema_editor() as editor:
+            with self.assertNumQueries(0):
+                operation.database_backwards('test_rfwdbc', editor, new_state, project_state)
+        self.assertColumnExists('test_rfwdbc_pony', 'db_fk_field')
+
     def test_rename_missing_field(self):
         state = ProjectState()
         state.add_model(ModelState('app', 'model', []))
