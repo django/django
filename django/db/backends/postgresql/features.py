32cb1fe1c66645671681407d47e8370b4afbfc3f
Tim Graham <timograham@gmail.com>
2020-07-30 00:38:02 -0400

    [3.1.x] Refs #31829 -- Added DatabaseFeatures.json_key_contains_list_matching_requires_list.
    CockroachDB's behavior matches PostgreSQL.
    Backport of 184a6eebb0ef56d5f1b1315a8e666830e37f3f81 from master

--- django/db/backends/postgresql/features.py
+++ django/db/backends/postgresql/features.py
@@ -59,6 +59,7 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     validates_explain_options = False  # A query will error on invalid options.
     supports_deferrable_unique_constraints = True
     has_json_operators = True
+    json_key_contains_list_matching_requires_list = True
 
     @cached_property
     def is_postgresql_9_6(self):
