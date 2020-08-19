7eaa2776e1f176863c00b961bc76b28a3b5293c7
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2020-08-19 12:06:00 +0200

    [3.1.x] Fixed #31895 -- Fixed crash when decoding invalid session data.
    Thanks Matt Hegarty for the report.
    Regression in d4fff711d4c97356bd6ba1273d2a5e349326eb5f.
    Backport of 4376c2c7f8cd9c7604a7d053b3c4022dd5ac2795 from master

--- tests/sessions_tests/tests.py
+++ tests/sessions_tests/tests.py
@@ -333,11 +333,16 @@ class SessionTestsMixin:
             self.assertEqual(self.session._legacy_decode(encoded), data)
 
     def test_decode_failure_logged_to_security(self):
-        bad_encode = base64.b64encode(b'flaskdj:alkdjf').decode('ascii')
-        with self.assertLogs('django.security.SuspiciousSession', 'WARNING') as cm:
-            self.assertEqual({}, self.session.decode(bad_encode))
-        # The failed decode is logged.
-        self.assertIn('corrupted', cm.output[0])
+        tests = [
+            base64.b64encode(b'flaskdj:alkdjf').decode('ascii'),
+            'bad:encoded:value',
+        ]
+        for encoded in tests:
+            with self.subTest(encoded=encoded):
+                with self.assertLogs('django.security.SuspiciousSession', 'WARNING') as cm:
+                    self.assertEqual(self.session.decode(encoded), {})
+                # The failed decode is logged.
+                self.assertIn('Session data corrupted', cm.output[0])
 
     def test_actual_expiry(self):
         # this doesn't work with JSONSerializer (serializing timedelta)
