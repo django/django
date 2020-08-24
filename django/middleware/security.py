3a42c0447b6a3187bd2aaae0bba566d835c47d22
Kevin Michel <kevin.michel@sereema.com>
2020-08-24 22:25:33 +0200

    [3.1.x] Fixed #31928 -- Fixed detecting an async get_response in various middlewares.
    SecurityMiddleware and the three cache middlewares were not calling
    super().__init__() during their initialization or calling the required
    MiddlewareMixin._async_check() method.
    
    This made the middlewares not properly present as coroutine and
    confused the middleware chain when used in a fully async context.
    
    Thanks Kordian Kowalski for the report.
    
    Backport of 825ce75faec63ce81601e31152c757a9c28fed13 from master

--- django/middleware/security.py
+++ django/middleware/security.py
@@ -9,7 +9,7 @@ class SecurityMiddleware(MiddlewareMixin):
     # RemovedInDjango40Warning: when the deprecation ends, replace with:
     #   def __init__(self, get_response):
     def __init__(self, get_response=None):
-        self._get_response_none_deprecation(get_response)
+        super().__init__(get_response)
         self.sts_seconds = settings.SECURE_HSTS_SECONDS
         self.sts_include_subdomains = settings.SECURE_HSTS_INCLUDE_SUBDOMAINS
         self.sts_preload = settings.SECURE_HSTS_PRELOAD
@@ -19,7 +19,6 @@ class SecurityMiddleware(MiddlewareMixin):
         self.redirect_host = settings.SECURE_SSL_HOST
         self.redirect_exempt = [re.compile(r) for r in settings.SECURE_REDIRECT_EXEMPT]
         self.referrer_policy = settings.SECURE_REFERRER_POLICY
-        self.get_response = get_response
 
     def process_request(self, request):
         path = request.path.lstrip("/")
