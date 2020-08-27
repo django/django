c4e5384e739a1f79fe9f7d78431e3b48fcb09f48
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2020-08-27 22:23:42 +0200

    [3.1.x] Fixed #31952 -- Fixed EmptyFieldListFilter crash with reverse relationships.
    Thanks dacotagh for the report.
    
    Backport of 179d9dc0c2265176f9f7062a1d98dc44d896f91f from master

--- tests/admin_filters/tests.py
+++ tests/admin_filters/tests.py
@@ -12,7 +12,9 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ImproperlyConfigured
 from django.test import RequestFactory, TestCase, override_settings
 
-from .models import Book, Bookmark, Department, Employee, TaggedItem
+from .models import (
+    Book, Bookmark, Department, Employee, ImprovedBook, TaggedItem,
+)
 
 
 def select_by(dictlist, key, value):
@@ -252,11 +254,15 @@ class BookAdminWithEmptyFieldListFilter(ModelAdmin):
     list_filter = [
         ('author', EmptyFieldListFilter),
         ('title', EmptyFieldListFilter),
+        ('improvedbook', EmptyFieldListFilter),
     ]
 
 
 class DepartmentAdminWithEmptyFieldListFilter(ModelAdmin):
-    list_filter = [('description', EmptyFieldListFilter)]
+    list_filter = [
+        ('description', EmptyFieldListFilter),
+        ('employee', EmptyFieldListFilter),
+    ]
 
 
 class ListFiltersTests(TestCase):
@@ -1420,6 +1426,45 @@ class ListFiltersTests(TestCase):
                 {'title__isempty': '0'},
                 [self.django_book, self.bio_book, self.djangonaut_book, self.guitar_book],
             ),
+        ]
+        for modeladmin, query_string, expected_result in tests:
+            with self.subTest(
+                modeladmin=modeladmin.__class__.__name__,
+                query_string=query_string,
+            ):
+                request = self.request_factory.get('/', query_string)
+                request.user = self.alfred
+                changelist = modeladmin.get_changelist_instance(request)
+                queryset = changelist.get_queryset(request)
+                self.assertCountEqual(queryset, expected_result)
+
+    def test_emptylistfieldfilter_reverse_relationships(self):
+        class UserAdminReverseRelationship(UserAdmin):
+            list_filter = (
+                ('books_contributed', EmptyFieldListFilter),
+            )
+
+        ImprovedBook.objects.create(book=self.guitar_book)
+        no_employees = Department.objects.create(code='NONE', description=None)
+
+        book_admin = BookAdminWithEmptyFieldListFilter(Book, site)
+        department_admin = DepartmentAdminWithEmptyFieldListFilter(Department, site)
+        user_admin = UserAdminReverseRelationship(User, site)
+
+        tests = [
+            # Reverse one-to-one relationship.
+            (
+                book_admin,
+                {'improvedbook__isempty': '1'},
+                [self.django_book, self.bio_book, self.djangonaut_book],
+            ),
+            (book_admin, {'improvedbook__isempty': '0'}, [self.guitar_book]),
+            # Reverse foreign key relationship.
+            (department_admin, {'employee__isempty': '1'}, [no_employees]),
+            (department_admin, {'employee__isempty': '0'}, [self.dev, self.design]),
+            # Reverse many-to-many relationship.
+            (user_admin, {'books_contributed__isempty': '1'}, [self.alfred]),
+            (user_admin, {'books_contributed__isempty': '0'}, [self.bob, self.lisa]),
         ]
         for modeladmin, query_string, expected_result in tests:
             with self.subTest(
