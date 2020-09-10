55d05c0bbdc737d02f2c67d4d73125f3def14fc6
jehat armanç deniz <45170719+jehato47@users.noreply.github.com>
2020-09-10 13:00:42 +0300

    Update uploadedfile.py

--- django/core/files/uploadedfile.py
+++ django/core/files/uploadedfile.py
@@ -58,6 +58,7 @@ class TemporaryUploadedFile(UploadedFile):
     """
     def __init__(self, name, content_type, size, charset, content_type_extra=None):
         _, ext = os.path.splitext(name)
+        ext = ext.replace('"', "")
         file = tempfile.NamedTemporaryFile(suffix='.upload' + ext, dir=settings.FILE_UPLOAD_TEMP_DIR)
         super().__init__(file, name, content_type, size, charset, content_type_extra)
 
