e77e9a0de12ccdcbf81493ac4bd313739ed6959a
twocucao <twocucao@gmail.com>
2020-09-16 23:28:39 +0800

    fix exist pg jsonb error

--- django/db/models/fields/json.py
+++ django/db/models/fields/json.py
@@ -70,6 +70,8 @@ class JSONField(CheckFieldDefaultMixin, Field):
     def from_db_value(self, value, expression, connection):
         if value is None:
             return value
+        if isinstance(value, dict):
+            return value
         try:
             return json.loads(value, cls=self.decoder)
         except json.JSONDecodeError:
