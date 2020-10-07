4fd7c6f0f2d57d2f998b058463c1c71b8aada940
manav014 <dpsman13016@gmail.com>
2020-10-08 04:34:34 +0530

    Made ManagementForm exception, in case of bad prefix, easier to understand

--- django/forms/formsets.py
+++ django/forms/formsets.py
@@ -90,7 +90,7 @@ class BaseFormSet:
             form = ManagementForm(self.data, auto_id=self.auto_id, prefix=self.prefix)
             if not form.is_valid():
                 raise ValidationError(
-                    _('ManagementForm data is missing or has been tampered with'),
+                    _('ManagementForm data is missing or has been tampered with %s' % form._errors),
                     code='missing_management_form',
                 )
         else:
