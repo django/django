8c403b17f94a4ace87bfdd7d40bbc3afb18f7577
Qi Zhao <zhaoqi99@outlook.com>
2020-10-08 21:51:14 +0800

    [3.1.x] Fixed #32080 -- Fixed displaying Unicode chars in forms.JSONField and read-only JSONField values in admin.
    Backport of de81676b51e4dad510ef387c3ae625f9091fe57f from master

--- django/forms/fields.py
+++ django/forms/fields.py
@@ -1265,7 +1265,7 @@ class JSONField(CharField):
     def prepare_value(self, value):
         if isinstance(value, InvalidJSONInput):
             return value
-        return json.dumps(value, cls=self.encoder)
+        return json.dumps(value, ensure_ascii=False, cls=self.encoder)
 
     def has_changed(self, initial, data):
         if super().has_changed(initial, data):
--- tests/forms_tests/field_tests/test_jsonfield.py
+++ tests/forms_tests/field_tests/test_jsonfield.py
@@ -29,6 +29,12 @@ class JSONFieldTest(SimpleTestCase):
         self.assertEqual(field.prepare_value({'a': 'b'}), '{"a": "b"}')
         self.assertEqual(field.prepare_value(None), 'null')
         self.assertEqual(field.prepare_value('foo'), '"foo"')
+        self.assertEqual(field.prepare_value('你好，世界'), '"你好，世界"')
+        self.assertEqual(field.prepare_value({'a': '😀🐱'}), '{"a": "😀🐱"}')
+        self.assertEqual(
+            field.prepare_value(["你好，世界", "jaźń"]),
+            '["你好，世界", "jaźń"]',
+        )
 
     def test_widget(self):
         field = JSONField()
