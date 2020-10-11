a4d5074b7d61bbdabca1d31e2f9cef25ab165b9a
Pallav parikh <pallav8parikh@gmail.com>
2020-10-11 22:43:13 +0530

    setting expire_date on load() call, Added documentation

--- django/contrib/sessions/backends/db.py
+++ django/contrib/sessions/backends/db.py
@@ -43,7 +43,10 @@ class SessionStore(SessionBase):
 
     def load(self):
         s = self._get_session_from_db()
-        return self.decode(s.session_data) if s else {}
+        if s:
+            self._expire_date = s.expire_date
+            return self.decode(s.session_data)
+        return {}
 
     def exists(self, session_key):
         return self.model.objects.filter(session_key=session_key).exists()
