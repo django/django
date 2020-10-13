b4b8ca489534621ba2633a4037e5b9b4710bf88f
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2020-10-13 08:35:01 +0200

    [2.2.x] Refs #31040 -- Doc'd Python 3.9 compatibility.
    Backport of e18156b6c35908f2a4026287b5225a6a4da8af1a from master.

--- setup.py
+++ setup.py
@@ -102,6 +102,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
