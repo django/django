c82c9cd1cc21d565ad11fdde666264f9787ae7fe
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2020-10-15 13:16:40 +0200

    Refs #32096 -- Made JSONField check respect Meta.required_db_vendor.
    Thanks Simon Charette for the implementation idea.

--- django/db/models/fields/json.py
+++ django/db/models/fields/json.py
@@ -45,6 +45,11 @@ class JSONField(CheckFieldDefaultMixin, Field):
             if not router.allow_migrate_model(db, self.model):
                 continue
             connection = connections[db]
+            if (
+                self.model._meta.required_db_vendor and
+                self.model._meta.required_db_vendor != connection.vendor
+            ):
+                continue
             if not (
                 'supports_json_field' in self.model._meta.required_db_features or
                 connection.features.supports_json_field
