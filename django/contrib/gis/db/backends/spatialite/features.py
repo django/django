0eee5c1b9c2e306aa2c2807daf146ee88676bc97
Tim Graham <timograham@gmail.com>
2020-10-19 06:41:52 -0400

    Added DatabaseFeatures.can_alter_geometry_field.

--- django/contrib/gis/db/backends/spatialite/features.py
+++ django/contrib/gis/db/backends/spatialite/features.py
@@ -6,6 +6,7 @@ from django.utils.functional import cached_property
 
 
 class DatabaseFeatures(BaseSpatialFeatures, SQLiteDatabaseFeatures):
+    can_alter_geometry_field = False  # Not implemented
     supports_3d_storage = True
 
     @cached_property
