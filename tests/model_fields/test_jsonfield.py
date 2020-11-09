0773837e15bb632afffb6848a58c59a791008fa1
Hannes Ljungberg <hannes.ljungberg@gmail.com>
2020-11-09 17:54:20 +0100

    Fixed #32182 -- Fixed crash of JSONField nested key transforms with subquery annotations on PostgreSQL.

--- tests/model_fields/test_jsonfield.py
+++ tests/model_fields/test_jsonfield.py
@@ -408,6 +408,18 @@ class TestQuerying(TestCase):
             [self.objs[4]],
         )
 
+    def test_nested_key_transform_on_subquery(self):
+        self.assertSequenceEqual(
+            NullableJSONModel.objects.filter(value__d__0__isnull=False).annotate(
+                subquery_value=Subquery(
+                    NullableJSONModel.objects.filter(pk=OuterRef('pk')).values('value')
+                ),
+                key=KeyTransform('d', 'subquery_value'),
+                chain=KeyTransform('f', KeyTransform('1', 'key')),
+            ).filter(chain='g'),
+            [self.objs[4]],
+        )
+
     def test_expression_wrapper_key_transform(self):
         self.assertSequenceEqual(
             NullableJSONModel.objects.annotate(
