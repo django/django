cc3870c30f0338a09e440430d5dcc028d23f1a02
Hannes Ljungberg <hannes.ljungberg@gmail.com>
2020-11-09 17:54:20 +0100

    [3.1.x] Fixed #32182 -- Fixed crash of JSONField nested key transforms with subquery annotations on PostgreSQL.
    Backport of 0773837e15bb632afffb6848a58c59a791008fa1 from master

--- django/db/models/fields/json.py
+++ django/db/models/fields/json.py
@@ -302,7 +302,8 @@ class KeyTransform(Transform):
     def as_postgresql(self, compiler, connection):
         lhs, params, key_transforms = self.preprocess_lhs(compiler, connection)
         if len(key_transforms) > 1:
-            return '(%s %s %%s)' % (lhs, self.postgres_nested_operator), params + [key_transforms]
+            sql = '(%s %s %%s)' % (lhs, self.postgres_nested_operator)
+            return sql, tuple(params) + (key_transforms,)
         try:
             lookup = int(self.key_name)
         except ValueError:
