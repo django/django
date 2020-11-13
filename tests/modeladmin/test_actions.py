fed8129276eac973fd48a2f33b7e1735a866628d
Nick Pope <nick.pope@flightdataservices.com>
2020-11-13 20:41:18 +0000

    Unified admin action description generation.
    Actions added to AdminSite.actions would not have the first character
    of their description capitalized.
    
    Co-authored-by: Mariusz Felisiak <felisiak.mariusz@gmail.com>

--- tests/modeladmin/test_actions.py
+++ tests/modeladmin/test_actions.py
@@ -77,6 +77,31 @@ class AdminActionsTests(TestCase):
         action_names = [name for _, name, _ in ma2._get_base_actions()]
         self.assertEqual(action_names, ['delete_selected'])
 
+    def test_global_actions_description(self):
+        def global_action_1(modeladmin, request, queryset):
+            pass
+
+        def global_action_2(modeladmin, request, queryset):
+            pass
+
+        global_action_1.short_description = 'Site-wide admin action 1.'
+        admin_site = admin.AdminSite()
+        admin_site.add_action(global_action_1)
+        admin_site.add_action(global_action_2)
+
+        class BandAdmin(admin.ModelAdmin):
+            pass
+
+        ma = BandAdmin(Band, admin_site)
+        self.assertEqual(
+            [description for _, _, description in ma._get_base_actions()],
+            [
+                'Delete selected %(verbose_name_plural)s',
+                'Site-wide admin action 1.',
+                'Global action 2',
+            ],
+        )
+
     def test_actions_replace_global_action(self):
         def global_action_1(modeladmin, request, queryset):
             pass
