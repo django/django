a06b977a91f043c509df781670fb4cf35cb437b7
Hannes Ljungberg <hannes.ljungberg@gmail.com>
2020-11-20 21:35:04 +0100

    Fixed #32776 -- Added support for Array subqueries on PostgreSQL.

---
+++ django/contrib/postgres/expressions.py
@@ -0,0 +1,14 @@
+from django.contrib.postgres.fields import ArrayField
+from django.db.models import Subquery
+from django.utils.functional import cached_property
+
+
+class ArraySubquery(Subquery):
+    template = 'ARRAY(%(subquery)s)'
+
+    def __init__(self, queryset, **kwargs):
+        super().__init__(queryset, **kwargs)
+
+    @cached_property
+    def output_field(self):
+        return ArrayField(self.query.output_field)
