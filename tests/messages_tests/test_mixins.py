bed6d8bdc6a3a07c82a925161201e684800d9dac
Craig Smith <hello@craigiansmith.com.au>
2020-11-22 13:48:13 +1100

    Remove debug print statements

--- tests/messages_tests/test_mixins.py
+++ tests/messages_tests/test_mixins.py
@@ -12,8 +12,6 @@ class SuccessMessageMixinTests(SimpleTestCase):
         author = {'name': 'John Doe', 'slug': 'success-msg'}
         add_url = reverse('add_success_msg')
         req = self.client.post(add_url, author)
-        print(type(req.cookies['messages'].value))
-        print(req.cookies['messages'].value)
         self.assertIn(
             bytes(ContactFormViewWithMsg.success_message % author, 'utf-8'),
             b64decode_decompress(req.cookies['messages'].value))
