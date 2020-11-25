97bfe0cba55720802abc7a7190ccb1ac575da50d
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2020-11-25 12:19:45 +0100

    [3.1.x] Fixed #32224 -- Avoided suppressing connection errors in supports_json_field on SQLite.
    Regression in 6789ded0a6ab797f0dcdfa6ad5d1cfa46e23abcd.
    
    Thanks Juan Garcia Alvite for the report.
    Backport of f5e5aac59ebbcea46b98d37834915de0f43d7cc8 from master

--- django/db/backends/sqlite3/features.py
+++ django/db/backends/sqlite3/features.py
@@ -54,11 +54,12 @@ class DatabaseFeatures(BaseDatabaseFeatures):
 
     @cached_property
     def supports_json_field(self):
-        try:
-            with self.connection.cursor() as cursor, transaction.atomic():
-                cursor.execute('SELECT JSON(\'{"a": "b"}\')')
-        except OperationalError:
-            return False
+        with self.connection.cursor() as cursor:
+            try:
+                with transaction.atomic(self.connection.alias):
+                    cursor.execute('SELECT JSON(\'{"a": "b"}\')')
+            except OperationalError:
+                return False
         return True
 
     can_introspect_json_field = property(operator.attrgetter('supports_json_field'))
