4555aa0a489cb9dcf764edf12339097cdfa5ff84
Giannis Adamopoulos <gadamopoulos@outlook.com>
2020-12-09 19:12:56 +0200

    Fixed #27674 -- Deprecated GeoModelAdmin and OSMGeoAdmin.
    Co-authored-by: Mariusz Felisiak <felisiak.mariusz@gmail.com>

--- django/contrib/gis/admin/__init__.py
+++ django/contrib/gis/admin/__init__.py
@@ -2,11 +2,15 @@ from django.contrib.admin import (
     HORIZONTAL, VERTICAL, AdminSite, ModelAdmin, StackedInline, TabularInline,
     action, autodiscover, display, register, site,
 )
-from django.contrib.gis.admin.options import GeoModelAdmin, OSMGeoAdmin
+from django.contrib.gis.admin.options import (
+    GeoModelAdmin, GISModelAdmin, OSMGeoAdmin,
+)
 from django.contrib.gis.admin.widgets import OpenLayersWidget
 
 __all__ = [
     'HORIZONTAL', 'VERTICAL', 'AdminSite', 'ModelAdmin', 'StackedInline',
     'TabularInline', 'action', 'autodiscover', 'display', 'register', 'site',
-    'GeoModelAdmin', 'OSMGeoAdmin', 'OpenLayersWidget',
+    'GISModelAdmin', 'OpenLayersWidget',
+    # RemovedInDjango50Warning.
+    'GeoModelAdmin', 'OSMGeoAdmin',
 ]
