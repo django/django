a891e1bb0a3e1b29a461784cd0bf717513e72f19
sage <laymonage@gmail.com>
2020-12-09 21:45:18 +0700

    [3.1.x] Fixed #32252 -- Fixed __isnull=True on key transforms on SQLite and Oracle.
    __isnull=True on key transforms should not match keys with NULL values.
    
    Backport of 8d7085e0fd004af5431389f3d903aba6220d7957 from master

--- tests/model_fields/test_jsonfield.py
+++ tests/model_fields/test_jsonfield.py
@@ -535,6 +535,10 @@ class TestQuerying(TestCase):
             self.objs[:3] + self.objs[5:],
         )
         self.assertSequenceEqual(
+            NullableJSONModel.objects.filter(value__j__isnull=True),
+            self.objs[:4] + self.objs[5:],
+        )
+        self.assertSequenceEqual(
             NullableJSONModel.objects.filter(value__a__isnull=False),
             [self.objs[3], self.objs[4]],
         )
