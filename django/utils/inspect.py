ef39a8829b6a81116876fe97cff8de6b8ec15f49
Adam Johnson <me@adamj.eu>
2020-12-14 07:34:35 +0100

    Added docstring to django.utils.inspect.func_accepts_kwargs().

--- django/utils/inspect.py
+++ django/utils/inspect.py
@@ -40,6 +40,7 @@ def get_func_full_args(func):
 
 
 def func_accepts_kwargs(func):
+    """Return True if function 'func' accepts keyword arguments **kwargs."""
     return any(
         p for p in _get_signature(func).parameters.values()
         if p.kind == p.VAR_KEYWORD
