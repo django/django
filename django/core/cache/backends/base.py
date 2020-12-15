a9d736d9c47d46e53c2956b04c171af1804165e7
Abhishek01039 <abhishekghaskata1999@gmail.com>
2020-12-15 10:18:41 +0530

    added quotation of key_prefix

--- django/core/cache/backends/base.py
+++ django/core/cache/backends/base.py
@@ -31,7 +31,7 @@ def default_key_func(key, key_prefix, version):
     Default function to generate keys.
 
     Construct the key used by all other methods. By default, prepend
-    the `key_prefix'. KEY_FUNCTION can be used to specify an alternate
+    the `key_prefix`. KEY_FUNCTION can be used to specify an alternate
     function with custom key making behavior.
     """
     return '%s:%s:%s' % (key_prefix, version, key)
