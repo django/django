8d582bf51058e3a8094a58f624c81bd95b6edfb1
Hasan Ramezani <hasan.r67@gmail.com>
2020-12-17 22:06:58 +0100

    Fixed #32262 -- Fixed migration optimization for model creation and Meta options removal.

--- django/db/migrations/operations/models.py
+++ django/db/migrations/operations/models.py
@@ -137,11 +137,15 @@ class CreateModel(ModelOperation):
                 ),
             ]
         elif isinstance(operation, AlterModelOptions) and self.name_lower == operation.name_lower:
+            options = {**self.options, **operation.options}
+            for key in operation.ALTER_OPTION_KEYS:
+                if key not in operation.options:
+                    options.pop(key, None)
             return [
                 CreateModel(
                     self.name,
                     fields=self.fields,
-                    options={**self.options, **operation.options},
+                    options=options,
                     bases=self.bases,
                     managers=self.managers,
                 ),
