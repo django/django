c978dd93fda87c6d2e965d385164c35f1a3e64b8
Hasan Ramezani <hasan.r67@gmail.com>
2020-12-27 21:21:59 +0100

    Fixed #32290 -- Fixed TemplateNotFound in {% include %} tag for relative path in variable.

--- tests/template_tests/test_extends_relative.py
+++ tests/template_tests/test_extends_relative.py
@@ -70,6 +70,12 @@ class IncludeRelativeBehaviorTests(SimpleTestCase):
         output = template.render(Context({}))
         self.assertEqual(output.strip(), 'dir2 include')
 
+    def test_normal_include_variable(self):
+        engine = Engine(dirs=[RELATIVE])
+        template = engine.get_template('dir1/dir2/inc3.html')
+        output = template.render(Context({'tmpl': './include_content.html'}))
+        self.assertEqual(output.strip(), 'dir2 include')
+
     def test_dir2_include(self):
         engine = Engine(dirs=[RELATIVE])
         template = engine.get_template('dir1/dir2/inc1.html')
