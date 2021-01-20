e45b944d69b6bacaccb22b351a76272aa4f93581
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2021-01-20 12:07:14 +0100

    [3.2.x] Fixed #32367 -- Fixed system check for specifying type of auto-created primary keys for inherited PKs.
    Regression in b5e12d490af3debca8c55ab3c1698189fdedbbdb.
    
    Thanks אורי for the report.
    
    Backport of a03a36121d22c8784985c7e45727eddef6a3ea7f from master

--- django/db/models/base.py
+++ django/db/models/base.py
@@ -1299,6 +1299,11 @@ class Model(metaclass=ModelBase):
     def _check_default_pk(cls):
         if (
             cls._meta.pk.auto_created and
+            # Inherited PKs are checked in parents models.
+            not (
+                isinstance(cls._meta.pk, OneToOneField) and
+                cls._meta.pk.remote_field.parent_link
+            ) and
             not settings.is_overridden('DEFAULT_AUTO_FIELD') and
             not cls._meta.app_config._is_default_auto_field_overridden
         ):
