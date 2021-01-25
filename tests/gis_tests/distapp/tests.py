ea45521ad0e30819d2bfd2f8aecd87cc0ecec31d
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2021-01-25 07:37:28 +0100

    Refs #32380 -- Added test for distance lookups with F() expression.
    Fixed in 5935a9aeade517aebdceea989467d2b46c44d96f.

--- tests/gis_tests/distapp/tests.py
+++ tests/gis_tests/distapp/tests.py
@@ -204,10 +204,12 @@ class DistanceTest(TestCase):
         # With spheroid param
         if connection.features.supports_distance_geodetic:
             hobart = AustraliaCity.objects.get(name='Hobart')
-            qs = AustraliaCity.objects.filter(
-                point__distance_lte=(hobart.point, F('radius') * 70, 'spheroid'),
-            ).order_by('name')
-            self.assertEqual(self.get_names(qs), ['Canberra', 'Hobart', 'Melbourne'])
+            AustraliaCity.objects.update(ref_point=hobart.point)
+            for ref_point in [hobart.point, F('ref_point')]:
+                qs = AustraliaCity.objects.filter(
+                    point__distance_lte=(ref_point, F('radius') * 70, 'spheroid'),
+                ).order_by('name')
+                self.assertEqual(self.get_names(qs), ['Canberra', 'Hobart', 'Melbourne'])
 
         # With a complex geometry expression
         self.assertFalse(SouthTexasCity.objects.filter(point__distance_gt=(Union('point', 'point'), 0)))
