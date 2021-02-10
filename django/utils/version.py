4372233ebf9acef606f3d2f7be4d6ba73b168084
Carlton Gibson <carlton.gibson@noumenal.es>
2021-02-10 15:56:30 +0100

    Refs #32355 -- Restored PY36 and PY37 version constants.
    Partially reverts ec0ff406311de88f4e2a135d784363424fe602aa.
    
    PY36 should be removed when Django 2.2 is EOL.
    PY37 should be removed when Django 3.2 is EOL.
    
    Thanks to Tim Graham for the report.

--- django/utils/version.py
+++ django/utils/version.py
@@ -9,6 +9,8 @@ from distutils.version import LooseVersion
 # or later". So that third-party apps can use these values, each constant
 # should remain as long as the oldest supported Django version supports that
 # Python version.
+PY36 = sys.version_info >= (3, 6)
+PY37 = sys.version_info >= (3, 7)
 PY38 = sys.version_info >= (3, 8)
 PY39 = sys.version_info >= (3, 9)
 
