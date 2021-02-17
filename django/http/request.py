860948617569ca84d80e4a422a62ec587eb8c54a
BeryCZ <martin-beran@seznam.cz>
2021-02-17 08:12:55 +0100

    Update request.py
    from django.http.multipartparser import MultiPartParser, MultiPartParserError
    ...
    from .multipartparser import parse_header
    
    I don't see the reason to have those separately

--- django/http/request.py
+++ django/http/request.py
@@ -11,7 +11,7 @@ from django.core.exceptions import (
     DisallowedHost, ImproperlyConfigured, RequestDataTooBig, TooManyFieldsSent,
 )
 from django.core.files import uploadhandler
-from django.http.multipartparser import MultiPartParser, MultiPartParserError
+from django.http.multipartparser import MultiPartParser, MultiPartParserError, parse_header
 from django.utils.datastructures import (
     CaseInsensitiveMapping, ImmutableList, MultiValueDict,
 )
@@ -19,8 +19,6 @@ from django.utils.encoding import escape_uri_path, iri_to_uri
 from django.utils.functional import cached_property
 from django.utils.http import is_same_domain
 from django.utils.regex_helper import _lazy_re_compile
-
-from .multipartparser import parse_header
 
 RAISE_ERROR = object()
 host_validation_re = _lazy_re_compile(r"^([a-z0-9.-]+|\[[a-f0-9]*:[a-f0-9\.:]+\])(:\d+)?$")
