0645aac1214cdc767a098889f2b9be67f7be4267
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-03-06 15:24:56 +0100

    An implementation of a SuccessDeleteMessageMixin regarding https://code.djangoproject.com/ticket/32514

--- django/contrib/messages/views.py
+++ django/contrib/messages/views.py
@@ -16,3 +16,21 @@ class SuccessMessageMixin:
 
     def get_success_message(self, cleaned_data):
         return self.success_message % cleaned_data
+
+
+class SuccessDeleteMessageMixin:
+    """
+    Add a success message on a successful DELETE request (and sometimes POST request
+    if this will make trigger delete).
+    """
+    success_message = ''
+
+    def delete(self, *args, **kwargs):
+        response = super().delete(*args, **kwargs)
+        success_message = self.get_success_message()
+        if success_message:
+            messages.success(self.request, success_message)
+        return response
+
+    def get_success_message(self):
+        return self.success_message
