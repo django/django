1a20e307b598461aac18b7dc6aafd2046df32f61
kaocher <kaocher82@gmail.com>
2021-03-25 17:29:50 +0600

    App configure
    Configuration

--- django/apps/__init__.py
+++ django/apps/__init__.py
@@ -1,4 +1,23 @@
-from .config import AppConfig
-from .registry import apps
 
-__all__ = ['AppConfig', 'apps']
+
+
+__all__ = [x for x in dir(context._default_context) if not x.startswith('_')]
+globals().update((name, getattr(context._default_context, name)) for name in __all__)
+
+#
+# XXX These should not really be documented or public.
+#
+
+SUBDEBUG = 5
+SUBWARNING = 25
+
+#
+# Alias for main module -- will be reset by bootstrapping child processes
+#
+
+if '__main__' in sys.modules:
+    sys.modules['__mp_main__'] = sys.modules['__main__']
+    from .config import AppConfig
+    from .registry import apps
+
+    __all__ = ['AppConfig', 'apps']
