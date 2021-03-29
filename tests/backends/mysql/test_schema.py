5c689248e612c6ba4928443dc0e864098b9b2c5f
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2021-03-29 10:10:19 +0200

    Fixed #32595 -- Fixed SchemaEditor.quote_value() crash with bytes.

--- tests/backends/mysql/test_schema.py
+++ tests/backends/mysql/test_schema.py
@@ -12,6 +12,7 @@ class SchemaEditorTests(TestCase):
         tested_values = [
             ('string', "'string'"),
             ('¿Tú hablas inglés?', "'¿Tú hablas inglés?'"),
+            (b'bytes', b"'bytes'"),
             (42, '42'),
             (1.754, '1.754e0' if MySQLdb.version_info >= (1, 3, 14) else '1.754'),
             (False, b'0' if MySQLdb.version_info >= (1, 4, 0) else '0'),
