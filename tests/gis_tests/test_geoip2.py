f243e7892f293cfc8c1b4cb097a55230b6c0e2b0
Nick Pope <nick@nickpope.me.uk>
2021-04-05 23:32:02 +0100

    Refs #35092 -- Exposed extra fields for GeoIP2.city() responses.
    Adds ``accuracy_radius``, ``metro_code``, ``region_code``, and
    ``region_name``. Preserves ``dma_code`` and ``region`` for backward
    compatibility.

--- tests/gis_tests/test_geoip2.py
+++ tests/gis_tests/test_geoip2.py
@@ -125,18 +125,23 @@ class GeoLite2Test(SimpleTestCase):
                 self.assertEqual(
                     g.city(query),
                     {
+                        "accuracy_radius": 100,
                         "city": "Boxford",
                         "continent_code": "EU",
                         "continent_name": "Europe",
                         "country_code": "GB",
                         "country_name": "United Kingdom",
-                        "dma_code": None,
                         "is_in_european_union": False,
                         "latitude": 51.75,
                         "longitude": -1.25,
+                        "metro_code": None,
                         "postal_code": "OX1",
-                        "region": "ENG",
+                        "region_code": "ENG",
+                        "region_name": "England",
                         "time_zone": "Europe/London",
+                        # Kept for backward compatibility.
+                        "dma_code": None,
+                        "region": "ENG",
                     },
                 )
 
