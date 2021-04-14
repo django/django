cbaf61e205c597eb77d5764c2b87f3d6ecc7f936
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2021-04-14 12:21:27 +0200

    Fixed #32645 -- Fixed QuerySet.update() crash when ordered by joined fields on MySQL/MariaDB.
    Thanks Matt Westcott for the report.
    
    Regression in 779e615e362108862f1681f965ee9e4f1d0ae6d2.

--- django/db/backends/mysql/compiler.py
+++ django/db/backends/mysql/compiler.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import FieldError
+from django.db.models.expressions import Col
 from django.db.models.sql import compiler
 
 
@@ -45,8 +46,16 @@ class SQLUpdateCompiler(compiler.SQLUpdateCompiler, SQLCompiler):
         if self.query.order_by:
             order_by_sql = []
             order_by_params = []
+            db_table = self.query.get_meta().db_table
             try:
-                for _, (sql, params, _) in self.get_order_by():
+                for resolved, (sql, params, _) in self.get_order_by():
+                    if (
+                        isinstance(resolved.expression, Col) and
+                        resolved.expression.alias != db_table
+                    ):
+                        # Ignore ordering if it contains joined fields, because
+                        # they cannot be used in the ORDER BY clause.
+                        raise FieldError
                     order_by_sql.append(sql)
                     order_by_params.extend(params)
                 update_query += ' ORDER BY ' + ', '.join(order_by_sql)
