23fa29f6a6659e0f600d216de6bcb79e7f6818c9
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2021-04-14 12:23:47 +0200

    Fixed #32649 -- Fixed ModelAdmin.search_fields crash when searching against phrases with unbalanced quotes.
    Thanks Dlis for the report.
    
    Regression in 26a413507abb38f7eee4cf62f2ee9727fdc7bf8d.

--- django/contrib/admin/options.py
+++ django/contrib/admin/options.py
@@ -1025,7 +1025,7 @@ class ModelAdmin(BaseModelAdmin):
             orm_lookups = [construct_search(str(search_field))
                            for search_field in search_fields]
             for bit in smart_split(search_term):
-                if bit.startswith(('"', "'")):
+                if bit.startswith(('"', "'")) and bit[0] == bit[-1]:
                     bit = unescape_string_literal(bit)
                 or_queries = [models.Q(**{orm_lookup: bit})
                               for orm_lookup in orm_lookups]
