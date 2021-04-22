79a12e9b6a5eb526e1985ad189d1e61aae028d70
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2021-04-22 18:10:56 +0200

    Fixed isolation of i18n.tests.FormattingTests.test_get_custom_format().

--- tests/i18n/tests.py
+++ tests/i18n/tests.py
@@ -1254,6 +1254,7 @@ class FormattingTests(SimpleTestCase):
         self.assertEqual(get_format('DEBUG'), 'DEBUG')
 
     def test_get_custom_format(self):
+        reset_format_cache()
         with self.settings(FORMAT_MODULE_PATH='i18n.other.locale'):
             with translation.override('fr', deactivate=True):
                 self.assertEqual('d/m/Y CUSTOM', get_format('CUSTOM_DAY_FORMAT'))
