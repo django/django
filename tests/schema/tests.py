d36d0004281aee78f8cb5e75675b13402e0f6f86
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2021-04-23 17:59:02 +0200

    Fixed isolation of test_rename_table_renames_deferred_sql_references().

--- tests/schema/tests.py
+++ tests/schema/tests.py
@@ -3817,10 +3817,14 @@ class SchemaTests(TransactionTestCase):
             editor.create_model(Book)
             editor.alter_db_table(Author, 'schema_author', 'schema_renamed_author')
             editor.alter_db_table(Author, 'schema_book', 'schema_renamed_book')
-            self.assertGreater(len(editor.deferred_sql), 0)
-            for statement in editor.deferred_sql:
-                self.assertIs(statement.references_table('schema_author'), False)
-                self.assertIs(statement.references_table('schema_book'), False)
+            try:
+                self.assertGreater(len(editor.deferred_sql), 0)
+                for statement in editor.deferred_sql:
+                    self.assertIs(statement.references_table('schema_author'), False)
+                    self.assertIs(statement.references_table('schema_book'), False)
+            finally:
+                editor.alter_db_table(Author, 'schema_renamed_author', 'schema_author')
+                editor.alter_db_table(Author, 'schema_renamed_book', 'schema_book')
 
     def test_rename_column_renames_deferred_sql_references(self):
         with connection.schema_editor() as editor:
