d5add5d3a26f98e961dfbcad67bb04d936f2f332
Simon Charette <charette.s@gmail.com>
2021-04-24 01:07:18 -0400

    [3.2.x] Fixed #32632, Fixed #32657 -- Removed flawed support for Subquery deconstruction.
    Subquery deconstruction support required implementing complex and
    expensive equality rules for sql.Query objects for little benefit as
    the latter cannot themselves be made deconstructible to their reference
    to model classes.
    
    Making Expression @deconstructible and not BaseExpression allows
    interested parties to conform to the "expression" API even if they are
    not deconstructible as it's only a requirement for expressions allowed
    in Model fields and meta options (e.g. constraints, indexes).
    
    Thanks Phillip Cutter for the report.
    
    This also fixes a performance regression in bbf141bcdc31f1324048af9233583a523ac54c94.
    
    Backport of c8b659430556dca0b2fe27cf2ea0f8290dbafecd from main

--- django/db/models/expressions.py
+++ django/db/models/expressions.py
@@ -147,7 +147,6 @@ class Combinable:
         )
 
 
-@deconstructible
 class BaseExpression:
     """Base class for all query expressions."""
 
@@ -389,6 +388,11 @@ class BaseExpression:
             return self.output_field.select_format(compiler, sql, params)
         return sql, params
 
+
+@deconstructible
+class Expression(BaseExpression, Combinable):
+    """An expression that can be combined with other expressions."""
+
     @cached_property
     def identity(self):
         constructor_signature = inspect.signature(self.__init__)
@@ -409,17 +413,12 @@ class BaseExpression:
         return tuple(identity)
 
     def __eq__(self, other):
-        if not isinstance(other, BaseExpression):
+        if not isinstance(other, Expression):
             return NotImplemented
         return other.identity == self.identity
 
     def __hash__(self):
         return hash(self.identity)
-
-
-class Expression(BaseExpression, Combinable):
-    """An expression that can be combined with other expressions."""
-    pass
 
 
 _connector_combinators = {
@@ -1085,7 +1084,7 @@ class Case(Expression):
         return super().get_group_by_cols(alias)
 
 
-class Subquery(Expression):
+class Subquery(BaseExpression, Combinable):
     """
     An explicit subquery. It may contain OuterRef() references to the outer
     query which will be resolved when it is applied to that query.
@@ -1098,16 +1097,6 @@ class Subquery(Expression):
         self.query = getattr(queryset, 'query', queryset)
         self.extra = extra
         super().__init__(output_field)
-
-    def __getstate__(self):
-        state = super().__getstate__()
-        args, kwargs = state['_constructor_args']
-        if args:
-            args = (self.query, *args[1:])
-        else:
-            kwargs['queryset'] = self.query
-        state['_constructor_args'] = args, kwargs
-        return state
 
     def get_source_expressions(self):
         return [self.query]
@@ -1185,6 +1174,7 @@ class Exists(Subquery):
         return sql, params
 
 
+@deconstructible
 class OrderBy(BaseExpression):
     template = '%(expression)s %(ordering)s'
     conditional = False
