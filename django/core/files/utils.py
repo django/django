38e577993872da70c43468271d04d329aee7ec13
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2021-05-10 09:06:59 +0200

    Fixed #32718 -- Relaxed file name validation in FileField.
    - Validate filename returned by FileField.upload_to() not a filename
      passed to the FileField.generate_filename() (upload_to() may
      completely ignored passed filename).
    - Allow relative paths (without dot segments) in the generated filename.
    
    Thanks Jakub Kleň for the report.
    Thanks Florian Apolloner and Markus Holtermann for the discussion and
    implementation idea.
    
    Regression in 0b79eb36915d178aef5c6a7bbce71b1e76d376d3.

--- django/core/files/utils.py
+++ django/core/files/utils.py
@@ -1,15 +1,25 @@
 import os
+import pathlib
 
 from django.core.exceptions import SuspiciousFileOperation
 
 
-def validate_file_name(name):
-    if name != os.path.basename(name):
-        raise SuspiciousFileOperation("File name '%s' includes path elements" % name)
-
+def validate_file_name(name, allow_relative_path=False):
     # Remove potentially dangerous names
-    if name in {'', '.', '..'}:
+    if os.path.basename(name) in {'', '.', '..'}:
         raise SuspiciousFileOperation("Could not derive file name from '%s'" % name)
+
+    if allow_relative_path:
+        # Use PurePosixPath() because this branch is checked only in
+        # FileField.generate_filename() where all file paths are expected to be
+        # Unix style (with forward slashes).
+        path = pathlib.PurePosixPath(name)
+        if path.is_absolute() or '..' in path.parts:
+            raise SuspiciousFileOperation(
+                "Detected path traversal attempt in '%s'" % name
+            )
+    elif name != os.path.basename(name):
+        raise SuspiciousFileOperation("File name '%s' includes path elements" % name)
 
     return name
 
