4318e60a8025371a6da550eecc798e32d3864f9d
Nick Pope <nick@nickpope.me.uk>
2021-05-10 21:32:10 +0100

    [3.2.x] Fixed #32732 -- Removed usage of deprecated 'db' and 'passwd' connection options in MySQL backend.
    The 'db' and 'passwd' connection options have been deprecated, use
    'database' and 'password' instead (available since mysqlclient >= 1.3.8).
    
    This also allows the 'database' option in DATABASES['OPTIONS'] on MySQL.
    
    Backport of 1061f5243646b4c9b8a758f8a36c9e2ccdded1cf from main

--- django/db/backends/mysql/base.py
+++ django/db/backends/mysql/base.py
@@ -201,9 +201,9 @@ class DatabaseWrapper(BaseDatabaseWrapper):
         if settings_dict['USER']:
             kwargs['user'] = settings_dict['USER']
         if settings_dict['NAME']:
-            kwargs['db'] = settings_dict['NAME']
+            kwargs['database'] = settings_dict['NAME']
         if settings_dict['PASSWORD']:
-            kwargs['passwd'] = settings_dict['PASSWORD']
+            kwargs['password'] = settings_dict['PASSWORD']
         if settings_dict['HOST'].startswith('/'):
             kwargs['unix_socket'] = settings_dict['HOST']
         elif settings_dict['HOST']:
--- tests/dbshell/test_mysql.py
+++ tests/dbshell/test_mysql.py
@@ -50,41 +50,49 @@ class MySqlDbshellCommandTestCase(SimpleTestCase):
             'optiondbname',
         ]
         expected_env = {'MYSQL_PWD': 'optionpassword'}
-        self.assertEqual(
-            self.settings_to_cmd_args_env({
-                'NAME': 'settingdbname',
-                'USER': 'settinguser',
-                'PASSWORD': 'settingpassword',
-                'HOST': 'settinghost',
-                'PORT': settings_port,
-                'OPTIONS': {
-                    'db': 'optiondbname',
-                    'user': 'optionuser',
-                    'passwd': 'optionpassword',
-                    'host': 'optionhost',
-                    'port': options_port,
-                },
-            }),
-            (expected_args, expected_env),
-        )
+        for keys in [('database', 'password'), ('db', 'passwd')]:
+            with self.subTest(keys=keys):
+                database, password = keys
+                self.assertEqual(
+                    self.settings_to_cmd_args_env({
+                        'NAME': 'settingdbname',
+                        'USER': 'settinguser',
+                        'PASSWORD': 'settingpassword',
+                        'HOST': 'settinghost',
+                        'PORT': settings_port,
+                        'OPTIONS': {
+                            database: 'optiondbname',
+                            'user': 'optionuser',
+                            password: 'optionpassword',
+                            'host': 'optionhost',
+                            'port': options_port,
+                        },
+                    }),
+                    (expected_args, expected_env),
+                )
 
-    def test_options_password(self):
+    def test_options_non_deprecated_keys_preferred(self):
         expected_args = [
             'mysql',
             '--user=someuser',
             '--host=somehost',
             '--port=444',
-            'somedbname',
+            'optiondbname',
         ]
         expected_env = {'MYSQL_PWD': 'optionpassword'}
         self.assertEqual(
             self.settings_to_cmd_args_env({
-                'NAME': 'somedbname',
+                'NAME': 'settingdbname',
                 'USER': 'someuser',
                 'PASSWORD': 'settingpassword',
                 'HOST': 'somehost',
                 'PORT': 444,
-                'OPTIONS': {'password': 'optionpassword'},
+                'OPTIONS': {
+                    'database': 'optiondbname',
+                    'db': 'deprecatedoptiondbname',
+                    'password': 'optionpassword',
+                    'passwd': 'deprecatedoptionpassword',
+                },
             }),
             (expected_args, expected_env),
         )
