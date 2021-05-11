530f58caaa5052e9e56bf8461caee4d821953bcb
Rohith PR <praroh2@gmail.com>
2021-05-11 16:09:28 +0530

    Fixed #32734 -- Fixed validation of startapp's directory with trailing slash.
    Regression in fc9566d42daf28cdaa25a5db1b5ade253ceb064f.

--- tests/admin_scripts/tests.py
+++ tests/admin_scripts/tests.py
@@ -2206,6 +2206,13 @@ class StartApp(AdminScriptTestCase):
             "another directory."
         )
 
+    def test_trailing_slash_in_target_app_directory_name(self):
+        app_dir = os.path.join(self.test_dir, 'apps', 'app1')
+        os.makedirs(app_dir)
+        _, err = self.run_django_admin(['startapp', 'app', os.path.join('apps', 'app1', '')])
+        self.assertNoOutput(err)
+        self.assertIs(os.path.exists(os.path.join(app_dir, 'apps.py')), True)
+
     def test_overlaying_app(self):
         # Use a subdirectory so it is outside the PYTHONPATH.
         os.makedirs(os.path.join(self.test_dir, 'apps/app1'))
