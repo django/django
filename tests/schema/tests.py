5e04e84d67da8163f365e9f5fcd169e2630e2873
Yuekui Li <liyuekui@gmail.com>
2021-05-19 16:30:15 -0700

    Fixed #32503 -- Fixed altering BLOB/TEXT field to non-nullable with default on MySQL 8.0.13+.
    MySQL 8.0.13+ supports defaults for BLOB/TEXT but not in the
    ALTER COLUMN statement.
    
    Regression in 6b16c91157512587017e9178d066ed1a683e7795.
    
    Thanks Matt Westcott for the report.

--- tests/schema/tests.py
+++ tests/schema/tests.py
@@ -778,6 +778,15 @@ class SchemaTests(TransactionTestCase):
         with connection.schema_editor() as editor:
             editor.alter_field(Note, old_field, new_field, strict=True)
 
+    def test_alter_text_field_to_not_null_with_default_value(self):
+        with connection.schema_editor() as editor:
+            editor.create_model(Note)
+        old_field = Note._meta.get_field('address')
+        new_field = TextField(blank=True, default='', null=False)
+        new_field.set_attributes_from_name('address')
+        with connection.schema_editor() as editor:
+            editor.alter_field(Note, old_field, new_field, strict=True)
+
     @skipUnlessDBFeature('can_defer_constraint_checks', 'can_rollback_ddl')
     def test_alter_fk_checks_deferred_constraints(self):
         """
