django/django

commit 981a072dd4dec586f8fc606712ed9a2ef116eeee

Fixed #31843 -- Fixed pickling named values from QuerySet.values_list().

django/db/models/utils.py
@@ -1,3 +1,7 @@
+import functools
+from collections import namedtuple
+
+
 def make_model_tuple(model):
     """
     Take a model or a string of the form "app_label.ModelName" and return a
@@ -28,3 +32,17 @@ def resolve_callables(mapping):
     """
     for k, v in mapping.items():
         yield k, v() if callable(v) else v
+
+
+def unpickle_named_row(names, values):
+    return create_namedtuple_class(*names)(*values)
+
+
+@functools.lru_cache()
+def create_namedtuple_class(*names):
+    # Cache type() with @lru_cache() since it's too slow to be called for every
+    # QuerySet evaluation.
+    def __reduce__(self):
+        return unpickle_named_row, (names, tuple(self))
+
+    return type('Row', (namedtuple('Row', names),), {'__reduce__': __reduce__})

tests/queries/tests.py
@@ -2408,6 +2408,11 @@ class ValuesQuerysetTests(TestCase):
         values = qs.first()
         self.assertEqual(values._fields, ('combinedexpression2', 'combinedexpression1'))
 
+    def test_named_values_pickle(self):
+        value = Number.objects.values_list('num', 'other_num', named=True).get()
+        self.assertEqual(value, (72, None))
+        self.assertEqual(pickle.loads(pickle.dumps(value)), value)
+
 
 class QuerySetSupportsPythonIdioms(TestCase):
 

AUTHORS
@@ -934,6 +934,7 @@ answer newbie questions, and generally made Django that much better:
     Vincent Foley <vfoleybourgon@yahoo.ca>
     Vinny Do <vdo.code@gmail.com>
     Vitaly Babiy <vbabiy86@gmail.com>
+    Vitaliy Yelnik <velnik@gmail.com>
     Vladimir Kuzma <vladimirkuzma.ch@gmail.com>
     Vlado <vlado@labath.org>
     Vsevolod Solovyov

django/db/models/query.py
@@ -5,8 +5,6 @@ The main QuerySet implementation. This provides the public API for the ORM.
 import copy
 import operator
 import warnings
-from collections import namedtuple
-from functools import lru_cache
 from itertools import chain
 
 import django
@@ -23,7 +21,7 @@ from django.db.models.expressions import Case, Expression, F, Value, When
 from django.db.models.functions import Cast, Trunc
 from django.db.models.query_utils import FilteredRelation, Q
 from django.db.models.sql.constants import CURSOR, GET_ITERATOR_CHUNK_SIZE
-from django.db.models.utils import resolve_callables
+from django.db.models.utils import create_namedtuple_class, resolve_callables
 from django.utils import timezone
 from django.utils.functional import cached_property, partition
 
@@ -148,13 +146,6 @@ class NamedValuesListIterable(ValuesListIterable):
     namedtuple for each row.
     """
 
-    @staticmethod
-    @lru_cache()
-    def create_namedtuple_class(*names):
-        # Cache namedtuple() with @lru_cache() since it's too slow to be
-        # called for every QuerySet evaluation.
-        return namedtuple('Row', names)
-
     def __iter__(self):
         queryset = self.queryset
         if queryset._fields:
@@ -162,7 +153,7 @@ class NamedValuesListIterable(ValuesListIterable):
         else:
             query = queryset.query
             names = [*query.extra_select, *query.values_select, *query.annotation_select]
-        tuple_class = self.create_namedtuple_class(*names)
+        tuple_class = create_namedtuple_class(*names)
         new = tuple.__new__
         for row in super().__iter__():
             yield new(tuple_class, row)

commit 0393b9262dcf1b8302d35a8a470e14837ca1300b

Fixed #32812 -- Restored immutability of named values from QuerySet.values_list().
Regression in 981a072dd4dec586f8fc606712ed9a2ef116eeee.

Thanks pirelle for the report.

django/db/models/utils.py
@@ -45,4 +45,8 @@ def create_namedtuple_class(*names):
     def __reduce__(self):
         return unpickle_named_row, (names, tuple(self))
 
-    return type('Row', (namedtuple('Row', names),), {'__reduce__': __reduce__})
+    return type(
+        'Row',
+        (namedtuple('Row', names),),
+        {'__reduce__': __reduce__, '__slots__': ()},
+    )

tests/model_utils/tests.py
@@ -1,0 +1,10 @@
+from django.db.models.utils import create_namedtuple_class
+from django.test import SimpleTestCase
+
+
+class NamedTupleClassTests(SimpleTestCase):
+    def test_immutability(self):
+        row_class = create_namedtuple_class('field1', 'field2')
+        row = row_class('value1', 'value2')
+        with self.assertRaises(AttributeError):
+            row.field3 = 'value3'

tests/prefetch_related/tests.py
@@ -309,6 +309,13 @@ class PrefetchRelatedTests(TestDataMixin, TestCase):
                     list(Book.objects.prefetch_related(relation))
                     self.assertEqual(add_q_mock.call_count, 1)
 
+    def test_named_values_list(self):
+        qs = Author.objects.prefetch_related('books')
+        self.assertCountEqual(
+            [value.name for value in qs.values_list('name', named=True)],
+            ['Anne', 'Charlotte', 'Emily', 'Jane'],
+        )
+
 
 class RawQuerySetTests(TestDataMixin, TestCase):
     def test_basic(self):