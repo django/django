66ed03e7c9ae4cd754aa918c6f4c9227cf424141
saeedblanchette <saiidblanchettel@outlook.com>
2021-06-08 17:00:00 +0100

    Refs #24121 -- Added __repr__() to AdminForm, BlockContext, BlockTranslateNode, and IncludeNode.

--- django/templatetags/i18n.py
+++ django/templatetags/i18n.py
@@ -112,6 +112,13 @@ class BlockTranslateNode(Node):
         self.asvar = asvar
         self.tag_name = tag_name
 
+    def __repr__(self):
+        return (
+            f'<{self.__class__.__qualname__}: '
+            f'extra_context={self.extra_context!r} '
+            f'singular={self.singular!r} plural={self.plural!r}>'
+        )
+
     def render_token_list(self, tokens):
         result = []
         vars = []
