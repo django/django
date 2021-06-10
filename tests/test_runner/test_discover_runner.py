d5cc4573a8c31f086854e585c81336d3de54c440
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2021-06-10 07:39:14 +0200

    Fixed typo in tests/test_runner/test_discover_runner.py.

--- tests/test_runner/test_discover_runner.py
+++ tests/test_runner/test_discover_runner.py
@@ -349,7 +349,7 @@ class DiscoverRunnerTests(SimpleTestCase):
             DiscoverRunner(pdb=True, parallel=2)
 
     def test_buffer_mode_test_pass(self):
-        runner = DiscoverRunner(buffer=True, verbose=0)
+        runner = DiscoverRunner(buffer=True, verbosity=0)
         with captured_stdout() as stdout, captured_stderr() as stderr:
             suite = runner.build_suite([
                 'test_runner_apps.buffer.tests_buffer.WriteToStdoutStderrTestCase.test_pass',
@@ -359,7 +359,7 @@ class DiscoverRunnerTests(SimpleTestCase):
         self.assertNotIn('Write to stdout.', stdout.getvalue())
 
     def test_buffer_mode_test_fail(self):
-        runner = DiscoverRunner(buffer=True, verbose=0)
+        runner = DiscoverRunner(buffer=True, verbosity=0)
         with captured_stdout() as stdout, captured_stderr() as stderr:
             suite = runner.build_suite([
                 'test_runner_apps.buffer.tests_buffer.WriteToStdoutStderrTestCase.test_fail',
