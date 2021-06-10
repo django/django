django/django

commit d4ac23bee1c84d8e4610350202ac068fc90f38c0

Fixed #32425 -- Fixed adding nullable field with default on MySQL.
Thanks Simon Charette for the review.

django/db/backends/base/schema.py
@@ -62,6 +62,7 @@ class BaseDatabaseSchemaEditor:
     sql_alter_column_not_null = "ALTER COLUMN %(column)s SET NOT NULL"
     sql_alter_column_default = "ALTER COLUMN %(column)s SET DEFAULT %(default)s"
     sql_alter_column_no_default = "ALTER COLUMN %(column)s DROP DEFAULT"
+    sql_alter_column_no_default_null = sql_alter_column_no_default
     sql_alter_column_collate = "ALTER COLUMN %(column)s TYPE %(type)s%(collation)s"
     sql_delete_column = "ALTER TABLE %(table)s DROP COLUMN %(column)s CASCADE"
     sql_rename_column = "ALTER TABLE %(table)s RENAME COLUMN %(old_column)s TO %(new_column)s"
@@ -891,7 +892,13 @@ class BaseDatabaseSchemaEditor:
             params = []
 
         new_db_params = new_field.db_parameters(connection=self.connection)
-        sql = self.sql_alter_column_no_default if drop else self.sql_alter_column_default
+        if drop:
+            if new_field.null:
+                sql = self.sql_alter_column_no_default_null
+            else:
+                sql = self.sql_alter_column_no_default
+        else:
+            sql = self.sql_alter_column_default
         return (
             sql % {
                 'column': self.quote_name(new_field.column),

tests/schema/tests.py
@@ -3095,6 +3095,33 @@ class SchemaTests(TransactionTestCase):
             if connection.features.can_introspect_default:
                 self.assertIsNone(field.default)
 
+    def test_add_field_default_nullable(self):
+        with connection.schema_editor() as editor:
+            editor.create_model(Author)
+        # Add new nullable CharField with a default.
+        new_field = CharField(max_length=15, blank=True, null=True, default='surname')
+        new_field.set_attributes_from_name('surname')
+        with connection.schema_editor() as editor:
+            editor.add_field(Author, new_field)
+        Author.objects.create(name='Anonymous1')
+        with connection.cursor() as cursor:
+            cursor.execute('SELECT surname FROM schema_author;')
+            item = cursor.fetchall()[0]
+            self.assertIsNone(item[0])
+            field = next(
+                f
+                for f in connection.introspection.get_table_description(
+                    cursor,
+                    'schema_author',
+                )
+                if f.name == 'surname'
+            )
+            # Field is still nullable.
+            self.assertTrue(field.null_ok)
+            # The database default is no longer set.
+            if connection.features.can_introspect_default:
+                self.assertIn(field.default, ['NULL', None])
+
     def test_alter_field_default_dropped(self):
         # Create the table
         with connection.schema_editor() as editor:

django/db/backends/mysql/schema.py
@@ -10,6 +10,7 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
     sql_alter_column_not_null = "MODIFY %(column)s %(type)s NOT NULL"
     sql_alter_column_type = "MODIFY %(column)s %(type)s"
     sql_alter_column_collate = "MODIFY %(column)s %(type)s%(collation)s"
+    sql_alter_column_no_default_null = 'ALTER COLUMN %(column)s SET DEFAULT NULL'
 
     # No 'CASCADE' which works as a no-op in MySQL but is undocumented
     sql_delete_column = "ALTER TABLE %(table)s DROP COLUMN %(column)s"

django/db/backends/oracle/schema.py
@@ -14,6 +14,7 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
     sql_alter_column_not_null = "MODIFY %(column)s NOT NULL"
     sql_alter_column_default = "MODIFY %(column)s DEFAULT %(default)s"
     sql_alter_column_no_default = "MODIFY %(column)s DEFAULT NULL"
+    sql_alter_column_no_default_null = sql_alter_column_no_default
     sql_alter_column_collate = "MODIFY %(column)s %(type)s%(collation)s"
 
     sql_delete_column = "ALTER TABLE %(table)s DROP COLUMN %(column)s"

commit 0e1ca02d71fa94edd73d1155535cdee74459d50e

Fixed #32832 -- Fixed adding BLOB/TEXT nullable field with default on MySQL 8.0.13+.
Regression in d4ac23bee1c84d8e4610350202ac068fc90f38c0.

Thanks Omkar Deshpande for the report.

django/db/backends/base/schema.py
@@ -225,7 +225,14 @@ class BaseDatabaseSchemaEditor:
         # Work out nullability
         null = field.null
         # If we were told to include a default value, do so
-        include_default = include_default and not self.skip_default(field)
+        include_default = (
+            include_default and
+            not self.skip_default(field) and
+            # Don't include a default value if it's a nullable field and the
+            # default cannot be dropped in the ALTER COLUMN statement (e.g.
+            # MySQL longtext and longblob).
+            not (null and self.skip_default_on_alter(field))
+        )
         if include_default:
             default_value = self.effective_default(field)
             column_default = ' DEFAULT ' + self._column_default_sql(field)
@@ -515,7 +522,7 @@ class BaseDatabaseSchemaEditor:
         self.execute(sql, params)
         # Drop the default if we need to
         # (Django usually does not use in-database defaults)
-        if not self.skip_default(field) and self.effective_default(field) is not None:
+        if not self.skip_default_on_alter(field) and self.effective_default(field) is not None:
             changes_sql, params = self._alter_column_default_sql(model, None, field, drop=True)
             sql = self.sql_alter_column % {
                 "table": self.quote_name(model._meta.db_table),

tests/schema/tests.py
@@ -3387,6 +3387,33 @@ class SchemaTests(TransactionTestCase):
             if connection.features.can_introspect_default:
                 self.assertIn(field.default, ['NULL', None])
 
+    def test_add_textfield_default_nullable(self):
+        with connection.schema_editor() as editor:
+            editor.create_model(Author)
+        # Add new nullable TextField with a default.
+        new_field = TextField(blank=True, null=True, default='text')
+        new_field.set_attributes_from_name('description')
+        with connection.schema_editor() as editor:
+            editor.add_field(Author, new_field)
+        Author.objects.create(name='Anonymous1')
+        with connection.cursor() as cursor:
+            cursor.execute('SELECT description FROM schema_author;')
+            item = cursor.fetchall()[0]
+            self.assertIsNone(item[0])
+            field = next(
+                f
+                for f in connection.introspection.get_table_description(
+                    cursor,
+                    'schema_author',
+                )
+                if f.name == 'description'
+            )
+            # Field is still nullable.
+            self.assertTrue(field.null_ok)
+            # The database default is no longer set.
+            if connection.features.can_introspect_default:
+                self.assertIn(field.default, ['NULL', None])
+
     def test_alter_field_default_dropped(self):
         # Create the table
         with connection.schema_editor() as editor: