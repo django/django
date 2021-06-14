9b34936c7b4ef97201d39e3690cea26d5e5aba4f
dbarnes18 <83948303+dbarnes18@users.noreply.github.com>
2021-06-14 17:54:43 +0100

    Fix typo (Annoys me every time I run the command)

--- django/contrib/auth/management/commands/createsuperuser.py
+++ django/contrib/auth/management/commands/createsuperuser.py
@@ -157,7 +157,7 @@ class Command(BaseCommand):
                         validate_password(password2, self.UserModel(**fake_user_data))
                     except exceptions.ValidationError as err:
                         self.stderr.write('\n'.join(err.messages))
-                        response = input('Bypass password validation and create user anyway? [y/N]: ')
+                        response = input('Bypass password validation and create user anyway? [y/n]: ')
                         if response.lower() != 'y':
                             continue
                     user_data[PASSWORD_FIELD] = password
