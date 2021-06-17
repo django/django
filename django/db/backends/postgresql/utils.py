022a2fcc1bde621ce209c42feeea1b5b95e56f03
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2021-06-17 02:35:40 +0100

    Fix compatibility with psycopg 2.9

--- django/db/backends/postgresql/utils.py
+++ django/db/backends/postgresql/utils.py
@@ -2,6 +2,7 @@ from django.utils.timezone import utc
 
 
 def utc_tzinfo_factory(offset):
-    if offset != 0:
+    # Offset is an int in psycopg2 < 2.9, then switched to timedelta.
+    if offset:
         raise AssertionError("database connection isn't set to UTC")
     return utc
