e07609a0d1cac573890380ee32b85d7743632650
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2021-06-22 06:10:54 +0200

    Refs #32858, Refs #32392 -- Restored using :: shortcut syntax in Cast() on PostgreSQL.
    This partly reverts commit fdfbc66331292def201c9344e3cd29fbcbcd076a
    unnecessary since b69b0c3fe871167a0ca01bb439508e335143801f.

--- django/db/models/functions/comparison.py
+++ django/db/models/functions/comparison.py
@@ -42,6 +42,12 @@ class Cast(Func):
             template = "JSON_EXTRACT(%(expressions)s, '$')"
         return self.as_sql(compiler, connection, template=template, **extra_context)
 
+    def as_postgresql(self, compiler, connection, **extra_context):
+        # CAST would be valid too, but the :: shortcut syntax is more readable.
+        # 'expressions' is wrapped in parentheses in case it's a complex
+        # expression.
+        return self.as_sql(compiler, connection, template='(%(expressions)s)::%(db_type)s', **extra_context)
+
     def as_oracle(self, compiler, connection, **extra_context):
         if self.output_field.get_internal_type() == 'JSONField':
             # Oracle doesn't support explicit cast to JSON.
