f5ea9aa2f32c7ca9a1fdf793580fddba3daae1b9
Dan Strokirk <dan.strokirk@gmail.com>
2021-06-24 17:15:25 +0200

    Fixed #32807 -- Fixed JSONField crash when redisplaying None values.
    Thanks to Alex Hill for the initial patch.

--- django/forms/fields.py
+++ django/forms/fields.py
@@ -1251,6 +1251,8 @@ class JSONField(CharField):
     def bound_data(self, data, initial):
         if self.disabled:
             return initial
+        if data is None:
+            return None
         try:
             return json.loads(data, cls=self.decoder)
         except json.JSONDecodeError:
