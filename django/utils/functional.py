6e5cefa950fbecb4675cf2c401c8e4fa2822c452
simon klemenc <hiaselhans@users.noreply.github.com>
2021-07-01 09:08:55 +0200

    provide a dedicated LazyObject.__class__ method
    fixes https://code.djangoproject.com/ticket/32894#ticket

--- django/utils/functional.py
+++ django/utils/functional.py
@@ -337,7 +337,13 @@ class LazyObject:
 
     # Need to pretend to be the wrapped class, for the sake of objects that
     # care about this (especially in equality tests)
-    __class__ = property(new_method_proxy(operator.attrgetter("__class__")))
+    
+    def __class__(self):
+        if self._wrapped is empty:
+            return type(self)
+        
+        return type(self._wrapped)
+    
     __eq__ = new_method_proxy(operator.eq)
     __lt__ = new_method_proxy(operator.lt)
     __gt__ = new_method_proxy(operator.gt)
