448d974d9646b96806ef07a9ceeceb96e4bac645
Nick Pope <nick@nickpope.me.uk>
2021-07-08 00:19:35 +0100

    Fixed #32499 -- Escaped additional metacharacters in simplify_regex().

--- django/contrib/admindocs/utils.py
+++ django/contrib/admindocs/utils.py
@@ -145,7 +145,7 @@ unnamed_group_matcher = _lazy_re_compile(r'\(')
 def replace_metacharacters(pattern):
     """Remove unescaped metacharacters from the pattern."""
     return re.sub(
-        r'((?:^|(?<!\\))(?:\\\\)*)(\\?)([?^$])',
+        r'((?:^|(?<!\\))(?:\\\\)*)(\\?)([?*+^$]|\\[bBAZ])',
         lambda m: m[1] + m[3] if m[2] else m[1],
         pattern,
     )
