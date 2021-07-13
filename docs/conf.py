37e8367c359cd115f109d82f99ff32be219f4928
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2021-07-13 20:21:22 +0200

    Refs #31676 -- Removed Django Core-Mentorship mailing list references in docs.

--- docs/conf.py
+++ docs/conf.py
@@ -265,7 +265,6 @@ modindex_common_prefix = ["django."]
 # Appended to every page
 rst_epilog = """
 .. |django-users| replace:: :ref:`django-users <django-users-mailing-list>`
-.. |django-core-mentorship| replace:: :ref:`django-core-mentorship <django-core-mentorship-mailing-list>`
 .. |django-developers| replace:: :ref:`django-developers <django-developers-mailing-list>`
 .. |django-announce| replace:: :ref:`django-announce <django-announce-mailing-list>`
 .. |django-updates| replace:: :ref:`django-updates <django-updates-mailing-list>`
