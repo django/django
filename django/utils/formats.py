59942a66ceb79868cb91844df3a72a24c63e39fa
Keryn Knight <keryn@kerynknight.com>
2021-07-18 14:06:30 +0100

    Fixed #32941 -- Removed get_format_modules()'s unused reverse argument.
    Unused since 0d8b523422fda71baa10807d5aebefd34bad7962.

--- django/utils/formats.py
+++ django/utils/formats.py
@@ -86,16 +86,13 @@ def iter_format_modules(lang, format_module_path=None):
                 pass
 
 
-def get_format_modules(lang=None, reverse=False):
+def get_format_modules(lang=None):
     """Return a list of the format modules found."""
     if lang is None:
         lang = get_language()
     if lang not in _format_modules_cache:
         _format_modules_cache[lang] = list(iter_format_modules(lang, settings.FORMAT_MODULE_PATH))
-    modules = _format_modules_cache[lang]
-    if reverse:
-        return list(reversed(modules))
-    return modules
+    return _format_modules_cache[lang]
 
 
 def get_format(format_type, lang=None, use_l10n=None):
