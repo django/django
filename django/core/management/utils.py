e920585d8ceb1c2a4c9aa5d1c7c60daa1aa1508e
Ali <40032682+AliRn76@users.noreply.github.com>
2021-07-20 13:01:00 +0430

    Add some character to secret_key chars

--- django/core/management/utils.py
+++ django/core/management/utils.py
@@ -78,7 +78,7 @@ def get_random_secret_key():
     """
     Return a 50 character random string usable as a SECRET_KEY setting value.
     """
-    chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+    chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+){[<?>]}'
     return get_random_string(50, chars)
 
 
