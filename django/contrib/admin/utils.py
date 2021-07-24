e4db8c34a2af4c226eee8bfc1271e0f54c9b29fd
Maciej Olko <maciej.olko@gmail.com>
2021-07-24 02:05:45 +0200

    Fixed #32797 -- Removed unused model_ngettext() util function.

--- django/contrib/admin/utils.py
+++ django/contrib/admin/utils.py
@@ -242,24 +242,6 @@ def model_format_dict(obj):
     }
 
 
-def model_ngettext(obj, n=None):
-    """
-    Return the appropriate `verbose_name` or `verbose_name_plural` value for
-    `obj` depending on the count `n`.
-
-    `obj` may be a `Model` instance, `Model` subclass, or `QuerySet` instance.
-    If `obj` is a `QuerySet` instance, `n` is optional and the length of the
-    `QuerySet` is used.
-    """
-    if isinstance(obj, models.query.QuerySet):
-        if n is None:
-            n = obj.count()
-        obj = obj.model
-    d = model_format_dict(obj)
-    singular, plural = d["verbose_name"], d["verbose_name_plural"]
-    return ngettext(singular, plural, n or 0)
-
-
 def lookup_field(name, obj, model_admin=None):
     opts = obj._meta
     try:
