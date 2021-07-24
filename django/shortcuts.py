75749453040f1e90cf2fe72c80476dcb77a74929
pradipbankar0097 <62244159+pradipbankar0097@users.noreply.github.com>
2021-07-24 22:42:47 +0530

    Grammatical error a/an

--- django/shortcuts.py
+++ django/shortcuts.py
@@ -13,7 +13,7 @@ from django.utils.functional import Promise
 
 def render(request, template_name, context=None, content_type=None, status=None, using=None):
     """
-    Return a HttpResponse whose content is filled with the result of calling
+    Return an HttpResponse whose content is filled with the result of calling
     django.template.loader.render_to_string() with the passed arguments.
     """
     content = loader.render_to_string(template_name, context, request, using=using)
