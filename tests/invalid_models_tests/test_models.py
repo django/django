django/django

commit c32d8f33d8e988a376e44997b8f3606d821f305e

Fixed #31926 -- Fixed recreating queryset with FilteredRelation when using a pickled Query.
In a pickled join, the join_fields had the same values, but weren't the
same object (contrary to when not pickling the QuerySet).

django/db/models/fields/reverse_related.py
@@ -11,6 +11,7 @@ they're the closest concept currently available.
 
 from django.core import exceptions
 from django.utils.functional import cached_property
+from django.utils.hashable import make_hashable
 
 from . import BLANK_CHOICE_DASH
 from .mixins import FieldCacheMixin
@@ -115,6 +116,28 @@ class ForeignObjectRel(FieldCacheMixin):
             self.related_model._meta.model_name,
         )
 
+    @property
+    def identity(self):
+        return (
+            self.field,
+            self.model,
+            self.related_name,
+            self.related_query_name,
+            tuple(sorted(make_hashable(self.limit_choices_to))),
+            self.parent_link,
+            self.on_delete,
+            self.symmetrical,
+            self.multiple,
+        )
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return NotImplemented
+        return self.identity == other.identity
+
+    def __hash__(self):
+        return hash(self.identity)
+
     def get_choices(
         self, include_blank=True, blank_choice=BLANK_CHOICE_DASH,
         limit_choices_to=None, ordering=(),
@@ -215,6 +238,10 @@ class ManyToOneRel(ForeignObjectRel):
         state.pop('related_model', None)
         return state
 
+    @property
+    def identity(self):
+        return super().identity + (self.field_name,)
+
     def get_related_field(self):
         """
         Return the Field in the 'to' object to which this relationship is tied.
@@ -278,6 +305,14 @@ class ManyToManyRel(ForeignObjectRel):
 
         self.symmetrical = symmetrical
         self.db_constraint = db_constraint
+
+    @property
+    def identity(self):
+        return super().identity + (
+            self.through,
+            self.through_fields,
+            self.db_constraint,
+        )
 
     def get_related_field(self):
         """

tests/queryset_pickle/tests.py
@@ -219,6 +219,40 @@ class PickleabilityTestCase(TestCase):
         with self.assertNumQueries(0):
             self.assert_pickles(groups)
 
+    def test_pickle_filteredrelation(self):
+        group = Group.objects.create(name='group')
+        event_1 = Event.objects.create(title='Big event', group=group)
+        event_2 = Event.objects.create(title='Small event', group=group)
+        Happening.objects.bulk_create([
+            Happening(event=event_1, number1=5),
+            Happening(event=event_2, number1=3),
+        ])
+        groups = Group.objects.annotate(
+            big_events=models.FilteredRelation(
+                'event',
+                condition=models.Q(event__title__startswith='Big'),
+            ),
+        ).annotate(sum_number=models.Sum('big_events__happening__number1'))
+        groups_query = pickle.loads(pickle.dumps(groups.query))
+        groups = Group.objects.all()
+        groups.query = groups_query
+        self.assertEqual(groups.get().sum_number, 5)
+
+    def test_pickle_filteredrelation_m2m(self):
+        group = Group.objects.create(name='group')
+        m2mmodel = M2MModel.objects.create()
+        m2mmodel.groups.add(group)
+        groups = Group.objects.annotate(
+            first_m2mmodels=models.FilteredRelation(
+                'm2mmodel',
+                condition=models.Q(m2mmodel__pk__lt=10),
+            ),
+        ).annotate(count_groups=models.Count('first_m2mmodels__groups'))
+        groups_query = pickle.loads(pickle.dumps(groups.query))
+        groups = Group.objects.all()
+        groups.query = groups_query
+        self.assertEqual(groups.get().count_groups, 1)
+
     def test_annotation_with_callable_default(self):
         # Happening.when has a callable default of datetime.datetime.now.
         qs = Happening.objects.annotate(latest_time=models.Max('when'))

commit 20226fcd461670334646f78a0c4d133e439b12b2

Fixed #32947 -- Fixed hash() crash on reverse M2M relation when through_fields is a list.
Regression in c32d8f33d8e988a376e44997b8f3606d821f305e.

AUTHORS
@@ -923,6 +923,7 @@ answer newbie questions, and generally made Django that much better:
     Tom Forbes <tom@tomforb.es>
     Tom Insam
     Tom Tobin
+    Tom Wojcik <me@tomwojcik.com>
     Tomáš Ehrlich <tomas.ehrlich@gmail.com>
     Tomáš Kopeček <permonik@m6.cz>
     Tome Cvitan <tome@cvitan.com>

django/db/models/fields/reverse_related.py
@@ -310,7 +310,7 @@ class ManyToManyRel(ForeignObjectRel):
     def identity(self):
         return super().identity + (
             self.through,
-            self.through_fields,
+            make_hashable(self.through_fields),
             self.db_constraint,
         )
 

tests/invalid_models_tests/test_models.py
@@ -821,6 +821,33 @@ class ShadowingFieldsTests(SimpleTestCase):
             )
         ])
 
+    def test_field_name_clash_with_m2m_through(self):
+        class Parent(models.Model):
+            clash_id = models.IntegerField()
+
+        class Child(Parent):
+            clash = models.ForeignKey('Child', models.CASCADE)
+
+        class Model(models.Model):
+            parents = models.ManyToManyField(
+                to=Parent,
+                through='Through',
+                through_fields=['parent', 'model'],
+            )
+
+        class Through(models.Model):
+            parent = models.ForeignKey(Parent, models.CASCADE)
+            model = models.ForeignKey(Model, models.CASCADE)
+
+        self.assertEqual(Child.check(), [
+            Error(
+                "The field 'clash' clashes with the field 'clash_id' from "
+                "model 'invalid_models_tests.parent'.",
+                obj=Child._meta.get_field('clash'),
+                id='models.E006',
+            )
+        ])
+
     def test_multiinheritance_clash(self):
         class Mother(models.Model):
             clash = models.IntegerField()

tests/m2m_through/models.py
@@ -11,6 +11,10 @@ class Person(models.Model):
         ordering = ('name',)
 
 
+class PersonChild(Person):
+    pass
+
+
 class Group(models.Model):
     name = models.CharField(max_length=128)
     members = models.ManyToManyField(Person, through='Membership')
@@ -85,8 +89,9 @@ class SymmetricalFriendship(models.Model):
 class Event(models.Model):
     title = models.CharField(max_length=50)
     invitees = models.ManyToManyField(
-        Person, through='Invitation',
-        through_fields=('event', 'invitee'),
+        to=Person,
+        through='Invitation',
+        through_fields=['event', 'invitee'],
         related_name='events_invited',
     )
 

tests/m2m_through/tests.py
@@ -6,8 +6,8 @@ from django.test import TestCase
 
 from .models import (
     CustomMembership, Employee, Event, Friendship, Group, Ingredient,
-    Invitation, Membership, Person, PersonSelfRefM2M, Recipe, RecipeIngredient,
-    Relationship, SymmetricalFriendship,
+    Invitation, Membership, Person, PersonChild, PersonSelfRefM2M, Recipe,
+    RecipeIngredient, Relationship, SymmetricalFriendship,
 )
 
 
@@ -19,6 +19,13 @@ class M2mThroughTests(TestCase):
         cls.jane = Person.objects.create(name='Jane')
         cls.rock = Group.objects.create(name='Rock')
         cls.roll = Group.objects.create(name='Roll')
+
+    def test_reverse_inherited_m2m_with_through_fields_list_hashable(self):
+        reverse_m2m = Person._meta.get_field('events_invited')
+        self.assertEqual(reverse_m2m.through_fields, ['event', 'invitee'])
+        inherited_reverse_m2m = PersonChild._meta.get_field('events_invited')
+        self.assertEqual(inherited_reverse_m2m.through_fields, ['event', 'invitee'])
+        self.assertEqual(hash(reverse_m2m), hash(inherited_reverse_m2m))
 
     def test_retrieve_intermediate_items(self):
         Membership.objects.create(person=self.jim, group=self.rock)