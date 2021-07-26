a5ddd9b9fd903d0831f088ba4172594789adadc8
Egor <Birdi7@users.noreply.github.com>
2021-07-26 08:58:22 +0500

    chore: remove unused arguments
    These arguments are not used anyhow in Q.resolve_expression, so why to keep it there?

--- django/db/models/query_utils.py
+++ django/db/models/query_utils.py
@@ -68,7 +68,7 @@ class Q(tree.Node):
         obj.negate()
         return obj
 
-    def resolve_expression(self, query=None, allow_joins=True, reuse=None, summarize=False, for_save=False):
+    def resolve_expression(self, query=None, allow_joins=True, reuse=None):
         # We must promote any new joins to left outer joins so that when Q is
         # used as an expression, rows aren't filtered due to joins.
         clause, joins = query._add_q(
