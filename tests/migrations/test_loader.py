52f9cfee9fbe9e19faf8a44e1063da8a3ec07ea7
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2021-08-04 09:15:23 +0200

    Used assertRaisesMessage() to test MigrationLoader.get_migration_by_prefix()'s error messages.

--- tests/migrations/test_loader.py
+++ tests/migrations/test_loader.py
@@ -179,9 +179,11 @@ class LoaderTests(TestCase):
             migration_loader.get_migration_by_prefix("migrations", "0001").name,
             "0001_initial",
         )
-        with self.assertRaises(AmbiguityError):
+        msg = "There is more than one migration for 'migrations' with the prefix '0'"
+        with self.assertRaisesMessage(AmbiguityError, msg):
             migration_loader.get_migration_by_prefix("migrations", "0")
-        with self.assertRaises(KeyError):
+        msg = "There no migrations for 'migrations' with the prefix 'blarg'"
+        with self.assertRaisesMessage(KeyError, msg):
             migration_loader.get_migration_by_prefix("migrations", "blarg")
 
     def test_load_import_error(self):
