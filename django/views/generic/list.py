4820d3f9924a7b6c76fd0b987e2cc1ef93e6462b
dzhuang <dzhuang.scut@gmail.com>
2021-08-09 14:37:48 +0800

    Case should be allowed to be used in order_by
    See https://stackoverflow.com/a/37648265/3437454

--- django/views/generic/list.py
+++ django/views/generic/list.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import ImproperlyConfigured
 from django.core.paginator import InvalidPage, Paginator
-from django.db.models import QuerySet
+from django.db.models import Case, QuerySet
 from django.http import Http404
 from django.utils.translation import gettext as _
 from django.views.generic.base import ContextMixin, TemplateResponseMixin, View
@@ -41,7 +41,7 @@ class MultipleObjectMixin(ContextMixin):
             )
         ordering = self.get_ordering()
         if ordering:
-            if isinstance(ordering, str):
+            if isinstance(ordering, (str, Case)):
                 ordering = (ordering,)
             queryset = queryset.order_by(*ordering)
 
