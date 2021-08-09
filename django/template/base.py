6fedd868e1d50421204ea0c05cc8d2200dcb3cb0
Chris Jerdonek <chris.jerdonek@gmail.com>
2021-08-08 23:49:09 -0400

    Refs #33002 -- Renamed variable from bit to token_string in Lexer.tokenize().

--- django/template/base.py
+++ django/template/base.py
@@ -354,10 +354,10 @@ class Lexer:
         in_tag = False
         lineno = 1
         result = []
-        for bit in tag_re.split(self.template_string):
-            if bit:
-                result.append(self.create_token(bit, None, lineno, in_tag))
-                lineno += bit.count('\n')
+        for token_string in tag_re.split(self.template_string):
+            if token_string:
+                result.append(self.create_token(token_string, None, lineno, in_tag))
+                lineno += token_string.count('\n')
             in_tag = not in_tag
         return result
 
