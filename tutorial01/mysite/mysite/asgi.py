69b397ac6f7ae6ff9099798aac41ab785f408cef
Steve Walters <stevewalters@qcsoftware.com>
2021-08-17 14:55:01 -0400

    Installed

---
+++ tutorial01/mysite/mysite/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mysite project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/dev/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+application = get_asgi_application()
