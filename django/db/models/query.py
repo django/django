12de34998432fbf622d9f745f21fd92db04a49c0
pwtail <abvit89s@gmail.com>
2021-08-24 02:05:33 +0300

    no comments

--- django/db/models/query.py
+++ django/db/models/query.py
@@ -113,54 +113,6 @@ class ModelIterable(BaseIterable):
             rows = yield objects
 
 
-    #
-    # def __iter__(self):
-    #     queryset = self.queryset
-    #     db = queryset.db
-    #     compiler = queryset.query.get_compiler(using=db)
-    #     # Execute the query. This will also fill compiler.select, klass_info,
-    #     # and annotations.
-    #     results = compiler.execute_sql(chunked_fetch=self.chunked_fetch, chunk_size=self.chunk_size)
-    #     select, klass_info, annotation_col_map = (compiler.select, compiler.klass_info,
-    #                                               compiler.annotation_col_map)
-    #     model_cls = klass_info['model']
-    #     select_fields = klass_info['select_fields']
-    #     model_fields_start, model_fields_end = select_fields[0], select_fields[-1] + 1
-    #     init_list = [f[0].target.attname
-    #                  for f in select[model_fields_start:model_fields_end]]
-    #     related_populators = get_related_populators(klass_info, select, db)
-    #     known_related_objects = [
-    #         (field, related_objs, operator.attrgetter(*[
-    #             field.attname
-    #             if from_field == 'self' else
-    #             queryset.model._meta.get_field(from_field).attname
-    #             for from_field in field.from_fields
-    #         ])) for field, related_objs in queryset._known_related_objects.items()
-    #     ]
-    #     for row in compiler.results_iter(results):
-    #         obj = model_cls.from_db(db, init_list, row[model_fields_start:model_fields_end])
-    #         for rel_populator in related_populators:
-    #             rel_populator.populate(row, obj)
-    #         if annotation_col_map:
-    #             for attr_name, col_pos in annotation_col_map.items():
-    #                 setattr(obj, attr_name, row[col_pos])
-    #
-    #         # Add the known related objects to the model.
-    #         for field, rel_objs, rel_getter in known_related_objects:
-    #             # Avoid overwriting objects loaded by, e.g., select_related().
-    #             if field.is_cached(obj):
-    #                 continue
-    #             rel_obj_id = rel_getter(obj)
-    #             try:
-    #                 rel_obj = rel_objs[rel_obj_id]
-    #             except KeyError:
-    #                 pass  # May happen in qs1 | qs2 scenarios.
-    #             else:
-    #                 setattr(obj, field.name, rel_obj)
-    #
-    #         yield obj
-
-
 class ValuesIterable(BaseIterable):
     """
     Iterable returned by QuerySet.values() that yields a dict for each row.
