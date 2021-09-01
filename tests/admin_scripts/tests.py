3686077d463685d383e14b4695eaaa2658919a42
sage <laymonage@gmail.com>
2021-09-01 11:03:03 +0200

    Refs #32309 -- Added test for excluding hidden directories in startproject command.

--- tests/admin_scripts/tests.py
+++ tests/admin_scripts/tests.py
@@ -2214,6 +2214,24 @@ class StartProject(LiveServerTestCase, AdminScriptTestCase):
                 'Some non-ASCII text for testing ticket #18091:',
                 'üäö €'])
 
+    def test_custom_project_template_hidden_directory_default_excluded(self):
+        """Hidden directories are excluded by default."""
+        template_path = os.path.join(custom_templates_dir, 'project_template')
+        args = [
+            'startproject',
+            '--template',
+            template_path,
+            'custom_project_template_hidden_directories',
+            'project_dir',
+        ]
+        testproject_dir = os.path.join(self.test_dir, 'project_dir')
+        os.mkdir(testproject_dir)
+
+        _, err = self.run_django_admin(args)
+        self.assertNoOutput(err)
+        hidden_dir = os.path.join(testproject_dir, '.hidden')
+        self.assertIs(os.path.exists(hidden_dir), False)
+
 
 class StartApp(AdminScriptTestCase):
 
