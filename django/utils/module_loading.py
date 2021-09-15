django/django

commit ecf87ad513fd8af6e4a6093ed918723a7d88d5ca

Fixed #33099 -- Improved performance of import_string().
This improves performance of import_string() by avoiding multiple
imports for the same path.

Thanks Andrew Godwin and Keryn Knight for the implementation idea.

django/utils/module_loading.py
@@ -1,7 +1,15 @@
 import copy
 import os
+import sys
 from importlib import import_module
 from importlib.util import find_spec as importlib_find
+
+
+def cached_import(module_path, class_name):
+    modules = sys.modules
+    if module_path not in modules:
+        import_module(module_path)
+    return getattr(modules[module_path], class_name)
 
 
 def import_string(dotted_path):
@@ -14,10 +22,8 @@ def import_string(dotted_path):
     except ValueError as err:
         raise ImportError("%s doesn't look like a module path" % dotted_path) from err
 
-    module = import_module(module_path)
-
     try:
-        return getattr(module, class_name)
+        return cached_import(module_path, class_name)
     except AttributeError as err:
         raise ImportError('Module "%s" does not define a "%s" attribute/class' % (
             module_path, class_name)

commit 143757476936beed4dd71d1e029c746d8f26bc70

Fixed #33107 -- Fixed import_string() crash on not fully initialized modules.
Regression in ecf87ad513fd8af6e4a6093ed918723a7d88d5ca.

Thanks Collin Anderson for the report.

django/utils/module_loading.py
@@ -7,7 +7,11 @@ from importlib.util import find_spec as importlib_find
 
 def cached_import(module_path, class_name):
     modules = sys.modules
-    if module_path not in modules:
+    if module_path not in modules or (
+        # Module is not fully initialized.
+        getattr(modules[module_path], '__spec__', None) is not None and
+        getattr(modules[module_path].__spec__, '_initializing', False) is True
+    ):
         import_module(module_path)
     return getattr(modules[module_path], class_name)
 