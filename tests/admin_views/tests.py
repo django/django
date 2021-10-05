aa9da1081f61f54507320d6642c7cb08d692fd70
Yash Jhunjhunwala <yash@jhunjhunwalaadvisors.com>
2021-10-05 17:03:14 +0530

    Refs #33029 -- Fixed window switching in Selenium test.
    Index based switching was not deterministic, particularly in headless mode.

--- tests/admin_views/tests.py
+++ tests/admin_views/tests.py
@@ -4992,32 +4992,35 @@ class SeleniumTests(AdminSeleniumTestCase):
         add_url = reverse('admin:admin_views_box_add', current_app=site.name)
         self.selenium.get(self.live_server_url + add_url)
 
+        base_window = self.selenium.current_window_handle
         self.selenium.find_element_by_id('add_id_next_box').click()
         self.wait_for_and_switch_to_popup()
 
+        popup_window_test = self.selenium.current_window_handle
         self.selenium.find_element_by_id('id_title').send_keys('test')
         self.selenium.find_element_by_id('add_id_next_box').click()
         self.wait_for_and_switch_to_popup(num_windows=3)
 
+        popup_window_test2 = self.selenium.current_window_handle
         self.selenium.find_element_by_id('id_title').send_keys('test2')
         self.selenium.find_element_by_id('add_id_next_box').click()
         self.wait_for_and_switch_to_popup(num_windows=4)
 
         self.selenium.find_element_by_id('id_title').send_keys('test3')
         self.selenium.find_element_by_xpath('//input[@value="Save"]').click()
-        self.selenium.switch_to.window(self.selenium.window_handles[-1])
+        self.selenium.switch_to.window(popup_window_test2)
         select = Select(self.selenium.find_element_by_id('id_next_box'))
         next_box_id = str(Box.objects.get(title="test3").id)
         self.assertEqual(select.first_selected_option.get_attribute('value'), next_box_id)
 
         self.selenium.find_element_by_xpath('//input[@value="Save"]').click()
-        self.selenium.switch_to.window(self.selenium.window_handles[-1])
+        self.selenium.switch_to.window(popup_window_test)
         select = Select(self.selenium.find_element_by_id('id_next_box'))
         next_box_id = str(Box.objects.get(title="test2").id)
         self.assertEqual(select.first_selected_option.get_attribute('value'), next_box_id)
 
         self.selenium.find_element_by_xpath('//input[@value="Save"]').click()
-        self.selenium.switch_to.window(self.selenium.window_handles[-1])
+        self.selenium.switch_to.window(base_window)
         select = Select(self.selenium.find_element_by_id('id_next_box'))
         next_box_id = str(Box.objects.get(title="test").id)
         self.assertEqual(select.first_selected_option.get_attribute('value'), next_box_id)
