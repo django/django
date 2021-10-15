f5802a21c401b92764a9f3e2886144f3c5d77573
Hannes Ljungberg <hannes.ljungberg@gmail.com>
2021-10-15 22:17:26 +0200

    [3.2.x] Fixed #33194 -- Fixed migrations when altering a field with functional indexes on SQLite.
    This adjusts Expressions.rename_table_references() to only update alias
    when needed.
    
    Regression in 83fcfc9ec8610540948815e127101f1206562ead.
    
    Co-authored-by: Simon Charette <charettes@users.noreply.github.com>
    
    Backport of 86971c40909430a798e4e55b140004c4b1fb02ff from main.

--- tests/migrations/test_operations.py
+++ tests/migrations/test_operations.py
@@ -2010,6 +2010,25 @@ class OperationTests(OperationTestBase):
         self.assertEqual(definition[1], [])
         self.assertEqual(definition[2], {'model_name': 'Pony', 'name': index_name})
 
+    @skipUnlessDBFeature('supports_expression_indexes')
+    def test_alter_field_with_func_index(self):
+        app_label = 'test_alfuncin'
+        index_name = f'{app_label}_pony_idx'
+        table_name = f'{app_label}_pony'
+        project_state = self.set_up_test_model(
+            app_label,
+            indexes=[models.Index(Abs('pink'), name=index_name)],
+        )
+        operation = migrations.AlterField('Pony', 'pink', models.IntegerField(null=True))
+        new_state = project_state.clone()
+        operation.state_forwards(app_label, new_state)
+        with connection.schema_editor() as editor:
+            operation.database_forwards(app_label, editor, project_state, new_state)
+        self.assertIndexNameExists(table_name, index_name)
+        with connection.schema_editor() as editor:
+            operation.database_backwards(app_label, editor, new_state, project_state)
+        self.assertIndexNameExists(table_name, index_name)
+
     def test_alter_field_with_index(self):
         """
         Test AlterField operation with an index to ensure indexes created via
