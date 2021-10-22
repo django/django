551c997feaaa293987af8b05efc3634b93305fac
Manaia Junior <manaiajr.23@gmail.com>
2021-10-22 02:55:25 -0300

    Fixed #33214 -- Added BaseDatabaseOperations.format_for_duration_arithmetic() stub method.

--- tests/backends/base/test_operations.py
+++ tests/backends/base/test_operations.py
@@ -85,6 +85,11 @@ class SimpleDatabaseOperationTests(SimpleTestCase):
         now = timezone.now()
         self.assertEqual(self.ops.adapt_timefield_value(now), str(now))
 
+    def test_format_for_duration_arithmetic(self):
+        msg = self.may_require_msg % 'format_for_duration_arithmetic'
+        with self.assertRaisesMessage(NotImplementedError, msg):
+            self.ops.format_for_duration_arithmetic(None)
+
     def test_date_extract_sql(self):
         with self.assertRaisesMessage(NotImplementedError, self.may_require_msg % 'date_extract_sql'):
             self.ops.date_extract_sql(None, None)
