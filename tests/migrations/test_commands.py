9e6d6316977d5668a1d2d1f77b861b2b9323dcdf
andrewdotn <andrew@neitsch.ca>
2021-11-02 00:13:42 -0600

    Fixed #33246 -- Made squashmigrations raise CommandError when squashed_name already exists.

--- tests/migrations/test_commands.py
+++ tests/migrations/test_commands.py
@@ -2082,6 +2082,15 @@ class SquashMigrationsTests(MigrationTestBase):
             squashed_migration_file = os.path.join(migration_dir, '0001_%s.py' % squashed_name)
             self.assertTrue(os.path.exists(squashed_migration_file))
 
+    def test_squashed_name_exists(self):
+        msg = 'Migration 0001_initial already exists. Use a different name.'
+        with self.temporary_migration_module(module='migrations.test_migrations'):
+            with self.assertRaisesMessage(CommandError, msg):
+                call_command(
+                    'squashmigrations', 'migrations', '0001', '0002',
+                    squashed_name='initial', interactive=False, verbosity=0,
+                )
+
 
 class AppLabelErrorTests(TestCase):
     """
