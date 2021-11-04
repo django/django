ef77566e4099e6fe16dbffd0f9327f720896631d
Carlton Gibson <carlton.gibson@noumenal.es>
2021-11-04 16:01:27 +0100

    Updated tests to use numbers in some identifiers.

--- tests/staticfiles_tests/test_storage.py
+++ tests/staticfiles_tests/test_storage.py
@@ -162,24 +162,24 @@ class TestHashedFiles:
 
     def test_module_import(self):
         relpath = self.hashed_file_path('cached/module.js')
-        self.assertEqual(relpath, 'cached/module.91b9cf9935da.js')
+        self.assertEqual(relpath, 'cached/module.2e1725d38436.js')
         tests = [
             # Relative imports.
-            b'import testConst from "./module_test.d489af3cf882.js";',
+            b'import testConst from "./module_test.477bbebe77f0.js";',
             b'import relativeModule from "../nested/js/nested.866475c46bb4.js";',
-            b'import { firstConst, secondConst } from "./module_test.d489af3cf882.js";',
+            b'import { firstConst, secondConst } from "./module_test.477bbebe77f0.js";',
             # Absolute import.
             b'import rootConst from "/static/absolute_root.5586327fe78c.js";',
             # Dynamic import.
-            b'const dynamicModule = import("./module_test.d489af3cf882.js");',
+            b'const dynamicModule = import("./module_test.477bbebe77f0.js");',
             # Creating a module object.
-            b'import * as NewModule from "./module_test.d489af3cf882.js";',
+            b'import * as NewModule from "./module_test.477bbebe77f0.js";',
             # Aliases.
-            b'import { testConst as alias } from "./module_test.d489af3cf882.js";',
+            b'import { testConst as alias } from "./module_test.477bbebe77f0.js";',
             b'import {\n'
-            b'    firstVar as firstVarAlias,\n'
-            b'    secondVar as secondVarAlias\n'
-            b'} from "./module_test.d489af3cf882.js";',
+            b'    firstVar1 as firstVarAlias,\n'
+            b'    secondVar2 as secondVarAlias\n'
+            b'} from "./module_test.477bbebe77f0.js";',
         ]
         with storage.staticfiles_storage.open(relpath) as relfile:
             content = relfile.read()
@@ -190,14 +190,14 @@ class TestHashedFiles:
 
     def test_aggregating_modules(self):
         relpath = self.hashed_file_path('cached/module.js')
-        self.assertEqual(relpath, 'cached/module.91b9cf9935da.js')
+        self.assertEqual(relpath, 'cached/module.2e1725d38436.js')
         tests = [
-            b'export * from "./module_test.d489af3cf882.js";',
-            b'export { testConst } from "./module_test.d489af3cf882.js";',
+            b'export * from "./module_test.477bbebe77f0.js";',
+            b'export { testConst } from "./module_test.477bbebe77f0.js";',
             b'export {\n'
             b'    firstVar as firstVarAlias,\n'
             b'    secondVar as secondVarAlias\n'
-            b'} from "./module_test.d489af3cf882.js";',
+            b'} from "./module_test.477bbebe77f0.js";',
         ]
         with storage.staticfiles_storage.open(relpath) as relfile:
             content = relfile.read()
