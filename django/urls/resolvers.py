333c65603032c377e682cdbd7388657a5463a05a
Florian Apolloner <florian@apolloner.eu>
2021-11-29 11:52:03 +0100

    [3.2.x] Fixed #30530, CVE-2021-44420 -- Fixed potential bypass of an upstream access control based on URL paths.
    Thanks Sjoerd Job Postmus and TengMA(@te3t123) for reports.
    
    Backport of d4dcd5b9dd9e462fec8220e33e3e6c822b7e88a6 from main.

--- django/urls/resolvers.py
+++ django/urls/resolvers.py
@@ -154,7 +154,11 @@ class RegexPattern(CheckURLMixin):
         self.converters = {}
 
     def match(self, path):
-        match = self.regex.search(path)
+        match = (
+            self.regex.fullmatch(path)
+            if self._is_endpoint and self.regex.pattern.endswith('$')
+            else self.regex.search(path)
+        )
         if match:
             # If there are any named groups, use those as kwargs, ignoring
             # non-named groups. Otherwise, pass all non-named arguments as
@@ -244,7 +248,7 @@ def _route_to_regex(route, is_endpoint=False):
         converters[parameter] = converter
         parts.append('(?P<' + parameter + '>' + converter.regex + ')')
     if is_endpoint:
-        parts.append('$')
+        parts.append(r'\Z')
     return ''.join(parts), converters
 
 
