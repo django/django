a208093397ab1df05c29bbc8eb866d0617f68f17
Allen Jonathan David <allenajdjonathan@gmail.com>
2021-12-13 20:47:19 +0530

    Modified @deconstructible to prevent changes to subclass paths

--- django/utils/deconstruct.py
+++ django/utils/deconstruct.py
@@ -23,7 +23,7 @@ def deconstructible(*args, path=None):
             and keyword arguments.
             """
             # Fallback version
-            if path:
+            if path and obj.__class__.__name__ in path:
                 module_name, _, name = path.rpartition('.')
             else:
                 module_name = obj.__module__
@@ -39,6 +39,12 @@ def deconstructible(*args, path=None):
                     "For more information, see "
                     "https://docs.djangoproject.com/en/%s/topics/migrations/#serializing-values"
                     % (name, module_name, get_docs_version()))
+            if obj.__class__.__name__ not in path:
+                return (
+                    '%s.%s' % (obj.__class__.__module__, name),
+                    obj._constructor_args[0],
+                    obj._constructor_args[1],
+                )
             return (
                 path or '%s.%s' % (obj.__class__.__module__, name),
                 obj._constructor_args[0],
