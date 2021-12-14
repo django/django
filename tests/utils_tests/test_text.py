e1d673c373a7d032060872b690a92fc95496612e
Florian Apolloner <florian@apolloner.eu>
2021-12-14 12:38:28 +0100

    Fixed unescape_string_literal() crash on empty strings.

--- tests/utils_tests/test_text.py
+++ tests/utils_tests/test_text.py
@@ -226,7 +226,7 @@ class TestUtilsText(SimpleTestCase):
                 self.assertEqual(text.unescape_string_literal(lazystr(value)), output)
 
     def test_unescape_string_literal_invalid_value(self):
-        items = ['abc', "'abc\""]
+        items = ['', 'abc', "'abc\""]
         for item in items:
             msg = f'Not a string literal: {item!r}'
             with self.assertRaisesMessage(ValueError, msg):
