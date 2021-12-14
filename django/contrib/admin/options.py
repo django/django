ac5cc6cf01463d90aa333d5f6f046c311019827b
mgaligniana <marcelogaligniana@gmail.com>
2021-12-14 03:03:36 -0300

    Fixed #33316 -- Added pagination to admin history view.

--- django/contrib/admin/options.py
+++ django/contrib/admin/options.py
@@ -1927,6 +1927,7 @@ class ModelAdmin(BaseModelAdmin):
     def history_view(self, request, object_id, extra_context=None):
         "The 'history' admin view for this model."
         from django.contrib.admin.models import LogEntry
+        from django.contrib.admin.views.main import PAGE_VAR
 
         # First check if the user can see this history.
         model = self.model
@@ -1945,11 +1946,19 @@ class ModelAdmin(BaseModelAdmin):
             content_type=get_content_type_for_model(model)
         ).select_related().order_by('action_time')
 
+        paginator = self.get_paginator(request, action_list, 100)
+        page_number = request.GET.get(PAGE_VAR, 1)
+        page_obj = paginator.get_page(page_number)
+        page_range = paginator.get_elided_page_range(page_obj.number)
+
         context = {
             **self.admin_site.each_context(request),
             'title': _('Change history: %s') % obj,
             'subtitle': None,
-            'action_list': action_list,
+            'action_list': page_obj,
+            'page_range': page_range,
+            'page_var': PAGE_VAR,
+            'pagination_required': paginator.count > 100,
             'module_name': str(capfirst(opts.verbose_name_plural)),
             'object': obj,
             'opts': opts,
