django/django

commit 3fd82a62415e748002435e7bad06b5017507777c

Refs #32468 -- Added error message on invalid usage of cache decorators.

django/views/decorators/cache.py
@@ -1,5 +1,6 @@
 from functools import wraps
 
+from django.http import HttpRequest
 from django.middleware.cache import CacheMiddleware
 from django.utils.cache import add_never_cache_headers, patch_cache_control
 from django.utils.decorators import decorator_from_middleware_with_args
@@ -28,6 +29,12 @@ def cache_control(**kwargs):
     def _cache_controller(viewfunc):
         @wraps(viewfunc)
         def _cache_controlled(request, *args, **kw):
+            if not isinstance(request, HttpRequest):
+                raise TypeError(
+                    "cache_control didn't receive an HttpRequest. If you are "
+                    "decorating a classmethod, be sure to use "
+                    "@method_decorator."
+                )
             response = viewfunc(request, *args, **kw)
             patch_cache_control(response, **kwargs)
             return response
@@ -41,6 +48,11 @@ def never_cache(view_func):
     """
     @wraps(view_func)
     def _wrapped_view_func(request, *args, **kwargs):
+        if not isinstance(request, HttpRequest):
+            raise TypeError(
+                "never_cache didn't receive an HttpRequest. If you are "
+                "decorating a classmethod, be sure to use @method_decorator."
+            )
         response = view_func(request, *args, **kwargs)
         add_never_cache_headers(response)
         return response

tests/decorators/tests.py
@@ -470,7 +470,7 @@ class XFrameOptionsDecoratorsTests(TestCase):
         self.assertIsNone(r.get('X-Frame-Options', None))
 
 
-class NeverCacheDecoratorTest(TestCase):
+class NeverCacheDecoratorTest(SimpleTestCase):
     def test_never_cache_decorator(self):
         @never_cache
         def a_view(request):
@@ -480,3 +480,30 @@ class NeverCacheDecoratorTest(TestCase):
             set(r.headers['Cache-Control'].split(', ')),
             {'max-age=0', 'no-cache', 'no-store', 'must-revalidate', 'private'},
         )
+
+    def test_never_cache_decorator_http_request(self):
+        class MyClass:
+            @never_cache
+            def a_view(self, request):
+                return HttpResponse()
+        msg = (
+            "never_cache didn't receive an HttpRequest. If you are decorating "
+            "a classmethod, be sure to use @method_decorator."
+        )
+        with self.assertRaisesMessage(TypeError, msg):
+            MyClass().a_view(HttpRequest())
+
+
+class CacheControlDecoratorTest(SimpleTestCase):
+    def test_cache_control_decorator_http_request(self):
+        class MyClass:
+            @cache_control(a='b')
+            def a_view(self, request):
+                return HttpResponse()
+
+        msg = (
+            "cache_control didn't receive an HttpRequest. If you are "
+            "decorating a classmethod, be sure to use @method_decorator."
+        )
+        with self.assertRaisesMessage(TypeError, msg):
+            MyClass().a_view(HttpRequest())

commit d1a9b9be8fd9983fe2068c8f179efc6b201a343c

Fixed #33350 -- Reallowed using cache decorators with duck-typed HttpRequest.
Regression in 3fd82a62415e748002435e7bad06b5017507777c.

Thanks Terence Honles for the report.

django/views/decorators/cache.py
@@ -1,6 +1,5 @@
 from functools import wraps
 
-from django.http import HttpRequest
 from django.middleware.cache import CacheMiddleware
 from django.utils.cache import add_never_cache_headers, patch_cache_control
 from django.utils.decorators import decorator_from_middleware_with_args
@@ -29,7 +28,8 @@ def cache_control(**kwargs):
     def _cache_controller(viewfunc):
         @wraps(viewfunc)
         def _cache_controlled(request, *args, **kw):
-            if not isinstance(request, HttpRequest):
+            # Ensure argument looks like a request.
+            if not hasattr(request, 'META'):
                 raise TypeError(
                     "cache_control didn't receive an HttpRequest. If you are "
                     "decorating a classmethod, be sure to use "
@@ -48,7 +48,8 @@ def never_cache(view_func):
     """
     @wraps(view_func)
     def _wrapped_view_func(request, *args, **kwargs):
-        if not isinstance(request, HttpRequest):
+        # Ensure argument looks like a request.
+        if not hasattr(request, 'META'):
             raise TypeError(
                 "never_cache didn't receive an HttpRequest. If you are "
                 "decorating a classmethod, be sure to use @method_decorator."

tests/decorators/tests.py
@@ -493,6 +493,15 @@ class XFrameOptionsDecoratorsTests(TestCase):
         self.assertIsNone(r.get('X-Frame-Options', None))
 
 
+class HttpRequestProxy:
+    def __init__(self, request):
+        self._request = request
+
+    def __getattr__(self, attr):
+        """Proxy to the underlying HttpRequest object."""
+        return getattr(self._request, attr)
+
+
 class NeverCacheDecoratorTest(SimpleTestCase):
 
     @mock.patch('time.time')
@@ -525,12 +534,27 @@ class NeverCacheDecoratorTest(SimpleTestCase):
             @never_cache
             def a_view(self, request):
                 return HttpResponse()
+
+        request = HttpRequest()
         msg = (
             "never_cache didn't receive an HttpRequest. If you are decorating "
             "a classmethod, be sure to use @method_decorator."
         )
         with self.assertRaisesMessage(TypeError, msg):
-            MyClass().a_view(HttpRequest())
+            MyClass().a_view(request)
+        with self.assertRaisesMessage(TypeError, msg):
+            MyClass().a_view(HttpRequestProxy(request))
+
+    def test_never_cache_decorator_http_request_proxy(self):
+        class MyClass:
+            @method_decorator(never_cache)
+            def a_view(self, request):
+                return HttpResponse()
+
+        request = HttpRequest()
+        response = MyClass().a_view(HttpRequestProxy(request))
+        self.assertIn('Cache-Control', response.headers)
+        self.assertIn('Expires', response.headers)
 
 
 class CacheControlDecoratorTest(SimpleTestCase):
@@ -544,5 +568,18 @@ class CacheControlDecoratorTest(SimpleTestCase):
             "cache_control didn't receive an HttpRequest. If you are "
             "decorating a classmethod, be sure to use @method_decorator."
         )
+        request = HttpRequest()
         with self.assertRaisesMessage(TypeError, msg):
-            MyClass().a_view(HttpRequest())
+            MyClass().a_view(request)
+        with self.assertRaisesMessage(TypeError, msg):
+            MyClass().a_view(HttpRequestProxy(request))
+
+    def test_cache_control_decorator_http_request_proxy(self):
+        class MyClass:
+            @method_decorator(cache_control(a='b'))
+            def a_view(self, request):
+                return HttpResponse()
+
+        request = HttpRequest()
+        response = MyClass().a_view(HttpRequestProxy(request))
+        self.assertEqual(response.headers['Cache-Control'], 'a=b')