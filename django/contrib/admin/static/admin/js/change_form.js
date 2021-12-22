57429aeeb4e6c9fda509b4b18f0eb3236a929b2e
mgaligniana <marcelogaligniana@gmail.com>
2021-12-22 16:01:24 -0300

    Made changes asked in review by adamchainz

--- django/contrib/admin/static/admin/js/change_form.js
+++ django/contrib/admin/static/admin/js/change_form.js
@@ -2,32 +2,32 @@
 {
     const inputTags = ['BUTTON', 'INPUT', 'SELECT', 'TEXTAREA'];
     const modelName = document.getElementById('django-admin-form-add-constants').dataset.modelName;
-    const buttons = document.querySelectorAll('input[type=submit]');
-    const disableSubmitButtons = () => {
+    const disableButtons = (buttons) => {
         buttons.forEach(button => {
-            button.setAttribute('disabled', true);
+            button.setAttribute('disabled', '');
         });
     };
-    const enableSubmitButtons = () => {
+    const enableButtons = (buttons) => {
         buttons.forEach(button => {
             button.removeAttribute('disabled');
         });
     };
 
-    // Enable buttons when browser displays the page
-    // In case the user is navigating forward
-    // and backward through history
-    window.addEventListener('pageshow', () => {
-        enableSubmitButtons();
-    });
-
     if (modelName) {
         const form = document.getElementById(modelName + '_form');
+        const submitButtons = form.querySelectorAll('input[type=submit]');
+
+        // Enable buttons when browser displays the page
+        // In case the user is navigating forward and
+        // backward through history
+        window.addEventListener('pageshow', () => {
+            enableButtons(submitButtons);
+        });
 
         // Disable buttons during submit to avoid multiple submissions
         form.addEventListener('submit', (event) => {
             event.preventDefault();
-            disableSubmitButtons();
+            disableButtons(submitButtons);
             event.target.submit();
         });
 
