92412aa94c97b5c36387fadb39d9f8edf25547fa
Jacob Walls <jacobtylerwalls@gmail.com>
2021-12-28 10:46:42 -0500

    Fixed #23273 -- Avoided creation of django_migrations table when there are no migrations to apply.

--- tests/migrations/test_executor.py
+++ tests/migrations/test_executor.py
@@ -759,6 +759,17 @@ class ExecutorTests(MigrationTestBase):
             False,
         )
 
+    @mock.patch.object(MigrationRecorder, 'has_table', return_value=False)
+    def test_migrate_skips_schema_creation(self, mocked_has_table):
+        """
+        The django_migrations table is not created if there are no migrations
+        to record.
+        """
+        executor = MigrationExecutor(connection)
+        # 0 queries, since the query for has_table is being mocked.
+        with self.assertNumQueries(0):
+            executor.migrate([], plan=[])
+
 
 class FakeLoader:
     def __init__(self, graph, applied):
