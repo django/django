9d8b694205f2617477faac0ba0edbd06c4d07605
Swann <swann.bouviermuller@gmail.com>
2022-01-06 15:34:31 -0500

    [4.1.x] Fixed documentation of Widget.id_for_label() empty return value.
    Backport of d0863429a39dbc99a52a6d781006e7075c743d33 from main

--- django/forms/widgets.py
+++ django/forms/widgets.py
@@ -301,8 +301,8 @@ class Widget(metaclass=MediaDefiningClass):
 
     def id_for_label(self, id_):
         """
-        Return the HTML ID attribute of this Widget for use by a <label>,
-        given the ID of the field. Return None if no ID is available.
+        Return the HTML ID attribute of this Widget for use by a <label>, given
+        the ID of the field. Return an empty string if no ID is available.
 
         This hook is necessary because some widgets have multiple HTML
         elements and, thus, multiple IDs. In that case, this method should
