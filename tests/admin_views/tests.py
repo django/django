django/django

commit 84609b3205905097d7d3038d32e6101f012c0619

Fixed #31993 -- Added subtitles to admin change/view pages.

tests/admin_views/tests.py
@@ -100,10 +100,16 @@ class AdminViewBasicTestCase(TestCase):
         cls.superuser = User.objects.create_superuser(username='super', password='secret', email='super@example.com')
         cls.s1 = Section.objects.create(name='Test section')
         cls.a1 = Article.objects.create(
-            content='<p>Middle content</p>', date=datetime.datetime(2008, 3, 18, 11, 54, 58), section=cls.s1
+            content='<p>Middle content</p>',
+            date=datetime.datetime(2008, 3, 18, 11, 54, 58),
+            section=cls.s1,
+            title='Article 1',
         )
         cls.a2 = Article.objects.create(
-            content='<p>Oldest content</p>', date=datetime.datetime(2000, 3, 18, 11, 54, 58), section=cls.s1
+            content='<p>Oldest content</p>',
+            date=datetime.datetime(2000, 3, 18, 11, 54, 58),
+            section=cls.s1,
+            title='Article 2',
         )
         cls.a3 = Article.objects.create(
             content='<p>Newest content</p>', date=datetime.datetime(2009, 3, 18, 11, 54, 58), section=cls.s1
@@ -1026,6 +1032,53 @@ class AdminViewBasicTest(AdminViewBasicTestCase):
         response = self.client.get(reverse('admin6:admin_views_color_changelist'))
         self.assertContains(response, '<th scope="col" class="column-value">')
         self.assertNotContains(response, '<th scope="col" class="sortable column')
+
+    def test_change_view_subtitle_per_object(self):
+        response = self.client.get(
+            reverse('admin:admin_views_article_change', args=(self.a1.pk,)),
+        )
+        self.assertContains(
+            response,
+            '<title>Article 1 | Change article | Django site admin</title>',
+        )
+        self.assertContains(response, '<h1>Change article</h1>')
+        self.assertContains(response, '<h2>Article 1</h2>')
+        response = self.client.get(
+            reverse('admin:admin_views_article_change', args=(self.a2.pk,)),
+        )
+        self.assertContains(
+            response,
+            '<title>Article 2 | Change article | Django site admin</title>',
+        )
+        self.assertContains(response, '<h1>Change article</h1>')
+        self.assertContains(response, '<h2>Article 2</h2>')
+
+    def test_view_subtitle_per_object(self):
+        viewuser = User.objects.create_user(
+            username='viewuser', password='secret', is_staff=True,
+        )
+        viewuser.user_permissions.add(
+            get_perm(Article, get_permission_codename('view', Article._meta)),
+        )
+        self.client.force_login(viewuser)
+        response = self.client.get(
+            reverse('admin:admin_views_article_change', args=(self.a1.pk,)),
+        )
+        self.assertContains(
+            response,
+            '<title>Article 1 | View article | Django site admin</title>',
+        )
+        self.assertContains(response, '<h1>View article</h1>')
+        self.assertContains(response, '<h2>Article 1</h2>')
+        response = self.client.get(
+            reverse('admin:admin_views_article_change', args=(self.a2.pk,)),
+        )
+        self.assertContains(
+            response,
+            '<title>Article 2 | View article | Django site admin</title>',
+        )
+        self.assertContains(response, '<h1>View article</h1>')
+        self.assertContains(response, '<h2>Article 2</h2>')
 
 
 @override_settings(TEMPLATES=[{

django/contrib/admin/options.py
@@ -1627,6 +1627,7 @@ class ModelAdmin(BaseModelAdmin):
         context = {
             **self.admin_site.each_context(request),
             'title': title % opts.verbose_name,
+            'subtitle': str(obj) if obj else None,
             'adminform': adminForm,
             'object_id': object_id,
             'original': obj,
@@ -1815,6 +1816,7 @@ class ModelAdmin(BaseModelAdmin):
             'selection_note': _('0 of %(cnt)s selected') % {'cnt': len(cl.result_list)},
             'selection_note_all': selection_note_all % {'total_count': cl.result_count},
             'title': cl.title,
+            'subtitle': None,
             'is_popup': cl.is_popup,
             'to_field': cl.to_field,
             'cl': cl,

django/contrib/admin/templates/admin/base.html
@@ -86,6 +86,7 @@
         <div id="content" class="{% block coltype %}colM{% endblock %}">
           {% block pretitle %}{% endblock %}
           {% block content_title %}{% if title %}<h1>{{ title }}</h1>{% endif %}{% endblock %}
+          {% block content_subtitle %}{% if subtitle %}<h2>{{ subtitle }}</h2>{% endif %}{% endblock %}
           {% block content %}
             {% block object-tools %}{% endblock %}
             {{ content }}

django/contrib/admin/templates/admin/base_site.html
@@ -1,6 +1,6 @@
 {% extends "admin/base.html" %}
 
-{% block title %}{{ title }} | {{ site_title|default:_('Django site admin') }}{% endblock %}
+{% block title %}{% if subtitle %}{{ subtitle }} | {% endif %}{{ title }} | {{ site_title|default:_('Django site admin') }}{% endblock %}
 
 {% block branding %}
 <h1 id="site-name"><a href="{% url 'admin:index' %}">{{ site_header|default:_('Django administration') }}</a></h1>

commit 23dbb970b5fc4ffacb1f07c77e396a64f1ab99bf

Refs #32681 -- Fixed VariableDoesNotExist when rendering some admin template.
Regression in 84609b3205905097d7d3038d32e6101f012c0619.

Follow up to 4e5bbb6ef2287126badd32842b239f4a8a7394ca.

Thanks Sourav Kumar for the report.

django/contrib/admin/actions.py
@@ -59,6 +59,7 @@ def delete_selected(modeladmin, request, queryset):
     context = {
         **modeladmin.admin_site.each_context(request),
         'title': title,
+        'subtitle': None,
         'objects_name': str(objects_name),
         'deletable_objects': [deletable_objects],
         'model_count': dict(model_count).items(),

django/contrib/admin/sites.py
@@ -400,6 +400,7 @@ class AdminSite:
         context = {
             **self.each_context(request),
             'title': _('Log in'),
+            'subtitle': None,
             'app_path': request.get_full_path(),
             'username': request.user.get_username(),
         }

django/contrib/auth/views.py
@@ -162,6 +162,7 @@ class LogoutView(SuccessURLAllowedHostsMixin, TemplateView):
             'site': current_site,
             'site_name': current_site.name,
             'title': _('Logged out'),
+            'subtitle': None,
             **(self.extra_context or {})
         })
         return context
@@ -204,6 +205,7 @@ class PasswordContextMixin:
         context = super().get_context_data(**kwargs)
         context.update({
             'title': self.title,
+            'subtitle': None,
             **(self.extra_context or {})
         })
         return context

tests/admin_views/tests.py
@@ -1122,14 +1122,28 @@ class AdminViewBasicTest(AdminViewBasicTestCase):
     def test_render_views_no_subtitle(self):
         tests = [
             reverse('admin:index'),
+            reverse('admin:password_change'),
             reverse('admin:app_list', args=('admin_views',)),
             reverse('admin:admin_views_article_delete', args=(self.a1.pk,)),
             reverse('admin:admin_views_article_history', args=(self.a1.pk,)),
+            # Login must be after logout.
+            reverse('admin:logout'),
+            reverse('admin:login'),
         ]
         for url in tests:
             with self.subTest(url=url):
                 with self.assertNoLogs('django.template', 'DEBUG'):
                     self.client.get(url)
+
+    def test_render_delete_selected_confirmation_no_subtitle(self):
+        post_data = {
+            'action': 'delete_selected',
+            'selected_across': '0',
+            'index': '0',
+            '_selected_action': self.a1.pk,
+        }
+        with self.assertNoLogs('django.template', 'DEBUG'):
+            self.client.post(reverse('admin:admin_views_article_changelist'), post_data)
 
 
 @override_settings(TEMPLATES=[{