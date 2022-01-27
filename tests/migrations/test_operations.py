django/django

commit 325d7710ce9f6155bb55610ad6b4580d31263557

Fixed #32743 -- Added foreign key altering when altering type of referenced primary key with MTI.

django/db/backends/base/schema.py
@@ -36,10 +36,16 @@ def _all_related_fields(model):
 def _related_non_m2m_objects(old_field, new_field):
     # Filter out m2m objects from reverse relations.
     # Return (old_relation, new_relation) tuples.
-    return zip(
+    related_fields = zip(
         (obj for obj in _all_related_fields(old_field.model) if _is_relevant_relation(obj, old_field)),
         (obj for obj in _all_related_fields(new_field.model) if _is_relevant_relation(obj, new_field)),
     )
+    for old_rel, new_rel in related_fields:
+        yield old_rel, new_rel
+        yield from _related_non_m2m_objects(
+            old_rel.remote_field,
+            new_rel.remote_field,
+        )
 
 
 class BaseDatabaseSchemaEditor:

tests/migrations/test_operations.py
@@ -1505,6 +1505,57 @@ class OperationTests(OperationTestBase):
             operation.database_backwards("test_alflpkfk", editor, new_state, project_state)
         assertIdTypeEqualsFkType()
 
+    def test_alter_field_pk_mti_fk(self):
+        app_label = 'test_alflpkmtifk'
+        project_state = self.set_up_test_model(app_label, mti_model=True)
+        project_state = self.apply_operations(app_label, project_state, [
+            migrations.CreateModel('ShetlandRider', fields=[
+                (
+                    'pony',
+                    models.ForeignKey(f'{app_label}.ShetlandPony', models.CASCADE),
+                ),
+            ]),
+        ])
+        operation = migrations.AlterField(
+            'Pony',
+            'id',
+            models.BigAutoField(primary_key=True),
+        )
+        new_state = project_state.clone()
+        operation.state_forwards(app_label, new_state)
+        self.assertIsInstance(
+            new_state.models[app_label, 'pony'].fields['id'],
+            models.BigAutoField,
+        )
+
+        def _get_column_id_type(cursor, table, column):
+            return [
+                c.type_code
+                for c in connection.introspection.get_table_description(
+                    cursor,
+                    f'{app_label}_{table}',
+                )
+                if c.name == column
+            ][0]
+
+        def assertIdTypeEqualsMTIFkType():
+            with connection.cursor() as cursor:
+                parent_id_type = _get_column_id_type(cursor, 'pony', 'id')
+                child_id_type = _get_column_id_type(cursor, 'shetlandpony', 'pony_ptr_id')
+                mti_id_type = _get_column_id_type(cursor, 'shetlandrider', 'pony_id')
+            self.assertEqual(parent_id_type, child_id_type)
+            self.assertEqual(parent_id_type, mti_id_type)
+
+        assertIdTypeEqualsMTIFkType()
+        # Alter primary key.
+        with connection.schema_editor() as editor:
+            operation.database_forwards(app_label, editor, project_state, new_state)
+        assertIdTypeEqualsMTIFkType()
+        # Reversal.
+        with connection.schema_editor() as editor:
+            operation.database_backwards(app_label, editor, new_state, project_state)
+        assertIdTypeEqualsMTIFkType()
+
     @skipUnlessDBFeature('supports_foreign_keys')
     def test_alter_field_reloads_state_on_fk_with_to_field_target_type_change(self):
         app_label = 'test_alflrsfkwtflttc'

commit 14f43dd7b86ecc0e9568d518741eb0143bdcef26

Fixed #33462 -- Fixed migration crash when altering type of primary key with MTI and foreign key.
This prevents duplicated operations when altering type of primary key
with MTI and foreign key. Previously, a foreign key to the base model
was added twice, once directly and once by the inheritance model.

Thanks bcail for the report.

Regression in 325d7710ce9f6155bb55610ad6b4580d31263557.

django/db/backends/base/schema.py
@@ -30,7 +30,9 @@ def _is_relevant_relation(relation, altered_field):
 
 
 def _all_related_fields(model):
-    return model._meta._get_fields(forward=False, reverse=True, include_hidden=True)
+    return model._meta._get_fields(
+        forward=False, reverse=True, include_hidden=True, include_parents=False,
+    )
 
 
 def _related_non_m2m_objects(old_field, new_field):

tests/migrations/test_operations.py
@@ -1622,6 +1622,73 @@ class OperationTests(OperationTestBase):
                 (f'{app_label}_shetlandpony', 'pony_ptr_id'),
             )
 
+    def test_alter_field_pk_mti_and_fk_to_base(self):
+        app_label = 'test_alflpkmtiftb'
+        project_state = self.set_up_test_model(
+            app_label, mti_model=True, related_model=True,
+        )
+        operation = migrations.AlterField(
+            'Pony',
+            'id',
+            models.BigAutoField(primary_key=True),
+        )
+        new_state = project_state.clone()
+        operation.state_forwards(app_label, new_state)
+        self.assertIsInstance(
+            new_state.models[app_label, 'pony'].fields['id'],
+            models.BigAutoField,
+        )
+
+        def _get_column_id_type(cursor, table, column):
+            return [
+                c.type_code
+                for c in connection.introspection.get_table_description(
+                    cursor,
+                    f'{app_label}_{table}',
+                )
+                if c.name == column
+            ][0]
+
+        def assertIdTypeEqualsMTIFkType():
+            with connection.cursor() as cursor:
+                parent_id_type = _get_column_id_type(cursor, 'pony', 'id')
+                fk_id_type = _get_column_id_type(cursor, 'rider', 'pony_id')
+                child_id_type = _get_column_id_type(cursor, 'shetlandpony', 'pony_ptr_id')
+            self.assertEqual(parent_id_type, child_id_type)
+            self.assertEqual(parent_id_type, fk_id_type)
+
+        assertIdTypeEqualsMTIFkType()
+        # Alter primary key.
+        with connection.schema_editor() as editor:
+            operation.database_forwards(app_label, editor, project_state, new_state)
+        assertIdTypeEqualsMTIFkType()
+        if connection.features.supports_foreign_keys:
+            self.assertFKExists(
+                f'{app_label}_shetlandpony',
+                ['pony_ptr_id'],
+                (f'{app_label}_pony', 'id'),
+            )
+            self.assertFKExists(
+                f'{app_label}_rider',
+                ['pony_id'],
+                (f'{app_label}_pony', 'id'),
+            )
+        # Reversal.
+        with connection.schema_editor() as editor:
+            operation.database_backwards(app_label, editor, new_state, project_state)
+        assertIdTypeEqualsMTIFkType()
+        if connection.features.supports_foreign_keys:
+            self.assertFKExists(
+                f'{app_label}_shetlandpony',
+                ['pony_ptr_id'],
+                (f'{app_label}_pony', 'id'),
+            )
+            self.assertFKExists(
+                f'{app_label}_rider',
+                ['pony_id'],
+                (f'{app_label}_pony', 'id'),
+            )
+
     @skipUnlessDBFeature('supports_foreign_keys')
     def test_alter_field_reloads_state_on_fk_with_to_field_target_type_change(self):
         app_label = 'test_alflrsfkwtflttc'