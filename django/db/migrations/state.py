fd170199b4df88e03b4cddac925456d95d065909
Richard Trebichavský <richard.trebichavsky@gmail.com>
2022-02-07 10:39:36 +0100

    Added back initialization of `models_module` attribute that got missing after `super().__init__()` call was removed

--- django/db/migrations/state.py
+++ django/db/migrations/state.py
@@ -521,6 +521,7 @@ class AppConfigStub(AppConfig):
     """Stub of an AppConfig. Only provides a label and a dict of models."""
     def __init__(self, label):
         self.apps = None
+        self.models_module = None
         self.models = {}
         # App-label and app-name are not the same thing, so technically passing
         # in the label here is wrong. In practice, migrations don't care about
