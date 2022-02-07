ecbb1d7eb1ff62a3000236ac4677e65e599128e0
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2022-02-07 16:53:17 +0100

    Refs #33482 -- Fixed QuerySet selectin and filtering againts negated Exists() with empty queryset.
    Regression in b7d1da5a62fe4141beff2bfea565f7ef0038c94c.

--- tests/expressions/tests.py
+++ tests/expressions/tests.py
@@ -1918,6 +1918,14 @@ class ExistsTests(TestCase):
         )
         self.assertSequenceEqual(qs, [manager])
 
+    def test_select_negated_empty_exists(self):
+        manager = Manager.objects.create()
+        qs = Manager.objects.annotate(
+            not_exists=~Exists(Manager.objects.none())
+        ).filter(pk=manager.pk)
+        self.assertSequenceEqual(qs, [manager])
+        self.assertIs(qs.get().not_exists, True)
+
 
 class FieldTransformTests(TestCase):
 
