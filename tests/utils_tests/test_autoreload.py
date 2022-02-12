7a9e260b4285b15ff5fa3c58e723fda0c7ff46f1
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2022-02-12 09:51:18 +0530

    Blackened changes

--- tests/utils_tests/test_autoreload.py
+++ tests/utils_tests/test_autoreload.py
@@ -155,9 +155,7 @@ class TestIterModulesAndFiles(SimpleTestCase):
     def test_module_without_spec(self):
         module = types.ModuleType("test_module")
         del module.__spec__
-        self.assertEqual(
-            autoreload.iter_modules_and_files((module,), frozenset()), ()
-        )
+        self.assertEqual(autoreload.iter_modules_and_files((module,), frozenset()), ())
 
     def test_main_module_is_resolved(self):
         main_module = sys.modules["__main__"]
