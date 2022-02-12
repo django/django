3b3f38b3b09b0f2373e51406ecb8c9c45d36aebc
David Smith <smithdc@gmail.com>
2022-02-12 20:40:12 +0000

    Fixed #31169 -- Adapted the parallel test runner to use spawn.
    Co-authored-by: Valz <ahmadahussein0@gmail.com>
    Co-authored-by: Nick Pope <nick@nickpope.me.uk>

--- tests/contenttypes_tests/test_management.py
+++ tests/contenttypes_tests/test_management.py
@@ -22,6 +22,13 @@ class RemoveStaleContentTypesTests(TestCase):
 
     @classmethod
     def setUpTestData(cls):
+        with captured_stdout():
+            call_command(
+                "remove_stale_contenttypes",
+                interactive=False,
+                include_stale_apps=True,
+                verbosity=2,
+            )
         cls.before_count = ContentType.objects.count()
         cls.content_type = ContentType.objects.create(
             app_label="contenttypes_tests", model="Fake"
