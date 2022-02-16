django/django

commit 43289707809c814a70f0db38ca4f82f35f43dbfd

Fixed #33366 -- Fixed case handling with swappable setting detection in migrations autodetector.
The migration framework uniquely identifies models by case insensitive
labels composed of their app label and model names and so does the app
registry in most of its methods (e.g. AppConfig.get_model) but it
wasn't the case for get_swappable_settings_name() until this change.

This likely slipped under the radar for so long and only regressed in
b9df2b74b98b4d63933e8061d3cfc1f6f39eb747 because prior to the changes
related to the usage of model states instead of rendered models in the
auto-detector the exact value settings value was never going through a
case folding hoop.

Thanks Andrew Chen Wang for the report and Keryn Knight for the
investigation.

tests/field_deconstruction/tests.py
@@ -211,6 +211,13 @@ class FieldDeconstructionTests(SimpleTestCase):
         self.assertEqual(args, [])
         self.assertEqual(kwargs, {"to": "auth.user", "on_delete": models.CASCADE})
         self.assertEqual(kwargs['to'].setting_name, "AUTH_USER_MODEL")
+        # Swap detection for lowercase swappable model.
+        field = models.ForeignKey('auth.user', models.CASCADE)
+        name, path, args, kwargs = field.deconstruct()
+        self.assertEqual(path, 'django.db.models.ForeignKey')
+        self.assertEqual(args, [])
+        self.assertEqual(kwargs, {'to': 'auth.user', 'on_delete': models.CASCADE})
+        self.assertEqual(kwargs['to'].setting_name, 'AUTH_USER_MODEL')
         # Test nonexistent (for now) model
         field = models.ForeignKey("something.Else", models.CASCADE)
         name, path, args, kwargs = field.deconstruct()
@@ -272,6 +279,19 @@ class FieldDeconstructionTests(SimpleTestCase):
         self.assertEqual(args, [])
         self.assertEqual(kwargs, {"to": "auth.permission", "on_delete": models.CASCADE})
         self.assertEqual(kwargs['to'].setting_name, "AUTH_USER_MODEL")
+
+        # Model names are case-insensitive.
+        with isolate_lru_cache(apps.get_swappable_settings_name):
+            # It doesn't matter that we swapped out user for permission;
+            # there's no validation. We just want to check the setting stuff
+            # works.
+            field = models.ForeignKey('auth.permission', models.CASCADE)
+            name, path, args, kwargs = field.deconstruct()
+
+        self.assertEqual(path, 'django.db.models.ForeignKey')
+        self.assertEqual(args, [])
+        self.assertEqual(kwargs, {'to': 'auth.permission', 'on_delete': models.CASCADE})
+        self.assertEqual(kwargs['to'].setting_name, 'AUTH_USER_MODEL')
 
     def test_one_to_one(self):
         # Test basic pointing

tests/migrations/test_autodetector.py
@@ -1965,6 +1965,22 @@ class AutodetectorTests(TestCase):
         self.assertOperationAttributes(changes, 'testapp', 0, 0, name="Author")
         self.assertMigrationDependencies(changes, 'testapp', 0, [("__setting__", "AUTH_USER_MODEL")])
 
+    def test_swappable_lowercase(self):
+        model_state = ModelState('testapp', 'Document', [
+            ('id', models.AutoField(primary_key=True)),
+            ('owner', models.ForeignKey(
+                settings.AUTH_USER_MODEL.lower(), models.CASCADE,
+            )),
+        ])
+        with isolate_lru_cache(apps.get_swappable_settings_name):
+            changes = self.get_changes([], [model_state])
+        self.assertNumberMigrations(changes, 'testapp', 1)
+        self.assertOperationTypes(changes, 'testapp', 0, ['CreateModel'])
+        self.assertOperationAttributes(changes, 'testapp', 0, 0, name='Document')
+        self.assertMigrationDependencies(
+            changes, 'testapp', 0, [('__setting__', 'AUTH_USER_MODEL')],
+        )
+
     def test_swappable_changed(self):
         with isolate_lru_cache(apps.get_swappable_settings_name):
             before = self.make_project_state([self.custom_user, self.author_with_user])

django/apps/registry.py
@@ -286,13 +286,14 @@ class Apps:
         change after Django has loaded the settings, there is no reason to get
         the respective settings attribute over and over again.
         """
+        to_string = to_string.lower()
         for model in self.get_models(include_swapped=True):
             swapped = model._meta.swapped
             # Is this model swapped out for the model given by to_string?
-            if swapped and swapped == to_string:
+            if swapped and swapped.lower() == to_string:
                 return model._meta.swappable
             # Is this model swappable and the one given by to_string?
-            if model._meta.swappable and model._meta.label == to_string:
+            if model._meta.swappable and model._meta.label_lower == to_string:
                 return model._meta.swappable
         return None
 

commit fa4f797d6d05ae44ff4e848768a3fb29110a73d0

Fixed #33515 -- Prevented recreation of migration for ManyToManyField to lowercased swappable setting.
Thanks Chris Lee for the report.

Regression in 43289707809c814a70f0db38ca4f82f35f43dbfd.

Refs #23916.

django/db/models/fields/related.py
@@ -1728,11 +1728,15 @@ class ManyToManyField(RelatedField):
             kwargs["db_table"] = self.db_table
         if self.remote_field.db_constraint is not True:
             kwargs["db_constraint"] = self.remote_field.db_constraint
-        # Rel needs more work.
+        # Lowercase model names as they should be treated as case-insensitive.
         if isinstance(self.remote_field.model, str):
-            kwargs["to"] = self.remote_field.model
+            if "." in self.remote_field.model:
+                app_label, model_name = self.remote_field.model.split(".")
+                kwargs["to"] = "%s.%s" % (app_label, model_name.lower())
+            else:
+                kwargs["to"] = self.remote_field.model.lower()
         else:
-            kwargs["to"] = self.remote_field.model._meta.label
+            kwargs["to"] = self.remote_field.model._meta.label_lower
         if getattr(self.remote_field, "through", None) is not None:
             if isinstance(self.remote_field.through, str):
                 kwargs["through"] = self.remote_field.through

tests/field_deconstruction/tests.py
@@ -475,34 +475,34 @@ class FieldDeconstructionTests(SimpleTestCase):
         name, path, args, kwargs = field.deconstruct()
         self.assertEqual(path, "django.db.models.ManyToManyField")
         self.assertEqual(args, [])
-        self.assertEqual(kwargs, {"to": "auth.Permission"})
+        self.assertEqual(kwargs, {"to": "auth.permission"})
         self.assertFalse(hasattr(kwargs["to"], "setting_name"))
         # Test swappable
         field = models.ManyToManyField("auth.User")
         name, path, args, kwargs = field.deconstruct()
         self.assertEqual(path, "django.db.models.ManyToManyField")
         self.assertEqual(args, [])
-        self.assertEqual(kwargs, {"to": "auth.User"})
+        self.assertEqual(kwargs, {"to": "auth.user"})
         self.assertEqual(kwargs["to"].setting_name, "AUTH_USER_MODEL")
         # Test through
         field = models.ManyToManyField("auth.Permission", through="auth.Group")
         name, path, args, kwargs = field.deconstruct()
         self.assertEqual(path, "django.db.models.ManyToManyField")
         self.assertEqual(args, [])
-        self.assertEqual(kwargs, {"to": "auth.Permission", "through": "auth.Group"})
+        self.assertEqual(kwargs, {"to": "auth.permission", "through": "auth.Group"})
         # Test custom db_table
         field = models.ManyToManyField("auth.Permission", db_table="custom_table")
         name, path, args, kwargs = field.deconstruct()
         self.assertEqual(path, "django.db.models.ManyToManyField")
         self.assertEqual(args, [])
-        self.assertEqual(kwargs, {"to": "auth.Permission", "db_table": "custom_table"})
+        self.assertEqual(kwargs, {"to": "auth.permission", "db_table": "custom_table"})
         # Test related_name
         field = models.ManyToManyField("auth.Permission", related_name="custom_table")
         name, path, args, kwargs = field.deconstruct()
         self.assertEqual(path, "django.db.models.ManyToManyField")
         self.assertEqual(args, [])
         self.assertEqual(
-            kwargs, {"to": "auth.Permission", "related_name": "custom_table"}
+            kwargs, {"to": "auth.permission", "related_name": "custom_table"}
         )
         # Test related_query_name
         field = models.ManyToManyField("auth.Permission", related_query_name="foobar")
@@ -510,7 +510,7 @@ class FieldDeconstructionTests(SimpleTestCase):
         self.assertEqual(path, "django.db.models.ManyToManyField")
         self.assertEqual(args, [])
         self.assertEqual(
-            kwargs, {"to": "auth.Permission", "related_query_name": "foobar"}
+            kwargs, {"to": "auth.permission", "related_query_name": "foobar"}
         )
         # Test limit_choices_to
         field = models.ManyToManyField(
@@ -520,7 +520,7 @@ class FieldDeconstructionTests(SimpleTestCase):
         self.assertEqual(path, "django.db.models.ManyToManyField")
         self.assertEqual(args, [])
         self.assertEqual(
-            kwargs, {"to": "auth.Permission", "limit_choices_to": {"foo": "bar"}}
+            kwargs, {"to": "auth.permission", "limit_choices_to": {"foo": "bar"}}
         )
 
     @override_settings(AUTH_USER_MODEL="auth.Permission")
@@ -533,7 +533,7 @@ class FieldDeconstructionTests(SimpleTestCase):
 
         self.assertEqual(path, "django.db.models.ManyToManyField")
         self.assertEqual(args, [])
-        self.assertEqual(kwargs, {"to": "auth.Permission"})
+        self.assertEqual(kwargs, {"to": "auth.permission"})
         self.assertEqual(kwargs["to"].setting_name, "AUTH_USER_MODEL")
 
     def test_many_to_many_field_related_name(self):
@@ -551,7 +551,7 @@ class FieldDeconstructionTests(SimpleTestCase):
         self.assertEqual(args, [])
         # deconstruct() should not include attributes which were not passed to
         # the field during initialization.
-        self.assertEqual(kwargs, {"to": "field_deconstruction.MyModel"})
+        self.assertEqual(kwargs, {"to": "field_deconstruction.mymodel"})
         # Passed attributes.
         name, path, args, kwargs = MyModel.m2m_related_name.field.deconstruct()
         self.assertEqual(path, "django.db.models.ManyToManyField")
@@ -559,7 +559,7 @@ class FieldDeconstructionTests(SimpleTestCase):
         self.assertEqual(
             kwargs,
             {
-                "to": "field_deconstruction.MyModel",
+                "to": "field_deconstruction.mymodel",
                 "related_query_name": "custom_query_name",
                 "limit_choices_to": {"flag": True},
             },

tests/migrations/test_autodetector.py
@@ -3279,6 +3279,31 @@ class AutodetectorTests(TestCase):
             [("__setting__", "AUTH_USER_MODEL")],
         )
 
+    @override_settings(AUTH_USER_MODEL="thirdapp.CustomUser")
+    def test_swappable_many_to_many_model_case(self):
+        document_lowercase = ModelState(
+            "testapp",
+            "Document",
+            [
+                ("id", models.AutoField(primary_key=True)),
+                ("owners", models.ManyToManyField(settings.AUTH_USER_MODEL.lower())),
+            ],
+        )
+        document = ModelState(
+            "testapp",
+            "Document",
+            [
+                ("id", models.AutoField(primary_key=True)),
+                ("owners", models.ManyToManyField(settings.AUTH_USER_MODEL)),
+            ],
+        )
+        with isolate_lru_cache(apps.get_swappable_settings_name):
+            changes = self.get_changes(
+                [self.custom_user, document_lowercase],
+                [self.custom_user, document],
+            )
+        self.assertEqual(len(changes), 0)
+
     def test_swappable_changed(self):
         with isolate_lru_cache(apps.get_swappable_settings_name):
             before = self.make_project_state([self.custom_user, self.author_with_user])