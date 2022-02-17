b2ed0d78f2dff9986ef15b9098c1b6d9ce720a99
Matthias Kestenholz <mk@feinheit.ch>
2022-02-17 09:45:34 +0100

    Refs #28358 -- Fixed infinite recursion in LazyObject.__getattribute__().
    Regression in 97d7990abde3fe4b525ae83958fd0b52d6a1d13f.
    
    Co-authored-by: Mariusz Felisiak <felisiak.mariusz@gmail.com>
    Co-authored-by: Theo Alexiou <theofilosalexiou@gmail.com>

--- django/utils/functional.py
+++ django/utils/functional.py
@@ -288,6 +288,9 @@ class LazyObject:
         self._wrapped = empty
 
     def __getattribute__(self, name):
+        if name == "_wrapped":
+            # Avoid recursion when getting wrapped object.
+            return super().__getattribute__(name)
         value = super().__getattribute__(name)
         # If attribute is a proxy method, raise an AttributeError to call
         # __getattr__() and use the wrapped object method.
