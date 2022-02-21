1299bc33e131a3c44b544b58c706bc998c4228ed
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2022-02-21 07:54:47 +0100

    Refs #33526 -- Made CSRF_COOKIE_SECURE/SESSION_COOKIE_SECURE/SESSION_COOKIE_HTTPONLY don't pass on truthy values.

--- django/core/checks/security/csrf.py
+++ django/core/checks/security/csrf.py
@@ -37,7 +37,7 @@ def check_csrf_cookie_secure(app_configs, **kwargs):
     passed_check = (
         settings.CSRF_USE_SESSIONS
         or not _csrf_middleware()
-        or settings.CSRF_COOKIE_SECURE
+        or settings.CSRF_COOKIE_SECURE is True
     )
     return [] if passed_check else [W016]
 
