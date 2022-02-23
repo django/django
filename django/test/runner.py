f6da0d9ffcbf6c177da75cb080eb2e90a7bb63dd
Valz <ahmadahussein0@gmail.com>
2022-02-23 11:34:22 +0100

    Refs #31169 -- Added DatabaseCreation.setup_worker_connection() hook.

--- django/test/runner.py
+++ django/test/runner.py
@@ -407,13 +407,7 @@ def _init_worker(counter):
 
     for alias in connections:
         connection = connections[alias]
-        settings_dict = connection.creation.get_test_db_clone_settings(str(_worker_id))
-        # connection.settings_dict must be updated in place for changes to be
-        # reflected in django.db.connections. If the following line assigned
-        # connection.settings_dict = settings_dict, new threads would connect
-        # to the default database instead of the appropriate clone.
-        connection.settings_dict.update(settings_dict)
-        connection.close()
+        connection.creation.setup_worker_connection(_worker_id)
 
 
 def _run_subsuite(args):
