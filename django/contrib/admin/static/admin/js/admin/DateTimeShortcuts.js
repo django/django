faf72c815188861c022996498395afa53a48fa1f
Shrikant Dhayje <35664193+shriekdj@users.noreply.github.com>
2022-02-24 11:48:24 +0530

    Incomplete string escaping or encoding
    This replaces only the first occurrence of "//", "/r", "/n", "/t" and "'",
    So We Should Use regex for replacing all occurrence.

--- django/contrib/admin/static/admin/js/admin/DateTimeShortcuts.js
+++ django/contrib/admin/static/admin/js/admin/DateTimeShortcuts.js
@@ -390,11 +390,11 @@
         handleCalendarCallback: function(num) {
             let format = get_format('DATE_INPUT_FORMATS')[0];
             // the format needs to be escaped a little
-            format = format.replace('\\', '\\\\')
-                .replace('\r', '\\r')
-                .replace('\n', '\\n')
-                .replace('\t', '\\t')
-                .replace("'", "\\'");
+            format = format.replace(/\\/g, '\\\\')
+                .replace(/\r/g, '\\r')
+                .replace(/\n/g, '\\n')
+                .replace(/\t/g, '\\t')
+                .replace(/'/g, "\\'");
             return function(y, m, d) {
                 DateTimeShortcuts.calendarInputs[num].value = new Date(y, m - 1, d).strftime(format);
                 DateTimeShortcuts.calendarInputs[num].focus();
