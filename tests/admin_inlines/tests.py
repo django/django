2d1f0b3eb9cdb8259ceb1561d9d90753490cc35f
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2022-02-28 09:13:05 +0100

    Fixed #33547 -- Fixed error when rendering invalid inlines with readonly fields in admin.
    Regression in de95c826673be9ea519acc86fd898631d1a11356.
    
    Thanks David Glenck for the report.

--- tests/admin_inlines/tests.py
+++ tests/admin_inlines/tests.py
@@ -241,6 +241,22 @@ class TestInline(TestDataMixin, TestCase):
         # column cells
         self.assertContains(response, "<p>Callable in QuestionInline</p>")
 
+    def test_model_error_inline_with_readonly_field(self):
+        poll = Poll.objects.create(name="Test poll")
+        data = {
+            "question_set-TOTAL_FORMS": 1,
+            "question_set-INITIAL_FORMS": 0,
+            "question_set-MAX_NUM_FORMS": 0,
+            "_save": "Save",
+            "question_set-0-text": "Question",
+            "question_set-0-poll": poll.pk,
+        }
+        response = self.client.post(
+            reverse("admin:admin_inlines_poll_change", args=(poll.pk,)),
+            data,
+        )
+        self.assertContains(response, "Always invalid model.")
+
     def test_help_text(self):
         """
         The inlines' model field help texts are displayed when using both the
