a46bc327e70f81b66800780edf3830f6137a89e3
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2022-03-04 09:51:52 +0100

    Refs #11293 -- Added test for filtering aggregates with negated & operator.

--- tests/aggregation_regress/tests.py
+++ tests/aggregation_regress/tests.py
@@ -1682,13 +1682,25 @@ class AggregationTests(TestCase):
             qs, ["Adrian Holovaty", "Peter Norvig"], lambda b: b.name
         )
 
-    def test_ticket_11293(self):
+    def test_filter_aggregates_or_connector(self):
         q1 = Q(price__gt=50)
         q2 = Q(authors__count__gt=1)
         query = Book.objects.annotate(Count("authors")).filter(q1 | q2).order_by("pk")
         self.assertQuerysetEqual(
             query,
             [self.b1.pk, self.b4.pk, self.b5.pk, self.b6.pk],
+            attrgetter("pk"),
+        )
+
+    def test_filter_aggregates_negated_and_connector(self):
+        q1 = Q(price__gt=50)
+        q2 = Q(authors__count__gt=1)
+        query = (
+            Book.objects.annotate(Count("authors")).filter(~(q1 & q2)).order_by("pk")
+        )
+        self.assertQuerysetEqual(
+            query,
+            [self.b1.pk, self.b2.pk, self.b3.pk, self.b4.pk, self.b6.pk],
             attrgetter("pk"),
         )
 
