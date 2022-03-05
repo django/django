615e3e4b1bd56beeb54a503c137d1e462c7b9b9a
adontz <adontz@gmail.com>
2022-03-05 20:09:42 +0400

    Fixed #7497 -- Allowed overriding the order of apps and models in admin.

--- tests/admin_views/customadmin.py
+++ tests/admin_views/customadmin.py
@@ -35,6 +35,14 @@ class Admin2(admin.AdminSite):
     def password_change(self, request, extra_context=None):
         return super().password_change(request, {"spam": "eggs"})
 
+    def get_app_list(self, request, app_label=None):
+        app_list = super().get_app_list(request, app_label=app_label)
+        # Reverse order of apps and models.
+        app_list = list(reversed(app_list))
+        for app in app_list:
+            app["models"].sort(key=lambda x: x["name"], reverse=True)
+        return app_list
+
 
 class UserLimitedAdmin(UserAdmin):
     # used for testing password change on a user not in queryset
