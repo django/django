06f01af39f7dbd8ca425ab814f91c51f77fdd1b5
Hameed Gifford <giff.h92@gmail.com>
2022-03-07 16:22:03 -0500

    Remove redundant QuerySet.all()
    This holds the fix in line with the work at this PR:
    https://github.com/django/django/pull/15328

--- django/contrib/contenttypes/migrations/0002_remove_content_type_name.py
+++ django/contrib/contenttypes/migrations/0002_remove_content_type_name.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 def add_legacy_name(apps, schema_editor):
     alias = schema_editor.connection.alias
     ContentType = apps.get_model("contenttypes", "ContentType")
-    for ct in ContentType.objects.using(alias).all():
+    for ct in ContentType.objects.using(alias):
         try:
             ct.name = apps.get_model(ct.app_label, ct.model)._meta.object_name
         except LookupError:
