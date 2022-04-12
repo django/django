adb08cf327a1e7e38ad1a62a1638a24b102a4267
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2022-04-12 11:35:54 +0200

    Fixed DatabaseFeatures.supports_expression_indexes on MySQL with MyISAM.

--- django/db/backends/mysql/features.py
+++ django/db/backends/mysql/features.py
@@ -336,5 +336,6 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     def supports_expression_indexes(self):
         return (
             not self.connection.mysql_is_mariadb
+            and self._mysql_storage_engine != "MyISAM"
             and self.connection.mysql_version >= (8, 0, 13)
         )
