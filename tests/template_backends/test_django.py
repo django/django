7b05044f2d097a2771e342aaa60140c67ffb4b05
Carlton Gibson <carlton.gibson@noumenal.es>
2022-04-12 16:21:02 +0200

    Fixed #33639 -- Enabled cached template loader in development.

--- tests/template_backends/test_django.py
+++ tests/template_backends/test_django.py
@@ -176,27 +176,25 @@ class DjangoTemplatesTests(TemplateStringsTests):
             "Hello, Bob &amp; Jim",
         )
 
-    default_loaders = [
-        "django.template.loaders.filesystem.Loader",
-        "django.template.loaders.app_directories.Loader",
-    ]
-
-    @override_settings(DEBUG=False)
-    def test_non_debug_default_template_loaders(self):
-        engine = DjangoTemplates(
-            {"DIRS": [], "APP_DIRS": True, "NAME": "django", "OPTIONS": {}}
-        )
-        self.assertEqual(
-            engine.engine.loaders,
-            [("django.template.loaders.cached.Loader", self.default_loaders)],
-        )
-
-    @override_settings(DEBUG=True)
-    def test_debug_default_template_loaders(self):
-        engine = DjangoTemplates(
-            {"DIRS": [], "APP_DIRS": True, "NAME": "django", "OPTIONS": {}}
-        )
-        self.assertEqual(engine.engine.loaders, self.default_loaders)
+    def test_default_template_loaders(self):
+        """The cached template loader is always enabled by default."""
+        for debug in (True, False):
+            with self.subTest(DEBUG=debug), self.settings(DEBUG=debug):
+                engine = DjangoTemplates(
+                    {"DIRS": [], "APP_DIRS": True, "NAME": "django", "OPTIONS": {}}
+                )
+                self.assertEqual(
+                    engine.engine.loaders,
+                    [
+                        (
+                            "django.template.loaders.cached.Loader",
+                            [
+                                "django.template.loaders.filesystem.Loader",
+                                "django.template.loaders.app_directories.Loader",
+                            ],
+                        )
+                    ],
+                )
 
     def test_dirs_pathlib(self):
         engine = DjangoTemplates(
