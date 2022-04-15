a1e4e86f923dc8387b0a9c3025bdd5d096a6ebb8
Alexandru Mărășteanu <alexei@users.noreply.github.com>
2022-04-15 23:00:28 +0300

    Fixed #33607 -- Made PostgresIndex.create_sql() respect the "using" argument.

--- django/contrib/postgres/indexes.py
+++ django/contrib/postgres/indexes.py
@@ -25,7 +25,7 @@ class PostgresIndex(Index):
     def create_sql(self, model, schema_editor, using="", **kwargs):
         self.check_supported(schema_editor)
         statement = super().create_sql(
-            model, schema_editor, using=" USING %s" % self.suffix, **kwargs
+            model, schema_editor, using=" USING %s" % (using or self.suffix), **kwargs
         )
         with_params = self.get_with_params()
         if with_params:
