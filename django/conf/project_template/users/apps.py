11d4856172a749cd119d28059be3d47b2bdbbf90
Allen, Timothy <tallen@wharton.upenn.edu>
2022-04-18 16:19:38 -0400

    Add a basic default apps.py and admin.py.

---
+++ django/conf/project_template/users/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class UsersConfig(AppConfig):
+    """
+    Configuration for the users app.
+    """
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "users"
