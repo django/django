4c2b26174f044adc4a6461154385720479eaee55
Tim Graham <timograham@gmail.com>
2022-05-01 21:44:04 -0400

    [3.2.x] Removed 'tests' path prefix in a couple tests.
    Backport of 694cf458f16b8d340a3195244196980b2dec34fd from main.

--- tests/sessions_tests/tests.py
+++ tests/sessions_tests/tests.py
@@ -915,9 +915,9 @@ class CookieSessionTests(SessionTestsMixin, SimpleTestCase):
 class ClearSessionsCommandTests(SimpleTestCase):
     def test_clearsessions_unsupported(self):
         msg = (
-            "Session engine 'tests.sessions_tests.no_clear_expired' doesn't "
+            "Session engine 'sessions_tests.no_clear_expired' doesn't "
             "support clearing expired sessions."
         )
-        with self.settings(SESSION_ENGINE='tests.sessions_tests.no_clear_expired'):
+        with self.settings(SESSION_ENGINE="sessions_tests.no_clear_expired"):
             with self.assertRaisesMessage(management.CommandError, msg):
                 management.call_command('clearsessions')
