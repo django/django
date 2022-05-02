694cf458f16b8d340a3195244196980b2dec34fd
Tim Graham <timograham@gmail.com>
2022-05-01 21:44:04 -0400

    Removed 'tests' path prefix in a couple tests.

--- tests/i18n/tests.py
+++ tests/i18n/tests.py
@@ -2358,7 +2358,7 @@ class WatchForTranslationChangesTests(SimpleTestCase):
 
     def test_i18n_app_dirs(self):
         mocked_sender = mock.MagicMock()
-        with self.settings(INSTALLED_APPS=["tests.i18n.sampleproject"]):
+        with self.settings(INSTALLED_APPS=["i18n.sampleproject"]):
             watch_for_translation_changes(mocked_sender)
         project_dir = Path(__file__).parent / "sampleproject" / "locale"
         mocked_sender.watch_dir.assert_any_call(project_dir, "**/*.mo")
