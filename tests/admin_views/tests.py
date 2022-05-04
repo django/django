08dfae7697145f1b774d2e4e62c449c53e9e2cc5
Daniel Hahler <git@thequod.de>
2022-05-04 11:38:12 +0200

    Fixed #33679 -- Fixed the last inline's id in admin for users without add permissions.

--- tests/admin_views/tests.py
+++ tests/admin_views/tests.py
@@ -2694,6 +2694,20 @@ class AdminViewPermissionsTest(TestCase):
         formset = response.context["inline_admin_formsets"][0]
         self.assertEqual(len(formset.forms), 3)
 
+    def test_change_view_with_view_only_last_inline(self):
+        self.viewuser.user_permissions.add(
+            get_perm(Section, get_permission_codename("view", Section._meta))
+        )
+        self.client.force_login(self.viewuser)
+        response = self.client.get(
+            reverse("admin:admin_views_section_change", args=(self.s1.pk,))
+        )
+        self.assertEqual(len(response.context["inline_admin_formsets"]), 1)
+        formset = response.context["inline_admin_formsets"][0]
+        self.assertEqual(len(formset.forms), 3)
+        # The last inline is not marked as empty.
+        self.assertContains(response, 'id="article_set-2"')
+
     def test_change_view_with_view_and_add_inlines(self):
         """User has view and add permissions on the inline model."""
         self.viewuser.user_permissions.add(
