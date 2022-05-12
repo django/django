3a82b5f655446f0ca89e3b6a92b100aa458f348f
Kapil Bansal <kapilbansal.gbpecdelhi@gmail.com>
2022-05-12 11:30:47 +0200

    Fixed #32559 -- Added 'step_size’ to numeric form fields.
    Co-authored-by: Jacob Rief <jacob.rief@uibk.ac.at>

--- tests/forms_tests/field_tests/test_floatfield.py
+++ tests/forms_tests/field_tests/test_floatfield.py
@@ -70,6 +70,21 @@ class FloatFieldTest(FormFieldAssertionsMixin, SimpleTestCase):
         self.assertEqual(f.max_value, 1.5)
         self.assertEqual(f.min_value, 0.5)
 
+    def test_floatfield_4(self):
+        f = FloatField(step_size=0.02)
+        self.assertWidgetRendersTo(
+            f,
+            '<input name="f" step="0.02" type="number" id="id_f" required>',
+        )
+        msg = "'Ensure this value is a multiple of step size 0.02.'"
+        with self.assertRaisesMessage(ValidationError, msg):
+            f.clean("0.01")
+        self.assertEqual(2.34, f.clean("2.34"))
+        self.assertEqual(2.1, f.clean("2.1"))
+        self.assertEqual(-0.50, f.clean("-.5"))
+        self.assertEqual(-1.26, f.clean("-1.26"))
+        self.assertEqual(f.step_size, 0.02)
+
     def test_floatfield_widget_attrs(self):
         f = FloatField(widget=NumberInput(attrs={"step": 0.01, "max": 1.0, "min": 0.0}))
         self.assertWidgetRendersTo(
