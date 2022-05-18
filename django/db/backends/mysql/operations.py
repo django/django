19297de2fe5a9c47e471c64249366f39fe12f16a
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2022-05-18 08:38:08 +0200

    Fixed #33713 -- Dropped support for MariaDB 10.3.

--- django/db/backends/mysql/operations.py
+++ django/db/backends/mysql/operations.py
@@ -441,7 +441,6 @@ class DatabaseOperations(BaseDatabaseOperations):
     def on_conflict_suffix_sql(self, fields, on_conflict, update_fields, unique_fields):
         if on_conflict == OnConflict.UPDATE:
             conflict_suffix_sql = "ON DUPLICATE KEY UPDATE %(fields)s"
-            field_sql = "%(field)s = VALUES(%(field)s)"
             # The use of VALUES() is deprecated in MySQL 8.0.20+. Instead, use
             # aliases for the new row and its columns available in MySQL
             # 8.0.19+.
@@ -449,8 +448,10 @@ class DatabaseOperations(BaseDatabaseOperations):
                 if self.connection.mysql_version >= (8, 0, 19):
                     conflict_suffix_sql = f"AS new {conflict_suffix_sql}"
                     field_sql = "%(field)s = new.%(field)s"
-            # VALUES() was renamed to VALUE() in MariaDB 10.3.3+.
-            elif self.connection.mysql_version >= (10, 3, 3):
+                else:
+                    field_sql = "%(field)s = VALUES(%(field)s)"
+            # Use VALUE() on MariaDB.
+            else:
                 field_sql = "%(field)s = VALUE(%(field)s)"
 
             fields = ", ".join(
--- tests/backends/mysql/tests.py
+++ tests/backends/mysql/tests.py
@@ -107,8 +107,8 @@ class Tests(TestCase):
     @mock.patch.object(connection, "get_database_version")
     def test_check_database_version_supported(self, mocked_get_database_version):
         if connection.mysql_is_mariadb:
-            mocked_get_database_version.return_value = (10, 2)
-            msg = "MariaDB 10.3 or later is required (found 10.2)."
+            mocked_get_database_version.return_value = (10, 3)
+            msg = "MariaDB 10.4 or later is required (found 10.3)."
         else:
             mocked_get_database_version.return_value = (5, 6)
             msg = "MySQL 5.7 or later is required (found 5.6)."
