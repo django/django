88b1650d567408e584f7c55782caeac4d8f55279
Hasan Ramezani <hasan.r67@gmail.com>
2022-05-19 21:47:18 +0200

    Fixed #33715 -- Prevented MySQL dbshell from exiting on Ctrl-C.

--- tests/dbshell/test_mysql.py
+++ tests/dbshell/test_mysql.py
@@ -1,8 +1,11 @@
 import os
+import signal
 import subprocess
 import sys
 from pathlib import Path
+from unittest import mock, skipUnless
 
+from django.db import connection
 from django.db.backends.mysql.client import DatabaseClient
 from django.test import SimpleTestCase
 
@@ -218,3 +221,19 @@ class MySqlDbshellCommandTestCase(SimpleTestCase):
         with self.assertRaises(subprocess.CalledProcessError) as ctx:
             subprocess.run(args, check=True, env=env)
         self.assertNotIn("somepassword", str(ctx.exception))
+
+    @skipUnless(connection.vendor == "mysql", "Requires a MySQL connection")
+    def test_sigint_handler(self):
+        """SIGINT is ignored in Python and passed to mysql to abort queries."""
+
+        def _mock_subprocess_run(*args, **kwargs):
+            handler = signal.getsignal(signal.SIGINT)
+            self.assertEqual(handler, signal.SIG_IGN)
+
+        sigint_handler = signal.getsignal(signal.SIGINT)
+        # The default handler isn't SIG_IGN.
+        self.assertNotEqual(sigint_handler, signal.SIG_IGN)
+        with mock.patch("subprocess.run", new=_mock_subprocess_run):
+            connection.client.runshell([])
+        # dbshell restores the original handler.
+        self.assertEqual(sigint_handler, signal.getsignal(signal.SIGINT))
