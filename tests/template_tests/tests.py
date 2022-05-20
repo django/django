04d74436bf1eef0ff4bee3fff961e2640ece3319
cheng <chengyuan.china@gmail.com>
2022-05-19 21:01:10 -0700

    Fixed #33653 -- Fixed template crash when calling methods for built-in types without required arguments.
    Regression in 09341856ed9008875c1cc883dc0c287670131458.

--- tests/template_tests/tests.py
+++ tests/template_tests/tests.py
@@ -183,6 +183,14 @@ class TemplateTestMixin:
         for node in template.nodelist:
             self.assertEqual(node.origin, template.origin)
 
+    def test_render_built_in_type_method(self):
+        """
+        Templates should not crash when rendering methods for built-in types
+        without required arguments.
+        """
+        template = self._engine().from_string("{{ description.count }}")
+        self.assertEqual(template.render(Context({"description": "test"})), "")
+
 
 class TemplateTests(TemplateTestMixin, SimpleTestCase):
     debug_engine = False
