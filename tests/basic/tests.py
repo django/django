9a4076469d8be6ccc5ec7bdb7124c817ddc65275
kshivakumar <kshivakumar@outlook.com>
2022-05-24 17:55:35 +0530

    Fixed #33733 -- Preserved wrapper assignment for manager methods.

--- tests/basic/tests.py
+++ tests/basic/tests.py
@@ -1,3 +1,4 @@
+import inspect
 import threading
 from datetime import datetime, timedelta
 from unittest import mock
@@ -739,6 +740,13 @@ class ManagerTest(SimpleTestCase):
     def test_manager_method_attributes(self):
         self.assertEqual(Article.objects.get.__doc__, models.QuerySet.get.__doc__)
         self.assertEqual(Article.objects.count.__name__, models.QuerySet.count.__name__)
+
+    def test_manager_method_signature(self):
+        self.assertEqual(
+            str(inspect.signature(Article.objects.bulk_create)),
+            "(objs, batch_size=None, ignore_conflicts=False, update_conflicts=False, "
+            "update_fields=None, unique_fields=None)",
+        )
 
 
 class SelectOnSaveTests(TestCase):
