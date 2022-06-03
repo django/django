00639dbd6d023aaba20efa5497988cd730923633
Adam Johnson <me@adamj.eu>
2022-06-03 12:04:59 +0100

    Refs #33765 -- Added tests for trailing whitespace in JavaScript source map references.

--- tests/staticfiles_tests/test_storage.py
+++ tests/staticfiles_tests/test_storage.py
@@ -280,6 +280,20 @@ class TestHashedFiles:
             )
         self.assertPostCondition()
 
+    def test_js_source_map_trailing_whitespace(self):
+        relpath = self.hashed_file_path("cached/source_map_trailing_whitespace.js")
+        self.assertEqual(
+            relpath, "cached/source_map_trailing_whitespace.cd45b8534a87.js"
+        )
+        with storage.staticfiles_storage.open(relpath) as relfile:
+            content = relfile.read()
+            self.assertNotIn(b"//# sourceMappingURL=source_map.js.map\t ", content)
+            self.assertIn(
+                b"//# sourceMappingURL=source_map.js.99914b932bd3.map",
+                content,
+            )
+        self.assertPostCondition()
+
     def test_js_source_map_sensitive(self):
         relpath = self.hashed_file_path("cached/source_map_sensitive.js")
         self.assertEqual(relpath, "cached/source_map_sensitive.5da96fdd3cb3.js")
