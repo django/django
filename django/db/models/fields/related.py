7fe15b4bbdca7624a980e7ca498f61ada9dcb8c0
Allen Jonathan David <allenajdjonathan@gmail.com>
2022-06-23 12:02:53 +0530

    Fixed #29799 -- Allowed registering lookups per field instances.
    Thanks Simon Charette and Mariusz Felisiak for reviews and mentoring
    this Google Summer of Code 2022 project.

--- django/db/models/fields/related.py
+++ django/db/models/fields/related.py
@@ -858,7 +858,7 @@ class ForeignObject(RelatedField):
 
     @classmethod
     @functools.lru_cache(maxsize=None)
-    def get_lookups(cls):
+    def get_class_lookups(cls):
         bases = inspect.getmro(cls)
         bases = bases[: bases.index(ForeignObject) + 1]
         class_lookups = [parent.__dict__.get("class_lookups", {}) for parent in bases]
