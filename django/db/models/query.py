14f53c5fd2f31593fcc9a775281fae2d23d225f6
DhruvaPatil98 <dhruvapatil98@gmail.com>
2022-06-23 12:32:41 +0530

    Fixed #33799, Refs #31685 -- Added parameters for updating conflicts to QuerySeta.abulk_create().

--- django/db/models/query.py
+++ django/db/models/query.py
@@ -834,11 +834,22 @@ class QuerySet:
 
         return objs
 
-    async def abulk_create(self, objs, batch_size=None, ignore_conflicts=False):
+    async def abulk_create(
+        self,
+        objs,
+        batch_size=None,
+        ignore_conflicts=False,
+        update_conflicts=False,
+        update_fields=None,
+        unique_fields=None,
+    ):
         return await sync_to_async(self.bulk_create)(
             objs=objs,
             batch_size=batch_size,
             ignore_conflicts=ignore_conflicts,
+            update_conflicts=update_conflicts,
+            update_fields=update_fields,
+            unique_fields=unique_fields,
         )
 
     def bulk_update(self, objs, fields, batch_size=None):
