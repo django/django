b99b68a7402d684f457c636efda5dd3119d40623
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2022-06-24 07:34:24 +0200

    Refs #32786 -- Made query clear ordering when ordered combined queryset is used in subquery on Oracle.

--- django/db/models/sql/query.py
+++ django/db/models/sql/query.py
@@ -1178,6 +1178,8 @@ class Query(BaseExpression):
             and not connection.features.ignores_unnecessary_order_by_in_subqueries
         ):
             self.clear_ordering(force=False)
+            for query in self.combined_queries:
+                query.clear_ordering(force=False)
         sql, params = self.get_compiler(connection=connection).as_sql()
         if self.subquery:
             sql = "(%s)" % sql
