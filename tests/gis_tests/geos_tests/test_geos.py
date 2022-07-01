acdc788a785b637f9aca5961c7ec4c7f275e072a
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2022-07-01 15:42:59 +0200

    Fixed GEOSTest.test_emptyCollections() on GEOS 3.8.0.
    It's a regression in GEOS 3.8.0 fixed in GEOS 3.8.1.

--- tests/gis_tests/geos_tests/test_geos.py
+++ tests/gis_tests/geos_tests/test_geos.py
@@ -1127,8 +1127,10 @@ class GEOSTest(SimpleTestCase, TestDataMixin):
 
             # Testing __getitem__ (doesn't work on Point or Polygon)
             if isinstance(g, Point):
-                with self.assertRaises(IndexError):
-                    g.x
+                # IndexError is not raised in GEOS 3.8.0.
+                if geos_version_tuple() != (3, 8, 0):
+                    with self.assertRaises(IndexError):
+                        g.x
             elif isinstance(g, Polygon):
                 lr = g.shell
                 self.assertEqual("LINEARRING EMPTY", lr.wkt)
