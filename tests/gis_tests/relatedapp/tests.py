22916c8c1f9648931344c7f77fe9f71069dc5765
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2022-07-05 19:05:03 +0200

    [3.2.x] Fixed RelatedGeoModelTest.test08_defer_only() on MySQL 8+ with MyISAM storage engine.
    Backport of 73766c118781a7f7052bf0a5fbee38b944964e31 from main.

--- tests/gis_tests/relatedapp/tests.py
+++ tests/gis_tests/relatedapp/tests.py
@@ -189,8 +189,8 @@ class RelatedGeoModelTest(TestCase):
 
     def test08_defer_only(self):
         "Testing defer() and only() on Geographic models."
-        qs = Location.objects.all()
-        def_qs = Location.objects.defer('point')
+        qs = Location.objects.all().order_by("pk")
+        def_qs = Location.objects.defer("point").order_by("pk")
         for loc, def_loc in zip(qs, def_qs):
             self.assertEqual(loc.point, def_loc.point)
 
