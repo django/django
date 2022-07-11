77330a089cdf33533a387225c01736ab353b20fe
Ikko Ashimine <eltociear@gmail.com>
2022-07-11 15:25:04 +0900

    Fixed typo in tests/sitemaps_tests/test_http.py.

--- tests/sitemaps_tests/test_http.py
+++ tests/sitemaps_tests/test_http.py
@@ -225,7 +225,7 @@ class HTTPSitemapTests(SitemapTestsBase):
 
     def test_sitemap_get_latest_lastmod_none(self):
         """
-        sitemapindex.lastmod is ommitted when Sitemap.lastmod is
+        sitemapindex.lastmod is omitted when Sitemap.lastmod is
         callable and Sitemap.get_latest_lastmod is not implemented
         """
         response = self.client.get("/lastmod/get-latest-lastmod-none-sitemap.xml")
