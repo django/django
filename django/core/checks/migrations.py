django/django

commit 41019e48bbf082c985e6ba3bad34d118b903bff1

Refs #27236 -- Added generic mechanism to handle the deprecation of migration operations.

django/core/checks/migrations.py
@@ -1,0 +1,12 @@
+from django.core.checks import Tags, register
+
+
+@register(Tags.migrations)
+def check_migration_operations(**kwargs):
+    from django.db.migrations.loader import MigrationLoader
+
+    errors = []
+    loader = MigrationLoader(None, ignore_no_migrations=True)
+    for migration in loader.disk_migrations.values():
+        errors.extend(migration.check())
+    return errors

tests/check_framework/test_migrations.py
@@ -1,0 +1,101 @@
+from django.core import checks
+from django.db import migrations
+from django.db.migrations.operations.base import Operation
+from django.test import TestCase
+
+
+class DeprecatedMigrationOperationTests(TestCase):
+    def test_default_operation(self):
+        class MyOperation(Operation):
+            system_check_deprecated_details = {}
+
+        my_operation = MyOperation()
+
+        class Migration(migrations.Migration):
+            operations = [my_operation]
+
+        self.assertEqual(
+            Migration("name", "app_label").check(),
+            [
+                checks.Warning(
+                    msg="MyOperation has been deprecated.",
+                    obj=my_operation,
+                    id="migrations.WXXX",
+                )
+            ],
+        )
+
+    def test_user_specified_details(self):
+        class MyOperation(Operation):
+            system_check_deprecated_details = {
+                "msg": "This operation is deprecated and will be removed soon.",
+                "hint": "Use something else.",
+                "id": "migrations.W999",
+            }
+
+        my_operation = MyOperation()
+
+        class Migration(migrations.Migration):
+            operations = [my_operation]
+
+        self.assertEqual(
+            Migration("name", "app_label").check(),
+            [
+                checks.Warning(
+                    msg="This operation is deprecated and will be removed soon.",
+                    obj=my_operation,
+                    hint="Use something else.",
+                    id="migrations.W999",
+                )
+            ],
+        )
+
+
+class RemovedMigrationOperationTests(TestCase):
+    def test_default_operation(self):
+        class MyOperation(Operation):
+            system_check_removed_details = {}
+
+        my_operation = MyOperation()
+
+        class Migration(migrations.Migration):
+            operations = [my_operation]
+
+        self.assertEqual(
+            Migration("name", "app_label").check(),
+            [
+                checks.Error(
+                    msg=(
+                        "MyOperation has been removed except for support in historical "
+                        "migrations."
+                    ),
+                    obj=my_operation,
+                    id="migrations.EXXX",
+                )
+            ],
+        )
+
+    def test_user_specified_details(self):
+        class MyOperation(Operation):
+            system_check_removed_details = {
+                "msg": "Support for this operation is gone.",
+                "hint": "Use something else.",
+                "id": "migrations.E999",
+            }
+
+        my_operation = MyOperation()
+
+        class Migration(migrations.Migration):
+            operations = [my_operation]
+
+        self.assertEqual(
+            Migration("name", "app_label").check(),
+            [
+                checks.Error(
+                    msg="Support for this operation is gone.",
+                    obj=my_operation,
+                    hint="Use something else.",
+                    id="migrations.E999",
+                )
+            ],
+        )

django/core/checks/__init__.py
@@ -19,6 +19,7 @@ import django.core.checks.caches  # NOQA isort:skip
 import django.core.checks.compatibility.django_4_0  # NOQA isort:skip
 import django.core.checks.database  # NOQA isort:skip
 import django.core.checks.files  # NOQA isort:skip
+import django.core.checks.migrations  # NOQA isort:skip
 import django.core.checks.model_checks  # NOQA isort:skip
 import django.core.checks.security.base  # NOQA isort:skip
 import django.core.checks.security.csrf  # NOQA isort:skip

django/core/checks/registry.py
@@ -15,6 +15,7 @@ class Tags:
     compatibility = "compatibility"
     database = "database"
     files = "files"
+    migrations = "migrations"
     models = "models"
     security = "security"
     signals = "signals"

django/db/migrations/migration.py
@@ -219,6 +219,12 @@ class Migration:
             name = new_name
         return name
 
+    def check(self):
+        errors = []
+        for operation in self.operations:
+            errors.extend(operation.check_deprecation_details())
+        return errors
+
 
 class SwappableTuple(tuple):
     """

django/db/migrations/operations/base.py
@@ -1,7 +1,8 @@
 from django.db import router
+from django.utils.deprecation import DeprecationForHistoricalMigrationMixin
 
 
-class Operation:
+class Operation(DeprecationForHistoricalMigrationMixin):
     """
     Base class for migration operations.
 
@@ -32,6 +33,8 @@ class Operation:
     elidable = False
 
     serialization_expand_args = []
+
+    check_type = "migrations"
 
     def __new__(cls, *args, **kwargs):
         # We capture the arguments to make returning them trivial

tests/db_functions/migrations/0001_setup_extensions.py
@@ -1,11 +1,22 @@
-from unittest import mock
-
 from django.db import migrations
+from django.db.migrations.operations.base import Operation
+
+
+class DummyOperation(Operation):
+    def state_forwards(self, app_label, state):
+        pass
+
+    def database_forwards(self, app_label, schema_editor, from_state, to_state):
+        pass
+
+    def database_backwards(self, app_label, schema_editor, from_state, to_state):
+        pass
+
 
 try:
     from django.contrib.postgres.operations import CryptoExtension
 except ImportError:
-    CryptoExtension = mock.Mock()
+    CryptoExtension = DummyOperation
 
 
 class Migration(migrations.Migration):

tests/postgres_tests/migrations/0001_setup_extensions.py
@@ -1,6 +1,17 @@
-from unittest import mock
-
 from django.db import connection, migrations
+from django.db.migrations.operations.base import Operation
+
+
+class DummyOperation(Operation):
+    def state_forwards(self, app_label, state):
+        pass
+
+    def database_forwards(self, app_label, schema_editor, from_state, to_state):
+        pass
+
+    def database_backwards(self, app_label, schema_editor, from_state, to_state):
+        pass
+
 
 try:
     from django.contrib.postgres.operations import (
@@ -15,14 +26,14 @@ try:
         UnaccentExtension,
     )
 except ImportError:
-    BloomExtension = mock.Mock()
-    BtreeGinExtension = mock.Mock()
-    BtreeGistExtension = mock.Mock()
-    CITextExtension = mock.Mock()
-    CreateExtension = mock.Mock()
-    HStoreExtension = mock.Mock()
-    TrigramExtension = mock.Mock()
-    UnaccentExtension = mock.Mock()
+    BloomExtension = DummyOperation
+    BtreeGinExtension = DummyOperation
+    BtreeGistExtension = DummyOperation
+    CITextExtension = DummyOperation
+    CreateExtension = DummyOperation
+    HStoreExtension = DummyOperation
+    TrigramExtension = DummyOperation
+    UnaccentExtension = DummyOperation
     needs_crypto_extension = False
 else:
     needs_crypto_extension = (
@@ -41,7 +52,7 @@ class Migration(migrations.Migration):
         # dash in its name.
         CreateExtension("uuid-ossp"),
         # CryptoExtension is required for RandomUUID() on PostgreSQL < 13.
-        CryptoExtension() if needs_crypto_extension else mock.Mock(),
+        CryptoExtension() if needs_crypto_extension else DummyOperation(),
         HStoreExtension(),
         TrigramExtension(),
         UnaccentExtension(),

commit 0ea61724f1549b6d21c7a54cb0407c313ace140a

Refs #27236 -- Made operation deprecation checks run only on unapplied migrations.
Thanks Collin Anderson for the report.

Regression in 41019e48bbf082c985e6ba3bad34d118b903bff1.

django/core/checks/migrations.py
@@ -1,12 +1,19 @@
 from django.core.checks import Tags, register
+from django.db import connections
 
 
 @register(Tags.migrations)
-def check_migration_operations(**kwargs):
-    from django.db.migrations.loader import MigrationLoader
+def check_migration_operations(databases=None, **kwargs):
+    from django.db.migrations.executor import MigrationExecutor
+
+    if databases is None:
+        return []
 
     errors = []
-    loader = MigrationLoader(None, ignore_no_migrations=True)
-    for migration in loader.disk_migrations.values():
-        errors.extend(migration.check())
+    for alias in databases:
+        connection = connections[alias]
+        executor = MigrationExecutor(connection)
+        targets = executor.loader.graph.leaf_nodes()
+        for migration, _ in executor.migration_plan(targets):
+            errors.extend(migration.check())
     return errors

tests/check_framework/test_migrations.py
@@ -2,7 +2,8 @@ from unittest.mock import ANY
 
 from django.core import checks
 from django.core.checks.migrations import check_migration_operations
-from django.db import migrations
+from django.db import connections, migrations
+from django.db.migrations.executor import MigrationExecutor
 from django.db.migrations.operations.base import Operation
 from django.test import TestCase
 from django.test.utils import override_settings
@@ -58,7 +59,8 @@ class DeprecatedMigrationOperationTests(TestCase):
         INSTALLED_APPS=["check_framework.migrations_test_apps.index_together_app"]
     )
     def tests_check_alter_index_together(self):
-        errors = check_migration_operations()
+        self.assertEqual(check_migration_operations(), [])
+        errors = check_migration_operations(databases={"default"})
         self.assertEqual(
             errors,
             [
@@ -70,6 +72,10 @@ class DeprecatedMigrationOperationTests(TestCase):
                 )
             ],
         )
+        # Mark migration as applied.
+        executor = MigrationExecutor(connections["default"])
+        executor.recorder.record_applied("index_together_app", "0001_initial")
+        self.assertEqual(check_migration_operations(databases={"default"}), [])
 
 
 class RemovedMigrationOperationTests(TestCase):