fbd5adec71d7b19bf09442397925afb1041f4808
John Mathews <johnmatthews4@wayfair.com>
2022-07-24 00:06:08 +0200

    cast return value as string to remove i18n bug

--- django/contrib/sitemaps/__init__.py
+++ django/contrib/sitemaps/__init__.py
@@ -114,7 +114,7 @@ class Sitemap:
             obj, lang_code = item
             # Activate language from item-tuple or forced one before calling location.
             with translation.override(force_lang_code or lang_code):
-                return self._get("location", item)
+                return str(self._get("location", item))
         return self._get("location", item)
 
     @property
