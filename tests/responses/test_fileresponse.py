b7d9529cbe0af4adabb6ea5d01ed8dcce3668fb3
Carlton Gibson <carlton.gibson@noumenal.es>
2022-07-27 10:27:42 +0200

    [4.0.x] Fixed CVE-2022-36359 -- Escaped filename in Content-Disposition header.
    Thanks to Motoyasu Saburi for the report.

--- tests/responses/test_fileresponse.py
+++ tests/responses/test_fileresponse.py
@@ -101,3 +101,38 @@ class FileResponseTests(SimpleTestCase):
             repr(response),
             '<FileResponse status_code=200, "application/octet-stream">',
         )
+
+    def test_content_disposition_escaping(self):
+        # fmt: off
+        tests = [
+            (
+                'multi-part-one";\" dummy".txt',
+                r"multi-part-one\";\" dummy\".txt"
+            ),
+        ]
+        # fmt: on
+        # Non-escape sequence backslashes are path segments on Windows, and are
+        # eliminated by an os.path.basename() check in FileResponse.
+        if sys.platform != "win32":
+            # fmt: off
+            tests += [
+                (
+                    'multi-part-one\\";\" dummy".txt',
+                    r"multi-part-one\\\";\" dummy\".txt"
+                ),
+                (
+                    'multi-part-one\\";\\\" dummy".txt',
+                    r"multi-part-one\\\";\\\" dummy\".txt"
+                )
+            ]
+            # fmt: on
+        for filename, escaped in tests:
+            with self.subTest(filename=filename, escaped=escaped):
+                response = FileResponse(
+                    io.BytesIO(b"binary content"), filename=filename, as_attachment=True
+                )
+                response.close()
+                self.assertEqual(
+                    response.headers["Content-Disposition"],
+                    f'attachment; filename="{escaped}"',
+                )
