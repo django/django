b1dbce326b91009ec2077f70b0857d29ab7222d0
Willem Van Onsem <vanonsem.willem@gmail.com>
2022-07-28 13:41:45 +0200

    rewrite the _get_base_actions method with list comprehension

--- django/contrib/admin/options.py
+++ django/contrib/admin/options.py
@@ -976,17 +976,18 @@ class ModelAdmin(BaseModelAdmin):
     def _get_base_actions(self):
         """Return the list of actions, prior to any request-based filtering."""
         actions = []
-        base_actions = (self.get_action(action) for action in self.actions or [])
+        base_actions = list(
+            filter(None, (self.get_action(action) for action in self.actions or []))
+        )
         # get_action might have returned None, so filter any of those out.
-        base_actions = [action for action in base_actions if action]
         base_action_names = {name for _, name, _ in base_actions}
 
         # Gather actions from the admin site first
-        for (name, func) in self.admin_site.actions:
-            if name in base_action_names:
-                continue
-            description = self._get_action_description(func, name)
-            actions.append((func, name, description))
+        actions = [
+            (func, name, self._get_action_description(func, name))
+            for name, func in self.admin_site.actions
+            if name not in base_action_names
+        ]
         # Add actions from this ModelAdmin.
         actions.extend(base_actions)
         return actions
