e01a5bb1610223a50364142664745cf4aa1bbee6
David Wobrock <david.wobrock@gmail.com>
2022-08-07 17:25:43 +0200

    Fixed #33895 -- Fixed Case() crash with filters that match everything in When().
    Thanks shukryzablah for the report.

--- tests/expressions_case/tests.py
+++ tests/expressions_case/tests.py
@@ -415,6 +415,16 @@ class CaseExpressionTests(TestCase):
         self.assertEqual(len(objects), CaseTestModel.objects.count())
         self.assertTrue(all(obj.selected == "not selected" for obj in objects))
 
+    def test_annotate_with_full_when(self):
+        objects = CaseTestModel.objects.annotate(
+            selected=Case(
+                When(~Q(pk__in=[]), then=Value("selected")),
+                default=Value("not selected"),
+            )
+        )
+        self.assertEqual(len(objects), CaseTestModel.objects.count())
+        self.assertTrue(all(obj.selected == "selected" for obj in objects))
+
     def test_combined_expression(self):
         self.assertQuerysetEqual(
             CaseTestModel.objects.annotate(
