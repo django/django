bed2064fe1680750abb1f6fecc69246fa512bc4f
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2022-08-08 12:02:35 +0200

    Fixed #33902 -- Fixed Meta.constraints validation crash with F() expressions.
    Thanks Adam Zahradník for the report.
    
    Bug in 667105877e6723c6985399803a364848891513cc.

--- tests/constraints/tests.py
+++ tests/constraints/tests.py
@@ -685,6 +685,20 @@ class UniqueConstraintTests(TestCase):
             exclude={"color"},
         )
 
+    def test_validate_expression_str(self):
+        constraint = models.UniqueConstraint("name", name="name_uniq")
+        msg = "Constraint “name_uniq” is violated."
+        with self.assertRaisesMessage(ValidationError, msg):
+            constraint.validate(
+                UniqueConstraintProduct,
+                UniqueConstraintProduct(name=self.p1.name),
+            )
+        constraint.validate(
+            UniqueConstraintProduct,
+            UniqueConstraintProduct(name=self.p1.name),
+            exclude={"name"},
+        )
+
     def test_name(self):
         constraints = get_constraints(UniqueConstraintProduct._meta.db_table)
         expected_name = "name_color_uniq"
