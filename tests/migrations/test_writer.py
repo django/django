96f28cbb5a02cbebe69783f621b5df7093d6e1f5
Willem Van Onsem <vanonsem.willem@gmail.com>
2022-08-20 22:42:18 +0200

    resolve PR comments

--- tests/migrations/test_writer.py
+++ tests/migrations/test_writer.py
@@ -349,7 +349,8 @@ class WriterTests(SimpleTestCase):
         self.assertSerializedResultEqual(
             IntFlagEnum.A | IntFlagEnum.B,
             (
-                "migrations.test_writer.IntFlagEnum['B'] | migrations.test_writer.IntFlagEnum['A']",
+                "migrations.test_writer.IntFlagEnum['B'] | "
+                "migrations.test_writer.IntFlagEnum['A']",
                 {"import migrations.test_writer"},
             ),
         )
