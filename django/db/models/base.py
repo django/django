9e4d3e26b2e8b72f90c43d780b99b92f889f8bce
François Granade <github@farialima.net>
2022-08-21 20:43:42 +0200

    undoing part of the commit which is not necessary (and even dangerous)

--- django/db/models/base.py
+++ django/db/models/base.py
@@ -2458,9 +2458,7 @@ class Model(metaclass=ModelBase):
 def method_set_order(self, ordered_obj, id_list, using=None):
     order_wrt = ordered_obj._meta.order_with_respect_to
     filter_args = order_wrt.get_forward_related_filter(self)
-    ordered_obj.objects.db_manager(using or self._state.db).filter(
-        **filter_args
-    ).bulk_update(
+    ordered_obj.objects.db_manager(using).filter(**filter_args).bulk_update(
         [ordered_obj(pk=pk, _order=order) for order, pk in enumerate(id_list)],
         ["_order"],
     )
