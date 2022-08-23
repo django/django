4cd6f8ef9edc54fd6e5212e0def8e788436e3751
mr_destructive <gormeet711@gmail.com>
2022-08-23 11:24:41 +0530

    auto add applabel to installed_apps settings

--- django/core/management/commands/startapp.py
+++ django/core/management/commands/startapp.py
@@ -11,4 +11,15 @@ class Command(TemplateCommand):
     def handle(self, **options):
         app_name = options.pop("name")
         target = options.pop("directory")
+        import re
+        import os
+        settings_file = os.environ['DJANGO_SETTINGS_MODULE'].replace(".", "/") + ".py"
+        with open(settings_file, 'r+') as f:
+            s = f.read()
+            app_list = re.search(r"INSTALLED_APPS.*?]", s, re.DOTALL)[0]
+            app_new_list = app_list.replace('\n]', '') + '\n    "' + app_name + '",\n]'
+            f.truncate(0)
+            f.seek(0)
+            settings_content = s.replace(app_list, app_new_list)
+            f.write(settings_content)
         super().handle("app", app_name, target, **options)
