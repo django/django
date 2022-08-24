61f7a4a89773fe6a776e89f9b1765ab13d31348c
Rob Sheahan <rsheahan27@gmail.com>
2022-08-24 01:31:25 +0000

    test2

---
+++ Production/Production/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Production project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Production.settings')
+
+application = get_asgi_application()
