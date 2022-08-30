4b744295fc6d7bc52e00d430e64a6d98a3cf4602
Marcelo Galigniana <marcelogaligniana@gmail.com>
2022-08-29 23:19:37 -0300

    Fixed #12090 -- Added admin actions to edit pages.

--- tests/admin_views/tests.py
+++ tests/admin_views/tests.py
@@ -4867,6 +4867,7 @@ class AdminInheritedInlinesTest(TestCase):
         response = self.client.get(reverse("admin:admin_views_persona_add"))
         names = name_re.findall(response.content)
         names.remove(b"csrfmiddlewaretoken")
+        names.remove(b"csrfmiddlewaretoken")
         # make sure we have no duplicate HTML names
         self.assertEqual(len(names), len(set(names)))
 
@@ -4903,6 +4904,7 @@ class AdminInheritedInlinesTest(TestCase):
             reverse("admin:admin_views_persona_change", args=(persona_id,))
         )
         names = name_re.findall(response.content)
+        names.remove(b"csrfmiddlewaretoken")
         names.remove(b"csrfmiddlewaretoken")
         # make sure we have no duplicate HTML names
         self.assertEqual(len(names), len(set(names)))
