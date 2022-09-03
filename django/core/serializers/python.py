f17e9931384b4c94b099eb10b18057f2525831aa
Mark Evans <mark@hellobaton.com>
2022-09-03 09:53:58 -0400

    Fixed #33937 -- Optimized serialization of related m2m fields without natural keys.

--- django/core/serializers/python.py
+++ django/core/serializers/python.py
@@ -70,14 +70,20 @@ class Serializer(base.Serializer):
                 def m2m_value(value):
                     return value.natural_key()
 
+                def queryset_iterator(obj, field):
+                    return getattr(obj, field.name).iterator()
+
             else:
 
                 def m2m_value(value):
                     return self._value_from_field(value, value._meta.pk)
 
+                def queryset_iterator(obj, field):
+                    return getattr(obj, field.name).only("pk").iterator()
+
             m2m_iter = getattr(obj, "_prefetched_objects_cache", {}).get(
                 field.name,
-                getattr(obj, field.name).iterator(),
+                queryset_iterator(obj, field),
             )
             self._current[field.name] = [m2m_value(related) for related in m2m_iter]
 
--- django/core/serializers/xml_serializer.py
+++ django/core/serializers/xml_serializer.py
@@ -146,14 +146,20 @@ class Serializer(base.Serializer):
                         self.xml.endElement("natural")
                     self.xml.endElement("object")
 
+                def queryset_iterator(obj, field):
+                    return getattr(obj, field.name).iterator()
+
             else:
 
                 def handle_m2m(value):
                     self.xml.addQuickElement("object", attrs={"pk": str(value.pk)})
 
+                def queryset_iterator(obj, field):
+                    return getattr(obj, field.name).only("pk").iterator()
+
             m2m_iter = getattr(obj, "_prefetched_objects_cache", {}).get(
                 field.name,
-                getattr(obj, field.name).iterator(),
+                queryset_iterator(obj, field),
             )
             for relobj in m2m_iter:
                 handle_m2m(relobj)
