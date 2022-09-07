django/django

commit 0e656c02fe945389246f0c08f51c6db4a0849bd2

Fixed #33342 -- Added support for using OpClass() in exclusion constraints.

tests/postgres_tests/test_constraints.py
@@ -198,6 +198,7 @@ class SchemaTests(PostgreSQLTestCase):
         Scene.objects.create(scene='ScEnE 10', setting="Sir Bedemir's Castle")
 
 
+@modify_settings(INSTALLED_APPS={'append': 'django.contrib.postgres'})
 class ExclusionConstraintTests(PostgreSQLTestCase):
     def get_constraints(self, table):
         """Get the constraints on the table using a new cursor."""
@@ -604,6 +605,24 @@ class ExclusionConstraintTests(PostgreSQLTestCase):
         )
         self._test_range_overlaps(constraint)
 
+    def test_range_overlaps_custom_opclass_expression(self):
+        class TsTzRange(Func):
+            function = 'TSTZRANGE'
+            output_field = DateTimeRangeField()
+
+        constraint = ExclusionConstraint(
+            name='exclude_overlapping_reservations_custom_opclass',
+            expressions=[
+                (
+                    OpClass(TsTzRange('start', 'end', RangeBoundary()), 'range_ops'),
+                    RangeOperators.OVERLAPS,
+                ),
+                (OpClass('room', 'gist_int4_ops'), RangeOperators.EQUAL),
+            ],
+            condition=Q(cancelled=False),
+        )
+        self._test_range_overlaps(constraint)
+
     def test_range_overlaps(self):
         constraint = ExclusionConstraint(
             name='exclude_overlapping_reservations',
@@ -913,6 +932,34 @@ class ExclusionConstraintTests(PostgreSQLTestCase):
         with connection.schema_editor() as editor:
             editor.add_constraint(RangesModel, constraint)
         self.assertIn(constraint_name, self.get_constraints(RangesModel._meta.db_table))
+
+    def test_opclass_expression(self):
+        constraint_name = 'ints_adjacent_opclass_expression'
+        self.assertNotIn(
+            constraint_name,
+            self.get_constraints(RangesModel._meta.db_table),
+        )
+        constraint = ExclusionConstraint(
+            name=constraint_name,
+            expressions=[(OpClass('ints', 'range_ops'), RangeOperators.ADJACENT_TO)],
+        )
+        with connection.schema_editor() as editor:
+            editor.add_constraint(RangesModel, constraint)
+        constraints = self.get_constraints(RangesModel._meta.db_table)
+        self.assertIn(constraint_name, constraints)
+        with editor.connection.cursor() as cursor:
+            cursor.execute(SchemaTests.get_opclass_query, [constraint_name])
+            self.assertEqual(
+                cursor.fetchall(),
+                [('range_ops', constraint_name)],
+            )
+        # Drop the constraint.
+        with connection.schema_editor() as editor:
+            editor.remove_constraint(RangesModel, constraint)
+        self.assertNotIn(
+            constraint_name,
+            self.get_constraints(RangesModel._meta.db_table),
+        )
 
     def test_range_equal_cast(self):
         constraint_name = 'exclusion_equal_room_cast'

django/contrib/postgres/constraints.py
@@ -1,11 +1,17 @@
+from django.contrib.postgres.indexes import OpClass
 from django.db import NotSupportedError
-from django.db.backends.ddl_references import Statement, Table
+from django.db.backends.ddl_references import Expressions, Statement, Table
 from django.db.models import Deferrable, F, Q
 from django.db.models.constraints import BaseConstraint
-from django.db.models.expressions import Col
+from django.db.models.expressions import ExpressionList
+from django.db.models.indexes import IndexExpression
 from django.db.models.sql import Query
 
 __all__ = ['ExclusionConstraint']
+
+
+class ExclusionConstraintExpression(IndexExpression):
+    template = '%(expressions)s WITH %(operator)s'
 
 
 class ExclusionConstraint(BaseConstraint):
@@ -63,24 +69,19 @@ class ExclusionConstraint(BaseConstraint):
         self.opclasses = opclasses
         super().__init__(name=name)
 
-    def _get_expression_sql(self, compiler, schema_editor, query):
+    def _get_expressions(self, schema_editor, query):
         expressions = []
         for idx, (expression, operator) in enumerate(self.expressions):
             if isinstance(expression, str):
                 expression = F(expression)
-            expression = expression.resolve_expression(query=query)
-            sql, params = compiler.compile(expression)
-            if not isinstance(expression, Col):
-                sql = f'({sql})'
             try:
-                opclass = self.opclasses[idx]
-                if opclass:
-                    sql = '%s %s' % (sql, opclass)
+                expression = OpClass(expression, self.opclasses[idx])
             except IndexError:
                 pass
-            sql = sql % tuple(schema_editor.quote_value(p) for p in params)
-            expressions.append('%s WITH %s' % (sql, operator))
-        return expressions
+            expression = ExclusionConstraintExpression(expression, operator=operator)
+            expression.set_wrapper_classes(schema_editor.connection)
+            expressions.append(expression)
+        return ExpressionList(*expressions).resolve_expression(query)
 
     def _get_condition_sql(self, compiler, schema_editor, query):
         if self.condition is None:
@@ -92,17 +93,20 @@ class ExclusionConstraint(BaseConstraint):
     def constraint_sql(self, model, schema_editor):
         query = Query(model, alias_cols=False)
         compiler = query.get_compiler(connection=schema_editor.connection)
-        expressions = self._get_expression_sql(compiler, schema_editor, query)
+        expressions = self._get_expressions(schema_editor, query)
+        table = model._meta.db_table
         condition = self._get_condition_sql(compiler, schema_editor, query)
         include = [model._meta.get_field(field_name).column for field_name in self.include]
-        return self.template % {
-            'name': schema_editor.quote_name(self.name),
-            'index_type': self.index_type,
-            'expressions': ', '.join(expressions),
-            'include': schema_editor._index_include_sql(model, include),
-            'where': ' WHERE (%s)' % condition if condition else '',
-            'deferrable': schema_editor._deferrable_constraint_sql(self.deferrable),
-        }
+        return Statement(
+            self.template,
+            table=Table(table, schema_editor.quote_name),
+            name=schema_editor.quote_name(self.name),
+            index_type=self.index_type,
+            expressions=Expressions(table, expressions, compiler, schema_editor.quote_value),
+            where=' WHERE (%s)' % condition if condition else '',
+            include=schema_editor._index_include_sql(model, include),
+            deferrable=schema_editor._deferrable_constraint_sql(self.deferrable),
+        )
 
     def create_sql(self, model, schema_editor):
         self.check_supported(schema_editor)

commit 19e838daa8872ee29fbea0bc471c2a6443f26835

Fixed #33982 -- Fixed migrations crash when adding model with ExclusionConstraint.
Regression in 0e656c02fe945389246f0c08f51c6db4a0849bd2.

django/db/backends/base/schema.py
@@ -269,7 +269,9 @@ class BaseDatabaseSchemaEditor:
         sql = self.sql_create_table % {
             "table": self.quote_name(model._meta.db_table),
             "definition": ", ".join(
-                constraint for constraint in (*column_sqls, *constraints) if constraint
+                str(constraint)
+                for constraint in (*column_sqls, *constraints)
+                if constraint
             ),
         }
         if model._meta.db_tablespace:

tests/postgres_tests/test_constraints.py
@@ -10,12 +10,14 @@ from django.db.models import (
     F,
     Func,
     IntegerField,
+    Model,
     Q,
     UniqueConstraint,
 )
 from django.db.models.fields.json import KeyTextTransform
 from django.db.models.functions import Cast, Left, Lower
 from django.test import ignore_warnings, modify_settings, skipUnlessDBFeature
+from django.test.utils import isolate_apps
 from django.utils import timezone
 from django.utils.deprecation import RemovedInDjango50Warning
 
@@ -1150,6 +1152,29 @@ class ExclusionConstraintTests(PostgreSQLTestCase):
         with connection.schema_editor() as editor:
             editor.add_constraint(Room, constraint)
         self.assertIn(constraint_name, self.get_constraints(Room._meta.db_table))
+
+    @isolate_apps("postgres_tests")
+    def test_table_create(self):
+        constraint_name = "exclusion_equal_number_tc"
+
+        class ModelWithExclusionConstraint(Model):
+            number = IntegerField()
+
+            class Meta:
+                app_label = "postgres_tests"
+                constraints = [
+                    ExclusionConstraint(
+                        name=constraint_name,
+                        expressions=[("number", RangeOperators.EQUAL)],
+                    )
+                ]
+
+        with connection.schema_editor() as editor:
+            editor.create_model(ModelWithExclusionConstraint)
+        self.assertIn(
+            constraint_name,
+            self.get_constraints(ModelWithExclusionConstraint._meta.db_table),
+        )
 
 
 @modify_settings(INSTALLED_APPS={"append": "django.contrib.postgres"})