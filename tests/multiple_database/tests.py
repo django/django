a52bdea5a27ba44b13eda93642231c65c581e083
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2022-09-13 19:00:57 +0530

    Fixed #33985 -- Used app_config.verbose_name in ContentType.__str__().

--- tests/multiple_database/tests.py
+++ tests/multiple_database/tests.py
@@ -1118,7 +1118,7 @@ class QueryTestCase(TestCase):
 
         # Set a foreign key with an object from a different database
         msg = (
-            'Cannot assign "<ContentType: multiple_database | book>": the '
+            'Cannot assign "<ContentType: Multiple_Database | book>": the '
             "current database router prevents this relation."
         )
         with self.assertRaisesMessage(ValueError, msg):
