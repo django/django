7d74520c82c08891b70b161fbc1470aa7f95a2e4
Clouds Flowing <clouds.flowing@gmail.com>
2022-09-18 07:23:08 +0800

    add resolve columns

--- django/db/models/fields/composite.py
+++ django/db/models/fields/composite.py
@@ -1,5 +1,6 @@
 from django.db.models.fields import Field
 from django.db.models.query_utils import DeferredAttribute
+from django.db.models.signals import class_prepared
 
 class CompositeType:
     _composite_type = None
@@ -62,7 +63,7 @@ class CompositeDeferredAttribute(DeferredAttribute):
     def _bind(self, instance, cls):
         if issubclass(cls, CompositeType):
             return cls(instance)
-        vals = [getattr(instance, name) for name in self.field.names]
+        vals = [getattr(instance, name) for name in self.field.component_names]
         return cls(*vals)
     def __get__(self, instance, cls=None):
         if instance is None:
@@ -76,39 +77,49 @@ class CompositeDeferredAttribute(DeferredAttribute):
     def __set__(self, instance, value):
         # logger.info(f"set field {self.field} of {instance.__class__.__name__} to {value}")
         data = instance.__dict__
-        for name, val in zip(self.field.names, value):
+        for name, val in zip(self.field.component_names, value):
             # logger.info(f"set field {name} of {instance.__class__.__name__} to {val}")
             data[name] = val
 
 class CompositeField(Field):
-    names = None
+    component_names = None
+    composite_columns = None
     python_type = tuple
 
     descriptor_class = CompositeDeferredAttribute
     def __init__(self, *names, **kwargs):
-        kwargs['auto_created'] = True
         kwargs['db_column'] = None
         kwargs['editable'] = False
         super().__init__(**kwargs)
-        self.names = names
-        # self.columns = None
+        self.component_names = tuple(names)
+        # this would deferred to post_init
+        self.component_fields = None
 
     def get_attname_column(self):
         return self.get_attname(), self.db_column
 
     def contribute_to_class(self, cls, name, private_only=False) -> None:
         # logger.info(f"{cls.__name__} {name} {cls._meta} {self}")
-        self.python_type = CompositeType.namedtuple(f"{cls.__name__}__{name}", list(self.names))
+        self.python_type = CompositeType.namedtuple(f"{cls.__name__}__{name}", self.component_names)
         result = super().contribute_to_class(cls, name, private_only)
         setattr(cls, self.attname, self.descriptor_class(self))
 
         if self.primary_key:
             cls._meta.pk = self
         if self.unique or self.primary_key:
-            cls._meta.unique_together = (*cls._meta.unique_together, tuple(self.names))
+            cls._meta.unique_together = (*cls._meta.unique_together, self.component_names)
 
             # logger.info(f"{cls.__name__}: unique {cls._meta.unique_together}")
         if self.db_index:
-            cls._meta.index_together = (*cls._meta.index_together, tuple(self.names))
+            cls._meta.index_together = (*cls._meta.index_together, self.component_names)
 
         return result
+
+def resolve_columns(*args, **kwargs):
+    cls = kwargs.pop('sender')
+    for field in cls._meta.local_fields:
+        # TODO: is local_fields enough?
+        if isinstance(field, CompositeField):
+            field.component_fields = tuple(cls._meta.get_field(name) for name in field.component_names)
+
+class_prepared.connect(resolve_columns)
