33d9247c8b8be89518158c19b48a85a8c2d649c5
David Sanders <shang.xiao.sanders@gmail.com>
2022-09-21 22:56:22 +1000

    [4.1.x] Fixed #34025 -- Fixed selecting ModelAdmin.autocomplete_fields after adding/changing related instances via popups.
    Regression in c72f6f36c13a21f6db3d4f85d2d3cec87bad45e6.
    
    Thanks Alexandre da Silva for the report.
    
    Backport of 9976f3d4b80cfb2e6f4c998438622b78eb1ac53e from main

--- tests/admin_views/tests.py
+++ tests/admin_views/tests.py
@@ -6292,17 +6292,23 @@ class SeleniumTests(AdminSeleniumTestCase):
         finally:
             self.selenium.set_window_size(current_size["width"], current_size["height"])
 
-    def test_updating_related_objects_updates_fk_selects(self):
+    def test_updating_related_objects_updates_fk_selects_except_autocompletes(self):
         from selenium.webdriver.common.by import By
         from selenium.webdriver.support.ui import Select
 
         born_country_select_id = "id_born_country"
         living_country_select_id = "id_living_country"
+        living_country_select2_textbox_id = "select2-id_living_country-container"
         favorite_country_to_vacation_select_id = "id_favorite_country_to_vacation"
         continent_select_id = "id_continent"
 
         def _get_HTML_inside_element_by_id(id_):
             return self.selenium.find_element(By.ID, id_).get_attribute("innerHTML")
+
+        def _get_text_inside_element_by_selector(selector):
+            return self.selenium.find_element(By.CSS_SELECTOR, selector).get_attribute(
+                "innerText"
+            )
 
         self.admin_login(
             username="super", password="secret", login_url=reverse("admin:index")
@@ -6328,12 +6334,16 @@ class SeleniumTests(AdminSeleniumTestCase):
             <option value="1" selected="">Argentina</option>
             """,
         )
-        self.assertHTMLEqual(
-            _get_HTML_inside_element_by_id(living_country_select_id),
-            """
-            <option value="" selected="">---------</option>
-            <option value="1">Argentina</option>
-            """,
+        # Argentina isn't added to the living_country select nor selected by
+        # the select2 widget.
+        self.assertEqual(
+            _get_text_inside_element_by_selector(f"#{living_country_select_id}"), ""
+        )
+        self.assertEqual(
+            _get_text_inside_element_by_selector(
+                f"#{living_country_select2_textbox_id}"
+            ),
+            "",
         )
         # Argentina won't appear because favorite_country_to_vacation field has
         # limit_choices_to.
@@ -6361,13 +6371,18 @@ class SeleniumTests(AdminSeleniumTestCase):
             <option value="2">Spain</option>
             """,
         )
-        self.assertHTMLEqual(
-            _get_HTML_inside_element_by_id(living_country_select_id),
-            """
-            <option value="" selected="">---------</option>
-            <option value="1">Argentina</option>
-            <option value="2" selected="">Spain</option>
-            """,
+
+        # Spain is added to the living_country select and it's also selected by
+        # the select2 widget.
+        self.assertEqual(
+            _get_text_inside_element_by_selector(f"#{living_country_select_id} option"),
+            "Spain",
+        )
+        self.assertEqual(
+            _get_text_inside_element_by_selector(
+                f"#{living_country_select2_textbox_id}"
+            ),
+            "Spain",
         )
         # Spain won't appear because favorite_country_to_vacation field has
         # limit_choices_to.
@@ -6397,13 +6412,17 @@ class SeleniumTests(AdminSeleniumTestCase):
             <option value="2">Italy</option>
             """,
         )
-        self.assertHTMLEqual(
-            _get_HTML_inside_element_by_id(living_country_select_id),
-            """
-            <option value="" selected="">---------</option>
-            <option value="1">Argentina</option>
-            <option value="2" selected="">Italy</option>
-            """,
+        # Italy is added to the living_country select and it's also selected by
+        # the select2 widget.
+        self.assertEqual(
+            _get_text_inside_element_by_selector(f"#{living_country_select_id} option"),
+            "Italy",
+        )
+        self.assertEqual(
+            _get_text_inside_element_by_selector(
+                f"#{living_country_select2_textbox_id}"
+            ),
+            "Italy",
         )
         # favorite_country_to_vacation field has no options.
         self.assertHTMLEqual(
