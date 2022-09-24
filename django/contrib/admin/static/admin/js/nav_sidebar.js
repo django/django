fe539d64cf7fed8e9e3615aa70b5bbc832264ff5
Rasmus Magnell <rasmus@personalkollen.se>
2022-09-24 12:21:11 +0200

    Fixed #34042 -- Improved accessibility of admin's navigation sidebar.

--- django/contrib/admin/static/admin/js/nav_sidebar.js
+++ django/contrib/admin/static/admin/js/nav_sidebar.js
@@ -2,47 +2,24 @@
 {
     const toggleNavSidebar = document.getElementById('toggle-nav-sidebar');
     if (toggleNavSidebar !== null) {
-        const navLinks = document.querySelectorAll('#nav-sidebar a');
-        function disableNavLinkTabbing() {
-            for (const navLink of navLinks) {
-                navLink.tabIndex = -1;
-            }
-        }
-        function enableNavLinkTabbing() {
-            for (const navLink of navLinks) {
-                navLink.tabIndex = 0;
-            }
-        }
-        function disableNavFilterTabbing() {
-            document.getElementById('nav-filter').tabIndex = -1;
-        }
-        function enableNavFilterTabbing() {
-            document.getElementById('nav-filter').tabIndex = 0;
-        }
-
+        const navSidebar = document.getElementById('nav-sidebar');
         const main = document.getElementById('main');
         let navSidebarIsOpen = localStorage.getItem('django.admin.navSidebarIsOpen');
         if (navSidebarIsOpen === null) {
             navSidebarIsOpen = 'true';
         }
-        if (navSidebarIsOpen === 'false') {
-            disableNavLinkTabbing();
-            disableNavFilterTabbing();
-        }
         main.classList.toggle('shifted', navSidebarIsOpen === 'true');
+        navSidebar.setAttribute('aria-expanded', navSidebarIsOpen);
 
         toggleNavSidebar.addEventListener('click', function() {
             if (navSidebarIsOpen === 'true') {
                 navSidebarIsOpen = 'false';
-                disableNavLinkTabbing();
-                disableNavFilterTabbing();
             } else {
                 navSidebarIsOpen = 'true';
-                enableNavLinkTabbing();
-                enableNavFilterTabbing();
             }
             localStorage.setItem('django.admin.navSidebarIsOpen', navSidebarIsOpen);
             main.classList.toggle('shifted');
+            navSidebar.setAttribute('aria-expanded', navSidebarIsOpen);
         });
     }
 
