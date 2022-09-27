3b79a32b75d642c4f96393641f05470f6aa9eb0a
Paolo Melchiorre <paolo@melchiorre.org>
2022-09-27 18:34:41 +0200

    Update ASCIIUsernameValidator message
    The error message for the ASCII user name value contains a reference to the "English" language which is partial and misleading for non-English speaking users. I propose to replace the word "English" with the more correct "ASCII", unless other clearer proposals.

--- django/contrib/auth/validators.py
+++ django/contrib/auth/validators.py
@@ -9,7 +9,7 @@ from django.utils.translation import gettext_lazy as _
 class ASCIIUsernameValidator(validators.RegexValidator):
     regex = r"^[\w.@+-]+\Z"
     message = _(
-        "Enter a valid username. This value may contain only English letters, "
+        "Enter a valid username. This value may contain only ASCII letters, "
         "numbers, and @/./+/-/_ characters."
     )
     flags = re.ASCII
