8c699b6496484b23e4eb1638d89f79fa8c0adac1
moonyGopher <88974951+moonyGopher@users.noreply.github.com>
2022-09-28 13:22:26 +0200

    updated test to make use of the span tag

--- tests/forms_tests/widget_tests/test_radioselect.py
+++ tests/forms_tests/widget_tests/test_radioselect.py
@@ -17,13 +17,13 @@ class RadioSelectTest(WidgetTest):
             "J",
             html="""
             <div>
-            <div><label><input type="radio" name="beatle" value=""> ------</label></div>
+            <div><label><input type="radio" name="beatle" value=""> <span>------</span></label></div>
             <div><label>
-            <input checked type="radio" name="beatle" value="J"> John</label></div>
-            <div><label><input type="radio" name="beatle" value="P"> Paul</label></div>
+            <input checked type="radio" name="beatle" value="J"> <span>John</span></label></div>
+            <div><label><input type="radio" name="beatle" value="P"> <span>Paul</span></label></div>
             <div><label>
-            <input type="radio" name="beatle" value="G"> George</label></div>
-            <div><label><input type="radio" name="beatle" value="R"> Ringo</label></div>
+            <input type="radio" name="beatle" value="G"> <span>George</span></label></div>
+            <div><label><input type="radio" name="beatle" value="R"> <span>Ringo</span></label></div>
             </div>
         """,
         )
@@ -38,26 +38,26 @@ class RadioSelectTest(WidgetTest):
         <div id="media">
         <div>
         <label for="media_0">
-        <input type="radio" name="nestchoice" value="unknown" id="media_0"> Unknown
+        <input type="radio" name="nestchoice" value="unknown" id="media_0"> <span>Unknown</span>
         </label></div>
         <div>
-        <label>Audio</label>
+        <label><span>Audio</span></label>
         <div>
         <label for="media_1_0">
-        <input type="radio" name="nestchoice" value="vinyl" id="media_1_0"> Vinyl
+        <input type="radio" name="nestchoice" value="vinyl" id="media_1_0"> <span>Vinyl</span>
         </label></div>
         <div> <label for="media_1_1">
-        <input type="radio" name="nestchoice" value="cd" id="media_1_1"> CD
+        <input type="radio" name="nestchoice" value="cd" id="media_1_1"> <span>CD</span>
         </label></div>
         </div><div>
-        <label>Video</label>
+        <label><span>Video</span></label>
         <div>
         <label for="media_2_0">
-        <input type="radio" name="nestchoice" value="vhs" id="media_2_0"> VHS
+        <input type="radio" name="nestchoice" value="vhs" id="media_2_0"> <span>VHS</span>
         </label></div>
         <div>
         <label for="media_2_1">
-        <input type="radio" name="nestchoice" value="dvd" id="media_2_1" checked> DVD
+        <input type="radio" name="nestchoice" value="dvd" id="media_2_1" checked> <span>DVD</span>
         </label></div>
         </div>
         </div>
@@ -80,14 +80,14 @@ class RadioSelectTest(WidgetTest):
         <div id="foo">
         <div>
         <label for="foo_0">
-        <input checked type="radio" id="foo_0" value="J" name="beatle"> John</label>
+        <input checked type="radio" id="foo_0" value="J" name="beatle"> <span>John</span></label>
         </div>
         <div><label for="foo_1">
-        <input type="radio" id="foo_1" value="P" name="beatle"> Paul</label></div>
+        <input type="radio" id="foo_1" value="P" name="beatle"> <span>Paul</span></label></div>
         <div><label for="foo_2">
-        <input type="radio" id="foo_2" value="G" name="beatle"> George</label></div>
+        <input type="radio" id="foo_2" value="G" name="beatle"> <span>George</span></label></div>
         <div><label for="foo_3">
-        <input type="radio" id="foo_3" value="R" name="beatle"> Ringo</label></div>
+        <input type="radio" id="foo_3" value="R" name="beatle"> <span>Ringo</span></label></div>
         </div>
         """
         self.check_html(widget, "beatle", "J", html=html)
@@ -101,14 +101,14 @@ class RadioSelectTest(WidgetTest):
         <div id="bar">
         <div>
         <label for="bar_0">
-        <input checked type="radio" id="bar_0" value="J" name="beatle"> John</label>
+        <input checked type="radio" id="bar_0" value="J" name="beatle"> <span>John</span></label>
         </div>
         <div><label for="bar_1">
-        <input type="radio" id="bar_1" value="P" name="beatle"> Paul</label></div>
+        <input type="radio" id="bar_1" value="P" name="beatle"> <span>Paul</span></label></div>
         <div><label for="bar_2">
-        <input type="radio" id="bar_2" value="G" name="beatle"> George</label></div>
+        <input type="radio" id="bar_2" value="G" name="beatle"> <span>George</span></label></div>
         <div><label for="bar_3">
-        <input type="radio" id="bar_3" value="R" name="beatle"> Ringo</label></div>
+        <input type="radio" id="bar_3" value="R" name="beatle"> <span>Ringo</span></label></div>
         </div>
         """
         self.check_html(
@@ -127,14 +127,14 @@ class RadioSelectTest(WidgetTest):
         html = """
         <div class="bar">
         <div><label>
-        <input checked type="radio" class="bar" value="J" name="beatle"> John</label>
+        <input checked type="radio" class="bar" value="J" name="beatle"> <span>John</span></label>
         </div>
         <div><label>
-        <input type="radio" class="bar" value="P" name="beatle"> Paul</label></div>
+        <input type="radio" class="bar" value="P" name="beatle"> <span>Paul</span></label></div>
         <div><label>
-        <input type="radio" class="bar" value="G" name="beatle"> George</label></div>
+        <input type="radio" class="bar" value="G" name="beatle"> <span>George</span></label></div>
         <div><label>
-        <input type="radio" class="bar" value="R" name="beatle"> Ringo</label></div>
+        <input type="radio" class="bar" value="R" name="beatle"> <span>Ringo</span></label></div>
         </div>
         """
         self.check_html(
@@ -154,11 +154,11 @@ class RadioSelectTest(WidgetTest):
         ]
         html = """
         <div>
-        <div><label><input type="radio" name="number" value="1"> One</label></div>
+        <div><label><input type="radio" name="number" value="1"> <span>One</span></label></div>
         <div><label>
-        <input type="radio" name="number" value="1000"> One thousand</label></div>
+        <input type="radio" name="number" value="1000"> <span>One thousand</span></label></div>
         <div><label>
-        <input type="radio" name="number" value="1000000"> One million</label></div>
+        <input type="radio" name="number" value="1000000"> <span>One million</span></label></div>
         </div>
         """
         self.check_html(self.widget(choices=choices), "number", None, html=html)
@@ -170,9 +170,9 @@ class RadioSelectTest(WidgetTest):
         html = """
         <div>
         <div><label>
-        <input type="radio" name="time" value="00:00:00"> midnight</label></div>
+        <input type="radio" name="time" value="00:00:00"> <span>midnight</span></label></div>
         <div><label>
-        <input type="radio" name="time" value="12:00:00"> noon</label></div>
+        <input type="radio" name="time" value="12:00:00"> <span>noon</span></label></div>
         </div>
         """
         self.check_html(self.widget(choices=choices), "time", None, html=html)
@@ -187,15 +187,15 @@ class RadioSelectTest(WidgetTest):
             html="""
             <div>
             <div><label>
-            <input type="radio" name="beatle_0" value=""> ------</label></div>
+            <input type="radio" name="beatle_0" value=""> <span>------</span></label></div>
             <div><label>
-            <input checked type="radio" name="beatle_0" value="J"> John</label></div>
+            <input checked type="radio" name="beatle_0" value="J"> <span>John</span></label></div>
             <div><label>
-            <input type="radio" name="beatle_0" value="P"> Paul</label></div>
+            <input type="radio" name="beatle_0" value="P"> <span>Paul</span></label></div>
             <div><label>
-            <input type="radio" name="beatle_0" value="G"> George</label></div>
+            <input type="radio" name="beatle_0" value="G"> <span>George</span></label></div>
             <div><label>
-            <input type="radio" name="beatle_0" value="R"> Ringo</label></div>
+            <input type="radio" name="beatle_0" value="R"> <span>Ringo</span></label></div>
             </div>
         """,
         )
@@ -212,13 +212,13 @@ class RadioSelectTest(WidgetTest):
         self.assertHTMLEqual(
             '<div><fieldset><legend>Field:</legend><div id="id_field">'
             '<div><label for="id_field_0">'
-            '<input type="radio" name="field" value="J" id="id_field_0"> John'
+            '<input type="radio" name="field" value="J" id="id_field_0"> <span>John</span>'
             '</label></div><div><label for="id_field_1">'
-            '<input type="radio" name="field" value="P" id="id_field_1">Paul'
+            '<input type="radio" name="field" value="P" id="id_field_1"><span>Paul</span>'
             '</label></div><div><label for="id_field_2"><input type="radio" '
-            'name="field" value="G" id="id_field_2"> George</label></div>'
+            'name="field" value="G" id="id_field_2"> <span>George</span></label></div>'
             '<div><label for="id_field_3"><input type="radio" name="field" '
-            'value="R" id="id_field_3">Ringo</label></div></div></fieldset>'
+            'value="R" id="id_field_3"><span>Ringo</span></label></div></div></fieldset>'
             "</div>",
             form.render(),
         )
