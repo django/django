ecf6506f44cd09021927a821eb14e3f7f726ab14
Antoine Lorence <contact@alorence.me>
2022-09-29 12:10:02 +0200

    [4.1.x] Fixed #34062 -- Updated View.http_method_not_allowed() to support async.
    As with the options() methods, wrap the response in a coroutine if
    the view is async.
    
    Co-authored-by: Carlton Gibson <carlton.gibson@noumenal.es>
    
    Backport of 9b0c9821ed4dd9920cc7c5e7b657720d91a89bdc from main

--- django/views/generic/base.py
+++ django/views/generic/base.py
@@ -148,7 +148,16 @@ class View:
             request.path,
             extra={"status_code": 405, "request": request},
         )
-        return HttpResponseNotAllowed(self._allowed_methods())
+        response = HttpResponseNotAllowed(self._allowed_methods())
+
+        if self.view_is_async:
+
+            async def func():
+                return response
+
+            return func()
+        else:
+            return response
 
     def options(self, request, *args, **kwargs):
         """Handle responding to requests for the OPTIONS HTTP verb."""
