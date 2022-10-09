4205516b43d3adb2864fd20a4b58b675c83ac319
Ion Alberdi <ion.alberdi@pricemoov.com>
2022-10-09 11:01:14 +0200

    Fixed #34080 -- Fixed __exact lookup when nested arrays contain only NULL values.
    Thanks jerch and David Sanders for reviews.

--- tests/postgres_tests/test_array.py
+++ tests/postgres_tests/test_array.py
@@ -253,6 +253,24 @@ class TestQuerying(PostgreSQLTestCase):
             [obj],
         )
 
+    def test_exact_null_only_nested_array(self):
+        obj1 = NullableIntegerArrayModel.objects.create(field_nested=[[None, None]])
+        obj2 = NullableIntegerArrayModel.objects.create(
+            field_nested=[[None, None], [None, None]],
+        )
+        self.assertSequenceEqual(
+            NullableIntegerArrayModel.objects.filter(
+                field_nested__exact=[[None, None]],
+            ),
+            [obj1],
+        )
+        self.assertSequenceEqual(
+            NullableIntegerArrayModel.objects.filter(
+                field_nested__exact=[[None, None], [None, None]],
+            ),
+            [obj2],
+        )
+
     def test_exact_with_expression(self):
         self.assertSequenceEqual(
             NullableIntegerArrayModel.objects.filter(field__exact=[Value(1)]),
