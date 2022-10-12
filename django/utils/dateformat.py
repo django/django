e92cc2891d8aba8e9364bde1237a7b920b31ac06
Nick Pope <nick@nickpope.me.uk>
2022-10-12 21:59:05 +0100

    Refs #????? -- Migrated specifier methods onto `django.utils.datetime.Formatter`.
    Migrated specifier methods onto the base class in preparation for
    fully merging `DateFormat` and `TimeFormat` implementations.

--- django/utils/dateformat.py
+++ django/utils/dateformat.py
@@ -54,23 +54,6 @@ class Formatter:
                 pieces.append(re_escaped.sub(r"\1", piece))
         return "".join(pieces)
 
-
-class TimeFormat(Formatter):
-    def __init__(self, obj):
-        self.data = obj
-        self.timezone = None
-
-        if isinstance(obj, datetime):
-            # Timezone is only supported when formatting datetime objects, not
-            # date objects (timezone information not appropriate), or time
-            # objects (against established django policy).
-            if is_naive(obj):
-                timezone = get_default_timezone()
-            else:
-                timezone = obj.tzinfo
-            if not _datetime_ambiguous_or_imaginary(obj, timezone):
-                self.timezone = timezone
-
     def a(self):
         "'a.m.' or 'p.m.'"
         if self.data.hour > 11:
@@ -82,6 +65,25 @@ class TimeFormat(Formatter):
         if self.data.hour > 11:
             return _("PM")
         return _("AM")
+
+    def b(self):
+        "Month, textual, 3 letters, lowercase; e.g. 'jan'"
+        return MONTHS_3[self.data.month]
+
+    def c(self):
+        """
+        ISO 8601 Format
+        Example : '2008-01-02T10:30:00.000123'
+        """
+        return self.data.isoformat()
+
+    def d(self):
+        "Day of the month, 2 digits with leading zeros; i.e. '01' to '31'"
+        return f"{self.data.day:02d}"
+
+    def D(self):
+        "Day of the week, textual, 3 letters; e.g. 'Fri'"
+        return WEEKDAYS_ABBR[self.data.weekday()]
 
     def e(self):
         """
@@ -99,6 +101,10 @@ class TimeFormat(Formatter):
             pass
         return ""
 
+    def E(self):
+        "Alternative month names as required by some locales. Proprietary extension."
+        return MONTHS_ALT[self.data.month]
+
     def f(self):
         """
         Time, in 12-hour hours and minutes, with minutes left off if they're
@@ -109,6 +115,10 @@ class TimeFormat(Formatter):
         hour = self.data.hour % 12 or 12
         minute = self.data.minute
         return f"{hour}:{minute:02d}" if minute else f"{hour}"
+
+    def F(self):
+        "Month, textual, long; e.g. 'January'"
+        return MONTHS[self.data.month]
 
     def g(self):
         "Hour, 12-hour format without leading zeros; i.e. '1' to '12'"
@@ -131,103 +141,6 @@ class TimeFormat(Formatter):
     def i(self):
         "Minutes; i.e. '00' to '59'"
         return f"{self.data.minute:02d}"
-
-    def O(self):  # NOQA: E743, E741
-        """
-        Difference to Greenwich time in hours; e.g. '+0200', '-0430'.
-
-        If timezone information is not available, return an empty string.
-        """
-        if self.timezone is None:
-            return ""
-
-        offset = self.timezone.utcoffset(self.data)
-        seconds = offset.days * 86400 + offset.seconds
-        sign = "-" if seconds < 0 else "+"
-        seconds = abs(seconds)
-        minutes = seconds // 60 % 60
-        hours = seconds // 3600
-        return f"{sign}{hours:02d}{minutes:02d}"
-
-    def P(self):
-        """
-        Time, in 12-hour hours, minutes and 'a.m.'/'p.m.', with minutes left off
-        if they're zero and the strings 'midnight' and 'noon' if appropriate.
-        Examples: '1 a.m.', '1:30 p.m.', 'midnight', 'noon', '12:30 p.m.'
-        Proprietary extension.
-        """
-        if self.data.minute == 0 and self.data.hour == 0:
-            return _("midnight")
-        if self.data.minute == 0 and self.data.hour == 12:
-            return _("noon")
-        return "%s %s" % (self.f(), self.a())
-
-    def s(self):
-        "Seconds; i.e. '00' to '59'"
-        return f"{self.data.second:02d}"
-
-    def T(self):
-        """
-        Time zone of this machine; e.g. 'EST' or 'MDT'.
-
-        If timezone information is not available, return an empty string.
-        """
-        if self.timezone is None:
-            return ""
-
-        return str(self.timezone.tzname(self.data))
-
-    def u(self):
-        "Microseconds; i.e. '000000' to '999999'"
-        return f"{self.data.microsecond:06d}"
-
-    def Z(self):
-        """
-        Time zone offset in seconds (i.e. '-43200' to '43200'). The offset for
-        timezones west of UTC is always negative, and for those east of UTC is
-        always positive.
-
-        If timezone information is not available, return an empty string.
-        """
-        if self.timezone is None:
-            return ""
-
-        offset = self.timezone.utcoffset(self.data)
-
-        # `offset` is a datetime.timedelta. For negative values (to the west of
-        # UTC) only days can be negative (days=-1) and seconds are always
-        # positive. e.g. UTC-1 -> timedelta(days=-1, seconds=82800, microseconds=0)
-        # Positive offsets have days=0
-        return str(offset.days * 86400 + offset.seconds)
-
-
-class DateFormat(TimeFormat):
-    def b(self):
-        "Month, textual, 3 letters, lowercase; e.g. 'jan'"
-        return MONTHS_3[self.data.month]
-
-    def c(self):
-        """
-        ISO 8601 Format
-        Example : '2008-01-02T10:30:00.000123'
-        """
-        return self.data.isoformat()
-
-    def d(self):
-        "Day of the month, 2 digits with leading zeros; i.e. '01' to '31'"
-        return f"{self.data.day:02d}"
-
-    def D(self):
-        "Day of the week, textual, 3 letters; e.g. 'Fri'"
-        return WEEKDAYS_ABBR[self.data.weekday()]
-
-    def E(self):
-        "Alternative month names as required by some locales. Proprietary extension."
-        return MONTHS_ALT[self.data.month]
-
-    def F(self):
-        "Month, textual, long; e.g. 'January'"
-        return MONTHS[self.data.month]
 
     def I(self):  # NOQA: E743, E741
         "'1' if daylight saving time, '0' otherwise."
@@ -267,6 +180,36 @@ class DateFormat(TimeFormat):
         "ISO 8601 year number matching the ISO week number (W)"
         return str(self.data.isocalendar().year)
 
+    def O(self):  # NOQA: E743, E741
+        """
+        Difference to Greenwich time in hours; e.g. '+0200', '-0430'.
+
+        If timezone information is not available, return an empty string.
+        """
+        if self.timezone is None:
+            return ""
+
+        offset = self.timezone.utcoffset(self.data)
+        seconds = offset.days * 86400 + offset.seconds
+        sign = "-" if seconds < 0 else "+"
+        seconds = abs(seconds)
+        minutes = seconds // 60 % 60
+        hours = seconds // 3600
+        return f"{sign}{hours:02d}{minutes:02d}"
+
+    def P(self):
+        """
+        Time, in 12-hour hours, minutes and 'a.m.'/'p.m.', with minutes left off
+        if they're zero and the strings 'midnight' and 'noon' if appropriate.
+        Examples: '1 a.m.', '1:30 p.m.', 'midnight', 'noon', '12:30 p.m.'
+        Proprietary extension.
+        """
+        if self.data.minute == 0 and self.data.hour == 0:
+            return _("midnight")
+        if self.data.minute == 0 and self.data.hour == 12:
+            return _("noon")
+        return "%s %s" % (self.f(), self.a())
+
     def r(self):
         "RFC 5322 formatted date; e.g. 'Thu, 21 Dec 2000 16:01:07 +0200'"
         value = self.data
@@ -277,6 +220,10 @@ class DateFormat(TimeFormat):
         elif is_naive(value):
             value = make_aware(value, timezone=self.timezone)
         return format_datetime_rfc5322(value)
+
+    def s(self):
+        "Seconds; i.e. '00' to '59'"
+        return f"{self.data.second:02d}"
 
     def S(self):
         """
@@ -297,6 +244,21 @@ class DateFormat(TimeFormat):
     def t(self):
         "Number of days in the given month; i.e. '28' to '31'"
         return str(calendar.monthrange(self.data.year, self.data.month)[1])
+
+    def T(self):
+        """
+        Time zone of this machine; e.g. 'EST' or 'MDT'.
+
+        If timezone information is not available, return an empty string.
+        """
+        if self.timezone is None:
+            return ""
+
+        return str(self.timezone.tzname(self.data))
+
+    def u(self):
+        "Microseconds; i.e. '000000' to '999999'"
+        return f"{self.data.microsecond:06d}"
 
     def U(self):
         "Seconds since the Unix epoch (January 1 1970 00:00:00 GMT)"
@@ -325,6 +287,46 @@ class DateFormat(TimeFormat):
     def z(self):
         """Day of the year, i.e. 1 to 366."""
         return str(self.data.timetuple().tm_yday)
+
+    def Z(self):
+        """
+        Time zone offset in seconds (i.e. '-43200' to '43200'). The offset for
+        timezones west of UTC is always negative, and for those east of UTC is
+        always positive.
+
+        If timezone information is not available, return an empty string.
+        """
+        if self.timezone is None:
+            return ""
+
+        offset = self.timezone.utcoffset(self.data)
+
+        # `offset` is a datetime.timedelta. For negative values (to the west of
+        # UTC) only days can be negative (days=-1) and seconds are always
+        # positive. e.g. UTC-1 -> timedelta(days=-1, seconds=82800, microseconds=0)
+        # Positive offsets have days=0
+        return str(offset.days * 86400 + offset.seconds)
+
+
+class TimeFormat(Formatter):
+    def __init__(self, obj):
+        self.data = obj
+        self.timezone = None
+
+        if isinstance(obj, datetime):
+            # Timezone is only supported when formatting datetime objects, not
+            # date objects (timezone information not appropriate), or time
+            # objects (against established django policy).
+            if is_naive(obj):
+                timezone = get_default_timezone()
+            else:
+                timezone = obj.tzinfo
+            if not _datetime_ambiguous_or_imaginary(obj, timezone):
+                self.timezone = timezone
+
+
+class DateFormat(TimeFormat):
+    pass
 
 
 def format(value, format_string):
