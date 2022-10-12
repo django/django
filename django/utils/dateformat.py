19cf67a8d05ff859e3f4e10d04e5f0ae45f97bc8
Nick Pope <nick@nickpope.me.uk>
2022-10-12 23:29:19 +0100

    Refs #????? -- Migrated `__init__()` onto `django.utils.datetime.Formatter`.

--- django/utils/dateformat.py
+++ django/utils/dateformat.py
@@ -39,6 +39,21 @@ class Formatter:
     date_specifiers = frozenset("bcdDEFIjlLmMnNorStUwWyYz")
     time_specifiers = frozenset("aAefgGhHiOPsTuZ")
     all_specifiers = date_specifiers | time_specifiers
+    timezone = None
+
+    def __init__(self, obj):
+        self.data = obj
+
+        if isinstance(obj, datetime):
+            # Timezone is only supported when formatting datetime objects, not
+            # date objects (timezone information not appropriate), or time
+            # objects (against established django policy).
+            if is_naive(obj):
+                timezone = get_default_timezone()
+            else:
+                timezone = obj.tzinfo
+            if not _datetime_ambiguous_or_imaginary(obj, timezone):
+                self.timezone = timezone
 
     def format(self, formatstr):
         pieces = []
@@ -309,20 +324,7 @@ class Formatter:
 
 
 class TimeFormat(Formatter):
-    def __init__(self, obj):
-        self.data = obj
-        self.timezone = None
-
-        if isinstance(obj, datetime):
-            # Timezone is only supported when formatting datetime objects, not
-            # date objects (timezone information not appropriate), or time
-            # objects (against established django policy).
-            if is_naive(obj):
-                timezone = get_default_timezone()
-            else:
-                timezone = obj.tzinfo
-            if not _datetime_ambiguous_or_imaginary(obj, timezone):
-                self.timezone = timezone
+    pass
 
 
 class DateFormat(TimeFormat):
