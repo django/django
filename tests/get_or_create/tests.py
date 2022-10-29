046680ed6c31ed32d771523f197885d76aa5bec4
Msvstl <59284206+sridhar562345@users.noreply.github.com>
2022-10-29 11:17:45 +0530

    fix incorrect test case in get_or_create tests

--- tests/get_or_create/tests.py
+++ tests/get_or_create/tests.py
@@ -367,7 +367,7 @@ class UpdateOrCreateTests(TestCase):
 
     def test_update_or_create_with_model_property_defaults(self):
         """Using a property with a setter implemented is allowed."""
-        t, _ = Thing.objects.get_or_create(
+        t, _ = Thing.objects.update_or_create(
             defaults={"capitalized_name_property": "annie"}, pk=1
         )
         self.assertEqual(t.name, "Annie")
