69744808cfe16ea8b99c65254fbe9456023290d4
Bhuvnesh <bhuvnesh875@gmail.com>
2022-11-04 00:41:19 +0530

    Refs #33646 -- Moved tests of QuerySet async interface into async tests.

--- tests/async/models.py
+++ tests/async/models.py
@@ -1,5 +1,11 @@
 from django.db import models
+from django.utils import timezone
+
+
+class RelatedModel(models.Model):
+    simple = models.ForeignKey("SimpleModel", models.CASCADE, null=True)
 
 
 class SimpleModel(models.Model):
     field = models.IntegerField()
+    created = models.DateTimeField(default=timezone.now)
