51faf4bd172cd4cb219a9793facbfa00246c9f3c
Francesco Panico <panico.francesco@gmail.com>
2022-11-17 22:45:06 +0100

    Fixed #34148 -- Reverted "Fixed #32901 -- Optimized BaseForm.__getitem__()."
    This reverts commit edde2a069929c93e37835dc3f7c9a229040058e2.
    
    Thanks Jan Pieter Waagmeester for the report.

--- tests/forms_tests/tests/test_forms.py
+++ tests/forms_tests/tests/test_forms.py
@@ -4579,6 +4579,22 @@ Options: <select multiple name="options" required>
             '<legend number="9999" for="id_first_name">First name:</legend>',
         )
 
+    def test_remove_cached_field(self):
+        class TestForm(Form):
+            name = CharField(max_length=10)
+
+            def __init__(self, *args, **kwargs):
+                super().__init__(*args, **kwargs)
+                # Populate fields cache.
+                [field for field in self]
+                # Removed cached field.
+                del self.fields["name"]
+
+        f = TestForm({"name": "abcde"})
+
+        with self.assertRaises(KeyError):
+            f["name"]
+
 
 @jinja2_tests
 class Jinja2FormsTestCase(FormsTestCase):
