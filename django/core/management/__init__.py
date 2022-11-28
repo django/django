a990929e145476315ec9c371df2d552252ed551e
Ben Cail <bcail@crossway.org>
2022-11-28 11:22:12 -0500

    run black

--- django/core/management/__init__.py
+++ django/core/management/__init__.py
@@ -383,8 +383,8 @@ class ManagementUtility:
         except ImproperlyConfigured as exc:
             self.settings_exception = exc
             # The following commands can be run even without a valid settings file configured
-            if subcommand not in {'startproject', 'startapp', 'makemessages'}:
-                sys.stderr.write(str(exc) + '\n')
+            if subcommand not in {"startproject", "startapp", "makemessages"}:
+                sys.stderr.write(str(exc) + "\n")
             self.settings_exception = exc
 
         if settings.configured:
