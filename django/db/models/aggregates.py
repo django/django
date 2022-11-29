0db8bf3d60f7a027391ce89555bdb4a95ad0a227
Simon Charette <charette.s@gmail.com>
2022-11-29 12:31:14 +0100

    Refs #10929 -- Fixed aggregates crash when passing strings as defaults.
    Previously strings were interpreted as F() expressions and default
    crashed with AttributeError:
        'F' object has no attribute 'empty_result_set_value'

--- django/db/models/aggregates.py
+++ django/db/models/aggregates.py
@@ -2,7 +2,7 @@
 Classes to represent the definitions of aggregate functions.
 """
 from django.core.exceptions import FieldError, FullResultSet
-from django.db.models.expressions import Case, Func, Star, When
+from django.db.models.expressions import Case, Func, Star, Value, When
 from django.db.models.fields import IntegerField
 from django.db.models.functions.comparison import Coalesce
 from django.db.models.functions.mixins import (
@@ -85,6 +85,8 @@ class Aggregate(Func):
             return c
         if hasattr(default, "resolve_expression"):
             default = default.resolve_expression(query, allow_joins, reuse, summarize)
+        else:
+            default = Value(default, c._output_field_or_none)
         c.default = None  # Reset the default argument before wrapping.
         coalesce = Coalesce(c, default, output_field=c._output_field_or_none)
         coalesce.is_summary = c.is_summary
