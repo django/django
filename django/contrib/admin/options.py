6f77d35032a7d0e86b764eccd3a68f739b297e16
Rodolfo Becerra <rodolvbg@divineslns.com>
2022-12-07 06:48:28 -0500

    comprehend actions

--- django/contrib/admin/options.py
+++ django/contrib/admin/options.py
@@ -982,11 +982,11 @@ class ModelAdmin(BaseModelAdmin):
         base_action_names = {name for _, name, _ in base_actions}
 
         # Gather actions from the admin site first
-        for (name, func) in self.admin_site.actions:
-            if name in base_action_names:
-                continue
-            description = self._get_action_description(func, name)
-            actions.append((func, name, description))
+        actions = [
+            (func, name, self._get_action_description(func, name))
+            for (name, func) in self.admin_site.actions
+            if name not in base_action_names
+        ]
         # Add actions from this ModelAdmin.
         actions.extend(base_actions)
         return actions
