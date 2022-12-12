2f38f7b8f9bd65905dc6bec4f3e2a30b5da8e338
Florian Apolloner <florian@apolloner.eu>
2022-12-12 09:08:46 +0100

    Refs #33308 -- Added psycopg_any.sql.quote() hook.

--- django/db/backends/postgresql/psycopg_any.py
+++ django/db/backends/postgresql/psycopg_any.py
@@ -1,6 +1,17 @@
-from psycopg2 import errors, extensions  # NOQA
+from psycopg2 import errors, extensions, sql  # NOQA
 from psycopg2.extras import DateRange, DateTimeRange, DateTimeTZRange, Inet  # NOQA
 from psycopg2.extras import Json as Jsonb  # NOQA
 from psycopg2.extras import NumericRange, Range  # NOQA
 
 RANGE_TYPES = (DateRange, DateTimeRange, DateTimeTZRange, NumericRange)
+
+
+def _quote(value, connection=None):
+    adapted = extensions.adapt(value)
+    if hasattr(adapted, "encoding"):
+        adapted.encoding = "utf8"
+    # getquoted() returns a quoted bytestring of the adapted value.
+    return adapted.getquoted().decode()
+
+
+sql.quote = _quote
