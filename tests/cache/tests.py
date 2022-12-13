e120d650a856bf6107c6433988710545ade57d55
Marti Raudsepp <marti@juffo.org>
2022-12-13 11:20:25 +0200

    Fixed #34209 -- Prevented FileBasedCache.has_key() crash caused by a race condition.

--- tests/cache/tests.py
+++ tests/cache/tests.py
@@ -1762,6 +1762,12 @@ class FileBasedCacheTests(BaseCacheTests, TestCase):
         with open(cache_file, "rb") as fh:
             self.assertIs(cache._is_expired(fh), True)
 
+    def test_has_key_race_handling(self):
+        self.assertIs(cache.add("key", "value"), True)
+        with mock.patch("builtins.open", side_effect=FileNotFoundError) as mocked_open:
+            self.assertIs(cache.has_key("key"), False)
+            mocked_open.assert_called_once()
+
 
 @unittest.skipUnless(RedisCache_params, "Redis backend not configured")
 @override_settings(
