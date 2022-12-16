80178f5c70cf073411c0f130448e0bf5ff1f2155
Leo <86375512+select-case@users.noreply.github.com>
2022-12-16 09:28:22 +0530

    Fixed #34212 -- Made RedisCacheClient.incr() use write connection.
    Co-authored-by: Sin-Woo Bang <sinwoobang@gmail.com>

--- tests/cache/tests.py
+++ tests/cache/tests.py
@@ -1787,6 +1787,14 @@ class RedisCacheTests(BaseCacheTests, TestCase):
     def incr_decr_type_error(self):
         return self.lib.ResponseError
 
+    def test_incr_write_connection(self):
+        cache.set("number", 42)
+        with mock.patch(
+            "django.core.cache.backends.redis.RedisCacheClient.get_client"
+        ) as mocked_get_client:
+            cache.incr("number")
+            self.assertEqual(mocked_get_client.call_args.kwargs, {"write": True})
+
     def test_cache_client_class(self):
         self.assertIs(cache._class, RedisCacheClient)
         self.assertIsInstance(cache._cache, RedisCacheClient)
