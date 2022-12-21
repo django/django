ad4095d86d3e0d1ea1a8ae3b83e4b96a8b07e79b
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2022-12-21 09:47:36 +0100

    Removed unnecessary commas in tests.

--- tests/aggregation/test_filter_argument.py
+++ tests/aggregation/test_filter_argument.py
@@ -139,7 +139,7 @@ class FilteredAggregateTests(TestCase):
         self.assertEqual(qs.get(pk__in=qs.values("pk")), self.a1)
 
     def test_filtered_aggregate_ref_annotation(self):
-        aggs = Author.objects.annotate(double_age=F("age") * 2,).aggregate(
+        aggs = Author.objects.annotate(double_age=F("age") * 2).aggregate(
             cnt=Count("pk", filter=Q(double_age__gt=100)),
         )
         self.assertEqual(aggs["cnt"], 2)
