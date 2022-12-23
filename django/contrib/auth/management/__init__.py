b2dc03c1f017f198c949dc8f94799e5b5ec580c3
David Wobrock <david.wobrock@gmail.com>
2022-12-23 18:16:05 +0100

    Fixed #34165 -- Made permissions creation respect the "using" parameter.

--- django/contrib/auth/management/__init__.py
+++ django/contrib/auth/management/__init__.py
@@ -95,11 +95,16 @@ def create_permissions(
         .values_list("content_type", "codename")
     )
 
-    perms = [
-        Permission(codename=codename, name=name, content_type=ct)
-        for ct, (codename, name) in searched_perms
-        if (ct.pk, codename) not in all_perms
-    ]
+    perms = []
+    for ct, (codename, name) in searched_perms:
+        if (ct.pk, codename) not in all_perms:
+            permission = Permission()
+            permission._state.db = using
+            permission.codename = codename
+            permission.name = name
+            permission.content_type = ct
+            perms.append(permission)
+
     Permission.objects.using(using).bulk_create(perms)
     if verbosity >= 2:
         for perm in perms:
