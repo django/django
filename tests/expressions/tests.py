97320a31b0202fdd828a9ee66372c444ef656d10
sarahboyce <sarahvboyce95@gmail.com>
2023-01-14 14:36:23 +0100

    Refs #30129 -- Added test for create() with F() expression in Subquery.
    Fixed in 35431298226165986ad07e91f9d3aca721ff38ec.

--- tests/expressions/tests.py
+++ tests/expressions/tests.py
@@ -776,6 +776,24 @@ class BasicExpressionsTests(TestCase):
         # contain nested aggregates.
         self.assertNotIn("GROUP BY", sql)
 
+    def test_object_create_with_f_expression_in_subquery(self):
+        Company.objects.create(
+            name="Big company", num_employees=100000, num_chairs=1, ceo=self.max
+        )
+        biggest_company = Company.objects.create(
+            name="Biggest company",
+            num_chairs=1,
+            ceo=self.max,
+            num_employees=Subquery(
+                Company.objects.order_by("-num_employees")
+                .annotate(max_num_employees=Max("num_employees"))
+                .annotate(new_num_employees=F("max_num_employees") + 1)
+                .values("new_num_employees")[:1]
+            ),
+        )
+        biggest_company.refresh_from_db()
+        self.assertEqual(biggest_company.num_employees, 100001)
+
     @skipUnlessDBFeature("supports_over_clause")
     def test_aggregate_rawsql_annotation(self):
         with self.assertNumQueries(1) as ctx:
