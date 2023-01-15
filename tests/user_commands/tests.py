beaa5f31e1e97bbde83a609c4b143193899f79ff
Adam Johnson <me@adamj.eu>
2023-01-15 09:19:52 +0000

    [4.2.x] Fixed #34259 -- Passed called_from_command_line to command subparsers.
    Backport of 017fa23d3b0aee9142f531c2a0002fc86c82a54c from main

--- tests/user_commands/tests.py
+++ tests/user_commands/tests.py
@@ -468,6 +468,30 @@ class CommandRunTests(AdminScriptTestCase):
         self.assertNoOutput(err)
         self.assertEqual(out.strip(), "Set foo")
 
+    def test_subparser_error_formatting(self):
+        self.write_settings("settings.py", apps=["user_commands"])
+        out, err = self.run_manage(["subparser", "foo", "twelve"])
+        self.maxDiff = None
+        self.assertNoOutput(out)
+        err_lines = err.splitlines()
+        self.assertEqual(len(err_lines), 2)
+        self.assertEqual(
+            err_lines[1],
+            "manage.py subparser foo: error: argument bar: invalid int value: 'twelve'",
+        )
+
+    def test_subparser_non_django_error_formatting(self):
+        self.write_settings("settings.py", apps=["user_commands"])
+        out, err = self.run_manage(["subparser_vanilla", "foo", "seven"])
+        self.assertNoOutput(out)
+        err_lines = err.splitlines()
+        self.assertEqual(len(err_lines), 2)
+        self.assertEqual(
+            err_lines[1],
+            "manage.py subparser_vanilla foo: error: argument bar: invalid int value: "
+            "'seven'",
+        )
+
 
 class UtilsTests(SimpleTestCase):
     def test_no_existent_external_program(self):
