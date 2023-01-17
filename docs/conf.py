81818cc55553caba8125bdcd18fa0427ac16c905
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2023-01-17 08:54:30 +0100

    [4.2.x] Bumped django_next_version in docs config.

--- docs/conf.py
+++ docs/conf.py
@@ -128,7 +128,7 @@ else:
     release = django_release()
 
 # The "development version" of Django
-django_next_version = "4.2"
+django_next_version = "5.0"
 
 extlinks = {
     "bpo": ("https://bugs.python.org/issue?@action=redirect&bpo=%s", "bpo-%s"),
