b332a96cd721defb6ff13ba1bc57958a984f0773
Matt Westcott <matt@west.co.tt>
2023-01-20 18:10:29 +0000

    [4.2.x] Fixed #34192 -- Preserved callable storage when it returns default_storage.
    Backport of ef85b6bf0bc5a8b194f0724cf5bbedbcee402b96 from main

--- django/db/models/fields/files.py
+++ django/db/models/fields/files.py
@@ -295,8 +295,9 @@ class FileField(Field):
         if kwargs.get("max_length") == 100:
             del kwargs["max_length"]
         kwargs["upload_to"] = self.upload_to
-        if self.storage is not default_storage:
-            kwargs["storage"] = getattr(self, "_storage_callable", self.storage)
+        storage = getattr(self, "_storage_callable", self.storage)
+        if storage is not default_storage:
+            kwargs["storage"] = storage
         return name, path, args, kwargs
 
     def get_internal_type(self):
