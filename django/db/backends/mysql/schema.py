5e0be0873ca6fe0c4ad7abe1398443bdf367d16e
sag᠎e <me@laymonage.com>
2023-01-31 10:52:07 +0000

    [4.2.x] Fixed #34304 -- Made MySQL's SchemaEditor.remove_constraint() don't create foreign key index when unique constraint is ignored.
    Regression in b731e8841558ee4caaba766c83f34ea9c7004f8b.
    Backport of 110b3b83567da22f19ec04210db134d0fe83d662 from main

--- django/db/backends/mysql/schema.py
+++ django/db/backends/mysql/schema.py
@@ -121,7 +121,10 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
             )
 
     def remove_constraint(self, model, constraint):
-        if isinstance(constraint, UniqueConstraint):
+        if (
+            isinstance(constraint, UniqueConstraint)
+            and constraint.create_sql(model, self) is not None
+        ):
             self._create_missing_fk_index(
                 model,
                 fields=constraint.fields,
