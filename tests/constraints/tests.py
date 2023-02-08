2fd755b361d3da2cd0440fc9839feb2bb69b027b
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2023-02-08 16:38:55 +0100

    Fixed #34319 -- Fixed Model.validate_constraints() crash on ValidationError with no code.
    Thanks Mateusz Kurowski for the report.
    
    Regression in 667105877e6723c6985399803a364848891513cc.

--- tests/constraints/tests.py
+++ tests/constraints/tests.py
@@ -3,7 +3,7 @@ from unittest import mock
 from django.core.exceptions import ValidationError
 from django.db import IntegrityError, connection, models
 from django.db.models import F
-from django.db.models.constraints import BaseConstraint
+from django.db.models.constraints import BaseConstraint, UniqueConstraint
 from django.db.models.functions import Lower
 from django.db.transaction import atomic
 from django.test import SimpleTestCase, TestCase, skipIfDBFeature, skipUnlessDBFeature
@@ -588,6 +588,26 @@ class UniqueConstraintTests(TestCase):
         msg = "Constraint “name_without_color_uniq” is violated."
         with self.assertRaisesMessage(ValidationError, msg):
             UniqueConstraintConditionProduct(name=obj2.name).validate_constraints()
+
+    def test_model_validation_constraint_no_code_error(self):
+        class ValidateNoCodeErrorConstraint(UniqueConstraint):
+            def validate(self, model, instance, **kwargs):
+                raise ValidationError({"name": ValidationError("Already exists.")})
+
+        class NoCodeErrorConstraintModel(models.Model):
+            name = models.CharField(max_length=255)
+
+            class Meta:
+                constraints = [
+                    ValidateNoCodeErrorConstraint(
+                        Lower("name"),
+                        name="custom_validate_no_code_error",
+                    )
+                ]
+
+        msg = "{'name': ['Already exists.']}"
+        with self.assertRaisesMessage(ValidationError, msg):
+            NoCodeErrorConstraintModel(name="test").validate_constraints()
 
     def test_validate(self):
         constraint = UniqueConstraintProduct._meta.constraints[0]
