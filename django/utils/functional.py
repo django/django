639244c2125da859c95493fd043035af6de6d6ec
Collin Anderson <cmawebsite@gmail.com>
2023-02-10 21:43:34 -0500

    Fixed #28358 -- Workaround for python bug

--- django/utils/functional.py
+++ django/utils/functional.py
@@ -281,8 +281,8 @@ class LazyObject:
     def __getattribute__(self, name):
         if name == "_wrapped":
             # Avoid recursion when getting wrapped object.
-            return super().__getattribute__(name)
-        value = super().__getattribute__(name)
+            return object.__getattribute__(self, name)
+        value = object.__getattribute__(self, name)
         # If attribute is a proxy method, raise an AttributeError to call
         # __getattr__() and use the wrapped object method.
         if not getattr(value, "_mask_wrapped", True):
