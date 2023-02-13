1f701ed43977ebacb01c00f20dd489e812c14332
Jon Janzen <jon@jonjanzen.com>
2023-02-13 18:24:35 -0500

    Fixed #31920 -- Made AuthenticationMiddleware add request.auser().

--- tests/auth_tests/test_middleware.py
+++ tests/auth_tests/test_middleware.py
@@ -43,3 +43,10 @@ class TestAuthenticationMiddleware(TestCase):
         )
         with self.assertRaisesMessage(ImproperlyConfigured, msg):
             self.middleware(HttpRequest())
+
+    async def test_auser(self):
+        self.middleware(self.request)
+        auser = await self.request.auser()
+        self.assertEqual(auser, self.user)
+        auser_second = await self.request.auser()
+        self.assertIs(auser, auser_second)
