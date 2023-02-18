740ab689c99a6a2babf8a1166a4d8b7f92dd9c5b
Simon Charette <charettes@users.noreply.github.com>
2023-02-18 14:53:08 +0100

    Refs #33308 -- Added tests for queryset ordered by annotation with nulls_first/nulls_last.

--- tests/ordering/tests.py
+++ tests/ordering/tests.py
@@ -165,6 +165,12 @@ class OrderingTests(TestCase):
             ),
             [self.a3, self.a4, self.a1, self.a2],
         )
+        self.assertQuerySetEqualReversible(
+            Article.objects.annotate(upper_name=Upper("author__name")).order_by(
+                F("upper_name").asc(nulls_last=True), "headline"
+            ),
+            [self.a3, self.a4, self.a1, self.a2],
+        )
 
     def test_order_by_nulls_first(self):
         Article.objects.filter(headline="Article 3").update(author=self.author_1)
@@ -187,6 +193,12 @@ class OrderingTests(TestCase):
         self.assertQuerySetEqualReversible(
             Article.objects.order_by(
                 Upper("author__name").desc(nulls_first=True), "headline"
+            ),
+            [self.a1, self.a2, self.a4, self.a3],
+        )
+        self.assertQuerySetEqualReversible(
+            Article.objects.annotate(upper_name=Upper("author__name")).order_by(
+                F("upper_name").desc(nulls_first=True), "headline"
             ),
             [self.a1, self.a2, self.a4, self.a3],
         )
