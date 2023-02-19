80cc439c8f8fff97303995a50bb63bfae44e41ae
vBlackOut <vladimir.souchet@cuby-hebergs.com>
2023-02-19 04:42:53 +0100

    fix black

--- django/core/management/commands/loadstatic.py
+++ django/core/management/commands/loadstatic.py
@@ -9,13 +9,13 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = 'load all url for static img,js,css'
+    help = "load all url for static img,js,css"
     pattern_executed = []
 
     def prepend_line(self, path, line):
-        with open(path, 'r') as old:
+        with open(path, "r") as old:
             os.unlink(path)
-            with open(path, 'w') as new:
+            with open(path, "w") as new:
                 new.write(str(line) + "\n")
                 shutil.copyfileobj(old, new)
 
@@ -23,7 +23,7 @@ class Command(BaseCommand):
         with fileinput.input(file_path, inplace=True) as file:
             for line in file:
                 new_line = line.replace(search_text, new_text)
-                print(new_line, end='')
+                print(new_line, end="")
 
     def checkfile_replace_need(self, file, lines):
         result_replace = False
@@ -39,32 +39,29 @@ class Command(BaseCommand):
         is_image = re.findall(r'<img.*?src=["|\'](.*?)["|\']', html, re.I)
 
         is_javascript = re.findall(
-                        r'<script[^<>]+src=["\']([^"\'<>]+\.js)["\']',
-                        html, re.I)
+            r'<script[^<>]+src=["\']([^"\'<>]+\.js)["\']', html, re.I
+        )
 
         is_css = re.findall(
-                        r'<link[^<>]+href=["\']([^"\'<>]+\.(?:css|png))["\']',
-                        html, re.I)
+            r'<link[^<>]+href=["\']([^"\'<>]+\.(?:css|png))["\']', html, re.I
+        )
 
         all_replace = is_image + is_javascript + is_css
-        file_paths = "".join(file_path.split(
-                        str(settings.TEMPLATES[0]['DIRS'][0])
-                     ))
+        file_paths = "".join(file_path.split(str(settings.TEMPLATES[0]["DIRS"][0])))
 
         if len(all_replace) >= 1:
             for replace in all_replace:
-                if "{%" not in replace and \
-                   replace not in self.pattern_executed:
+                if "{%" not in replace and replace not in self.pattern_executed:
                     search = replace
                     replace_string = " ".join(
-                        ["{% static", '"{}"'.format(replace), '%}']
+                        ["{% static", '"{}"'.format(replace), "%}"]
                     )
                     replace_need = True
                     if not just_check:
                         print(
-                             "replace {} to \"{}\" file {}".format(
+                            'replace {} to "{}" file {}'.format(
                                 search, replace_string, file_paths
-                             )
+                            )
                         )
                         self.replace_in_file(file_path, search, replace_string)
                         self.pattern_executed.append(search)
@@ -75,29 +72,28 @@ class Command(BaseCommand):
             return False
 
     def handle(self, *args, **options):
-
-        for file in glob.glob(str(settings.TEMPLATES[0]['DIRS'][0])+"/*.html"):
+        for file in glob.glob(str(settings.TEMPLATES[0]["DIRS"][0]) + "/*.html"):
             fs = open(file, "r")
             lines = [line.strip() for line in fs.readlines()]
 
-            file_path = "".join(file.split(
-                            str(settings.TEMPLATES[0]['DIRS'][0])
-                         ))
+            file_path = "".join(file.split(str(settings.TEMPLATES[0]["DIRS"][0])))
 
             if "{% load static %}" not in lines:
                 # print("no detect load_static")
                 self.stdout.write(
-                  self.style.ERROR(
-                   'no detect load_static inside template {}'.format(file_path)
-                  )
+                    self.style.ERROR(
+                        "no detect load_static inside template {}".format(file_path)
+                    )
                 )
                 self.prepend_line(file, "{% load static %}")
 
             check_replace = self.checkfile_replace_need(file, lines)
 
             if check_replace is True:
-                text_input = "do you want to automaticaly for file {}" \
-                             " replace assets path ? [Y/n]\n".format(file_path)
+                text_input = (
+                    "do you want to automaticaly for file {}"
+                    " replace assets path ? [Y/n]\n".format(file_path)
+                )
                 lineinput = input(text_input)
 
                 if "n" in lineinput or "N" in lineinput:
