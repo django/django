a7d9633454409f8bb6b9a9d26ebbde0b8f738ecd
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2023-02-21 12:47:52 +0100

    Refs #31445 -- Added test for nesting QuerySet.union().
    This was fixed in MySQL 8.0.31.

--- tests/queries/test_qs_combinators.py
+++ tests/queries/test_qs_combinators.py
@@ -114,6 +114,15 @@ class QuerySetSetOperationTests(TestCase):
             [1, 2, 3, 4, 6, 7, 8, 9, 10, None],
         )
 
+    def test_union_nested(self):
+        qs1 = Number.objects.all()
+        qs2 = qs1.union(qs1)
+        self.assertNumbersEqual(
+            qs1.union(qs2),
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+            ordered=False,
+        )
+
     @skipUnlessDBFeature("supports_select_intersection")
     def test_intersection_with_empty_qs(self):
         qs1 = Number.objects.all()
