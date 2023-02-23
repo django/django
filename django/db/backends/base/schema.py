16c966ff7fc9ce01e3afd87ef2af55859cadb587
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2023-02-23 21:12:17 +0100

    Refs #30060, Refs #34217 -- Made SchemaEditor not generate SQL for CheckConstraint if not supported.
    The new logic mirrors the logic in SchemaEditor._delete_check_sql()
    added in 68ef274bc505cd44f305c03cbf84cf08826200a8.
    
    Thanks Tim Graham for the report.

--- django/db/backends/base/schema.py
+++ django/db/backends/base/schema.py
@@ -1726,6 +1726,8 @@ class BaseDatabaseSchemaEditor:
         }
 
     def _create_check_sql(self, model, name, check):
+        if not self.connection.features.supports_table_check_constraints:
+            return None
         return Statement(
             self.sql_create_check,
             table=Table(model._meta.db_table, self.quote_name),
