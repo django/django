385df800f06da82a1314e2dd11d84ff070e91e3d
Simon Charette <charette.s@gmail.com>
2023-02-26 13:43:00 +0100

    Fixed #34368 -- Made subquery raise NotSupportedError when referencing outer window expression.
    Regression in f387d024fc75569d2a4a338bfda76cc2f328f627.
    
    Co-authored-by: Jannis Vajen <jvajen@gmail.com>

--- django/db/models/expressions.py
+++ django/db/models/expressions.py
@@ -857,6 +857,11 @@ class ResolvedOuterRef(F):
 
     def resolve_expression(self, *args, **kwargs):
         col = super().resolve_expression(*args, **kwargs)
+        if col.contains_over_clause:
+            raise NotSupportedError(
+                f"Referencing outer query window expression is not supported: "
+                f"{self.name}."
+            )
         # FIXME: Rename possibly_multivalued to multivalued and fix detection
         # for non-multivalued JOINs (e.g. foreign key fields). This should take
         # into account only many-to-many and one-to-many relationships.
--- tests/expressions_window/tests.py
+++ tests/expressions_window/tests.py
@@ -1587,6 +1587,25 @@ class WindowUnsupportedTests(TestCase):
                     dense_rank=Window(expression=DenseRank())
                 ).get()
 
+    def test_filter_subquery(self):
+        qs = Employee.objects.annotate(
+            department_salary_rank=Window(
+                Rank(), partition_by="department", order_by="-salary"
+            )
+        )
+        msg = (
+            "Referencing outer query window expression is not supported: "
+            "department_salary_rank."
+        )
+        with self.assertRaisesMessage(NotSupportedError, msg):
+            qs.annotate(
+                employee_name=Subquery(
+                    Employee.objects.filter(
+                        age=OuterRef("department_salary_rank")
+                    ).values("name")[:1]
+                )
+            )
+
 
 class NonQueryWindowTests(SimpleTestCase):
     def test_window_repr(self):
