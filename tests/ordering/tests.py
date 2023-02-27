e603fd4bd4459df0626844b3aa14e1fc77c26795
Simon Charette <charette.s@gmail.com>
2023-02-27 01:10:19 -0500

    Fixed #34372 -- Fixed queryset crash on order by aggregation using OrderBy.
    Regression in 278881e37619278789942513916acafaa88d26f3 caused by a lack
    of expression copying when an OrderBy expression is explicitly provided.
    
    Thanks Jannis Vajen for the report and regression test.

--- tests/ordering/tests.py
+++ tests/ordering/tests.py
@@ -638,3 +638,9 @@ class OrderingTests(TestCase):
             .first(),
             self.a1,
         )
+
+    def test_order_by_expr_query_reuse(self):
+        qs = Author.objects.annotate(num=Count("article")).order_by(
+            F("num").desc(), "pk"
+        )
+        self.assertCountEqual(qs, qs.iterator())
