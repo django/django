32d4b61c313be5169137047e9fb3668da20a5d89
nabil-rady <midorady9999@gmail.com>
2023-03-02 00:13:00 +0200

    Fixed #34370 -- Added integer fields validation as 64-bit on SQLite.

--- django/db/backends/sqlite3/operations.py
+++ django/db/backends/sqlite3/operations.py
@@ -382,8 +382,15 @@ class DatabaseOperations(BaseDatabaseOperations):
         return "django_format_dtdelta(%s)" % ", ".join(fn_params)
 
     def integer_field_range(self, internal_type):
-        # SQLite doesn't enforce any integer constraints
-        return (None, None)
+        # SQLite doesn't enforce any integer constraints, but sqlite3 supports
+        # integers up to 64 bits.
+        if internal_type in [
+            "PositiveBigIntegerField",
+            "PositiveIntegerField",
+            "PositiveSmallIntegerField",
+        ]:
+            return (0, 9223372036854775807)
+        return (-9223372036854775808, 9223372036854775807)
 
     def subtract_temporals(self, internal_type, lhs, rhs):
         lhs_sql, lhs_params = lhs
