c914d6cff176ae6bfab2f33a84bcfd45208f1894
Dmitriy Sintsov <questpc256@gmail.com>
2023-03-05 12:07:35 +0300

    Fixed #34386 -- Made SMTP backend load default system root CA certificates by default.
    Regression in 2848e5d0ce5cf3c31fe87525536093b21d570f69.

--- django/core/mail/backends/smtp.py
+++ django/core/mail/backends/smtp.py
@@ -57,10 +57,12 @@ class EmailBackend(BaseEmailBackend):
 
     @cached_property
     def ssl_context(self):
-        ssl_context = ssl.SSLContext(protocol=ssl.PROTOCOL_TLS_CLIENT)
         if self.ssl_certfile or self.ssl_keyfile:
+            ssl_context = ssl.SSLContext(protocol=ssl.PROTOCOL_TLS_CLIENT)
             ssl_context.load_cert_chain(self.ssl_certfile, self.ssl_keyfile)
-        return ssl_context
+            return ssl_context
+        else:
+            return ssl.create_default_context()
 
     def open(self):
         """
