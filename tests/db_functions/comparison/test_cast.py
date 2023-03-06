cd330f50c8984ed24ab1833980af18d594810d29
David Wobrock <david.wobrock@gmail.com>
2023-03-06 21:17:58 +0100

    Refs #16055 -- Added test for casting to ForeignKey.

--- tests/db_functions/comparison/test_cast.py
+++ tests/db_functions/comparison/test_cast.py
@@ -186,3 +186,12 @@ class CastTests(TestCase):
             ).get(),
             "1",
         )
+
+    def test_cast_to_foreign_key_field(self):
+        numbers = Author.objects.annotate(
+            cast_fk=Cast(
+                models.Value("0"),
+                models.ForeignKey(Author, on_delete=models.SET_NULL),
+            )
+        )
+        self.assertEqual(numbers.get().cast_fk, 0)
