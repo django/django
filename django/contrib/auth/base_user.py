ba1654cb54eccef3ba29e455cd5065aed84e1f90
David Wobrock <david.wobrock@gmail.com>
2023-03-06 16:18:03 +0100

    [4.1.x] Fixed #34384 -- Fixed session validation when rotation secret keys.
    Bug in 0dcd549bbe36c060f536ec270d34d9e7d4b8e6c7.
    
    Thanks Eric Zarowny for the report.
    
    Backport of 2396933ca99c6bfb53bda9e53968760316646e01 from main

--- django/contrib/auth/base_user.py
+++ django/contrib/auth/base_user.py
@@ -4,6 +4,7 @@ not in INSTALLED_APPS.
 """
 import unicodedata
 
+from django.conf import settings
 from django.contrib.auth import password_validation
 from django.contrib.auth.hashers import (
     check_password,
@@ -128,10 +129,18 @@ class AbstractBaseUser(models.Model):
         """
         Return an HMAC of the password field.
         """
+        return self._get_session_auth_hash()
+
+    def get_session_auth_fallback_hash(self):
+        for fallback_secret in settings.SECRET_KEY_FALLBACKS:
+            yield self._get_session_auth_hash(secret=fallback_secret)
+
+    def _get_session_auth_hash(self, secret=None):
         key_salt = "django.contrib.auth.models.AbstractBaseUser.get_session_auth_hash"
         return salted_hmac(
             key_salt,
             self.password,
+            secret=secret,
             algorithm="sha256",
         ).hexdigest()
 
