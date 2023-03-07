553ed3fff60b37f1363a486f757cfb2131e25b16
oluwayhemisi <ismailoluwayemisi@gmail.com>
2023-03-07 20:45:55 +0100

    "Fixed Admin widget don't quote keys from client side rendering"

---
+++ js_tests/admin/RelatedObjectLookups.test.js
@@ -0,0 +1,34 @@
+/* global QUnit, RelatedObjectLookups */
+'use strict';
+QUnit.module('admin.RelatedObjectLookups');
+
+QUnit.test('quote value', function(assert) {
+    const result = customEncodeURIComponent('_40');
+    const expected = '%5F40';
+    assert.equal(result, expected, "expected %5F40");
+
+});
+
+QUnit.test('updateRelatedObjectLinks properly quotes URL value', function(assert) {
+    const triggeringLink = $('<input class="vForeignKeyRawIdAdminField" type="text" value="_40">');
+    const relatedLink = $(`<a href="#" data-href-template="/admin_widgets/house/change/?name__iexact=__fk__">change</a>`);
+    relatedLink.addClass('view-related change-related delete-related');
+
+    const parent = $('<div class="vForeignKeyRawIdAdminField"></div>');
+    parent.append(triggeringLink);
+    parent.append(relatedLink);
+    $('body').append(parent);
+
+    relatedLink.appendTo(triggeringLink.parent());
+
+    updateRelatedObjectLinks(triggeringLink);
+    const href = relatedLink.attr('href');
+    const expectedValue = '/admin_widgets/house/change/?name__iexact=%5F40'; // expected value with encoded underscore
+
+    assert.equal(href, expectedValue, 'URL value is properly quoted');
+});
+
+
+
+
+
