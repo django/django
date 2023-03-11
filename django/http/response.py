e49ec56161e634232cba7dcd495381b4c2cdd27a
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2023-03-11 08:34:22 +0100

    Fixed #34405 -- Fixed setting Content-Type header in FileResponse for compress and brotli.
    Thanks Chamal De Silva for the report.

--- django/http/response.py
+++ django/http/response.py
@@ -609,7 +609,9 @@ class FileResponse(StreamingHttpResponse):
                 # Encoding isn't set to prevent browsers from automatically
                 # uncompressing files.
                 content_type = {
+                    "br": "application/x-brotli",
                     "bzip2": "application/x-bzip",
+                    "compress": "application/x-compress",
                     "gzip": "application/gzip",
                     "xz": "application/x-xz",
                 }.get(encoding, content_type)
