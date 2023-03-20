73989ffbcf1e427814702655a87b04348947aa15
Jure Slak <jure.slak@gmail.com>
2023-03-20 22:42:18 +0100

    Fixed #34424 -- Fixed SelectDateWidget crash for inputs raising OverflowError.

--- django/forms/widgets.py
+++ django/forms/widgets.py
@@ -1161,6 +1161,8 @@ class SelectDateWidget(Widget):
                 # Return pseudo-ISO dates with zeros for any unselected values,
                 # e.g. '2017-0-23'.
                 return "%s-%s-%s" % (y or 0, m or 0, d or 0)
+            except OverflowError:
+                return "0-0-0"
             return date_value.strftime(input_format)
         return data.get(name)
 
