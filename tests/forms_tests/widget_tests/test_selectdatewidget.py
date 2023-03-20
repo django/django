d22209cb428ecb083548fd21cae714a60c7fd5f7
Jure Slak <jure.slak@gmail.com>
2023-03-20 22:42:18 +0100

    Fixed #34424 -- Fixed SelectDateWidget crash for inputs raising OverflowError.

--- tests/forms_tests/widget_tests/test_selectdatewidget.py
+++ tests/forms_tests/widget_tests/test_selectdatewidget.py
@@ -1,3 +1,4 @@
+import sys
 from datetime import date
 
 from django.forms import DateField, Form, SelectDateWidget
@@ -610,6 +611,7 @@ class SelectDateWidgetTest(WidgetTest):
             ((None, "12", "1"), None),
             (("2000", None, "1"), None),
             (("2000", "12", None), None),
+            ((str(sys.maxsize + 1), "12", "1"), "0-0-0"),
         ]
         for values, expected in tests:
             with self.subTest(values=values):
