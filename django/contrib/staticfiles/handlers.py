6c49b38fdde39bdc11a2114f5e7bf906fece6878
Carlton Gibson <carlton.gibson@noumenal.es>
2023-03-21 20:51:53 +0100

    Fixed #34428 -- Made ASGIStaticFilesHandler adapt response to async iterator.
    Bug in 0bd2c0c9015b53c41394a1c0989afbfd94dc2830.

--- django/contrib/staticfiles/handlers.py
+++ django/contrib/staticfiles/handlers.py
@@ -103,4 +103,13 @@ class ASGIStaticFilesHandler(StaticFilesHandlerMixin, ASGIHandler):
     async def get_response_async(self, request):
         response = await super().get_response_async(request)
         response._resource_closers.append(request.close)
+        # FileResponse is not async compatible.
+        if response.streaming and not response.is_async:
+            _iterator = response.streaming_content
+
+            async def awrapper():
+                for part in await sync_to_async(list)(_iterator):
+                    yield part
+
+            response.streaming_content = awrapper()
         return response
