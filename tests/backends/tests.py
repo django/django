afd686c113492f9526f75eb95f340560b7a06473
Simon Charette <charette.s@gmail.com>
2023-03-26 19:20:09 -0400

    Removed unnecessary usage of .extra() in tests.

--- tests/backends/tests.py
+++ tests/backends/tests.py
@@ -91,9 +91,7 @@ class LastExecutedQueryTest(TestCase):
 
     def test_query_encoding(self):
         """last_executed_query() returns a string."""
-        data = RawData.objects.filter(raw_data=b"\x00\x46  \xfe").extra(
-            select={"föö": 1}
-        )
+        data = RawData.objects.filter(raw_data=b"\x00\x46  \xfe").annotate(föö=Value(1))
         sql, params = data.query.sql_with_params()
         with data.query.get_compiler("default").execute_sql(CURSOR) as cursor:
             last_sql = cursor.db.ops.last_executed_query(cursor, sql, params)
