1da8b32b53c0a90b95ae388bdb35768f5e52c73a
David Sanders <shang.xiao.sanders@gmail.com>
2023-03-28 19:46:24 +1100

    Update tests/template_tests/test_context.py
    Co-authored-by: Mariusz Felisiak <felisiak.mariusz@gmail.com>

--- tests/template_tests/test_context.py
+++ tests/template_tests/test_context.py
@@ -285,7 +285,6 @@ class RequestContextTests(SimpleTestCase):
         TEMPLATES=[
             {
                 "BACKEND": "django.template.backends.django.DjangoTemplates",
-                "APP_DIRS": True,
                 "OPTIONS": {
                     "context_processors": [
                         "django.template.context_processors.request",
