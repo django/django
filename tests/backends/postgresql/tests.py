e03cc61b283480667e546a5d209db081d8f9798f
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2023-04-07 08:21:50 +0200

    Fixed #34470 -- Enforced UTF-8 encoding on PostgreSQL.
    Regression in 6a2165816394ab4bb259f6171e82417e098e97a6.

--- tests/backends/postgresql/tests.py
+++ tests/backends/postgresql/tests.py
@@ -323,6 +323,18 @@ class Tests(TestCase):
         finally:
             new_connection.close()
 
+    def test_client_encoding_utf8_enforce(self):
+        new_connection = connection.copy()
+        new_connection.settings_dict["OPTIONS"]["client_encoding"] = "iso-8859-2"
+        try:
+            new_connection.connect()
+            if is_psycopg3:
+                self.assertEqual(new_connection.connection.info.encoding, "utf-8")
+            else:
+                self.assertEqual(new_connection.connection.encoding, "UTF8")
+        finally:
+            new_connection.close()
+
     def _select(self, val):
         with connection.cursor() as cursor:
             cursor.execute("SELECT %s::text[]", (val,))
