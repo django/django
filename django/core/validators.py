1fe0b167af3611cca79e8a8092ee929312193c6f
Jacob Rief <jacob.rief@gmail.com>
2023-04-08 22:10:17 +0200

    Fixed #34473 -- Fixed step validation for form fields with non-zero minimum value.

--- django/core/validators.py
+++ django/core/validators.py
@@ -397,8 +397,37 @@ class StepValueValidator(BaseValidator):
     message = _("Ensure this value is a multiple of step size %(limit_value)s.")
     code = "step_size"
 
+    def __init__(self, limit_value, message=None, offset=None):
+        super().__init__(limit_value, message)
+        if offset is not None:
+            self.message = _(
+                "Ensure this value is a multiple of step size %(limit_value)s, "
+                "starting from %(offset)s, e.g. %(offset)s, %(valid_value1)s, "
+                "%(valid_value2)s, and so on."
+            )
+        self.offset = offset
+
+    def __call__(self, value):
+        if self.offset is None:
+            super().__call__(value)
+        else:
+            cleaned = self.clean(value)
+            limit_value = (
+                self.limit_value() if callable(self.limit_value) else self.limit_value
+            )
+            if self.compare(cleaned, limit_value):
+                offset = cleaned.__class__(self.offset)
+                params = {
+                    "limit_value": limit_value,
+                    "offset": offset,
+                    "valid_value1": offset + limit_value,
+                    "valid_value2": offset + 2 * limit_value,
+                }
+                raise ValidationError(self.message, code=self.code, params=params)
+
     def compare(self, a, b):
-        return not math.isclose(math.remainder(a, b), 0, abs_tol=1e-9)
+        offset = 0 if self.offset is None else self.offset
+        return not math.isclose(math.remainder(a - offset, b), 0, abs_tol=1e-9)
 
 
 @deconstructible
--- tests/forms_tests/field_tests/test_floatfield.py
+++ tests/forms_tests/field_tests/test_floatfield.py
@@ -84,6 +84,18 @@ class FloatFieldTest(FormFieldAssertionsMixin, SimpleTestCase):
         self.assertEqual(-1.26, f.clean("-1.26"))
         self.assertEqual(f.step_size, 0.02)
 
+    def test_floatfield_step_size_min_value(self):
+        f = FloatField(step_size=0.02, min_value=0.01)
+        msg = (
+            "Ensure this value is a multiple of step size 0.02, starting from 0.01, "
+            "e.g. 0.01, 0.03, 0.05, and so on."
+        )
+        with self.assertRaisesMessage(ValidationError, msg):
+            f.clean("0.02")
+        self.assertEqual(f.clean("2.33"), 2.33)
+        self.assertEqual(f.clean("0.11"), 0.11)
+        self.assertEqual(f.step_size, 0.02)
+
     def test_floatfield_widget_attrs(self):
         f = FloatField(widget=NumberInput(attrs={"step": 0.01, "max": 1.0, "min": 0.0}))
         self.assertWidgetRendersTo(
