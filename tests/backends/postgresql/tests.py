5995f4f21fada5354d44e403177c5211bed3afb3
Scott Macpherson <scott@zerosleeps.com>
2023-04-13 21:07:32 +1000

    Fixed #34486 -- Fixed DatabaseOperations.compose_sql() crash with no existing database connection on PostgreSQL.
    Regression in 09ffc5c1212d4ced58b708cbbf3dfbfb77b782ca.

--- tests/backends/postgresql/tests.py
+++ tests/backends/postgresql/tests.py
@@ -420,3 +420,13 @@ class Tests(TestCase):
         with self.assertRaisesMessage(NotSupportedError, msg):
             connection.check_database_version_supported()
         self.assertTrue(mocked_get_database_version.called)
+
+    def test_compose_sql_when_no_connection(self):
+        new_connection = connection.copy()
+        try:
+            self.assertEqual(
+                new_connection.ops.compose_sql("SELECT %s", ["test"]),
+                "SELECT 'test'",
+            )
+        finally:
+            new_connection.close()
