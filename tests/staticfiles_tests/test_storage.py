6fa7a21a5638e823da1db9c4bb48405f5f41436c
Hielke Walinga <hielkewalinga@gmail.com>
2023-04-17 14:30:10 +0200

    Fixed #34496 -- Fixed handling source maps with data URI in ManifestStaticFilesStorage.
    Regression in 781b44240a06f0c868254f40f36ce46c927f56d1.

--- tests/staticfiles_tests/test_storage.py
+++ tests/staticfiles_tests/test_storage.py
@@ -269,6 +269,18 @@ class TestHashedFiles:
             )
         self.assertPostCondition()
 
+    def test_css_source_map_data_uri(self):
+        relpath = self.hashed_file_path("cached/source_map_data_uri.css")
+        self.assertEqual(relpath, "cached/source_map_data_uri.3166be10260d.css")
+        with storage.staticfiles_storage.open(relpath) as relfile:
+            content = relfile.read()
+            source_map_data_uri = (
+                b"/*# sourceMappingURL=data:application/json;charset=utf8;base64,"
+                b"eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIl9zcmMv*/"
+            )
+            self.assertIn(source_map_data_uri, content)
+        self.assertPostCondition()
+
     def test_js_source_map(self):
         relpath = self.hashed_file_path("cached/source_map.js")
         self.assertEqual(relpath, "cached/source_map.cd45b8534a87.js")
@@ -305,6 +317,18 @@ class TestHashedFiles:
                 b"//# sourceMappingURL=source_map.js.99914b932bd3.map",
                 content,
             )
+        self.assertPostCondition()
+
+    def test_js_source_map_data_uri(self):
+        relpath = self.hashed_file_path("cached/source_map_data_uri.js")
+        self.assertEqual(relpath, "cached/source_map_data_uri.a68d23cbf6dd.js")
+        with storage.staticfiles_storage.open(relpath) as relfile:
+            content = relfile.read()
+            source_map_data_uri = (
+                b"//# sourceMappingURL=data:application/json;charset=utf8;base64,"
+                b"eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIl9zcmMv"
+            )
+            self.assertIn(source_map_data_uri, content)
         self.assertPostCondition()
 
     @override_settings(
