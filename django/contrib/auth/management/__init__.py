bd994822b39f20ade7dabce7ad458b1e6c641db8
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2023-04-19 08:56:23 -0500

    Reorder imports in alphabetical order.

--- django/contrib/auth/management/__init__.py
+++ django/contrib/auth/management/__init__.py
@@ -2,8 +2,8 @@
 Creates permissions for all installed apps that need permissions.
 """
 import getpass
-import unicodedata
 import logging
+import unicodedata
 
 from django.apps import apps as global_apps
 from django.contrib.auth import get_permission_codename
