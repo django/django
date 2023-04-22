83c9765f45e4622e4a5af3adcd92263a28b13624
Simon Charette <charette.s@gmail.com>
2023-04-21 23:10:15 -0400

    Refs #33766 -- Removed sql.Query.build_filtered_relation_q().
    It was a copy of sql.Query._add_q that avoided JOIN updates.

--- django/db/models/query_utils.py
+++ django/db/models/query_utils.py
@@ -433,9 +433,13 @@ class FilteredRelation:
 
     def resolve_expression(self, query, reuse, *args, **kwargs):
         clone = self.clone()
-        clone.resolved_condition = query.build_filtered_relation_q(
-            self.condition, reuse=reuse
-        )
+        clone.resolved_condition = query.build_filter(
+            self.condition,
+            can_reuse=reuse,
+            allow_joins=True,
+            split_subq=False,
+            update_join_types=False,
+        )[0]
         return clone
 
     def as_sql(self, compiler, connection):
