c813fb327cb1b09542be89c5ceed367826236bc2
Bakdolot <80908236+Bakdolot@users.noreply.github.com>
2023-04-24 17:14:35 +0600

    Fixed #34481 -- Added system check for reverse related fields in ModelAdmin.list_display.

--- tests/modeladmin/test_checks.py
+++ tests/modeladmin/test_checks.py
@@ -537,7 +537,20 @@ class ListDisplayTests(CheckTestCase):
         self.assertIsInvalid(
             TestModelAdmin,
             ValidationTestModel,
-            "The value of 'list_display[0]' must not be a ManyToManyField.",
+            "The value of 'list_display[0]' must not be a many-to-many field or a "
+            "reverse foreign key.",
+            "admin.E109",
+        )
+
+    def test_invalid_reverse_related_field(self):
+        class TestModelAdmin(ModelAdmin):
+            list_display = ["song_set"]
+
+        self.assertIsInvalid(
+            TestModelAdmin,
+            Band,
+            "The value of 'list_display[0]' must not be a many-to-many field or a "
+            "reverse foreign key.",
             "admin.E109",
         )
 
