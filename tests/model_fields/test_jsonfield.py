django/django

commit 5c23d9f0c32f166c81ecb6f3f01d5077a6084318

Refs #33308 -- Used get_db_prep_value() to adapt JSONFields.

django/db/models/fields/json.py
@@ -6,7 +6,11 @@ from django.db import NotSupportedError, connections, router
 from django.db.models import lookups
 from django.db.models.constants import LOOKUP_SEP
 from django.db.models.fields import TextField
-from django.db.models.lookups import PostgresOperatorLookup, Transform
+from django.db.models.lookups import (
+    FieldGetDbPrepValueMixin,
+    PostgresOperatorLookup,
+    Transform,
+)
 from django.utils.translation import gettext_lazy as _
 
 from . import Field
@@ -92,10 +96,15 @@ class JSONField(CheckFieldDefaultMixin, Field):
     def get_internal_type(self):
         return "JSONField"
 
-    def get_prep_value(self, value):
+    def get_db_prep_value(self, value, connection, prepared=False):
+        if hasattr(value, "as_sql"):
+            return value
+        return connection.ops.adapt_json_value(value, self.encoder)
+
+    def get_db_prep_save(self, value, connection):
         if value is None:
             return value
-        return json.dumps(value, cls=self.encoder)
+        return self.get_db_prep_value(value, connection)
 
     def get_transform(self, name):
         transform = super().get_transform(name)
@@ -141,7 +150,7 @@ def compile_json_path(key_transforms, include_root=True):
     return "".join(path)
 
 
-class DataContains(PostgresOperatorLookup):
+class DataContains(FieldGetDbPrepValueMixin, PostgresOperatorLookup):
     lookup_name = "contains"
     postgres_operator = "@>"
 
@@ -156,7 +165,7 @@ class DataContains(PostgresOperatorLookup):
         return "JSON_CONTAINS(%s, %s)" % (lhs, rhs), params
 
 
-class ContainedBy(PostgresOperatorLookup):
+class ContainedBy(FieldGetDbPrepValueMixin, PostgresOperatorLookup):
     lookup_name = "contained_by"
     postgres_operator = "<@"
 

django/db/backends/base/operations.py
@@ -1,5 +1,6 @@
 import datetime
 import decimal
+import json
 from importlib import import_module
 
 import sqlparse
@@ -574,6 +575,9 @@ class BaseDatabaseOperations:
         type for the backend driver.
         """
         return value or None
+
+    def adapt_json_value(self, value, encoder):
+        return json.dumps(value, cls=encoder)
 
     def year_lookup_bounds_for_date_field(self, value, iso_year=False):
         """

django/db/backends/postgresql/operations.py
@@ -1,9 +1,20 @@
+import json
+from functools import lru_cache, partial
+
 from psycopg2.extras import Inet
+from psycopg2.extras import Json as Jsonb
 
 from django.conf import settings
 from django.db.backends.base.operations import BaseDatabaseOperations
 from django.db.backends.utils import split_tzname_delta
 from django.db.models.constants import OnConflict
+
+
+@lru_cache
+def get_json_dumps(encoder):
+    if encoder is None:
+        return json.dumps
+    return partial(json.dumps, cls=encoder)
 
 
 class DatabaseOperations(BaseDatabaseOperations):
@@ -307,6 +318,9 @@ class DatabaseOperations(BaseDatabaseOperations):
         if value:
             return Inet(value)
         return None
+
+    def adapt_json_value(self, value, encoder):
+        return Jsonb(value, dumps=get_json_dumps(encoder))
 
     def subtract_temporals(self, internal_type, lhs, rhs):
         if internal_type == "DateField":

commit 0ec60661e61b153e6bcec64649b1b7f524eb3e18

Fixed #34539 -- Restored get_prep_value() call when adapting JSONFields.
Regression in 5c23d9f0c32f166c81ecb6f3f01d5077a6084318.

AUTHORS
@@ -532,6 +532,7 @@ answer newbie questions, and generally made Django that much better:
     Julia Elman
     Julia Matsieva <julia.matsieva@gmail.com>
     Julian Bez
+    Julie Rymer <rymerjulie.pro@gmail.com>
     Julien Phalip <jphalip@gmail.com>
     Junyoung Choi <cupjoo@gmail.com>
     junzhang.jn@gmail.com

django/db/models/fields/json.py
@@ -99,6 +99,8 @@ class JSONField(CheckFieldDefaultMixin, Field):
         return "JSONField"
 
     def get_db_prep_value(self, value, connection, prepared=False):
+        if not prepared:
+            value = self.get_prep_value(value)
         # RemovedInDjango51Warning: When the deprecation ends, replace with:
         # if (
         #     isinstance(value, expressions.Value)

tests/model_fields/test_jsonfield.py
@@ -103,6 +103,29 @@ class TestMethods(SimpleTestCase):
         with self.assertRaisesMessage(TypeError, msg):
             KeyTransformTextLookupMixin(transform)
 
+    def test_get_prep_value(self):
+        class JSONFieldGetPrepValue(models.JSONField):
+            def get_prep_value(self, value):
+                if value is True:
+                    return {"value": True}
+                return value
+
+        def noop_adapt_json_value(value, encoder):
+            return value
+
+        field = JSONFieldGetPrepValue()
+        with mock.patch.object(
+            connection.ops, "adapt_json_value", noop_adapt_json_value
+        ):
+            self.assertEqual(
+                field.get_db_prep_value(True, connection, prepared=False),
+                {"value": True},
+            )
+            self.assertIs(
+                field.get_db_prep_value(True, connection, prepared=True), True
+            )
+            self.assertEqual(field.get_db_prep_value(1, connection, prepared=False), 1)
+
 
 class TestValidation(SimpleTestCase):
     def test_invalid_encoder(self):