061a8a1bd818ca2c8a6493f33cae2379e34e181f
Arthur Moreira <moreirarthur96@gmail.com>
2023-05-19 19:33:51 -0300

    Fixed #34577 -- Added escapeseq template filter.

--- django/template/defaultfilters.py
+++ django/template/defaultfilters.py
@@ -445,6 +445,16 @@ def escape_filter(value):
 
 
 @register.filter(is_safe=True)
+def escapeseq(value):
+    """
+    An "escape" filter for sequences. Mark each element in the sequence,
+    individually, as a string that should be auto-escaped. Return a list with
+    the results.
+    """
+    return [conditional_escape(obj) for obj in value]
+
+
+@register.filter(is_safe=True)
 @stringfilter
 def force_escape(value):
     """
