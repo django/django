9059d9689d07abf7e65a67c7f9308b945c2d37de
Amin <a.aminian321@gmail.com>
2023-05-20 12:31:52 +0330

    default is unsupported for RegrCount

--- tests/postgres_tests/test_aggregates.py
+++ tests/postgres_tests/test_aggregates.py
@@ -929,7 +929,7 @@ class TestStatisticsAggregate(PostgreSQLTestCase):
     def test_regr_count_default(self):
         msg = "RegrCount does not allow default."
         with self.assertRaisesMessage(TypeError, msg):
-            RegrCount(y="int2", x="int1", default=0)
+            RegrCount(y="int2", x="int1", default=1)
 
     def test_regr_intercept_general(self):
         values = StatTestModel.objects.aggregate(
