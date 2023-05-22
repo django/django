c78a4421de0fc3240b91d59e8f9028331777c624
Simon Charette <charette.s@gmail.com>
2023-05-21 23:57:49 -0400

    [4.2.x] Fixed #34551 -- Fixed QuerySet.aggregate() crash when referencing subqueries.
    Regression in 59bea9efd2768102fc9d3aedda469502c218e9b7.
    
    Refs #28477.
    
    Thanks Denis Roldán and Mariusz for the test.
    
    Backport of e5c844d6f2a4ac6ae674d741b5f1fa2a688cedf4 from main

--- django/db/models/expressions.py
+++ django/db/models/expressions.py
@@ -1506,6 +1506,7 @@ class Subquery(BaseExpression, Combinable):
     template = "(%(subquery)s)"
     contains_aggregate = False
     empty_result_set_value = None
+    subquery = True
 
     def __init__(self, queryset, output_field=None, **extra):
         # Allow the usage of both QuerySet and sql.Query objects.
