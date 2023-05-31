91be6e1818c3cb70890608491eea79e984f98ede
Kacper Wolkiewicz <45897171+wolkiewiczk@users.noreply.github.com>
2023-05-31 12:57:40 +0200

    Fixed #34606 -- Fixed Right() function with zero length on Oracle and SQLite.

--- django/db/models/functions/text.py
+++ django/db/models/functions/text.py
@@ -276,7 +276,9 @@ class Right(Left):
 
     def get_substr(self):
         return Substr(
-            self.source_expressions[0], self.source_expressions[1] * Value(-1)
+            self.source_expressions[0],
+            self.source_expressions[1] * Value(-1),
+            self.source_expressions[1],
         )
 
 
