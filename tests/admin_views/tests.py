0ec212b804c2260d4a21f9f4e6a3e77273a4994d
Cheukting <cheukting.ho@gmail.com>
2023-06-01 12:43:56 +0100

    Fixed #34617 -- adding test for zooming on mobile

--- tests/admin_views/tests.py
+++ tests/admin_views/tests.py
@@ -1505,6 +1505,13 @@ class AdminViewBasicTest(AdminViewBasicTestCase):
             response, '<div class="help" id="id_new_password1_helptext">'
         )
 
+    def test_enable_zooming_on_mobile(self):
+        response = self.client.get(reverse("admin:index"))
+        self.assertContains(
+            response,
+            '<meta name="viewport" content="width=device-width, initial-scale=1.0">',
+        )
+
 
 @override_settings(
     AUTH_PASSWORD_VALIDATORS=[
