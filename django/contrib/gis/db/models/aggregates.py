1b754d638dc5682265b5b4d1fcf5a94d0312113e
Olivier Le Thanh Duong <olivier@lethanh.be>
2023-06-03 15:54:22 +0200

    Fixed #34629 -- Added filtering support to GIS aggregates.

--- django/contrib/gis/db/models/aggregates.py
+++ django/contrib/gis/db/models/aggregates.py
@@ -53,8 +53,8 @@ class GeoAggregate(Aggregate):
         self, query=None, allow_joins=True, reuse=None, summarize=False, for_save=False
     ):
         c = super().resolve_expression(query, allow_joins, reuse, summarize, for_save)
-        for expr in c.get_source_expressions():
-            if not hasattr(expr.field, "geom_type"):
+        for field in c.get_source_fields():
+            if not hasattr(field, "geom_type"):
                 raise ValueError(
                     "Geospatial aggregates only allowed on geometry fields."
                 )
