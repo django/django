f9936deed1ff13b20e18bd9ca2b0750b52706b6c
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2023-06-04 20:49:07 +0200

    Fixed #34620 -- Fixed serialization crash on m2m fields without natural keys when base querysets use select_related().
    Regression in 19e0587ee596debf77540d6a08ccb6507e60b6a7.
    
    Thanks Martin Svoboda for the report.

--- django/core/serializers/xml_serializer.py
+++ django/core/serializers/xml_serializer.py
@@ -155,7 +155,9 @@ class Serializer(base.Serializer):
                     self.xml.addQuickElement("object", attrs={"pk": str(value.pk)})
 
                 def queryset_iterator(obj, field):
-                    return getattr(obj, field.name).only("pk").iterator()
+                    return (
+                        getattr(obj, field.name).select_related().only("pk").iterator()
+                    )
 
             m2m_iter = getattr(obj, "_prefetched_objects_cache", {}).get(
                 field.name,
