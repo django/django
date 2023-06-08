6580ac9be940075dd49e4bf9fd17eae470da4be8
Akash Kumar Sen <Akash-Kumar-Sen@users.noreply.github.com>
2023-06-08 20:40:26 +0530

    Add more tests

--- tests/internal_types/models.py
+++ tests/internal_types/models.py
@@ -13,7 +13,25 @@ class ManyModel(models.Model):
     pass
 
 
+class InheritedForeignkey(models.ForeignKey):
+    pass
+
+
+class InheritedManyToManyField(models.ManyToManyField):
+    pass
+
+
+class InheritedOneToOneField(models.OneToOneField):
+    pass
+
+
 class TestModel(models.Model):
     foreign_test = models.ForeignKey(ForeignModel, on_delete=models.CASCADE)
     many_test = models.ManyToManyField(ManyModel, related_name="testmodels")
     one_test = models.OneToOneField(OneModel, on_delete=models.CASCADE)
+
+
+class TestInheritedKeyModel(models.Model):
+    foreign_test = InheritedForeignkey(ForeignModel, on_delete=models.CASCADE)
+    many_test = InheritedManyToManyField(ManyModel, related_name="another_model")
+    one_test = InheritedOneToOneField(OneModel, on_delete=models.CASCADE)
--- tests/internal_types/tests.py
+++ tests/internal_types/tests.py
@@ -1,6 +1,6 @@
 from django.test import SimpleTestCase
 
-from .models import TestModel
+from .models import TestInheritedKeyModel, TestModel
 
 
 class InternalTypeTests(SimpleTestCase):
@@ -8,6 +8,15 @@ class InternalTypeTests(SimpleTestCase):
         foreign_key_field = TestModel._meta.get_field("foreign_test")
         many_to_many_field = TestModel._meta.get_field("many_test")
         one_to_one_field = TestModel._meta.get_field("one_test")
+
+        self.assertEqual(foreign_key_field.get_internal_type(), "ForeignKey")
+        self.assertEqual(many_to_many_field.get_internal_type(), "ManyToManyField")
+        self.assertEqual(one_to_one_field.get_internal_type(), "OneToOneField")
+
+    def test_internal_types_for_related_fields_on_inheritance(self):
+        foreign_key_field = TestInheritedKeyModel._meta.get_field("foreign_test")
+        many_to_many_field = TestInheritedKeyModel._meta.get_field("many_test")
+        one_to_one_field = TestInheritedKeyModel._meta.get_field("one_test")
 
         self.assertEqual(foreign_key_field.get_internal_type(), "ForeignKey")
         self.assertEqual(many_to_many_field.get_internal_type(), "ManyToManyField")
