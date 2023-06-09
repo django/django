6f98ed40043f7864c7afff8bfddcfd02275f9b7c
Nick Pope <nick@nickpope.me.uk>
2023-06-09 16:02:17 +0100

    Refs #10088 -- Simplified some more select_for_update() tests.

--- tests/filtered_relation/tests.py
+++ tests/filtered_relation/tests.py
@@ -138,26 +138,23 @@ class FilteredRelationTests(TestCase):
 
     @skipUnlessDBFeature("has_select_for_update", "has_select_for_update_of")
     def test_select_related_foreign_key_for_update_of(self):
-        with transaction.atomic():
-            qs = (
-                Book.objects.annotate(
-                    author_join=FilteredRelation("author"),
-                )
-                .select_related("author_join")
-                .select_for_update(of=("self",))
-                .order_by("pk")
+        qs = (
+            Book.objects.annotate(author_join=FilteredRelation("author"))
+            .select_related("author_join")
+            .select_for_update(of=("self",))
+            .order_by("pk")
+        )
+        with transaction.atomic(), self.assertNumQueries(1):
+            self.assertQuerySetEqual(
+                qs,
+                [
+                    (self.book1, self.author1),
+                    (self.book2, self.author2),
+                    (self.book3, self.author2),
+                    (self.book4, self.author1),
+                ],
+                lambda x: (x, x.author_join),
             )
-            with self.assertNumQueries(1):
-                self.assertQuerySetEqual(
-                    qs,
-                    [
-                        (self.book1, self.author1),
-                        (self.book2, self.author2),
-                        (self.book3, self.author2),
-                        (self.book4, self.author1),
-                    ],
-                    lambda x: (x, x.author_join),
-                )
 
     def test_without_join(self):
         self.assertCountEqual(
