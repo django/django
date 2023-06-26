9862187533910c7f1c31fe6384ad12fbb8189873
Andrew Northall <andrew@northall.me.uk>
2023-06-26 19:32:18 +0100

    Add comment explaining the function of traceback=True

--- django/urls/exceptions.py
+++ django/urls/exceptions.py
@@ -3,6 +3,9 @@ from django.http import Http404
 
 class Resolver404(Http404):
     def __init__(self, msg, traceback=True):
+        # traceback changes the handling of Resolver404 when DEBUG=True
+        # traceback=True results in a 500 error page with a traceback
+        # traceback=False results in the standard 404 page
         self.traceback = traceback
         super(Resolver404, self).__init__(msg)
 
