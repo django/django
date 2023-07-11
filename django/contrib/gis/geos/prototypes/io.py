7f2bc365b3652eaa8a8c19e58ddd35f062c68dbc
Olivier Tabone <olivier.tabone@ripplemotion.fr>
2023-07-11 14:13:19 +0200

    Removed redundant check from WKTWriter.__init__().
    This is already checked in trim.setter.

--- django/contrib/gis/geos/prototypes/io.py
+++ django/contrib/gis/geos/prototypes/io.py
@@ -189,8 +189,7 @@ class WKTWriter(IOBase):
 
     def __init__(self, dim=2, trim=False, precision=None):
         super().__init__()
-        if bool(trim) != self._trim:
-            self.trim = trim
+        self.trim = trim
         if precision is not None:
             self.precision = precision
         self.outdim = dim
