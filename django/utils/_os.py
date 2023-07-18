752257e01960b17a6daa45a493fd6f3825e3ce30
geyaning <geyaning@uniontech.com>
2023-07-18 11:17:43 +0800

    Type judgment is done using Python's own duck-typing mechanism

--- django/utils/_os.py
+++ django/utils/_os.py
@@ -55,8 +55,6 @@ def symlinks_supported():
 
 def to_path(value):
     """Convert value to a pathlib.Path instance, if not already a Path."""
-    if isinstance(value, Path):
-        return value
-    elif not isinstance(value, str):
-        raise TypeError("Invalid path type: %s" % type(value).__name__)
-    return Path(value)
+    if not isinstance(value, Path):
+        value = Path(value)
+    return value
