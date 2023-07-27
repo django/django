76c3e310dd37a1d77642a8744db636a3a4337af2
David Sanders <shang.xiao.sanders@gmail.com>
2023-07-27 17:07:48 +1000

    Fixed #34744 -- Prevented recreation of migration for constraints with a dict_keys.
    Co-authored-by: Mariusz Felisiak <felisiak.mariusz@gmail.com>

--- tests/migrations/test_autodetector.py
+++ tests/migrations/test_autodetector.py
@@ -2793,6 +2793,43 @@ class AutodetectorTests(BaseAutodetectorTests):
             ["CreateModel", "AddField", "AddConstraint"],
         )
 
+    def test_add_constraints_with_dict_keys(self):
+        book_types = {"F": "Fantasy", "M": "Mystery"}
+        book_with_type = ModelState(
+            "testapp",
+            "Book",
+            [
+                ("id", models.AutoField(primary_key=True)),
+                ("type", models.CharField(max_length=1)),
+            ],
+            {
+                "constraints": [
+                    models.CheckConstraint(
+                        check=models.Q(type__in=book_types.keys()),
+                        name="book_type_check",
+                    ),
+                ],
+            },
+        )
+        book_with_resolved_type = ModelState(
+            "testapp",
+            "Book",
+            [
+                ("id", models.AutoField(primary_key=True)),
+                ("type", models.CharField(max_length=1)),
+            ],
+            {
+                "constraints": [
+                    models.CheckConstraint(
+                        check=models.Q(("type__in", tuple(book_types))),
+                        name="book_type_check",
+                    ),
+                ],
+            },
+        )
+        changes = self.get_changes([book_with_type], [book_with_resolved_type])
+        self.assertEqual(len(changes), 0)
+
     def test_add_index_with_new_model(self):
         book_with_index_title_and_pony = ModelState(
             "otherapp",
