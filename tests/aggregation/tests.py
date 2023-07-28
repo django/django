4087367ba869be9cf305dac39a8887d4aa4041d2
Simon Charette <charette.s@gmail.com>
2023-07-28 07:45:23 -0400

    Fixed #34748 -- Fixed queryset crash when grouping by a reference in a subquery.
    Regression in dd68af62b2b27ece50d434f6a351877212e15c3f.
    
    Thanks Toan Vuong for the report.

--- tests/aggregation/tests.py
+++ tests/aggregation/tests.py
@@ -2116,6 +2116,16 @@ class AggregateTestCase(TestCase):
             },
         )
 
+    def test_group_by_reference_subquery(self):
+        author_qs = (
+            Author.objects.annotate(publisher_id=F("book__publisher"))
+            .values("publisher_id")
+            .annotate(cnt=Count("*"))
+            .values("publisher_id")
+        )
+        qs = Publisher.objects.filter(pk__in=author_qs)
+        self.assertCountEqual(qs, [self.p1, self.p2, self.p3, self.p4])
+
 
 class AggregateAnnotationPruningTests(TestCase):
     @classmethod
