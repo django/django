1ad7761ee616341295f36c80f78b86ff79d5b513
nessita <124304+nessita@users.noreply.github.com>
2023-07-31 15:17:10 -0300

    Refs #34655 -- Made cosmetic edits to RadioSelect tests.
    Follow up to f9c5958b8fe452983122b6a13c8f806e4e4e1eef.

--- tests/forms_tests/widget_tests/test_radioselect.py
+++ tests/forms_tests/widget_tests/test_radioselect.py
@@ -179,7 +179,7 @@ class RadioSelectTest(ChoiceWidgetTest):
 
     def test_compare_to_str(self):
         """
-        The value is compared to its str()
+        The value is compared to its str().
         """
         html = """
         <div>
@@ -235,21 +235,21 @@ class RadioSelectTest(ChoiceWidgetTest):
                 yield (i, i)
 
         html = """
-       <div>
-         <div>
-           <label><input type="radio" name="num" value="0">0</label>
-         </div>
-         <div>
-           <label><input type="radio" name="num" value="1">1</label>
-         </div>
-         <div>
-           <label><input type="radio" name="num" value="2">2</label>
-         </div>
-         <div>
-           <label><input checked type="radio" name="num" value="3">3</label>
-         </div>
-       </div>
-       """
+        <div>
+          <div>
+            <label><input type="radio" name="num" value="0">0</label>
+          </div>
+          <div>
+            <label><input type="radio" name="num" value="1">1</label>
+          </div>
+          <div>
+            <label><input type="radio" name="num" value="2">2</label>
+          </div>
+          <div>
+            <label><input checked type="radio" name="num" value="3">3</label>
+          </div>
+        </div>
+        """
         widget = self.widget(choices=get_choices())
         self.check_html(widget, "num", 3, html=html)
 
@@ -292,7 +292,7 @@ class RadioSelectTest(ChoiceWidgetTest):
 
     def test_choices_optgroup(self):
         """
-        Choices can be nested one level in order to create HTML optgroups
+        Choices can be nested one level in order to create HTML optgroups.
         """
         html = """
         <div>
