7665525664918411569a1d8c2ac2ecca98c43330
Nick Pope <nick@nickpope.me.uk>
2023-08-08 18:55:10 +0200

    Added IntegerField tests for checks for invalid choices.

--- tests/invalid_models_tests/test_ordinary_fields.py
+++ tests/invalid_models_tests/test_ordinary_fields.py
@@ -845,6 +845,41 @@ class IntegerFieldTests(SimpleTestCase):
                     ],
                 )
 
+    def test_non_iterable_choices(self):
+        class Model(models.Model):
+            field = models.IntegerField(choices=123)
+
+        field = Model._meta.get_field("field")
+        self.assertEqual(
+            field.check(),
+            [
+                Error(
+                    "'choices' must be an iterable (e.g., a list or tuple).",
+                    obj=field,
+                    id="fields.E004",
+                ),
+            ],
+        )
+
+    def test_non_iterable_choices_number(self):
+        """An integer isn't a valid choice pair."""
+
+        class Model(models.Model):
+            field = models.IntegerField(choices=[123])
+
+        field = Model._meta.get_field("field")
+        self.assertEqual(
+            field.check(),
+            [
+                Error(
+                    "'choices' must be an iterable containing (actual value, human "
+                    "readable name) tuples.",
+                    obj=field,
+                    id="fields.E005",
+                ),
+            ],
+        )
+
 
 @isolate_apps("invalid_models_tests")
 class TimeFieldTests(SimpleTestCase):
