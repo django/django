a859c1f406669b718517f79830ac786c51877ebc
Olivier Dalang <olivier.dalang@gmail.com>
2023-08-16 22:08:28 +0200

    add test for choicefield use case

---
+++ tests/contribute_to_meta/apps/modelchoicefield/models.py
@@ -0,0 +1,1 @@
+# (models will be created dynamically)
--- tests/contribute_to_meta/fields.py
+++ tests/contribute_to_meta/fields.py
@@ -12,3 +12,18 @@ class ConstraintField(models.CharField):
                 name=f"test_constraint_{cls.__name__.lower()}",
             )
         )
+
+
+class ChoiceField(models.CharField):
+    """A field that contributes a DB contraint to the model's Meta
+    to enforce choice values"""
+
+    def contribute_to_class(self, cls, name, private_only=False):
+        super().contribute_to_class(cls, name, private_only)
+        accepted_values = [c[0] for c in self.choices]
+        cls._meta.constraints.append(
+            models.CheckConstraint(
+                check=models.Q(**{f"{name}__in": accepted_values}),
+                name=f"%(app_label)s_%(class)s_{name}_valid_choices",
+            )
+        )
