21e9453126fd8e886b08f16e251bd31ba2a677ea
Aaron Linville <aaron@linville.org>
2023-08-17 18:06:25 -0400

    Fixed #24018 -- Allowed setting pragma options on SQLite.

--- tests/backends/sqlite/tests.py
+++ tests/backends/sqlite/tests.py
@@ -116,6 +116,29 @@ class Tests(TestCase):
             connection.check_database_version_supported()
         self.assertTrue(mocked_get_database_version.called)
 
+    def test_init_command(self):
+        settings_dict = {
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3",
+                "NAME": ":memory:",
+                "OPTIONS": {
+                    "init_command": "PRAGMA synchronous=3; PRAGMA cache_size=2000;",
+                },
+            }
+        }
+        connections = ConnectionHandler(settings_dict)
+        connections["default"].ensure_connection()
+        try:
+            with connections["default"].cursor() as cursor:
+                cursor.execute("PRAGMA synchronous")
+                value = cursor.fetchone()[0]
+                self.assertEqual(value, 3)
+                cursor.execute("PRAGMA cache_size")
+                value = cursor.fetchone()[0]
+                self.assertEqual(value, 2000)
+        finally:
+            connections["default"].close()
+
 
 @unittest.skipUnless(connection.vendor == "sqlite", "SQLite tests")
 @isolate_apps("backends")
