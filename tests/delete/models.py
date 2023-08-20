5f6b7015a5614e834262b429fd28e1227d3523fb
Akash-Kumar-Sen <akashkumarsen4@gmail.com>
2023-08-20 18:44:13 +0530

    Change the location for tests

--- tests/delete/models.py
+++ tests/delete/models.py
@@ -241,3 +241,112 @@ class GenericDeleteBottomParent(models.Model):
     generic_delete_bottom = models.ForeignKey(
         GenericDeleteBottom, on_delete=models.CASCADE
     )
+
+
+class Foo(models.Model):
+    """Initial model named Foo"""
+
+
+class Bar(models.Model):
+    """First level foreignkey child for Foo
+    Implemented using database level cascading"""
+
+    foo = models.ForeignKey(
+        Foo,
+        on_delete=models.DB_CASCADE,
+    )
+
+
+class Baz(models.Model):
+    """Second level foreignkey child for Foo
+    Implemented using in DB cascading"""
+
+    bar = models.ForeignKey(
+        Bar,
+        on_delete=models.DB_CASCADE,
+    )
+
+
+class RestrictBar(models.Model):
+    """First level child of foo with cascading set to restrict"""
+
+    foo = models.ForeignKey(
+        Foo,
+        on_delete=models.DB_RESTRICT,
+    )
+
+
+class RestrictBaz(models.Model):
+    """Second level child of foo with cascading set to restrict"""
+
+    bar = models.ForeignKey(
+        Bar,
+        on_delete=models.DB_RESTRICT,
+    )
+
+
+class SetNullBar(models.Model):
+    """First level child of foo with cascading set to null"""
+
+    foo = models.ForeignKey(
+        Foo,
+        on_delete=models.DB_SET_NULL,
+        null=True,
+    )
+    another_field = models.CharField(max_length=20)
+
+
+class SetNullBaz(models.Model):
+    """Second level child of foo with cascading set to null"""
+
+    bar = models.ForeignKey(
+        Bar,
+        on_delete=models.DB_SET_NULL,
+        null=True,
+    )
+    another_field = models.CharField(max_length=20)
+
+
+class AnotherSetNullBaz(models.Model):
+    """Second level child of foo with cascading set to null"""
+
+    setnullbar = models.ForeignKey(
+        SetNullBar,
+        on_delete=models.DB_SET_NULL,
+        null=True,
+    )
+    another_field = models.CharField(max_length=20)
+
+
+class DBLevelChild(Foo):
+    grandparent_ptr = models.OneToOneField(
+        Foo, primary_key=True, parent_link=True, on_delete=models.DB_RESTRICT
+    )
+
+
+class NormalParent(Foo):
+    grandparent_ptr = models.OneToOneField(
+        Foo, primary_key=True, parent_link=True, on_delete=models.DB_CASCADE
+    )
+
+
+class DiamondParent(Foo):
+    gp_ptr = models.OneToOneField(
+        Foo, primary_key=True, parent_link=True, on_delete=models.DB_CASCADE
+    )
+
+
+class DiamondChild(NormalParent, DiamondParent):
+    parent_ptr = models.OneToOneField(
+        NormalParent, primary_key=True, parent_link=True, on_delete=models.DB_CASCADE
+    )
+
+    diamondparent_ptr = models.OneToOneField(
+        DiamondParent, parent_link=True, on_delete=models.DB_CASCADE
+    )
+
+
+class DBDefaultsFK(models.Model):
+    language_code = models.ForeignKey(
+        Foo, db_default=1, on_delete=models.DB_SET_DEFAULT
+    )
