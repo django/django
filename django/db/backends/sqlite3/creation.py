e5dfc624fe20b5f9d83d4395ebce3ad3bf4d801f
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2023-08-20 21:53:40 +0200

    Fixed DatabaseCreation.get_test_db_clone_settings() for empty database name on SQLite.
    Empty string should be considered an in-memory SQLite database.

--- django/db/backends/sqlite3/creation.py
+++ django/db/backends/sqlite3/creation.py
@@ -53,7 +53,7 @@ class DatabaseCreation(BaseDatabaseCreation):
 
     def get_test_db_clone_settings(self, suffix):
         orig_settings_dict = self.connection.settings_dict
-        source_database_name = orig_settings_dict["NAME"]
+        source_database_name = orig_settings_dict["NAME"] or ":memory:"
 
         if not self.is_in_memory_db(source_database_name):
             root, ext = os.path.splitext(source_database_name)
