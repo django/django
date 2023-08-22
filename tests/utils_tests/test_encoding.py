3f41d6d62929dfe53eda8109b3b836f26645bdce
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2023-08-22 08:53:03 +0200

    Fixed CVE-2023-41164 -- Fixed potential DoS in django.utils.encoding.uri_to_iri().
    Thanks MProgrammer (https://hackerone.com/mprogrammer) for the report.
    
    Co-authored-by: nessita <124304+nessita@users.noreply.github.com>

--- tests/utils_tests/test_encoding.py
+++ tests/utils_tests/test_encoding.py
@@ -1,9 +1,10 @@
 import datetime
+import inspect
 import sys
 import unittest
 from pathlib import Path
 from unittest import mock
-from urllib.parse import quote_plus
+from urllib.parse import quote, quote_plus
 
 from django.test import SimpleTestCase
 from django.utils.encoding import (
@@ -119,6 +120,24 @@ class TestEncodingUtils(SimpleTestCase):
             )
         except RecursionError:
             self.fail("Unexpected RecursionError raised.")
+
+    def test_repercent_broken_unicode_small_fragments(self):
+        data = b"test\xfctest\xfctest\xfc"
+        decoded_paths = []
+
+        def mock_quote(*args, **kwargs):
+            # The second frame is the call to repercent_broken_unicode().
+            decoded_paths.append(inspect.currentframe().f_back.f_locals["path"])
+            return quote(*args, **kwargs)
+
+        with mock.patch("django.utils.encoding.quote", mock_quote):
+            self.assertEqual(repercent_broken_unicode(data), b"test%FCtest%FCtest%FC")
+
+        # decode() is called on smaller fragment of the path each time.
+        self.assertEqual(
+            decoded_paths,
+            [b"test\xfctest\xfctest\xfc", b"test\xfctest\xfc", b"test\xfc"],
+        )
 
 
 class TestRFC3987IEncodingUtils(unittest.TestCase):
