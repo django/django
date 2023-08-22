48a1929ca050f1333927860ff561f6371706968a
konsti <konstin@mailbox.org>
2023-08-22 12:42:57 +0200

    Removed unnecessary trailing commas in tests.

--- tests/serializers/tests.py
+++ tests/serializers/tests.py
@@ -73,7 +73,7 @@ class SerializerRegistrationTests(SimpleTestCase):
         all_formats = set(serializers.get_serializer_formats())
         public_formats = set(serializers.get_public_serializer_formats())
 
-        self.assertIn("xml", all_formats),
+        self.assertIn("xml", all_formats)
         self.assertIn("xml", public_formats)
 
         self.assertIn("json2", all_formats)
