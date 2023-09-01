django/django

commit c67ea79aa981ae82595d89f8018a41fcd842e7c9

Fixed #34368 -- Made subquery raise NotSupportedError when referencing outer window expression.
Regression in f387d024fc75569d2a4a338bfda76cc2f328f627.

Co-authored-by: Jannis Vajen <jvajen@gmail.com>

django/db/models/expressions.py
@@ -857,6 +857,11 @@ class ResolvedOuterRef(F):
 
     def resolve_expression(self, *args, **kwargs):
         col = super().resolve_expression(*args, **kwargs)
+        if col.contains_over_clause:
+            raise NotSupportedError(
+                f"Referencing outer query window expression is not supported: "
+                f"{self.name}."
+            )
         # FIXME: Rename possibly_multivalued to multivalued and fix detection
         # for non-multivalued JOINs (e.g. foreign key fields). This should take
         # into account only many-to-many and one-to-many relationships.

tests/expressions_window/tests.py
@@ -1587,6 +1587,25 @@ class WindowUnsupportedTests(TestCase):
                     dense_rank=Window(expression=DenseRank())
                 ).get()
 
+    def test_filter_subquery(self):
+        qs = Employee.objects.annotate(
+            department_salary_rank=Window(
+                Rank(), partition_by="department", order_by="-salary"
+            )
+        )
+        msg = (
+            "Referencing outer query window expression is not supported: "
+            "department_salary_rank."
+        )
+        with self.assertRaisesMessage(NotSupportedError, msg):
+            qs.annotate(
+                employee_name=Subquery(
+                    Employee.objects.filter(
+                        age=OuterRef("department_salary_rank")
+                    ).values("name")[:1]
+                )
+            )
+
 
 class NonQueryWindowTests(SimpleTestCase):
     def test_window_repr(self):

django/db/models/sql/compiler.py
@@ -676,7 +676,7 @@ class SQLCompiler:
                 )
             )
         inner_query_compiler = inner_query.get_compiler(
-            self.using, elide_empty=self.elide_empty
+            self.using, connection=self.connection, elide_empty=self.elide_empty
         )
         inner_sql, inner_params = inner_query_compiler.as_sql(
             # The limits must be applied to the outer query to avoid pruning

commit 9cc0d7f7f85cecc3ad15bbc471fe6a08e4f515b6

Fixed #34803 -- Fixed queryset crash when filtering againts deeply nested OuterRef annotations.
Thanks Pierre-Nicolas Rigal for the report.

Regression in c67ea79aa981ae82595d89f8018a41fcd842e7c9.

AUTHORS
@@ -1033,6 +1033,7 @@ answer newbie questions, and generally made Django that much better:
     Will Ayd <william.ayd@icloud.com>
     William Schwartz <wkschwartz@gmail.com>
     Will Hardy <django@willhardy.com.au>
+    Will Zhao <www.gnomeek@gmail.com>
     Wilson Miner <wminer@gmail.com>
     Wim Glenn <hey@wimglenn.com>
     wojtek

django/db/models/expressions.py
@@ -885,6 +885,7 @@ class ResolvedOuterRef(F):
 
 class OuterRef(F):
     contains_aggregate = False
+    contains_over_clause = False
 
     def resolve_expression(self, *args, **kwargs):
         if isinstance(self.name, self.__class__):

tests/expressions/models.py
@@ -8,6 +8,9 @@ from django.db import models
 
 class Manager(models.Model):
     name = models.CharField(max_length=50)
+    secretary = models.ForeignKey(
+        "Employee", models.CASCADE, null=True, related_name="managers"
+    )
 
 
 class Employee(models.Model):
@@ -15,6 +18,7 @@ class Employee(models.Model):
     lastname = models.CharField(max_length=50)
     salary = models.IntegerField(blank=True, null=True)
     manager = models.ForeignKey(Manager, models.CASCADE, null=True)
+    based_in_eu = models.BooleanField(default=False)
 
     def __str__(self):
         return "%s %s" % (self.firstname, self.lastname)

tests/expressions/tests.py
@@ -870,6 +870,36 @@ class BasicExpressionsTests(TestCase):
         ).filter(ceo_company__isnull=False)
         self.assertEqual(qs.get().ceo_company, "Test GmbH")
 
+    def test_annotation_with_deeply_nested_outerref(self):
+        bob = Employee.objects.create(firstname="Bob", based_in_eu=True)
+        self.max.manager = Manager.objects.create(name="Rock", secretary=bob)
+        self.max.save()
+        qs = Employee.objects.filter(
+            Exists(
+                Manager.objects.filter(
+                    Exists(
+                        Employee.objects.filter(
+                            pk=OuterRef("secretary__pk"),
+                        )
+                        .annotate(
+                            secretary_based_in_eu=OuterRef(OuterRef("based_in_eu"))
+                        )
+                        .filter(
+                            Exists(
+                                Company.objects.filter(
+                                    # Inner OuterRef refers to an outer
+                                    # OuterRef (not ResolvedOuterRef).
+                                    based_in_eu=OuterRef("secretary_based_in_eu")
+                                )
+                            )
+                        )
+                    ),
+                    secretary__pk=OuterRef("pk"),
+                )
+            )
+        )
+        self.assertEqual(qs.get(), bob)
+
     def test_pickle_expression(self):
         expr = Value(1)
         expr.convert_value  # populate cached property