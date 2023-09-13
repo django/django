264656ef80eceb04f575a900d13934fd7831b92d
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2023-09-13 10:40:59 +0200

    Corrected logger in CsrfViewMiddlewareTestMixin.test_ensures_csrf_cookie_no_logging().
    Logger was changed in 55fec16aafed30a9daa06d6ecdf8ca3ad361279e.

--- tests/csrf_tests/tests.py
+++ tests/csrf_tests/tests.py
@@ -857,7 +857,7 @@ class CsrfViewMiddlewareTestMixin(CsrfFunctionTestMixin):
         """
         ensure_csrf_cookie() doesn't log warnings (#19436).
         """
-        with self.assertNoLogs("django.request", "WARNING"):
+        with self.assertNoLogs("django.security.csrf", "WARNING"):
             req = self._get_request()
             ensure_csrf_cookie_view(req)
 
