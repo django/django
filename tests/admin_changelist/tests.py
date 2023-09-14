2da526b9ba955b9919104c87ce5aa8650533c19f
Lemuel Sta Ana <lpgstaana@lemuels-mbp.home>
2023-09-13 22:37:07 -0400

    Fixed #34834 -- Added role="search" to the admin changelist search form.

--- tests/admin_changelist/tests.py
+++ tests/admin_changelist/tests.py
@@ -1585,6 +1585,16 @@ class ChangeListTests(TestCase):
             'aria-describedby="searchbar_helptext">',
         )
 
+    def test_search_role(self):
+        m = BandAdmin(Band, custom_site)
+        m.search_fields = ["name"]
+        request = self._mocked_authenticated_request("/band/", self.superuser)
+        response = m.changelist_view(request)
+        self.assertContains(
+            response,
+            '<form id="changelist-search" method="get" role="search">',
+        )
+
     def test_search_bar_total_link_preserves_options(self):
         self.client.force_login(self.superuser)
         url = reverse("admin:auth_user_changelist")
