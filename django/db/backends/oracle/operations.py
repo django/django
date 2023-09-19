324797eafc5c088013be5ac4e9cf59d97b1b74a2
Lily Foote <code@lilyf.org>
2023-09-19 11:13:27 +0100

    Delete unused import

--- django/db/backends/oracle/operations.py
+++ django/db/backends/oracle/operations.py
@@ -8,7 +8,6 @@ from django.db.backends.base.operations import BaseDatabaseOperations
 from django.db.backends.utils import split_tzname_delta, strip_quotes, truncate_name
 from django.db.models import AutoField, Exists, ExpressionWrapper, Lookup
 from django.db.models.expressions import RawSQL
-from django.db.models.lookups import Exact
 from django.db.models.sql.where import WhereNode
 from django.utils import timezone
 from django.utils.encoding import force_bytes, force_str
