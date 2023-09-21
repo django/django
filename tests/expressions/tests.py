4de31ec680df062e5964b630f1b881ead5004e15
toan <toan.vuong@hyperscience.com>
2023-09-21 15:51:45 -0700

    Fixed #34858 -- Corrected resolving output_field for PositiveIntegerField.
    Regression in 40b8a6174f001a310aa33f7880db0efeeb04d4c4.

--- tests/expressions/tests.py
+++ tests/expressions/tests.py
@@ -34,6 +34,7 @@ from django.db.models import (
     Model,
     OrderBy,
     OuterRef,
+    PositiveIntegerField,
     Q,
     StdDev,
     Subquery,
@@ -2455,6 +2456,23 @@ class CombinableTests(SimpleTestCase):
 
 
 class CombinedExpressionTests(SimpleTestCase):
+    def test_resolve_output_field_positive_integer(self):
+        connectors = [
+            Combinable.ADD,
+            Combinable.MUL,
+            Combinable.DIV,
+            Combinable.MOD,
+            Combinable.POW,
+        ]
+        for connector in connectors:
+            with self.subTest(connector=connector):
+                expr = CombinedExpression(
+                    Expression(PositiveIntegerField()),
+                    connector,
+                    Expression(PositiveIntegerField()),
+                )
+                self.assertIsInstance(expr.output_field, PositiveIntegerField)
+
     def test_resolve_output_field_number(self):
         tests = [
             (IntegerField, AutoField, IntegerField),
