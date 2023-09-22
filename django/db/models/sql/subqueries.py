c3a0ff4ede372a87bded3a65361cf28e9404081f
Aivars Kalvans <aivars.kalvans@gmail.com>
2023-09-22 20:10:49 +0300

    Fixed #32406 Support UPDATE RETURNING
    Implement a new QuerySet.update_returning() that executes a
    QuerySet.update() and returns modified rows on databases that support
    UPDATE RETURNING (SQLite, PostgreSQL, Oracle)

--- django/db/models/sql/subqueries.py
+++ django/db/models/sql/subqueries.py
@@ -59,12 +59,14 @@ class UpdateQuery(Query):
         Run on initialization and at the end of chaining. Any attributes that
         would normally be set in __init__() should go here instead.
         """
-        self.values = []
+        if not hasattr(self, "values"):
+            self.values = []
         self.related_ids = None
         self.related_updates = {}
 
     def clone(self):
         obj = super().clone()
+        obj.values = list(self.values)
         obj.related_updates = self.related_updates.copy()
         return obj
 
