django/django

commit fbd16438f46bc2128926958ad24331da5d1b406f

Fixed #33143 -- Raised RuntimeWarning when performing import-time queries.

django/db/backends/utils.py
@@ -3,9 +3,11 @@ import decimal
 import functools
 import logging
 import time
+import warnings
 from contextlib import contextmanager
 from hashlib import md5
 
+from django.apps import apps
 from django.db import NotSupportedError
 from django.utils.dateparse import parse_time
 
@@ -18,6 +20,12 @@ class CursorWrapper:
         self.db = db
 
     WRAP_ERROR_ATTRS = frozenset(["fetchone", "fetchmany", "fetchall", "nextset"])
+
+    APPS_NOT_READY_WARNING_MSG = (
+        "Accessing the database during app initialization is discouraged. To fix this "
+        "warning, avoid executing queries in AppConfig.ready() or when your app "
+        "modules are imported."
+    )
 
     def __getattr__(self, attr):
         cursor_attr = getattr(self.cursor, attr)
@@ -53,6 +61,8 @@ class CursorWrapper:
                 "Keyword parameters for callproc are not supported on this "
                 "database backend."
             )
+        if not apps.ready:
+            warnings.warn(self.APPS_NOT_READY_WARNING_MSG, category=RuntimeWarning)
         self.db.validate_no_broken_transaction()
         with self.db.wrap_database_errors:
             if params is None and kparams is None:
@@ -80,6 +90,8 @@ class CursorWrapper:
         return executor(sql, params, many, context)
 
     def _execute(self, sql, params, *ignored_wrapper_args):
+        if not apps.ready:
+            warnings.warn(self.APPS_NOT_READY_WARNING_MSG, category=RuntimeWarning)
         self.db.validate_no_broken_transaction()
         with self.db.wrap_database_errors:
             if params is None:
@@ -89,6 +101,8 @@ class CursorWrapper:
                 return self.cursor.execute(sql, params)
 
     def _executemany(self, sql, param_list, *ignored_wrapper_args):
+        if not apps.ready:
+            warnings.warn(self.APPS_NOT_READY_WARNING_MSG, category=RuntimeWarning)
         self.db.validate_no_broken_transaction()
         with self.db.wrap_database_errors:
             return self.cursor.executemany(sql, param_list)

tests/apps/tests.py
@@ -1,11 +1,18 @@
 import os
+from unittest.mock import patch
 
+import django
 from django.apps import AppConfig, apps
 from django.apps.registry import Apps
 from django.contrib.admin.models import LogEntry
 from django.core.exceptions import AppRegistryNotReady, ImproperlyConfigured
-from django.db import models
-from django.test import SimpleTestCase, override_settings
+from django.db import connections, models
+from django.test import (
+    SimpleTestCase,
+    TransactionTestCase,
+    override_settings,
+    skipUnlessDBFeature,
+)
 from django.test.utils import extend_sys_path, isolate_apps
 
 from .models import SoAlternative, TotallyNormal, new_apps
@@ -539,3 +546,77 @@ class NamespacePackageAppTests(SimpleTestCase):
             with self.settings(INSTALLED_APPS=["nsapp.apps.NSAppConfig"]):
                 app_config = apps.get_app_config("nsapp")
                 self.assertEqual(app_config.path, self.app_path)
+
+
+class QueryPerformingAppTests(TransactionTestCase):
+    available_apps = ["apps"]
+    databases = {"default", "other"}
+    expected_msg = (
+        "Accessing the database during app initialization is discouraged. To fix this "
+        "warning, avoid executing queries in AppConfig.ready() or when your app "
+        "modules are imported."
+    )
+
+    def test_query_default_database_using_model(self):
+        query_results = self.run_setup("QueryDefaultDatabaseModelAppConfig")
+        self.assertSequenceEqual(query_results, [("new name",)])
+
+    def test_query_other_database_using_model(self):
+        query_results = self.run_setup("QueryOtherDatabaseModelAppConfig")
+        self.assertSequenceEqual(query_results, [("new name",)])
+
+    def test_query_default_database_using_cursor(self):
+        query_results = self.run_setup("QueryDefaultDatabaseCursorAppConfig")
+        self.assertSequenceEqual(query_results, [(42,)])
+
+    def test_query_other_database_using_cursor(self):
+        query_results = self.run_setup("QueryOtherDatabaseCursorAppConfig")
+        self.assertSequenceEqual(query_results, [(42,)])
+
+    def test_query_many_default_database_using_cursor(self):
+        self.run_setup("QueryDefaultDatabaseCursorManyAppConfig")
+
+    def test_query_many_other_database_using_cursor(self):
+        self.run_setup("QueryOtherDatabaseCursorManyAppConfig")
+
+    @skipUnlessDBFeature("create_test_procedure_without_params_sql")
+    def test_query_default_database_using_stored_procedure(self):
+        connection = connections["default"]
+        with connection.cursor() as cursor:
+            cursor.execute(connection.features.create_test_procedure_without_params_sql)
+
+        try:
+            self.run_setup("QueryDefaultDatabaseStoredProcedureAppConfig")
+        finally:
+            with connection.schema_editor() as editor:
+                editor.remove_procedure("test_procedure")
+
+    @skipUnlessDBFeature("create_test_procedure_without_params_sql")
+    def test_query_other_database_using_stored_procedure(self):
+        connection = connections["other"]
+        with connection.cursor() as cursor:
+            cursor.execute(connection.features.create_test_procedure_without_params_sql)
+
+        try:
+            self.run_setup("QueryOtherDatabaseStoredProcedureAppConfig")
+        finally:
+            with connection.schema_editor() as editor:
+                editor.remove_procedure("test_procedure")
+
+    def run_setup(self, app_config_name):
+        custom_settings = override_settings(
+            INSTALLED_APPS=[f"apps.query_performing_app.apps.{app_config_name}"]
+        )
+        # Ignore the RuntimeWarning, as override_settings.enable() calls
+        # AppConfig.ready() which will trigger the warning.
+        with self.assertWarnsMessage(RuntimeWarning, self.expected_msg):
+            custom_settings.enable()
+        try:
+            with patch.multiple(apps, ready=False, loading=False, app_configs={}):
+                with self.assertWarnsMessage(RuntimeWarning, self.expected_msg):
+                    django.setup()
+
+                app_config = apps.get_app_config("query_performing_app")
+                return app_config.query_results
+        finally:
+            custom_settings.disable()

tests/apps/query_performing_app/apps.py
@@ -1,0 +1,92 @@
+from django.apps import AppConfig
+from django.db import connections
+
+
+class BaseAppConfig(AppConfig):
+    name = "apps.query_performing_app"
+    database = "default"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.query_results = []
+
+    def ready(self):
+        self.query_results = []
+        self._perform_query()
+
+    def _perform_query(self):
+        raise NotImplementedError
+
+
+class ModelQueryAppConfig(BaseAppConfig):
+    def _perform_query(self):
+        from ..models import TotallyNormal
+
+        queryset = TotallyNormal.objects.using(self.database)
+        queryset.update_or_create(name="new name")
+        self.query_results = list(queryset.values_list("name"))
+
+
+class QueryDefaultDatabaseModelAppConfig(ModelQueryAppConfig):
+    database = "default"
+
+
+class QueryOtherDatabaseModelAppConfig(ModelQueryAppConfig):
+    database = "other"
+
+
+class CursorQueryAppConfig(BaseAppConfig):
+    def _perform_query(self):
+        connection = connections[self.database]
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT 42" + connection.features.bare_select_suffix)
+            self.query_results = cursor.fetchall()
+
+
+class QueryDefaultDatabaseCursorAppConfig(CursorQueryAppConfig):
+    database = "default"
+
+
+class QueryOtherDatabaseCursorAppConfig(CursorQueryAppConfig):
+    database = "other"
+
+
+class CursorQueryManyAppConfig(BaseAppConfig):
+    def _perform_query(self):
+        from ..models import TotallyNormal
+
+        connection = connections[self.database]
+        table_meta = TotallyNormal._meta
+        with connection.cursor() as cursor:
+            cursor.executemany(
+                "INSERT INTO %s (%s) VALUES(%%s)"
+                % (
+                    connection.introspection.identifier_converter(table_meta.db_table),
+                    connection.ops.quote_name(table_meta.get_field("name").column),
+                ),
+                [("test name 1",), ("test name 2",)],
+            )
+            self.query_results = []
+
+
+class QueryDefaultDatabaseCursorManyAppConfig(CursorQueryManyAppConfig):
+    database = "default"
+
+
+class QueryOtherDatabaseCursorManyAppConfig(CursorQueryManyAppConfig):
+    database = "other"
+
+
+class StoredProcedureQueryAppConfig(BaseAppConfig):
+    def _perform_query(self):
+        with connections[self.database].cursor() as cursor:
+            cursor.callproc("test_procedure")
+            self.query_results = []
+
+
+class QueryDefaultDatabaseStoredProcedureAppConfig(StoredProcedureQueryAppConfig):
+    database = "default"
+
+
+class QueryOtherDatabaseStoredProcedureAppConfig(StoredProcedureQueryAppConfig):
+    database = "other"

tests/inspectdb/models.py
@@ -1,5 +1,6 @@
 from django.db import connection, models
 from django.db.models.functions import Lower
+from django.utils.functional import SimpleLazyObject
 
 
 class People(models.Model):
@@ -94,7 +95,9 @@ class JSONFieldColumnType(models.Model):
         }
 
 
-test_collation = connection.features.test_collations.get("non_default")
+test_collation = SimpleLazyObject(
+    lambda: connection.features.test_collations.get("non_default")
+)
 
 
 class CharFieldDbCollation(models.Model):

commit 4f2ae0644de34791b84b7beb11a966ce4bc48fb6

Fixed #34849 -- Avoided raising RuntimeWarning about import-time queries when apps are reinitialized with test tools.
Regression in fbd16438f46bc2128926958ad24331da5d1b406f.

django/db/backends/utils.py
@@ -61,7 +61,9 @@ class CursorWrapper:
                 "Keyword parameters for callproc are not supported on this "
                 "database backend."
             )
-        if not apps.ready:
+        # Raise a warning during app initialization (stored_app_configs is only
+        # ever set during testing).
+        if not apps.ready and not apps.stored_app_configs:
             warnings.warn(self.APPS_NOT_READY_WARNING_MSG, category=RuntimeWarning)
         self.db.validate_no_broken_transaction()
         with self.db.wrap_database_errors:
@@ -90,7 +92,9 @@ class CursorWrapper:
         return executor(sql, params, many, context)
 
     def _execute(self, sql, params, *ignored_wrapper_args):
-        if not apps.ready:
+        # Raise a warning during app initialization (stored_app_configs is only
+        # ever set during testing).
+        if not apps.ready and not apps.stored_app_configs:
             warnings.warn(self.APPS_NOT_READY_WARNING_MSG, category=RuntimeWarning)
         self.db.validate_no_broken_transaction()
         with self.db.wrap_database_errors:
@@ -101,7 +105,9 @@ class CursorWrapper:
                 return self.cursor.execute(sql, params)
 
     def _executemany(self, sql, param_list, *ignored_wrapper_args):
-        if not apps.ready:
+        # Raise a warning during app initialization (stored_app_configs is only
+        # ever set during testing).
+        if not apps.ready and not apps.stored_app_configs:
             warnings.warn(self.APPS_NOT_READY_WARNING_MSG, category=RuntimeWarning)
         self.db.validate_no_broken_transaction()
         with self.db.wrap_database_errors:

tests/apps/tests.py
@@ -607,10 +607,9 @@ class QueryPerformingAppTests(TransactionTestCase):
         custom_settings = override_settings(
             INSTALLED_APPS=[f"apps.query_performing_app.apps.{app_config_name}"]
         )
-        # Ignore the RuntimeWarning, as override_settings.enable() calls
-        # AppConfig.ready() which will trigger the warning.
-        with self.assertWarnsMessage(RuntimeWarning, self.expected_msg):
-            custom_settings.enable()
+        custom_settings.enable()
+        old_stored_app_configs = apps.stored_app_configs
+        apps.stored_app_configs = []
         try:
             with patch.multiple(apps, ready=False, loading=False, app_configs={}):
                 with self.assertWarnsMessage(RuntimeWarning, self.expected_msg):
@@ -619,4 +618,5 @@ class QueryPerformingAppTests(TransactionTestCase):
                 app_config = apps.get_app_config("query_performing_app")
                 return app_config.query_results
         finally:
+            setattr(apps, "stored_app_configs", old_stored_app_configs)
             custom_settings.disable()

tests/postgres_tests/test_apps.py
@@ -5,7 +5,7 @@ from django.db import connection
 from django.db.backends.signals import connection_created
 from django.db.migrations.writer import MigrationWriter
 from django.test import TestCase
-from django.test.utils import modify_settings
+from django.test.utils import CaptureQueriesContext, modify_settings, override_settings
 
 try:
     from django.contrib.postgres.fields import (
@@ -14,6 +14,7 @@ try:
         DecimalRangeField,
         IntegerRangeField,
     )
+    from django.contrib.postgres.signals import get_hstore_oids
     from django.db.backends.postgresql.psycopg_any import (
         DateRange,
         DateTimeRange,
@@ -27,6 +28,15 @@ except ImportError:
 
 @unittest.skipUnless(connection.vendor == "postgresql", "PostgreSQL specific tests")
 class PostgresConfigTests(TestCase):
+    def test_install_app_no_warning(self):
+        # Clear cache to force queries when (re)initializing the
+        # "django.contrib.postgres" app.
+        get_hstore_oids.cache_clear()
+        with CaptureQueriesContext(connection) as captured_queries:
+            with override_settings(INSTALLED_APPS=["django.contrib.postgres"]):
+                pass
+        self.assertGreaterEqual(len(captured_queries), 1)
+
     def test_register_type_handlers_connection(self):
         from django.contrib.postgres.signals import register_type_handlers
 