f4e72e6523e6968d9628dfbff914ab57dbf19e6b
faishalmanzar <faishalmanzar@gmail.com>
2023-09-29 03:26:10 +0530

    Fixed #32602 -- Clarified wording of TestCase class.

--- django/test/testcases.py
+++ django/test/testcases.py
@@ -250,7 +250,7 @@ class SimpleTestCase(unittest.TestCase):
     def __call__(self, result=None):
         """
         Wrapper around default __call__ method to perform common Django test
-        set up. This means that user-defined Test Cases aren't required to
+        set up. This means that user-defined TestCases aren't required to
         include a call to super().setUp().
         """
         self._setup_and_call(result)
