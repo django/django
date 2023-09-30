e478bb196cf3562909b3440e5aca429a72143264
theumang100 <theumangpatelu2p@gmail.com>
2023-09-29 19:19:01 -0700

    added "order by" clause

--- django/db/models/sql/query.py
+++ django/db/models/sql/query.py
@@ -986,6 +986,12 @@ class Query(BaseExpression):
             key: col.relabeled_clone(change_map)
             for key, col in self.annotations.items()
         }
+        self.order_by = tuple(
+            expr.relabeled_clone(change_map)
+            if hasattr(expr, "relabeled_clone")
+            else expr
+            for expr in self.order_by
+        )
 
         # 2. Rename the alias in the internal table/alias datastructures.
         for old_alias, new_alias in change_map.items():
@@ -1196,6 +1202,12 @@ class Query(BaseExpression):
 
     def resolve_expression(self, query, *args, **kwargs):
         clone = self.clone()
+        clone.order_by = tuple(
+            expr.resolve_expression(self, *args, *kwargs)
+            if hasattr(expr, "resolve_expression")
+            else expr
+            for expr in clone.order_by
+        )
         # Subqueries need to use a different set of aliases than the outer query.
         clone.bump_prefix(query)
         clone.subquery = True
@@ -1213,6 +1225,12 @@ class Query(BaseExpression):
             if hasattr(resolved, "external_aliases"):
                 resolved.external_aliases.update(clone.external_aliases)
             clone.annotations[key] = resolved
+        clone.order_by = tuple(
+            expr.resolve_expression(query, *args, *kwargs)
+            if hasattr(expr, "resolve_expression")
+            else expr
+            for expr in clone.order_by
+        )
         # Outer query's aliases are considered external.
         for alias, table in query.alias_map.items():
             clone.external_aliases[alias] = (
