176de461485e5bc83f1ced8a471304478c56f5f0
Dr. Shubham Dipt <shubham.dipt@gmail.com>
2023-10-14 00:40:32 +0200

    [fix] Use USERNAME_FIELD to pass in authenticate when custom models are used

--- django/contrib/auth/forms.py
+++ django/contrib/auth/forms.py
@@ -191,7 +191,7 @@ class AuthenticationForm(forms.Form):
         password = self.cleaned_data.get('password')
 
         if username is not None and password:
-            self.user_cache = authenticate(self.request, username=username, password=password)
+            self.user_cache = authenticate(self.request, **{UserModel.USERNAME_FIELD: username}, password=password)
             if self.user_cache is None:
                 raise self.get_invalid_login_error()
             else:
