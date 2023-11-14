9eb22aa25b42b0e2d6642fc1ff6eb7801c6a2635
arkueli <arkueli6@gmail.com>
2023-11-14 18:18:30 +0000

    Fixed #99999 -- Added a shortcut function to make toast.

---
+++ DjangonautsProject/DjangonautsProject/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for DjangonautsProject project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangonautsProject.settings')
+
+application = get_asgi_application()
