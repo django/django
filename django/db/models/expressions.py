911b1619ab62317aaa7594d5fd52c1b8e43969a0
Simon Charette <charette.s@gmail.com>
2023-11-17 19:56:19 -0500

    Refs #34975 -- Handled optional source expressions in Expression.get_refs().
    While no code is directly exercising get_refs in a way that triggers
    a crash some expressions such as Window stash None in source_expressions
    which can obscure the origin of some bugs.
    
    Handling None values like we do in other source_expression traversing
    methods such as .contains_aggregates ensures we don't run into surprises
    in the future where get_refs() might be used for a different purpose.

--- django/db/models/expressions.py
+++ django/db/models/expressions.py
@@ -417,6 +417,8 @@ class BaseExpression:
     def get_refs(self):
         refs = set()
         for expr in self.get_source_expressions():
+            if expr is None:
+                continue
             refs |= expr.get_refs()
         return refs
 
