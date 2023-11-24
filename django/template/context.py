c427ad0daf1082f02d787d122f8356e51c49bf34
Nick Pope <nick@nickpope.me.uk>
2023-11-24 11:09:11 +0000

    Fixed Context.update() to compare using collections.abc.Mapping.
    Checking on `hasattr(…, "__getitem__")` is incorrect as this could allow
    this to pass for an sequence object. Note that `BaseContext` doesn't
    implement `__len__()` so we also need to check for that explicitly.
    
    See https://docs.python.org/3/library/collections.abc.html#collections-abstract-base-classes

--- django/template/context.py
+++ django/template/context.py
@@ -1,3 +1,4 @@
+from collections.abc import Mapping
 from contextlib import contextmanager
 from copy import copy
 
@@ -161,7 +162,7 @@ class Context(BaseContext):
 
     def update(self, other_dict):
         "Push other_dict to the stack of dictionaries in the Context"
-        if not hasattr(other_dict, "__getitem__"):
+        if not isinstance(other_dict, (BaseContext, Mapping)):
             raise TypeError("other_dict must be a mapping (dictionary-like) object.")
         if isinstance(other_dict, BaseContext):
             other_dict = other_dict.dicts[1:].pop()
