0fcd72bc48eade21ce9202d7a71692f9b5a42bd2
Aman Pandey <aman2001mi@gmail.com>
2023-11-27 23:21:33 +0530

    Fixed #34633 -- Made create() method of reverse many-to-one managers clear prefetch_related() cache.

--- django/db/models/fields/related_descriptors.py
+++ django/db/models/fields/related_descriptors.py
@@ -840,6 +840,7 @@ def create_reverse_many_to_one_manager(superclass, rel):
 
         def create(self, **kwargs):
             self._check_fk_val()
+            self._remove_prefetched_objects()
             kwargs[self.field.name] = self.instance
             db = router.db_for_write(self.model, instance=self.instance)
             return super(RelatedManager, self.db_manager(db)).create(**kwargs)
--- tests/many_to_one/tests.py
+++ tests/many_to_one/tests.py
@@ -800,6 +800,14 @@ class ManyToOneTests(TestCase):
         # refs #21563
         self.assertFalse(hasattr(Article(), "reporter"))
 
+    def test_create_after_prefetch(self):
+        c = City.objects.create(name="Musical City")
+        d1 = District.objects.create(name="Ladida", city=c)
+        city = City.objects.prefetch_related("districts").get(id=c.id)
+        self.assertSequenceEqual(city.districts.all(), [d1])
+        d2 = city.districts.create(name="Goa")
+        self.assertSequenceEqual(city.districts.all(), [d1, d2])
+
     def test_clear_after_prefetch(self):
         c = City.objects.create(name="Musical City")
         d = District.objects.create(name="Ladida", city=c)
