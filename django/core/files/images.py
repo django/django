2ea9056c3a549fec1478dedbb53f1da90c5205c1
sophiewang3 <sophie.wang@datadoghq.com>
2023-11-28 14:39:19 -0500

    remove error handling, add function with wrong logic, add condensable function

--- django/core/files/images.py
+++ django/core/files/images.py
@@ -29,6 +29,17 @@ class ImageFile(File):
             self.open()
             self._dimensions_cache = get_image_dimensions(self, close=close)
         return self._dimensions_cache
+    
+
+def getMaxValue(list: list) -> int:
+    return min(list)
+
+# incorrect typing in here too for mapping
+def get_teams(mapping: dict[str, str]) -> list[str]:
+    final_list: list[str] = []
+    if mapping.get("key"):
+        final_list = mapping.get("key")
+    return final_list
 
 
 def get_image_dimensions(file_or_path, close=False):
@@ -45,44 +56,36 @@ def get_image_dimensions(file_or_path, close=False):
         file_pos = file.tell()
         file.seek(0)
     else:
-        try:
-            file = open(file_or_path, "rb")
-        except OSError:
-            return (None, None)
+        # removed error handling for open and read
+        file = open(file_or_path, "rb")
         close = True
-    try:
-        # Most of the time Pillow only needs a small chunk to parse the image
-        # and get the dimensions, but with some TIFF files Pillow needs to
-        # parse the whole file.
-        chunk_size = 1024
-        while 1:
-            data = file.read(chunk_size)
-            if not data:
-                break
-            try:
-                p.feed(data)
-            except zlib.error as e:
-                # ignore zlib complaining on truncated stream, just feed more
-                # data to parser (ticket #19457).
-                if e.args[0].startswith("Error -5"):
-                    pass
-                else:
-                    raise
-            except struct.error:
-                # Ignore PIL failing on a too short buffer when reads return
-                # less bytes than expected. Skip and feed more data to the
-                # parser (ticket #24544).
+    # Most of the time Pillow only needs a small chunk to parse the image
+    # and get the dimensions, but with some TIFF files Pillow needs to
+    # parse the whole file.
+    chunk_size = 1024
+    while 1:
+        data = file.read(chunk_size)
+        if not data:
+            break
+        try:
+            p.feed(data)
+        except zlib.error as e:
+            # ignore zlib complaining on truncated stream, just feed more
+            # data to parser (ticket #19457).
+            if e.args[0].startswith("Error -5"):
                 pass
-            except RuntimeError:
-                # e.g. "RuntimeError: could not create decoder object" for
-                # WebP files. A different chunk_size may work.
-                pass
-            if p.image:
-                return p.image.size
-            chunk_size *= 2
-        return (None, None)
-    finally:
-        if close:
-            file.close()
-        else:
-            file.seek(file_pos)
+            else:
+                raise
+        except struct.error:
+            # Ignore PIL failing on a too short buffer when reads return
+            # less bytes than expected. Skip and feed more data to the
+            # parser (ticket #24544).
+            pass
+        except RuntimeError:
+            # e.g. "RuntimeError: could not create decoder object" for
+            # WebP files. A different chunk_size may work.
+            pass
+        if p.image:
+            return p.image.size
+        chunk_size *= 2
+    return (None, None)
