2f8a799e8d7e8267d5db63e1996c95973a4e6b38
Roberto Aguilar <r@rreboto.com>
2023-12-11 09:59:37 -0500

    Ask Flake8 to ignore the "unused import"

--- django/core/management/commands/show_migrations.py
+++ django/core/management/commands/show_migrations.py
@@ -4,4 +4,5 @@ An alias to the `showmigrations` command
 All other `show*` management commands contain an underscore except this one, which makes
 it harder for muscle memory to call this command correctly.
 """
+# flake8: noqa
 from .showmigrations import Command
