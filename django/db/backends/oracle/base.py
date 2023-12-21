29dcc88bd309fdba8ee695a6178a43531a0886ef
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2023-12-21 06:20:21 +0100

    Fixed #35054 -- Fixed crash on Oracle when fetching JSONFields with oracledb 2.0.0.

--- django/db/backends/oracle/base.py
+++ django/db/backends/oracle/base.py
@@ -453,7 +453,7 @@ class FormatStylePlaceholderCursor:
     def _output_type_handler(cursor, name, defaultType, length, precision, scale):
         """
         Called for each db column fetched from cursors. Return numbers as the
-        appropriate Python type.
+        appropriate Python type, and NCLOB with JSON as strings.
         """
         if defaultType == Database.NUMBER:
             if scale == -127:
@@ -483,6 +483,10 @@ class FormatStylePlaceholderCursor:
                 arraysize=cursor.arraysize,
                 outconverter=outconverter,
             )
+        # oracledb 2.0.0+ returns NLOB columns with IS JSON constraints as
+        # dicts. Use a no-op converter to avoid this.
+        elif defaultType == Database.DB_TYPE_NCLOB:
+            return cursor.var(Database.DB_TYPE_NCLOB, arraysize=cursor.arraysize)
 
     def _format_params(self, params):
         try:
