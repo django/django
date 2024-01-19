002c72c383ecbac578df678d07e43ababb900121
Sushil2308 <SushilPrasad60649@gmail.com>
2024-01-19 17:00:51 +0530

    Add ABS Method in Experession

--- django/db/models/expressions.py
+++ django/db/models/expressions.py
@@ -12,7 +12,6 @@ from django.core.exceptions import EmptyResultSet, FieldError, FullResultSet
 from django.db import DatabaseError, NotSupportedError, connection
 from django.db.models import fields
 from django.db.models.fields import IntegerField, FloatField, DecimalField
-
 from django.db.models.constants import LOOKUP_SEP
 from django.db.models.query_utils import Q
 from django.utils.deconstruct import deconstructible
@@ -2028,7 +2027,6 @@ class ValueRange(WindowFrame):
 
     def window_frame_start_end(self, connection, start, end):
         return connection.ops.window_frame_range_start_end(start, end)
-
 
 @deconstructible(path="django.db.models.ABS")
 class ABS(Func):
