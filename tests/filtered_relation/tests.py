a5440054d2285b2abf54251a33990fd9a6f94060
Nicolas Delaby <nicolas.delaby@infarm.com>
2024-01-23 11:51:24 +0100

    [5.0.x] Fixed #35135 -- Made FilteredRelation raise ValueError on querysets as rhs.
    Regression in 59f475470494ce5b8cbff816b1e5dafcbd10a3a3.
    
    Backport of 820c5f1bacd41713bd30d8b5fefb66752ff15c4c from main

--- tests/filtered_relation/tests.py
+++ tests/filtered_relation/tests.py
@@ -828,6 +828,16 @@ class FilteredRelationTests(TestCase):
         ).filter(rel__isnull=True)
         self.assertSequenceEqual(qs, [])
 
+    def test_conditional_expression_does_not_support_queryset(self):
+        msg = "Passing a QuerySet within a FilteredRelation is not supported."
+        with self.assertRaisesMessage(ValueError, msg):
+            Author.objects.annotate(
+                poem_book=FilteredRelation(
+                    "book",
+                    condition=Q(book__in=Book.objects.filter(title__istartswith="a")),
+                ),
+            ).filter(poem_book__isnull=False)
+
 
 class FilteredRelationAggregationTests(TestCase):
     @classmethod
