django/django

commit 139135627650ed6aaaf4c755b82c3bd43f2b8f51

Refs #29984 -- Made QuerySet.iterator() without chunk_size raise ValueError after prefetch_related().
Per deprecation timeline.

tests/prefetch_related/tests.py
@@ -12,8 +12,7 @@ from django.test import (
     skipIfDBFeature,
     skipUnlessDBFeature,
 )
-from django.test.utils import CaptureQueriesContext, ignore_warnings
-from django.utils.deprecation import RemovedInDjango50Warning
+from django.test.utils import CaptureQueriesContext
 
 from .models import (
     Article,
@@ -385,25 +384,12 @@ class PrefetchRelatedTests(TestDataMixin, TestCase):
             [self.author1, self.author1, self.author3, self.author4],
         )
 
-    @ignore_warnings(category=RemovedInDjango50Warning)
-    def test_m2m_prefetching_iterator_without_chunks(self):
-        # prefetch_related() is ignored.
-        with self.assertNumQueries(5):
-            authors = [
-                b.authors.first()
-                for b in Book.objects.prefetch_related("authors").iterator()
-            ]
-        self.assertEqual(
-            authors,
-            [self.author1, self.author1, self.author3, self.author4],
-        )
-
-    def test_m2m_prefetching_iterator_without_chunks_warning(self):
+    def test_m2m_prefetching_iterator_without_chunks_error(self):
         msg = (
-            "Using QuerySet.iterator() after prefetch_related() without "
-            "specifying chunk_size is deprecated."
+            "chunk_size must be provided when using QuerySet.iterator() after "
+            "prefetch_related()."
         )
-        with self.assertWarnsMessage(RemovedInDjango50Warning, msg):
+        with self.assertRaisesMessage(ValueError, msg):
             Book.objects.prefetch_related("authors").iterator()
 
 

django/db/models/query.py
@@ -33,7 +33,6 @@ from django.db.models.utils import (
     resolve_callables,
 )
 from django.utils import timezone
-from django.utils.deprecation import RemovedInDjango50Warning
 from django.utils.functional import cached_property, partition
 
 # The maximum number of results to fetch in a get() query.
@@ -529,16 +528,9 @@ class QuerySet(AltersData):
         """
         if chunk_size is None:
             if self._prefetch_related_lookups:
-                # When the deprecation ends, replace with:
-                # raise ValueError(
-                #     'chunk_size must be provided when using '
-                #     'QuerySet.iterator() after prefetch_related().'
-                # )
-                warnings.warn(
-                    "Using QuerySet.iterator() after prefetch_related() "
-                    "without specifying chunk_size is deprecated.",
-                    category=RemovedInDjango50Warning,
-                    stacklevel=2,
+                raise ValueError(
+                    "chunk_size must be provided when using QuerySet.iterator() after "
+                    "prefetch_related()."
                 )
         elif chunk_size <= 0:
             raise ValueError("Chunk size must be strictly positive.")

commit 23f628479fe4ac396df3512188582ab25559b62f

Fixed #35159 -- Fixed dumpdata crash when base querysets use prefetch_related().
Regression in 139135627650ed6aaaf4c755b82c3bd43f2b8f51
following deprecation in edbf930287cb72e9afab1f7208c24b1146b0c4ec.

Thanks Andrea F for the report.

django/core/management/commands/dumpdata.py
@@ -219,7 +219,10 @@ class Command(BaseCommand):
                     if count_only:
                         yield queryset.order_by().count()
                     else:
-                        yield from queryset.iterator()
+                        chunk_size = (
+                            2000 if queryset._prefetch_related_lookups else None
+                        )
+                        yield from queryset.iterator(chunk_size=chunk_size)
 
         try:
             self.stdout.ending = None

tests/fixtures/models.py
@@ -101,9 +101,15 @@ class ProxySpy(Spy):
         proxy = True
 
 
+class VisaManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().prefetch_related("permissions")
+
+
 class Visa(models.Model):
     person = models.ForeignKey(Person, models.CASCADE)
     permissions = models.ManyToManyField(Permission, blank=True)
+    objects = VisaManager()
 
     def __str__(self):
         return "%s %s" % (

tests/fixtures/tests.py
@@ -830,6 +830,22 @@ class FixtureLoadingTests(DumpDataAssertMixin, TestCase):
             )
         self.assertEqual(len(warning_list), 0)
 
+    def test_dumpdata_objects_with_prefetch_related(self):
+        management.call_command(
+            "loaddata", "fixture6.json", "fixture8.json", verbosity=0
+        )
+        with self.assertNumQueries(5):
+            self._dumpdata_assert(
+                ["fixtures.visa"],
+                '[{"fields": {"permissions": [["add_user", "auth", "user"]],'
+                '"person": ["Stephane Grappelli"]},'
+                '"model": "fixtures.visa", "pk": 2},'
+                '{"fields": {"permissions": [], "person": ["Prince"]},'
+                '"model": "fixtures.visa", "pk": 3}]',
+                natural_foreign_keys=True,
+                primary_keys="2,3",
+            )
+
     def test_compress_format_loading(self):
         # Load fixture 4 (compressed), using format specification
         management.call_command("loaddata", "fixture4.json", verbosity=0)