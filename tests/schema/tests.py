dfc77637ea5c1aa81caa72b1cf900e6931d61b54
Simon Charette <charette.s@gmail.com>
2024-02-03 10:54:51 -0500

    Fixed #35162 -- Fixed crash when adding fields with db_default on MySQL.
    MySQL doesn't allow literal DEFAULT values to be used for BLOB, TEXT,
    GEOMETRY or JSON columns and requires expression to be used instead.
    
    Regression in 7414704e88d73dafbcfbb85f9bc54cb6111439d3.

--- tests/schema/tests.py
+++ tests/schema/tests.py
@@ -2303,6 +2303,19 @@ class SchemaTests(TransactionTestCase):
         columns = self.column_classes(Author)
         self.assertEqual(columns["birth_year"][1].default, "1988")
 
+    @isolate_apps("schema")
+    def test_add_text_field_with_db_default(self):
+        class Author(Model):
+            description = TextField(db_default="(missing)")
+
+            class Meta:
+                app_label = "schema"
+
+        with connection.schema_editor() as editor:
+            editor.create_model(Author)
+        columns = self.column_classes(Author)
+        self.assertIn("(missing)", columns["description"][1].default)
+
     @skipUnlessDBFeature(
         "supports_column_check_constraints", "can_introspect_check_constraints"
     )
