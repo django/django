16c5d435c144496ecc0577f515df066b055a6137
Bharat Naik <bharat.anaik2003@gmail.com>
2024-02-09 15:40:20 +0530

    get_object_or_none added

--- django/shortcuts.py
+++ django/shortcuts.py
@@ -90,6 +90,33 @@ def get_object_or_404(klass, *args, **kwargs):
         )
 
 
+def get_object_or_none(klass, *args, **kwargs):
+    """
+    Use get() to return an object, or return None if the object
+    does not exist.
+
+    klass may be a Model, Manager, or QuerySet object. All other passed
+    arguments and keyword arguments are used in the get() query.
+
+    Like with QuerySet.get(), MultipleObjectsReturned is raised if more than
+    one object is found.
+    """
+    queryset = _get_queryset(klass)
+    if not hasattr(queryset, "get"):
+        klass__name = (
+            klass.__name__ if isinstance(klass, type) else klass.__class__.__name__
+        )
+        raise ValueError(
+            "First argument to get_object_or_none() must be a Model, Manager, "
+            "or QuerySet, not '%s'." % klass__name
+        )
+    try:
+        return queryset.get(*args, **kwargs)
+    except queryset.model.DoesNotExist:
+        return None
+
+
+
 async def aget_object_or_404(klass, *args, **kwargs):
     """See get_object_or_404()."""
     queryset = _get_queryset(klass)
