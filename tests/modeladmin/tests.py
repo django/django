django/django

commit f80669d2f5a5f1db9e9b73ca893fefba34f955e7

Fixed #35020 -- Fixed ModelAdmin.lookup_allowed() for non-autofield primary keys.
Thanks Joshua Goodwin for the report.

Regression in 45ecd9acca9b36093e274f47b6877a5f79108d9e.

django/contrib/admin/options.py
@@ -474,24 +474,24 @@ class BaseModelAdmin(metaclass=forms.MediaDefiningClass):
                 # Lookups on nonexistent fields are ok, since they're ignored
                 # later.
                 break
-            if not prev_field or (
-                prev_field.is_relation
-                and field not in model._meta.parents.values()
-                and field is not model._meta.auto_field
-                and (
-                    model._meta.auto_field is None
-                    or part not in getattr(prev_field, "to_fields", [])
-                )
-            ):
-                relation_parts.append(part)
             if not getattr(field, "path_infos", None):
                 # This is not a relational field, so further parts
                 # must be transforms.
                 break
+            if (
+                not prev_field
+                or (field.is_relation and field not in model._meta.parents.values())
+                or (
+                    prev_field.is_relation
+                    and model._meta.auto_field is None
+                    and part not in getattr(prev_field, "to_fields", [])
+                )
+            ):
+                relation_parts.append(part)
             prev_field = field
             model = field.path_infos[-1].to_opts.model
 
-        if len(relation_parts) <= 1:
+        if not relation_parts:
             # Either a local field filter, or no fields at all.
             return True
         valid_lookups = {self.date_hierarchy}

tests/modeladmin/tests.py
@@ -163,6 +163,20 @@ class ModelAdminTests(TestCase):
         )
 
     @isolate_apps("modeladmin")
+    def test_lookup_allowed_non_autofield_primary_key(self):
+        class Country(models.Model):
+            id = models.CharField(max_length=2, primary_key=True)
+
+        class Place(models.Model):
+            country = models.ForeignKey(Country, models.CASCADE)
+
+        class PlaceAdmin(ModelAdmin):
+            list_filter = ["country"]
+
+        ma = PlaceAdmin(Place, self.site)
+        self.assertIs(ma.lookup_allowed("country__id__exact", "DE", request), True)
+
+    @isolate_apps("modeladmin")
     def test_lookup_allowed_foreign_primary(self):
         class Country(models.Model):
             name = models.CharField(max_length=256)

commit ebbab6f74e38e1144e535acef04512c871da299e

Fixed #35173 -- Fixed ModelAdmin.lookup_allowed() for lookups on foreign keys when not included in ModelAdmin.list_filters.
Regression in f80669d2f5a5f1db9e9b73ca893fefba34f955e7.

Thanks Sarah Boyce for the review.

django/contrib/admin/options.py
@@ -475,24 +475,25 @@ class BaseModelAdmin(metaclass=forms.MediaDefiningClass):
                 # Lookups on nonexistent fields are ok, since they're ignored
                 # later.
                 break
+            if not prev_field or (
+                prev_field.is_relation
+                and field not in model._meta.parents.values()
+                and field is not model._meta.auto_field
+                and (
+                    model._meta.auto_field is None
+                    or part not in getattr(prev_field, "to_fields", [])
+                )
+                and (field.is_relation or not field.primary_key)
+            ):
+                relation_parts.append(part)
             if not getattr(field, "path_infos", None):
                 # This is not a relational field, so further parts
                 # must be transforms.
                 break
-            if (
-                not prev_field
-                or (field.is_relation and field not in model._meta.parents.values())
-                or (
-                    prev_field.is_relation
-                    and model._meta.auto_field is None
-                    and part not in getattr(prev_field, "to_fields", [])
-                )
-            ):
-                relation_parts.append(part)
             prev_field = field
             model = field.path_infos[-1].to_opts.model
 
-        if not relation_parts or len(parts) == 1:
+        if len(relation_parts) <= 1:
             # Either a local field filter, or no fields at all.
             return True
         valid_lookups = {self.date_hierarchy}

tests/modeladmin/tests.py
@@ -174,7 +174,19 @@ class ModelAdminTests(TestCase):
             pass
 
         ma = PlaceAdmin(Place, self.site)
-        self.assertIs(ma.lookup_allowed("country", "1", request), True)
+
+        cases = [
+            ("country", "1"),
+            ("country__exact", "1"),
+            ("country__id", "1"),
+            ("country__id__exact", "1"),
+            ("country__isnull", True),
+            ("country__isnull", False),
+            ("country__id__isnull", False),
+        ]
+        for lookup, lookup_value in cases:
+            with self.subTest(lookup=lookup):
+                self.assertIs(ma.lookup_allowed(lookup, lookup_value, request), True)
 
     @isolate_apps("modeladmin")
     def test_lookup_allowed_non_autofield_primary_key(self):