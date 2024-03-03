faa30bec11723de6fd7e34d7e0c4ac863bedacc4
Mitchina <juhnicacio.dev@gmail.com>
2024-03-04 00:26:02 +0100

    reformatted files with flake8

--- django/forms/models.py
+++ django/forms/models.py
@@ -15,7 +15,7 @@ from django.core.exceptions import (
 from django.db.models.utils import AltersData
 from django.forms.fields import ChoiceField, Field
 from django.forms.forms import BaseForm, DeclarativeFieldsMetaclass
-from django.forms.formsets import BaseFormSet, FormSet, FormSetMeta, formset_factory
+from django.forms.formsets import BaseFormSet, FormSetMeta, formset_factory
 from django.forms.utils import ErrorList
 from django.forms.widgets import (
     HiddenInput,
