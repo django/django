9f4f63e9ebb7bf6cb9547ee4e2526b9b96703270
Natalia <124304+nessita@users.noreply.github.com>
2024-03-20 13:55:21 -0300

    [5.0.x] Fixed CVE-2024-39330 -- Added extra file name validation in Storage's save method.
    Thanks to Josh Schneier for the report, and to Carlton Gibson and Sarah
    Boyce for the reviews.

--- django/core/files/storage/base.py
+++ django/core/files/storage/base.py
@@ -34,7 +34,18 @@ class Storage:
         if not hasattr(content, "chunks"):
             content = File(content, name)
 
+        # Ensure that the name is valid, before and after having the storage
+        # system potentially modifying the name. This duplicates the check made
+        # inside `get_available_name` but it's necessary for those cases where
+        # `get_available_name` is overriden and validation is lost.
+        validate_file_name(name, allow_relative_path=True)
+
+        # Potentially find a different name depending on storage constraints.
         name = self.get_available_name(name, max_length=max_length)
+        # Validate the (potentially) new name.
+        validate_file_name(name, allow_relative_path=True)
+
+        # The save operation should return the actual name of the file saved.
         name = self._save(name, content)
         # Ensure that the name returned from the storage system is still valid.
         validate_file_name(name, allow_relative_path=True)
