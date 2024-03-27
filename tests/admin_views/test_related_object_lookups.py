5421f31bcd5f6d7f29e2d7225dd24ab6452fde9f
Devin Cox <dcox@surefyre.co>
2024-03-27 11:41:20 -0700

    Fixed Ticket #35330 (bug): The update of related objects fails in the admin when the related model is camel case.

--- tests/admin_views/test_related_object_lookups.py
+++ tests/admin_views/test_related_object_lookups.py
@@ -76,3 +76,46 @@ class SeleniumTests(AdminSeleniumTestCase):
             with self.subTest(link_id):
                 link = self.selenium.find_element(By.XPATH, f'//*[@id="{link_id}"]')
                 self.assertIsNone(link.get_attribute("aria-disabled"))
+
+    def test_related_object_update_with_camel_casing(self):
+        from selenium.webdriver.common.by import By
+
+        def _get_HTML_inside_element_by_id(id_):
+            return self.selenium.find_element(By.ID, id_).get_attribute("innerHTML")
+
+        add_url = reverse("admin:admin_views_camelcaserelatedmodel_add")
+        self.selenium.get(self.live_server_url + add_url)
+        name = "test"
+
+        # Add a CamelCaseModel from the plus button on the Target field
+        self.selenium.find_element(By.ID, "add_id_fk").click()
+
+        # Switch to the popup window
+        self.wait_for_and_switch_to_popup()
+
+        # Find the name field and add text to it
+        self.selenium.find_element(By.ID, "id_name").send_keys(name)
+
+        # Now find the save button and click it
+        self.selenium.find_element(By.NAME, "_save").click()
+
+        # Return to the main window
+        self.wait_until(lambda d: len(d.window_handles) == 1, 1)
+        self.selenium.switch_to.window(self.selenium.window_handles[0])
+
+        # Now check that the state shows in the fk box
+        self.assertHTMLEqual(
+            _get_HTML_inside_element_by_id("id_fk"),
+            f"""
+            <option value="" selected="">---------</option>
+            <option value="1" selected>{name}</option>
+            """,
+        )
+
+        # Check that the state is in the Available Source box
+        self.assertHTMLEqual(
+            _get_HTML_inside_element_by_id("id_m2m_from"),
+            f"""
+            <option value="1">{name}</option>
+            """,
+        )
