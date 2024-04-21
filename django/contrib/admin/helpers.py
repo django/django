55c987a399290e462544f560e60de9cca39d9f56
Willem Van Onsem <vanonsem.willem@gmail.com>
2024-04-21 20:06:12 +0200

    Fixed #35393 -- Added excluded pk as a hidden field to the inline admin.

--- django/contrib/admin/helpers.py
+++ django/contrib/admin/helpers.py
@@ -509,6 +509,11 @@ class InlineAdminForm(AdminForm):
             # Auto fields are editable, so check for auto or non-editable pk.
             self.form._meta.model._meta.auto_field
             or not self.form._meta.model._meta.pk.editable
+            # The pk can be editable, but excluded from the inline.
+            or (
+                self.form._meta.exclude
+                and self.form._meta.model._meta.pk.name in self.form._meta.exclude
+            )
             or
             # Also search any parents for an auto field. (The pk info is
             # propagated to child models so that does not need to be checked
