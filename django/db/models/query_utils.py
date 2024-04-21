fa202d5cb1f16b9bbfd9da72eb03125fabc34bb8
David Sanders <>
2024-04-21 16:46:26 +1000

    [5.0.x] Refs #34007, Refs #35359 -- Added Q.referenced_based_fields property.
    Thank you to Mariusz Felisiak and Natalia Bidart for the reviews.
    
    Backport of 97d48cd3c6f409584b5cc19fbddfca917bae78fd from main

--- django/db/models/query_utils.py
+++ django/db/models/query_utils.py
@@ -175,6 +175,19 @@ class Q(tree.Node):
     def __hash__(self):
         return hash(self.identity)
 
+    @cached_property
+    def referenced_base_fields(self):
+        """
+        Retrieve all base fields referenced directly or through F expressions
+        excluding any fields referenced through joins.
+        """
+        # Avoid circular imports.
+        from django.db.models.sql import query
+
+        return {
+            child.split(LOOKUP_SEP, 1)[0] for child in query.get_children_from_q(self)
+        }
+
 
 class DeferredAttribute:
     """
