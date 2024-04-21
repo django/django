3e6157469d74234c0917d50e6f81cb69124955ba
Willem Van Onsem <vanonsem.willem@gmail.com>
2024-04-21 20:06:12 +0200

    also look for an excluded primary key, to include this in the hidden fields for the inline admin, and thus ensure that this matches with the original element, fixing #35393.

--- django/contrib/admin/helpers.py
+++ django/contrib/admin/helpers.py
@@ -498,6 +498,8 @@ class InlineAdminForm(AdminForm):
             # Auto fields are editable, so check for auto or non-editable pk.
             self.form._meta.model._meta.auto_field
             or not self.form._meta.model._meta.pk.editable
+            # the primary key can be editable, but excluded from the inline as well
+            or self.form._meta.model._meta.pk.name in (self.form._meta.exclude or ())
             or
             # Also search any parents for an auto field. (The pk info is
             # propagated to child models so that does not need to be checked
