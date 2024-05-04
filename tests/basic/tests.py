ceea86baa36b91d0002911770340a2d7bd4f64b7
Jacob Walls <jacobtylerwalls@gmail.com>
2024-05-04 17:11:07 -0400

    Fixed #35425 -- Avoided INSERT with force_update and explicit pk.
    Affected models where the primary key field is defined with a
    default or db_default, such as UUIDField.

--- tests/basic/tests.py
+++ tests/basic/tests.py
@@ -186,6 +186,12 @@ class ModelInstanceCreationTests(TestCase):
         with self.assertNumQueries(1):
             PrimaryKeyWithDefault().save()
 
+    def test_save_primary_with_default_force_update(self):
+        # An UPDATE attempt is made if explicitly requested.
+        obj = PrimaryKeyWithDefault.objects.create()
+        with self.assertNumQueries(1):
+            PrimaryKeyWithDefault(uuid=obj.pk).save(force_update=True)
+
     def test_save_primary_with_db_default(self):
         # An UPDATE attempt is skipped when a primary key has db_default.
         with self.assertNumQueries(1):
