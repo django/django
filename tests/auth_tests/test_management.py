0933b7fd38bd6f2e4debfd99f686af985ec7eaf0
Adam Johnson <me@adamj.eu>
2024-05-10 12:06:27 +0100

    Fixed #35408 -- Optimized post-migrate permission creation.
    co-authored-by: Mariusz Felisiak <felisiak.mariusz@gmail.com>

--- tests/auth_tests/test_management.py
+++ tests/auth_tests/test_management.py
@@ -1528,7 +1528,7 @@ class CreatePermissionsMultipleDatabasesTests(TestCase):
 
     def test_set_permissions_fk_to_using_parameter(self):
         Permission.objects.using("other").delete()
-        with self.assertNumQueries(6, using="other") as captured_queries:
+        with self.assertNumQueries(4, using="other") as captured_queries:
             create_permissions(apps.get_app_config("auth"), verbosity=0, using="other")
         self.assertIn("INSERT INTO", captured_queries[-1]["sql"].upper())
         self.assertGreater(Permission.objects.using("other").count(), 0)
