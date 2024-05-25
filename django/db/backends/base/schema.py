99f23eaabd8da653f046dc1d19f5008c030a4f79
Jacob Walls <jacobtylerwalls@gmail.com>
2024-05-25 17:17:15 -0400

    Fixed #35469 -- Removed deferred SQL to create index removed by AlterField operation.

--- django/db/backends/base/schema.py
+++ django/db/backends/base/schema.py
@@ -1582,11 +1582,22 @@ class BaseDatabaseSchemaEditor:
         )
 
     def _delete_index_sql(self, model, name, sql=None):
-        return Statement(
+        statement = Statement(
             sql or self.sql_delete_index,
             table=Table(model._meta.db_table, self.quote_name),
             name=self.quote_name(name),
         )
+
+        # Remove all deferred statements referencing the deleted index.
+        table_name = statement.parts["table"].table
+        index_name = statement.parts["name"]
+        for sql in list(self.deferred_sql):
+            if isinstance(sql, Statement) and sql.references_index(
+                table_name, index_name
+            ):
+                self.deferred_sql.remove(sql)
+
+        return statement
 
     def _rename_index_sql(self, model, old_name, new_name):
         return Statement(
