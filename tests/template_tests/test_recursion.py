be77e00539ed4f3ac3047c693d3e49d36ef0115d
Gabriel Nick Pivovarov <113646197+gnpivo@users.noreply.github.com>
2024-06-04 14:43:30 -0700

    Fixed #35493 -- Allowed `./` and `../` when recursively including templates

---
+++ tests/template_tests/test_recursion.py
@@ -0,0 +1,37 @@
+import os
+
+from django.template import (
+    Context,
+    Template,
+    TemplateSyntaxError,
+    TemplateDoesNotExist,
+    loader)
+from django.test import SimpleTestCase
+
+context = {
+    "items": [
+        {
+            "name": "foo",
+            "items": [],
+        },
+        {
+            "name": "bar",
+            "items": [
+                {
+                    "name": "baz",
+                    "items": [],
+                },
+            ],
+        },
+    ],
+}
+
+expected_result = "(foo)(bar(baz))"
+
+
+class RecursionTests(SimpleTestCase):
+    def test_recursion(self):
+        for i in range(3):
+            template = loader.get_template(f"recursion/include{i}.html")
+            actual_result = "".join(template.render(context).split())
+            self.assertEqual(actual_result, expected_result)
