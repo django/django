12be19d9ac7e2bd867195f9d934d561e9467bcfa
Bam_boogie <65554208+Kudzmat@users.noreply.github.com>
2024-06-08 16:39:36 +0200

    Delete tests/shortcuts/test_make_toast.py
    Deleted test_make_toast.py

--- tests/shortcuts/test_make_toast.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import make_toast
-from django.test import SimpleTestCase
-
-
-class MakeToastTests(SimpleTestCase):
-    def test_make_toast(self):
-        self.assertEqual(make_toast(), "toast")
