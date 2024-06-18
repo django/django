20c2d625d3d5062e43918d1d7b6f623202491dd4
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2024-06-18 22:19:11 +0200

    Refs #35074 -- Avoided failed attempts to remove spatial indexes on nullable fields on MySQL.
    MySQL doesn't support spatial indexes on NULL columns, so there is no
    point in removing them.

--- tests/gis_tests/gis_migrations/test_operations.py
+++ tests/gis_tests/gis_migrations/test_operations.py
@@ -133,6 +133,24 @@ class OperationTests(OperationTestCase):
         if self.has_spatial_indexes:
             self.assertSpatialIndexExists("gis_neighborhood", "path")
 
+    @skipUnless(connection.vendor == "mysql", "MySQL specific test")
+    def test_remove_geom_field_nullable_with_index(self):
+        # MySQL doesn't support spatial indexes on NULL columns.
+        with self.assertNumQueries(1) as ctx:
+            self.alter_gis_model(
+                migrations.AddField,
+                "Neighborhood",
+                "path",
+                fields.LineStringField,
+                field_class_kwargs={"null": True},
+            )
+        self.assertColumnExists("gis_neighborhood", "path")
+        self.assertNotIn("CREATE SPATIAL INDEX", ctx.captured_queries[0]["sql"])
+
+        with self.assertNumQueries(1), self.assertNoLogs("django.contrib.gis", "ERROR"):
+            self.alter_gis_model(migrations.RemoveField, "Neighborhood", "path")
+        self.assertColumnNotExists("gis_neighborhood", "path")
+
     @skipUnless(HAS_GEOMETRY_COLUMNS, "Backend doesn't support GeometryColumns.")
     def test_geom_col_name(self):
         self.assertEqual(
