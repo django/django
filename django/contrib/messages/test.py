45b94ce0777b13bccc640decbb635044c6e5d2bf
Andrew Miller <info@akmiller.co.uk>
2024-06-19 20:37:38 +0100

    Initial application of last patch from #13376 and made tests pass

--- django/contrib/messages/test.py
+++ django/contrib/messages/test.py
@@ -4,5 +4,6 @@ from .api import get_messages
 class MessagesTestMixin:
     def assertMessages(self, response, expected_messages, *, ordered=True):
         request_messages = list(get_messages(response.wsgi_request))
+        [i.on_display() for i in expected_messages]
         assertion = self.assertEqual if ordered else self.assertCountEqual
         assertion(request_messages, expected_messages)
