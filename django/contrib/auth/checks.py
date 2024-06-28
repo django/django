20912f53ea6ee59efbc1987ae6dcf8457dd768a2
Sarah Boyce <42296566+sarahboyce@users.noreply.github.com>
2024-06-28 13:35:58 +0200

    Reverted "Fixed #35564 -- Improved readability of subclass identification."
    This reverts commit f0d05a747f7a099e6c6bc58c42a787546d2212e7 due to a performance regression.

--- django/contrib/auth/checks.py
+++ django/contrib/auth/checks.py
@@ -1,4 +1,3 @@
-import contextlib
 from itertools import chain
 from types import MethodType
 
@@ -16,10 +15,13 @@ def _subclass_index(class_path, candidate_paths):
     list of candidate paths. If it does not exist, return -1.
     """
     cls = import_string(class_path)
-    for i, path in enumerate(candidate_paths):
-        with contextlib.suppress(ImportError, TypeError):
-            if issubclass(import_string(path), cls):
-                return i
+    for index, path in enumerate(candidate_paths):
+        try:
+            candidate_cls = import_string(path)
+            if issubclass(candidate_cls, cls):
+                return index
+        except (ImportError, TypeError):
+            continue
     return -1
 
 
