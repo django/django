a1d4d1b2464cd86360708b882e2c0030d55b4a93
bcail <bcail@crossway.org>
2024-07-09 10:25:54 -0400

    update method name
    Co-authored-by: Sarah Boyce <42296566+sarahboyce@users.noreply.github.com>

--- django/db/backends/postgresql/schema.py
+++ django/db/backends/postgresql/schema.py
@@ -140,9 +140,7 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
                     return sequence["name"]
         return None
 
-    def _is_changing_type_of_text_column_with_index(
-        self, old_field, old_type, new_type
-    ):
+    def _is_changing_type_of_indexed_text_column(self, old_field, old_type, new_type):
         return (old_field.db_index or old_field.unique) and (
             (old_type.startswith("varchar") and not new_type.startswith("varchar"))
             or (old_type.startswith("text") and not new_type.startswith("text"))
