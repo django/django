fa53819b45fd143fb51ba5834be85e23d2c1fed6
Bendeguz Csirmaz <csirmazbendeguz@gmail.com>
2024-07-11 23:15:06 +0800

    Fixed #373 - Add tests.

--- tests/composite_pk/tests.py
+++ tests/composite_pk/tests.py
@@ -213,3 +213,21 @@ class CompositePKFixturesTests(TestCase):
                 },
             ],
         )
+
+    def test_serialize_yaml(self):
+        users = User.objects.filter(pk=(2, 3))
+        result = serializers.serialize("yaml", users)
+        self.assertEqual(
+            yaml.safe_load(result),
+            [
+                {
+                    "model": "composite_pk.user",
+                    "pk": [2, 3],
+                    "fields": {
+                        "email": "user0003@example.com",
+                        "id": 3,
+                        "tenant": 2,
+                    },
+                },
+            ],
+        )
