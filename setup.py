31ee62f9d713e0a523ea69462584904237657bf4
Shubham <mukati88shubh@gmail.com>
2024-07-18 14:23:04 +0530

    Fixed #99999 -- Added a shortcut function to make toast.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Django",
+    version="3.2.4",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        # List all dependencies
+    ],
+    entry_points={
+        'console_scripts': [
+            # List console scripts if any
+        ],
+    },
+)
