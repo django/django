55f529270125e17bf428585734dbcb33332a46de
Simon Charette <charette.s@gmail.com>
2024-08-02 15:21:12 -0400

    [5.1.x] Fixed #35643 -- Fixed a crash when ordering a QuerySet by a reference containing "__".
    Regression in b0ad41198b3e333f57351e3fce5a1fb47f23f376.
    
    Refs #34013. The initial logic did not consider that annotation aliases
    can include lookup or transform separators.
    
    Thanks Gert Van Gool for the report and Mariusz Felisiak for the review.
    Backport of a16f13a8661297eda12c4177bb01fa2e5b5ccc56 from main.

--- tests/aggregation/tests.py
+++ tests/aggregation/tests.py
@@ -1750,6 +1750,26 @@ class AggregateTestCase(TestCase):
             ],
         )
 
+    def test_order_by_aggregate_default_alias(self):
+        publisher_books = (
+            Publisher.objects.values("book")
+            .annotate(Count("book"))
+            .order_by("book__count", "book__id")
+            .values_list("book", flat=True)
+        )
+        self.assertQuerySetEqual(
+            publisher_books,
+            [
+                None,
+                self.b1.id,
+                self.b2.id,
+                self.b3.id,
+                self.b4.id,
+                self.b5.id,
+                self.b6.id,
+            ],
+        )
+
     def test_empty_result_optimization(self):
         with self.assertNumQueries(0):
             self.assertEqual(
