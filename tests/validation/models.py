7e8cc6b90626ee6d7c670994d64325522e8fadca
David Sanders <shang.xiao.sanders@gmail.com>
2024-08-05 08:22:29 +0200

    Fixed #35638 -- Updated validate_constraints to consider db_default.

--- tests/validation/models.py
+++ tests/validation/models.py
@@ -48,7 +48,7 @@ class ModelToValidate(models.Model):
 
 class UniqueFieldsModel(models.Model):
     unique_charfield = models.CharField(max_length=100, unique=True)
-    unique_integerfield = models.IntegerField(unique=True)
+    unique_integerfield = models.IntegerField(unique=True, db_default=42)
     non_unique_field = models.IntegerField()
 
 
