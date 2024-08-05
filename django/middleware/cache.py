20eb410c7e03d8e4114ae9b396bdd2fcd7bd7725
Wassef Ben Ahmed <1.wassef911@gmail.com>
2024-08-05 17:13:14 +0100

    Update django/middleware/cache.py
    Co-authored-by: Sarah Boyce <42296566+sarahboyce@users.noreply.github.com>

--- django/middleware/cache.py
+++ django/middleware/cache.py
@@ -121,9 +121,7 @@ class UpdateCacheMiddleware(MiddlewareMixin):
             cache_key = learn_cache_key(
                 request, response, timeout, self.key_prefix, cache=self.cache
             )
-            # avoid multiple cache sets
-            if response.has_header("X-Cache-Success"):
-                return response
+            # Avoid duplicating cache.
             response["X-Cache-Success"] = "true"
 
             if hasattr(response, "render") and callable(response.render):
