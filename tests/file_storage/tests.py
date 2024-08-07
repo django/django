e2ec84e479a8644da3dd22e417a3c289bcad47e7
lucasesposito <espositolucas95@gmail.com>
2024-08-07 03:45:16 +0200

    Fixed #35658 -- Initialized InMemoryFileNode instances with a name.

--- tests/file_storage/tests.py
+++ tests/file_storage/tests.py
@@ -1024,6 +1024,23 @@ class FileFieldStorageTests(TestCase):
         with temp_storage.open("tests/stringio") as f:
             self.assertEqual(f.read(), b"content")
 
+    @override_settings(
+        STORAGES={
+            DEFAULT_STORAGE_ALIAS: {
+                "BACKEND": "django.core.files.storage.InMemoryStorage"
+            }
+        }
+    )
+    def test_create_file_field_from_another_file_field_in_memory_storage(self):
+        f = ContentFile("content", "file.txt")
+        obj = Storage.objects.create(storage_callable_default=f)
+        new_obj = Storage.objects.create(
+            storage_callable_default=obj.storage_callable_default.file
+        )
+        storage = callable_default_storage()
+        with storage.open(new_obj.storage_callable_default.name) as f:
+            self.assertEqual(f.read(), b"content")
+
 
 class FieldCallableFileStorageTests(SimpleTestCase):
     def setUp(self):
