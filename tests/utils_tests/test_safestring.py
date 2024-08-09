f44830d8b99bac5347e933fd9f696e1997df1ea2
Matthias Kestenholz <mk@feinheit.ch>
2024-08-09 17:01:27 +0200

    Refs #35648 -- Added a test for more str and SafeString addition cases

--- tests/utils_tests/test_safestring.py
+++ tests/utils_tests/test_safestring.py
@@ -121,3 +121,14 @@ class SafeStringTest(SimpleTestCase):
         msg = "object has no attribute 'dynamic_attr'"
         with self.assertRaisesMessage(AttributeError, msg):
             s.dynamic_attr = True
+
+    def test_add_str(self):
+        s = SafeString("a&b")
+        cases = [
+            ("test", "a&amp;btest"),
+            ("<p>unsafe</p>", "a&amp;b&lt;p&gt;unsafe&lt;/p&gt;"),
+            (SafeString("<p>safe</p>"), SafeString("a&b<p>safe</p>")),
+        ]
+        for case, expected in cases:
+            with self.subTest(case=case):
+                self.assertRenderEqual("{{ s }}", expected, s=s + case)
