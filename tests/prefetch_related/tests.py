django/django

commit e16d0c176e9b89628cdec5e58c418378c4a2436a

Fixed #35064 -- Fixed Window(order_by) crash with DecimalFields on SQLite.
This avoids cast of Window(order_by) for DecimalFields on SQLite.

This was achieved by piggy-backing ExpressionList which already
implements a specialized as_sqlite() method to override the inherited
behaviour of Func through SQLiteNumericMixin.

Refs #31723.

Thanks Quoates for the report.

django/db/models/expressions.py
@@ -1267,12 +1267,12 @@ class ExpressionList(Func):
 
     def get_group_by_cols(self):
         group_by_cols = []
-        for partition in self.get_source_expressions():
-            group_by_cols.extend(partition.get_group_by_cols())
+        for expr in self.get_source_expressions():
+            group_by_cols.extend(expr.get_group_by_cols())
         return group_by_cols
 
 
-class OrderByList(Func):
+class OrderByList(ExpressionList):
     allowed_default = False
     template = "ORDER BY %(expressions)s"
 
@@ -1286,17 +1286,6 @@ class OrderByList(Func):
             for expr in expressions
         )
         super().__init__(*expressions, **extra)
-
-    def as_sql(self, *args, **kwargs):
-        if not self.source_expressions:
-            return "", ()
-        return super().as_sql(*args, **kwargs)
-
-    def get_group_by_cols(self):
-        group_by_cols = []
-        for order_by in self.get_source_expressions():
-            group_by_cols.extend(order_by.get_group_by_cols())
-        return group_by_cols
 
 
 @deconstructible(path="django.db.models.ExpressionWrapper")

tests/expressions_window/tests.py
@@ -354,6 +354,29 @@ class WindowFunctionTests(TestCase):
             transform=lambda row: (row.name, row.bonus, row.department, row.lag),
         )
 
+    def test_order_by_decimalfield(self):
+        qs = Employee.objects.annotate(
+            rank=Window(expression=Rank(), order_by="bonus")
+        ).order_by("-bonus", "id")
+        self.assertQuerySetEqual(
+            qs,
+            [
+                ("Miller", 250.0, 12),
+                ("Johnson", 200.0, 11),
+                ("Wilkinson", 150.0, 10),
+                ("Smith", 137.5, 9),
+                ("Brown", 132.5, 8),
+                ("Adams", 125.0, 7),
+                ("Jones", 112.5, 5),
+                ("Jenson", 112.5, 5),
+                ("Johnson", 100.0, 4),
+                ("Smith", 95.0, 3),
+                ("Williams", 92.5, 2),
+                ("Moore", 85.0, 1),
+            ],
+            transform=lambda row: (row.name, float(row.bonus), row.rank),
+        )
+
     def test_first_value(self):
         qs = Employee.objects.annotate(
             first_value=Window(
@@ -1934,8 +1957,7 @@ class NonQueryWindowTests(SimpleTestCase):
         )
         self.assertEqual(
             repr(Window(expression=Avg("salary"), order_by=F("department").asc())),
-            "<Window: Avg(F(salary)) OVER (OrderByList(OrderBy(F(department), "
-            "descending=False)))>",
+            "<Window: Avg(F(salary)) OVER (OrderBy(F(department), descending=False))>",
         )
 
     def test_window_frame_repr(self):

commit 34e8426015d5fa7a1744692161aba7c6673382da

Fixed #35665 -- Fixed a crash when passing an empty order_by to Window.
This also caused un-ordered sliced prefetches to crash as they rely on Window.

Regression in e16d0c176e9b89628cdec5e58c418378c4a2436a that made OrderByList
piggy-back ExpressionList without porting the empty handling that the latter
provided.

Supporting explicit empty ordering on Window functions and slicing is arguably
a foot-gun design due to how backends will return undeterministic results but
this is a problem that requires a larger discussion.

Refs #35064.

Thanks Andrew Backer for the report.

django/db/models/expressions.py
@@ -1423,15 +1423,13 @@ class ExpressionList(Func):
 
     template = "%(expressions)s"
 
-    def __init__(self, *expressions, **extra):
-        if not expressions:
-            raise ValueError(
-                "%s requires at least one expression." % self.__class__.__name__
-            )
-        super().__init__(*expressions, **extra)
-
     def __str__(self):
         return self.arg_joiner.join(str(arg) for arg in self.source_expressions)
+
+    def as_sql(self, *args, **kwargs):
+        if not self.source_expressions:
+            return "", ()
+        return super().as_sql(*args, **kwargs)
 
     def as_sqlite(self, compiler, connection, **extra_context):
         # Casting to numeric is unnecessary.

tests/expressions/tests.py
@@ -2315,11 +2315,6 @@ class ValueTests(TestCase):
         self.assertNotEqual(value, other_value)
         self.assertNotEqual(value, no_output_field)
 
-    def test_raise_empty_expressionlist(self):
-        msg = "ExpressionList requires at least one expression"
-        with self.assertRaisesMessage(ValueError, msg):
-            ExpressionList()
-
     def test_compile_unresolved(self):
         # This test might need to be revisited later on if #25425 is enforced.
         compiler = Time.objects.all().query.get_compiler(connection=connection)

tests/expressions_window/tests.py
@@ -928,6 +928,20 @@ class WindowFunctionTests(TestCase):
             ),
         )
 
+    def test_empty_ordering(self):
+        """
+        Explicit empty ordering makes little sense but it is something that
+        was historically allowed.
+        """
+        qs = Employee.objects.annotate(
+            sum=Window(
+                expression=Sum("salary"),
+                partition_by="department",
+                order_by=[],
+            )
+        ).order_by("department", "sum")
+        self.assertEqual(len(qs), 12)
+
     def test_related_ordering_with_count(self):
         qs = Employee.objects.annotate(
             department_sum=Window(

tests/prefetch_related/tests.py
@@ -1999,6 +1999,21 @@ class PrefetchLimitTests(TestDataMixin, TestCase):
         with self.assertRaisesMessage(NotSupportedError, msg):
             list(Book.objects.prefetch_related(Prefetch("authors", authors[1:])))
 
+    @skipUnlessDBFeature("supports_over_clause")
+    def test_empty_order(self):
+        authors = Author.objects.order_by()
+        with self.assertNumQueries(3):
+            books = list(
+                Book.objects.prefetch_related(
+                    Prefetch("authors", authors),
+                    Prefetch("authors", authors[:1], to_attr="authors_sliced"),
+                )
+            )
+        for book in books:
+            with self.subTest(book=book):
+                self.assertEqual(len(book.authors_sliced), 1)
+                self.assertIn(book.authors_sliced[0], list(book.authors.all()))
+
 
 class DeprecationTests(TestCase):
     def test_get_current_queryset_warning(self):