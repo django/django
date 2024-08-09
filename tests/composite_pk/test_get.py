438a9fce87bfb052ca921e2aad3c249bd7fb62d7
Bendeguz Csirmaz <csirmazbendeguz@gmail.com>
2024-08-09 23:29:31 +0800

    Fixed #373 - Add one more test

--- tests/composite_pk/test_get.py
+++ tests/composite_pk/test_get.py
@@ -137,9 +137,21 @@ class CompositePKGetTests(TestCase):
                 user = users.get(id=user_id)
                 self.assertEqual(user.comments_count, count)
 
-    def test_get_user_values_annotated_with_comments_id_count(self):
+    def test_users_annotated_with_comments_id_count(self):
+        user_1, user_2, user_3 = User.objects.annotate(Count("comments__id")).order_by(
+            "pk"
+        )
+
+        self.assertEqual(user_1, self.user_1)
+        self.assertEqual(user_1.comments__id__count, 1)
+        self.assertEqual(user_2, self.user_2)
+        self.assertEqual(user_2.comments__id__count, 0)
+        self.assertEqual(user_3, self.user_3)
+        self.assertEqual(user_3.comments__id__count, 0)
+
+    def test_user_values_annotated_with_comments_id_count(self):
         self.assertSequenceEqual(
-            User.objects.values("pk").annotate(Count("comments__id")),
+            User.objects.values("pk").annotate(Count("comments__id")).order_by("pk"),
             (
                 {"pk": self.user_1.pk, "comments__id__count": 1},
                 {"pk": self.user_2.pk, "comments__id__count": 0},
