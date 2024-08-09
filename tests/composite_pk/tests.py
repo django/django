11a86ef32a8c9c52b96410f9c6ac23a660ea6299
Bendeguz Csirmaz <csirmazbendeguz@gmail.com>
2024-08-09 23:11:29 +0800

    Fixed #373 - Add test for .only()

--- tests/composite_pk/tests.py
+++ tests/composite_pk/tests.py
@@ -167,6 +167,18 @@ class CompositePKTests(TestCase):
         users = User.objects.values_list("pk").order_by("pk")
         self.assertNotIn('AS "pk"', str(users.query))
 
+    def test_only(self):
+        users = User.objects.only("pk")
+        self.assertSequenceEqual(users, (self.user,))
+        user = users[0]
+
+        with self.assertNumQueries(0):
+            self.assertEqual(user.pk, (self.user.tenant_id, self.user.id))
+            self.assertEqual(user.tenant_id, self.user.tenant_id)
+            self.assertEqual(user.id, self.user.id)
+        with self.assertNumQueries(1):
+            self.assertEqual(user.email, self.user.email)
+
 
 class CompositePKFixturesTests(TestCase):
     fixtures = ["tenant"]
