7a7aabfcbcbd942010b61702037d2ecdac19ef7a
Chris Muthig <camuthig@gmail.com>
2024-08-14 13:54:09 -0600

    Refs #35444 -- Deprecated contrib.postgres.OrderableAggMixin.
    This commit does not create any functional changes, but marks the
    existing `OrderableAggMixin` class as deprecated so that developers
    using it directly can be made aware of its future removal.

--- django/contrib/postgres/aggregates/mixins.py
+++ django/contrib/postgres/aggregates/mixins.py
@@ -1,6 +1,7 @@
+# RemovedInDjango70Warning: When the deprecation ends, remove completely.
 import warnings
 
-from django.utils.deprecation import RemovedInDjango61Warning
+from django.utils.deprecation import RemovedInDjango61Warning, RemovedInDjango70Warning
 
 
 # RemovedInDjango61Warning.
@@ -19,10 +20,17 @@ class _DeprecatedOrdering:
         super().__init__(*expressions, order_by=order_by, **extra)
 
 
+# RemovedInDjango70Warning.
 # RemovedInDjango61Warning: When the deprecation ends, replace with:
 # class OrderableAggMixin:
 class OrderableAggMixin(_DeprecatedOrdering):
     allow_order_by = True
 
     def __init_subclass__(cls, /, *args, **kwargs):
+        warnings.warn(
+            "OrderableAggMixin is deprecated. Use Aggregate and allow_order_by "
+            "instead.",
+            category=RemovedInDjango70Warning,
+            stacklevel=1,
+        )
         super().__init_subclass__(*args, **kwargs)
