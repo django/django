b95b102be11a88a34a94aaa94513dcfdc1d20992
Yash <yashraj504300@gmail.com>
2024-08-18 19:28:10 +0530

    Fixed #35682 -- Made Docstring updates in BaseDetailView and BaseListView.

--- django/views/generic/detail.py
+++ django/views/generic/detail.py
@@ -102,7 +102,11 @@ class SingleObjectMixin(ContextMixin):
 
 
 class BaseDetailView(SingleObjectMixin, View):
-    """A base view for displaying a single object."""
+    """
+    A base view for displaying a single object.
+    
+    Using this base class requires subclassing to provide a response mixin.
+    """
 
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
