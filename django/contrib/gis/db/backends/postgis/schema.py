7f93c091266b10d4f9f02c8478fbc5389cef03fa
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2024-08-18 07:12:51 +0200

    Fixed #35074 -- Fixed adding/removing indexes when spatial_index is changed on MySQL, PostgreSQL, and Oracle.
    Co-authored-by: Mário Falcão <mario@falcao.dev>

--- django/contrib/gis/db/backends/postgis/schema.py
+++ django/contrib/gis/db/backends/postgis/schema.py
@@ -1,3 +1,4 @@
+from django.contrib.gis.db.models import GeometryField
 from django.db.backends.postgresql.schema import DatabaseSchemaEditor
 from django.db.models.expressions import Col, Func
 
@@ -58,6 +59,39 @@ class PostGISSchemaEditor(DatabaseSchemaEditor):
             [],
         )
 
+    def _alter_field(
+        self,
+        model,
+        old_field,
+        new_field,
+        old_type,
+        new_type,
+        old_db_params,
+        new_db_params,
+        strict=False,
+    ):
+        super()._alter_field(
+            model,
+            old_field,
+            new_field,
+            old_type,
+            new_type,
+            old_db_params,
+            new_db_params,
+            strict=strict,
+        )
+
+        old_field_spatial_index = (
+            isinstance(old_field, GeometryField) and old_field.spatial_index
+        )
+        new_field_spatial_index = (
+            isinstance(new_field, GeometryField) and new_field.spatial_index
+        )
+        if not old_field_spatial_index and new_field_spatial_index:
+            self.execute(self._create_spatial_index_sql(model, new_field))
+        elif old_field_spatial_index and not new_field_spatial_index:
+            self.execute(self._delete_spatial_index_sql(model, old_field))
+
     def _create_spatial_index_name(self, model, field):
         return self._create_index_name(model._meta.db_table, [field.column], "_id")
 
@@ -84,3 +118,7 @@ class PostGISSchemaEditor(DatabaseSchemaEditor):
             opclasses=opclasses,
             expressions=expressions,
         )
+
+    def _delete_spatial_index_sql(self, model, field):
+        index_name = self._create_spatial_index_name(model, field)
+        return self._delete_index_sql(model, index_name)
