django/django

commit 01ed59f753139afb514170ee7f7384c155ecbc2d

Refs #35189 -- Improved admin fieldset's accessibility by setting aria-labelledby.
Before this change, HTML <fieldset> elements in the admin site did not
have an associated label to describe them. This commit defines a unique
HTML id for the heading labeling a fieldset, and sets its
aria-labelledby property to link the heading with the fieldset.

django/contrib/admin/templates/admin/change_form.html
@@ -47,7 +47,7 @@
 
 {% block field_sets %}
 {% for fieldset in adminform %}
-  {% include "admin/includes/fieldset.html" %}
+  {% include "admin/includes/fieldset.html" with heading_level=2 id_suffix=forloop.counter0 %}
 {% endfor %}
 {% endblock %}
 

django/contrib/admin/templates/admin/edit_inline/stacked.html
@@ -3,12 +3,14 @@
      id="{{ inline_admin_formset.formset.prefix }}-group"
      data-inline-type="stacked"
      data-inline-formset="{{ inline_admin_formset.inline_formset_data }}">
-<fieldset class="module {{ inline_admin_formset.classes }}">
+<fieldset class="module {{ inline_admin_formset.classes }}" aria-labelledby="{{ inline_admin_formset.formset.prefix }}-heading">
+  <h2 id="{{ inline_admin_formset.formset.prefix }}-heading" class="inline-heading">
   {% if inline_admin_formset.formset.max_num == 1 %}
-    <h2>{{ inline_admin_formset.opts.verbose_name|capfirst }}</h2>
+    {{ inline_admin_formset.opts.verbose_name|capfirst }}
   {% else %}
-    <h2>{{ inline_admin_formset.opts.verbose_name_plural|capfirst }}</h2>
+    {{ inline_admin_formset.opts.verbose_name_plural|capfirst }}
   {% endif %}
+  </h2>
 {{ inline_admin_formset.formset.management_form }}
 {{ inline_admin_formset.formset.non_form_errors }}
 
@@ -19,9 +21,13 @@
     {% if inline_admin_formset.formset.can_delete and inline_admin_formset.has_delete_permission and inline_admin_form.original %}<span class="delete">{{ inline_admin_form.deletion_field.field }} {{ inline_admin_form.deletion_field.label_tag }}</span>{% endif %}
   </h3>
   {% if inline_admin_form.form.non_field_errors %}{{ inline_admin_form.form.non_field_errors }}{% endif %}
-  {% for fieldset in inline_admin_form %}
-    {% include "admin/includes/fieldset.html" %}
-  {% endfor %}
+
+  {% with parent_counter=forloop.counter0 %}
+    {% for fieldset in inline_admin_form %}
+      {% include "admin/includes/fieldset.html" with heading_level=4 id_prefix=parent_counter id_suffix=forloop.counter0 %}
+    {% endfor %}
+  {% endwith %}
+
   {% if inline_admin_form.needs_explicit_pk_field %}{{ inline_admin_form.pk_field.field }}{% endif %}
   {% if inline_admin_form.fk_field %}{{ inline_admin_form.fk_field.field }}{% endif %}
 </div>{% endfor %}

django/contrib/admin/templates/admin/includes/fieldset.html
@@ -1,5 +1,8 @@
-<fieldset class="module aligned {{ fieldset.classes }}">
-    {% if fieldset.name %}<h2>{{ fieldset.name }}</h2>{% endif %}
+{% with prefix=fieldset.formset.prefix|default:"fieldset" id_prefix=id_prefix|default:"0" id_suffix=id_suffix|default:"0" name=fieldset.name|default:""|slugify %}
+<fieldset class="module aligned {{ fieldset.classes }}"{% if name %} aria-labelledby="{{ prefix }}-{{ id_prefix}}-{{ name }}-{{ id_suffix }}-heading"{% endif %}>
+    {% if name %}
+        <h{{ heading_level|default:2 }} id="{{ prefix }}-{{ id_prefix}}-{{ name }}-{{ id_suffix }}-heading" class="fieldset-heading">{{ fieldset.name }}</h{{ heading_level|default:2 }}>
+    {% endif %}
     {% if fieldset.description %}
         <div class="description">{{ fieldset.description|safe }}</div>
     {% endif %}
@@ -32,3 +35,4 @@
         </div>
     {% endfor %}
 </fieldset>
+{% endwith %}

django/contrib/admin/templates/admin/edit_inline/tabular.html
@@ -4,12 +4,14 @@
      data-inline-formset="{{ inline_admin_formset.inline_formset_data }}">
   <div class="tabular inline-related {% if forloop.last %}last-related{% endif %}">
 {{ inline_admin_formset.formset.management_form }}
-<fieldset class="module {{ inline_admin_formset.classes }}">
-   {% if inline_admin_formset.formset.max_num == 1 %}
-     <h2>{{ inline_admin_formset.opts.verbose_name|capfirst }}</h2>
-   {% else %}
-     <h2>{{ inline_admin_formset.opts.verbose_name_plural|capfirst }}</h2>
-   {% endif %}
+<fieldset class="module {{ inline_admin_formset.classes }}" aria-labelledby="{{ inline_admin_formset.formset.prefix }}-heading">
+  <h2 id="{{ inline_admin_formset.formset.prefix }}-heading" class="inline-heading">
+  {% if inline_admin_formset.formset.max_num == 1 %}
+    {{ inline_admin_formset.opts.verbose_name|capfirst }}
+  {% else %}
+    {{ inline_admin_formset.opts.verbose_name_plural|capfirst }}
+  {% endif %}
+  </h2>
    {{ inline_admin_formset.formset.non_form_errors }}
    <table>
      <thead><tr>

tests/admin_inlines/admin.py
@@ -40,6 +40,8 @@ from .models import (
     OutfitItem,
     ParentModelWithCustomPk,
     Person,
+    Photo,
+    Photographer,
     Poll,
     Profile,
     ProfileCollection,
@@ -95,6 +97,57 @@ class AuthorAdmin(admin.ModelAdmin):
         EditablePKBookTabularInline,
         EditablePKBookStackedInline,
         NonAutoPKBookChildTabularInline,
+    ]
+
+
+class PhotoInlineMixin:
+    model = Photo
+    extra = 2
+    fieldsets = [
+        (None, {"fields": ["image", "title"]}),
+        (
+            "Details",
+            {"fields": ["description", "creation_date"], "classes": ["collapse"]},
+        ),
+        (
+            "Details",  # Fieldset name intentionally duplicated
+            {"fields": ["update_date", "updated_by"]},
+        ),
+    ]
+
+
+class PhotoTabularInline(PhotoInlineMixin, admin.TabularInline):
+    pass
+
+
+class PhotoStackedExtra2Inline(PhotoInlineMixin, admin.StackedInline):
+    pass
+
+
+class PhotoStackedExtra3Inline(PhotoInlineMixin, admin.StackedInline):
+    extra = 3
+
+
+class PhotoStackedCollapsibleInline(PhotoInlineMixin, admin.StackedInline):
+    fieldsets = []
+    classes = ["collapse"]
+
+
+class PhotographerAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["firstname", "fullname"]}),
+        ("Advanced options", {"fields": ["nationality", "residency"]}),
+        (
+            "Advanced options",  # Fieldset name intentionally duplicated
+            {"fields": ["siblings", "children"], "classes": ["collapse"]},
+        ),
+    ]
+    inlines = [
+        PhotoTabularInline,
+        PhotoTabularInline,
+        PhotoStackedExtra2Inline,
+        PhotoStackedExtra3Inline,
+        PhotoStackedCollapsibleInline,
     ]
 
 
@@ -454,6 +507,7 @@ site.register(Teacher, TeacherAdmin)
 site.register(Chapter, inlines=[FootNoteNonEditableInlineCustomForm])
 site.register(OutfitItem, inlines=[WeaknessInlineCustomForm])
 site.register(Person, inlines=[AuthorTabularInline, FashonistaStackedInline])
+site.register(Photographer, PhotographerAdmin)
 site.register(Course, ClassAdminStackedHorizontal)
 site.register(CourseProxy, ClassAdminStackedVertical)
 site.register(CourseProxy1, ClassAdminTabularVertical)

tests/admin_inlines/models.py
@@ -180,6 +180,27 @@ class ShoppingWeakness(models.Model):
     item = models.ForeignKey(OutfitItem, models.CASCADE)
 
 
+# Models for #35189
+
+
+class Photographer(Person):
+    fullname = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=100)
+    residency = models.CharField(max_length=100)
+    siblings = models.IntegerField()
+    children = models.IntegerField()
+
+
+class Photo(models.Model):
+    photographer = models.ForeignKey(Photographer, on_delete=models.CASCADE)
+    image = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    creation_date = models.DateField()
+    update_date = models.DateField()
+    updated_by = models.CharField(max_length=100)
+
+
 # Models for #13510
 
 

commit a2a2b4d2773d5e890a522617c403851ca11d67be

Fixed #35716 -- Fixed VariableDoesNotExist when rendering admin fieldsets.
Regression in 01ed59f753139afb514170ee7f7384c155ecbc2d.

Thank you to Fábio Domingues and Marijke Luttekes for the report,
and thank you to Natalia Bidart for the review.

django/contrib/admin/templates/admin/change_form.html
@@ -47,7 +47,7 @@
 
 {% block field_sets %}
 {% for fieldset in adminform %}
-  {% include "admin/includes/fieldset.html" with heading_level=2 id_suffix=forloop.counter0 %}
+  {% include "admin/includes/fieldset.html" with heading_level=2 prefix="fieldset" id_prefix=0 id_suffix=forloop.counter0 %}
 {% endfor %}
 {% endblock %}
 

django/contrib/admin/templates/admin/edit_inline/stacked.html
@@ -26,7 +26,7 @@
 
   {% with parent_counter=forloop.counter0 %}
     {% for fieldset in inline_admin_form %}
-      {% include "admin/includes/fieldset.html" with heading_level=4 id_prefix=parent_counter id_suffix=forloop.counter0 %}
+      {% include "admin/includes/fieldset.html" with heading_level=4 prefix=fieldset.formset.prefix id_prefix=parent_counter id_suffix=forloop.counter0 %}
     {% endfor %}
   {% endwith %}
 

django/contrib/admin/templates/admin/includes/fieldset.html
@@ -1,4 +1,4 @@
-{% with prefix=fieldset.formset.prefix|default:"fieldset" id_prefix=id_prefix|default:"0" id_suffix=id_suffix|default:"0" name=fieldset.name|default:""|slugify %}
+{% with name=fieldset.name|default:""|slugify %}
 <fieldset class="module aligned {{ fieldset.classes }}"{% if name %} aria-labelledby="{{ prefix }}-{{ id_prefix}}-{{ name }}-{{ id_suffix }}-heading"{% endif %}>
     {% if name %}
         {% if fieldset.is_collapsible %}<details><summary>{% endif %}

tests/admin_inlines/tests.py
@@ -1768,6 +1768,13 @@ class TestInlineWithFieldsets(TestDataMixin, TestCase):
     def setUp(self):
         self.client.force_login(self.superuser)
 
+    @override_settings(DEBUG=True)
+    def test_fieldset_context_fully_set(self):
+        url = reverse("admin:admin_inlines_photographer_add")
+        with self.assertRaisesMessage(AssertionError, "no logs"):
+            with self.assertLogs("django.template", "DEBUG"):
+                self.client.get(url)
+
     def test_inline_headings(self):
         response = self.client.get(reverse("admin:admin_inlines_photographer_add"))
         # Page main title.