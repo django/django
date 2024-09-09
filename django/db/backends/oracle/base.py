8dbc8bc5d23c40615541f34662f6b08db6b13da1
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2024-09-09 10:39:15 +0200

    Refs #33817 -- Corrected errors raised when Oracle driver is not installed.
    oracledb_any should reraise ImportError instead of raising
    ImproperlyConfigured.

--- django/db/backends/oracle/base.py
+++ django/db/backends/oracle/base.py
@@ -14,12 +14,16 @@ from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.db import IntegrityError
 from django.db.backends.base.base import BaseDatabaseWrapper
-from django.db.backends.oracle.oracledb_any import oracledb as Database
 from django.db.backends.utils import debug_transaction
 from django.utils.asyncio import async_unsafe
 from django.utils.encoding import force_bytes, force_str
 from django.utils.functional import cached_property
 from django.utils.version import get_version_tuple
+
+try:
+    from django.db.backends.oracle.oracledb_any import oracledb as Database
+except ImportError as e:
+    raise ImproperlyConfigured(f"Error loading oracledb module: {e}")
 
 
 def _setup_environment(environ):
