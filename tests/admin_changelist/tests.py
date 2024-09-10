d2c824796722a61f4b830f30ed1ade26ca68ce15
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2024-09-11 00:30:57 +0200

    Reverted patch and implemented suggestion by Sarah (see ticket #35747)

--- tests/admin_changelist/tests.py
+++ tests/admin_changelist/tests.py
@@ -1375,6 +1375,20 @@ class ChangeListTests(TestCase):
         OrderedObjectAdmin.ordering = ["id", "bool"]
         check_results_order(ascending=True)
 
+    def test_ordering_from_model_meta(self):
+        Swallow.objects.create(origin="Swallow A", load=4, speed=2)
+        Swallow.objects.create(origin="Swallow B", load=2, speed=1)
+        Swallow.objects.create(origin="Swallow C", load=5, speed=1)
+        m = SwallowAdmin(Swallow, custom_site)
+        request = self._mocked_authenticated_request("/swallow/?o=", self.superuser)
+        changelist = m.get_changelist_instance(request)
+        queryset = changelist.get_queryset(request)
+        self.assertQuerySetEqual(
+            queryset,
+            [(1.0, 2.0), (1.0, 5.0), (2.0, 4.0)],
+            lambda s: (s.speed, s.load),
+        )
+
     @isolate_apps("admin_changelist")
     def test_total_ordering_optimization(self):
         class Related(models.Model):
