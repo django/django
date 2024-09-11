4317852579fcd205ccdce1316df0dd9af24f76c4
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2024-09-11 09:04:46 +0200

    Fixed #35747 -- Used default ordering when the ORDER_VAR param is blank in the admin changelist.

--- tests/admin_changelist/tests.py
+++ tests/admin_changelist/tests.py
@@ -1328,6 +1328,20 @@ class ChangeListTests(TestCase):
         UnorderedObjectAdmin.ordering = ["id", "bool"]
         check_results_order(ascending=True)
 
+    def test_ordering_from_model_meta(self):
+        Swallow.objects.create(origin="Swallow A", load=4, speed=2)
+        Swallow.objects.create(origin="Swallow B", load=2, speed=1)
+        Swallow.objects.create(origin="Swallow C", load=5, speed=1)
+        m = SwallowAdmin(Swallow, custom_site)
+        request = self._mocked_authenticated_request("/swallow/?o=", self.superuser)
+        changelist = m.get_changelist_instance(request)
+        queryset = changelist.get_queryset(request)
+        self.assertQuerySetEqual(
+            queryset,
+            [(1.0, 2.0), (1.0, 5.0), (2.0, 4.0)],
+            lambda s: (s.speed, s.load),
+        )
+
     def test_deterministic_order_for_model_ordered_by_its_manager(self):
         """
         The primary key is used in the ordering of the changelist's results to
