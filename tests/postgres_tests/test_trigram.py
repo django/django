django/django

commit 6364b6ee1071381eb3a23ba6b821fc0d6f0fce75

Fixed #34955 -- Made Concat() use || operator on PostgreSQL.
This also avoids casting string based expressions in Concat() on
PostgreSQL.

django/db/models/functions/text.py
@@ -73,7 +73,7 @@ class ConcatPair(Func):
 
     function = "CONCAT"
 
-    def as_sqlite(self, compiler, connection, **extra_context):
+    def pipes_concat_sql(self, compiler, connection, **extra_context):
         coalesced = self.coalesce()
         return super(ConcatPair, coalesced).as_sql(
             compiler,
@@ -83,19 +83,19 @@ class ConcatPair(Func):
             **extra_context,
         )
 
+    as_sqlite = pipes_concat_sql
+
     def as_postgresql(self, compiler, connection, **extra_context):
-        copy = self.copy()
-        copy.set_source_expressions(
+        c = self.copy()
+        c.set_source_expressions(
             [
-                Cast(expression, TextField())
-                for expression in copy.get_source_expressions()
+                expression
+                if isinstance(expression.output_field, (CharField, TextField))
+                else Cast(expression, TextField())
+                for expression in c.get_source_expressions()
             ]
         )
-        return super(ConcatPair, copy).as_sql(
-            compiler,
-            connection,
-            **extra_context,
-        )
+        return c.pipes_concat_sql(compiler, connection, **extra_context)
 
     def as_mysql(self, compiler, connection, **extra_context):
         # Use CONCAT_WS with an empty separator so that NULLs are ignored.
@@ -132,16 +132,20 @@ class Concat(Func):
     def __init__(self, *expressions, **extra):
         if len(expressions) < 2:
             raise ValueError("Concat must take at least two expressions")
-        paired = self._paired(expressions)
+        paired = self._paired(expressions, output_field=extra.get("output_field"))
         super().__init__(paired, **extra)
 
-    def _paired(self, expressions):
+    def _paired(self, expressions, output_field):
         # wrap pairs of expressions in successive concat functions
         # exp = [a, b, c, d]
         # -> ConcatPair(a, ConcatPair(b, ConcatPair(c, d))))
         if len(expressions) == 2:
-            return ConcatPair(*expressions)
-        return ConcatPair(expressions[0], self._paired(expressions[1:]))
+            return ConcatPair(*expressions, output_field=output_field)
+        return ConcatPair(
+            expressions[0],
+            self._paired(expressions[1:], output_field=output_field),
+            output_field=output_field,
+        )
 
 
 class Left(Func):

tests/db_functions/text/test_concat.py
@@ -75,7 +75,10 @@ class ConcatTests(TestCase):
         expected = article.title + " - " + article.text
         self.assertEqual(expected.upper(), article.title_text)
 
-    @skipUnless(connection.vendor == "sqlite", "sqlite specific implementation detail.")
+    @skipUnless(
+        connection.vendor in ("sqlite", "postgresql"),
+        "SQLite and PostgreSQL specific implementation detail.",
+    )
     def test_coalesce_idempotent(self):
         pair = ConcatPair(V("a"), V("b"))
         # Check nodes counts
@@ -89,3 +92,18 @@ class ConcatTests(TestCase):
         qs = Article.objects.annotate(description=Concat("title", V(": "), "summary"))
         # Multiple compilations should not alter the generated query.
         self.assertEqual(str(qs.query), str(qs.all().query))
+
+    def test_concat_non_str(self):
+        Author.objects.create(name="The Name", age=42)
+        with self.assertNumQueries(1) as ctx:
+            author = Author.objects.annotate(
+                name_text=Concat(
+                    "name", V(":"), "alias", V(":"), "age", output_field=TextField()
+                ),
+            ).get()
+        self.assertEqual(author.name_text, "The Name::42")
+        # Only non-string columns are casted on PostgreSQL.
+        self.assertEqual(
+            ctx.captured_queries[0]["sql"].count("::text"),
+            1 if connection.vendor == "postgresql" else 0,
+        )

commit 95b7da5e14f010495fb5c130cbf730d52aa99cb9

Fixed #35732 -- Wrapped ConcatPair expression in parentheses to ensure operator precedence.
When ConcatPair was updated to use || this lost the implicit wrapping from CONCAT(...).
This broke the WHERE clauses when used in combination with PostgreSQL trigram similarity.

Regression in 6364b6ee1071381eb3a23ba6b821fc0d6f0fce75.

Co-authored-by: Emiliano Cuenca <106986074+emicuencac@users.noreply.github.com>

django/db/models/functions/text.py
@@ -78,7 +78,7 @@ class ConcatPair(Func):
         return super(ConcatPair, coalesced).as_sql(
             compiler,
             connection,
-            template="%(expressions)s",
+            template="(%(expressions)s)",
             arg_joiner=" || ",
             **extra_context,
         )

tests/postgres_tests/test_trigram.py
@@ -1,3 +1,6 @@
+from django.db.models import F, Value
+from django.db.models.functions import Concat
+
 from . import PostgreSQLTestCase
 from .models import CharFieldModel, TextFieldModel
 
@@ -147,6 +150,21 @@ class TrigramTest(PostgreSQLTestCase):
                 {"field": "Cat sat on mat.", "word_distance": 0.5},
                 {"field": "Matthew", "word_distance": 0.5555556},
             ],
+        )
+
+    def test_trigram_concat_precedence(self):
+        search_term = "im matthew"
+        self.assertSequenceEqual(
+            self.Model.objects.annotate(
+                concat_result=Concat(
+                    Value("I'm "),
+                    F("field"),
+                    output_field=self.Model._meta.get_field("field"),
+                ),
+            )
+            .filter(concat_result__trigram_similar=search_term)
+            .values("field"),
+            [{"field": "Matthew"}],
         )
 
 