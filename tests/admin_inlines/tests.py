django/django

commit 96a598356a9ea8c2c05b22cadc12e256a3b295fd

Used CSS flex for form rows.

django/contrib/admin/templates/admin/includes/fieldset.html
@@ -19,12 +19,12 @@
                             {{ field.field }}
                         {% endif %}
                     {% endif %}
-                    {% if field.field.help_text %}
-                        <div class="help"{% if field.field.id_for_label %} id="{{ field.field.id_for_label }}_helptext"{% endif %}>
-                          {{ field.field.help_text|safe }}
-                        </div>
-                    {% endif %}
                 </div>
+                {% if field.field.help_text %}
+                    <div class="help"{% if field.field.id_for_label %} id="{{ field.field.id_for_label }}_helptext"{% endif %}>
+                        {{ field.field.help_text|safe }}
+                    </div>
+                {% endif %}
             {% endfor %}
         </div>
     {% endfor %}

django/contrib/admin/static/admin/css/forms.css
@@ -22,6 +22,11 @@ form .form-row p {
     padding-left: 0;
 }
 
+.form-row > div {
+    display: flex;
+    flex-wrap: wrap;
+}
+
 /* FORM LABELS */
 
 label {
@@ -69,7 +74,6 @@ form ul.inline li {
 .aligned label {
     display: block;
     padding: 4px 10px 0 0;
-    float: left;
     width: 160px;
     word-wrap: break-word;
     line-height: 1;
@@ -82,12 +86,17 @@ form ul.inline li {
     height: 26px;
 }
 
-.aligned label + p, .aligned label + div.help, .aligned label + div.readonly {
+.aligned label + p, .aligned .checkbox-row + div.help, .aligned label + div.readonly {
     padding: 6px 0;
     margin-top: 0;
     margin-bottom: 0;
     margin-left: 170px;
     overflow-wrap: break-word;
+}
+
+.aligned label + div.readonly,
+.aligned label + .datetime {
+    margin-left: 0;
 }
 
 .aligned ul label {
@@ -117,7 +126,6 @@ form .aligned div.radiolist {
 
 form .aligned p.help,
 form .aligned div.help {
-    clear: left;
     margin-top: 0;
     margin-left: 160px;
     padding-left: 10px;
@@ -129,8 +137,7 @@ form .aligned p.datetime div.help.timezonewarning {
     font-weight: normal;
 }
 
-form .aligned label + p.help,
-form .aligned label + div.help {
+form .aligned .checkbox-row + .help {
     margin-left: 0;
     padding-left: 0;
 }

django/contrib/admin/static/admin/css/rtl.css
@@ -111,7 +111,6 @@ thead th.sorted .text {
 
 .aligned label {
     padding: 0 0 3px 1em;
-    float: right;
 }
 
 .submit-row a.deletelink {
@@ -127,10 +126,6 @@ thead th.sorted .text {
     margin-left: 5px;
 }
 
-form .aligned p.help, form .aligned div.help {
-    clear: right;
-}
-
 form .aligned ul {
     margin-right: 163px;
     margin-left: 0;
@@ -140,6 +135,17 @@ form ul.inline li {
     float: right;
     padding-right: 0;
     padding-left: 7px;
+}
+
+form .aligned p.help,
+form .aligned div.help {
+    margin-right: 160px;
+    padding-right: 10px;
+}
+
+form .aligned .checkbox-row + .help {
+    margin-right: 0;
+    padding-right: 0;
 }
 
 .submit-row {

commit 2ded36b712fa934a87177b531cc9258c2a5d06a2

Fixed #35755 -- Hid help text of hidden fields in admin fieldsets.
Regression in 96a598356a9ea8c2c05b22cadc12e256a3b295fd.

django/contrib/admin/templates/admin/includes/fieldset.html
@@ -27,7 +27,7 @@
                             {% endif %}
                         </div>
                     {% if field.field.help_text %}
-                        <div class="help"{% if field.field.id_for_label %} id="{{ field.field.id_for_label }}_helptext"{% endif %}>
+                        <div class="help{% if field.field.is_hidden %} hidden{% endif %}"{% if field.field.id_for_label %} id="{{ field.field.id_for_label }}_helptext"{% endif %}>
                             <div>{{ field.field.help_text|safe }}</div>
                         </div>
                     {% endif %}

tests/admin_inlines/models.py
@@ -332,7 +332,7 @@ class SomeParentModel(models.Model):
 
 class SomeChildModel(models.Model):
     name = models.CharField(max_length=1)
-    position = models.PositiveIntegerField()
+    position = models.PositiveIntegerField(help_text="Position help_text.")
     parent = models.ForeignKey(SomeParentModel, models.CASCADE)
     readonly_field = models.CharField(max_length=1)
 

tests/admin_inlines/tests.py
@@ -349,7 +349,12 @@ class TestInline(TestDataMixin, TestCase):
         )
         response = self.client.get(url)
         self.assertInHTML(
-            '<th class="column-position hidden">Position</th>',
+            '<th class="column-position hidden">Position'
+            '<img src="/static/admin/img/icon-unknown.svg" '
+            'class="help help-tooltip" width="10" height="10" '
+            'alt="(Position help_text.)" '
+            'title="Position help_text.">'
+            "</th>",
             response.rendered_content,
         )
         self.assertInHTML(
@@ -379,13 +384,15 @@ class TestInline(TestDataMixin, TestCase):
         self.assertInHTML(
             '<div class="flex-container fieldBox field-position hidden">'
             '<label class="inline">Position:</label>'
-            '<div class="readonly">0</div></div>',
+            '<div class="readonly">0</div></div>'
+            '<div class="help hidden"><div>Position help_text.</div></div>',
             response.rendered_content,
         )
         self.assertInHTML(
             '<div class="flex-container fieldBox field-position hidden">'
             '<label class="inline">Position:</label>'
-            '<div class="readonly">1</div></div>',
+            '<div class="readonly">1</div></div>'
+            '<div class="help hidden"><div>Position help_text.</div></div>',
             response.rendered_content,
         )
 
@@ -407,13 +414,17 @@ class TestInline(TestDataMixin, TestCase):
         self.assertInHTML(
             '<div class="form-row hidden field-position">'
             '<div><div class="flex-container"><label>Position:</label>'
-            '<div class="readonly">0</div></div></div></div>',
+            '<div class="readonly">0</div></div>'
+            '<div class="help hidden"><div>Position help_text.</div></div>'
+            "</div></div>",
             response.rendered_content,
         )
         self.assertInHTML(
             '<div class="form-row hidden field-position">'
             '<div><div class="flex-container"><label>Position:</label>'
-            '<div class="readonly">1</div></div></div></div>',
+            '<div class="readonly">1</div></div>'
+            '<div class="help hidden"><div>Position help_text.</div></div>'
+            "</div></div>",
             response.rendered_content,
         )
 
@@ -448,7 +459,12 @@ class TestInline(TestDataMixin, TestCase):
         self.assertInHTML(
             '<thead><tr><th class="original"></th>'
             '<th class="column-name required">Name</th>'
-            '<th class="column-position required hidden">Position</th>'
+            '<th class="column-position required hidden">Position'
+            '<img src="/static/admin/img/icon-unknown.svg" '
+            'class="help help-tooltip" width="10" height="10" '
+            'alt="(Position help_text.)" '
+            'title="Position help_text.">'
+            "</th>"
             "<th>Delete?</th></tr></thead>",
             response.rendered_content,
         )