658d51bd02c477a0adaea87130c8249efd336e46
saJaeHyukc <wogur981208@gmail.com>
2024-09-13 20:07:37 +0900

    Fixed #34887 -- Added support for unlimited models.CharField on SQLite.
    Signed-off-by: saJaeHyukc <wogur981208@gmail.com>

--- django/db/backends/sqlite3/features.py
+++ django/db/backends/sqlite3/features.py
@@ -60,6 +60,7 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     """
     insert_test_table_with_defaults = 'INSERT INTO {} ("null") VALUES (1)'
     supports_default_keyword_in_insert = False
+    supports_unlimited_charfield = True
 
     @cached_property
     def django_test_skips(self):
