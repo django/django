cb4bd6e32cc76f976b05ef1d7e1ab446f660e913
unknown <mahmoudyassin.rpa@gmail.com>
2024-09-23 01:29:01 +0300

    Fix linting issues

--- tests/model_fields/test_datetimefield.py
+++ tests/model_fields/test_datetimefield.py
@@ -1,12 +1,12 @@
 import datetime
+from unittest import mock
 
 from django.db import models
 from django.test import SimpleTestCase, TestCase, override_settings, skipUnlessDBFeature
 from django.test.utils import requires_tz_support
 from django.utils import timezone
-from unittest import mock
 
-from .models import DateTimeModel, AutoNowDateModel
+from .models import AutoNowDateModel, DateTimeModel
 
 
 class DateTimeFieldTests(TestCase):
@@ -89,11 +89,10 @@ class DateFieldAutoNowTests(TestCase):
         obj.refresh_from_db()
         self.assertEqual(obj.d, current_time.date())
 
-    @mock.patch('django.utils.timezone.now')
+    @mock.patch("django.utils.timezone.now")
     def test_auto_now_near_midnight(self, mock_timezone_now):
         mock_time = timezone.make_aware(datetime.datetime(2024, 9, 22, 23, 59, 59))
         mock_timezone_now.return_value = mock_time
-        
         obj = AutoNowDateModel.objects.create()
         obj.refresh_from_db()
         self.assertEqual(obj.d, mock_time.date())
