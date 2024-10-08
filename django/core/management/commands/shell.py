77727da7ad1b6e83e01f6230e481d655b8d3e7c0
Salvo Polizzi <salvopolizzi03@gmail.com>
2024-10-08 17:06:20 +0200

    Fixed #35515 -- Added automatic model imports to shell command.

--- django/core/management/commands/shell.py
+++ django/core/management/commands/shell.py
@@ -3,6 +3,7 @@ import select
 import sys
 import traceback
 
+from django.apps import apps
 from django.core.management import BaseCommand, CommandError
 from django.utils.datastructures import OrderedSet
 
@@ -47,18 +48,21 @@ class Command(BaseCommand):
     def ipython(self, options):
         from IPython import start_ipython
 
-        start_ipython(argv=[])
+        start_ipython(
+            argv=[],
+            user_ns=self.get_and_report_namespace(options["verbosity"]),
+        )
 
     def bpython(self, options):
         import bpython
 
-        bpython.embed()
+        bpython.embed(self.get_and_report_namespace(options["verbosity"]))
 
     def python(self, options):
         import code
 
         # Set up a dictionary to serve as the environment for the shell.
-        imported_objects = {}
+        imported_objects = self.get_and_report_namespace(options["verbosity"])
 
         # We want to honor both $PYTHONSTARTUP and .pythonrc.py, so follow system
         # conventions and get $PYTHONSTARTUP first then .pythonrc.py.
@@ -111,10 +115,66 @@ class Command(BaseCommand):
         # Start the interactive interpreter.
         code.interact(local=imported_objects)
 
+    def get_and_report_namespace(self, verbosity):
+        namespace = self.get_namespace()
+
+        if verbosity >= 1:
+            self.stdout.write(
+                f"{len(namespace)} objects imported automatically",
+                self.style.SUCCESS,
+            )
+        if verbosity >= 2:
+            imports_by_module = {}
+            imports_by_alias = {}
+            for obj_name, obj in namespace.items():
+                if hasattr(obj, "__module__") and (
+                    (hasattr(obj, "__qualname__") and obj.__qualname__.find(".") == -1)
+                    or not hasattr(obj, "__qualname__")
+                ):
+                    module = obj.__module__
+                    collected_imports = imports_by_module.get(module, [])
+                    imports_by_module[module] = collected_imports + [obj_name]
+                if not hasattr(obj, "__module__") and hasattr(obj, "__name__"):
+                    tokens = obj.__name__.split(".")
+                    if obj_name in tokens:
+                        tokens.remove(obj_name)
+                        module = ".".join(tokens)
+                        collected_imports = imports_by_module.get(module, [])
+                        imports_by_module[module] = collected_imports + [obj_name]
+                    else:
+                        module = ".".join(tokens)
+                        imports_by_alias[module] = obj_name
+
+            for module, imported_objects in imports_by_module.items():
+                self.stdout.write(
+                    f"from {module} import {', '.join(imported_objects)}",
+                    self.style.SUCCESS,
+                )
+            for module, alias in imports_by_alias.items():
+                self.stdout.write(f"import {module} as {alias}", self.style.SUCCESS)
+
+        return namespace
+
+    def get_namespace(self):
+        apps_models = apps.get_models()
+        apps_models_modules = [
+            (app.models_module, app.label)
+            for app in apps.get_app_configs()
+            if app.models_module is not None
+        ]
+        namespace = {}
+        for model in reversed(apps_models):
+            if model.__module__:
+                namespace[model.__name__] = model
+        for app_models_module, app_label in apps_models_modules:
+            if f"{app_label}_models" not in namespace:
+                namespace[f"{app_label}_models"] = app_models_module
+        return namespace
+
     def handle(self, **options):
         # Execute the command and exit.
         if options["command"]:
-            exec(options["command"], globals())
+            exec(options["command"], {**globals(), **self.get_namespace()})
             return
 
         # Execute stdin if it has anything to read and exit.
@@ -124,7 +184,7 @@ class Command(BaseCommand):
             and not sys.stdin.isatty()
             and select.select([sys.stdin], [], [], 0)[0]
         ):
-            exec(sys.stdin.read(), globals())
+            exec(sys.stdin.read(), {**globals(), **self.get_namespace()})
             return
 
         available_shells = (
