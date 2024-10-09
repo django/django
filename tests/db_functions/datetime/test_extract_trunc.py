87731a1129eb00c9de91c7f95d573d6c2cae322d
wesley <umpenn46@gmail.com>
2024-10-09 18:40:11 -0400

    ticket #34699. reformatting code

--- tests/db_functions/datetime/test_extract_trunc.py
+++ tests/db_functions/datetime/test_extract_trunc.py
@@ -1952,7 +1952,7 @@ class DateFunctionWithTimeZoneTests(DateFunctionTests):
         later = now + timedelta(hours=2)
         non_utc_model = self.create_model(now, later)
         models_qs = DTModel.objects.annotate(
-            start_trunc=TruncSecond('start_datetime')
+            start_trunc=TruncSecond("start_datetime")
         ).filter(id=non_utc_model.id, start_trunc__lte=now)
         self.assertEqual(models_qs.count(), 1)
         test_timezones = [
@@ -1965,13 +1965,13 @@ class DateFunctionWithTimeZoneTests(DateFunctionTests):
                 later = now + timedelta(hours=2)
                 non_utc_model = self.create_model(now, later)
                 models_qs = DTModel.objects.annotate(
-                    start_trunc=TruncSecond('start_datetime')
+                    start_trunc=TruncSecond("start_datetime")
                 ).filter(id=non_utc_model.id, start_trunc__lte=now)
                 self.assertNotEqual(models_qs.count(), 1)
                 adjusted_now = timezone.localtime(now).replace(
                     tzinfo=zoneinfo.ZoneInfo(key='UTC')
                 )
                 models_qs = DTModel.objects.annotate(
-                    start_trunc=TruncSecond('start_datetime')
+                    start_trunc=TruncSecond("start_datetime")
                 ).filter(id=non_utc_model.id, start_trunc__lte=adjusted_now)
                 self.assertEqual(models_qs.count(), 1)
