8809fb5443b789edd5790fe3ac91d892e4080534
Ben Cail <bcail@crossway.org>
2024-10-09 12:41:44 -0400

    fix failing test

--- tests/auth_tests/test_validators.py
+++ tests/auth_tests/test_validators.py
@@ -151,15 +151,14 @@ class MinimumLengthValidatorCustomErrorTest(SimpleTestCase):
             def get_error_message(self):
                 return "Your password must be %d characters long" % self.min_length
 
-        expected_error = "Your password must be 8 characters long"
+        expected_error = "Your password must be %d characters long"
 
-        with self.assertRaisesMessage(ValidationError, expected_error) as cm:
+        with self.assertRaisesMessage(ValidationError, expected_error % 8) as cm:
             CustomMinimumLengthValidator().validate("1234567")
         self.assertEqual(cm.exception.error_list[0].code, "password_too_short")
 
-        with self.assertRaises(ValidationError) as cm:
+        with self.assertRaisesMessage(ValidationError, expected_error % 3) as cm:
             CustomMinimumLengthValidator(min_length=3).validate("12")
-        self.assertEqual(cm.exception.messages, [expected_error % 3])
 
 
 class UserAttributeSimilarityValidatorTest(TestCase):
