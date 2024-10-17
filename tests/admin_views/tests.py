2bf583a3594f94887498032d51d78d1969c393a3
Tim Graham <timograham@gmail.com>
2024-10-17 14:38:05 -0400

    Fixed #35903 -- Made admin's "view on site" URL accept non-integer ContentType pks.

--- tests/admin_views/tests.py
+++ tests/admin_views/tests.py
@@ -8664,6 +8664,19 @@ class AdminViewOnSiteTests(TestCase):
             ),
         )
 
+    def test_view_on_site_url_non_integer_ids(self):
+        """The view_on_site URL accepts non-integer ids."""
+        self.assertEqual(
+            reverse(
+                "admin:view_on_site",
+                kwargs={
+                    "content_type_id": "37156b6a-8a82",
+                    "object_id": "37156b6a-8a83",
+                },
+            ),
+            "/test_admin/admin/r/37156b6a-8a82/37156b6a-8a83/",
+        )
+
 
 @override_settings(ROOT_URLCONF="admin_views.urls")
 class InlineAdminViewOnSiteTest(TestCase):
