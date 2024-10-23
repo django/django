1c622dda17d17ecad5a5663d4fabf56a3ceaae51
David Winiecki <david.winiecki@gmail.com>
2024-10-22 22:18:24 -0700

    Reduced risk of breaking RemoteTestResult dependents.

--- django/test/runner.py
+++ django/test/runner.py
@@ -295,8 +295,9 @@ failure and get a correct traceback.
         self.check_picklable(test, err)
 
         event_occurred_before_first_test = self.test_index == -1
-        if event_occurred_before_first_test:
-            assert isinstance(test, unittest.suite._ErrorHolder)
+        if event_occurred_before_first_test and isinstance(
+            test, unittest.suite._ErrorHolder
+        ):
             self.events.append(("addError", self.test_index, test.id(), err))
         else:
             self.events.append(("addError", self.test_index, err))
