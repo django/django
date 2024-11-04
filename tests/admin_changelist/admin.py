85e7b5e83bee1b00a98b4ffdf4e806e914778fb2
Sarah Boyce <42296566+sarahboyce@users.noreply.github.com>
2024-11-04 16:09:55 +0100

    Refs #26001 -- Handled relationship exact lookups in ModelAdmin.search_fields.

--- tests/admin_changelist/admin.py
+++ tests/admin_changelist/admin.py
@@ -56,6 +56,7 @@ class ChildAdmin(admin.ModelAdmin):
 
 class GrandChildAdmin(admin.ModelAdmin):
     list_display = ["name", "parent__name", "parent__parent__name"]
+    search_fields = ["parent__name__exact", "parent__age__exact"]
 
 
 site.register(GrandChild, GrandChildAdmin)
