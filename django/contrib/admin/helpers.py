bf0e0dcac32880486f9aab3773bc7ddc6ef8ec71
John Parton <john.parton.iv@gmail.com>
2024-11-05 11:05:13 -0600

    Refs #23968 -- Removed unnecessary list comprehension in contrib.admin.helpers.

--- django/contrib/admin/helpers.py
+++ django/contrib/admin/helpers.py
@@ -121,7 +121,7 @@ class Fieldset:
 
     @cached_property
     def is_collapsible(self):
-        if any([field in self.fields for field in self.form.errors]):
+        if any(field in self.fields for field in self.form.errors):
             return False
         return "collapse" in self.classes
 
