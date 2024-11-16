a5a36df94906e6e0325ad1930d9470505597db3e
Simon Charette <charette.s@gmail.com>
2024-11-16 10:02:11 -0500

    Refs #27236 -- Removed references to index_together from ModelState.from_model.
    It's not possible for ModelMeta.index_together to exist anymore.

--- django/db/migrations/state.py
+++ django/db/migrations/state.py
@@ -827,9 +827,6 @@ class ModelState:
                 if name == "unique_together":
                     ut = model._meta.original_attrs["unique_together"]
                     options[name] = set(normalize_together(ut))
-                elif name == "index_together":
-                    it = model._meta.original_attrs["index_together"]
-                    options[name] = set(normalize_together(it))
                 elif name == "indexes":
                     indexes = [idx.clone() for idx in model._meta.indexes]
                     for index in indexes:
@@ -845,7 +842,7 @@ class ModelState:
         # If we're ignoring relationships, remove all field-listing model
         # options (that option basically just means "make a stub model")
         if exclude_rels:
-            for key in ["unique_together", "index_together", "order_with_respect_to"]:
+            for key in ["unique_together", "order_with_respect_to"]:
                 if key in options:
                     del options[key]
         # Private fields are ignored, so remove options that refer to them.
