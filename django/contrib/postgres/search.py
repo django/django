0f657987b5192aaa7dfa8eb33214c5a93e4185a8
GappleBee <irrationalmathematicspro7@gmail.com>
2024-11-17 14:49:49 +0000

    Made isort happy.

--- django/contrib/postgres/search.py
+++ django/contrib/postgres/search.py
@@ -1,5 +1,3 @@
-import re
-
 from django.db.models import (
     CharField,
     Expression,
@@ -12,6 +10,8 @@ from django.db.models import (
 )
 from django.db.models.expressions import CombinedExpression, register_combinable_fields
 from django.db.models.functions import Cast, Coalesce
+
+import re
 
 
 _SEARCH_SPEC_CHARS = r"['\0\[\]()|&:*!@<>\\]"
