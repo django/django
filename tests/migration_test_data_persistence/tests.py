d4b2e06a67c2e1458305c3eac6c4b2b3e917daf9
Jacob Walls <jacobtylerwalls@gmail.com>
2024-11-20 09:07:11 -0500

    Fixed #35921 -- Fixed failure when running tests in parallel on postgres.
    Follow-up to a060a22ee2dde7aa29a5a29120087c4864887325.

--- tests/migration_test_data_persistence/tests.py
+++ tests/migration_test_data_persistence/tests.py
@@ -32,7 +32,7 @@ class MigrationDataPersistenceClassSetup(TransactionTestCase):
     @classmethod
     def setUpClass(cls):
         # Simulate another TransactionTestCase having just torn down.
-        call_command("flush", verbosity=0, interactive=False)
+        call_command("flush", verbosity=0, interactive=False, allow_cascade=True)
         super().setUpClass()
         cls.book = Book.objects.first()
 
