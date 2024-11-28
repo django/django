ab715b336da42fe1b597445ad38fdf6f11139479
Salvo Polizzi <salvopolizzi03@gmail.com>
2024-11-28 13:52:25 +0100

    Refs #35038 -- Added test for drop and recreation of a constraint.

--- tests/migrations/test_autodetector.py
+++ tests/migrations/test_autodetector.py
@@ -2981,6 +2981,43 @@ class AutodetectorTests(BaseAutodetectorTests):
             changes, "testapp", 0, 0, model_name="author", name="name_contains_bob"
         )
 
+    def test_constraint_dropped_and_recreated(self):
+        altered_constraint = models.CheckConstraint(
+            condition=models.Q(name__contains="bob"),
+            name="name_contains_bob",
+        )
+        author_name_check_constraint_lowercased = copy.deepcopy(
+            self.author_name_check_constraint
+        )
+        author_name_check_constraint_lowercased.options = {
+            "constraints": [altered_constraint]
+        }
+        changes = self.get_changes(
+            [self.author_name_check_constraint],
+            [author_name_check_constraint_lowercased],
+        )
+
+        self.assertNumberMigrations(changes, "testapp", 1)
+        self.assertOperationTypes(
+            changes, "testapp", 0, ["RemoveConstraint", "AddConstraint"]
+        )
+        self.assertOperationAttributes(
+            changes,
+            "testapp",
+            0,
+            0,
+            model_name="author",
+            name="name_contains_bob",
+        )
+        self.assertOperationAttributes(
+            changes,
+            "testapp",
+            0,
+            1,
+            model_name="author",
+            constraint=altered_constraint,
+        )
+
     def test_add_unique_together(self):
         """Tests unique_together detection."""
         changes = self.get_changes(
