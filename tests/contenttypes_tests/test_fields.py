95112b3b07c362924cf74c1f4589b82c223985a7
Adam Johnson <me@adamj.eu>
2024-11-30 08:03:55 +0000

    Fixed #35950 -- Restored refreshing of relations when fields deferred.
    Thank you to Simon Charette and Sarah Boyce for the review.
    
    Regression in 73df8b54a2fab53bec4c7573cda5ad8c869c2fd8.

--- tests/contenttypes_tests/test_fields.py
+++ tests/contenttypes_tests/test_fields.py
@@ -57,6 +57,15 @@ class GenericForeignKeyTests(TestCase):
         self.assertIsNot(answer.question, old_question_obj)
         self.assertEqual(answer.question, old_question_obj)
 
+    def test_clear_cached_generic_relation_when_deferred(self):
+        question = Question.objects.create(text="question")
+        Answer.objects.create(text="answer", question=question)
+        answer = Answer.objects.defer("text").get()
+        old_question_obj = answer.question
+        # The reverse relation is refreshed even when the text field is deferred.
+        answer.refresh_from_db()
+        self.assertIsNot(answer.question, old_question_obj)
+
 
 class GenericRelationTests(TestCase):
     def test_value_to_string(self):
