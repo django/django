10c52894255adff1fc9d1b0ac05bd28bf19181ab
Juan Pablo Mallarino <mjuanpablo@google.com>
2024-12-05 08:14:59 -0500

    Fixed #35973 -- Improved makemessages locale validation to handle numeric region codes.

--- django/core/management/commands/makemessages.py
+++ django/core/management/commands/makemessages.py
@@ -40,7 +40,7 @@ def check_programs(*programs):
 
 
 def is_valid_locale(locale):
-    return re.match(r"^[a-z]+$", locale) or re.match(r"^[a-z]+_[A-Z].*$", locale)
+    return re.match(r"^[a-z]+$", locale) or re.match(r"^[a-z]+_[A-Z0-9].*$", locale)
 
 
 @total_ordering
