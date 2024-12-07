django/django

commit e9814029f570bd0866dc859147bca90340bcc913

Fixed #10692 -- Fixed DecimalField lookups for extreme values.
git-svn-id: http://code.djangoproject.com/svn/django/trunk@10545 bcc190cf-cafb-0310-a4f2-bffc1f526a37

django/db/models/fields/__init__.py
@@ -620,9 +620,12 @@ class DecimalField(Field):
         from django.db.backends import util
         return util.format_number(value, self.max_digits, self.decimal_places)
 
-    def get_db_prep_value(self, value):
+    def get_db_prep_save(self, value):
         return connection.ops.value_to_db_decimal(self.to_python(value),
                 self.max_digits, self.decimal_places)
+
+    def get_db_prep_value(self, value):
+        return self.to_python(value)
 
     def formfield(self, **kwargs):
         defaults = {

tests/regressiontests/model_fields/tests.py
@@ -1,32 +1,35 @@
 import datetime
 import unittest
+
 import django.test
 from django import forms
 from django.db import models
 from django.core.exceptions import ValidationError
+
 from models import Foo, Bar, Whiz, BigD, BigS
+
 try:
     from decimal import Decimal
 except ImportError:
     from django.utils._decimal import Decimal
-    
+
 class DecimalFieldTests(django.test.TestCase):
     def test_to_python(self):
         f = models.DecimalField(max_digits=4, decimal_places=2)
         self.assertEqual(f.to_python(3), Decimal("3"))
         self.assertEqual(f.to_python("3.14"), Decimal("3.14"))
         self.assertRaises(ValidationError, f.to_python, "abc")
-    
+
     def test_default(self):
         f = models.DecimalField(default=Decimal("0.00"))
         self.assertEqual(f.get_default(), Decimal("0.00"))
-    
+
     def test_format(self):
         f = models.DecimalField(max_digits=5, decimal_places=1)
         self.assertEqual(f._format(f.to_python(2)), u'2.0')
         self.assertEqual(f._format(f.to_python('2.6')), u'2.6')
         self.assertEqual(f._format(None), None)
-    
+
     def test_get_db_prep_lookup(self):
         f = models.DecimalField(max_digits=5, decimal_places=1)
         self.assertEqual(f.get_db_prep_lookup('exact', None), [None])
@@ -38,7 +41,7 @@ class DecimalFieldTests(django.test.TestCase):
         Foo.objects.create(id=1, a='abc', d=Decimal("12.34"))
         self.assertEqual(list(Foo.objects.filter(d=u'1.23')), [])
 
-    def test_save_wihout_float_conversion(self):
+    def test_save_without_float_conversion(self):
         """
         Ensure decimals don't go through a corrupting float conversion during
         save (#5079).
@@ -47,6 +50,14 @@ class DecimalFieldTests(django.test.TestCase):
         bd.save()
         bd = BigD.objects.get(pk=bd.pk)
         self.assertEqual(bd.d, Decimal("12.9"))
+
+    def test_lookup_really_big_value(self):
+        """
+        Ensure that really big values can be used in a filter statement, even
+        with older Python versions.
+        """
+        # This should not crash. That counts as a win for our purposes.
+        Foo.objects.filter(d__gte=100000000000)
 
 class ForeignKeyTests(django.test.TestCase):
     def test_callable_default(self):
@@ -63,11 +74,11 @@ class DateTimeFieldTests(unittest.TestCase):
                          datetime.datetime(2001, 1, 2, 3, 4, 5, 6))
         self.assertEqual(f.to_python('2001-01-02 03:04:05.999999'),
                          datetime.datetime(2001, 1, 2, 3, 4, 5, 999999))
-        
+
     def test_timefield_to_python_usecs(self):
         """TimeField.to_python should support usecs"""
         f = models.TimeField()
-        self.assertEqual(f.to_python('01:02:03.000004'), 
+        self.assertEqual(f.to_python('01:02:03.000004'),
                          datetime.time(1, 2, 3, 4))
         self.assertEqual(f.to_python('01:02:03.999999'),
                          datetime.time(1, 2, 3, 999999))
@@ -111,7 +122,7 @@ class ChoicesTests(django.test.TestCase):
         self.assertEqual(Whiz(c=9).get_c_display(), 9)          # Invalid value
         self.assertEqual(Whiz(c=None).get_c_display(), None)    # Blank value
         self.assertEqual(Whiz(c='').get_c_display(), '')        # Empty value
-        
+
 class SlugFieldTests(django.test.TestCase):
     def test_slugfield_max_length(self):
         """
@@ -120,3 +131,4 @@ class SlugFieldTests(django.test.TestCase):
         bs = BigS.objects.create(s = 'slug'*50)
         bs = BigS.objects.get(pk=bs.pk)
         self.assertEqual(bs.s, 'slug'*50)
+

commit e567ac6e0b2fefc126d9ec931c83845a9476d05d

Fixed #35982 -- Made DecimalField.get_db_prep_value() call DatabaseOperations.adapt_decimalfield_value().
Regression in e9814029f570bd0866dc859147bca90340bcc913.

Thanks Simon Charette for advice and review.

django/db/models/fields/__init__.py
@@ -1828,9 +1828,8 @@ class DecimalField(Field):
             )
         return decimal_value
 
-    def get_db_prep_save(self, value, connection):
-        if hasattr(value, "as_sql"):
-            return value
+    def get_db_prep_value(self, value, connection, prepared=False):
+        value = super().get_db_prep_value(value, connection, prepared)
         return connection.ops.adapt_decimalfield_value(
             self.to_python(value), self.max_digits, self.decimal_places
         )

tests/model_fields/test_decimalfield.py
@@ -1,9 +1,10 @@
 import math
 from decimal import Decimal
+from unittest import mock
 
 from django.core import validators
 from django.core.exceptions import ValidationError
-from django.db import models
+from django.db import connection, models
 from django.test import TestCase
 
 from .models import BigD, Foo
@@ -47,6 +48,20 @@ class DecimalFieldTests(TestCase):
         f = models.DecimalField(max_digits=5, decimal_places=1)
         self.assertIsNone(f.get_prep_value(None))
         self.assertEqual(f.get_prep_value("2.4"), Decimal("2.4"))
+
+    def test_get_db_prep_value(self):
+        """
+        DecimalField.get_db_prep_value() must call
+        DatabaseOperations.adapt_decimalfield_value().
+        """
+        f = models.DecimalField(max_digits=5, decimal_places=1)
+        # None of the built-in database backends implement
+        # adapt_decimalfield_value(), so this must be confirmed with mocking.
+        with mock.patch.object(
+            connection.ops.__class__, "adapt_decimalfield_value"
+        ) as adapt_decimalfield_value:
+            f.get_db_prep_value("2.4", connection)
+        adapt_decimalfield_value.assert_called_with(Decimal("2.4"), 5, 1)
 
     def test_filter_with_strings(self):
         """
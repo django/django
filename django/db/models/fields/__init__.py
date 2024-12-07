e567ac6e0b2fefc126d9ec931c83845a9476d05d
Tim Graham <timograham@gmail.com>
2024-12-06 20:44:35 -0500

    Fixed #35982 -- Made DecimalField.get_db_prep_value() call DatabaseOperations.adapt_decimalfield_value().
    Regression in e9814029f570bd0866dc859147bca90340bcc913.
    
    Thanks Simon Charette for advice and review.

--- django/db/models/fields/__init__.py
+++ django/db/models/fields/__init__.py
@@ -1828,9 +1828,8 @@ class DecimalField(Field):
             )
         return decimal_value
 
-    def get_db_prep_save(self, value, connection):
-        if hasattr(value, "as_sql"):
-            return value
+    def get_db_prep_value(self, value, connection, prepared=False):
+        value = super().get_db_prep_value(value, connection, prepared)
         return connection.ops.adapt_decimalfield_value(
             self.to_python(value), self.max_digits, self.decimal_places
         )
