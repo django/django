8baa3b98731d12068e740b6f1eb3dd8b637799a8
Joris Benschop <joris@digital-engines.nl>
2024-12-07 16:21:17 +0100

    fix postgresql NULL insertion test

--- tests/fixtures/test_force_insert.py
+++ tests/fixtures/test_force_insert.py
@@ -635,7 +635,7 @@ class BulkCreateLoadingTests(DumpDataAssertMixin, TestCase):
     @skipUnlessDBFeature("prohibits_null_characters_in_text_exception")
     def test_loaddata_null_characters_on_postgresql(self):
         error, msg = connection.features.prohibits_null_characters_in_text_exception
-        msg = f"Could not load fixtures.Article(pk=2): {msg}"
+        # msg = f"Could not load fixtures.Article(pk=2): {msg}"
         with self.assertRaisesMessage(error, msg):
             management.call_command(
                 "loaddata", "--bulk_create", "null_character_in_field_value.json"
