87f2190220c77901d49ebfb486cdd00935da9775
Adam Johnson <me@adamj.eu>
2024-12-09 15:40:47 +0000

    Fixed #35987 -- Made ErrorList.copy() copy the renderer attribute.

--- tests/forms_tests/tests/test_utils.py
+++ tests/forms_tests/tests/test_utils.py
@@ -2,6 +2,7 @@ import copy
 import json
 
 from django.core.exceptions import ValidationError
+from django.forms.renderers import DjangoTemplates
 from django.forms.utils import (
     ErrorDict,
     ErrorList,
@@ -160,6 +161,17 @@ class FormsUtilsTestCase(SimpleTestCase):
             '<ul class="errorlist"><li>nameExample of link: '
             '<a href="http://www.example.com/">example</a></li></ul>',
         )
+
+    def test_error_list_copy_attributes(self):
+        class CustomRenderer(DjangoTemplates):
+            pass
+
+        renderer = CustomRenderer()
+        e = ErrorList(error_class="woopsies", renderer=renderer)
+
+        e_copy = e.copy()
+        self.assertEqual(e.error_class, e_copy.error_class)
+        self.assertEqual(e.renderer, e_copy.renderer)
 
     def test_error_dict_copy(self):
         e = ErrorDict()
