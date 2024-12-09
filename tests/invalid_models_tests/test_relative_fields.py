cdaea8642189a000edf2d6b42aefa7947e7dc645
Anthony Joseph <ajosephau@users.noreply.github.com>
2024-12-10 06:55:18 +1100

    Fixed #22977 -- Added system check for clashing managers and reverse related fields.
    With thanks to  Konrad Świat, Loïc Bistuer, Russell Keith-Magee,
    and Mariusz Felisiak.
    
    Co-authored-by: Mariusz Felisiak <felisiak.mariusz@gmail.com>

--- tests/invalid_models_tests/test_relative_fields.py
+++ tests/invalid_models_tests/test_relative_fields.py
@@ -1537,6 +1537,32 @@ class ExplicitRelatedQueryNameClashTests(SimpleTestCase):
 
 
 @isolate_apps("invalid_models_tests")
+class RelatedQueryNameClashWithManagerTests(SimpleTestCase):
+    def test_clash_between_related_query_name_and_manager(self):
+        class Author(models.Model):
+            authors = models.Manager()
+            mentor = models.ForeignKey(
+                "self", related_name="authors", on_delete=models.CASCADE
+            )
+
+        self.assertEqual(
+            Author.check(),
+            [
+                Error(
+                    "Related name 'authors' for 'Author.mentor' clashes with the name "
+                    "of a model manager.",
+                    hint=(
+                        "Rename the model manager or change the related_name argument "
+                        "in the definition for field 'Author.mentor'."
+                    ),
+                    obj=Author._meta.get_field("mentor"),
+                    id="fields.E348",
+                )
+            ],
+        )
+
+
+@isolate_apps("invalid_models_tests")
 class SelfReferentialM2MClashTests(SimpleTestCase):
     def test_clash_between_accessors(self):
         class Model(models.Model):
