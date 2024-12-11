1ec18cd53bc27ea3d5f36428a5bc0666344271ce
Erica Pisani <erica.pisani@floatcard.com>
2024-12-11 13:16:55 -0500

    Modify existing test to provide coverage for this fix

--- tests/serializers/tests.py
+++ tests/serializers/tests.py
@@ -10,7 +10,7 @@ from django.db import connection, transaction
 from django.http import HttpResponse
 from django.test import SimpleTestCase, override_settings, skipUnlessDBFeature
 from django.test.utils import Approximate
-
+from django.db.models import Prefetch
 from .models import (
     Actor,
     Article,
@@ -280,7 +280,14 @@ class SerializersTestBase:
         with self.assertNumQueries(4):
             serializers.serialize(
                 self.serializer_name,
-                Article.objects.prefetch_related("categories", "meta_data", "topics"),
+                Article.objects.prefetch_related(
+                    "meta_data",
+                    "topics",
+                    Prefetch(
+                        "categories",
+                        queryset=Category.objects.prefetch_related("meta_data"),
+                    ),
+                ),
             )
         # One query for the Article table, and three m2m queries for each
         # article.
