235d7f2800608bfddca6ae17abd1a3cf84d99c13
Sarah Boyce <42296566+sarahboyce@users.noreply.github.com>
2024-12-12 16:42:20 +0100

    Refs #34547 -- Removed DatabaseOperations.field_cast_sql() per deprecation timeline.

--- django/db/backends/base/operations.py
+++ django/db/backends/base/operations.py
@@ -1,7 +1,6 @@
 import datetime
 import decimal
 import json
-import warnings
 from importlib import import_module
 
 import sqlparse
@@ -10,7 +9,6 @@ from django.conf import settings
 from django.db import NotSupportedError, transaction
 from django.db.models.expressions import Col
 from django.utils import timezone
-from django.utils.deprecation import RemovedInDjango60Warning
 from django.utils.encoding import force_str
 
 
@@ -213,23 +211,6 @@ class BaseDatabaseOperations:
         statement into a table, return the newly created data.
         """
         return cursor.fetchone()
-
-    def field_cast_sql(self, db_type, internal_type):
-        """
-        Given a column type (e.g. 'BLOB', 'VARCHAR') and an internal type
-        (e.g. 'GenericIPAddressField'), return the SQL to cast it before using
-        it in a WHERE statement. The resulting string should contain a '%s'
-        placeholder for the column being searched against.
-        """
-        warnings.warn(
-            (
-                "DatabaseOperations.field_cast_sql() is deprecated use "
-                "DatabaseOperations.lookup_cast() instead."
-            ),
-            RemovedInDjango60Warning,
-            stacklevel=2,
-        )
-        return "%s"
 
     def force_group_by(self):
         """
