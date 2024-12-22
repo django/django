a881a2e907feabd7d6b3b8bda2bd095f2aed2f93
Chris Muthig <camuthig@gmail.com>
2024-12-22 16:30:55 +0100

    Fixed #35444 -- Added generic support for Aggregate.order_by.
    This moves the behaviors of `order_by` used in Postgres aggregates into
    the `Aggregate` class. This allows for creating aggregate functions that
    support this behavior across all database engines. This is shown by
    moving the `StringAgg` class into the shared `aggregates` module and
    adding support for all databases. The Postgres `StringAgg` class is now
    a thin wrapper on the new shared `StringAgg` class.
    
    Thank you Simon Charette for the review.

--- django/contrib/gis/db/models/aggregates.py
+++ django/contrib/gis/db/models/aggregates.py
@@ -33,15 +33,14 @@ class GeoAggregate(Aggregate):
         if not self.is_extent:
             tolerance = self.extra.get("tolerance") or getattr(self, "tolerance", 0.05)
             clone = self.copy()
-            source_expressions = self.get_source_expressions()
-            source_expressions.pop()  # Don't wrap filters with SDOAGGRTYPE().
+            *source_exprs, filter_expr, order_by_expr = self.get_source_expressions()
             spatial_type_expr = Func(
-                *source_expressions,
+                *source_exprs,
                 Value(tolerance),
                 function="SDOAGGRTYPE",
                 output_field=self.output_field,
             )
-            source_expressions = [spatial_type_expr, self.filter]
+            source_expressions = [spatial_type_expr, filter_expr, order_by_expr]
             clone.set_source_expressions(source_expressions)
             return clone.as_sql(compiler, connection, **extra_context)
         return self.as_sql(compiler, connection, **extra_context)
