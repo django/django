102b054aca02e7f25eb5f653532439878fd5c585
Aman Sharma <210100011@iitb.ac.in>
2024-12-24 18:35:40 +0530

    Added test

--- tests/postgres_tests/test_ranges.py
+++ tests/postgres_tests/test_ranges.py
@@ -304,6 +304,12 @@ class TestQuerying(PostgreSQLTestCase):
             [self.objs[0]],
         )
 
+    def test_decimal_contains_range(self):
+        self.assertSequenceEqual(
+            RangesModel.objects.filter(decimals__contains = 199),
+            []
+        )
+
     def test_contained_by(self):
         self.assertSequenceEqual(
             RangesModel.objects.filter(ints__contained_by=NumericRange(0, 20)),
