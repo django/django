97ee8b82c2c99bf352df5359bb24a42ea78585b8
antoliny0919 <antoliny0919@gmail.com>
2024-12-25 11:35:27 +0900

    Fixed #36032 -- Rendered URLField values as links in the admin.

--- tests/admin_changelist/tests.py
+++ tests/admin_changelist/tests.py
@@ -1059,12 +1059,21 @@ class ChangeListTests(TestCase):
 
     def test_link_field_display_links(self):
         self.client.force_login(self.superuser)
-        g = Genre.objects.create(name="Blues", file="documents/blues_history.txt")
+        g = Genre.objects.create(
+            name="Blues",
+            file="documents/blues_history.txt",
+            url="http://blues_history.com",
+        )
         response = self.client.get(reverse("admin:admin_changelist_genre_changelist"))
         self.assertContains(
             response,
             '<a href="/admin/admin_changelist/genre/%s/change/">'
             "documents/blues_history.txt</a>" % g.pk,
+        )
+        self.assertContains(
+            response,
+            '<a href="/admin/admin_changelist/genre/%s/change/">'
+            "http://blues_history.com</a>" % g.pk,
         )
 
     def test_clear_all_filters_link(self):
