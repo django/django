d8f093908c504ae0dbc39d3f5231f7d7920dde37
Tim Graham <timograham@gmail.com>
2025-01-04 20:27:28 -0500

    Fixed #35448 -- Fixed formatting of test --debug-sql output.
    Also adds DatabaseOperations.format_debug_sql() hook for backends
    (e.g. NoSQL) to customize formatting.

--- tests/backends/tests.py
+++ tests/backends/tests.py
@@ -77,7 +77,12 @@ class LastExecutedQueryTest(TestCase):
             connection.ops.last_executed_query(cursor, "", ())
 
     def test_debug_sql(self):
-        list(Reporter.objects.filter(first_name="test"))
+        qs = Reporter.objects.filter(first_name="test")
+        ops = connections[qs.db].ops
+        with mock.patch.object(ops, "format_debug_sql") as format_debug_sql:
+            list(qs)
+        # Queries are formatted with DatabaseOperations.format_debug_sql().
+        format_debug_sql.assert_called()
         sql = connection.queries[-1]["sql"].lower()
         self.assertIn("select", sql)
         self.assertIn(Reporter._meta.db_table, sql)
