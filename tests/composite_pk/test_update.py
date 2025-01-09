af6336f2c82fd6e5a0e42a7650a3a132c3362a0f
Simon Charette <charette.s@gmail.com>
2025-01-09 00:29:58 -0500

    Fixed #36074 -- Excluded composite primary key fields on save() updates.

--- tests/composite_pk/test_update.py
+++ tests/composite_pk/test_update.py
@@ -1,3 +1,4 @@
+from django.db import connection
 from django.test import TestCase
 
 from .models import Comment, Tenant, Token, User
@@ -45,7 +46,11 @@ class CompositePKUpdateTests(TestCase):
         email = "user9314@example.com"
         user = User.objects.get(pk=self.user_1.pk)
         user.email = email
-        user.save()
+        with self.assertNumQueries(1) as ctx:
+            user.save()
+        sql = ctx[0]["sql"]
+        self.assertEqual(sql.count(connection.ops.quote_name("tenant_id")), 1)
+        self.assertEqual(sql.count(connection.ops.quote_name("id")), 1)
         user.refresh_from_db()
         self.assertEqual(user.email, email)
         user = User.objects.get(pk=self.user_1.pk)
