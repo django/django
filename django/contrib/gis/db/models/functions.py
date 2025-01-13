a3b3e4e9ede4e81bf8b03f0b20849a173bb6960b
enprava <epradavazquez@gmail.com>
2025-01-13 23:50:10 +0100

    Refs #35705 -- Set origin argument to None initially

--- django/contrib/gis/db/models/functions.py
+++ django/contrib/gis/db/models/functions.py
@@ -531,14 +531,15 @@ class Reverse(GeoFunc):
 
 
 class Rotate(GeomOutputGeoFunc):
-    def __init__(self, expression, angle, origin=Point(0, 0), **extra):
-        if not isinstance(origin, Point):
-            raise TypeError("origin argument must be a Point")
+    def __init__(self, expression, angle, origin=None, **extra):
         expressions = [
             expression,
             self._handle_param(angle, "angle", NUMERIC_TYPES),
-            Value(origin.wkt, output_field=GeometryField()),
         ]
+        if origin is not None:
+            if not isinstance(origin, Point):
+                raise TypeError("origin argument must be a Point")
+            expressions.append(Value(origin.wkt, output_field=GeometryField()))
         super().__init__(*expressions, **extra)
 
 
