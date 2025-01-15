1eb62e5b622ef7fd6e0123d8bbf6662d893d5d08
Sarah Boyce <42296566+sarahboyce@users.noreply.github.com>
2025-01-15 21:19:43 +0100

    [5.2.x] Bumped django_next_version in docs config.

--- docs/conf.py
+++ docs/conf.py
@@ -135,7 +135,7 @@ else:
     release = django_release()
 
 # The "development version" of Django
-django_next_version = "5.2"
+django_next_version = "6.0"
 
 extlinks = {
     "bpo": ("https://bugs.python.org/issue?@action=redirect&bpo=%s", "bpo-%s"),
