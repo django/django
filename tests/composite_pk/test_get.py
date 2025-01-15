1602666b79ab438930b74b576f2fa1b6bcf0377b
Jacob Walls <jacobtylerwalls@gmail.com>
2025-01-15 07:49:12 -0500

    Fixed #36100 -- Checked if composite pk is set in get_next/get_previous.

--- tests/composite_pk/test_get.py
+++ tests/composite_pk/test_get.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from .models import Comment, Tenant, User
+from .models import Comment, Tenant, TimeStamped, User
 
 
 class CompositePKGetTests(TestCase):
@@ -124,3 +124,12 @@ class CompositePKGetTests(TestCase):
 
     def test_get_user_by_comments(self):
         self.assertEqual(User.objects.get(comments=self.comment_1), self.user_1)
+
+    def test_get_previous_by_field(self):
+        stamp_1 = TimeStamped.objects.create(id=1)
+        stamp_2 = TimeStamped(id=2)
+        msg = "get_next/get_previous cannot be used on unsaved objects."
+        with self.assertRaisesMessage(ValueError, msg):
+            stamp_2.get_previous_by_created()
+        stamp_2.save()
+        self.assertEqual(stamp_2.get_previous_by_created(), stamp_1)
