276fc6dbaf128ebf47c8e7fc58d337f47631d6d8
Simon Charette <charette.s@gmail.com>
2025-01-19 21:35:50 -0500

    Fixed #36116 -- Optimized multi-column ForwardManyToOne prefetching.
    Rely on ColPairs and TupleIn which support a single column to be specified
    to avoid special casing ForwardManyToOne.get_prefetch_querysets().
    
    Thanks Jacob Walls for the report.

--- tests/foreign_object/models/person.py
+++ tests/foreign_object/models/person.py
@@ -107,6 +107,6 @@ class Friendship(models.Model):
         Person,
         from_fields=["to_friend_country_id", "to_friend_id"],
         to_fields=["person_country_id", "id"],
-        related_name="to_friend",
+        related_name="to_friend+",
         on_delete=models.CASCADE,
     )
