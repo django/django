acfb30b6f09b7e3d9c84e01766ff68db00dbe6b8
Mike Edmunds <medmunds@gmail.com>
2025-01-26 16:08:03 -0800

    Fixed #36477 -- Implemented deprecate_posargs decorator.
    Added a `@deprecate_posargs` decorator to simplify converting
    positional-or-keyword parameters to keyword-only, with a deprecation
    period.

--- django/utils/deprecation.py
+++ django/utils/deprecation.py
@@ -1,5 +1,7 @@
+import functools
 import inspect
 import warnings
+from collections import Counter
 
 from asgiref.sync import iscoroutinefunction, markcoroutinefunction, sync_to_async
 
@@ -81,6 +83,204 @@ class RenameMethodsBase(type):
                     setattr(base, old_method_name, wrapper(new_method))
 
         return new_class
+
+
+def deprecate_posargs(category, *, moved):
+    """
+    Function/method decorator to deprecate some or all positional arguments.
+    The decorated function will map any positional arguments after the `*`
+    to the corresponding keyword arguments and issue a deprecation warning.
+
+    Works on both functions and methods. To apply to a class constructor,
+    decorate its __init__() method. To apply to a staticmethod or classmethod,
+    use @deprecate_posargs after @staticmethod or @classmethod.
+
+    Example: to deprecate passing option1 or option2 as posargs, change::
+
+        def some_func(request, option1, option2=True):
+            ...
+
+    to::
+
+        @deprecate_posargs(RemovedInDjangoXXWarning, moved=["option1", "option2"])
+        def some_func(request, *, option1, option2=True):
+            ...
+
+    `moved` must be a list of the formerly positional-or-keyword parameters
+    that are now keyword-only, in their original positional order.
+
+    After the deprecation period, remove the decorator (but keep the `*`)::
+
+        def some_func(request, *, option1, option2=True):
+            ...
+
+    Caution: during the deprecation period, do not add any new _positional_
+    parameters or change the remaining (un-moved) ones. This attempt to add
+    a new param would break code using the deprecated option posargs::
+
+        @deprecate_posargs(RemovedInDjangoXXWarning, moved=["option1", "option2"])
+        def some_func(request, wrong_new_param=None, *, option1, option2=True):
+            # Broken: existing code may set wrong_new_param to option1's value.
+            ...
+
+    However, it's acceptable to add new _keyword-only_ parameters and to re-order
+    the existing ones (so long as `moved` remains in the original order).
+    This change will work without breaking existing code::
+
+        @deprecate_posargs(RemovedInDjangoXXWarning, moved=["option1", "option2"])
+        def some_func(request, *, new_param=None, option2=True, option1):
+            ...
+
+    The @deprecate_posargs decorator adds a small amount of overhead. In most cases
+    it won't be significant, but use with care in performance-critical code paths.
+    """
+
+    def decorator(func):
+        if isinstance(func, type):
+            raise TypeError(
+                "@deprecate_posargs cannot be applied to a class."
+                " (Apply it to the __init__ method.)"
+            )
+        if isinstance(func, classmethod):
+            raise TypeError("Apply @classmethod before @deprecate_posargs.")
+        if isinstance(func, staticmethod):
+            raise TypeError("Apply @staticmethod before @deprecate_posargs.")
+
+        params = inspect.signature(func).parameters
+        num_by_kind = Counter(param.kind for param in params.values())
+
+        if num_by_kind[inspect.Parameter.VAR_POSITIONAL] > 0:
+            raise TypeError(
+                "@deprecate_posargs() cannot be used with variable positional `*args`."
+            )
+
+        num_positional_params = (
+            num_by_kind[inspect.Parameter.POSITIONAL_ONLY]
+            + num_by_kind[inspect.Parameter.POSITIONAL_OR_KEYWORD]
+        )
+        num_keyword_only_params = num_by_kind[inspect.Parameter.KEYWORD_ONLY]
+        if num_keyword_only_params < 1:
+            raise TypeError(
+                "@deprecate_posargs() requires at least one keyword-only parameter"
+                " (after a `*` entry in the parameters list)."
+            )
+        if any(
+            name not in params or params[name].kind != inspect.Parameter.KEYWORD_ONLY
+            for name in moved
+        ):
+            raise TypeError(
+                "@deprecate_posargs() `moved` names must"
+                " all be keyword-only parameters."
+            )
+
+        num_moveable_args = len(moved)
+        max_positional_args = num_positional_params + num_moveable_args
+
+        param_names = list(params.keys())
+
+        # The deprecation message logic needs to know if func is a bound method.
+        # Because decorators always receive plain, unbound functions, the class
+        # information is not available here. (inspect.ismethod(func) will always
+        # return False, and func.__self__ will never be set.) This only affects
+        # message construction, not argument mapping logic, so it's fine to just
+        # look for the customary bound parameter names.
+        num_bound_params = 1 if param_names[0] in ("self", "cls") else 0
+        func_name = func.__name__
+        if func_name == "__init__":
+            # In the warning, show "ClassName(...)" rather than "__init__(...)".
+            # The class isn't defined yet, but its name is in __qualname__.
+            # Some examples: "ClassName.__init__", "Nested.ClassName.__init__",
+            # "MyTests.test_case.<locals>.ClassName.__init__".
+            try:
+                local_name = func.__qualname__.rsplit("<locals>.", 1)[-1]
+                class_name = local_name.replace(".__init__", "")
+            except (AttributeError, IndexError, TypeError):
+                # __init__ outside a class or other oddity. The __name__ is fine.
+                pass
+            else:
+                func_name = class_name
+
+        if num_positional_params > num_bound_params:
+            # Clarify that only "some" (not all) positional arguments are affected.
+            message_template = (
+                "Use of some positional arguments is deprecated."
+                " Change to `{replacement}`."
+            )
+        else:
+            message_template = (
+                "Use of positional arguments is deprecated."
+                " Change to `{replacement}`."
+            )
+
+        def remap_deprecated_args(args, kwargs):
+            """
+            Move deprecated positional args to kwargs and issue a warning.
+            Returns updated (args, kwargs).
+            """
+            num_positional_args = len(args)
+            if num_positional_args > max_positional_args:
+                raise TypeError(
+                    f"{func_name}() takes"
+                    f" at most {max_positional_args} positional argument(s)"
+                    f" (including {num_moveable_args} deprecated)"
+                    f" but {num_positional_args} were given."
+                )
+
+            moved_names = moved[: num_positional_args - num_positional_params]
+            conflicts = set(moved_names) & set(kwargs)
+            if conflicts:
+                # Report duplicate param names in original parameter order.
+                conflicts_str = ", ".join(
+                    f"'{name}'" for name in moved_names if name in conflicts
+                )
+                raise TypeError(
+                    f"{func_name}() got both deprecated positional and keyword"
+                    f" argument values for {conflicts_str}."
+                )
+
+            # Do the remapping.
+            moved_kwargs = dict(zip(moved_names, args[num_positional_params:]))
+            remaining_args = args[:num_positional_params]
+            updated_kwargs = kwargs | moved_kwargs
+
+            # Issue the deprecation warning. Construct a suggested replacement
+            # showing the affected arguments:
+            #     "Change to `func_name(..., kwonly1=..., kwonly2=..., ...)`."
+            # - Initial "..." represents remaining (non-remapped) positional args,
+            #   excluding `self` or `cls`.
+            # - Trailing "..." represents other (non-remapped) keyword args.
+            replacement_args = [f"{name}=..." for name in moved_names]
+            if len(remaining_args) > num_bound_params:
+                replacement_args.insert(0, "...")
+            if kwargs:
+                replacement_args.append("...")
+            replacement_args_str = ", ".join(replacement_args)
+            replacement = f"{func_name}({replacement_args_str})"
+
+            message = message_template.format(replacement=replacement)
+            warnings.warn(message, category, stacklevel=3)
+
+            return remaining_args, updated_kwargs
+
+        if iscoroutinefunction(func):
+
+            @functools.wraps(func)
+            async def wrapper(*args, **kwargs):
+                if len(args) > num_positional_params:
+                    args, kwargs = remap_deprecated_args(args, kwargs)
+                return await func(*args, **kwargs)
+
+        else:
+
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs):
+                if len(args) > num_positional_params:
+                    args, kwargs = remap_deprecated_args(args, kwargs)
+                return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
 
 
 class MiddlewareMixin:
