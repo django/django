771c250b106f8f4beda42c104c60498ed568b5e2
Simon Charette <charette.s@gmail.com>
2025-02-09 21:42:06 -0500

    [5.2.x] Fixed #36181 -- Allowed Subquery usage in __in lookups against composite pks.
    Thanks Jacob Walls for the report.
    
    Backport of 8561100425876bde3be4b2a22324655f74ff9609 from main.

--- tests/composite_pk/test_filter.py
+++ tests/composite_pk/test_filter.py
@@ -442,6 +442,11 @@ class CompositePKFilterTests(TestCase):
         with self.assertRaisesMessage(ValueError, msg):
             Comment.objects.filter(text__gt=Cast(F("pk"), TextField())).count()
 
+    def test_explicit_subquery(self):
+        subquery = Subquery(User.objects.values("pk"))
+        self.assertEqual(User.objects.filter(pk__in=subquery).count(), 4)
+        self.assertEqual(Comment.objects.filter(user__in=subquery).count(), 5)
+
     def test_filter_case_when(self):
         msg = "When expression does not support composite primary keys."
         with self.assertRaisesMessage(ValueError, msg):
