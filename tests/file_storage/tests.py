0d1dd6bba0c18b7feb6caa5cbd8df80fbac54afd
Gaël Utard <gael.utard@eedf.fr>
2025-02-15 15:55:33 +0100

    Fixed #36191 -- Truncated the overwritten file content in FileSystemStorage.

--- tests/file_storage/tests.py
+++ tests/file_storage/tests.py
@@ -634,6 +634,18 @@ class OverwritingStorageTests(FileStorageTests):
         finally:
             self.storage.delete(name)
 
+    def test_save_overwrite_behavior_truncate(self):
+        name = "test.file"
+        original_content = b"content extra extra extra"
+        new_smaller_content = b"content"
+        self.storage.save(name, ContentFile(original_content))
+        try:
+            self.storage.save(name, ContentFile(new_smaller_content))
+            with self.storage.open(name) as fp:
+                self.assertEqual(fp.read(), new_smaller_content)
+        finally:
+            self.storage.delete(name)
+
     def test_save_overwrite_behavior_temp_file(self):
         """Saving to same file name twice overwrites the first file."""
         name = "test.file"
