38faca9a1053e59bae7ea392efd3d39e807643cd
Sarah Boyce <42296566+sarahboyce@users.noreply.github.com>
2025-02-19 09:42:27 +0100

    Refs #35167 -- Fixed test test_bulk_update_custom_get_prep_value on Oracle < 23c.

--- tests/model_fields/test_jsonfield.py
+++ tests/model_fields/test_jsonfield.py
@@ -305,14 +305,12 @@ class TestSaveLoad(TestCase):
         self.assertEqual(obj.value, value)
 
     def test_bulk_update_custom_get_prep_value(self):
-        objs = CustomSerializationJSONModel.objects.bulk_create(
-            [CustomSerializationJSONModel(pk=1, json_field={"version": "1"})]
-        )
-        objs[0].json_field["version"] = "1-alpha"
-        CustomSerializationJSONModel.objects.bulk_update(objs, ["json_field"])
+        obj = CustomSerializationJSONModel.objects.create(json_field={"version": "1"})
+        obj.json_field = {"version": "2"}
+        CustomSerializationJSONModel.objects.bulk_update([obj], ["json_field"])
         self.assertSequenceEqual(
             CustomSerializationJSONModel.objects.values("json_field"),
-            [{"json_field": '{"version": "1-alpha"}'}],
+            [{"json_field": '{"version": "2"}'}],
         )
 
 
