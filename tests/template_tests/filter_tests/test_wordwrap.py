e88f7376fe68dbf4ebaf11fad1513ce700b45860
Sarah Boyce <42296566+sarahboyce@users.noreply.github.com>
2025-02-25 09:40:54 +0100

    [4.2.x] Fixed CVE-2025-26699 -- Mitigated potential DoS in wordwrap template filter.
    Thanks sw0rd1ight for the report.
    
    Backport of 55d89e25f4115c5674cdd9b9bcba2bb2bb6d820b from main.

--- tests/template_tests/filter_tests/test_wordwrap.py
+++ tests/template_tests/filter_tests/test_wordwrap.py
@@ -78,3 +78,14 @@ class FunctionTests(SimpleTestCase):
             "this is a long\nparagraph of\ntext that\nreally needs\nto be wrapped\n"
             "I'm afraid",
         )
+
+    def test_wrap_long_text(self):
+        long_text = (
+            "this is a long paragraph of text that really needs"
+            " to be wrapped I'm afraid " * 20_000
+        )
+        self.assertIn(
+            "this is a\nlong\nparagraph\nof text\nthat\nreally\nneeds to\nbe wrapped\n"
+            "I'm afraid",
+            wordwrap(long_text, 10),
+        )
