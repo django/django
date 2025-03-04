django/django

commit 40b3975e7d3e1464a733c69171ad7d38f8814280

Fixed #34462 -- Made admin log actions in bulk.
This also deprecates ModelAdmin.log_deletion() and
LogEntryManager.log_action().

django/contrib/admin/models.py
@@ -1,4 +1,5 @@
 import json
+import warnings
 
 from django.conf import settings
 from django.contrib.admin.utils import quote
@@ -6,6 +7,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.urls import NoReverseMatch, reverse
 from django.utils import timezone
+from django.utils.deprecation import RemovedInDjango60Warning
 from django.utils.text import get_text_list
 from django.utils.translation import gettext
 from django.utils.translation import gettext_lazy as _
@@ -33,6 +35,11 @@ class LogEntryManager(models.Manager):
         action_flag,
         change_message="",
     ):
+        warnings.warn(
+            "LogEntryManager.log_action() is deprecated. Use log_actions() instead.",
+            RemovedInDjango60Warning,
+            stacklevel=2,
+        )
         if isinstance(change_message, list):
             change_message = json.dumps(change_message)
         return self.model.objects.create(
@@ -43,6 +50,55 @@ class LogEntryManager(models.Manager):
             action_flag=action_flag,
             change_message=change_message,
         )
+
+    def log_actions(
+        self, user_id, queryset, action_flag, change_message="", *, single_object=False
+    ):
+        # RemovedInDjango60Warning.
+        if type(self).log_action != LogEntryManager.log_action:
+            warnings.warn(
+                "The usage of log_action() is deprecated. Implement log_actions() "
+                "instead.",
+                RemovedInDjango60Warning,
+                stacklevel=2,
+            )
+            return [
+                self.log_action(
+                    user_id=user_id,
+                    content_type_id=ContentType.objects.get_for_model(
+                        obj, for_concrete_model=False
+                    ).id,
+                    object_id=obj.pk,
+                    object_repr=str(obj),
+                    action_flag=action_flag,
+                    change_message=change_message,
+                )
+                for obj in queryset
+            ]
+
+        if isinstance(change_message, list):
+            change_message = json.dumps(change_message)
+
+        log_entry_list = [
+            self.model(
+                user_id=user_id,
+                content_type_id=ContentType.objects.get_for_model(
+                    obj, for_concrete_model=False
+                ).id,
+                object_id=obj.pk,
+                object_repr=str(obj)[:200],
+                action_flag=action_flag,
+                change_message=change_message,
+            )
+            for obj in queryset
+        ]
+
+        if single_object and log_entry_list:
+            instance = log_entry_list[0]
+            instance.save()
+            return instance
+
+        return self.model.objects.bulk_create(log_entry_list)
 
 
 class LogEntry(models.Model):

django/contrib/admin/options.py
@@ -2,6 +2,7 @@ import copy
 import enum
 import json
 import re
+import warnings
 from functools import partial, update_wrapper
 from urllib.parse import parse_qsl
 from urllib.parse import quote as urlquote
@@ -54,6 +55,7 @@ from django.http.response import HttpResponseBase
 from django.template.response import SimpleTemplateResponse, TemplateResponse
 from django.urls import reverse
 from django.utils.decorators import method_decorator
+from django.utils.deprecation import RemovedInDjango60Warning
 from django.utils.html import format_html
 from django.utils.http import urlencode
 from django.utils.safestring import mark_safe
@@ -945,13 +947,12 @@ class ModelAdmin(BaseModelAdmin):
         """
         from django.contrib.admin.models import ADDITION, LogEntry
 
-        return LogEntry.objects.log_action(
+        return LogEntry.objects.log_actions(
             user_id=request.user.pk,
-            content_type_id=get_content_type_for_model(obj).pk,
-            object_id=obj.pk,
-            object_repr=str(obj),
+            queryset=[obj],
             action_flag=ADDITION,
             change_message=message,
+            single_object=True,
         )
 
     def log_change(self, request, obj, message):
@@ -962,13 +963,12 @@ class ModelAdmin(BaseModelAdmin):
         """
         from django.contrib.admin.models import CHANGE, LogEntry
 
-        return LogEntry.objects.log_action(
+        return LogEntry.objects.log_actions(
             user_id=request.user.pk,
-            content_type_id=get_content_type_for_model(obj).pk,
-            object_id=obj.pk,
-            object_repr=str(obj),
+            queryset=[obj],
             action_flag=CHANGE,
             change_message=message,
+            single_object=True,
         )
 
     def log_deletion(self, request, obj, object_repr):
@@ -978,6 +978,11 @@ class ModelAdmin(BaseModelAdmin):
 
         The default implementation creates an admin LogEntry object.
         """
+        warnings.warn(
+            "ModelAdmin.log_deletion() is deprecated. Use log_deletions() instead.",
+            RemovedInDjango60Warning,
+            stacklevel=2,
+        )
         from django.contrib.admin.models import DELETION, LogEntry
 
         return LogEntry.objects.log_action(
@@ -985,6 +990,31 @@ class ModelAdmin(BaseModelAdmin):
             content_type_id=get_content_type_for_model(obj).pk,
             object_id=obj.pk,
             object_repr=object_repr,
+            action_flag=DELETION,
+        )
+
+    def log_deletions(self, request, queryset):
+        """
+        Log that objects will be deleted. Note that this method must be called
+        before the deletion.
+
+        The default implementation creates admin LogEntry objects.
+        """
+        from django.contrib.admin.models import DELETION, LogEntry
+
+        # RemovedInDjango60Warning.
+        if type(self).log_deletion != ModelAdmin.log_deletion:
+            warnings.warn(
+                "The usage of log_deletion() is deprecated. Implement log_deletions() "
+                "instead.",
+                RemovedInDjango60Warning,
+                stacklevel=2,
+            )
+            return [self.log_deletion(request, obj, str(obj)) for obj in queryset]
+
+        return LogEntry.objects.log_actions(
+            user_id=request.user.pk,
+            queryset=queryset,
             action_flag=DELETION,
         )
 
@@ -2174,7 +2204,7 @@ class ModelAdmin(BaseModelAdmin):
             obj_display = str(obj)
             attr = str(to_field) if to_field else self.opts.pk.attname
             obj_id = obj.serializable_value(attr)
-            self.log_deletion(request, obj, obj_display)
+            self.log_deletions(request, [obj])
             self.delete_model(request, obj)
 
             return self.response_delete(request, obj_display, obj_id)

tests/admin_changelist/tests.py
@@ -16,7 +16,6 @@ from django.contrib.admin.views.main import (
     TO_FIELD_VAR,
 )
 from django.contrib.auth.models import User
-from django.contrib.contenttypes.models import ContentType
 from django.contrib.messages.storage.cookie import CookieStorage
 from django.db import DatabaseError, connection, models
 from django.db.models import F, Field, IntegerField
@@ -1636,8 +1635,7 @@ class GetAdminLogTests(TestCase):
         """{% get_admin_log %} works without specifying a user."""
         user = User(username="jondoe", password="secret", email="super@example.com")
         user.save()
-        ct = ContentType.objects.get_for_model(User)
-        LogEntry.objects.log_action(user.pk, ct.pk, user.pk, repr(user), 1)
+        LogEntry.objects.log_actions(user.pk, [user], 1, single_object=True)
         context = Context({"log_entries": LogEntry.objects.all()})
         t = Template(
             "{% load log %}"
@@ -1646,7 +1644,7 @@ class GetAdminLogTests(TestCase):
             "{{ entry|safe }}"
             "{% endfor %}"
         )
-        self.assertEqual(t.render(context), "Added “<User: jondoe>”.")
+        self.assertEqual(t.render(context), "Added “jondoe”.")
 
     def test_missing_args(self):
         msg = "'get_admin_log' statements require two arguments"

tests/admin_utils/test_logentry.py
@@ -8,9 +8,10 @@ from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase, override_settings
 from django.urls import reverse
 from django.utils import translation
+from django.utils.deprecation import RemovedInDjango60Warning
 from django.utils.html import escape
 
-from .models import Article, ArticleProxy, Car, Site
+from .models import Article, ArticleProxy, Car, InheritedLogEntryManager, Site
 
 
 @override_settings(ROOT_URLCONF="admin_utils.urls")
@@ -26,14 +27,22 @@ class LogEntryTests(TestCase):
             title="Title",
             created=datetime(2008, 3, 12, 11, 54),
         )
-        content_type_pk = ContentType.objects.get_for_model(Article).pk
-        LogEntry.objects.log_action(
+        cls.a2 = Article.objects.create(
+            site=cls.site,
+            title="Title 2",
+            created=datetime(2009, 3, 12, 11, 54),
+        )
+        cls.a3 = Article.objects.create(
+            site=cls.site,
+            title="Title 3",
+            created=datetime(2010, 3, 12, 11, 54),
+        )
+        LogEntry.objects.log_actions(
             cls.user.pk,
-            content_type_pk,
-            cls.a1.pk,
-            repr(cls.a1),
+            [cls.a1],
             CHANGE,
             change_message="Changed something",
+            single_object=True,
         )
 
     def setUp(self):
@@ -227,17 +236,94 @@ class LogEntryTests(TestCase):
         logentry = LogEntry.objects.first()
         self.assertEqual(repr(logentry), str(logentry.action_time))
 
+    # RemovedInDjango60Warning.
     def test_log_action(self):
-        content_type_pk = ContentType.objects.get_for_model(Article).pk
-        log_entry = LogEntry.objects.log_action(
-            self.user.pk,
-            content_type_pk,
-            self.a1.pk,
-            repr(self.a1),
-            CHANGE,
-            change_message="Changed something else",
-        )
+        msg = "LogEntryManager.log_action() is deprecated. Use log_actions() instead."
+        content_type_val = ContentType.objects.get_for_model(Article).pk
+        with self.assertWarnsMessage(RemovedInDjango60Warning, msg):
+            log_entry = LogEntry.objects.log_action(
+                self.user.pk,
+                content_type_val,
+                self.a1.pk,
+                repr(self.a1),
+                CHANGE,
+                change_message="Changed something else",
+            )
         self.assertEqual(log_entry, LogEntry.objects.latest("id"))
+
+    def test_log_actions(self):
+        queryset = Article.objects.all().order_by("-id")
+        msg = "Deleted Something"
+        content_type = ContentType.objects.get_for_model(self.a1)
+        self.assertEqual(len(queryset), 3)
+        with self.assertNumQueries(1):
+            LogEntry.objects.log_actions(
+                self.user.pk,
+                queryset,
+                DELETION,
+                change_message=msg,
+            )
+        logs = (
+            LogEntry.objects.filter(action_flag=DELETION)
+            .order_by("id")
+            .values_list(
+                "user",
+                "content_type",
+                "object_id",
+                "object_repr",
+                "action_flag",
+                "change_message",
+            )
+        )
+        expected_log_values = [
+            (
+                self.user.pk,
+                content_type.id,
+                str(obj.pk),
+                str(obj),
+                DELETION,
+                msg,
+            )
+            for obj in queryset
+        ]
+        self.assertSequenceEqual(logs, expected_log_values)
+
+    # RemovedInDjango60Warning.
+    def test_log_action_fallback(self):
+        LogEntry.objects2 = InheritedLogEntryManager()
+        queryset = Article.objects.all().order_by("-id")
+        content_type = ContentType.objects.get_for_model(self.a1)
+        self.assertEqual(len(queryset), 3)
+        msg = (
+            "The usage of log_action() is deprecated. Implement log_actions() instead."
+        )
+        with self.assertNumQueries(3):
+            with self.assertWarnsMessage(RemovedInDjango60Warning, msg):
+                LogEntry.objects2.log_actions(self.user.pk, queryset, DELETION)
+        log_values = (
+            LogEntry.objects.filter(action_flag=DELETION)
+            .order_by("id")
+            .values_list(
+                "user",
+                "content_type",
+                "object_id",
+                "object_repr",
+                "action_flag",
+                "change_message",
+            )
+        )
+        expected_log_values = [
+            (
+                self.user.pk,
+                content_type.id,
+                str(obj.pk),
+                "Test Repr",
+                DELETION,
+                "",
+            )
+            for obj in queryset
+        ]
+        self.assertSequenceEqual(log_values, expected_log_values)
 
     def test_recentactions_without_content_type(self):
         """
@@ -248,7 +334,7 @@ class LogEntryTests(TestCase):
         link = reverse("admin:admin_utils_article_change", args=(quote(self.a1.pk),))
         should_contain = """<a href="%s">%s</a>""" % (
             escape(link),
-            escape(repr(self.a1)),
+            escape(str(self.a1)),
         )
         self.assertContains(response, should_contain)
         should_contain = "Article"
@@ -320,28 +406,29 @@ class LogEntryTests(TestCase):
                 self.assertEqual(log.get_action_flag_display(), display_name)
 
     def test_hook_get_log_entries(self):
-        LogEntry.objects.log_action(
+        LogEntry.objects.log_actions(
             self.user.pk,
-            ContentType.objects.get_for_model(Article).pk,
-            self.a1.pk,
-            "Article changed",
+            [self.a1],
             CHANGE,
             change_message="Article changed message",
+            single_object=True,
         )
         c1 = Car.objects.create()
-        LogEntry.objects.log_action(
+        LogEntry.objects.log_actions(
             self.user.pk,
-            ContentType.objects.get_for_model(Car).pk,
-            c1.pk,
-            "Car created",
+            [c1],
             ADDITION,
             change_message="Car created message",
+            single_object=True,
         )
+        exp_str_article = escape(str(self.a1))
+        exp_str_car = escape(str(c1))
+
         response = self.client.get(reverse("admin:index"))
-        self.assertContains(response, "Article changed")
-        self.assertContains(response, "Car created")
+        self.assertContains(response, exp_str_article)
+        self.assertContains(response, exp_str_car)
 
         # site "custom_admin" only renders log entries of registered models
         response = self.client.get(reverse("custom_admin:index"))
-        self.assertContains(response, "Article changed")
-        self.assertNotContains(response, "Car created")
+        self.assertContains(response, exp_str_article)
+        self.assertNotContains(response, exp_str_car)

tests/admin_views/test_history_view.py
@@ -1,7 +1,6 @@
 from django.contrib.admin.models import CHANGE, LogEntry
 from django.contrib.admin.tests import AdminSeleniumTestCase
 from django.contrib.auth.models import User
-from django.contrib.contenttypes.models import ContentType
 from django.core.paginator import Paginator
 from django.test import TestCase, override_settings
 from django.urls import reverse
@@ -61,15 +60,13 @@ class SeleniumTests(AdminSeleniumTestCase):
             password="secret",
             email="super@example.com",
         )
-        content_type_pk = ContentType.objects.get_for_model(User).pk
         for i in range(1, 1101):
-            LogEntry.objects.log_action(
+            LogEntry.objects.log_actions(
                 self.superuser.pk,
-                content_type_pk,
-                self.superuser.pk,
-                repr(self.superuser),
+                [self.superuser],
                 CHANGE,
                 change_message=f"Changed something {i}",
+                single_object=True,
             )
         self.admin_login(
             username="super",

tests/admin_views/tests.py
@@ -3808,33 +3808,27 @@ class AdminViewStringPrimaryKeyTest(TestCase):
             r"""-_.!~*'() ;/?:@&=+$, <>#%" {}|\^[]`"""
         )
         cls.m1 = ModelWithStringPrimaryKey.objects.create(string_pk=cls.pk)
-        content_type_pk = ContentType.objects.get_for_model(
-            ModelWithStringPrimaryKey
-        ).pk
         user_pk = cls.superuser.pk
-        LogEntry.objects.log_action(
+        LogEntry.objects.log_actions(
             user_pk,
-            content_type_pk,
-            cls.pk,
-            cls.pk,
+            [cls.m1],
             2,
             change_message="Changed something",
+            single_object=True,
         )
-        LogEntry.objects.log_action(
+        LogEntry.objects.log_actions(
             user_pk,
-            content_type_pk,
-            cls.pk,
-            cls.pk,
+            [cls.m1],
             1,
             change_message="Added something",
+            single_object=True,
         )
-        LogEntry.objects.log_action(
+        LogEntry.objects.log_actions(
             user_pk,
-            content_type_pk,
-            cls.pk,
-            cls.pk,
+            [cls.m1],
             3,
             change_message="Deleted something",
+            single_object=True,
         )
 
     def setUp(self):

tests/admin_utils/models.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.admin.models import LogEntry, LogEntryManager
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -86,3 +87,26 @@ class VehicleMixin(Vehicle):
 
 class Car(VehicleMixin):
     pass
+
+
+class InheritedLogEntryManager(LogEntryManager):
+    model = LogEntry
+
+    def log_action(
+        self,
+        user_id,
+        content_type_id,
+        object_id,
+        object_repr,
+        action_flag,
+        change_message="",
+    ):
+        return LogEntry.objects.create(
+            user_id=user_id,
+            content_type_id=content_type_id,
+            object_id=str(object_id),
+            # Changing actual repr to test repr
+            object_repr="Test Repr",
+            action_flag=action_flag,
+            change_message=change_message,
+        )

commit 0ef82adaecb9a519e7de66a0533c11490fddf0f0

Fixed #36217 -- Restored pre_save/post_save signal emission via LogEntry.save() for single-object deletion in the admin.
Regression in 40b3975e7d3e1464a733c69171ad7d38f8814280.

Thanks smiling-watermelon for the report.

Co-authored-by: Sarah Boyce <42296566+sarahboyce@users.noreply.github.com>

django/contrib/admin/models.py
@@ -24,9 +24,7 @@ ACTION_FLAG_CHOICES = [
 class LogEntryManager(models.Manager):
     use_in_migrations = True
 
-    def log_actions(
-        self, user_id, queryset, action_flag, change_message="", *, single_object=False
-    ):
+    def log_actions(self, user_id, queryset, action_flag, change_message=""):
         if isinstance(change_message, list):
             change_message = json.dumps(change_message)
 
@@ -44,7 +42,7 @@ class LogEntryManager(models.Manager):
             for obj in queryset
         ]
 
-        if single_object and log_entry_list:
+        if len(log_entry_list) == 1:
             instance = log_entry_list[0]
             instance.save()
             return instance

django/contrib/admin/options.py
@@ -946,7 +946,6 @@ class ModelAdmin(BaseModelAdmin):
             queryset=[obj],
             action_flag=ADDITION,
             change_message=message,
-            single_object=True,
         )
 
     def log_change(self, request, obj, message):
@@ -962,7 +961,6 @@ class ModelAdmin(BaseModelAdmin):
             queryset=[obj],
             action_flag=CHANGE,
             change_message=message,
-            single_object=True,
         )
 
     def log_deletions(self, request, queryset):

tests/admin_changelist/tests.py
@@ -1845,7 +1845,7 @@ class GetAdminLogTests(TestCase):
         """{% get_admin_log %} works without specifying a user."""
         user = User(username="jondoe", password="secret", email="super@example.com")
         user.save()
-        LogEntry.objects.log_actions(user.pk, [user], 1, single_object=True)
+        LogEntry.objects.log_actions(user.pk, [user], 1)
         context = Context({"log_entries": LogEntry.objects.all()})
         t = Template(
             "{% load log %}"

tests/admin_utils/test_logentry.py
@@ -5,6 +5,7 @@ from django.contrib.admin.models import ADDITION, CHANGE, DELETION, LogEntry
 from django.contrib.admin.utils import quote
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
+from django.db.models.signals import post_save, pre_save
 from django.test import TestCase, override_settings
 from django.urls import reverse
 from django.utils import translation
@@ -41,11 +42,23 @@ class LogEntryTests(TestCase):
             [cls.a1],
             CHANGE,
             change_message="Changed something",
-            single_object=True,
         )
 
     def setUp(self):
         self.client.force_login(self.user)
+        self.signals = []
+
+        pre_save.connect(self.pre_save_listener, sender=LogEntry)
+        self.addCleanup(pre_save.disconnect, self.pre_save_listener, sender=LogEntry)
+
+        post_save.connect(self.post_save_listener, sender=LogEntry)
+        self.addCleanup(post_save.disconnect, self.post_save_listener, sender=LogEntry)
+
+    def pre_save_listener(self, instance, **kwargs):
+        self.signals.append(("pre_save", instance))
+
+    def post_save_listener(self, instance, created, **kwargs):
+        self.signals.append(("post_save", instance, created))
 
     def test_logentry_save(self):
         """
@@ -271,6 +284,7 @@ class LogEntryTests(TestCase):
             for obj in queryset
         ]
         self.assertSequenceEqual(logs, expected_log_values)
+        self.assertEqual(self.signals, [])
 
     def test_recentactions_without_content_type(self):
         """
@@ -314,6 +328,8 @@ class LogEntryTests(TestCase):
             "created_1": "00:00",
         }
         changelist_url = reverse("admin:admin_utils_articleproxy_changelist")
+        expected_signals = []
+        self.assertEqual(self.signals, expected_signals)
 
         # add
         proxy_add_url = reverse("admin:admin_utils_articleproxy_add")
@@ -322,6 +338,10 @@ class LogEntryTests(TestCase):
         proxy_addition_log = LogEntry.objects.latest("id")
         self.assertEqual(proxy_addition_log.action_flag, ADDITION)
         self.assertEqual(proxy_addition_log.content_type, proxy_content_type)
+        expected_signals.extend(
+            [("pre_save", proxy_addition_log), ("post_save", proxy_addition_log, True)]
+        )
+        self.assertEqual(self.signals, expected_signals)
 
         # change
         article_id = proxy_addition_log.object_id
@@ -334,6 +354,10 @@ class LogEntryTests(TestCase):
         proxy_change_log = LogEntry.objects.latest("id")
         self.assertEqual(proxy_change_log.action_flag, CHANGE)
         self.assertEqual(proxy_change_log.content_type, proxy_content_type)
+        expected_signals.extend(
+            [("pre_save", proxy_change_log), ("post_save", proxy_change_log, True)]
+        )
+        self.assertEqual(self.signals, expected_signals)
 
         # delete
         proxy_delete_url = reverse(
@@ -344,6 +368,10 @@ class LogEntryTests(TestCase):
         proxy_delete_log = LogEntry.objects.latest("id")
         self.assertEqual(proxy_delete_log.action_flag, DELETION)
         self.assertEqual(proxy_delete_log.content_type, proxy_content_type)
+        expected_signals.extend(
+            [("pre_save", proxy_delete_log), ("post_save", proxy_delete_log, True)]
+        )
+        self.assertEqual(self.signals, expected_signals)
 
     def test_action_flag_choices(self):
         tests = ((1, "Addition"), (2, "Change"), (3, "Deletion"))
@@ -358,7 +386,6 @@ class LogEntryTests(TestCase):
             [self.a1],
             CHANGE,
             change_message="Article changed message",
-            single_object=True,
         )
         c1 = Car.objects.create()
         LogEntry.objects.log_actions(
@@ -366,7 +393,6 @@ class LogEntryTests(TestCase):
             [c1],
             ADDITION,
             change_message="Car created message",
-            single_object=True,
         )
         exp_str_article = escape(str(self.a1))
         exp_str_car = escape(str(c1))

tests/admin_views/test_history_view.py
@@ -66,7 +66,6 @@ class SeleniumTests(AdminSeleniumTestCase):
                 [self.superuser],
                 CHANGE,
                 change_message=f"Changed something {i}",
-                single_object=True,
             )
         self.admin_login(
             username="super",

tests/admin_views/tests.py
@@ -3884,21 +3884,18 @@ class AdminViewStringPrimaryKeyTest(TestCase):
             [cls.m1],
             2,
             change_message="Changed something",
-            single_object=True,
         )
         LogEntry.objects.log_actions(
             user_pk,
             [cls.m1],
             1,
             change_message="Added something",
-            single_object=True,
         )
         LogEntry.objects.log_actions(
             user_pk,
             [cls.m1],
             3,
             change_message="Deleted something",
-            single_object=True,
         )
 
     def setUp(self):