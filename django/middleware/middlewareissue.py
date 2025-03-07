90e1ff847d625142978ae0cd5a9d35d1d926df12
Bhupesh Panwar <panwarbhupesh3@gmail.com>
2025-03-07 23:35:23 +0530

    issue suggestion fix 29466

--- django/middleware/middlewareissue.py
+++ django/middleware/middlewareissue.py
@@ -1,20 +1,18 @@
 from django.template.exceptions import TemplateSyntaxError
+
+
 class ExampleMiddleware:
-    def __init__(self,get_response):
+    def __init__(self, get_response):
         self.get_response = get_response
 
-    def __call__(self,request):
+    def __call__(self, request):
         response = self.get_response(request)
-        view_context = getattr(request, 'view_context', None)
+        view_context = getattr(request, "view_context", None)
         try:
-             response = self.get_response(request)
-             print('This should work')
-             return response
+            response = self.get_response(request)
+            print("This should work")
+            return response
         except TemplateSyntaxError as error:
             if "Could not parse the remainder" in str(error):
-                view_context = getattr(request, 'view_context', None)
+                view_context = getattr(request, "view_context", None)
                 print(f"Context from view: {view_context}")
-       
-
-        
-        
