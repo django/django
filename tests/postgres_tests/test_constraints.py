647dca413262d8778471479e789f96099044af39
Sarah Boyce <42296566+sarahboyce@users.noreply.github.com>
2025-03-07 10:14:30 +0100

    Corrected test case in ExclusionConstraintTests.test_invalid_expressions().

--- tests/postgres_tests/test_constraints.py
+++ tests/postgres_tests/test_constraints.py
@@ -309,7 +309,7 @@ class ExclusionConstraintTests(PostgreSQLTestCase):
 
     def test_invalid_expressions(self):
         msg = "The expressions must be a list of 2-tuples."
-        for expressions in (["foo"], ["foo"], [("foo_1", "foo_2", "foo_3")]):
+        for expressions in (["foo"], [("foo",)], [("foo_1", "foo_2", "foo_3")]):
             with self.subTest(expressions), self.assertRaisesMessage(ValueError, msg):
                 ExclusionConstraint(
                     index_type="GIST",
