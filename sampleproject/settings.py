18ed8ad04168b8133212f5c8a524a2777abad42d
SHRISHTISHUKLA-0 <shrishtishukla01332@gmail.com>
2025-03-08 22:30:29 +0530

    Fixed pending changes

--- sampleproject/settings.py
+++ sampleproject/settings.py
@@ -52,7 +52,7 @@ WSGI_APPLICATION = 'sampleproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR) + "/db.sqlite3",
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
