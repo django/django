ec7044c706f48f5ab3d9e4c35e4078b9f9dcaaf2
Ahmed Nassar <a.moh.nassar00@gmail.com>
2025-03-08 16:35:10 +0200

    Fixed #36000 -- Deprecated HTTP as the default protocol in urlize and urlizetrunc.

--- django/conf/global_settings.py
+++ django/conf/global_settings.py
@@ -662,3 +662,8 @@ SECURE_REDIRECT_EXEMPT = []
 SECURE_REFERRER_POLICY = "same-origin"
 SECURE_SSL_HOST = None
 SECURE_SSL_REDIRECT = False
+
+# RemovedInDjango70Warning: A transitional setting helpful in early adoption of
+# HTTPS as the default protocol in urlize and urlizetrunc when no protocol is
+# provided. Set to True to assume HTTPS during the Django 6.x release cycle.
+URLIZE_ASSUME_HTTPS = False
