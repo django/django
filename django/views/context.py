821e4c53e6784be9048fdab633dc09ab5a0245f2
Bhupesh Panwar <panwarbhupesh3@gmail.com>
2025-03-18 02:27:08 +0530

    suggestionfix 29446

---
+++ django/views/context.py
@@ -0,0 +1,9 @@
+def get_context():
+
+    return {
+        "message": "Hell this is Bhupesh Panwar",
+        "message1": "this is Bhupesh Panwar"
+    }
+
+# for example this is an context( this is an suggestion fix). if somehow we import context/dictionary from the user view to debug.py file. we are able to show the context
+#in error page
