0aa0224107f09c02fbb30bdf6bbc4be49df8c0bf
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2025-03-30 17:54:15 +0200

    [5.1.x] Fixed warnings per flake8 7.2.0.
    https://github.com/PyCQA/flake8/releases/tag/7.2.0
    
    Backport of 281910ff8e9ae98fa78ee5d26ae3f0b713ccf418 from main.

--- django/db/backends/base/base.py
+++ django/db/backends/base/base.py
@@ -220,7 +220,6 @@ class BaseDatabaseWrapper:
 
     def init_connection_state(self):
         """Initialize the database connection settings."""
-        global RAN_DB_VERSION_CHECK
         if self.alias not in RAN_DB_VERSION_CHECK:
             self.check_database_version_supported()
             RAN_DB_VERSION_CHECK.add(self.alias)
