6f13cc8bf2571176e157dde10daf88f86f9593ca
Arfey <Arfey17.mg@gmail.com>
2025-04-17 01:52:42 +0300

    added AsyncConnectionHandler

--- django/db/utils.py
+++ django/db/utils.py
@@ -6,7 +6,7 @@ from django.core.exceptions import ImproperlyConfigured
 
 # For backwards compatibility with Django < 3.2
 from django.utils.connection import ConnectionDoesNotExist  # NOQA: F401
-from django.utils.connection import BaseConnectionHandler
+from django.utils.connection import BaseConnectionHandler, BaseAsyncConnectionHandler
 from django.utils.functional import cached_property
 from django.utils.module_loading import import_string
 
@@ -103,6 +103,43 @@ class DatabaseErrorWrapper:
         return inner
 
 
+def configure_db_settings(databases):
+    if databases == {}:
+        databases[DEFAULT_DB_ALIAS] = {"ENGINE": "django.db.backends.dummy"}
+    elif DEFAULT_DB_ALIAS not in databases:
+        raise ImproperlyConfigured(
+            f"You must define a '{DEFAULT_DB_ALIAS}' database."
+        )
+    elif databases[DEFAULT_DB_ALIAS] == {}:
+        databases[DEFAULT_DB_ALIAS]["ENGINE"] = "django.db.backends.dummy"
+
+    # Configure default settings.
+    for conn in databases.values():
+        conn.setdefault("ATOMIC_REQUESTS", False)
+        conn.setdefault("AUTOCOMMIT", True)
+        conn.setdefault("ENGINE", "django.db.backends.dummy")
+        if conn["ENGINE"] == "django.db.backends." or not conn["ENGINE"]:
+            conn["ENGINE"] = "django.db.backends.dummy"
+        conn.setdefault("CONN_MAX_AGE", 0)
+        conn.setdefault("CONN_HEALTH_CHECKS", False)
+        conn.setdefault("OPTIONS", {})
+        conn.setdefault("TIME_ZONE", None)
+        for setting in ["NAME", "USER", "PASSWORD", "HOST", "PORT"]:
+            conn.setdefault(setting, "")
+
+        test_settings = conn.setdefault("TEST", {})
+        default_test_settings = [
+            ("CHARSET", None),
+            ("COLLATION", None),
+            ("MIGRATE", True),
+            ("MIRROR", None),
+            ("NAME", None),
+        ]
+        for key, value in default_test_settings:
+            test_settings.setdefault(key, value)
+    return databases
+
+
 def load_backend(backend_name):
     """
     Return a database backend's "base" module given a fully qualified database
@@ -148,41 +185,7 @@ class ConnectionHandler(BaseConnectionHandler):
     thread_critical = True
 
     def configure_settings(self, databases):
-        databases = super().configure_settings(databases)
-        if databases == {}:
-            databases[DEFAULT_DB_ALIAS] = {"ENGINE": "django.db.backends.dummy"}
-        elif DEFAULT_DB_ALIAS not in databases:
-            raise ImproperlyConfigured(
-                f"You must define a '{DEFAULT_DB_ALIAS}' database."
-            )
-        elif databases[DEFAULT_DB_ALIAS] == {}:
-            databases[DEFAULT_DB_ALIAS]["ENGINE"] = "django.db.backends.dummy"
-
-        # Configure default settings.
-        for conn in databases.values():
-            conn.setdefault("ATOMIC_REQUESTS", False)
-            conn.setdefault("AUTOCOMMIT", True)
-            conn.setdefault("ENGINE", "django.db.backends.dummy")
-            if conn["ENGINE"] == "django.db.backends." or not conn["ENGINE"]:
-                conn["ENGINE"] = "django.db.backends.dummy"
-            conn.setdefault("CONN_MAX_AGE", 0)
-            conn.setdefault("CONN_HEALTH_CHECKS", False)
-            conn.setdefault("OPTIONS", {})
-            conn.setdefault("TIME_ZONE", None)
-            for setting in ["NAME", "USER", "PASSWORD", "HOST", "PORT"]:
-                conn.setdefault(setting, "")
-
-            test_settings = conn.setdefault("TEST", {})
-            default_test_settings = [
-                ("CHARSET", None),
-                ("COLLATION", None),
-                ("MIGRATE", True),
-                ("MIRROR", None),
-                ("NAME", None),
-            ]
-            for key, value in default_test_settings:
-                test_settings.setdefault(key, value)
-        return databases
+        return configure_db_settings(super().configure_settings(databases))
 
     @property
     def databases(self):
@@ -195,6 +198,24 @@ class ConnectionHandler(BaseConnectionHandler):
         db = self.settings[alias]
         backend = load_backend(db["ENGINE"])
         return backend.DatabaseWrapper(db, alias)
+
+
+class AsyncConnectionHandler(BaseAsyncConnectionHandler):
+    settings_name = "DATABASES"
+
+    def configure_settings(self, databases):
+        return configure_db_settings(super().configure_settings(databases))
+
+    def create_connection(self, alias):
+        db = self.settings[alias]
+        backend = load_backend(db["ENGINE"])
+
+        if not hasattr(backend, 'AsyncDatabaseWrapper'):
+            raise self.exception_class(
+                f"The async connection '{alias}' doesn't exist."
+            )
+
+        return backend.AsyncDatabaseWrapper(db, alias)
 
 
 class ConnectionRouter:
