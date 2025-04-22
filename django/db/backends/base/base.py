a8d896c7debeae45d584c3e278da362cb789b89b
Flavio Curella <flavio.curella@gmail.com>
2025-04-22 10:50:25 -0500

    Implement `ais_usable`

--- django/db/backends/base/base.py
+++ django/db/backends/base/base.py
@@ -828,6 +828,19 @@ class BaseDatabaseWrapper:
             "subclasses of BaseDatabaseWrapper may require an is_usable() method"
         )
 
+    async def ais_usable(self):
+        """
+        Test if the database connection is usable.
+
+        This method may assume that self.connection is not None.
+
+        Actual implementations should take care not to raise exceptions
+        as that may prevent Django from recycling unusable connections.
+        """
+        raise NotImplementedError(
+            "subclasses of BaseDatabaseWrapper may require an ais_usable() method"
+        )
+
     def close_if_health_check_failed(self):
         """Close existing connection if it fails a health check."""
         if (
