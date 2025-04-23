django/django

commit 55d89e25f4115c5674cdd9b9bcba2bb2bb6d820b

Fixed CVE-2025-26699 -- Mitigated potential DoS in wordwrap template filter.
Thanks sw0rd1ight for the report.

django/utils/text.py
@@ -1,6 +1,7 @@
 import gzip
 import re
 import secrets
+import textwrap
 import unicodedata
 from collections import deque
 from gzip import GzipFile
@@ -49,24 +50,15 @@ def wrap(text, width):
     ``width``.
     """
 
-    def _generator():
-        for line in text.splitlines(True):  # True keeps trailing linebreaks
-            max_width = min((line.endswith("\n") and width + 1 or width), width)
-            while len(line) > max_width:
-                space = line[: max_width + 1].rfind(" ") + 1
-                if space == 0:
-                    space = line.find(" ") + 1
-                    if space == 0:
-                        yield line
-                        line = ""
-                        break
-                yield "%s\n" % line[: space - 1]
-                line = line[space:]
-                max_width = min((line.endswith("\n") and width + 1 or width), width)
-            if line:
-                yield line
-
-    return "".join(_generator())
+    wrapper = textwrap.TextWrapper(
+        width=width,
+        break_long_words=False,
+        break_on_hyphens=False,
+    )
+    result = []
+    for line in text.splitlines(True):
+        result.extend(wrapper.wrap(line))
+    return "\n".join(result)
 
 
 def add_truncation_text(text, truncate=None):

tests/template_tests/filter_tests/test_wordwrap.py
@@ -78,3 +78,14 @@ class FunctionTests(SimpleTestCase):
             "this is a long\nparagraph of\ntext that\nreally needs\nto be wrapped\n"
             "I'm afraid",
         )
+
+    def test_wrap_long_text(self):
+        long_text = (
+            "this is a long paragraph of text that really needs"
+            " to be wrapped I'm afraid " * 20_000
+        )
+        self.assertIn(
+            "this is a\nlong\nparagraph\nof text\nthat\nreally\nneeds to\nbe wrapped\n"
+            "I'm afraid",
+            wordwrap(long_text, 10),
+        )

commit cdf5cd4bc60ff2611778779e1798acf191c343d8

Fixed #36341 -- Preserved whitespaces in wordwrap template filter.
Regression in 55d89e25f4115c5674cdd9b9bcba2bb2bb6d820b.

This work improves the django.utils.text.wrap() function to ensure that
empty lines and lines with whitespace only are kept instead of being
dropped.

Thanks Matti Pohjanvirta for the report and fix.

Co-authored-by: Natalia <124304+nessita@users.noreply.github.com>

django/utils/text.py
@@ -54,10 +54,19 @@ def wrap(text, width):
         width=width,
         break_long_words=False,
         break_on_hyphens=False,
+        replace_whitespace=False,
     )
     result = []
-    for line in text.splitlines(True):
-        result.extend(wrapper.wrap(line))
+    for line in text.splitlines():
+        wrapped = wrapper.wrap(line)
+        if not wrapped:
+            # If `line` contains only whitespaces that are dropped, restore it.
+            result.append(line)
+        else:
+            result.extend(wrapped)
+    if text.endswith("\n"):
+        # If `text` ends with a newline, preserve it.
+        result.append("")
     return "\n".join(result)
 
 

tests/template_tests/filter_tests/test_wordwrap.py
@@ -89,3 +89,44 @@ class FunctionTests(SimpleTestCase):
             "I'm afraid",
             wordwrap(long_text, 10),
         )
+
+    def test_wrap_preserve_newlines(self):
+        cases = [
+            (
+                "this is a long paragraph of text that really needs to be wrapped\n\n"
+                "that is followed by another paragraph separated by an empty line\n",
+                "this is a long paragraph of\ntext that really needs to be\nwrapped\n\n"
+                "that is followed by another\nparagraph separated by an\nempty line\n",
+                30,
+            ),
+            ("\n\n\n", "\n\n\n", 5),
+            ("\n\n\n\n\n\n", "\n\n\n\n\n\n", 5),
+        ]
+        for text, expected, width in cases:
+            with self.subTest(text=text):
+                self.assertEqual(wordwrap(text, width), expected)
+
+    def test_wrap_preserve_whitespace(self):
+        width = 5
+        width_spaces = " " * width
+        cases = [
+            (
+                f"first line\n{width_spaces}\nsecond line",
+                f"first\nline\n{width_spaces}\nsecond\nline",
+            ),
+            (
+                "first line\n \t\t\t \nsecond line",
+                "first\nline\n \t\t\t \nsecond\nline",
+            ),
+            (
+                f"first line\n{width_spaces}\nsecond line\n\nthird{width_spaces}\n",
+                f"first\nline\n{width_spaces}\nsecond\nline\n\nthird\n",
+            ),
+            (
+                f"first line\n{width_spaces}{width_spaces}\nsecond line",
+                f"first\nline\n{width_spaces}{width_spaces}\nsecond\nline",
+            ),
+        ]
+        for text, expected in cases:
+            with self.subTest(text=text):
+                self.assertEqual(wordwrap(text, width), expected)