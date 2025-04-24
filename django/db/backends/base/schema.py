02da6c2bc553c7acac92e54939f9b9598a7ec379
Baptiste Mispelon <baptiste.mispelon@torchbox.com>
2025-04-24 10:34:58 +0200

    Fixed 36350 -- Respected Field.db_check() when deciding whether to recreate constraint
    This is a regression introduced by 9953c804a9375956a542da94665662d306dff48d

--- django/db/backends/base/schema.py
+++ django/db/backends/base/schema.py
@@ -923,13 +923,11 @@ class BaseDatabaseSchemaEditor:
     def _field_db_check(self, field, field_db_params):
         # Always check constraints with the same mocked column name to avoid
         # recreating constrains when the column is renamed.
-        check_constraints = self.connection.data_type_check_constraints
+        if (constraint := field.db_check(self.connection)) is None:
+            return None
         data = field.db_type_parameters(self.connection)
         data["column"] = "__column_name__"
-        try:
-            return check_constraints[field.get_internal_type()] % data
-        except KeyError:
-            return None
+        return constraint % data
 
     def _alter_field(
         self,
