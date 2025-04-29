4bd35ef4f90f34da58be4d29f15437db83755944
Asif Saif Uddin <auvipy@gmail.com>
2025-04-29 15:37:06 +0600

    removed relevant tests for make_naive() functions checks

--- tests/utils_tests/test_timezone.py
+++ tests/utils_tests/test_timezone.py
@@ -149,10 +149,6 @@ class TimezoneTests(SimpleTestCase):
             datetime.datetime(2011, 9, 1, 13, 20, 30),
         )
 
-        with self.assertRaisesMessage(
-            ValueError, "make_naive() cannot be applied to a naive datetime"
-        ):
-            timezone.make_naive(datetime.datetime(2011, 9, 1, 13, 20, 30), EAT)
 
     def test_make_naive_no_tz(self):
         self.assertEqual(
