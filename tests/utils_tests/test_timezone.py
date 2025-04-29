ff830693e9a85f9b60099f8a713d4eb190c6f626
Asif Saif Uddin <auvipy@gmail.com>
2025-04-29 15:41:52 +0600

    removed too many blank lines

--- tests/utils_tests/test_timezone.py
+++ tests/utils_tests/test_timezone.py
@@ -149,7 +149,6 @@ class TimezoneTests(SimpleTestCase):
             datetime.datetime(2011, 9, 1, 13, 20, 30),
         )
 
-
     def test_make_naive_no_tz(self):
         self.assertEqual(
             timezone.make_naive(datetime.datetime(2011, 9, 1, 13, 20, 30, tzinfo=EAT)),
