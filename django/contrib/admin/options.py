aa2137676d438e773f2c1a290a4ba2acb7e0f057
Marcelo Galigniana <marcelogaligniana@gmail.com>
2025-05-05 13:00:41 -0300

    Minor docstring update

--- django/contrib/admin/options.py
+++ django/contrib/admin/options.py
@@ -429,7 +429,7 @@ class BaseModelAdmin(metaclass=forms.MediaDefiningClass):
     def get_queryset(self, request):
         """
         Return a QuerySet of all model instances that can be edited by the
-        admin site. This is used by changelist_view.
+        admin site. This is used by changelist_view and changeform_view.
         """
         qs = self.model._default_manager.get_queryset()
         # TODO: this should be handled by some parameter to the ChangeList.
