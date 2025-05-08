0176fa72d134d41e7f8d74f35c33a429a951bed8
Anthony Sottile <asottile@umich.edu>
2025-05-08 16:37:11 -0400

    added hints support to postgres CreateExtension operations #36377.

--- django/contrib/postgres/operations.py
+++ django/contrib/postgres/operations.py
@@ -13,15 +13,16 @@ class CreateExtension(Operation):
     reversible = True
     category = OperationCategory.ADDITION
 
-    def __init__(self, name):
+    def __init__(self, name, hints=None):
         self.name = name
+        self.hints = hints or {}
 
     def state_forwards(self, app_label, state):
         pass
 
     def database_forwards(self, app_label, schema_editor, from_state, to_state):
         if schema_editor.connection.vendor != "postgresql" or not router.allow_migrate(
-            schema_editor.connection.alias, app_label
+            schema_editor.connection.alias, app_label, **self.hints
         ):
             return
         if not self.extension_exists(schema_editor, self.name):
@@ -42,7 +43,9 @@ class CreateExtension(Operation):
             )
 
     def database_backwards(self, app_label, schema_editor, from_state, to_state):
-        if not router.allow_migrate(schema_editor.connection.alias, app_label):
+        if not router.allow_migrate(
+            schema_editor.connection.alias, app_label, **self.hints
+        ):
             return
         if self.extension_exists(schema_editor, self.name):
             schema_editor.execute(
@@ -69,43 +72,43 @@ class CreateExtension(Operation):
 
 
 class BloomExtension(CreateExtension):
-    def __init__(self):
-        self.name = "bloom"
+    def __init__(self, **kwargs):
+        super().__init__("bloom", **kwargs)
 
 
 class BtreeGinExtension(CreateExtension):
-    def __init__(self):
-        self.name = "btree_gin"
+    def __init__(self, **kwargs):
+        super().__init__("btree_gin", **kwargs)
 
 
 class BtreeGistExtension(CreateExtension):
-    def __init__(self):
-        self.name = "btree_gist"
+    def __init__(self, **kwargs):
+        super().__init__("btree_gist", **kwargs)
 
 
 class CITextExtension(CreateExtension):
-    def __init__(self):
-        self.name = "citext"
+    def __init__(self, **kwargs):
+        super().__init__("citext", **kwargs)
 
 
 class CryptoExtension(CreateExtension):
-    def __init__(self):
-        self.name = "pgcrypto"
+    def __init__(self, **kwargs):
+        super().__init__("pgcrypto", **kwargs)
 
 
 class HStoreExtension(CreateExtension):
-    def __init__(self):
-        self.name = "hstore"
+    def __init__(self, **kwargs):
+        super().__init__("hstore", **kwargs)
 
 
 class TrigramExtension(CreateExtension):
-    def __init__(self):
-        self.name = "pg_trgm"
+    def __init__(self, **kwargs):
+        super().__init__("pg_trgm", **kwargs)
 
 
 class UnaccentExtension(CreateExtension):
-    def __init__(self):
-        self.name = "unaccent"
+    def __init__(self, **kwargs):
+        super().__init__("unaccent", **kwargs)
 
 
 class NotInTransactionMixin:
