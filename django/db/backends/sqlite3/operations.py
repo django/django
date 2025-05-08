358fd21c47cdf7bda520ce73c5cfd82bba57827b
Sage Abdullah <me@laymonage.com>
2025-05-08 09:10:44 +0200

    Fixed #36143 -- Made max_query_params respect SQLITE_LIMIT_VARIABLE_NUMBER.
    Co-authored-by: Xavier Frankline <xf.xavierfrank@gmail.com>

--- django/db/backends/sqlite3/operations.py
+++ django/db/backends/sqlite3/operations.py
@@ -30,8 +30,8 @@ class DatabaseOperations(BaseDatabaseOperations):
 
     def bulk_batch_size(self, fields, objs):
         """
-        SQLite has a compile-time default (SQLITE_LIMIT_VARIABLE_NUMBER) of
-        999 variables per query.
+        SQLite has a variable limit defined by SQLITE_LIMIT_VARIABLE_NUMBER
+        (reflected in max_query_params).
 
         If there's only a single field to insert, the limit is 500
         (SQLITE_MAX_COMPOUND_SELECT).
