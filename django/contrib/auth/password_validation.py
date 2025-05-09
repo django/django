6aed0b38642a474f1a2a51ddf069fbd95e75c0dd
Marius Burfey <6870808+mariusburfey@users.noreply.github.com>
2025-05-09 09:58:22 +0200

    #36378: Repaired translations for error message in MinimumLengthValidator.

--- django/contrib/auth/password_validation.py
+++ django/contrib/auth/password_validation.py
@@ -113,14 +113,12 @@ class MinimumLengthValidator:
             )
 
     def get_error_message(self):
-        return (
-            ngettext(
-                "This password is too short. It must contain at least %d character.",
-                "This password is too short. It must contain at least %d characters.",
-                self.min_length,
-            )
-            % self.min_length
-        )
+        return ngettext(
+            "This password is too short. It must contain at least %(min_length)d character.",
+            "This password is too short. It must contain at least %(min_length)d characters.",
+            self.min_length,
+        ) % {"min_length": self.min_length}
+
 
     def get_help_text(self):
         return ngettext(
--- tests/auth_tests/test_validators.py
+++ tests/auth_tests/test_validators.py
@@ -155,8 +155,8 @@ class MinimumLengthValidatorTest(SimpleTestCase):
         with self.subTest("get_error_message"):
             MinimumLengthValidator().get_error_message()
             mock_ngettext.assert_called_with(
-                "This password is too short. It must contain at least %d character.",
-                "This password is too short. It must contain at least %d characters.",
+                "This password is too short. It must contain at least %(min_length)d character.",
+                "This password is too short. It must contain at least %(min_length)d characters.",
                 8,
             )
         mock_ngettext.reset()
