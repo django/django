e2528e14eb33403b6b4567db66f24c65a3ef9b36
Adam Johnson <me@adamj.eu>
2025-05-11 23:07:47 +0200

    Refs #36383 -- Extended DeconstructibleSerializer to support non-identifier keyword arguments.
    In Python, keyword arguments must normally be valid identifiers (i.e.,
    variable names that follow Python's naming rules). However, Python dicts
    can have keys that aren't valid identifiers, like "foo-bar" or "123foo".
    
    This commit ensures that keyword arguments that are nt valid
    identifiers, are properly handled when deconstructing an object.

--- tests/migrations/test_writer.py
+++ tests/migrations/test_writer.py
@@ -41,6 +41,13 @@ class DeconstructibleInstances:
         return ("DeconstructibleInstances", [], {})
 
 
+@deconstructible
+class DeconstructibleArbitrary:
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+
 class Money(decimal.Decimal):
     def deconstruct(self):
         return (
@@ -1142,6 +1149,24 @@ class WriterTests(SimpleTestCase):
             string,
             "models.CharField(default=migrations.test_writer.DeconstructibleInstances)",
         )
+
+    def test_serialize_non_identifier_keyword_args(self):
+        instance = DeconstructibleArbitrary(
+            **{"kebab-case": 1, "my_list": [1, 2, 3], "123foo": {"456bar": set()}},
+            regular="kebab-case",
+            **{"simple": 1, "complex": 3.1416},
+        )
+        string, imports = MigrationWriter.serialize(instance)
+        self.assertEqual(
+            string,
+            "migrations.test_writer.DeconstructibleArbitrary(complex=3.1416, "
+            "my_list=[1, 2, 3], regular='kebab-case', simple=1, "
+            "**{'123foo': {'456bar': set()}, 'kebab-case': 1})",
+        )
+        self.assertEqual(imports, {"import migrations.test_writer"})
+        result = self.serialize_round_trip(instance)
+        self.assertEqual(result.args, instance.args)
+        self.assertEqual(result.kwargs, instance.kwargs)
 
     def test_register_serializer(self):
         class ComplexSerializer(BaseSerializer):
