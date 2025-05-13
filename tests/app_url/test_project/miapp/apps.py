8e2bf36b97cb00fa4b387bddca0ea0bccca6db4d
Camilo Benitez <camilo.benitez@domotes.co>
2025-05-13 14:54:59 -0500

    Add urls.py to app_template to be created by startapp

---
+++ tests/app_url/test_project/miapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MiappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'miapp'
---
+++ tests/app_url/test_project/miapp/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+
+urlpatterns = [
+    # path('', views.home, name='home'),
+]
