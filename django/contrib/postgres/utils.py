7594e6b3458592023effcfaa8d88f7a5c9dd56fb
Clifford Gama <cliffygamy@gmail.com>
2025-05-16 23:48:38 +0200

    Fixed #32770 -- Added system check to ensure django.contrib.postgres is installed when using its features.
    Added postgres.E005 to validate 'django.contrib.postgres' is in INSTALLED_APPS
    when using:
    * PostgreSQL-specific fields (ArrayField, HStoreField, range fields, SearchVectorField),
    * PostgreSQL indexes (PostgresIndex and all subclasses), and
    * ExclusionConstraint
    
    The check provides immediate feedback during system checks rather than failing
    later with obscure runtime and database errors.
    
    Thanks to Simon Charette and Sarah Boyce for reviews.

--- django/contrib/postgres/utils.py
+++ django/contrib/postgres/utils.py
@@ -1,3 +1,5 @@
+from django.apps import apps
+from django.core import checks
 from django.core.exceptions import ValidationError
 from django.utils.functional import SimpleLazyObject
 from django.utils.text import format_lazy
@@ -27,3 +29,25 @@ def prefix_validation_error(error, prefix, code, params):
     return ValidationError(
         [prefix_validation_error(e, prefix, code, params) for e in error.error_list]
     )
+
+
+class CheckPostgresInstalledMixin:
+    def _check_postgres_installed(self, *args):
+        # When subclassed by Index or BaseConstraint subclasses, args is
+        # (model, connection).
+        obj = args[0] if args else self
+        if not apps.is_installed("django.contrib.postgres"):
+            return [
+                checks.Error(
+                    "'django.contrib.postgres' must be in INSTALLED_APPS in "
+                    "order to use %s." % self.__class__.__name__,
+                    obj=obj,
+                    id="postgres.E005",
+                )
+            ]
+        return []
+
+    def check(self, *args, **kwargs):
+        errors = super().check(*args, **kwargs)
+        errors.extend(self._check_postgres_installed(*args))
+        return errors
