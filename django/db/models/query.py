c2d019b1771b5d431632d4ed9278a1f1d8819669
Jacob Walls <jacobtylerwalls@gmail.com>
2025-05-24 14:03:07 -0400

    Fixed #36416 -- Made QuerySet.in_bulk() account for composite pks in id_list.

--- django/db/models/query.py
+++ django/db/models/query.py
@@ -1180,7 +1180,9 @@ class QuerySet(AltersData):
             if not id_list:
                 return {}
             filter_key = "{}__in".format(field_name)
-            batch_size = connections[self.db].features.max_query_params
+            max_params = connections[self.db].features.max_query_params or 0
+            num_fields = len(opts.pk_fields) if field_name == "pk" else 1
+            batch_size = max_params // num_fields
             id_list = tuple(id_list)
             # If the database has a limit on the number of query parameters
             # (e.g. SQLite), retrieve objects in batches if necessary.
