c1fa3fdd040718356e5a3b9a0fe699d73f47a940
Clifford Gama <cliffygamy@gmail.com>
2025-05-26 16:44:08 +0200

    Fixed #36419 -- Ensured for_save was propagated when resolving expressions.
    The for_save flag wasn't properly propagated when resolving expressions, which
    prevented get_db_prep_save() from being called in some cases. This affected
    fields like JSONField where None would be saved as JSON null instead of SQL NULL.
    
    Regression in 00c690efbc0b10f67924687f24a7b30397bf47d9.
    
    Thanks to David Sanders and Simon Charette for reviews.
    
    Co-authored-by: Adam Johnson <me@adamj.eu>

--- tests/queries/test_bulk_update.py
+++ tests/queries/test_bulk_update.py
@@ -4,7 +4,7 @@ from math import ceil
 from django.core.exceptions import FieldDoesNotExist
 from django.db import connection
 from django.db.models import F
-from django.db.models.functions import Lower
+from django.db.models.functions import Coalesce, Lower
 from django.db.utils import IntegrityError
 from django.test import TestCase, override_settings, skipUnlessDBFeature
 
@@ -299,6 +299,21 @@ class BulkUpdateTests(TestCase):
         self.assertCountEqual(
             JSONFieldNullable.objects.filter(json_field__has_key="c"), objs
         )
+
+    @skipUnlessDBFeature("supports_json_field")
+    def test_json_field_sql_null(self):
+        obj = JSONFieldNullable.objects.create(json_field={})
+        test_cases = [
+            ("direct_none_assignment", None),
+            ("expression_none_assignment", Coalesce(None, None)),
+        ]
+        for label, value in test_cases:
+            with self.subTest(case=label):
+                obj.json_field = value
+                JSONFieldNullable.objects.bulk_update([obj], fields=["json_field"])
+                obj.refresh_from_db()
+                sql_null_qs = JSONFieldNullable.objects.filter(json_field__isnull=True)
+                self.assertSequenceEqual(sql_null_qs, [obj])
 
     def test_nullable_fk_after_related_save(self):
         parent = RelatedObject.objects.create()
