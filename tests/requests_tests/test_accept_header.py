3faee80f74d6fcb2ddb32609228c0e12868e47bd
Jake Howard <git@theorangeone.net>
2025-05-30 22:56:01 +0100

    Add test for quality from RFC
    Currently, it fails

--- tests/requests_tests/test_accept_header.py
+++ tests/requests_tests/test_accept_header.py
@@ -226,3 +226,26 @@ class AcceptHeaderTests(TestCase):
             ),
             "text/vcard",
         )
+
+    def test_quality(self):
+        """
+        Example as per https://datatracker.ietf.org/doc/html/rfc7231#section-5.3.2
+        """
+
+        request = HttpRequest()
+        request.META["HTTP_ACCEPT"] = (
+            "text/*;q=0.3, text/html;q=0.7, text/html;level=1, text/html;level=2;q=0.4, */*;q=0.5"
+        )
+
+        for media_type, quality in [
+            ("text/html;level=1", 1),
+            ("text/html", 0.7),
+            ("text/plain", 0.3),
+            ("image/jpeg", 0.5),
+            ("text/html;level=2", 0.4),
+            ("text/html;level=3", 0.7),
+        ]:
+            with self.subTest(media_type):
+                accepted_media_type = request.accepted_type(media_type)
+                self.assertIsNotNone(accepted_media_type)
+                self.assertEqual(accepted_media_type.quality, quality)
