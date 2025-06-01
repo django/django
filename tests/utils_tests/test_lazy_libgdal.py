15dc1f9e09fa0daf8a331ccca8396a57e0f9a2ed
Josh <josh@joshthomas.dev>
2025-06-01 18:25:48 -0500

    simplify laziness tests

--- tests/utils_tests/test_lazy_libgdal.py
+++ tests/utils_tests/test_lazy_libgdal.py
@@ -1,8 +1,7 @@
 import os
 import sys
-from unittest import mock, skipIf
+from unittest import skipIf
 
-from django.core.exceptions import ImproperlyConfigured
 from django.test import SimpleTestCase
 from django.utils.functional import SimpleLazyObject, empty
 
@@ -13,30 +12,17 @@ class GDALLazyLoadingTest(SimpleTestCase):
     is installed - verifying that:
 
     - The GDAL library is not loaded upon importing `django.contrib.gis.gdal.libgdal`.
-    - The library is loaded only once upon the first access to an attribute
-      of the lazy object.
-    - Subsequent accesses do not reload the library.
-    - The lazy objects use the `empty` sentinel from `django.utils.functional`
-      to indicate whether the loading function has been called.
-    - Appropriate exceptions are raised when the GDAL library cannot be found.
+    - The library loading is attempted when the lazy object is accessed.
 
-    The tests avoid dependency on the actual GDAL library by handling
-    `AttributeError` from accessing non-existent attributes and by mocking
-    where necessary.
+    The tests work regardless of whether GDAL is installed by tracking
+    whether the load function is called, not whether it succeeds.
     """
 
     def setUp(self):
-        # Clean up modules before each test
-        self.modules_to_clean = ["django.contrib.gis.gdal.libgdal"]
-        for module in self.modules_to_clean:
-            if module in sys.modules:
-                del sys.modules[module]
+        libgdal_mod = "django.contrib.gis.gdal.libgdal"
 
-    def tearDown(self):
-        # Clean up after test
-        for module in self.modules_to_clean:
-            if module in sys.modules:
-                del sys.modules[module]
+        if libgdal_mod in sys.modules:
+            del sys.modules[libgdal_mod]
 
     def test_lgdal_not_loaded_on_import(self):
         from django.contrib.gis.gdal import libgdal
@@ -58,16 +44,26 @@ class GDALLazyLoadingTest(SimpleTestCase):
 
         self.assertIs(libgdal.lgdal._wrapped, empty)
 
-        try:
-            # Try to access a non-existent attribute
-            # This should trigger the lazy loading
-            with self.assertRaises(AttributeError):
-                libgdal.lgdal.some_attribute
+        load_called = False
+        lgdal_setupfunc = libgdal.lgdal.__dict__["_setupfunc"]
 
-            self.assertIsNot(libgdal.lgdal._wrapped, empty)
-        except ImproperlyConfigured:
-            # GDAL is not installed, but our wrapper worked
-            self.skipTest("GDAL is not installed")
+        def track_load():
+            nonlocal load_called
+            load_called = True
+            return lgdal_setupfunc()
+
+        # Modify __dict__ directly to avoid triggering lazy loading
+        libgdal.lgdal.__dict__["_setupfunc"] = track_load
+
+        try:
+            libgdal.lgdal["GDALOpen"]
+        except Exception:
+            # Don't care if it fails, just that it tried
+            pass
+        finally:
+            libgdal.lgdal.__dict__["_setupfunc"] = lgdal_setupfunc
+
+        self.assertTrue(load_called)
 
     @skipIf(os.name != "nt", "lwingdal is Windows-specific")
     def test_lwingdal_loaded_on_first_access(self):
@@ -75,78 +71,24 @@ class GDALLazyLoadingTest(SimpleTestCase):
 
         self.assertIs(libgdal.lwingdal._wrapped, empty)
 
-        try:
-            # Try to access a non-existent attribute
-            # This should trigger the lazy loading
-            with self.assertRaises(AttributeError):
-                libgdal.lwingdal.some_attribute
+        load_called = False
+        lwingdal_setupfunc = libgdal.lwingdal.__dict__["_setupfunc"]
 
-            self.assertIsNot(libgdal.lwingdal._wrapped, empty)
-        except ImproperlyConfigured:
-            # GDAL is not installed, but our wrapper worked
-            self.skipTest("GDAL is not installed")
+        def track_load():
+            nonlocal load_called
+            load_called = True
+            return lwingdal_setupfunc()
 
-    def test_lgdal_load_is_cached(self):
-        from django.contrib.gis.gdal import libgdal
-
-        self.assertIs(libgdal.lgdal._wrapped, empty)
+        # Modify __dict__ directly to avoid triggering lazy loading
+        libgdal.lwingdal.__dict__["_setupfunc"] = track_load
 
         try:
-            # First access
-            with self.assertRaises(AttributeError):
-                libgdal.lgdal.some_attribute
+            # Access lwingdal to trigger loading
+            libgdal.lwingdal["OSRNewSpatialReference"]
+        except Exception:
+            # Don't care if it fails, just that it tried
+            pass
+        finally:
+            libgdal.lwingdal.__dict__["_setupfunc"] = lwingdal_setupfunc
 
-            first_loaded_object = libgdal.lgdal._wrapped
-            self.assertIsNot(first_loaded_object, empty)
-
-            # Second access
-            with self.assertRaises(AttributeError):
-                libgdal.lgdal.another_attribute
-
-            second_loaded_object = libgdal.lgdal._wrapped
-            self.assertIsNot(second_loaded_object, empty)
-            self.assertIs(second_loaded_object, first_loaded_object)
-        except ImproperlyConfigured:
-            # GDAL is not installed, but our wrapper worked
-            self.skipTest("GDAL is not installed")
-
-    @skipIf(os.name != "nt", "lwingdal is Windows-specific")
-    def test_lwingdal_load_is_cached(self):
-        from django.contrib.gis.gdal import libgdal
-
-        self.assertIs(libgdal.lwingdal._wrapped, empty)
-
-        try:
-            # First access
-            with self.assertRaises(AttributeError):
-                libgdal.lwingdal.some_attribute
-
-            first_loaded_object = libgdal.lwingdal._wrapped
-            self.assertIsNot(first_loaded_object, empty)
-
-            # Second access
-            with self.assertRaises(AttributeError):
-                libgdal.lwingdal.another_attribute
-
-            second_loaded_object = libgdal.lwingdal._wrapped
-            self.assertIsNot(second_loaded_object, empty)
-            self.assertIs(second_loaded_object, first_loaded_object)
-        except ImproperlyConfigured:
-            # GDAL is not installed, but our wrapper worked
-            self.skipTest("GDAL is not installed")
-
-    @mock.patch("ctypes.util.find_library")
-    def test_load_gdal_failure(self, mock_find_library):
-        mock_find_library.return_value = None
-
-        # Need to ensure the module is not already loaded
-        if "django.contrib.gis.gdal.libgdal" in sys.modules:
-            del sys.modules["django.contrib.gis.gdal.libgdal"]
-
-        from django.contrib.gis.gdal import libgdal
-
-        with self.assertRaisesMessage(
-            ImproperlyConfigured, "Could not find the GDAL library"
-        ):
-            # Trigger the lazy loading
-            libgdal.lgdal.some_attribute
+        self.assertTrue(load_called)
