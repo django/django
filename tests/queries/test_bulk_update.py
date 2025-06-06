django/django

commit 00c690efbc0b10f67924687f24a7b30397bf47d9

Fixed #36117 -- Raised ValueError when providing composite expressions to case / when.
Remove redundant Case and When.resolve_expression to delegate composite
expression support to BaseExpression.

Thanks Jacob Tyler Walls for the report and test.

django/db/models/expressions.py
@@ -1577,20 +1577,6 @@ class When(Expression):
         # We're only interested in the fields of the result expressions.
         return [self.result._output_field_or_none]
 
-    def resolve_expression(
-        self, query=None, allow_joins=True, reuse=None, summarize=False, for_save=False
-    ):
-        c = self.copy()
-        c.is_summary = summarize
-        if hasattr(c.condition, "resolve_expression"):
-            c.condition = c.condition.resolve_expression(
-                query, allow_joins, reuse, summarize, False
-            )
-        c.result = c.result.resolve_expression(
-            query, allow_joins, reuse, summarize, for_save
-        )
-        return c
-
     def as_sql(self, compiler, connection, template=None, **extra_context):
         connection.ops.check_expression_support(self)
         template_params = extra_context
@@ -1657,20 +1643,6 @@ class Case(SQLiteNumericMixin, Expression):
 
     def set_source_expressions(self, exprs):
         *self.cases, self.default = exprs
-
-    def resolve_expression(
-        self, query=None, allow_joins=True, reuse=None, summarize=False, for_save=False
-    ):
-        c = self.copy()
-        c.is_summary = summarize
-        for pos, case in enumerate(c.cases):
-            c.cases[pos] = case.resolve_expression(
-                query, allow_joins, reuse, summarize, for_save
-            )
-        c.default = c.default.resolve_expression(
-            query, allow_joins, reuse, summarize, for_save
-        )
-        return c
 
     def copy(self):
         c = super().copy()

tests/composite_pk/test_filter.py
@@ -1,4 +1,13 @@
-from django.db.models import F, FilteredRelation, OuterRef, Q, Subquery, TextField
+from django.db.models import (
+    Case,
+    F,
+    FilteredRelation,
+    OuterRef,
+    Q,
+    Subquery,
+    TextField,
+    When,
+)
 from django.db.models.functions import Cast
 from django.db.models.lookups import Exact
 from django.test import TestCase
@@ -408,6 +417,14 @@ class CompositePKFilterTests(TestCase):
         msg = "Cast expression does not support composite primary keys."
         with self.assertRaisesMessage(ValueError, msg):
             Comment.objects.filter(text__gt=Cast(F("pk"), TextField())).count()
+
+    def test_filter_case_when(self):
+        msg = "When expression does not support composite primary keys."
+        with self.assertRaisesMessage(ValueError, msg):
+            Comment.objects.filter(text=Case(When(text="", then="pk")))
+        msg = "Case expression does not support composite primary keys."
+        with self.assertRaisesMessage(ValueError, msg):
+            Comment.objects.filter(text=Case(When(text="", then="text"), default="pk"))
 
     def test_outer_ref_pk(self):
         subquery = Subquery(Comment.objects.filter(pk=OuterRef("pk")).values("id"))

commit 47e5bb3f818dda4fd7dc84e35e0ca64db977089d

Fixed #36419 -- Ensured for_save was propagated when resolving expressions.
The for_save flag wasn't properly propagated when resolving expressions, which
prevented get_db_prep_save() from being called in some cases. This affected
fields like JSONField where None would be saved as JSON null instead of SQL NULL.

Regression in 00c690efbc0b10f67924687f24a7b30397bf47d9.

Thanks to David Sanders and Simon Charette for reviews.

Co-authored-by: Adam Johnson <me@adamj.eu>

django/db/models/expressions.py
@@ -297,7 +297,7 @@ class BaseExpression:
         c.is_summary = summarize
         source_expressions = [
             (
-                expr.resolve_expression(query, allow_joins, reuse, summarize)
+                expr.resolve_expression(query, allow_joins, reuse, summarize, for_save)
                 if expr is not None
                 else None
             )

tests/queries/test_bulk_update.py
@@ -4,7 +4,7 @@ from math import ceil
 from django.core.exceptions import FieldDoesNotExist
 from django.db import connection
 from django.db.models import F
-from django.db.models.functions import Lower
+from django.db.models.functions import Coalesce, Lower
 from django.db.utils import IntegrityError
 from django.test import TestCase, override_settings, skipUnlessDBFeature
 
@@ -299,6 +299,21 @@ class BulkUpdateTests(TestCase):
         self.assertCountEqual(
             JSONFieldNullable.objects.filter(json_field__has_key="c"), objs
         )
+
+    @skipUnlessDBFeature("supports_json_field")
+    def test_json_field_sql_null(self):
+        obj = JSONFieldNullable.objects.create(json_field={})
+        test_cases = [
+            ("direct_none_assignment", None),
+            ("expression_none_assignment", Coalesce(None, None)),
+        ]
+        for label, value in test_cases:
+            with self.subTest(case=label):
+                obj.json_field = value
+                JSONFieldNullable.objects.bulk_update([obj], fields=["json_field"])
+                obj.refresh_from_db()
+                sql_null_qs = JSONFieldNullable.objects.filter(json_field__isnull=True)
+                self.assertSequenceEqual(sql_null_qs, [obj])
 
     def test_nullable_fk_after_related_save(self):
         parent = RelatedObject.objects.create()