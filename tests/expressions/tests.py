django/django

commit c1fa3fdd040718356e5a3b9a0fe699d73f47a940

Fixed #36419 -- Ensured for_save was propagated when resolving expressions.
The for_save flag wasn't properly propagated when resolving expressions, which
prevented get_db_prep_save() from being called in some cases. This affected
fields like JSONField where None would be saved as JSON null instead of SQL NULL.

Regression in 00c690efbc0b10f67924687f24a7b30397bf47d9.

Thanks to David Sanders and Simon Charette for reviews.

Co-authored-by: Adam Johnson <me@adamj.eu>

django/db/models/expressions.py
@@ -297,7 +297,7 @@ class BaseExpression:
         c.is_summary = summarize
         source_expressions = [
             (
-                expr.resolve_expression(query, allow_joins, reuse, summarize)
+                expr.resolve_expression(query, allow_joins, reuse, summarize, for_save)
                 if expr is not None
                 else None
             )

tests/queries/test_bulk_update.py
@@ -4,7 +4,7 @@ from math import ceil
 from django.core.exceptions import FieldDoesNotExist
 from django.db import connection
 from django.db.models import F
-from django.db.models.functions import Lower
+from django.db.models.functions import Coalesce, Lower
 from django.db.utils import IntegrityError
 from django.test import TestCase, override_settings, skipUnlessDBFeature
 
@@ -299,6 +299,21 @@ class BulkUpdateTests(TestCase):
         self.assertCountEqual(
             JSONFieldNullable.objects.filter(json_field__has_key="c"), objs
         )
+
+    @skipUnlessDBFeature("supports_json_field")
+    def test_json_field_sql_null(self):
+        obj = JSONFieldNullable.objects.create(json_field={})
+        test_cases = [
+            ("direct_none_assignment", None),
+            ("expression_none_assignment", Coalesce(None, None)),
+        ]
+        for label, value in test_cases:
+            with self.subTest(case=label):
+                obj.json_field = value
+                JSONFieldNullable.objects.bulk_update([obj], fields=["json_field"])
+                obj.refresh_from_db()
+                sql_null_qs = JSONFieldNullable.objects.filter(json_field__isnull=True)
+                self.assertSequenceEqual(sql_null_qs, [obj])
 
     def test_nullable_fk_after_related_save(self):
         parent = RelatedObject.objects.create()

commit 3594c96bd5c17f3ddd65a7673e9cb3250b703950

Fixed #36453 -- Made When.condition resolve with for_save=False.
This fixes a regression in c1fa3fdd040718356e5a3b9a0fe699d73f47a940 which made
Value(None, JSONField()) when used in When.condition incorrectly resolve with
for_save=True. As a result, the value was serialized as SQL NULL instead of
JSON null.

Thanks to Thomas McKay for the report, and to Sarah, David, and Simon for reviews.

Co-authored-by: Sarah Boyce <42296566+sarahboyce@users.noreply.github.com>

django/db/models/expressions.py
@@ -1620,6 +1620,17 @@ class When(Expression):
     def set_source_expressions(self, exprs):
         self.condition, self.result = exprs
 
+    def resolve_expression(
+        self, query=None, allow_joins=True, reuse=None, summarize=False, for_save=False
+    ):
+        c = super().resolve_expression(query, allow_joins, reuse, summarize, for_save)
+        if for_save and c.condition is not None:
+            # Resolve condition with for_save=False, since it's used as a filter.
+            c.condition = self.condition.resolve_expression(
+                query, allow_joins, reuse, summarize, for_save=False
+            )
+        return c
+
     def get_source_fields(self):
         # We're only interested in the fields of the result expressions.
         return [self.result._output_field_or_none]

tests/expressions/tests.py
@@ -29,6 +29,7 @@ from django.db.models import (
     FloatField,
     Func,
     IntegerField,
+    JSONField,
     Max,
     Min,
     Model,
@@ -83,6 +84,7 @@ from .models import (
     Company,
     Employee,
     Experiment,
+    JSONFieldModel,
     Manager,
     Number,
     RemoteEmployee,
@@ -366,6 +368,21 @@ class BasicExpressionsTests(TestCase):
         self.assertQuerySetEqual(
             Number.objects.all(), [None, None], lambda n: n.float, ordered=False
         )
+
+    @skipUnlessDBFeature("supports_json_field")
+    def test_update_jsonfield_case_when_key_is_null(self):
+        obj = JSONFieldModel.objects.create(data={"key": None})
+        updated = JSONFieldModel.objects.update(
+            data=Case(
+                When(
+                    data__key=Value(None, JSONField()),
+                    then=Value({"key": "something"}, JSONField()),
+                ),
+            )
+        )
+        self.assertEqual(updated, 1)
+        obj.refresh_from_db()
+        self.assertEqual(obj.data, {"key": "something"})
 
     def test_filter_with_join(self):
         # F Expressions can also span joins