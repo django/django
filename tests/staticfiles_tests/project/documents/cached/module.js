b43d6db89ffbf1c8a49a02c88698f173db01a97c
James Bligh <blighj@users.noreply.github.com>
2025-06-13 06:22:04 +0100

    Fixed #34322 -- Used JsLex for ES module support in ManifestStaticFilesStorage.

--- tests/staticfiles_tests/project/documents/cached/module.js
+++ tests/staticfiles_tests/project/documents/cached/module.js
@@ -1,3 +1,4 @@
+export const moduleConst = "module";
 // Static imports.
 import rootConst from "/static/absolute_root.js";
 import testConst from "./module_test.js";
@@ -17,6 +18,15 @@ import relativeModule from "../nested/js/nested.js";
 // Dynamic imports.
 const dynamicModule = import("./module_test.js");
 
+// import with assert
+import k from"./other.css"assert{type:"css"};
+
+import*as l from "/static/absolute_root.js";
+import*as h from "/static/absolute_root.js";
+import*as m from "/static/absolute_root.js";
+import {BaseComponent as g} from "/static/absolute_root.js";
+
+
 // Modules exports to aggregate modules.
 export * from "./module_test.js";
 export { testConst } from "./module_test.js";
@@ -24,3 +34,16 @@ export {
     firstVar as firstVarAlias,
     secondVar as secondVarAlias
 } from "./module_test.js";
+
+
+// These should not be processed
+// @returns {import("./non-existent-1").something}
+/* @returns {import("./non-existent-2").something} */
+'import("./non-existent-3")'
+"import('./non-existent-4')"
+`import("./non-existent-5")`
+r = /import/;
+/**
+ * @param {HTMLElement} elt
+ * @returns {import("./htmx").HtmxTriggerSpecification[]}
+ */
