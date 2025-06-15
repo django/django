92a0e2ca6944f3d78a2ee78637cd2841f24740d1
Matthias Kestenholz <mk@feinheit.ch>
2025-06-15 16:38:10 +0200

    Refs #25706 -- Avoided adding an unneeded and potentially non-unique ID.
    This amends the code from https://github.com/django/django/pull/18494 to
    avoid setting an URL for the script containing the widget
    configuration in JSON format. The ID generated by json_script would be
    non-unique when using the widget several times on the same page.

--- django/contrib/gis/static/gis/js/OLMapWidget.js
+++ django/contrib/gis/static/gis/js/OLMapWidget.js
@@ -266,7 +266,7 @@ function initMapWidgetInSection(section) {
         if (wrapper.id.includes('__prefix__')) {
             return;
         }
-        const options = JSON.parse(wrapper.querySelector("#mapwidget-options").textContent);
+        const options = JSON.parse(wrapper.querySelector("script").textContent);
         options.id = wrapper.querySelector("textarea").id;
         options.map_id = wrapper.querySelector(".dj_map").id;
         maps.push(new MapWidget(options));
