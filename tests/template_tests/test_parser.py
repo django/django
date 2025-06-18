django/django

commit 5183f7c287a9a5d61ca1103b55166cda52d9c647

Fixed #35816 -- Handled parsing of scientific notation in DTL. (#19213)
* Refs #35816 -- Improved test coverage of FilterExpression.

* Fixed #35816 -- Made FilterExpression parse scientific numbers.

---------

Co-authored-by: Sarah Boyce <42296566+sarahboyce@users.noreply.github.com>

django/template/base.py
@@ -633,19 +633,18 @@ constant_string = constant_string.replace("\n", "")
 
 filter_raw_string = r"""
 ^(?P<constant>%(constant)s)|
-^(?P<var>[%(var_chars)s]+|%(num)s)|
+^(?P<var>[%(var_chars)s]+)|
  (?:\s*%(filter_sep)s\s*
      (?P<filter_name>\w+)
          (?:%(arg_sep)s
              (?:
               (?P<constant_arg>%(constant)s)|
-              (?P<var_arg>[%(var_chars)s]+|%(num)s)
+              (?P<var_arg>[%(var_chars)s]+)
              )
          )?
  )""" % {
     "constant": constant_string,
-    "num": r"[-+.]?\d[\d.e]*",
-    "var_chars": r"\w\.",
+    "var_chars": r"\w\.\+-",
     "filter_sep": re.escape(FILTER_SEPARATOR),
     "arg_sep": re.escape(FILTER_ARGUMENT_SEPARATOR),
 }

tests/template_tests/test_parser.py
@@ -11,6 +11,7 @@ from django.template.base import (
     Token,
     TokenType,
     Variable,
+    VariableDoesNotExist,
 )
 from django.template.defaultfilters import register as filter_library
 from django.test import SimpleTestCase
@@ -71,6 +72,27 @@ class ParserTests(SimpleTestCase):
         )
         with self.assertRaisesMessage(TemplateSyntaxError, msg):
             FilterExpression("article._hidden|upper", p)
+
+    def test_cannot_parse_characters(self):
+        p = Parser("", builtins=[filter_library])
+        for filter_expression, characters in [
+            ('<>|default:"Default"|upper', '|<>||default:"Default"|upper'),
+            ("test|<>|upper", "test||<>||upper"),
+        ]:
+            with self.subTest(filter_expression=filter_expression):
+                with self.assertRaisesMessage(
+                    TemplateSyntaxError,
+                    f"Could not parse some characters: {characters}",
+                ):
+                    FilterExpression(filter_expression, p)
+
+    def test_cannot_find_variable(self):
+        p = Parser("", builtins=[filter_library])
+        with self.assertRaisesMessage(
+            TemplateSyntaxError,
+            'Could not find variable at start of |default:"Default"',
+        ):
+            FilterExpression('|default:"Default"', p)
 
     def test_variable_parsing(self):
         c = {"article": {"section": "News"}}
@@ -148,3 +170,49 @@ class ParserTests(SimpleTestCase):
             '1|two_one_opt_arg:"1"',
         ):
             FilterExpression(expr, parser)
+
+    def test_filter_numeric_argument_parsing(self):
+        p = Parser("", builtins=[filter_library])
+
+        cases = {
+            "5": 5,
+            "-5": -5,
+            "5.2": 5.2,
+            ".4": 0.4,
+            "5.2e3": 5200.0,  # 5.2 × 10³ = 5200.0.
+            "5.2E3": 5200.0,  # Case-insensitive.
+            "5.2e-3": 0.0052,  # Negative exponent.
+            "-1.5E4": -15000.0,
+            "+3.0e2": 300.0,
+            ".5e2": 50.0,  # 0.5 × 10² = 50.0
+        }
+        for num, expected in cases.items():
+            with self.subTest(num=num):
+                self.assertEqual(FilterExpression(num, p).resolve({}), expected)
+                self.assertEqual(
+                    FilterExpression(f"0|default:{num}", p).resolve({}), expected
+                )
+
+        invalid_numbers = [
+            "abc123",
+            "123abc",
+            "foo",
+            "error",
+            "1e",
+            "e400",
+            "1e.2",
+            "1e2.",
+            "1e2.0",
+            "1e2a",
+            "1e2e3",
+            "1e-",
+            "1e-a",
+        ]
+
+        for num in invalid_numbers:
+            with self.subTest(num=num):
+                self.assertIsNone(
+                    FilterExpression(num, p).resolve({}, ignore_failures=True)
+                )
+                with self.assertRaises(VariableDoesNotExist):
+                    FilterExpression(f"0|default:{num}", p).resolve({})

commit 38cf15f866f7ef4db181f8d62d5b987e0c722133

Fixed #36465, Refs #35816 -- Disallowed '+' and '-' characters in template variable names.
Regression in 5183f7c287a9a5d61ca1103b55166cda52d9c647.

Thank you to Jon Banafato and Baptiste Mispelon for the report.

django/template/base.py
@@ -852,6 +852,13 @@ class Variable:
                         "Variables and attributes may "
                         "not begin with underscores: '%s'" % var
                     )
+                # Disallow characters that are allowed in numbers but not in a
+                # variable name.
+                for c in ["+", "-"]:
+                    if c in var:
+                        raise TemplateSyntaxError(
+                            "Invalid character ('%s') in variable name: '%s'" % (c, var)
+                        )
                 self.lookups = tuple(var.split(VARIABLE_ATTRIBUTE_SEPARATOR))
 
     def resolve(self, context):

tests/template_tests/test_base.py
@@ -78,9 +78,6 @@ class VariableTests(SimpleTestCase):
 
     def test_nonliterals(self):
         """Variable names that aren't resolved as literals."""
-        var_names = []
-        for var in ("inf", "infinity", "iNFiniTy", "nan"):
-            var_names.extend((var, "-" + var, "+" + var))
-        for var in var_names:
+        for var in ["inf", "infinity", "iNFiniTy", "nan"]:
             with self.subTest(var=var):
                 self.assertIsNone(Variable(var).literal)

tests/template_tests/test_parser.py
@@ -126,6 +126,16 @@ class ParserTests(SimpleTestCase):
         ):
             Variable({})
 
+        # Variables should raise when invalid characters in name.
+        for c in ["+", "-"]:
+            with self.subTest(invalid_character=c):
+                variable_name = f"variable{c}name"
+                with self.assertRaisesMessage(
+                    TemplateSyntaxError,
+                    f"Invalid character ('{c}') in variable name: '{variable_name}'",
+                ):
+                    Variable(variable_name)
+
     def test_filter_args_count(self):
         parser = Parser("")
         register = Library()
@@ -174,6 +184,7 @@ class ParserTests(SimpleTestCase):
     def test_filter_numeric_argument_parsing(self):
         p = Parser("", builtins=[filter_library])
 
+        # Values that resolve to a numeric literal.
         cases = {
             "5": 5,
             "-5": -5,
@@ -193,6 +204,7 @@ class ParserTests(SimpleTestCase):
                     FilterExpression(f"0|default:{num}", p).resolve({}), expected
                 )
 
+        # Values that are interpreted as names of variables that do not exist.
         invalid_numbers = [
             "abc123",
             "123abc",
@@ -205,8 +217,6 @@ class ParserTests(SimpleTestCase):
             "1e2.0",
             "1e2a",
             "1e2e3",
-            "1e-",
-            "1e-a",
         ]
 
         for num in invalid_numbers:
@@ -215,4 +225,18 @@ class ParserTests(SimpleTestCase):
                     FilterExpression(num, p).resolve({}, ignore_failures=True)
                 )
                 with self.assertRaises(VariableDoesNotExist):
+                    FilterExpression(f"0|default:{num}", p).resolve({})
+
+        # Values that are interpreted as an invalid variable name.
+        invalid_numbers_and_var_names = [
+            "1e-",
+            "1e-a",
+            "1+1",
+            "1-1",
+        ]
+        for num in invalid_numbers_and_var_names:
+            with self.subTest(num=num):
+                with self.assertRaises(TemplateSyntaxError):
+                    FilterExpression(num, p).resolve({})
+                with self.assertRaises(TemplateSyntaxError):
                     FilterExpression(f"0|default:{num}", p).resolve({})