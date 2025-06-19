38845ba5e98f5e07cd9bfcb4f6990d67241a822b
A. Rafey Khan <khanxbahria@gmail.com>
2025-06-19 23:12:26 +0300

    Added basic keyboard shortcuts to admin: - Show keyboard shortcuts dialog - Save, Save and add another, Save and continue editing - Delete - Confirm deletion - Cancel deletion - Go to admin index

---
+++ django/contrib/admin/static/admin/js/shortcuts.js
@@ -0,0 +1,105 @@
+'use strict';
+{
+    const inputNonTextFieldTypes = ['submit', 'reset', 'checkbox', 'radio', 'file', 'button'];
+
+    function isFocusedTextField() {
+        const tag = document.activeElement.nodeName;
+        const type = document.activeElement.type;
+        const isContentEditable = document.activeElement.isContentEditable;
+        return (
+            tag === 'TEXTAREA' ||
+            tag === 'SELECT' ||
+            (tag === 'INPUT' && !inputNonTextFieldTypes.includes(type)) ||
+            isContentEditable
+        );
+    }
+
+    let previousKey = undefined;
+    const shortcutFunctions = new Map([
+        ["g i", () => { document.location.href = "/admin/"; }],
+    ]);
+
+    function registerDeclarativeShortcuts() {
+        const elements = document.querySelectorAll('[data-keyboard-shortcut]');
+        for (const element of elements) {
+            shortcutFunctions.set(element.getAttribute('data-keyboard-shortcut'), () => {
+                element.click();
+            });
+        }
+    }
+
+    function isApple() {
+        return (navigator.platform.indexOf("Mac") === 0 || navigator.platform === "iPhone");
+    }
+
+    function removePreviousKey(key) {
+        if (previousKey === key) {
+            previousKey = undefined;
+        }
+    }
+
+    function storePreviousKey(key) {
+        previousKey = key;
+        setTimeout(function() {
+            removePreviousKey(key);
+        }, 5000);
+    }
+
+    function showDialog(id) {
+        const dialog = document.getElementById(id);
+        dialog.showModal();
+    }
+
+    function showShortcutsDialog() {
+        showDialog("shortcuts-dialog");
+    }
+
+    function showDialogOnClick() {
+        const dialogButton = document.getElementById("open-shortcuts");
+        dialogButton.addEventListener("click", showShortcutsDialog);
+    }
+
+    function handleKeyDown(event) {
+        // If we're in a focused text field, don't apply keyboard shortcuts
+        if (isFocusedTextField()) {
+            return;
+        }
+
+        // If there's a previous key, we first check whether the combination of the
+        // previous key followed by the current key are a shortcut
+        const shortcutWithPreviousKey = previousKey ? `${previousKey} ${event.key}` : null;
+        if (shortcutWithPreviousKey && shortcutFunctions.has(shortcutWithPreviousKey)) {
+            shortcutFunctions.get(shortcutWithPreviousKey)();
+            return;
+        }
+
+        // Otherwise, check if the new key has a shortcut, e.g `?`
+        if (shortcutFunctions.has(event.key)) {
+            shortcutFunctions.get(event.key)();
+            return;
+        }
+
+        // Simply store the key for the next keyDown
+        storePreviousKey(event.key);
+
+    }
+
+    function replaceModifiers() {
+        if (isApple()) {
+            document.querySelectorAll(".shortcut-keys .alt").forEach(function(modifier) {
+                modifier.innerHTML = "⌥";
+            });
+        }
+    }
+
+    if (document.readyState === "loading") {
+        document.addEventListener("DOMContentLoaded", showDialogOnClick);
+        document.addEventListener("DOMContentLoaded", replaceModifiers);
+        document.addEventListener("DOMContentLoaded", registerDeclarativeShortcuts);
+    } else {
+        showDialogOnClick();
+        replaceModifiers();
+        registerDeclarativeShortcuts();
+    }
+    document.addEventListener("keydown", handleKeyDown);
+}
