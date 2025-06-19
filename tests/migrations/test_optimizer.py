bbbf4a29b0fbc7316cf8f340898384bc476e279e
Jacob Walls <jacobtylerwalls@gmail.com>
2025-06-19 06:22:23 -0400

    Refs #35038 -- Reduced CreateModel/AlterConstraint operations when optimizing migrations.

--- tests/migrations/test_optimizer.py
+++ tests/migrations/test_optimizer.py
@@ -1434,6 +1434,54 @@ class OptimizerTests(OptimizerTestBase):
             ],
         )
 
+    def test_create_model_alter_constraint(self):
+        original_constraint = models.CheckConstraint(
+            condition=models.Q(weight__gt=0), name="pony_weight_gt_0"
+        )
+        altered_constraint = models.CheckConstraint(
+            condition=models.Q(weight__gt=0),
+            name="pony_weight_gt_0",
+            violation_error_message="incorrect weight",
+        )
+        self.assertOptimizesTo(
+            [
+                migrations.CreateModel(
+                    name="Pony",
+                    fields=[
+                        ("weight", models.IntegerField()),
+                    ],
+                    options={
+                        "constraints": [
+                            original_constraint,
+                            models.UniqueConstraint(
+                                "weight", name="pony_weight_unique"
+                            ),
+                        ],
+                    },
+                ),
+                migrations.AlterConstraint(
+                    "Pony", "pony_weight_gt_0", altered_constraint
+                ),
+            ],
+            [
+                migrations.CreateModel(
+                    name="Pony",
+                    fields=[
+                        ("weight", models.IntegerField()),
+                    ],
+                    options={
+                        "constraints": [
+                            models.UniqueConstraint(
+                                "weight",
+                                name="pony_weight_unique",
+                            ),
+                            altered_constraint,
+                        ]
+                    },
+                ),
+            ],
+        )
+
     def test_create_model_remove_constraint(self):
         self.assertOptimizesTo(
             [
