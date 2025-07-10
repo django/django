cf5c808b602d8492f05df06c3a5cec60d397197d
sobolevn <mail@sobolevn.me>
2025-07-10 14:52:35 +0300

    [micro-optimization] Speed up form field validation
    Now we won't have to resolve `clean_%s` attribute twice which will give a small speedup without changing any logic / contracts.

--- django/forms/forms.py
+++ django/forms/forms.py
@@ -339,12 +339,13 @@ class BaseForm(RenderableFormMixin):
         self._post_clean()
 
     def _clean_fields(self):
+        sentinel = object()
         for name, bf in self._bound_items():
             field = bf.field
             try:
                 self.cleaned_data[name] = field._clean_bound_field(bf)
-                if hasattr(self, "clean_%s" % name):
-                    value = getattr(self, "clean_%s" % name)()
+                if (field_clean := getattr(self, f"clean_{name}", sentinel)) is not sentinel:
+                    value = field_clean()
                     self.cleaned_data[name] = value
             except ValidationError as e:
                 self.add_error(name, e)
