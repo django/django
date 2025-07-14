578ef30376fd279865d1719fcad064a00c625a6b
Natalia <124304+nessita@users.noreply.github.com>
2025-07-14 14:45:03 -0300

    Fixed #36499 -- Adjusted utils_tests.test_html.TestUtilsHtml.test_strip_tags following Python's HTMLParser new behavior.

--- tests/test_utils/tests.py
+++ tests/test_utils/tests.py
@@ -959,10 +959,10 @@ class HTMLEqualTests(SimpleTestCase):
             self.assertHTMLEqual("", "<p>")
         error_msg = (
             "First argument is not valid HTML:\n"
-            "('Unexpected end tag `div` (Line 1, Column 6)', (1, 6))"
+            "('Unexpected end tag `div` (Line 1, Column 0)', (1, 0))"
         )
         with self.assertRaisesMessage(AssertionError, error_msg):
-            self.assertHTMLEqual("< div></ div>", "<div></div>")
+            self.assertHTMLEqual("</div>", "<div></div>")
         with self.assertRaises(HTMLParseError):
             parse_html("</p>")
 
