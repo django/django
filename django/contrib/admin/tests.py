d4de30dfc7ed408a5171b0e50f77c47dce27442a
Sarah Boyce <42296566+sarahboyce@users.noreply.github.com>
2025-07-16 12:20:41 +0200

    Triggered window resize to fix layout issues.

--- django/contrib/admin/tests.py
+++ django/contrib/admin/tests.py
@@ -148,6 +148,14 @@ class AdminSeleniumTestCase(SeleniumTestCase, StaticLiveServerTestCase):
 
         self.wait_page_ready(timeout=timeout)
 
+    def trigger_resize(self):
+        width = self.selenium.get_window_size()["width"]
+        height = self.selenium.get_window_size()["height"]
+        self.selenium.set_window_size(width + 1, height)
+        self.wait_page_ready()
+        self.selenium.set_window_size(width, height)
+        self.wait_page_ready()
+
     def admin_login(self, username, password, login_url="/admin/"):
         """
         Log in to the admin.
