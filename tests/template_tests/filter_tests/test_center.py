ab612a0d31ca838b6faeecb8900866d35a4494d2
mriduldhall <mriduldhall1@gmail.com>
2025-07-25 16:26:36 +0100

    Added test for various widths in tests/template_tests/filter_tests/test_center.py.

--- tests/template_tests/filter_tests/test_center.py
+++ tests/template_tests/filter_tests/test_center.py
@@ -34,3 +34,9 @@ class FunctionTests(SimpleTestCase):
 
     def test_non_string_input(self):
         self.assertEqual(center(123, 5), " 123 ")
+
+    def test_widths(self):
+        value = "something"
+        for i in range(-1, len(value) + 1):
+            with self.subTest(i=i):
+                self.assertEqual(center(value, i), value)
