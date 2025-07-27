1bc7124d3d68adc148c2ea6cfce98bbc7dc4a2c4
Jkhall81 <jason.kei.hall@gmail.com>
2025-07-26 18:10:10 -0700

    Fix get_or_create() to re-raise IntegrityError when object still doesnt exist

--- django/db/models/query.py
+++ django/db/models/query.py
@@ -967,29 +967,22 @@ class QuerySet(AltersData):
     abulk_update.alters_data = True
 
     def get_or_create(self, defaults=None, **kwargs):
-        """
-        Look up an object with the given kwargs, creating one if necessary.
-        Return a tuple of (object, created), where created is a boolean
-        specifying whether an object was created.
-        """
-        # The get() needs to be targeted at the write database in order
-        # to avoid potential transaction consistency problems.
         self._for_write = True
         try:
             return self.get(**kwargs), False
         except self.model.DoesNotExist:
             params = self._extract_model_params(defaults, **kwargs)
-            # Try to create an object using passed params.
             try:
                 with transaction.atomic(using=self.db):
                     params = dict(resolve_callables(params))
                     return self.create(**params), True
-            except IntegrityError:
+            except IntegrityError as e:
                 try:
                     return self.get(**kwargs), False
                 except self.model.DoesNotExist:
-                    pass
-                raise
+                    # Re-raise if the object still doesn't exist.
+                    # this wasn't a race condition but an integrity error.
+                    raise e
 
     get_or_create.alters_data = True
 
