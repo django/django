django/django

commit 849f8307a5bb33465252d0891a9b2c47dde65889

Fixed #34917 -- Underlined links in the main content area of the admin.

django/contrib/admin/static/admin/css/base.css
@@ -118,6 +118,15 @@ a:focus {
     text-decoration: underline;
 }
 
+a:not(
+    [role="button"],
+    #header a,
+    #nav-sidebar a,
+    #content-main.app-list a
+) {
+    text-decoration: underline;
+}
+
 a img {
     border: none;
 }

django/contrib/admin/templates/admin/change_form_object_tools.html
@@ -2,7 +2,7 @@
 {% block object-tools-items %}
 <li>
     {% url opts|admin_urlname:'history' original.pk|admin_urlquote as history_url %}
-    <a href="{% add_preserved_filters history_url %}" class="historylink">{% translate "History" %}</a>
+    <a role="button" href="{% add_preserved_filters history_url %}" class="historylink">{% translate "History" %}</a>
 </li>
 {% if has_absolute_url %}<li><a href="{{ absolute_url }}" class="viewsitelink">{% translate "View on site" %}</a></li>{% endif %}
 {% endblock %}

django/contrib/admin/templates/admin/change_list_object_tools.html
@@ -4,7 +4,7 @@
   {% if has_add_permission %}
   <li>
     {% url cl.opts|admin_urlname:'add' as add_url %}
-    <a href="{% add_preserved_filters add_url is_popup to_field %}" class="addlink">
+    <a role="button" href="{% add_preserved_filters add_url is_popup to_field %}" class="addlink">
       {% blocktranslate with cl.opts.verbose_name as name %}Add {{ name }}{% endblocktranslate %}
     </a>
   </li>

tests/admin_views/tests.py
@@ -2683,8 +2683,8 @@ class AdminViewPermissionsTest(TestCase):
         self.assertContains(response, "<label>Extra form field:</label>")
         self.assertContains(
             response,
-            '<a href="/test_admin/admin/admin_views/article/" class="closelink">Close'
-            "</a>",
+            '<a role="button" href="/test_admin/admin/admin_views/article/" '
+            'class="closelink">Close</a>',
         )
         self.assertEqual(response.context["title"], "View article")
         post = self.client.post(article_change_url, change_dict)
@@ -2835,8 +2835,8 @@ class AdminViewPermissionsTest(TestCase):
         self.assertContains(response, "<h1>View article</h1>")
         self.assertContains(
             response,
-            '<a href="/test_admin/admin9/admin_views/article/" class="closelink">Close'
-            "</a>",
+            '<a role="button" href="/test_admin/admin9/admin_views/article/" '
+            'class="closelink">Close</a>',
         )
 
     def test_change_view_save_as_new(self):
@@ -4056,7 +4056,8 @@ class AdminViewStringPrimaryKeyTest(TestCase):
             args=(quote(self.pk),),
         )
         self.assertContains(
-            response, '<a href="%s" class="historylink"' % escape(expected_link)
+            response,
+            '<a role="button" href="%s" class="historylink"' % escape(expected_link),
         )
 
     def test_redirect_on_add_view_continue_button(self):
@@ -8313,13 +8314,14 @@ class AdminKeepChangeListFiltersTests(TestCase):
 
         # Check the history link.
         history_link = re.search(
-            '<a href="(.*?)" class="historylink">History</a>', response.text
+            '<a role="button" href="(.*?)" class="historylink">History</a>',
+            response.text,
         )
         self.assertURLEqual(history_link[1], self.get_history_url())
 
         # Check the delete link.
         delete_link = re.search(
-            '<a href="(.*?)" class="deletelink">Delete</a>', response.text
+            '<a role="button" href="(.*?)" class="deletelink">Delete</a>', response.text
         )
         self.assertURLEqual(delete_link[1], self.get_delete_url())
 
@@ -8359,7 +8361,7 @@ class AdminKeepChangeListFiltersTests(TestCase):
         self.client.force_login(viewuser)
         response = self.client.get(self.get_change_url())
         close_link = re.search(
-            '<a href="(.*?)" class="closelink">Close</a>', response.text
+            '<a role="button" href="(.*?)" class="closelink">Close</a>', response.text
         )
         close_link = close_link[1].replace("&amp;", "&")
         self.assertURLEqual(close_link, self.get_changelist_url())

tests/admin_inlines/tests.py
@@ -1454,12 +1454,13 @@ class TestReadOnlyChangeViewInlinePermissions(TestCase):
         response = self.client.get(self.change_url)
         self.assertContains(
             response,
-            '<a href="/admin/admin_inlines/poll/" class="closelink">Close</a>',
+            '<a role="button" href="/admin/admin_inlines/poll/" class="closelink">'
+            "Close</a>",
             html=True,
         )
         delete_link = (
-            '<a href="/admin/admin_inlines/poll/%s/delete/" class="deletelink">Delete'
-            "</a>"
+            '<a role="button" href="/admin/admin_inlines/poll/%s/delete/" '
+            'class="deletelink">Delete</a>'
         )
         self.assertNotContains(response, delete_link % self.poll.id, html=True)
         self.assertNotContains(

django/contrib/admin/static/admin/js/admin/DateTimeShortcuts.js
@@ -108,6 +108,7 @@
             const now_link = document.createElement('a');
             now_link.href = "#";
             now_link.textContent = gettext('Now');
+            now_link.role = 'button';
             now_link.addEventListener('click', function(e) {
                 e.preventDefault();
                 DateTimeShortcuts.handleClockQuicklink(num, -1);
@@ -163,7 +164,7 @@
             // where name is the name attribute of the <input>.
             const name = typeof DateTimeShortcuts.clockHours[inp.name] === 'undefined' ? 'default_' : inp.name;
             DateTimeShortcuts.clockHours[name].forEach(function(element) {
-                const time_link = quickElement('a', quickElement('li', time_list), gettext(element[0]), 'href', '#');
+                const time_link = quickElement('a', quickElement('li', time_list), gettext(element[0]), 'role', 'button', 'href', '#');
                 time_link.addEventListener('click', function(e) {
                     e.preventDefault();
                     DateTimeShortcuts.handleClockQuicklink(num, element[1]);
@@ -172,7 +173,7 @@
 
             const cancel_p = quickElement('p', clock_box);
             cancel_p.className = 'calendar-cancel';
-            const cancel_link = quickElement('a', cancel_p, gettext('Cancel'), 'href', '#');
+            const cancel_link = quickElement('a', cancel_p, gettext('Cancel'), 'role', 'button', 'href', '#');
             cancel_link.addEventListener('click', function(e) {
                 e.preventDefault();
                 DateTimeShortcuts.dismissClock(num);
@@ -235,6 +236,7 @@
             inp.parentNode.insertBefore(shortcuts_span, inp.nextSibling);
             const today_link = document.createElement('a');
             today_link.href = '#';
+            today_link.role = 'button';
             today_link.appendChild(document.createTextNode(gettext('Today')));
             today_link.addEventListener('click', function(e) {
                 e.preventDefault();
@@ -309,19 +311,19 @@
             // calendar shortcuts
             const shortcuts = quickElement('div', cal_box);
             shortcuts.className = 'calendar-shortcuts';
-            let day_link = quickElement('a', shortcuts, gettext('Yesterday'), 'href', '#');
+            let day_link = quickElement('a', shortcuts, gettext('Yesterday'), 'role', 'button', 'href', '#');
             day_link.addEventListener('click', function(e) {
                 e.preventDefault();
                 DateTimeShortcuts.handleCalendarQuickLink(num, -1);
             });
             shortcuts.appendChild(document.createTextNode('\u00A0|\u00A0'));
-            day_link = quickElement('a', shortcuts, gettext('Today'), 'href', '#');
+            day_link = quickElement('a', shortcuts, gettext('Today'), 'role', 'button', 'href', '#');
             day_link.addEventListener('click', function(e) {
                 e.preventDefault();
                 DateTimeShortcuts.handleCalendarQuickLink(num, 0);
             });
             shortcuts.appendChild(document.createTextNode('\u00A0|\u00A0'));
-            day_link = quickElement('a', shortcuts, gettext('Tomorrow'), 'href', '#');
+            day_link = quickElement('a', shortcuts, gettext('Tomorrow'), 'role', 'button', 'href', '#');
             day_link.addEventListener('click', function(e) {
                 e.preventDefault();
                 DateTimeShortcuts.handleCalendarQuickLink(num, +1);
@@ -330,7 +332,7 @@
             // cancel bar
             const cancel_p = quickElement('p', cal_box);
             cancel_p.className = 'calendar-cancel';
-            const cancel_link = quickElement('a', cancel_p, gettext('Cancel'), 'href', '#');
+            const cancel_link = quickElement('a', cancel_p, gettext('Cancel'), 'role', 'button', 'href', '#');
             cancel_link.addEventListener('click', function(e) {
                 e.preventDefault();
                 DateTimeShortcuts.dismissCalendar(num);

django/contrib/admin/static/admin/js/calendar.js
@@ -160,7 +160,7 @@ depends on core.js for utility functions like removeChildren or quickElement
                 }
 
                 const cell = quickElement('td', tableRow, '', 'class', todayClass);
-                const link = quickElement('a', cell, currentDay, 'href', '#');
+                const link = quickElement('a', cell, currentDay, 'role', 'button', 'href', '#');
                 link.addEventListener('click', calendarMonth(year, month));
                 currentDay++;
             }

django/contrib/admin/templates/admin/change_list_results.html
@@ -18,7 +18,7 @@
          <a href="{{ header.url_toggle }}" class="toggle {{ header.ascending|yesno:'ascending,descending' }}" title="{% translate "Toggle sorting" %}"></a>
        </div>
    {% endif %}
-   <div class="text">{% if header.sortable %}<a href="{{ header.url_primary }}">{{ header.text|capfirst }}</a>{% else %}<span>{{ header.text|capfirst }}</span>{% endif %}</div>
+   <div class="text">{% if header.sortable %}<a role="button" href="{{ header.url_primary }}">{{ header.text|capfirst }}</a>{% else %}<span>{{ header.text|capfirst }}</span>{% endif %}</div>
    <div class="clear"></div>
 </th>{% endfor %}
 </tr>

commit 8aae39612754e36e996e5d7ec1b0277cb3423ce7

Fixed #36528, Refs #34917 -- Removed role="button" from object-tools links.
Regression in 849f8307a5bb33465252d0891a9b2c47dde65889.
In order to prevent underlines on links styled like buttons, role="button" was added.
This has been removed and the style updated to reflect that these are links.

django/contrib/admin/static/admin/css/base.css
@@ -129,7 +129,8 @@ a:not(
     [role="button"],
     #header a,
     #nav-sidebar a,
-    #content-main.app-list a
+    #content-main.app-list a,
+    .object-tools a,
 ) {
     text-decoration: underline;
 }

django/contrib/admin/templates/admin/change_form_object_tools.html
@@ -2,7 +2,7 @@
 {% block object-tools-items %}
 <li>
     {% url opts|admin_urlname:'history' original.pk|admin_urlquote as history_url %}
-    <a role="button" href="{% add_preserved_filters history_url %}" class="historylink">{% translate "History" %}</a>
+    <a href="{% add_preserved_filters history_url %}" class="historylink">{% translate "History" %}</a>
 </li>
 {% if has_absolute_url %}<li><a href="{{ absolute_url }}" class="viewsitelink">{% translate "View on site" %}</a></li>{% endif %}
 {% endblock %}

django/contrib/admin/templates/admin/change_list_object_tools.html
@@ -4,7 +4,7 @@
   {% if has_add_permission %}
   <li>
     {% url cl.opts|admin_urlname:'add' as add_url %}
-    <a role="button" href="{% add_preserved_filters add_url is_popup to_field %}" class="addlink">
+    <a href="{% add_preserved_filters add_url is_popup to_field %}" class="addlink">
       {% blocktranslate with cl.opts.verbose_name as name %}Add {{ name }}{% endblocktranslate %}
     </a>
   </li>

tests/admin_views/tests.py
@@ -4086,7 +4086,7 @@ class AdminViewStringPrimaryKeyTest(TestCase):
         )
         self.assertContains(
             response,
-            '<a role="button" href="%s" class="historylink"' % escape(expected_link),
+            '<a href="%s" class="historylink"' % escape(expected_link),
         )
 
     def test_redirect_on_add_view_continue_button(self):
@@ -6957,6 +6957,34 @@ class SeleniumTests(AdminSeleniumTestCase):
             save_button.click()
 
     @screenshot_cases(["desktop_size", "mobile_size", "rtl", "dark", "high_contrast"])
+    def test_object_tools(self):
+        from selenium.webdriver.common.by import By
+
+        state = State.objects.create(name="Korea")
+        city = City.objects.create(state=state, name="Gwangju")
+        self.admin_login(
+            username="super", password="secret", login_url=reverse("admin:index")
+        )
+        self.selenium.get(
+            self.live_server_url + reverse("admin:admin_views_city_changelist")
+        )
+        object_tools = self.selenium.find_elements(
+            By.CSS_SELECTOR, "ul.object-tools li a"
+        )
+        self.assertEqual(len(object_tools), 1)
+        self.take_screenshot("changelist")
+
+        self.selenium.get(
+            self.live_server_url
+            + reverse("admin:admin_views_city_change", args=(city.pk,))
+        )
+        object_tools = self.selenium.find_elements(
+            By.CSS_SELECTOR, "ul.object-tools li a"
+        )
+        self.assertEqual(len(object_tools), 2)
+        self.take_screenshot("changeform")
+
+    @screenshot_cases(["desktop_size", "mobile_size", "rtl", "dark", "high_contrast"])
     def test_long_header_with_object_tools_layout(self):
         from selenium.webdriver.common.by import By
 
@@ -8415,7 +8443,7 @@ class AdminKeepChangeListFiltersTests(TestCase):
 
         # Check the history link.
         history_link = re.search(
-            '<a role="button" href="(.*?)" class="historylink">History</a>',
+            '<a href="(.*?)" class="historylink">History</a>',
             response.text,
         )
         self.assertURLEqual(history_link[1], self.get_history_url())