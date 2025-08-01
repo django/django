django/django

commit 65ab92f6a83644bbb555d0eff3a02d8d9301aba4

Fixed writer_name deprecation warning in docutils 0.22+.

django/contrib/admindocs/utils.py
@@ -82,7 +82,7 @@ def parse_rst(text, default_reference_context, thing_being_parsed=None):
         source % text,
         source_path=thing_being_parsed,
         destination_path=None,
-        writer_name="html",
+        writer="html",
         settings_overrides=overrides,
     )
     return mark_safe(parts["fragment"])

commit 62904cb3bf5ab537bb2d48e3c5f55fdadf412684

Fixed #36535 -- Ensured compatibility with docutils 0.19 through 0.22.
Regression in 65ab92f6a83644bbb555d0eff3a02d8d9301aba4.

django/contrib/admindocs/utils.py
@@ -13,6 +13,7 @@ try:
     import docutils.core
     import docutils.nodes
     import docutils.parsers.rst.roles
+    import docutils.writers
 except ImportError:
     docutils_is_available = False
 else:
@@ -78,11 +79,13 @@ def parse_rst(text, default_reference_context, thing_being_parsed=None):
 
 .. default-role::
 """
+    # Ensure compatibility with docutils < 0.22 and also 0.22+.
+    writer_instance = docutils.writers.get_writer_class("html")()
     parts = docutils.core.publish_parts(
         source % text,
         source_path=thing_being_parsed,
         destination_path=None,
-        writer="html",
+        writer=writer_instance,
         settings_overrides=overrides,
     )
     return mark_safe(parts["fragment"])