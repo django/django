5e55496af91ca25ef232c82e534ce2eb5af41799
Mridul Dhall <mriduldhall@Mriduls-MacBook-Pro.local>
2025-08-08 17:12:31 +0100

    Fixed #36543 -- Fixed time formats for fr_CA.
    Thanks Chris Anderson for the report.

--- tests/i18n/tests.py
+++ tests/i18n/tests.py
@@ -1158,6 +1158,27 @@ class FormattingTests(SimpleTestCase):
                 ),
             )
 
+    def test_uncommon_locale_formats(self):
+        testcases = {
+            # French Canadian locale uses 'h' as time format seperator.
+            ("fr-ca", time_format, (self.t, "TIME_FORMAT")): "10\xa0h\xa015",
+            (
+                "fr-ca",
+                date_format,
+                (self.dt, "DATETIME_FORMAT"),
+            ): "31 décembre 2009, 20\xa0h\xa050",
+            (
+                "fr-ca",
+                date_format,
+                (self.dt, "SHORT_DATETIME_FORMAT"),
+            ): "2009-12-31 20\xa0h\xa050",
+        }
+        for testcase, expected in testcases.items():
+            locale, format_function, format_args = testcase
+            with self.subTest(locale=locale, expected=expected):
+                with translation.override(locale, deactivate=True):
+                    self.assertEqual(expected, format_function(*format_args))
+
     def test_sub_locales(self):
         """
         Check if sublocales fall back to the main locale
