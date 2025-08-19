21616c9478af7bca93492d3d05e5a96b89b83cb3
Xinyi Rong <Lizard.rar@gmail.com>
2025-08-19 02:13:19 -0700

    Fixed #36561 -- Used request.auser() in contrib.auth.aupdate_session_auth_hash().

--- django/contrib/auth/__init__.py
+++ django/contrib/auth/__init__.py
@@ -408,5 +408,5 @@ def update_session_auth_hash(request, user):
 async def aupdate_session_auth_hash(request, user):
     """See update_session_auth_hash()."""
     await request.session.acycle_key()
-    if hasattr(user, "get_session_auth_hash") and request.user == user:
+    if hasattr(user, "get_session_auth_hash") and await request.auser() == user:
         await request.session.aset(HASH_SESSION_KEY, user.get_session_auth_hash())
