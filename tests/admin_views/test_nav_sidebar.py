4b34ce43286104cca0974b15120f2f911ab2d5af
antoliny0919 <antoliny0919@gmail.com>
2025-08-24 16:17:47 +0900

    Fixed #34044 -- Added admin sidebar to index and app index pages.

--- tests/admin_views/test_nav_sidebar.py
+++ tests/admin_views/test_nav_sidebar.py
@@ -40,13 +40,6 @@ class AdminSidebarTests(TestCase):
     def setUp(self):
         self.client.force_login(self.superuser)
 
-    def test_sidebar_not_on_index(self):
-        response = self.client.get(reverse("test_with_sidebar:index"))
-        self.assertContains(response, '<div class="main" id="main">')
-        self.assertNotContains(
-            response, '<nav class="sticky" id="nav-sidebar" aria-label="Sidebar">'
-        )
-
     def test_sidebar_disabled(self):
         response = self.client.get(reverse("test_without_sidebar:index"))
         self.assertNotContains(
