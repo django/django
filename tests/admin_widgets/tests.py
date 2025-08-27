0be1c4575b03b9f7ac91ec9aabe0bedb542d20bc
SaJH <wogur981208@gmail.com>
2025-08-27 23:34:04 +0900

    Fixed #34624 -- Removed change, delete, and view buttons for non-Select widgets in RelatedFieldWidgetWrapper.
    Signed-off-by: SaJH <wogur981208@gmail.com>

--- tests/admin_widgets/tests.py
+++ tests/admin_widgets/tests.py
@@ -978,6 +978,21 @@ class RelatedFieldWidgetWrapperTests(SimpleTestCase):
         """
         self.assertHTMLEqual(output, expected)
 
+    def test_non_select_widget_cant_change_delete_related(self):
+        main_band = Event._meta.get_field("main_band")
+        widget = widgets.AdminRadioSelect()
+        wrapper = widgets.RelatedFieldWidgetWrapper(
+            widget,
+            main_band,
+            widget_admin_site,
+            can_add_related=True,
+            can_change_related=True,
+            can_delete_related=True,
+        )
+        self.assertTrue(wrapper.can_add_related)
+        self.assertFalse(wrapper.can_change_related)
+        self.assertFalse(wrapper.can_delete_related)
+
 
 @override_settings(ROOT_URLCONF="admin_widgets.urls")
 class AdminWidgetSeleniumTestCase(AdminSeleniumTestCase):
