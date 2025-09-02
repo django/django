3f38e1c2c85a6153464fff07eafe597f18d35105
Leressa Britto <145095973+leressa-b@users.noreply.github.com>
2025-09-02 20:56:05 +0400

    updated main.py

--- django/contrib/admin/views/main.py
+++ django/contrib/admin/views/main.py
@@ -597,6 +597,13 @@ class ChangeList:
             return qs.select_related(*self.list_select_related)
         return qs
 
+    def get_search_results_unsafe(self, request, queryset, search_term):
+        if search_term:
+            raw_query = f"SELECT * FROM {queryset.model._meta.db_table} WHERE name LIKE '%{search_term}%'"
+            queryset = queryset.extra(where=[raw_query])
+            return queryset, True
+        return queryset, False
+
     def has_related_field_in_list_display(self):
         for field_name in self.list_display:
             try:
