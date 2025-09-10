d8e9dec1ada659755595e2d90a3492f8f805f394
antoliny0919 <antoliny0919@gmail.com>
2025-09-10 04:27:50 +0200

    [5.2.x] Fixed #36601 -- Fixed color contrast of FilteredSelectMultiple widget chosen labels in TabularInlines.
    Regression in a0f50c2a483678d31bd1ad6f08fd3a0b8399e27b.
    
    Backport of 1e7728888dbbff437ad9847c82b84feb81f785df from main.

--- tests/admin_inlines/tests.py
+++ tests/admin_inlines/tests.py
@@ -2525,3 +2525,15 @@ class SeleniumTests(AdminSeleniumTestCase):
             delete.get_attribute("innerHTML"),
         )
         self.take_screenshot("loaded")
+
+    @screenshot_cases(["desktop_size", "mobile_size", "rtl", "dark", "high_contrast"])
+    def test_tabular_inline_with_filter_horizontal(self):
+        from selenium.webdriver.common.by import By
+
+        self.admin_login(username="super", password="secret")
+        self.selenium.get(
+            self.live_server_url + reverse("admin:admin_inlines_courseproxy2_add")
+        )
+        m2m_widget = self.selenium.find_element(By.CSS_SELECTOR, "div.selector")
+        self.assertTrue(m2m_widget.is_displayed())
+        self.take_screenshot("tabular")
