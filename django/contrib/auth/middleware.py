41bc48ac1ed1d515977ebe965993b1ef83eafd02
Adam Johnson <me@adamj.eu>
2025-09-11 10:09:53 +0100

    Fixed #36603 -- Optimized check order in LoginRequiredMiddleware.

--- django/contrib/auth/middleware.py
+++ django/contrib/auth/middleware.py
@@ -51,10 +51,10 @@ class LoginRequiredMiddleware(MiddlewareMixin):
     redirect_field_name = REDIRECT_FIELD_NAME
 
     def process_view(self, request, view_func, view_args, view_kwargs):
-        if request.user.is_authenticated:
+        if not getattr(view_func, "login_required", True):
             return None
 
-        if not getattr(view_func, "login_required", True):
+        if request.user.is_authenticated:
             return None
 
         return self.handle_no_permission(request, view_func)
