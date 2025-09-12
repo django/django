17d54ebbcb74ef401f54892b4d076ca0d1795518
Shubham Singh <ssingh@multimediallc.com>
2025-09-12 14:32:35 -0500

    Fixed #36491 -- Fixed crash in ParallelTestRunner with --buffer.
    Thanks Javier Buzzi and Adam Johnson for reviews.
    
    Co-authored-by: Simon Charette <charette.s@gmail.com>

--- tests/test_runner/test_parallel.py
+++ tests/test_runner/test_parallel.py
@@ -277,3 +277,27 @@ class ParallelTestSuiteTest(SimpleTestCase):
 
         self.assertEqual(len(result.errors), 0)
         self.assertEqual(len(result.failures), 0)
+
+    def test_buffer_mode_reports_setupclass_failure(self):
+        test = SampleErrorTest("dummy_test")
+        remote_result = RemoteTestResult()
+        suite = TestSuite([test])
+        suite.run(remote_result)
+
+        pts = ParallelTestSuite([suite], processes=2, buffer=True)
+        pts.serialized_aliases = set()
+        test_result = TestResult()
+        test_result.buffer = True
+
+        with unittest.mock.patch("multiprocessing.Pool") as mock_pool:
+
+            def fake_next(*args, **kwargs):
+                test_result.shouldStop = True
+                return (0, remote_result.events)
+
+            mock_pool.return_value.imap_unordered.return_value = unittest.mock.Mock(
+                next=fake_next
+            )
+            pts.run(test_result)
+
+        self.assertIn("ValueError: woops", test_result.errors[0][1])
