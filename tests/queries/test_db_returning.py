4fcc2883faa8e33152d9e45744354cff35636975
Simon Charette <charette.s@gmail.com>
2025-09-16 18:10:27 -0400

    Refs #27222 -- Restored Model.save()'s refreshing of db_returning fields even if a value is set.
    The logic could likely be adjusted to assign the pre_save value in most cases
    to avoid the database transit but it could break in subtle ways so it's not
    worth the complexity it would require.
    
    Regression in 94680437a45a71c70ca8bd2e68b72aa1e2eff337.
    
    Co-authored-by: Tim Graham <timograham@gmail.com>

--- tests/queries/test_db_returning.py
+++ tests/queries/test_db_returning.py
@@ -26,6 +26,11 @@ class ReturningValuesTests(TestCase):
         self.assertTrue(obj.created)
         self.assertIsInstance(obj.created, datetime.datetime)
 
+    def test_insert_returning_non_integer_from_literal_value(self):
+        obj = NonIntegerPKReturningModel.objects.create(pk="2025-01-01")
+        self.assertTrue(obj.created)
+        self.assertIsInstance(obj.created, datetime.datetime)
+
     def test_insert_returning_multiple(self):
         with CaptureQueriesContext(connection) as captured_queries:
             obj = ReturningModel.objects.create()
