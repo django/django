django/django

commit a0f50c2a483678d31bd1ad6f08fd3a0b8399e27b

Refs #34619 -- Fixed labels width in FilteredSelectMultiple in the admin.
Visual regression in 857b1048d53ebf5fc5581c110e85c212b81ca83a.

django/contrib/admin/static/admin/css/widgets.css
@@ -49,14 +49,19 @@
     padding: 8px;
 }
 
-.selector-chosen-title label {
+.aligned .selector-chosen-title label {
     color: var(--header-link-color);
+    width: 100%;
 }
 
 .selector-available-title {
     background: var(--darkened-bg);
     color: var(--body-quiet-color);
     padding: 8px;
+}
+
+.aligned .selector-available-title label {
+    width: 100%;
 }
 
 .selector .selector-filter {

commit 185df7f52cb54b5a6bd0374ab6a9daceb181485e

Fixed #36601 -- Fixed color contrast of FilteredSelectMultiple widget chosen labels in TabularInlines.
Regression in a0f50c2a483678d31bd1ad6f08fd3a0b8399e27b.

django/contrib/admin/static/admin/css/widgets.css
@@ -49,7 +49,7 @@
     padding: 8px;
 }
 
-.aligned .selector-chosen-title label {
+.selector-chosen-title label {
     color: var(--header-link-color);
     width: 100%;
 }
@@ -60,7 +60,7 @@
     padding: 8px;
 }
 
-.aligned .selector-available-title label {
+.selector-available-title label {
     width: 100%;
 }
 

tests/admin_inlines/tests.py
@@ -2558,3 +2558,15 @@ class SeleniumTests(AdminSeleniumTestCase):
         self.assertTrue(object_str.is_displayed())
         self.assertIn("Desert", object_str.text)
         self.take_screenshot("tabular")
+
+    @screenshot_cases(["desktop_size", "mobile_size", "rtl", "dark", "high_contrast"])
+    def test_tabular_inline_with_filter_horizontal(self):
+        from selenium.webdriver.common.by import By
+
+        self.admin_login(username="super", password="secret")
+        self.selenium.get(
+            self.live_server_url + reverse("admin:admin_inlines_courseproxy2_add")
+        )
+        m2m_widget = self.selenium.find_element(By.CSS_SELECTOR, "div.selector")
+        self.assertTrue(m2m_widget.is_displayed())
+        self.take_screenshot("tabular")