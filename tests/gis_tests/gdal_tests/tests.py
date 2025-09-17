cc05be43f26645bfc176fe743c69ed2e106dc22f
Josh <josh@joshthomas.dev>
2025-09-16 21:38:47 -0500

    update tests per code review

--- tests/gis_tests/gdal_tests/tests.py
+++ tests/gis_tests/gdal_tests/tests.py
@@ -1,4 +1,7 @@
+import ctypes.util
 import unittest
+from importlib.util import find_spec, module_from_spec
+from unittest import mock
 
 from django.contrib.gis.gdal import GDAL_VERSION, gdal_full_version, gdal_version
 
@@ -16,3 +19,15 @@ class GDALTest(unittest.TestCase):
         full_version = gdal_full_version()
         self.assertIn(gdal_version(), full_version)
         self.assertTrue(full_version.startswith(b"GDAL"))
+
+    def test_gdal_not_available(self):
+        with mock.patch.object(ctypes.util, "find_library", return_value=None):
+            # Does not raise ImproperlyConfigured
+            reimport_module("django.contrib.gis.gdal.libgdal")
+
+
+def reimport_module(name):
+    spec = find_spec(name)
+    module = module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
