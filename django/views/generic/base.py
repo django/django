0f75f8f1ff2078cde3c1701851c754b91c6baf44
sobolevn <mail@sobolevn.me>
2025-10-12 23:09:46 +0300

    Optimized View.dispatch() a bit.

--- django/views/generic/base.py
+++ django/views/generic/base.py
@@ -136,10 +136,9 @@ class View:
         # Try to dispatch to the right method; if a method doesn't exist,
         # defer to the error handler. Also defer to the error handler if the
         # request method isn't on the approved list.
-        if request.method.lower() in self.http_method_names:
-            handler = getattr(
-                self, request.method.lower(), self.http_method_not_allowed
-            )
+        method = request.method.lower()
+        if method in self.http_method_names:
+            handler = getattr(self, method, self.http_method_not_allowed)
         else:
             handler = self.http_method_not_allowed
         return handler(request, *args, **kwargs)
