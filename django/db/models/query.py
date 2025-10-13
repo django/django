a8b73415c8f4b9ccdce6c7448f3102c3107ca37b
Jacob Walls <jacobtylerwalls@gmail.com>
2025-10-13 17:22:17 -0400

    Fixed #36648, Refs #33772 -- Accounted for composite pks in first()/last() when aggregating.

--- django/db/models/query.py
+++ django/db/models/query.py
@@ -2141,8 +2141,14 @@ class QuerySet(AltersData):
             raise TypeError(f"Cannot use {operator_} operator with combined queryset.")
 
     def _check_ordering_first_last_queryset_aggregation(self, method):
-        if isinstance(self.query.group_by, tuple) and not any(
-            col.output_field is self.model._meta.pk for col in self.query.group_by
+        if (
+            isinstance(self.query.group_by, tuple)
+            # Raise if the pk fields are not in the group_by.
+            and self.model._meta.pk
+            not in {col.output_field for col in self.query.group_by}
+            and set(self.model._meta.pk_fields).difference(
+                {col.target for col in self.query.group_by}
+            )
         ):
             raise TypeError(
                 f"Cannot use QuerySet.{method}() on an unordered queryset performing "
