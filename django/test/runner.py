ab734fa7de104879712883c9f4365244dad44329
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2025-10-21 20:58:36 +0200

    Fixed typo in RemoteTestResult._confirm_picklable()'s docstring.

--- django/test/runner.py
+++ django/test/runner.py
@@ -201,7 +201,7 @@ class RemoteTestResult(unittest.TestResult):
         """
         Confirm that obj can be pickled and unpickled as multiprocessing will
         need to pickle the exception in the child process and unpickle it in
-        the parent process. Let the exception rise, if not.
+        the parent process. Let the exception raise, if not.
         """
         pickle.loads(pickle.dumps(obj))
 
