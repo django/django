bde0e84ea7fa62ea318e5c4850df9d4584d70736
Unai Loidi <uloidi@codesyntax.eus>
2025-10-22 12:22:47 +0200

    [5.2.x] Refs #36679 -- Updated Basque (eu) locale date formats.

--- django/conf/locale/eu/formats.py
+++ django/conf/locale/eu/formats.py
@@ -2,11 +2,11 @@
 #
 # The *_FORMAT strings use the Django date format syntax,
 # see https://docs.djangoproject.com/en/dev/ref/templates/builtins/#date
-DATE_FORMAT = r"Y\k\o N j\a"
+DATE_FORMAT = r"Y(\e)\k\o N j(\a)"
 TIME_FORMAT = "H:i"
-DATETIME_FORMAT = r"Y\k\o N j\a, H:i"
-YEAR_MONTH_FORMAT = r"Y\k\o F"
-MONTH_DAY_FORMAT = r"F\r\e\n j\a"
+DATETIME_FORMAT = r"Y(\e)\k\o N j(\a), H:i"
+YEAR_MONTH_FORMAT = r"Y(\e)\k\o F"
+MONTH_DAY_FORMAT = r"F\r\e\n j(\a)"
 SHORT_DATE_FORMAT = "Y-m-d"
 SHORT_DATETIME_FORMAT = "Y-m-d H:i"
 FIRST_DAY_OF_WEEK = 1  # Astelehena
