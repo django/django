a7c7a319f784f322ed0bfde4f7cbf9d054b823fa
Clifford Gama <cliffygamy@gmail.com>
2025-11-10 13:28:07 +0200

    Fixed #36722 -- Moved AutoFieldMixin validate_autopk_value() check to get_db_prep_save.
    The validation in validate_autopk_value is specific to saving. Having it in
    get_db_prep_value caused Value(0, AutoField()) to fail unexpectedly when used
    in a filter on MySQL.
    
    Thanks Jacob Walls for the review.

--- django/db/models/fields/__init__.py
+++ django/db/models/fields/__init__.py
@@ -2842,10 +2842,14 @@ class AutoFieldMixin:
         pass
 
     def get_db_prep_value(self, value, connection, prepared=False):
-        if not prepared:
-            value = self.get_prep_value(value)
-            value = connection.ops.validate_autopk_value(value)
-        return value
+        return value if prepared else self.get_prep_value(value)
+
+    def get_db_prep_save(self, value, connection):
+        if hasattr(value, "as_sql"):
+            return value
+        value = self.get_prep_value(value)
+        value = connection.ops.validate_autopk_value(value)
+        return self.get_db_prep_value(value, connection=connection, prepared=True)
 
     def contribute_to_class(self, cls, name, **kwargs):
         if cls._meta.auto_field:
