1bf78a04d030f55da052741913629167059a8474
Lily Acorn <code@lilyf.org>
2025-11-15 16:29:12 +0000

    Fixed #36735 -- Added UUID4 and UUID7 database functions.
    Co-authored-by: Mariusz Felisiak <felisiak.mariusz@gmail.com>

--- tests/db_functions/models.py
+++ tests/db_functions/models.py
@@ -55,3 +55,7 @@ class IntegerModel(models.Model):
 class FloatModel(models.Model):
     f1 = models.FloatField(null=True, blank=True)
     f2 = models.FloatField(null=True, blank=True)
+
+
+class UUIDModel(models.Model):
+    uuid = models.UUIDField(null=True)
