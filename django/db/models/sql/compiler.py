368e0741926f41000960b4d34aa880f2d202d1f1
utkarsh.arya@zomato.com <utkarsh.arya@zomato.com>
2025-11-15 22:58:55 +0000

    Fix performance regression in Model.objects.all().delete()
    Avoid generating unnecessary subquery for full table deletes to restore fast deletes and compatibility with MySQL LOCK TABLES.
    References #660

--- django/db/models/sql/compiler.py
+++ django/db/models/sql/compiler.py
@@ -1407,6 +1407,8 @@ class SQLInsertCompiler(SQLCompiler):
 class SQLDeleteCompiler(SQLCompiler):
     @cached_property
     def single_alias(self):
+        # Ensure base table is in aliases.
+        self.query.get_initial_alias()
         return sum(self.query.alias_refcount[t] > 0 for t in self.query.alias_map) == 1
 
     def _as_sql(self, query):
