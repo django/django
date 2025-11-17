faf73d9529fc2f7a7f1d68f0e1eb902dedb3b44e
varunkasyap <varunkasyap@hotmail.com>
2025-11-17 21:02:47 +0530

    draft

--- django/contrib/gis/gdal/libgdal.py
+++ django/contrib/gis/gdal/libgdal.py
@@ -22,6 +22,7 @@ if lib_path:
 elif os.name == "nt":
     # Windows NT shared libraries
     lib_names = [
+        "gdal312",
         "gdal311",
         "gdal310",
         "gdal309",
@@ -39,6 +40,7 @@ elif os.name == "posix":
     lib_names = [
         "gdal",
         "GDAL",
+        "gdal3.12.0",
         "gdal3.11.0",
         "gdal3.10.0",
         "gdal3.9.0",
