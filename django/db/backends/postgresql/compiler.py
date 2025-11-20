ac9bdcabe10fb7ac0c7e9ebcd879f5e34bee776f
Chris Wesseling <chris@maykinmedia.nl>
2025-11-20 15:01:14 +0100

    [5.2.x] Fixed #36748 -- Filtered non-standard placeholders from UNNEST queries.
    Backport of 5834643f43a767fe19f2c6d10217b204e7584ec8 from main.

--- django/db/backends/postgresql/compiler.py
+++ django/db/backends/postgresql/compiler.py
@@ -36,6 +36,11 @@ class SQLInsertCompiler(BaseSQLInsertCompiler):
             # Lack of fields denote the usage of the DEFAULT keyword
             # for the insertion of empty rows.
             or any(field is None for field in fields)
+            # Field.get_placeholder takes value as an argument, so the
+            # resulting placeholder might be dependent on the value.
+            # in UNNEST requires a single placeholder to "fit all values" in
+            # the array.
+            or any(hasattr(field, "get_placeholder") for field in fields)
             # Fields that don't use standard internal types might not be
             # unnest'able (e.g. array and geometry types are known to be
             # problematic).
