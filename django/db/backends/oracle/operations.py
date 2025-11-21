7e4c4f43f51e829e6378a0305bfda81479a368ab
Clifford Gama <cliffygamy@gmail.com>
2025-11-21 12:20:12 +0200

    Fixed #36718 -- Added JSONField support for negative array indexing on Oracle 21c+.

--- django/db/backends/oracle/operations.py
+++ django/db/backends/oracle/operations.py
@@ -729,3 +729,8 @@ END;
         if isinstance(expression, RawSQL) and expression.conditional:
             return True
         return False
+
+    def format_json_path_numeric_index(self, num):
+        if num < 0:
+            return "[last-%s]" % abs(num + 1)  # Indexing is zero-based.
+        return super().format_json_path_numeric_index(num)
