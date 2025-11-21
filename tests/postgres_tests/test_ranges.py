be0620b49fce5ddd179356575603c4160390965e
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2025-11-21 17:55:28 +0100

    Refs #36031 -- Added tests of DecimalRangeField __contains lookup with unbounded decimal ranges.
    Co-authored-by: Aman Sharma <210100011@iitb.ac.in>

--- tests/postgres_tests/test_ranges.py
+++ tests/postgres_tests/test_ranges.py
@@ -304,6 +304,25 @@ class TestQuerying(PostgreSQLTestCase):
             [self.objs[0]],
         )
 
+    def test_decimal_contains_range(self):
+        decimals = RangesModel.objects.bulk_create(
+            [
+                RangesModel(decimals=NumericRange(None, 10)),
+                RangesModel(decimals=NumericRange(10, None)),
+                RangesModel(decimals=NumericRange(5, 15)),
+                RangesModel(decimals=NumericRange(5, 15, "(]")),
+            ]
+        )
+        for contains, objs in [
+            (199, [decimals[1]]),
+            (1, [decimals[0]]),
+            (15, [decimals[1], decimals[3]]),
+        ]:
+            with self.subTest(decimal_contains=contains):
+                self.assertSequenceEqual(
+                    RangesModel.objects.filter(decimals__contains=contains), objs
+                )
+
     def test_contained_by(self):
         self.assertSequenceEqual(
             RangesModel.objects.filter(ints__contained_by=NumericRange(0, 20)),
