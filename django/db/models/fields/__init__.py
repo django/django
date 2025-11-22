fb5dea785689affccdc241bd6d3e72bbb3623b6a
Simon Charette <charette.s@gmail.com>
2025-11-22 13:32:34 -0500

    Fixed #36727 -- Deprecated Field.get_placeholder in favor of get_placeholder_sql.
    The lack of ability of the get_placeholder call chain to return SQL and
    parameters separated so they can be mogrified by the backend at execution time
    forced implementations to dangerously interpolate potentially user controlled
    values.
    
    The get_placeholder_sql name was chosen due to its proximity to the previous
    method, but other options such as Field.as_sql were considered but ultimately
    rejected due to its different input signature compared to Expression.as_sql
    that might have lead to confusion.
    
    There is a lot of overlap between what Field.get_db_prep_value and
    get_placeholder_sql do but folding the latter in the former would require
    changing its return signature to return expression which is a way more invasive
    change than what is proposed here.
    
    Given we always call get_db_prep_value it might still be an avenue worth
    exploring in the future to offer a publicly documented interface to allow field
    to take an active part in the compilation chain.
    
    Thanks Jacob for the review.

--- django/db/models/fields/__init__.py
+++ django/db/models/fields/__init__.py
@@ -30,6 +30,7 @@ from django.utils.dateparse import (
     parse_duration,
     parse_time,
 )
+from django.utils.deprecation import RemovedInDjango70Warning, django_file_prefixes
 from django.utils.duration import duration_string
 from django.utils.functional import Promise, cached_property
 from django.utils.ipv6 import MAX_IPV6_ADDRESS_LENGTH, clean_ipv6_address
@@ -180,6 +181,32 @@ class Field(RegisterLookupMixin):
         }
 
     description = property(_description)
+
+    def __init_subclass__(cls, **kwargs):
+        # RemovedInDjango70Warning: When the deprecation ends, remove
+        # completely.
+        # Allow for both `get_placeholder` and `get_placeholder_sql` to
+        # be declared to ease the deprecation process for third-party apps.
+        if (
+            get_placeholder := cls.__dict__.get("get_placeholder")
+        ) is not None and "get_placeholder_sql" not in cls.__dict__:
+            warnings.warn(
+                "Field.get_placeholder is deprecated in favor of get_placeholder_sql. "
+                f"Define {cls.__module__}.{cls.__qualname__}.get_placeholder_sql "
+                "to return both SQL and parameters instead.",
+                category=RemovedInDjango70Warning,
+                skip_file_prefixes=django_file_prefixes(),
+            )
+
+            def get_placeholder_sql(self, value, compiler, connection):
+                placeholder = get_placeholder(self, value, compiler, connection)
+                if hasattr(value, "as_sql"):
+                    sql, params = compiler.compile(value)
+                    return placeholder % sql, params
+                return placeholder, (value,)
+
+            setattr(cls, "get_placeholder_sql", get_placeholder_sql)
+        return super().__init_subclass__(**kwargs)
 
     def __init__(
         self,
@@ -2735,8 +2762,8 @@ class BinaryField(Field):
     def get_internal_type(self):
         return "BinaryField"
 
-    def get_placeholder(self, value, compiler, connection):
-        return connection.ops.binary_placeholder_sql(value)
+    def get_placeholder_sql(self, value, compiler, connection):
+        return connection.ops.binary_placeholder_sql(value, compiler)
 
     def get_default(self):
         if self.has_default() and not callable(self.default):
