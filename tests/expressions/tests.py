django/django

commit b8e5a8a9a2a767f584cbe89a878a42363706f939

Fixed #36404 -- Fixed Aggregate.filter using OuterRef.
Regression in a76035e925ff4e6d8676c65cb135c74b993b1039.
Thank you to Simon Charette for the review.

co-authored-by: Simon Charette <charette.s@gmail.com>

tests/aggregation/test_filter_argument.py
@@ -84,6 +84,10 @@ class FilteredAggregateTests(TestCase):
                     Author.objects.aggregate(age=agg)["age"], expected_result
                 )
 
+    def test_empty_filtered_aggregates(self):
+        agg = Count("pk", filter=Q())
+        self.assertEqual(Author.objects.aggregate(count=agg)["count"], 3)
+
     def test_double_filtered_aggregates(self):
         agg = Sum("age", filter=Q(Q(name="test2") & ~Q(name="test")))
         self.assertEqual(Author.objects.aggregate(age=agg)["age"], 60)
@@ -181,6 +185,23 @@ class FilteredAggregateTests(TestCase):
             )
         )
         self.assertEqual(aggregate, {"max_rating": 4.5})
+
+    def test_filtered_aggregrate_ref_in_subquery_annotation(self):
+        aggs = (
+            Author.objects.annotate(
+                count=Subquery(
+                    Book.objects.annotate(
+                        weird_count=Count(
+                            "pk",
+                            filter=Q(pages=OuterRef("age")),
+                        )
+                    ).values("weird_count")[:1]
+                ),
+            )
+            .order_by("pk")
+            .aggregate(sum=Sum("count"))
+        )
+        self.assertEqual(aggs["sum"], 0)
 
     def test_filtered_aggregate_on_exists(self):
         aggregate = Book.objects.values("publisher").aggregate(

django/db/models/aggregates.py
@@ -95,7 +95,7 @@ class Aggregate(Func):
             raise TypeError(f"{self.__class__.__name__} does not allow default.")
 
         self.distinct = distinct
-        self.filter = filter and AggregateFilter(filter)
+        self.filter = None if filter is None else AggregateFilter(filter)
         self.default = default
         self.order_by = AggregateOrderBy.from_param(
             f"{self.__class__.__name__}.order_by", order_by
@@ -120,11 +120,6 @@ class Aggregate(Func):
     ):
         # Aggregates are not allowed in UPDATE queries, so ignore for_save
         c = super().resolve_expression(query, allow_joins, reuse, summarize)
-        c.filter = (
-            c.filter.resolve_expression(query, allow_joins, reuse, summarize)
-            if c.filter
-            else None
-        )
         c.order_by = (
             c.order_by.resolve_expression(query, allow_joins, reuse, summarize)
             if c.order_by

commit 9b6ccbe8bc80f17f0036bc8b2b599d94ac79d289

Fixed #36751 -- Fixed empty filtered aggregation crash over annotated queryset.
Regression in b8e5a8a9a2a767f584cbe89a878a42363706f939.

Refs #36404.

The replace_expressions method was innapropriately dealing with falsey but not
None source expressions causing them to also be potentially evaluated when
__bool__ was invoked (e.g. QuerySet.__bool__ evaluates the queryset).

The changes introduced in b8e5a8a9a2, which were to deal with a similar issue,
surfaced the problem as aggregation over an annotated queryset requires an
inlining (or pushdown) of aggregate references which is achieved through
replace_expressions.

In cases where an empty Q object was provided as an aggregate filter, such
as when the admin facetting feature was used as reported, it would wrongly be
turned into None, instead of an empty WhereNode, causing a crash at aggregate
filter compilation.

Note that the crash signature differed depending on whether or not the backend
natively supports aggregate filtering (supports_aggregate_filter_clause) as
the fallback, which makes use Case / When expressions, would result in a
TypeError instead of a NoneType AttributeError.

Thanks Rafael Urben for the report, Antoliny and Youngkwang Yang for the triage.

django/db/models/expressions.py
@@ -426,7 +426,7 @@ class BaseExpression:
         clone = self.copy()
         clone.set_source_expressions(
             [
-                expr.replace_expressions(replacements) if expr else None
+                None if expr is None else expr.replace_expressions(replacements)
                 for expr in source_expressions
             ]
         )

tests/aggregation/test_filter_argument.py
@@ -88,6 +88,17 @@ class FilteredAggregateTests(TestCase):
         agg = Count("pk", filter=Q())
         self.assertEqual(Author.objects.aggregate(count=agg)["count"], 3)
 
+    def test_empty_filtered_aggregates_with_annotation(self):
+        agg = Count("pk", filter=Q())
+        self.assertEqual(
+            Author.objects.annotate(
+                age_annotation=F("age"),
+            ).aggregate(
+                count=agg
+            )["count"],
+            3,
+        )
+
     def test_double_filtered_aggregates(self):
         agg = Sum("age", filter=Q(Q(name="test2") & ~Q(name="test")))
         self.assertEqual(Author.objects.aggregate(age=agg)["age"], 60)

tests/expressions/tests.py
@@ -1571,6 +1571,24 @@ class SimpleExpressionTests(SimpleTestCase):
         with self.assertRaisesMessage(ValueError, msg):
             expression.get_expression_for_validation()
 
+    def test_replace_expressions_falsey(self):
+        class AssignableExpression(Expression):
+            def __init__(self, *source_expressions):
+                super().__init__()
+                self.set_source_expressions(list(source_expressions))
+
+            def get_source_expressions(self):
+                return self.source_expressions
+
+            def set_source_expressions(self, exprs):
+                self.source_expressions = exprs
+
+        expression = AssignableExpression()
+        falsey = Q()
+        expression.set_source_expressions([falsey])
+        replaced = expression.replace_expressions({"replacement": Expression()})
+        self.assertEqual(replaced.get_source_expressions(), [falsey])
+
 
 class ExpressionsNumericTests(TestCase):
     @classmethod