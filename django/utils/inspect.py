34186e731ca20a2344b1f88fd543a854d6b13a00
Jacob Walls <jacobtylerwalls@gmail.com>
2025-11-29 18:45:39 -0500

    Fixed #36712 -- Evaluated type annotations lazily in template tag registration.
    Ideally, this will be reverted when an upstream solution is available for
    https://github.com/python/cpython/issues/141560.
    
    Thanks Patrick Rauscher for the report and Augusto Pontes for the
    first iteration and test.

--- django/utils/inspect.py
+++ django/utils/inspect.py
@@ -1,10 +1,18 @@
 import functools
 import inspect
+import threading
+from contextlib import contextmanager
 
 from django.utils.version import PY314
 
 if PY314:
     import annotationlib
+
+    lock = threading.Lock()
+    safe_signature_from_callable = functools.partial(
+        inspect._signature_from_callable,
+        annotation_format=annotationlib.Format.FORWARDREF,
+    )
 
 
 @functools.lru_cache(maxsize=512)
@@ -98,3 +106,27 @@ def is_module_level_function(func):
         return False
 
     return True
+
+
+@contextmanager
+def lazy_annotations():
+    """
+    inspect.getfullargspec eagerly evaluates type annotations. To add
+    compatibility with Python 3.14+ deferred evaluation, patch the module-level
+    helper to provide the annotation_format that we are using elsewhere.
+
+    This private helper could be removed when there is an upstream solution for
+    https://github.com/python/cpython/issues/141560.
+
+    This context manager is not reentrant.
+    """
+    if not PY314:
+        yield
+        return
+    with lock:
+        original_helper = inspect._signature_from_callable
+        inspect._signature_from_callable = safe_signature_from_callable
+        try:
+            yield
+        finally:
+            inspect._signature_from_callable = original_helper
