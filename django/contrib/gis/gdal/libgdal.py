c02a71e66c8bbd7bc26b23642eea0d792d319da3
Aditya Chopra <nytc120605@gmail.com>
2025-12-02 00:42:35 +0530

    Modernize GDAL version parsing regex by bounding optional subminor group ($) – behavior-compatible refactor

--- django/contrib/gis/gdal/libgdal.py
+++ django/contrib/gis/gdal/libgdal.py
@@ -111,9 +111,9 @@ def gdal_full_version():
 
 def gdal_version_info():
     ver = gdal_version()
-    m = re.match(rb"^(?P<major>\d+)\.(?P<minor>\d+)(?:\.(?P<subminor>\d+))?", ver)
+    m = re.match(rb"^(?P<major>\d+)\.(?P<minor>\d+)(?:\.(?P<subminor>\d+))?$", ver)
     if not m:
-        raise GDALException('Could not parse GDAL version string "%s"' % ver)
+        raise GDALException(f'Could not parse GDAL version string "{ver}"')
     major, minor, subminor = m.groups()
     return (int(major), int(minor), subminor and int(subminor))
 
