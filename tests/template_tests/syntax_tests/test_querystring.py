fb50a33a7f6a0c23cc1ce34d05380985bd2e85f5
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2025-12-08 09:42:43 -0500

    Reproduced querystring bug with failing test.
    Assert that lists should be serialized by `{% querysting %}`.
    
    Ref ticket-36783.

--- tests/template_tests/syntax_tests/test_querystring.py
+++ tests/template_tests/syntax_tests/test_querystring.py
@@ -58,6 +58,13 @@ class QueryStringTagTests(SimpleTestCase):
         context = RequestContext(request)
         self.assertRenderEqual("querystring_multiple", context, expected="?x=y&amp;a=b")
 
+    @setup({"querystring_multiple_lists": "{% querystring %}"})
+    def test_querystring_multiple_lists(self):
+        request = self.request_factory.get("/", {"x": ["y", "z"], "a": ["b", "c"]})
+        context = RequestContext(request)
+        expected = "?x=y&amp;x=z&amp;a=b&amp;a=c"
+        self.assertRenderEqual("querystring_multiple_lists", context, expected=expected)
+
     @setup({"querystring_empty_params": "{% querystring qd %}"})
     def test_querystring_empty_params(self):
         cases = [{}, QueryDict()]
