33a0a6f033b9243e0cc98e76bd494de73cf144eb
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2025-12-12 23:17:38 +0000

    Fixed #36791 -- Made MigrationAutodetector recreate through table when m2m target model changes.
    Co-Authored-By: Jacob Walls <38668450+jacobtylerwalls@users.noreply.github.com>
    Co-Authored-By: Clifford Gama <cliffygamy@gmail.com>

--- tests/migrations/test_autodetector.py
+++ tests/migrations/test_autodetector.py
@@ -741,6 +741,13 @@ class AutodetectorTests(BaseAutodetectorTests):
             ("publishers", models.ManyToManyField("testapp.Publisher", blank=True)),
         ],
     )
+    other_publisher = ModelState(
+        "testapp",
+        "OtherPublisher",
+        [
+            ("id", models.AutoField(primary_key=True)),
+        ],
+    )
     author_with_m2m_through = ModelState(
         "testapp",
         "Author",
@@ -4556,6 +4563,46 @@ class AutodetectorTests(BaseAutodetectorTests):
             changes, "testapp", 0, 2, name="publishers", model_name="author"
         )
         self.assertOperationFieldAttributes(changes, "testapp", 0, 2, max_length=100)
+
+    def test_m2m_target_change_generates_remove_and_add(self):
+        before = [
+            self.publisher,
+            self.other_publisher,
+            self.author_with_m2m,  # m2m to self.publisher.
+        ]
+
+        after = [
+            self.publisher,
+            self.other_publisher,
+            ModelState(
+                "testapp",
+                "Author",
+                [
+                    ("id", models.AutoField(primary_key=True)),
+                    # Repoint m2m to self.other_publisher.
+                    ("publishers", models.ManyToManyField("testapp.OtherPublisher")),
+                ],
+            ),
+        ]
+        changes = self.get_changes(before, after)
+        self.assertNumberMigrations(changes, "testapp", 1)
+        self.assertOperationTypes(changes, "testapp", 0, ["RemoveField", "AddField"])
+        self.assertOperationAttributes(
+            changes,
+            "testapp",
+            0,
+            0,
+            name="publishers",
+            model_name="author",
+        )
+        self.assertOperationAttributes(
+            changes,
+            "testapp",
+            0,
+            1,
+            name="publishers",
+            model_name="author",
+        )
 
     def test_non_circular_foreignkey_dependency_removal(self):
         """
