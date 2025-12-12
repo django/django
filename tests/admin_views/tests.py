fe7896395f1248e7f2d32a83f9459d42211e59c9
antoliny0919 <antoliny0919@gmail.com>
2025-12-12 19:03:03 +0900

    Add test for consecutive whitespace object subtitle on the history page

--- tests/admin_views/tests.py
+++ tests/admin_views/tests.py
@@ -3688,6 +3688,10 @@ class AdminConsecutiveWhiteSpaceObjectDisplayTest(TestCase):
     def test_display_consecutive_whitespace_object_in_sub_title(self):
         response = self.client.get(self.change_link)
         self.assertContains(response, "<h2>-</h2>")
+        response = self.client.get(
+            reverse("admin:admin_views_coverletter_history", args=(self.obj.pk,))
+        )
+        self.assertContains(response, "<h1>Change history: -</h1>")
 
 
 @override_settings(
