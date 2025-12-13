507d32e02d1b28171c984ff20d6cd784aab16abb
farthestmage <arnavkamboj511@gmail.com>
2025-12-13 17:25:08 +0530

    Fix #36700 -- Implemented try-except for the solution

--- django/core/handlers/asgi.py
+++ django/core/handlers/asgi.py
@@ -212,9 +212,12 @@ class ASGIHandler(base.BaseHandler):
 
     async def listen_for_disconnect(self, receive):
         """Listen for disconnect from the client."""
-        message = await receive()
-        if message["type"] == "http.disconnect":
-            raise RequestAborted()
+        try:
+            message = await receive()
+            if message["type"] == "http.disconnect":
+                raise RequestAborted()
+        except RequestAborted:
+            return
         # This should never happen.
         assert False, "Invalid ASGI message after request body: %s" % message["type"]
 
