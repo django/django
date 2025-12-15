8c0d5c83c1e58740fc10373a6bc570516f4fb14a
Krishnaprasad MG <krishna@holvi.com>
2025-12-15 15:39:06 +0100

    Fixed #36487 -- Fixed logger error message with partial callbacks.

--- django/test/testcases.py
+++ django/test/testcases.py
@@ -1485,11 +1485,11 @@ class TestCase(TransactionTestCase):
                             try:
                                 callback()
                             except Exception as e:
-                                logger.error(
-                                    f"Error calling {callback.__qualname__} in "
-                                    f"on_commit() (%s).",
+                                name = getattr(callback, "__qualname__", callback)
+                                logger.exception(
+                                    "Error calling %s in on_commit() (%s).",
+                                    name,
                                     e,
-                                    exc_info=True,
                                 )
                         else:
                             callback()
