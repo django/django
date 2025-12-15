f5b722d3346c96f28cad20d5e53971d567770c77
kundan223 <kundan0707y@gmail.com>
2025-12-15 16:34:12 +0530

    Add tests for lazy route include resolution

--- tests/urlpatterns/test_resolvers.py
+++ tests/urlpatterns/test_resolvers.py
@@ -1,6 +1,7 @@
 from django.test import SimpleTestCase
 from django.test.utils import override_settings
-from django.urls import include, path, resolve
+from django.urls import get_resolver, include, path, resolve
+from django.urls.resolvers import RegexPattern
 from django.utils.translation import gettext_lazy as _
 
 urlpatterns = [
@@ -13,6 +14,29 @@ urlpatterns = [
         ),
     ),
 ]
+
+
+class RegexPatternTests(SimpleTestCase):
+    def test_str(self):
+        self.assertEqual(
+            str(RegexPattern(_("^translated/$"))),
+            "^translated/$",
+        )
+
+
+class ResolverCacheTests(SimpleTestCase):
+    @override_settings(ROOT_URLCONF="urlpatterns.path_urls")
+    def test_resolver_cache_default__root_urlconf(self):
+        # Resolver for the default URLconf (no argument) and for
+        # settings.ROOT_URLCONF should be the same cached object.
+        self.assertIs(
+            get_resolver(),
+            get_resolver("urlpatterns.path_urls"),
+        )
+        self.assertIsNot(
+            get_resolver(),
+            get_resolver("urlpatterns.path_dynamic_urls"),
+        )
 
 
 class LazyRouteIncludeTests(SimpleTestCase):
