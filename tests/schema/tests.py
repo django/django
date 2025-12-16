django/django

commit f9a44cc0fac653f8e0c2ab1cdfb12b2cc5c63fc2

Fixed #35453 -- Made ManyToManyField.concrete False.
ManyToManyField was already excluded from fields, concrete_fields,
and local_concrete_fields in Options.

django/db/backends/base/schema.py
@@ -1660,7 +1660,9 @@ class BaseDatabaseSchemaEditor:
         return output
 
     def _field_should_be_altered(self, old_field, new_field, ignore=None):
-        if not old_field.concrete and not new_field.concrete:
+        if (not (old_field.concrete or old_field.many_to_many)) and (
+            not (new_field.concrete or new_field.many_to_many)
+        ):
             return False
         ignore = ignore or set()
         _, old_path, old_args, old_kwargs = old_field.deconstruct()
@@ -1692,8 +1694,10 @@ class BaseDatabaseSchemaEditor:
         ):
             old_kwargs.pop("db_default")
             new_kwargs.pop("db_default")
-        return self.quote_name(old_field.column) != self.quote_name(
-            new_field.column
+        return (
+            old_field.concrete
+            and new_field.concrete
+            and (self.quote_name(old_field.column) != self.quote_name(new_field.column))
         ) or (old_path, old_args, old_kwargs) != (new_path, new_args, new_kwargs)
 
     def _field_should_be_indexed(self, model, field):

django/db/backends/sqlite3/schema.py
@@ -144,7 +144,7 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
             # Choose a default and insert it into the copy map
             if (
                 not create_field.has_db_default()
-                and not (create_field.many_to_many or create_field.generated)
+                and not create_field.generated
                 and create_field.concrete
             ):
                 mapping[create_field.column] = self.prepare_default(

tests/update/tests.py
@@ -157,43 +157,32 @@ class AdvancedTests(TestCase):
         self.assertEqual(bar_qs[0].foo_id, b_foo.target)
 
     def test_update_m2m_field(self):
-        msg = (
-            "Cannot update model field "
-            "<django.db.models.fields.related.ManyToManyField: m2m_foo> "
-            "(only non-relations and foreign keys permitted)."
-        )
+        rel = "<django.db.models.fields.related.ManyToManyField: m2m_foo>"
+        msg = f"Cannot update model field {rel} (only concrete fields are permitted)."
         with self.assertRaisesMessage(FieldError, msg):
             Bar.objects.update(m2m_foo="whatever")
 
     def test_update_reverse_m2m_descriptor(self):
-        msg = (
-            "Cannot update model field <ManyToManyRel: update.bar> "
-            "(only non-relations and foreign keys permitted)."
-        )
+        rel = "<ManyToManyRel: update.bar>"
+        msg = f"Cannot update model field {rel} (only concrete fields are permitted)."
         with self.assertRaisesMessage(FieldError, msg):
             Foo.objects.update(m2m_foo="whatever")
 
     def test_update_reverse_fk_descriptor(self):
-        msg = (
-            "Cannot update model field <ManyToOneRel: update.bar> "
-            "(only non-relations and foreign keys permitted)."
-        )
+        rel = "<ManyToOneRel: update.bar>"
+        msg = f"Cannot update model field {rel} (only concrete fields are permitted)."
         with self.assertRaisesMessage(FieldError, msg):
             Foo.objects.update(bar="whatever")
 
     def test_update_reverse_o2o_descriptor(self):
-        msg = (
-            "Cannot update model field <OneToOneRel: update.bar> "
-            "(only non-relations and foreign keys permitted)."
-        )
+        rel = "<OneToOneRel: update.bar>"
+        msg = f"Cannot update model field {rel} (only concrete fields are permitted)."
         with self.assertRaisesMessage(FieldError, msg):
             Foo.objects.update(o2o_bar="whatever")
 
     def test_update_reverse_mti_parent_link_descriptor(self):
-        msg = (
-            "Cannot update model field <OneToOneRel: update.uniquenumberchild> "
-            "(only non-relations and foreign keys permitted)."
-        )
+        rel = "<OneToOneRel: update.uniquenumberchild>"
+        msg = f"Cannot update model field {rel} (only concrete fields are permitted)."
         with self.assertRaisesMessage(FieldError, msg):
             UniqueNumber.objects.update(uniquenumberchild="whatever")
 

django/db/models/fields/related.py
@@ -1849,6 +1849,10 @@ class ManyToManyField(RelatedField):
             )
         return name, path, args, kwargs
 
+    def get_attname_column(self):
+        attname, _ = super().get_attname_column()
+        return attname, None
+
     def _get_path_info(self, direct=False, filtered_relation=None):
         """Called by both direct and indirect m2m traversal."""
         int_model = self.remote_field.through

django/db/models/query.py
@@ -725,7 +725,7 @@ class QuerySet(AltersData):
                     "Unique fields that can trigger the upsert must be provided."
                 )
             # Updating primary keys and non-concrete fields is forbidden.
-            if any(not f.concrete or f.many_to_many for f in update_fields):
+            if any(not f.concrete for f in update_fields):
                 raise ValueError(
                     "bulk_create() can only be used with concrete fields in "
                     "update_fields."
@@ -736,7 +736,7 @@ class QuerySet(AltersData):
                     "update_fields."
                 )
             if unique_fields:
-                if any(not f.concrete or f.many_to_many for f in unique_fields):
+                if any(not f.concrete for f in unique_fields):
                     raise ValueError(
                         "bulk_create() can only be used with concrete fields "
                         "in unique_fields."
@@ -916,7 +916,7 @@ class QuerySet(AltersData):
             raise ValueError("All bulk_update() objects must have a primary key set.")
         opts = self.model._meta
         fields = [opts.get_field(name) for name in fields]
-        if any(not f.concrete or f.many_to_many for f in fields):
+        if any(not f.concrete for f in fields):
             raise ValueError("bulk_update() can only be used with concrete fields.")
         all_pk_fields = set(opts.pk_fields)
         for parent in opts.all_parents:

django/db/models/sql/subqueries.py
@@ -92,10 +92,10 @@ class UpdateQuery(Query):
                 raise FieldError(
                     "Composite primary key fields must be updated individually."
                 )
-            if not field.concrete or (field.is_relation and field.many_to_many):
+            if not field.concrete:
                 raise FieldError(
-                    "Cannot update model field %r (only non-relations and "
-                    "foreign keys permitted)." % field
+                    "Cannot update model field %r (only concrete fields are permitted)."
+                    % field
                 )
             if model is not self.get_meta().concrete_model:
                 self.add_related_update(model, field, val)

tests/composite_pk/test_update.py
@@ -172,7 +172,7 @@ class CompositePKUpdateTests(TestCase):
     def test_cant_update_relation(self):
         msg = (
             "Cannot update model field <django.db.models.fields.related.ForeignObject: "
-            "user> (only non-relations and foreign keys permitted)"
+            "user> (only concrete fields are permitted)"
         )
 
         with self.assertRaisesMessage(FieldError, msg):

tests/model_fields/test_field_flags.py
@@ -6,6 +6,7 @@ from .models import AllFieldsModel
 
 NON_CONCRETE_FIELDS = (
     models.ForeignObject,
+    models.ManyToManyField,
     GenericForeignKey,
     GenericRelation,
 )
@@ -209,7 +210,7 @@ class FieldFlagsTests(test.SimpleTestCase):
     def test_model_and_reverse_model_should_equal_on_relations(self):
         for field in AllFieldsModel._meta.get_fields():
             is_concrete_forward_field = field.concrete and field.related_model
-            if is_concrete_forward_field:
+            if is_concrete_forward_field or field.many_to_many:
                 reverse_field = field.remote_field
                 self.assertEqual(field.model, reverse_field.related_model)
                 self.assertEqual(field.related_model, reverse_field.model)

commit 1b990d4023e174f6ad7fa7117072f944f0dc1811

Fixed #36800 -- Restored ManyToManyField renaming in BaseDatabaseSchemaEditor.alter_field().
Regression in f9a44cc0fac653f8e0c2ab1cdfb12b2cc5c63fc2.

Now that ManyToManyField is no longer concrete the decision of whether or not
it should be altered, which is also relied on by field renaming, should take
into consideration name changes even if it doesn't have a column associated
with it, as auto-created many-to-many relationship table names are a base of it.

Note that there is room for optimization here where a rename can be entirely
avoided if ManyToManyField.db_table remains stable between .name changes, just
like we do with Field.db_column remaining stable, but since this is a
regression and meant to be backported the current patch focuses on correctness
over further improvements.

Thanks Josik for the report.

Co-authored-by: Simon Charette <charette.s@gmail.com>

django/db/backends/base/schema.py
@@ -1690,7 +1690,7 @@ class BaseDatabaseSchemaEditor:
         _, old_path, old_args, old_kwargs = old_field.deconstruct()
         _, new_path, new_args, new_kwargs = new_field.deconstruct()
         # Don't alter when:
-        # - changing only a field name
+        # - changing only a field name (unless it's a many-to-many)
         # - changing an attribute that doesn't affect the schema
         # - changing an attribute in the provided set of ignored attributes
         # - adding only a db_column and the column name is not changed
@@ -1708,7 +1708,7 @@ class BaseDatabaseSchemaEditor:
         ):
             old_kwargs.pop("to", None)
             new_kwargs.pop("to", None)
-        # db_default can take many form but result in the same SQL.
+        # db_default can take many forms but result in the same SQL.
         if (
             old_kwargs.get("db_default")
             and new_kwargs.get("db_default")
@@ -1716,11 +1716,19 @@ class BaseDatabaseSchemaEditor:
         ):
             old_kwargs.pop("db_default")
             new_kwargs.pop("db_default")
-        return (
+        if (
             old_field.concrete
             and new_field.concrete
             and (self.quote_name(old_field.column) != self.quote_name(new_field.column))
-        ) or (old_path, old_args, old_kwargs) != (new_path, new_args, new_kwargs)
+        ):
+            return True
+        if (
+            old_field.many_to_many
+            and new_field.many_to_many
+            and old_field.name != new_field.name
+        ):
+            return True
+        return (old_path, old_args, old_kwargs) != (new_path, new_args, new_kwargs)
 
     def _field_should_be_indexed(self, model, field):
         return field.db_index and not field.unique

tests/schema/tests.py
@@ -2889,6 +2889,40 @@ class SchemaTests(TransactionTestCase):
     def test_m2m_repoint_inherited(self):
         self._test_m2m_repoint(InheritedManyToManyField)
 
+    def test_m2m_rename(self):
+        class LocalBook(Model):
+            authors = ManyToManyField("schema.Author")
+
+            class Meta:
+                app_label = "schema"
+                apps = new_apps
+
+        self.local_models = [LocalBook]
+        with connection.schema_editor() as editor:
+            editor.create_model(Author)
+            editor.create_model(LocalBook)
+        old_field = LocalBook._meta.get_field("authors")
+        new_field = ManyToManyField("schema.Author")
+        new_field.contribute_to_class(LocalBook, "writers")
+        with connection.schema_editor() as editor:
+            editor.alter_field(LocalBook, old_field, new_field, strict=True)
+        # Ensure old M2M is gone.
+        with self.assertRaises(DatabaseError):
+            self.column_classes(
+                LocalBook._meta.get_field("authors").remote_field.through
+            )
+        if connection.features.supports_foreign_keys:
+            self.assertForeignKeyExists(
+                new_field.remote_field.through,
+                "author_id",
+                "schema_author",
+            )
+        new_through_table = new_field.remote_field.through._meta.db_table
+        self.assertIn("writers", new_through_table)
+        self.assertNotIn("authors", new_through_table)
+        # Remove the old field from meta for tearDown().
+        LocalBook._meta.local_many_to_many.remove(old_field)
+
     @isolate_apps("schema")
     def test_m2m_rename_field_in_target_model(self):
         class LocalTagM2MTest(Model):