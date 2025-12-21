16ae1e8cc5d7b5373360018bc8288fcaa67fcfd4
MehrazRumman <stardark394@gmail.com>
2025-12-22 00:07:57 +0600

    fix tests

--- tests/postgres_tests/test_array.py
+++ tests/postgres_tests/test_array.py
@@ -831,15 +831,11 @@ class TestOtherTypesExactQuerying(PostgreSQLTestCase):
 @isolate_apps("postgres_tests")
 class TestChecks(PostgreSQLSimpleTestCase):
     def test_field_checks(self):
-        class MyModel(PostgreSQLModel):
-            field = ArrayField(models.CharField(max_length=-1))
+        msg = "'max_length' must be a positive integer."
+        with self.assertRaisesMessage(ValueError, msg):
 
-        model = MyModel()
-        errors = model.check()
-        self.assertEqual(len(errors), 1)
-        # The inner CharField has a non-positive max_length.
-        self.assertEqual(errors[0].id, "postgres.E001")
-        self.assertIn("max_length", errors[0].msg)
+            class MyModel(PostgreSQLModel):
+                field = ArrayField(models.CharField(max_length=-1))
 
     def test_base_field_check_kwargs(self):
         passed_kwargs = None
@@ -911,15 +907,11 @@ class TestChecks(PostgreSQLSimpleTestCase):
         Nested ArrayFields are permitted.
         """
 
-        class MyModel(PostgreSQLModel):
-            field = ArrayField(ArrayField(models.CharField(max_length=-1)))
+        msg = "'max_length' must be a positive integer."
+        with self.assertRaisesMessage(ValueError, msg):
 
-        model = MyModel()
-        errors = model.check()
-        self.assertEqual(len(errors), 1)
-        # The inner CharField has a non-positive max_length.
-        self.assertEqual(errors[0].id, "postgres.E001")
-        self.assertIn("max_length", errors[0].msg)
+            class MyModel(PostgreSQLModel):
+                field = ArrayField(ArrayField(models.CharField(max_length=-1)))
 
     def test_choices_tuple_list(self):
         class MyModel(PostgreSQLModel):
