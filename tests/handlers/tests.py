87d2065d901d6eccede0a5d73ece1a9e457f4400
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2025-12-21 22:55:43 +0300

    Enable UP012

--- tests/handlers/tests.py
+++ tests/handlers/tests.py
@@ -62,7 +62,7 @@ class HandlerTests(SimpleTestCase):
         Non-ASCII cookies set in JavaScript are properly decoded (#20557).
         """
         environ = self.request_factory.get("/").environ
-        raw_cookie = 'want="café"'.encode("utf-8").decode("iso-8859-1")
+        raw_cookie = 'want="café"'.encode().decode("iso-8859-1")
         environ["HTTP_COOKIE"] = raw_cookie
         request = WSGIRequest(environ)
         self.assertEqual(request.COOKIES["want"], "café")
