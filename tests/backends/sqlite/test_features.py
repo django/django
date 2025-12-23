django/django

commit 358fd21c47cdf7bda520ce73c5cfd82bba57827b

Fixed #36143 -- Made max_query_params respect SQLITE_LIMIT_VARIABLE_NUMBER.
Co-authored-by: Xavier Frankline <xf.xavierfrank@gmail.com>

django/db/backends/sqlite3/features.py
@@ -1,4 +1,5 @@
 import operator
+import sqlite3
 
 from django.db import transaction
 from django.db.backends.base.features import BaseDatabaseFeatures
@@ -13,7 +14,6 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     test_db_allows_multiple_connections = False
     supports_unspecified_pk = True
     supports_timezones = False
-    max_query_params = 999
     supports_transactions = True
     atomic_transactions = False
     can_rollback_ddl = True
@@ -139,6 +139,16 @@ class DatabaseFeatures(BaseDatabaseFeatures):
             "GenericIPAddressField": "CharField",
             "SmallAutoField": "AutoField",
         }
+
+    @property
+    def max_query_params(self):
+        """
+        SQLite has a variable limit per query. The limit can be changed using
+        the SQLITE_MAX_VARIABLE_NUMBER compile-time option (which defaults to
+        999 in versions < 3.32.0 or 32766 in newer versions) or lowered per
+        connection at run-time with setlimit(SQLITE_LIMIT_VARIABLE_NUMBER, N).
+        """
+        return self.connection.connection.getlimit(sqlite3.SQLITE_LIMIT_VARIABLE_NUMBER)
 
     @cached_property
     def supports_json_field(self):

tests/backends/sqlite/test_features.py
@@ -1,3 +1,4 @@
+import sqlite3
 from unittest import mock, skipUnless
 
 from django.db import OperationalError, connection
@@ -17,3 +18,14 @@ class FeaturesTests(TestCase):
         ):
             with self.assertRaisesMessage(OperationalError, msg):
                 connection.features.supports_json_field
+
+    def test_max_query_params_respects_variable_limit(self):
+        limit_name = sqlite3.SQLITE_LIMIT_VARIABLE_NUMBER
+        current_limit = connection.features.max_query_params
+        new_limit = min(42, current_limit)
+        try:
+            connection.connection.setlimit(limit_name, new_limit)
+            self.assertEqual(connection.features.max_query_params, new_limit)
+        finally:
+            connection.connection.setlimit(limit_name, current_limit)
+        self.assertEqual(connection.features.max_query_params, current_limit)

django/db/backends/sqlite3/operations.py
@@ -30,8 +30,8 @@ class DatabaseOperations(BaseDatabaseOperations):
 
     def bulk_batch_size(self, fields, objs):
         """
-        SQLite has a compile-time default (SQLITE_LIMIT_VARIABLE_NUMBER) of
-        999 variables per query.
+        SQLite has a variable limit defined by SQLITE_LIMIT_VARIABLE_NUMBER
+        (reflected in max_query_params).
 
         If there's only a single field to insert, the limit is 500
         (SQLITE_MAX_COMPOUND_SELECT).

tests/backends/sqlite/test_operations.py
@@ -1,3 +1,4 @@
+import sqlite3
 import unittest
 
 from django.core.management.color import no_style
@@ -107,4 +108,33 @@ class SQLiteOperationsTests(TestCase):
                 [composite_pk, first_name_field], [Person()]
             ),
             connection.features.max_query_params // 3,
+        )
+
+    def test_bulk_batch_size_respects_variable_limit(self):
+        first_name_field = Person._meta.get_field("first_name")
+        last_name_field = Person._meta.get_field("last_name")
+        limit_name = sqlite3.SQLITE_LIMIT_VARIABLE_NUMBER
+        current_limit = connection.features.max_query_params
+        self.assertEqual(
+            connection.ops.bulk_batch_size(
+                [first_name_field, last_name_field], [Person()]
+            ),
+            current_limit // 2,
+        )
+        new_limit = min(42, current_limit)
+        try:
+            connection.connection.setlimit(limit_name, new_limit)
+            self.assertEqual(
+                connection.ops.bulk_batch_size(
+                    [first_name_field, last_name_field], [Person()]
+                ),
+                new_limit // 2,
+            )
+        finally:
+            connection.connection.setlimit(limit_name, current_limit)
+        self.assertEqual(
+            connection.ops.bulk_batch_size(
+                [first_name_field, last_name_field], [Person()]
+            ),
+            current_limit // 2,
         )

commit cf041f692ecafe5ce028fad27ed1954806dd5ed4

Fixed #36818 -- Ensured SQLite connection before accessing max_query_params.
Regression in 358fd21c47cdf7bda520ce73c5cfd82bba57827b.

django/db/backends/sqlite3/features.py
@@ -157,6 +157,7 @@ class DatabaseFeatures(BaseDatabaseFeatures):
         32766) or lowered per connection at run-time with
         setlimit(SQLITE_LIMIT_VARIABLE_NUMBER, N).
         """
+        self.connection.ensure_connection()
         return self.connection.connection.getlimit(sqlite3.SQLITE_LIMIT_VARIABLE_NUMBER)
 
     @cached_property

tests/backends/sqlite/test_features.py
@@ -1,3 +1,4 @@
+import copy
 import sqlite3
 from unittest import mock, skipUnless
 
@@ -29,3 +30,13 @@ class FeaturesTests(TestCase):
         finally:
             connection.connection.setlimit(limit_name, current_limit)
         self.assertEqual(connection.features.max_query_params, current_limit)
+
+    def test_max_query_params_without_established_connection(self):
+        new_connection = connection.copy()
+        new_connection.settings_dict = copy.deepcopy(connection.settings_dict)
+        self.assertIsNone(new_connection.connection)
+        try:
+            result = new_connection.features.max_query_params
+            self.assertIsInstance(result, int)
+        finally:
+            new_connection._close()