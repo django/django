030c63d329c4814da221528e823a4aaaaa40e4f1
Jacob Walls <jacobtylerwalls@gmail.com>
2026-01-05 13:58:58 -0500

    Fixed #36843, #36793 -- Reverted "Fixed #27489 -- Renamed permissions upon model renaming in migrations."
    This reverts commits f02b49d2f3bf84f5225de920ca510149f1f9f1da and 6e89271a8507fe272d11814975500a1b40303a04.

--- django/contrib/auth/apps.py
+++ django/contrib/auth/apps.py
@@ -1,12 +1,12 @@
 from django.apps import AppConfig
 from django.core import checks
 from django.db.models.query_utils import DeferredAttribute
-from django.db.models.signals import post_migrate, pre_migrate
+from django.db.models.signals import post_migrate
 from django.utils.translation import gettext_lazy as _
 
 from . import get_user_model
 from .checks import check_middleware, check_models_permissions, check_user_model
-from .management import create_permissions, rename_permissions
+from .management import create_permissions
 from .signals import user_logged_in
 
 
@@ -20,11 +20,6 @@ class AuthConfig(AppConfig):
             create_permissions,
             dispatch_uid="django.contrib.auth.management.create_permissions",
         )
-        pre_migrate.connect(
-            rename_permissions,
-            dispatch_uid="django.contrib.auth.management.rename_permissions",
-        )
-
         last_login_field = getattr(get_user_model(), "last_login", None)
         # Register the handler only if UserModel.last_login is a field.
         if isinstance(last_login_field, DeferredAttribute):
