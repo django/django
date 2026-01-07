a2231d7bdf56ec25c56e628d9d8e6f30d4d1d21a
Moksha Choksi <choksimoksha92@gmail.com>
2026-01-07 15:24:10 +0530

    Adjust Postgres Left negative-length tests to match actual PostgreSQL behavior

--- tests/db_functions/text/test_left.py
+++ tests/db_functions/text/test_left.py
@@ -39,7 +39,7 @@ class LeftTests(TestCase):
         authors = Author.objects.annotate(name_part=Left("name", -5))
         self.assertQuerySetEqual(
             authors.order_by("name"),
-            ["John ", "Rh"],
+            ["John ", "R"],
             lambda a: a.name_part,
         )
 
