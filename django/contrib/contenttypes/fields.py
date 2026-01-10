54ad38b29fd81144e2b42a7b537c5b7ad4bc8a75
Keryn Knight <keryn@kerynknight.com>
2026-01-10 08:39:45 +0100

    Refs #28455 -- Avoided QuerySet cloning in simple prefetch_related() usages.
    manager.get_queryset() always returns freshly instantiated per-instance
    QuerySet which doesn't need subsequent cloning.
    
    Based on work originally done by Anssi Kääriäinen and Tim Graham.

--- django/contrib/contenttypes/fields.py
+++ django/contrib/contenttypes/fields.py
@@ -651,11 +651,12 @@ def create_generic_related_manager(superclass, rel):
             Filter the queryset for the instance this manager is bound to.
             """
             db = self._db or router.db_for_read(self.model, instance=self.instance)
-            return (
-                queryset.using(db)
-                .fetch_mode(self.instance._state.fetch_mode)
-                .filter(**self.core_filters)
-            )
+            with queryset._avoid_cloning():
+                return (
+                    queryset.using(db)
+                    .fetch_mode(self.instance._state.fetch_mode)
+                    .filter(**self.core_filters)
+                )
 
         def _remove_prefetched_objects(self):
             try:
