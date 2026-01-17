377d09a5840af78f09d58545eec2b3a44f14ba90
JaeHyuck Sa <wogur981208@gmail.com>
2026-01-18 02:39:05 +0900

    Fixed #36246 -- Caught GDALException in BaseGeometryWidget.deserialize.
    Signed-off-by: JaeHyuck Sa <wogur981208@gmail.com>

--- django/contrib/gis/forms/fields.py
+++ django/contrib/gis/forms/fields.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.contrib.gis.gdal import GDALException
 from django.contrib.gis.geos import GEOSException, GEOSGeometry
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
@@ -41,10 +40,7 @@ class GeometryField(forms.Field):
 
         if not isinstance(value, GEOSGeometry):
             if hasattr(self.widget, "deserialize"):
-                try:
-                    value = self.widget.deserialize(value)
-                except GDALException:
-                    value = None
+                value = self.widget.deserialize(value)
             else:
                 try:
                     value = GEOSGeometry(value)
