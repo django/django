9a436621e6e3feeb009107f71a5f05fe27abb587
Alok Raj <alokwork2016@gmail.com>
2026-01-17 22:24:40 +0530

    Fix the error missing 1 positional argument

--- tests/model_options/test_tablespaces.py
+++ tests/model_options/test_tablespaces.py
@@ -16,7 +16,7 @@ from .models.tablespaces import (
 )
 
 
-def sql_for_table(self, model):
+def sql_for_table(model):
     with connection.schema_editor(collect_sql=True) as editor:
         editor.create_model(model)
     return editor.collected_sql[0]
