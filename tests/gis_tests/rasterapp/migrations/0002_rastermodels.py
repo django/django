1d7e9a24ddec95e475b40c99c873f4061ef3e8cf
Skyiesac <jainsachi1202@gmail.com>
2026-01-19 12:43:50 -0500

    Refs #36639 -- Updated test migrations to match models.

--- tests/gis_tests/rasterapp/migrations/0002_rastermodels.py
+++ tests/gis_tests/rasterapp/migrations/0002_rastermodels.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
             fields=[
                 (
                     "id",
-                    models.AutoField(
+                    models.BigAutoField(
                         auto_created=True,
                         primary_key=True,
                         serialize=False,
@@ -49,7 +49,7 @@ class Migration(migrations.Migration):
             fields=[
                 (
                     "id",
-                    models.AutoField(
+                    models.BigAutoField(
                         auto_created=True,
                         primary_key=True,
                         serialize=False,
