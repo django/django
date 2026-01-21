69065ca869b0970dff8fdd8fafb390bf8b3bf222
Jacob Walls <jacobtylerwalls@gmail.com>
2026-01-21 17:53:52 -0500

    Fixed CVE-2026-1312 -- Protected order_by() from SQL injection via aliases with periods.
    Before, `order_by()` treated a period in a field name as a sign that it
    was requested via `.extra(order_by=...)` and thus should be passed
    through as raw table and column names, even if `extra()` was not used.
    Since periods are permitted in aliases, this meant user-controlled
    aliases could force the `order_by()` clause to resolve to a raw table
    and column pair instead of the actual target field for the alias.
    
    In practice, only `FilteredRelation` was affected, as the other
    expressions we tested, e.g. `F`, aggressively optimize away the ordering
    expressions into ordinal positions, e.g. ORDER BY 2, instead of ORDER BY
    "table".column.
    
    Thanks Solomon Kebede for the report, and Simon Charette and Jake Howard
    for reviews.

--- tests/ordering/tests.py
+++ tests/ordering/tests.py
@@ -8,6 +8,7 @@ from django.db.models import (
     Count,
     DateTimeField,
     F,
+    FilteredRelation,
     IntegerField,
     Max,
     OrderBy,
@@ -17,6 +18,7 @@ from django.db.models import (
     When,
 )
 from django.db.models.functions import Length, Upper
+from django.db.utils import DatabaseError
 from django.test import TestCase
 
 from .models import (
@@ -394,6 +396,29 @@ class OrderingTests(TestCase):
             ],
             attrgetter("headline"),
         )
+
+    def test_alias_with_period_shadows_table_name(self):
+        """
+        Aliases with periods are not confused for table names from extra().
+        """
+        Article.objects.update(author=self.author_2)
+        Article.objects.create(
+            headline="Backdated", pub_date=datetime(1900, 1, 1), author=self.author_1
+        )
+        crafted = "ordering_article.pub_date"
+
+        qs = Article.objects.annotate(**{crafted: F("author")}).order_by("-" + crafted)
+        self.assertNotEqual(qs[0].headline, "Backdated")
+
+        relation = FilteredRelation("author")
+        qs2 = Article.objects.annotate(**{crafted: relation}).order_by(crafted)
+        with self.assertRaises(DatabaseError):
+            # Before, unlike F(), which causes ordering expressions to be
+            # replaced by ordinals like n in ORDER BY n, these were ordered by
+            # pub_date instead of author.
+            # The Article model orders by -pk, so sorting on author will place
+            # first any article by author2 instead of the backdated one.
+            self.assertNotEqual(qs2[0].headline, "Backdated")
 
     def test_order_by_pk(self):
         """
