f9bfb5279892dba27345270a6b7198c12571ffb7
Jacob Walls <jacobtylerwalls@gmail.com>
2026-01-23 15:10:31 -0500

    [6.0.x] Fixed #36788 -- Fixed horizontal form field alignment under <fieldset> in the admin.
    Thanks Antoliny for the review.
    
    Regression in 4187da258fe212d494cb578a0bc2b52c4979ab95.
    
    Backport of b665a67d61a8a4fd2ad85aeb77282bc49541723f from main.

--- tests/admin_views/admin.py
+++ tests/admin_views/admin.py
@@ -1193,6 +1193,18 @@ class CamelCaseAdmin(admin.ModelAdmin):
 
 class CourseAdmin(admin.ModelAdmin):
     radio_fields = {"difficulty": admin.VERTICAL}
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    ("title", "difficulty"),
+                    ("materials", "start_datetime"),
+                    ("categories"),
+                ),
+            },
+        ),
+    )
 
 
 site = admin.AdminSite(name="admin")
--- tests/admin_views/tests.py
+++ tests/admin_views/tests.py
@@ -6919,10 +6919,10 @@ class SeleniumTests(AdminSeleniumTestCase):
             title="Django Class", materials="django_documents"
         )
         expected_legend_tags_text = [
-            "Materials:",
             "Difficulty:",
-            "Categories:",
+            "Materials:",
             "Start datetime:",
+            "Categories:",
         ]
         url = reverse("admin:admin_views_course_change", args=(course.pk,))
         self.selenium.get(self.live_server_url + url)
@@ -6932,6 +6932,29 @@ class SeleniumTests(AdminSeleniumTestCase):
         for index, fieldset in enumerate(fieldsets):
             legend = fieldset.find_element(By.TAG_NAME, "legend")
             self.assertEqual(legend.text, expected_legend_tags_text[index])
+
+    @screenshot_cases(["desktop_size", "mobile_size", "rtl", "dark", "high_contrast"])
+    def test_use_fieldset_with_grouped_fields(self):
+        from selenium.webdriver.common.by import By
+
+        self.admin_login(
+            username="super", password="secret", login_url=reverse("admin:index")
+        )
+        self.selenium.get(
+            self.live_server_url + reverse("admin:admin_views_course_add")
+        )
+        multiline = self.selenium.find_element(
+            By.CSS_SELECTOR, "#content-main .field-difficulty, .form-multiline"
+        )
+        # Two field boxes.
+        field_boxes = multiline.find_elements(By.CSS_SELECTOR, "div > div.fieldBox")
+        self.assertEqual(len(field_boxes), 2)
+        # One of them is under a <fieldset>.
+        under_fieldset = multiline.find_elements(
+            By.CSS_SELECTOR, "fieldset > div > div.fieldBox"
+        )
+        self.assertEqual(len(under_fieldset), 1)
+        self.take_screenshot("horizontal_fieldset")
 
     @screenshot_cases(["desktop_size", "mobile_size", "rtl", "dark", "high_contrast"])
     @override_settings(MESSAGE_LEVEL=10)
