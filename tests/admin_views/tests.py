8408f170910b190eff7e1d478175e1d697110f01
jafarkhan83 <jafarkhan.master@gmail.com>
2026-01-27 08:17:41 +0500

    trailing whitespace removed

--- tests/admin_views/tests.py
+++ tests/admin_views/tests.py
@@ -9326,7 +9326,7 @@ class ListEditableAccessibilityTests(TestCase):
     def test_aria_labelledby_on_editable_fields(self):
         """
         Verify that editable inputs in the changelist have the correct
-        aria-labelledby attributes pointing to the Row Header and 
+        aria-labelledby attributes pointing to the Row Header and
         Column Header.
         """
         url = reverse("admin:admin_views_article_changelist")
