349002aa49fe353a2a38f5be0c4d316149d60cff
Yousef Abu Shanab <93343012+youzarsiph@users.noreply.github.com>
2026-02-07 08:36:54 +0000

    Added PaginationMixin.
    Refactored MultipleObjectMixin to add PaginationMixin.

--- django/views/generic/list.py
+++ django/views/generic/list.py
@@ -6,17 +6,111 @@ from django.utils.translation import gettext as _
 from django.views.generic.base import ContextMixin, TemplateResponseMixin, View
 
 
-class MultipleObjectMixin(ContextMixin):
-    """A mixin for views manipulating multiple objects."""
+class PaginationMixin:
+    """A mixin for views paginating multiple objects."""
 
     allow_empty = True
-    queryset = None
-    model = None
     paginate_by = None
     paginate_orphans = 0
-    context_object_name = None
     paginator_class = Paginator
     page_kwarg = "page"
+
+    def paginate_queryset(self, queryset, page_size, *args, **kwargs):
+        """Paginate the queryset, if needed."""
+        paginator = self.get_paginator(
+            queryset,
+            page_size,
+            orphans=self.get_paginate_orphans(),
+            allow_empty_first_page=self.get_allow_empty(),
+        )
+        page_kwarg = self.page_kwarg
+        kw_args = self.kwargs if self.kwargs else kwargs
+        request = self.request if self.request else kwargs.get("request")
+        page = kw_args.get(page_kwarg) or request.GET.get(page_kwarg) or 1
+        try:
+            page_number = int(page)
+        except ValueError:
+            if page == "last":
+                page_number = paginator.num_pages
+            else:
+                raise Http404(
+                    _("Page is not “last”, nor can it be converted to an int.")
+                )
+        try:
+            page = paginator.page(page_number)
+            return (paginator, page, page.object_list, page.has_other_pages())
+        except InvalidPage as e:
+            raise Http404(
+                _("Invalid page (%(page_number)s): %(message)s")
+                % {"page_number": page_number, "message": str(e)}
+            )
+
+    def get_paginate_by(self, queryset):
+        """
+        Get the number of items to paginate by, or ``None`` for no pagination.
+        """
+        return self.paginate_by
+
+    def get_paginator(
+        self,
+        queryset,
+        per_page,
+        orphans=0,
+        allow_empty_first_page=True,
+        **kwargs,
+    ):
+        """Return an instance of the paginator for this view."""
+        return self.paginator_class(
+            queryset,
+            per_page,
+            orphans=orphans,
+            allow_empty_first_page=allow_empty_first_page,
+            **kwargs,
+        )
+
+    def get_paginate_orphans(self):
+        """
+        Return the maximum number of orphans extend the last page by when
+        paginating.
+        """
+        return self.paginate_orphans
+
+    def get_allow_empty(self):
+        """
+        Return ``True`` if the view should display empty lists and ``False``
+        if a 404 should be raised instead.
+        """
+        return self.allow_empty
+
+    def get_pagination_context(self, queryset, *args, **kwargs):
+        """Get context for pagination."""
+        page_size = self.get_paginate_by(queryset)
+        if page_size:
+            paginator, page, queryset, is_paginated = self.paginate_queryset(
+                queryset, page_size, *args, **kwargs
+            )
+            context = {
+                "paginator": paginator,
+                "page_obj": page,
+                "is_paginated": is_paginated,
+                "object_list": queryset,
+            }
+        else:
+            context = {
+                "paginator": None,
+                "page_obj": None,
+                "is_paginated": False,
+                "object_list": queryset,
+            }
+        return context
+
+
+class MultipleObjectMixin(PaginationMixin, ContextMixin):
+    """A mixin for views manipulating multiple objects."""
+
+    queryset = None
+    model = None
+    context_object_name = None
     ordering = None
 
     def get_queryset(self):
@@ -50,66 +144,6 @@ class MultipleObjectMixin(ContextMixin):
         """Return the field or fields to use for ordering the queryset."""
         return self.ordering
 
-    def paginate_queryset(self, queryset, page_size):
-        """Paginate the queryset, if needed."""
-        paginator = self.get_paginator(
-            queryset,
-            page_size,
-            orphans=self.get_paginate_orphans(),
-            allow_empty_first_page=self.get_allow_empty(),
-        )
-        page_kwarg = self.page_kwarg
-        page = self.kwargs.get(page_kwarg) or self.request.GET.get(page_kwarg) or 1
-        try:
-            page_number = int(page)
-        except ValueError:
-            if page == "last":
-                page_number = paginator.num_pages
-            else:
-                raise Http404(
-                    _("Page is not “last”, nor can it be converted to an int.")
-                )
-        try:
-            page = paginator.page(page_number)
-            return (paginator, page, page.object_list, page.has_other_pages())
-        except InvalidPage as e:
-            raise Http404(
-                _("Invalid page (%(page_number)s): %(message)s")
-                % {"page_number": page_number, "message": str(e)}
-            )
-
-    def get_paginate_by(self, queryset):
-        """
-        Get the number of items to paginate by, or ``None`` for no pagination.
-        """
-        return self.paginate_by
-
-    def get_paginator(
-        self, queryset, per_page, orphans=0, allow_empty_first_page=True, **kwargs
-    ):
-        """Return an instance of the paginator for this view."""
-        return self.paginator_class(
-            queryset,
-            per_page,
-            orphans=orphans,
-            allow_empty_first_page=allow_empty_first_page,
-            **kwargs,
-        )
-
-    def get_paginate_orphans(self):
-        """
-        Return the maximum number of orphans extend the last page by when
-        paginating.
-        """
-        return self.paginate_orphans
-
-    def get_allow_empty(self):
-        """
-        Return ``True`` if the view should display empty lists and ``False``
-        if a 404 should be raised instead.
-        """
-        return self.allow_empty
-
     def get_context_object_name(self, object_list):
         """Get the name of the item to be used in the context."""
         if self.context_object_name:
@@ -122,27 +156,10 @@ class MultipleObjectMixin(ContextMixin):
     def get_context_data(self, *, object_list=None, **kwargs):
         """Get the context for this view."""
         queryset = object_list if object_list is not None else self.object_list
-        page_size = self.get_paginate_by(queryset)
         context_object_name = self.get_context_object_name(queryset)
-        if page_size:
-            paginator, page, queryset, is_paginated = self.paginate_queryset(
-                queryset, page_size
-            )
-            context = {
-                "paginator": paginator,
-                "page_obj": page,
-                "is_paginated": is_paginated,
-                "object_list": queryset,
-            }
-        else:
-            context = {
-                "paginator": None,
-                "page_obj": None,
-                "is_paginated": False,
-                "object_list": queryset,
-            }
+        context = self.get_pagination_context(queryset, **kwargs)
         if context_object_name is not None:
-            context[context_object_name] = queryset
+            context[context_object_name] = context.get("object_list", queryset)
         context.update(kwargs)
         return super().get_context_data(**context)
 
