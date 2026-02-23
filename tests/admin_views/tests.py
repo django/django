9a8461f92250b49994aa133b564ec68617f6d78c
Zac Iloka <zachary.iloka@gmail.com>
2026-02-23 14:44:28 +1300

    Fixed a typo in tests/admin_views/tests.py.

--- tests/admin_views/tests.py
+++ tests/admin_views/tests.py
@@ -9444,7 +9444,7 @@ class AdminSiteFinalCatchAllPatternTests(TestCase):
         response = self.client.get(url)
         self.assertRedirects(response, "%s?next=%s" % (reverse("admin:login"), url))
 
-    def test_unkown_url_without_trailing_slash_if_not_authenticated(self):
+    def test_unknown_url_without_trailing_slash_if_not_authenticated(self):
         url = reverse("admin:article_extra_json")[:-1]
         response = self.client.get(url)
         self.assertRedirects(response, "%s?next=%s" % (reverse("admin:login"), url))
