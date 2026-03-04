4f96299e0062eafe03ee91dc8d7252db20215e6c
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2026-03-04 13:15:57 -0500

    Reproduced bug scenario with a failing test

--- tests/test_client/tests.py
+++ tests/test_client/tests.py
@@ -1053,6 +1053,13 @@ class ClientTest(TestCase):
                         query_params={"q": "terms"},
                     )
 
+    def test_follow_redirect_with_query_params(self):
+        response = self.client.get(
+            "/redirect_view/", query_params={"next": "x"}, follow=True
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.wsgi_request.get_full_path(), "/get_view/?next=x")
+
 
 @override_settings(
     MIDDLEWARE=["django.middleware.csrf.CsrfViewMiddleware"],
