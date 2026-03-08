f64fc876f3de12d803c5dca9a5e59f7b881f271a
Harish-Bonu03 <harish.bonu03@gmail.com>
2026-03-08 16:12:44 +0530

    Fixed #36938 -- Removed unnecessary ORDER BY from UNION components in Oracle subqueries.

--- django/db/models/sql/compiler.py
+++ django/db/models/sql/compiler.py
@@ -641,15 +641,14 @@ class SQLCompiler:
     def _get_combinator_part_sql(self, compiler):
         features = self.connection.features
         # Some backends (e.g. Oracle) don't allow unnecessary ORDER BY in
-        # subqueries used as compound statement components.
+        # UNION component queries.
         if (
             self.query.subquery
+            and self.query.combinator == "union"
             and not features.ignores_unnecessary_order_by_in_subqueries
         ):
             compiler.query = compiler.query.clone()
             compiler.query.clear_ordering(force=False)
-            for query in compiler.query.combined_queries:
-                query.clear_ordering(force=False)
         # If the columns list is limited, then all combined queries
         # must have the same columns list. Set the selects defined on
         # the query on all combined queries, if not already set.
