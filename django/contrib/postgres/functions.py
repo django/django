84e74c81bf473e2666152b937265457c8e6afbdc
shubh-gitpush <shubhrai598@gmail.com>
2026-03-12 21:21:17 +0530

    added postgresql num_nonmull orm function

--- django/contrib/postgres/functions.py
+++ django/contrib/postgres/functions.py
@@ -1,4 +1,4 @@
-from django.db.models import DateTimeField, Func, UUIDField
+from django.db.models import DateTimeField, Func, UUIDField, IntegerField
 
 
 class RandomUUID(Func):
@@ -9,3 +9,10 @@ class RandomUUID(Func):
 class TransactionNow(Func):
     template = "CURRENT_TIMESTAMP"
     output_field = DateTimeField()
+
+
+class NumNonNulls(Func):
+    function = "num_nonnulls"
+    output_field = IntegerField()
+
+
