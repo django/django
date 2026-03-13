6a5b2269aeb61f4f64a54f6b14f405449e724740
Jacob Walls <jacobtylerwalls@gmail.com>
2026-03-13 17:06:34 -0400

    Fixed #36987 -- Observed prepared argument in UUIDField.get_db_prep_value().
    This avoids two isinstance() calls per UUID value.

--- django/db/models/fields/__init__.py
+++ django/db/models/fields/__init__.py
@@ -2821,7 +2821,7 @@ class UUIDField(Field):
     def get_db_prep_value(self, value, connection, prepared=False):
         if value is None:
             return None
-        if not isinstance(value, uuid.UUID):
+        if not prepared and not isinstance(value, uuid.UUID):
             value = self.to_python(value)
 
         if connection.features.has_native_uuid_field:
