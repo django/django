76c27d71a09c065a4e40ab1bf9f092fe6e4d08f2
Tejas Attarde <tejasattardexx@gmail.com>
2026-03-13 19:10:05 -0400

    Set a timeout on manage translations HTTP calls

--- scripts/manage_translations.py
+++ scripts/manage_translations.py
@@ -72,7 +72,7 @@ def get_api_response(endpoint, api_token=None, params=None, verbosity=0):
     url = f"https://rest.api.transifex.com/{endpoint}"
     if verbosity > 2:
         print(f"\n>>> GET {url=} {params=}")
-    response = requests.get(url, headers=headers, params=params)
+    response = requests.get(url, headers=headers, params=params, timeout=10.0)
     if verbosity > 2:
         print(f">>>> GET {response=}\n")
     response.raise_for_status()
