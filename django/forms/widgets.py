b503d9d2e92fe8163118bbd40ed8aa59759a1127
Aarav Sharma <aaravbhai7@gmail.com>
2026-03-20 19:44:40 +0530

    Refs #31295 -- Avoid additional query in Select.use_required_attribute when using ModelChoiceIterator.

--- django/forms/widgets.py
+++ django/forms/widgets.py
@@ -847,11 +847,24 @@ class Select(ChoiceWidget):
         invalid HTML.
         """
         use_required_attribute = super().use_required_attribute(initial)
+
         # 'required' is always okay for <select multiple>.
         if self.allow_multiple_selected:
             return use_required_attribute
 
-        first_choice = next(iter(self.choices), None)
+        iterator = iter(self.choices)
+
+        # Avoid triggering an additional database query
+        # when using ModelChoiceIterator
+        try:
+            empty_label = iterator.field.empty_label
+        except AttributeError:
+            empty_label = None
+
+        if empty_label is not None:
+            return use_required_attribute
+
+        first_choice = next(iterator, None)
         return (
             use_required_attribute
             and first_choice is not None
