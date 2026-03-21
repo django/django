4485a12403a53a49741c1ac47b78e4e31146f3e3
Anna Makarudze <amakarudze@gmail.com>
2026-03-21 19:49:02 +0100

    Fixed #36912 -- Added connector validation to Q.create().
    Co-authored-by: Jacob Walls <jacobtylerwalls@gmail.com>

--- tests/queries/test_q.py
+++ tests/queries/test_q.py
@@ -273,7 +273,7 @@ class QTests(SimpleTestCase):
                 )
 
     def test_connector_validation(self):
-        msg = f"_connector must be one of {Q.AND!r}, {Q.OR!r}, {Q.XOR!r}, or None."
+        msg = f"connector must be one of {Q.AND!r}, {Q.OR!r}, {Q.XOR!r}, or None."
         with self.assertRaisesMessage(ValueError, msg):
             Q(_connector="evil")
 
