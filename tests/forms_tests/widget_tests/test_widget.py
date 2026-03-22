1c8c8bee6c87a9fbdc8fd9fb04c40a4286b16dc4
Aarav Sharma <aaravbhai7@gmail.com>
2026-03-22 23:43:17 +0530

    Refs #31295 -- Add test for Select.use_required_attribute to ensure no unnecessary queries with ModelChoiceIterator.

--- tests/forms_tests/widget_tests/test_widget.py
+++ tests/forms_tests/widget_tests/test_widget.py
@@ -1,5 +1,8 @@
+from django import forms
+from django.contrib.auth.models import User
 from django.forms import Widget
 from django.forms.widgets import Input
+from django.test import TestCase
 
 from .base import WidgetTest
 
@@ -16,7 +19,8 @@ class WidgetTests(WidgetTest):
         widget = Widget()
         self.assertIs(widget.value_omitted_from_data({}, {}, "field"), True)
         self.assertIs(
-            widget.value_omitted_from_data({"field": "value"}, {}, "field"), False
+            widget.value_omitted_from_data({"field": "value"}, {}, "field"),
+            False,
         )
 
     def test_no_trailing_newline_in_attrs(self):
@@ -31,3 +35,23 @@ class WidgetTests(WidgetTest):
     def test_attr_false_not_rendered(self):
         html = '<input type="None" name="name" value="value">'
         self.check_html(Input(), "name", "value", html=html, attrs={"readonly": False})
+
+
+class SelectUseRequiredAttributeTests(TestCase):
+    def test_does_not_trigger_query_with_model_choice_iterator(self):
+        # Create test data
+        User.objects.create(username="a")
+        User.objects.create(username="b")
+
+        class TestForm(forms.Form):
+            field = forms.ModelChoiceField(
+                queryset=User.objects.all(),
+                empty_label="Select one",
+            )
+
+        form = TestForm()
+        widget = form.fields["field"].widget
+
+        # Should NOT trigger any DB queries
+        with self.assertNumQueries(0):
+            widget.use_required_attribute(initial=None)
