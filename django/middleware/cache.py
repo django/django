b7b23f4697850e232486d787df3459f19bd16dba
Jacob Walls <jacobtylerwalls@gmail.com>
2026-03-24 14:06:22 -0400

    [6.1.x] Fixed CVE-2026-35193 -- Varied on Authorization when caching non-public responses.
    Thanks Shai Berger for the report, and Natalia Bidart and Sarah Boyce for reviews.
    
    Backport of a2faa8e895926ac5d63f72879b5ccf671b5b4ba9 from main.

--- django/middleware/cache.py
+++ django/middleware/cache.py
@@ -41,6 +41,8 @@ More details about how the caching works:
 * This middleware also sets ETag, Last-Modified, Expires and Cache-Control
   headers on the response object.
 
+* If the request had an Authorization header and the response was not marked
+  "Cache-Control: public", the response will vary on Authorization.
 """
 
 import time
@@ -53,6 +55,7 @@ from django.utils.cache import (
     has_vary_header,
     learn_cache_key,
     patch_response_headers,
+    patch_vary_headers,
 )
 from django.utils.deprecation import MiddlewareMixin
 from django.utils.http import parse_http_date_safe
@@ -130,6 +133,12 @@ class UpdateCacheMiddleware(MiddlewareMixin):
                 # max-age was set to 0, don't cache.
                 return response
         patch_response_headers(response, timeout)
+        # Make the response vary on Authorization if the request bears that
+        # header, unless allowed by "public" per RFC 9111, Section 3.5. No
+        # exceptions are made for "s-maxage" and "must-revalidate" since these
+        # are not currently implemented by Django.
+        if request.headers.get("Authorization") and "public" not in cache_control:
+            patch_vary_headers(response, ("Authorization",))
         if timeout and response.status_code == 200:
             cache_key = learn_cache_key(
                 request, response, timeout, self.key_prefix, cache=self.cache
