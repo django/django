504d189de52c533df18a670032bdfc5e62424b9f
Vignesh Anand <vigneshanandmay13@gmail.com>
2026-03-25 03:24:02 +0530

    Refs #36494 -- Prevented crash in JSONField numeric lookups with expressions.

--- tests/model_fields/test_jsonfield.py
+++ tests/model_fields/test_jsonfield.py
@@ -1304,6 +1304,35 @@ class TestQuerying(TestCase):
         ).first()
         self.assertEqual(result.coalesced_value, "This is valid JSON primitive.")
 
+    def test_numeric_lookups_with_expression(self):
+        obj_greater = NullableJSONModel.objects.create(
+            value={"target": 5, "comparison": 2}
+        )
+        obj_lesser = NullableJSONModel.objects.create(
+            value={"target": 2, "comparison": 5}
+        )
+        obj_equal = NullableJSONModel.objects.create(
+            value={"target": 2, "comparison": 2}
+        )
+        objs = [obj_greater.pk, obj_lesser.pk, obj_equal.pk]
+
+        tests = [
+            ("gt", [obj_greater]),
+            ("lt", [obj_lesser]),
+            ("gte", [obj_greater, obj_equal]),
+            ("lte", [obj_lesser, obj_equal]),
+        ]
+
+        for lookup, expected in tests:
+            with self.subTest(lookup=lookup):
+                self.assertCountEqual(
+                    NullableJSONModel.objects.filter(
+                        id__in=objs,
+                        **{f"value__target__{lookup}": F("value__comparison")},
+                    ),
+                    expected,
+                )
+
 
 @skipUnlessDBFeature("supports_primitives_in_json_field")
 class JSONNullTests(TestCase):
