e0f2675b5ff83256e54c4e9ecb62b65ec149016c
Mike Edmunds <medmunds@gmail.com>
2026-04-03 15:33:03 -0700

    Refs #35514 -- Simplified creating custom connections in mail tests.
    In tests for the `connection` arg to various email APIs, replaced
    `get_connection("mail.custombackend.EmailBackend")` with direct
    `custombackend.EmailBackend()` construction. This avoids soon-to-be-
    deprecated usage of `get_connection(backend_path)` in tests that aren't
    specifically covering creating a connection from an import path.

--- tests/mail/tests.py
+++ tests/mail/tests.py
@@ -1895,7 +1895,9 @@ class SendMailTests(SimpleTestCase, MailTestsMixin):
 
     def test_connection_arg(self):
         # Send using non-default connection.
-        connection = mail.get_connection("mail.custombackend.EmailBackend")
+        from .custombackend import EmailBackend
+
+        connection = EmailBackend()
         send_mail(
             "Subject",
             "Content",
@@ -1993,7 +1995,9 @@ class SendMassMailTests(SimpleTestCase):
 
     def test_connection_arg(self):
         # Send using non-default connection.
-        connection = mail.get_connection("mail.custombackend.EmailBackend")
+        from .custombackend import EmailBackend
+
+        connection = EmailBackend()
         send_mass_mail(
             [
                 ("Subject1", "Content1", "from1@example.com", ["to1@example.com"]),
@@ -2200,7 +2204,9 @@ class MailAdminsAndManagersTests(SimpleTestCase, MailTestsMixin):
     @override_settings(ADMINS=["nobody@example.com"])
     def test_connection_arg_mail_admins(self):
         # Send using non-default connection.
-        connection = mail.get_connection("mail.custombackend.EmailBackend")
+        from .custombackend import EmailBackend
+
+        connection = EmailBackend()
         mail_admins("Admin message", "Content", connection=connection)
         self.assertEqual(mail.outbox, [])
         self.assertEqual(len(connection.test_outbox), 1)
@@ -2209,7 +2215,9 @@ class MailAdminsAndManagersTests(SimpleTestCase, MailTestsMixin):
     @override_settings(MANAGERS=["nobody@example.com"])
     def test_connection_arg_mail_managers(self):
         # Send using non-default connection.
-        connection = mail.get_connection("mail.custombackend.EmailBackend")
+        from .custombackend import EmailBackend
+
+        connection = EmailBackend()
         mail_managers("Manager message", "Content", connection=connection)
         self.assertEqual(mail.outbox, [])
         self.assertEqual(len(connection.test_outbox), 1)
