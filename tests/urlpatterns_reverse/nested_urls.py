2eda7686ff94facfd75197ea9fc4c242b69eb2bd
ezvany <yannick@hellowatt.fr>
2026-04-19 10:54:19 +0200

    Fixed #32565 -- Moved internal URLResolver used only by admindocs to admindocs.
    Extracted function `_is_callback`, `lookup_str` and attribute `_callback_strs` from UrlResolver to be closer to use inside admindocs.

--- tests/urlpatterns_reverse/nested_urls.py
+++ tests/urlpatterns_reverse/nested_urls.py
@@ -14,10 +14,17 @@ class View3(View):
     pass
 
 
+def view4(request):
+    pass
+
+
+sub_nested = [path("view4/", view4, name="view4")]
+
 nested = (
     [
         path("view1/", view1, name="view1"),
         path("view3/", View3.as_view(), name="view3"),
+        path("sub_nested/", include(sub_nested)),
     ],
     "backend",
 )
