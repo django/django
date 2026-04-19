07f380dff30fd91c672929a2f154a9e5c21b9bd9
Tom Carrick <tom@carrick.eu>
2026-04-19 11:54:17 +0300

    Formatted JavaScript files.

--- django/contrib/admin/static/admin/js/jquery.init.js
+++ django/contrib/admin/static/admin/js/jquery.init.js
@@ -1,8 +1,8 @@
 /*global jQuery:false*/
-'use strict';
+"use strict";
 /* Puts the included jQuery into our own namespace using noConflict and passing
  * it 'true'. This ensures that the included jQuery doesn't pollute the global
  * namespace (i.e. this preserves pre-existing values for both window.$ and
  * window.jQuery).
  */
-window.django = {jQuery: jQuery.noConflict(true)};
+window.django = { jQuery: jQuery.noConflict(true) };
