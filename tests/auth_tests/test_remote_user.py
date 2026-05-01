31f8475ff68520650339a2a89c7f4f7c47f08d65
Jacob Walls <jacobtylerwalls@gmail.com>
2026-05-01 09:20:41 -0400

    fixup! Use

--- tests/auth_tests/test_remote_user.py
+++ tests/auth_tests/test_remote_user.py
@@ -545,7 +545,7 @@ class PersistentRemoteUserTest(RemoteUserTest):
         await User.objects.acreate(username="knownuser")
         # Known user authenticates
         response = await self.async_client.get(
-            "/remote_user/", headers={self.header: self.known_user}
+            "/remote_user/", **{self.header: self.known_user}
         )
         self.assertEqual(response.context["user"].username, "knownuser")
         # Should stay logged in if the REMOTE_USER header disappears.
