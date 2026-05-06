6e705e74d093bce6fe5252d087107ad5398b4182
RD-Bench V2 <rdbench@rubberduck.io>
2026-05-06 12:58:17 +0000

    [RD-Bench V2] rdbench-v2/django__django-10999

--- django/utils/dateparse.py
+++ django/utils/dateparse.py
@@ -29,7 +29,7 @@ datetime_re = re.compile(
 standard_duration_re = re.compile(
     r'^'
     r'(?:(?P<days>-?\d+) (days?, )?)?'
-    r'((?:(?P<hours>-?\d+):)(?=\d+:\d+))?'
+    r'((?:(?P<hours>-?\d+):)(?=-?\d+:-?\d+))?'
     r'(?:(?P<minutes>-?\d+):)?'
     r'(?P<seconds>-?\d+)'
     r'(?:\.(?P<microseconds>\d{1,6})\d{0,6})?'
