fbe902d4a4b8b9dcb371509b25ba8feff3852e64
Jacob Walls <jacobtylerwalls@gmail.com>
2026-05-13 10:31:27 -0400

    Refs #37097 -- Removed compilation-time order clearing on combined queries on Oracle.
    Thanks Simon Charette, JaeHyuck Sa, and Shai Berger for reviews.

--- django/db/models/query.py
+++ django/db/models/query.py
@@ -1703,8 +1703,8 @@ class QuerySet(AltersData):
         # Clear limits and ordering so they can be reapplied
         clone.query.clear_ordering(force=True)
         clone.query.default_ordering = True
+        self._clear_ordering_in_combined_queries(clone.query, other_qs)
         clone.query.clear_limits()
-        clone.query.combined_queries = (self.query, *(qs.query for qs in other_qs))
         clone.query.combinator = combinator
         clone.query.combinator_all = all
         return clone
@@ -2334,6 +2334,14 @@ class QuerySet(AltersData):
                 f"Cannot use QuerySet.{method}() on an unordered queryset performing "
                 f"aggregation. Add an ordering with order_by()."
             )
+
+    def _clear_ordering_in_combined_queries(self, cloned_query, other_qs):
+        combined_queries = [self.query]
+        for qs in other_qs:
+            query = qs.query.clone()
+            query.clear_ordering(force=False, clear_default=False)
+            combined_queries.append(query)
+        cloned_query.combined_queries = tuple(combined_queries)
 
 
 class InstanceCheckMeta(type):
