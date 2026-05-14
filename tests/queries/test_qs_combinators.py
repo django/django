django/django

commit 087bb9e8f3478d53f12b1737af865992af17c5f2

Refs #36644 -- Applied default ordering after union().

tests/queries/test_qs_combinators.py
@@ -418,6 +418,15 @@ class QuerySetSetOperationTests(TestCase):
         qs2 = base_qs.filter(name="a2")
         self.assertEqual(qs1.union(qs2).first(), a1)
 
+    @skipUnlessDBFeature("supports_slicing_ordering_in_compound")
+    def test_union_applies_default_ordering_afterward(self):
+        c = Tag.objects.create(name="C")
+        Tag.objects.create(name="B")
+        a = Tag.objects.create(name="A")
+        qs1 = Tag.objects.filter(name__in=["A", "B"])[:1]
+        qs2 = Tag.objects.filter(name__in=["C"])[:1]
+        self.assertSequenceEqual(qs1.union(qs2), [a, c])
+
     def test_union_multiple_models_with_values_list_and_order(self):
         reserved_name = ReservedName.objects.create(name="rn1", order=0)
         qs1 = Celebrity.objects.all()

django/db/models/query.py
@@ -1679,6 +1679,7 @@ class QuerySet(AltersData):
         clone = self._chain()
         # Clear limits and ordering so they can be reapplied
         clone.query.clear_ordering(force=True)
+        clone.query.default_ordering = True
         clone.query.clear_limits()
         clone.query.combined_queries = (self.query, *(qs.query for qs in other_qs))
         clone.query.combinator = combinator

commit a9cf35c6062c67af989f5c27ef1d63fd192200f4

Fixed #37097 -- Made Query.clear_ordering() clear ordering on combined queries also.
Thanks Shai Berger for the report.

Regression in 087bb9e8f3478d53f12b1737af865992af17c5f2.

(That commit drove more traffic into an error that would have been
reachable only with an explicit order_by() after each union().)

Co-authored-by: Simon Charette <charettes@gmail.com>
Co-authored-by: siddus <dcsid10@gmail.com>

django/db/models/sql/query.py
@@ -2402,6 +2402,8 @@ class Query(BaseExpression):
         self.extra_order_by = ()
         if clear_default:
             self.default_ordering = False
+        for query in self.combined_queries:
+            query.clear_ordering(force=False, clear_default=clear_default)
 
     def set_group_by(self, allow_aliases=True):
         """

tests/queries/test_qs_combinators.py
@@ -578,6 +578,23 @@ class QuerySetSetOperationTests(TestCase):
             ordered=False,
         )
 
+    def test_double_union_in_with_default_ordering(self):
+        e1 = ExtraInfo.objects.create(value=7, info="e1")
+        Author.objects.bulk_create(
+            [Author(num=i, name=f"a{i}", extra=e1) for i in range(1, 8)]
+        )
+        qs1 = Author.objects.filter(num__lt=2)
+        qs2 = Author.objects.filter(num__gt=6)
+        qs3 = Author.objects.filter(num=4)
+        double_union = qs1.union(qs2).union(qs3)
+        # Target a column (num) other than the ordering column (name). Before,
+        # on Postgres: "each UNION query must have the same number of columns".
+        self.assertQuerySetEqual(
+            Author.objects.filter(num__in=double_union.values("num")).order_by("-name"),
+            ["a7", "a4", "a1"],
+            transform=lambda au: au.name,
+        )
+
     @skipUnlessDBFeature(
         "supports_slicing_ordering_in_compound", "allow_sliced_subqueries_with_in"
     )