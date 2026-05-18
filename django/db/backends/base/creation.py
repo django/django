65a9f14196c338d70889bd54753370606b3fb4eb
Leland <lboeman@arizona.edu>
2026-05-18 15:32:12 -0700

    Fixed #36864 -- Supported modules and submodules in import_string().
    Before, only submodules worked (and only if already imported). Now,
    all modules work regardless of prior import state.
    
    Updated expected exception in auth tests where import_string() returns
    a non-callable.

--- django/db/backends/base/creation.py
+++ django/db/backends/base/creation.py
@@ -372,21 +372,8 @@ class BaseDatabaseCreation:
         test_app = test_name.split(".")[0]
         # Importing a test app that isn't installed raises RuntimeError.
         if test_app in settings.INSTALLED_APPS:
-            try:
-                test_frame = import_string(module_or_class_name)
-            except ImportError:
-                # import_string() can raise ImportError if a submodule's parent
-                # module hasn't already been imported during test discovery.
-                # This can happen in at least two cases:
-                # 1. When running a subset of tests in a module, the test
-                #    runner won't import tests in that module's other
-                #    submodules.
-                # 2. When the parallel test runner spawns workers with an empty
-                #    import cache.
-                test_to_mark = import_string(test_name)
-                test_frame = sys.modules.get(test_to_mark.__module__)
-            else:
-                test_to_mark = getattr(test_frame, name_to_mark)
+            test_frame = import_string(module_or_class_name)
+            test_to_mark = getattr(test_frame, name_to_mark)
             if skip_reason:
                 setattr(test_frame, name_to_mark, skip(skip_reason)(test_to_mark))
             else:
---
+++ tests/utils_tests/test_module/collision/__init__.py
@@ -0,0 +1,1 @@
+collider = "I'm an object"
