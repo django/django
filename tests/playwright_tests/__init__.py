4828cfb1944fdd327145d824c3d2eb2f37e5d890
Varun Kasyap <varunkasyap@hotmail.com>
2026-05-24 11:41:03 +0530

    Fixed #37154, #37149 -- Switched from Selenium to Playwright for integration testing.

---
+++ tests/playwright_tests/__init__.py
@@ -0,0 +1,9 @@
+from .admin import AdminPlaywrightTestCase
+from .base import PlaywrightTestCase, PlaywrightTestCaseMeta, screenshot_cases
+
+__all__ = (
+    "AdminPlaywrightTestCase",
+    "PlaywrightTestCase",
+    "PlaywrightTestCaseMeta",
+    "screenshot_cases",
+)
