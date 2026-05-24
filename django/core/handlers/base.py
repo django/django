bb93e16930911f932b6847a07abec7c58ebd1a0f
Josef Vacha <69599105+JosefVacha@users.noreply.github.com>
2026-05-24 23:51:53 +0000

    Fix trailing period in url resolver comment (F53)

--- django/core/handlers/base.py
+++ django/core/handlers/base.py
@@ -137,7 +137,7 @@ class BaseHandler:
 
     def get_response(self, request):
         """Return an HttpResponse object for the given HttpRequest."""
-        # Setup default url resolver for this thread
+        # Setup default url resolver for this thread.
         set_urlconf(settings.ROOT_URLCONF)
         response = self._middleware_chain(request)
         response._resource_closers.append(request.close)
