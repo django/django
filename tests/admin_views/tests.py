888b7f09b56deafdb7ba199ad6f63f07c4c96cc6
Malte Gerth <mail@malte-gerth.de>
2026-05-27 12:07:20 +0200

    Fixed #36498 -- Changed inline admin button text to use "Add" instead of "Add another"
    Co-authored-by: DaniforNons <danifornons@gmail.com>

--- tests/admin_views/tests.py
+++ tests/admin_views/tests.py
@@ -6408,9 +6408,7 @@ class SeleniumTests(AdminSeleniumTestCase):
         self.assertEqual(num_initial_select2_inputs, 4)
 
         # Add an inline
-        self.selenium.find_elements(By.LINK_TEXT, "Add another Related prepopulated")[
-            0
-        ].click()
+        self.selenium.find_elements(By.LINK_TEXT, "Add Related prepopulated")[0].click()
         self.assertEqual(
             len(self.selenium.find_elements(By.CLASS_NAME, "select2-selection")),
             num_initial_select2_inputs + 2,
@@ -6470,9 +6468,9 @@ class SeleniumTests(AdminSeleniumTestCase):
 
         # Add an inline
         # Button may be outside the browser frame.
-        element = self.selenium.find_elements(
-            By.LINK_TEXT, "Add another Related prepopulated"
-        )[1]
+        element = self.selenium.find_elements(By.LINK_TEXT, "Add Related prepopulated")[
+            1
+        ]
         self.selenium.execute_script("window.scrollTo(0, %s);" % element.location["y"])
         element.click()
         self.assertEqual(
@@ -6502,9 +6500,7 @@ class SeleniumTests(AdminSeleniumTestCase):
         # Add an inline without an initial inline.
         # The button is outside of the browser frame.
         self.selenium.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        self.selenium.find_elements(By.LINK_TEXT, "Add another Related prepopulated")[
-            2
-        ].click()
+        self.selenium.find_elements(By.LINK_TEXT, "Add Related prepopulated")[2].click()
         self.assertEqual(
             len(self.selenium.find_elements(By.CLASS_NAME, "select2-selection")),
             num_initial_select2_inputs + 6,
@@ -6531,7 +6527,7 @@ class SeleniumTests(AdminSeleniumTestCase):
         # Add inline.
         add_link = self.selenium.find_elements(
             By.LINK_TEXT,
-            "Add another Related prepopulated",
+            "Add Related prepopulated",
         )[3]
         self.selenium.execute_script("arguments[0].scrollIntoView();", add_link)
         add_link.click()
