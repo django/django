c32e088b09e213d04900e244a5f05ef5e0d0d32f
Mike Edmunds <medmunds@gmail.com>
2026-06-10 12:53:04 -0700

    Refs #37150 -- Synced docs/make.bat with Makefile.
    In docs/make.bat, changed "djangohtml" Sphinx builder (removed in
    9b1db9c) to "html", matching docs/Makefile.

--- docs/make.bat
+++ docs/make.bat
@@ -47,7 +47,7 @@ if "%1" == "clean" (
 )
 
 if "%1" == "html" (
-	%SPHINXBUILD% -b djangohtml %ALLSPHINXOPTS% %BUILDDIR%/html
+	%SPHINXBUILD% -b html %ALLSPHINXOPTS% %BUILDDIR%/html
 	if errorlevel 1 exit /b 1
 	echo.
 	echo.Build finished. The HTML pages are in %BUILDDIR%/html.
