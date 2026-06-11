3a720d0d8bf2529253b98968f10ca73daf6d693c
Natalia <124304+nessita@users.noreply.github.com>
2026-06-11 15:17:31 -0300

    Fixed CVE-2026-53878 -- Prevented newlines from being accepted in DomainNameValidator.
    Thanks Bence Nagy for the report, and Jake Howard for reviews.

--- django/core/validators.py
+++ django/core/validators.py
@@ -99,20 +99,20 @@ class DomainNameValidator(RegexValidator):
     def __init__(self, **kwargs):
         self.accept_idna = kwargs.pop("accept_idna", True)
 
-        if self.accept_idna:
-            self.regex = _lazy_re_compile(
-                r"^" + self.hostname_re + self.domain_re + self.tld_re + r"$",
-                re.IGNORECASE,
-            )
-        else:
-            self.regex = _lazy_re_compile(
-                r"^"
-                + self.ascii_only_hostname_re
-                + self.ascii_only_domain_re
-                + self.ascii_only_tld_re
-                + r"$",
-                re.IGNORECASE,
-            )
+        regex_parts = [
+            "^",
+            *(
+                (self.hostname_re, self.domain_re, self.tld_re)
+                if self.accept_idna
+                else (
+                    self.ascii_only_hostname_re,
+                    self.ascii_only_domain_re,
+                    self.ascii_only_tld_re,
+                )
+            ),
+            r"\Z",
+        ]
+        self.regex = _lazy_re_compile("".join(regex_parts), re.IGNORECASE)
         super().__init__(**kwargs)
 
     def __call__(self, value):
