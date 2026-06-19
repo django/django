9f3027a676112abb86f971dc2b9b677346a3b322
Jacob Walls <jacobtylerwalls@gmail.com>
2026-06-19 14:40:37 -0400

    Fixed #37177 -- Reduced per-middleware context switching under ASGI.
    Relying on MiddlewareMixin.__acall__() to handle async requests imposed
    per-middleware context switching, defeating BaseHandler's intent
    to transition only once per middleware chain.
    
    To minimize breaking changes, MiddlewareMixin's default value did not
    change: only the values on Django's own subclasses. Use cases where the
    prior behavior is preferable (in order to avoid pinning a thread per
    request) are fleshed out in docs.
    
    Thanks Mykhailo Havelia for the report.

--- django/contrib/sites/middleware.py
+++ django/contrib/sites/middleware.py
@@ -8,5 +8,7 @@ class CurrentSiteMiddleware(MiddlewareMixin):
     Middleware that sets `site` attribute to request object.
     """
 
+    async_capable = False
+
     def process_request(self, request):
         request.site = get_current_site(request)
