a79e30c9246dc16b6106e0cdae95573633e5918e
Mike Edmunds <medmunds@gmail.com>
2026-07-06 08:27:30 -0700

    Fixed #37138 -- Changed docs to use pydata-sphinx-theme.

--- docs/conf.py
+++ docs/conf.py
@@ -204,12 +204,25 @@ suppress_warnings = ["app.add_directive", "epub.duplicated_toc_entry"]
 
 # The theme to use for HTML and HTML Help pages. See the documentation for
 # a list of builtin themes.
-html_theme = "djangodocs"
+html_theme = "pydata_sphinx_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further. For a list of options available for each theme, see the
 # documentation.
-# html_theme_options = {}
+html_theme_options = {
+    "show_nav_level": 2,  # Left nav initially expanded (default 1).
+    "show_toc_level": 3,  # Right nav ("on this page"; default 2).
+    # Add genindex and py-modindex links below global toc in left nav.
+    # (And keep ad-placement hint for RTD preview builds.)
+    "primary_sidebar_end": ["indices.html", "sidebar-ethical-ads.html"],
+    "navbar_start": [],  # Skip the "logo".
+    "header_links_before_dropdown": 6,
+    "external_links": [
+        {"name": "Project homepage", "url": "https://www.djangoproject.com"},
+    ],
+    "footer_start": ["copyright", "last-updated"],
+    "footer_end": ["sphinx-version", "theme-version"],
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ["_theme"]
@@ -237,6 +250,7 @@ html_static_path = ["_static"]
 
 html_css_files = [
     "console-tabs.css",
+    "custom.css",
 ]
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
@@ -247,7 +261,12 @@ html_last_updated_fmt = "%b %d, %Y"
 # html_index = ''
 
 # Custom sidebar templates, maps document names to template names.
-# html_sidebars = {}
+html_sidebars = {
+    # Remove "Section navigation" sidebar on pages where it's empty.
+    "genindex": [],
+    "glossary": [],
+    "index": [],
+}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
