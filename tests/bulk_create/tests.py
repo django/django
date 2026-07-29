django/django

commit 7d9aab8da0a06787d649762a702e1a518d843a63

Refs #36260 -- Moved _is_pk_set checks into _prepare_for_bulk_create().
To avoid looping over objs twice.

django/db/models/query.py
@@ -33,7 +33,7 @@ from django.db.models.utils import (
     resolve_callables,
 )
 from django.utils import timezone
-from django.utils.functional import cached_property, partition
+from django.utils.functional import cached_property
 
 # The maximum number of results to fetch in a get() query.
 MAX_GET_RESULTS = 21
@@ -670,11 +670,20 @@ class QuerySet(AltersData):
     acreate.alters_data = True
 
     def _prepare_for_bulk_create(self, objs):
+        objs_with_pk, objs_without_pk = [], []
         for obj in objs:
-            if not obj._is_pk_set():
-                # Populate new PK values.
+            if isinstance(obj.pk, DatabaseDefault):
+                objs_without_pk.append(obj)
+            elif obj._is_pk_set():
+                objs_with_pk.append(obj)
+            else:
                 obj.pk = obj._meta.pk.get_pk_value_on_save(obj)
+                if obj._is_pk_set():
+                    objs_with_pk.append(obj)
+                else:
+                    objs_without_pk.append(obj)
             obj._prepare_related_fields_for_save(operation_name="bulk_create")
+        return objs_with_pk, objs_without_pk
 
     def _check_bulk_create_options(
         self, ignore_conflicts, update_conflicts, update_fields, unique_fields
@@ -787,12 +796,8 @@ class QuerySet(AltersData):
         self._for_write = True
         fields = [f for f in opts.concrete_fields if not f.generated]
         objs = list(objs)
-        self._prepare_for_bulk_create(objs)
+        objs_with_pk, objs_without_pk = self._prepare_for_bulk_create(objs)
         with transaction.atomic(using=self.db, savepoint=False):
-            objs_without_pk, objs_with_pk = partition(
-                lambda o: o._is_pk_set() and not isinstance(o.pk, DatabaseDefault),
-                objs,
-            )
             if objs_with_pk:
                 returned_columns = self._batched_insert(
                     objs_with_pk,

commit 4d2fe637018afe5c0ab141d6f9ab0b3fcc8084a3

Fixed #37234 -- Fixed bulk_create() for late-saved related primary keys.
Objects with a primary key supplied by a late-saved related instance
were partitioned before their related fields were prepared. This
caused an assertion failure on backends returning rows from bulk
inserts.

Regression in 7d9aab8da0a06787d649762a702e1a518d843a63.

django/db/models/query.py
@@ -747,6 +747,7 @@ class QuerySet(AltersData):
     def _prepare_for_bulk_create(self, objs):
         objs_with_pk, objs_without_pk = [], []
         for obj in objs:
+            obj._prepare_related_fields_for_save(operation_name="bulk_create")
             if isinstance(obj.pk, DatabaseDefault):
                 objs_without_pk.append(obj)
             elif obj._is_pk_set():
@@ -757,7 +758,6 @@ class QuerySet(AltersData):
                     objs_with_pk.append(obj)
                 else:
                     objs_without_pk.append(obj)
-            obj._prepare_related_fields_for_save(operation_name="bulk_create")
         return objs_with_pk, objs_without_pk
 
     def _check_bulk_create_options(

tests/bulk_create/tests.py
@@ -439,6 +439,13 @@ class BulkCreateTests(TestCase):
         child = NullableFields.objects.get(integer_field=88)
         self.assertEqual(child.auto_field, parent)
 
+    def test_pk_from_related_instance_saved_after_init(self):
+        country = Country(name="Syldavia", iso_two_letter="SW")
+        related = RelatedModel(country=country)
+        country.save()
+        RelatedModel.objects.bulk_create([related])
+        self.assertEqual(related.country_id, country.pk)
+
     def test_unsaved_parent(self):
         parent = NoFields()
         msg = (