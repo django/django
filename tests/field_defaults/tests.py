django/django

commit a2348c85fc6c20087935c74cd99340dd4ef2dcdc

Fixed #37139 -- Fixed inlines crash on parent models with db_default on primary key.

django/db/models/base.py
@@ -709,9 +709,17 @@ class Model(AltersData, metaclass=ModelBase):
 
     def _is_pk_set(self, meta=None):
         pk_val = self._get_pk_val(meta)
+
+        def _is_set(value):
+            return (
+                value is None
+                # Empty value when db_default is used.
+                or isinstance(value, DatabaseDefault)
+            )
+
         return not (
-            pk_val is None
-            or (isinstance(pk_val, tuple) and any(f is None for f in pk_val))
+            _is_set(pk_val)
+            or (isinstance(pk_val, tuple) and any(_is_set(f) for f in pk_val))
         )
 
     def get_deferred_fields(self):

tests/basic/tests.py
@@ -554,6 +554,7 @@ class ModelTest(TestCase):
         cases = [
             Article(),
             Article(id=None),
+            PrimaryKeyWithDbDefault(),
         ]
         for case in cases:
             with self.subTest(case=case):

tests/composite_pk/tests.py
@@ -17,7 +17,7 @@ from django.db.models import CompositePrimaryKey
 from django.forms import modelform_factory
 from django.test import TestCase
 
-from .models import Comment, Post, Tenant, TimeStamped, Token, User
+from .models import Comment, Post, PostDbDefault, Tenant, TimeStamped, Token, User
 
 
 class CommentForm(forms.ModelForm):
@@ -63,6 +63,12 @@ class CompositePKTests(TestCase):
         self.assertEqual(user.tenant_id, 9132)
         self.assertIsNone(user.id)
         self.assertIs(user._is_pk_set(), False)
+
+    def test_pk_not_set_db_default(self):
+        post = PostDbDefault(tenant=self.tenant)
+        self.assertEqual(post.tenant_id, self.tenant.pk)
+        self.assertIsNotNone(post.id)
+        self.assertIs(post._is_pk_set(), False)
 
     def test_hash(self):
         self.assertEqual(hash(User(pk=(1, 2))), hash((1, 2)))

tests/inline_formsets/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.functions import UUID4
 
 
 class School(models.Model):
@@ -19,6 +20,27 @@ class Child(models.Model):
         constraints = [
             models.UniqueConstraint("mother", "father", name="unique_parents"),
         ]
+
+
+class ParentUUIDPk(models.Model):
+    uuid = models.UUIDField(primary_key=True, db_default=UUID4(), editable=False)
+
+    class Meta:
+        required_db_features = {
+            "supports_uuid4_function",
+            "supports_expression_defaults",
+        }
+
+
+class ChildUUIDPk(models.Model):
+    parent = models.ForeignKey(ParentUUIDPk, models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        required_db_features = {
+            "supports_uuid4_function",
+            "supports_expression_defaults",
+        }
 
 
 class Poet(models.Model):

tests/inline_formsets/tests.py
@@ -1,7 +1,7 @@
 from django.forms.models import ModelForm, inlineformset_factory
 from django.test import TestCase, skipUnlessDBFeature
 
-from .models import Child, Parent, Poem, Poet, School
+from .models import Child, ChildUUIDPk, Parent, ParentUUIDPk, Poem, Poet, School
 
 
 class DeletionTests(TestCase):
@@ -112,6 +112,18 @@ class DeletionTests(TestCase):
             obj.father = father
             obj.save()
         self.assertEqual(school.child_set.count(), 1)
+
+    @skipUnlessDBFeature("supports_uuid4_function", "supports_expression_defaults")
+    def test_add_form_uuid_pk(self):
+        ChildFormSet = inlineformset_factory(ParentUUIDPk, ChildUUIDPk, fields=["name"])
+        data = {
+            "child_set-TOTAL_FORMS": "1",
+            "child_set-INITIAL_FORMS": "1",
+            "child_set-MAX_NUM_FORMS": "0",
+            "child_set-0-name": "child",
+        }
+        formset = ChildFormSet(data)
+        self.assertIs(formset.is_valid(), False)
 
 
 class InlineFormsetFactoryTest(TestCase):

tests/composite_pk/models/__init__.py
@@ -1,8 +1,9 @@
-from .tenant import Comment, Post, Tenant, TimeStamped, Token, User
+from .tenant import Comment, Post, PostDbDefault, Tenant, TimeStamped, Token, User
 
 __all__ = [
     "Comment",
     "Post",
+    "PostDbDefault",
     "Tenant",
     "TimeStamped",
     "Token",

tests/composite_pk/models/tenant.py
@@ -55,6 +55,12 @@ class Post(models.Model):
     id = models.UUIDField(default=uuid.uuid4)
 
 
+class PostDbDefault(models.Model):
+    pk = models.CompositePrimaryKey("tenant_id", "id")
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE, default=1)
+    id = models.IntegerField(db_default=1)
+
+
 class TimeStamped(models.Model):
     pk = models.CompositePrimaryKey("id", "created")
     id = models.SmallIntegerField(unique=True)

commit 9e833c28e5049b4a16eaa1a7252ae79fb2a435b6

Fixed #37238 -- Prevented fallback to python default for a pk with a db_default.
Regression in a2348c85fc6c20087935c74cd99340dd4ef2dcdc.

django/db/models/base.py
@@ -1094,7 +1094,9 @@ class Model(AltersData, metaclass=ModelBase):
                 if f.name in update_fields or f.attname in update_fields
             ]
 
-        if not self._is_pk_set(meta):
+        if not self._is_pk_set(meta) and not isinstance(
+            getattr(self, meta.pk.attname), DatabaseDefault
+        ):
             pk_val = meta.pk.get_pk_value_on_save(self)
             setattr(self, meta.pk.attname, pk_val)
         pk_set = self._is_pk_set(meta)

tests/field_defaults/models.py
@@ -68,3 +68,13 @@ class DBDefaultsFK(models.Model):
     language_code = models.ForeignKey(
         DBDefaultsPK, db_default="fr", on_delete=models.CASCADE
     )
+
+
+class DBDefaultsOneToOnePK(models.Model):
+    language_code = models.OneToOneField(
+        DBDefaultsPK,
+        primary_key=True,
+        default="tr",
+        db_default="kr",
+        on_delete=models.CASCADE,
+    )

tests/field_defaults/tests.py
@@ -23,6 +23,7 @@ from .models import (
     DBDefaults,
     DBDefaultsFK,
     DBDefaultsFunction,
+    DBDefaultsOneToOnePK,
     DBDefaultsPK,
 )
 
@@ -136,6 +137,23 @@ class DefaultTests(TestCase):
             parent2 = DBDefaultsPK.objects.get(pk="en")
         child2 = DBDefaultsFK.objects.create(language_code=parent2)
         self.assertEqual(child2.language_code, parent2)
+
+    def test_primary_key_assigned_db_default_expression(self):
+        # Create the target of the foreign key.
+        DBDefaultsPK.objects.create(language_code="kr")
+        # Create an object that happens to match the python default for
+        # DBDefaultsOneToOnePK.language_code.
+        DBDefaultsPK.objects.create(language_code="tr")
+
+        related = DBDefaultsPK()
+        obj = DBDefaultsOneToOnePK(language_code_id=related.pk)
+        related.save()
+        obj.save()
+
+        # The python default is not used.
+        # This should either be "en" or "kr" depending on which model's
+        # DatabaseDefault is used, but this behavior varies across databases.
+        self.assertNotEqual(obj.language_code_id, "tr")
 
     @skipUnlessDBFeature("supports_expression_defaults")
     def test_case_when_db_default_returning(self):