django/django

commit 0f581cd29d42d1b5ed1dafb67794c2f3ce6705c9

Fixed #35514 -- Implemented dictionary-based MAILERS.
See DEP 0018.

Added:
* MAILERS setting.
* django.core.mail.mailers dict-like EmailBackend factory.
* `using` argument to mail sending APIs.
* `sent_using` attribute to mail.outbox messages in locmem backend.
* MAILERS in startproject settings template, set to console backend.
* AdminLogHandler.using argument.
* BrokenLinkEmailsMiddleware.send_mail() method.

Updated:
* BaseEmailBackend to track the MAILERS alias used to construct it, and
  to report errors for unknown kwargs (OPTIONS).
* EmailBackend implementations to initialize from kwargs (OPTIONS) only
  when MAILERS is being used.
* smtp.EmailBackend to require `host` option and to default `port`
  option based on SSL/TLS options.
* SimpleTestCase setup to substitute the locmem backend for all defined
  MAILERS configurations.
* Django's tests that send mail to define MAILERS.

Deprecated:
* EMAIL_BACKEND and other backend-related EMAIL_* settings.
* mail.get_connection().
* The `connection`, `fail_silently`, `auth_user`, and `auth_password`
  arguments to mail functions.
* The EmailMessage.connection attribute.
* BaseEmailBackend support for `fail_silently`. Backends that support
  fail_silently (SMTP, console, file) now implement it directly.
* AdminEmailHandler.email_backend argument.

Removed undocumented features without deprecation:
* EmailMessage.get_connection() method. (send() now raises an error if a
  subclass has attempted to override it.)
* EmailMessage.send() no longer sets self.connection to the connection
  used for sending. (It still _uses_ a pre-existing self.connection.)
* AdminEmailHandler.connection() method. (Init now raises an error if a
  subclass has attempted to override it.)

Thanks to Natalia Bidart for shepherding DEP 0018 and for extensive
reviews and suggestions on the implementation.

Thanks to Jacob Rief for the initial implementation and multiple
iterations while refining the design.

django/middleware/common.py
@@ -3,7 +3,7 @@ from urllib.parse import urlsplit
 
 from django.conf import settings
 from django.core.exceptions import PermissionDenied
-from django.core.mail import mail_managers
+from django.core.mail import MailerDoesNotExist, mail_managers, mailers
 from django.http import HttpResponsePermanentRedirect
 from django.urls import is_valid_path
 from django.utils.deprecation import MiddlewareMixin
@@ -118,6 +118,9 @@ class CommonMiddleware(MiddlewareMixin):
 
 
 class BrokenLinkEmailsMiddleware(MiddlewareMixin):
+    # Set to override the mail.mailers alias used for sending the email.
+    using = None
+
     def process_response(self, request, response):
         """Send broken link emails for relevant 404 NOT FOUND responses."""
         if response.status_code == 404 and not settings.DEBUG:
@@ -128,7 +131,7 @@ class BrokenLinkEmailsMiddleware(MiddlewareMixin):
             if not self.is_ignorable_request(request, path, domain, referer):
                 ua = request.META.get("HTTP_USER_AGENT", "<none>")
                 ip = request.META.get("REMOTE_ADDR", "<none>")
-                mail_managers(
+                self.send_mail(
                     "Broken %slink on %s"
                     % (
                         (
@@ -140,9 +143,19 @@ class BrokenLinkEmailsMiddleware(MiddlewareMixin):
                     ),
                     "Referrer: %s\nRequested URL: %s\nUser agent: %s\n"
                     "IP address: %s\n" % (referer, path, ua, ip),
-                    fail_silently=True,
                 )
         return response
+
+    def send_mail(self, subject, message, *args, **kwargs):
+        # RemovedInDjango70Warning.
+        if not mailers._is_configured:
+            mail_managers(subject, message, *args, fail_silently=True, **kwargs)
+            return
+
+        try:
+            mail_managers(subject, message, *args, using=self.using, **kwargs)
+        except MailerDoesNotExist:
+            pass
 
     def is_internal_request(self, domain, referer):
         """

django/utils/log.py
@@ -1,11 +1,13 @@
 import logging
 import logging.config  # needed when logging_config doesn't start with logging.config
+import warnings
 from copy import copy
 
 from django.conf import settings
 from django.core import mail
-from django.core.mail import get_connection
+from django.core.exceptions import ImproperlyConfigured
 from django.core.management.color import color_style
+from django.utils.deprecation import RemovedInDjango70Warning, django_file_prefixes
 from django.utils.module_loading import import_string
 
 request_logger = logging.getLogger("django.request")
@@ -83,10 +85,36 @@ class AdminEmailHandler(logging.Handler):
     request data will be provided in the email report.
     """
 
-    def __init__(self, include_html=False, email_backend=None, reporter_class=None):
+    def __init__(
+        self, include_html=False, email_backend=None, reporter_class=None, using=None
+    ):
         super().__init__()
+
+        # RemovedInDjango70Warning: email_backend arg and connection error.
+        if email_backend:
+            if using:
+                raise ImproperlyConfigured(
+                    "The 'email_backend' argument is not compatible with 'using'."
+                )
+            if mail.mailers._is_configured:
+                raise ImproperlyConfigured(
+                    "The 'email_backend' argument is not valid when "
+                    "settings.MAILERS is defined."
+                )
+            warnings.warn(
+                "The 'email_backend' argument is deprecated. Use 'using' instead.",
+                RemovedInDjango70Warning,
+                skip_file_prefixes=django_file_prefixes(),
+            )
+        if hasattr(self, "connection"):
+            raise AttributeError(
+                "The undocumented AdminEmailHandler.connection() method is no longer "
+                "used."
+            )
+
         self.include_html = include_html
         self.email_backend = email_backend
+        self.using = using
         self.reporter_class = import_string(
             reporter_class or settings.DEFAULT_EXCEPTION_REPORTER
         )
@@ -135,12 +163,18 @@ class AdminEmailHandler(logging.Handler):
         self.send_mail(subject, message, html_message=html_message)
 
     def send_mail(self, subject, message, *args, **kwargs):
-        mail.mail_admins(
-            subject, message, *args, connection=self.connection(), **kwargs
-        )
+        # RemovedInDjango70Warning.
+        if not mail.mailers._is_configured:
+            connection = mail.get_connection(
+                backend=self.email_backend, fail_silently=True
+            )
+            mail.mail_admins(subject, message, *args, connection=connection, **kwargs)
+            return
 
-    def connection(self):
-        return get_connection(backend=self.email_backend, fail_silently=True)
+        try:
+            mail.mail_admins(subject, message, *args, using=self.using, **kwargs)
+        except mail.MailerDoesNotExist:
+            pass
 
     def format_subject(self, subject):
         """

tests/logging_tests/tests.py
@@ -4,17 +4,27 @@ from io import StringIO
 from unittest import TestCase, mock
 
 from admin_scripts.tests import AdminScriptTestCase
+from mail import (
+    ignore_no_default_mailer_warning,
+    override_deprecated_email_settings,
+)
 from mail.custombackend import FailingEmailBackend, OptionsCapturingBackend
 
 from django.conf import settings
 from django.core import mail
-from django.core.exceptions import DisallowedHost, PermissionDenied, SuspiciousOperation
+from django.core.exceptions import (
+    DisallowedHost,
+    ImproperlyConfigured,
+    PermissionDenied,
+    SuspiciousOperation,
+)
 from django.core.files.temp import NamedTemporaryFile
 from django.core.management import color
 from django.http import HttpResponse
 from django.http.multipartparser import MultiPartParserError
 from django.test import RequestFactory, SimpleTestCase, override_settings
 from django.test.utils import LoggingCaptureMixin
+from django.utils.deprecation import RemovedInDjango70Warning
 from django.utils.log import (
     DEFAULT_LOGGING,
     AdminEmailHandler,
@@ -275,6 +285,9 @@ class CallbackFilterTest(SimpleTestCase):
         self.assertEqual(collector, ["a record"])
 
 
+@override_settings(
+    MAILERS={"default": {"BACKEND": "django.core.mail.backends.locmem.EmailBackend"}}
+)
 class AdminEmailHandlerTest(SimpleTestCase):
     logger = logging.getLogger("django")
     request_factory = RequestFactory()
@@ -294,11 +307,13 @@ class AdminEmailHandlerTest(SimpleTestCase):
             record.request = self.request_factory.get(url_path, *args, **kwargs)
         return record
 
-    @override_settings(
+    # RemovedInDjango70Warning.
+    @override_deprecated_email_settings(
         ADMINS=["admin@example.com"],
         EMAIL_BACKEND="mail.custombackend.FailingEmailBackend",
     )
     def test_sends_using_fail_silently(self):
+        del settings.MAILERS
         self.addCleanup(FailingEmailBackend.reset)
         self.logger.error("All work and no play makes Jack a dull boy")
         self.assertIs(FailingEmailBackend.init_kwargs[0]["fail_silently"], True)
@@ -396,16 +411,80 @@ class AdminEmailHandlerTest(SimpleTestCase):
         self.assertNotIn("\r", mail.outbox[0].subject)
         self.assertEqual(mail.outbox[0].subject, expected_subject)
 
+    # RemovedInDjango70Warning.
     @override_settings(ADMINS=["admin@example.com"])
+    @ignore_no_default_mailer_warning()
     def test_uses_custom_email_backend(self):
+        del settings.MAILERS
         self.addCleanup(OptionsCapturingBackend.reset)
-        handler = AdminEmailHandler(
-            email_backend="mail.custombackend.OptionsCapturingBackend"
-        )
+        msg = "The 'email_backend' argument is deprecated. Use 'using' instead."
+        with self.assertWarnsMessage(RemovedInDjango70Warning, msg):
+            handler = AdminEmailHandler(
+                email_backend="mail.custombackend.OptionsCapturingBackend"
+            )
         handler.emit(self.make_log_record("/"))
         self.assertEqual(len(mail.outbox), 0)
         self.assertIs(OptionsCapturingBackend.init_kwargs[0]["fail_silently"], True)
         self.assertEqual(len(OptionsCapturingBackend.sent_messages), 1)
+
+    @override_settings(ADMINS=["admin@example.com"])
+    def test_sends_using_default_mailer(self):
+        handler = AdminEmailHandler()
+        handler.emit(self.make_log_record())
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].sent_using, "default")
+
+    @override_settings(
+        ADMINS=["admin@example.com"],
+        MAILERS={},
+    )
+    def test_no_error_when_email_not_configured(self):
+        handler = AdminEmailHandler()
+        handler.emit(self.make_log_record())
+        self.assertEqual(len(mail.outbox), 0)
+
+    @override_settings(
+        ADMINS=["admin@example.com"],
+        MAILERS={
+            "custom": {"BACKEND": "django.core.mail.backends.locmem.EmailBackend"}
+        },
+    )
+    def test_using_arg(self):
+        handler = AdminEmailHandler(using="custom")
+        handler.emit(self.make_log_record())
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].sent_using, "custom")
+
+    # RemovedInDjango70Warning.
+    def test_using_conflicts_with_email_backend(self):
+        msg = "The 'email_backend' argument is not compatible with 'using'."
+        with self.assertRaisesMessage(ImproperlyConfigured, msg):
+            AdminEmailHandler(
+                email_backend="logging_tests.logconfig.MyEmailBackend", using="custom"
+            )
+
+    # RemovedInDjango70Warning.
+    @override_settings(MAILERS={})
+    def test_email_backend_not_valid_when_mailers_defined(self):
+        msg = (
+            "The 'email_backend' argument is not valid when "
+            "settings.MAILERS is defined."
+        )
+        with self.assertRaisesMessage(ImproperlyConfigured, msg):
+            AdminEmailHandler(email_backend="logging_tests.logconfig.MyEmailBackend")
+
+    # RemovedInDjango70Warning.
+    def test_error_when_subclass_defines_undocumented_connection_method(self):
+
+        class CustomAdminEmailHandler(AdminEmailHandler):
+            def connection(self):
+                return mail.get_connection(some_important_option=True)
+
+        with self.assertRaisesMessage(
+            AttributeError,
+            "The undocumented AdminEmailHandler.connection() method is no longer used.",
+        ):
+            CustomAdminEmailHandler()
 
     @override_settings(
         ADMINS=["admin@example.com"],
@@ -616,6 +695,9 @@ class SecurityLoggerTest(LoggingAssertionMixin, SimpleTestCase):
     @override_settings(
         ADMINS=["admin@example.com"],
         DEBUG=False,
+        MAILERS={
+            "default": {"BACKEND": "django.core.mail.backends.locmem.EmailBackend"}
+        },
     )
     def test_suspicious_email_admins(self):
         self.client.get("/suspicious/")

tests/middleware/tests.py
@@ -7,6 +7,7 @@ from io import BytesIO
 from unittest import mock
 from urllib.parse import quote
 
+from mail import override_deprecated_email_settings
 from mail.custombackend import FailingEmailBackend
 
 from django.conf import settings
@@ -406,6 +407,7 @@ class CommonMiddlewareTest(SimpleTestCase):
 @override_settings(
     IGNORABLE_404_URLS=[re.compile(r"foo")],
     MANAGERS=["manager@example.com"],
+    MAILERS={"default": {"BACKEND": "django.core.mail.backends.locmem.EmailBackend"}},
 )
 class BrokenLinkEmailsMiddlewareTest(SimpleTestCase):
     rf = RequestFactory()
@@ -501,12 +503,47 @@ class BrokenLinkEmailsMiddlewareTest(SimpleTestCase):
         BrokenLinkEmailsMiddleware(self.get_response)(self.req)
         self.assertEqual(len(mail.outbox), 1)
 
-    @override_settings(EMAIL_BACKEND="mail.custombackend.FailingEmailBackend")
+    # RemovedInDjango70Warning.
+    @override_deprecated_email_settings(
+        EMAIL_BACKEND="mail.custombackend.FailingEmailBackend"
+    )
     def test_sends_using_fail_silently(self):
+        del settings.MAILERS
         self.addCleanup(FailingEmailBackend.reset)
         self.req.META["HTTP_REFERER"] = "/another/url/"
         BrokenLinkEmailsMiddleware(self.get_response)(self.req)
         self.assertIs(FailingEmailBackend.init_kwargs[0]["fail_silently"], True)
+
+    def test_sends_using_default_mailer(self):
+        self.req.META["HTTP_REFERER"] = "/another/url/"
+        BrokenLinkEmailsMiddleware(self.get_response)(self.req)
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].sent_using, "default")
+
+    @override_settings(MAILERS={})
+    def test_no_error_when_email_not_configured(self):
+        self.req.META["HTTP_REFERER"] = "/another/url/"
+        BrokenLinkEmailsMiddleware(self.get_response)(self.req)
+        self.assertEqual(len(mail.outbox), 0)
+
+    @override_settings(
+        MAILERS={
+            "custom": {"BACKEND": "django.core.mail.backends.locmem.EmailBackend"}
+        },
+    )
+    def test_custom_send_mail(self):
+        class SubclassedMiddleware(BrokenLinkEmailsMiddleware):
+            using = "custom"
+
+            def send_mail(self, subject, message, *args, **kwargs):
+                super().send_mail("custom subject", "custom message", *args, **kwargs)
+
+        self.req.META["HTTP_REFERER"] = "/another/url/"
+        SubclassedMiddleware(self.get_response)(self.req)
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].sent_using, "custom")
+        self.assertEqual(mail.outbox[0].subject, "[Django] custom subject")
+        self.assertEqual(mail.outbox[0].body, "custom message")
 
 
 @override_settings(ROOT_URLCONF="middleware.cond_get_urls")

tests/admin_views/tests.py
@@ -305,6 +305,9 @@ class AdminViewBasicTestCase(TestCase):
         )
 
 
+@override_settings(
+    MAILERS={"default": {"BACKEND": "django.core.mail.backends.locmem.EmailBackend"}}
+)
 class AdminViewBasicTest(AdminViewBasicTestCase):
     def test_trailing_slash_required(self):
         """
@@ -2346,6 +2349,7 @@ def get_perm(Model, codename):
             },
         }
     ],
+    MAILERS={"default": {"BACKEND": "django.core.mail.backends.locmem.EmailBackend"}},
 )
 class AdminViewPermissionsTest(TestCase):
     """Tests for Admin Views Permissions."""

tests/test_client/tests.py
@@ -61,7 +61,10 @@ def async_middleware_urlconf(get_response):
     return middleware
 
 
-@override_settings(ROOT_URLCONF="test_client.urls")
+@override_settings(
+    ROOT_URLCONF="test_client.urls",
+    MAILERS={"default": {"BACKEND": "django.core.mail.backends.locmem.EmailBackend"}},
+)
 class ClientTest(TestCase):
     @classmethod
     def setUpTestData(cls):

tests/test_utils/tests.py
@@ -1,3 +1,4 @@
+import copy
 import os
 import sys
 import threading
@@ -37,6 +38,7 @@ from django.test import (
     SimpleTestCase,
     TestCase,
     TransactionTestCase,
+    ignore_warnings,
     skipIfDBFeature,
     skipUnlessDBFeature,
 )
@@ -51,6 +53,7 @@ from django.test.utils import (
     teardown_test_environment,
 )
 from django.urls import NoReverseMatch, path, reverse, reverse_lazy
+from django.utils.deprecation import RemovedInDjango70Warning
 from django.utils.html import VOID_ELEMENTS
 
 from .models import Car, Person, PossessedCar
@@ -1841,6 +1844,8 @@ class SetupTestEnvironmentTests(SimpleTestCase):
                     setup_test_environment()
                     self.assertEqual(settings.ALLOWED_HOSTS, ["*", "testserver"])
 
+    # RemovedInDjango70Warning.
+    @ignore_warnings(category=RemovedInDjango70Warning)
     def test_email_backend_override(self):
         with (
             self.mock_test_state(),
@@ -1853,11 +1858,46 @@ class SetupTestEnvironmentTests(SimpleTestCase):
                 settings.EMAIL_BACKEND,
                 "django.core.mail.backends.locmem.EmailBackend",
             )
+            self.assertFalse(hasattr(settings, "MAILERS"))
             teardown_test_environment()
             self.assertEqual(
                 settings.EMAIL_BACKEND,
                 "django.core.mail.backends.console.EmailBackend",
             )
+            self.assertFalse(hasattr(settings, "MAILERS"))
+
+    def test_mailers_override(self):
+        expected_value = {
+            "default": {
+                "OPTIONS": {"host": "localhost"},
+            },
+            "custom": {
+                "BACKEND": "path.to.custom.EmailBackend",
+                "OPTIONS": {"custom": "option"},
+            },
+        }
+        settings_value = copy.deepcopy(expected_value)
+        with self.mock_test_state(), self.settings(MAILERS=settings_value):
+            setup_test_environment()
+            self.assertEqual(
+                settings.MAILERS,
+                {
+                    "default": {
+                        "BACKEND": "django.core.mail.backends.locmem.EmailBackend"
+                    },
+                    "custom": {
+                        "BACKEND": "django.core.mail.backends.locmem.EmailBackend"
+                    },
+                },
+            )
+            # setup_test_environment() shouldn't mutate original setting value.
+            self.assertEqual(settings_value, expected_value)
+            # RemovedInDjango70Warning: Remove both EMAIL_BACKEND assertions.
+            self.assertFalse(hasattr(settings, "EMAIL_BACKEND"))
+
+            teardown_test_environment()
+            self.assertEqual(settings.MAILERS, expected_value)
+            self.assertFalse(hasattr(settings, "EMAIL_BACKEND"))
 
 
 class OverrideSettingsTests(SimpleTestCase):

commit 809826ce73f6aa5cba78388cd2fb441d12adb174

Fixed #37261 -- Prevented error reporting emails from raising under MAILERS.
When `settings.MAILERS` is configured, `AdminEmailHandler.send_mail()` and
`BrokenLinkEmailsMiddleware.send_mail()` caught only `MailerDoesNotExist`,
so any transport error raised while sending (SMTP server down, DNS
failure, authentication error, etc.) propagated out of
`AdminEmailHandler.emit()` into the caller of `logger.error()` -- typically
Django's own error handling of a 500 response -- and out of
`BrokenLinkEmailsMiddleware.process_response()` on referred 404s. The
legacy `EMAIL_BACKEND` path still passed `fail_silently=True`, which
swallowed all such errors.

The fix is to catch `Exception` instead, per the 'Migrating email to mailers'
documentation.

Regression in 0f581cd29d42d1b5ed1dafb67794c2f3ce6705c9.

django/middleware/common.py
@@ -3,7 +3,7 @@ from urllib.parse import urlsplit
 
 from django.conf import settings
 from django.core.exceptions import PermissionDenied
-from django.core.mail import MailerDoesNotExist, mail_managers, mailers
+from django.core.mail import mail_managers, mailers
 from django.http import HttpResponsePermanentRedirect
 from django.middleware import MiddlewareMixin
 from django.urls import is_valid_path
@@ -154,7 +154,9 @@ class BrokenLinkEmailsMiddleware(MiddlewareMixin):
 
         try:
             mail_managers(subject, message, *args, using=self.using, **kwargs)
-        except MailerDoesNotExist:
+        except Exception:
+            # Ignore all errors, including an unavailable mailer and any
+            # failure sending the email, to avoid failing the response.
             pass
 
     def is_internal_request(self, domain, referer):

django/utils/log.py
@@ -174,7 +174,10 @@ class AdminEmailHandler(logging.Handler):
 
         try:
             mail.mail_admins(subject, message, *args, using=self.using, **kwargs)
-        except mail.MailerDoesNotExist:
+        except Exception:
+            # Ignore all errors, including an unavailable mailer and any
+            # failure sending the email, to avoid a cascading failure in the
+            # error handler.
             pass
 
     def format_subject(self, subject):

tests/logging_tests/tests.py
@@ -445,6 +445,21 @@ class AdminEmailHandlerTest(SimpleTestCase):
 
     @override_settings(
         ADMINS=["admin@example.com"],
+        MAILERS={"default": {"BACKEND": "mail.custombackend.FailingEmailBackend"}},
+    )
+    def test_no_error_when_sending_fails(self):
+        """
+        An error sending the email is ignored to avoid a cascading failure in
+        the error handler (matching the fail_silently=True behavior of the
+        deprecated EMAIL_BACKEND setting).
+        """
+        self.addCleanup(FailingEmailBackend.reset)
+        # Does not raise the error from FailingEmailBackend.send_messages().
+        self.logger.error("All work and no play makes Jack a dull boy")
+        self.assertEqual(len(mail.outbox), 0)
+
+    @override_settings(
+        ADMINS=["admin@example.com"],
         MAILERS={
             "custom": {"BACKEND": "django.core.mail.backends.locmem.EmailBackend"}
         },

tests/middleware/tests.py
@@ -527,6 +527,22 @@ class BrokenLinkEmailsMiddlewareTest(SimpleTestCase):
         self.assertEqual(len(mail.outbox), 0)
 
     @override_settings(
+        MAILERS={"default": {"BACKEND": "mail.custombackend.FailingEmailBackend"}}
+    )
+    def test_no_error_when_sending_fails(self):
+        """
+        An error sending the email is ignored and the response is returned
+        (matching the fail_silently=True behavior of the deprecated
+        EMAIL_BACKEND setting).
+        """
+        self.addCleanup(FailingEmailBackend.reset)
+        self.req.META["HTTP_REFERER"] = "/another/url/"
+        # Does not raise the error from FailingEmailBackend.send_messages().
+        response = BrokenLinkEmailsMiddleware(self.get_response)(self.req)
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(len(mail.outbox), 0)
+
+    @override_settings(
         MAILERS={
             "custom": {"BACKEND": "django.core.mail.backends.locmem.EmailBackend"}
         },