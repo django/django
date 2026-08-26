78a256de3fbe86afb7f3882f18b72297a6176efd
Norbert Kwizera <norkans7@gmail.com>
2026-08-26 17:52:05 +0200

    Fixed #37300 -- Restored db hints on custom prefetch querysets of FK/O2O relations.
    Regression in 821619aa876158e8d6dcedebd0dbc2e0e0b41295 which moved the
    instance hint into get_queryset() on ForwardManyToOneDescriptor and
    ReverseOneToOneDescriptor, leaving the custom queryset branch of their
    get_prefetch_querysets() without hints. Custom Prefetch() querysets were
    then routed to the default database instead of the database of the
    instances being prefetched.

--- tests/prefetch_related/tests.py
+++ tests/prefetch_related/tests.py
@@ -1696,6 +1696,56 @@ class MultiDbTests(TestCase):
 
         self.assertEqual(ages, "50, 49")
 
+    def test_using_is_honored_custom_qs_fkey(self):
+        B = Book.objects.using("other")
+        A = Author.objects.using("other")
+        book1 = B.create(title="Poems")
+        book2 = B.create(title="Sense and Sensibility")
+
+        A.create(name="Charlotte Bronte", first_book=book1)
+        A.create(name="Jane Austen", first_book=book2)
+
+        # Implicit hinting
+        with self.assertNumQueries(2, using="other"):
+            prefetch = Prefetch("first_book", queryset=Book.objects.all())
+            books = ", ".join(a.first_book.title for a in A.prefetch_related(prefetch))
+        self.assertEqual("Poems, Sense and Sensibility", books)
+
+        # Explicit using on the same db.
+        with self.assertNumQueries(2, using="other"):
+            prefetch = Prefetch("first_book", queryset=Book.objects.using("other"))
+            books = ", ".join(a.first_book.title for a in A.prefetch_related(prefetch))
+        self.assertEqual("Poems, Sense and Sensibility", books)
+
+        # Explicit using on a different db.
+        with (
+            self.assertNumQueries(1, using="default"),
+            self.assertNumQueries(1, using="other"),
+        ):
+            prefetch = Prefetch("first_book", queryset=Book.objects.using("default"))
+            list(A.prefetch_related(prefetch))
+
+    def test_using_is_honored_custom_qs_o2o(self):
+        B = BookWithYear.objects.using("other")
+        A = AuthorWithAge.objects.using("other")
+        book1 = B.create(title="Poems", published_year=2010)
+        A.create(name="Jane", first_book=book1, age=50)
+        A.create(name="Tom", first_book=book1, age=49)
+
+        # parent link
+        with self.assertNumQueries(2, using="other"):
+            prefetch = Prefetch("author", queryset=Author.objects.all())
+            authors = ", ".join(a.author.name for a in A.prefetch_related(prefetch))
+        self.assertEqual(authors, "Jane, Tom")
+
+        # child link
+        with self.assertNumQueries(2, using="other"):
+            prefetch = Prefetch("authorwithage", queryset=AuthorWithAge.objects.all())
+            ages = ", ".join(
+                str(a.authorwithage.age) for a in A.prefetch_related(prefetch)
+            )
+        self.assertEqual(ages, "50, 49")
+
     def test_using_is_honored_custom_qs(self):
         B = Book.objects.using("other")
         A = Author.objects.using("other")
