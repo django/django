django/django

commit 821619aa8771ef211c4c4922001efdf914201ca3

Refs #28586 -- Simplified related descriptor get_queryset() methods.
Modify these methods to accept an instance parameter which is clearer and
allows us to set the instance hint earlier.

django/db/models/fields/related_descriptors.py
@@ -166,8 +166,10 @@ class ForwardManyToOneDescriptor:
     def is_cached(self, instance):
         return self.field.is_cached(instance)
 
-    def get_queryset(self, **hints):
-        return self.field.remote_field.model._base_manager.db_manager(hints=hints).all()
+    def get_queryset(self, *, instance):
+        return self.field.remote_field.model._base_manager.db_manager(
+            hints={"instance": instance}
+        ).all()
 
     def get_prefetch_querysets(self, instances, querysets=None):
         if querysets and len(querysets) != 1:
@@ -175,8 +177,9 @@ class ForwardManyToOneDescriptor:
                 "querysets argument of get_prefetch_querysets() should have a length "
                 "of 1."
             )
-        queryset = querysets[0] if querysets else self.get_queryset()
-        queryset._add_hints(instance=instances[0])
+        queryset = (
+            querysets[0] if querysets else self.get_queryset(instance=instances[0])
+        )
 
         rel_obj_attr = self.field.get_foreign_related_value
         instance_attr = self.field.get_local_related_value
@@ -456,8 +459,10 @@ class ReverseOneToOneDescriptor:
     def is_cached(self, instance):
         return self.related.is_cached(instance)
 
-    def get_queryset(self, **hints):
-        return self.related.related_model._base_manager.db_manager(hints=hints).all()
+    def get_queryset(self, *, instance):
+        return self.related.related_model._base_manager.db_manager(
+            hints={"instance": instance}
+        ).all()
 
     def get_prefetch_querysets(self, instances, querysets=None):
         if querysets and len(querysets) != 1:
@@ -465,8 +470,9 @@ class ReverseOneToOneDescriptor:
                 "querysets argument of get_prefetch_querysets() should have a length "
                 "of 1."
             )
-        queryset = querysets[0] if querysets else self.get_queryset()
-        queryset._add_hints(instance=instances[0])
+        queryset = (
+            querysets[0] if querysets else self.get_queryset(instance=instances[0])
+        )
 
         rel_obj_attr = self.related.field.get_local_related_value
         instance_attr = self.related.field.get_foreign_related_value

commit 5674cf0500c2559cd746091098923ad40de306a5

Fixed #37300 -- Preserved parent instance hints on custom Prefetch querysets.
Regression in 821619aa8771ef211c4c4922001efdf914201ca3.

Thanks Norbert Kwizera for the report.

django/db/models/fields/related_descriptors.py
@@ -180,6 +180,7 @@ class ForwardManyToOneDescriptor:
                     "length of 1."
                 )
             queryset = querysets[0]
+            queryset._add_hints(instance=instances[0])
         else:
             _cloning_disabled = True
             queryset = self.get_queryset(instance=instances[0])._disable_cloning()
@@ -480,6 +481,7 @@ class ReverseOneToOneDescriptor:
                     "length of 1."
                 )
             queryset = querysets[0]
+            queryset._add_hints(instance=instances[0])
         else:
             _cloning_disabled = True
             queryset = self.get_queryset(instance=instances[0])._disable_cloning()

tests/prefetch_related/tests.py
@@ -1747,6 +1747,38 @@ class MultiDbTests(TestCase):
             )
         self.assertEqual(books, "Poems ()\n" "Sense and Sensibility ()\n")
 
+    def test_forward_fk_prefetch_custom_queryset_uses_parent_database(self):
+        book1 = Book.objects.using("default").create(title="Dive into Python")
+        Author.objects.using("default").create(name="Mark", first_book=book1)
+        book2 = Book.objects.using("other").create(title="Dive into Rust")
+        Author.objects.using("other").create(name="Mark", first_book=book2)
+
+        author = (
+            Author.objects.using("other")
+            .prefetch_related(Prefetch("first_book", queryset=Book.objects.all()))
+            .get(name="Mark")
+        )
+
+        self.assertEqual(author._state.db, "other")
+        self.assertEqual(author.first_book._state.db, "other")
+        self.assertEqual(author.first_book.title, "Dive into Rust")
+
+    def test_reverse_o2o_prefetch_custom_queryset_uses_parent_database(self):
+        BookWithYear.objects.using("default").create(title="Poems", published_year=2000)
+        BookWithYear.objects.using("other").create(title="Poems", published_year=2001)
+
+        book = (
+            Book.objects.using("other")
+            .prefetch_related(
+                Prefetch("bookwithyear", queryset=BookWithYear.objects.all())
+            )
+            .get(title="Poems")
+        )
+
+        self.assertEqual(book._state.db, "other")
+        self.assertEqual(book.bookwithyear._state.db, "other")
+        self.assertEqual(book.bookwithyear.published_year, 2001)
+
 
 class Ticket19607Tests(TestCase):
     @classmethod