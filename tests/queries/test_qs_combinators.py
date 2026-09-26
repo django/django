flbr2024/django-fork

Deprecated QuerySet.extra() except for the tables option.

tests/queries/test_qs_combinators.py
@@ -3,7 +3,8 @@ import operator
 from django.db import DatabaseError, NotSupportedError, connection
 from django.db.models import Exists, F, IntegerField, OuterRef, Subquery, Value
 from django.test import TestCase, skipIfDBFeature, skipUnlessDBFeature
-from django.test.utils import CaptureQueriesContext
+from django.test.utils import CaptureQueriesContext, ignore_warnings
+from django.utils.deprecation import RemovedInDjango60Warning
 
 from .models import Author, Celebrity, ExtraInfo, Number, ReservedName
 
@@ -248,6 +249,8 @@ class QuerySetSetOperationTests(TestCase):
         )
         self.assertCountEqual(qs1.union(qs2), [(1, 0), (1, 2)])
 
+    # Entire test can be removed once deprecation period ends.
+    @ignore_warnings(category=RemovedInDjango60Warning)
     def test_union_with_extra_and_values_list(self):
         qs1 = (
             Number.objects.filter(num=1)
@@ -359,6 +362,8 @@ class QuerySetSetOperationTests(TestCase):
             [reserved_name.pk],
         )
 
+    # Entire test can be removed once deprecation period ends.
+    @ignore_warnings(category=RemovedInDjango60Warning)
     def test_union_multiple_models_with_values_list_and_order_by_extra_select(self):
         reserved_name = ReservedName.objects.create(name="rn1", order=0)
         qs1 = Celebrity.objects.extra(select={"extra_name": "name"})

django/django

tests/queries/test_qs_combinators.py
@@ -14,7 +14,8 @@ from django.db.models import (
 )
 from django.db.models.functions import Cast, Mod
 from django.test import TestCase, skipIfDBFeature, skipUnlessDBFeature
-from django.test.utils import CaptureQueriesContext
+from django.test.utils import CaptureQueriesContext, ignore_warnings
+from django.utils.deprecation import RemovedInDjango2029Warning
 
 from .models import (
     Annotation,
@@ -319,6 +320,8 @@ class QuerySetSetOperationTests(TestCase):
         )
         self.assertCountEqual(qs1.union(qs2), [(1, 0), (0, 2)])
 
+    # Entire test can be removed once deprecation period ends.
+    @ignore_warnings(category=RemovedInDjango2029Warning)
     def test_union_with_extra_and_values_list(self):
         qs1 = (
             Number.objects.filter(num=1)
@@ -455,6 +458,8 @@ class QuerySetSetOperationTests(TestCase):
             [reserved_name.pk],
         )
 
+    # Entire test can be removed once deprecation period ends.
+    @ignore_warnings(category=RemovedInDjango2029Warning)
     def test_union_multiple_models_with_values_list_and_order_by_extra_select(self):
         reserved_name = ReservedName.objects.create(name="rn1", order=0)
         qs1 = Celebrity.objects.extra(select={"extra_name": "name"})